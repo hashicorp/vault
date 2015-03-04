0099f62efa8d79df72d0f2fe3f88e398a67026a9
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2015-03-03 23:03:24 -0800

    basic main boilerplate stuff

---
+++ commands.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"os"
+
+	"github.com/hashicorp/vault/command"
+	"github.com/mitchellh/cli"
+)
+
+// Commands is the mapping of all the available Consul commands.
+var Commands map[string]cli.CommandFactory
+
+func init() {
+	ui := &cli.BasicUi{Writer: os.Stdout}
+
+	Commands = map[string]cli.CommandFactory{
+		"version": func() (cli.Command, error) {
+			ver := Version
+			rel := VersionPrerelease
+			if GitDescribe != "" {
+				ver = GitDescribe
+			}
+			if GitDescribe == "" && rel == "" {
+				rel = "dev"
+			}
+
+			return &command.VersionCommand{
+				Revision:          GitCommit,
+				Version:           ver,
+				VersionPrerelease: rel,
+				Ui:                ui,
+			}, nil
+		},
+	}
+}
---
+++ main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/mitchellh/cli"
+)
+
+func main() {
+	os.Exit(realMain())
+}
+
+func realMain() int {
+	// Get the command line args. We shortcut "--version" and "-v" to
+	// just show the version.
+	args := os.Args[1:]
+	for _, arg := range args {
+		if arg == "-v" || arg == "--version" {
+			newArgs := make([]string, len(args)+1)
+			newArgs[0] = "version"
+			copy(newArgs[1:], args)
+			args = newArgs
+			break
+		}
+	}
+
+	cli := &cli.CLI{
+		Args:     args,
+		Commands: Commands,
+		HelpFunc: cli.BasicHelpFunc("vault"),
+	}
+
+	exitCode, err := cli.Run()
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error executing CLI: %s\n", err.Error())
+		return 1
+	}
+
+	return exitCode
+}
