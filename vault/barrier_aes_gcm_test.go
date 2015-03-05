f1c9e929b3e39def04945fd484f5bf8213185e17
Armon Dadgar <armon.dadgar@gmail.com>
2015-03-05 14:03:00 -0800

    vault: Basic test for barrier confidentiality and integrity

--- vault/barrier_aes_gcm_test.go
+++ vault/barrier_aes_gcm_test.go
@@ -1,6 +1,7 @@
 package vault
 
 import (
+	"bytes"
 	"testing"
 
 	"github.com/hashicorp/vault/physical"
@@ -15,10 +16,74 @@ func TestAESGCMBarrier_Basic(t *testing.T) {
 	testBarrier(t, b)
 }
 
+// Verify data sent through is encrypted
 func TestAESGCMBarrier_Confidential(t *testing.T) {
-	// TODO: Verify data sent through is encrypted
+	inm := physical.NewInmem()
+	b, err := NewAESGCMBarrier(inm)
+	if err != nil {
+		t.Fatalf("err: %v", err)
+	}
+
+	// Initialize and unseal
+	key, _ := b.GenerateKey()
+	b.Initialize(key)
+	b.Unseal(key)
+
+	// Put a logical entry
+	entry := &Entry{Key: "test", Value: []byte("test")}
+	err = b.Put(entry)
+	if err != nil {
+		t.Fatalf("err: %v", err)
+	}
+
+	// Check the physcial entry
+	pe, err := inm.Get("test")
+	if err != nil {
+		t.Fatalf("err: %v", err)
+	}
+	if pe == nil {
+		t.Fatalf("missing physical entry")
+	}
+
+	if pe.Key != "test" {
+		t.Fatalf("bad: %#v", pe)
+	}
+	if bytes.Equal(pe.Value, entry.Value) {
+		t.Fatalf("bad: %#v", pe)
+	}
 }
 
+// Verify data sent through is cannot be tampered
 func TestAESGCMBarrier_Integrity(t *testing.T) {
-	// TODO: Verify data sent through is cannot be tampered
+	inm := physical.NewInmem()
+	b, err := NewAESGCMBarrier(inm)
+	if err != nil {
+		t.Fatalf("err: %v", err)
+	}
+
+	// Initialize and unseal
+	key, _ := b.GenerateKey()
+	b.Initialize(key)
+	b.Unseal(key)
+
+	// Put a logical entry
+	entry := &Entry{Key: "test", Value: []byte("test")}
+	err = b.Put(entry)
+	if err != nil {
+		t.Fatalf("err: %v", err)
+	}
+
+	// Change a byte in the underlying physical entry
+	pe, _ := inm.Get("test")
+	pe.Value[15]++
+	err = inm.Put(pe)
+	if err != nil {
+		t.Fatalf("err: %v", err)
+	}
+
+	// Read from the barrier
+	_, err = b.Get("test")
+	if err == nil {
+		t.Fatalf("should fail!")
+	}
 }
