886072849e2c8f57ecd2e89fc72386e9adcc8a22
Armon Dadgar <armon.dadgar@gmail.com>
2015-03-11 11:57:05 -0700

    vault: Testing initialization

--- vault/core_test.go
+++ vault/core_test.go
@@ -1,6 +1,7 @@
 package vault
 
 import (
+	"reflect"
 	"testing"
 
 	"github.com/hashicorp/vault/physical"
@@ -23,6 +24,15 @@ func TestCore_Init(t *testing.T) {
 		t.Fatalf("should not be init")
 	}
 
+	// Check the seal configuration
+	outConf, err := c.SealConfig()
+	if err != nil {
+		t.Fatalf("err: %v", err)
+	}
+	if outConf != nil {
+		t.Fatalf("bad: %v", outConf)
+	}
+
 	sealConf := &SealConfig{
 		SecretShares:    1,
 		SecretThreshold: 1,
@@ -36,6 +46,11 @@ func TestCore_Init(t *testing.T) {
 		t.Fatalf("Bad: %v", res)
 	}
 
+	_, err = c.Initialize(sealConf)
+	if err != ErrAlreadyInit {
+		t.Fatalf("err: %v", err)
+	}
+
 	init, err = c.Initialized()
 	if err != nil {
 		t.Fatalf("err: %v", err)
@@ -45,9 +60,23 @@ func TestCore_Init(t *testing.T) {
 		t.Fatalf("should be init")
 	}
 
+	// Check the seal configuration
+	outConf, err = c.SealConfig()
+	if err != nil {
+		t.Fatalf("err: %v", err)
+	}
+	if !reflect.DeepEqual(outConf, sealConf) {
+		t.Fatalf("bad: %v expect: %v", outConf, sealConf)
+	}
+
 	// New Core, same backend
 	c2, err := NewCore(conf)
 	if err != nil {
+		t.Fatalf("err: %v", err)
+	}
+
+	_, err = c2.Initialize(sealConf)
+	if err != ErrAlreadyInit {
 		t.Fatalf("err: %v", err)
 	}
 
@@ -58,5 +87,45 @@ func TestCore_Init(t *testing.T) {
 
 	if !init {
 		t.Fatalf("should be init")
+	}
+
+	// Check the seal configuration
+	outConf, err = c2.SealConfig()
+	if err != nil {
+		t.Fatalf("err: %v", err)
+	}
+	if !reflect.DeepEqual(outConf, sealConf) {
+		t.Fatalf("bad: %v expect: %v", outConf, sealConf)
+	}
+}
+
+func TestCore_Init_MultiShare(t *testing.T) {
+	inm := physical.NewInmem()
+	conf := &CoreConfig{physical: inm}
+	c, err := NewCore(conf)
+	if err != nil {
+		t.Fatalf("err: %v", err)
+	}
+
+	sealConf := &SealConfig{
+		SecretShares:    5,
+		SecretThreshold: 3,
+	}
+	res, err := c.Initialize(sealConf)
+	if err != nil {
+		t.Fatalf("err: %v", err)
+	}
+
+	if len(res.SecretShares) != 5 {
+		t.Fatalf("Bad: %v", res)
+	}
+
+	// Check the seal configuration
+	outConf, err := c.SealConfig()
+	if err != nil {
+		t.Fatalf("err: %v", err)
+	}
+	if !reflect.DeepEqual(outConf, sealConf) {
+		t.Fatalf("bad: %v expect: %v", outConf, sealConf)
 	}
 }
