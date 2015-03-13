cf259c3f22b8024440e8252ff9fa9a438d3f3679
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2015-03-13 09:57:28 -0700

    command/sever: copy the TCP keep alive listener

--- command/server/listener_tcp.go
+++ command/server/listener_tcp.go
@@ -3,6 +3,7 @@ package server
 import (
 	"fmt"
 	"net"
+	"time"
 )
 
 func tcpListenerFactory(config map[string]string) (net.Listener, error) {
@@ -16,5 +17,26 @@ func tcpListenerFactory(config map[string]string) (net.Listener, error) {
 		return nil, err
 	}
 
+	ln = tcpKeepAliveListener{ln.(*net.TCPListener)}
 	return listenerWrapTLS(ln, config)
+}
+
+// tcpKeepAliveListener sets TCP keep-alive timeouts on accepted
+// connections. It's used by ListenAndServe and ListenAndServeTLS so
+// dead TCP connections (e.g. closing laptop mid-download) eventually
+// go away.
+//
+// This is copied directly from the Go source code.
+type tcpKeepAliveListener struct {
+	*net.TCPListener
+}
+
+func (ln tcpKeepAliveListener) Accept() (c net.Conn, err error) {
+	tc, err := ln.AcceptTCP()
+	if err != nil {
+		return
+	}
+	tc.SetKeepAlive(true)
+	tc.SetKeepAlivePeriod(3 * time.Minute)
+	return tc, nil
 }
