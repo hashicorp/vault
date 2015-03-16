e7f7f7a221fa85a0b443964d1c19302fe3bc2e7a
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2015-03-15 17:07:54 -0700

    vault: passthrough backend uses logical/framework

--- vault/logical_passthrough.go
+++ vault/logical_passthrough.go
@@ -3,14 +3,39 @@ package vault
 import (
 	"encoding/json"
 	"fmt"
+	"strings"
 	"time"
 
 	"github.com/hashicorp/vault/logical"
+	"github.com/hashicorp/vault/logical/framework"
 )
 
 // logical.Factory
 func PassthroughBackendFactory(map[string]string) (logical.Backend, error) {
-	return new(PassthroughBackend), nil
+	var b PassthroughBackend
+	return &framework.Backend{
+		Paths: []*framework.Path{
+			&framework.Path{
+				Pattern: ".*",
+				Fields: map[string]*framework.FieldSchema{
+					"lease": &framework.FieldSchema{
+						Type:        framework.TypeString,
+						Description: "Lease time for this key when read. Ex: 1h",
+					},
+				},
+
+				Callbacks: map[logical.Operation]framework.OperationFunc{
+					logical.ReadOperation:   b.handleRead,
+					logical.WriteOperation:  b.handleWrite,
+					logical.DeleteOperation: b.handleDelete,
+					logical.ListOperation:   b.handleList,
+				},
+
+				HelpSynopsis:    strings.TrimSpace(passthroughHelpSynopsis),
+				HelpDescription: strings.TrimSpace(passthroughHelpDescription),
+			},
+		},
+	}, nil
 }
 
 // PassthroughBackend is used storing secrets directly into the physical
@@ -19,28 +44,8 @@ func PassthroughBackendFactory(map[string]string) (logical.Backend, error) {
 // fancy.
 type PassthroughBackend struct{}
 
-func (b *PassthroughBackend) HandleRequest(req *logical.Request) (*logical.Response, error) {
-	// TODO(mitchellh): help, let's just do it when we migrate to helper/backend
-
-	switch req.Operation {
-	case logical.ReadOperation:
-		return b.handleRead(req)
-	case logical.WriteOperation:
-		return b.handleWrite(req)
-	case logical.DeleteOperation:
-		return b.handleDelete(req)
-	case logical.ListOperation:
-		return b.handleList(req)
-	default:
-		return nil, logical.ErrUnsupportedOperation
-	}
-}
-
-func (b *PassthroughBackend) RootPaths() []string {
-	return nil
-}
-
-func (b *PassthroughBackend) handleRead(req *logical.Request) (*logical.Response, error) {
+func (b *PassthroughBackend) handleRead(
+	req *logical.Request, data *framework.FieldData) (*logical.Response, error) {
 	// Read the path
 	out, err := req.Storage.Get(req.Path)
 	if err != nil {
@@ -83,7 +88,8 @@ func (b *PassthroughBackend) handleRead(req *logical.Request) (*logical.Response
 	return resp, nil
 }
 
-func (b *PassthroughBackend) handleWrite(req *logical.Request) (*logical.Response, error) {
+func (b *PassthroughBackend) handleWrite(
+	req *logical.Request, data *framework.FieldData) (*logical.Response, error) {
 	// Check that some fields are given
 	if len(req.Data) == 0 {
 		return nil, fmt.Errorf("missing data fields")
@@ -107,7 +113,8 @@ func (b *PassthroughBackend) handleWrite(req *logical.Request) (*logical.Respons
 	return nil, nil
 }
 
-func (b *PassthroughBackend) handleDelete(req *logical.Request) (*logical.Response, error) {
+func (b *PassthroughBackend) handleDelete(
+	req *logical.Request, data *framework.FieldData) (*logical.Response, error) {
 	// Delete the key at the request path
 	if err := req.Storage.Delete(req.Path); err != nil {
 		return nil, err
@@ -116,7 +123,8 @@ func (b *PassthroughBackend) handleDelete(req *logical.Request) (*logical.Respon
 	return nil, nil
 }
 
-func (b *PassthroughBackend) handleList(req *logical.Request) (*logical.Response, error) {
+func (b *PassthroughBackend) handleList(
+	req *logical.Request, data *framework.FieldData) (*logical.Response, error) {
 	// List the keys at the prefix given by the request
 	keys, err := req.Storage.List(req.Path)
 	if err != nil {
@@ -126,3 +134,19 @@ func (b *PassthroughBackend) handleList(req *logical.Request) (*logical.Response
 	// Generate the response
 	return logical.ListResponse(keys), nil
 }
+
+const passthroughHelpSynopsis = `
+Pass-through secret storage to the physical backend, allowing you to
+read/write arbitrary data into secret storage.
+`
+
+const passthroughHelpDescription = `
+The pass-through backend reads and writes arbitrary data into secret storage,
+encrypting it along the way.
+
+A lease can be specified when writing with the "lease" field. If given, then
+when the secret is read, Vault will report a lease with that duration. It
+is expected that the consumer of this backend properly writes renewed keys
+before the lease is up. In addition, revocation must be handled by the
+user of this backend.
+`
