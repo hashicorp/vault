104c252283f9f4522c6bedf647ed529f21342e15
Armon Dadgar <armon.dadgar@gmail.com>
2015-04-01 13:54:50 -0700

    audit: Adding basic interface methods

--- audit/audit.go
+++ audit/audit.go
@@ -1,10 +1,19 @@
 package audit
 
+import "github.com/hashicorp/vault/logical"
+
 // Backend interface must be implemented for an audit
 // mechanism to be made available. Audit backends can be enabled to
 // sink information to different backends such as logs, file, databases,
 // or other external services.
 type Backend interface {
+	// LogRequest is used to syncronously log a request. This is done after the
+	// request is authorized but before the request is executed.
+	LogRequest(*logical.Auth, *logical.Request) error
+
+	// LogResponse is used to syncronously log a response. This is done after
+	// the request is processed but before the response is sent.
+	LogResponse(*logical.Auth, *logical.Request, *logical.Response, error) error
 }
 
 // Factory is the factory function to create an audit backend.
