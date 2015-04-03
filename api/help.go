3ad31ea6e226af247402ba58d0f95513babd8e7b
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2015-04-02 22:26:34 -0700

    api: help

--- api/help.go
+++ api/help.go
@@ -1,6 +1,24 @@
 package api
 
+import (
+	"fmt"
+)
+
 // Help reads the help information for the given path.
-func (c *Client) Help(path string) (string, error) {
-	return "", nil
+func (c *Client) Help(path string) (*Help, error) {
+	r := c.NewRequest("GET", fmt.Sprintf("/v1/%s?help=1", path))
+	resp, err := c.RawRequest(r)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	var result Help
+	err = resp.DecodeJSON(&result)
+	return &result, err
+}
+
+type Help struct {
+	Help    string   `json:"help"`
+	SeeAlso []string `json:"see_also"`
 }
