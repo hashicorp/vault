0454e1f48ecc0a33205e5f735728a394fb7210a9
Armon Dadgar <armon.dadgar@gmail.com>
2015-04-20 10:44:51 -0700

    api: Allow reseting of request body

--- api/request_test.go
+++ api/request_test.go
@@ -29,3 +29,35 @@ func TestRequestSetJSONBody(t *testing.T) {
 		t.Fatalf("bad: %d", len(actual))
 	}
 }
+
+func TestRequestResetJSONBody(t *testing.T) {
+	var r Request
+	raw := map[string]interface{}{"foo": "bar"}
+	if err := r.SetJSONBody(raw); err != nil {
+		t.Fatalf("err: %s", err)
+	}
+
+	var buf bytes.Buffer
+	if _, err := io.Copy(&buf, r.Body); err != nil {
+		t.Fatalf("err: %s", err)
+	}
+
+	if err := r.ResetJSONBody(); err != nil {
+		t.Fatalf("err: %s", err)
+	}
+
+	var buf2 bytes.Buffer
+	if _, err := io.Copy(&buf2, r.Body); err != nil {
+		t.Fatalf("err: %s", err)
+	}
+
+	expected := `{"foo":"bar"}`
+	actual := strings.TrimSpace(buf2.String())
+	if actual != expected {
+		t.Fatalf("bad: %s", actual)
+	}
+
+	if int64(len(buf2.String())) != r.BodySize {
+		t.Fatalf("bad: %d", len(actual))
+	}
+}
