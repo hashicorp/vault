6be765b6c555307c1c54734dd878b06ad29097e7
Armon Dadgar <armon.dadgar@gmail.com>
2015-04-20 11:59:24 -0700

    http: Adding sys/leader endpoint

---
+++ http/sys_leader.go
@@ -0,0 +1,43 @@
+package http
+
+import (
+	"net/http"
+
+	"github.com/hashicorp/vault/vault"
+)
+
+func handleSysLeader(core *vault.Core) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		switch r.Method {
+		case "GET":
+			handleSysLeaderGet(core, w, r)
+		default:
+			respondError(w, http.StatusMethodNotAllowed, nil)
+		}
+	})
+}
+
+func handleSysLeaderGet(core *vault.Core, w http.ResponseWriter, r *http.Request) {
+	haEnabled := true
+	isLeader, address, err := core.Leader()
+	if err == vault.ErrHANotEnabled {
+		haEnabled = false
+		err = nil
+	}
+	if err != nil {
+		respondError(w, http.StatusInternalServerError, err)
+		return
+	}
+
+	respondOk(w, &LeaderResponse{
+		HAEnabled:     haEnabled,
+		IsSelf:        isLeader,
+		LeaderAddress: address,
+	})
+}
+
+type LeaderResponse struct {
+	HAEnabled     bool   `json:"ha_enabled"`
+	IsSelf        bool   `json:"is_self"`
+	LeaderAddress string `json:"leader_address"`
+}
---
+++ http/sys_leader_test.go
@@ -0,0 +1,32 @@
+package http
+
+import (
+	"net/http"
+	"reflect"
+	"testing"
+
+	"github.com/hashicorp/vault/vault"
+)
+
+func TestSysLeader_get(t *testing.T) {
+	core, _, _ := vault.TestCoreUnsealed(t)
+	ln, addr := TestServer(t, core)
+	defer ln.Close()
+
+	resp, err := http.Get(addr + "/v1/sys/leader")
+	if err != nil {
+		t.Fatalf("err: %s", err)
+	}
+
+	var actual map[string]interface{}
+	expected := map[string]interface{}{
+		"ha_enabled":     false,
+		"is_self":        false,
+		"leader_address": "",
+	}
+	testResponseStatus(t, resp, 200)
+	testResponseBody(t, resp, &actual)
+	if !reflect.DeepEqual(actual, expected) {
+		t.Fatalf("bad: %#v", actual)
+	}
+}
