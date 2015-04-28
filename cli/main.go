efbe3fc4042966e553c15ea9796133aa2fabcdb5
Seth Vargo <sethvargo@gmail.com>
2015-04-28 12:57:44 -0400

    Accept -version too

--- cli/main.go
+++ cli/main.go
@@ -15,7 +15,7 @@ func RunCustom(args []string, commands map[string]cli.CommandFactory) int {
 	// Get the command line args. We shortcut "--version" and "-v" to
 	// just show the version.
 	for _, arg := range args {
-		if arg == "-v" || arg == "--version" {
+		if arg == "-v" || arg == "-version" || arg == "--version" {
 			newArgs := make([]string, len(args)+1)
 			newArgs[0] = "version"
 			copy(newArgs[1:], args)
