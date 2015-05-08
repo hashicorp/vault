9e8b045308851758a125e9df77c6353c9c67130b
Giovanni Bajo <rasky@develer.com>
2015-05-08 09:28:26 +0200

    auth/ldap: implement login renew

--- builtin/credential/ldap/backend.go
+++ builtin/credential/ldap/backend.go
@@ -1,8 +1,12 @@
 package ldap
 
 import (
+	"fmt"
+	"strings"
+
 	"github.com/hashicorp/vault/logical"
 	"github.com/hashicorp/vault/logical/framework"
+	"github.com/vanackere/ldap"
 )
 
 func Factory(map[string]string) (logical.Backend, error) {
@@ -31,7 +35,7 @@ func Backend() *framework.Backend {
 			pathGroups(&b),
 		}),
 
-		// AuthRenew: b.pathLoginRenew,
+		AuthRenew: b.pathLoginRenew,
 	}
 
 	return b.Backend
@@ -39,6 +43,58 @@ func Backend() *framework.Backend {
 
 type backend struct {
 	*framework.Backend
+}
+
+func (b *backend) Login(req *logical.Request, username string, password string) ([]string, *logical.Response, error) {
+
+	cfg, err := b.Config(req)
+	if err != nil {
+		return nil, nil, err
+	}
+	if cfg == nil {
+		return nil, logical.ErrorResponse("ldap backend not configured"), nil
+	}
+
+	c, err := cfg.DialLDAP()
+	if err != nil {
+		return nil, logical.ErrorResponse(err.Error()), nil
+	}
+
+	// Try to authenticate to the server using the provided credentials
+	binddn := fmt.Sprintf("%s=%s,%s", cfg.UserAttr, username, cfg.UserDN)
+	if err = c.Bind(binddn, password); err != nil {
+		return nil, logical.ErrorResponse(fmt.Sprintf("LDAP bind failed: %v", err)), nil
+	}
+
+	// Enumerate all groups the user is member of. The search filter should
+	// work with both openldap and MS AD standard schemas.
+	sresult, err := c.Search(&ldap.SearchRequest{
+		BaseDN: cfg.GroupDN,
+		Scope:  2, // subtree
+		Filter: fmt.Sprintf("(|(memberUid=%s)(member=%s)(uniqueMember=%s))", username, binddn, binddn),
+	})
+	if err != nil {
+		return nil, logical.ErrorResponse(fmt.Sprintf("LDAP search failed: %v", err)), nil
+	}
+
+	var allgroups []string
+	var policies []string
+	for _, e := range sresult.Entries {
+		// Expected syntax for group DN: cn=groupanem,ou=Group,dc=example,dc=com
+		dn := strings.Split(e.DN, ",")
+		gname := strings.SplitN(dn[0], "=", 2)[1]
+		allgroups = append(allgroups, gname)
+		group, err := b.Group(req.Storage, gname)
+		if err == nil && group != nil {
+			policies = append(policies, group.Policies...)
+		}
+	}
+
+	if len(policies) == 0 {
+		return nil, logical.ErrorResponse("user is not member of any authorized group"), nil
+	}
+
+	return policies, nil, nil
 }
 
 const backendHelp = `
--- builtin/credential/ldap/path_login.go
+++ builtin/credential/ldap/path_login.go
@@ -1,12 +1,12 @@
 package ldap
 
 import (
-	"fmt"
+	"sort"
 	"strings"
+	"time"
 
 	"github.com/hashicorp/vault/logical"
 	"github.com/hashicorp/vault/logical/framework"
-	"github.com/vanackere/ldap"
 )
 
 func pathLogin(b *backend) *framework.Path {
@@ -38,75 +38,45 @@ func (b *backend) pathLogin(
 	username := d.Get("username").(string)
 	password := d.Get("password").(string)
 
-	cfg, err := b.Config(req)
-	if err != nil {
-		return nil, err
-	}
-	if cfg == nil {
-		return logical.ErrorResponse("ldap backend not configured"), nil
-	}
-
-	c, err := cfg.DialLDAP()
-	if err != nil {
-		return logical.ErrorResponse(err.Error()), nil
-	}
-
-	// Try to authenticate to the server using the provided credentials
-	binddn := fmt.Sprintf("%s=%s,%s", cfg.UserAttr, username, cfg.UserDN)
-	if err = c.Bind(binddn, password); err != nil {
-		return logical.ErrorResponse(fmt.Sprintf("LDAP bind failed: %v", err)), nil
-	}
-
-	// Enumerate all groups the user is member of. The search filter should
-	// work with both openldap and MS AD standard schemas.
-	sresult, err := c.Search(&ldap.SearchRequest{
-		BaseDN: cfg.GroupDN,
-		Scope:  2, // subtree
-		Filter: fmt.Sprintf("(|(memberUid=%s)(member=%s)(uniqueMember=%s))", username, binddn, binddn),
-	})
-	if err != nil {
-		return logical.ErrorResponse(fmt.Sprintf("LDAP search failed: %v", err)), nil
-	}
-
-	var policies []string
-	for _, e := range sresult.Entries {
-		// Expected syntax for group DN: cn=groupanem,ou=Group,dc=example,dc=com
-		dn := strings.Split(e.DN, ",")
-		group, err := b.Group(req.Storage, dn[0][3:])
-		if err == nil && group != nil {
-			policies = append(policies, group.Policies...)
-		}
-	}
-
+	policies, resp, err := b.Login(req, username, password)
 	if len(policies) == 0 {
-		return logical.ErrorResponse("user is not member of any authorized group"), nil
+		return resp, err
 	}
+
+	sort.Strings(policies)
 
 	return &logical.Response{
 		Auth: &logical.Auth{
 			Policies: policies,
 			Metadata: map[string]string{
 				"username": username,
+				"password": password,
+				"policies": strings.Join(policies, ","),
 			},
 			DisplayName: username,
 		},
 	}, nil
 }
 
-// func (b *backend) pathLoginRenew(
-// 	req *logical.Request, d *framework.FieldData) (*logical.Response, error) {
-// 	// Get the user and validate auth
-// 	user, err := b.User(req.Storage, req.Auth.Metadata["username"])
-// 	if err != nil {
-// 		return nil, err
-// 	}
-// 	if user == nil {
-// 		// User no longer exists, do not renew
-// 		return nil, nil
-// 	}
+func (b *backend) pathLoginRenew(
+	req *logical.Request, d *framework.FieldData) (*logical.Response, error) {
 
-// 	return framework.LeaseExtend(1*time.Hour, 0)(req, d)
-// }
+	username := req.Auth.Metadata["username"]
+	password := req.Auth.Metadata["password"]
+	prevpolicies := req.Auth.Metadata["password"]
+
+	policies, resp, err := b.Login(req, username, password)
+	if len(policies) == 0 {
+		return resp, err
+	}
+
+	sort.Strings(policies)
+	if strings.Join(policies, ",") != prevpolicies {
+		return logical.ErrorResponse("policies have changed, revoking login"), nil
+	}
+
+	return framework.LeaseExtend(1*time.Hour, 0)(req, d)
+}
 
 const pathLoginSyn = `
 Log in with a username and password.
