52eeb8551e8b8104c5339b29eb1b46fd2999d809
Robin Walsh <robin@counsyl.com>
2015-08-26 09:26:20 -0700

    Adding one more test (for no-op case)

--- builtin/logical/aws/secret_access_keys_test.go
+++ builtin/logical/aws/secret_access_keys_test.go
@@ -9,7 +9,17 @@ func TestNormalizeDisplayName(t *testing.T) {
 	expectedName := "___test_name_should_be_normalized___"
 	normalizedName := normalizeDisplayName(invalidName)
 	if normalizedName != expectedName {
-		t.Fatalf("normalizeDisplayName does not normalize AWS name correctly: %s", normalizedName)
+		t.Fatalf(
+			"normalizeDisplayName does not normalize AWS name correctly: %s",
+			normalizedName)
+	}
+
+	validName := "test_name_should_normalize_to_itself@example.com"
+	normalizedValidName := normalizeDisplayName(validName)
+	if normalizedValidName != validName {
+		t.Fatalf(
+			"normalizeDisplayName erroneously normalizes valid names: %s",
+			normalizedName)
 	}
 
 }
