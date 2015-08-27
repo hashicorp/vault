5ae58a6a52b5b7136ab16174c11b0b2cec1dee7a
Jeff Mitchell <jeffrey.mitchell@gmail.com>
2015-08-27 09:14:03 -0700

    Add some documentation to SystemConfig

--- logical/system_config.go
+++ logical/system_config.go
@@ -2,9 +2,15 @@ package logical
 
 import "time"
 
-// LogicalSystemConfig exposes system configuration information in a safe way
+// SystemConfig exposes system configuration information in a safe way
 // for other logical backends to consume
 type SystemConfig struct {
+
+	// DefaultLeaseTTL returns the default lease TTL set in Vault configuration
 	DefaultLeaseTTL func() time.Duration
-	MaxLeaseTTL     func() time.Duration
+
+	// MaxLeaseTTL returns the max lease TTL set in Vault configuration; backend
+	// authors should take care not to issue credentials that last longer than
+	// this value, as Vault will revoke them
+	MaxLeaseTTL func() time.Duration
 }
