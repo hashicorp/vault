9405f70d3327fdbc47f96979e8b60befb844c712
eladapps <elad.schapira@appsflyer.com>
2015-10-13 07:09:26 +0300

    jwt removal

--- cli/commands.go
+++ cli/commands.go
@@ -17,7 +17,6 @@ import (
 	"github.com/hashicorp/vault/builtin/logical/aws"
 	"github.com/hashicorp/vault/builtin/logical/cassandra"
 	"github.com/hashicorp/vault/builtin/logical/consul"
-	"github.com/hashicorp/vault/builtin/logical/jwt"
 	"github.com/hashicorp/vault/builtin/logical/mysql"
 	"github.com/hashicorp/vault/builtin/logical/pki"
 	"github.com/hashicorp/vault/builtin/logical/postgresql"
@@ -78,7 +77,6 @@ func Commands(metaPtr *command.Meta) map[string]cli.CommandFactory {
 					"transit":    transit.Factory,
 					"mysql":      mysql.Factory,
 					"ssh":        ssh.Factory,
-					"jwt":        jwt.Factory,
 				},
 				ShutdownCh: makeShutdownCh(),
 			}, nil
