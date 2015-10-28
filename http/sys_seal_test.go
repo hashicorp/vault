d7f528a768d08ee3b99381276bbd6bb8d43c6bf6
Jeff Mitchell <jeffrey.mitchell@gmail.com>
2015-10-28 15:59:39 -0400

    Add reset support to the unseal command.
    Reset clears the provided unseal keys, allowing the process to be begun
    again. Includes documentation and unit test changes.
    
    Fixes #695

--- http/sys_seal_test.go
+++ http/sys_seal_test.go
@@ -129,3 +129,62 @@ func TestSysUnseal_badKey(t *testing.T) {
 		t.Fatalf("bad: %#v", actual)
 	}
 }
+
+func TestSysUnseal_Reset(t *testing.T) {
+	core := vault.TestCore(t)
+	ln, addr := TestServer(t, core)
+	defer ln.Close()
+
+	thresh := 3
+	resp := testHttpPut(t, "", addr+"/v1/sys/init", map[string]interface{}{
+		"secret_shares":    5,
+		"secret_threshold": thresh,
+	})
+
+	var actual map[string]interface{}
+	testResponseStatus(t, resp, 200)
+	testResponseBody(t, resp, &actual)
+	keysRaw, ok := actual["keys"]
+	if !ok {
+		t.Fatalf("no keys: %#v", actual)
+	}
+	for i, key := range keysRaw.([]interface{}) {
+		if i > thresh-2 {
+			break
+		}
+
+		resp := testHttpPut(t, "", addr+"/v1/sys/unseal", map[string]interface{}{
+			"key": key.(string),
+		})
+
+		var actual map[string]interface{}
+		expected := map[string]interface{}{
+			"sealed":   true,
+			"t":        float64(3),
+			"n":        float64(5),
+			"progress": float64(i + 1),
+		}
+		testResponseStatus(t, resp, 200)
+		testResponseBody(t, resp, &actual)
+		if !reflect.DeepEqual(actual, expected) {
+			t.Fatalf("\nexpected:\n%#v\nactual:\n%#v\n", expected, actual)
+		}
+	}
+
+	resp = testHttpPut(t, "", addr+"/v1/sys/unseal", map[string]interface{}{
+		"reset": true,
+	})
+
+	actual = map[string]interface{}{}
+	expected := map[string]interface{}{
+		"sealed":   true,
+		"t":        float64(3),
+		"n":        float64(5),
+		"progress": float64(0),
+	}
+	testResponseStatus(t, resp, 200)
+	testResponseBody(t, resp, &actual)
+	if !reflect.DeepEqual(actual, expected) {
+		t.Fatalf("\nexpected:\n%#v\nactual:\n%#v\n", expected, actual)
+	}
+}
