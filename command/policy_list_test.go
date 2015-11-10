50e31110741f057597ed56777f0a1d0899c6a2c7
Sergey Romanov <xxsmotur@gmail.com>
2015-11-10 19:58:50 +0500

    Policies. Add test for detailed information of policies

--- command/policy_list_test.go
+++ command/policy_list_test.go
@@ -50,3 +50,25 @@ func TestPolicyRead(t *testing.T) {
 		t.Fatalf("bad: %d\n\n%s", code, ui.ErrorWriter.String())
 	}
 }
+
+func TestPolicyOutputDetailed(t *testing.T) {
+	core, _, token := vault.TestCoreUnsealed(t)
+	ln, addr := http.TestServer(t, core)
+	defer ln.Close()
+
+	ui := new(cli.MockUi)
+	c := &PolicyListCommand{
+		Meta: Meta{
+			ClientToken: token,
+			Ui:          ui,
+		},
+	}
+
+	args := []string{
+		"-detailed",
+		"-address", addr,
+	}
+	if code := c.Run(args); code != 0 {
+		t.Fatalf("bad: %d\n\n%s", code, ui.ErrorWriter.String())
+	}
+}
