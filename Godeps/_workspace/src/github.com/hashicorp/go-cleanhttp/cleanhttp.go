bf246af333361e991e1f14f2a2b232aebec1a758
Jeff Mitchell <jeffrey.mitchell@gmail.com>
2015-12-17 15:08:19 -0500

    Update go-cleanhttp dependency.
    Probably fixes #867

--- Godeps/_workspace/src/github.com/hashicorp/go-cleanhttp/cleanhttp.go
+++ Godeps/_workspace/src/github.com/hashicorp/go-cleanhttp/cleanhttp.go
@@ -3,13 +3,14 @@ package cleanhttp
 import (
 	"net"
 	"net/http"
+	"runtime"
 	"time"
 )
 
 // DefaultTransport returns a new http.Transport with the same default values
 // as http.DefaultTransport
 func DefaultTransport() *http.Transport {
-	return &http.Transport{
+	transport := &http.Transport{
 		Proxy: http.ProxyFromEnvironment,
 		Dial: (&net.Dialer{
 			Timeout:   30 * time.Second,
@@ -17,6 +18,8 @@ func DefaultTransport() *http.Transport {
 		}).Dial,
 		TLSHandshakeTimeout: 10 * time.Second,
 	}
+	SetTransportFinalizer(transport)
+	return transport
 }
 
 // DefaultClient returns a new http.Client with the same default values as
@@ -25,4 +28,13 @@ func DefaultClient() *http.Client {
 	return &http.Client{
 		Transport: DefaultTransport(),
 	}
+}
+
+// SetTransportFinalizer sets a finalizer on the transport to ensure that
+// idle connections are closed prior to garbage collection; otherwise
+// these may leak
+func SetTransportFinalizer(transport *http.Transport) {
+	runtime.SetFinalizer(&transport, func(t **http.Transport) {
+		(*t).CloseIdleConnections()
+	})
 }
