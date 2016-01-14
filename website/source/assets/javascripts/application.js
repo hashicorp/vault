3c2b29d528d50c94b580d0273dccd0dd7474fc40
Seth Vargo <sethvargo@gmail.com>
2016-01-14 15:00:40 -0500

    Do not use compressed javascripts
    Minifier gets really confused when you give it already-compressed
    javascript.

--- website/source/assets/javascripts/application.js
+++ website/source/assets/javascripts/application.js
@@ -1,9 +1,9 @@
 //= require jquery
 //= require bootstrap
-//= require jquery.waypoints.min
+//= require jquery.waypoints
 //= require lib/ember-template-compiler
-//= require lib/ember-1-10.min
-//= require lib/ember-data-1-0.min
+//= require lib/ember-1-10
+//= require lib/ember-data-1-0
 
 //= require lib/String.substitute
 //= require lib/Function.prototype.bind
