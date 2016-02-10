a78270ea0ef6d71c5dbb72c260d42398b5de03c8
Jeff Mitchell <jeffrey.mitchell@gmail.com>
2016-02-10 12:08:29 -0500

    Bump website download links to 0.5.0

--- website/config.rb
+++ website/config.rb
@@ -2,7 +2,7 @@ set :base_url, "https://www.vaultproject.io/"
 
 activate :hashicorp do |h|
   h.name         = "vault"
-  h.version      = "0.4.1"
+  h.version      = "0.5.0"
   h.github_slug  = "hashicorp/vault"
   h.website_root = "website"
 end
