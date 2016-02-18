cabc43ee5bb6d8ba30252d3bba05872d1155681b
Jeff Mitchell <jeffrey.mitchell@gmail.com>
2016-02-18 16:43:43 -0500

    Bump version

--- version/version.go
+++ version/version.go
@@ -10,12 +10,12 @@ var GitCommit string
 var GitDescribe string
 
 // The main version number that is being run at the moment.
-const Version = "0.5.0"
+const Version = "0.5.1"
 
 // A pre-release marker for the version. If this is "" (empty string)
 // then it means that it is a final release. Otherwise, this is a pre-release
 // such as "dev" (in development), "beta", "rc1", etc.
-const VersionPrerelease = ""
+const VersionPrerelease = "dev"
 
 // VersionInfo
 type VersionInfo struct {
