2f79bb12ee34acc280f07634a9c6632b8f7b9fbd
Jeff Mitchell <jeffrey.mitchell@gmail.com>
2016-02-25 11:29:03 -0500

    Bump TF variables

--- terraform/aws/variables.tf
+++ terraform/aws/variables.tf
@@ -3,7 +3,7 @@
 //-------------------------------------------------------------------
 
 variable "download-url" {
-    default = "https://releases.hashicorp.com/vault/0.5.0/vault_0.5.0_linux_amd64.zip"
+    default = "https://releases.hashicorp.com/vault/0.5.1/vault_0.5.1_linux_amd64.zip"
     description = "URL to download Vault"
 }
 
