5b9ef4ec2e15fc916e29697438f7fe7aa87cf95e
vishalnayak <vishalnayakv@gmail.com>
2016-03-03 14:00:36 -0500

    testcase changes

--- command/capabilities_test.go
+++ command/capabilities_test.go
@@ -9,7 +9,7 @@ import (
 )
 
 func TestCapabilities_Args(t *testing.T) {
-	core, key, _ := vault.TestCoreUnsealed(t)
+	core, _, _ := vault.TestCoreUnsealed(t)
 	ln, _ := http.TestServer(t, core)
 	defer ln.Close()
 
@@ -25,13 +25,30 @@ func TestCapabilities_Args(t *testing.T) {
 		t.Fatalf("expected failure due to no args")
 	}
 
-	args = []string{"test"}
-	if code := c.Run(args); code != 0 {
-		t.Fatalf("bad: %d\n\n%s", code, ui.ErrorWriter.String())
+	args = []string{"invalidtoken", "test"}
+	if code := c.Run(args); code == 0 {
+		t.Fatalf("expected failure due to no invalid token")
 	}
 
-	args = []string{string(key), "test"}
-	if code := c.Run(args); code != 0 {
-		t.Fatalf("bad: %d\n\n%s", code, ui.ErrorWriter.String())
-	}
+	/*
+		args = []string{"test"}
+		if code := c.Run(args); code != 0 {
+			t.Fatalf("bad: %d\n\n%s", code, ui.ErrorWriter.String())
+		}
+
+			log.Printf("result1: %s\n", string(ui.OutputWriter.Bytes()))
+			if !strings.Contains(string(ui.OutputWriter.Bytes()), "This is a 'root' token.") {
+				t.Fatalf("bad: %s", ui.OutputWriter.String())
+			}
+
+			args = []string{string(key), "test"}
+			if code := c.Run(args); code != 0 {
+				t.Fatalf("bad: %d\n\n%s", code, ui.ErrorWriter.String())
+			}
+
+			log.Printf("result2: %s\n", string(ui.OutputWriter.Bytes()))
+			if !strings.Contains(string(ui.OutputWriter.Bytes()), "This is a 'root' token.") {
+				t.Fatalf("bad: %s", ui.OutputWriter.String())
+			}
+	*/
 }
