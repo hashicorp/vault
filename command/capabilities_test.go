0f82724f1f618a445a3c3fd7349408df5d6150d4
vishalnayak <vishalnayakv@gmail.com>
2016-03-05 00:03:55 -0500

    test cases for capabilities endpoint

--- command/capabilities_test.go
+++ command/capabilities_test.go
@@ -8,47 +8,37 @@ import (
 	"github.com/mitchellh/cli"
 )
 
-func TestCapabilities_Args(t *testing.T) {
-	core, _, _ := vault.TestCoreUnsealed(t)
-	ln, _ := http.TestServer(t, core)
+func TestCapabilities_Basic(t *testing.T) {
+	core, _, token := vault.TestCoreUnsealed(t)
+	ln, addr := http.TestServer(t, core)
 	defer ln.Close()
-
 	ui := new(cli.MockUi)
 	c := &CapabilitiesCommand{
 		Meta: Meta{
-			Ui: ui,
+			ClientToken: token,
+			Ui:          ui,
 		},
 	}
 
-	args := []string{}
+	var args []string
+
+	args = []string{"-address", addr}
 	if code := c.Run(args); code == 0 {
 		t.Fatalf("expected failure due to no args")
 	}
 
-	args = []string{"invalidtoken", "test"}
+	args = []string{"-address", addr, "testpath"}
+	if code := c.Run(args); code != 0 {
+		t.Fatalf("bad: %d\n\n%s", code, ui.ErrorWriter.String())
+	}
+
+	args = []string{"-address", addr, token, "test"}
+	if code := c.Run(args); code != 0 {
+		t.Fatalf("bad: %d\n\n%s", code, ui.ErrorWriter.String())
+	}
+
+	args = []string{"-address", addr, "invalidtoken", "test"}
 	if code := c.Run(args); code == 0 {
 		t.Fatalf("expected failure due to no invalid token")
 	}
-
-	/*
-		args = []string{"test"}
-		if code := c.Run(args); code != 0 {
-			t.Fatalf("bad: %d\n\n%s", code, ui.ErrorWriter.String())
-		}
-
-			log.Printf("result1: %s\n", string(ui.OutputWriter.Bytes()))
-			if !strings.Contains(string(ui.OutputWriter.Bytes()), "This is a 'root' token.") {
-				t.Fatalf("bad: %s", ui.OutputWriter.String())
-			}
-
-			args = []string{string(key), "test"}
-			if code := c.Run(args); code != 0 {
-				t.Fatalf("bad: %d\n\n%s", code, ui.ErrorWriter.String())
-			}
-
-			log.Printf("result2: %s\n", string(ui.OutputWriter.Bytes()))
-			if !strings.Contains(string(ui.OutputWriter.Bytes()), "This is a 'root' token.") {
-				t.Fatalf("bad: %s", ui.OutputWriter.String())
-			}
-	*/
 }
--- http/sys_capabilities_test.go
+++ http/sys_capabilities_test.go
@@ -1,1 +1,82 @@
 package http
+
+import (
+	"reflect"
+	"testing"
+
+	"github.com/hashicorp/vault/vault"
+)
+
+func TestSysCapabilities(t *testing.T) {
+	core, _, token := vault.TestCoreUnsealed(t)
+	ln, addr := TestServer(t, core)
+	defer ln.Close()
+	TestServerAuth(t, addr, token)
+
+	// Send both token and path
+	resp := testHttpPost(t, token, addr+"/v1/sys/capabilities", map[string]interface{}{
+		"token": token,
+		"path":  "testpath",
+	})
+
+	var actual map[string][]string
+	testResponseStatus(t, resp, 200)
+	testResponseBody(t, resp, &actual)
+
+	expected := map[string][]string{
+		"capabilities": []string{"root"},
+	}
+	if !reflect.DeepEqual(actual, expected) {
+		t.Fatalf("bad: got\n%#v\nexpected\n%#v\n", actual, expected)
+	}
+
+	// Send only path to capabilities-self
+	resp = testHttpPost(t, token, addr+"/v1/sys/capabilities-self", map[string]interface{}{
+		"path": "testpath",
+	})
+	testResponseStatus(t, resp, 200)
+	testResponseBody(t, resp, &actual)
+	if !reflect.DeepEqual(actual, expected) {
+		t.Fatalf("bad: got\n%#v\nexpected\n%#v\n", actual, expected)
+	}
+
+	// Testing for non-root tokens
+
+	// Create a policy first
+	resp = testHttpPost(t, token, addr+"/v1/sys/policy/foo", map[string]interface{}{
+		"rules": `path "testpath" {capabilities = ["read","sudo"]}`,
+	})
+	testResponseStatus(t, resp, 204)
+
+	// Create a token against the test policy
+	resp = testHttpPost(t, token, addr+"/v1/auth/token/create", map[string]interface{}{
+		"policies": []string{"foo"},
+	})
+
+	var tokenResp map[string]interface{}
+	testResponseStatus(t, resp, 200)
+	testResponseBody(t, resp, &tokenResp)
+
+	// Check if desired policies are present in the token
+	auth := tokenResp["auth"].(map[string]interface{})
+	actualPolicies := auth["policies"]
+	expectedPolicies := []interface{}{"default", "foo"}
+	if !reflect.DeepEqual(actualPolicies, expectedPolicies) {
+		t.Fatalf("bad: got\n%#v\nexpected\n%#v\n", actualPolicies, expectedPolicies)
+	}
+
+	// Check the capabilities with the created non-root token
+	resp = testHttpPost(t, token, addr+"/v1/sys/capabilities", map[string]interface{}{
+		"token": auth["client_token"],
+		"path":  "testpath",
+	})
+	testResponseStatus(t, resp, 200)
+	testResponseBody(t, resp, &actual)
+
+	expected = map[string][]string{
+		"capabilities": []string{"sudo", "read"},
+	}
+	if !reflect.DeepEqual(actual, expected) {
+		t.Fatalf("bad: got\n%#v\nexpected\n%#v\n", actual, expected)
+	}
+}
