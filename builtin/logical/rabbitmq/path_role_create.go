450f8675e6914fc8cbff20cc2d996a247c8f39b2
Kevin Pike <kpike@doubledutch.me>
2016-04-08 09:50:44 -0700

    Do not provide a default lease

--- builtin/logical/rabbitmq/path_role_create.go
+++ builtin/logical/rabbitmq/path_role_create.go
@@ -2,7 +2,6 @@ package rabbitmq
 
 import (
 	"fmt"
-	"time"
 
 	"github.com/hashicorp/uuid"
 	"github.com/hashicorp/vault/logical"
@@ -48,7 +47,7 @@ func (b *backend) pathRoleCreateRead(
 		return nil, err
 	}
 	if lease == nil {
-		lease = &configLease{Lease: 1 * time.Hour}
+		lease = &configLease{}
 	}
 
 	displayName := req.DisplayName
