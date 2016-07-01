339aec975122aaf29fb9b3c6811f963ac95ad5ad
Matt Hurne <mhurne@commercehub.com>
2016-07-01 09:12:26 -0400

    mongodb secret backend: Refactor URI parsing logic to leverage url.Parse

--- builtin/logical/mongodb/util.go
+++ builtin/logical/mongodb/util.go
@@ -3,7 +3,6 @@ package mongodb
 import (
 	"crypto/tls"
 	"errors"
-	"fmt"
 	"net"
 	"net/url"
 	"strconv"
@@ -15,120 +14,65 @@ import (
 
 // Unfortunately, mgo doesn't support the ssl parameter in its MongoDB URI parsing logic, so we have to handle that
 // ourselves. See https://github.com/go-mgo/mgo/issues/84
-func parseMongoURI(uri string) (*mgo.DialInfo, error) {
-	uinfo, err := extractMongoURL(uri)
+func parseMongoURI(rawUri string) (*mgo.DialInfo, error) {
+	uri, err := url.Parse(rawUri)
 	if err != nil {
 		return nil, err
 	}
-	direct := false
-	mechanism := ""
-	service := ""
-	source := ""
-	setName := ""
-	poolLimit := 0
-	ssl := false
-	for k, v := range uinfo.options {
-		switch k {
+
+	info := mgo.DialInfo{
+		Addrs:    strings.Split(uri.Host, ","),
+		Database: strings.TrimPrefix(uri.Path, "/"),
+		Timeout:  10 * time.Second,
+	}
+
+	if uri.User != nil {
+		info.Username = uri.User.Username()
+		password, _ := uri.User.Password()
+		info.Password = password
+	}
+
+	query := uri.Query()
+	for key, values := range query {
+		var value string
+		if len(values) > 0 {
+			value = values[0]
+		}
+
+		switch key {
 		case "authSource":
-			source = v
+			info.Source = value
 		case "authMechanism":
-			mechanism = v
+			info.Mechanism = value
 		case "gssapiServiceName":
-			service = v
+			info.Service = value
 		case "replicaSet":
-			setName = v
+			info.ReplicaSetName = value
 		case "maxPoolSize":
-			poolLimit, err = strconv.Atoi(v)
+			poolLimit, err := strconv.Atoi(value)
 			if err != nil {
-				return nil, errors.New("bad value for maxPoolSize: " + v)
+				return nil, errors.New("bad value for maxPoolSize: " + value)
 			}
+			info.PoolLimit = poolLimit
 		case "ssl":
-			if v == "true" {
-				ssl = true
+			if value == "true" {
+				info.DialServer = func(addr *mgo.ServerAddr) (net.Conn, error) {
+					return tls.Dial("tcp", addr.String(), &tls.Config{})
+				}
 			}
 		case "connect":
-			if v == "direct" {
-				direct = true
+			if value == "direct" {
+				info.Direct = true
 				break
 			}
-			if v == "replicaSet" {
+			if value == "replicaSet" {
 				break
 			}
 			fallthrough
 		default:
-			return nil, errors.New("unsupported connection URL option: " + k + "=" + v)
+			return nil, errors.New("unsupported connection URL option: " + key + "=" + value)
 		}
 	}
-	info := mgo.DialInfo{
-		Addrs:          uinfo.addrs,
-		Direct:         direct,
-		Database:       uinfo.db,
-		Username:       uinfo.user,
-		Password:       uinfo.pass,
-		Mechanism:      mechanism,
-		Service:        service,
-		Source:         source,
-		PoolLimit:      poolLimit,
-		ReplicaSetName: setName,
-		Timeout:        10 * time.Second,
-	}
-	if ssl {
-		info.DialServer = func(addr *mgo.ServerAddr) (net.Conn, error) {
-			return tls.Dial("tcp", addr.String(), &tls.Config{})
-		}
-	}
+
 	return &info, nil
-}
-
-func extractMongoURL(s string) (*urlInfo, error) {
-	if strings.HasPrefix(s, "mongodb://") {
-		s = s[10:]
-	}
-	info := &urlInfo{options: make(map[string]string)}
-	if c := strings.Index(s, "?"); c != -1 {
-		for _, pair := range strings.FieldsFunc(s[c+1:], isOptSep) {
-			l := strings.SplitN(pair, "=", 2)
-			if len(l) != 2 || l[0] == "" || l[1] == "" {
-				return nil, errors.New("connection option must be key=value: " + pair)
-			}
-			info.options[l[0]] = l[1]
-		}
-		s = s[:c]
-	}
-	if c := strings.Index(s, "@"); c != -1 {
-		pair := strings.SplitN(s[:c], ":", 2)
-		if len(pair) > 2 || pair[0] == "" {
-			return nil, errors.New("credentials must be provided as user:pass@host")
-		}
-		var err error
-		info.user, err = url.QueryUnescape(pair[0])
-		if err != nil {
-			return nil, fmt.Errorf("cannot unescape username in URL: %q", pair[0])
-		}
-		if len(pair) > 1 {
-			info.pass, err = url.QueryUnescape(pair[1])
-			if err != nil {
-				return nil, fmt.Errorf("cannot unescape password in URL")
-			}
-		}
-		s = s[c+1:]
-	}
-	if c := strings.Index(s, "/"); c != -1 {
-		info.db = s[c+1:]
-		s = s[:c]
-	}
-	info.addrs = strings.Split(s, ",")
-	return info, nil
-}
-
-func isOptSep(c rune) bool {
-	return c == ';' || c == '&'
-}
-
-type urlInfo struct {
-	addrs   []string
-	user    string
-	pass    string
-	db      string
-	options map[string]string
 }
