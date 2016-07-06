60df9d3461347d5043588d44d7288bd6a9c59d62
Jeff Mitchell <jeffrey.mitchell@gmail.com>
2016-07-06 16:42:34 -0400

    Make the API client retry on 5xx errors.
    This should help with transient issues. Full control over min/max delays
    and number of retries (and ability to turn off) is provided in the API
    and via env vars.
    
    Fix tests.

--- vault/expiration.go
+++ vault/expiration.go
@@ -363,7 +363,7 @@ func (m *ExpirationManager) RenewToken(req *logical.Request, source string, toke
 	// Check if the lease is renewable. Note that this also checks for a nil
 	// lease and errors in that case as well.
 	if err := le.renewable(); err != nil {
-		return nil, err
+		return logical.ErrorResponse(err.Error()), logical.ErrInvalidRequest
 	}
 
 	// Attempt to renew the auth entry
