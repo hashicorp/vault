2dc811d1b39d869383b63a0caa53a9b23771f08b
vishalnayak <vishalnayakv@gmail.com>
2016-07-07 11:29:38 -0400

    Input validations for jsonutil

--- helper/jsonutil/json.go
+++ helper/jsonutil/json.go
@@ -8,6 +8,7 @@ import (
 	"reflect"
 )
 
+// Encodes/Marshals the given object into JSON
 func EncodeJSON(in interface{}) ([]byte, error) {
 	var buf bytes.Buffer
 	enc := json.NewEncoder(&buf)
@@ -17,20 +18,36 @@ func EncodeJSON(in interface{}) ([]byte, error) {
 	return buf.Bytes(), nil
 }
 
+// Decodes/Unmarshals the given JSON into a desired object
 func DecodeJSON(data []byte, out interface{}) error {
-	// Decoding requires a pointer type to be supplied
-	value := reflect.ValueOf(out)
-	if value.Kind() != reflect.Ptr {
-		return fmt.Errorf("decoding the value into an invalid type: %v", reflect.TypeOf(out))
+	if data == nil {
+		return fmt.Errorf("'data' being decoded is nil")
+	}
+	if out == nil {
+		return fmt.Errorf("output parameter 'out' is nil")
 	}
 
 	return DecodeJSONFromReader(bytes.NewReader(data), out)
 }
 
+// Decodes/Unmarshals the given io.Reader pointing to a JSON, into a desired object
 func DecodeJSONFromReader(r io.Reader, out interface{}) error {
+	if r == nil {
+		return fmt.Errorf("'io.Reader' being decoded is nil")
+	}
+	if out == nil {
+		return fmt.Errorf("output parameter 'out' is nil")
+	}
+
+	// Decoding requires a pointer type to be supplied
+	value := reflect.ValueOf(out)
+	if value.Kind() != reflect.Ptr {
+		return fmt.Errorf("decoding the value into a non-pointer type: %v", reflect.TypeOf(out))
+	}
+
 	dec := json.NewDecoder(r)
 
-	// While decoding JSON values, intepret the integer values as numbers instead of floats.
+	// While decoding JSON values, intepret the integer values as `json.Number`s instead of `float64`.
 	dec.UseNumber()
 
 	// Since 'out' is an interface representing a pointer, pass it to the decoder without an '&'
