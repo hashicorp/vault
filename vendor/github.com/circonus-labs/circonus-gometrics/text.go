a1b50427f2b767e65fd8a7b6539a33d77bfd9226
matt maier <maier@users.noreply.github.com>
2016-07-22 15:49:23 -0400

    Circonus integration for telemetry metrics

---
+++ vendor/github.com/circonus-labs/circonus-gometrics/text.go
@@ -0,0 +1,37 @@
+package circonusgometrics
+
+// A Text metric is an arbitrary string
+//
+
+// SetText sets a text metric
+func (m *CirconusMetrics) SetText(metric string, val string) {
+	m.SetTextValue(metric, val)
+}
+
+// SetTextValue sets a text metric
+func (m *CirconusMetrics) SetTextValue(metric string, val string) {
+	m.tm.Lock()
+	defer m.tm.Unlock()
+	m.text[metric] = val
+}
+
+// RemoveText removes a text metric
+func (m *CirconusMetrics) RemoveText(metric string) {
+	m.tm.Lock()
+	defer m.tm.Unlock()
+	delete(m.text, metric)
+}
+
+// SetTextFunc sets a text metric to a function [called at flush interval]
+func (m *CirconusMetrics) SetTextFunc(metric string, fn func() string) {
+	m.tfm.Lock()
+	defer m.tfm.Unlock()
+	m.textFuncs[metric] = fn
+}
+
+// RemoveTextFunc a text metric function
+func (m *CirconusMetrics) RemoveTextFunc(metric string) {
+	m.tfm.Lock()
+	defer m.tfm.Unlock()
+	delete(m.textFuncs, metric)
+}
