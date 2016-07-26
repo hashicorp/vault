bae3e090711c664215932aee6444a2a2387814e8
vishalnayak <vishalnayakv@gmail.com>
2016-07-26 09:18:38 -0400

    As it is

--- vault/cluster.go
+++ vault/cluster.go
@@ -10,10 +10,7 @@ import (
 
 const (
 	// Storage path where the local cluster name and identifier are stored
-	coreClusterLocalPath = "core/cluster/local"
-
-	// Storage path where the global cluster name and identifier are stored
-	coreClusterGlobalPath = "core/cluster/global"
+	coreClusterPath = "core/cluster/local"
 )
 
 // Structure representing the storage entry that holds cluster information
@@ -27,16 +24,9 @@ type Cluster struct {
 
 // Cluster fetches the details of either local or global cluster based on the
 // input. This method errors out when Vault is sealed.
-func (c *Core) Cluster(isLocal bool) (*Cluster, error) {
-	var key string
-	if isLocal {
-		key = coreClusterLocalPath
-	} else {
-		key = coreClusterGlobalPath
-	}
-
+func (c *Core) Cluster() (*Cluster, error) {
 	// Fetch the storage entry. This call fails when Vault is sealed.
-	entry, err := c.barrier.Get(key)
+	entry, err := c.barrier.Get(coreClusterPath)
 	if err != nil {
 		return nil, err
 	}
@@ -54,26 +44,11 @@ func (c *Core) Cluster(isLocal bool) (*Cluster, error) {
 }
 
 // setupCluster creates storage entries for holding Vault cluster information.
-// Entries will be created only if they are not already present.
+// Entries will be created only if they are not already present. If clusterName
+// is not supplied, this method will auto-generate it.
 func (c *Core) setupCluster() error {
-	// Create or store a local name and local cluster ID, if not already stored
-	if err := c.setCluster(true, c.localClusterName, c.localClusterID); err != nil {
-		return err
-	}
-
-	// Create or store a global name and global cluster ID, if not already stored
-	if err := c.setCluster(false, c.globalClusterName, c.globalClusterID); err != nil {
-		return err
-	}
-	return nil
-}
-
-// setCluster creates a local or global storage index for a set of cluster
-// information. If cluster name or cluster ID is not supplied, this method will
-// auto-generate them respectively.
-func (c *Core) setCluster(isLocal bool, clusterName, clusterID string) error {
 	// Check if storage index is already present or not
-	cluster, err := c.Cluster(isLocal)
+	cluster, err := c.Cluster()
 	if err != nil {
 		c.logger.Printf("[ERR] core: failed to get cluster details: %v", err)
 		return err
@@ -83,35 +58,27 @@ func (c *Core) setCluster(isLocal bool, clusterName, clusterID string) error {
 		return nil
 	}
 
-	// If clusterName is not supplied, generate one
-	if clusterName == "" {
+	// If cluster name is not supplied, generate one
+	if c.clusterName == "" {
 		clusterNameBytes, err := uuid.GenerateRandomBytes(4)
 		if err != nil {
 			c.logger.Printf("[ERR] core: failed to generate cluster name: %v", err)
 			return err
 		}
-		prefix := "vault-"
-		if isLocal {
-			prefix += "local-"
-		} else {
-			prefix += "global-"
-		}
-		clusterName = fmt.Sprintf("%s%08x", prefix, clusterNameBytes)
+		c.clusterName = fmt.Sprintf("vaule-cluster-%08x", clusterNameBytes)
 	}
 
-	// If clusterName is not supplied, generate one
-	if clusterID == "" {
-		var err error
-		clusterID, err = uuid.GenerateUUID()
-		if err != nil {
-			c.logger.Printf("[ERR] core: failed to generate cluster identifier: %v", err)
-			return err
-		}
+	// Generate a clusterID
+	var err error
+	clusterID, err := uuid.GenerateUUID()
+	if err != nil {
+		c.logger.Printf("[ERR] core: failed to generate cluster identifier: %v", err)
+		return err
 	}
 
 	// Encode the cluster information into as a JSON string
 	rawCluster, err := json.Marshal(&Cluster{
-		Name: clusterName,
+		Name: c.clusterName,
 		ID:   clusterID,
 	})
 	if err != nil {
@@ -119,17 +86,10 @@ func (c *Core) setCluster(isLocal bool, clusterName, clusterID string) error {
 		return err
 	}
 
-	// Determine the storage path
-	var key string
-	if isLocal {
-		key = coreClusterLocalPath
-	} else {
-		key = coreClusterGlobalPath
-	}
-
 	// Store it
 	return c.barrier.Put(&Entry{
-		Key:   key,
+		Key:   coreClusterPath,
 		Value: rawCluster,
 	})
+
 }
