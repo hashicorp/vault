c26612ec8c60781c53e5f5c9aa2efb3462fdb916
Jeff Mitchell <jeffrey.mitchell@gmail.com>
2016-08-17 11:17:06 -0400

    Protobuf experiments

--- helper/requestutil/forwarded_request.go
+++ helper/requestutil/forwarded_request.go
@@ -6,9 +6,13 @@ import (
 	"crypto/x509"
 	"net/http"
 	"net/url"
+	"os"
 
+	"github.com/gogo/protobuf/proto"
 	"github.com/hashicorp/vault/helper/compressutil"
 	"github.com/hashicorp/vault/helper/jsonutil"
+
+	fr "github.com/hashicorp/vault/helper/requestutil/proto"
 )
 
 type bufCloser struct {
@@ -20,38 +24,31 @@ func (b bufCloser) Close() error {
 	return nil
 }
 
-type ForwardedRequest struct {
-	// The original method
-	Method string `json:"method"`
-
-	// The original URL object
-	URL *url.URL `json:"url"`
-
-	// The original headers
-	Header http.Header `json:"header"`
-
-	// The request body
-	Body []byte `json:"body"`
-
-	// The specified host
-	Host string `json:"host"`
-
-	// The remote address
-	RemoteAddr string `json:"remote_addr"`
-
-	// The client's TLS peer certificates
-	PeerCertificates [][]byte `json:"peer_certificates"`
-}
-
 // GenerateForwardedRequest generates a new http.Request that contains the
 // original requests's information in the new request's body.
 func GenerateForwardedRequest(req *http.Request, addr string) (*http.Request, error) {
-	fq := ForwardedRequest{
-		Method:     req.Method,
-		URL:        req.URL,
-		Header:     req.Header,
-		Host:       req.Host,
-		RemoteAddr: req.RemoteAddr,
+	fq := fr.ForwardedRequest{
+		Method:        req.Method,
+		HeaderEntries: make(map[string]*fr.HeaderEntry, len(req.Header)),
+		Host:          req.Host,
+		RemoteAddr:    req.RemoteAddr,
+	}
+
+	reqURL := req.URL
+	fq.Url = &fr.URL{
+		Scheme:   reqURL.Scheme,
+		Opaque:   reqURL.Opaque,
+		Host:     reqURL.Host,
+		Path:     reqURL.Path,
+		RawPath:  reqURL.RawPath,
+		RawQuery: reqURL.RawQuery,
+		Fragment: reqURL.Fragment,
+	}
+
+	for k, v := range req.Header {
+		fq.HeaderEntries[k] = &fr.HeaderEntry{
+			Values: v,
+		}
 	}
 
 	if req.TLS.PeerCertificates != nil && len(req.TLS.PeerCertificates) > 0 {
@@ -68,9 +65,19 @@ func GenerateForwardedRequest(req *http.Request, addr string) (*http.Request, er
 	}
 	fq.Body = buf.Bytes()
 
-	newBody, err := jsonutil.EncodeJSONAndCompress(&fq, &compressutil.CompressionConfig{
-		Type: compressutil.CompressionTypeLzw,
-	})
+	var newBody []byte
+	switch os.Getenv("VAULT_MESSAGE_TYPE") {
+	case "json":
+		newBody, err = jsonutil.EncodeJSON(&fq)
+	case "proto3":
+		newBody, err = proto.Marshal(&fq)
+	case "json_compress":
+		fallthrough
+	default:
+		newBody, err = jsonutil.EncodeJSONAndCompress(&fq, &compressutil.CompressionConfig{
+			Type: compressutil.CompressionTypeLzw,
+		})
+	}
 	if err != nil {
 		return nil, err
 	}
@@ -95,8 +102,13 @@ func ParseForwardedRequest(req *http.Request) (*http.Request, error) {
 		return nil, err
 	}
 
-	var fq ForwardedRequest
-	err = jsonutil.DecodeJSON(buf.Bytes(), &fq)
+	fq := new(fr.ForwardedRequest)
+	switch os.Getenv("VAULT_MESSAGE_TYPE") {
+	case "proto3":
+		err = proto.Unmarshal(buf.Bytes(), fq)
+	default:
+		err = jsonutil.DecodeJSON(buf.Bytes(), fq)
+	}
 	if err != nil {
 		return nil, err
 	}
@@ -109,11 +121,24 @@ func ParseForwardedRequest(req *http.Request) (*http.Request, error) {
 
 	ret := &http.Request{
 		Method:     fq.Method,
-		URL:        fq.URL,
-		Header:     fq.Header,
+		Header:     make(map[string][]string, len(fq.HeaderEntries)),
 		Body:       buf,
 		Host:       fq.Host,
 		RemoteAddr: fq.RemoteAddr,
+	}
+
+	ret.URL = &url.URL{
+		Scheme:   fq.Url.Scheme,
+		Opaque:   fq.Url.Opaque,
+		Host:     fq.Url.Host,
+		Path:     fq.Url.Path,
+		RawPath:  fq.Url.RawPath,
+		RawQuery: fq.Url.RawQuery,
+		Fragment: fq.Url.Fragment,
+	}
+
+	for k, v := range fq.HeaderEntries {
+		ret.Header[k] = v.Values
 	}
 
 	if fq.PeerCertificates != nil && len(fq.PeerCertificates) > 0 {
