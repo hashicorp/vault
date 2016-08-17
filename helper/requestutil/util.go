86ac08ba8b9c89113a80231e2e1bf1d71ac857c3
Jeff Mitchell <jeff@hashicorp.com>
2016-08-17 16:15:15 -0400

    Protobuf for forwarding (#1743)

--- helper/requestutil/util.go
+++ helper/requestutil/util.go
@@ -6,7 +6,9 @@ import (
 	"crypto/x509"
 	"net/http"
 	"net/url"
+	"os"
 
+	"github.com/golang/protobuf/proto"
 	"github.com/hashicorp/vault/helper/compressutil"
 	"github.com/hashicorp/vault/helper/jsonutil"
 )
@@ -20,41 +22,34 @@ func (b bufCloser) Close() error {
 	return nil
 }
 
-type ForwardedRequest struct {
-	// The original method
-	Method string `json:"method"`
-
-	// The original URL object
-	URL *url.URL `json:"url"`
-
-	// The original headers
-	Header http.Header `json:"header"`
-
-	// The request body
-	Body []byte `json:"body"`
-
-	// The specified host
-	Host string `json:"host"`
-
-	// The remote address
-	RemoteAddr string `json:"remote_addr"`
-
-	// The client's TLS peer certificates
-	PeerCertificates [][]byte `json:"peer_certificates"`
-}
-
 // GenerateForwardedRequest generates a new http.Request that contains the
 // original requests's information in the new request's body.
 func GenerateForwardedRequest(req *http.Request, addr string) (*http.Request, error) {
 	fq := ForwardedRequest{
-		Method:     req.Method,
-		URL:        req.URL,
-		Header:     req.Header,
-		Host:       req.Host,
-		RemoteAddr: req.RemoteAddr,
+		Method:        req.Method,
+		HeaderEntries: make(map[string]*HeaderEntry, len(req.Header)),
+		Host:          req.Host,
+		RemoteAddr:    req.RemoteAddr,
 	}
 
-	if req.TLS.PeerCertificates != nil && len(req.TLS.PeerCertificates) > 0 {
+	reqURL := req.URL
+	fq.Url = &URL{
+		Scheme:   reqURL.Scheme,
+		Opaque:   reqURL.Opaque,
+		Host:     reqURL.Host,
+		Path:     reqURL.Path,
+		RawPath:  reqURL.RawPath,
+		RawQuery: reqURL.RawQuery,
+		Fragment: reqURL.Fragment,
+	}
+
+	for k, v := range req.Header {
+		fq.HeaderEntries[k] = &HeaderEntry{
+			Values: v,
+		}
+	}
+
+	if req.TLS != nil && req.TLS.PeerCertificates != nil && len(req.TLS.PeerCertificates) > 0 {
 		fq.PeerCertificates = make([][]byte, len(req.TLS.PeerCertificates))
 		for i, cert := range req.TLS.PeerCertificates {
 			fq.PeerCertificates[i] = cert.Raw
@@ -68,9 +63,19 @@ func GenerateForwardedRequest(req *http.Request, addr string) (*http.Request, er
 	}
 	fq.Body = buf.Bytes()
 
-	newBody, err := jsonutil.EncodeJSONAndCompress(&fq, &compressutil.CompressionConfig{
-		Type: compressutil.CompressionTypeLzw,
-	})
+	var newBody []byte
+	switch os.Getenv("VAULT_MESSAGE_TYPE") {
+	case "json":
+		newBody, err = jsonutil.EncodeJSON(&fq)
+	case "json_compress":
+		newBody, err = jsonutil.EncodeJSONAndCompress(&fq, &compressutil.CompressionConfig{
+			Type: compressutil.CompressionTypeLzw,
+		})
+	case "proto3":
+		fallthrough
+	default:
+		newBody, err = proto.Marshal(&fq)
+	}
 	if err != nil {
 		return nil, err
 	}
@@ -95,8 +100,13 @@ func ParseForwardedRequest(req *http.Request) (*http.Request, error) {
 		return nil, err
 	}
 
-	var fq ForwardedRequest
-	err = jsonutil.DecodeJSON(buf.Bytes(), &fq)
+	fq := new(ForwardedRequest)
+	switch os.Getenv("VAULT_MESSAGE_TYPE") {
+	case "json", "json_compress":
+		err = jsonutil.DecodeJSON(buf.Bytes(), fq)
+	default:
+		err = proto.Unmarshal(buf.Bytes(), fq)
+	}
 	if err != nil {
 		return nil, err
 	}
@@ -109,11 +119,24 @@ func ParseForwardedRequest(req *http.Request) (*http.Request, error) {
 
 	ret := &http.Request{
 		Method:     fq.Method,
-		URL:        fq.URL,
-		Header:     fq.Header,
+		Header:     make(map[string][]string, len(fq.HeaderEntries)),
 		Body:       buf,
 		Host:       fq.Host,
 		RemoteAddr: fq.RemoteAddr,
+	}
+
+	ret.URL = &url.URL{
+		Scheme:   fq.Url.Scheme,
+		Opaque:   fq.Url.Opaque,
+		Host:     fq.Url.Host,
+		Path:     fq.Url.Path,
+		RawPath:  fq.Url.RawPath,
+		RawQuery: fq.Url.RawQuery,
+		Fragment: fq.Url.Fragment,
+	}
+
+	for k, v := range fq.HeaderEntries {
+		ret.Header[k] = v.Values
 	}
 
 	if fq.PeerCertificates != nil && len(fq.PeerCertificates) > 0 {
@@ -125,7 +148,7 @@ func ParseForwardedRequest(req *http.Request) (*http.Request, error) {
 			if err != nil {
 				return nil, err
 			}
-			req.TLS.PeerCertificates[i] = cert
+			ret.TLS.PeerCertificates[i] = cert
 		}
 	}
 
