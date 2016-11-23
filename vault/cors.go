ca2487f57a11a66933b4defe15c6e583e4caf1e1
Aaron Salvo <aaron.salvo@teamaol.com>
2016-11-23 13:48:47 -0500

    Lots of refactoring and clean up.

--- vault/cors.go
+++ vault/cors.go
@@ -7,6 +7,8 @@ import (
 	"strings"
 )
 
+var errCORSNotConfigured = errors.New("CORS is not configured")
+
 type CORSConfig struct {
 	enabled          bool
 	allowedOrigins   *regexp.Regexp
@@ -33,89 +35,71 @@ func (c *CORSConfig) Enabled() bool {
 	return c.enabled
 }
 
-func (c *CORSConfig) AddMethod(newMethod string) *[]string {
-	// Do not duplicate methods.
-	for _, method := range *c.allowedMethods {
-		if method == newMethod {
-			return c.allowedMethods
-		}
+func (c *CORSConfig) Enable(s string) error {
+	if s == "" {
+		return errors.New("regexp cannot be an empty string")
 	}
 
-	*c.allowedMethods = append(*c.allowedMethods, newMethod)
-	return c.allowedMethods
-}
-
-func (c *CORSConfig) AllowedMethods() []string {
-	return *c.allowedMethods
-}
-
-func (c *CORSConfig) ApplyHeaders(w http.ResponseWriter, r *http.Request) error {
-	// check that the origin is valid & set the header.
-	origin := r.Header.Get("Origin")
-	validOrigin, err := c.validOrigin(origin)
+	allowedOrigins, err := regexp.Compile(s)
 	if err != nil {
 		return err
 	}
-	if !validOrigin {
-		return nil
-	}
 
-	methods := strings.Join(c.AllowedMethods(), ",")
-	w.Header().Set("Allow", methods)
-
-	// add the credentials header if allowed.
-	if c.allowCredentials {
-		w.Header().Set("Access-Control-Allow-Credentials", "true")
-	}
-
-	// apply headers for preflight requests
-	if r.Method == http.MethodOptions {
-		for k, v := range *c.allowedHeaders {
-			w.Header().Set(k, v)
-		}
-	}
+	c.allowedOrigins = allowedOrigins
+	c.enabled = true
 
 	return nil
 }
 
 // Disable sets CORS to disabled and clears the allowed origins
 func (c *CORSConfig) Disable() {
-	c.enabled = false
-	c.allowedOrigins = &regexp.Regexp{}
+	c = nil
+	// c.enabled = false
+	// c.allowedOrigins = &regexp.Regexp{}
+}
+
+func (c *CORSConfig) AllowedMethods() []string {
+	return *c.allowedMethods
+}
+
+func (c *CORSConfig) ApplyHeaders(w http.ResponseWriter, r *http.Request) {
+	// check that the origin is valid & set the header.
+	origin := r.Header.Get("Origin")
+	if c.validOrigin(origin) {
+
+		methods := strings.Join(c.AllowedMethods(), ",")
+		w.Header().Set("Allow", methods)
+
+		// add the credentials header if allowed.
+		if c.allowCredentials {
+			w.Header().Set("Access-Control-Allow-Credentials", "true")
+		}
+
+		// apply headers for preflight requests
+		if r.Method == http.MethodOptions {
+			for k, v := range *c.allowedHeaders {
+				w.Header().Set(k, v)
+			}
+		}
+	}
 }
 
 func (c *CORSConfig) AllowedOrigins() *regexp.Regexp {
 	return c.allowedOrigins
 }
 
-func (c *CORSConfig) EnableCORS(s string) (*regexp.Regexp, error) {
-	if s == "" {
-		return nil, errors.New("regexp cannot be an empty string")
-	}
-
-	allowedOrigins, err := regexp.Compile(s)
-	if err != nil {
-		return nil, err
-	}
-
-	c.allowedOrigins = allowedOrigins
-	c.enabled = true
-
-	return c.allowedOrigins, nil
-}
-
-func (c *CORSConfig) validOrigin(origin string) (bool, error) {
+func (c *CORSConfig) validOrigin(origin string) bool {
 	if len(origin) == 0 {
-		return false, errors.New("origin is empty")
+		return false
 	}
 
 	if c.allowedOrigins == nil {
-		return false, errors.New("no origins are allowed")
+		return false
 	}
 
 	if c.allowedOrigins.MatchString(origin) {
-		return true, nil
+		return true
 	}
 
-	return false, nil
+	return false
 }
