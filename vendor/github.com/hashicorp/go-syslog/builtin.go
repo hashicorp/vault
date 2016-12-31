1c7f9a6954ec089765e341a3f6792afd5c892baf
Conor Mongey <mongeyc@gmail.com>
2016-12-31 16:22:25 +0000

    Update go-syslog package (#2219)

--- vendor/github.com/hashicorp/go-syslog/builtin.go
+++ vendor/github.com/hashicorp/go-syslog/builtin.go
@@ -199,7 +199,7 @@ func (n *netConn) close() error {
 // local machine using a Unix domain socket.
 func unixSyslog() (conn serverConn, err error) {
 	logTypes := []string{"unixgram", "unix"}
-	logPaths := []string{"/dev/log", "/var/run/syslog"}
+	logPaths := []string{"/dev/log", "/var/run/syslog", "/var/run/log"}
 	for _, network := range logTypes {
 		for _, path := range logPaths {
 			conn, err := net.DialTimeout(network, path, localDeadline)
