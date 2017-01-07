819f2811551deb6add57c093b067093567a3e149
Oliver Beattie <oliver@obeattie.com>
2017-01-07 17:05:07 +0000

    Add dockertest support for Cassandra (it takes a while though ⏳)

--- physical/cassandra_test.go
+++ physical/cassandra_test.go
@@ -1,25 +1,29 @@
 package physical
 
 import (
+	"net"
 	"os"
 	"reflect"
+	"strconv"
 	"testing"
+	"time"
 
+	"github.com/gocql/gocql"
 	"github.com/hashicorp/vault/helper/logformat"
 	log "github.com/mgutz/logxi/v1"
-
-	_ "github.com/lib/pq"
+	"github.com/ory-am/dockertest"
 )
 
 func TestCassandraBackend(t *testing.T) {
-	hosts := os.Getenv("CASSANDRA_HOSTS")
-	if hosts == "" {
-		t.SkipNow()
+	if testing.Short() {
+		t.Skipf("skipping in short mode")
 	}
+
+	cid, hosts := prepareCassandraTestContainer(t)
+	defer cleanupCassandraTestContainer(t, cid)
 
 	// Run vault tests
 	logger := logformat.NewVaultLogger(log.LevelTrace)
-
 	b, err := NewBackend("cassandra", logger, map[string]string{
 		"hosts": hosts,
 	})
@@ -33,7 +37,6 @@ func TestCassandraBackend(t *testing.T) {
 }
 
 func TestCassandraBackendBuckets(t *testing.T) {
-	t.Parallel()
 	expectations := map[string][]string{
 		"":          {"."},
 		"a":         {"."},
@@ -46,5 +49,61 @@ func TestCassandraBackendBuckets(t *testing.T) {
 		if !reflect.DeepEqual(actual, expected) {
 			t.Errorf("bad: %v expected: %v", actual, expected)
 		}
+	}
+}
+
+func prepareCassandraTestContainer(t *testing.T) (dockertest.ContainerID, string) {
+	if os.Getenv("CASSANDRA_HOSTS") != "" {
+		return "", os.Getenv("CASSANDRA_HOSTS")
+	}
+
+	dockertest.Pull(dockertest.CassandraImageName)
+	hosts := ""
+	cid, connErr := dockertest.ConnectToCassandra("3.9", 90, time.Second, func(connAddress string) bool {
+		host, _port, _ := net.SplitHostPort(connAddress)
+		port, _ := strconv.Atoi(_port)
+
+		cluster := gocql.NewCluster(host)
+		cluster.Port = port
+		cluster.Timeout = 5 * time.Second
+		sess, err := cluster.CreateSession()
+		if err != nil {
+			return false
+		}
+
+		// Create keyspace
+		q := sess.Query(`CREATE KEYSPACE "vault" WITH REPLICATION = { 'class' : 'SimpleStrategy', 'replication_factor' : 1 };`)
+		if err := q.Exec(); err != nil {
+			t.Fatalf("could not create cassandra keyspace: %v", err)
+		}
+
+		// Create table
+		q = sess.Query(`CREATE TABLE "vault"."entries" (
+		    bucket text,
+		    key text,
+		    value blob,
+		    PRIMARY KEY (bucket, key)
+		) WITH CLUSTERING ORDER BY (key ASC);`)
+		if err := q.Exec(); err != nil {
+			t.Fatalf("could not create cassandra table: %v", err)
+		}
+
+		hosts = connAddress
+		return true
+	})
+
+	if connErr != nil {
+		t.Fatalf("could not connect to cassandra: %v", connErr)
+	}
+
+	return cid, hosts
+}
+
+func cleanupCassandraTestContainer(t *testing.T, cid dockertest.ContainerID) {
+	if cid == "" {
+		return
+	}
+	if err := cid.KillRemove(); err != nil {
+		t.Fatal(err)
 	}
 }
