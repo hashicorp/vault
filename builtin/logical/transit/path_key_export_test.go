f247054180750f94dd1be0346c8b5818ca7a0d9f
Chris Hoffman <christopher.hoffman@gmail.com>
2017-01-15 20:20:10 -0500

    Adding version tests

--- builtin/logical/transit/path_key_export_test.go
+++ builtin/logical/transit/path_key_export_test.go
@@ -6,6 +6,103 @@ import (
 	"github.com/hashicorp/vault/logical"
 )
 
+func TestTransit_PathVersion_ExportsCorrectVersion(t *testing.T) {
+	var b *backend
+	sysView := logical.TestSystemView()
+	storage := &logical.InmemStorage{}
+
+	b = Backend(&logical.BackendConfig{
+		StorageView: storage,
+		System:      sysView,
+	})
+
+	// First create a key, v1
+	req := &logical.Request{
+		Storage:   storage,
+		Operation: logical.UpdateOperation,
+		Path:      "keys/foo",
+	}
+	req.Data = map[string]interface{}{
+		"exportable": true,
+	}
+	_, err := b.HandleRequest(req)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	req.Path = "keys/foo/rotate"
+	// v2
+	_, err = b.HandleRequest(req)
+	if err != nil {
+		t.Fatal(err)
+	}
+	// v3
+	_, err = b.HandleRequest(req)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	req = &logical.Request{
+		Storage:   storage,
+		Operation: logical.ReadOperation,
+		Path:      "export/encryption-key/foo/v1",
+	}
+	rsp, err := b.HandleRequest(req)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if _, ok := rsp.Data["version"]; !ok {
+		t.Error("no version returned from export")
+	}
+	version, ok := rsp.Data["version"].(int)
+	if !ok {
+		t.Error("could not cast to version")
+	}
+	if version != 1 {
+		t.Errorf("expected version 1, received version %d", version)
+	}
+
+	req = &logical.Request{
+		Storage:   storage,
+		Operation: logical.ReadOperation,
+		Path:      "export/encryption-key/foo/1",
+	}
+	rsp, err = b.HandleRequest(req)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if _, ok := rsp.Data["version"]; !ok {
+		t.Error("no version returned from export")
+	}
+	version, ok = rsp.Data["version"].(int)
+	if !ok {
+		t.Error("could not cast to version")
+	}
+	if version != 1 {
+		t.Errorf("expected version 1, received version %d", version)
+	}
+
+	req = &logical.Request{
+		Storage:   storage,
+		Operation: logical.ReadOperation,
+		Path:      "export/encryption-key/foo/latest",
+	}
+	rsp, err = b.HandleRequest(req)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if _, ok := rsp.Data["version"]; !ok {
+		t.Error("no version returned from export")
+	}
+	version, ok = rsp.Data["version"].(int)
+	if !ok {
+		t.Error("could not cast to version")
+	}
+	if version != 3 {
+		t.Errorf("expected version 3, received version %d", version)
+	}
+}
+
 func TestTransit_PathKeys_ExportValidVersionsOnly(t *testing.T) {
 	var b *backend
 	sysView := logical.TestSystemView()
