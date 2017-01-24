027e5466f34fbfccff273ea0177e7e38571bb6fc
Aaron Salvo <aaron.salvo@teamaol.com>
2017-01-24 10:41:35 -0500

    Merged the handleCORS() func into wrapCORSHandler(). Was not necessary to have two functions.

--- http/cors.go
+++ http/cors.go
@@ -9,30 +9,24 @@ import (
 
 func wrapCORSHandler(h http.Handler, core *vault.Core) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
-		// If the help parameter is not blank, then show the help
-		if req.Header.Get(NoCORS) == "" {
-			statusCode := handleCORS(core, w, req)
-			if statusCode != http.StatusOK {
-				respondRaw(w, req, &logical.Response{
-					Data: map[string]interface{}{
-						logical.HTTPStatusCode: statusCode,
-						logical.HTTPRawBody:    []byte(""),
-					},
-				})
-				return
-			}
+		corsConf := core.CORSConfig()
+		statusCode := corsConf.ApplyHeaders(w, req)
+		if statusCode != http.StatusOK {
+			respondRaw(w, req, &logical.Response{
+				Data: map[string]interface{}{
+					logical.HTTPStatusCode: statusCode,
+					logical.HTTPRawBody:    []byte(""),
+				},
+			})
+			return
+		}
+
+		// For pre-flight requests just send back the headers and return.
+		if req.Method == http.MethodOptions {
+			return
 		}
 
 		h.ServeHTTP(w, req)
 		return
 	})
-}
-
-// HandleCORS adds required headers to properly respond to
-// requests that require Cross Origin Resource Sharing (CORS) headers.
-func handleCORS(core *vault.Core, w http.ResponseWriter, req *http.Request) int {
-	corsConf := core.CORSConfig()
-	statusCode := corsConf.ApplyHeaders(w, req)
-
-	return statusCode
 }
