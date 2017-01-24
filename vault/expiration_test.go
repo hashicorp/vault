b3c2578b550f3e9b7edfbfc3df11094740d4dfb5
Jeff Mitchell <jeffrey.mitchell@gmail.com>
2017-01-24 10:21:41 -0500

    Use time.Now rather than using time as a struct

--- vault/expiration_test.go
+++ vault/expiration_test.go
@@ -908,6 +908,7 @@ func TestExpiration_renewAuthEntry(t *testing.T) {
 
 func TestExpiration_PersistLoadDelete(t *testing.T) {
 	exp := mockExpiration(t)
+	lastTime := time.Now()
 	le := &leaseEntry{
 		LeaseID: "foo/bar/1234",
 		Path:    "foo/bar",
@@ -919,9 +920,9 @@ func TestExpiration_PersistLoadDelete(t *testing.T) {
 				TTL: time.Minute,
 			},
 		},
-		IssueTime:       time.Now(),
-		ExpireTime:      time.Now(),
-		LastRenewalTime: time.Time{},
+		IssueTime:       lastTime,
+		ExpireTime:      lastTime,
+		LastRenewalTime: lastTime,
 	}
 	if err := exp.persistEntry(le); err != nil {
 		t.Fatalf("err: %v", err)
