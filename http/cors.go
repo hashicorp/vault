0859a858525867ae050ba74bd44d6cfea9b8c0a3
Aaron Salvo <aaron.salvo@teamaol.com>
2017-01-31 10:48:31 -0500

    Moved logic of when to apply the CORS headers here. This makes the logic easier to follow.

--- http/cors.go
+++ http/cors.go
@@ -3,21 +3,24 @@ package http
 import (
 	"net/http"
 
-	"github.com/hashicorp/vault/logical"
 	"github.com/hashicorp/vault/vault"
 )
 
 func wrapCORSHandler(h http.Handler, core *vault.Core) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
 		corsConf := core.CORSConfig()
+
+		// If CORS is not enabled or if no Origin header is present (i.e. the request
+		// is from the Vault CLI. A browser will always send an Origin header), then
+		// just return a 204.
+		if !corsConf.Enabled() || req.Header.Get("Origin") == "" {
+			h.ServeHTTP(w, req)
+			return
+		}
+
 		statusCode := corsConf.ApplyHeaders(w, req)
-		if statusCode != http.StatusOK {
-			respondRaw(w, req, &logical.Response{
-				Data: map[string]interface{}{
-					logical.HTTPStatusCode: statusCode,
-					logical.HTTPRawBody:    []byte(""),
-				},
-			})
+		if statusCode != http.StatusNoContent {
+			h.ServeHTTP(w, req)
 			return
 		}
 
