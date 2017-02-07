d8a689a5ea7250f38880339a86994ac5e44af809
Aaron Salvo <aaron.salvo@teamaol.com>
2017-02-07 13:31:47 -0500

    Exported the Enabled and AllowedOrigins fields of the CORSConfig struct for JSON encoding. Added funcs to load the existing CORS config at unseal and save the CORS config to teardown. Embeded the RWMutex in the CORSConfig struct, instead of making it a field. Refactored to support this. Renamed validOrigin func to IsValid, because it reads better. Added the IsValidMethod func to check if the verb of the HTTP request is acceptable for cross-origin requests.

--- vault/cors.go
+++ vault/cors.go
@@ -2,11 +2,13 @@ package vault
 
 import (
 	"errors"
+	"fmt"
 	"net/http"
 	"strings"
 	"sync"
 
 	"github.com/hashicorp/vault/helper/strutil"
+	"github.com/hashicorp/vault/logical"
 )
 
 var errCORSNotConfigured = errors.New("CORS is not configured")
@@ -28,77 +30,103 @@ var allowedMethods = []string{
 
 // CORSConfig stores the state of the CORS configuration.
 type CORSConfig struct {
-	isEnabled      bool
-	allowedOrigins []string
-	mutex          *sync.RWMutex
+	Enabled        bool     `json:"enabled"`
+	AllowedOrigins []string `json:"allowed_origins"`
+	sync.RWMutex
+}
+
+func (c *Core) saveCORSConfig() error {
+	view := c.systemBarrierView.SubView("config/")
+
+	entry, err := logical.StorageEntryJSON("cors", c.corsConfig)
+	if err != nil {
+		return fmt.Errorf("failed to create CORS confif entry: %v", err)
+	}
+
+	if err := view.Put(entry); err != nil {
+		return fmt.Errorf("failed to save CORS config: %v", err)
+	}
+
+	return nil
+}
+
+func (c *Core) loadCORSConfig() error {
+	view := c.systemBarrierView.SubView("config/")
+
+	// Load the config in
+	out, err := view.Get("cors")
+	if err != nil {
+		return fmt.Errorf("failed to read CORS config: %v", err)
+	}
+	if out == nil {
+		return nil
+	}
+
+	config := new(CORSConfig)
+	err = out.DecodeJSON(config)
+	if err != nil {
+		return err
+	}
+
+	c.corsConfig = config
+
+	return nil
 }
 
 // Enable takes either a '*' or a comma-seprated list of URLs that can make
 // cross-origin requests to Vault.
 func (c *CORSConfig) Enable(s string) error {
-	c.mutex.Lock()
-	defer c.mutex.Unlock()
+	c.RWMutex.Lock()
+	defer c.RWMutex.Unlock()
 
 	if strings.Contains("*", s) && len(s) > 1 {
 		return errors.New("wildcard must be the only value")
 	}
 
-	c.allowedOrigins = strings.Split(s, ",")
-	c.isEnabled = true
+	c.AllowedOrigins = strings.Split(s, ",")
+	c.Enabled = true
 
 	return nil
 }
 
 // Get returns the state of the CORS configuration.
 func (c *CORSConfig) Get() *CORSConfig {
-	c.mutex.Lock()
-	defer c.mutex.Unlock()
+	c.RWMutex.Lock()
+	defer c.RWMutex.Unlock()
 
 	return c
 }
 
-// Enabled returns the value of CORSConfig.isEnabled
-func (c *CORSConfig) Enabled() bool {
-	c.mutex.Lock()
-	defer c.mutex.Unlock()
+// IsEnabled returns the value of CORSConfig.isEnabled
+func (c *CORSConfig) IsEnabled() bool {
+	c.RWMutex.Lock()
+	defer c.RWMutex.Unlock()
 
-	return c.isEnabled
+	return c.Enabled
 }
 
 // Disable sets CORS to disabled and clears the allowed origins
 func (c *CORSConfig) Disable() {
-	c.mutex.Lock()
-	defer c.mutex.Unlock()
+	c.RWMutex.Lock()
+	defer c.RWMutex.Unlock()
 
-	c.isEnabled = false
-	c.allowedOrigins = []string{}
+	c.Enabled = false
+	c.AllowedOrigins = []string{}
 }
 
 // ApplyHeaders examines the CORS configuration and the request to determine
 // if the CORS headers should be returned with the response.
 func (c *CORSConfig) ApplyHeaders(w http.ResponseWriter, r *http.Request) int {
-	c.mutex.Lock()
-	defer c.mutex.Unlock()
+	c.RWMutex.Lock()
+	defer c.RWMutex.Unlock()
 
 	origin := r.Header.Get("Origin")
-
-	// Return a 403 if the origin is not
-	// allowed to make cross-origin requests.
-	if !c.validOrigin(origin) {
-		return http.StatusForbidden
-	}
 
 	w.Header().Set("Access-Control-Allow-Origin", origin)
 	w.Header().Set("Vary", "Origin")
 
 	// apply headers for preflight requests
 	if r.Method == http.MethodOptions {
-		requestedMethod := r.Header.Get("Access-Control-Request-Method")
-
-		if !strutil.StrListContains(allowedMethods, requestedMethod) {
-			return http.StatusMethodNotAllowed
-		}
-
 		w.Header().Set("Access-Control-Allow-Methods", strings.Join(allowedMethods, ","))
 
 		for k, v := range preflightHeaders {
@@ -109,14 +137,25 @@ func (c *CORSConfig) ApplyHeaders(w http.ResponseWriter, r *http.Request) int {
 	return http.StatusNoContent
 }
 
-func (c *CORSConfig) validOrigin(origin string) bool {
-	if c.allowedOrigins == nil {
+// IsValidOrigin determines if the origin of the request is allowed to make
+// cross-origin requests based on the CORSConfig.
+func (c *CORSConfig) IsValidOrigin(origin string) bool {
+	if c.AllowedOrigins == nil {
 		return false
 	}
 
-	if len(c.allowedOrigins) == 1 && (c.allowedOrigins)[0] == "*" {
+	if len(c.AllowedOrigins) == 1 && (c.AllowedOrigins)[0] == "*" {
 		return true
 	}
 
-	return strutil.StrListContains(c.allowedOrigins, origin)
+	return strutil.StrListContains(c.AllowedOrigins, origin)
+}
+
+// IsValidMethod determines if the verb of the HTTP request is allowed.
+func (c *CORSConfig) IsValidMethod(method string) bool {
+	if method == "" {
+		return false
+	}
+
+	return strutil.StrListContains(allowedMethods, method)
 }
