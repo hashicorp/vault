6bc49a53f03ab3bff807c17383e7796b801c64ce
Aaron Salvo <aaron.salvo@teamaol.com>
2017-02-07 13:28:44 -0500

    Moved logic to determine when to apply the CORS headers to the http handler. This was a more logical place to put it.

--- http/cors.go
+++ http/cors.go
@@ -10,24 +10,30 @@ func wrapCORSHandler(h http.Handler, core *vault.Core) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
 		corsConf := core.CORSConfig()
 
+		origin := req.Header.Get("Origin")
+		requestMethod := req.Header.Get("Access-Control-Request-Method")
+
 		// If CORS is not enabled or if no Origin header is present (i.e. the request
 		// is from the Vault CLI. A browser will always send an Origin header), then
 		// just return a 204.
-		if !corsConf.Enabled() || req.Header.Get("Origin") == "" {
+		if !corsConf.IsEnabled() || origin == "" {
 			h.ServeHTTP(w, req)
 			return
 		}
 
-		statusCode := corsConf.ApplyHeaders(w, req)
-		if statusCode != http.StatusNoContent {
-			h.ServeHTTP(w, req)
+		// Return a 403 if the origin is not
+		// allowed to make cross-origin requests.
+		if !corsConf.IsValidOrigin(origin) {
+			w.WriteHeader(http.StatusForbidden)
 			return
 		}
 
-		// For pre-flight requests just send back the headers and return.
-		if req.Method == http.MethodOptions {
+		if req.Method == http.MethodOptions && !corsConf.IsValidMethod(requestMethod) {
+			w.WriteHeader(http.StatusMethodNotAllowed)
 			return
 		}
+
+		corsConf.ApplyHeaders(w, req)
 
 		h.ServeHTTP(w, req)
 		return
