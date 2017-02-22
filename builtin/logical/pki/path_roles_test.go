2e220f6d37dab5521c72a959845d83972fb7da5c
vishalnayak <vishalnayakv@gmail.com>
2017-02-22 18:33:58 -0500

    pki: add tests for leased and non-leased certs

--- builtin/logical/pki/path_roles_test.go
+++ builtin/logical/pki/path_roles_test.go
@@ -32,7 +32,6 @@ func TestPki_RoleGenerateLease(t *testing.T) {
 	roleData := map[string]interface{}{
 		"allowed_domains": "myvault.com",
 		"ttl":             "5h",
-		"ip_sans":         "127.0.0.1",
 	}
 
 	roleReq := &logical.Request{
@@ -98,5 +97,110 @@ func TestPki_RoleGenerateLease(t *testing.T) {
 	// Upgrade should set generate_lease to `true`
 	if !*generateLease {
 		t.Fatalf("generate_lease should be set after an upgrade")
+	}
+
+	// Make sure that setting generate_lease to `true` works properly
+	roleReq.Operation = logical.UpdateOperation
+	roleReq.Path = "roles/testrole2"
+	roleReq.Data["generate_lease"] = true
+
+	resp, err = b.HandleRequest(roleReq)
+	if err != nil || (resp != nil && resp.IsError()) {
+		t.Fatalf("bad: err: %v resp: %#v", err, resp)
+	}
+
+	roleReq.Operation = logical.ReadOperation
+	resp, err = b.HandleRequest(roleReq)
+	if err != nil || (resp != nil && resp.IsError()) {
+		t.Fatalf("bad: err: %v resp: %#v", err, resp)
+	}
+
+	generateLease = resp.Data["generate_lease"].(*bool)
+	if generateLease == nil {
+		t.Fatalf("generate_lease should not be nil")
+	}
+	if !*generateLease {
+		t.Fatalf("generate_lease should have been set")
+	}
+}
+
+func TestPki_CertsLease(t *testing.T) {
+	var resp *logical.Response
+	var err error
+	b, storage := createBackendWithStorage(t)
+
+	caData := map[string]interface{}{
+		"common_name": "myvault.com",
+		"ttl":         "5h",
+		"ip_sans":     "127.0.0.1",
+	}
+
+	caReq := &logical.Request{
+		Operation: logical.UpdateOperation,
+		Path:      "root/generate/internal",
+		Storage:   storage,
+		Data:      caData,
+	}
+
+	resp, err = b.HandleRequest(caReq)
+	if err != nil || (resp != nil && resp.IsError()) {
+		t.Fatalf("bad: err: %v resp: %#v", err, resp)
+	}
+
+	roleData := map[string]interface{}{
+		"allowed_domains":  "myvault.com",
+		"allow_subdomains": true,
+		"ttl":              "2h",
+	}
+
+	roleReq := &logical.Request{
+		Operation: logical.UpdateOperation,
+		Path:      "roles/testrole",
+		Storage:   storage,
+		Data:      roleData,
+	}
+
+	resp, err = b.HandleRequest(roleReq)
+	if err != nil || (resp != nil && resp.IsError()) {
+		t.Fatalf("bad: err: %v resp: %#v", err, resp)
+	}
+
+	issueData := map[string]interface{}{
+		"common_name": "cert.myvault.com",
+		"format":      "pem",
+		"ip_sans":     "127.0.0.1",
+	}
+	issueReq := &logical.Request{
+		Operation: logical.UpdateOperation,
+		Path:      "issue/testrole",
+		Storage:   storage,
+		Data:      issueData,
+	}
+
+	resp, err = b.HandleRequest(issueReq)
+	if err != nil || (resp != nil && resp.IsError()) {
+		t.Fatalf("bad: err: %v resp: %#v", err, resp)
+	}
+
+	if resp.Secret != nil {
+		t.Fatalf("expected a response that does not contain a secret")
+	}
+
+	// Turn on the lease generation and issue a certificate. The response
+	// should have a `Secret` object populated.
+	roleData["generate_lease"] = true
+
+	resp, err = b.HandleRequest(roleReq)
+	if err != nil || (resp != nil && resp.IsError()) {
+		t.Fatalf("bad: err: %v resp: %#v", err, resp)
+	}
+
+	resp, err = b.HandleRequest(issueReq)
+	if err != nil || (resp != nil && resp.IsError()) {
+		t.Fatalf("bad: err: %v resp: %#v", err, resp)
+	}
+
+	if resp.Secret == nil {
+		t.Fatalf("expected a response that contains a secret")
 	}
 }
