2a5a5ef79bd5908e30a69d3d7fd420b14d12d776
Aaron Salvo <aaron.salvo@teamaol.com>
2017-03-01 17:25:40 -0500

    Removed ApplyHeaders and Get(). Applied correct locks where needed. Fixed typos, and put the embedded Mutex struct as the first field in the CORSConfig struct for the sake of readability.

--- vault/cors.go
+++ vault/cors.go
@@ -3,7 +3,6 @@ package vault
 import (
 	"errors"
 	"fmt"
-	"net/http"
 	"strings"
 	"sync"
 
@@ -13,26 +12,11 @@ import (
 
 var errCORSNotConfigured = errors.New("CORS is not configured")
 
-var preflightHeaders = map[string]string{
-	"Access-Control-Allow-Headers":     "*",
-	"Access-Control-Max-Age":           "1800",
-	"Access-Control-Allow-Credentials": "true",
-}
-
-var allowedMethods = []string{
-	http.MethodDelete,
-	http.MethodGet,
-	http.MethodOptions,
-	http.MethodPost,
-	http.MethodPut,
-	"LIST", // LIST is not an official HTTP method, but Vault supports it.
-}
-
 // CORSConfig stores the state of the CORS configuration.
 type CORSConfig struct {
+	sync.RWMutex
 	Enabled        bool     `json:"enabled"`
 	AllowedOrigins []string `json:"allowed_origins"`
-	sync.RWMutex
 }
 
 func (c *Core) saveCORSConfig() error {
@@ -40,7 +24,7 @@ func (c *Core) saveCORSConfig() error {
 
 	entry, err := logical.StorageEntryJSON("cors", c.corsConfig)
 	if err != nil {
-		return fmt.Errorf("failed to create CORS confif entry: %v", err)
+		return fmt.Errorf("failed to create CORS config entry: %v", err)
 	}
 
 	if err := view.Put(entry); err != nil {
@@ -75,71 +59,44 @@ func (c *Core) loadCORSConfig() error {
 
 // Enable takes either a '*' or a comma-seprated list of URLs that can make
 // cross-origin requests to Vault.
-func (c *CORSConfig) Enable(s string) error {
-	c.RWMutex.Lock()
-	defer c.RWMutex.Unlock()
+func (c *CORSConfig) Enable(urls string) error {
 
-	if strings.Contains("*", s) && len(s) > 1 {
+	if strings.Contains("*", urls) && len(urls) > 1 {
 		return errors.New("wildcard must be the only value")
 	}
 
-	c.AllowedOrigins = strings.Split(s, ",")
+	c.Lock()
+	defer c.Unlock()
+
+	c.AllowedOrigins = strings.Split(urls, ",")
 	c.Enabled = true
 
 	return nil
 }
 
-// Get returns the state of the CORS configuration.
-func (c *CORSConfig) Get() *CORSConfig {
-	c.RWMutex.Lock()
-	defer c.RWMutex.Unlock()
-
-	return c
-}
-
 // IsEnabled returns the value of CORSConfig.isEnabled
 func (c *CORSConfig) IsEnabled() bool {
-	c.RWMutex.Lock()
-	defer c.RWMutex.Unlock()
+	c.RLock()
+	defer c.RUnlock()
 
 	return c.Enabled
 }
 
 // Disable sets CORS to disabled and clears the allowed origins
 func (c *CORSConfig) Disable() {
-	c.RWMutex.Lock()
-	defer c.RWMutex.Unlock()
+	c.Lock()
+	defer c.Unlock()
 
 	c.Enabled = false
 	c.AllowedOrigins = []string{}
 }
 
-// ApplyHeaders examines the CORS configuration and the request to determine
-// if the CORS headers should be returned with the response.
-func (c *CORSConfig) ApplyHeaders(w http.ResponseWriter, r *http.Request) int {
-	c.RWMutex.Lock()
-	defer c.RWMutex.Unlock()
-
-	origin := r.Header.Get("Origin")
-
-	w.Header().Set("Access-Control-Allow-Origin", origin)
-	w.Header().Set("Vary", "Origin")
-
-	// apply headers for preflight requests
-	if r.Method == http.MethodOptions {
-		w.Header().Set("Access-Control-Allow-Methods", strings.Join(allowedMethods, ","))
-
-		for k, v := range preflightHeaders {
-			w.Header().Set(k, v)
-		}
-	}
-
-	return http.StatusNoContent
-}
-
 // IsValidOrigin determines if the origin of the request is allowed to make
 // cross-origin requests based on the CORSConfig.
 func (c *CORSConfig) IsValidOrigin(origin string) bool {
+	c.RLock()
+	defer c.RUnlock()
+
 	if c.AllowedOrigins == nil {
 		return false
 	}
@@ -149,13 +106,4 @@ func (c *CORSConfig) IsValidOrigin(origin string) bool {
 	}
 
 	return strutil.StrListContains(c.AllowedOrigins, origin)
-}
-
-// IsValidMethod determines if the verb of the HTTP request is allowed.
-func (c *CORSConfig) IsValidMethod(method string) bool {
-	if method == "" {
-		return false
-	}
-
-	return strutil.StrListContains(allowedMethods, method)
 }
