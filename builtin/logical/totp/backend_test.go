1a7fe62aba37d8539dee6de6c58dd4853e8af3fa
mymercurialsky <michael.gonsalves84@gmail.com>
2017-03-10 11:48:39 -0800

    Modified read credential test step, not working yet

--- builtin/logical/totp/backend_test.go
+++ builtin/logical/totp/backend_test.go
@@ -5,10 +5,12 @@ import (
 	"log"
 	"path"
 	"testing"
+	"time"
 
 	"github.com/hashicorp/vault/logical"
 	logicaltest "github.com/hashicorp/vault/logical/testing"
 	"github.com/mitchellh/mapstructure"
+	//otplib "github.com/pquerna/otp/"
 	totplib "github.com/pquerna/otp/totp"
 )
 
@@ -43,7 +45,7 @@ func TestBackend_basic(t *testing.T) {
 	}
 
 	// Generate a new shared key
-	key := createKey()
+	key, _ := createKey()
 
 	roleData := map[string]interface{}{
 		"issuer":       "Vault",
@@ -55,12 +57,12 @@ func TestBackend_basic(t *testing.T) {
 		Backend: b,
 		Steps: []logicaltest.TestStep{
 			testAccStepCreateRole(t, "test", roleData, false),
-			testAccStepReadCreds(t, b, config.StorageView, "test", key),
+			testAccStepReadCreds(t, b, config.StorageView, "test"),
 		},
 	})
 }
 
-func TestBackend_roleCrud(t *testing.T) {
+func TestBackend_roleCrudDefaultValues(t *testing.T) {
 	config := logical.TestBackendConfig()
 	config.StorageView = &logical.InmemStorage{}
 	b, err := Factory(config)
@@ -68,7 +70,7 @@ func TestBackend_roleCrud(t *testing.T) {
 		t.Fatal(err)
 	}
 
-	key := createKey()
+	key, _ := createKey()
 
 	roleData := map[string]interface{}{
 		"issuer":       "Vault",
@@ -90,16 +92,16 @@ func TestBackend_roleCrud(t *testing.T) {
 			testAccStepCreateRole(t, "test", roleData, false),
 			testAccStepReadRole(t, "test", expected),
 			testAccStepDeleteRole(t, "test"),
-			testAccStepReadRole(t, "test", ""),
+			testAccStepReadRole(t, "test", nil),
 		},
 	})
 }
 
-func testAccStepCreateRole(t *testing.T, name string, data map[string]interface{}, expectFail bool) logicaltest.TestStep {
+func testAccStepCreateRole(t *testing.T, name string, roleData map[string]interface{}, expectFail bool) logicaltest.TestStep {
 	return logicaltest.TestStep{
 		Operation: logical.UpdateOperation,
 		Path:      path.Join("roles", name),
-		Data:      data,
+		Data:      roleData,
 		ErrorOk:   expectFail,
 	}
 }
@@ -111,7 +113,7 @@ func testAccStepDeleteRole(t *testing.T, name string) logicaltest.TestStep {
 	}
 }
 
-func testAccStepReadCreds(t *testing.T, b logical.Backend, s logical.Storage, name string, key string) logicaltest.TestStep {
+func testAccStepReadCreds(t *testing.T, b logical.Backend, s logical.Storage, name string) logicaltest.TestStep {
 	return logicaltest.TestStep{
 		Operation: logical.ReadOperation,
 		Path:      path.Join("creds", name),
@@ -124,7 +126,47 @@ func testAccStepReadCreds(t *testing.T, b logical.Backend, s logical.Storage, na
 			}
 			log.Printf("[TRACE] Generated credentials: %v", d)
 
-			valid := totplib.Validate(d.Token, key)
+			role, err := (backend) b.Role(s, name)
+
+			if err != nil {
+				t.Fatalf("Error retrieving role.")
+			}
+
+			if role == nil {
+				t.Fatalf("Retrieved role is nil.")
+			}
+
+			// Translate digits and algorithm to a format the totp library understands
+			var digits otplib.Digits
+			switch role.Digits {
+			case 6:
+				digits = otplib.DigitsSix
+			case 8:
+				digits = otplib.DigitsEight
+			}
+
+			var algorithm otplib.Algorithm
+			switch role.Algorithm {
+			case "SHA1":
+				algorithm = otplib.AlgorithmSHA1
+			case "SHA256":
+				algorithm = otplib.AlgorithmSHA256
+			case "SHA512":
+				algorithm = otplib.AlgorithmSHA512
+			case "MD5":
+				algorithm = otplib.AlgorithmMD5
+			default:
+				algorithm = otplib.AlgorithmSHA1
+			}
+
+			period := uint(role.Period)
+
+			valid := totplib.ValidateCustom(d.Token, role.Key, time.Now().UTC(), totplib.ValidateOpts{
+				Period:    period,
+				Skew:      1,
+				Digits:    digits,
+				Algorithm: algorithm,
+			})
 
 			if !valid {
 				t.Fatalf("Generated token isn't valid.")
@@ -160,16 +202,16 @@ func testAccStepReadRole(t *testing.T, name string, expected map[string]interfac
 			}
 
 			switch {
-			case d.Issuer != expected.Get("issuer"):
-				return fmt.Errorf("Issuer should equal: %s", expected.Get("issuer"))
-			case d.Account_Name != expected.Get("account_name"):
-				return fmt.Errorf("Account_Name should equal: %s", expected.Get("account_name"))
-			case d.Period != expected.Get("period"):
-				return fmt.Errorf("Period should equal: %i", expected.Get("period"))
-			case d.Algorithm != expected.Get("algorithm"):
-				return fmt.Errorf("Algorithm should equal: %s", expected.Get("algorithm"))
-			case d.Digits != expected.Get("digits"):
-				return fmt.Errorf("Digits should equal: %i", expected.Get("digits"))
+			case d.Issuer != expected["issuer"]:
+				return fmt.Errorf("Issuer should equal: %s", expected["issuer"])
+			case d.Account_Name != expected["account_name"]:
+				return fmt.Errorf("Account_Name should equal: %s", expected["account_name"])
+			case d.Period != expected["period"]:
+				return fmt.Errorf("Period should equal: %i", expected["period"])
+			case d.Algorithm != expected["algorithm"]:
+				return fmt.Errorf("Algorithm should equal: %s", expected["algorithm"])
+			case d.Digits != expected["digits"]:
+				return fmt.Errorf("Digits should equal: %i", expected["digits"])
 			}
 
 			return nil
