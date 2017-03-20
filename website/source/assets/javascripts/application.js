d66b53e79e78f52eda8a582f5e9d07e778ef1cda
Seth Vargo <sethvargo@gmail.com>
2017-03-20 01:25:42 -0400

    Remove unused javascript
    It looks like these came over from Nomad(?), but we do not use them
    anywhere. This saves about 4kb on the compressed javascript, so it's a
    big savings. Also, it causes namespace conflicts.

--- website/source/assets/javascripts/application.js
+++ website/source/assets/javascripts/application.js
@@ -1,16 +1,7 @@
 //= require turbolinks
 //= require jquery
 //= require bootstrap
-//= require jquery.waypoints
-
-//= require lib/String.substitute
-//= require lib/Function.prototype.bind
-//= require lib/Base
-//= require lib/Chainable
-//= require lib/dbg
 
 //= require hashicorp/mega-nav
 
 //= require app/Sidebar
-//= require app/DotLockup
-//= require app/Init
