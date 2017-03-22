175a9a31e30b4a463dc1f4cc0e1bf5114fd003ae
James Hall <james.hall@infinityworks.com>
2017-03-22 19:11:35 +0000

    fix command/generate-share_test

--- command/generate-share_test.go
+++ command/generate-share_test.go
@@ -1,6 +1,7 @@
 package command
 
 import (
+	"encoding/base64"
 	"encoding/hex"
 	"os"
 	"strings"
@@ -8,7 +9,6 @@ import (
 
 	"github.com/hashicorp/vault/helper/pgpkeys"
 	"github.com/hashicorp/vault/http"
-	"github.com/hashicorp/vault/logical"
 	"github.com/hashicorp/vault/meta"
 	"github.com/hashicorp/vault/vault"
 	"github.com/mitchellh/cli"
@@ -95,7 +95,7 @@ func TestGenerateShare_status(t *testing.T) {
 }
 
 func TestGenerateShare_PGP(t *testing.T) {
-	core, ts, keys, _ := vault.TestCoreWithTokenStore(t)
+	core, keys, token := vault.TestCoreUnsealed(t)
 	ln, addr := http.TestServer(t, core)
 	defer ln.Close()
 
@@ -139,6 +139,7 @@ func TestGenerateShare_PGP(t *testing.T) {
 		}
 	}
 
+	// Get new share
 	beforeNAfter := strings.Split(ui.OutputWriter.String(), "Share: ")
 	if len(beforeNAfter) != 2 {
 		t.Fatalf("did not find share in %s", ui.OutputWriter.String())
@@ -153,28 +154,51 @@ func TestGenerateShare_PGP(t *testing.T) {
 		t.Fatal("returned plain text buffer is nil")
 	}
 
-	share := ptBuf.String()
+	newShare := ptBuf.String()
+	newShareBytes, err := base64.StdEncoding.DecodeString(newShare)
 
-	req := logical.TestRequest(t, logical.ReadOperation, "lookup-self")
-	req.ClientToken = share
+	keys[0] = newShareBytes
 
-	resp, err := ts.HandleRequest(req)
+	// Seal the vault
+	sc := &SealCommand{
+		Meta: meta.Meta{
+			ClientToken: token,
+			Ui:          ui,
+		},
+	}
+	args = []string{"-address", addr}
+	if code := sc.Run(args); code != 0 {
+		t.Fatalf("bad: %d\n\n%s", code, ui.ErrorWriter.String())
+	}
+
+	sealed, err := core.Sealed()
 	if err != nil {
-		t.Fatalf("error running share lookup-self: %v", err)
+		t.Fatalf("err: %s", err)
 	}
-	if resp == nil {
-		t.Fatalf("got nil resp with share lookup-self")
-	}
-	if resp.Data == nil {
-		t.Fatalf("got nil resp.Data with share lookup-self")
+	if !sealed {
+		t.Fatal("should be sealed")
 	}
 
-	if resp.Data["orphan"].(bool) != true ||
-		resp.Data["ttl"].(int64) != 0 ||
-		resp.Data["num_uses"].(int) != 0 ||
-		resp.Data["meta"].(map[string]string) != nil ||
-		len(resp.Data["policies"].([]string)) != 1 ||
-		resp.Data["policies"].([]string)[0] != "root" {
-		t.Fatalf("bad: %#v", resp.Data)
+	// Unseal using new share
+	for _, key := range keys {
+		c := &UnsealCommand{
+			Key: hex.EncodeToString(key),
+			Meta: meta.Meta{
+				Ui: ui,
+			},
+		}
+
+		args := []string{"-address", addr}
+		if code := c.Run(args); code != 0 {
+			t.Fatalf("bad: %d\n\n%s", code, ui.ErrorWriter.String())
+		}
+	}
+
+	sealed, err = core.Sealed()
+	if err != nil {
+		t.Fatalf("err: %s", err)
+	}
+	if sealed {
+		t.Fatal("should not be sealed")
 	}
 }
