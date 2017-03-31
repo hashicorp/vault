ccff704acf9745218604ce928abd37a22fda8bd2
Calvin Leung Huang <cleung2010@gmail.com>
2017-03-31 15:59:42 -0700

    WIP on mssql secret backend refactor

--- builtin/logical/database/dbs/connectionproducer.go
+++ builtin/logical/database/dbs/connectionproducer.go
@@ -10,6 +10,7 @@ import (
 	"time"
 
 	// Import sql drivers
+	_ "github.com/denisenkom/go-mssqldb"
 	_ "github.com/go-sql-driver/mysql"
 	_ "github.com/lib/pq"
 	"github.com/mitchellh/mapstructure"
