6b55ab5db0b3e07b49c5aefe6d98242ae7c3c48d
Chris Hoffman <christopher.hoffman@gmail.com>
2017-04-18 17:49:59 -0400

    Mssql driver update (#2610)
    * Switching driver from mssql to sqlserver
    * Adding explicit database to sp_msloginmappings call

--- builtin/logical/mssql/secret_creds.go
+++ builtin/logical/mssql/secret_creds.go
@@ -102,7 +102,7 @@ func (b *backend) secretCredsRevoke(
 	// we need to drop the database users before we can drop the login and the role
 	// This isn't done in a transaction because even if we fail along the way,
 	// we want to remove as much access as possible
-	stmt, err := db.Prepare(fmt.Sprintf("EXEC sp_msloginmappings '%s';", username))
+	stmt, err := db.Prepare(fmt.Sprintf("EXEC master.dbo.sp_msloginmappings '%s';", username))
 	if err != nil {
 		return nil, err
 	}
