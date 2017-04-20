754c8ddcb2eeb3fe813d5b17be8084a85206f652
Chris Hoffman <choffman@hashicorp.com>
2017-04-20 11:46:03 -0400

    Only generate xCoordinates for the number of parts

--- shamir/shamir.go
+++ shamir/shamir.go
@@ -168,7 +168,7 @@ func Split(secret []byte, parts, threshold int) ([][]byte, error) {
 	}
 
 	// Generate random list of x coordinates
-	xCoordinates := mathrand.Perm(255)
+	xCoordinates := mathrand.Perm(parts)
 
 	// Allocate the output array, initialize the final byte
 	// of the output with the offset. The representation of each
