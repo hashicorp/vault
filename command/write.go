6972a33c4f5ffe00ee869e224f5dc100935f1515
Jeff Mitchell <jeffrey.mitchell@gmail.com>
2017-05-04 06:40:11 -0400

    Write always needs a path, even with force.
    Fixes #2674

--- command/write.go
+++ command/write.go
@@ -32,6 +32,12 @@ func (c *WriteCommand) Run(args []string) int {
 	}
 
 	args = flags.Args()
+	if len(args) < 1 {
+		c.Ui.Error("write requires a path")
+		flags.Usage()
+		return 1
+	}
+
 	if len(args) < 2 && !force {
 		c.Ui.Error("write expects at least two arguments; use -f to perform the write anyways")
 		flags.Usage()
