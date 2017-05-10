4fbeadb562f9c3ec19a95ca66372a583b1b4cd32
Aaron Salvo <aaron.salvo@teamaol.com>
2017-05-10 09:42:58 -0400

    Added Access-Control-Max-Age header to list of expected headers.

--- http/handler_test.go
+++ http/handler_test.go
@@ -79,6 +79,7 @@ func TestHandler_cors(t *testing.T) {
 	expHeaders := map[string]string{
 		"Access-Control-Allow-Origin":  addr,
 		"Access-Control-Allow-Headers": "*",
+		"Access-Control-Max-Age":       "300",
 		"Vary": "Origin",
 	}
 
