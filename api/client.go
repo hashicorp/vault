638ef2c9b8e30549f5778cf8eb464dec75648ad0
Gobin Sougrakpam <gobins@users.noreply.github.com>
2017-07-18 23:48:31 +1000

    Adding option to set custom vault client timeout using env variable VAULT_CLIENT_TIMEOUT (#3022)

--- api/client.go
+++ api/client.go
@@ -16,6 +16,7 @@ import (
 
 	"github.com/hashicorp/go-cleanhttp"
 	"github.com/hashicorp/go-rootcerts"
+	"github.com/hashicorp/vault/helper/parseutil"
 	"github.com/sethgrid/pester"
 )
 
@@ -24,6 +25,7 @@ const EnvVaultCACert = "VAULT_CACERT"
 const EnvVaultCAPath = "VAULT_CAPATH"
 const EnvVaultClientCert = "VAULT_CLIENT_CERT"
 const EnvVaultClientKey = "VAULT_CLIENT_KEY"
+const EnvVaultClientTimeout = "VAULT_CLIENT_TIMEOUT"
 const EnvVaultInsecure = "VAULT_SKIP_VERIFY"
 const EnvVaultTLSServerName = "VAULT_TLS_SERVER_NAME"
 const EnvVaultWrapTTL = "VAULT_WRAP_TTL"
@@ -54,6 +56,9 @@ type Config struct {
 	// MaxRetries controls the maximum number of times to retry when a 5xx error
 	// occurs. Set to 0 or less to disable retrying. Defaults to 0.
 	MaxRetries int
+
+	// Timeout is for setting custom timeout parameter in the HttpClient
+	Timeout time.Duration
 }
 
 // TLSConfig contains the parameters needed to configure TLS on the HTTP client
@@ -156,6 +161,7 @@ func (c *Config) ReadEnvironment() error {
 	var envCAPath string
 	var envClientCert string
 	var envClientKey string
+	var envClientTimeout time.Duration
 	var envInsecure bool
 	var envTLSServerName string
 	var envMaxRetries *uint64
@@ -182,6 +188,13 @@ func (c *Config) ReadEnvironment() error {
 	}
 	if v := os.Getenv(EnvVaultClientKey); v != "" {
 		envClientKey = v
+	}
+	if t := os.Getenv(EnvVaultClientTimeout); t != "" {
+		clientTimeout, err := parseutil.ParseDurationSecond(t)
+		if err != nil {
+			return fmt.Errorf("Could not parse %s", EnvVaultClientTimeout)
+		}
+		envClientTimeout = clientTimeout
 	}
 	if v := os.Getenv(EnvVaultInsecure); v != "" {
 		var err error
@@ -213,6 +226,10 @@ func (c *Config) ReadEnvironment() error {
 
 	if envMaxRetries != nil {
 		c.MaxRetries = int(*envMaxRetries) + 1
+	}
+
+	if envClientTimeout != 0 {
+		c.Timeout = envClientTimeout
 	}
 
 	return nil
@@ -304,6 +321,11 @@ func (c *Client) SetMaxRetries(retries int) {
 	c.config.MaxRetries = retries
 }
 
+// SetClientTimeout sets the client request timeout
+func (c *Client) SetClientTimeout(timeout time.Duration) {
+	c.config.Timeout = timeout
+}
+
 // SetWrappingLookupFunc sets a lookup function that returns desired wrap TTLs
 // for a given operation and path
 func (c *Client) SetWrappingLookupFunc(lookupFunc WrappingLookupFunc) {
@@ -361,6 +383,9 @@ func (c *Client) NewRequest(method, requestPath string) *Request {
 		req.WrapTTL = c.wrappingLookupFunc(method, lookupPath)
 	} else {
 		req.WrapTTL = DefaultWrappingLookupFunc(method, lookupPath)
+	}
+	if c.config.Timeout != 0 {
+		c.config.HttpClient.Timeout = c.config.Timeout
 	}
 
 	return req
