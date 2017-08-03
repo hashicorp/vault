5b717e3506855fbbca7603fa22032df980b95320
Calvin Leung Huang <cleung2010@gmail.com>
2017-08-03 00:09:14 -0400

    Revert back to using hyphens, update cassandra db plugin to perform conversion

--- plugins/helper/database/credsutil/credsutil.go
+++ plugins/helper/database/credsutil/credsutil.go
@@ -19,11 +19,11 @@ type CredentialsProducer interface {
 }
 
 const (
-	reqStr    = `A1a_`
+	reqStr    = `A1a-`
 	minStrLen = 10
 )
 
-// RandomAlphaNumeric returns a random string of characters [A-Za-z0-9_]
+// RandomAlphaNumeric returns a random string of characters [A-Za-z0-9-]
 // of the provided length. The string generated takes up to 4 characters
 // of space that are predefined and prepended to ensure password
 // character requirements. It also requires a min length of 10 characters.
