094e2a87a5cc208e2a91c2e24b0389067637bf67
Aaron Salvo <aaron.salvo@teamaol.com>
2017-08-06 13:58:56 -0400

    An origin to allow must be required when calling Enable.

--- vault/cors.go
+++ vault/cors.go
@@ -85,6 +85,10 @@ func (c *Core) loadCORSConfig() error {
 // Enable takes either a '*' or a comma-seprated list of URLs that can make
 // cross-origin requests to Vault.
 func (c *CORSConfig) Enable(urls []string, headers []string) error {
+	if len(urls) == 0 {
+		return errors.New("at least one origin or the wildcard must be provided.")
+	}
+
 	if strutil.StrListContains(urls, "*") && len(urls) > 1 {
 		return errors.New("to allow all origins the '*' must be the only value for allowed_origins")
 	}
