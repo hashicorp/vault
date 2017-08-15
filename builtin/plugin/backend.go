e8fe99991b4a90672d41d0746bda2781b80af94f
Calvin Leung Huang <cleung2010@gmail.com>
2017-08-15 09:21:00 -0400

    Refactor builtin/backend

--- builtin/plugin/backend.go
+++ builtin/plugin/backend.go
@@ -2,6 +2,8 @@ package plugin
 
 import (
 	"fmt"
+	"net/rpc"
+	"sync"
 
 	"github.com/hashicorp/vault/logical"
 	bplugin "github.com/hashicorp/vault/logical/plugin"
@@ -27,13 +29,76 @@ func Factory(conf *logical.BackendConfig) (logical.Backend, error) {
 // Backend returns an instance of the backend, either as a plugin if external
 // or as a concrete implementation if builtin, casted as logical.Backend.
 func Backend(conf *logical.BackendConfig) (logical.Backend, error) {
+	var b backend
 	name := conf.Config["plugin_name"]
 	sys := conf.System
 
-	b, err := bplugin.NewBackend(name, sys)
+	raw, err := bplugin.NewBackend(name, sys)
 	if err != nil {
 		return nil, err
 	}
+	b.Backend = raw
+	b.config = conf
 
-	return b, nil
+	return &b, nil
+}
+
+// backend is a thin wrapper around plugin.BackendPluginClient
+type backend struct {
+	logical.Backend
+	sync.Mutex
+
+	config *logical.BackendConfig
+}
+
+func (b *backend) reloadBackend() error {
+	b.Lock()
+	defer b.Unlock()
+
+	pluginName := b.config.Config["plugin_name"]
+	b.Logger().Trace("plugin: reloading plugin backend", "plugin", pluginName)
+
+	nb, err := bplugin.NewBackend(pluginName, b.config.System)
+	if err != nil {
+		return err
+	}
+	err = nb.Setup(b.config)
+	if err != nil {
+		return err
+	}
+	b.Backend = nb
+
+	return nil
+}
+
+// HandleRequest is a thin wrapper implementation of HandleRequest that includes automatic plugin reload.
+func (b *backend) HandleRequest(req *logical.Request) (*logical.Response, error) {
+	resp, err := b.Backend.HandleRequest(req)
+	if err == rpc.ErrShutdown {
+		// Reload plugin if it's an rpc.ErrShutdown
+		err := b.reloadBackend()
+		if err != nil {
+			return nil, err
+		}
+
+		// Try handle request once more
+		return b.Backend.HandleRequest(req)
+	}
+	return resp, err
+}
+
+// HandleExistenceCheck is a thin wrapper implementation of HandleRequest that includes automatic plugin reload.
+func (b *backend) HandleExistenceCheck(req *logical.Request) (bool, bool, error) {
+	checkFound, exists, err := b.Backend.HandleExistenceCheck(req)
+	if err == rpc.ErrShutdown {
+		// Reload plugin if it's an rpc.ErrShutdown
+		err := b.reloadBackend()
+		if err != nil {
+			return false, false, err
+		}
+
+		// Try handle request once more
+		return b.Backend.HandleExistenceCheck(req)
+	}
+	return checkFound, exists, err
 }
