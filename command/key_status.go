f93e3e3e705fc82c64bc04c1119ea7a5f37efb5a
Seth Vargo <seth@sethvargo.com>
2017-09-05 00:02:09 -0400

    Update key-status command

--- command/key_status.go
+++ command/key_status.go
@@ -4,38 +4,17 @@ import (
 	"fmt"
 	"strings"
 
-	"github.com/hashicorp/vault/meta"
+	"github.com/mitchellh/cli"
+	"github.com/posener/complete"
 )
+
+// Ensure we are implementing the right interfaces.
+var _ cli.Command = (*KeyStatusCommand)(nil)
+var _ cli.CommandAutocomplete = (*KeyStatusCommand)(nil)
 
 // KeyStatusCommand is a Command that provides information about the key status
 type KeyStatusCommand struct {
-	meta.Meta
-}
-
-func (c *KeyStatusCommand) Run(args []string) int {
-	flags := c.Meta.FlagSet("key-status", meta.FlagSetDefault)
-	flags.Usage = func() { c.Ui.Error(c.Help()) }
-	if err := flags.Parse(args); err != nil {
-		return 1
-	}
-
-	client, err := c.Client()
-	if err != nil {
-		c.Ui.Error(fmt.Sprintf(
-			"Error initializing client: %s", err))
-		return 2
-	}
-
-	status, err := client.Sys().KeyStatus()
-	if err != nil {
-		c.Ui.Error(fmt.Sprintf(
-			"Error reading audits: %s", err))
-		return 2
-	}
-
-	c.Ui.Output(fmt.Sprintf("Key Term: %d", status.Term))
-	c.Ui.Output(fmt.Sprintf("Installation Time: %v", status.InstallTime))
-	return 0
+	*BaseCommand
 }
 
 func (c *KeyStatusCommand) Synopsis() string {
@@ -49,7 +28,49 @@ Usage: vault key-status [options]
   Provides information about the active encryption key. Specifically,
   the current key term and the key installation time.
 
-General Options:
-` + meta.GeneralOptionsUsage()
+` + c.Flags().Help()
+
 	return strings.TrimSpace(helpText)
+}
+
+func (c *KeyStatusCommand) Flags() *FlagSets {
+	return c.flagSet(FlagSetHTTP)
+}
+
+func (c *KeyStatusCommand) AutocompleteArgs() complete.Predictor {
+	return nil
+}
+
+func (c *KeyStatusCommand) AutocompleteFlags() complete.Flags {
+	return c.Flags().Completions()
+}
+
+func (c *KeyStatusCommand) Run(args []string) int {
+	f := c.Flags()
+
+	if err := f.Parse(args); err != nil {
+		c.UI.Error(err.Error())
+		return 1
+	}
+
+	args = f.Args()
+	if len(args) > 0 {
+		c.UI.Error(fmt.Sprintf("Too many arguments (expected 0, got %d)", len(args)))
+		return 1
+	}
+
+	client, err := c.Client()
+	if err != nil {
+		c.UI.Error(err.Error())
+		return 2
+	}
+
+	status, err := client.Sys().KeyStatus()
+	if err != nil {
+		c.UI.Error(fmt.Sprintf("Error reading key status: %s", err))
+		return 2
+	}
+
+	c.UI.Output(printKeyStatus(status))
+	return 0
 }
