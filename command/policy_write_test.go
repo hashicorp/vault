0d598a7f1ecda9591983e09b24297ee6ef0c49da
Seth Vargo <seth@sethvargo.com>
2017-09-05 00:03:29 -0400

    Update policy-write command

--- command/policy_write_test.go
+++ command/policy_write_test.go
@@ -1,33 +1,207 @@
 package command
 
 import (
+	"bytes"
+	"io"
+	"io/ioutil"
+	"os"
+	"reflect"
+	"strings"
 	"testing"
 
-	"github.com/hashicorp/vault/http"
-	"github.com/hashicorp/vault/meta"
-	"github.com/hashicorp/vault/vault"
 	"github.com/mitchellh/cli"
 )
 
-func TestPolicyWrite(t *testing.T) {
-	core, _, token := vault.TestCoreUnsealed(t)
-	ln, addr := http.TestServer(t, core)
-	defer ln.Close()
+func testPolicyWriteCommand(tb testing.TB) (*cli.MockUi, *PolicyWriteCommand) {
+	tb.Helper()
 
-	ui := new(cli.MockUi)
-	c := &PolicyWriteCommand{
-		Meta: meta.Meta{
-			ClientToken: token,
-			Ui:          ui,
+	ui := cli.NewMockUi()
+	return ui, &PolicyWriteCommand{
+		BaseCommand: &BaseCommand{
+			UI: ui,
+		},
+	}
+}
+
+func testPolicyWritePolicyContents(tb testing.TB) []byte {
+	return bytes.TrimSpace([]byte(`
+path "secret/" {
+  capabilities = ["read"]
+}
+	`))
+}
+
+func TestPolicyWriteCommand_Run(t *testing.T) {
+	t.Parallel()
+
+	cases := []struct {
+		name string
+		args []string
+		out  string
+		code int
+	}{
+		{
+			"too_many_args",
+			[]string{"foo", "bar", "baz"},
+			"Too many arguments",
+			1,
+		},
+		{
+			"not_enough_args",
+			[]string{"foo"},
+			"Not enough arguments",
+			1,
+		},
+		{
+			"bad_file",
+			[]string{"my-policy", "/not/a/real/path.hcl"},
+			"Error opening policy file",
+			2,
 		},
 	}
 
-	args := []string{
-		"-address", addr,
-		"foo",
-		"./test-fixtures/policy.hcl",
-	}
-	if code := c.Run(args); code != 0 {
-		t.Fatalf("bad: %d\n\n%s", code, ui.ErrorWriter.String())
-	}
+	t.Run("validations", func(t *testing.T) {
+		t.Parallel()
+
+		for _, tc := range cases {
+			tc := tc
+
+			t.Run(tc.name, func(t *testing.T) {
+				t.Parallel()
+
+				client, closer := testVaultServer(t)
+				defer closer()
+
+				ui, cmd := testPolicyWriteCommand(t)
+				cmd.client = client
+
+				code := cmd.Run(tc.args)
+				if code != tc.code {
+					t.Errorf("expected %d to be %d", code, tc.code)
+				}
+
+				combined := ui.OutputWriter.String() + ui.ErrorWriter.String()
+				if !strings.Contains(combined, tc.out) {
+					t.Errorf("expected %q to contain %q", combined, tc.out)
+				}
+			})
+		}
+	})
+
+	t.Run("file", func(t *testing.T) {
+		t.Parallel()
+
+		policy := testPolicyWritePolicyContents(t)
+		f, err := ioutil.TempFile("", "vault-policy-write")
+		if err != nil {
+			t.Fatal(err)
+		}
+		if _, err := f.Write(policy); err != nil {
+			t.Fatal(err)
+		}
+		if err := f.Close(); err != nil {
+			t.Fatal(err)
+		}
+		defer os.Remove(f.Name())
+
+		client, closer := testVaultServer(t)
+		defer closer()
+
+		ui, cmd := testPolicyWriteCommand(t)
+		cmd.client = client
+
+		code := cmd.Run([]string{
+			"my-policy", f.Name(),
+		})
+		if exp := 0; code != exp {
+			t.Errorf("expected %d to be %d", code, exp)
+		}
+
+		expected := "Success! Uploaded policy: my-policy"
+		combined := ui.OutputWriter.String() + ui.ErrorWriter.String()
+		if !strings.Contains(combined, expected) {
+			t.Errorf("expected %q to contain %q", combined, expected)
+		}
+
+		policies, err := client.Sys().ListPolicies()
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		list := []string{"default", "my-policy", "root"}
+		if !reflect.DeepEqual(policies, list) {
+			t.Errorf("expected %q to be %q", policies, list)
+		}
+	})
+
+	t.Run("stdin", func(t *testing.T) {
+		t.Parallel()
+
+		stdinR, stdinW := io.Pipe()
+		go func() {
+			policy := testPolicyWritePolicyContents(t)
+			stdinW.Write(policy)
+			stdinW.Close()
+		}()
+
+		client, closer := testVaultServer(t)
+		defer closer()
+
+		ui, cmd := testPolicyWriteCommand(t)
+		cmd.client = client
+		cmd.testStdin = stdinR
+
+		code := cmd.Run([]string{
+			"my-policy", "-",
+		})
+		if exp := 0; code != exp {
+			t.Errorf("expected %d to be %d", code, exp)
+		}
+
+		expected := "Success! Uploaded policy: my-policy"
+		combined := ui.OutputWriter.String() + ui.ErrorWriter.String()
+		if !strings.Contains(combined, expected) {
+			t.Errorf("expected %q to contain %q", combined, expected)
+		}
+
+		policies, err := client.Sys().ListPolicies()
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		list := []string{"default", "my-policy", "root"}
+		if !reflect.DeepEqual(policies, list) {
+			t.Errorf("expected %q to be %q", policies, list)
+		}
+	})
+
+	t.Run("communication_failure", func(t *testing.T) {
+		t.Parallel()
+
+		client, closer := testVaultServerBad(t)
+		defer closer()
+
+		ui, cmd := testPolicyWriteCommand(t)
+		cmd.client = client
+
+		code := cmd.Run([]string{
+			"my-policy", "-",
+		})
+		if exp := 2; code != exp {
+			t.Errorf("expected %d to be %d", code, exp)
+		}
+
+		expected := "Error uploading policy: "
+		combined := ui.OutputWriter.String() + ui.ErrorWriter.String()
+		if !strings.Contains(combined, expected) {
+			t.Errorf("expected %q to contain %q", combined, expected)
+		}
+	})
+
+	t.Run("no_tabs", func(t *testing.T) {
+		t.Parallel()
+
+		_, cmd := testPolicyWriteCommand(t)
+		assertNoTabs(t, cmd)
+	})
 }
