f244e03fda08d1c5af61e182aac080bc79a6132a
Seth Vargo <seth@sethvargo.com>
2017-09-05 00:02:55 -0400

    Update path-help command

--- command/path_help.go
+++ command/path_help.go
@@ -4,73 +4,103 @@ import (
 	"fmt"
 	"strings"
 
-	"github.com/hashicorp/vault/meta"
+	"github.com/mitchellh/cli"
+	"github.com/posener/complete"
 )
+
+// Ensure we are implementing the right interfaces.
+var _ cli.Command = (*PathHelpCommand)(nil)
+var _ cli.CommandAutocomplete = (*PathHelpCommand)(nil)
+
+var pathHelpVaultSealedMessage = strings.TrimSpace(`
+Error: Vault is sealed.
+
+The "path-help" command requires the Vault to be unsealed so that the mount
+points of the secret backends are known.
+`)
 
 // PathHelpCommand is a Command that lists the mounts.
 type PathHelpCommand struct {
-	meta.Meta
-}
-
-func (c *PathHelpCommand) Run(args []string) int {
-	flags := c.Meta.FlagSet("help", meta.FlagSetDefault)
-	flags.Usage = func() { c.Ui.Error(c.Help()) }
-	if err := flags.Parse(args); err != nil {
-		return 1
-	}
-
-	args = flags.Args()
-	if len(args) != 1 {
-		flags.Usage()
-		c.Ui.Error("\nhelp expects a single argument")
-		return 1
-	}
-
-	path := args[0]
-
-	client, err := c.Client()
-	if err != nil {
-		c.Ui.Error(fmt.Sprintf(
-			"Error initializing client: %s", err))
-		return 2
-	}
-
-	help, err := client.Help(path)
-	if err != nil {
-		if strings.Contains(err.Error(), "Vault is sealed") {
-			c.Ui.Error(`Error: Vault is sealed.
-
-The path-help command requires the vault to be unsealed so that
-mount points of secret backends are known.`)
-		} else {
-			c.Ui.Error(fmt.Sprintf(
-				"Error reading help: %s", err))
-		}
-		return 1
-	}
-
-	c.Ui.Output(help.Help)
-	return 0
+	*BaseCommand
 }
 
 func (c *PathHelpCommand) Synopsis() string {
-	return "Look up the help for a path"
+	return "Retrieves API help for paths"
 }
 
 func (c *PathHelpCommand) Help() string {
 	helpText := `
 Usage: vault path-help [options] path
 
-  Look up the help for a path.
+  Retrieves API help for paths. All endpoints in Vault provide built-in help
+  in markdown format. This includes system paths, secret paths, and credential
+  providers.
 
-  All endpoints in Vault from system paths, secret paths, and credential
-  providers provide built-in help. This command looks up and outputs that
-  help.
+  A backend must be mounted before help is available:
 
-  The command requires that the vault be unsealed, because otherwise
-  the mount points of the backends are unknown.
+      $ vault mount database
+      $ vault path-help database/
 
-General Options:
-` + meta.GeneralOptionsUsage()
+  The response object will return additional paths to retrieve help:
+
+      $ vault path-help database/roles/
+
+  Each backend produces different help output. For additional information,
+  please view the online documentation.
+
+` + c.Flags().Help()
+
 	return strings.TrimSpace(helpText)
+}
+
+func (c *PathHelpCommand) Flags() *FlagSets {
+	return c.flagSet(FlagSetHTTP)
+}
+
+func (c *PathHelpCommand) AutocompleteArgs() complete.Predictor {
+	return complete.PredictAnything // TODO: programatic way to invoke help
+}
+
+func (c *PathHelpCommand) AutocompleteFlags() complete.Flags {
+	return c.Flags().Completions()
+}
+
+func (c *PathHelpCommand) Run(args []string) int {
+	f := c.Flags()
+
+	if err := f.Parse(args); err != nil {
+		c.UI.Error(err.Error())
+		return 1
+	}
+
+	args = f.Args()
+	path, kvs, err := extractPath(args)
+	if err != nil {
+		c.UI.Error(err.Error())
+		return 1
+	}
+
+	if len(kvs) > 0 {
+		c.UI.Error(fmt.Sprintf("Too many arguments (expected 1, got %d)", len(args)))
+		return 1
+	}
+
+	client, err := c.Client()
+	if err != nil {
+		c.UI.Error(err.Error())
+		return 2
+	}
+
+	help, err := client.Help(path)
+	if err != nil {
+		if strings.Contains(err.Error(), "Vault is sealed") {
+			c.UI.Error(pathHelpVaultSealedMessage)
+		} else {
+			c.UI.Error(fmt.Sprintf("Error retrieving help: %s", err))
+		}
+		return 2
+	}
+
+	c.UI.Output(help.Help)
+	return 0
 }
