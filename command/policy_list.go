cfd378187a75831b6558363580c88f5d5b583641
Seth Vargo <seth@sethvargo.com>
2017-09-05 00:03:21 -0400

    Update policy-list command

--- command/policy_list.go
+++ command/policy_list.go
@@ -4,89 +4,102 @@ import (
 	"fmt"
 	"strings"
 
-	"github.com/hashicorp/vault/meta"
+	"github.com/mitchellh/cli"
+	"github.com/posener/complete"
 )
+
+// Ensure we are implementing the right interfaces.
+var _ cli.Command = (*PolicyListCommand)(nil)
+var _ cli.CommandAutocomplete = (*PolicyListCommand)(nil)
 
 // PolicyListCommand is a Command that enables a new endpoint.
 type PolicyListCommand struct {
-	meta.Meta
-}
-
-func (c *PolicyListCommand) Run(args []string) int {
-	flags := c.Meta.FlagSet("policy-list", meta.FlagSetDefault)
-	flags.Usage = func() { c.Ui.Error(c.Help()) }
-	if err := flags.Parse(args); err != nil {
-		return 1
-	}
-
-	args = flags.Args()
-	if len(args) == 1 {
-		return c.read(args[0])
-	} else if len(args) == 0 {
-		return c.list()
-	} else {
-		flags.Usage()
-		c.Ui.Error(fmt.Sprintf(
-			"\npolicies expects zero or one arguments"))
-		return 1
-	}
-}
-
-func (c *PolicyListCommand) list() int {
-	client, err := c.Client()
-	if err != nil {
-		c.Ui.Error(fmt.Sprintf(
-			"Error initializing client: %s", err))
-		return 2
-	}
-
-	policies, err := client.Sys().ListPolicies()
-	if err != nil {
-		c.Ui.Error(fmt.Sprintf(
-			"Error: %s", err))
-		return 1
-	}
-
-	for _, p := range policies {
-		c.Ui.Output(p)
-	}
-
-	return 0
-}
-
-func (c *PolicyListCommand) read(n string) int {
-	client, err := c.Client()
-	if err != nil {
-		c.Ui.Error(fmt.Sprintf(
-			"Error initializing client: %s", err))
-		return 2
-	}
-
-	rules, err := client.Sys().GetPolicy(n)
-	if err != nil {
-		c.Ui.Error(fmt.Sprintf(
-			"Error: %s", err))
-		return 1
-	}
-
-	c.Ui.Output(rules)
-	return 0
+	*BaseCommand
 }
 
 func (c *PolicyListCommand) Synopsis() string {
-	return "List the policies on the server"
+	return "Lists the installed policies"
 }
 
 func (c *PolicyListCommand) Help() string {
 	helpText := `
-Usage: vault policies [options] [name]
+Usage: vault policies [options] [NAME]
 
-  List the policies that are available or read a single policy.
+  Lists the policies that are installed on the Vault server. If the optional
+  argument is given, this command returns the policy's contents.
 
-  This command lists the policies that are written to the Vault server.
-  If a name of a policy is specified, that policy is outputted.
+  List all policies stored in Vault:
 
-General Options:
-` + meta.GeneralOptionsUsage()
+      $ vault policies
+
+  Read the contents of the policy named "my-policy":
+
+      $ vault policies my-policy
+
+  For a full list of examples, please see the documentation.
+
+` + c.Flags().Help()
+
 	return strings.TrimSpace(helpText)
+}
+
+func (c *PolicyListCommand) Flags() *FlagSets {
+	return c.flagSet(FlagSetHTTP)
+}
+
+func (c *PolicyListCommand) AutocompleteArgs() complete.Predictor {
+	return c.PredictVaultPolicies()
+}
+
+func (c *PolicyListCommand) AutocompleteFlags() complete.Flags {
+	return c.Flags().Completions()
+}
+
+func (c *PolicyListCommand) Run(args []string) int {
+	f := c.Flags()
+
+	if err := f.Parse(args); err != nil {
+		c.UI.Error(err.Error())
+		return 1
+	}
+
+	args = f.Args()
+	switch len(args) {
+	case 0, 1:
+	default:
+		c.UI.Error(fmt.Sprintf("Too many arguments (expected 0-2, got %d)", len(args)))
+		return 1
+	}
+
+	client, err := c.Client()
+	if err != nil {
+		c.UI.Error(err.Error())
+		return 2
+	}
+
+	switch len(args) {
+	case 0:
+		policies, err := client.Sys().ListPolicies()
+		if err != nil {
+			c.UI.Error(fmt.Sprintf("Error listing policies: %s", err))
+			return 2
+		}
+		for _, p := range policies {
+			c.UI.Output(p)
+		}
+	case 1:
+		name := strings.ToLower(strings.TrimSpace(args[0]))
+		rules, err := client.Sys().GetPolicy(name)
+		if err != nil {
+			c.UI.Error(fmt.Sprintf("Error reading policy %s: %s", name, err))
+			return 2
+		}
+		if rules == "" {
+			c.UI.Error(fmt.Sprintf("Error reading policy: no policy named: %s", name))
+			return 2
+		}
+		c.UI.Output(strings.TrimSpace(rules))
+	}
+
+	return 0
 }
