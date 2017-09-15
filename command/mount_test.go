ed1fa9f5ffa89a88ce9b40d307695696fceb69bd
Chris Hoffman <choffman@hashicorp.com>
2017-09-15 02:10:26 -0400

    adding back test to verify generic functionality

--- command/mount_test.go
+++ command/mount_test.go
@@ -48,3 +48,43 @@ func TestMount(t *testing.T) {
 		t.Fatal("should be kv type")
 	}
 }
+
+func TestMount_Generic(t *testing.T) {
+	core, _, token := vault.TestCoreUnsealed(t)
+	ln, addr := http.TestServer(t, core)
+	defer ln.Close()
+
+	ui := new(cli.MockUi)
+	c := &MountCommand{
+		Meta: meta.Meta{
+			ClientToken: token,
+			Ui:          ui,
+		},
+	}
+
+	args := []string{
+		"-address", addr,
+		"generic",
+	}
+	if code := c.Run(args); code != 0 {
+		t.Fatalf("bad: %d\n\n%s", code, ui.ErrorWriter.String())
+	}
+
+	client, err := c.Client()
+	if err != nil {
+		t.Fatalf("err: %s", err)
+	}
+
+	mounts, err := client.Sys().ListMounts()
+	if err != nil {
+		t.Fatalf("err: %s", err)
+	}
+
+	mount, ok := mounts["generic/"]
+	if !ok {
+		t.Fatal("should have generic mount path")
+	}
+	if mount.Type != "generic" {
+		t.Fatal("should be generic type")
+	}
+}
