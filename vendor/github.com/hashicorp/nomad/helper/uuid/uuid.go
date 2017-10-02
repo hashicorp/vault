a393b20cb6d96c73e52eb5af776c892b8107a45d
Chris Hoffman <christopher.hoffman@gmail.com>
2017-10-02 13:47:03 -0400

    fixing dependencies

---
+++ vendor/github.com/hashicorp/nomad/helper/uuid/uuid.go
@@ -0,0 +1,21 @@
+package uuid
+
+import (
+	crand "crypto/rand"
+	"fmt"
+)
+
+// Generate is used to generate a random UUID
+func Generate() string {
+	buf := make([]byte, 16)
+	if _, err := crand.Read(buf); err != nil {
+		panic(fmt.Errorf("failed to read random bytes: %v", err))
+	}
+
+	return fmt.Sprintf("%08x-%04x-%04x-%04x-%12x",
+		buf[0:4],
+		buf[4:6],
+		buf[6:8],
+		buf[8:10],
+		buf[10:16])
+}
