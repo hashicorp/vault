84eeddeb12a1f1c3332a3e7e02f79c81d92ab9b0
Jeff Mitchell <jeffrey.mitchell@gmail.com>
2017-10-27 14:43:01 -0400

    Change some instances of adding headers to setting headers, since really we want to replace anything that might be there (e.g. for request forwarding and content-type).
    Hopefully fixes #3485

--- http/handler.go
+++ http/handler.go
@@ -209,9 +209,7 @@ func handleRequestForwarding(core *vault.Core, handler http.Handler) http.Handle
 
 		if header != nil {
 			for k, v := range header {
-				for _, j := range v {
-					w.Header().Add(k, j)
-				}
+				w.Header()[k] = v
 			}
 		}
 
@@ -332,7 +330,7 @@ func requestWrapInfo(r *http.Request, req *logical.Request) (*logical.Request, e
 func respondError(w http.ResponseWriter, status int, err error) {
 	logical.AdjustErrorStatusCode(&status, err)
 
-	w.Header().Add("Content-Type", "application/json")
+	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(status)
 
 	resp := &ErrorResponse{Errors: make([]string, 0, 1)}
@@ -355,7 +353,7 @@ func respondErrorCommon(w http.ResponseWriter, req *logical.Request, resp *logic
 }
 
 func respondOk(w http.ResponseWriter, body interface{}) {
-	w.Header().Add("Content-Type", "application/json")
+	w.Header().Set("Content-Type", "application/json")
 
 	if body == nil {
 		w.WriteHeader(http.StatusNoContent)
