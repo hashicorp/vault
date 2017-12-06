0a1a8fcc740a16736cf292d35f21919063621c5e
Brian Shumate <brian@brianshumate.com>
2017-12-06 12:11:29 -0500

    Update open function to bypass chmod if mode is zero

--- builtin/audit/file/backend.go
+++ builtin/audit/file/backend.go
@@ -249,13 +249,15 @@ func (b *Backend) open() error {
 	}
 
 	// Change the file mode in case the log file already existed. We special
-	// case /dev/null since we can't chmod it
+	// case /dev/null since we can't chmod it and bypass if the mode is zero
 	switch b.path {
 	case "/dev/null":
 	default:
-		err = os.Chmod(b.path, b.mode)
-		if err != nil {
-			return err
+		if b.mode != 0 {
+			err = os.Chmod(b.path, b.mode)
+			if err != nil {
+				return err
+			}
 		}
 	}
 
