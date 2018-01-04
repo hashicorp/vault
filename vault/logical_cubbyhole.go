00bc22ec58df4566a8250e5fe59d85645cbac09e
Brian Kassouf <bkassouf@hashicorp.com>
2018-01-04 11:47:05 -0800

    Unindent cubbyhole

--- vault/logical_cubbyhole.go
+++ vault/logical_cubbyhole.go
@@ -22,14 +22,14 @@ func CubbyholeBackendFactory(conf *logical.BackendConfig) (logical.Backend, erro
 				Pattern: ".*",
 
 				Callbacks: map[logical.Operation]framework.OperationFunc{
-					logical.ReadOperation:   b.handleRead(),
-					logical.CreateOperation: b.handleWrite(),
-					logical.UpdateOperation: b.handleWrite(),
-					logical.DeleteOperation: b.handleDelete(),
-					logical.ListOperation:   b.handleList(),
+					logical.ReadOperation:   b.handleRead,
+					logical.CreateOperation: b.handleWrite,
+					logical.UpdateOperation: b.handleWrite,
+					logical.DeleteOperation: b.handleDelete,
+					logical.ListOperation:   b.handleList,
 				},
 
-				ExistenceCheck: b.handleExistenceCheck(),
+				ExistenceCheck: b.handleExistenceCheck,
 
 				HelpSynopsis:    strings.TrimSpace(cubbyholeHelpSynopsis),
 				HelpDescription: strings.TrimSpace(cubbyholeHelpDescription),
@@ -68,124 +68,114 @@ func (b *CubbyholeBackend) revoke(saltedToken string) error {
 	return nil
 }
 
-func (b *CubbyholeBackend) handleExistenceCheck() framework.ExistenceFunc {
-	return func(ctx context.Context, req *logical.Request, data *framework.FieldData) (bool, error) {
-		out, err := req.Storage.Get(req.ClientToken + "/" + req.Path)
-		if err != nil {
-			return false, fmt.Errorf("existence check failed: %v", err)
-		}
-
-		return out != nil, nil
+func (b *CubbyholeBackend) handleExistenceCheck(ctx context.Context, req *logical.Request, data *framework.FieldData) (bool, error) {
+	out, err := req.Storage.Get(req.ClientToken + "/" + req.Path)
+	if err != nil {
+		return false, fmt.Errorf("existence check failed: %v", err)
 	}
+
+	return out != nil, nil
 }
 
-func (b *CubbyholeBackend) handleRead() framework.OperationFunc {
-	return func(ctx context.Context, req *logical.Request, data *framework.FieldData) (*logical.Response, error) {
-		if req.ClientToken == "" {
-			return nil, fmt.Errorf("cubbyhole read: client token empty")
-		}
-
-		// Read the path
-		out, err := req.Storage.Get(req.ClientToken + "/" + req.Path)
-		if err != nil {
-			return nil, fmt.Errorf("read failed: %v", err)
-		}
-
-		// Fast-path the no data case
-		if out == nil {
-			return nil, nil
-		}
-
-		// Decode the data
-		var rawData map[string]interface{}
-		if err := jsonutil.DecodeJSON(out.Value, &rawData); err != nil {
-			return nil, fmt.Errorf("json decoding failed: %v", err)
-		}
-
-		// Generate the response
-		resp := &logical.Response{
-			Data: rawData,
-		}
-
-		return resp, nil
+func (b *CubbyholeBackend) handleRead(ctx context.Context, req *logical.Request, data *framework.FieldData) (*logical.Response, error) {
+	if req.ClientToken == "" {
+		return nil, fmt.Errorf("cubbyhole read: client token empty")
 	}
-}
 
-func (b *CubbyholeBackend) handleWrite() framework.OperationFunc {
-	return func(ctx context.Context, req *logical.Request, data *framework.FieldData) (*logical.Response, error) {
-		if req.ClientToken == "" {
-			return nil, fmt.Errorf("cubbyhole write: client token empty")
-		}
-		// Check that some fields are given
-		if len(req.Data) == 0 {
-			return nil, fmt.Errorf("missing data fields")
-		}
+	// Read the path
+	out, err := req.Storage.Get(req.ClientToken + "/" + req.Path)
+	if err != nil {
+		return nil, fmt.Errorf("read failed: %v", err)
+	}
 
-		// JSON encode the data
-		buf, err := json.Marshal(req.Data)
-		if err != nil {
-			return nil, fmt.Errorf("json encoding failed: %v", err)
-		}
-
-		// Write out a new key
-		entry := &logical.StorageEntry{
-			Key:   req.ClientToken + "/" + req.Path,
-			Value: buf,
-		}
-		if req.WrapInfo != nil && req.WrapInfo.SealWrap {
-			entry.SealWrap = true
-		}
-		if err := req.Storage.Put(entry); err != nil {
-			return nil, fmt.Errorf("failed to write: %v", err)
-		}
-
+	// Fast-path the no data case
+	if out == nil {
 		return nil, nil
 	}
+
+	// Decode the data
+	var rawData map[string]interface{}
+	if err := jsonutil.DecodeJSON(out.Value, &rawData); err != nil {
+		return nil, fmt.Errorf("json decoding failed: %v", err)
+	}
+
+	// Generate the response
+	resp := &logical.Response{
+		Data: rawData,
+	}
+
+	return resp, nil
 }
 
-func (b *CubbyholeBackend) handleDelete() framework.OperationFunc {
-	return func(ctx context.Context, req *logical.Request, data *framework.FieldData) (*logical.Response, error) {
-		if req.ClientToken == "" {
-			return nil, fmt.Errorf("cubbyhole delete: client token empty")
-		}
-		// Delete the key at the request path
-		if err := req.Storage.Delete(req.ClientToken + "/" + req.Path); err != nil {
-			return nil, err
-		}
-
-		return nil, nil
+func (b *CubbyholeBackend) handleWrite(ctx context.Context, req *logical.Request, data *framework.FieldData) (*logical.Response, error) {
+	if req.ClientToken == "" {
+		return nil, fmt.Errorf("cubbyhole write: client token empty")
 	}
+	// Check that some fields are given
+	if len(req.Data) == 0 {
+		return nil, fmt.Errorf("missing data fields")
+	}
+
+	// JSON encode the data
+	buf, err := json.Marshal(req.Data)
+	if err != nil {
+		return nil, fmt.Errorf("json encoding failed: %v", err)
+	}
+
+	// Write out a new key
+	entry := &logical.StorageEntry{
+		Key:   req.ClientToken + "/" + req.Path,
+		Value: buf,
+	}
+	if req.WrapInfo != nil && req.WrapInfo.SealWrap {
+		entry.SealWrap = true
+	}
+	if err := req.Storage.Put(entry); err != nil {
+		return nil, fmt.Errorf("failed to write: %v", err)
+	}
+
+	return nil, nil
 }
 
-func (b *CubbyholeBackend) handleList() framework.OperationFunc {
-	return func(ctx context.Context, req *logical.Request, data *framework.FieldData) (*logical.Response, error) {
-		if req.ClientToken == "" {
-			return nil, fmt.Errorf("cubbyhole list: client token empty")
-		}
-
-		// Right now we only handle directories, so ensure it ends with / We also
-		// check if it's empty so we don't end up doing a listing on '<client
-		// token>//'
-		path := req.Path
-		if path != "" && !strings.HasSuffix(path, "/") {
-			path = path + "/"
-		}
-
-		// List the keys at the prefix given by the request
-		keys, err := req.Storage.List(req.ClientToken + "/" + path)
-		if err != nil {
-			return nil, err
-		}
-
-		// Strip the token
-		strippedKeys := make([]string, len(keys))
-		for i, key := range keys {
-			strippedKeys[i] = strings.TrimPrefix(key, req.ClientToken+"/")
-		}
-
-		// Generate the response
-		return logical.ListResponse(strippedKeys), nil
+func (b *CubbyholeBackend) handleDelete(ctx context.Context, req *logical.Request, data *framework.FieldData) (*logical.Response, error) {
+	if req.ClientToken == "" {
+		return nil, fmt.Errorf("cubbyhole delete: client token empty")
 	}
+	// Delete the key at the request path
+	if err := req.Storage.Delete(req.ClientToken + "/" + req.Path); err != nil {
+		return nil, err
+	}
+
+	return nil, nil
+}
+
+func (b *CubbyholeBackend) handleList(ctx context.Context, req *logical.Request, data *framework.FieldData) (*logical.Response, error) {
+	if req.ClientToken == "" {
+		return nil, fmt.Errorf("cubbyhole list: client token empty")
+	}
+
+	// Right now we only handle directories, so ensure it ends with / We also
+	// check if it's empty so we don't end up doing a listing on '<client
+	// token>//'
+	path := req.Path
+	if path != "" && !strings.HasSuffix(path, "/") {
+		path = path + "/"
+	}
+
+	// List the keys at the prefix given by the request
+	keys, err := req.Storage.List(req.ClientToken + "/" + path)
+	if err != nil {
+		return nil, err
+	}
+
+	// Strip the token
+	strippedKeys := make([]string, len(keys))
+	for i, key := range keys {
+		strippedKeys[i] = strings.TrimPrefix(key, req.ClientToken+"/")
+	}
+
+	// Generate the response
+	return logical.ListResponse(strippedKeys), nil
 }
 
 const cubbyholeHelp = `
