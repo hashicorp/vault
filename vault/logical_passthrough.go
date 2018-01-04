9992017abde9ceff1969a2a7466d3e04b00c22fd
Brian Kassouf <bkassouf@hashicorp.com>
2018-01-04 11:42:36 -0800

    Unindent passthrough

--- vault/logical_passthrough.go
+++ vault/logical_passthrough.go
@@ -44,14 +44,14 @@ func LeaseSwitchedPassthroughBackend(conf *logical.BackendConfig, leases bool) (
 				Pattern: ".*",
 
 				Callbacks: map[logical.Operation]framework.OperationFunc{
-					logical.ReadOperation:   b.handleRead(),
-					logical.CreateOperation: b.handleWrite(),
-					logical.UpdateOperation: b.handleWrite(),
-					logical.DeleteOperation: b.handleDelete(),
-					logical.ListOperation:   b.handleList(),
+					logical.ReadOperation:   b.handleRead,
+					logical.CreateOperation: b.handleWrite,
+					logical.UpdateOperation: b.handleWrite,
+					logical.DeleteOperation: b.handleDelete,
+					logical.ListOperation:   b.handleList,
 				},
 
-				ExistenceCheck: b.handleExistenceCheck(),
+				ExistenceCheck: b.handleExistenceCheck,
 
 				HelpSynopsis:    strings.TrimSpace(passthroughHelpSynopsis),
 				HelpDescription: strings.TrimSpace(passthroughHelpDescription),
@@ -63,8 +63,8 @@ func LeaseSwitchedPassthroughBackend(conf *logical.BackendConfig, leases bool) (
 		&framework.Secret{
 			Type: "kv",
 
-			Renew:  b.handleRead(),
-			Revoke: b.handleRevoke(),
+			Renew:  b.handleRead,
+			Revoke: b.handleRevoke,
 		},
 	}
 
@@ -85,148 +85,136 @@ type PassthroughBackend struct {
 	generateLeases bool
 }
 
-func (b *PassthroughBackend) handleRevoke() framework.OperationFunc {
-	return func(ctx context.Context, req *logical.Request, data *framework.FieldData) (*logical.Response, error) {
-		// This is a no-op
+func (b *PassthroughBackend) handleRevoke(ctx context.Context, req *logical.Request, data *framework.FieldData) (*logical.Response, error) {
+	// This is a no-op
+	return nil, nil
+}
+
+func (b *PassthroughBackend) handleExistenceCheck(ctx context.Context, req *logical.Request, data *framework.FieldData) (bool, error) {
+	out, err := req.Storage.Get(req.Path)
+	if err != nil {
+		return false, fmt.Errorf("existence check failed: %v", err)
+	}
+
+	return out != nil, nil
+}
+
+func (b *PassthroughBackend) handleRead(ctx context.Context, req *logical.Request, data *framework.FieldData) (*logical.Response, error) {
+	// Read the path
+	out, err := req.Storage.Get(req.Path)
+	if err != nil {
+		return nil, fmt.Errorf("read failed: %v", err)
+	}
+
+	// Fast-path the no data case
+	if out == nil {
 		return nil, nil
 	}
-}
 
-func (b *PassthroughBackend) handleExistenceCheck() framework.ExistenceFunc {
-	return func(ctx context.Context, req *logical.Request, data *framework.FieldData) (bool, error) {
-		out, err := req.Storage.Get(req.Path)
-		if err != nil {
-			return false, fmt.Errorf("existence check failed: %v", err)
-		}
+	// Decode the data
+	var rawData map[string]interface{}
 
-		return out != nil, nil
+	if err := jsonutil.DecodeJSON(out.Value, &rawData); err != nil {
+		return nil, fmt.Errorf("json decoding failed: %v", err)
 	}
-}
 
-func (b *PassthroughBackend) handleRead() framework.OperationFunc {
-	return func(ctx context.Context, req *logical.Request, data *framework.FieldData) (*logical.Response, error) {
-		// Read the path
-		out, err := req.Storage.Get(req.Path)
-		if err != nil {
-			return nil, fmt.Errorf("read failed: %v", err)
+	var resp *logical.Response
+	if b.generateLeases {
+		// Generate the response
+		resp = b.Secret("kv").Response(rawData, nil)
+		resp.Secret.Renewable = false
+	} else {
+		resp = &logical.Response{
+			Secret: &logical.Secret{},
+			Data:   rawData,
+		}
+	}
+
+	// Ensure seal wrapping is carried through if the response is
+	// response-wrapped
+	if out.SealWrap {
+		if resp.WrapInfo == nil {
+			resp.WrapInfo = &wrapping.ResponseWrapInfo{}
+		}
+		resp.WrapInfo.SealWrap = out.SealWrap
+	}
+
+	// Check if there is a ttl key
+	ttlDuration := b.System().DefaultLeaseTTL()
+	ttlRaw, ok := rawData["ttl"]
+	if !ok {
+		ttlRaw, ok = rawData["lease"]
+	}
+	if ok {
+		dur, err := parseutil.ParseDurationSecond(ttlRaw)
+		if err == nil {
+			ttlDuration = dur
 		}
 
-		// Fast-path the no data case
-		if out == nil {
-			return nil, nil
-		}
-
-		// Decode the data
-		var rawData map[string]interface{}
-
-		if err := jsonutil.DecodeJSON(out.Value, &rawData); err != nil {
-			return nil, fmt.Errorf("json decoding failed: %v", err)
-		}
-
-		var resp *logical.Response
 		if b.generateLeases {
-			// Generate the response
-			resp = b.Secret("kv").Response(rawData, nil)
-			resp.Secret.Renewable = false
-		} else {
-			resp = &logical.Response{
-				Secret: &logical.Secret{},
-				Data:   rawData,
-			}
+			resp.Secret.Renewable = true
 		}
-
-		// Ensure seal wrapping is carried through if the response is
-		// response-wrapped
-		if out.SealWrap {
-			if resp.WrapInfo == nil {
-				resp.WrapInfo = &wrapping.ResponseWrapInfo{}
-			}
-			resp.WrapInfo.SealWrap = out.SealWrap
-		}
-
-		// Check if there is a ttl key
-		ttlDuration := b.System().DefaultLeaseTTL()
-		ttlRaw, ok := rawData["ttl"]
-		if !ok {
-			ttlRaw, ok = rawData["lease"]
-		}
-		if ok {
-			dur, err := parseutil.ParseDurationSecond(ttlRaw)
-			if err == nil {
-				ttlDuration = dur
-			}
-
-			if b.generateLeases {
-				resp.Secret.Renewable = true
-			}
-		}
-
-		resp.Secret.TTL = ttlDuration
-
-		return resp, nil
 	}
+
+	resp.Secret.TTL = ttlDuration
+
+	return resp, nil
 }
 
 func (b *PassthroughBackend) GeneratesLeases() bool {
 	return b.generateLeases
 }
 
-func (b *PassthroughBackend) handleWrite() framework.OperationFunc {
-	return func(ctx context.Context, req *logical.Request, data *framework.FieldData) (*logical.Response, error) {
-		// Check that some fields are given
-		if len(req.Data) == 0 {
-			return logical.ErrorResponse("missing data fields"), nil
-		}
-
-		// JSON encode the data
-		buf, err := json.Marshal(req.Data)
-		if err != nil {
-			return nil, fmt.Errorf("json encoding failed: %v", err)
-		}
-
-		// Write out a new key
-		entry := &logical.StorageEntry{
-			Key:   req.Path,
-			Value: buf,
-		}
-		if err := req.Storage.Put(entry); err != nil {
-			return nil, fmt.Errorf("failed to write: %v", err)
-		}
-
-		return nil, nil
+func (b *PassthroughBackend) handleWrite(ctx context.Context, req *logical.Request, data *framework.FieldData) (*logical.Response, error) {
+	// Check that some fields are given
+	if len(req.Data) == 0 {
+		return logical.ErrorResponse("missing data fields"), nil
 	}
+
+	// JSON encode the data
+	buf, err := json.Marshal(req.Data)
+	if err != nil {
+		return nil, fmt.Errorf("json encoding failed: %v", err)
+	}
+
+	// Write out a new key
+	entry := &logical.StorageEntry{
+		Key:   req.Path,
+		Value: buf,
+	}
+	if err := req.Storage.Put(entry); err != nil {
+		return nil, fmt.Errorf("failed to write: %v", err)
+	}
+
+	return nil, nil
 }
 
-func (b *PassthroughBackend) handleDelete() framework.OperationFunc {
-	return func(ctx context.Context, req *logical.Request, data *framework.FieldData) (*logical.Response, error) {
-		// Delete the key at the request path
-		if err := req.Storage.Delete(req.Path); err != nil {
-			return nil, err
-		}
-
-		return nil, nil
+func (b *PassthroughBackend) handleDelete(ctx context.Context, req *logical.Request, data *framework.FieldData) (*logical.Response, error) {
+	// Delete the key at the request path
+	if err := req.Storage.Delete(req.Path); err != nil {
+		return nil, err
 	}
+
+	return nil, nil
 }
 
-func (b *PassthroughBackend) handleList() framework.OperationFunc {
-	return func(ctx context.Context, req *logical.Request, data *framework.FieldData) (*logical.Response, error) {
-		// Right now we only handle directories, so ensure it ends with /; however,
-		// some physical backends may not handle the "/" case properly, so only add
-		// it if we're not listing the root
-		path := req.Path
-		if path != "" && !strings.HasSuffix(path, "/") {
-			path = path + "/"
-		}
-
-		// List the keys at the prefix given by the request
-		keys, err := req.Storage.List(path)
-		if err != nil {
-			return nil, err
-		}
-
-		// Generate the response
-		return logical.ListResponse(keys), nil
+func (b *PassthroughBackend) handleList(ctx context.Context, req *logical.Request, data *framework.FieldData) (*logical.Response, error) {
+	// Right now we only handle directories, so ensure it ends with /; however,
+	// some physical backends may not handle the "/" case properly, so only add
+	// it if we're not listing the root
+	path := req.Path
+	if path != "" && !strings.HasSuffix(path, "/") {
+		path = path + "/"
 	}
+
+	// List the keys at the prefix given by the request
+	keys, err := req.Storage.List(path)
+	if err != nil {
+		return nil, err
+	}
+
+	// Generate the response
+	return logical.ListResponse(keys), nil
 }
 
 const passthroughHelp = `
