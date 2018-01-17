1a8559145abe8328c1c8f1f8fb54173fa0c25a8b
Calvin Leung Huang <cleung2010@gmail.com>
2018-01-17 02:49:01 -0500

    Pass in approle's period via role's period

--- builtin/credential/approle/path_login_test.go
+++ builtin/credential/approle/path_login_test.go
@@ -70,8 +70,61 @@ func TestAppRole_RoleLogin(t *testing.T) {
 		t.Fatalf("expected period value from response to be 400s, got: %s", resp.Auth.TTL)
 	}
 
+	///
 	// Test renewal with period
-	loginResp.Auth.Period = 600 * time.Second
+	///
+
+	// Create role
+	period := 600 * time.Second
+	roleData := map[string]interface{}{
+		"policies": "a,b,c",
+		"period":   period.String(),
+	}
+	roleReq := &logical.Request{
+		Operation: logical.CreateOperation,
+		Path:      "role/" + "role-period",
+		Storage:   storage,
+		Data:      roleData,
+	}
+	resp, err = b.HandleRequest(context.Background(), roleReq)
+	if err != nil || (resp != nil && resp.IsError()) {
+		t.Fatalf("err:%v resp:%#v", err, resp)
+	}
+
+	roleRoleIDReq = &logical.Request{
+		Operation: logical.ReadOperation,
+		Path:      "role/role-period/role-id",
+		Storage:   storage,
+	}
+	resp, err = b.HandleRequest(context.Background(), roleRoleIDReq)
+	if err != nil || (resp != nil && resp.IsError()) {
+		t.Fatalf("err:%v resp:%#v", err, resp)
+	}
+	roleID = resp.Data["role_id"]
+
+	roleSecretIDReq = &logical.Request{
+		Operation: logical.UpdateOperation,
+		Path:      "role/role-period/secret-id",
+		Storage:   storage,
+	}
+	resp, err = b.HandleRequest(context.Background(), roleSecretIDReq)
+	if err != nil || (resp != nil && resp.IsError()) {
+		t.Fatalf("err:%v resp:%#v", err, resp)
+	}
+	secretID = resp.Data["secret_id"]
+
+	loginData["role_id"] = roleID
+	loginData["secret_id"] = secretID
+
+	loginResp, err = b.HandleRequest(context.Background(), loginReq)
+	if err != nil || (loginResp != nil && loginResp.IsError()) {
+		t.Fatalf("err:%v resp:%#v", err, loginResp)
+	}
+
+	if loginResp.Auth == nil {
+		t.Fatalf("expected a non-nil auth object in the response")
+	}
+
 	renewReq = generateRenewRequest(storage, loginResp.Auth)
 
 	resp, err = b.HandleRequest(context.Background(), renewReq)
@@ -79,8 +132,8 @@ func TestAppRole_RoleLogin(t *testing.T) {
 		t.Fatalf("err:%v resp:%#v", err, resp)
 	}
 
-	if resp.Auth.Period != 600*time.Second {
-		t.Fatalf("expected period value from response to be 600s, got: %s", resp.Auth.Period)
+	if resp.Auth.Period != period {
+		t.Fatalf("expected period value of %d in the response, got: %s", period, resp.Auth.Period)
 	}
 }
 
