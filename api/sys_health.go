54f245822db50a6cbec686250a44e9117109b516
Jeff Mitchell <jeffrey.mitchell@gmail.com>
2018-01-17 23:50:37 -0500

    Allow API to return health response when in a custom state

--- api/sys_health.go
+++ api/sys_health.go
@@ -5,8 +5,10 @@ func (c *Sys) Health() (*HealthResponse, error) {
 	// If the code is 400 or above it will automatically turn into an error,
 	// but the sys/health API defaults to returning 5xx when not sealed or
 	// inited, so we force this code to be something else so we parse correctly
-	r.Params.Add("sealedcode", "299")
 	r.Params.Add("uninitcode", "299")
+	r.Params.Add("sealedcode", "299")
+	r.Params.Add("standbycode", "299")
+	r.Params.Add("drsecondarycode", "299")
 	resp, err := c.c.RawRequest(r)
 	if err != nil {
 		return nil, err
