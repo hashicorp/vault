add7d19433dcb1e734401b1b268bd705b622cb01
Jeff Mitchell <jeffrey.mitchell@gmail.com>
2018-02-22 21:38:52 -0500

    Update comment to replication consts

--- helper/consts/replication.go
+++ helper/consts/replication.go
@@ -7,10 +7,9 @@ const (
 	OldReplicationPrimary
 	OldReplicationSecondary
 	OldReplicationBootstrapping
-	// Don't add anything here. This was meant to be in two separate const
-	// blocks, to reset iota, but it didn't happen that way, so iota carries
-	// over. Adding anything to this Old block would cause the rest of the
-	// values to change below.
+	// Don't add anything here. Adding anything to this Old block would cause
+	// the rest of the values to change below. This was done originally to
+	// ensure no overlap between old and new values.
 
 	ReplicationUnknown            ReplicationState = 0
 	ReplicationPerformancePrimary ReplicationState = 1 << iota
