9335680bcaa31951cd4fd76bf50a802095a1a582
Josh Soref <jsoref@users.noreply.github.com>
2018-03-11 05:41:12 +0000

    spelling: instance

--- builtin/credential/aws/path_config_certificate.go
+++ builtin/credential/aws/path_config_certificate.go
@@ -416,7 +416,7 @@ func (b *backend) pathConfigCertificateCreateUpdate(ctx context.Context, req *lo
 }
 
 // Struct awsPublicCert holds the AWS Public Key that is used to verify the PKCS#7 signature
-// of the instnace identity document.
+// of the instance identity document.
 type awsPublicCert struct {
 	AWSPublicCert string `json:"aws_public_cert"`
 	Type          string `json:"type"`
