c680ebac30fc67db5d7ac400329d792985985843
Josh Soref <jsoref@users.noreply.github.com>
2018-03-11 06:14:37 +0000

    spelling: physical

--- vault/barrier_aes_gcm_test.go
+++ vault/barrier_aes_gcm_test.go
@@ -208,7 +208,7 @@ func TestAESGCMBarrier_Confidential(t *testing.T) {
 		t.Fatalf("err: %v", err)
 	}
 
-	// Check the physcial entry
+	// Check the physical entry
 	pe, err := inm.Get(context.Background(), "test")
 	if err != nil {
 		t.Fatalf("err: %v", err)
