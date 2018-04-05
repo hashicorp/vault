d717eddc5a249846b0f7ab0fc5ccead2df6e6280
Matthew Irish <matthew.irish@gmail.com>
2018-04-04 21:32:51 -0500

    404 when the item_type is invalid for the identity route

--- ui/app/routes/vault/cluster/access/identity.js
+++ ui/app/routes/vault/cluster/access/identity.js
@@ -1,4 +1,5 @@
 import Ember from 'ember';
+import DS from 'ember-data';
 
 const MODEL_FROM_PARAM = {
   entities: 'entity',
@@ -8,8 +9,11 @@ const MODEL_FROM_PARAM = {
 export default Ember.Route.extend({
   model(params) {
     let model = MODEL_FROM_PARAM[params.item_type];
-
-    //TODO 404 behavior;
+    if (!model) {
+      const error = new DS.AdapterError();
+      Ember.set(error, 'httpStatus', 404);
+      throw error;
+    }
     return model;
   },
 });
