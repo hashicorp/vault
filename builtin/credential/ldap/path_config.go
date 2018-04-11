41de71f04b7489b2336f6125f73836dfa6691bd8
Becca Petrin <beccapetrin@posteo.net>
2018-04-11 14:25:09 -0700

    run make fmt

--- builtin/credential/ldap/path_config.go
+++ builtin/credential/ldap/path_config.go
@@ -368,29 +368,29 @@ func (b *backend) pathConfigWrite(ctx context.Context, req *logical.Request, d *
 }
 
 type ConfigEntry struct {
-	logger             log.Logger
-	Url                string `json:"url"`
-	UserDN             string `json:"userdn"`
-	GroupDN            string `json:"groupdn"`
-	GroupFilter        string `json:"groupfilter"`
-	GroupAttr          string `json:"groupattr"`
-	UPNDomain          string `json:"upndomain"`
-	UserAttr           string `json:"userattr"`
-	Certificate        string `json:"certificate"`
-	InsecureTLS        bool   `json:"insecure_tls"`
-	StartTLS           bool   `json:"starttls"`
-	BindDN             string `json:"binddn"`
-	BindPassword       string `json:"bindpass"`
-	DenyNullBind       bool   `json:"deny_null_bind"`
-	DiscoverDN         bool   `json:"discoverdn"`
-	TLSMinVersion      string `json:"tls_min_version"`
-	TLSMaxVersion      string `json:"tls_max_version"`
+	logger        log.Logger
+	Url           string `json:"url"`
+	UserDN        string `json:"userdn"`
+	GroupDN       string `json:"groupdn"`
+	GroupFilter   string `json:"groupfilter"`
+	GroupAttr     string `json:"groupattr"`
+	UPNDomain     string `json:"upndomain"`
+	UserAttr      string `json:"userattr"`
+	Certificate   string `json:"certificate"`
+	InsecureTLS   bool   `json:"insecure_tls"`
+	StartTLS      bool   `json:"starttls"`
+	BindDN        string `json:"binddn"`
+	BindPassword  string `json:"bindpass"`
+	DenyNullBind  bool   `json:"deny_null_bind"`
+	DiscoverDN    bool   `json:"discoverdn"`
+	TLSMinVersion string `json:"tls_min_version"`
+	TLSMaxVersion string `json:"tls_max_version"`
 
 	// This json tag deviates from snake case because there was a past issue
 	// where the tag was being ignored, causing it to be jsonified as "CaseSensitiveNames".
 	// To continue reading in users' previously stored values,
 	// we chose to carry that forward.
-	CaseSensitiveNames *bool  `json:"CaseSensitiveNames,omitempty"`
+	CaseSensitiveNames *bool `json:"CaseSensitiveNames,omitempty"`
 }
 
 func (c *ConfigEntry) GetTLSConfig(host string) (*tls.Config, error) {
