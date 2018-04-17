d051c9025f7967006b224f73c62388724b46529a
Jeff Mitchell <jeffrey.mitchell@gmail.com>
2018-04-17 13:43:36 -0400

    Finish tests and parallelize them

--- http/forwarded_for_test.go
+++ http/forwarded_for_test.go
@@ -22,109 +22,230 @@ func TestHandler_XForwardedFor(t *testing.T) {
 	}
 
 	// First: test reject not present
-	testHandler := func(c *vault.Core) http.Handler {
-		origHandler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			w.WriteHeader(http.StatusOK)
-			w.Write([]byte(r.RemoteAddr))
+	t.Run("reject_not_present", func(t *testing.T) {
+		t.Parallel()
+		testHandler := func(c *vault.Core) http.Handler {
+			origHandler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+				w.WriteHeader(http.StatusOK)
+				w.Write([]byte(r.RemoteAddr))
+			})
+			return WrapForwardedForHandler(origHandler, []*sockaddr.SockAddrMarshaler{
+				&sockaddr.SockAddrMarshaler{
+					SockAddr: goodAddr,
+				},
+			}, true, false, 0)
+		}
+
+		cluster := vault.NewTestCluster(t, nil, &vault.TestClusterOptions{
+			HandlerFunc: testHandler,
 		})
-		return WrapForwardedForHandler(origHandler, []*sockaddr.SockAddrMarshaler{
-			&sockaddr.SockAddrMarshaler{
-				SockAddr: goodAddr,
-			},
-		}, true, false, 0)
-	}
+		cluster.Start()
+		defer cluster.Cleanup()
+		client := cluster.Cores[0].Client
 
-	cluster := vault.NewTestCluster(t, nil, &vault.TestClusterOptions{
-		HandlerFunc: testHandler,
+		req := client.NewRequest("GET", "/")
+		_, err = client.RawRequest(req)
+		if err == nil {
+			t.Fatal("expected error")
+		}
+		if !strings.Contains(err.Error(), "missing x-forwarded-for") {
+			t.Fatalf("bad error message: %v", err)
+		}
+		req = client.NewRequest("GET", "/")
+		req.Headers = make(http.Header)
+		req.Headers.Set("x-forwarded-for", "1.2.3.4")
+		resp, err := client.RawRequest(req)
+		if err != nil {
+			t.Fatal(err)
+		}
+		defer resp.Body.Close()
+		buf := bytes.NewBuffer(nil)
+		buf.ReadFrom(resp.Body)
+		if !strings.HasPrefix(buf.String(), "1.2.3.4:") {
+			t.Fatalf("bad body: %s", buf.String())
+		}
 	})
-	cluster.Start()
-	defer cluster.Cleanup()
-	client := cluster.Cores[0].Client
-
-	req := client.NewRequest("GET", "/")
-	_, err = client.RawRequest(req)
-	if err == nil {
-		t.Fatal("expected error")
-	}
-	if !strings.Contains(err.Error(), "missing x-forwarded-for") {
-		t.Fatalf("bad error message: %v", err)
-	}
-	req = client.NewRequest("GET", "/")
-	req.Headers = make(http.Header)
-	req.Headers.Set("x-forwarded-for", "1.2.3.4")
-	resp, err := client.RawRequest(req)
-	if err != nil {
-		t.Fatal(err)
-	}
-	defer resp.Body.Close()
-	buf := bytes.NewBuffer(nil)
-	buf.ReadFrom(resp.Body)
-	if !strings.HasPrefix(buf.String(), "1.2.3.4:") {
-		t.Fatalf("bad body: %s", buf.String())
-	}
 
 	// Next: test allow unauth
-	testHandler = func(c *vault.Core) http.Handler {
-		origHandler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			w.WriteHeader(http.StatusOK)
-			w.Write([]byte(r.RemoteAddr))
+	t.Run("allow_unauth", func(t *testing.T) {
+		t.Parallel()
+		testHandler := func(c *vault.Core) http.Handler {
+			origHandler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+				w.WriteHeader(http.StatusOK)
+				w.Write([]byte(r.RemoteAddr))
+			})
+			return WrapForwardedForHandler(origHandler, []*sockaddr.SockAddrMarshaler{
+				&sockaddr.SockAddrMarshaler{
+					SockAddr: badAddr,
+				},
+			}, true, false, 0)
+		}
+
+		cluster := vault.NewTestCluster(t, nil, &vault.TestClusterOptions{
+			HandlerFunc: testHandler,
 		})
-		return WrapForwardedForHandler(origHandler, []*sockaddr.SockAddrMarshaler{
-			&sockaddr.SockAddrMarshaler{
-				SockAddr: badAddr,
-			},
-		}, true, false, 0)
-	}
+		cluster.Start()
+		defer cluster.Cleanup()
+		client := cluster.Cores[0].Client
 
-	cluster = vault.NewTestCluster(t, nil, &vault.TestClusterOptions{
-		HandlerFunc: testHandler,
+		req := client.NewRequest("GET", "/")
+		req.Headers = make(http.Header)
+		req.Headers.Set("x-forwarded-for", "5.6.7.8")
+		resp, err := client.RawRequest(req)
+		if err != nil {
+			t.Fatal(err)
+		}
+		defer resp.Body.Close()
+		buf := bytes.NewBuffer(nil)
+		buf.ReadFrom(resp.Body)
+		if !strings.HasPrefix(buf.String(), "127.0.0.1:") {
+			t.Fatalf("bad body: %s", buf.String())
+		}
 	})
-	cluster.Start()
-	defer cluster.Cleanup()
-	client = cluster.Cores[0].Client
-
-	req = client.NewRequest("GET", "/")
-	req.Headers = make(http.Header)
-	req.Headers.Set("x-forwarded-for", "5.6.7.8")
-	resp, err = client.RawRequest(req)
-	if err != nil {
-		t.Fatal(err)
-	}
-	defer resp.Body.Close()
-	buf = bytes.NewBuffer(nil)
-	buf.ReadFrom(resp.Body)
-	if !strings.HasPrefix(buf.String(), "127.0.0.1:") {
-		t.Fatalf("bad body: %s", buf.String())
-	}
 
 	// Next: test fail unauth
-	testHandler = func(c *vault.Core) http.Handler {
-		origHandler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			w.WriteHeader(http.StatusOK)
-			w.Write([]byte(r.RemoteAddr))
+	t.Run("fail_unauth", func(t *testing.T) {
+		t.Parallel()
+		testHandler := func(c *vault.Core) http.Handler {
+			origHandler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+				w.WriteHeader(http.StatusOK)
+				w.Write([]byte(r.RemoteAddr))
+			})
+			return WrapForwardedForHandler(origHandler, []*sockaddr.SockAddrMarshaler{
+				&sockaddr.SockAddrMarshaler{
+					SockAddr: badAddr,
+				},
+			}, true, true, 0)
+		}
+
+		cluster := vault.NewTestCluster(t, nil, &vault.TestClusterOptions{
+			HandlerFunc: testHandler,
 		})
-		return WrapForwardedForHandler(origHandler, []*sockaddr.SockAddrMarshaler{
-			&sockaddr.SockAddrMarshaler{
-				SockAddr: badAddr,
-			},
-		}, true, true, 0)
-	}
+		cluster.Start()
+		defer cluster.Cleanup()
+		client := cluster.Cores[0].Client
 
-	cluster = vault.NewTestCluster(t, nil, &vault.TestClusterOptions{
-		HandlerFunc: testHandler,
+		req := client.NewRequest("GET", "/")
+		req.Headers = make(http.Header)
+		req.Headers.Set("x-forwarded-for", "5.6.7.8")
+		_, err = client.RawRequest(req)
+		if err == nil {
+			t.Fatal("expected error")
+		}
+		if !strings.Contains(err.Error(), "not authorized for x-forwarded-for") {
+			t.Fatalf("bad error message: %v", err)
+		}
 	})
-	cluster.Start()
-	defer cluster.Cleanup()
-	client = cluster.Cores[0].Client
 
-	req = client.NewRequest("GET", "/")
-	req.Headers = make(http.Header)
-	req.Headers.Set("x-forwarded-for", "5.6.7.8")
-	_, err = client.RawRequest(req)
-	if err == nil {
-		t.Fatal("expected error")
-	}
-	if !strings.Contains(err.Error(), "not authorized for x-forwarded-for") {
-		t.Fatalf("bad error message: %v", err)
-	}
+	// Next: test bad hops (too many)
+	t.Run("too_many_hops", func(t *testing.T) {
+		t.Parallel()
+		testHandler := func(c *vault.Core) http.Handler {
+			origHandler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+				w.WriteHeader(http.StatusOK)
+				w.Write([]byte(r.RemoteAddr))
+			})
+			return WrapForwardedForHandler(origHandler, []*sockaddr.SockAddrMarshaler{
+				&sockaddr.SockAddrMarshaler{
+					SockAddr: goodAddr,
+				},
+			}, true, true, 4)
+		}
+
+		cluster := vault.NewTestCluster(t, nil, &vault.TestClusterOptions{
+			HandlerFunc: testHandler,
+		})
+		cluster.Start()
+		defer cluster.Cleanup()
+		client := cluster.Cores[0].Client
+
+		req := client.NewRequest("GET", "/")
+		req.Headers = make(http.Header)
+		req.Headers.Set("x-forwarded-for", "2.3.4.5,3.4.5.6")
+		_, err = client.RawRequest(req)
+		if err == nil {
+			t.Fatal("expected error")
+		}
+		if !strings.Contains(err.Error(), "would skip before earliest") {
+			t.Fatalf("bad error message: %v", err)
+		}
+	})
+
+	// Next: test picking correct value
+	t.Run("correct_hop_skipping", func(t *testing.T) {
+		t.Parallel()
+		testHandler := func(c *vault.Core) http.Handler {
+			origHandler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+				w.WriteHeader(http.StatusOK)
+				w.Write([]byte(r.RemoteAddr))
+			})
+			return WrapForwardedForHandler(origHandler, []*sockaddr.SockAddrMarshaler{
+				&sockaddr.SockAddrMarshaler{
+					SockAddr: goodAddr,
+				},
+			}, true, true, 1)
+		}
+
+		cluster := vault.NewTestCluster(t, nil, &vault.TestClusterOptions{
+			HandlerFunc: testHandler,
+		})
+		cluster.Start()
+		defer cluster.Cleanup()
+		client := cluster.Cores[0].Client
+
+		req := client.NewRequest("GET", "/")
+		req.Headers = make(http.Header)
+		req.Headers.Set("x-forwarded-for", "2.3.4.5,3.4.5.6,4.5.6.7,5.6.7.8")
+		resp, err := client.RawRequest(req)
+		if err != nil {
+			t.Fatal(err)
+		}
+		defer resp.Body.Close()
+		buf := bytes.NewBuffer(nil)
+		buf.ReadFrom(resp.Body)
+		if !strings.HasPrefix(buf.String(), "4.5.6.7:") {
+			t.Fatalf("bad body: %s", buf.String())
+		}
+	})
+
+	// Next: multi-header approach
+	t.Run("correct_hop_skipping_multi_header", func(t *testing.T) {
+		t.Parallel()
+		testHandler := func(c *vault.Core) http.Handler {
+			origHandler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+				w.WriteHeader(http.StatusOK)
+				w.Write([]byte(r.RemoteAddr))
+			})
+			return WrapForwardedForHandler(origHandler, []*sockaddr.SockAddrMarshaler{
+				&sockaddr.SockAddrMarshaler{
+					SockAddr: goodAddr,
+				},
+			}, true, true, 1)
+		}
+
+		cluster := vault.NewTestCluster(t, nil, &vault.TestClusterOptions{
+			HandlerFunc: testHandler,
+		})
+		cluster.Start()
+		defer cluster.Cleanup()
+		client := cluster.Cores[0].Client
+
+		req := client.NewRequest("GET", "/")
+		req.Headers = make(http.Header)
+		req.Headers.Add("x-forwarded-for", "2.3.4.5")
+		req.Headers.Add("x-forwarded-for", "3.4.5.6")
+		req.Headers.Add("x-forwarded-for", "4.5.6.7")
+		req.Headers.Add("x-forwarded-for", "5.6.7.8")
+		resp, err := client.RawRequest(req)
+		if err != nil {
+			t.Fatal(err)
+		}
+		defer resp.Body.Close()
+		buf := bytes.NewBuffer(nil)
+		buf.ReadFrom(resp.Body)
+		if !strings.HasPrefix(buf.String(), "4.5.6.7:") {
+			t.Fatalf("bad body: %s", buf.String())
+		}
+	})
+
 }
