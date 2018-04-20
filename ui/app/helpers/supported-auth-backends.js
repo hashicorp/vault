7763007cf5521ba39dde91ef74e9c9a3600ba5c3
Matthew Irish <meirish@users.noreply.github.com>
2018-04-20 15:39:45 -0500

    UI - Login fix (#4403)
    * fix login
    
    * add tests for login submission

--- ui/app/helpers/supported-auth-backends.js
+++ ui/app/helpers/supported-auth-backends.js
@@ -6,30 +6,35 @@ const SUPPORTED_AUTH_BACKENDS = [
     description: 'Token authentication.',
     tokenPath: 'id',
     displayNamePath: 'display_name',
+    formAttributes: ['token'],
   },
   {
     type: 'userpass',
     description: 'A simple username and password backend.',
     tokenPath: 'client_token',
     displayNamePath: 'metadata.username',
+    formAttributes: ['username', 'password'],
   },
   {
     type: 'LDAP',
     description: 'LDAP authentication.',
     tokenPath: 'client_token',
     displayNamePath: 'metadata.username',
+    formAttributes: ['username', 'password'],
   },
   {
     type: 'Okta',
     description: 'Authenticate with your Okta username and password.',
     tokenPath: 'client_token',
     displayNamePath: 'metadata.username',
+    formAttributes: ['username', 'password'],
   },
   {
     type: 'GitHub',
     description: 'GitHub authentication.',
     tokenPath: 'client_token',
     displayNamePath: ['metadata.org', 'metadata.username'],
+    formAttributes: ['token'],
   },
 ];
 
