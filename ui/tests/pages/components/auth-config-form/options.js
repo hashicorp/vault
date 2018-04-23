ae5ad982acc1146fe7b58d2a63558e59b1bdca0d
Matthew Irish <matthew.irish@gmail.com>
2018-04-23 15:24:21 -0500

    fix typo

--- ui/tests/pages/components/auth-config-form/options.js
+++ ui/tests/pages/components/auth-config-form/options.js
@@ -3,7 +3,7 @@ import { clickable, fillable } from 'ember-cli-page-object';
 import fields from '../form-field';
 export default {
   ...fields,
-  ttlVaule: fillable('[data-test-ttl-value]'),
+  ttlValue: fillable('[data-test-ttl-value]'),
   ttlUnit: fillable('[data-test-ttl-value]'),
   save: clickable('[data-test-save-config]'),
 };
