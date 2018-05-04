a25ebb2457c0112bedd8b2a552b598d517b3a4e1
Matthew Irish <matthew.irish@gmail.com>
2018-05-03 22:48:57 -0500

    tests for console adapter

---
+++ ui/tests/unit/adapters/console-test.js
@@ -0,0 +1,15 @@
+import { moduleFor, test } from 'ember-qunit';
+import Ember from 'ember';
+
+moduleFor('adapter:console', 'Unit | Adapter | console', {
+  needs: ['service:auth', 'service:flash-messages', 'service:version'],
+});
+
+
+test('it builds the correct URL', function(assert) {
+  let adapter = this.subject();
+  let sysPath = 'sys/health';
+  let awsPath = 'aws/roles/my-other-role';
+  assert.equal(adapter.buildURL(sysPath), '/v1/sys/health');
+  assert.equal(adapter.buildURL(awsPath), '/v1/aws/roles/my-other-role');
+});
