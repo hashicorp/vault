d937cee28de04d4e16f15e869579667f8a2e3ade
Matthew Irish <matthew.irish@gmail.com>
2018-05-07 13:35:38 -0500

    add tests for processResponse

--- ui/tests/unit/components/console/ui-panel-test.js
+++ ui/tests/unit/components/console/ui-panel-test.js
@@ -113,10 +113,163 @@ test('#extractDataAndFlags', function(assert) {
 });
 
 
+let testResponseCases = [
+  {
+    name: 'write response, no content',
+    args: [null, 'vault write', 'foo/bar', 'write', {}],
+    expectedCommand: 'vault write',
+    expectedLogArgs: [
+      {
+        type: 'text',
+        content: 'Success! Data written to: foo/bar'
+      }
+    ]
+  },
+  {
+    name: 'delete response, no content',
+    args: [null, 'vault delete', 'foo/bar', 'delete', {}],
+    expectedCommand: 'vault delete',
+    expectedLogArgs: [
+      {
+        type: 'text',
+        content: 'Success! Data deleted (if it existed) at: foo/bar'
+      }
+    ]
+  },
+  {
+    name: 'write, with content',
+    args: [{data: {one: 'two'}}, 'vault write', 'foo/bar', 'write', {}],
+    expectedCommand: 'vault write',
+    expectedLogArgs: [
+      {
+        type: 'object',
+        content: { one: 'two'}
+      }
+    ]
+  },
+  {
+    name: 'with wrap-ttl flag',
+    args: [{wrap_info: {one: 'two'}}, 'vault read', 'foo/bar', 'read', {wrapTTL: '1h'}],
+    expectedCommand: 'vault read',
+    expectedLogArgs: [
+      {
+        type: 'object',
+        content: { one: 'two'}
+      }
+    ]
+  },
+  {
+    name: 'with -format=json flag and wrap-ttl flag',
+    args: [{foo: 'bar', wrap_info: {one: 'two'}}, 'vault read', 'foo/bar', 'read', {format: 'json', wrapTTL: '1h'}],
+    expectedCommand: 'vault read',
+    expectedLogArgs: [
+      {
+        type: 'json',
+        content: {foo: 'bar', wrap_info: {one: 'two'}}
+      }
+    ]
+  },
+  {
+    name: 'with -format=json and -field flags',
+    args: [{foo: 'bar', data: {one: 'two'}}, 'vault read', 'foo/bar', 'read', {format: 'json', field: 'one'}],
+    expectedCommand: 'vault read',
+    expectedLogArgs: [
+      {
+        type: 'text',
+        content:'two'
+      }
+    ]
+  },
+  {
+    name: 'with -format=json and -field, and -wrap-ttl flags',
+    args: [{foo: 'bar', wrap_info: {one: 'two'}}, 'vault read', 'foo/bar', 'read', {format: 'json', wrapTTL: '1h', field: 'one'}],
+    expectedCommand: 'vault read',
+    expectedLogArgs: [
+      {
+        type: 'text',
+        content: 'two'
+      }
+    ]
+  },
+  {
+    name: 'with string field flag and wrap-ttl flag',
+    args: [{foo: 'bar', wrap_info: {one: 'two'}}, 'vault read', 'foo/bar', 'read', {field: 'one', wrapTTL: '1h'}],
+    expectedCommand: 'vault read',
+    expectedLogArgs: [
+      {
+        type: 'text',
+        content: 'two'
+      }
+    ]
+  },
+  {
+    name: 'with object field flag and wrap-ttl flag',
+    args: [{foo: 'bar', wrap_info: {one: {two: 'three'}}}, 'vault read', 'foo/bar', 'read', {field: 'one', wrapTTL: '1h'}],
+    expectedCommand: 'vault read',
+    expectedLogArgs: [
+      {
+        type: 'object',
+        content: {two: 'three'}
+      }
+    ]
+  },
+  {
+    name: 'with response data and string field flag',
+    args: [{foo: 'bar', data: {one: 'two'}}, 'vault read', 'foo/bar', 'read', {field: 'one', wrapTTL: '1h'}],
+    expectedCommand: 'vault read',
+    expectedLogArgs: [
+      {
+        type: 'text',
+        content: 'two'
+      }
+    ]
+  },
+  {
+    name: 'with response data and object field flag ',
+    args: [{foo: 'bar', data: {one: {two: 'three'}}}, 'vault read', 'foo/bar', 'read', {field: 'one', wrapTTL: '1h'}],
+    expectedCommand: 'vault read',
+    expectedLogArgs: [
+      {
+        type: 'object',
+        content: {two: 'three'}
+      }
+    ]
+  },
+  {
+    name: 'response with data',
+    args: [{foo: 'bar', data: {one: 'two'}}, 'vault read', 'foo/bar', 'read', {}],
+    expectedCommand: 'vault read',
+    expectedLogArgs: [
+      {
+        type: 'object',
+        content: {one: 'two'}
+      }
+    ]
+  },
+  {
+    name: 'with response data, field flag, and field missing',
+    args: [{foo: 'bar', data: {one: 'two'}}, 'vault read', 'foo/bar', 'read', {field: 'foo'}],
+    expectedCommand: 'vault read',
+    expectedLogArgs: [
+      {
+        type: 'error',
+        content: 'Field "foo" not present in secret'
+      }
+    ]
+  },
+];
+
 test('#processResponse', function(assert) {
- let panel = this.subject();
+  let panel = this.subject({
+    appendToLog: sinon.spy()
+  });
+  testResponseCases.forEach(test => {
+    panel.processResponse(...test.args);
 
-})
-
-
-
+    let spy = panel.appendToLog;
+    let commandArgs = spy.getCall(spy.callCount - 2).args;
+    let appendArgs = spy.lastCall.args;
+    assert.deepEqual(commandArgs[0], {type: 'command', content: test.expectedCommand}, `${test.name}: calls appendToLog with the expected args`);
+    assert.deepEqual(appendArgs, test.expectedLogArgs, `${test.name}: calls appendToLog with the expected args`);
+  });
+});
