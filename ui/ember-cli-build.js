fbe0d8ad322a5a5e527350b1f178134d11a3fec0
Madalyn Parker <madalyn@Madalyns-MBP-2.hsd1.mi.comcast.net>
2018-05-08 13:13:57 -0400

    adding columnify and ember-cli-cjs-transform

--- ui/ember-cli-build.js
+++ ui/ember-cli-build.js
@@ -55,6 +55,12 @@ module.exports = function(defaults) {
   app.import('node_modules/text-encoder-lite/index.js');
   app.import('node_modules/Duration.js/duration.js');
 
+  app.import('node_modules/columnify/columnify.js', {
+    using: [
+      { transformation: 'cjs', as: 'columnify' }
+    ]
+  });
+
   // Use `app.import` to add additional libraries to the generated
   // output files.
   //
