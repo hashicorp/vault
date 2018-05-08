c41beebcb83015a21bee2eed15fdb344622a668f
Madalyn Parker <madalyn@Madalyns-MBP-2.hsd1.mi.comcast.net>
2018-05-08 13:14:30 -0400

    add components and templates to format different log types

--- ui/app/components/console/log-object.js
+++ ui/app/components/console/log-object.js
@@ -1,4 +1,16 @@
 import Ember from 'ember';
+import columnify from 'columnify';
+import { capitalize } from 'vault/helpers/capitalize';
+const { computed } = Ember;
 
 export default Ember.Component.extend({
+	content: null,
+	columns: computed('content', function(){
+		return columnify(this.get('content'), { 
+			preserveNewLines: true, 
+            headingTransform: function(heading) {
+              	return capitalize([heading]);
+            }
+		});
+	}),
 });
