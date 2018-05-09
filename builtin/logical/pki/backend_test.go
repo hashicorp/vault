187c051ef34872beed574c8c30580c8f28c538c4
Jeff Mitchell <jeff@hashicorp.com>
2018-05-09 10:29:54 -0400

    Update PKI to natively use time.Duration (#4493)
    * Update PKI to natively use time.Duration
    
    Among other things this now means PKI will output durations in seconds
    like other backends, instead of as Go strings.
    
    * Add a warning when refusing to blow away an existing root instead of just returning success
    
    * Fix another issue found while debugging this...
    
    The reason it wasn't caught on tests in the first place is that the ttl
    and max ttl were only being compared if in addition to a provided csr, a
    role was also provided. This was because the check was in the role !=
    nil block instead of outside of it. This has been fixed, which made the
    problem occur in all sign-verbatim cases and the changes in this PR have
    now verified the fix.

--- builtin/logical/pki/backend_test.go
+++ builtin/logical/pki/backend_test.go
@@ -1487,7 +1487,7 @@ func generateCATestingSteps(t *testing.T, caCert, caKey, otherCaCert string, int
 // Generates steps to test out various role permutations
 func generateRoleSteps(t *testing.T, useCSRs bool) []logicaltest.TestStep {
 	roleVals := roleEntry{
-		MaxTTL:    "12h",
+		MaxTTL:    12 * time.Hour,
 		KeyType:   "rsa",
 		KeyBits:   2048,
 		RequireCN: true,
@@ -1866,7 +1866,7 @@ func generateRoleSteps(t *testing.T, useCSRs bool) []logicaltest.TestStep {
 
 			issueTestStep.ErrorOk = !allowed
 
-			validity, _ := time.ParseDuration(roleVals.MaxTTL)
+			validity := roleVals.MaxTTL
 
 			var testBitSize int
 
@@ -2088,16 +2088,16 @@ func generateRoleSteps(t *testing.T, useCSRs bool) []logicaltest.TestStep {
 	{
 		roleTestStep.ErrorOk = true
 		roleVals.Lease = ""
-		roleVals.MaxTTL = ""
+		roleVals.MaxTTL = 0
 		addTests(nil)
 
 		roleVals.Lease = "12h"
-		roleVals.MaxTTL = "6h"
+		roleVals.MaxTTL = 6 * time.Hour
 		addTests(nil)
 
 		roleTestStep.ErrorOk = false
 		roleVals.TTL = ""
-		roleVals.MaxTTL = "12h"
+		roleVals.MaxTTL = 12 * time.Hour
 	}
 
 	// Listing test
