bf226590501c4373cc7f38b93218bc5782b641e8
Madalyn Parker <madalyn@Madalyns-MBP-2.hsd1.mi.comcast.net>
2018-05-10 12:40:08 -0400

    add integration test for cycling through commands in input

--- ui/tests/integration/components/console/ui-panel-test.js
+++ ui/tests/integration/components/console/ui-panel-test.js
@@ -43,7 +43,41 @@ test('it adds command to history on enter', function(assert) {
 
   component.consoleInput('list this/thing/here').enter();
   wait().then(() => component.up());
-  return wait().then(() => {
+  wait().then(() => {
     assert.equal(component.consoleInputValue, "list this/thing/here", 'populates console input with previous command on up after enter');
+  });
+  wait().then(() => component.down());
+  return wait().then(() => {
+    assert.equal(component.consoleInputValue, "", 'populates console input with next command on down');
+  });
+});
+
+test('it cycles through history with more than one command', function(assert) {
+
+  this.render(hbs`{{console/ui-panel}}`);
+
+  component.consoleInput('list this/thing/here').enter();
+  wait().then(() => component.consoleInput('read that/thing/there').enter());
+  wait().then(() => component.consoleInput('qwerty').enter());
+
+  wait().then(() => component.up());
+  wait().then(() => {
+    assert.equal(component.consoleInputValue, "qwerty", 'populates console input with previous command on up after enter');
+  });
+  wait().then(() => component.up());
+  wait().then(() => {
+    assert.equal(component.consoleInputValue, "read that/thing/there", 'populates console input with previous command on up');
+  });
+  wait().then(() => component.up());
+  wait().then(() => {
+    assert.equal(component.consoleInputValue, "list this/thing/here", 'populates console input with previous command on up');
+  });
+  wait().then(() => component.up());
+  wait().then(() => {
+    assert.equal(component.consoleInputValue, "qwerty", 'populates console input with initial command if cycled through all previous commands');
+  });
+  wait().then(() => component.down());
+  return wait().then(() => {
+    assert.equal(component.consoleInputValue, "", 'clears console input if down pressed after history is on most recent command');
   });
 });
