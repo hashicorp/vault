2ef345829b4641367e31c8b9038982b1128b9f76
Matthew Irish <matthew.irish@gmail.com>
2018-05-18 17:11:32 -0500

    move abstraction to secret-list-header and remove secret-tabs

---
+++ ui/app/components/secret-list-header.js
@@ -0,0 +1,14 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+  tagName: '',
+
+  // api
+  isCertTab: false,
+  isConfigure: false,
+  baseKey: null,
+  backendCrumb: null,
+  model: null,
+
+
+});
