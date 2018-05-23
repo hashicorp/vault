c8dcb683e5f5f38b45a79531bc7c6b7ac254b1e9
Yoko Hyakuna <yoko@hashicorp.com>
2018-05-23 12:56:33 -0700

    Added more steps

--- website/source/assets/javascripts/demo/initializer/load-steps.js
+++ website/source/assets/javascripts/demo/initializer/load-steps.js
@@ -13,8 +13,14 @@ Ember.Application.initializer({
         { id: 4, name: 'auth', humanName: "Step 4: Authorize your requests"},
         { id: 5, name: 'list', humanName: "Step 5: List available secret engines"},
         { id: 6, name: 'secrets', humanName: "Step 6: Read and write secrets"},
-        { id: 7, name: 'seal', humanName: "Step 7: Seal your Vault"},
-        { id: 8, name: 'finish', humanName: "You're finished!"},
+        { id: 7, name: 'update', humanName: "Step 7: Update the secret data"},
+        { id: 8, name: 'versions', humanName: "Step 8: Work with different data versions"},
+        { id: 9, name: 'delete', humanName: "Step 9: Delete the data"},
+        { id: 10, name: 'recover', humanName: "Step 10: Recover the deleted data"},
+        { id: 11, name: 'destroy', humanName: "Step 11: Permanently delete data"},
+        { id: 12, name: 'help', humanName: "Step 12: Get Help"},
+        { id: 13, name: 'seal', humanName: "Step 13: Seal your Vault"},
+        { id: 14, name: 'finish', humanName: "You're finished!"},
       ]
     };
 
