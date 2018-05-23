499b874f26d5f544288f102c92500bab6264bb34
Yoko Hyakuna <yoko@hashicorp.com>
2018-05-23 13:24:16 -0700

    Added more tutorial steps

--- website/source/assets/javascripts/demo/initializer/load-steps.js
+++ website/source/assets/javascripts/demo/initializer/load-steps.js
@@ -11,9 +11,16 @@ Ember.Application.initializer({
         { id: 2, name: 'init', humanName: "Step 2: Initialize your Vault"},
         { id: 3, name: 'unseal', humanName: "Step 3: Unsealing your Vault"},
         { id: 4, name: 'auth', humanName: "Step 4: Authorize your requests"},
-        { id: 5, name: 'secrets', humanName: "Step 6: Read and write secrets"},
-        { id: 6, name: 'seal', humanName: "Step 7: Seal your Vault"},
-        { id: 7, name: 'finish', humanName: "You're finished!"},
+        { id: 5, name: 'list', humanName: "Step 5: List available secret engines"},
+        { id: 6, name: 'secrets', humanName: "Step 6: Read and write secrets"},
+        { id: 7, name: 'update', humanName: "Step 7: Update the secret data"},
+        { id: 8, name: 'versions', humanName: "Step 8: Work with different data versions"},
+        { id: 9, name: 'delete', humanName: "Step 9: Delete the data"},
+        { id: 10, name: 'recover', humanName: "Step 10: Recover the deleted data"},
+        { id: 11, name: 'destroy', humanName: "Step 11: Permanently delete data"},
+        { id: 12, name: 'help', humanName: "Step 12: Get Help"},
+        { id: 13, name: 'seal', humanName: "Step 13: Seal your Vault"},
+        { id: 14, name: 'finish', humanName: "You're finished!"},
       ]
     };
 
