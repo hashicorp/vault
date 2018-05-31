7351865064a8f598dc71a222154b22f86c39b542
vishalnayak <vishalnayakv@gmail.com>
2018-05-31 10:08:49 -0400

    passthrough entity id

--- vault/router.go
+++ vault/router.go
@@ -435,17 +435,6 @@ func (r *Router) routeCommon(ctx context.Context, req *logical.Request, existenc
 	// Attach the storage view for the request
 	req.Storage = re.storageView
 
-	originalEntityID := req.EntityID
-
-	// Allow EntityID to passthrough to the system backend. This is required to
-	// allow clients to generate MFA credentials in respective entity objects
-	// in identity store via the system backend.
-	switch {
-	case strings.HasPrefix(originalPath, "sys/"):
-	default:
-		req.EntityID = ""
-	}
-
 	// Hash the request token unless the request is being routed to the token
 	// or system backend.
 	clientToken := req.ClientToken
@@ -513,8 +502,6 @@ func (r *Router) routeCommon(ctx context.Context, req *logical.Request, existenc
 		// This will be used for attaching the mount accessor for the identities
 		// returned by the authentication backends
 		req.MountAccessor = re.mountEntry.Accessor
-
-		req.EntityID = originalEntityID
 	}()
 
 	// Invoke the backend
