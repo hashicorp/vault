74862e8d06aa37c093fb9a7cbfe72b047635acea
Matthew Irish <matthew.irish@gmail.com>
2018-06-01 11:46:26 -0500

    acceptance test for refresh command

--- ui/tests/pages/secrets/backends.js
+++ ui/tests/pages/secrets/backends.js
@@ -1,6 +1,9 @@
 import { create, visitable, collection, clickable, text } from 'ember-cli-page-object';
+import uiPanel from 'vault/tests/pages/components/console/ui-panel';
 
 export default create({
+  console: uiPanel,
+  consoleToggle: clickable('[data-test-console-toggle]'),
   visit: visitable('/vault/secrets'),
   rows: collection({
     itemScope: '[data-test-secret-backend-row]',
