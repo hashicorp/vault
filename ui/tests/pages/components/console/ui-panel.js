1c6adc504d1b0a0eeb4b8e721f5e83bb710c450d
Matthew Irish <matthew.irish@gmail.com>
2018-06-07 18:15:43 -0500

    add acceptance test for redirect of control group

--- ui/tests/pages/components/console/ui-panel.js
+++ ui/tests/pages/components/console/ui-panel.js
@@ -1,10 +1,20 @@
-import { text, triggerable, fillable, value, isPresent } from 'ember-cli-page-object';
+import { text, triggerable, collection, fillable, value, isPresent } from 'ember-cli-page-object';
+import { getter } from 'ember-cli-page-object/macros';
+
 import keys from 'vault/lib/keycodes';
 
 export default {
   consoleInput: fillable('[data-test-component="console/command-input"] input'),
   consoleInputValue: value('[data-test-component="console/command-input"] input'),
   logOutput: text('[data-test-component="console/output-log"]'),
+  logOutputItems: collection('[data-test-component="console/output-log"] > div', {
+    text: text(),
+  }),
+  lastLogOutput: getter(function() {
+    let count = this.logOutputItems.length;
+    return this.logOutputItems.objectAt(count - 1).text;
+  }),
+
   up: triggerable('keyup', '[data-test-component="console/command-input"] input', {
     eventProperties: { keyCode: keys.UP },
   }),
@@ -15,4 +25,11 @@ export default {
     eventProperties: { keyCode: keys.ENTER },
   }),
   hasInput: isPresent('[data-test-component="console/command-input"] input'),
+  runCommands(commands) {
+    let toExecute = Array.isArray(commands) ? commands : [commands];
+    return toExecute.forEach(command => {
+      this.consoleInput(command);
+      this.enter();
+    });
+  }
 };
