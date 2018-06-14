f7b2a823ab4a93b6432fe4597fcbbcc9c0ab2d49
Matthew Irish <meirish@users.noreply.github.com>
2018-06-14 08:43:38 -0500

    Skip flakey UI test (#4762)
    * skip flakey ui test
    
    * only show failures in JS test output

--- ui/testem.js
+++ ui/testem.js
@@ -1,11 +1,11 @@
 /*jshint node:true*/
+
 module.exports = {
-  "framework": "qunit",
-  "test_page": "tests/index.html?hidepassed",
-  "disable_watching": true,
-  "launch_in_ci": [
-    "Chrome"
-  ],
+  framework: 'qunit',
+  test_page: 'tests/index.html?hidepassed',
+  tap_quiet_logs: true,
+  disable_watching: true,
+  launch_in_ci: ['Chrome'],
   browser_args: {
     Chrome: {
       mode: 'ci',
@@ -16,18 +16,17 @@ module.exports = {
         '--disable-gpu',
         '--headless',
         '--remote-debugging-port=0',
-        '--window-size=1440,900'
-      ].filter(Boolean)
-    }
+        '--window-size=1440,900',
+      ].filter(Boolean),
+    },
   },
-  "launch_in_dev": [
-    "Chrome"
-  ],
-  "on_exit": "[ -e ../../vault-ui-integration-server.pid ] && node ../../scripts/start-vault.js `cat ../../vault-ui-integration-server.pid`; [ -e ../../vault-ui-integration-server.pid ] && rm ../../vault-ui-integration-server.pid",
+  launch_in_dev: ['Chrome'],
+  on_exit:
+    '[ -e ../../vault-ui-integration-server.pid ] && node ../../scripts/start-vault.js `cat ../../vault-ui-integration-server.pid`; [ -e ../../vault-ui-integration-server.pid ] && rm ../../vault-ui-integration-server.pid',
 
   proxies: {
-    "/v1": {
-      "target": "http://localhost:9200"
-    }
+    '/v1': {
+      target: 'http://localhost:9200',
+    },
   },
 };
