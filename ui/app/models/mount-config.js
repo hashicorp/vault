4ecc1bfe298013fbcd2a23ef72ec90157724db3f
Matthew Irish <matthew.irish@gmail.com>
2018-06-27 00:46:38 -0500

    add new fields for auth methods

--- ui/app/models/mount-config.js
+++ ui/app/models/mount-config.js
@@ -10,4 +10,25 @@ export default Fragment.extend({
     label: 'Max Lease TTL',
     editType: 'ttl',
   }),
+  auditNonHmacRequestKeys: attr({
+    label: 'Request keys excluded from HMACing in audit',
+    editType: 'stringArray',
+    helpText: "Keys that will not be HMAC'd by audit devices in the request data object.",
+  }),
+  auditNonHmacResponseKeys: attr({
+    label: 'Response keys excluded from HMACing in audit',
+    editType: 'stringArray',
+    helpText: "Keys that will not be HMAC'd by audit devices in the response data object.",
+  }),
+  listingVisibility: attr('string', {
+    editType: 'boolean',
+    label: 'List method when unauthenticated',
+    trueValue: 'unauth',
+    falseValue: '',
+  }),
+  passthroughRequestHeaders: attr({
+    label: 'Allowed Passthrough Request headers',
+    helpText: 'Headers to whitelist and pass from the request to the backend',
+    editType: 'stringArray',
+  }),
 });
