87d70fbe5b6521eaf7f99a723230bd3c87ac8433
Matthew Irish <meirish@users.noreply.github.com>
2018-07-05 13:28:12 -0500

    UI - unauthed login methods (#4854)
    * fetch auth methods when going to the auth route and pass them to the auth form component
    
    * add boolean editType for form-fields
    
    * look in the data hash in the serializer
    
    * remove renderInPlace for info-tooltips as it does something goofy with widths
    
    * add new fields for auth methods
    
    * fix console refresh command on routes that use lazyPaginatedQuery
    
    * add wrapped_token param that logs you in via the token backend and show other backends if your list contains supported ones
    
    * handle casing when looking up supported backends
    
    * change listingVisibility to match the new API
    
    * move wrapped_token up to the vault route level so it works from the app root

--- ui/app/helpers/nav-to-route.js
+++ ui/app/helpers/nav-to-route.js
@@ -3,11 +3,11 @@ import Ember from 'ember';
 const { Helper, inject } = Ember;
 
 export default Helper.extend({
-  routing: inject.service('-routing'),
+  router: inject.service(),
 
   compute([routeName, ...models], { replace = false }) {
     return () => {
-      const router = this.get('routing.router');
+      const router = this.get('router');
       const method = replace ? router.replaceWith : router.transitionTo;
       return method.call(router, routeName, ...models);
     };
--- ui/app/routes/vault/cluster/auth.js
+++ ui/app/routes/vault/cluster/auth.js
@@ -1,1 +1,29 @@
-export { default } from './cluster-route-base';
+import ClusterRouteBase from './cluster-route-base';
+import Ember from 'ember';
+
+const { RSVP } = Ember;
+
+export default ClusterRouteBase.extend({
+  beforeModel() {
+    return this.store.unloadAll('auth-method');
+  },
+  model() {
+    let cluster = this._super(...arguments);
+    return this.store
+      .findAll('auth-method', {
+        adapterOptions: {
+          unauthenticated: true,
+        },
+      })
+      .then(result => {
+        return RSVP.hash({
+          cluster,
+          methods: result,
+        });
+      });
+  },
+  resetController(controller) {
+    controller.set('wrappedToken', '');
+    controller.set('authMethod', '');
+  },
+});
