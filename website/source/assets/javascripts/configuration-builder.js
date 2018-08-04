9d3e5db2e56cd534cf53ee156067f4ba288be0a4
Joshua Ogle <joshua@joshuaogle.com>
2018-08-03 22:57:17 -0600

    JS Feedback fixes, make UI default on click

--- website/source/assets/javascripts/configuration-builder.js
+++ website/source/assets/javascripts/configuration-builder.js
@@ -4,44 +4,44 @@ function downloadConfiguration() {
 
   // Add Listener stanza
   if (document.getElementById("include_tcp_listener").checked) {
-    config += `listener "tcp" {
-${addFieldsToStanza("listener")}}
-`;
+    config += 'listener "tcp" {
+' + addFieldsToStanza("listener") + '}
+';
   }
 
   // Add Storage stanza
   if (document.getElementById("include_storage").checked) {
     var backend = document.getElementById("storage").value;
-    config += `
+    config += '
 storage "${backend}" {
-${addFieldsToStanza("storage")}}
-`;
+' + addFieldsToStanza("storage") + '}
+';
   }
 
   // Add Telemetry stanza
   if (document.getElementById("include_telemetry").checked) {
     var provider = document.getElementById("telemetry").value;
-    config += `
+    config += '
 telemetry {
-${addFieldsToStanza("telemetry")}}
-`;
+' + addFieldsToStanza("telemetry") + '}
+';
   }
 
   // Add Seal stanza
   if (document.getElementById("include_seal").checked) {
     var type = document.getElementById("seal").value;
-    config += `
-seal "${type}" {
-${addFieldsToStanza("seal")}}
-`;
+    config += '
+seal "' + type + '" {
+' + addFieldsToStanza("seal") + '}
+';
   }
 
   // Add UI stanza
   if (document.getElementById("include_ui").checked) {
-    config += `
-ui = true`;
+    config += '
+ui = true';
     var startServerLink = document.querySelector(".start-server-link")
-    startServerLink.href = `${startServerLink.href}?tab=ui`;
+    startServerLink.href = startServerLink.href + "?tab=ui";
   }
 
   config = config.replace(/([^\r])\n/g, "$1\r\n");
@@ -49,11 +49,10 @@ ui = true`;
   saveAs(blob, "vault-config.hcl");
   document.querySelector(".form-actions").style.display = "none";
   document.querySelector("#download-confirm").style.display = "block";
-  return false;
 }
 
 function addFieldsToStanza(stanza) {
-  var fieldsets = document.querySelectorAll(`[data-config-stanza="${stanza}"] .nested-fields fieldset`);
+  var fieldsets = document.querySelectorAll('[data-config-stanza="' + stanza + '"] .nested-fields fieldset');
   var lines = "";
 
   for (i = 0; i < fieldsets.length; i++) {
@@ -70,17 +69,17 @@ function addFieldsToStanza(stanza) {
 
 function fieldsetToLine(fieldset) {
   var parameter = fieldset.getAttribute("name");
-  var isChecked = document.querySelector(`#include_${parameter}`).checked;
+  var isChecked = document.querySelector("#include_" + parameter).checked;
   if (isChecked) {
-    var field = fieldset.querySelector(`#${parameter}`);
+    var field = fieldset.querySelector("#" + parameter);
     var value = field.value;
 
     if (field.getAttribute("type") == "number") {
-      return `  ${parameter} = ${value}
-`;
+      return '  ' + parameter + ' = ' + value + '
+';
     } else {
-      return `  ${parameter} = "${value}"
-`;
+      return '  ' + parameter + ' = "' + value + '"
+';
     }
   }
   return;
@@ -108,7 +107,7 @@ document.addEventListener("turbolinks:load", function() {
 
       if (reveal.querySelector(".config-reveal-select")) {
         var selection = reveal.querySelector(".config-reveal-select").value;
-        document.querySelector(`[data-if-option="${selection}"]`).classList.toggle("active");
+        document.querySelector('[data-if-option="' + selection + '"]').classList.toggle("active");
       }
     });
   }
@@ -118,7 +117,7 @@ document.addEventListener("turbolinks:load", function() {
       var configSelect = clickEvent.currentTarget;
       var selection = configSelect.value;
       var section = configSelect.closest("section");
-      var reveal = section.querySelector(`[data-if-option='${selection}']`);
+      var reveal = section.querySelector('[data-if-option="' + selection + ']');
       var nestedOptions = section.querySelectorAll("[data-if-option]");
 
       for (i = 0; i < nestedOptions.length; i++) {
