7fbdbe7d933a32825c7144f76b564dfad7f6663f
Jim Kalafut <jim@kalafut.net>
2018-08-06 09:02:04 -0700

    Add plugin user-agent helper (#5039)

--- helper/useragent/useragent_test.go
+++ helper/useragent/useragent_test.go
@@ -2,6 +2,8 @@ package useragent
 
 import (
 	"testing"
+
+	"github.com/hashicorp/vault/logical"
 )
 
 func TestUserAgent(t *testing.T) {
@@ -12,6 +14,30 @@ func TestUserAgent(t *testing.T) {
 	act := String()
 
 	exp := "Vault/1.2.3 (+https://vault-test.com; go5.0)"
+	if exp != act {
+		t.Errorf("expected %q to be %q", act, exp)
+	}
+}
+
+func TestUserAgentPlugin(t *testing.T) {
+	projectURL = "https://vault-test.com"
+	rt = "go5.0"
+	env := &logical.PluginEnvironment{
+		VaultVersion: "1.2.3",
+	}
+	pluginName := "azure-auth"
+
+	act := PluginString(env, pluginName)
+
+	exp := "Vault/1.2.3 (+https://vault-test.com; azure-auth; go5.0)"
+	if exp != act {
+		t.Errorf("expected %q to be %q", act, exp)
+	}
+
+	pluginName = ""
+	act = PluginString(env, pluginName)
+
+	exp = "Vault/1.2.3 (+https://vault-test.com; go5.0)"
 	if exp != act {
 		t.Errorf("expected %q to be %q", act, exp)
 	}
