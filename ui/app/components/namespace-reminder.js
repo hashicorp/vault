2112000d7e15892265b295fc8df6192b7646ec26
Matthew Irish <matthew.irish@gmail.com>
2018-08-07 22:39:32 -0500

    fill out namespace-reminder component

--- ui/app/components/namespace-reminder.js
+++ ui/app/components/namespace-reminder.js
@@ -5,4 +5,14 @@ const { Component, inject, computed } = Ember;
 export default Component.extend({
   namespace: inject.service(),
   showMessage: computed.not('namespace.inRootNamespace'),
+  //public API
+  noun: null,
+  mode: 'edit',
+  modeVerb: computed(function() {
+    let mode = this.get('mode');
+    if (!mode) {
+      return '';
+    }
+    return mode.endsWith('e') ? `${mode}d` : `${mode}ed`;
+  }),
 });
