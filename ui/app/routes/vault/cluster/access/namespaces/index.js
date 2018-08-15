339e1567657b08c8101b3ebcb735577a74e4144e
Matthew Irish <matthew.irish@gmail.com>
2018-08-15 00:24:51 -0500

    return a different model if we dont have the namespaces feature and show upgrade page

--- ui/app/routes/vault/cluster/access/namespaces/index.js
+++ ui/app/routes/vault/cluster/access/namespaces/index.js
@@ -1,16 +1,24 @@
 import Ember from 'ember';
 import UnloadModel from 'vault/mixins/unload-model-route';
 
+const { inject } = Ember;
+
 export default Ember.Route.extend(UnloadModel, {
+  version: inject.service(),
   beforeModel() {
-    return this.store.unloadAll('namespace');
+    this.store.unloadAll('namespace');
+    return this.get('version').fetchFeatures().then(() => {
+      return this._super(...arguments);
+    });
   },
   model() {
-    return this.store.findAll('namespace').catch(e => {
-      if (e.httpStatus === 404) {
-        return [];
-      }
-      throw e;
-    });
+    return this.get('version.hasNamespaces')
+      ? this.store.findAll('namespace').catch(e => {
+          if (e.httpStatus === 404) {
+            return [];
+          }
+          throw e;
+        })
+      : null;
   },
 });
