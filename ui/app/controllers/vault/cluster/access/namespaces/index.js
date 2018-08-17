8c94775092b37ab2a70d790daa4ef9d34b151987
Matthew Irish <matthew.irish@gmail.com>
2018-08-17 00:13:30 -0500

    add switch link on namespace link page if user has access to the namespace

---
+++ ui/app/controllers/vault/cluster/access/namespaces/index.js
@@ -0,0 +1,8 @@
+import Ember from 'ember';
+
+const { computed, inject, Controller } = Ember;
+export default Controller.extend({
+  namespaceService: inject.service('namespace'),
+  accessibleNamespaces: computed.alias('namespaceService.accessibleNamespaces'),
+  currentNamespace: computed.alias('namespaceService.path'),
+});
