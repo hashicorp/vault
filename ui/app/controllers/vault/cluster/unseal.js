f2f9f9eab8f5d2e2de64a9a8582f7eb2c62dd43a
Matthew Irish <matthew.irish@gmail.com>
2018-08-17 17:44:24 -0500

    add hooks for unseal and setting component state on the service

--- ui/app/controllers/vault/cluster/unseal.js
+++ ui/app/controllers/vault/cluster/unseal.js
@@ -5,12 +5,16 @@ export default Ember.Controller.extend({
 
   actions: {
     transitionToCluster(resp) {
-      debugger;
       return this.get('model').reload().then(() => {
         this.get('wizard').transitionTutorialMachine(this.get('wizard.currentState'), 'CONTINUE', resp);
         return this.transitionToRoute('vault.cluster', this.get('model.name'));
       });
     },
+
+    setUnsealState(resp) {
+      this.get('wizard').set('componentState', resp);
+    },
+
     isUnsealed(data) {
       return data.sealed === false;
     },
