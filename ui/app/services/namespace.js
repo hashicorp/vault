d6ab7bcd54702c89dcd5dc6f5dfd2bc6eb3a417b
Matthew Irish <meirish@users.noreply.github.com>
2018-08-22 11:13:28 -0500

    UI namespace additions (#5150)
    * add switch link on namespace link page if user has access to the namespace
    * refresh list when you delete, only show manage if you can list
    * fix bug where disconnected namespaces wouldn't show the picker properly
    * namespaces list should end in a slash
    * end full namespace paths with a /
    * shorten pop up menu link

--- ui/app/services/namespace.js
+++ ui/app/services/namespace.js
@@ -1,5 +1,6 @@
 import Ember from 'ember';
 import { task } from 'ember-concurrency';
+import lazyCapabilities, { apiPath } from 'vault/macros/lazy-capabilities';
 
 const { Service, computed, inject } = Ember;
 const ROOT_NAMESPACE = '';
@@ -18,6 +19,8 @@ export default Service.extend({
   setNamespace(path) {
     this.set('path', path);
   },
+  listPath: lazyCapabilities(apiPath`sys/namespaces/`, 'path'),
+  canList: computed.alias('listPath.canList'),
 
   findNamespacesForUser: task(function*() {
     // uses the adapter and the raw response here since
@@ -25,14 +28,28 @@ export default Service.extend({
     // want to keep track of these separately
     let store = this.get('store');
     let adapter = store.adapterFor('namespace');
+    let userRoot = this.get('auth.authData.userRootNamespace');
     try {
       let ns = yield adapter.findAll(store, 'namespace', null, {
         adapterOptions: {
           forUser: true,
-          namespace: this.get('userRootNamespace'),
+          namespace: userRoot,
         },
       });
-      this.set('accessibleNamespaces', ns.data.keys.map(n => n.replace(/\/$/, '')));
+      this.set(
+        'accessibleNamespaces',
+        ns.data.keys.map(n => {
+          let fullNS = n;
+          // if the user's root isn't '', then we need to construct
+          // the paths so they connect to the user root to the list
+          // otherwise using the current ns to grab the correct leaf
+          // node in the graph doesn't work
+          if (userRoot) {
+            fullNS = `${userRoot}/${n}`;
+          }
+          return fullNS.replace(/\/$/, '');
+        })
+      );
     } catch (e) {
       //do nothing here
     }
