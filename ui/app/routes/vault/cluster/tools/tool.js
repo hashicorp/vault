93b1d6b8df1d16faaaa20ca62f3f7e15b4f4866f
Madalyn Parker <madalyn@hashicorp.com>
2018-08-23 12:17:48 -0400

    hook up tools machine transitions

--- ui/app/routes/vault/cluster/tools/tool.js
+++ ui/app/routes/vault/cluster/tools/tool.js
@@ -2,6 +2,8 @@ import Ember from 'ember';
 import { toolsActions } from 'vault/helpers/tools-actions';
 
 export default Ember.Route.extend({
+  wizard: Ember.inject.service(),
+
   beforeModel(transition) {
     const supportedActions = toolsActions();
     const { selectedAction } = this.paramsFor(this.routeName);
@@ -14,6 +16,10 @@ export default Ember.Route.extend({
   actions: {
     didTransition() {
       const params = this.paramsFor(this.routeName);
+      this.get('wizard').transitionFeatureMachine(
+        this.get('wizard.featureState'),
+        params.selectedAction.toUpperCase()
+      );
       this.controller.setProperties(params);
     },
   },
