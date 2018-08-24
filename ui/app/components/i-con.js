5fe262f19a3a3d65d11c2919991e385f96025272
Matthew Irish <matthew.irish@gmail.com>
2018-08-24 00:46:19 -0500

    add partner icon svgs and change i-con component to render them at 48px

--- ui/app/components/i-con.js
+++ ui/app/components/i-con.js
@@ -44,11 +44,12 @@ export default Ember.Component.extend({
   glyph: null,
 
   excludeSVG: computed('glyph', function() {
-    return GLYPHS_WITH_SVG_TAG.includes(this.get('glyph'));
+    let glyph = this.get('glyph');
+    return glyph.startsWith('enable/') || GLYPHS_WITH_SVG_TAG.includes(glyph);
   }),
 
-  size: computed(function() {
-    return 12;
+  size: computed('glyph', function() {
+    return this.get('glyph').startsWith('enable/') ? 48 : 12;
   }),
 
   partialName: computed('glyph', function() {
