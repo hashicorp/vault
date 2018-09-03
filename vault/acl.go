ede8db07200f8ce059e1037590f3a39c941a96eb
Chris Hoffman <choffman@hashicorp.com>
2018-09-03 09:43:25 -0400

    fixing capabilities check for templated policies

--- vault/acl.go
+++ vault/acl.go
@@ -231,10 +231,8 @@ func (a *ACL) Capabilities(path string) (pathCapabilities []string) {
 	_, raw, ok = a.globRules.LongestPrefix(path)
 	if !ok {
 		return []string{DenyCapability}
-	} else {
-		perm := raw.(*ACLPermissions)
-		capabilities = perm.CapabilitiesBitmap
 	}
+	capabilities = raw.(*ACLPermissions).CapabilitiesBitmap
 
 CHECK:
 	if capabilities&SudoCapabilityInt > 0 {
