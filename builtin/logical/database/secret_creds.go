90ec5d57954270a16053797421a2e19f5875c3a3
Jeff Mitchell <jeffrey.mitchell@gmail.com>
2018-09-04 13:48:13 -0400

    Add a comment

--- builtin/logical/database/secret_creds.go
+++ builtin/logical/database/secret_creds.go
@@ -111,6 +111,9 @@ func (b *databaseBackend) secretCredsRevoke() framework.OperationFunc {
 			if statementsRaw, ok := req.Secret.InternalData["revocation_statements"]; !ok {
 				return nil, fmt.Errorf("error during revoke: could not find role with name %q or embedded revocation statement data", req.Secret.InternalData["role"])
 			} else {
+				// If we don't actually have any statements, because none were
+				// set in the role, we'll end up with an empty one and the
+				// default for the db type will be attempted
 				if statementsRaw != nil {
 					statementsSlice, ok := statementsRaw.([]interface{})
 					if !ok {
