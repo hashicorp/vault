6a07f917c8ce4f8b180d846308bd08a0f5195b75
Madalyn Parker <madalyn@hashicorp.com>
2018-09-06 17:02:50 -0400

    add tests for replication machine

---
+++ ui/tests/unit/machines/replication-machine-test.js
@@ -0,0 +1,41 @@
+import { Machine } from 'xstate';
+import { moduleFor, test } from 'ember-qunit';
+import ReplicationMachineConfig from 'vault/machines/replication-machine';
+
+moduleFor('machine:replication-machine', 'Unit | Machine | replication-machine', {
+  beforeEach() {},
+  afterEach() {},
+});
+
+const replicationMachine = Machine(ReplicationMachineConfig);
+
+const testCases = [
+  {
+    currentState: replicationMachine.initialState,
+    event: 'ENABLEREPLICATION',
+    params: null,
+    expectedResults: {
+      value: 'details',
+      actions: [{ type: 'render', level: 'feature', component: 'wizard/replication-details' }],
+    },
+  },
+  {
+    currentState: 'details',
+    event: 'CONTINUE',
+    params: null,
+    expectedResults: {
+      value: 'complete',
+      actions: ['completeFeature'],
+    },
+  },
+];
+
+testCases.forEach(testCase => {
+  test(`transition: ${testCase.event} for currentState ${testCase.currentState} and componentState ${testCase.params}`, function(
+    assert
+  ) {
+    let result = replicationMachine.transition(testCase.currentState, testCase.event, testCase.params);
+    assert.equal(result.value, testCase.expectedResults.value);
+    assert.deepEqual(result.actions, testCase.expectedResults.actions);
+  });
+});
