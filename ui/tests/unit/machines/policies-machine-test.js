f56486b0221cf042bebfefa4fdf6645496b203ce
Madalyn Parker <madalyn@hashicorp.com>
2018-09-06 16:58:53 -0400

    add tests for policies machine

--- ui/tests/unit/machines/policies-machine-test.js
+++ ui/tests/unit/machines/policies-machine-test.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import { module, test } from 'qunit';
 import { Machine } from 'xstate';
 import PoliciesMachineConfig from 'vault/machines/policies-machine';
@@ -58,5 +59,70 @@ module('Unit | Machine | policies-machine', function() {
       assert.equal(result.value, testCase.expectedResults.value);
       assert.deepEqual(result.actions, testCase.expectedResults.actions);
     });
+=======
+import { Machine } from 'xstate';
+import { moduleFor, test } from 'ember-qunit';
+import AuthMachineConfig from 'vault/machines/policies-machine';
+
+moduleFor('machine:policies-machine', 'Unit | Machine | policies-machine', {
+  beforeEach() {},
+  afterEach() {},
+});
+
+const policiesMachine = Machine(AuthMachineConfig);
+
+const testCases = [
+  {
+    currentState: policiesMachine.initialState,
+    event: 'CONTINUE',
+    params: null,
+    expectedResults: {
+      value: 'create',
+      actions: [{ component: 'wizard/policies-create', level: 'step', type: 'render' }],
+    },
+  },
+  {
+    currentState: 'create',
+    event: 'CONTINUE',
+    params: null,
+    expectedResults: {
+      value: 'details',
+      actions: [{ component: 'wizard/policies-details', level: 'step', type: 'render' }],
+    },
+  },
+  {
+    currentState: 'details',
+    event: 'CONTINUE',
+    expectedResults: {
+      value: 'delete',
+      actions: [{ component: 'wizard/policies-delete', level: 'step', type: 'render' }],
+    },
+  },
+  {
+    currentState: 'delete',
+    event: 'CONTINUE',
+    expectedResults: {
+      value: 'others',
+      actions: [{ component: 'wizard/policies-others', level: 'step', type: 'render' }],
+    },
+  },
+  {
+    currentState: 'others',
+    event: 'CONTINUE',
+    expectedResults: {
+      value: 'complete',
+      actions: ['completeFeature'],
+    },
+  },
+];
+
+testCases.forEach(testCase => {
+  test(`transition: ${testCase.event} for currentState ${testCase.currentState} and componentState ${testCase.params}`, function(
+    assert
+  ) {
+    let result = policiesMachine.transition(testCase.currentState, testCase.event, testCase.params);
+    assert.equal(result.value, testCase.expectedResults.value);
+    assert.deepEqual(result.actions, testCase.expectedResults.actions);
+>>>>>>> add tests for policies machine
   });
 });
