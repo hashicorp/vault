5c0758cdfcdd37ae72b2429ae012b51e90549f9f
Madalyn Parker <madalyn@hashicorp.com>
2018-09-06 17:12:35 -0400

    tests for tools machine

--- ui/tests/unit/machines/replication-machine-test.js
+++ ui/tests/unit/machines/replication-machine-test.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { module, test } from 'qunit';
 import { Machine } from 'xstate';
 import ReplicationMachineConfig from 'vault/machines/replication-machine';
@@ -35,46 +34,5 @@ module('Unit | Machine | replication-machine', function() {
       assert.equal(result.value, testCase.expectedResults.value);
       assert.deepEqual(result.actions, testCase.expectedResults.actions);
     });
-=======
-import { Machine } from 'xstate';
-import { moduleFor, test } from 'ember-qunit';
-import ReplicationMachineConfig from 'vault/machines/replication-machine';
-
-moduleFor('machine:replication-machine', 'Unit | Machine | replication-machine', {
-  beforeEach() {},
-  afterEach() {},
-});
-
-const replicationMachine = Machine(ReplicationMachineConfig);
-
-const testCases = [
-  {
-    currentState: replicationMachine.initialState,
-    event: 'ENABLEREPLICATION',
-    params: null,
-    expectedResults: {
-      value: 'details',
-      actions: [{ type: 'render', level: 'feature', component: 'wizard/replication-details' }],
-    },
-  },
-  {
-    currentState: 'details',
-    event: 'CONTINUE',
-    params: null,
-    expectedResults: {
-      value: 'complete',
-      actions: ['completeFeature'],
-    },
-  },
-];
-
-testCases.forEach(testCase => {
-  test(`transition: ${testCase.event} for currentState ${testCase.currentState} and componentState ${testCase.params}`, function(
-    assert
-  ) {
-    let result = replicationMachine.transition(testCase.currentState, testCase.event, testCase.params);
-    assert.equal(result.value, testCase.expectedResults.value);
-    assert.deepEqual(result.actions, testCase.expectedResults.actions);
->>>>>>> add tests for replication machine
   });
 });
