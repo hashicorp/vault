acae415b1897ff2a14f008255b5d4e57467b3078
Madalyn Parker <madalyn@hashicorp.com>
2018-09-07 14:27:43 -0400

    add more tests for wizard service

--- ui/app/machines/policies-machine.js
+++ ui/app/machines/policies-machine.js
@@ -15,25 +15,25 @@ export default {
       on: {
         CONTINUE: 'details',
       },
-      onEntry: [{ type: 'render', level: 'step', component: 'wizard/policies-create' }],
+      onEntry: [{ type: 'render', level: 'feature', component: 'wizard/policies-create' }],
     },
     details: {
       on: {
         CONTINUE: 'delete',
       },
-      onEntry: [{ type: 'render', level: 'step', component: 'wizard/policies-details' }],
+      onEntry: [{ type: 'render', level: 'feature', component: 'wizard/policies-details' }],
     },
     delete: {
       on: {
         CONTINUE: 'others',
       },
-      onEntry: [{ type: 'render', level: 'step', component: 'wizard/policies-delete' }],
+      onEntry: [{ type: 'render', level: 'feature', component: 'wizard/policies-delete' }],
     },
     others: {
       on: {
         CONTINUE: 'complete',
       },
-      onEntry: [{ type: 'render', level: 'step', component: 'wizard/policies-others' }],
+      onEntry: [{ type: 'render', level: 'feature', component: 'wizard/policies-others' }],
     },
     complete: {
       onEntry: ['completeFeature'],
