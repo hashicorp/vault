7a45e84f185c2d6649b9f7752d422ba250b1cbc0
Matthew Irish <matthew.irish@gmail.com>
2018-09-14 22:29:57 -0500

    reduce flash timeout in test

--- ui/tests/acceptance/console-test.js
+++ ui/tests/acceptance/console-test.js
@@ -11,18 +11,15 @@ module('Acceptance | console', function(hooks) {
   });
 
   test("refresh reloads the current route's data", async function(assert) {
-    let numEngines;
     await enginesPage.visit();
-    numEngines = enginesPage.rows.length;
+    let numEngines = enginesPage.rows.length;
     await enginesPage.consoleToggle();
     let now = Date.now();
     for (let num of [1, 2, 3]) {
       let inputString = `write sys/mounts/${now + num} type=kv`;
-      await enginesPage.console.consoleInput(inputString);
-      await enginesPage.console.enter();
+      await enginesPage.console.runCommands(inputString);
     }
-    await enginesPage.console.consoleInput('refresh');
-    await enginesPage.console.enter();
+    await enginesPage.console.runCommands('refresh');
     assert.equal(enginesPage.rows.length, numEngines + 3, 'new engines were added to the page');
   });
 });
