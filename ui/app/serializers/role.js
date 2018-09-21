1dfb041daa936bc20efc2053acbf7a6f498dc14b
Matthew Irish <matthew.irish@gmail.com>
2018-09-21 11:25:50 -0500

    move roles to the application serializer and fix up the role-ssh adapter

--- ui/app/serializers/role.js
+++ ui/app/serializers/role.js
@@ -1,13 +1,6 @@
-import { isNone, isBlank } from '@ember/utils';
-import { assign } from '@ember/polyfills';
-import { decamelize } from '@ember/string';
-import DS from 'ember-data';
+import ApplicationSerializer from './application';
 
-export default DS.RESTSerializer.extend({
-  keyForAttribute: function(attr) {
-    return decamelize(attr);
-  },
-
+export default ApplicationSerializer.extend({
   extractLazyPaginatedData(payload) {
     let ret;
     if (payload.zero_address_roles) {
@@ -29,53 +22,5 @@ export default DS.RESTSerializer.extend({
     });
     delete payload.data.key_info;
     return ret;
-  },
-
-  normalizeItems(payload) {
-    if (payload.data.keys && Array.isArray(payload.data.keys)) {
-      return payload.data.keys;
-    }
-    assign(payload, payload.data);
-    delete payload.data;
-    return [payload];
-  },
-  modelNameFromPayloadKey(payloadType) {
-    return payloadType;
-  },
-
-  normalizeResponse(store, primaryModelClass, payload, id, requestType) {
-    const nullResponses = ['updateRecord', 'createRecord', 'deleteRecord'];
-    const responseJSON = nullResponses.includes(requestType) ? { id } : this.normalizeItems(payload);
-    const { modelName } = primaryModelClass;
-    let transformedPayload = { [modelName]: responseJSON };
-    // just return the single object because ember is picky
-    if (requestType === 'queryRecord') {
-      transformedPayload = { [modelName]: responseJSON[0] };
-    }
-
-    return this._super(store, primaryModelClass, transformedPayload, id, requestType);
-  },
-
-  serializeAttribute(snapshot, json, key, attributes) {
-    const val = snapshot.attr(key);
-    if (attributes.options.readOnly) {
-      return;
-    }
-    if (
-      attributes.type === 'object' &&
-      val &&
-      Object.keys(val).length > 0 &&
-      isNone(snapshot.changedAttributes()[key])
-    ) {
-      return;
-    }
-    if (isBlank(val) && isNone(snapshot.changedAttributes()[key])) {
-      return;
-    }
-
-    this._super(snapshot, json, key, attributes);
-  },
-  serialize() {
-    return this._super(...arguments);
   },
 });
