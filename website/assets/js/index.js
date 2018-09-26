52e8f9980877473532a7530522aa97bd54c7cb5f
Jeff Escalante <gh.je@mailhero.io>
2018-09-26 15:48:29 -0400

    add new mega nav

--- website/assets/js/index.js
+++ website/assets/js/index.js
@@ -6,11 +6,13 @@ import footer from '@hashicorp/hashi-footer'
 import newsletterSignupForm from '@hashicorp/hashi-newsletter-signup-form'
 import productSubnav from '@hashicorp/hashi-product-subnav'
 import docsSidebar from './components/docs-sidebar'
+import megaNav from '@hashicorp/hashi-mega-nav'
 
 const components = initializeComponents({
   nav,
   footer,
   newsletterSignupForm,
   docsSidebar,
-  productSubnav
+  productSubnav,
+  megaNav
 })
