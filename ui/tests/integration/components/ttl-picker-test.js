25beb510a35b768c32ebee0701d5a627e23b06e0
Matthew Irish <matthew.irish@gmail.com>
2018-09-26 09:54:19 -0500

    fix linting

--- ui/tests/integration/components/ttl-picker-test.js
+++ ui/tests/integration/components/ttl-picker-test.js
@@ -1,6 +1,6 @@
 import { module, test } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
-import { render, click, fillIn, blur, find, pauseTest } from '@ember/test-helpers';
+import { render, fillIn } from '@ember/test-helpers';
 import hbs from 'htmlbars-inline-precompile';
 import sinon from 'sinon';
 
