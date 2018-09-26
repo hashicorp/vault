a477d1175269424d6cfde2c9badb63c8f0f87a25
Jeff Escalante <gh.je@mailhero.io>
2018-09-26 15:48:29 -0400

    add new mega nav

--- website/assets/reshape.js
+++ website/assets/reshape.js
@@ -1,6 +1,7 @@
 const footer = require('@hashicorp/hashi-footer')
 const nav = require('@hashicorp/hashi-nav')
 const button = require('@hashicorp/hashi-button')
+const megaNav = require('@hashicorp/hashi-mega-nav')
 const productSubnav = require('@hashicorp/hashi-product-subnav')
 const content = require('@hashicorp/hashi-content')
 
@@ -11,6 +12,7 @@ module.exports = {
   'hashi-nav': nav,
   'hashi-button': button,
   'hashi-docs-sidebar': docsSidebar,
+  'hashi-mega-nav': megaNav,
   'hashi-product-subnav': productSubnav,
   'hashi-content': content
 }
