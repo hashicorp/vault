11abcec826a968a3d8a3e856405ff4af4a2b089d
Becca Petrin <beccapetrin@posteo.net>
2018-10-01 10:05:08 -0700

    point at a fork of aliyun-oss-go-sdk (#5358)

--- physical/alicloudoss/alicloudoss_test.go
+++ physical/alicloudoss/alicloudoss_test.go
@@ -7,15 +7,15 @@ import (
 	"testing"
 	"time"
 
-	"github.com/aliyun/aliyun-oss-go-sdk/oss"
-
 	log "github.com/hashicorp/go-hclog"
 	"github.com/hashicorp/vault/helper/logging"
 	"github.com/hashicorp/vault/physical"
+	"github.com/tyrannosaurus-becks/aliyun-oss-go-sdk/oss"
 )
 
 func TestAliCloudOSSBackend(t *testing.T) {
 
+	// ex. http://oss-us-east-1.aliyuncs.com
 	endpoint := os.Getenv("ALICLOUD_OSS_ENDPOINT")
 	accessKeyID := os.Getenv("ALICLOUD_ACCESS_KEY")
 	accessKeySecret := os.Getenv("ALICLOUD_SECRET_KEY")
