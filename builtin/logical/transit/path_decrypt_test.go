3fc2628536e6d613095f2dae5b0dddf10f849ba3
Calvin Leung Huang <cleung2010@gmail.com>
2018-10-08 16:07:27 -0700

    Remove unnecessary test

--- builtin/logical/transit/path_decrypt_test.go
+++ builtin/logical/transit/path_decrypt_test.go
@@ -7,51 +7,7 @@ import (
 	"github.com/hashicorp/vault/logical"
 )
 
-// Case1: If batch decryption input is not base64 encoded, it should fail.
-func TestTransit_BatchDecryptionCase1(t *testing.T) {
-	var resp *logical.Response
-	var err error
-
-	b, s := createBackendWithStorage(t)
-
-	batchEncryptionInput := []interface{}{
-		map[string]interface{}{"plaintext": "dGhlIHF1aWNrIGJyb3duIGZveA=="},
-		map[string]interface{}{"plaintext": "Cg=="},
-	}
-
-	batchEncryptionData := map[string]interface{}{
-		"batch_input": batchEncryptionInput,
-	}
-
-	batchEncryptionReq := &logical.Request{
-		Operation: logical.CreateOperation,
-		Path:      "encrypt/upserted_key",
-		Storage:   s,
-		Data:      batchEncryptionData,
-	}
-	resp, err = b.HandleRequest(context.Background(), batchEncryptionReq)
-	if err != nil || (resp != nil && resp.IsError()) {
-		t.Fatalf("err:%v resp:%#v", err, resp)
-	}
-
-	batchDecryptionData := map[string]interface{}{
-		"batch_input": resp.Data["batch_results"],
-	}
-
-	batchDecryptionReq := &logical.Request{
-		Operation: logical.UpdateOperation,
-		Path:      "decrypt/upserted_key",
-		Storage:   s,
-		Data:      batchDecryptionData,
-	}
-	resp, err = b.HandleRequest(context.Background(), batchDecryptionReq)
-	if err == nil {
-		t.Fatalf("expected an error")
-	}
-}
-
-// Case2: Normal case of batch decryption
-func TestTransit_BatchDecryptionCase2(t *testing.T) {
+func TestTransit_BatchDecryption(t *testing.T) {
 	var resp *logical.Response
 	var err error
 
@@ -107,8 +63,7 @@ func TestTransit_BatchDecryptionCase2(t *testing.T) {
 	}
 }
 
-// Case3: Test batch decryption with a derived key
-func TestTransit_BatchDecryptionCase3(t *testing.T) {
+func TestTransit_BatchDecryption_DerivedKey(t *testing.T) {
 	var resp *logical.Response
 	var err error
 
