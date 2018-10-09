0aa074e3a673f53e7ee7743fbddc301730c374d0
Jeff Escalante <gh.je@mailhero.io>
2018-10-09 17:47:50 -0400

    community page

--- website/assets/reshape.js
+++ website/assets/reshape.js
@@ -3,9 +3,10 @@ const nav = require('@hashicorp/hashi-nav')
 const button = require('@hashicorp/hashi-button')
 const megaNav = require('@hashicorp/hashi-mega-nav')
 const productSubnav = require('@hashicorp/hashi-product-subnav')
+const verticalTextBlockList = require('@hashicorp/hashi-vertical-text-block-list')
+const sectionHeader = require('@hashicorp/hashi-section-header')
 const content = require('@hashicorp/hashi-content')
-
-const docsSidebar = require('./js/components/docs-sidebar')
+const docsSidebar = require('@hashicorp/hashi-docs-sidenav')
 
 module.exports = {
   'hashi-footer': footer,
@@ -14,5 +15,7 @@ module.exports = {
   'hashi-docs-sidebar': docsSidebar,
   'hashi-mega-nav': megaNav,
   'hashi-product-subnav': productSubnav,
+  'hashi-vertical-text-block-list': verticalTextBlockList,
+  'hashi-section-header': sectionHeader,
   'hashi-content': content
 }
