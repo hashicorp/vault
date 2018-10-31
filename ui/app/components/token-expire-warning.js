83cca3c9714ac5a2911fec46a9acba38bb0d2f3c
Matthew Irish <matthew.irish@gmail.com>
2018-10-31 14:32:08 -0500

    move warning banner out of token-expire-warning and into user menu

--- ui/app/components/token-expire-warning.js
+++ ui/app/components/token-expire-warning.js
@@ -6,26 +6,10 @@ export default Component.extend({
   router: service(),
   classNames: 'token-expire-warning',
 
-  transitionToRoute: function() {
-    this.get('router').transitionTo(...arguments);
-  },
-
-  isDismissed: false,
-
   actions: {
     reauthenticate() {
       this.get('auth').deleteCurrentToken();
-      this.transitionToRoute('vault.cluster');
-    },
-
-    renewToken() {
-      const auth = this.get('auth');
-      auth.renew();
-      auth.setLastFetch(Date.now());
-    },
-
-    dismiss() {
-      this.set('isDismissed', true);
+      this.get('router').transitionTo('vault.cluster');
     },
   },
 });
