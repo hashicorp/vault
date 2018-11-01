344bdb6bcc75ac8087d8023f4b14e0ee41f8e59d
Matthew Irish <matthew.irish@gmail.com>
2018-11-01 11:27:40 -0500

    use a link in the token-expire-warning

--- ui/app/components/token-expire-warning.js
+++ ui/app/components/token-expire-warning.js
@@ -2,14 +2,5 @@ import { inject as service } from '@ember/service';
 import Component from '@ember/component';
 
 export default Component.extend({
-  auth: service(),
-  router: service(),
   classNames: 'token-expire-warning',
-
-  actions: {
-    reauthenticate() {
-      this.get('auth').deleteCurrentToken();
-      this.get('router').transitionTo('vault.cluster');
-    },
-  },
 });
