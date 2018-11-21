0e8484ac9453e7cf5c8235360424c8466c76da6a
Matthew Irish <matthew@hashicorp.com>
2018-11-20 23:05:47 -0600

    move page-object to native events mode

--- ui/tests/test-helper.js
+++ ui/tests/test-helper.js
@@ -3,6 +3,9 @@ import config from '../config/environment';
 import { setApplication } from '@ember/test-helpers';
 import { start } from 'ember-qunit';
 import './helpers/flash-message';
+import { useNativeEvents } from 'ember-cli-page-object/extend';
+
+useNativeEvents();
 
 setApplication(Application.create(config.APP));
 
