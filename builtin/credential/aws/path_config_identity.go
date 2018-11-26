09ca161ab0287c605ed70b9178f67de81a55e5c7
vishalnayak <vishalnayakv@gmail.com>
2018-11-26 11:07:43 -0500

    Add option to configure ec2_alias values

--- builtin/credential/aws/path_config_identity.go
+++ builtin/credential/aws/path_config_identity.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 
+	"github.com/hashicorp/errwrap"
 	"github.com/hashicorp/vault/helper/strutil"
 	"github.com/hashicorp/vault/logical"
 	"github.com/hashicorp/vault/logical/framework"
@@ -18,6 +19,11 @@ func pathConfigIdentity(b *backend) *framework.Path {
 				Default:     identityAliasIAMUniqueID,
 				Description: fmt.Sprintf("Configure how the AWS auth method generates entity aliases when using IAM auth. Valid values are %q and %q", identityAliasIAMUniqueID, identityAliasIAMFullArn),
 			},
+			"ec2_alias": {
+				Type:        framework.TypeString,
+				Default:     identityAliasEC2InstanceID,
+				Description: fmt.Sprintf("Configure how the AWS auth method generates entity alias when using EC2 auth. Valid values are %q and %q", identityAliasEC2InstanceID, identityAliasEC2ImageID),
+			},
 		},
 
 		Callbacks: map[logical.Operation]framework.OperationFunc{
@@ -30,21 +36,43 @@ func pathConfigIdentity(b *backend) *framework.Path {
 	}
 }
 
+func identityConfigEntry(ctx context.Context, s logical.Storage) (*identityConfig, error) {
+	entryRaw, err := s.Get(ctx, "config/identity")
+	if err != nil {
+		return nil, errwrap.Wrapf("failed to retrieve identity config: {{err}}", err)
+	}
+
+	var entry identityConfig
+	if entryRaw == nil {
+		entry.IAMAlias = identityAliasIAMUniqueID
+		entry.EC2Alias = identityAliasEC2InstanceID
+		return &entry, nil
+	}
+
+	if err = entryRaw.DecodeJSON(&entry); err != nil {
+		return nil, errwrap.Wrapf("failed to parse stored identity config: {{err}}", err)
+	}
+
+	switch {
+	case entry.IAMAlias == "":
+		entry.IAMAlias = identityAliasIAMUniqueID
+	case entry.EC2Alias == "":
+		entry.EC2Alias = identityAliasEC2InstanceID
+	}
+
+	return &entry, nil
+}
+
 func pathConfigIdentityRead(ctx context.Context, req *logical.Request, data *framework.FieldData) (*logical.Response, error) {
-	entry, err := req.Storage.Get(ctx, "config/identity")
+	config, err := identityConfigEntry(ctx, req.Storage)
 	if err != nil {
 		return nil, err
 	}
-	if entry == nil {
-		return nil, nil
-	}
-	var result identityConfig
-	if err := entry.DecodeJSON(&result); err != nil {
-		return nil, err
-	}
+
 	return &logical.Response{
 		Data: map[string]interface{}{
-			"iam_alias": result.IAMAlias,
+			"iam_alias": config.IAMAlias,
+			"ec2_alias": config.EC2Alias,
 		},
 	}, nil
 }
@@ -60,23 +88,40 @@ func pathConfigIdentityUpdate(ctx context.Context, req *logical.Request, data *f
 			return logical.ErrorResponse(fmt.Sprintf("iam_alias of %q not in set of allowed values: %v", iamAlias, allowedIAMAliasValues)), nil
 		}
 		configEntry.IAMAlias = iamAlias
-		entry, err := logical.StorageEntryJSON("config/identity", configEntry)
-		if err != nil {
-			return nil, err
-		}
-		if err := req.Storage.Put(ctx, entry); err != nil {
-			return nil, err
-		}
 	}
+
+	ec2AliasRaw, ok := data.GetOk("ec2_alias")
+	if ok {
+		ec2Alias := ec2AliasRaw.(string)
+		allowedEC2AliasValues := []string{identityAliasEC2InstanceID, identityAliasEC2ImageID}
+		if !strutil.StrListContains(allowedEC2AliasValues, ec2Alias) {
+			return logical.ErrorResponse(fmt.Sprintf("ec2_alias of %q not in set of allowed values: %v", ec2Alias, allowedEC2AliasValues)), nil
+		}
+		configEntry.EC2Alias = ec2Alias
+	}
+
+	entry, err := logical.StorageEntryJSON("config/identity", configEntry)
+	if err != nil {
+		return nil, err
+	}
+
+	err = req.Storage.Put(ctx, entry)
+	if err != nil {
+		return nil, err
+	}
+
 	return nil, nil
 }
 
 type identityConfig struct {
 	IAMAlias string `json:"iam_alias"`
+	EC2Alias string `json:"ec2_alias"`
 }
 
 const identityAliasIAMUniqueID = "unique_id"
 const identityAliasIAMFullArn = "full_arn"
+const identityAliasEC2InstanceID = "instance-id"
+const identityAliasEC2ImageID = "image-id"
 
 const pathConfigIdentityHelpSyn = `
 Configure the way the AWS auth method interacts with the identity store
