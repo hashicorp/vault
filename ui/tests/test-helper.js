dc180a22c5c0747343cf7a5c5530b6add8bf2f1b
Matthew Irish <matthew@hashicorp.com>
2018-11-26 10:57:33 -0600

    native events mode in ecpo is also not necessary with the new style tests

--- ui/tests/test-helper.js
+++ ui/tests/test-helper.js
@@ -3,9 +3,6 @@ import config from '../config/environment';
 import { setApplication } from '@ember/test-helpers';
 import { start } from 'ember-qunit';
 import './helpers/flash-message';
-import { useNativeEvents } from 'ember-cli-page-object/extend';
-
-useNativeEvents();
 
 setApplication(Application.create(config.APP));
 
