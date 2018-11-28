02cb3a473dae1eefbf9db4eebf5347ae0446037c
Matthew Irish <matthew@hashicorp.com>
2018-11-28 09:56:13 -0600

    fix bug in trim-right util where the last ending wasn't trimming from the end of the string

--- ui/app/utils/trim-right.js
+++ ui/app/utils/trim-right.js
@@ -1,4 +1,10 @@
-export default function(fileName, toTrimArray = []) {
-  const extension = new RegExp(toTrimArray.join('$|'));
-  return fileName.replace(extension, '');
+// will trim a given set of endings from the end of a string
+// if isExtension is true, the first char of that string will be escaped
+// in the regex
+export default function(str, endings = [], isExtension = true) {
+  let trimRegex = new RegExp(endings.map(ext => `${ext}$`).join('|'));
+  if (isExtension) {
+    trimRegex = new RegExp(endings.map(ext => `\\${ext}$`).join('|'));
+  }
+  return str.replace(trimRegex, '');
 }
