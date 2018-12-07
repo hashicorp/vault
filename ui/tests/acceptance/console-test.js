6414fc32bf25de8db48802308d96e5b31993b07a
Matthew Irish <meirish@users.noreply.github.com>
2018-12-07 14:00:52 -0600

    UI - web cli layout (#5909)
    * move console/ui-panel into nav-header component
    
    * notch-specific CSS
    
    * add layout test in the application for the console

--- ui/tests/acceptance/console-test.js
+++ ui/tests/acceptance/console-test.js
@@ -1,7 +1,12 @@
 import { module, test } from 'qunit';
+import { create } from 'ember-cli-page-object';
+import { later } from '@ember/runloop';
 import { setupApplicationTest } from 'ember-qunit';
 import enginesPage from 'vault/tests/pages/secrets/backends';
 import authPage from 'vault/tests/pages/auth';
+import consoleClass from 'vault/tests/pages/components/console/ui-panel';
+
+const consoleComponent = create(consoleClass);
 
 module('Acceptance | console', function(hooks) {
   setupApplicationTest(hooks);
@@ -13,13 +18,31 @@ module('Acceptance | console', function(hooks) {
   test("refresh reloads the current route's data", async function(assert) {
     await enginesPage.visit();
     let numEngines = enginesPage.rows.length;
-    await enginesPage.consoleToggle();
+    await consoleComponent.toggle();
     let now = Date.now();
     for (let num of [1, 2, 3]) {
       let inputString = `write sys/mounts/${now + num} type=kv`;
-      await enginesPage.console.runCommands(inputString);
+      await consoleComponent.runCommands(inputString);
     }
-    await enginesPage.console.runCommands('refresh');
+    await consoleComponent.runCommands('refresh');
     assert.equal(enginesPage.rows.length, numEngines + 3, 'new engines were added to the page');
+  });
+
+  test('fullscreen command expands the cli panel', async function(assert) {
+    await consoleComponent.toggle();
+    await consoleComponent.runCommands('fullscreen');
+
+    // have to wrap in a later so that we can wait for the CSS transition to finish
+    await later(() => {
+      let consoleEle = document.querySelector('[data-test-component="console/ui-panel"]');
+
+      assert.equal(
+        consoleEle.offsetHeight,
+        window.innerHeight,
+        'fullscreen is the same height as the window'
+      );
+
+      assert.equal(consoleEle.offsetTop, 0, 'fullscreen is aligned to the top of window');
+    }, 300);
   });
 });
