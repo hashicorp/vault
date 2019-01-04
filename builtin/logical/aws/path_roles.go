2dcd0aed2a242f53dae03318b4d68693f7d92b81
Jeff Mitchell <jeffrey.mitchell@gmail.com>
2019-01-04 14:49:53 -0500

    Change credential_types output to credential_type (#5975)
    Fixes #5972

--- builtin/logical/aws/path_roles.go
+++ builtin/logical/aws/path_roles.go
@@ -429,12 +429,12 @@ type awsRoleEntry struct {
 
 func (r *awsRoleEntry) toResponseData() map[string]interface{} {
 	respData := map[string]interface{}{
-		"credential_types": r.CredentialTypes,
-		"policy_arns":      r.PolicyArns,
-		"role_arns":        r.RoleArns,
-		"policy_document":  r.PolicyDocument,
-		"default_sts_ttl":  int64(r.DefaultSTSTTL.Seconds()),
-		"max_sts_ttl":      int64(r.MaxSTSTTL.Seconds()),
+		"credential_type": strings.Join(r.CredentialTypes, ","),
+		"policy_arns":     r.PolicyArns,
+		"role_arns":       r.RoleArns,
+		"policy_document": r.PolicyDocument,
+		"default_sts_ttl": int64(r.DefaultSTSTTL.Seconds()),
+		"max_sts_ttl":     int64(r.MaxSTSTTL.Seconds()),
 	}
 	if r.InvalidData != "" {
 		respData["invalid_data"] = r.InvalidData
