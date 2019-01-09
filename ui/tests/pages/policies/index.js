93430ed1d78d55fe31fcf73e261d753be7f7a2a8
Matthew Irish <matthew@hashicorp.com>
2019-01-09 09:54:10 -0600

    fix context menu on policy page and add test for deletion

--- ui/tests/pages/policies/index.js
+++ ui/tests/pages/policies/index.js
@@ -1,10 +1,19 @@
-import { text, create, collection, visitable } from 'ember-cli-page-object';
+import { text, create, collection, clickable, visitable } from 'ember-cli-page-object';
 export default create({
   visit: visitable('/vault/policies/:type'),
   policies: collection('[data-test-policy-item]', {
     name: text('[data-test-policy-name]'),
+    menu: clickable('[data-test-popup-menu-trigger]'),
   }),
   findPolicyByName(name) {
     return this.policies.filterBy('name', name)[0];
   },
+  delete: clickable('[data-test-confirm-action-trigger]', {
+    scope: '[data-test-item-delete]',
+    testContainer: '#ember-testing',
+  }),
+  confirmDelete: clickable('[data-test-confirm-button]', {
+    scope: '[data-test-item-delete]',
+    testContainer: '#ember-testing',
+  }),
 });
