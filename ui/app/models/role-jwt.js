72785496f1f1028bf21a1bbda94ce677594a9ab1
Matthew Irish <matthew@hashicorp.com>
2019-01-16 11:26:15 -0600

    show OIDC provider name on the button

--- ui/app/models/role-jwt.js
+++ ui/app/models/role-jwt.js
@@ -1,6 +1,20 @@
 import DS from 'ember-data';
+import { computed } from '@ember/object';
+import parseURL from 'vault/utils/parse-url';
 const { attr } = DS;
 
+const DOMAIN_STRINGS = {
+  google: 'Google',
+  ping: 'Ping',
+  okta: 'Okta',
+  auth0: 'Auth0',
+};
 export default DS.Model.extend({
   authUrl: attr('string'),
+  providerName: computed('authUrl', function() {
+    let { hostname } = parseURL(this.authUrl);
+    let firstMatch = Object.keys(DOMAIN_STRINGS).find(name => hostname.includes(name));
+
+    return DOMAIN_STRINGS[firstMatch] || null;
+  }),
 });
