9c166f5804c4ff0b48dfeda0b0e7573fcc0c1e7e
Clint Shryock <clint@ctshryock.com>
2019-01-31 15:51:04 -0600

    test for new, pushitem

--- helper/queue/item.go
+++ helper/queue/item.go
@@ -1,11 +1,9 @@
 package queue
 
-import "time"
-
 // Item is something managed in the priority queue
 type Item struct {
-        value     interface{}
-        priority  int64 // priority of item in queue
-        index     int   // index is needed by update and maintained by heap package
-        createdAt time.Time
+        Key      string // key is used as an index in the internal map of a Queue
+        Value    interface{}
+        Priority int64 // priority of item in queue
+        index    int   // index is needed by update and maintained by heap package
 }
--- helper/queue/timequeue_test.go
+++ helper/queue/timequeue_test.go
@@ -1,6 +1,9 @@
 package queue
 
 import (
+        "container/heap"
+        "fmt"
+        "reflect"
         "testing"
         "time"
 )
@@ -8,31 +11,79 @@ import (
 // Compile time test to enforce TimeQueue satisfies the heap.Interface interface
 var _ PriorityQueue = &TimeQueue{}
 
-func TestNew(t *testing.T) {
+var secondOffsets = []time.Duration{
+        0,
+        15,
+        45,
+        300,     // 5 minutes
+        900,     // 15 minutes
+        7200,    // 2 hours
+        7201,    // 2 hours, 1 second
+        115200,  // 32 hours
+        183600,  // 51 hours
+        183600,  // 51 hours
+        1209600, // 2 weeks
+}
 
-        secondOffsets := []time.Duration{
-                0,
-                15,
-                45,
-                300,  // 5 minutes
-                900,  // 15 minutes
-                7200, // 2 hours
-        }
-
-        testCases := make([]*Item, len(secondOffsets))
-
+func testCases() []*Item {
+        tc := make([]*Item, len(secondOffsets))
         // create a slice of items with priority / times offest by these seconds
         for i, m := range secondOffsets {
                 n := time.Now()
                 ft := n.Add(time.Second * m)
-                testCases[i] = &Item{
-                        value:    1,
-                        priority: ft.Unix(),
+                tc[i] = &Item{
+                        Key:      fmt.Sprintf("item-%d", i),
+                        Value:    1,
+                        Priority: ft.Unix(),
+                }
+        }
+        fmt.Println("test cases")
+        for i, t := range tc {
+                fmt.Printf("\t %d - %s\n", i, time.Unix(t.Priority, 0).String())
+        }
+        return tc
+}
+
+func TestNewTimeQueue(t *testing.T) {
+        tq := NewTimeQueue()
+
+        tqi := tq.(*TimeQueue)
+        if len(tqi.data) != len(tqi.dataMap) || len(tqi.data) != 0 {
+                t.Fatalf("error in queue/map size, expected data and map to be initialized, got (%d) and (%d)", len(tqi.data), len(tqi.dataMap))
+        }
+
+        if tq.Size() != 0 {
+                t.Fatalf("expected new queue to have zero size, got (%d)", tq.Size())
+        }
+}
+
+func TestTimeQueue_PushItem(t *testing.T) {
+        tq := NewTimeQueue()
+
+        tc := testCases()
+        tcl := len(tc)
+        for _, i := range tc {
+                if err := tq.PushItem(i); err != nil {
+                        t.Fatal(err)
                 }
         }
 
-        tq := NewTimeQueue(testCases)
-        if tq.Size() != len(testCases) {
-                t.Fatalf("error in queue size, expected (%d), got (%d)", len(testCases), tq.Size())
+        if tq.Size() != tcl {
+                t.Fatalf("error adding items, expected (%d) items, got (%d)", tcl, tq.Size())
         }
+
+        // check the internal data structures
+        tqi := tq.(*TimeQueue)
+        if len(tqi.data) != len(tqi.dataMap) {
+                t.Fatalf("error in queue/map size, expected data and map to be initialized, got (%d) and (%d)", len(tqi.data), len(tqi.dataMap))
+        }
+
+        item := heap.Pop(tq).(*Item)
+        if tc[0].Priority != item.Priority {
+                t.Fatalf("expected tc[0] and popped item to match, got (%q) and (%q)", tc[0], item.Priority)
+        }
+        if !reflect.DeepEqual(tc[0], item) {
+                t.Fatal("expected test case and popped item match")
+        }
+
 }
