3fb55b6fcce702566df28b8cb422bcf86ad54611
vishalnayak <vishalnayakv@gmail.com>
2019-02-04 16:02:09 -0500

    Both primary and secondaries won't persist during startup and invalidations

--- vault/identity_store_util.go
+++ vault/identity_store_util.go
@@ -333,22 +333,16 @@ func (i *IdentityStore) upsertEntityInTxn(ctx context.Context, txn *memdb.Txn, e
 			// into merging
 			fallthrough
 		default:
-			// `persist` will be `false` during startup and invalidations.
-			// During startup, if there are conflicting alias names, log the
-			// fact and bring up identity store to operate in case-sensitive
-			// manner by returning errDuplicateIdentityName, and by setting
-			// disableLowerCasedNames to `true`. This implies that, by the time
-			// invalidations are happening, only 2 things can happen: 1)
-			// disableLowerCasedNames is `false` and when that's the case this
-			// default case will never be hit. 2) This default case will be hit
-			// but only when disableLowerCasedNames is set to `true`. Hence
-			// invalidations workflow will execute the `mergeEntity` call while
-			// the startup workflow will not. That's the desired behavior as
-			// the `mergeEntity` call will result in a read-only error in
-			// secondaries during startup causing unseal failure.
-			if !persist && !i.disableLowerCasedNames {
+			// During startup and invalidations, log the conflicts and don't
+			// hit the storage, regardless of being primary or secondary. Also,
+			// ensure that case-insensitivity is assumed only after all the
+			// conflicts are resolved.
+			if !persist {
 				i.logger.Warn(errDuplicateIdentityName.Error(), "alias_name", alias.Name, "mount_accessor", alias.MountAccessor, "entity_name", entity.Name, "action", "delete one of the duplicate aliases")
-				return errDuplicateIdentityName
+				if !i.disableLowerCasedNames {
+					return errDuplicateIdentityName
+				}
+				break
 			}
 
 			i.logger.Warn("alias is already tied to a different entity; these entities are being merged", "alias_id", alias.ID, "other_entity_id", aliasByFactors.CanonicalID, "entity_aliases", entity.Aliases, "alias_by_factors", aliasByFactors)
