a163169c38d7c1f2a9e2740e7eed6826b213649a
Clint Shryock <clint@ctshryock.com>
2019-02-11 16:55:03 -0600

    mistaken

--- builtin/logical/database/path_roles.go
+++ builtin/logical/database/path_roles.go
@@ -1,15 +1,15 @@
 package database
 
 import (
-        "context"
-        "fmt"
-        "time"
+	"context"
+	"fmt"
+	"time"
 
-        "github.com/hashicorp/vault/builtin/logical/database/dbplugin"
-        "github.com/hashicorp/vault/helper/parseutil"
-        "github.com/hashicorp/vault/helper/strutil"
-        "github.com/hashicorp/vault/logical"
-        "github.com/hashicorp/vault/logical/framework"
+	"github.com/hashicorp/vault/builtin/logical/database/dbplugin"
+	"github.com/hashicorp/vault/helper/parseutil"
+	"github.com/hashicorp/vault/helper/strutil"
+	"github.com/hashicorp/vault/logical"
+	"github.com/hashicorp/vault/logical/framework"
 )
 
 func pathListRoles(b *databaseBackend) *framework.Path {
@@ -76,10 +76,10 @@ func pathRoles(b *databaseBackend) *framework.Path {
 				Description: "Maximum time a credential is valid for",
 			},
 
-                        "static_account": &framework.FieldSchema{
-                                Type:        framework.TypeMap,
-                                Description: `Static account thing. only accpets a few things`,
-                        },
+			"static_account": &framework.FieldSchema{
+				Type:        framework.TypeMap,
+				Description: `Static account thing. only accpets a few things`,
+			},
 		},
 
 		ExistenceCheck: b.pathRoleExistenceCheck(),
@@ -96,21 +96,21 @@ func pathRoles(b *databaseBackend) *framework.Path {
 }
 
 func (b *databaseBackend) pathRoleExistenceCheck() framework.ExistenceFunc {
-        return func(ctx context.Context, req *logical.Request, data *framework.FieldData) (bool, error) {
-                role, err := b.Role(ctx, req.Storage, data.Get("name").(string))
-                if err != nil {
-                        return false, err
-                }
+	return func(ctx context.Context, req *logical.Request, data *framework.FieldData) (bool, error) {
+		role, err := b.Role(ctx, req.Storage, data.Get("name").(string))
+		if err != nil {
+			return false, err
+		}
 
-                return role != nil, nil
-        }
+		return role != nil, nil
+	}
 }
 
 func (b *databaseBackend) pathRoleDelete() framework.OperationFunc {
-        return func(ctx context.Context, req *logical.Request, data *framework.FieldData) (*logical.Response, error) {
-                err := req.Storage.Delete(ctx, "role/"+data.Get("name").(string))
-                if err != nil {
-                        return nil, err
+	return func(ctx context.Context, req *logical.Request, data *framework.FieldData) (*logical.Response, error) {
+		err := req.Storage.Delete(ctx, "role/"+data.Get("name").(string))
+		if err != nil {
+			return nil, err
 		}
 
 		return nil, nil
@@ -132,12 +132,12 @@ func (b *databaseBackend) pathRoleRead() framework.OperationFunc {
 			"creation_statements":   role.Statements.Creation,
 			"revocation_statements": role.Statements.Revocation,
 			"rollback_statements":   role.Statements.Rollback,
-                        "renew_statements":      role.Statements.Renewal,
-                        "default_ttl":           role.DefaultTTL.Seconds(),
-                        "max_ttl":               role.MaxTTL.Seconds(),
-                }
-                if len(role.Statements.Creation) == 0 {
-                        data["creation_statements"] = []string{}
+			"renew_statements":      role.Statements.Renewal,
+			"default_ttl":           role.DefaultTTL.Seconds(),
+			"max_ttl":               role.MaxTTL.Seconds(),
+		}
+		if len(role.Statements.Creation) == 0 {
+			data["creation_statements"] = []string{}
 		}
 		if len(role.Statements.Revocation) == 0 {
 			data["revocation_statements"] = []string{}
@@ -149,18 +149,18 @@ func (b *databaseBackend) pathRoleRead() framework.OperationFunc {
 			data["renew_statements"] = []string{}
 		}
 
-                if role.StaticAccount != nil {
-                        sa := make(map[string]interface{})
-                        sa["username"] = role.StaticAccount.Username
-                        sa["rotation_frequency"] = role.StaticAccount.RotationFrequency.Nanoseconds()
-                        if role.StaticAccount.Password != "" {
-                                sa["password"] = role.StaticAccount.Password
-                        }
-                        data["static_account"] = sa
-                        // TODO return LastVaultRotation once it's set
-                }
+		if role.StaticAccount != nil {
+			sa := make(map[string]interface{})
+			sa["username"] = role.StaticAccount.Username
+			sa["rotation_frequency"] = role.StaticAccount.RotationFrequency.Nanoseconds()
+			if role.StaticAccount.Password != "" {
+				sa["password"] = role.StaticAccount.Password
+			}
+			data["static_account"] = sa
+			// TODO return LastVaultRotation once it's set
+		}
 
-                return &logical.Response{
+		return &logical.Response{
 			Data: data,
 		}, nil
 	}
@@ -185,56 +185,56 @@ func (b *databaseBackend) pathRoleCreateUpdate() framework.OperationFunc {
 		}
 
 		role, err := b.Role(ctx, req.Storage, data.Get("name").(string))
-                if err != nil {
-                        return nil, err
-                }
+		if err != nil {
+			return nil, err
+		}
 
-                // createRole is a boolean to indicate if this is a new role creation. This
-                // is used to ensure we do not allow an existing role to be "migrated" to
-                // roel with a static account. If createRole is false and static_account
-                // data is given, return an error
-                var createRole bool
-                if role == nil {
-                        role = &roleEntry{}
-                        createRole = true
-                }
+		// createRole is a boolean to indicate if this is a new role creation. This
+		// is used to ensure we do not allow an existing role to be "migrated" to
+		// roel with a static account. If createRole is false and static_account
+		// data is given, return an error
+		var createRole bool
+		if role == nil {
+			role = &roleEntry{}
+			createRole = true
+		}
 
-                // Static Account information
-                staticRaw := data.Get("static_account").(map[string]interface{})
-                if len(staticRaw) > 0 && !createRole {
-                        return logical.ErrorResponse("cannot change role to a static account"), nil
-                }
+		// Static Account information
+		staticRaw := data.Get("static_account").(map[string]interface{})
+		if len(staticRaw) > 0 && !createRole {
+			return logical.ErrorResponse("cannot change role to a static account"), nil
+		}
 
-                if len(staticRaw) > 0 {
-                        var sa *staticAccount
-                        sa = &staticAccount{}
+		if len(staticRaw) > 0 {
+			var sa *staticAccount
+			sa = &staticAccount{}
 
-                        if v, ok := staticRaw["username"].(string); ok {
-                                sa.Username = v
-                        } else {
-                                return logical.ErrorResponse("username is a required field for static accounts"), nil
-                        }
+			if v, ok := staticRaw["username"].(string); ok {
+				sa.Username = v
+			} else {
+				return logical.ErrorResponse("username is a required field for static accounts"), nil
+			}
 
-                        if v, ok := staticRaw["rotation_frequency"]; ok {
-                                sa.RotationFrequency, err = parseutil.ParseDurationSecond(v)
-                                if err != nil {
-                                        return logical.ErrorResponse(fmt.Sprintf("invalid rotation_frequency: %s", err)), nil
-                                }
-                        } else {
-                                return logical.ErrorResponse("rotation_frequency is a required field for static accounts"), nil
-                        }
+			if v, ok := staticRaw["rotation_frequency"]; ok {
+				sa.RotationFrequency, err = parseutil.ParseDurationSecond(v)
+				if err != nil {
+					return logical.ErrorResponse(fmt.Sprintf("invalid rotation_frequency: %s", err)), nil
+				}
+			} else {
+				return logical.ErrorResponse("rotation_frequency is a required field for static accounts"), nil
+			}
 
-                        if p, ok := staticRaw["password"].(string); ok {
-                                sa.Password = p
-                        }
-                        if sa != nil {
-                                role.StaticAccount = sa
-                        }
-                }
+			if p, ok := staticRaw["password"].(string); ok {
+				sa.Password = p
+			}
+			if sa != nil {
+				role.StaticAccount = sa
+			}
+		}
 
-                // DB Attributes
-                {
-                        if dbNameRaw, ok := data.GetOk("db_name"); ok {
+		// DB Attributes
+		{
+			if dbNameRaw, ok := data.GetOk("db_name"); ok {
 				role.DBName = dbNameRaw.(string)
 			} else if req.Operation == logical.CreateOperation {
 				role.DBName = data.Get("db_name").(string)
@@ -291,14 +291,14 @@ func (b *databaseBackend) pathRoleCreateUpdate() framework.OperationFunc {
 			role.Statements.RollbackStatements = ""
 		}
 
-                role.Statements.Revocation = strutil.RemoveEmpty(role.Statements.Revocation)
+		role.Statements.Revocation = strutil.RemoveEmpty(role.Statements.Revocation)
 
-                // TODO branch out and create static account in Database
+		// TODO branch out and create static account in Database
 
-                // END create static account
+		// END create static account
 
-                // Store it
-                entry, err := logical.StorageEntryJSON("role/"+name, role)
+		// Store it
+		entry, err := logical.StorageEntryJSON("role/"+name, role)
 		if err != nil {
 			return nil, err
 		}
@@ -311,39 +311,39 @@ func (b *databaseBackend) pathRoleCreateUpdate() framework.OperationFunc {
 }
 
 type roleEntry struct {
-        DBName        string              `json:"db_name"`
-        Statements    dbplugin.Statements `json:"statements"`
-        DefaultTTL    time.Duration       `json:"default_ttl"`
-        MaxTTL        time.Duration       `json:"max_ttl"`
-        StaticAccount *staticAccount      `json:"static_account" mapstructure:"static_account"`
+	DBName        string              `json:"db_name"`
+	Statements    dbplugin.Statements `json:"statements"`
+	DefaultTTL    time.Duration       `json:"default_ttl"`
+	MaxTTL        time.Duration       `json:"max_ttl"`
+	StaticAccount *staticAccount      `json:"static_account" mapstructure:"static_account"`
 }
 
 type staticAccount struct {
-        // Fields used in Static Accounts and automatic password rotation
+	// Fields used in Static Accounts and automatic password rotation
 
-        // Username to create or assume management for static accounts
-        Username string `json:"username"`
+	// Username to create or assume management for static accounts
+	Username string `json:"username"`
 
-        // Password is the current password for static accounts. As an input, this is
-        // used/required when trying to assume management of an existing static
-        // account. Return this on credential request if it exists.
-        Password string `json:"password"`
+	// Password is the current password for static accounts. As an input, this is
+	// used/required when trying to assume management of an existing static
+	// account. Return this on credential request if it exists.
+	Password string `json:"password"`
 
-        // LastVaultRotation represents the last time Vault rotated the password
-        // PasswordLastSet represents the last time a manual password rotation was
-        // preformed, using the Vault endpoint
-        LastVaultRotation time.Time `json:"last_vault_rotation"`
-        PasswordLastSet   time.Time `json:"password_last_set"`
+	// LastVaultRotation represents the last time Vault rotated the password
+	// PasswordLastSet represents the last time a manual password rotation was
+	// preformed, using the Vault endpoint
+	LastVaultRotation time.Time `json:"last_vault_rotation"`
+	PasswordLastSet   time.Time `json:"password_last_set"`
 
-        // RotationFrequency is numer in seconds between each rotation, effectively a
-        // "time to live". This value is compared to the LastVaultRotation to
-        // determine if a password needs to be rotated
-        RotationFrequency time.Duration `json:"rotation_frequency"`
+	// RotationFrequency is numer in seconds between each rotation, effectively a
+	// "time to live". This value is compared to the LastVaultRotation to
+	// determine if a password needs to be rotated
+	RotationFrequency time.Duration `json:"rotation_frequency"`
 
-        // previousPassword is used to preserve the previous password during a
-        // rotation. If any step in the process fails, we have record of the previous
-        // password and can attempt to roll back.
-        previousPassword string
+	// previousPassword is used to preserve the previous password during a
+	// rotation. If any step in the process fails, we have record of the previous
+	// password and can attempt to roll back.
+	previousPassword string
 }
 
 const pathRoleHelpSyn = `
--- logical/identity.pb.go
+++ logical/identity.pb.go
@@ -5,9 +5,8 @@ package logical
 
 import (
 	fmt "fmt"
+	proto "github.com/golang/protobuf/proto"
 	math "math"
-
-        proto "github.com/golang/protobuf/proto"
 )
 
 // Reference imports to suppress errors if they are not otherwise used.
