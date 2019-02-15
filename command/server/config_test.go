6dce7915233e7c94cdd25f3c00fe91881dcd463d
Chris Hoffman <99742+chrishoffman@users.noreply.github.com>
2019-02-15 10:48:54 -0500

    fixing command server tests

--- command/server/config_test.go
+++ command/server/config_test.go
@@ -48,11 +48,12 @@ func TestLoadConfigFile(t *testing.T) {
 		},
 
 		Telemetry: &Telemetry{
-			StatsdAddr:      "bar",
-			StatsiteAddr:    "foo",
-			DisableHostname: false,
-			DogStatsDAddr:   "127.0.0.1:7254",
-			DogStatsDTags:   []string{"tag_1:val_1", "tag_2:val_2"},
+			StatsdAddr:              "bar",
+			StatsiteAddr:            "foo",
+			DisableHostname:         false,
+			DogStatsDAddr:           "127.0.0.1:7254",
+			DogStatsDTags:           []string{"tag_1:val_1", "tag_2:val_2"},
+			PrometheusRetentionTime: prometheusDefaultRetentionTime,
 		},
 
 		DisableCache:             true,
@@ -121,11 +122,13 @@ func TestLoadConfigFile_topLevel(t *testing.T) {
 		},
 
 		Telemetry: &Telemetry{
-			StatsdAddr:      "bar",
-			StatsiteAddr:    "foo",
-			DisableHostname: false,
-			DogStatsDAddr:   "127.0.0.1:7254",
-			DogStatsDTags:   []string{"tag_1:val_1", "tag_2:val_2"},
+			StatsdAddr:                 "bar",
+			StatsiteAddr:               "foo",
+			DisableHostname:            false,
+			DogStatsDAddr:              "127.0.0.1:7254",
+			DogStatsDTags:              []string{"tag_1:val_1", "tag_2:val_2"},
+			PrometheusRetentionTime:    30 * time.Second,
+			PrometheusRetentionTimeRaw: "30s",
 		},
 
 		DisableCache:    true,
@@ -202,6 +205,7 @@ func TestLoadConfigFile_json(t *testing.T) {
 			CirconusCheckTags:                  "",
 			CirconusBrokerID:                   "",
 			CirconusBrokerSelectTag:            "",
+			PrometheusRetentionTime:            prometheusDefaultRetentionTime,
 		},
 
 		MaxLeaseTTL:          10 * time.Hour,
@@ -288,6 +292,8 @@ func TestLoadConfigFile_json2(t *testing.T) {
 			CirconusCheckTags:                  "cat1:tag1,cat2:tag2",
 			CirconusBrokerID:                   "0",
 			CirconusBrokerSelectTag:            "dc:sfo",
+			PrometheusRetentionTime:            30 * time.Second,
+			PrometheusRetentionTimeRaw:         "30s",
 		},
 	}
 	if !reflect.DeepEqual(config, expected) {
@@ -336,9 +342,10 @@ func TestLoadConfigDir(t *testing.T) {
 		EnableRawEndpoint: true,
 
 		Telemetry: &Telemetry{
-			StatsiteAddr:    "qux",
-			StatsdAddr:      "baz",
-			DisableHostname: true,
+			StatsiteAddr:            "qux",
+			StatsdAddr:              "baz",
+			DisableHostname:         true,
+			PrometheusRetentionTime: prometheusDefaultRetentionTime,
 		},
 
 		MaxLeaseTTL:     10 * time.Hour,
