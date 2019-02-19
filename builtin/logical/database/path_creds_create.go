59f212bb4b4db42fb020dcd0ef6509a0e28d28ba
Clint Shryock <clint@ctshryock.com>
2019-02-19 09:35:39 -0600

    check for static account, return existing password if found

--- builtin/logical/database/path_creds_create.go
+++ builtin/logical/database/path_creds_create.go
@@ -9,6 +9,7 @@ import (
 	"github.com/hashicorp/vault/helper/strutil"
 	"github.com/hashicorp/vault/logical"
 	"github.com/hashicorp/vault/logical/framework"
+	"github.com/y0ssar1an/q"
 )
 
 func pathCredsCreate(b *databaseBackend) *framework.Path {
@@ -43,6 +44,19 @@ func (b *databaseBackend) pathCredsCreateRead() framework.OperationFunc {
 			return logical.ErrorResponse(fmt.Sprintf("unknown role: %s", name)), nil
 		}
 
+		// check static account.
+		// - if static, return password
+		if role.StaticAccount != nil {
+			q.Q("path creds create, static:", role.StaticAccount)
+			return &logical.Response{
+				Data: map[string]interface{}{
+					"username":            role.StaticAccount.Username,
+					"password":            role.StaticAccount.Password,
+					"last_vault_rotation": role.StaticAccount.LastVaultRotation,
+				},
+			}, nil
+		}
+
 		dbConfig, err := b.DatabaseConfig(ctx, req.Storage, role.DBName)
 		if err != nil {
 			return nil, err
@@ -59,7 +73,7 @@ func (b *databaseBackend) pathCredsCreateRead() framework.OperationFunc {
 		if err != nil {
 			return nil, err
 		}
-                // branch for static account around here
+		// branch for static account around here
 
 		db.RLock()
 		defer db.RUnlock()
