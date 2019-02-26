1bbef26e967cb3455b3a78e51b20de6346dc3a22
Clint Shryock <clint@ctshryock.com>
2019-02-26 11:33:17 -0600

    update static role rotation tests

--- builtin/logical/database/path_rotate_role_credentials_test.go
+++ builtin/logical/database/path_rotate_role_credentials_test.go
@@ -13,7 +13,7 @@ import (
         _ "github.com/lib/pq"
 )
 
-func TestBackend_Static_Account_Rotate(t *testing.T) {
+func TestBackend_Role_Rotate_Static(t *testing.T) {
         cluster, sys := getCluster(t)
         defer cluster.Cleanup()
 
@@ -135,6 +135,111 @@ func TestBackend_Static_Account_Rotate(t *testing.T) {
 
         // verify new username/password
         verifyPgConn(t, username, newPassword, connURL)
+}
+
+// sanity check to make sure we don't allow an attempt of rotating credentials
+// for non-static accounts, which doesn't make sense anyway, but doesn't hurt to
+// verify we return an error
+func TestBackend_Role_Rotate_Normal(t *testing.T) {
+        cluster, sys := getCluster(t)
+        defer cluster.Cleanup()
+
+        config := logical.TestBackendConfig()
+        config.StorageView = &logical.InmemStorage{}
+        config.System = sys
+
+        lb, err := Factory(context.Background(), config)
+        if err != nil {
+                t.Fatal(err)
+        }
+        b, ok := lb.(*databaseBackend)
+        if !ok {
+                t.Fatal("could not convert to db backend")
+        }
+        defer b.Cleanup(context.Background())
+
+        cleanup, connURL := preparePostgresTestContainer(t, config.StorageView, b)
+        defer cleanup()
+
+        // Configure a connection
+        data := map[string]interface{}{
+                "connection_url":    connURL,
+                "plugin_name":       "postgresql-database-plugin",
+                "verify_connection": false,
+                "allowed_roles":     []string{"*"},
+                "name":              "plugin-test",
+        }
+
+        req := &logical.Request{
+                Operation: logical.UpdateOperation,
+                Path:      "config/plugin-test",
+                Storage:   config.StorageView,
+                Data:      data,
+        }
+        resp, err := b.HandleRequest(namespace.RootContext(nil), req)
+        if err != nil || (resp != nil && resp.IsError()) {
+                t.Fatalf("err:%s resp:%#v\n", err, resp)
+        }
+
+        data = map[string]interface{}{
+                "name":                  "plugin-role-test",
+                "db_name":               "plugin-test",
+                "creation_statements":   testRoleStaticCreate,
+                "rotation_statements":   testRoleStaticUpdate,
+                "revocation_statements": defaultRevocationSQL,
+        }
+
+        req = &logical.Request{
+                Operation: logical.CreateOperation,
+                Path:      "roles/plugin-role-test",
+                Storage:   config.StorageView,
+                Data:      data,
+        }
+
+        resp, err = b.HandleRequest(namespace.RootContext(nil), req)
+        if err != nil || (resp != nil && resp.IsError()) {
+                t.Fatalf("err:%s resp:%#v\n", err, resp)
+        }
+
+        // Read the creds
+        data = map[string]interface{}{}
+        req = &logical.Request{
+                Operation: logical.ReadOperation,
+                Path:      "creds/plugin-role-test",
+                Storage:   config.StorageView,
+                Data:      data,
+        }
+        resp, err = b.HandleRequest(namespace.RootContext(nil), req)
+        if err != nil || (resp != nil && resp.IsError()) {
+                t.Fatalf("err:%s resp:%#v\n", err, resp)
+        }
+
+        username := resp.Data["username"].(string)
+        password := resp.Data["password"].(string)
+        if username == "" || password == "" {
+                t.Fatalf("empty username (%s) or password (%s)", username, password)
+        }
+
+        // verify username/password
+        verifyPgConn(t, username, password, connURL)
+
+        // trigger rotation
+        data = map[string]interface{}{"name": "plugin-role-test"}
+        req = &logical.Request{
+                Operation: logical.UpdateOperation,
+                Path:      "rotate-role/plugin-role-test",
+                Storage:   config.StorageView,
+                Data:      data,
+        }
+        // expect resp to be an error
+        resp, _ = b.HandleRequest(namespace.RootContext(nil), req)
+        if !resp.IsError() {
+                t.Fatalf("expected error rotating non-static role")
+        }
+
+        if resp.Error().Error() != "cannot rotate credentials of non-static accounts" {
+                t.Fatalf("wrong error message: %s", err)
+        }
 }
 
 func verifyPgConn(t *testing.T, username, password, connURL string) {
