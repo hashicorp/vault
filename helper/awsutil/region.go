f9895518f0937cc4baa3e16f52e9dd88d92a0fba
Becca Petrin <beccapetrin@posteo.net>
2019-02-27 13:20:34 -0800

    add explanatory comment

--- helper/awsutil/region.go
+++ helper/awsutil/region.go
@@ -20,6 +20,27 @@ var (
 	ec2MetadataBaseURL = "http://169.254.169.254"
 )
 
+/*
+
+It's impossible to mimic "normal" AWS behavior here because it's not consistent
+or well-defined. For example, boto3, the Python SDK (which the aws cli uses),
+loads `~/.aws/config` by default and only reads the `AWS_DEFAULT_REGION` environment
+variable (and not `AWS_REGION`, while the golang SDK does _mostly_ the opposite -- it
+reads the region **only** from `AWS_REGION` and not at all `~/.aws/config`, **unless**
+the `AWS_SDK_LOAD_CONFIG` environment variable is set. So, we must define our own
+approach to walking AWS config and deciding what to use.
+
+Our chosen approach is:
+
+	"More specific takes precedence over less specific."
+
+1. User-provided configuration is the most explicit.
+2. Environment variables are potentially shared across many invocations and so they have less precedence.
+3. Configuration in `~/.aws/config` is shared across all invocations of a given user and so this has even less precedence.
+4. Configuration retrieved from the EC2 instance metadata service is shared by all invocations on a given machine, and so it has the lowest precedence.
+
+This approach should be used in future updates to this logic.
+*/
 func GetOrDefaultRegion(logger hclog.Logger, configuredRegion string) string {
 	if configuredRegion != "" {
 		return configuredRegion
