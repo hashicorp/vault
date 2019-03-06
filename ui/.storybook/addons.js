d6bcccc86a8881387d0ffd68d9f9f3cc1dba673b
Noelle Daley <adriannenoelle@gmail.com>
2019-03-06 15:07:46 -0800

    add notes addon

--- ui/.storybook/addons.js
+++ ui/.storybook/addons.js
@@ -2,3 +2,4 @@ import '@storybook/addon-actions/register';
 import '@storybook/addon-links/register';
 import '@storybook/addon-knobs/register';
 import '@storybook/addon-storysource/register';
+import '@storybook/addon-notes/register';
