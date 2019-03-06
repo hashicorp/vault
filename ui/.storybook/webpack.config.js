51316784382d04b3c35742b270009eb0db92aa1d
Noelle Daley <adriannenoelle@gmail.com>
2019-03-06 15:15:22 -0800

    remove storybook source addon

--- ui/.storybook/webpack.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = ({ config, mode }) => {
-  config.module.rules.push({
-    test: /\.stories\.js?$/,
-    loaders: [require.resolve('@storybook/addon-storysource/loader')],
-    enforce: 'pre',
-  });
-
-  return config;
-};
