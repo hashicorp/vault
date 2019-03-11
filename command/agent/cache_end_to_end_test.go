36de62d6b2964bfc64ab86405762be7b609a4457
Nick Cabatoff <ncabatoff@hashicorp.com>
2019-03-11 15:16:06 -0400

    Create a simple test to validate how the API interacts with auto-auth.

--- command/agent/cache_end_to_end_test.go
+++ command/agent/cache_end_to_end_test.go
@@ -20,7 +20,6 @@ import (
 	"github.com/hashicorp/vault/command/agent/sink"
 	"github.com/hashicorp/vault/command/agent/sink/file"
 	"github.com/hashicorp/vault/command/agent/sink/inmem"
-	"github.com/hashicorp/vault/helper/consts"
 	"github.com/hashicorp/vault/helper/logging"
 	vaulthttp "github.com/hashicorp/vault/http"
 	"github.com/hashicorp/vault/logical"
@@ -36,7 +35,7 @@ type (
 	}
 )
 
-func newTestAuthHelper(ctx context.Context, client *api.Client, policyBody string) (*testAuthHelper, error) {
+func newTestAuthHelper(ctx context.Context, client *api.Client, policyBody string, tokenTtl time.Duration) (*testAuthHelper, error) {
 	logger := logging.NewVaultLogger(log.Trace)
 
 	// Add an kv-admin policy
@@ -54,7 +53,7 @@ func newTestAuthHelper(ctx context.Context, client *api.Client, policyBody strin
 
 	_, err = client.Logical().Write("auth/approle/role/test1", map[string]interface{}{
 		"bind_secret_id": "true",
-		"token_ttl":      "3s",
+		"token_ttl":      tokenTtl.String(),
 		"token_max_ttl":  "10s",
 		"policies":       []string{"test-autoauth"},
 	})
@@ -141,7 +140,6 @@ func (tah *testAuthHelper) getToken(tokenFile string) (string, error) {
 		}
 		val, err := ioutil.ReadFile(tokenFile)
 		if err == nil {
-			os.Remove(tokenFile)
 			if len(val) == 0 {
 				return "", fmt.Errorf("written token was empty")
 			}
@@ -157,7 +155,7 @@ func (tah *testAuthHelper) getToken(tokenFile string) (string, error) {
 	}
 }
 
-func TestCache_UsingAutoAuthToken(t *testing.T) {
+func testhelperAutoAuth(t *testing.T, fileSinks []*sink.SinkConfig, tokenFile string, tokenTtl time.Duration) (cleanup func(), client *api.Client, agentAddr string) {
 	logger := logging.NewVaultLogger(log.Trace)
 	coreConfig := &vault.CoreConfig{
 		DisableMlock: true,
@@ -176,10 +174,9 @@ func TestCache_UsingAutoAuthToken(t *testing.T) {
 	})
 
 	cluster.Start()
-	defer cluster.Cleanup()
 
 	vault.TestWaitActive(t, cluster.Cores[0].Core)
-	client := cluster.Cores[0].Client
+	client = cluster.Cores[0].Client
 
 	defer os.Setenv(api.EnvVaultAddress, os.Getenv(api.EnvVaultAddress))
 	os.Setenv(api.EnvVaultAddress, client.Address())
@@ -208,7 +205,6 @@ func TestCache_UsingAutoAuthToken(t *testing.T) {
 	timer := time.AfterFunc(30*time.Second, func() {
 		cancelFunc()
 	})
-	defer timer.Stop()
 
 	// Create the API proxier
 	apiProxy, err := cache.NewAPIProxy(&cache.APIProxyConfig{
@@ -230,6 +226,82 @@ func TestCache_UsingAutoAuthToken(t *testing.T) {
 	if err != nil {
 		t.Fatal(err)
 	}
+
+	ss := sink.NewSinkServer(&sink.SinkServerConfig{
+		Logger: logger.Named("sink.server"),
+		Client: client,
+	})
+
+	inmemSinkConfig := &sink.SinkConfig{
+		Logger: logger.Named("sink.inmem"),
+	}
+
+	inmemSink, err := inmem.New(inmemSinkConfig, leaseCache)
+	if err != nil {
+		t.Fatal(err)
+	}
+	inmemSinkConfig.Sink = inmemSink
+
+	policyBody := `
+path "/kv/*" {
+	capabilities = ["sudo", "create", "read", "update", "delete", "list"]
+}
+
+path "/auth/token/create" {
+	capabilities = ["create", "update"]
+}
+`
+	testAuthHelper, err := newTestAuthHelper(ctx, client, policyBody, tokenTtl)
+
+	sinks := append([]*sink.SinkConfig{inmemSinkConfig}, fileSinks...)
+	go ss.Run(ctx, testAuthHelper.authHandler.OutputCh, sinks)
+
+	// Check that no sink file exists
+	_, err = os.Lstat(tokenFile)
+	if err == nil {
+		t.Fatal("expected err")
+	}
+	if !os.IsNotExist(err) {
+		t.Fatal("expected notexist err")
+	}
+
+	token, err := testAuthHelper.getToken(tokenFile)
+	if err != nil {
+		t.Fatal(err)
+	}
+	t.Logf("auto-auth token: %q", token)
+
+	listener, err := net.Listen("tcp", "127.0.0.1:0")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// Create a muxer and add paths relevant for the lease cache layer
+	mux := cache.AgentMux(ctx, cacheLogger, leaseCache, inmemSink, nil)
+	server := &http.Server{
+		Handler:           mux,
+		ReadHeaderTimeout: 10 * time.Second,
+		ReadTimeout:       30 * time.Second,
+		IdleTimeout:       5 * time.Minute,
+		ErrorLog:          cacheLogger.StandardLogger(nil),
+	}
+	go server.Serve(listener)
+
+	cleanup = func() {
+		os.Remove(tokenFile)
+		listener.Close()
+		cancelFunc()
+		<-ss.DoneCh
+		testAuthHelper.cleanup()
+		timer.Stop()
+		cluster.Cleanup()
+	}
+
+	return cleanup, client, "http://" + listener.Addr().String()
+}
+
+func TestCache_UsingAutoAuthToken(t *testing.T) {
+	logger := logging.NewVaultLogger(log.Trace)
 
 	var out string
 	{
@@ -256,85 +328,21 @@ func TestCache_UsingAutoAuthToken(t *testing.T) {
 	}
 	config.Sink = fs
 
-	ss := sink.NewSinkServer(&sink.SinkServerConfig{
-		Logger: logger.Named("sink.server"),
-		Client: client,
-	})
+	tokenTtl := 3 * time.Second
+	cleanup, realClient, agentAddr := testhelperAutoAuth(t, []*sink.SinkConfig{config}, out, tokenTtl)
+	defer cleanup()
 
-	inmemSinkConfig := &sink.SinkConfig{
-		Logger: logger.Named("sink.inmem"),
-	}
-
-	inmemSink, err := inmem.New(inmemSinkConfig, leaseCache)
-	if err != nil {
-		t.Fatal(err)
-	}
-	inmemSinkConfig.Sink = inmemSink
-
-	policyBody := `
-path "/kv/*" {
-	capabilities = ["sudo", "create", "read", "update", "delete", "list"]
-}
-
-path "/auth/token/create" {
-	capabilities = ["create", "update"]
-}
-`
-	testAuthHelper, err := newTestAuthHelper(ctx, client, policyBody)
-	defer testAuthHelper.cleanup()
-
-	go ss.Run(ctx, testAuthHelper.authHandler.OutputCh, []*sink.SinkConfig{config, inmemSinkConfig})
-	defer func() {
-		<-ss.DoneCh
-	}()
-
-	// This has to be after the other defers so it happens first
-	defer cancelFunc()
-
-	// Check that no sink file exists
-	_, err = os.Lstat(out)
-	if err == nil {
-		t.Fatal("expected err")
-	}
-	if !os.IsNotExist(err) {
-		t.Fatal("expected notexist err")
-	}
-
-	token, err := testAuthHelper.getToken(out)
-	if err != nil {
-		t.Fatal(err)
-	}
-	t.Logf("auto-auth token: %q", token)
-
-	listener, err := net.Listen("tcp", "127.0.0.1:0")
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	defer listener.Close()
-
-	// Create a muxer and add paths relevant for the lease cache layer
-	mux := cache.AgentMux(ctx, cacheLogger, leaseCache, inmemSink, nil)
-	server := &http.Server{
-		Handler:           mux,
-		ReadHeaderTimeout: 10 * time.Second,
-		ReadTimeout:       30 * time.Second,
-		IdleTimeout:       5 * time.Minute,
-		ErrorLog:          cacheLogger.StandardLogger(nil),
-	}
-	go server.Serve(listener)
+	// Wait for listeners to come up
+	time.Sleep(2 * time.Second)
 
 	testClient, err := api.NewClient(api.DefaultConfig())
 	if err != nil {
 		t.Fatal(err)
 	}
 
-	if err := testClient.SetAddress("http://" + listener.Addr().String()); err != nil {
+	if err := testClient.SetAddress(agentAddr); err != nil {
 		t.Fatal(err)
 	}
-
-	// Wait for listeners to come up
-	time.Sleep(2 * time.Second)
 
 	// This block tests that no token on the client is detected by the agent
 	// and the auto-auth token is used
@@ -402,14 +410,77 @@ path "/auth/token/create" {
 	// token on the request will be prioritized.
 	{
 		// Empty the token in the client to ensure that auto-auth token is used
-		testClient.SetToken(client.Token())
+		testClient.SetToken(realClient.Token())
 
 		resp, err := testClient.Logical().Read("auth/token/lookup-self")
 		if err != nil {
 			t.Fatal(err)
 		}
-		if resp == nil || resp.Data["id"] != client.Token() {
+		if resp == nil || resp.Data["id"] != realClient.Token() {
 			t.Fatalf("failed to use the cluster client token to perform lookup-self")
 		}
 	}
+}
+
+func TestCache_ClientAutoAuth(t *testing.T) {
+	logger := logging.NewVaultLogger(log.Trace)
+
+	var out string
+	{
+		// We close and rm this file right away because we're just basically testing
+		// permissions and finding a usable file name for the sink to use below.
+		ouf, err := ioutil.TempFile("", "auth.tokensink.test.")
+		if err != nil {
+			t.Fatal(err)
+		}
+		out = ouf.Name()
+		ouf.Close()
+		os.Remove(out)
+		t.Logf("output: %s", out)
+	}
+	config := &sink.SinkConfig{
+		Logger: logger.Named("sink.file"),
+		Config: map[string]interface{}{
+			"path": out,
+		},
+	}
+	fs, err := file.NewFileSink(config)
+	if err != nil {
+		t.Fatal(err)
+	}
+	config.Sink = fs
+
+	tokenTtl := 200 * time.Millisecond
+	cleanup, _, agentAddr := testhelperAutoAuth(t, []*sink.SinkConfig{config}, out, tokenTtl)
+	defer cleanup()
+
+	// Wait for listeners to come up
+	time.Sleep(2 * time.Second)
+
+	clientConfig := api.DefaultConfig()
+	clientConfig.AgentFileSinkPath = out
+	testClient, err := api.NewClient(clientConfig)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if err := testClient.SetAddress(agentAddr); err != nil {
+		t.Fatal(err)
+	}
+
+	// This block tests token renewal and the client reading in file updates.
+	{
+		_, err := testClient.Logical().Read("kv/foo")
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		time.Sleep(tokenTtl)
+
+		_, err = testClient.Logical().Read("kv/foo")
+		if err != nil {
+			t.Fatal(err)
+		}
+	}
+
 }
