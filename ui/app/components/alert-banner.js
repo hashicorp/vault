cde7bbfa9ed586dbbea53c9b6478bc362d4925b4
Madalyn Parker <madalyn@hashicorp.com>
2019-03-12 15:25:09 -0400

    add jsdoc comments to alert banner component

--- ui/app/components/alert-banner.js
+++ ui/app/components/alert-banner.js
@@ -2,9 +2,31 @@ import Component from '@ember/component';
 import { computed } from '@ember/object';
 
 import { messageTypes } from 'vault/helpers/message-types';
-
+/**
+ * `AlertBanners` are used to inform users of important messages.
+ * @example
+ **  <AlertBanner
+ **   @type="danger"
+ **   @message="{{model.keyId}} is not a valid lease ID"
+ **  />
+ **/
 export default Component.extend({
+  /**
+   * The banner type. Should either be `info`, `warning`, `success`, or `danger`.
+   * @type {'info' | 'warning' | 'success' | 'danger'}
+   * @default null
+   * @example 'warning'
+   *
+   */
   type: null,
+
+  /**
+   * The message to display within the banner.
+   * @type String
+   * @default null
+   * @example 'hello'
+   */
+  message: null,
 
   yieldWithoutColumn: false,
 
