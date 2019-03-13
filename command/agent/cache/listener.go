eb9ce4d4ff666be69fb99d6b1e3aff0ccc6409cc
vishalnayak <vishalnayakv@gmail.com>
2019-03-12 20:55:30 -0400

    address review feedback

--- command/agent/cache/listener.go
+++ command/agent/cache/listener.go
@@ -5,7 +5,6 @@ import (
 	"fmt"
 	"net"
 
-	"strconv"
 	"strings"
 
 	"github.com/hashicorp/vault/command/agent/config"
@@ -19,57 +18,28 @@ func StartListener(lnConfig *config.Listener, unixSocketsConfig *config.UnixSock
 		return nil, nil, fmt.Errorf("invalid address")
 	}
 
-	bindProto := "tcp"
+	var ln net.Listener
+	var err error
 	switch lnConfig.Type {
 	case "tcp":
 		if addr == "" {
 			addr = "127.0.0.1:8200"
 		}
 
+		bindProto := "tcp"
 		// If they've passed 0.0.0.0, we only want to bind on IPv4
 		// rather than golang's dual stack default
 		if strings.HasPrefix(addr, "0.0.0.0:") {
 			bindProto = "tcp4"
 		}
 
-	case "unix":
-		addr = "unix://" + addr
-	default:
-		return nil, nil, fmt.Errorf("invalid listener type: %q", lnConfig.Type)
-	}
-
-	var netAddr net.Addr
-	switch {
-	case strings.HasPrefix(addr, "unix://"):
-		netAddr = &net.UnixAddr{
-			Name: addr[len("unix://"):],
-			Net:  "unix",
-		}
-	default:
-		host, port, err := net.SplitHostPort(addr)
+		ln, err = net.Listen(bindProto, addr)
 		if err != nil {
 			return nil, nil, err
 		}
+		ln = &server.TCPKeepAliveListener{ln.(*net.TCPListener)}
 
-		nPort, err := strconv.Atoi(port)
-		if err != nil {
-			return nil, nil, fmt.Errorf("invalid tcp port %q", port)
-		}
-
-		ip := net.ParseIP(host)
-		if ip == nil {
-			return nil, nil, fmt.Errorf("invalid ip address %q", addr)
-		}
-		netAddr = &net.TCPAddr{
-			IP:   ip,
-			Port: nPort,
-		}
-	}
-
-	var ln net.Listener
-	var err error
-	switch addrType := netAddr.(type) {
-	case *net.UnixAddr:
+	case "unix":
 		var uConfig *listenerutil.UnixSocketsConfig
 		if unixSocketsConfig != nil {
 			uConfig.Mode = unixSocketsConfig.Mode
@@ -77,17 +47,13 @@ func StartListener(lnConfig *config.Listener, unixSocketsConfig *config.UnixSock
 			uConfig.Group = unixSocketsConfig.Group
 		}
 
-		ln, err = listenerutil.UnixSocketListener(addrType.Name, uConfig)
+		ln, err = listenerutil.UnixSocketListener(addr, uConfig)
 		if err != nil {
 			return nil, nil, err
 		}
 
-	case *net.TCPAddr:
-		ln, err = net.Listen(bindProto, addrType.String())
-		if err != nil {
-			return nil, nil, err
-		}
-		ln = &server.TCPKeepAliveListener{ln.(*net.TCPListener)}
+	default:
+		return nil, nil, fmt.Errorf("invalid listener type: %q", lnConfig.Type)
 	}
 
 	props := map[string]string{"addr": ln.Addr().String()}
