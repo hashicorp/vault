8de190f31de99124becf81b9e0ba97cdc60d343d
Jim Kalafut <jim@kalafut.net>
2019-03-14 13:57:28 -0700

    Fix improperly formatted help output strings (#6414)

--- command/secrets_tune.go
+++ command/secrets_tune.go
@@ -57,14 +57,14 @@ func (c *SecretsTuneCommand) Flags() *FlagSets {
 	f.StringSliceVar(&StringSliceVar{
 		Name:   flagNameAuditNonHMACRequestKeys,
 		Target: &c.flagAuditNonHMACRequestKeys,
-		Usage: "Comma-separated string or list of keys that will not be HMAC'd by audit" +
+		Usage: "Comma-separated string or list of keys that will not be HMAC'd by audit " +
 			"devices in the request data object.",
 	})
 
 	f.StringSliceVar(&StringSliceVar{
 		Name:   flagNameAuditNonHMACResponseKeys,
 		Target: &c.flagAuditNonHMACResponseKeys,
-		Usage: "Comma-separated string or list of keys that will not be HMAC'd by audit" +
+		Usage: "Comma-separated string or list of keys that will not be HMAC'd by audit " +
 			"devices in the response data object.",
 	})
 
@@ -82,14 +82,14 @@ func (c *SecretsTuneCommand) Flags() *FlagSets {
 	f.StringVar(&StringVar{
 		Name:   flagNameDescription,
 		Target: &c.flagDescription,
-		Usage: "Human-friendly description of this secret engine. This overrides the" +
+		Usage: "Human-friendly description of this secret engine. This overrides the " +
 			"current stored value, if any.",
 	})
 
 	f.StringVar(&StringVar{
 		Name:   flagNameListingVisibility,
 		Target: &c.flagListingVisibility,
-		Usage: "Determines the visibility of the mount in the UI-specific listing" +
+		Usage: "Determines the visibility of the mount in the UI-specific listing " +
 			"endpoint.",
 	})
 
