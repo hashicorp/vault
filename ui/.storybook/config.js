d8a1ea404295cb31a1057915e10643a4cf1f69ed
Noelle Daley <adriannenoelle@gmail.com>
2019-03-27 09:47:35 -0700

    center all stories

--- ui/.storybook/config.js
+++ ui/.storybook/config.js
@@ -1,9 +1,12 @@
-import { configure, addParameters } from '@storybook/ember';
+import { configure, addParameters, addDecorator } from '@storybook/ember';
+import Centered from '@storybook/addon-centered/ember';
 
 function loadStories() {
   // automatically import all files ending in *.stories.js
   const req = require.context('../stories/', true, /.stories.js$/);
   req.keys().forEach(filename => req(filename));
 }
+
+addDecorator(Centered);
 
 configure(loadStories, module);
