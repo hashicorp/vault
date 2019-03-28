5aa7ee0d11b95e2b8101541e4ce56be5fc923820
Madalyn Parker <madalyn@hashicorp.com>
2019-03-28 14:45:32 -0400

    remove see links from alert-banner

--- ui/app/components/alert-banner.js
+++ ui/app/components/alert-banner.js
@@ -11,11 +11,9 @@ import { messageTypes } from 'vault/helpers/message-types';
  * <AlertBanner @type="danger" @message="{{model.keyId}} is not a valid lease ID"/>
  * ```
  *
- * @property [AlertBanner.type=null] {String} - The banner type. This comes from the message-types helper.
+ * @property type=null {String} - The banner type. This comes from the message-types helper.
  * @property message=null {String} - The message to display within the banner.
  *
- * @see {@link https://github.com/hashicorp/vault/search?l=Handlebars&q=AlertBanner|Uses of AlertBanner}
- * @see {@link https://github.com/hashicorp/vault/blob/master/ui/app/components/alert-banner.js|AlertBanner Source Code}
  */
 
 export default Component.extend({
