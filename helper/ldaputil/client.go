db8f0155d7d6e6d9f7ffab178f9b39f779cd3df8
Artem Alexandrov <artem.alexandrov@corp.mail.ru>
2019-04-02 17:59:22 +0300

    Fix getCN during group fetching in auth/ldap

--- helper/ldaputil/client.go
+++ helper/ldaputil/client.go
@@ -428,7 +428,7 @@ func getCN(dn string) string {
 
 	for _, rdn := range parsedDN.RDNs {
 		for _, rdnAttr := range rdn.Attributes {
-			if rdnAttr.Type == "CN" {
+			if strings.ToLower(rdnAttr.Type) == "cn" {
 				return rdnAttr.Value
 			}
 		}
