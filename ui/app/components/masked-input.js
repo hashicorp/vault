4f1bdc37b1876b53f4cf725d4fc0e71541f416a6
Matthew Irish <matthew@hashicorp.com>
2019-04-15 17:11:40 -0500

    fix doc changes

--- ui/app/components/masked-input.js
+++ ui/app/components/masked-input.js
@@ -7,20 +7,18 @@ import autosize from 'autosize';
  * `MaskedInput` components are textarea inputs where the input is hidden. They are used to enter sensitive information like passwords.
  *
  * @example
- * ```js
  * <MaskedInput
  *  @value={{attr.options.defaultValue}}
  *  @placeholder="secret"
  *  @allowCopy={{true}}
  *  @onChange={{action "someAction"}}
  * />
- * ```
  *
  * @param [value] {String} - The value to display in the input.
  * @param [placeholder=value] {String} - The placeholder to display before the user has entered any input.
  * @param [allowCopy=null] {bool} - Whether or not the input should render with a copy button.
  * @param [displayOnly=false] {bool} - Whether or not to display the value as a display only `pre` element or as an input.
- * @param [onChange=false] {Function|action} - A function to call when the value of the input changes.
+ * @param [onChange=Function.prototype] {Function|action} - A function to call when the value of the input changes.
  *
  *
  */
--- ui/stories/masked-input.md
+++ ui/stories/masked-input.md
@@ -10,13 +10,18 @@
 | [placeholder] | <code>String</code> | <code>value</code> | The placeholder to display before the user has entered any input. |
 | [allowCopy] | <code>bool</code> | <code></code> | Whether or not the input should render with a copy button. |
 | [displayOnly] | <code>bool</code> | <code>false</code> | Whether or not to display the value as a display only `pre` element or as an input. |
-| [onChange] | <code>function</code> \| <code>action</code> | <code>false</code> | A function to call when the value of the input changes. |
+| [onChange] | <code>function</code> \| <code>action</code> | <code>Function.prototype</code> | A function to call when the value of the input changes. |
 
 **Example**
   
 ```js
-<MaskedInput
- 
+ <MaskedInput
+  @value={{attr.options.defaultValue}}
+  @placeholder="secret"
+  @allowCopy={{true}}
+  @onChange={{action "someAction"}}
+ />
+```
 
 **See**
 
