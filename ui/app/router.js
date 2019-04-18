c014f00ccc7577f768bcfdadeb0fbc2731153b62
Matthew Irish <matthew@hashicorp.com>
2019-04-18 16:36:52 -0500

    move replication routes to a lazy engine

--- ui/app/router.js
+++ ui/app/router.js
@@ -114,22 +114,6 @@ Router.map(function() {
         this.route('edit', { path: '/:policy_name/edit' });
       });
       this.route('replication-dr-promote');
-      this.route('replication', function() {
-        this.route('index', { path: '/' });
-        this.route('mode', { path: '/:replication_mode' }, function() {
-          //details
-          this.route('index', { path: '/' });
-          this.route('manage');
-          this.route('secondaries', function() {
-            this.route('add', { path: '/add' });
-            this.route('revoke', { path: '/revoke' });
-            this.route('config-show', { path: '/config/show/:secondary_id' });
-            this.route('config-edit', { path: '/config/edit/:secondary_id' });
-            this.route('config-create', { path: '/config/create/:secondary_id' });
-          });
-        });
-      });
-
       if (config.addRootMounts) {
         config.addRootMounts.call(this);
       }
