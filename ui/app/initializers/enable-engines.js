0bff3151f4ba37250306124f6176ca2472d532e5
Matthew Irish <matthew@hashicorp.com>
2019-04-18 15:40:48 -0500

    start moving components needed for replication to core addon

--- ui/app/initializers/enable-engines.js
+++ ui/app/initializers/enable-engines.js
@@ -1,11 +1,11 @@
-import config from '../config/environment';
+//import config from '../config/environment';
 
 export function initialize(/* application */) {
   // attach mount hooks to the environment config
   // context will be the router DSL
-  config.addRootMounts = function() {
-    console.log(this);
-  };
+  //config.addRootMounts = function() {
+  //console.log(this);
+  //};
 }
 
 export default {
