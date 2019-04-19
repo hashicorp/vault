48806928b1443d7e6fa2f2ad914349d20b880f64
Matthew Irish <matthew@hashicorp.com>
2019-04-19 12:49:28 -0500

    share services with the replication engine

--- ui/lib/replication/addon/engine.js
+++ ui/lib/replication/addon/engine.js
@@ -4,10 +4,13 @@ import Resolver from './resolver';
 import config from './config/environment';
 
 const { modulePrefix } = config;
-
+/* eslint-disable ember/avoid-leaking-state-in-ember-objects */
 const Eng = Engine.extend({
   modulePrefix,
   Resolver,
+  dependencies: {
+    services: ['store', 'replication-mode'],
+  },
 });
 
 loadInitializers(Eng, modulePrefix);
