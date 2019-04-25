af56b7309ef0afd8ebfd41b345dc9a3e7d2b230a
Matthew Irish <matthew@hashicorp.com>
2019-04-25 15:11:01 -0500

    remove includes helper, use contains from ember-composable-helpers

--- ui/app/helpers/includes.js
+++
@@ -1,7 +0,0 @@
-import { helper as buildHelper } from '@ember/component/helper';
-
-export function includes([haystack, needle]) {
-  return haystack.includes(needle);
-}
-
-export default buildHelper(includes);
