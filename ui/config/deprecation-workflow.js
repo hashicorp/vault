4741369835c7c5a844c8546698ed651594fd6dae
Matthew Irish <matthew@hashicorp.com>
2019-05-02 16:01:07 -0500

    ignore test-helper jquery context warning

--- ui/config/deprecation-workflow.js
+++ ui/config/deprecation-workflow.js
@@ -5,5 +5,7 @@ self.deprecationWorkflow.config = {
     // ivy-codemirror and ember-radio-button still use send-action
     { handler: 'silence', matchId: 'ember-component.send-action' },
     { handler: 'silence', matchId: 'ember-runtime.deprecate-copy-copyable' },
+    // ember-cli-page-object uses jquery's this.$() by default - this will change when we remove jquery
+    { handler: 'silence', matchId: 'ember-test-helpers.rendering-context.jquery-element' },
   ],
 };
