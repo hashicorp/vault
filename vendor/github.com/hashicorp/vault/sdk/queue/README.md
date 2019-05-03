2ba681d85f161d5489a7e351ee08c20c3a7d5a91
Clint Shryock <clint@ctshryock.com>
2019-05-03 15:50:57 -0500

    vendor dependencies with 'go mod vendor'

--- vendor/github.com/hashicorp/vault/sdk/queue/README.md
+++ vendor/github.com/hashicorp/vault/sdk/queue/README.md
@@ -6,3 +6,4 @@ as an in-memory list of `queue.Item` sorted by their `priority`, and offers
 methods to find or remove items by their key. Internally it
 uses `container/heap`; see [Example Priority
 Queue](https://golang.org/pkg/container/heap/#example__priorityQueue)
+
--- vendor/github.com/hashicorp/vault/sdk/queue/priority_queue.go
+++ vendor/github.com/hashicorp/vault/sdk/queue/priority_queue.go
@@ -6,13 +6,11 @@
 package queue
 
 import (
-        "container/heap"
-        "errors"
-        "fmt"
-        "sort"
-        "sync"
+	"container/heap"
+	"errors"
+	"sync"
 
-        "github.com/mitchellh/copystructure"
+	"github.com/mitchellh/copystructure"
 )
 
 // ErrEmpty is returned for queues with no items
@@ -23,32 +21,15 @@ var ErrEmpty = errors.New("queue is empty")
 // error. If an Item needs to be updated or replaced, pop the item first.
 var ErrDuplicateItem = errors.New("duplicate item")
 
-// ErrItemNotFound is a struct that implements the error interface
-var _ error = &ErrItemNotFound{}
-
-type ErrItemNotFound struct {
-        Key string
-}
-
-func (e *ErrItemNotFound) Error() string {
-        return fmt.Sprintf("queue item with key (%s) not found", e.Key)
-}
-
-func newErrItemNotFound(key string) *ErrItemNotFound {
-        return &ErrItemNotFound{
-                Key: key,
-        }
-}
-
 // New initializes the internal data structures and returns a new
 // PriorityQueue
 func New() *PriorityQueue {
-        pq := PriorityQueue{
-                data:    make(pQueue, 0),
-                dataMap: make(map[string]*Item),
-        }
-        heap.Init(&pq.data)
-        return &pq
+	pq := PriorityQueue{
+		data:    make(queue, 0),
+		dataMap: make(map[string]*Item),
+	}
+	heap.Init(&pq.data)
+	return &pq
 }
 
 // PriorityQueue facilitates queue of Items, providing Push, Pop, and
@@ -59,55 +40,57 @@ func New() *PriorityQueue {
 // must be kept in sync with the data slice.
 // See https://golang.org/pkg/container/heap/#example__priorityQueue
 type PriorityQueue struct {
-        // data is the internal structure that holds the queue, and is operated on by
-        // heap functions
-        data pQueue
+	// data is the internal structure that holds the queue, and is operated on by
+	// heap functions
+	data queue
 
-        // dataMap represents all the items in the queue, with unique indexes, used
-        // for finding specific items. dataMap is kept in sync with the data slice
-        dataMap map[string]*Item
+	// dataMap represents all the items in the queue, with unique indexes, used
+	// for finding specific items. dataMap is kept in sync with the data slice
+	dataMap map[string]*Item
 
-        // mapMutex is used to facilitate read/write locks on the dataMap
-        dataMutex sync.Mutex
+	// mapMutex is used to facilitate read/write locks on the dataMap
+	sync.Mutex
 }
 
-// pQueue is the internal data structure used to satisfy heap.Interface. This
+// queue is the internal data structure used to satisfy heap.Interface. This
 // prevents users from calling Pop and Push heap methods directly
-type pQueue []*Item
+type queue []*Item
 
-var _ heap.Interface = &pQueue{}
-var _ sort.Interface = &pQueue{}
+var _ heap.Interface = &queue{}
 
 // Item is something managed in the priority queue
 type Item struct {
-        // Key is a unique string used to identify items in the internal data map
-        Key string
-        // Value is an unspecified type that implementations can use to store
-        // information
-        Value interface{}
+	// Key is a unique string used to identify items in the internal data map
+	Key string
+	// Value is an unspecified type that implementations can use to store
+	// information
+	Value interface{}
 
-        // Priority determines ordering in the queue, with the lowest value being the
-        // highest priority
-        Priority int64
+	// Priority determines ordering in the queue, with the lowest value being the
+	// highest priority
+	Priority int64
 
-        // index is an internal value used by the heap package, and should not be
-        // modified by any consumer of the priority queue
-        index int
+	// index is an internal value used by the heap package, and should not be
+	// modified by any consumer of the priority queue
+	index int
 }
+
+// Len returns the count of items in the Priority Queue
+func (pq *PriorityQueue) Len() int { return pq.data.Len() }
 
 // Pop pops the highest priority item from the queue. This is a
 // wrapper/convenience method that calls heap.Pop, so consumers do not need to
 // invoke heap functions directly
 func (pq *PriorityQueue) Pop() (*Item, error) {
-        pq.dataMutex.Lock()
-        defer pq.dataMutex.Unlock()
+	pq.Lock()
+	defer pq.Unlock()
 
-        if pq.Len() == 0 {
-                return nil, ErrEmpty
-        }
-        item := heap.Pop(&pq.data).(*Item)
-        delete(pq.dataMap, item.Key)
-        return item, nil
+	if pq.Len() == 0 {
+		return nil, ErrEmpty
+	}
+	item := heap.Pop(&pq.data).(*Item)
+	delete(pq.dataMap, item.Key)
+	return item, nil
 }
 
 // Push pushes an item on to the queue. This is a wrapper/convenience
@@ -116,91 +99,89 @@ func (pq *PriorityQueue) Pop() (*Item, error) {
 // cannot be updated. To modify an Item, users must first remove it and re-push
 // it after modifications
 func (pq *PriorityQueue) Push(i *Item) error {
-        if i.Key == "" {
-                return errors.New("error adding item: Item Key is required")
-        }
+	if i == nil || i.Key == "" {
+		return errors.New("error adding item: Item Key is required")
+	}
 
-        pq.dataMutex.Lock()
-        defer pq.dataMutex.Unlock()
+	pq.Lock()
+	defer pq.Unlock()
 
-        if _, ok := pq.dataMap[i.Key]; ok {
-                return ErrDuplicateItem
-        }
+	if _, ok := pq.dataMap[i.Key]; ok {
+		return ErrDuplicateItem
+	}
+	// copy the item value(s) so that modifications to the source item does not
+	// affect the item on the queue
+	clone, err := copystructure.Copy(i)
+	if err != nil {
+		return err
+	}
 
-        // copy the item value(s) so that modifications to the source item does not
-        // affect the item on the queue
-        clone, err := copystructure.Copy(i)
-        if err != nil {
-                return err
-        }
-
-        pq.dataMap[i.Key] = clone.(*Item)
-        heap.Push(&pq.data, clone)
-        return nil
+	pq.dataMap[i.Key] = clone.(*Item)
+	heap.Push(&pq.data, clone)
+	return nil
 }
 
 // PopByKey searches the queue for an item with the given key and removes it
 // from the queue if found. Returns ErrItemNotFound(key) if not found. This
 // method must fix the queue after removal.
 func (pq *PriorityQueue) PopByKey(key string) (*Item, error) {
-        pq.dataMutex.Lock()
-        defer pq.dataMutex.Unlock()
+	pq.Lock()
+	defer pq.Unlock()
 
-        item, ok := pq.dataMap[key]
-        if !ok {
-                return nil, newErrItemNotFound(key)
-        }
+	item, ok := pq.dataMap[key]
+	if !ok {
+		return nil, nil
+	}
 
-        // remove the item the heap and delete it from the dataMap
-        itemRaw := heap.Remove(&pq.data, item.index)
-        delete(pq.dataMap, key)
+	// remove the item the heap and delete it from the dataMap
+	itemRaw := heap.Remove(&pq.data, item.index)
+	delete(pq.dataMap, key)
 
-        if i, ok := itemRaw.(*Item); ok {
-                return i, nil
-        }
+	if i, ok := itemRaw.(*Item); ok {
+		return i, nil
+	}
 
-        return nil, newErrItemNotFound(key)
+	return nil, nil
 }
 
 // Len returns the number of items in the queue data structure. Do not use this
 // method directly on the queue, use PriorityQueue.Len() instead.
-func (pq pQueue) Len() int { return len(pq) }
+func (q queue) Len() int { return len(q) }
 
-// Len returns the count of items in the Priority Queue
-func (q *PriorityQueue) Len() int { return q.data.Len() }
-
-// Less returns the less of two things, which in this case, we return the
-// highest value thing, because the priority ordering is "lowest value, highest
-// priority"
-func (pq pQueue) Less(i, j int) bool {
-        return pq[i].Priority < pq[j].Priority
+// Less returns whether the Item with index i should sort before the Item with
+// index j in the queue. This method is used by the queue to determine priority
+// internally; the Item with the lower value wins. (priority zero is higher
+// priority than 1). The priority of Items with equal values is undetermined.
+func (q queue) Less(i, j int) bool {
+	return q[i].Priority < q[j].Priority
 }
 
 // Swap swaps things in-place; part of sort.Interface
-func (pq pQueue) Swap(i, j int) {
-        pq[i], pq[j] = pq[j], pq[i]
-        pq[i].index = i
-        pq[j].index = j
+func (q queue) Swap(i, j int) {
+	q[i], q[j] = q[j], q[i]
+	q[i].index = i
+	q[j].index = j
 }
 
 // Push is used by heap.Interface to push items onto the heap. This method is
 // invoked by container/heap, and should not be used directly.
 // See: https://golang.org/pkg/container/heap/#Interface
-func (pq *pQueue) Push(x interface{}) {
-        n := len(*pq)
-        item := x.(*Item)
-        item.index = n
-        *pq = append(*pq, item)
+func (q *queue) Push(x interface{}) {
+	n := len(*q)
+	item := x.(*Item)
+	item.index = n
+	*q = append(*q, item)
 }
 
 // Pop is used by heap.Interface to pop items off of the heap. This method is
 // invoked by container/heap, and should not be used directly.
 // See: https://golang.org/pkg/container/heap/#Interface
-func (pq *pQueue) Pop() interface{} {
-        old := *pq
-        n := len(old)
-        item := old[n-1]
-        item.index = -1 //for saftey
-        *pq = old[0 : n-1]
-        return item
+func (q *queue) Pop() interface{} {
+	old := *q
+	n := len(old)
+	item := old[n-1]
+	old[n-1] = nil  // avoid memory leak
+	item.index = -1 // for safety
+	*q = old[0 : n-1]
+	return item
 }
