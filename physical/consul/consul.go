5dd4ce28ea9a6840dfb74becd83144799a1790d2
Mark Gritter <mgritter@hashicorp.com>
2019-05-08 11:48:31 -0500

    Determine scheme from configured address. (#6689)
    * Determine scheme from configured address.
    
    * Added test; use parsed version of address for TLS config.

--- physical/consul/consul.go
+++ physical/consul/consul.go
@@ -225,6 +225,23 @@ func NewConsulBackend(conf map[string]string, logger log.Logger) (physical.Backe
 		if logger.IsDebug() {
 			logger.Debug("config address set", "address", addr)
 		}
+
+		// Copied from the Consul API module; set the Scheme based on
+		// the protocol field if address looks ike a URL.
+		// This can enable the TLS configuration below.
+		parts := strings.SplitN(addr, "://", 2)
+		if len(parts) == 2 {
+			if parts[0] == "http" || parts[0] == "https" {
+				consulConf.Scheme = parts[0]
+				consulConf.Address = parts[1]
+				if logger.IsDebug() {
+					logger.Debug("config address parsed", "scheme", parts[0])
+					logger.Debug("config scheme parsed", "address", parts[1])
+				}
+			} else {
+				return nil, errors.New("address should be host[:port], not URL")
+			}
+		}
 	}
 	if scheme, ok := conf["scheme"]; ok {
 		consulConf.Scheme = scheme
@@ -238,7 +255,8 @@ func NewConsulBackend(conf map[string]string, logger log.Logger) (physical.Backe
 	}
 
 	if consulConf.Scheme == "https" {
-		tlsClientConfig, err := setupTLSConfig(conf)
+		// Use the parsed Address instead of the raw conf['address']
+		tlsClientConfig, err := setupTLSConfig(conf, consulConf.Address)
 		if err != nil {
 			return nil, err
 		}
@@ -301,8 +319,8 @@ func NewConsulBackend(conf map[string]string, logger log.Logger) (physical.Backe
 	return c, nil
 }
 
-func setupTLSConfig(conf map[string]string) (*tls.Config, error) {
-	serverName, _, err := net.SplitHostPort(conf["address"])
+func setupTLSConfig(conf map[string]string, address string) (*tls.Config, error) {
+	serverName, _, err := net.SplitHostPort(address)
 	switch {
 	case err == nil:
 	case strings.Contains(err.Error(), "missing port"):
