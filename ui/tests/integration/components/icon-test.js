de84917d9a8c22bbd3bf186df81c422b5f5fad5d
Matthew Irish <matthew@hashicorp.com>
2019-05-09 15:53:34 -0500

    remove old i-con, add tests for icon and chevron components

--- ui/tests/integration/components/icon-test.js
+++ ui/tests/integration/components/icon-test.js
@@ -3,24 +3,20 @@ import { setupRenderingTest } from 'ember-qunit';
 import { render } from '@ember/test-helpers';
 import hbs from 'htmlbars-inline-precompile';
 
-module('Integration | Component | ic-on', function(hooks) {
+module('Integration | Component | icon', function(hooks) {
   setupRenderingTest(hooks);
 
   test('it renders', async function(assert) {
-    // Set any properties with this.set('myProperty', 'value');
-    // Handle any actions with this.set('myAction', function(val) { ... });
+    await render(hbs`<Icon class="i-con" />`);
+    assert.dom('.i-con').exists('renders');
 
-    await render(hbs`{{ic-on}}`);
+    await render(hbs`<Icon @glyph="vault-logo" />`);
+    assert.dom('.vault-logo').exists('inlines the SVG');
 
-    assert.equal(this.element.textContent.trim(), '');
+    await render(hbs`<Icon class="ah" aria-hidden="true" />`);
+    assert.dom('.ah').hasAttribute('aria-hidden', 'true', 'renders aria-hidden');
 
-    // Template block usage:
-    await render(hbs`
-      {{#ic-on}}
-        template block text
-      {{/ic-on}}
-    `);
-
-    assert.equal(this.element.textContent.trim(), 'template block text');
+    await render(hbs`<Icon class="al" aria-label="Testing" />`);
+    assert.dom('.al').hasAttribute('aria-label', 'Testing', 'renders aria-label');
   });
 });
