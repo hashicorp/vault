e5a245260179c4062faaf5f70b73fd7269a6c2b6
Joshua Ogle <joshua@joshuaogle.com>
2019-05-09 11:31:26 -0600

    Move toolbar back out of secretlistheader

--- ui/app/components/secret-list-header.js
+++ ui/app/components/secret-list-header.js
@@ -1,5 +1,4 @@
 import Component from '@ember/component';
-import { computed } from '@ember/object';
 
 export default Component.extend({
   tagName: '',
@@ -11,14 +10,4 @@ export default Component.extend({
   backendCrumb: null,
   model: null,
   options: null,
-  hasItems: computed('model.meta.total', function() {
-    return this.get('model.meta.total');
-  }),
-  isConfigurable: computed('model.type', function() {
-    const configurableEngines = ['aws', 'ssh', 'pki'];
-    return configurableEngines.includes(this.get('model.type'));
-  }),
-  isConfigurableTab: computed('isCertTab', 'isConfigure', function() {
-    return this.get('isCertTab') || this.get('isConfigure');
-  }),
 });
