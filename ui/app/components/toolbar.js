1abbb9e134c07837a5c95018a70e68d7c452f89a
Matthew Irish <matthew@hashicorp.com>
2019-05-09 21:29:05 -0500

    move new components to core addon and comment out ICon usage

--- ui/app/components/toolbar.js
+++
@@ -1,6 +0,0 @@
-import OuterHTML from './outer-html';
-
-export default OuterHTML.extend({
-  classNames: ['toolbar'],
-  tagName: 'nav',
-});
---
+++ ui/lib/core/addon/components/toolbar.js
@@ -0,0 +1,7 @@
+import Component from '@ember/component';
+import layout from '../templates/components/toolbar';
+
+export default Component.extend({
+  tagName: '',
+  layout,
+});
