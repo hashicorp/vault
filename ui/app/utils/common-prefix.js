b88a88c4edffbb474c6f54e58cd49664879beb57
Matthew Irish <matthew@hashicorp.com>
2019-05-20 15:36:44 -0500

    add test

--- ui/app/utils/common-prefix.js
+++ ui/app/utils/common-prefix.js
@@ -2,8 +2,9 @@ import { get } from '@ember/object';
 
 export default function(arr, attribute = 'id') {
   let content = arr || [];
-  // this assumes an already sorted array, if we have a match,
-  // it will have matched the first one so we also want the last one
+  // this assumes an already sorted array
+  // if the array is sorted, we want to compare the first and last
+  // item in the array - if they share a prefix, all of the items do
   let firstString = get(content[0], attribute);
   let lastString = get(content[arr.length - 1], attribute);
 
@@ -11,13 +12,13 @@ export default function(arr, attribute = 'id') {
   let targetLength = firstString.length;
   let prefixLength = 0;
   // walk the two strings, and if they match at the current length,
-  // increment the prefix and try again
+  // increment the prefixLength and try again
   while (
     prefixLength < targetLength &&
     firstString.charAt(prefixLength) === lastString.charAt(prefixLength)
   ) {
     prefixLength++;
   }
-  // slice the prefix from the match
+  // slice the prefix from the first item
   return firstString.substring(0, prefixLength);
 }
