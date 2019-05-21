be2704cc917eb02421cc8791dd152c70d31e6f07
Joshua Ogle <joshua@joshuaogle.com>
2019-05-20 18:10:42 -0600

    UI: Add Toolbar to Storybook (#6761)
    * Add Toolbar stories
    * Update ConfirmAction story

--- ui/lib/core/addon/components/toolbar-link.js
+++ ui/lib/core/addon/components/toolbar-link.js
@@ -1,3 +1,23 @@
+/**
+ * @module ToolbarLink
+ * `ToolbarLink` components style links and buttons for the Toolbar
+ * It should only be used inside of `Toolbar`.
+ *
+ * @example
+ * ```js
+ * <Toolbar>
+ *   <ToolbarActions>
+ *     <ToolbarLink @params={{array 'vault.cluster.policies.create'}} @type="add">
+ *       Create policy
+ *     </ToolbarLink>
+ *   </ToolbarActions>
+ * </Toolbar>
+ * ```
+ *
+ * @param params=''{Array} Array to pass to LinkTo
+ * @param type=''{String} Use "add" to change icon
+ */
+
 import Component from '@ember/component';
 import { computed } from '@ember/object';
 import layout from '../templates/components/toolbar-link';
