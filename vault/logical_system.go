4e1b8b690728f62b128274e2493898675b868f64
Jeff Mitchell <jeffrey.mitchell@gmail.com>
2019-06-18 14:58:45 -0400

    Switch a gogo -> proto instance

--- vault/logical_system.go
+++ vault/logical_system.go
@@ -19,7 +19,7 @@ import (
 	"sync"
 	"time"
 
-	"github.com/gogo/protobuf/proto"
+	"github.com/golang/protobuf/proto"
 
 	"github.com/hashicorp/vault/physical/raft"
 	"github.com/hashicorp/vault/vault/seal"
