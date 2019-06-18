cdaaf3c00e81b0fa87e9e99a4599b897ceb8c060
Lexman42 <awybo@hashicorp.com>
2019-06-17 23:28:09 -0400

    adds tests for public keys returned by .well-known endpoint when keys are created, rotated, and deleted

--- vault/identity_store_oidc_test.go
+++ vault/identity_store_oidc_test.go
@@ -1,12 +1,14 @@
 package vault
 
 import (
+	"encoding/json"
 	"fmt"
 	"testing"
 
 	"github.com/go-test/deep"
 	"github.com/hashicorp/vault/helper/namespace"
 	"github.com/hashicorp/vault/sdk/logical"
+	"gopkg.in/square/go-jose.v2"
 )
 
 // TestOIDC_Path_OIDCRoleRole tests CRUD operations for roles
@@ -15,16 +17,15 @@ func TestOIDC_Path_OIDCRoleRole(t *testing.T) {
 	ctx := namespace.RootContext(nil)
 	storage := &logical.InmemStorage{}
 
-	// Create a test key "test-key" -- should succeed
-	resp, err := c.identityStore.HandleRequest(ctx, &logical.Request{
+	// Create a test key "test-key"
+	c.identityStore.HandleRequest(ctx, &logical.Request{
 		Path:      "oidc/key/test-key",
 		Operation: logical.UpdateOperation,
 		Storage:   storage,
 	})
-	expectSuccess(t, resp, err)
 
 	// Create a test role "test-role1" with a valid key -- should succeed
-	resp, err = c.identityStore.HandleRequest(ctx, &logical.Request{
+	resp, err := c.identityStore.HandleRequest(ctx, &logical.Request{
 		Path:      "oidc/role/test-role1",
 		Operation: logical.UpdateOperation,
 		Data: map[string]interface{}{
@@ -167,6 +168,233 @@ func TestOIDC_Path_OIDCRole(t *testing.T) {
 	// validate list response
 	delete(expectedStrings, "test-role2")
 	expectStrings(t, respListRoleAfterDelete.Data["keys"].([]string), expectedStrings)
+}
+
+// TestOIDC_Path_OIDCKeyKey tests CRUD operations for keys
+func TestOIDC_Path_OIDCKeyKey(t *testing.T) {
+	c, _, _ := TestCoreUnsealed(t)
+	ctx := namespace.RootContext(nil)
+	storage := &logical.InmemStorage{}
+
+	// Create a test key "test-key" -- should succeed
+	resp, err := c.identityStore.HandleRequest(ctx, &logical.Request{
+		Path:      "oidc/key/test-key",
+		Operation: logical.UpdateOperation,
+		Storage:   storage,
+	})
+	expectSuccess(t, resp, err)
+
+	// Read "test-key"
+	respReadTestKey, err1 := c.identityStore.HandleRequest(ctx, &logical.Request{
+		Path:      "oidc/key/test-key",
+		Operation: logical.ReadOperation,
+		Storage:   storage,
+	})
+	expectSuccess(t, respReadTestKey, err1)
+
+	// Update "test-key" -- should succeed
+	resp, err = c.identityStore.HandleRequest(ctx, &logical.Request{
+		Path:      "oidc/key/test-key",
+		Operation: logical.UpdateOperation,
+		Data: map[string]interface{}{
+			"rotation_period":  "1m",
+			"verification_ttl": "2h",
+		},
+		Storage: storage,
+	})
+	expectSuccess(t, resp, err)
+
+	// Read "test-key" again
+	respReadTestKeyAfterUpdate, err2 := c.identityStore.HandleRequest(ctx, &logical.Request{
+		Path:      "oidc/key/test-key",
+		Operation: logical.ReadOperation,
+		Storage:   storage,
+	})
+	expectSuccess(t, respReadTestKeyAfterUpdate, err2)
+
+	// Compare response for "test-key" before and after it was updated
+	expectedDiff := map[string]interface{}{
+		"Data.map[rotation_period]: 21600 != 60":    true,
+		"Data.map[verification_ttl]: 21600 != 7200": true,
+	}
+	diff := deep.Equal(respReadTestKey, respReadTestKeyAfterUpdate)
+	expectStrings(t, diff, expectedDiff)
+
+	// Create a role that depends on test key
+	c.identityStore.HandleRequest(ctx, &logical.Request{
+		Path:      "oidc/role/test-role",
+		Operation: logical.UpdateOperation,
+		Data: map[string]interface{}{
+			"key": "test-key",
+		},
+		Storage: storage,
+	})
+
+	// Delete test-key -- should fail because test-role depends on test-key
+	resp, err = c.identityStore.HandleRequest(ctx, &logical.Request{
+		Path:      "oidc/key/test-key",
+		Operation: logical.DeleteOperation,
+		Storage:   storage,
+	})
+	expectError(t, resp, err)
+	// validate error message
+	expectedStrings := map[string]interface{}{
+		"Unable to delete the key: \"test-key\" because it is currently referenced by these roles: test-role": true,
+	}
+	expectStrings(t, []string{resp.Data["error"].(string)}, expectedStrings)
+
+	// Delete test-role
+	c.identityStore.HandleRequest(ctx, &logical.Request{
+		Path:      "oidc/role/test-role",
+		Operation: logical.DeleteOperation,
+		Storage:   storage,
+	})
+
+	// Delete test-key -- should succeed this time because no roles depend on test-key
+	resp, err = c.identityStore.HandleRequest(ctx, &logical.Request{
+		Path:      "oidc/key/test-key",
+		Operation: logical.DeleteOperation,
+		Storage:   storage,
+	})
+	fmt.Printf("delete test-key:\n%#v", resp)
+	expectSuccess(t, resp, err)
+}
+
+// TestOIDC_Path_OIDCKey tests the List operation for keys
+func TestOIDC_Path_OIDCKey(t *testing.T) {
+	c, _, _ := TestCoreUnsealed(t)
+	ctx := namespace.RootContext(nil)
+	storage := &logical.InmemStorage{}
+
+	// Prepare two keys, test-key1 and test-key2
+	c.identityStore.HandleRequest(ctx, &logical.Request{
+		Path:      "oidc/key/test-key1",
+		Operation: logical.UpdateOperation,
+		Storage:   storage,
+	})
+
+	c.identityStore.HandleRequest(ctx, &logical.Request{
+		Path:      "oidc/key/test-key2",
+		Operation: logical.UpdateOperation,
+		Storage:   storage,
+	})
+
+	// list keys
+	respListKey, listErr := c.identityStore.HandleRequest(ctx, &logical.Request{
+		Path:      "oidc/key",
+		Operation: logical.ListOperation,
+		Storage:   storage,
+	})
+	expectSuccess(t, respListKey, listErr)
+
+	// validate list response
+	expectedStrings := map[string]interface{}{"test-key1": true, "test-key2": true}
+	expectStrings(t, respListKey.Data["keys"].([]string), expectedStrings)
+
+	// delete test-key2
+	c.identityStore.HandleRequest(ctx, &logical.Request{
+		Path:      "oidc/key/test-key2",
+		Operation: logical.DeleteOperation,
+		Storage:   storage,
+	})
+
+	// list keyes again and validate response
+	respListKeyAfterDelete, listErrAfterDelete := c.identityStore.HandleRequest(ctx, &logical.Request{
+		Path:      "oidc/key",
+		Operation: logical.ListOperation,
+		Storage:   storage,
+	})
+	expectSuccess(t, respListKeyAfterDelete, listErrAfterDelete)
+
+	// validate list response
+	delete(expectedStrings, "test-key2")
+	expectStrings(t, respListKeyAfterDelete.Data["keys"].([]string), expectedStrings)
+}
+
+// TestOIDC_PublicKeys tests that public keys are updated by
+// key creation, rotation, and deletion
+func TestOIDC_PublicKeys(t *testing.T) {
+	c, _, _ := TestCoreUnsealed(t)
+	ctx := namespace.RootContext(nil)
+	storage := &logical.InmemStorage{}
+
+	// Create a test key "test-key"
+	c.identityStore.HandleRequest(ctx, &logical.Request{
+		Path:      "oidc/key/test-key",
+		Operation: logical.UpdateOperation,
+		Storage:   storage,
+	})
+
+	//todo THIS NAMING IS ALL WRONG - SHOULD BE PUBLIC KEYS, NOT CERTS...
+	// .well-known/certs should contain 1 public key
+	resp, err := c.identityStore.HandleRequest(ctx, &logical.Request{
+		Path:      "oidc/.well-known/certs",
+		Operation: logical.ReadOperation,
+		Storage:   storage,
+	})
+	expectSuccess(t, resp, err)
+	// parse response
+	responseJWKS := &jose.JSONWebKeySet{}
+	json.Unmarshal(resp.Data["http_raw_body"].([]byte), responseJWKS)
+	if len(responseJWKS.Keys) != 1 {
+		t.Fatalf("expected 1 public key but instead got %d", len(responseJWKS.Keys))
+	}
+
+	// rotate test-key a few times, each rotate should increase the length of public keys returned
+	// by the .well-known endpoint
+	resp, err = c.identityStore.HandleRequest(ctx, &logical.Request{
+		Path:      "oidc/key/test-key/rotate",
+		Operation: logical.UpdateOperation,
+		Storage:   storage,
+	})
+	expectSuccess(t, resp, err)
+	c.identityStore.HandleRequest(ctx, &logical.Request{
+		Path:      "oidc/key/test-key/rotate",
+		Operation: logical.UpdateOperation,
+		Storage:   storage,
+	})
+
+	// .well-known/certs should contain 3 public keys
+	resp, err = c.identityStore.HandleRequest(ctx, &logical.Request{
+		Path:      "oidc/.well-known/certs",
+		Operation: logical.ReadOperation,
+		Storage:   storage,
+	})
+	expectSuccess(t, resp, err)
+	// parse response
+	json.Unmarshal(resp.Data["http_raw_body"].([]byte), responseJWKS)
+	if len(responseJWKS.Keys) != 3 {
+		t.Fatalf("expected 3 public keya but instead got %d", len(responseJWKS.Keys))
+	}
+
+	// create another named key
+	c.identityStore.HandleRequest(ctx, &logical.Request{
+		Path:      "oidc/key/test-key2",
+		Operation: logical.UpdateOperation,
+		Storage:   storage,
+	})
+
+	// delete test key
+	c.identityStore.HandleRequest(ctx, &logical.Request{
+		Path:      "oidc/key/test-key",
+		Operation: logical.DeleteOperation,
+		Storage:   storage,
+	})
+
+	// .well-known/certs should contain 1 public key, all of the public keys
+	// from named key "test-key" should have been deleted
+	resp, err = c.identityStore.HandleRequest(ctx, &logical.Request{
+		Path:      "oidc/.well-known/certs",
+		Operation: logical.ReadOperation,
+		Storage:   storage,
+	})
+	expectSuccess(t, resp, err)
+	// parse response
+	json.Unmarshal(resp.Data["http_raw_body"].([]byte), responseJWKS)
+	if len(responseJWKS.Keys) != 1 {
+		t.Fatalf("expected 1 public keya but instead got %d", len(responseJWKS.Keys))
+	}
+
 }
 
 // some helpers
