baabb31bb1ba8ed6ccb5bee207f11f7f6429f23a
Matthew Irish <matthew@hashicorp.com>
2019-06-19 16:01:55 -0500

    update kmip config model so defaultValue doesn't error

--- ui/app/models/kmip/config.js
+++ ui/app/models/kmip/config.js
@@ -3,14 +3,19 @@ import { computed } from '@ember/object';
 import { combineFieldGroups } from 'vault/utils/openapi-to-attrs';
 import fieldToAttrs, { expandAttributeMeta } from 'vault/utils/field-to-attrs';
 
+const { attr } = DS;
 export default DS.Model.extend({
   useOpenAPI: true,
   getHelpUrl(path) {
     return `/v1/${path}/config?help=1`;
   },
 
+  listenAddrs: attr({
+    editType: 'stringArray',
+    defaultValue: '127.0.0.1:5696',
+  }),
   fieldGroups: computed(function() {
-    let groups = [{ default: ['listenAddr', 'connectionTimeout'] }];
+    let groups = [{ default: ['listenAddrs', 'connectionTimeout'] }];
 
     return fieldToAttrs(this, groups);
   }),
