5fcb5bd1d04a320f16015b5eb702bf210884d3fc
Madalyn Parker <madalyn@hashicorp.com>
2019-06-20 14:30:41 -0400

    update fieldGroups for github to put things in correct order

--- ui/app/models/auth-config/github.js
+++ ui/app/models/auth-config/github.js
@@ -1,5 +1,30 @@
+import { computed } from '@ember/object';
+import DS from 'ember-data';
 import AuthConfig from '../auth-config';
+import fieldToAttrs from 'vault/utils/field-to-attrs';
+import { combineFieldGroups } from 'vault/utils/openapi-to-attrs';
+
+const { attr } = DS;
 
 export default AuthConfig.extend({
   useOpenAPI: true,
+  organization: attr('string'),
+  baseUrl: attr('string', {
+    label: 'Base URL',
+  }),
+
+  fieldGroups: computed(function() {
+    let groups = [
+      { default: ['organization'] },
+      {
+        'GitHub Options': ['baseUrl'],
+      },
+      { TTLs: ['ttl', 'maxTtl'] },
+    ];
+    if (this.newFields) {
+      groups = combineFieldGroups(groups, this.newFields, []);
+    }
+
+    return fieldToAttrs(this, groups);
+  }),
 });
