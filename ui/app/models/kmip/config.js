7d2da8821a6c15ce14f8389b83bba60b4b9c3235
Matthew Irish <matthew@hashicorp.com>
2019-06-21 15:38:36 -0500

    remove listenAddrs and set default val on ttl if a val is passed in

--- ui/app/models/kmip/config.js
+++ ui/app/models/kmip/config.js
@@ -3,17 +3,12 @@ import { computed } from '@ember/object';
 import { combineFieldGroups } from 'vault/utils/openapi-to-attrs';
 import fieldToAttrs from 'vault/utils/field-to-attrs';
 
-const { attr } = DS;
 export default DS.Model.extend({
   useOpenAPI: true,
   getHelpUrl(path) {
     return `/v1/${path}/config?help=1`;
   },
 
-  listenAddrs: attr({
-    editType: 'stringArray',
-    defaultValue: '127.0.0.1:5696',
-  }),
   fieldGroups: computed(function() {
     let groups = [{ default: ['listenAddrs', 'connectionTimeout'] }];
 
