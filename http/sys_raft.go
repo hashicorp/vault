c356df69a194ad4bfabefb6dac93e9a30edf935f
Vishal Nayak <vishalnayak@users.noreply.github.com>
2019-06-21 17:41:07 -0400

    raft join tls (#6932)

--- http/sys_raft.go
+++ http/sys_raft.go
@@ -2,6 +2,10 @@ package http
 
 import (
 	"context"
+	"crypto/tls"
+	"crypto/x509"
+	"errors"
+	"fmt"
 	"io"
 	"net/http"
 
@@ -27,7 +31,40 @@ func handleSysRaftJoinPost(core *vault.Core, w http.ResponseWriter, r *http.Requ
 		return
 	}
 
-	joined, err := core.JoinRaftCluster(context.Background(), req.LeaderAddr, nil, req.Retry)
+	var tlsConfig *tls.Config
+	switch {
+	case req.LeaderCACert != "" && req.LeaderClientCert != "" && req.LeaderClientKey != "":
+		// Create TLS Config
+		pool := x509.NewCertPool()
+		pool.AppendCertsFromPEM([]byte(req.LeaderCACert))
+
+		cert, err := tls.X509KeyPair([]byte(req.LeaderClientCert), []byte(req.LeaderClientKey))
+		if err != nil {
+			respondError(w, http.StatusBadRequest, fmt.Errorf("invalid key pair: %v", err))
+			return
+		}
+
+		tlsConfig = &tls.Config{
+			Certificates: []tls.Certificate{cert},
+			RootCAs:      pool,
+			ClientAuth:   tls.RequireAndVerifyClientCert,
+			MinVersion:   tls.VersionTLS12,
+		}
+
+		tlsConfig.BuildNameToCertificate()
+
+	case req.LeaderCACert != "":
+		respondError(w, http.StatusBadRequest, errors.New("ca_cert, client_key, client_cert must all be set; or none should be set"))
+		return
+	case req.LeaderClientCert != "":
+		respondError(w, http.StatusBadRequest, errors.New("ca_cert, client_key, client_cert must all be set; or none should be set"))
+		return
+	case req.LeaderClientKey != "":
+		respondError(w, http.StatusBadRequest, errors.New("ca_cert, client_key, client_cert must all be set; or none should be set"))
+		return
+	}
+
+	joined, err := core.JoinRaftCluster(context.Background(), req.LeaderAPIAddr, tlsConfig, req.Retry)
 	if err != nil {
 		respondError(w, http.StatusInternalServerError, err)
 		return
@@ -44,7 +81,9 @@ type JoinResponse struct {
 }
 
 type JoinRequest struct {
-	LeaderAddr string `json:"leader_api_addr"`
-	CACert     string `json:"ca_cert":`
-	Retry      bool   `json:"retry"`
+	LeaderAPIAddr    string `json:"leader_api_addr"`
+	LeaderCACert     string `json:"leader_ca_cert":`
+	LeaderClientCert string `json:"leader_client_cert"`
+	LeaderClientKey  string `json:"leader_client_key"`
+	Retry            bool   `json:"retry"`
 }
