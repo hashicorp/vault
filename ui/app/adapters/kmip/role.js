deb137ebf037cae59b6d638b0ebe55eeb7e672d8
Matthew Irish <meirish@users.noreply.github.com>
2019-06-25 15:57:50 -0500

    UI - kmip role edit form (#6973)
    * extend edit form with a custom kmip role form
    
    * adjust model fields and use new kmip role edit form
    
    * customize serialize adapter hook for kmip/role
    
    * refresh list routes in the list mixin
    
    * style up kmip role edit form
    
    * return a promise from preSave so that the queue helper waits to call save
    
    * add serialize tests for the kmip/role adapter
    
    * rename component to edit-form-kmip-role
    
    * add tests for edit-form-kmip-role
    
    * add some clarifying comments
    
    * make input more realistic in tests
    
    * remove delete toolbar

--- ui/app/adapters/kmip/role.js
+++ ui/app/adapters/kmip/role.js
@@ -11,12 +11,27 @@ export default BaseAdapter.extend({
       },
       name
     );
-    return this.ajax(url, 'POST', { data: snapshot.serialize() }).then(() => {
+    return this.ajax(url, 'POST', { data: this.serialize(snapshot) }).then(() => {
       return {
         id: name,
         name,
       };
     });
+  },
+
+  serialize(snapshot) {
+    // the endpoint here won't allow sending `operation_all` and `operation_none` at the same time or with
+    // other values, so we manually check for them and send an abbreviated object
+    let json = snapshot.serialize();
+    if (json.operation_all) {
+      return { operation_all: true };
+    }
+    if (json.operation_none) {
+      return { operation_none: true };
+    }
+    delete json.operation_none;
+    delete json.operation_all;
+    return json;
   },
 
   updateRecord() {
