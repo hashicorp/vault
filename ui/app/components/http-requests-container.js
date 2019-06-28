ad76eae45bf88148f7e6befbb06f0708cb058d48
Noelle Daley <adriannenoelle@gmail.com>
2019-06-28 15:43:08 -0700

    don't use fake data

--- ui/app/components/http-requests-container.js
+++ ui/app/components/http-requests-container.js
@@ -16,15 +16,9 @@ import addMonths from 'date-fns/add_months';
  * @param [param2=value] {String} - param2 is... //brackets mean it is optional and = sets the default value
  */
 
-const COUNTERS = [
-  { start_time: '2018-04-01T00:00:00Z', total: 5500 },
-  { start_time: '2019-05-01T00:00:00Z', total: 4500 },
-  { start_time: '2019-06-01T00:00:00Z', total: 5000 },
-];
-
 export default Component.extend({
   classNames: ['http-requests-container'],
-  counters: COUNTERS,
+  counters: null,
   timeWindow: 'All',
   filteredCounters: computed('timeWindow', function() {
     const { counters, timeWindow } = this;
