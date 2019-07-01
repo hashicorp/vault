98cf913d7275bca5d0c2e43d7fd9b9e1894475cb
Matthew Irish <matthew@hashicorp.com>
2019-06-30 23:41:57 -0500

    move flash-message to alert-banner and fix namespace reminder with a block

--- ui/lib/open-api-explorer/addon/routes/index.js
+++ ui/lib/open-api-explorer/addon/routes/index.js
@@ -1,17 +1,7 @@
 import Route from '@ember/routing/route';
-import { inject as service } from '@ember/service';
 
 export default Route.extend({
-  flashMessages: service(),
   // without an empty model hook here, ember likes to use the parent model, and then things get weird with
   // query params, so here we're no-op'ing the model hook
   model() {},
-  afterModel() {
-    this.flashMessages.warning(
-      'The "Try it out" functionality in this API explorer will communicate to this Vault server\'s endpoints with your current token. Your token will also be shown on the screen in the example curl command output.',
-      {
-        sticky: true,
-      }
-    );
-  },
 });
