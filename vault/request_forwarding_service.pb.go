cd779cd2f540c611a8efaa55b2abc719b08af921
Jeff Mitchell <jeffrey.mitchell@gmail.com>
2019-07-08 19:01:19 -0400

    Rerun proto gen as some got gen'd with old proto version

--- vault/request_forwarding_service.pb.go
+++ vault/request_forwarding_service.pb.go
@@ -9,6 +9,8 @@ import (
 	proto "github.com/golang/protobuf/proto"
 	forwarding "github.com/hashicorp/vault/helper/forwarding"
 	grpc "google.golang.org/grpc"
+	codes "google.golang.org/grpc/codes"
+	status "google.golang.org/grpc/status"
 	math "math"
 )
 
@@ -473,6 +475,20 @@ type RequestForwardingServer interface {
 	ForwardRequest(context.Context, *forwarding.Request) (*forwarding.Response, error)
 	Echo(context.Context, *EchoRequest) (*EchoReply, error)
 	PerformanceStandbyElectionRequest(*PerfStandbyElectionInput, RequestForwarding_PerformanceStandbyElectionRequestServer) error
+}
+
+// UnimplementedRequestForwardingServer can be embedded to have forward compatible implementations.
+type UnimplementedRequestForwardingServer struct {
+}
+
+func (*UnimplementedRequestForwardingServer) ForwardRequest(ctx context.Context, req *forwarding.Request) (*forwarding.Response, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method ForwardRequest not implemented")
+}
+func (*UnimplementedRequestForwardingServer) Echo(ctx context.Context, req *EchoRequest) (*EchoReply, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method Echo not implemented")
+}
+func (*UnimplementedRequestForwardingServer) PerformanceStandbyElectionRequest(req *PerfStandbyElectionInput, srv RequestForwarding_PerformanceStandbyElectionRequestServer) error {
+	return status.Errorf(codes.Unimplemented, "method PerformanceStandbyElectionRequest not implemented")
 }
 
 func RegisterRequestForwardingServer(s *grpc.Server, srv RequestForwardingServer) {
