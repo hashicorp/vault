f633b2d4c9756296cb42e58ef9e20fb33a3cd006
Calvin Leung Huang <cleung2010@gmail.com>
2019-07-17 10:59:29 -0700

    add back raft bit

--- http/logical.go
+++ http/logical.go
@@ -57,10 +57,10 @@ func buildLogicalRequest(core *vault.Core, w http.ResponseWriter, r *http.Reques
 
 		if !list {
 			data = parseQuery(queryVals)
+		}
 
-			if path == "sys/storage/raft/snapshot" {
-				responseWriter = w
-			}
+		if path == "sys/storage/raft/snapshot" {
+			responseWriter = w
 		}
 
 	case "POST", "PUT":
