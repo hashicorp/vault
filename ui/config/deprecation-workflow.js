c40ab7c21c597c559b21fc846d342dff84c2a27e
Matthew Irish <matthew@hashicorp.com>
2019-07-30 14:22:29 -0500

    update ember-cli-deprecation-workflow

--- ui/config/deprecation-workflow.js
+++ ui/config/deprecation-workflow.js
@@ -2,10 +2,10 @@
 self.deprecationWorkflow = self.deprecationWorkflow || {};
 self.deprecationWorkflow.config = {
   workflow: [
-    // ivy-codemirror and ember-radio-button still use send-action
-    { handler: 'silence', matchId: 'ember-component.send-action' },
-    { handler: 'silence', matchId: 'ember-runtime.deprecate-copy-copyable' },
-    // ember-cli-page-object uses jquery's this.$() by default - this will change when we remove jquery
-    { handler: 'silence', matchId: 'ember-test-helpers.rendering-context.jquery-element' },
+    { handler: 'throw', matchId: 'ember-component.send-action' },
+    { handler: 'throw', matchId: 'transition-state' },
+    { handler: 'throw', matchId: 'ember-polyfills.deprecate-merge' },
+    { handler: 'throw', matchId: 'events.inherited-function-listeners' },
+    { handler: 'throw', matchId: 'ember-runtime.deprecate-copy-copyable' },
   ],
 };
