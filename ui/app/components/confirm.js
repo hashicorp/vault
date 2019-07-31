b0b0de7b4bc36e7a63fd391ddd0a7360f093d50b
Noelle Daley <adriannenoelle@gmail.com>
2019-07-31 14:41:13 -0700

    use maybe-in-element instead of ember-wormhole

--- ui/app/components/confirm.js
+++ ui/app/components/confirm.js
@@ -1,6 +1,13 @@
 import Component from '@ember/component';
+import { computed } from '@ember/object';
 
 export default Component.extend({
-  tagName: '',
-  isAwaitingConfirmAction: false,
+  classNames: ['confirm-wrapper'],
+  wormholeReference: null,
+  wormholeId: computed(function() {
+    return `confirm-${this.elementId}`;
+  }),
+  didInsertElement() {
+    this.set('wormholeReference', this.element.querySelector(`#${this.wormholeId}`));
+  },
 });
