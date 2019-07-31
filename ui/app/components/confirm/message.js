972061b1f879feed6d9036e33129648c601048f6
Noelle Daley <adriannenoelle@gmail.com>
2019-07-31 15:44:35 -0700

    hide overlay initially

--- ui/app/components/confirm/message.js
+++ ui/app/components/confirm/message.js
@@ -5,6 +5,12 @@ export default Component.extend({
   tagName: '',
   renderedTrigger: null,
   id: null,
+  onCancel() {},
+  onConfirm() {},
+  title: 'Delete this?',
+  message: 'You will not be able to recover it later.',
+  confirmButtonText: 'Delete',
+  cancelButtonText: 'Cancel',
   shouldYield: computed('id', 'renderedTrigger', function() {
     return this.id === this.renderedTrigger;
   }),
