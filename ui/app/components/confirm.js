8eea597a5d89158678d968645eaf253b5198c3b3
Noelle Daley <adriannenoelle@gmail.com>
2019-07-31 16:28:28 -0700

    animate confirm overlay left and right on click

--- ui/app/components/confirm.js
+++ ui/app/components/confirm.js
@@ -3,11 +3,23 @@ import { computed } from '@ember/object';
 
 export default Component.extend({
   classNames: ['confirm-wrapper'],
+  itemId: null,
+  transitionDirection: '',
   wormholeReference: null,
   wormholeId: computed(function() {
     return `confirm-${this.elementId}`;
   }),
   didInsertElement() {
     this.set('wormholeReference', this.element.querySelector(`#${this.wormholeId}`));
+  },
+  actions: {
+    onTrigger: function(itemId) {
+      this.set('openTrigger', itemId);
+      this.set('transitionDirection', 'left');
+    },
+    onCancel: function() {
+      this.set('transitionDirection', 'right');
+      // this.set('openTrigger', '');
+    },
   },
 });
