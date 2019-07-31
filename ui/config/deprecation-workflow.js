3bf960ef6374b748dfd839700300e88736987563
Matthew Irish <matthew@hashicorp.com>
2019-07-31 09:18:49 -0500

    no more deprecations in the workflow

--- ui/config/deprecation-workflow.js
+++ ui/config/deprecation-workflow.js
@@ -1,11 +1,5 @@
 /* global self */
 self.deprecationWorkflow = self.deprecationWorkflow || {};
 self.deprecationWorkflow.config = {
-  workflow: [
-    { handler: 'throw', matchId: 'ember-component.send-action' },
-    { handler: 'throw', matchId: 'transition-state' },
-    { handler: 'throw', matchId: 'ember-polyfills.deprecate-merge' },
-    { handler: 'throw', matchId: 'events.inherited-function-listeners' },
-    { handler: 'throw', matchId: 'ember-runtime.deprecate-copy-copyable' },
-  ],
+  workflow: [],
 };
