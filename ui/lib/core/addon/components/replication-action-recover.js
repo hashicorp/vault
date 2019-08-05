4dafd599cca0d8c87d4df712a1c7b47190e89695
Matthew Irish <meirish@users.noreply.github.com>
2019-08-05 15:49:04 -0500

    move replication-actions into core addon so that secondary recovery works (#7226) (#7258)
    * move replication-actions component into core addon so that secondary recovery works
    
    * move the rest of replication action components

---
+++ ui/lib/core/addon/components/replication-action-recover.js
@@ -0,0 +1,6 @@
+import Actions from 'core/components/replication-actions-single';
+import layout from '../templates/components/replication-action-recover';
+
+export default Actions.extend({
+  layout,
+});
---
+++ ui/lib/core/addon/components/replication-action-update-primary.js
@@ -0,0 +1,6 @@
+import Actions from './replication-actions-single';
+import layout from '../templates/components/replication-action-update-primary';
+
+export default Actions.extend({
+  layout,
+});
