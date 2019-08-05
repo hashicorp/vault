98abdf18741e83ad27cdd2f44f3e2b772972de64
Jeff Mitchell <jeffrey.mitchell@gmail.com>
2019-08-05 18:03:40 -0400

    Bump sdk/api

--- vendor/github.com/hashicorp/vault/sdk/version/version_base.go
+++ vendor/github.com/hashicorp/vault/sdk/version/version_base.go
@@ -8,7 +8,7 @@ var (
 	// Whether cgo is enabled or not; set at build time
 	CgoEnabled bool
 
-	Version           = "1.2.0"
+	Version           = "1.2.1"
 	VersionPrerelease = ""
 	VersionMetadata   = ""
 )
