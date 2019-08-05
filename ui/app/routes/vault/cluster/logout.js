46c89444ee7574cd10da6166177b7673bc94f4e0
Matthew Irish <meirish@users.noreply.github.com>
2019-08-05 15:48:54 -0500

    Backport 1.2.1: ui namespace fixes (#7251)
    * ancestorKeysForKey always returns an array (#6205)
    
    * clear namespaces on logout and properly render nested namespaces in the namepace picker (#7186)
    
    * reset namespace cache when a user logs out
    
    * fix issue where if you log in to a namespace with an initial /, nested namespaces would not show up in the navigation
    
    * set an empty list of namespaces instead of ignoring error
    
    * add tests for namespace bugs
    
    * use this consistently in template

--- ui/app/routes/vault/cluster/logout.js
+++ ui/app/routes/vault/cluster/logout.js
@@ -9,19 +9,21 @@ export default Route.extend(ModelBoundaryRoute, {
   flashMessages: service(),
   console: service(),
   permissions: service(),
+  namespaceService: service('namespace'),
 
   modelTypes: computed(function() {
     return ['secret', 'secret-engine'];
   }),
 
   beforeModel() {
-    this.get('auth').deleteCurrentToken();
-    this.get('controlGroup').deleteTokens();
-    this.get('console').set('isOpen', false);
-    this.get('console').clearLog(true);
+    this.auth.deleteCurrentToken();
+    this.controlGroup.deleteTokens();
+    this.namespaceService.reset();
+    this.console.set('isOpen', false);
+    this.console.clearLog(true);
     this.clearModelCache();
     this.replaceWith('vault.cluster');
-    this.get('flashMessages').clearMessages();
-    this.get('permissions').reset();
+    this.flashMessages.clearMessages();
+    this.permissions.reset();
   },
 });
