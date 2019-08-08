e8e609a12af13c46566a98c52ce522c8e2e1c40c
Noelle Daley <adriannenoelle@gmail.com>
2019-08-08 16:06:23 -0700

    consolidate Message into Trigger to make Confirm easier to use

--- ui/lib/core/addon/components/confirm/trigger.js
+++ ui/lib/core/addon/components/confirm/trigger.js
@@ -1,7 +1,20 @@
 import Component from '@ember/component';
+import { computed } from '@ember/object';
 import layout from '../../templates/components/confirm/trigger';
 
 export default Component.extend({
   layout,
   tagName: '',
+  renderedTrigger: null,
+  id: null,
+  onCancel() {},
+  onConfirm() {},
+  title: 'Delete this?',
+  message: 'You will not be able to recover it later.',
+  triggerText: null,
+  confirmButtonText: 'Delete',
+  cancelButtonText: 'Cancel',
+  showConfirm: computed('renderedTrigger', function() {
+    return !!this.renderedTrigger;
+  }),
 });
