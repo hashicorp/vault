236f461f95eb914b97e5a2218c4561ffac56d07e
Noelle Daley <adriannenoelle@gmail.com>
2019-08-09 10:40:35 -0700

    add jsdoc comments to Trigger

--- ui/lib/core/addon/components/confirm/trigger.js
+++ ui/lib/core/addon/components/confirm/trigger.js
@@ -2,6 +2,35 @@ import Component from '@ember/component';
 import { computed } from '@ember/object';
 import layout from '../../templates/components/confirm/trigger';
 
+/**
+ * @module Trigger
+ * `Trigger` components trigger a confirmation message. They should only be used within a `Confirm` component.
+ *
+ * @example
+ * ```js
+ * <div class="box">
+ * <Confirm as |c|>
+ *   <c.Trigger
+ *     @id={{item.id}}
+ *     @onTrigger={{action c.onTrigger item.id}}
+ *     @triggerText="Delete"
+ *     @message="This will permanently delete this secret and all its vesions."
+ *     @onConfirm={{action "delete" item "secret"}}
+ *     />
+ * </Confirm>
+ * </div>
+ * ```
+ *
+ * @param id=null {ID} - A unique identifier used to bind a trigger to a confirmation message.
+ * @param onTrigger {Func} - A function that displays the confirmation message. This must receive the `id` listed above.
+ * @param onConfirm=null {Func} - The action to take when the user clicks the confirm button.
+ * @param [title='Delete this?'] {String} - The header text to display in the confirmation message.
+ * @param [triggerText='Delete'] {String} - The text on the trigger button.
+ * @param [message='You will not be able to recover it later.'] {String} -
+ * @param [confirmButtonText='Delete'] {String} - The text to display on the confirm button.
+ * @param [cancelButtonText='Cancel'] {String} - The text to display on the cancel button.
+ */
+
 export default Component.extend({
   layout,
   tagName: '',
@@ -11,7 +40,7 @@ export default Component.extend({
   onConfirm() {},
   title: 'Delete this?',
   message: 'You will not be able to recover it later.',
-  triggerText: null,
+  triggerText: 'Delete',
   confirmButtonText: 'Delete',
   cancelButtonText: 'Cancel',
   showConfirm: computed('renderedTrigger', function() {
