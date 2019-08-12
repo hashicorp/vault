b819544aff88f672312afd2fb58f578615102094
Joel Thompson <joel@jthompson.io>
2019-08-12 15:17:52 -0400

    Run makefmt

--- builtin/logical/aws/path_roles.go
+++ builtin/logical/aws/path_roles.go
@@ -269,7 +269,6 @@ func (b *backend) pathRolesWrite(ctx context.Context, req *logical.Request, d *f
 		roleEntry.UserPath = userPathRaw.(string)
 	}
 
-
 	if legacyRole != "" {
 		roleEntry = upgradeLegacyPolicyEntry(legacyRole)
 		if roleEntry.InvalidData != "" {
@@ -488,8 +487,8 @@ func (r *awsRoleEntry) validate() error {
 	}
 
 	if r.MaxSTSTTL > 0 &&
-	r.DefaultSTSTTL > 0 &&
-	r.DefaultSTSTTL > r.MaxSTSTTL {
+		r.DefaultSTSTTL > 0 &&
+		r.DefaultSTSTTL > r.MaxSTSTTL {
 		errors = multierror.Append(errors, fmt.Errorf(`"default_sts_ttl" value must be less than or equal to "max_sts_ttl" value`))
 	}
 
