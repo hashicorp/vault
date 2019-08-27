e31b878bea6ccfff657c1a20d4b054e7aee9123b
Noelle Daley <adriannenoelle@gmail.com>
2019-08-27 15:39:41 -0700

    remove ember-cli-valid-component-name since we no longer need it

--- ui/blueprints/component/index.js
+++ ui/blueprints/component/index.js
@@ -3,7 +3,6 @@
 const path = require('path');
 const stringUtil = require('ember-cli-string-utils');
 const pathUtil = require('ember-cli-path-utils');
-const validComponentName = require('ember-cli-valid-component-name');
 const getPathOption = require('ember-cli-get-component-path-option');
 const normalizeEntityName = require('ember-cli-normalize-entity-name');
 
@@ -50,9 +49,7 @@ module.exports = {
   },
 
   normalizeEntityName: function(entityName) {
-    entityName = normalizeEntityName(entityName);
-
-    return validComponentName(entityName);
+    return normalizeEntityName(entityName);
   },
 
   locals: function(options) {
