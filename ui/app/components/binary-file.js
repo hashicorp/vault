50deac2637aa2139f1c5eb70b73c978747102f55
Matthew Irish <matthew@hashicorp.com>
2019-08-27 17:26:45 -0500

    add view components and binary-file component

---
+++ ui/app/components/binary-file.js
@@ -0,0 +1,50 @@
+import Component from '@ember/component';
+
+export default Component.extend({
+  classNames: ['box', 'is-fullwidth', 'is-marginless', 'is-shadowless'],
+  onChange: () => {},
+  file: null,
+  fileName: null,
+
+  /*
+   * @public
+   * @param String
+   * Text to use as the label for the file input
+   * If null, a default will be rendered
+   */
+  label: null,
+
+  /*
+   * @public
+   * @param String
+   * Text to use as help under the file input
+   * If null, a default will be rendered
+   */
+  fileHelpText: null,
+
+  readFile(file) {
+    const reader = new FileReader();
+    reader.onload = () => this.send('onChange', reader.result, file.name);
+    reader.readAsBinaryString(file);
+  },
+
+  actions: {
+    pickedFile(e) {
+      let { files } = e.target;
+      if (!files.length) {
+        return;
+      }
+      for (let i = 0, len = files.length; i < len; i++) {
+        this.readFile(files[i]);
+      }
+    },
+    clearFile() {
+      this.send('onChange');
+    },
+    onChange(file, filename) {
+      this.set('file', file);
+      this.set('fileName', filename);
+      this.onChange(file, filename);
+    },
+  },
+});
---
+++ ui/app/components/raft-storage-overview.js
@@ -0,0 +1,22 @@
+import Component from '@ember/component';
+import { getOwner } from '@ember/application';
+
+export default Component.extend({
+  actions: {
+    async downloadSnapshot() {
+      let adapter = getOwner(this).lookup('adapter:application');
+      let resp = await adapter.rawRequest('/v1/sys/storage/raft/snapshot', 'GET');
+      let blob = await resp.blob();
+
+      let filename = 'raft.snap';
+      let file = new Blob([blob], { type: 'application/x-gzip' });
+      file.name = filename;
+      let a = document.createElement('a');
+      a.href = window.URL.createObjectURL(file);
+      a.download = filename;
+      document.body.appendChild(a);
+      a.click();
+      a.remove();
+    },
+  },
+});
