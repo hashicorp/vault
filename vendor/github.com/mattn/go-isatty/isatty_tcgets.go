50ad2d726901db619f6081f13c7dfe085e1ebb8a
Aaron Bennett <10927621+rackonnoiter@users.noreply.github.com>
2019-08-29 12:42:30 -0500

    update dependencies

--- vendor/github.com/mattn/go-isatty/isatty_tcgets.go
+++ vendor/github.com/mattn/go-isatty/isatty_tcgets.go
@@ -1,20 +1,15 @@
-// +build darwin freebsd openbsd netbsd dragonfly
+// +build linux aix
 // +build !appengine
+// +build !android
 
 package isatty
 
-import (
-	"syscall"
-	"unsafe"
-)
-
-const ioctlReadTermios = syscall.TIOCGETA
+import "golang.org/x/sys/unix"
 
 // IsTerminal return true if the file descriptor is terminal.
 func IsTerminal(fd uintptr) bool {
-	var termios syscall.Termios
-	_, _, err := syscall.Syscall6(syscall.SYS_IOCTL, fd, ioctlReadTermios, uintptr(unsafe.Pointer(&termios)), 0, 0, 0)
-	return err == 0
+	_, err := unix.IoctlGetTermios(int(fd), unix.TCGETS)
+	return err == nil
 }
 
 // IsCygwinTerminal return true if the file descriptor is a cygwin or msys2
