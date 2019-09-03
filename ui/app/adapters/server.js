0eaad678a86a116616501d31cb5fa42c75762abd
Matthew Irish <matthew@hashicorp.com>
2019-09-03 10:48:29 -0500

    add remove peer functionality

--- ui/app/adapters/server.js
+++ ui/app/adapters/server.js
@@ -5,4 +5,12 @@ export default ApplicationAdapter.extend({
   urlForFindAll() {
     return '/v1/sys/storage/raft/configuration';
   },
+  urlForDeleteRecord() {
+    return '/v1/sys/storage/raft/remove-peer';
+  },
+  deleteRecord(store, type, snapshot) {
+    let server_id = snapshot.attr('nodeId');
+    let url = '/v1/sys/storage/raft/remove-peer';
+    return this.ajax(url, 'POST', { data: { server_id } });
+  },
 });
