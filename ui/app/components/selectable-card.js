c5d1a28a3053e89cf3393e68fa08fc05be92f732
Noelle Daley <adriannenoelle@gmail.com>
2019-09-04 10:09:13 -0700

    add SelectableCard

---
+++ ui/app/components/selectable-card.js
@@ -0,0 +1,16 @@
+/**
+ * @module SelectableCard
+ * SelectableCard components are used to...
+ *
+ * @example
+ * ```js
+ * <SelectableCard @requiredParam={requiredParam} @optionalParam={optionalParam} @param1={{param1}}/>
+ * ```
+ * @param {object} requiredParam - requiredParam is...
+ * @param {string} [optionalParam] - optionalParam is...
+ * @param {string} [param1=defaultValue] - param1 is...
+ */
+
+import Component from '@ember/component';
+
+export default Component.extend({});
