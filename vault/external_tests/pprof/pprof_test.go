1d40cb01e37cdf0b42567c908910c4f08bad0476
Calvin Leung Huang <cleung2010@gmail.com>
2019-09-19 12:59:47 -0700

    add max request duration test

--- vault/external_tests/pprof/pprof_test.go
+++ vault/external_tests/pprof/pprof_test.go
@@ -4,6 +4,8 @@ import (
 	"encoding/json"
 	"io/ioutil"
 	"net/http"
+	"strconv"
+	"strings"
 	"testing"
 
 	"github.com/hashicorp/go-cleanhttp"
@@ -93,13 +95,14 @@ func TestSysPprof(t *testing.T) {
 		if err != nil {
 			t.Fatal(err)
 		}
-		// Make sure that we don't get a error response
+
 		httpResp := make(map[string]interface{})
 
 		// Skip this error check since some endpoints return binary blobs, we
 		// only care about the ok check right after as an existence check.
 		_ = json.Unmarshal(httpRespBody, &httpResp)
 
+		// Make sure that we don't get a error response
 		if _, ok := httpResp["errors"]; ok {
 			t.Fatalf("unexpected error response: %v", httpResp["errors"])
 		}
@@ -107,12 +110,64 @@ func TestSysPprof(t *testing.T) {
 		if len(httpRespBody) == 0 {
 			t.Fatal("no pprof index returned")
 		}
-		t.Log(string(httpRespBody))
 	}
 
 	for _, tc := range cases {
 		t.Run(tc.name, func(t *testing.T) {
 			pprofRequest(tc.path, tc.seconds)
 		})
+	}
+}
+
+func TestSysPprof_MaxRequestDuration(t *testing.T) {
+	cluster := vault.NewTestCluster(t, nil, &vault.TestClusterOptions{
+		HandlerFunc: vaulthttp.Handler,
+	})
+	cluster.Start()
+	defer cluster.Cleanup()
+	client := cluster.Cores[0].Client
+
+	transport := cleanhttp.DefaultPooledTransport()
+	transport.TLSClientConfig = cluster.Cores[0].TLSConfig.Clone()
+	if err := http2.ConfigureTransport(transport); err != nil {
+		t.Fatal(err)
+	}
+	httpClient := &http.Client{
+		Transport: transport,
+	}
+
+	sec := strconv.Itoa(int(vault.DefaultMaxRequestDuration.Seconds()) + 1)
+
+	req := client.NewRequest("GET", "/v1/sys/pprof/profile")
+	req.Params.Set("seconds", sec)
+	httpReq, err := req.ToHTTP()
+	if err != nil {
+		t.Fatal(err)
+	}
+	resp, err := httpClient.Do(httpReq)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer resp.Body.Close()
+
+	httpRespBody, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	httpResp := make(map[string]interface{})
+
+	// If we error here, it means that profiling likely happened, which is not
+	// what we're checking for in this case.
+	if err := json.Unmarshal(httpRespBody, &httpResp); err != nil {
+		t.Fatalf("expected valid error response, got: %v", err)
+	}
+
+	errs, ok := httpResp["errors"].([]interface{})
+	if !ok {
+		t.Fatalf("expected error response, got: %v", httpResp)
+	}
+	if len(errs) == 0 || !strings.Contains(errs[0].(string), "exceeds max request duration") {
+		t.Fatalf("unexptected error returned: %v", errs)
 	}
 }
