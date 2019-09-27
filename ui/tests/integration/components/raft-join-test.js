b2937f5fdc252cdaf57461d9ce4367652d5c5921
Matthew Irish <matthew@hashicorp.com>
2019-09-27 11:04:39 -0500

    add component tests

--- ui/tests/integration/components/raft-join-test.js
+++ ui/tests/integration/components/raft-join-test.js
@@ -1,26 +1,37 @@
 import { module, test } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
-import { render } from '@ember/test-helpers';
+import { click, render } from '@ember/test-helpers';
 import hbs from 'htmlbars-inline-precompile';
+import sinon from 'sinon';
 
 module('Integration | Component | raft-join', function(hooks) {
   setupRenderingTest(hooks);
 
   test('it renders', async function(assert) {
-    // Set any properties with this.set('myProperty', 'value');
-    // Handle any actions with this.set('myAction', function(val) { ... });
+    await render(hbs`<RaftJoin />`);
+    assert.dom('[data-test-join-choice]').exists();
+  });
 
-    await render(hbs`{{raft-join}}`);
+  test('it shows the join form when clicking next', async function(assert) {
+    await render(hbs`<RaftJoin />`);
+    await click('[data-test-next]');
+    assert.dom('[data-test-join-header]').exists();
+  });
+  test('it returns to the first screen when clicking back', async function(assert) {
+    await render(hbs`<RaftJoin />`);
+    await click('[data-test-next]');
+    assert.dom('[data-test-join-header]').exists();
+    await click('[data-test-cancel-button]');
+    assert.dom('[data-test-join-choice]').exists();
+  });
 
-    assert.equal(this.element.textContent.trim(), '');
+  test('it calls onDismiss when a user chooses to init', async function(assert) {
+    let spy = sinon.spy();
+    this.set('onDismiss', spy);
+    await render(hbs`<RaftJoin @onDismiss={{onDismiss}} />`);
 
-    // Template block usage:
-    await render(hbs`
-      {{#raft-join}}
-        template block text
-      {{/raft-join}}
-    `);
-
-    assert.equal(this.element.textContent.trim(), 'template block text');
+    await click('[data-test-join-init]');
+    await click('[data-test-next]');
+    assert.ok(spy.calledOnce, 'it calls the passed onDismiss');
   });
 });
