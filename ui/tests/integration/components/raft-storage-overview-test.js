dd190a9c72e7422b4a5d030f99ca8bc2b076faab
Matthew Irish <matthew@hashicorp.com>
2019-09-30 14:18:01 -0500

    fix linting

--- ui/tests/integration/components/raft-storage-overview-test.js
+++ ui/tests/integration/components/raft-storage-overview-test.js
@@ -1,8 +1,7 @@
 import { module, test } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
-import { click, render } from '@ember/test-helpers';
+import { render } from '@ember/test-helpers';
 import hbs from 'htmlbars-inline-precompile';
-import sinon from 'sinon';
 
 module('Integration | Component | raft-storage-overview', function(hooks) {
   setupRenderingTest(hooks);
