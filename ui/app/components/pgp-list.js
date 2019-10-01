e53f62a3b5f915a4c4fb1440b4cda305dd478378
Matthew Irish <matthew@hashicorp.com>
2019-10-01 14:47:57 -0500

    add tests for pgp-list component and refactor so initialization works

--- ui/app/components/pgp-list.js
+++ ui/app/components/pgp-list.js
@@ -3,6 +3,51 @@ import Component from '@ember/component';
 
 export default Component.extend({
   onDataUpdate: () => {},
+  listLength: 0,
+  listData: null,
+
+  init() {
+    this._super(...arguments);
+    let num = this.listLength;
+    if (num) {
+      num = parseInt(num, 10);
+    }
+    let list = this.newList(num);
+    this.set('listData', list);
+  },
+
+  didReceiveAttrs() {
+    this._super(...arguments);
+    let list;
+    if (!this.listLength) {
+      this.set('listData', []);
+      return;
+    }
+    // no update needed
+    if (this.listData.length === this.listLength) {
+      return;
+    }
+    // shorten the current list
+    if (this.listLength < this.listData.length) {
+      list = this.listData.slice(0, this.listLength);
+    }
+    // add to the current list by creating a new list and copying over existing list
+    if (this.listLength > this.listData.length) {
+      list = this.newList(this.listLength);
+      if (this.listData.length) {
+        list.splice(0, this.listData.length, ...this.listData);
+      }
+    }
+    this.set('listData', list || this.listData);
+    this.onDataUpdate((list || this.listData).compact().map(k => k.value));
+  },
+
+  newList(length) {
+    return Array(length || 0)
+      .fill(null)
+      .map(() => ({ value: '' }));
+  },
+
   listData: computed('listLength', function() {
     let num = this.get('listLength');
     if (num) {
@@ -12,12 +57,12 @@ export default Component.extend({
       .fill(null)
       .map(() => ({ value: '' }));
   }),
-  listLength: 0,
+
   actions: {
     setKey(index, key) {
-      let listData = this.get('listData');
-      listData.replace(index, 1, key);
-      this.get('onDataUpdate')(listData.compact().map(k => k.value));
+      let { listData } = this;
+      listData.splice(index, 1, key);
+      this.onDataUpdate(listData.compact().map(k => k.value));
     },
   },
 });
