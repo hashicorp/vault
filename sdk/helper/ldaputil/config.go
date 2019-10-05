05dca0695ebb773fb884c15481d992aba9716c6b
Aaron France <aaron.l.france@gmail.com>
2019-10-05 13:15:20 +0200

    feat: provide a way to specify client certificates
    * GSuite LDAP requires that clients use client TLS certificates/keys
    in order to authenticate against their LDAP.

--- sdk/helper/ldaputil/config.go
+++ sdk/helper/ldaputil/config.go
@@ -236,18 +236,31 @@ func NewConfigEntry(existing *ConfigEntry, d *framework.FieldData) (*ConfigEntry
 	if _, ok := d.Raw["certificate"]; ok || !hadExisting {
 		certificate := d.Get("certificate").(string)
 		if certificate != "" {
-			block, _ := pem.Decode([]byte(certificate))
-
-			if block == nil || block.Type != "CERTIFICATE" {
-				return nil, errors.New("failed to decode PEM block in the certificate")
-			}
-			_, err := x509.ParseCertificate(block.Bytes)
-			if err != nil {
-				return nil, errwrap.Wrapf("failed to parse certificate: {{err}}", err)
+			if err := validateCertificate([]byte(certificate)); err != nil {
+				return nil, errwrap.Wrapf("failed to parse server tls cert: {{err}}", err)
 			}
 		}
-
 		cfg.Certificate = certificate
+	}
+
+	if _, ok := d.Raw["client_tls_cert"]; ok || !hadExisting {
+		clientTLSCert := d.Get("client_tls_cert").(string)
+		if clientTLSCert != "" {
+			if err := validateCertificate([]byte(clientTLSCert)); err != nil {
+				return nil, errwrap.Wrapf("failed to parse client tls cert: {{err}}", err)
+			}
+		}
+		cfg.ClientTLSCert = clientTLSCert
+	}
+
+	if _, ok := d.Raw["client_tls_key"]; ok || !hadExisting {
+		clientTLSKey := d.Get("client_tls_key").(string)
+		if clientTLSKey != "" {
+			if err := validateCertificate([]byte(clientTLSKey)); err != nil {
+				return nil, errwrap.Wrapf("failed to parse client tls key: {{err}}", err)
+			}
+		}
+		cfg.ClientTLSKey = clientTLSKey
 	}
 
 	if _, ok := d.Raw["insecure_tls"]; ok || !hadExisting {
@@ -324,6 +337,8 @@ type ConfigEntry struct {
 	UPNDomain                string `json:"upndomain"`
 	UserAttr                 string `json:"userattr"`
 	Certificate              string `json:"certificate"`
+	ClientTLSCert            string `json:"client_tls_cert`
+	ClientTLSKey             string `json:"client_tls_key`
 	InsecureTLS              bool   `json:"insecure_tls"`
 	StartTLS                 bool   `json:"starttls"`
 	BindDN                   string `json:"binddn"`
@@ -377,6 +392,18 @@ func (c *ConfigEntry) PasswordlessMap() map[string]interface{} {
 	return m
 }
 
+func validateCertificate(pemBlock []byte) error {
+	block, _ := pem.Decode([]byte(pemBlock))
+	if block == nil || block.Type != "CERTIFICATE" {
+		return errors.New("failed to decode PEM block in the certificate")
+	}
+	_, err := x509.ParseCertificate(block.Bytes)
+	if err != nil {
+		return fmt.Errorf("failed to parse certificate %s", err.Error())
+	}
+	return nil
+}
+
 func (c *ConfigEntry) Validate() error {
 	if len(c.Url) == 0 {
 		return errors.New("at least one url must be provided")
@@ -398,13 +425,16 @@ func (c *ConfigEntry) Validate() error {
 		return errors.New("'tls_max_version' must be greater than or equal to 'tls_min_version'")
 	}
 	if c.Certificate != "" {
-		block, _ := pem.Decode([]byte(c.Certificate))
-		if block == nil || block.Type != "CERTIFICATE" {
-			return errors.New("failed to decode PEM block in the certificate")
+		if err := validateCertificate([]byte(c.Certificate)); err != nil {
+			return errwrap.Wrapf("failed to parse server tls cert: {{err}}", err)
 		}
-		_, err := x509.ParseCertificate(block.Bytes)
-		if err != nil {
-			return fmt.Errorf("failed to parse certificate %s", err.Error())
+	}
+	if c.ClientTLSCert != "" && c.ClientTLSKey != "" {
+		if err := validateCertificate([]byte(c.ClientTLSCert)); err != nil {
+			return errwrap.Wrapf("failed to parse client tls cert: {{err}}", err)
+		}
+		if err := validateCertificate([]byte(c.ClientTLSKey)); err != nil {
+			return errwrap.Wrapf("failed to parse client tls key: {{err}}", err)
 		}
 	}
 	return nil
