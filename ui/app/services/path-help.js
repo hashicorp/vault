c8dbec2e593f12bd898f62353b4441d69ca49037
Noelle Daley <adriannenoelle@gmail.com>
2019-10-08 08:46:00 -0700

    properly log errors to the console

--- ui/app/services/path-help.js
+++ ui/app/services/path-help.js
@@ -85,7 +85,7 @@ export default Service.extend({
         return this.registerNewModelWithProps(helpUrl, backend, newModel, modelName);
       })
       .catch(err => {
-        console.err(err);
+        console.error(err);
       });
   },
 
