03c1c5a82faad0b88727c32b17d26773dfaff3a8
Nick Cabatoff <ncabatoff@hashicorp.com>
2019-10-10 16:21:37 -0400

    Check barrier seal instead of Core.sealed.

--- vault/generate_root.go
+++ vault/generate_root.go
@@ -146,8 +146,12 @@ func (c *Core) GenerateRootInit(otp, pgpKey string, strategy GenerateRootStrateg
 	if c.Sealed() && !c.recoveryMode {
 		return consts.ErrSealed
 	}
-	if !c.Sealed() && c.recoveryMode {
-		return errors.New("attempt to generate recovery token when already unsealed")
+	barrierSealed, err := c.barrier.Sealed()
+	if err != nil {
+		return errors.New("unable to check barrier seal status")
+	}
+	if !barrierSealed && c.recoveryMode {
+		return errors.New("attempt to generate recovery operation token when already unsealed")
 	}
 	if c.standby && !c.recoveryMode {
 		return consts.ErrStandby
@@ -227,9 +231,15 @@ func (c *Core) GenerateRootUpdate(ctx context.Context, key []byte, nonce string,
 	if c.Sealed() && !c.recoveryMode {
 		return nil, consts.ErrSealed
 	}
-	if !c.Sealed() && c.recoveryMode {
-		return nil, errors.New("attempt to generate recovery token when already unsealed")
+
+	barrierSealed, err := c.barrier.Sealed()
+	if err != nil {
+		return nil, errors.New("unable to check barrier seal status")
 	}
+	if !barrierSealed && c.recoveryMode {
+		return nil, errors.New("attempt to generate recovery operation token when already unsealed")
+	}
+
 	if c.standby && !c.recoveryMode {
 		return nil, consts.ErrStandby
 	}
@@ -329,7 +339,7 @@ func (c *Core) GenerateRootUpdate(ctx context.Context, key []byte, nonce string,
 			// If we are in recovery mode, being able to unseal
 			// the barrier is how we establish authentication
 			if err := c.barrier.Unseal(ctx, combinedKey); err != nil {
-				c.logger.Error("root generation aborted, recovery token verification failed", "error", err)
+				c.logger.Error("root generation aborted, recovery operation token verification failed", "error", err)
 				return nil, err
 			}
 		default:
