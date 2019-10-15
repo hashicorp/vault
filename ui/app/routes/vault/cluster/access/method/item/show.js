0de5c4dfa0292287c27e9bfbc4882b7b5c3f09de
Noelle Daley <adriannenoelle@gmail.com>
2019-10-14 17:16:08 -0700

    add TODO for finding a better way to store ids

--- ui/app/routes/vault/cluster/access/method/item/show.js
+++ ui/app/routes/vault/cluster/access/method/item/show.js
@@ -13,6 +13,7 @@ export default Route.extend({
     const modelType = `generated-${singularize(itemType)}-${type}`;
     // this is necessary to prevent name clashing in the case where there are 2 generated items
     // with the same name under different auth backends. for example, userpass/bob and radius/bob
+    // TODO: this could cause problems if users named their user or group mygroup/foo/bar
     const id = `${method}/${itemName}`;
     return this.store.findRecord(modelType, id);
   },
