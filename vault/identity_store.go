c27a1c1840a4f73674c5a511b7a2ed5a44cc73bb
Vishal Nayak <vishalnayak@users.noreply.github.com>
2019-11-08 12:13:53 -0500

    Ensure only primary stores the case sensitivity state (#7820)
    * Ensure only primary stores the case sensitivity state
    
    * Update the check

--- vault/identity_store.go
+++ vault/identity_store.go
@@ -112,6 +112,11 @@ func (i *IdentityStore) paths() []*framework.Path {
 }
 
 func (i *IdentityStore) initialize(ctx context.Context, req *logical.InitializationRequest) error {
+	// Only primary should write the status
+	if i.System().ReplicationState().HasState(consts.ReplicationPerformanceSecondary | consts.ReplicationPerformanceStandby | consts.ReplicationDRSecondary) {
+		return nil
+	}
+
 	entry, err := logical.StorageEntryJSON(caseSensitivityKey, &casesensitivity{
 		DisableLowerCasedNames: i.disableLowerCasedNames,
 	})
