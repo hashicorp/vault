870e0f8ca3a460baf3c07b03f1d128d2c2069367
Michel Vocks <michelvocks@gmail.com>
2019-12-19 16:41:20 +0100

    Add debug line

--- plugins/database/mysql/mysql_test.go
+++ plugins/database/mysql/mysql_test.go
@@ -384,7 +384,7 @@ func TestMySQL_Initialize_ReservedChars(t *testing.T) {
 	db := new(MetadataLen, MetadataLen, UsernameLen)
 	_, err := db.Init(context.Background(), connectionDetails, true)
 	if err != nil {
-		t.Fatalf("connURL: %s; err: %s", connURL, err)
+		t.Fatalf("connURL: %s; err: %s", db.ConnectionURL, err)
 	}
 
 	if !db.Initialized {
