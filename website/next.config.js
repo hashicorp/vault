68474019c3b4fdbc3aab200db1a8666b8aecc39b
Jeff Escalante <jescalan@users.noreply.github.com>
2020-01-17 19:18:42 -0500

    [stable-website] new documentation website! (#8181)
    * new website
    
    * fix render issue
    
    * a few more fixes and updates
    
    * markdown fixes

---
+++ website/next.config.js
@@ -0,0 +1,47 @@
+const withHashicorp = require('@hashicorp/nextjs-scripts')
+const path = require('path')
+
+module.exports = withHashicorp({
+  defaultLayout: true,
+  transpileModules: ['is-absolute-url', '@hashicorp/react-mega-nav'],
+  mdx: { resolveIncludes: path.join(__dirname, 'pages') }
+})({
+  experimental: {
+    css: true,
+    granularChunks: true,
+    rewrites: () => [
+      {
+        source: '/api/:path*',
+        destination: '/api-docs/:path*'
+      }
+    ],
+    redirects: () => [
+      {
+        source: '/intro',
+        destination: '/intro/getting-started',
+        permanent: true
+      }
+    ]
+  },
+  exportTrailingSlash: true,
+  webpack(config) {
+    // Add polyfills
+    const originalEntry = config.entry
+    config.entry = async () => {
+      const entries = await originalEntry()
+      let polyEntry = entries['static/runtime/polyfills.js']
+      if (polyEntry && !polyEntry.includes('./lib/polyfills.js')) {
+        if (!Array.isArray(polyEntry)) {
+          entries['static/runtime/polyfills.js'] = [polyEntry]
+        }
+        entries['static/runtime/polyfills.js'].unshift('./lib/polyfills.js')
+      }
+      return entries
+    }
+
+    return config
+  },
+  env: {
+    HASHI_ENV: process.env.HASHI_ENV
+  }
+})
