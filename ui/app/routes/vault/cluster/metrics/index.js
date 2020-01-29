a6a10963c5e2b7430e0d28b04c826297dcdff442
Angel Garbarino <argarbarino@gmail.com>
2020-01-29 11:49:26 -0700

    clean up

--- ui/app/routes/vault/cluster/metrics/index.js
+++ ui/app/routes/vault/cluster/metrics/index.js
@@ -5,18 +5,17 @@ import { hash } from 'rsvp';
 export default Route.extend(ClusterRoute, {
   model() {
     let entitiesModel = this.store.queryRecord('entities', {}).then(response => {
-      return response.counters.entities.total || 0;
+      return response.counters.entities.total;
     });
 
     let httpsModel = this.store.queryRecord('http-requests', {}).then(response => {
       // reverse array so that most recent month shows
-      // TODO: what if this month didn't have any data?
       let reverseArray = response.counters.reverse();
       return reverseArray[0].total;
     });
 
     let tokenModel = this.store.queryRecord('tokens', {}).then(response => {
-      return response.counters.service_tokens.total || 0;
+      return response.counters.service_tokens.total;
     });
 
     return hash({
