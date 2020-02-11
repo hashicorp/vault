70bc1edb31fa22f151b574a7db0d7e5eeea73085
Angel Garbarino <argarbarino@gmail.com>
2020-02-11 09:40:24 -0700

    move into one component

--- ui/app/components/selectable-card-container.js
+++ ui/app/components/selectable-card-container.js
@@ -15,9 +15,33 @@ import { computed } from '@ember/object';
  */
 
 export default Component.extend({
-  classNames: ['selectable-card-container'],
+  classNameBindings: ['isGraphContainer'],
+  isGraphContainer: computed('counters', function() {
+    return this.counters.httpsRequests.length > 1
+      ? 'selectable-card-container-graph'
+      : 'selectable-card-container';
+  }),
   totalHttpRequests: computed('counters', function() {
     let httpsRequestsArray = this.counters.httpsRequests || [];
     return httpsRequestsArray.firstObject.total;
+  }),
+  // Limit number of months returned to the most recent 12
+  filteredHttpsRequests: computed('counters', function() {
+    let httpsRequestsArray = this.counters.httpsRequests || [];
+    if (httpsRequestsArray.length > 12) {
+      httpsRequestsArray = httpsRequestsArray.slice(0, 12);
+    }
+    return httpsRequestsArray;
+  }),
+  percentChange: computed('counters', function() {
+    let httpsRequestsArray = this.counters.httpsRequests || [];
+    let lastTwoMonthsArray = httpsRequestsArray.slice(0, 2);
+    let previousMonthVal = lastTwoMonthsArray.lastObject.total;
+    let thisMonthVal = lastTwoMonthsArray.firstObject.total;
+
+    let percentChange = (((previousMonthVal - thisMonthVal) / previousMonthVal) * 100).toFixed(1);
+    // a negative value indicates a percentage increase, so we swap the value
+    percentChange = -percentChange;
+    return percentChange;
   }),
 });
--- ui/tests/integration/components/selectable-card-container-test.js
+++ ui/tests/integration/components/selectable-card-container-test.js
@@ -1,6 +1,6 @@
 import { module, test } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
-import { render, pauseTest } from '@ember/test-helpers';
+import { render } from '@ember/test-helpers';
 import hbs from 'htmlbars-inline-precompile';
 
 const MODEL = {
@@ -9,11 +9,35 @@ const MODEL = {
   totalTokens: 1,
 };
 
+const MODEL_GRAPH = {
+  httpsRequests: [
+    { start_time: '2018-11-01T00:00:00Z', total: 5500 },
+    { start_time: '2018-12-01T00:00:00Z', total: 4500 },
+    { start_time: '2019-01-01T00:00:00Z', total: 5000 },
+    { start_time: '2019-02-01T00:00:00Z', total: 5000 },
+    { start_time: '2019-03-01T00:00:00Z', total: 5000 },
+    { start_time: '2019-04-01T00:00:00Z', total: 5500 },
+    { start_time: '2019-05-01T00:00:00Z', total: 4500 },
+    { start_time: '2019-06-01T00:00:00Z', total: 5000 },
+    { start_time: '2019-07-01T00:00:00Z', total: 5000 },
+    { start_time: '2019-08-01T00:00:00Z', total: 5000 },
+    { start_time: '2019-09-01T00:00:00Z', total: 5000 },
+    { start_time: '2019-10-01T00:00:00Z', total: 5000 },
+    { start_time: '2019-11-01T00:00:00Z', total: 5000 },
+    { start_time: '2019-12-01T00:00:00Z', total: 5000 },
+    { start_time: '2020-01-01T00:00:00Z', total: 5000 },
+    { start_time: '2020-02-01T00:00:00Z', total: 5000 },
+  ],
+  totalEntities: 0,
+  totalTokens: 1,
+};
+
 module('Integration | Component | selectable-card-container', function(hooks) {
   setupRenderingTest(hooks);
 
   hooks.beforeEach(function() {
     this.set('model', MODEL);
+    this.set('modelGraph', MODEL_GRAPH);
   });
 
   test('it renders', async function(assert) {
@@ -28,5 +52,20 @@ module('Integration | Component | selectable-card-container', function(hooks) {
     assert.dom(`[data-test-selectable-card-title=Requests]`).exists();
     assert.dom(`[data-test-selectable-card-title=Entities]`).exists();
     assert.dom(`[data-test-selectable-card-title=Tokens]`).exists();
+  });
+
+  test('it renders with more than one month of data', async function(assert) {
+    await render(hbs`<SelectableCardContainer @counters={{modelGraph}}/>`);
+    assert.dom('.selectable-card-container-graph').exists();
+  });
+
+  test('it renders 3 selectable cards when there is more than one month of data', async function(assert) {
+    await render(hbs`<SelectableCardContainer @counters={{modelGraph}}/>`);
+    assert.dom('.selectable-card').exists({ count: 3 });
+  });
+
+  test('it only renders a bar chart with the last 12 months of data', async function(assert) {
+    await render(hbs`<SelectableCardContainer @counters={{modelGraph}}/>`);
+    assert.dom('rect').exists({ count: 12 });
   });
 });
