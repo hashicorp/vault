b97963fe639f1945c83c5ce6beb6756f640860a7
Jorge Heleno <jorge.c.heleno@gmail.com>
2020-02-19 12:20:13 +0000

    fix: change anonymous_group_search to camel case

--- ui/app/models/auth-config/ldap.js
+++ ui/app/models/auth-config/ldap.js
@@ -24,7 +24,7 @@ export default AuthConfig.extend({
           'clientTLSKey',
           'userattr',
           'upndomain',
-          'anonymous_group_search',
+          'anonymousGroupSearch',
         ],
       },
       {
