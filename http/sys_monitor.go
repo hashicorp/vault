6d69c249835f90df59cb7af65596fc4256f25157
Josh Black <raskchanky@gmail.com>
2020-03-06 14:43:17 -0800

    Rename New and new to NewMonitor and newMonitor

--- http/sys_monitor.go
+++ http/sys_monitor.go
@@ -29,7 +29,7 @@ func handleSysMonitor(core *vault.Core) http.Handler {
 		}
 
 		isJson := core.LogFormat() == "json"
-		monitor := monitor.New(512, core.Logger(), &log.LoggerOptions{
+		monitor := monitor.NewMonitor(512, core.Logger(), &log.LoggerOptions{
 			Level:      logLevel,
 			JSONFormat: isJson,
 		})
