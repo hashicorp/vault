7cefc0bcba11b9586fc4139f2e9244528f1c0e10
Balkrishna Pandey <sachit.nep@gmail.com>
2020-03-06 10:32:47 -0700

    Unique lock key based on database and table name
    Signed-off-by: Balkrishna Pandey <sachit.nep@gmail.com>

--- physical/mysql/mysql.go
+++ physical/mysql/mysql.go
@@ -573,12 +573,18 @@ func NewMySQLLock(in *MySQLBackend, l log.Logger, key, value string) (*MySQLLock
 	// the rest of the MySQL backend and any cleanup that might need to be done.
 	conn, _ := NewMySQLClient(in.conf, in.logger)
 
+	// If multiple instances of Vault deployment trying to use the same MySQL
+	// DB endpoint, the first deployment will hold lock forever and others
+	// deployment will fail to insert record in the lock table,
+	// the unique lock key will resolve this problem.
+	lockKey := key + "/" + in.conf["database"] + "/" + in.conf["table"]
+
 	m := &MySQLLock{
 		parentConn: in,
 		in:         conn,
 		logger:     l,
 		statements: make(map[string]*sql.Stmt),
-		key:        key,
+		key:        lockKey,
 		value:      value,
 	}
 
