36accd68aedb80dfa17a8badd1fe8bd90cb6f479
Nick Cabatoff <ncabatoff@hashicorp.com>
2020-03-10 15:30:24 -0400

    Add comments.

--- vault/deadlock.go
+++ vault/deadlock.go
@@ -6,9 +6,16 @@ import (
 	"github.com/sasha-s/go-deadlock"
 )
 
+// DeadlockMutexW, when the build tag `deadlock` is present, behaves like a
+// sync.Mutex but does periodic checking to see if outstanding locks and requests
+// look like a deadlock.  If it finds a deadlock candidate it will output it
+// prefixed with "POTENTIAL DEADLOCK", as described at
+// https://github.com/sasha-s/go-deadlock
 type DeadlockMutexW struct {
 	deadlock.Mutex
 }
+
+// DeadlockMutexRW is the RW version of DeadlockMutexW.
 type DeadlockMutexRW struct {
 	deadlock.RWMutex
 }
--- vault/lock.go
+++ vault/lock.go
@@ -6,9 +6,14 @@ import (
 	"sync"
 )
 
+// DeadlockMutexW is just a sync.Mutex when the build tag `deadlock` is absent.
+// See its other definition in the corresponding deadlock-build-tag-constrained
+// file for more details.
 type DeadlockMutexW struct {
 	sync.Mutex
 }
+
+// DeadlockMutexRW is the RW version of DeadlockMutexW.
 type DeadlockMutexRW struct {
 	sync.RWMutex
 }
