4375fbbeb97888735e68b36dd6f6922bd0d2c28f
Josh Black <raskchanky@gmail.com>
2020-03-13 17:00:00 -0700

    feedback updates

--- command/monitor_test.go
+++ command/monitor_test.go
@@ -74,13 +74,18 @@ func TestMonitorCommand_Run(t *testing.T) {
 			cmd.ShutdownCh = shutdownCh
 
 			go testhelpers.GenerateDebugLogs(t, stopCh, client)
-
 			go func() {
 				atomic.StoreInt64(&code, int64(cmd.Run(tc.args)))
 			}()
 
+			// This is possibly just an artifact of how this test
+			// and testhelpers.GenerateDebugLogs are written, but this
+			// turns out to be fairly timing dependent. 4 seconds seems to
+			// be the magic number here. Less, and the ticker in the test
+			// helper keeps ticking before the channel is detected as closed,
+			// so you get connection refused errors from the tests.
 			select {
-			case <-time.After(3 * time.Second):
+			case <-time.After(4 * time.Second):
 				close(stopCh)
 				close(shutdownCh)
 			}
--- vendor/github.com/hashicorp/vault/api/sys_monitor.go
+++ vendor/github.com/hashicorp/vault/api/sys_monitor.go
@@ -5,7 +5,7 @@ import (
 	"context"
 )
 
-func (c *Sys) Monitor(logLevel string, stopCh chan struct{}) (chan string, error) {
+func (c *Sys) Monitor(ctx context.Context, logLevel string) (chan string, error) {
 	r := c.c.NewRequest("GET", "/v1/sys/monitor")
 
 	if logLevel == "" {
@@ -14,7 +14,6 @@ func (c *Sys) Monitor(logLevel string, stopCh chan struct{}) (chan string, error
 		r.Params.Add("log_level", logLevel)
 	}
 
-	ctx, cancelFunc := context.WithCancel(context.Background())
 	resp, err := c.c.RawRequestWithContext(ctx, r)
 	if err != nil {
 		return nil, err
@@ -22,38 +21,25 @@ func (c *Sys) Monitor(logLevel string, stopCh chan struct{}) (chan string, error
 
 	logCh := make(chan string, 64)
 	go func() {
+		defer close(logCh)
 		defer resp.Body.Close()
 		scanner := bufio.NewScanner(resp.Body)
 
 		for {
-			select {
-			case <-stopCh:
-				close(logCh)
-				cancelFunc()
+			if ctx.Err() != nil {
 				return
-			case <-ctx.Done():
-				stopCh <- struct{}{}
-				close(logCh)
-				cancelFunc()
-				return
-			default:
 			}
 
-			if scanner.Scan() {
-				// An empty string signals to the caller that
-				// the scan is done, so make sure we only emit
-				// that when the scanner says it's done, not if
-				// we happen to ingest an empty line.
-				if text := scanner.Text(); text != "" {
-					logCh <- text
-				}
-			} else {
-				// If Scan() returns false, that means the context deadline was exceeded, so
-				// terminate this routine and start a new request.
-				stopCh <- struct{}{}
-				close(logCh)
-				cancelFunc()
+			if !scanner.Scan() {
 				return
+			}
+
+			// An empty string signals to the caller that
+			// the scan is done, so make sure we only emit
+			// that when the scanner says it's done, not if
+			// we happen to ingest an empty line.
+			if text := scanner.Text(); text != "" {
+				logCh <- text
 			}
 		}
 	}()
