f496bbbab89d2d159e994e7e7451f51fb9908a16
Angel Garbarino <argarbarino@gmail.com>
2020-04-01 14:22:04 -0600

    setup option to show tabs or not

--- ui/app/components/replication/replication-toggle.js
+++ ui/app/components/replication/replication-toggle.js
@@ -2,6 +2,7 @@ import Component from '@ember/component';
 import { computed } from '@ember/object';
 
 export default Component.extend({
+  // ARG TODO: if other components need access to this response, will need to move it higher up to replication parent.
   checkedValue: false,
   handleToggleOff: computed('checkedValue', function() {
     console.log(this.checkedValue, 'setup to handle change later');
