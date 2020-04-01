e51ba172c1eee3a8bba6012061cd0a4a23d59e21
Angel Garbarino <Monkeychip@users.noreply.github.com>
2020-04-01 08:52:10 -0600

    Sidebranch: add new route on DR secondary (#8640)
    * setup, not complete
    
    * update routing
    
    * clean up
    
    * add test
    
    * add link from status menu
    
    * clean up
    
    * fixes per pr comments
    
    * revert back to two if statements due to refresh bug

---
+++ ui/tests/acceptance/replication/dr/secondary-test.js
@@ -0,0 +1,18 @@
+import { module, test } from 'qunit';
+import { setupApplicationTest } from 'ember-qunit';
+import { currentURL, visit } from '@ember/test-helpers';
+
+import authPage from 'vault/tests/pages/auth';
+
+module('Acceptance | DR secondary details', function(hooks) {
+  setupApplicationTest(hooks);
+
+  hooks.beforeEach(function() {
+    return authPage.login();
+  });
+
+  test('it visits the Secondary Disaster Recovery Details page', async function(assert) {
+    await visit('/vault/replication-dr-promote/details');
+    assert.equal(currentURL(), '/vault/replication-dr-promote/details');
+  });
+});
