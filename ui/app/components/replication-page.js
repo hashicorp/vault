d8357daa1a24a105f7612aca5b0326146091af41
Angel Garbarino <argarbarino@gmail.com>
2020-04-02 11:03:41 -0600

    clean up based on pr feedback

--- ui/app/components/replication-page.js
+++ ui/app/components/replication-page.js
@@ -1,4 +1,3 @@
-import { inject as service } from '@ember/service';
 import Component from '@ember/component';
 
 export default Component.extend({});
--- ui/app/components/replication-page/replication-toggle.js
+++ ui/app/components/replication-page/replication-toggle.js
@@ -2,11 +2,5 @@ import Component from '@ember/component';
 import { computed } from '@ember/object';
 
 export default Component.extend({
-  // ARG TODO: if other components need access to this response, will need to move it higher up to replication parent.
   checkedValue: false,
-  handleToggleOff: computed('checkedValue', function() {
-    console.log(this.checkedValue, 'setup to handle change later');
-    let status = this.checkedValue ? 'on' : 'off';
-    return status;
-  }),
 });
