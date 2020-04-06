13f93de47a908faf135f535334cbaed0c7564085
Noelle Daley <adriannenoelle@gmail.com>
2020-04-06 13:55:55 -0700

    use table element for secondaries card

--- ui/lib/replication/addon/components/replication-table.js
+++ ui/lib/replication/addon/components/replication-table.js
@@ -1,20 +1,5 @@
 import Component from '@ember/component';
-import layout from '../templates/components/replication-table';
-
-/**
- * @module ReplicationTable
- * ReplicationTable components are used to...
- *
- * @example
- * ```js
- * <ReplicationTable @requiredParam={requiredParam} @optionalParam={optionalParam} @param1={{param1}}/>
- * ```
- * @param {object} requiredParam - requiredParam is...
- * @param {string} [optionalParam] - optionalParam is...
- * @param {string} [param1=defaultValue] - param1 is...
- */
 
 export default Component.extend({
-  layout,
   data: null,
 });
