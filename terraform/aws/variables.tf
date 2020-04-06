345df4acaecc1f6558000b2ab3634726dcc2614b
Mike Jarmy <mjarmy@gmail.com>
2020-04-06 13:32:13 -0400

    update version in terraform variables

--- terraform/aws/variables.tf
+++ terraform/aws/variables.tf
@@ -3,7 +3,7 @@
 //-------------------------------------------------------------------
 
 variable "download-url" {
-    default = "https://releases.hashicorp.com/vault/1.3.0/vault_1.3.0_linux_amd64.zip"
+    default = "https://releases.hashicorp.com/vault/1.4.0/vault_1.4.0_linux_amd64.zip"
     description = "URL to download Vault"
 }
 
