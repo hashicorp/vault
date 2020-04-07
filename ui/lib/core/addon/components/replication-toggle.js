d7c51e8a7aaca79085e60f3950344c5e26d02dfe
Noelle Daley <adriannenoelle@gmail.com>
2020-04-07 13:20:56 -0700

    move replication toggle and toggle into core addon

---
+++ ui/lib/core/addon/components/replication-toggle.js
@@ -0,0 +1,7 @@
+import Component from '@ember/component';
+import layout from '../templates/components/replication-toggle';
+
+export default Component.extend({
+  layout,
+  checkedValue: false,
+});
