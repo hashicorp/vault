0df690363168185bc734838bed0c605fe790355a
Angel Garbarino <argarbarino@gmail.com>
2020-04-08 15:25:34 -0600

    calc delta

--- ui/lib/core/addon/components/replication-secondary-card.js
+++ ui/lib/core/addon/components/replication-secondary-card.js
@@ -5,8 +5,12 @@
  */
 
 import Component from '@ember/component';
+import { computed } from '@ember/object';
 import layout from '../templates/components/replication-secondary-card';
 
 export default Component.extend({
   layout,
+  delta: computed(function() {
+    return Math.abs(this.metric_1 - this.metric_2);
+  }),
 });
