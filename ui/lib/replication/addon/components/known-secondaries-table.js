41cbe1fbf09259b60efa68c36b761b306b0360a7
Noelle Daley <adriannenoelle@gmail.com>
2020-04-08 12:38:22 -0700

    populate table with actual secondary ids

--- ui/lib/replication/addon/components/known-secondaries-table.js
+++ ui/lib/replication/addon/components/known-secondaries-table.js
@@ -1,5 +1,10 @@
 import Component from '@ember/component';
+import { computed } from '@ember/object';
 
 export default Component.extend({
   data: null,
+  knownSecondaries: computed('data', function() {
+    const { data } = this.data;
+    return data.knownSecondaries;
+  }),
 });
