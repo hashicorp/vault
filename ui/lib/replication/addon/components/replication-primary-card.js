bf23bc064b5a3213d30e4a501caa589884b3930c
Noelle Daley <noelledaley@users.noreply.github.com>
2020-04-08 11:15:10 -0700

    Ui/dr primary/initial page setup (#8671)
    * add helperText param to InfoTableRow
    
    * initial page setup
    
    * format card with padding and correct number of rows
    
    * style card titles with margin
    
    * move styles inside replication class; add todos
    
    * move replication-summary styles into core app so hot reloading works
    
    * prevent known secondaries card from being cut off on the right hand side
    
    * make cards have the correct column span
    
    * make code elements inside tables black
    
    * WIP - start VltTable component
    
    * simplify css
    
    * renamed VltTable to ReplicationTable and use divs instead of table elements
    
    * fix position of known secondaries
    
    * use table element for secondaries card
    
    * add todo
    
    * move replication components to replication engine
    
    * Revert "move replication components to replication engine"
    
    This reverts commit 2228b8392199a1376815dc8b7642de438aad68b5.
    
    * move ReplicationPrimaryCards to components
    
    * remove hover box shadow since cards are not selectable yet
    
    * only apply padding to replication selectable-cards
    
    * specify replication vlt-table in classname
    
    * move replication toggle and toggle into core addon
    
    * remove extra toolbar border
    
    * remove duplicate css
    
    * move ReplicationTableRows to core addon and use them on DR primary page
    
    * clean up todos
    
    * add jsdoc comments
    
    * rename ReplicationTable to KnownSecondaries
    
    * update replicaiton table api to accept flexible data
    
    * rename replicationAttrs to data
    
    * move replication components to core addon

---
+++ ui/lib/replication/addon/components/replication-primary-card.js
@@ -0,0 +1,25 @@
+import Component from '@ember/component';
+
+/**
+ * @module ReplicationPrimaryCard
+ * ReplicationPrimaryCard components
+ *
+ * @example
+ * ```js
+ * <ReplicationPrimaryCard
+    @title='Last WAL entry'
+    @description='Index of last Write Ahead Logs entry written on local storage.'
+    @metric={{replicationAttrs.lastWAL}}
+    />
+ * ```
+ * @param {string} [title=null] - The title to be displayed on the top left corner of the card.
+ * @param {string} [description=null] - Helper text to describe the metric on the card.
+ * @param {string} metric=null - The main metric to highlight on the card.
+ */
+
+export default Component.extend({
+  tagName: '',
+  title: null,
+  description: null,
+  metric: null,
+});
