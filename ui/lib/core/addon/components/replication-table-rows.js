161bf47cb06075267e4d4fcc5f2235b36d2bd9ae
Noelle Daley <adriannenoelle@gmail.com>
2020-04-08 11:10:26 -0700

    update replicaiton table api to accept flexible data

--- ui/lib/core/addon/components/replication-table-rows.js
+++ ui/lib/core/addon/components/replication-table-rows.js
@@ -1,6 +1,28 @@
 import Component from '@ember/component';
+import { computed } from '@ember/object';
 import layout from '../templates/components/replication-table-rows';
 
 export default Component.extend({
   layout,
+  data: null,
+  clusterDetails: computed('data', function() {
+    const { data } = this.data;
+    return data.dr || data;
+  }),
+  mode: computed('clusterDetails', function() {
+    const { clusterDetails } = this;
+    return clusterDetails.mode || 'unknown';
+  }),
+  merkleRoot: computed('clusterDetails', function() {
+    const { clusterDetails } = this;
+    return clusterDetails.merkleRoot || 'unknown';
+  }),
+  clusterId: computed('clusterDetails', function() {
+    const { clusterDetails } = this;
+    return clusterDetails.clusterId || 'unknown';
+  }),
+  syncProgress: computed('clusterDetails', function() {
+    const { clusterDetails } = this;
+    return clusterDetails.syncProgress || false;
+  }),
 });
