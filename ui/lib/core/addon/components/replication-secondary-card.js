9ce336bf5e7851c5a77aeb039262a765e345510e
Angel Garbarino <argarbarino@gmail.com>
2020-04-08 13:28:15 -0600

    move replication secondary card component

---
+++ ui/lib/core/addon/components/replication-secondary-card.js
@@ -0,0 +1,12 @@
+/**
+ * @module ReplicationSecondaryCard
+ * ARG TODO finish
+ *
+ */
+
+import Component from '@ember/component';
+import layout from '../templates/components/replication-secondary-card';
+
+export default Component.extend({
+  layout,
+});
