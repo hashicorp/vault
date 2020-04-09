31e2e2c100344dc92424bbdcaa98fe228078c8ad
Noelle Daley <adriannenoelle@gmail.com>
2020-04-08 17:28:46 -0700

    fix Add Secondary link; bring in capabilities model to repliation index route so we can check for adding secondaries

--- ui/lib/replication/addon/components/known-secondaries-card.js
+++ ui/lib/replication/addon/components/known-secondaries-card.js
@@ -2,16 +2,18 @@ import Component from '@ember/component';
 
 /**
  * @module KnownSecondariesCard
- * KnownSecondariesCard components
+ * KnownSecondariesCard components are used on the Replication Details dashboards to display a table of known secondary clusters.
  *
  * @example
  * ```js
- * <KnownSecondariesCard @replicationAttrs={{replicationAttrs}} />
+ * <KnownSecondariesCard @cluster={{clusterModel}} @replicationAttrs={{replicationAttrs}} />
  * ```
- * @param {string} [replicationAttrs=null] - The attributes passed directly from the cluster model. These are passed down to the KnownSecondariesTable.
+ * @param {object} cluster=null - The cluster model.
+ * @param {string} replicationAttrs=null - The attributes passed directly from the cluster model. These are passed down to the KnownSecondariesTable.
  */
 
 export default Component.extend({
   tagName: '',
-  replicationAttrs: null,
+  cluster: null,
+  data: null,
 });
