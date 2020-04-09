2eb3c0d936869c96a2d8338c3c3a64b3d9d3c7c2
Noelle Daley <noelledaley@users.noreply.github.com>
2020-04-09 11:59:53 -0700

    Ui/dr primary components (#8711)
    * populate table with actual secondary ids
    
    * add todo
    
    * make KnownSecondariesCard component
    
    * move KnownSecondariesCard styles to own file
    
    * add EmptyState when there are no known secondaries
    
    * fix known secondaries manage link
    
    * fix Add Secondary link; bring in capabilities model to repliation index route so we can check for adding secondaries
    
    * fix JSDOC comments and updata data to replicationAttrs

--- ui/lib/replication/addon/components/known-secondaries-table.js
+++ ui/lib/replication/addon/components/known-secondaries-table.js
@@ -1,5 +1,21 @@
 import Component from '@ember/component';
+import { computed } from '@ember/object';
+
+/**
+ * @module KnownSecondariesTable
+ * KnownSecondariesTable components are used on the Replication Details dashboards to display a table of known secondary clusters.
+ *
+ * @example
+ * ```js
+ * <KnownSecondariesTable @replicationAttrs={{replicationAttrs}} />
+ * ```
+ * @param {object} replicationAttrs=null - The attributes passed directly from the cluster model used to access the array of known secondaries.
+ */
 
 export default Component.extend({
-  data: null,
+  replicationAttrs: null,
+  knownSecondaries: computed('replicationAttrs', function() {
+    const { replicationAttrs } = this;
+    return replicationAttrs.knownSecondaries;
+  }),
 });
