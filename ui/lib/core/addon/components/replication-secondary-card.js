4811cfb5130b3b2f20f578d6bcccfcd7a1adcc5b
Angel Garbarino <argarbarino@gmail.com>
2020-04-13 15:53:16 -0600

    move to computed components

--- ui/lib/core/addon/components/replication-secondary-card.js
+++ ui/lib/core/addon/components/replication-secondary-card.js
@@ -10,6 +10,21 @@ import layout from '../templates/components/replication-secondary-card';
 
 export default Component.extend({
   layout,
+  state: computed('data', function() {
+    let dr = this.data.dr;
+    return dr.state ? dr.state : 'unknown';
+  }),
+  connection: computed('data', function() {
+    return this.data.drStateDisplay ? this.data.drStateDisplay : 'unknown';
+  }),
+  lastWAL: computed('data', function() {
+    let dr = this.data.dr;
+    return dr.lastWAL ? dr.lastWAL : '0';
+  }),
+  lastRemoteWAL: computed('data', function() {
+    let dr = this.data.dr;
+    return dr.lastRemoteWAL ? dr.lastRemoteWAL : '0';
+  }),
   delta: computed('data', function() {
     let dr = this.data.dr;
     let lastWAL = dr && dr.lastWAL ? dr.lastWAL : 0;
