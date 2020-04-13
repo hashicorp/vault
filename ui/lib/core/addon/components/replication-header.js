f8123ed804191ef25555328cdf5d0ca8858441f9
Angel Garbarino <Monkeychip@users.noreply.github.com>
2020-04-13 17:25:59 -0600

    Sidebranch: DR Secondary Dashboard - pr4 (#8706)
    * setup styling for delta
    
    * clean up
    
    * replication table remove and rename model to data
    
    * remove old replication header component now that it is in addons
    
    * move replication secondary card component
    
    * calc delta
    
    * clean up
    
    * remove unused components that are now in addon
    
    * address pr comments
    
    * remove test
    
    * fix failing test
    
    * address pr comments
    
    * attempting to fix test
    
    * move to computed components
    
    * fix test error
    
    * fix state of null error

---
+++ ui/lib/core/addon/components/replication-header.js
@@ -0,0 +1,16 @@
+/**
+ * @module ReplicationHeader
+ * ARG TODO: finish
+ *
+ * @example
+ * ```js
+ * <ReplicationHeader finish/>
+ * ```
+ */
+
+import Component from '@ember/component';
+import layout from '../templates/components/replication-header';
+
+export default Component.extend({
+  layout,
+});
