bfde73f68855a5b77358dbb9537eff0fed0937f3
Angel Garbarino <argarbarino@gmail.com>
2020-04-13 14:22:41 -0600

    address pr comments

--- ui/lib/core/addon/components/replication-secondary-card.js
+++ ui/lib/core/addon/components/replication-secondary-card.js
@@ -11,10 +11,10 @@ import layout from '../templates/components/replication-secondary-card';
 export default Component.extend({
   layout,
   delta: computed('data', function() {
-    // last_wal
-    let lastWAL = this.data.dr.lastWAL ? this.data.dr.lastWAL : 0;
-    // last_remote_wal
-    let lastRemoteWAL = this.data.dr.lastRemoteWAL ? this.data.dr.lastRemoteWAL : 0;
+    let dr = this.data.dr;
+    let lastWAL = dr && dr.lastWAL ? dr.lastWAL : 0;
+    let lastRemoteWAL = dr.lastRemoteWAL ? dr.lastRemoteWAL : 0;
+
     return Math.abs(lastWAL - lastRemoteWAL);
   }),
 });
