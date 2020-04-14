f5ff623cd665066e7c3d0dea810588823839954c
Angel Garbarino <argarbarino@gmail.com>
2020-04-14 10:01:52 -0600

    clean up

--- ui/lib/core/addon/components/replication-header.js
+++ ui/lib/core/addon/components/replication-header.js
@@ -9,8 +9,16 @@
  */
 
 import Component from '@ember/component';
+import { computed } from '@ember/object';
 import layout from '../templates/components/replication-header';
 
 export default Component.extend({
   layout,
+  data: null,
+  isSecondary: computed('data', function() {
+    let data = this.data;
+    if (data.dr.mode === 'secondary' && data.rm.mode == 'dr') {
+      return true;
+    }
+  }),
 });
--- ui/lib/core/addon/components/replication-secondary-card.js
+++ ui/lib/core/addon/components/replication-secondary-card.js
@@ -10,31 +10,35 @@ import layout from '../templates/components/replication-secondary-card';
 
 export default Component.extend({
   layout,
-  state: computed('data', function() {
+  data: null,
+  dr: computed('data', function() {
     let dr = this.data.dr;
-    return dr && dr.state ? dr.state : 'unknown';
+    if (!dr) {
+      return false;
+    }
+    return dr;
+  }),
+  state: computed('data', function() {
+    return this.dr && this.dr.state ? this.dr.state : 'unknown';
   }),
   connection: computed('data', function() {
     return this.data.drStateDisplay ? this.data.drStateDisplay : 'unknown';
   }),
   lastWAL: computed('data', function() {
-    let dr = this.data.dr;
-    return dr && dr.lastWAL ? dr.lastWAL : 0;
+    return this.dr && this.dr.lastWAL ? this.dr.lastWAL : 0;
   }),
   lastRemoteWAL: computed('data', function() {
-    let dr = this.data.dr;
-    return dr && dr.lastRemoteWAL ? dr.lastRemoteWAL : 0;
+    return this.dr && this.dr.lastRemoteWAL ? this.dr.lastRemoteWAL : 0;
   }),
   delta: computed('data', function() {
     return Math.abs(this.get('lastWAL') - this.get('lastRemoteWAL'));
   }),
-  errorClass: computed('data', 'title', 'state', 'connection', function() {
-    let dr = this.data.dr;
-
-    if (!dr) {
-      return false;
+  inSyncState: computed('state', function() {
+    if (this.state === 'stream-wals') {
+      return true;
     }
-
+  }),
+  hasErrorClass: computed('data', 'title', 'state', 'connection', function() {
     if (this.title === 'States') {
       if (this.state === 'idle' || this.connection === 'transient-failure') {
         return true;
