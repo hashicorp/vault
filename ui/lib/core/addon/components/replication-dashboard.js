5198e11b2a0f1917f099e5e6216d1ff6e5cd5593
Angel Garbarino <argarbarino@gmail.com>
2020-04-14 11:14:28 -0600

    setup alert banner and documentation link

--- ui/lib/core/addon/components/replication-dashboard.js
+++ ui/lib/core/addon/components/replication-dashboard.js
@@ -1,6 +1,20 @@
 import Component from '@ember/component';
+import { computed } from '@ember/object';
 import layout from '../templates/components/replication-dashboard';
 
 export default Component.extend({
   layout,
+  data: null,
+  dr: computed('data', function() {
+    let dr = this.data.dr;
+    if (!dr) {
+      return false;
+    }
+    return dr;
+  }),
+  isSyncing: computed('data', function() {
+    if (this.dr.state === 'merkle_sync' || this.dr.state === 'stream-wals') {
+      return true;
+    }
+  }),
 });
