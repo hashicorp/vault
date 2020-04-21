4553a300c7edba218a4dfbba8d948046b7e48b6d
Noelle Daley <adriannenoelle@gmail.com>
2020-04-20 17:24:02 -0700

    WIP

--- ui/lib/replication/addon/components/replication-primary-card.js
+++ ui/lib/replication/addon/components/replication-primary-card.js
@@ -1,4 +1,5 @@
 import Component from '@ember/component';
+import { computed } from '@ember/object';
 
 /**
  * @module ReplicationPrimaryCard
@@ -24,4 +25,14 @@ export default Component.extend({
   description: null,
   metric: null,
   glyph: null,
+  hasError: computed('title', 'metric', function() {
+    // TODO: can we make a map somewhere in the cluster that keeps track of all the good and bad states
+    // as well as their glyphs? this could replace the cluster StateDiplay and StateGlyph
+    // TODO: then add tests to ensure we show the correct error msg
+    return this.title === 'State' && this.metric !== 'running';
+  }),
+  errorMessage: computed('hasError', function() {
+    // TODO figure out if we need another error message
+    return this.hasError ? 'Check server logs!' : false;
+  }),
 });
