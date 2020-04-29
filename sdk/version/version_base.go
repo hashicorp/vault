29fce8f27ce405107d0786351455ea01c82b30fd
Mike Jarmy <mjarmy@gmail.com>
2020-04-29 14:27:04 -0400

    update sdk version to 1.4.1

--- sdk/version/version_base.go
+++ sdk/version/version_base.go
@@ -8,7 +8,7 @@ var (
 	// Whether cgo is enabled or not; set at build time
 	CgoEnabled bool
 
-	Version           = "1.4.0"
-	VersionPrerelease = "beta1"
+	Version           = "1.4.1"
+	VersionPrerelease = ""
 	VersionMetadata   = ""
 )
