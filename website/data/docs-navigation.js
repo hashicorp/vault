b534e48611942340c57757bea0b2ce0b593aeb81
Becca Petrin <beccapetrin@gmail.com>
2020-04-29 09:56:40 -0700

    only add guide for 1.3.5

--- website/data/docs-navigation.js
+++ website/data/docs-navigation.js
@@ -386,8 +386,7 @@ export default [
       'upgrade-to-1.3.3',
       'upgrade-to-1.3.4',
       'upgrade-to-1.3.5',
-      'upgrade-to-1.4.0',
-      'upgrade-to-1.4.1'
+      'upgrade-to-1.4.0'
     ]
   },
   '----------------',
