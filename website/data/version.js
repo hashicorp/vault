5b15c29a37efe9d7b69724b176054212b09f8621
Calvin Leung Huang <cleung2010@gmail.com>
2020-04-30 12:41:41 -0700

    website: update changelog URL

--- website/data/version.js
+++ website/data/version.js
@@ -1,3 +1,3 @@
 export const VERSION = '1.4.1'
 export const CHANGELOG_URL =
-  'https://github.com/hashicorp/vault/blob/master/CHANGELOG.md#141-april-30-2020'
+  'https://github.com/hashicorp/vault/blob/master/CHANGELOG.md#141-april-30th-2020'
