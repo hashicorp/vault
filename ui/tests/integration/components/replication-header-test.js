7711ef2d5fe2d146e51dd0631bef41c2ef0eefbf
Angel Garbarino <argarbarino@gmail.com>
2020-04-30 12:10:56 -0600

    finish rep header test

--- ui/tests/integration/components/replication-header-test.js
+++ ui/tests/integration/components/replication-header-test.js
@@ -14,42 +14,35 @@ const DATA = {
   unsealed: 'good',
 };
 
+const TITLE = 'Disaster Recovery';
+
 module('Integration | Enterprise | Component | replication-header', function(hooks) {
   setupRenderingTest(hooks);
 
   hooks.beforeEach(function() {
     this.set('data', DATA);
+    this.set('title', TITLE);
+    this.set('isSecondary', true);
   });
-  console.log(DATA);
+
   test('it renders', async function(assert) {
-    await render(
-      hbs`<ReplicationHeader @data={{data}} @isSecondary={{true}} @title={{'Disaster Recover'}}/>`
-    );
-    //  ARG I should be able to remove isSecondary, but I get mode undefined, when it should be defined.
-    await pauseTest();
+    await render(hbs`<ReplicationHeader @data={{data}} @isSecondary={{isSecondary}} @title={{title}}/>`);
+
     assert.dom('[data-test-replication-header]').exists();
   });
 
   test('it renders with clusterId and mode when set', async function(assert) {
-    await render(hbs`<ReplicationHeader
-      @data={{data}}
-      @isSecondary={{true}}
-      @showTabs={{true}}
-      @title={{'Disaster Recover'}}/>`);
+    await render(hbs`<ReplicationHeader @data={{data}} @isSecondary={{isSecondary}} @title={{title}}/>`);
 
     assert
       .dom('[data-test-clusterId]')
       .includesText(DATA.dr.clusterIdDisplay, `shows the correct clusterId value`);
+
     assert.dom('[data-test-mode]').includesText(DATA.dr.mode, `shows the correct mode value`);
   });
 
   test('it does not show tabs when showTabs is not set', async function(assert) {
-    await render(hbs`<ReplicationHeader
-      @data={{data}}
-      @isSecondary={{true}}
-      @showTabs={{false}}
-      @title={{'Disaster Recover'}}/>`);
-    await pauseTest();
+    await render(hbs`<ReplicationHeader @data={{data}} @isSecondary={{isSecondary}} @title={{title}}/>`);
 
     assert.dom('[data-test-tabs]').doesNotExist();
   });
