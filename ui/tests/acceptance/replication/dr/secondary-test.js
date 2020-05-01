2dbf648aa54afbf63d38e457d3bb124ecce94cce
Angel Garbarino <argarbarino@gmail.com>
2020-05-01 11:51:09 -0600

    add to secondary test

--- ui/tests/acceptance/replication/dr/secondary-test.js
+++ ui/tests/acceptance/replication/dr/secondary-test.js
@@ -1,6 +1,6 @@
 import { module, test } from 'qunit';
 import { setupApplicationTest } from 'ember-qunit';
-import { currentURL, visit } from '@ember/test-helpers';
+import { pauseTest, currentURL, visit } from '@ember/test-helpers';
 
 import authPage from 'vault/tests/pages/auth';
 
@@ -14,5 +14,11 @@ module('Acceptance | DR secondary details', function(hooks) {
   test('it visits the Secondary Disaster Recovery Details page', async function(assert) {
     await visit('/vault/replication-dr-promote/details');
     assert.equal(currentURL(), '/vault/replication-dr-promote/details');
+  });
+
+  test('I should see the error message if I am not a DR secondary cluster', async function(assert) {
+    // not passing any data, so it should always show empty state
+    await visit('/vault/replication-dr-promote/details');
+    assert.dom('[data-test-component="empty-state"]').exists();
   });
 });
