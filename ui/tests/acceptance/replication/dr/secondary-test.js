2a1317734310c1b68b130db5b2f14aefffbda852
Angel Garbarino <argarbarino@gmail.com>
2020-05-04 09:49:01 -0600

    add enterprise to test

--- ui/tests/acceptance/replication/dr/secondary-test.js
+++ ui/tests/acceptance/replication/dr/secondary-test.js
@@ -1,19 +1,19 @@
 import { module, test } from 'qunit';
 import { setupApplicationTest } from 'ember-qunit';
-import { currentURL, visit } from '@ember/test-helpers';
+import { pauseTest, currentURL, visit } from '@ember/test-helpers';
 
 import authPage from 'vault/tests/pages/auth';
 
-module('Acceptance | DR secondary details', function(hooks) {
+module('Acceptance| Enterprise | DR secondary details', function(hooks) {
   setupApplicationTest(hooks);
 
   hooks.beforeEach(function() {
-    this.set('mode', 'dr');
     return authPage.login();
   });
 
   test('it visits the Secondary Disaster Recovery Details page', async function(assert) {
     await visit('/vault/replication-dr-promote/details');
+    await pauseTest();
     assert.equal(currentURL(), '/vault/replication-dr-promote/details');
   });
 });
