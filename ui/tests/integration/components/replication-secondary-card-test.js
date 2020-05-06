83e0cc95ea6afd8300126b83ccdd0fa12900695b
Angel Garbarino <argarbarino@gmail.com>
2020-05-06 13:44:31 -0600

    remove dup hasErrorClass key

--- ui/tests/integration/components/replication-secondary-card-test.js
+++ ui/tests/integration/components/replication-secondary-card-test.js
@@ -78,7 +78,6 @@ module('Integration | Enterprise | Component | replication-secondary-card', func
 
   test('it renders hasErrorMessage when connection is transient_failure', async function(assert) {
     this.set('connectionError', CONNECTION_ERROR);
-    this.set('hasErrorClass', true);
     await render(hbs`<ReplicationSecondaryCard @replicationDetails={{connectionError}} @title={{title}} />`);
     assert.dom('[data-test-error]').includesText('connection_state', 'show correct error title');
     assert
