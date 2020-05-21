dd81d580622050357f3e2ec90e51d30c77cb99f4
Chelsea Shaw <chelshaw.dev@gmail.com>
2020-05-21 13:05:55 -0500

    Ui/replication mgmt action block (#9053)
    This does some low-impact work to prepare for the refactor of replication-actions. Includes:
    
    - Move modal to addon in lib/core
    - Update modal to take a "type" param which changes the header color + icon
    - Add tests for modal changes
    - Add action-block style only component
    - Add styles-only replication-action grid that the action-blocks will live inside of

--- ui/lib/core/addon/components/modal.js
+++ ui/lib/core/addon/components/modal.js
@@ -10,12 +10,32 @@
  * @param {function} onClose - onClose is the action taken when someone clicks the modal background or close button (if shown).
  * @param {string} [title] - This text shows up in the header section of the modal.
  * @param {boolean} [showCloseButton=false] - controls whether the close button in the top right corner shows.
+ * @param {string} type=null - The header type. This comes from the message-types helper.
  */
 
 import Component from '@ember/component';
+import { computed } from '@ember/object';
+import { messageTypes } from 'core/helpers/message-types';
+import layout from '../templates/components/modal';
 
 export default Component.extend({
+  layout,
   title: null,
   showCloseButton: false,
+  type: null,
+  glyph: computed('type', function() {
+    const modalType = this.get('type');
+    if (!modalType) {
+      return {};
+    }
+    return messageTypes([this.get('type')]);
+  }),
+  modalClass: computed('type', function() {
+    const modalType = this.get('type');
+    if (!modalType) {
+      return 'modal';
+    }
+    return 'modal ' + messageTypes([this.get('type')]).class;
+  }),
   onClose: () => {},
 });
--- ui/tests/integration/components/modal-test.js
+++ ui/tests/integration/components/modal-test.js
@@ -25,5 +25,18 @@ module('Integration | Component | modal', function(hooks) {
 
     assert.equal(this.element.textContent.trim(), 'template block text', 'renders with interior content');
     assert.equal(findAll('[data-test-modal-close-button]').length, 1, 'renders close modal button');
+    assert.dom('[data-test-modal-glyph]').doesNotExist('Glyph is not rendered by default');
+  });
+
+  test('it adds the correct type class', async function(assert) {
+    await render(hbs`
+      <Modal @type="warning">
+        template block text
+      </Modal>
+      <div id="modal-wormhole"></div>
+    `);
+
+    assert.dom('.modal.is-highlight').exists('Modal exists with is-highlight class');
+    assert.dom('[data-test-modal-glyph]').exists('Glyph is rendered');
   });
 });
