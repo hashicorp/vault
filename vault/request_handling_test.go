e4eef1fdd00d751bf02f7e7ec0fd05c1a0339766
mgritter <mgritter@hashicorp.com>
2020-05-21 14:51:15 -0700

    Add counter covering token creation for response wrapping.

--- vault/request_handling_test.go
+++ vault/request_handling_test.go
@@ -148,6 +148,63 @@ func TestRequestHandling_LoginWrapping(t *testing.T) {
 	}
 }
 
+func labelsMatch(actual, expected map[string]string) bool {
+	for expected_label, expected_val := range expected {
+		if v, ok := actual[expected_label]; ok {
+			if v != expected_val {
+				return false
+			}
+		} else {
+			return false
+		}
+	}
+	return true
+}
+
+func checkCounter(t *testing.T, inmemSink *metrics.InmemSink, keyPrefix string, expectedLabels map[string]string) {
+	t.Helper()
+
+	intervals := inmemSink.Data()
+	if len(intervals) > 1 {
+		t.Skip("Detected interval crossing.")
+	}
+
+	var counter *metrics.SampledValue = nil
+	var labels map[string]string
+	for _, c := range intervals[0].Counters {
+		if !strings.HasPrefix(c.Name, keyPrefix) {
+			continue
+		}
+		counter = &c
+
+		labels = make(map[string]string)
+		for _, l := range counter.Labels {
+			labels[l.Name] = l.Value
+		}
+
+		// Distinguish between different label sets
+		if labelsMatch(labels, expectedLabels) {
+			break
+		}
+	}
+	if counter == nil {
+		t.Fatalf("No %q counter found with matching labels", keyPrefix)
+	}
+
+	if !labelsMatch(labels, expectedLabels) {
+		t.Errorf("No matching label set, found %v", labels)
+	}
+
+	if counter.Count != 1 {
+		t.Errorf("Counter number of samples %v is not 1.", counter.Count)
+	}
+
+	if counter.Sum != 1.0 {
+		t.Errorf("Counter sum %v is not 1.", counter.Sum)
+	}
+
+}
+
 func TestRequestHandling_LoginMetric(t *testing.T) {
 	core, _, root := TestCoreUnsealed(t)
 
@@ -197,12 +254,15 @@ func TestRequestHandling_LoginMetric(t *testing.T) {
 		t.Fatalf("bad: %#v", resp)
 	}
 
-	// Login
+	// Login with response wrapping
 	req = &logical.Request{
 		Path:      "auth/userpass/login/test",
 		Operation: logical.UpdateOperation,
 		Data: map[string]interface{}{
 			"password": "foo",
+		},
+		WrapInfo: &logical.RequestWrapInfo{
+			TTL: time.Duration(15 * time.Second),
 		},
 		Connection: &logical.Connection{},
 	}
@@ -213,60 +273,27 @@ func TestRequestHandling_LoginMetric(t *testing.T) {
 	if resp == nil {
 		t.Fatalf("bad: %v", resp)
 	}
-	if resp.WrapInfo != nil {
-		t.Fatalf("bad: %#v", resp)
-	}
 
-	intervals := inmemSink.Data()
-	if len(intervals) > 1 {
-		t.Skip("Detected interval crossing.")
-	}
-
-	// TODO: can this sort of check go into a test utility for
-	// use in multiple test cases? (It's a copy of code over
-	// in token_store_test.go)
-	keyPrefix := "token.creation"
-	var counter *metrics.SampledValue = nil
-
-	for _, c := range intervals[0].Counters {
-		if strings.HasPrefix(c.Name, keyPrefix) {
-			counter = &c
-			break
-		}
-	}
-	if counter == nil {
-		t.Fatal("No token.creation counter found.")
-	}
-
-	if counter.Count != 1 {
-		t.Errorf("Counter number of samples %v is not 1.", counter.Count)
-	}
-
-	if counter.Sum != 1.0 {
-		t.Errorf("Counter sum %v is not 1.", counter.Sum)
-	}
-
-	labels := make(map[string]string)
-	for _, l := range counter.Labels {
-		labels[l.Name] = l.Value
-	}
-	// FIXME: keep the final / in metrics, or not?
-	expected := map[string]string{
-		"cluster":      "test-cluster",
-		"namespace":    "root",
-		"auth_method":  "userpass",
-		"mount_point":  "auth/userpass/",
-		"creation_ttl": "+Inf",
-		"token_type":   "service",
-	}
-	for expected_label, expected_val := range expected {
-		if v, ok := labels[expected_label]; ok {
-			if v != expected_val {
-				t.Errorf("Label %q incorrect, expected %q, got %q", expected_label, expected_val, v)
-			}
-		} else {
-			t.Errorf("Label %q missing", expected_label)
-		}
-	}
+	// There should be two counters
+	checkCounter(t, inmemSink, "token.creation",
+		map[string]string{
+			"cluster":      "test-cluster",
+			"namespace":    "root",
+			"auth_method":  "userpass",
+			"mount_point":  "auth/userpass/",
+			"creation_ttl": "+Inf",
+			"token_type":   "service",
+		},
+	)
+	checkCounter(t, inmemSink, "token.creation",
+		map[string]string{
+			"cluster":      "test-cluster",
+			"namespace":    "root",
+			"auth_method":  "response_wrapping",
+			"mount_point":  "auth/userpass/",
+			"creation_ttl": "1m",
+			"token_type":   "service",
+		},
+	)
 
 }
