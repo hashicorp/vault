3d20aa3c93c91ada24c1e628ce7d1c7b0310ee67
Mike Jarmy <mjarmy@gmail.com>
2020-05-21 07:24:37 -0400

    update vendor for 1.4.2

--- vendor/github.com/hashicorp/vault/sdk/version/version_base.go
+++ vendor/github.com/hashicorp/vault/sdk/version/version_base.go
@@ -8,7 +8,7 @@ var (
 	// Whether cgo is enabled or not; set at build time
 	CgoEnabled bool
 
-	Version           = "1.4.1"
+	Version           = "1.4.2"
 	VersionPrerelease = ""
 	VersionMetadata   = ""
 )
