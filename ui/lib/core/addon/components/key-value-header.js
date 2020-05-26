befb62947ab68cbd96ca38145bdbb35f2657b47d
Angel Garbarino <argarbarino@gmail.com>
2020-05-26 10:09:39 -0600

    move key value to lib/core/addon so I can use inside replication engine

--- ui/lib/core/addon/components/key-value-header.js
+++ ui/lib/core/addon/components/key-value-header.js
@@ -1,6 +1,7 @@
 import { computed } from '@ember/object';
 import Component from '@ember/component';
 import utils from 'vault/lib/key-utils';
+import layout from '../templates/components/key-value-header';
 import { encodePath } from 'vault/utils/path-encoding-helpers';
 
 export default Component.extend({
