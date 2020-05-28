c4925c36dd59c2bac4993ed476b2d04fb6705daf
Aleksandr Bezobchuk <aleks.bezobchuk@gmail.com>
2020-05-28 16:18:14 -0400

    Add key_version to sig verify

--- builtin/logical/transit/path_sign_verify.go
+++ builtin/logical/transit/path_sign_verify.go
@@ -23,6 +23,9 @@ type batchResponseSignItem struct {
 	// request item
 	Signature string `json:"signature,omitempty" mapstructure:"signature"`
 
+	// The key version to be used for encryption
+	KeyVersion int `json:"key_version" structs:"key_version" mapstructure:"key_version"`
+
 	PublicKey []byte `json:"publickey,omitempty" mapstructure:"publickey"`
 
 	// Error, if set represents a failure encountered while encrypting a
@@ -329,8 +332,14 @@ func (b *backend) pathSignWrite(ctx context.Context, req *logical.Request, d *fr
 		} else if sig == nil {
 			response[i].err = fmt.Errorf("signature could not be computed")
 		} else {
+			keyVersion := ver
+			if keyVersion == 0 {
+				keyVersion = p.LatestVersion
+			}
+
 			response[i].Signature = sig.Signature
 			response[i].PublicKey = sig.PublicKey
+			response[i].KeyVersion = keyVersion
 		}
 	}
 
@@ -351,8 +360,8 @@ func (b *backend) pathSignWrite(ctx context.Context, req *logical.Request, d *fr
 		}
 
 		resp.Data = map[string]interface{}{
-			"signature": response[0].Signature,
-			"version":   ver,
+			"signature":   response[0].Signature,
+			"key_version": response[0].KeyVersion,
 		}
 
 		if len(response[0].PublicKey) > 0 {
