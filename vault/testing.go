31eb3a7422855058103ca9a1c0bd3311cb5b1d34
Mike Jarmy <mjarmy@gmail.com>
2020-05-28 10:35:26 -0400

    add cleanup func to TestCore

--- vault/testing.go
+++ vault/testing.go
@@ -838,7 +838,14 @@ func (c *TestClusterCore) Seal(t testing.T) {
 	}
 }
 
-func (c *TestClusterCore) Shutdown() error {
+func (c *TestClusterCore) Cleanup(t testing.T) {
+	t.Helper()
+	if err := c.cleanup(); err != nil {
+		t.Fatal(err)
+	}
+}
+
+func (c *TestClusterCore) cleanup() error {
 	if c.Listeners != nil {
 		for _, ln := range c.Listeners {
 			ln.Close()
@@ -890,8 +897,8 @@ func (c *TestCluster) Cleanup() {
 
 		go func() {
 			defer wg.Done()
-			if err := lc.Shutdown(); err != nil {
-				lc.Logger().Error("error during shutdown; abandoning sealing", "error", err)
+			if err := lc.cleanup(); err != nil {
+				lc.Logger().Error("error during cleanup", "error", err)
 			}
 		}()
 	}
