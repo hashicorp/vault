f1679719ab3352cf0a11db5ddb070528a8e71bba
Angel Garbarino <argarbarino@gmail.com>
2020-06-01 14:28:54 -0600

    jsdocs

--- ui/lib/core/addon/components/replication-header.js
+++ ui/lib/core/addon/components/replication-header.js
@@ -1,6 +1,26 @@
 import Component from '@ember/component';
 import layout from '../templates/components/replication-header';
 
+/**
+ * @module ReplicationHeader
+ * The `ReplicationHeader` component is a header component used on the Replication Dashboards.
+ * It is a contextual component of the Replication Page component.
+ *
+ * @example
+ * ```js
+ * <ReplicationHeader
+    @data={{model}}
+    @title="Secondary"
+    @secondaryID="meep_123"
+    @isSummaryDashboard=false
+    />
+ * ```
+ * @param {Object} model=null - An Ember data object pulled form the Ember cluster model.
+ * @param {String} title=null - The title of the header.
+ * @param {String} [secondaryID=null] - The secondaryID pulled off of the model object. 
+ * @param {Boolean} isSummaryDashboard=false - if the Dashboard is for when you have both a primary performance and dr cluster, then this is true.
+ */
+
 export default Component.extend({
   layout,
   classNames: ['replication-header'],
--- ui/lib/core/addon/components/replication-table-rows.js
+++ ui/lib/core/addon/components/replication-table-rows.js
@@ -2,6 +2,21 @@ import Component from '@ember/component';
 import { computed } from '@ember/object';
 import layout from '../templates/components/replication-table-rows';
 
+/**
+ * @module ReplicationTableRows
+ * The `ReplicationTableRows` component is table component.  It displays cluster mode details specific to the cluster of the Dashboard it is used on.
+ *
+ * @example
+ * ```js
+ * <ReplicationTableRows
+    @replicationDetails={{replicationDetails}}
+    @clusterMode="primary"
+    />
+ * ```
+ * @param {Object} replicationDetails=null - An Ember data object pulled from the Ember Model. It contains details specific to the whether the replication is dr or performance.
+ * @param {String} clusterMode=null - The cluster mode passed through to a table component. 
+ */
+
 export default Component.extend({
   layout,
   classNames: ['replication-table-rows'],
