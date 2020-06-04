ddacb15a509b0965ee5538bf43790ab8a443671b
Angel Garbarino <argarbarino@gmail.com>
2020-06-04 10:34:22 -0600

    fix date-format test after moving it in addon

--- ui/tests/integration/helpers/date-format-test.js
+++ ui/tests/integration/helpers/date-format-test.js
@@ -1,27 +1,37 @@
 import { module, test } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
-import { dateFormat } from '../../../helpers/date-format';
+import { render } from '@ember/test-helpers';
+import hbs from 'htmlbars-inline-precompile';
 
 module('Integration | Helper | date-format', function(hooks) {
   setupRenderingTest(hooks);
 
-  test('it is able to format a date object', function(assert) {
+  test('it is able to format a date object', async function(assert) {
     let today = new Date();
-    let result = dateFormat([today, 'YYYY']);
-    assert.ok(typeof result === 'string');
-    assert.ok(result !== 'Invalid Date', 'it is not an invalid date');
-    assert.ok(Number(result) >= 2017);
+    this.set('today', today);
+
+    await render(hbs`<p data-test-date-format>Date: {{date-format today "YYYY"}}</p>`);
+    assert
+      .dom('[data-test-date-format]')
+      .includesText(today.getFullYear(), 'it renders the date in the year format');
   });
 
-  test('it supports date timestamps', function(assert) {
-    let today = new Date().getTime();
-    let result = dateFormat([today, 'YYYY']);
-    assert.ok(Number(result) >= 2017);
+  test('it formats the date as specified', async function(assert) {
+    let today = new Date();
+    this.set('today', today);
+
+    await render(hbs`<p class="date-format">{{date-format today 'hh:mm:ss'}}</p>`);
+    let formattedDate = document.querySelector('.date-format').innerText;
+    assert.ok(formattedDate.match(/^\d{2}:\d{2}:\d{2}$/));
   });
 
-  test('it supports date strings', function(assert) {
-    let today = new Date().toString();
-    let result = dateFormat([today, 'YYYY']);
-    assert.ok(Number(result) >= 2017);
+  test('it supports date strings', async function(assert) {
+    let todayString = new Date().getFullYear().toString();
+    this.set('todayString', todayString);
+
+    await render(hbs`<p data-test-date-format>Date: {{date-format todayString}}</p>`);
+    assert
+      .dom('[data-test-date-format]')
+      .includesText(todayString, 'it renders the a date if passed in as a string');
   });
 });
