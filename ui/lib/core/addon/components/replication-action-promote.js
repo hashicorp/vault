fc7b64eceb4c375051c06447cb2a366e755adb25
Angel Garbarino <argarbarino@gmail.com>
2020-06-11 10:37:04 -0600

    handle esc key

--- ui/lib/core/addon/components/replication-action-promote.js
+++ ui/lib/core/addon/components/replication-action-promote.js
@@ -1,6 +1,18 @@
 import Actions from './replication-actions-single';
 import layout from '../templates/components/replication-action-promote';
+import keys from 'vault/lib/keycodes';
 
 export default Actions.extend({
   layout,
+  onSubmit() {},
+  actions: {
+    onSubmit() {
+      if (event.keyCode === keys.ESC) {
+        // if escape close modal and return
+        this.toggleProperty('isModalActive');
+        return;
+      }
+      return this.onSubmit(...arguments);
+    },
+  },
 });
--- ui/lib/core/addon/components/replication-action-reindex.js
+++ ui/lib/core/addon/components/replication-action-reindex.js
@@ -1,6 +1,18 @@
 import Actions from 'core/components/replication-actions-single';
 import layout from '../templates/components/replication-action-reindex';
+import keys from 'vault/lib/keycodes';
 
 export default Actions.extend({
   layout,
+  onSubmit() {},
+  actions: {
+    onSubmit() {
+      if (event.keyCode === keys.ESC) {
+        // if escape close modal and return
+        this.toggleProperty('isModalActive');
+        return;
+      }
+      return this.onSubmit(...arguments);
+    },
+  },
 });
