fc888908bc313ff91804d17dde2b03f405f5f256
Angel Garbarino <argarbarino@gmail.com>
2020-06-11 11:20:16 -0600

    fix escape and enter when confirmationText is needed

--- ui/lib/core/addon/components/replication-action-demote.js
+++ ui/lib/core/addon/components/replication-action-demote.js
@@ -1,18 +1,6 @@
 import Actions from 'core/components/replication-actions-single';
 import layout from '../templates/components/replication-action-demote';
-import keys from 'vault/lib/keycodes';
 
 export default Actions.extend({
   layout,
-  onSubmit() {},
-  actions: {
-    onSubmit() {
-      if (event.keyCode === keys.ESC) {
-        // if escape close modal and return
-        this.toggleProperty('isModalActive');
-        return;
-      }
-      return this.onSubmit(...arguments);
-    },
-  },
 });
