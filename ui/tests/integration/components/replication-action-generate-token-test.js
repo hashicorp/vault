1049f8dcc16bf98e4e5c03f420972c0509ee9865
Chelsea Shaw <chelshaw.dev@gmail.com>
2020-06-12 18:09:13 -0500

    Add test coverage for replication-action-generate-token

--- ui/tests/integration/components/replication-action-generate-token-test.js
+++ ui/tests/integration/components/replication-action-generate-token-test.js
@@ -1,26 +1,21 @@
 import { module, test } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
-import { render } from '@ember/test-helpers';
+import { render, find } from '@ember/test-helpers';
 import hbs from 'htmlbars-inline-precompile';
 
 module('Integration | Component | replication-action-generate-token', function(hooks) {
   setupRenderingTest(hooks);
 
-  test('it renders', async function(assert) {
+  test('it renders with the expected elements', async function(assert) {
     // Set any properties with this.set('myProperty', 'value');
     // Handle any actions with this.set('myAction', function(val) { ... });
 
     await render(hbs`{{replication-action-generate-token}}`);
-
-    assert.equal(this.element.textContent.trim(), '');
-
-    // Template block usage:
-    await render(hbs`
-      {{#replication-action-generate-token}}
-        template block text
-      {{/replication-action-generate-token}}
-    `);
-
-    assert.equal(this.element.textContent.trim(), 'template block text');
+    assert.equal(find('h4.title').textContent.trim(), 'Generate operation token', 'renders default title');
+    assert.equal(
+      find('[data-test-replication-action-trigger]').textContent.trim(),
+      'Generate token',
+      'renders default CTA'
+    );
   });
 });
