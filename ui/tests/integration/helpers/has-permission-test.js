855d8493e56dfa301aaad6c3f7d3461fc4759cf7
Noelle Daley <adriannenoelle@gmail.com>
2020-06-15 16:32:38 -0700

    add notes about stubbing services

--- ui/tests/integration/helpers/has-permission-test.js
+++ ui/tests/integration/helpers/has-permission-test.js
@@ -5,7 +5,13 @@ import { run } from '@ember/runloop';
 import hbs from 'htmlbars-inline-precompile';
 import Service from '@ember/service';
 
-// create a mock permissions service
+/*
+Notice that Ember gives us the ability to create a stub service by
+importing and extending Service directly. This is helpful because
+the real permissions service makes API calls which are unnecessary
+for these tests.
+*/
+
 const Permissions = Service.extend({
   globPaths: null,
   hasNavPermission() {
@@ -15,7 +21,16 @@ const Permissions = Service.extend({
 
 module('Integration | Helper | has-permission', function(hooks) {
   setupRenderingTest(hooks);
+
   hooks.beforeEach(function() {
+    /*
+    Thanks to setupRenderingTest above, which gives us access to
+    Ember's dependency injection system, we can ensure that the
+    helper here uses our stub permissions service instead of the
+    real one. We do this by registering the service, and then
+    pointing the test instance of the permissions service to the one
+    we just registered.
+    */
     this.owner.register('service:permissions', Permissions);
     this.permissions = this.owner.lookup('service:permissions');
   });
