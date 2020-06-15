ceb5fc1122e7903fb3a9d18534578c6b4cd9c7de
Noelle Daley <adriannenoelle@gmail.com>
2020-06-15 11:45:52 -0700

    handle errors

--- ui/lib/core/addon/helpers/changelog-url-for.js
+++ ui/lib/core/addon/helpers/changelog-url-for.js
@@ -16,18 +16,22 @@ etc.
 export function changelogUrlFor([version]) {
   let url = 'http://www.github.com/hashicorp/vault/blob/master/CHANGELOG.md#';
 
-  // strip the '+prem' from enterprise versions and remove periods
-  let versionNumber = version
-    .split('+')[0]
-    .split('.')
-    .join('');
+  try {
+    // strip the '+prem' from enterprise versions and remove periods
+    let versionNumber = version
+      .split('+')[0]
+      .split('.')
+      .join('');
 
-  // only recent versions have a predictable url
-  if (versionNumber >= '143') {
-    return url.concat('v', versionNumber);
-  } else {
-    return url;
+    // only recent versions have a predictable url
+    if (versionNumber >= '143') {
+      return url.concat('v', versionNumber);
+    }
+  } catch (e) {
+    console.log(e);
+    console.log('Cannot generate URL for version: ', version);
   }
+  return url;
 }
 
 export default helper(changelogUrlFor);
