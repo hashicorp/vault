59a33bdc87114269ca9ae5de9a0e425a553daf1e
Chelsea Shaw <chelshaw.dev@gmail.com>
2020-06-16 17:27:09 -0500

    Add modal div to test

--- ui/tests/integration/components/replication-action-generate-token-test.js
+++ ui/tests/integration/components/replication-action-generate-token-test.js
@@ -7,7 +7,10 @@ module('Integration | Component | replication-action-generate-token', function(h
   setupRenderingTest(hooks);
 
   test('it renders with the expected elements', async function(assert) {
-    await render(hbs`{{replication-action-generate-token}}`);
+    await render(hbs`
+      <div id="modal-wormhole"></div>
+      {{replication-action-generate-token}}
+    `);
     assert.equal(find('h4.title').textContent.trim(), 'Generate operation token', 'renders default title');
     assert.equal(
       find('[data-test-replication-action-trigger]').textContent.trim(),
