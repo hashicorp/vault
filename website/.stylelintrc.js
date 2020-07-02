9d764237d6c413c3c96bd7ab36d7092422f4750d
Jeff Escalante <jescalan@users.noreply.github.com>
2020-07-02 13:45:50 -0400

    refreshing with the latest dep updates

--- website/.stylelintrc.js
+++ website/.stylelintrc.js
@@ -1,13 +1,4 @@
 module.exports = {
   ...require('@hashicorp/nextjs-scripts/.stylelintrc.js'),
-  ignoreFiles: ['out/**'],
-  rules: {
-    'selector-pseudo-class-no-unknown': [
-      true,
-      {
-        ignorePseudoClasses: ['first', 'last'],
-      },
-    ],
-  },
   /* Specify overrides here */
 }
