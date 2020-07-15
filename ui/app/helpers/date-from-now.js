bd735351ba2acb63c417e96aaa9870d53732f81f
Noelle Daley <adriannenoelle@gmail.com>
2020-07-14 17:42:09 -0700

    update date-fns to fix date-from-now NaN error; use new date format

--- ui/app/helpers/date-from-now.js
+++
@@ -1,8 +0,0 @@
-import { helper } from '@ember/component/helper';
-import { distanceInWordsToNow } from 'date-fns';
-
-export function dateFromNow([date], options = {}) {
-  return distanceInWordsToNow(date, options);
-}
-
-export default helper(dateFromNow);
