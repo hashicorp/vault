81b1a4a800e270b9373eaf6f606a30e357189407
Noelle Daley <adriannenoelle@gmail.com>
2020-07-17 11:17:52 -0700

    parse ISO strings first

--- ui/app/helpers/date-from-now.js
+++ ui/app/helpers/date-from-now.js
@@ -1,13 +1,29 @@
 import { helper } from '@ember/component/helper';
 import { formatDistanceToNow } from 'date-fns';
+import parseISO from 'date-fns/parseISO';
 
 export function dateFromNow([date], options = {}) {
   let d = date;
+
+  if (typeof date === 'string') {
+    // try to parse string assuming ISO format
+    d = parseISO(date);
+
+    // if that resulted in invalid date, try with new Date()
+    if (!d.getTime()) {
+      d = new Date(date);
+    }
+
+    // if that also failed, return just passed date string;
+    if (!d.getTime()) {
+      return date;
+    }
+  }
+
   try {
     // expects date obj or number only
     return formatDistanceToNow(d, { ...options });
   } catch (e) {
-    // if we can't determine the distance, show nothing
     return '';
   }
 }
