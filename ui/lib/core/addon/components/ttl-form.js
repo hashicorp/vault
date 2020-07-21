89027c421a953470e1199f6a016a8e8841791153
Chelsea Shaw <chelshaw.dev@gmail.com>
2020-07-21 09:49:29 -0500

    Add TtlForm addon component from generator

---
+++ ui/lib/core/addon/components/ttl-form.js
@@ -0,0 +1,16 @@
+/**
+ * @module TtlForm
+ * TtlForm components are used to...
+ *
+ * @example
+ * ```js
+ * <TtlForm @requiredParam={requiredParam} @optionalParam={optionalParam} @param1={{param1}}/>
+ * ```
+ * @param {object} requiredParam - requiredParam is...
+ * @param {string} [optionalParam] - optionalParam is...
+ * @param {string} [param1=defaultValue] - param1 is...
+ */
+
+import Component from '@ember/component';
+
+export default Component.extend({});
