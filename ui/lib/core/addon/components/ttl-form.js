d6c5090399e080c46ffb376eded9da67981b10c6
Chelsea Shaw <chelshaw.dev@gmail.com>
2020-07-22 14:59:48 -0500

    TtlForm working as expected

--- ui/lib/core/addon/components/ttl-form.js
+++ ui/lib/core/addon/components/ttl-form.js
@@ -1,11 +1,13 @@
 /**
  * @module TtlForm
- * TtlForm components are used to enter a Time To Live (TTL) input. This component does not include a label.
- * This component is designed to take a time and unit, and pass an object when those two values are changed.
+ * TtlForm components are used to enter a Time To Live (TTL) input.
+ * This component does not include a label and is designed to take
+ * a time and unit, and pass an object including seconds and
+ * timestring when those two values are changed.
  *
  * @example
  * ```js
- * <TtlForm @requiredParam={requiredParam} @optionalParam={optionalParam} @param1={{param1}}/>
+ * <TtlForm @onChange={action handleChange} @unit={{m}}/>
  * ```
  * @param {function} onChange - This function will be called when the user changes the value. An object will be passed in as a parameter with values seconds{number}, timeString{string}
  * @param {number} [time] - Time is the value that will be passed into the value input. Can be null/undefined to start if input is required.
@@ -18,26 +20,32 @@ import { computed } from '@ember/object';
 import { task, timeout } from 'ember-concurrency';
 import layout from '../templates/components/ttl-form';
 
-// const secondsMap = {
-//   s: 1,
-//   m: 60,
-//   h: 3600,
-//   d: 86400,
-// };
-// const convertToSeconds = (time, unit) => {
-//   return time * secondsMap[unit];
-// };
-// const convertFromSeconds = (seconds, unit) => {
-//   return seconds / secondsMap[unit];
-// };
+const secondsMap = {
+  s: 1,
+  m: 60,
+  h: 3600,
+  d: 86400,
+};
+const convertToSeconds = (time, unit) => {
+  return time * secondsMap[unit];
+};
+const convertFromSeconds = (seconds, unit) => {
+  return seconds / secondsMap[unit];
+};
 
 export default Component.extend({
   layout,
-  // onChange: () => {},
   time: '',
-  recalculationTimeout: 5000,
   unit: 's',
+
+  init() {
+    this._super(...arguments);
+  },
+
+  /* Used internally */
+  recalculationTimeout: 5000,
   recalculateSeconds: false,
+  errorMessage: null,
   unitOptions: computed(function() {
     return [
       { label: 'seconds', value: 's' },
@@ -46,45 +54,41 @@ export default Component.extend({
       { label: 'days', value: 'd' },
     ];
   }),
-
   handleChange() {
-    console.log('handling change from ttl-form');
-    // let { time, unit, enableTTL, seconds } = this.getProperties('time', 'unit', 'enableTTL', 'seconds');
-    // const ttl = {
-    //   enabled: enableTTL,
-    //   seconds,
-    //   timeString: time + unit,
-    // };
-    // this.onChange(ttl);
+    let { time, unit, seconds } = this.getProperties('time', 'unit', 'seconds');
+    const ttl = {
+      seconds,
+      timeString: time + unit,
+    };
+    console.log('changing', ttl);
+    this.onChange(ttl);
   },
   keepSecondsRecalculate(newUnit) {
-    console.log('recalculating time');
-    // const newTime = convertFromSeconds(this.seconds, newUnit);
-    // this.setProperties({
-    //   time: newTime,
-    //   unit: newUnit,
-    // });
+    const newTime = convertFromSeconds(this.seconds, newUnit);
+    this.setProperties({
+      time: newTime,
+      unit: newUnit,
+    });
   },
   updateTime: task(function*(newTime) {
-    // this.set('errorMessage', '');
-    // console.log('update time');
-    // let parsedTime;
-    // parsedTime = parseInt(newTime, 10);
-    // if (!newTime) {
-    //   this.set('errorMessage', 'This field is required');
-    //   return;
-    // } else if (Number.isNaN(parsedTime)) {
-    //   this.set('errorMessage', 'Value must be a number');
-    //   return;
-    // }
-    // this.set('time', parsedTime);
-    // this.handleChange();
-    // if (Ember.testing) {
-    //   return;
-    // }
-    // this.set('recalculateSeconds', true);
-    // yield timeout(this.recalculationTimeout);
-    // this.set('recalculateSeconds', false);
+    this.set('errorMessage', '');
+    let parsedTime;
+    parsedTime = parseInt(newTime, 10);
+    if (!newTime) {
+      this.set('errorMessage', 'This field is required');
+      return;
+    } else if (Number.isNaN(parsedTime)) {
+      this.set('errorMessage', 'Value must be a number');
+      return;
+    }
+    this.set('time', parsedTime);
+    this.handleChange();
+    if (Ember.testing) {
+      return;
+    }
+    this.set('recalculateSeconds', true);
+    yield timeout(this.recalculationTimeout);
+    this.set('recalculateSeconds', false);
   }).restartable(),
 
   seconds: computed('time', 'unit', function() {
@@ -93,12 +97,9 @@ export default Component.extend({
 
   actions: {
     updateUnit(newUnit) {
-      console.log('recalc seconds', this.recalculateSeconds);
       if (this.recalculateSeconds) {
-        console.log(`setting unit to ${newUnit}`);
         this.set('unit', newUnit);
       } else {
-        console.log('keep seconds val and recalculate');
         this.keepSecondsRecalculate(newUnit);
       }
       this.handleChange();
