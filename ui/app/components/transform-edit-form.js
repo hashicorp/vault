eb93dab0370bac5cd5e92cf229d04e1214da3893
Angel Garbarino <argarbarino@gmail.com>
2020-07-28 10:39:34 -0600

    create two transform edit components

---
+++ ui/app/components/transform-edit-form.js
@@ -0,0 +1,15 @@
+import RoleEdit from './role-edit';
+
+export default RoleEdit.extend({
+  init() {
+    this._super(...arguments);
+    this.set('backendType', 'transform');
+  },
+  actions: {
+    updateTtl(path, val) {
+      const model = this.get('model');
+      let valueToSet = val.enabled === true ? `${val.seconds}s` : undefined;
+      model.set(path, valueToSet);
+    },
+  },
+});
