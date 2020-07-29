705c4bdf6ea754fd8d585b7cfa9fdd9e46e23733
Angel Garbarino <argarbarino@gmail.com>
2020-07-29 14:09:50 -0600

    clean up linting errors

--- ui/app/models/transform/role.js
+++ ui/app/models/transform/role.js
@@ -1,9 +1,3 @@
-import { alias } from '@ember/object/computed';
-import { computed } from '@ember/object';
 import DS from 'ember-data';
-import lazyCapabilities, { apiPath } from 'vault/macros/lazy-capabilities';
-import fieldToAttrs, { expandAttributeMeta } from 'vault/utils/field-to-attrs';
-
-const { attr } = DS;
 
 export default DS.Model.extend({});
