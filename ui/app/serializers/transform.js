d3dba7224634263c9491b1c285ab3111849e458c
Angel Garbarino <argarbarino@gmail.com>
2020-08-11 15:37:32 -0600

    address pr comments

--- ui/app/serializers/transform.js
+++ ui/app/serializers/transform.js
@@ -5,16 +5,7 @@ export default ApplicationSerializer.extend({
     if (payload.data.masking_character) {
       payload.data.masking_character = String.fromCharCode(payload.data.masking_character);
     }
-
-    if (payload.data.templates && payload.data.templates.length === 1) {
-      // add space after comma in returned array length one of a string of templates
-      payload.data.templates[0] = payload.data.templates[0].replace(/,/g, ', ');
-    }
-
-    // TODO: something similar her for roles, however it's a little tough because each role comes back as an item in an array.
-    // Also note, the BE is working on a ticket to amend these response, so revisit.
-    // TODO: QA to make sure this is only normalizing the response and not changing data on edit (e.g. add a space after the comma when rePOST on edit)
-
+    // TODO: the BE is working on a ticket to amend these response, so revisit.
     return this._super(store, primaryModelClass, payload, id, requestType);
   },
 });
