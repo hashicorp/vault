2b3a0de42a50ce9ab2da104406570676252b5a8b
Chelsea Shaw <chelshaw.dev@gmail.com>
2020-08-20 10:42:49 -0500

    Update role and template adapters

--- ui/app/adapters/transform/template.js
+++ ui/app/adapters/transform/template.js
@@ -1,20 +1,30 @@
 import ApplicationAdapater from '../application';
+import { encodePath } from 'vault/utils/path-encoding-helpers';
 
 export default ApplicationAdapater.extend({
   namespace: 'v1',
+  modelName: 'transform/template',
+
   pathForType(type) {
-    return type;
+    return type.replace('transform/', '');
   },
 
-  urlForQuery() {
-    return this._super(...arguments) + '?list=true';
+  _url(modelType, backend, id) {
+    let type = this.pathForType(modelType);
+    let base = `${this.buildURL()}/${encodePath(backend)}/${type}`;
+    if (id) {
+      return `${base}/${encodePath(id)}`;
+    }
+    return base + '?list=true';
   },
 
-  query(store, type) {
-    return this.ajax(this.buildURL(type.modelName, null, null, 'query'), 'GET');
+  query(store, type, query) {
+    return this.ajax(this._url(this.modelName, query.backend), 'GET').then(result => {
+      return result;
+    });
   },
 
-  buildURL(modelName, id, snapshot, requestType, query) {
-    return this._super(`${modelName}/`, id, snapshot, requestType, query);
-  },
+  // buildURL(modelName, id, snapshot, requestType, query) {
+  //   return this._super(`${modelName}/`, id, snapshot, requestType, query);
+  // },
 });
