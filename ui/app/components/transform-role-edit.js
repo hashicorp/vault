b18fafebefe63b9c30df8f5a13ceedec935d5c27
Chelsea Shaw <chelshaw.dev@gmail.com>
2020-08-24 08:57:16 -0500

    WIP // fix transform breadcrumbs, add transform-role-edit component

--- ui/app/components/transform-role-edit.js
+++ ui/app/components/transform-role-edit.js
@@ -1,3 +1,26 @@
-import TransformBase from './transform-edit-base';
+/**
+ * @module TransformRoleEdit
+ * TransformRoleEdit components are used to...
+ *
+ * @example
+ * ```js
+ * <TransformRoleEdit @requiredParam={requiredParam} @optionalParam={optionalParam} @param1={{param1}}/>
+ * ```
+ * @param {object} requiredParam - requiredParam is...
+ * @param {string} [optionalParam] - optionalParam is...
+ * @param {string} [param1=defaultValue] - param1 is...
+ */
 
-export default TransformBase.extend({});
+// import Component from '@ember/component';
+
+// export default Component.extend({
+// });
+
+import RoleEdit from './role-edit';
+
+export default RoleEdit.extend({
+  init() {
+    this._super(...arguments);
+    this.set('backendType', 'transform');
+  },
+});
