19bc619bd5b2c8b1a98f195158c628dcfc37d057
Chelsea Shaw <chelshaw.dev@gmail.com>
2020-08-24 09:18:03 -0500

    Remove extraeneous set backend type on transform components

--- ui/app/components/transform-create-form.js
+++ ui/app/components/transform-create-form.js
@@ -1,8 +1,3 @@
 import TransformBase from './transform-edit-base';
 
-export default TransformBase.extend({
-  init() {
-    this._super(...arguments);
-    this.set('backendType', 'transform');
-  },
-});
+export default TransformBase.extend({});
