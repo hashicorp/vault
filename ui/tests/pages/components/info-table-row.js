0b847a8f3a39312dc51e31921f896c0222d2c7e3
Angel Garbarino <argarbarino@gmail.com>
2020-08-27 11:41:30 -0600

    remove todo

--- ui/tests/pages/components/info-table-row.js
+++ ui/tests/pages/components/info-table-row.js
@@ -1,5 +1,5 @@
 import { text, isPresent } from 'ember-cli-page-object';
-//  ARG TODO: add test that shows table value concatenated when over 10 in length.
+
 export default {
   hasLabel: isPresent('[data-test-row-label]'),
   rowLabel: text('[data-test-row-label]'),
