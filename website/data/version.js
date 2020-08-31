2e60271edb2e1b1b012c82242f3dff99a91a5806
Meggie <meggie@hashicorp.com>
2020-08-31 13:15:37 -0400

    Update version.js

--- website/data/version.js
+++ website/data/version.js
@@ -1,3 +1,3 @@
-export const VERSION = '1.5.2'
+export const VERSION = '1.5.3'
 export const CHANGELOG_URL =
-  'https://github.com/hashicorp/vault/blob/master/CHANGELOG.md#152'
+  'https://github.com/hashicorp/vault/blob/master/CHANGELOG.md#153'
