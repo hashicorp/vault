32c3ba7e1716e9952604940004c534bf00f58962
Calvin Leung Huang <cleung2010@gmail.com>
2020-09-01 17:13:08 -0700

    tag oracle db as external, fix kerberos misspelling

--- website/layouts/docs.jsx
+++ website/layouts/docs.jsx
@@ -5,10 +5,11 @@ import { MDXProvider } from '@mdx-js/react'
 import Head from 'next/head'
 import Link from 'next/link'
 import Columns from 'components/columns'
+import Tag from 'components/inline-tag'
 import Tabs, { Tab } from 'components/tabs'
 import EnterpriseAlert from 'components/enterprise-alert'
 
-const DEFAULT_COMPONENTS = { Columns, Tabs, Tab, EnterpriseAlert }
+const DEFAULT_COMPONENTS = { Tabs, Tab, EnterpriseAlert, Columns, Tag }
 
 export default function DocsLayoutWrapper(pageMeta) {
   function DocsLayout(props) {
