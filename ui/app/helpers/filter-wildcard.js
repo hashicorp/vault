ed123ecc3397a22642a222ca69284a51d666b03a
Angel Garbarino <argarbarino@gmail.com>
2020-09-03 12:14:39 -0600

    filter-wildcard helper test

--- ui/app/helpers/filter-wildcard.js
+++ ui/app/helpers/filter-wildcard.js
@@ -1,6 +1,12 @@
 import { helper as buildHelper } from '@ember/component/helper';
 
 export function filterWildcard([string, array]) {
+  if (!string) {
+    return;
+  }
+  if (!string.id && string) {
+    string = { id: string };
+  }
   let stringId = string.id;
   const filterBy = stringId =>
     array.filter(item => new RegExp('^' + stringId.replace(/\*/g, '.*') + '$').test(item));
