034c8945a6681fd5288873d9136bcb3f96d4bb2f
Chelsea Shaw <chelshaw.dev@gmail.com>
2020-09-03 11:19:24 -0500

    Role edit extends TransformBase, roles list uses generic transform list item

--- ui/app/components/transform-role-edit.js
+++ ui/app/components/transform-role-edit.js
@@ -1,26 +1,3 @@
-/**
- * @module TransformRoleEdit
- * TransformRoleEdit components are used to...
- *
- * @example
- * ```js
- * <TransformRoleEdit @requiredParam={requiredParam} @optionalParam={optionalParam} @param1={{param1}}/>
- * ```
- * @param {object} requiredParam - requiredParam is...
- * @param {string} [optionalParam] - optionalParam is...
- * @param {string} [param1=defaultValue] - param1 is...
- */
+import TransformBase from './transform-edit-base';
 
-// import Component from '@ember/component';
-
-// export default Component.extend({
-// });
-
-import RoleEdit from './role-edit';
-
-export default RoleEdit.extend({
-  init() {
-    this._super(...arguments);
-    this.set('backendType', 'transform');
-  },
-});
+export default TransformBase.extend({});
