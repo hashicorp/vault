9dccad75f15ded5c32d80751ae6afb6298b22a11
Chelsea Shaw <chelshaw.dev@gmail.com>
2020-09-03 11:09:55 -0500

    Add test for creating a transform role

--- ui/tests/acceptance/enterprise-transform-test.js
+++ ui/tests/acceptance/enterprise-transform-test.js
@@ -1,12 +1,13 @@
 import { module, test } from 'qunit';
 import { setupApplicationTest } from 'ember-qunit';
-import { currentURL } from '@ember/test-helpers';
+import { currentURL, click } from '@ember/test-helpers';
 import { create } from 'ember-cli-page-object';
 import { selectChoose, clickTrigger } from 'ember-power-select/test-support/helpers';
 
 import authPage from 'vault/tests/pages/auth';
 import mountSecrets from 'vault/tests/pages/settings/mount-secret-backend';
 import transformationsPage from 'vault/tests/pages/secrets/backend/transform/transformations';
+import rolesPage from 'vault/tests/pages/secrets/backend/transform/roles';
 import searchSelect from 'vault/tests/pages/components/search-select';
 
 const searchSelectComponent = create(searchSelect);
@@ -41,11 +42,13 @@ module('Acceptance | Enterprise | Transform secrets', function(hooks) {
     assert.dom('[data-test-tab="Alphabets"]').exists('Has Alphabets tab');
   });
 
-  test('it can create a transformation', async function(assert) {
+  test('it can create a transformation and role', async function(assert) {
     let path = await mount();
+    const transformationName = 'foo';
+    const roleName = 'foo-role';
     await transformationsPage.createLink();
     assert.equal(currentURL(), `/vault/secrets/${path}/create`, 'redirects to create transformation page');
-    await transformationsPage.name('foo');
+    await transformationsPage.name(transformationName);
 
     assert.dom('[data-test-input="type"').hasValue('fpe', 'Has type FPE by default');
     assert.dom('[data-test-input="tweak_source"]').exists('Shows tweak source when FPE');
@@ -54,15 +57,34 @@ module('Acceptance | Enterprise | Transform secrets', function(hooks) {
       .dom('[data-test-input="masking_character"]')
       .exists('Shows masking character input when changed to masking type');
     assert.dom('[data-test-input="tweak_source"]').doesNotExist('Does not show tweak source when masking');
-
     await clickTrigger('#template');
     assert.equal(searchSelectComponent.options.length, 2, 'list shows two builtin options by default');
     await selectChoose('#template', '.ember-power-select-option', 0);
     await transformationsPage.submit();
     assert.equal(
       currentURL(),
-      `/vault/secrets/${path}/show/foo`,
+      `/vault/secrets/${path}/show/${transformationName}`,
       'redirects to show transformation page after submit'
+    );
+    await click(`[data-test-secret-breadcrumb="${path}"]`);
+    assert.equal(currentURL(), `/vault/secrets/${path}/list`, 'Links back to list view from breadcrumb');
+    await click('[data-test-tab="Roles"]');
+    assert.equal(currentURL(), `/vault/secrets/${path}/list?tab=role`, 'links to role list page');
+    await rolesPage.createLink();
+    assert.equal(
+      currentURL(),
+      `/vault/secrets/${path}/create?itemType=role`,
+      'redirects to create role page'
+    );
+    await rolesPage.name(roleName);
+    await clickTrigger('#transformations');
+    assert.equal(searchSelectComponent.options.length, 1, 'lists the transformation that was just created');
+    await selectChoose('#transformations', '.ember-power-select-option', 0);
+    await rolesPage.submit();
+    assert.equal(
+      currentURL(),
+      `/vault/secrets/${path}/show/role/${roleName}`,
+      'redirects to show role page after submit'
     );
   });
 });
