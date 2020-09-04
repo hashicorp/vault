bc7ce293167d8043e56b090f541169aa88196b7c
Mark Gritter <mgritter@hashicorp.com>
2020-09-04 17:33:53 -0500

    Define activity fragments and starter methods for manipulating them. (#1441)

--- vault/activity_log.go
+++ vault/activity_log.go
@@ -2,9 +2,13 @@ package vault
 
 import (
 	"context"
+	"os"
+	"sync"
+	"time"
 
 	log "github.com/hashicorp/go-hclog"
 	"github.com/hashicorp/vault/sdk/logical"
+	"github.com/hashicorp/vault/vault/activity"
 )
 
 const (
@@ -23,13 +27,29 @@ type ActivityLog struct {
 	// view is the storage location used by ActivityLog,
 	// defaults to sys/activity.
 	view logical.Storage
+
+	// nodeID is the ID to use for all fragments that
+	// are generated.
+	// TODO: use secondary ID when available?
+	nodeID string
+
+	// current log fragment (may be nil) and a mutex to protect it
+	fragmentLock     sync.RWMutex
+	fragment         *activity.LogFragment
+	fragmentCreation time.Time
 }
 
 // NewActivityLog creates an activity log.
 func NewActivityLog(_ context.Context, logger log.Logger, view logical.Storage) (*ActivityLog, error) {
+	hostname, err := os.Hostname()
+	if err != nil {
+		return nil, err
+	}
+
 	return &ActivityLog{
 		logger: logger,
 		view:   view,
+		nodeID: hostname,
 	}, nil
 }
 
@@ -45,4 +65,44 @@ func (c *Core) setupActivityLog(ctx context.Context) error {
 	}
 	c.activityLog = manager
 	return nil
+}
+
+func (a *ActivityLog) AddEntityToFragment(entityID string, namespaceID string, timestamp time.Time) {
+	a.fragmentLock.Lock()
+	defer a.fragmentLock.Unlock()
+
+	// TODO: check whether entity ID already recorded
+
+	a.createCurrentFragment()
+
+	a.fragment.Entities = append(a.fragment.Entities,
+		&activity.EntityRecord{
+			EntityID:    entityID,
+			NamespaceID: namespaceID,
+			Timestamp:   timestamp.UnixNano(),
+		})
+}
+
+func (a *ActivityLog) AddTokenToFragment(namespaceID string) {
+	a.fragmentLock.Lock()
+	defer a.fragmentLock.Unlock()
+
+	a.createCurrentFragment()
+
+	a.fragment.NonEntityTokens[namespaceID] += 1
+}
+
+// Create the current fragment if it doesn't already exist.
+// Must be called with the lock held.
+func (a *ActivityLog) createCurrentFragment() {
+	if a.fragment == nil {
+		a.fragment = &activity.LogFragment{
+			OriginatingNode: a.nodeID,
+			Entities:        make([]*activity.EntityRecord, 0, 120),
+			NonEntityTokens: make(map[string]uint64),
+		}
+		a.fragmentCreation = time.Now()
+
+		// TODO: start a timer to send it, if we're a performance standby
+	}
 }
