c76d1354d93c7f353cb565168e8c8bcb4a510aba
Chelsea Shaw <chelshaw.dev@gmail.com>
2020-09-08 13:38:26 -0500

    Update transformation after role transformations changed works

--- ui/app/components/transform-role-edit.js
+++ ui/app/components/transform-role-edit.js
@@ -1,6 +1,5 @@
 import TransformBase from './transform-edit-base';
 import { inject as service } from '@ember/service';
-// import transform from '../adapters/transform';
 
 export default TransformBase.extend({
   store: service(),
@@ -8,18 +7,13 @@ export default TransformBase.extend({
 
   init() {
     this._super(...arguments);
-    // CBS TODO: set initialTransformations for compare on createOrUpdate
     this.set('initialTransformations', this.get('model.transformations'));
   },
 
   handleAddedTransformations(added, roleId) {
     if (!added) return;
-
-    // get just one for now
-    // const testTransform = added[0];
-    // console.log({ testTransform });
     const backend = this.get('model.backend');
-    // console.log(this.get('model'));
+
     // For each added transformation, add current role id to allowed_roles
     added.forEach(transformName => {
       this.store
@@ -28,36 +22,23 @@ export default TransformBase.extend({
           id: transformName,
         })
         .then(function(transformation) {
-          transformation.set('backend', backend);
-          console.log('get successful', transformation);
           let roles = transformation.allowed_roles;
           roles.push(roleId);
-          transformation.set('allowed_roles', roles);
-          // transformation.allowed_roles = [roleId];
+          transformation.setProperties({
+            backend,
+            allowed_roles: roles.uniq(),
+          });
           transformation.save();
         });
+      // TODO: Handle errors if no read or write access
     });
-    // this.store
-    //   .queryRecord('transform', {
-    //     backend,
-    //     id: testTransform,
-    //   })
-    //   .then(function(transformation) {
-    //     transformation.set('backend', backend);
-    //     console.log('get successful', transformation);
-    //     let roles = transformation.allowed_roles;
-    //     roles.push(roleId);
-    //     transformation.set('allowed_roles', roles);
-    //     // transformation.allowed_roles = [roleId];
-    //     transformation.save();
-    //   });
   },
 
-  handleRemovedTransformations(removed) {
+  handleRemovedTransformations(removed, roleId) {
     if (!removed) return;
     const backend = this.get('model.backend');
 
-    // For each added transformation, remove current role id from allowed_roles
+    // For each removed transformation, remove current role id from allowed_roles
     removed.forEach(transformName => {
       this.store
         .queryRecord('transform', {
@@ -65,14 +46,17 @@ export default TransformBase.extend({
           id: transformName,
         })
         .then(function(transformation) {
-          transformation.set('backend', backend);
-          console.log('get successful', transformation);
           let roles = transformation.allowed_roles;
-          roles.push(roleId);
-          transformation.set('allowed_roles', roles);
-          // transformation.allowed_roles = [roleId];
+          const index = roles.indexOf(roleId);
+          if (index < 0) return;
+          roles.removeAt(index);
+          transformation.setProperties({
+            backend,
+            allowed_roles: roles.uniq(),
+          });
           transformation.save();
         });
+      // TODO: Handle errors if no read or write access
     });
   },
 
@@ -81,7 +65,7 @@ export default TransformBase.extend({
       event.preventDefault();
 
       this.applyChanges('save', () => {
-        console.log('-> -> -> This is where we compare initialTransformations to appliedTransformations');
+        const roleId = this.get('model.id');
         const newModelTransformations = this.get('model.transformations');
         if (!this.initialTransformations) {
           this.handleAddedTransformations(newModelTransformations);
@@ -93,9 +77,8 @@ export default TransformBase.extend({
         const removedTransformations = this.initialTransformations.filter(
           t => newModelTransformations.indexOf(t) < 0
         );
-        console.log(this.initialTransformations, this.get('model.transformations'));
-        this.handleAddedTransformations(addedTransformations, this.get('model.id'));
-        this.handleRemovedTransformations(removedTransformations);
+        this.handleAddedTransformations(addedTransformations, roleId);
+        this.handleRemovedTransformations(removedTransformations, roleId);
       });
     },
   },
