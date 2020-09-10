97244b2dc85f509e671e194bae3217e5126c2a1f
Chelsea Shaw <chelshaw.dev@gmail.com>
2020-09-10 13:23:49 -0500

    Add role to allowed_roles on added transformations and remove from removed transformations on role save, with flash message

--- ui/app/components/transform-role-edit.js
+++ ui/app/components/transform-role-edit.js
@@ -1,8 +1,24 @@
 import TransformBase from './transform-edit-base';
 import { inject as service } from '@ember/service';
 
+const addToList = (list, itemToAdd) => {
+  if (!list || !Array.isArray(list)) return list;
+  list.push(itemToAdd);
+  return list.uniq();
+};
+
+const removeFromList = (list, itemToRemove) => {
+  if (!list) return list;
+  const index = list.indexOf(itemToRemove);
+  if (index < 0) return list;
+  const newList = list.removeAt(index, 1);
+  return newList.uniq();
+};
+
 export default TransformBase.extend({
   store: service(),
+  flashMessages: service(),
+
   initialTransformations: null,
 
   init() {
@@ -10,53 +26,64 @@ export default TransformBase.extend({
     this.set('initialTransformations', this.get('model.transformations'));
   },
 
-  handleAddedTransformations(added, roleId) {
-    if (!added) return;
+  handleUpdateTransformations(updateTransformations, roleId, type = 'update') {
+    if (!updateTransformations) return;
     const backend = this.get('model.backend');
-
-    // For each added transformation, add current role id to allowed_roles
-    added.forEach(transformName => {
-      this.store
+    const promises = updateTransformations.map(transform => {
+      return this.store
         .queryRecord('transform', {
           backend,
-          id: transformName,
+          id: transform.id,
         })
         .then(function(transformation) {
           let roles = transformation.allowed_roles;
-          roles.push(roleId);
+          if (transform.action === 'ADD') {
+            roles = addToList(roles, roleId);
+          } else if (transform.action === 'REMOVE') {
+            roles = removeFromList(roles, roleId);
+          }
+
           transformation.setProperties({
             backend,
-            allowed_roles: roles.uniq(),
+            allowed_roles: roles,
           });
-          transformation.save();
+
+          return transformation
+            .save()
+            .then(() => {
+              return 'Successfully saved';
+            })
+            .catch(e => {
+              return { errorStatus: e.httpStatus, ...transform };
+            });
         });
-      // TODO: Handle errors if no read or write access
     });
-  },
 
-  handleRemovedTransformations(removed, roleId) {
-    if (!removed) return;
-    const backend = this.get('model.backend');
+    Promise.all(promises).then(res => {
+      let hasError = res.find(r => !!r.errorStatus);
 
-    // For each removed transformation, remove current role id from allowed_roles
-    removed.forEach(transformName => {
-      this.store
-        .queryRecord('transform', {
-          backend,
-          id: transformName,
-        })
-        .then(function(transformation) {
-          let roles = transformation.allowed_roles;
-          const index = roles.indexOf(roleId);
-          if (index < 0) return;
-          roles.removeAt(index);
-          transformation.setProperties({
-            backend,
-            allowed_roles: roles.uniq(),
-          });
-          transformation.save();
-        });
-      // TODO: Handle errors if no read or write access
+      if (hasError) {
+        let errorAdding = res.find(r => r.errorStatus === 403 && r.type && r.type === 'ADD');
+        let errorRemoving = res.find(r => r.errorStatus === 403 && r.type && r.type === 'REMOVE');
+
+        let message =
+          'The edits to this role were successful, but allowed_roles for its transformations was not edited due to a lack of permissions.';
+        if (type === 'create') {
+          message =
+            'Transformations have been attached to this role, but the role was not added to those transformations’ allowed_roles due to a lack of permissions.';
+        }
+        if (errorAdding && errorRemoving) {
+          message =
+            'This role was edited to both add and remove transformations; however, this role was not added or removed from those transformations’ allowed_roles due to a lack of permissions.';
+        } else if (errorAdding) {
+          message =
+            'This role was edited to include new transformations, but this role was not added to those transformations’ allowed_roles due to a lack of permissions.';
+        } else if (errorRemoving) {
+          message =
+            'This role was edited to remove transformations, but this role was not removed from those transformations’ allowed_roles due to a lack of permissions.';
+        }
+        this.get('flashMessages').stickyInfo(message);
+      }
     });
   },
 
@@ -67,18 +94,37 @@ export default TransformBase.extend({
       this.applyChanges('save', () => {
         const roleId = this.get('model.id');
         const newModelTransformations = this.get('model.transformations');
+
         if (!this.initialTransformations) {
-          this.handleAddedTransformations(newModelTransformations);
+          this.handleUpdatedTransformations(
+            newModelTransformations.map(t => ({
+              id: t,
+              action: 'ADD',
+            })),
+            roleId,
+            type
+          );
           return;
         }
-        const addedTransformations = newModelTransformations.filter(
-          t => this.initialTransformations.indexOf(t) < 0
-        );
-        const removedTransformations = this.initialTransformations.filter(
-          t => newModelTransformations.indexOf(t) < 0
-        );
-        this.handleAddedTransformations(addedTransformations, roleId);
-        this.handleRemovedTransformations(removedTransformations, roleId);
+
+        const updateTransformations = [...newModelTransformations, ...this.initialTransformations]
+          .map(t => {
+            if (this.initialTransformations.indexOf(t) < 0) {
+              return {
+                id: t,
+                action: 'ADD',
+              };
+            }
+            if (newModelTransformations.indexOf(t) < 0) {
+              return {
+                id: t,
+                action: 'REMOVE',
+              };
+            }
+            return null;
+          })
+          .filter(t => !!t);
+        this.handleUpdateTransformations(updateTransformations, roleId);
       });
     },
   },
