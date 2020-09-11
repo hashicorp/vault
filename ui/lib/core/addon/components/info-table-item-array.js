423502d98af70d70e7fb93821f43266c8b3b269c
Angel Garbarino <argarbarino@gmail.com>
2020-09-11 12:17:51 -0600

    setup the logic and more logic

--- ui/lib/core/addon/components/info-table-item-array.js
+++ ui/lib/core/addon/components/info-table-item-array.js
@@ -3,41 +3,61 @@ import Component from '@ember/component';
 import { inject as service } from '@ember/service';
 import { task } from 'ember-concurrency';
 import layout from '../templates/components/info-table-item-array';
+import { isWildcardString } from 'vault/helpers/is-wildcard-string';
 
 /**
  * @module InfoTableItemArray
- * `InfoTableItemArray` //
+ * The `InfoTableItemArray` component handles arrays in the info-table-row component.
+ * If an array has more than 10 items, then only 5 are displayed and a count of total items is displayed next to the five.
+ * If a isLink is true than a link can be set for the use to click on the specific array item
+ * If a wildcard is a potential variable in the string of an item, then you can use the modelType and wildcardLabel parameters to
+ * return a wildcard count similar to what is done in the searchSelect component.
  *
  * @example
  * ```js
- * <InfoTableItemArray @value={{['test-1','test-2','test-3']}} @isLink={{true}} @modelType="transform/role"/>
+ * <InfoTableItemArray @displayArray={{['test-1','test-2','test-3']}} @isLink={{true}} @modelType="transform/role"/ @queryParam="role/">
  * ```
  *
- * @param value=null {array} - This array of data to be displayed.  If there are more than 10 items in the array only five will show and a count of the other number in the array will show.
+ * @param displayArray=null {array} - This array of data to be displayed.  If there are more than 10 items in the array only five will show and a count of the other number in the array will show.
  * @param [isLink=true] {Boolean} - Indicates if the item should contain a link-to component.  Only setup for arrays, but this could be changed if needed.
  * @param [modelType=null] {string} - Tells what model you want data for the allOptions to be returned from.  Used in conjunction with the the isLink.
+ * @param [wildcardLabel] {String} - when you want the component to return a count on the model for options returned when using a wildcard you must provide a label of the count e.g. role.  Should be singular.
+ * @param [queryParam] {String} - If you want to specific a tab for the View All XX to display to.  Ex: role/
  */
 export default Component.extend({
   layout,
   allOptions: null,
-  value: null,
+  displayArray: null,
+  wildcardInDisplayArray: false,
   store: service(),
+  displayArrayAmended: computed('displayArray', function() {
+    let { displayArray } = this;
+    // cut down array to length 5 if more than 10 items
+    if (displayArray.length >= 10) {
+      // if array greater than 10 in length only display the first 5
+      displayArray = displayArray.slice(0, 5);
+    }
+
+    return displayArray;
+  }),
+
+  checkWildcardInArray: task(function*() {
+    let filteredArray = yield this.displayArray.filter(item => isWildcardString(item));
+    this.set('wildcardInDisplayArray', filteredArray.length > 0 ? true : false);
+  }).on('didInsertElement'),
+
   fetchOptions: task(function*() {
     if (this.isLink && this.modelType) {
-      try {
-        let queryOptions = {};
-        let backendModel = yield this.store.peekAll('secret-engine');
-        let array = backendModel.toArray().map(option => {
-          return option.id;
-        });
-        if (array) {
-          queryOptions = { backend: array.get('firstObject') };
-        }
-        let options = yield this.store.query(this.modelType, queryOptions);
-        this.formatOptions(options);
-      } catch (err) {
-        throw err;
+      let queryOptions = {};
+      let backendModel = yield this.store.peekAll('secret-engine');
+      let array = backendModel.toArray().map(option => {
+        return option.id;
+      });
+      if (array) {
+        queryOptions = { backend: array.get('firstObject') };
       }
+      let options = yield this.store.query(this.modelType, queryOptions);
+      this.formatOptions(options);
     }
   }).on('didInsertElement'),
 
