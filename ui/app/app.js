cc2d71cbabe59dc67201345b0a959d26ac0d3af1
Angel Garbarino <argarbarino@gmail.com>
2020-09-17 11:52:06 -0600

    add back engine dependencies, automatically removed during ember-cli-upgrade

--- ui/app/app.js
+++ ui/app/app.js
@@ -7,6 +7,48 @@ export default class App extends Application {
   modulePrefix = config.modulePrefix;
   podModulePrefix = config.podModulePrefix;
   Resolver = Resolver;
+  engines = {
+    openApiExplorer: {
+      dependencies: {
+        services: ['auth', 'flash-messages', 'namespace', 'router', 'version'],
+      },
+    },
+    replication: {
+      dependencies: {
+        services: [
+          'auth',
+          'flash-messages',
+          'namespace',
+          'replication-mode',
+          'router',
+          'store',
+          'version',
+          'wizard',
+        ],
+        externalRoutes: {
+          replication: 'vault.cluster.replication.index',
+        },
+      },
+    },
+    kmip: {
+      dependencies: {
+        services: [
+          'auth',
+          'flash-messages',
+          'namespace',
+          'path-help',
+          'router',
+          'store',
+          'version',
+          'wizard',
+          'secret-mount-path',
+        ],
+        externalRoutes: {
+          secrets: 'vault.cluster.secrets.backends',
+        },
+      },
+    },
+  };
 }
 
 loadInitializers(App, config.modulePrefix);
