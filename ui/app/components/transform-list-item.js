6113efa930986b49770d508c0b543bf5b972d305
Chelsea Shaw <chelshaw.dev@gmail.com>
2020-09-17 09:30:20 -0500

    Add tests for transform-list-item component and cleanup

--- ui/app/components/transform-list-item.js
+++ ui/app/components/transform-list-item.js
@@ -1,10 +1,11 @@
 /**
  * @module TransformListItem
- * TransformListItem components are used to...
+ * TransformListItem components are used for the list items for the Transform Secret Engines for all but Transformations.
+ * This component automatically handles read-only list items if capabilities are not granted or the item is internal only.
  *
  * @example
  * ```js
- * <TransformListItem @requiredParam={requiredParam} @optionalParam={optionalParam} @param1={{param1}}/>
+ * <TransformListItem @item={item} @itemPath="role/my-item" @itemType="role" />
  * ```
  * @param {object} item - item refers to the model item used on the list item partial
  * @param {string} itemPath - usually the id of the item, but can be prefixed with the model type (see transform/role)
