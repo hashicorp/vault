165a08e5e7a9e827bce2eeb4c036393a644ad5af
Aleksandr Bezobchuk <aleks.bezobchuk@gmail.com>
2020-09-21 21:35:18 -0400

    lint++

--- vault/quotas/quotas_rate_limit.go
+++ vault/quotas/quotas_rate_limit.go
@@ -11,14 +11,10 @@ import (
 	log "github.com/hashicorp/go-hclog"
 	"github.com/hashicorp/go-uuid"
 	"github.com/hashicorp/vault/helper/metricsutil"
-
-	// "github.com/hashicorp/vault/sdk/helper/pathmanager"
 	"github.com/sethvargo/go-limiter"
 	"github.com/sethvargo/go-limiter/httplimit"
 	"github.com/sethvargo/go-limiter/memorystore"
 )
-
-// var rateLimitExemptPaths = pathmanager.New()
 
 const (
 	// DefaultRateLimitPurgeInterval defines the default purge interval used by a
@@ -32,19 +28,6 @@ const (
 	// requests that get rejected due to rate limit quota violations.
 	EnvVaultEnableRateLimitAuditLogging = "VAULT_ENABLE_RATE_LIMIT_AUDIT_LOGGING"
 )
-
-// func init() {
-// 	rateLimitExemptPaths.AddPaths([]string{
-// 		"sys/internal/ui/mounts",
-// 		"sys/generate-recovery-token/attempt",
-// 		"sys/generate-recovery-token/update",
-// 		"sys/generate-root/attempt",
-// 		"sys/generate-root/update",
-// 		"sys/health",
-// 		"sys/seal-status",
-// 		"sys/unseal",
-// 	})
-// }
 
 // Ensure that RateLimitQuota implements the Quota interface
 var _ Quota = (*RateLimitQuota)(nil)
