004e912271c9295c34beec9e11eef8c52fda0780
Meggie Ladlow <meggie@hashicorp.com>
2020-09-22 17:38:16 -0400

    Updating SDK version_base for 1.5.4

--- sdk/version/version_base.go
+++ sdk/version/version_base.go
@@ -8,7 +8,7 @@ var (
 	// Whether cgo is enabled or not; set at build time
 	CgoEnabled bool
 
-	Version           = "1.5.0"
+	Version           = "1.5.4"
 	VersionPrerelease = ""
 	VersionMetadata   = ""
 )
