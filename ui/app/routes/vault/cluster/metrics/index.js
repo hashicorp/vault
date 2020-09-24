15aec842f4f4b71e2b629e169ee904764ddbb95d
Chelsea Shaw <chelshaw.dev@gmail.com>
2020-09-24 17:01:35 -0500

    Query activity and return fake data on adapterError

--- ui/app/routes/vault/cluster/metrics/index.js
+++ ui/app/routes/vault/cluster/metrics/index.js
@@ -3,6 +3,17 @@ import ClusterRoute from 'vault/mixins/cluster-route';
 import { hash } from 'rsvp';
 
 export default Route.extend(ClusterRoute, {
+  /* Asynchronous example
+  model: function(params) {
+    var reviewPromise = this.store.findRecord('review', params.id);
+    return Ember.RSVP.hash({
+      review: reviewPromise,
+      user: reviewPromise.then(review => {
+        return this.store.findRecord('user', review.get('userId'));
+      })
+    });
+  }
+  */
   model() {
     let totalEntities = this.store.queryRecord('metrics/entity', {}).then(response => {
       return response.entities.total;
@@ -17,7 +28,22 @@ export default Route.extend(ClusterRoute, {
       return response.service_tokens.total;
     });
 
+    let activity = this.store
+      .queryRecord('metrics/activity', {})
+      .then(response => {
+        console.log({ response });
+        return response.data;
+      })
+      .catch(e => {
+        return {
+          active_clients: 1000,
+          unique_entities: 635,
+          direct_tokens: 465,
+        };
+      });
+
     return hash({
+      activity,
       totalEntities,
       httpsRequests,
       totalTokens,
