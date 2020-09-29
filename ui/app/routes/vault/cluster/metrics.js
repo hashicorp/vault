8fb10d703d57e701190f11104f6073d28698abb4
Chelsea Shaw <chelshaw.dev@gmail.com>
2020-09-29 13:28:33 -0500

    start and end params on metrics route are passed to activity api call

--- ui/app/routes/vault/cluster/metrics.js
+++ ui/app/routes/vault/cluster/metrics.js
@@ -1,6 +1,33 @@
 import Route from '@ember/routing/route';
 import ClusterRoute from 'vault/mixins/cluster-route';
 import { hash } from 'rsvp';
+import { endOfMonth } from 'date-fns';
+
+const getActivityParams = ({ start, end }) => {
+  // TODO: minStart, maxEnd
+  let params = {};
+  if (start) {
+    let datePieces = start.split('/');
+    if (datePieces.length > 1) {
+      let startDate = new Date(Date.UTC(datePieces[1], datePieces[0] - 1, 1));
+      if (isValid(startDate)) {
+        // TODO: Replace with formatRFC3339
+        params.start_time = startDate.getTime() / 1000;
+      }
+    }
+  }
+  if (end) {
+    let datePieces = end.split('/');
+    if (datePieces.length > 1) {
+      let endDate = new Date(Date.UTC(datePieces[1], datePieces[0], 1));
+      if (isValid(endDate)) {
+        // TODO: Replace with formatRFC3339
+        params.end_time = endOfMonth(endDate).getTime() / 1000;
+      }
+    }
+  }
+  return params;
+};
 
 export default Route.extend(ClusterRoute, {
   queryParams: {
@@ -14,8 +41,8 @@ export default Route.extend(ClusterRoute, {
 
   model(params) {
     let config = this.store.queryRecord('metrics/config', {});
-
-    let activity = this.store.queryRecord('metrics/activity', params);
+    const activityParams = getActivityParams(params);
+    let activity = this.store.queryRecord('metrics/activity', activityParams);
 
     return hash({
       activity,
