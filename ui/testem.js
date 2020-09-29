f6b176ac9b275acce06aef8a8b7255400ac4a676
Angel Garbarino <argarbarino@gmail.com>
2020-09-29 09:43:47 -0600

    fix failing test with ui-wizard issue

--- ui/testem.js
+++ ui/testem.js
@@ -6,7 +6,6 @@ module.exports = {
   tap_failed_tests_only: true,
   disable_watching: true,
   launch_in_ci: ['Chrome'],
-  launch_in_dev: ['Chrome'],
   browser_start_timeout: 120,
   browser_args: {
     Chrome: {
