2565315e6d25637dc22567390bd93071fe09860d
Chelsea Shaw <chelshaw.dev@gmail.com>
2020-10-06 12:43:38 -0500

    Expand metrics config model attrs for form fields

--- ui/app/models/metrics/config.js
+++ ui/app/models/metrics/config.js
@@ -1,10 +1,32 @@
 import DS from 'ember-data';
+import { computed } from '@ember/object';
+// import { apiPath } from 'vault/macros/lazy-capabilities';
+// import attachCapabilities from 'vault/lib/attach-capabilities';
+import { expandAttributeMeta } from 'vault/utils/field-to-attrs';
 
 const { attr } = DS;
 
 export default DS.Model.extend({
   queriesAvailable: attr('boolean'),
-  defaultReportMonths: attr('number'),
-  retentionMonths: attr('number'),
-  enabled: attr('string'),
+  defaultReportMonths: attr('number', {
+    label: 'Default display',
+    subText: 'The number of months we’ll display in the Vault usage dashboard by default.',
+  }),
+  retentionMonths: attr('number', {
+    label: 'Retention period',
+    subText: 'The number of months of activity logs to maintain for client tracking.',
+  }),
+  enabled: attr('string', {
+    editType: 'boolean',
+    trueValue: 'On',
+    falseValue: 'Off',
+    label: 'Enable usage data collection',
+    helpText:
+      'Enable or disable client tracking. Keep in mind that disabling tracking will delete the data for the current month.',
+  }),
+
+  configAttrs: computed(function() {
+    let keys = ['enabled', 'defaultReportMonths', 'retentionMonths'];
+    return expandAttributeMeta(this, keys);
+  }),
 });
