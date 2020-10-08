991ea0002bc59baae3960455195c4bf8f7f2727b
Chelsea Shaw <chelshaw.dev@gmail.com>
2020-10-08 10:23:59 -0500

    Ui/pricing metric config (#10102)
    * Fix routing on metrics
    
    * Show current metrics config
    
    * Ability to edit metrics config

--- ui/app/serializers/metrics/config.js
+++ ui/app/serializers/metrics/config.js
@@ -2,6 +2,10 @@ import ApplicationSerializer from '../application';
 
 export default ApplicationSerializer.extend({
   normalizeResponse(store, primaryModelClass, payload, id, requestType) {
+    if (!payload.data) {
+      // CBS TODO: Remove this if block once API is published
+      return this._super(store, primaryModelClass, payload, id, requestType);
+    }
     const normalizedPayload = {
       id: payload.id,
       data: {
@@ -10,5 +14,20 @@ export default ApplicationSerializer.extend({
       },
     };
     return this._super(store, primaryModelClass, normalizedPayload, id, requestType);
+  },
+
+  serialize() {
+    let json = this._super(...arguments);
+    if (json.enabled === 'On' || json.enabled === 'Off') {
+      const oldEnabled = json.enabled;
+      json.enabled = oldEnabled === 'On' ? 'enabled' : 'disabled';
+    }
+    json.default_report_months = parseInt(json.default_report_months, 10);
+    json.retention_months = parseInt(json.retention_months, 10);
+    if (isNaN(json.default_report_months) || isNaN(json.retention_months)) {
+      throw new Error('Invalid number value');
+    }
+    delete json.queries_available;
+    return json;
   },
 });
