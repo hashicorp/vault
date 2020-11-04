1bc24acd38533dd8731d7af5970ef40ce17abf3f
Meggie Ladlow <meggie@hashicorp.com>
2020-11-04 17:51:42 -0500

    Updating website version

--- website/data/version.js
+++ website/data/version.js
@@ -1,3 +1,3 @@
-export const VERSION = '1.5.5'
+export const VERSION = '1.6.0-rc'
 export const CHANGELOG_URL =
-  'https://github.com/hashicorp/vault/blob/master/CHANGELOG.md#155'
+  'https://github.com/hashicorp/vault/blob/master/CHANGELOG.md#160-rc'
