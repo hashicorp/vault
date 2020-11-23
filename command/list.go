60f44132a1ed45db1fae8988a4304071c4948190
Michel Vocks <michel@hashicorp.com>
2020-11-23 20:38:14 +0100

    Fix KV list command with whitespaces (#8017)
    * Fix KV list command with whitespaces
    
    * Fix kv list whitespace
    
    * Fix list whitespace
    
    * Fix failing test
    
    Co-authored-by: swayne275 <swayne@hashicorp.com>

--- command/list.go
+++ command/list.go
@@ -75,8 +75,13 @@ func (c *ListCommand) Run(args []string) int {
 		return 2
 	}
 
-	path := ensureTrailingSlash(sanitizePath(args[0]))
+	// Append trailing slash
+	path := args[0]
+	if !strings.HasSuffix(path , "/") {
+		path += "/"
+	}
 
+	path = sanitizePath(path)
 	secret, err := client.Logical().List(path)
 	if err != nil {
 		c.UI.Error(fmt.Sprintf("Error listing %s: %s", path, err))
