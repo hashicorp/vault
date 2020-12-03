d71178dd1ba7dc074048f65065b2faa1ba836691
Angel Garbarino <argarbarino@gmail.com>
2020-12-03 14:41:39 -0700

    small cleanup based on chelseas comments.

--- ui/app/components/flash-message.js
+++ ui/app/components/flash-message.js
@@ -6,7 +6,7 @@ export default FlashMessage.extend({
   //https://github.com/poteto/ember-cli-flash/blob/master/addon/components/flash-message.js#L35
   alertType: computed('flash.type', {
     get() {
-      const flashType = this.flash.type === undefined ? '' : this.flash.type;
+      const flashType = this.flash.type || '';
       let prefix = 'is-';
 
       return `${prefix}${flashType}`;
