5799790292fd6f65b08b63df8e8f0981c29c8d15
Scott Miller <smiller@hashicorp.com>
2020-12-14 16:50:47 -0600

    SDK update, 1.5.x. (#10571)

--- vendor/github.com/hashicorp/vault/sdk/version/version_base.go
+++ vendor/github.com/hashicorp/vault/sdk/version/version_base.go
@@ -8,7 +8,7 @@ var (
 	// Whether cgo is enabled or not; set at build time
 	CgoEnabled bool
 
-	Version           = "1.5.5"
+	Version           = "1.5.6"
 	VersionPrerelease = ""
 	VersionMetadata   = ""
 )
