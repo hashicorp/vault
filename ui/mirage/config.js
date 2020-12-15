3448f0b31a1836bf6fbb623a0458848f948504ed
Chelsea Shaw <chelshaw.dev@gmail.com>
2020-12-15 11:37:13 -0600

    Update mirage with vault-config mock route

--- ui/mirage/config.js
+++ ui/mirage/config.js
@@ -1,4 +1,23 @@
+import Response from 'ember-cli-mirage/response';
+
 export default function() {
+  this.get(
+    'http://localhost:4200/vault-config',
+    function() {
+      console.log('getting vault config');
+      return new Response(
+        201,
+        {
+          'Content-Type': 'application/json',
+        },
+        {
+          managedNamespaceRoot: 'admin',
+        }
+      );
+    },
+    { timing: 3000 }
+  );
+
   this.namespace = 'v1';
 
   this.get('sys/internal/counters/activity', function(db) {
@@ -19,5 +38,6 @@ export default function() {
       data: db['metrics/configs'].first(),
     };
   });
+
   this.passthrough();
 }
