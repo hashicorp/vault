54b28ae774031c32001db151dbd56eac37379d42
Angel Garbarino <Monkeychip@users.noreply.github.com>
2021-01-04 16:30:49 -0700

    Bug: Fix issue with double encoding on space in secret history route (#10596) (#10642)
    * setup for concept it works, but probably not the best solution
    
    * add comment and remove console and test var
    
    * use normalize path higher up to fix issu
    
    * add test for bug that fixing
    
    * forgot a couple of changes
    
    * changelog

--- ui/tests/acceptance/secrets/backend/kv/secret-test.js
+++ ui/tests/acceptance/secrets/backend/kv/secret-test.js
@@ -1,4 +1,4 @@
-import { visit, settled, currentURL, currentRouteName } from '@ember/test-helpers';
+import { click, visit, settled, currentURL, currentRouteName } from '@ember/test-helpers';
 import { create } from 'ember-cli-page-object';
 import { module, test } from 'qunit';
 import { setupApplicationTest } from 'ember-qunit';
@@ -285,6 +285,34 @@ module('Acceptance | secrets/secret/create', function(hooks) {
         `${path}: show page renders correctly`
       );
     }
+  });
+
+  test('create secret with space shows version data', async function(assert) {
+    let enginePath = `kv-${new Date().getTime()}`;
+    let secretPath = 'space space';
+    // mount version 2
+    await mountSecrets.visit();
+    await mountSecrets.selectType('kv');
+    await mountSecrets
+      .next()
+      .path(enginePath)
+      .submit();
+    await settled();
+    await listPage.create();
+    await editPage.createSecret(secretPath, 'foo', 'bar');
+    await settled();
+    await click('[data-test-popup-menu-trigger="history"]');
+    await settled();
+    await click('[data-test-version-history]');
+    await settled();
+    assert.dom('[data-test-list-item-content]').exists('renders the version and not an error state');
+    // click on version
+    await click('[data-test-popup-menu-trigger="true"]');
+    await click('[data-test-version]');
+    await settled();
+    // perform encode function that should be done by the encodePath
+    let encodedSecretPath = secretPath.replace(/ /g, '%20');
+    assert.equal(currentURL(), `/vault/secrets/${enginePath}/show/${encodedSecretPath}?version=1`);
   });
 
   // the web cli does not handle a quote as part of a path, so we test it here via the UI
