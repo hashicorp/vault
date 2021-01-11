605d9c319f07a5eb16d251829affd45dd48c5dd4
Mike Wickett <mike@wickett.ca>
2021-01-11 09:37:41 -0500

    website: add alert banner to promote webinar

---
+++ website/data/alert-banner.js
@@ -0,0 +1,13 @@
+export const ALERT_BANNER_ACTIVE = true
+
+// https://github.com/hashicorp/react-components/tree/master/packages/alert-banner
+export default {
+  tag: 'Webinar',
+  url:
+    'https://www.hashicorp.com/events/webinars/cloud-service-networking-secrets-management',
+  text: 'Getting Started with Service Networking & Secrets Management on Cloud',
+  linkText: 'Register Now',
+  // Set the `expirationDate prop with a datetime string (e.g. `2020-01-31T12:00:00-07:00`)
+  // if you'd like the component to stop showing at or after a certain date
+  expirationDate: '2021-02-09T11:59:00-05:00',
+}
