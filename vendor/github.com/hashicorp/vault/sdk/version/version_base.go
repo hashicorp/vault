9ce48bd156d5e571a0dda525e852146fc9a35a20
Josh Black <raskchanky@gmail.com>
2021-01-20 12:20:51 -0800

    Update SDK and vendor the changes

--- vendor/github.com/hashicorp/vault/sdk/version/version_base.go
+++ vendor/github.com/hashicorp/vault/sdk/version/version_base.go
@@ -8,7 +8,7 @@ var (
 	// Whether cgo is enabled or not; set at build time
 	CgoEnabled bool
 
-	Version           = "1.6.1"
+	Version           = "1.6.2"
 	VersionPrerelease = ""
 	VersionMetadata   = ""
 )
