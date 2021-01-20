001a457012e31c9b1764874eee1591e2740e9c82
Angel Garbarino <argarbarino@gmail.com>
2021-01-20 14:12:50 -0700

    creds request based on dynamic or static and unload the store by record creds when they transition away.

--- ui/app/adapters/database/credential.js
+++ ui/app/adapters/database/credential.js
@@ -4,8 +4,9 @@ export default ApplicationAdapter.extend({
   namespace: 'v1',
 
   fetchByQuery(store, query) {
-    const { backend, secret } = query;
-    return this.ajax(`${this.buildURL()}/${backend}/creds/${secret}`, 'GET').then(resp => {
+    const { backend, roleType, secret } = query;
+    let creds = roleType === 'static' ? 'static-creds' : 'creds';
+    return this.ajax(`${this.buildURL()}/${backend}/${creds}/${secret}`, 'GET').then(resp => {
       return resp;
     });
   },
--- ui/app/models/database/role.js
+++ ui/app/models/database/role.js
@@ -1,10 +1,12 @@
 import Model, { attr } from '@ember-data/model';
 import { alias } from '@ember/object/computed';
 import lazyCapabilities, { apiPath } from 'vault/macros/lazy-capabilities';
-
+// ARG TODO confirm that canRead is the correct policy path
 export default Model.extend({
   backend: attr('string', { readOnly: true }),
   secretPath: lazyCapabilities(apiPath`${'backend'}/roles/${'id'}`, 'backend', 'id'),
   canRead: alias('secretPath.canRead'),
   canEdit: alias('secretPath.canUpdate'),
+  credentialPath: lazyCapabilities(apiPath`${'backend'}/creds/${'id'}`, 'backend', 'id'),
+  canGenerateCredentials: alias('credentialPath.canRead'),
 });
