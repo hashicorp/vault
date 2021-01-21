709d15bce16265b6a523327ca089f79ebba9b332
Angel Garbarino <argarbarino@gmail.com>
2021-01-21 14:05:01 -0700

    setup async for handling permission errors on overivew

--- ui/app/components/get-credentials-card.js
+++ ui/app/components/get-credentials-card.js
@@ -1,7 +1,3 @@
-import Component from '@glimmer/component';
-import { inject as service } from '@ember/service';
-import { action } from '@ember/object';
-import { tracked } from '@glimmer/tracking';
 /**
  * @module GetCredentialsCard
  * GetCredentialsCard components are card-like components that display a title, and SearchSelect component that sends you to a credentials route for the selected item.
@@ -15,6 +11,12 @@ import { tracked } from '@glimmer/tracking';
  * @param searchLabel=null {String} - The text above the searchSelect component
  * @param models=null {Array} - An array of model types to fetch from the API.  Passed through to SearchSelect component
  */
+
+import Component from '@glimmer/component';
+import { inject as service } from '@ember/service';
+import { action } from '@ember/object';
+import { tracked } from '@glimmer/tracking';
+import { task } from 'ember-concurrency';
 export default class GetCredentialsCard extends Component {
   @service router;
   @service store;
@@ -22,12 +24,26 @@ export default class GetCredentialsCard extends Component {
   dynamicRole = '';
   staticRole = '';
   @tracked buttonDisabled = true;
+  @tracked roleDoesNotExist = false;
 
   @action
-  getSelectedValue(selectValue) {
+  async getSelectedValue(selectValue) {
     this.role = selectValue[0];
-    if (this.role) {
-      this.roleType = this.store.peekRecord('database/role', this.role) ? 'dynamic' : 'static';
+    let role = this.role;
+    if (role) {
+      let dynamicRole = this.store.peekRecord('database/role', role);
+      let staticRole = this.store.peekRecord('database/static-role', role);
+      if (!dynamicRole && !staticRole) {
+        // situation when they type in the role and they don't have list permissions
+        // make network request to creds
+        // if nothing is returned, then return error message
+        let fetchCredentialsResult = await this.fetchCredentials.perform(role);
+        console.log(fetchCredentialsResult, 'FETCH CREDS RESULT');
+        if (fetchCredentialsResult.length === 2) {
+          // returned an array of two errors, which means both requests failed and no role exist.
+          this.roleDoesNotExist = !this.roleDoesNotExist;
+        }
+      }
     }
     // Ember Octane way of getting away from toggleProperty
     this.buttonDisabled = !this.buttonDisabled;
@@ -43,4 +59,31 @@ export default class GetCredentialsCard extends Component {
       queryParams: { roleType: roleType },
     });
   }
+  @task(function*(role) {
+    let backend = this.args.backend;
+    let errors = [];
+    try {
+      let staticRoleAttempt = yield this.store.queryRecord('database/credential', {
+        backend,
+        secret: role,
+        roleType: 'static',
+      });
+      // if successful will return result
+      return staticRoleAttempt;
+    } catch (error) {
+      errors.push(error.errors);
+    }
+    try {
+      let dynamicRoleAttempt = yield this.store.queryRecord('database/credential', {
+        backend,
+        secret: role,
+        roleType: 'dynamic',
+      });
+      return dynamicRoleAttempt;
+    } catch (error) {
+      errors.push(error.errors);
+    }
+    return errors;
+  })
+  fetchCredentials;
 }
--- ui/app/routes/vault/cluster/secrets/backend/overview.js
+++ ui/app/routes/vault/cluster/secrets/backend/overview.js
@@ -7,14 +7,39 @@ export default Route.extend({
     let { backend } = this.paramsFor('vault.cluster.secrets.backend');
     return backend;
   },
+  async fetchConnection(queryOptions) {
+    try {
+      return await this.store.query('database/connection', queryOptions);
+    } catch (e) {
+      console.log('catch');
+      return [];
+    }
+  },
+  async fetchStaticRoles(queryOptions) {
+    try {
+      return await this.store.query('database/static-role', queryOptions);
+    } catch (e) {
+      console.log('catch');
+      return [];
+    }
+  },
+  async fetchDynamicRoles(queryOptions) {
+    try {
+      return await this.store.query('database/role', queryOptions);
+    } catch (e) {
+      console.log('catch');
+      return [];
+    }
+  },
   model() {
     let backend = this.enginePathParam();
     let queryOptions = { backend, id: '' };
     let secretEngine = this.store.peekRecord('secret-engine', backend);
     let type = secretEngine && secretEngine.get('engineType');
-    let connection = this.store.query('database/connection', queryOptions);
-    let role = this.store.query('database/role', queryOptions);
-    let staticRole = this.store.query('database/static-role', queryOptions);
+
+    let connection = this.fetchConnection(queryOptions);
+    let role = this.fetchDynamicRoles(queryOptions);
+    let staticRole = this.fetchStaticRoles(queryOptions);
 
     return hash({
       connections: connection,
