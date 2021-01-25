57e71302463c1d7b2ff819c3d7dcbbd3da5c616a
Angel Garbarino <argarbarino@gmail.com>
2021-01-25 13:00:47 -0700

    remove query param for role type

--- ui/app/controllers/vault/cluster/secrets/backend/credentials.js
+++ ui/app/controllers/vault/cluster/secrets/backend/credentials.js
@@ -1,9 +1,8 @@
 import Controller from '@ember/controller';
 
 export default Controller.extend({
-  queryParams: ['action', 'roleType'],
+  queryParams: ['action'],
   action: '',
-  roleType: '',
   reset() {
     this.set('action', '');
   },
