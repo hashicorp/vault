5968a433436a4144a670a3624cf12b87fc091446
mgritter <mgritter@hashicorp.com>
2021-01-25 12:34:43 -0800

    Removing a feature added in 1.7 from backport.

--- vault/logical_system.go
+++ vault/logical_system.go
@@ -3741,14 +3741,14 @@ func (core *Core) GetSealStatus(ctx context.Context) (*SealStatusResponse, error
 }
 
 type LeaderResponse struct {
-	HAEnabled                bool      `json:"ha_enabled"`
-	IsSelf                   bool      `json:"is_self"`
-	ActiveTime               time.Time `json:"active_time,omitempty"`
-	LeaderAddress            string    `json:"leader_address"`
-	LeaderClusterAddress     string    `json:"leader_cluster_address"`
-	PerfStandby              bool      `json:"performance_standby"`
-	PerfStandbyLastRemoteWAL uint64    `json:"performance_standby_last_remote_wal"`
-	LastWAL                  uint64    `json:"last_wal,omitempty"`
+	HAEnabled bool `json:"ha_enabled"`
+	IsSelf    bool `json:"is_self"`
+
+	LeaderAddress            string `json:"leader_address"`
+	LeaderClusterAddress     string `json:"leader_cluster_address"`
+	PerfStandby              bool   `json:"performance_standby"`
+	PerfStandbyLastRemoteWAL uint64 `json:"performance_standby_last_remote_wal"`
+	LastWAL                  uint64 `json:"last_wal,omitempty"`
 
 	// Raft Indexes for this node
 	RaftCommittedIndex uint64 `json:"raft_committed_index,omitempty"`
@@ -3772,9 +3772,6 @@ func (core *Core) GetLeaderStatus() (*LeaderResponse, error) {
 		LeaderAddress:        address,
 		LeaderClusterAddress: clusterAddr,
 		PerfStandby:          core.PerfStandby(),
-	}
-	if isLeader {
-		resp.ActiveTime = core.ActiveTime()
 	}
 	if resp.PerfStandby {
 		resp.PerfStandbyLastRemoteWAL = LastRemoteWAL(core)
