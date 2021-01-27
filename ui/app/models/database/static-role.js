55f8e19f19f1de69241318597838c5b0452f3e21
Angel Garbarino <argarbarino@gmail.com>
2021-01-27 12:00:23 -0700

    add computed to role and static role models to clean up permissions

--- ui/app/models/database/static-role.js
+++ ui/app/models/database/static-role.js
@@ -1,12 +1,12 @@
 import Model, { attr } from '@ember-data/model';
+import { computed } from '@ember/object';
 import { alias } from '@ember/object/computed';
 import lazyCapabilities, { apiPath } from 'vault/macros/lazy-capabilities';
 
 export default Model.extend({
   backend: attr('string', { readOnly: true }),
   secretPath: lazyCapabilities(apiPath`${'backend'}/static-roles/${'id'}`, 'backend', 'id'),
-  canRead: alias('secretPath.canRead'),
-  canEdit: alias('secretPath.canUpdate'),
+  canEditRole: computed.or('secretPath.canUpdate', 'secretPath.canCreate'),
   credentialPath: lazyCapabilities(apiPath`${'backend'}/static-creds/${'id'}`, 'backend', 'id'),
   canGenerateCredentials: alias('credentialPath.canRead'),
 });
