65a266099f1149409a48c2f342adf810e312d17c
mgritter <mgritter@hashicorp.com>
2021-01-29 15:09:13 -0800

    Stub "operator diagnose" command.

---
+++ command/operator_diagnose.go
@@ -0,0 +1,98 @@
+package command
+
+import (
+	"strings"
+
+	"github.com/mitchellh/cli"
+	"github.com/posener/complete"
+)
+
+const OperatorDiagnoseEnableEnv = "VAULT_DIAGNOSE"
+
+var _ cli.Command = (*OperatorDiagnoseCommand)(nil)
+var _ cli.CommandAutocomplete = (*OperatorDiagnoseCommand)(nil)
+
+type OperatorDiagnoseCommand struct {
+	*BaseCommand
+
+	flagDebug   bool
+	flagSkips   []string
+	flagConfigs []string
+}
+
+func (c *OperatorDiagnoseCommand) Synopsis() string {
+	return "Troubleshoot problems starting Vault"
+}
+
+func (c *OperatorDiagnoseCommand) Help() string {
+	helpText := `
+Usage: vault operator diagnose 
+
+  This command troubleshoots Vault startup issues, such as TLS configuration or
+  auto-unseal. It should be run using the same environment variables and configuration
+  files as the "vault server" command, so that startup problems can be accurately
+  reproduced.
+
+  Start diagnose with a configuration file:
+    
+     $ vault operator diagnose -config=/etc/vault/config.hcl
+
+  Perform a diagnostic check while Vault is still running:
+
+     $ vault operator diagnose -config=/etc/vault/config.hcl -skip=listener
+
+` + c.Flags().Help()
+	return strings.TrimSpace(helpText)
+}
+
+func (c *OperatorDiagnoseCommand) Flags() *FlagSets {
+	set := c.flagSet(FlagSetNone)
+	f := set.NewFlagSet("Command Options")
+
+	f.StringSliceVar(&StringSliceVar{
+		Name:   "config",
+		Target: &c.flagConfigs,
+		Completion: complete.PredictOr(
+			complete.PredictFiles("*.hcl"),
+			complete.PredictFiles("*.json"),
+			complete.PredictDirs("*"),
+		),
+		Usage: "Path to a Vault configuration file or directory of configuration " +
+			"files. This flag can be specified multiple times to load multiple " +
+			"configurations. If the path is a directory, all files which end in " +
+			".hcl or .json are loaded.",
+	})
+
+	f.StringSliceVar(&StringSliceVar{
+		Name:   "skip",
+		Target: &c.flagSkips,
+		Usage:  "Skip the health checks named as arguments. May be 'listener', 'storage', or 'autounseal'.",
+	})
+
+	f.BoolVar(&BoolVar{
+		Name:    "debug",
+		Target:  &c.flagDebug,
+		Default: false,
+		Usage:   "Dump all information collected by Diagnose.",
+	})
+	return set
+}
+
+func (c *OperatorDiagnoseCommand) AutocompleteArgs() complete.Predictor {
+	return complete.PredictNothing
+}
+
+func (c *OperatorDiagnoseCommand) AutocompleteFlags() complete.Flags {
+	return c.Flags().Completions()
+}
+
+func (c *OperatorDiagnoseCommand) Run(args []string) int {
+	f := c.Flags()
+	if err := f.Parse(args); err != nil {
+		c.UI.Error(err.Error())
+		return 1
+	}
+
+	c.UI.Output("No implementation yet.")
+	return 0
+}
