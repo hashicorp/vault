0cf87906994939401cd8a5455674c376ac8ed597
Brandon Romano <BrandonRRomano@gmail.com>
2021-02-01 17:59:43 -0800

    Web Updates for HCP (2/2)
    Adds HCP Vault section to homepage
    
    Updates the Nav
    
    Hack in a third 'try cloud' link
    
    Update banner to point to HCP site

--- website/data/alert-banner.js
+++ website/data/alert-banner.js
@@ -3,11 +3,10 @@ export const ALERT_BANNER_ACTIVE = true
 // https://github.com/hashicorp/react-components/tree/master/packages/alert-banner
 export default {
   tag: 'Announcement',
-  url:
-    'https://www.hashicorp.com/blog/vault-on-the-hashicorp-cloud-platform-public-beta',
+  url: 'https://cloud.hashicorp.com/',
   text: 'HashiCorp Vault is now available on HCP in Public Beta',
   linkText: 'Learn More',
   // Set the `expirationDate prop with a datetime string (e.g. `2020-01-31T12:00:00-07:00`)
   // if you'd like the component to stop showing at or after a certain date
-  expirationDate: '2021-02-09T11:59:00-05:00',
+  expirationDate: '2021-02-14T11:59:00-05:00',
 }
