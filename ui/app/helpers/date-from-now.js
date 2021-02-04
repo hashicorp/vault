2c66537b4f320302f416f66242ba0abe6bd72aa4
Angel Garbarino <argarbarino@gmail.com>
2021-02-04 14:17:48 -0700

    fix dumb options on new method

--- ui/app/helpers/date-from-now.js
+++ ui/app/helpers/date-from-now.js
@@ -2,7 +2,7 @@ import { helper } from '@ember/component/helper';
 import { formatDistanceToNow } from 'date-fns';
 
 export function dateFromNow([date], options = {}) {
-  return formatDistanceToNow(date, options);
+  return formatDistanceToNow(date, [options]);
 }
 
 export default helper(dateFromNow);
