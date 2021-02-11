7bfe785092b3e058f2e9a14776c457684b3490cc
Michael Golowka <72365+pcman312@users.noreply.github.com>
2021-02-11 14:08:32 -0700

    MySQL - Add username customization (#10834)

--- vault/testing.go
+++ vault/testing.go
@@ -2061,7 +2061,7 @@ func (m *mockBuiltinRegistry) Get(name string, pluginType consts.PluginType) (fu
 	if name == "postgresql-database-plugin" {
 		return dbPostgres.New, true
 	}
-	return dbMysql.New(dbMysql.MetadataLen, dbMysql.MetadataLen, dbMysql.UsernameLen), true
+	return dbMysql.New(dbMysql.DefaultUserNameTemplate), true
 }
 
 // Keys only supports getting a realistic list of the keys for database plugins.
