38f00ad2fa7cfc405a281d4f74914b280b7fa36f
Chelsea Shaw <chelshaw.dev@gmail.com>
2021-02-17 15:21:15 -0600

    fix readonly form field test and lint

--- ui/tests/integration/components/readonly-form-field-test.js
+++ ui/tests/integration/components/readonly-form-field-test.js
@@ -1,4 +1,5 @@
 import { module, test } from 'qunit';
+import EmberObject from '@ember/object';
 import { setupRenderingTest } from 'ember-qunit';
 import { render } from '@ember/test-helpers';
 import { hbs } from 'ember-cli-htmlbars';
@@ -20,13 +21,13 @@ module('Integration | Component | readonly-form-field', function(hooks) {
   setupRenderingTest(hooks);
 
   test('it renders', async function(assert) {
-    this.set('attr', minimumAttr);
+    this.set('attr', EmberObject.create(minimumAttr));
     await render(hbs`<ReadonlyFormField @attr={{attr}} @value="value" />`);
     assert
       .dom('[data-test-readonly-label]')
       .includesText('My input', 'formats the attr name when no label provided');
-    assert.dom(`input`).hasValue('value', 'Uses the value as passed');
-    assert.dom(`input`).hasAttribute('readonly');
+    assert.dom(`[data-test-input="${minimumAttr.name}"]`).hasValue('value', 'Uses the value as passed');
+    assert.dom(`[data-test-input="${minimumAttr.name}"]`).hasAttribute('readonly');
   });
 
   test('it renders with options', async function(assert) {
@@ -36,7 +37,9 @@ module('Integration | Component | readonly-form-field', function(hooks) {
       .dom('[data-test-readonly-label]')
       .includesText('Custom-label', 'Uses the provided label as passed');
     assert.dom('.sub-text').includesText('Subtext here', 'Renders subtext');
-    assert.dom('input').hasValue('another value', 'Uses the value as passed');
-    assert.dom('input').hasAttribute('readonly');
+    assert
+      .dom(`[data-test-input="${customLabelAttr.name}"]`)
+      .hasValue('another value', 'Uses the value as passed');
+    assert.dom(`[data-test-input="${customLabelAttr.name}"]`).hasAttribute('readonly');
   });
 });
