0a56c1fa33be7d61da3d976106f535784bdbbd5d
Jason O'Donnell <2160810+jasonodonnell@users.noreply.github.com>
2021-02-19 09:42:35 -0500

    Update command/agent/template/template_test.go
    Co-authored-by: Theron Voran <tvoran@users.noreply.github.com>

--- command/agent/template/template_test.go
+++ command/agent/template/template_test.go
@@ -140,10 +140,6 @@ func TestCacheConfigHTTP(t *testing.T) {
 		t.Fatalf("unexpected error: %s", err)
 	}
 
-	if !strings.HasPrefix(*ctConfig.Vault.Address, "http") {
-		t.Fatalf("expected http address, got %s", *ctConfig.Vault.Address)
-	}
-
 	expected := "http://127.0.0.1:8300"
 	if *ctConfig.Vault.Address != expected {
 		t.Fatalf("expected %s, got %s", expected, *ctConfig.Vault.Address)
