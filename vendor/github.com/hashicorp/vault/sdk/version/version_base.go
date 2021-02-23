3f7e852080f9b3597d00255a0cfc6b9af2b62a91
swayne275 <swayne275@gmail.com>
2021-02-23 15:21:52 -0700

    update dependencies for 1.6.3 (#10984)

--- vendor/github.com/hashicorp/vault/sdk/version/version_base.go
+++ vendor/github.com/hashicorp/vault/sdk/version/version_base.go
@@ -8,7 +8,7 @@ var (
 	// Whether cgo is enabled or not; set at build time
 	CgoEnabled bool
 
-	Version           = "1.6.2"
+	Version           = "1.6.3"
 	VersionPrerelease = ""
 	VersionMetadata   = ""
 )
