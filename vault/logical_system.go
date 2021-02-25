4fa33d1904b8a7e60fe7321004c11bbb27848624
Scott G. Miller <smiller@hashicorp.com>
2021-02-25 13:13:05 -0600

    Add missing encs field, not sure where this got lost.

--- vault/logical_system.go
+++ vault/logical_system.go
@@ -2525,6 +2525,7 @@ func (b *SystemBackend) handleKeyStatus(ctx context.Context, req *logical.Reques
 		Data: map[string]interface{}{
 			"term":         info.Term,
 			"install_time": info.InstallTime.Format(time.RFC3339Nano),
+			"encryptions":  info.Encryptions,
 		},
 	}
 	return resp, nil
