7d935d21faa82687a75ba072126d33827b804516
Mike Wickett <mwickett@hashicorp.com>
2021-03-11 13:53:35 -0500

    Update website/data/alert-banner.js
    Co-authored-by: Zachary Shilton <4624598+zchsh@users.noreply.github.com>

--- website/data/alert-banner.js
+++ website/data/alert-banner.js
@@ -9,5 +9,5 @@ export default {
   linkText: 'Try today',
   // Set the `expirationDate prop with a datetime string (e.g. `2020-01-31T12:00:00-07:00`)
   // if you'd like the component to stop showing at or after a certain date
-  expirationDate: '2021-04-7T11:59:00-05:00',
+  expirationDate: '2021-04-07T11:59:00-05:00',
 }
