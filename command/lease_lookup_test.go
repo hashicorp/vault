e3678ee3d573083421d649e2dc11ece3dda1de3d
catsby <clint@ctshryock.com>
2021-03-15 16:48:12 -0500

    basic test

--- command/lease_lookup_test.go
+++ command/lease_lookup_test.go
@@ -1,8 +1,10 @@
 package command
 
 import (
+	"strings"
 	"testing"
 
+	"github.com/hashicorp/vault/api"
 	"github.com/mitchellh/cli"
 )
 
@@ -17,140 +19,82 @@ func testLeaseLookupCommand(tb testing.TB) (*cli.MockUi, *LeaseLookupCommand) {
 	}
 }
 
-// // testLeaseLookupCommandMountAndLease mounts a leased secret backend and returns
-// // the leaseID of an item.
-// func testLeaseLookupCommandMountAndLease(tb testing.TB, client *api.Client) string {
-// 	if err := client.Sys().Mount("testing", &api.MountInput{
-// 		Type: "generic-leased",
-// 	}); err != nil {
-// 		tb.Fatal(err)
-// 	}
+// testLeaseLookupCommandMountAndLease mounts a leased secret backend and returns
+// the leaseID of an item.
+func testLeaseLookupCommandMountAndLease(tb testing.TB, client *api.Client) string {
+	if err := client.Sys().Mount("testing", &api.MountInput{
+		Type: "generic-leased",
+	}); err != nil {
+		tb.Fatal(err)
+	}
 
-// 	if _, err := client.Logical().Write("testing/foo", map[string]interface{}{
-// 		"key":   "value",
-// 		"lease": "5m",
-// 	}); err != nil {
-// 		tb.Fatal(err)
-// 	}
+	if _, err := client.Logical().Write("testing/foo", map[string]interface{}{
+		"key":   "value",
+		"lease": "5m",
+	}); err != nil {
+		tb.Fatal(err)
+	}
 
-// 	// Read the secret back to get the leaseID
-// 	secret, err := client.Logical().Read("testing/foo")
-// 	if err != nil {
-// 		tb.Fatal(err)
-// 	}
-// 	if secret == nil || secret.LeaseID == "" {
-// 		tb.Fatalf("missing secret or lease: %#v", secret)
-// 	}
+	// Read the secret back to get the leaseID
+	secret, err := client.Logical().Read("testing/foo")
+	if err != nil {
+		tb.Fatal(err)
+	}
+	if secret == nil || secret.LeaseID == "" {
+		tb.Fatalf("missing secret or lease: %#v", secret)
+	}
 
-// 	return secret.LeaseID
-// }
+	return secret.LeaseID
+}
 
-// func TestLeaseLookupCommand_Run(t *testing.T) {
-// 	t.Parallel()
+// TestLeaseLookupCommand_Run tests basic lookup
+func TestLeaseLookupCommand_Run(t *testing.T) {
+	t.Parallel()
 
-// 	cases := []struct {
-// 		name string
-// 		args []string
-// 		out  string
-// 		code int
-// 	}{
-// 		{
-// 			"empty",
-// 			nil,
-// 			"Missing ID!",
-// 			1,
-// 		},
-// 		{
-// 			"increment",
-// 			[]string{"-increment", "60s"},
-// 			"foo",
-// 			0,
-// 		},
-// 		{
-// 			"increment_no_suffix",
-// 			[]string{"-increment", "60"},
-// 			"foo",
-// 			0,
-// 		},
-// 	}
+	t.Run("empty", func(t *testing.T) {
+		t.Parallel()
 
-// 	t.Run("group", func(t *testing.T) {
-// 		t.Parallel()
+		client, closer := testVaultServer(t)
+		defer closer()
 
-// 		for _, tc := range cases {
-// 			tc := tc
+		_ = testLeaseLookupCommandMountAndLease(t, client)
 
-// 			t.Run(tc.name, func(t *testing.T) {
-// 				t.Parallel()
+		ui, cmd := testLeaseLookupCommand(t)
+		cmd.client = client
 
-// 				client, closer := testVaultServer(t)
-// 				defer closer()
+		code := cmd.Run(nil)
+		if exp := 1; code != exp {
+			t.Errorf("expected %d to be %d", code, exp)
+		}
 
-// 				leaseID := testLeaseLookupCommandMountAndLease(t, client)
+		combined := ui.OutputWriter.String() + ui.ErrorWriter.String()
+		expectedMsg := "Missing ID!"
+		if !strings.Contains(combined, expectedMsg) {
+			t.Errorf("expected %q to contain %q", combined, expectedMsg)
+		}
+	})
 
-// 				ui, cmd := testLeaseLookupCommand(t)
-// 				cmd.client = client
+	t.Run("integration", func(t *testing.T) {
+		t.Parallel()
 
-// 				if tc.args != nil {
-// 					tc.args = append(tc.args, leaseID)
-// 				}
-// 				code := cmd.Run(tc.args)
-// 				if code != tc.code {
-// 					t.Errorf("expected %d to be %d", code, tc.code)
-// 				}
+		client, closer := testVaultServer(t)
+		defer closer()
 
-// 				combined := ui.OutputWriter.String() + ui.ErrorWriter.String()
-// 				if !strings.Contains(combined, tc.out) {
-// 					t.Errorf("expected %q to contain %q", combined, tc.out)
-// 				}
-// 			})
-// 		}
-// 	})
+		leaseID := testLeaseLookupCommandMountAndLease(t, client)
 
-// 	t.Run("integration", func(t *testing.T) {
-// 		t.Parallel()
+		_, cmd := testLeaseLookupCommand(t)
+		cmd.client = client
 
-// 		client, closer := testVaultServer(t)
-// 		defer closer()
+		code := cmd.Run([]string{leaseID})
+		if exp := 0; code != exp {
+			t.Errorf("expected %d to be %d", code, exp)
+		}
+	})
 
-// 		leaseID := testLeaseLookupCommandMountAndLease(t, client)
+	t.Run("no_tabs", func(t *testing.T) {
+		t.Parallel()
 
-// 		_, cmd := testLeaseLookupCommand(t)
-// 		cmd.client = client
-
-// 		code := cmd.Run([]string{leaseID})
-// 		if exp := 0; code != exp {
-// 			t.Errorf("expected %d to be %d", code, exp)
-// 		}
-// 	})
-
-// 	t.Run("communication_failure", func(t *testing.T) {
-// 		t.Parallel()
-
-// 		client, closer := testVaultServerBad(t)
-// 		defer closer()
-
-// 		ui, cmd := testLeaseLookupCommand(t)
-// 		cmd.client = client
-
-// 		code := cmd.Run([]string{
-// 			"foo/bar",
-// 		})
-// 		if exp := 2; code != exp {
-// 			t.Errorf("expected %d to be %d", code, exp)
-// 		}
-
-// 		expected := "Error renewing foo/bar: "
-// 		combined := ui.OutputWriter.String() + ui.ErrorWriter.String()
-// 		if !strings.Contains(combined, expected) {
-// 			t.Errorf("expected %q to contain %q", combined, expected)
-// 		}
-// 	})
-
-// 	t.Run("no_tabs", func(t *testing.T) {
-// 		t.Parallel()
-
-// 		_, cmd := testLeaseLookupCommand(t)
-// 		assertNoTabs(t, cmd)
-// 	})
-// }
+		_, cmd := testLeaseLookupCommand(t)
+		assertNoTabs(t, cmd)
+	})
+}
