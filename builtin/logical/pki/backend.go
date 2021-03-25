4d661490e38fecec6d2bb1083a307799c1b843f5
HridoyRoy <roy@hashicorp.com>
2021-03-25 11:00:41 -0700

    initial fix

--- builtin/logical/pki/backend.go
+++ builtin/logical/pki/backend.go
@@ -2,6 +2,7 @@ package pki
 
 import (
 	"context"
+	"fmt"
 	"strings"
 	"sync"
 	"time"
@@ -23,7 +24,8 @@ func Factory(ctx context.Context, conf *logical.BackendConfig) (logical.Backend,
 func Backend(conf *logical.BackendConfig) *backend {
 	var b backend
 	b.Backend = &framework.Backend{
-		Help: strings.TrimSpace(backendHelp),
+		PeriodicFunc: b.periodicFunc,
+		Help:         strings.TrimSpace(backendHelp),
 
 		PathsSpecial: &logical.Paths{
 			Unauthenticated: []string{
@@ -91,6 +93,42 @@ func Backend(conf *logical.BackendConfig) *backend {
 	return &b
 }
 
+func (b *backend) periodicFunc(ctx context.Context, req *logical.Request) error {
+	fmt.Printf("period function has been called")
+	// skip the function if autoTidy isn't being used
+	zeroDuration, _ := time.ParseDuration("0s")
+	if b.crlAutoTidy == zeroDuration {
+		fmt.Printf("zero duration")
+		return nil
+	}
+	// If autoTidy hasn't been run and the periodic function goes off, then simply
+	// set the next time for tidying to the appropriate value
+	if b.nextCrlAutoTidy.IsZero() {
+		fmt.Printf("first tidy push out")
+		b.nextCrlAutoTidy = time.Now().Add(b.crlAutoTidy)
+		return nil
+	}
+
+	// We're ready to autoTidy
+	if !time.Now().Before(b.nextCrlAutoTidy) && !b.nextCrlAutoTidy.IsZero() {
+		fmt.Printf("ready to tidy")
+		// tidy_revoked_certs is all we need, I think
+		rawFieldData := map[string]interface{}{}
+		rawFieldData["tidy_revoked_certs"] = true
+		tidyFd := &framework.FieldData{
+			Raw:    rawFieldData,
+			Schema: pathTidy(b).Fields,
+		}
+		_, err := b.pathTidyWrite(ctx, req, tidyFd)
+		if err != nil {
+			fmt.Printf("couldn't tidy")
+			return err
+		}
+	}
+	fmt.Printf("returning from tidy")
+	return nil
+}
+
 type backend struct {
 	*framework.Backend
 
@@ -98,6 +136,8 @@ type backend struct {
 	crlLifetime       time.Duration
 	revokeStorageLock sync.RWMutex
 	tidyCASGuard      *uint32
+	crlAutoTidy       time.Duration
+	nextCrlAutoTidy   time.Time
 }
 
 const backendHelp = `
