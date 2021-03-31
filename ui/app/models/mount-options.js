3300fed6e65dfff6f7b841f92e7969e18b68a7d5
Chelsea Shaw <chelshaw.dev@gmail.com>
2021-03-31 16:31:22 -0500

    Update default form values for kv

--- ui/app/models/mount-options.js
+++ ui/app/models/mount-options.js
@@ -7,6 +7,6 @@ export default Fragment.extend({
     helpText:
       'The KV Secrets Engine can operate in different modes. Version 1 is the original generic Secrets Engine the allows for storing of static key/value pairs. Version 2 added more features including data versioning, TTLs, and check and set.',
     possibleValues: [2, 1],
-    defaultFormValue: 2,
+    defaultValue: 2,
   }),
 });
