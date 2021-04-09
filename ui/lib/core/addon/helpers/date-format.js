f83052fda71b04dff26df63408bf631a3b362e6e
Angel Garbarino <argarbarino@gmail.com>
2021-04-09 09:32:52 -0600

    work on certificates

--- ui/lib/core/addon/helpers/date-format.js
+++ ui/lib/core/addon/helpers/date-format.js
@@ -4,6 +4,9 @@ import { format, parseISO } from 'date-fns';
 export function dateFormat([date, style]) {
   // see format breaking in upgrade to date-fns 2.x https://github.com/date-fns/date-fns/blob/master/CHANGELOG.md#changed-5
   let number = typeof date === 'string' ? parseISO(date) : date;
+  if (number.toString().length === 10) {
+    number = new Date(number * 1000);
+  }
   return format(number, style);
 }
 
