0cc27c1b473deafcbe6b3a2a40fca2f54df7f53e
Nick Cabatoff <ncabatoff@hashicorp.com>
2021-04-13 13:21:59 -0400

    Remove hard-coded version string.  We'll rely solely on ldflags.

--- vendor/github.com/hashicorp/vault/sdk/version/version_base.go
+++ vendor/github.com/hashicorp/vault/sdk/version/version_base.go
@@ -8,7 +8,7 @@ var (
 	// Whether cgo is enabled or not; set at build time
 	CgoEnabled bool
 
-	Version           = "1.7.0"
+	Version           = ""
 	VersionPrerelease = ""
 	VersionMetadata   = ""
 )
