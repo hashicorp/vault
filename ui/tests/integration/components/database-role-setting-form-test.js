89bc00886f9ed6410fdc8f36327401cfd42bcd16
Chelsea Shaw <chelshaw.dev@gmail.com>
2021-04-14 16:07:07 -0500

    UI/database mssql (#11231)
    Add MSSQL plugin support in database secrets engine

--- ui/tests/integration/components/database-role-setting-form-test.js
+++ ui/tests/integration/components/database-role-setting-form-test.js
@@ -4,18 +4,35 @@ import { setupRenderingTest } from 'ember-qunit';
 import { render } from '@ember/test-helpers';
 import hbs from 'htmlbars-inline-precompile';
 
-const STANDARD_FIELDS = [
-  'name',
-  'ttl',
-  'max_ttl',
-  'username',
-  'rotation_period',
-  'creation_statements',
-  'revocation_statements',
-  'rotation_statements',
+const testCases = [
+  {
+    // default case should show all possible fields for each type
+    pluginType: '',
+    staticRoleFields: ['name', 'username', 'rotation_period', 'rotation_statements'],
+    dynamicRoleFields: [
+      'name',
+      'ttl',
+      'max_ttl',
+      'creation_statements',
+      'revocation_statements',
+      'rollback_statements',
+      'renew_statements',
+    ],
+  },
+  {
+    pluginType: 'mongodb-database-plugin',
+    staticRoleFields: ['username', 'rotation_period'],
+    dynamicRoleFields: ['creation_statement', 'revocation_statement', 'ttl', 'max_ttl'],
+    statementsHidden: true,
+  },
+  {
+    pluginType: 'mssql-database-plugin',
+    staticRoleFields: ['username', 'rotation_period'],
+    dynamicRoleFields: ['creation_statements', 'revocation_statements', 'ttl', 'max_ttl'],
+  },
 ];
-const MONGODB_STATIC_FIELDS = ['username', 'rotation_period'];
-const MONGODB_DYNAMIC_FIELDS = ['creation_statement', 'revocation_statement', 'ttl', 'max_ttl'];
+
+// used to calculate checks that fields do NOT show up
 const ALL_ATTRS = [
   { name: 'ttl', type: 'string', options: {} },
   { name: 'max_ttl', type: 'string', options: {} },
@@ -26,6 +43,8 @@ const ALL_ATTRS = [
   { name: 'revocation_statements', type: 'string', options: {} },
   { name: 'revocation_statement', type: 'string', options: {} },
   { name: 'rotation_statements', type: 'string', options: {} },
+  { name: 'rollback_statements', type: 'string', options: {} },
+  { name: 'renew_statements', type: 'string', options: {} },
 ];
 const getFields = nameArray => {
   const show = ALL_ATTRS.filter(attr => nameArray.indexOf(attr.name) >= 0);
@@ -51,7 +70,7 @@ module('Integration | Component | database-role-setting-form', function(hooks) {
     assert.dom('[data-test-component="empty-state"]').exists({ count: 2 }, 'Two empty states exist');
   });
 
-  test('it shows appropriate fields based on roleType with default db type', async function(assert) {
+  test('it shows appropriate fields based on roleType and db plugin', async function(assert) {
     this.set('roleType', 'static');
     this.set('dbType', '');
     await render(hbs`
@@ -63,68 +82,47 @@ module('Integration | Component | database-role-setting-form', function(hooks) {
       />
     `);
     assert.dom('[data-test-component="empty-state"]').doesNotExist('Does not show empty states');
-    const defaultFields = getFields(STANDARD_FIELDS);
-    defaultFields.show.forEach(attr => {
-      assert
-        .dom(`[data-test-input="${attr.name}"]`)
-        .exists(`${attr.name} attribute exists for default db type`);
-    });
-    defaultFields.hide.forEach(attr => {
-      assert
-        .dom(`[data-test-input="${attr.name}"]`)
-        .doesNotExist(`${attr.name} attribute does not exist for default db type`);
-    });
-
-    this.set('roleType', 'dynamic');
-    defaultFields.show.forEach(attr => {
-      assert
-        .dom(`[data-test-input="${attr.name}"]`)
-        .exists(`${attr.name} attribute exists for default db with dynamic type`);
-    });
-    defaultFields.hide.forEach(attr => {
-      assert
-        .dom(`[data-test-input="${attr.name}"]`)
-        .doesNotExist(`${attr.name} attribute does not exist for default db with dynamic type`);
-    });
-  });
-  test('it shows appropriate fields based on roleType with mongodb', async function(assert) {
-    this.set('roleType', 'static');
-    this.set('dbType', 'mongodb-database-plugin');
-    await render(hbs`
-      <DatabaseRoleSettingForm
-        @attrs={{model.attrs}}
-        @model={{model}}
-        @roleType={{roleType}}
-        @dbType={{dbType}}
-      />
-    `);
-    assert.dom('[data-test-component="empty-state"]').doesNotExist('Does not show empty states');
-    const staticFields = getFields(MONGODB_STATIC_FIELDS);
-    staticFields.show.forEach(attr => {
-      assert
-        .dom(`[data-test-input="${attr.name}"]`)
-        .exists(`${attr.name} attribute exists for mongodb static role`);
-    });
-    staticFields.hide.forEach(attr => {
-      assert
-        .dom(`[data-test-input="${attr.name}"]`)
-        .doesNotExist(`${attr.name} attribute does not exist for mongodb static role`);
-    });
-    assert
-      .dom('[data-test-statements-section]')
-      .doesNotExist('Statements section is hidden for dynamic mongodb role');
-
-    this.set('roleType', 'dynamic');
-    const dynamicFields = getFields(MONGODB_DYNAMIC_FIELDS);
-    dynamicFields.show.forEach(attr => {
-      assert
-        .dom(`[data-test-input="${attr.name}"]`)
-        .exists(`${attr.name} attribute exists for mongodb dynamic role`);
-    });
-    dynamicFields.hide.forEach(attr => {
-      assert
-        .dom(`[data-test-input="${attr.name}"]`)
-        .doesNotExist(`${attr.name} attribute does not exist for mongodb dynamic role`);
-    });
+    for (let testCase of testCases) {
+      let staticFields = getFields(testCase.staticRoleFields);
+      let dynamicFields = getFields(testCase.dynamicRoleFields);
+      this.set('dbType', testCase.pluginType);
+      this.set('roleType', 'static');
+      staticFields.show.forEach(attr => {
+        assert
+          .dom(`[data-test-input="${attr.name}"]`)
+          .exists(
+            `${attr.name} attribute exists on static role for ${testCase.pluginType || 'default'} db type`
+          );
+      });
+      staticFields.hide.forEach(attr => {
+        assert
+          .dom(`[data-test-input="${attr.name}"]`)
+          .doesNotExist(
+            `${attr.name} attribute does not exist on static role for ${testCase.pluginType ||
+              'default'} db type`
+          );
+      });
+      if (testCase.statementsHidden) {
+        assert
+          .dom('[data-test-statements-section]')
+          .doesNotExist(`Statements section is hidden for static ${testCase.pluginType} role`);
+      }
+      this.set('roleType', 'dynamic');
+      dynamicFields.show.forEach(attr => {
+        assert
+          .dom(`[data-test-input="${attr.name}"]`)
+          .exists(
+            `${attr.name} attribute exists on dynamic role for ${testCase.pluginType || 'default'} db type`
+          );
+      });
+      dynamicFields.hide.forEach(attr => {
+        assert
+          .dom(`[data-test-input="${attr.name}"]`)
+          .doesNotExist(
+            `${attr.name} attribute does not exist on dynamic role for ${testCase.pluginType ||
+              'default'} db type`
+          );
+      });
+    }
   });
 });
