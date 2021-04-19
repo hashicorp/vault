620ecf239c29836e866005d0ce575b7b704d14a4
Jim Kalafut <jkalafut@hashicorp.com>
2021-04-19 07:30:22 -0700

    Update SDK version to 1.6.4 (#11393)

--- sdk/version/version_base.go
+++ sdk/version/version_base.go
@@ -8,7 +8,7 @@ var (
 	// Whether cgo is enabled or not; set at build time
 	CgoEnabled bool
 
-	Version           = "1.6.3"
+	Version           = "1.6.4"
 	VersionPrerelease = ""
 	VersionMetadata   = ""
 )
