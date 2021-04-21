a3f1bf166ae7300eba44cc6cf2a6c7bfb3b8fd41
hashishaw <cshaw@hashicorp.com>
2021-04-21 16:58:04 -0500

    Fix linting error

--- ui/tests/integration/components/transform-list-item-test.js
+++ ui/tests/integration/components/transform-list-item-test.js
@@ -1,7 +1,7 @@
 import EmberObject from '@ember/object';
 import { module, test } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
-import { render, findAll, click } from '@ember/test-helpers';
+import { render, click } from '@ember/test-helpers';
 import hbs from 'htmlbars-inline-precompile';
 
 module('Integration | Component | transform-list-item', function(hooks) {
