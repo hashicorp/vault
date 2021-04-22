f73e50d03a56df7811afa2b4dec72eaf32bd7e81
Zach Shilton <4624598+zchsh@users.noreply.github.com>
2021-04-22 12:40:00 -0400

    website: temp support for unlinked content

--- website/pages/api-docs/[[...page]].jsx
+++ website/pages/api-docs/[[...page]].jsx
@@ -1,10 +1,19 @@
 import { productName, productSlug } from 'data/metadata'
 import DocsPage from '@hashicorp/react-docs-page'
+// Imports below are used in server-side only
+/**
+ * DEBT: short term patch for "hidden" docs-sidenav items.
+ * See components/_temp-enable-hidden-pages for details.
+ * Revert to importing from @hashicorp/react-docs-page/server
+ * once https://app.asana.com/0/1100423001970639/1200197752405255/f
+ * is complete.
+ **/
 import {
   generateStaticPaths,
   generateStaticProps,
-} from '@hashicorp/react-docs-page/server'
+} from 'components/_temp-enable-hidden-pages'
 
+const NAV_DATA_FILE_HIDDEN = 'data/api-docs-nav-data-hidden.json'
 const NAV_DATA_FILE = 'data/api-docs-nav-data.json'
 const CONTENT_DIR = 'content/api-docs'
 const basePath = 'api-docs'
@@ -24,6 +33,7 @@ export async function getStaticPaths() {
     fallback: false,
     paths: await generateStaticPaths({
       navDataFile: NAV_DATA_FILE,
+      navDataFileHidden: NAV_DATA_FILE_HIDDEN,
       localContentDir: CONTENT_DIR,
     }),
   }
@@ -33,6 +43,7 @@ export async function getStaticProps({ params }) {
   return {
     props: await generateStaticProps({
       navDataFile: NAV_DATA_FILE,
+      navDataFileHidden: NAV_DATA_FILE_HIDDEN,
       localContentDir: CONTENT_DIR,
       product: { name: productName, slug: productSlug },
       params,
--- website/pages/docs/[[...page]].jsx
+++ website/pages/docs/[[...page]].jsx
@@ -1,12 +1,21 @@
 import { productName, productSlug } from 'data/metadata'
 import DocsPage from '@hashicorp/react-docs-page'
+import Columns from 'components/columns'
+import Tag from 'components/inline-tag'
+// Imports below are used in server-side only
+/**
+ * DEBT: short term patch for "hidden" docs-sidenav items.
+ * See components/_temp-enable-hidden-pages for details.
+ * Revert to importing from @hashicorp/react-docs-page/server
+ * once https://app.asana.com/0/1100423001970639/1200197752405255/f
+ * is complete.
+ **/
 import {
   generateStaticPaths,
   generateStaticProps,
-} from '@hashicorp/react-docs-page/server'
-import Columns from 'components/columns'
-import Tag from 'components/inline-tag'
+} from 'components/_temp-enable-hidden-pages'
 
+const NAV_DATA_FILE_HIDDEN = 'data/docs-nav-data-hidden.json'
 const NAV_DATA_FILE = 'data/docs-nav-data.json'
 const CONTENT_DIR = 'content/docs'
 const basePath = 'docs'
@@ -28,6 +37,7 @@ export async function getStaticPaths() {
     fallback: false,
     paths: await generateStaticPaths({
       navDataFile: NAV_DATA_FILE,
+      navDataFileHidden: NAV_DATA_FILE_HIDDEN,
       localContentDir: CONTENT_DIR,
     }),
   }
@@ -37,6 +47,7 @@ export async function getStaticProps({ params }) {
   return {
     props: await generateStaticProps({
       navDataFile: NAV_DATA_FILE,
+      navDataFileHidden: NAV_DATA_FILE_HIDDEN,
       localContentDir: CONTENT_DIR,
       product: { name: productName, slug: productSlug },
       params,
