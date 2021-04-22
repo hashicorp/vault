e4bac8bcf6f0f04b64d88a928c6a64c57dc07810
hashishaw <cshaw@hashicorp.com>
2021-04-22 10:31:30 -0500

    Codemod ember-data-codemod

--- ui/app/components/pricing-metrics-dates.js
+++ ui/app/components/pricing-metrics-dates.js
@@ -15,8 +15,7 @@
  * @param {number} [defaultSpan=12] - setting for default time between start and end input dates
  * @param {number} [retentionMonths=24] - setting for the retention months, which informs valid dates to query by
  */
-import { set } from '@ember/object';
-import { computed } from '@ember/object';
+import { set, computed } from '@ember/object';
 import { inject as service } from '@ember/service';
 import Component from '@ember/component';
 import {
