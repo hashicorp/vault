ec939ee39ff2934caca15f19b6c73d120f573bd1
catsby <clint@ctshryock.com>
2021-04-29 10:34:26 -0500

    Update GCP Auth to v0.9.1

--- vendor/github.com/hashicorp/vault-plugin-auth-gcp/plugin/login_util.go
+++ vendor/github.com/hashicorp/vault-plugin-auth-gcp/plugin/login_util.go
@@ -3,14 +3,14 @@ package gcpauth
 import (
 	"encoding/json"
 	"fmt"
-	"github.com/hashicorp/go-gcp-common/gcputil"
-	"google.golang.org/api/iam/v1"
 	"time"
+
+	"github.com/hashicorp/go-gcp-common/gcputil"
+	"google.golang.org/api/iamcredentials/v1"
 )
 
-func ServiceAccountLoginJwt(
-	iamClient *iam.Service, exp time.Time, aud, project, serviceAccount string) (*iam.SignJwtResponse, error) {
-	accountResource := fmt.Sprintf(gcputil.ServiceAccountTemplate, project, serviceAccount)
+func ServiceAccountLoginJwt(iamClient *iamcredentials.Service, exp time.Time, aud, serviceAccount string) (*iamcredentials.SignJwtResponse, error) {
+	accountResource := fmt.Sprintf(gcputil.ServiceAccountCredentialsTemplate, serviceAccount)
 
 	payload, err := json.Marshal(map[string]interface{}{
 		"sub": serviceAccount,
@@ -20,7 +20,7 @@ func ServiceAccountLoginJwt(
 	if err != nil {
 		return nil, err
 	}
-	jwtReq := &iam.SignJwtRequest{
+	jwtReq := &iamcredentials.SignJwtRequest{
 		Payload: string(payload),
 	}
 	return iamClient.Projects.ServiceAccounts.SignJwt(accountResource, jwtReq).Do()
