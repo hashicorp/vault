71e5ff9a022c15af6415d4cd48fea4ccc88c74d7
Arnav Palnitkar <arnav@hashicorp.com>
2021-05-05 14:03:27 -0700

    Added test for toolbar

--- ui/tests/integration/components/json-editor-test.js
+++ ui/tests/integration/components/json-editor-test.js
@@ -10,17 +10,19 @@ const component = create(jsonEditor);
 module('Integration | Component | json-editor', function(hooks) {
   setupRenderingTest(hooks);
 
-  const setup = async function(context, title, value, options) {
+  const setup = async function(context, title, value, options, showToolbar = true) {
     context.set('value', JSON.stringify(value));
     context.set('options', options);
     context.set('title', title);
-    await render(hbs`{{json-editor title=title value=value options=options}}`);
+    context.set('showToolbar', showToolbar);
+    await render(hbs`{{json-editor title=title value=value options=options showToolbar=showToolbar}}`);
   };
 
   test('it renders', async function(assert) {
     let value = '';
     await setup(this, 'Test title', value, null);
-    assert.equal(component.title, 'Test title', 'renders the title');
+    assert.equal(component.title, 'Test title', 'renders the provided title');
+    assert.equal(component.hasToolbar, true, 'renders the toolbar');
     assert.equal(component.hasJSONEditor, true, 'renders the ivy code mirror component');
     assert.equal(component.canEdit, true, 'json editor is in read only mode');
   });
@@ -32,5 +34,11 @@ module('Integration | Component | json-editor', function(hooks) {
     };
     await setup(this, 'Test title', value, options);
     assert.equal(component.canEdit, false, 'editor is in read only mode');
+  });
+
+  test('it renders the editor without toolbar', async function(assert) {
+    let value = '';
+    await setup(this, 'Test title', value, null, false);
+    assert.equal(component.hasToolbar, false, 'toolbar is not rendered');
   });
 });
--- ui/tests/pages/components/json-editor.js
+++ ui/tests/pages/components/json-editor.js
@@ -2,6 +2,7 @@ import { isPresent, isVisible, text } from 'ember-cli-page-object';
 
 export default {
   title: text('[data-test-component=json-editor-title]'),
+  hasToolbar: isPresent('[data-test-component=json-editor-toolbar]'),
   hasJSONEditor: isPresent('[data-test-component=json-editor]'),
   canEdit: isVisible('div.CodeMirror-gutters'),
 };
