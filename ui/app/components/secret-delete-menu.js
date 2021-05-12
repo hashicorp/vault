e56898393b9e4033db085e5a7efe5af1416a5854
Angel Garbarino <argarbarino@gmail.com>
2021-05-12 12:19:44 -0600

    refining

--- ui/app/components/secret-delete-menu.js
+++ ui/app/components/secret-delete-menu.js
@@ -14,6 +14,28 @@ export default class SecretDeleteMenu extends Component {
   @maybeQueryRecord(
     'capabilities',
     context => {
+      if (!context.args.model) {
+        return;
+      }
+      let backend = context.args.model.backend;
+      let id = context.args.model.id;
+      let path = context.args.isV2 ? `${backend}/data/${id}` : `${backend}/${id}`;
+      return {
+        id: path,
+      };
+    },
+    'isV2',
+    'model',
+    'model.id',
+    'mode'
+  )
+  updatePath;
+  @alias('updatePath.canDelete') canDelete;
+  @alias('updatePath.canUpdate') canUpdate;
+
+  @maybeQueryRecord(
+    'capabilities',
+    context => {
       if (!context.args || !context.modelForData || !context.modelForData.id) return;
       let [backend, id] = JSON.parse(context.args.modelForData.id);
       return {
@@ -91,7 +113,7 @@ export default class SecretDeleteMenu extends Component {
     if (!deleteType) {
       return;
     }
-    if (deleteType === 'destroy-all-versions') {
+    if (deleteType === 'destroy-all-versions' || deleteType === 'v1') {
       let { id } = this.args.model;
       this.args.model.destroyRecord().then(() => {
         this.args.navToNearestAncestor.perform(id);
