0c474552f460abd5c8e9456153557f21eb12e65c
Jimmy Merritello <7191639+jmfury@users.noreply.github.com>
2021-05-17 15:47:43 -0500

    Add HashiConf info to alert banner

--- website/data/alert-banner.js
+++ website/data/alert-banner.js
@@ -1,13 +1,13 @@
-export const ALERT_BANNER_ACTIVE = false
+export const ALERT_BANNER_ACTIVE = true
 
 // https://github.com/hashicorp/react-components/tree/master/packages/alert-banner
 export default {
-  tag: 'New',
-  url:
-    'https://portal.cloud.hashicorp.com/sign-up?utm_source=vault_io&utm_campaign=hcp_vault_ga',
-  text: 'HCP Vault is now Generally Available',
-  linkText: 'Sign up Today',
+  linkText: 'View Schedule',
+  url: 'https://hashiconf.com/europe/?utm_source=DocsBanner',
+  tag: 'June 8-11',
+  text:
+    'The countdown to HashiConf Europe is on, and the full schedule is now live.',
   // Set the `expirationDate prop with a datetime string (e.g. `2020-01-31T12:00:00-07:00`)
   // if you'd like the component to stop showing at or after a certain date
-  expirationDate: '2021-04-30T11:59:00-05:00',
+  expirationDate: null,
 }
