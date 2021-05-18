8c32aab61ad3794544513d73c6d5c3d82496bb10
HridoyRoy <roy@hashicorp.com>
2021-05-18 12:13:47 -0700

    make packages with upgraded go version and sdk

--- vendor/github.com/hashicorp/vault/sdk/version/version_base.go
+++ vendor/github.com/hashicorp/vault/sdk/version/version_base.go
@@ -8,7 +8,7 @@ var (
 	// Whether cgo is enabled or not; set at build time
 	CgoEnabled bool
 
-	Version           = "1.6.4"
+	Version           = "1.6.5"
 	VersionPrerelease = ""
 	VersionMetadata   = ""
 )
