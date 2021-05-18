2c6c988d95698d6e0e7264d0940e26a18369b21b
HridoyRoy <roy@hashicorp.com>
2021-05-18 10:28:14 -0700

    go mod vendor and upgrade sdk version

--- sdk/version/version_base.go
+++ sdk/version/version_base.go
@@ -8,7 +8,7 @@ var (
 	// Whether cgo is enabled or not; set at build time
 	CgoEnabled bool
 
-	Version           = "1.5.8"
-	VersionPrerelease = ""
+	Version           = "1.5.9"
+	VersionPrerelease = "rc"
 	VersionMetadata   = ""
 )
