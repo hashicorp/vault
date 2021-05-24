acbf9663f7ea90f6f203082171e36c05ec8d4fe8
Arnav Palnitkar <arnav@hashicorp.com>
2021-05-24 14:01:47 -0700

    Close menu when link is clicked

--- ui/app/components/cluster-info.js
+++ ui/app/components/cluster-info.js
@@ -2,10 +2,22 @@ import { inject as service } from '@ember/service';
 import { reads } from '@ember/object/computed';
 import Component from '@ember/component';
 
+/**
+ * @module ClusterInfo
+ *
+ * @example
+ * ```js
+ * <ClusterInfo @cluster={{cluster}} @onLinkClick={{action}} />
+ * ```
+ *
+ * @param {object} cluster - details of the current cluster, passed from the parent.
+ * @param {Function} onLinkClick - parent action which determines the behavior on link click
+ */
 export default Component.extend({
   auth: service(),
   store: service(),
   version: service(),
+  cluster: null,
 
   transitionToRoute: function() {
     this.router.transitionTo(...arguments);
