6db9dbbe868509d612b1dd0a2ad9b57e76051935
Angel Garbarino <argarbarino@gmail.com>
2021-05-24 09:35:15 -0600

    triggering test suite

--- ui/app/components/tool-hash.js
+++ ui/app/components/tool-hash.js
@@ -15,7 +15,7 @@ import { action } from '@ember/object';
  *  @errors={{errors}}/>
  * ```
  * @param onClear {Function} - parent action that is passed through. Must be passed as {{action "onClear"}}
- * @param sum=null {String} - property passed from parent to child and then passed back up to parent
+ * @param sum=null {String} - property passed from parent to child and then passed back up to parent.
  * @param algorithm {String} - property returned from parent.
  * @param format {String} - property returned from parent.
  * @param error=null {Object} - errors passed from parent as default then from child back to parent.
