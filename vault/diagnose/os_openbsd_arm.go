767ffd6d01560d13132ba88df6d6a8e1fd48b061
Scott G. Miller <smiller@hashicorp.com>
2021-06-07 15:34:37 -0500

    ...

--- vault/diagnose/os_openbsd_arm.go
+++ vault/diagnose/os_openbsd_arm.go
@@ -5,5 +5,6 @@ package diagnose
 import "context"
 
 func diskUsage(ctx context.Context) error {
-	return SpotSkipped(ctx, "disk usage", "unsupported on this platform")
+	SpotSkipped(ctx, "disk usage", "unsupported on this platform")
+	return nil
 }
