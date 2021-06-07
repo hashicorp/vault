79b5da328446524a77d80f3dd56280bb06103764
Scott G. Miller <smiller@hashicorp.com>
2021-06-07 15:33:48 -0500

    Fix the third dumb thing in this file

--- vault/diagnose/os_openbsd_arm.go
+++ vault/diagnose/os_openbsd_arm.go
@@ -5,5 +5,5 @@ package diagnose
 import "context"
 
 func diskUsage(ctx context.Context) error {
-	SpotSkipped(ctx, "disk usage", "unsupported on this platform")
+	return SpotSkipped(ctx, "disk usage", "unsupported on this platform")
 }
