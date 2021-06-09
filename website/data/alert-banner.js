b9e1af3efceedb08df06fd96516d0b9ad5634c97
Mike Wickett <mike@wickett.ca>
2021-06-09 09:02:37 -0400

    website: update alert banner

--- website/data/alert-banner.js
+++ website/data/alert-banner.js
@@ -1,12 +1,12 @@
 export const ALERT_BANNER_ACTIVE = true
 
-// https://github.com/hashicorp/react-components/tree/main/packages/alert-banner
+// https://github.com/hashicorp/web-components/tree/master/packages/alert-banner
 export default {
-  tag: 'June 8-11',
-  url: 'https://hashiconf.com/europe/?utm_source=DocsBanner',
-  text: 'HashiConf Europe is happening now. Join all sessions live June 8 & 9.',
-  linkText: 'Join Now',
+  tag: 'Thank you',
+  url: 'https://hashiconf.com/europe',
+  text: 'HashiConf Europe is a wrap. Watch this year’s sessions on-demand.',
+  linkText: 'Watch Now',
   // Set the `expirationDate prop with a datetime string (e.g. `2020-01-31T12:00:00-07:00`)
   // if you'd like the component to stop showing at or after a certain date
-  expirationDate: `2021-06-12T12:00:00-07:00`,
+  expirationDate: `2021-06-20T12:00:00-07:00`,
 }
