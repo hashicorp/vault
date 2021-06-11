38b6271de1eb3344f365964be83239a93ea8f9ea
Josh Black <raskchanky@users.noreply.github.com>
2021-06-11 09:08:19 -0700

    OSS parts of sighup license reload test (#11816)

--- command/server.go
+++ command/server.go
@@ -99,10 +99,11 @@ type ServerCommand struct {
 
 	cleanupGuard sync.Once
 
-	reloadFuncsLock *sync.RWMutex
-	reloadFuncs     *map[string][]reloadutil.ReloadFunc
-	startedCh       chan (struct{}) // for tests
-	reloadedCh      chan (struct{}) // for tests
+	reloadFuncsLock   *sync.RWMutex
+	reloadFuncs       *map[string][]reloadutil.ReloadFunc
+	startedCh         chan (struct{}) // for tests
+	reloadedCh        chan (struct{}) // for tests
+	licenseReloadedCh chan (error)    // for tests
 
 	allLoggers []log.Logger
 
@@ -1564,8 +1565,13 @@ func (c *ServerCommand) Run(args []string) int {
 			}
 
 			// Reload license file
-			if err := vault.LicenseReload(core); err != nil {
-				c.UI.Error(fmt.Sprintf("Error reloading license: %v", err))
+			if err = vault.LicenseReload(core); err != nil {
+				c.UI.Error(err.Error())
+			}
+
+			select {
+			case c.licenseReloadedCh <- err:
+			default:
 			}
 
 		case <-c.SigUSR2Ch:
