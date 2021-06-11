f033fb7607638521ca90b2291783657c6a0bc981
Arnav Palnitkar <arnav@hashicorp.com>
2021-06-11 11:11:08 -0700

    Leverage parent action directly from components

--- ui/app/components/transit-key-action/export.js
+++ ui/app/components/transit-key-action/export.js
@@ -1,14 +1,5 @@
 import Component from '@glimmer/component';
-import { action } from '@ember/object';
-
 export default class ExportComponent extends Component {
-  @action
-  onSubmit(...args) {
-    this.args.doSubmit(...args);
-  }
-
-  @action
-  toggleModal(data) {
-    this.args.toggleModal(data);
-  }
+  wrapTTL = null;
+  exportVersion = false;
 }
