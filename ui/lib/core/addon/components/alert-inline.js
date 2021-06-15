a036b3a4d15ed59d7d83ff5d65f5403d740701b1
Angel Garbarino <Monkeychip@users.noreply.github.com>
2021-06-15 09:21:54 -0600

    Implement ember-cp-validations on KV secret engine (#11785)
    * initial setup
    
    * initial validation setup for empty path object.
    
    * removal console logs
    
    * validation on keyup for kv
    
    * in progress
    
    * making some progress
    
    * more progress
    
    * closer
    
    * done with create page now to fix edit page that I broke
    
    * fix secret edit display on create
    
    * test and final touches
    
    * cleanup mountbackendform
    
    * cleanup
    
    * add changelog
    
    * address pr comments
    
    * address styling pr comment

--- ui/lib/core/addon/components/alert-inline.js
+++ ui/lib/core/addon/components/alert-inline.js
@@ -16,7 +16,7 @@ import layout from '../templates/components/alert-inline';
  * @param [message=null]{String} - The message to display within the alert.
  * @param [sizeSmall=false]{Boolean} - Whether or not to display a small font with padding below of alert message.
  * @param [paddingTop=false]{Boolean} - Whether or not to add padding above component.
- *
+ * @param [isMarginless=false]{Boolean} - Whether or not to remove margin bottom below component.
  */
 
 export default Component.extend({
@@ -26,7 +26,7 @@ export default Component.extend({
   sizeSmall: false,
   paddingTop: false,
   classNames: ['message-inline'],
-  classNameBindings: ['sizeSmall:size-small', 'paddingTop:padding-top'],
+  classNameBindings: ['sizeSmall:size-small', 'paddingTop:padding-top', 'isMarginless:is-marginless'],
 
   textClass: computed('type', function() {
     if (this.type == 'danger') {
