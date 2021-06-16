dccf6527dfb342489d2f7e148b0f2d104b560043
HridoyRoy <roy@hashicorp.com>
2021-06-16 09:26:59 -0700

    add todo comment for windows root ownership

--- vault/diagnose/file_checks_windows.go
+++ vault/diagnose/file_checks_windows.go
@@ -4,7 +4,8 @@ package diagnose
 
 import "io/fs"
 
-// IsOwnedByRoot does nothing on windows
+// IsOwnedByRoot does nothing on windows for now.
+// TODO: find an equivalent check for file ownership in windows.
 func IsOwnedByRoot(info fs.FileInfo) bool {
 	return false
 }
