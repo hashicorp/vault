e6ddc8382d3dcb2a708febc80c1b8f05dd03adfc
Meggie <meggie@hashicorp.com>
2021-07-16 09:39:51 -0400

    Updating changelog link to main (#12110)

--- website/data/version.js
+++ website/data/version.js
@@ -1,6 +1,6 @@
 export const VERSION = '1.7.3'
 export const CHANGELOG_URL =
-  'https://github.com/hashicorp/vault/blob/master/CHANGELOG.md#173'
+  'https://github.com/hashicorp/vault/blob/main/CHANGELOG.md#173'
 
 // HashiCorp officially supported package managers
 export const packageManagers = [
