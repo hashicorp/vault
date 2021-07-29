640f8eb5b3ab4d76647ed5a95dd08df63d5bea0f
Angel Garbarino <argarbarino@gmail.com>
2021-07-29 14:06:28 -0600

    show metadata

--- ui/app/routes/vault/cluster/secrets/backend/metadata.js
+++ ui/app/routes/vault/cluster/secrets/backend/metadata.js
@@ -7,43 +7,21 @@ export default class MetadataShow extends Route {
   @service store;
   // ARG TODO unloadmodelroute
   model(params) {
-    console.log('model hook', params);
     let { secret } = params; // of dynamic route /*secret
-    let parentModel = this.modelFor('vault.cluster.secrets.backend');
     return this.store
       .queryRecord('secret-v2', {
         backend: 'kv',
         id: secret,
       })
       .then(record => {
-        console.log(record, 'RECORD!!!');
+        // ARG TODO remove when API is done
+        record.customMetadata = {
+          foo: 'abc',
+          bar: '123',
+          baz: '5c07d823-3810-48f6-a147-4c06b5219e84',
+        };
         return record;
       });
     // make an API request that uses the id
   }
 }
-// params because its a dynamic segment
-// constructor() {
-//   super(...arguments);
-//   let test = this.paramsFor(this.routeName);
-//   console.log(test, 'l;ajdf');
-//   // this.fetchCapabilities();
-// }
-
-// controller.setProperties({
-//   model: model.lease,
-//   capabilities: model.capabilities,
-//   baseKey: { id: secret },
-// });
-// @service store;
-// // …
-// @action
-// async visitUserProfile(id) {
-//   this.store.findRecord('user', id).then(function (user) {
-//     // Success callback
-//     this.transitionTo('user.profile', user);
-//   }).catch(function () {
-//     // Error callback
-//     this.transitionTo('not-found', 404);
-//   }
-// }
