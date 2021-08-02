fa488baa869a687b0d8123e75612ee81f15a290a
HridoyRoy <roy@hashicorp.com>
2021-08-02 14:22:29 -0700

    fix advice

--- vault/diagnose/os_common.go
+++ vault/diagnose/os_common.go
@@ -32,11 +32,11 @@ partLoop:
 			Warn(ctx, fmt.Sprintf("Could not obtain partition usage for %s: %v.", partition.Mountpoint, err))
 		} else {
 			if usage.UsedPercent > 95 {
-				SpotWarn(ctx, testName, fmt.Sprintf(partition.Mountpoint+" is %.2f percent full.", usage.UsedPercent))
-				Advise(ctx, "It is recommended to have more than five percent of the partition free.")
+				SpotWarn(ctx, testName, fmt.Sprintf(partition.Mountpoint+" is %.2f percent full.", usage.UsedPercent),
+					Advice("It is recommended to have more than five percent of the partition free."))
 			} else if usage.Free < 1<<30 {
-				SpotWarn(ctx, testName, fmt.Sprintf(partition.Mountpoint+" has %s free.", humanize.Bytes(usage.Free)))
-				Advise(ctx, "It is recommended to have at least 1 GB of space free per partition.")
+				SpotWarn(ctx, testName, fmt.Sprintf(partition.Mountpoint+" has %s free.", humanize.Bytes(usage.Free)),
+					Advice("It is recommended to have at least 1 GB of space free per partition."))
 			} else {
 				SpotOk(ctx, testName, partition.Mountpoint+" usage ok.")
 			}
