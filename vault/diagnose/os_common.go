5454df9f815b23b851c6301f694be9efcc45f563
HridoyRoy <roy@hashicorp.com>
2021-08-02 07:34:27 -0700

    fix diagnose formatting errors

--- vault/diagnose/os_common.go
+++ vault/diagnose/os_common.go
@@ -31,10 +31,10 @@ partLoop:
 			Warn(ctx, fmt.Sprintf("Could not obtain partition usage for %s: %v.", partition.Mountpoint, err))
 		} else {
 			if usage.UsedPercent > 95 {
-				SpotWarn(ctx, testName, fmt.Sprintf(partition.Mountpoint+" is %d percent full.", usage.UsedPercent))
+				SpotWarn(ctx, testName, fmt.Sprintf(partition.Mountpoint+" is %f percent full.", usage.UsedPercent))
 				Advise(ctx, "It is recommended to have more than five percent of the partition free.")
 			} else if usage.Free < 2<<30 {
-				SpotWarn(ctx, testName, partition.Mountpoint+" has %d bytes full.")
+				SpotWarn(ctx, testName, fmt.Sprintf(partition.Mountpoint+" has %d bytes free.", usage.Free))
 				Advise(ctx, "It is recommended to have at least 1 GB of space free per partition.")
 			} else {
 				SpotOk(ctx, testName, partition.Mountpoint+" usage ok.")
