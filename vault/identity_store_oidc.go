194510d425b8d4d4abd53b2250afc8be6487cbbc
JM Faircloth <jmfaircloth@hashicorp.com>
2021-08-04 11:59:39 -0500

    identity: allow creating a role with a non-existent key

--- vault/identity_store_oidc.go
+++ vault/identity_store_oidc.go
@@ -989,7 +989,7 @@ func (i *IdentityStore) pathOIDCCreateUpdateRole(ctx context.Context, req *logic
 		role.TokenTTL = time.Duration(d.Get("ttl").(int)) * time.Second
 	}
 
-	// get the key referenced by this role
+	// get the key referenced by this role if it exists
 	var key namedKey
 	entry, err := req.Storage.Get(ctx, namedKeyConfigPath+role.Key)
 	if err != nil {
@@ -999,10 +999,11 @@ func (i *IdentityStore) pathOIDCCreateUpdateRole(ctx context.Context, req *logic
 		if err := entry.DecodeJSON(&key); err != nil {
 			return nil, err
 		}
-	}
 
-	if role.TokenTTL > key.VerificationTTL {
-		return logical.ErrorResponse("a role's token ttl cannot be longer than the verification_ttl of the key it references"), nil
+		if role.TokenTTL > key.VerificationTTL {
+			return logical.ErrorResponse("a role's token ttl cannot be longer than the verification_ttl of the key it references"), nil
+		}
+
 	}
 
 	if clientID, ok := d.GetOk("client_id"); ok {
