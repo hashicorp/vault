b5b94417c427cef6b25ace934ae0a7b67ed21a1e
Claire Bontempo <cbontempo@hashicorp.com>
2021-08-04 11:52:04 -0700

    select months

--- ui/app/components/calendar-widget.js
+++ ui/app/components/calendar-widget.js
@@ -58,46 +58,72 @@ class CalendarWidget extends Component {
     }
   }
 
+  createRange(start, end) {
+    return Array(end - start + 1)
+      .fill()
+      .map((_, idx) => start + idx);
+  }
+
   @action
-  selectMonths(e) {
-    const innerText = e.target.textContent;
-    let result;
-    let months = [];
-    switch (innerText) {
-      case 'Last month':
-        result = this.calculateLastMonth();
-        this.selectJuly = true;
-        months.push(format(result, 'MMMM'));
-        console.log(months);
-        break;
-      case 'Last 3 months':
-        result = eachMonthOfInterval({
-          start: sub(this.currentDate(), { months: 3 }),
-          end: this.currentDate(),
-        });
-        result.forEach(date => months.push(format(date, 'MMMM')));
-        console.log(months);
-        break;
-      case 'Last 6 months':
-        result = eachMonthOfInterval({
-          start: sub(this.currentDate(), { months: 6 }),
-          end: this.currentDate(),
-        });
-        result.forEach(date => months.push(format(date, 'MMMM')));
-        console.log(months);
-        break;
-      case 'Last 12 months':
-        result = eachMonthOfInterval({
-          start: sub(this.currentDate(), { months: 12 }),
-          end: this.currentDate(),
-        });
-        result.forEach(date => months.push(format(date, 'MMMM')));
-        console.log(months);
-        break;
-      default:
-        console.log('Incorrect input');
-    }
+  selectMonths(number) {
+    // define current month
+    // add 12 if negative
+    let lastMonth = parseInt(format(this.currentDate(), 'M')) - 1;
+    console.log(lastMonth);
+    let startRange = lastMonth - number; // subtract one to skip current month
+    let selectedRange = this.createRange(startRange, lastMonth); // array of integers
+    let selectedRangeIds = selectedRange.map(n => `month-${n}`);
+    let allMonths = document.querySelectorAll('.month-list');
+    let range = [];
+    allMonths.forEach(monthElement => {
+      selectedRangeIds.includes(monthElement.id) ? range.push(monthElement) : '';
+    });
+    range.forEach(element => element.classList.add('is-selected'));
+    allMonths.forEach(monthElement => {
+      range.includes(monthElement) ? '' : monthElement.classList.remove('is-selected');
+    });
   }
 }
-
 export default setComponentTemplate(layout, CalendarWidget);
+// change class to "is-selected"
+
+//   @action
+//   selectMonths(e) {
+//     const innerText = e.target.textContent;
+//     let result;
+//     let months = [];
+//     switch (innerText) {
+//       case 'Last month':
+//         result = this.calculateLastMonth();
+//         this.selectJuly = true;
+//         months.push(format(result, 'MMMM'));
+//         console.log(months);
+//         break;
+//       case 'Last 3 months':
+//         result = eachMonthOfInterval({
+//           start: sub(this.currentDate(), { months: 3 }),
+//           end: this.currentDate(),
+//         });
+//         result.forEach(date => months.push(format(date, 'MMMM')));
+//         console.log(months);
+//         break;
+//       case 'Last 6 months':
+//         result = eachMonthOfInterval({
+//           start: sub(this.currentDate(), { months: 6 }),
+//           end: this.currentDate(),
+//         });
+//         result.forEach(date => months.push(format(date, 'MMMM')));
+//         console.log(months);
+//         break;
+//       case 'Last 12 months':
+//         result = eachMonthOfInterval({
+//           start: sub(this.currentDate(), { months: 12 }),
+//           end: this.currentDate(),
+//         });
+//         result.forEach(date => months.push(format(date, 'MMMM')));
+//         console.log(months);
+//         break;
+//       default:
+//         console.log('Incorrect input');
+//     }
+//   }
