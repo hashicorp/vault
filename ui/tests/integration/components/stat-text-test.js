29987bd5faf1ec8560b5a3b1b79cb63e6c7ebb79
Claire Bontempo <cbontempo@hashicorp.com>
2021-08-12 10:20:28 -0500

    finishes tests

--- ui/tests/integration/components/stat-text-test.js
+++ ui/tests/integration/components/stat-text-test.js
@@ -3,7 +3,7 @@ import { setupRenderingTest } from 'ember-qunit';
 import { render } from '@ember/test-helpers';
 import { hbs } from 'ember-cli-htmlbars';
 
-module('Integration | Component | stat-text', function(hooks) {
+module('Integration | Component | StatText', function(hooks) {
   setupRenderingTest(hooks);
 
   test('it renders', async function(assert) {
@@ -13,14 +13,20 @@ module('Integration | Component | stat-text', function(hooks) {
     await render(hbs`<StatText />`);
 
     assert.equal(this.element.textContent.trim(), '');
+  });
 
-    // Template block usage:
-    await render(hbs`
-      <StatText>
-        template block text
-      </StatText>
-    `);
+  test('it renders passed in attributes', async function(assert) {
+    this.set('label', 'A Label');
+    this.set('value', '9,999');
+    this.set('size', 'l');
+    this.set('subText', 'This is my description');
 
-    assert.equal(this.element.textContent.trim(), 'template block text');
+    await render(
+      hbs`<StatText @label={{this.label}} @size={{this.size}} @value={{this.value}} @subText={{this.subText}}/>`
+    );
+
+    assert.dom('.stat-label').hasText(this.label, 'renders label');
+    assert.dom('.stat-text').hasText(this.subText, 'renders subtext');
+    assert.dom('.stat-value').hasText(this.value, 'renders value');
   });
 });
