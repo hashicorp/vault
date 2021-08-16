0ec54d1d3572bb99e09c473922a8b9bc2506ae61
Claire Bontempo <cbontempo@hashicorp.com>
2021-08-16 15:40:46 -0700

    WIP//barchart

--- ui/lib/core/addon/components/bar-chart.js
+++ ui/lib/core/addon/components/bar-chart.js
@@ -11,29 +11,29 @@
  * @param {string} [param1=defaultValue] - param1 is...
  */
 
-import Component from '@ember/component';
-// import Component from '@glimmer/component';
+import Component from '@glimmer/component';
 import layout from '../templates/components/bar-chart';
 import { setComponentTemplate } from '@ember/component';
-import { select } from 'd3-selection';
 import { action } from '@ember/object';
+import { tracked } from '@glimmer/tracking';
+import { select } from 'd3-selection';
 import { scaleLinear } from 'd3-scale';
 class BarChart extends Component {
-  dataArray = [
-    { namespace: 'top-namespace', count: 1512 },
-    { namespace: 'namespace2', count: 1300 },
-    { namespace: 'longnamenamespace', count: 1200 },
-    { namespace: 'anothernamespace', count: 1004 },
-    { namespace: 'namespacesomething', count: 950 },
-    { namespace: 'namespace5', count: 800 },
-    { namespace: 'namespace', count: 700 },
-    { namespace: 'namespace999', count: 650 },
-    { namespace: 'name-space', count: 600 },
-    { namespace: 'path/to/namespace', count: 300 },
+  dataset = [
+    { label: 'top-namespace', count: 1512 },
+    { label: 'namespace2', count: 1300 },
+    { label: 'longnamenamespace', count: 1200 },
+    { label: 'anothernamespace', count: 1004 },
+    { label: 'namespacesomething', count: 950 },
+    { label: 'namespace5', count: 800 },
+    { label: 'namespace', count: 700 },
+    { label: 'namespace999', count: 650 },
+    { label: 'name-space', count: 600 },
+    { label: 'path/to/namespace', count: 300 },
   ];
 
-  calculateMaxValue(dataset) {
-    let counts = dataset.map(data => data.count);
+  calculateMaxValue(data) {
+    let counts = data.map(data => data.count);
     // turns array of counts into an argument list
     return Math.max(...counts);
   }
@@ -41,18 +41,18 @@ class BarChart extends Component {
   @action
   renderBarChart() {
     let xScale = scaleLinear()
-      .domain([0, this.calculateMaxValue(this.dataArray)]) // min and max values of dataset
-      .range([0, 650]);
+      .domain([0, this.calculateMaxValue(this.dataset)]) // min and max values of dataset
+      .range([0, 90]); // bar length will expand to 90% of container
 
     let svg = select('#bar-chart');
     svg
       .selectAll('rect')
-      .data(this.dataArray)
+      .data(this.dataset)
       .enter()
       .append('rect')
-      .attr('width', namespace => xScale(namespace.count))
+      .attr('width', label => `${xScale(label.count)}%`)
       .attr('height', 6)
-      .attr('y', (namespace, index) => index * 20)
+      .attr('y', (label, index) => index * 20)
       .attr('fill', '#BFD4FF');
   }
 }
