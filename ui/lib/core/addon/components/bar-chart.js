3095db03987e728d57c20c74febf4b0b37536e9e
Claire Bontempo <cbontempo@hashicorp.com>
2021-08-16 15:23:05 -0700

    fixes width of bars

--- ui/lib/core/addon/components/bar-chart.js
+++ ui/lib/core/addon/components/bar-chart.js
@@ -17,17 +17,24 @@ import layout from '../templates/components/bar-chart';
 import { setComponentTemplate } from '@ember/component';
 import { select } from 'd3-selection';
 import { action } from '@ember/object';
-import { scaleLinear, scaleBand } from 'd3-scale';
+import { scaleLinear } from 'd3-scale';
 class BarChart extends Component {
   dataArray = [
-    { namespace: 'top-namespace', count: 87 },
-    { namespace: 'namespace2', count: 43 },
-    { namespace: 'longnamenamespace', count: 23 },
-    { namespace: 'path/to/namespace', count: 14 },
+    { namespace: 'top-namespace', count: 1512 },
+    { namespace: 'namespace2', count: 1300 },
+    { namespace: 'longnamenamespace', count: 1200 },
+    { namespace: 'anothernamespace', count: 1004 },
+    { namespace: 'namespacesomething', count: 950 },
+    { namespace: 'namespace5', count: 800 },
+    { namespace: 'namespace', count: 700 },
+    { namespace: 'namespace999', count: 650 },
+    { namespace: 'name-space', count: 600 },
+    { namespace: 'path/to/namespace', count: 300 },
   ];
 
   calculateMaxValue(dataset) {
     let counts = dataset.map(data => data.count);
+    // turns array of counts into an argument list
     return Math.max(...counts);
   }
 
@@ -35,7 +42,7 @@ class BarChart extends Component {
   renderBarChart() {
     let xScale = scaleLinear()
       .domain([0, this.calculateMaxValue(this.dataArray)]) // min and max values of dataset
-      .range([0, 150]);
+      .range([0, 650]);
 
     let svg = select('#bar-chart');
     svg
@@ -45,7 +52,8 @@ class BarChart extends Component {
       .append('rect')
       .attr('width', namespace => xScale(namespace.count))
       .attr('height', 6)
-      .attr('y', (namespace, index) => index * 20);
+      .attr('y', (namespace, index) => index * 20)
+      .attr('fill', '#BFD4FF');
   }
 }
 
