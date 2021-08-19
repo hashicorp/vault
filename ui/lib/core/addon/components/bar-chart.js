3611871c6af1fc2038f8e28be585574f4449020c
Claire Bontempo <cbontempo@hashicorp.com>
2021-08-19 10:11:59 -0700

    fixes styling and spacing

--- ui/lib/core/addon/components/bar-chart.js
+++ ui/lib/core/addon/components/bar-chart.js
@@ -23,8 +23,8 @@ import { axisLeft } from 'd3-axis';
 
 const BAR_THICKNESS = 6; // bar thickness in pixels;
 const BAR_SPACING = 20; // spacing between bars in pixels
-const CHART_MARGIN = { top: 0, right: 24, bottom: 26, left: 137 };
-
+const CHART_MARGIN = { top: 10, right: 24, bottom: 26, left: 120 };
+const BAR_COLORS = ['#BFD4FF', '#8AB1FF'];
 class BarChart extends Component {
   // make xValue and yValue consts? i.e. yValue = dataset.map(d => d.label)
   dataset = [
@@ -34,7 +34,7 @@ class BarChart extends Component {
     { label: 'anothernamespace', count: 1004, unique: 150 },
     { label: 'namespacesomething', count: 950, unique: 100 },
     { label: 'namespace5', count: 800, unique: 75 },
-    { label: 'namespace', count: 700, unique: 50 },
+    { label: 'namespace', count: 400, unique: 300 },
     { label: 'namespace999', count: 650, unique: 40 },
     { label: 'name-space', count: 600, unique: 20 },
     { label: 'path/to/namespace', count: 300, unique: 10 },
@@ -48,16 +48,16 @@ class BarChart extends Component {
 
     let xScale = scaleLinear()
       .domain([0, max(dataset, d => d.count + d.unique)]) // min and max values of dataset
-      .range([0, 100]); // range is 0-100%
+      .range([0, 75]); // range in percent (25% reserved for margins)
 
     let yScale = scaleBand()
       .domain(dataset.map(d => d.label))
-      .range([0, 193]);
-
-    let yAxis = axisLeft(yScale);
+      .range([0, dataset.length * 24]) // each bar element has a thickness (bar + padding) of 24 pixels
+      // paddingInner takes a number between 0 and 1
+      // it tells the scale the percent of the total width it should reserve for white space between bars
+      .paddingInner(0.765);
 
     let svg = select(element);
-    let colors = ['#BFD4FF', '#8AB1FF'];
     // add a group for each row of data
     let groups = svg
       .selectAll('g')
@@ -65,19 +65,21 @@ class BarChart extends Component {
       .enter()
       .append('g')
       .attr('transform', `translate(${CHART_MARGIN.left}, ${CHART_MARGIN.top})`)
-      .style('fill', (d, i) => colors[i]);
+      .style('fill', (d, i) => BAR_COLORS[i]);
 
+    let yAxis = axisLeft(yScale);
     yAxis(groups.append('g'));
+
     // add a rect for each data value
     let rects = groups
       .selectAll('rect')
       .data(d => d)
       .enter()
       .append('rect')
-      .attr('width', value => `${xScale(value[1] - value[0])}%`)
-      .attr('height', BAR_THICKNESS)
-      .attr('x', value => `${xScale(value[0])}%`)
-      .attr('y', (label, index) => index * BAR_SPACING);
+      .attr('width', data => `${xScale(data[1] - data[0])}%`)
+      .attr('height', yScale.bandwidth())
+      .attr('x', data => `${xScale(data[0])}%`)
+      .attr('y', data => yScale(data.data.label));
   }
 }
 
