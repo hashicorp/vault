d4ece1ddc2ba62bd73b0b10f125336464dc405ec
Angel Garbarino <argarbarino@gmail.com>
2021-08-20 12:56:53 -0600

    add changelog

--- ui/app/controllers/vault/cluster/secrets/backend/actions.js
+++ ui/app/controllers/vault/cluster/secrets/backend/actions.js
@@ -1,4 +1,17 @@
 import Controller from '@ember/controller';
 import BackendCrumbMixin from 'vault/mixins/backend-crumb';
 
-export default Controller.extend(BackendCrumbMixin, {});
+// export default Controller.extend(BackendCrumbMixin, {});
+export default Controller.extend(BackendCrumbMixin, {
+  queryParams: {
+    selectedAction: 'action',
+  },
+
+  actions: {
+    refresh: function() {
+      // closure actions don't bubble to routes,
+      // so we have to manually bubble here
+      this.send('refreshModel');
+    },
+  },
+});
