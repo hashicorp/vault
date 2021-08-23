e48e3aa076d626100c5a97a5ba03242218a9793c
Pratyoy Mukhopadhyay <prat@hashicorp.com>
2021-08-23 13:54:21 -0700

    [VAULT-3268] Update sdk vault version

--- vendor/github.com/hashicorp/vault/sdk/version/version_base.go
+++ vendor/github.com/hashicorp/vault/sdk/version/version_base.go
@@ -8,7 +8,7 @@ var (
 	// Whether cgo is enabled or not; set at build time
 	CgoEnabled bool
 
-	Version           = "1.7.3"
+	Version           = "1.7.4"
 	VersionPrerelease = ""
 	VersionMetadata   = ""
 )
