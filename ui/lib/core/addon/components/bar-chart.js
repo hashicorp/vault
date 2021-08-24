addc811f51dd933e080784557cb1f5851e13e909
Claire Bontempo <cbontempo@hashicorp.com>
2021-08-24 15:55:53 -0700

    WIP//clean up time

--- ui/lib/core/addon/components/bar-chart.js
+++ ui/lib/core/addon/components/bar-chart.js
@@ -4,7 +4,7 @@
  *
  * @example
  * ```js
- * <BarChart @requiredParam={requiredParam} @optionalParam={optionalParam} @param1={{param1}}/>
+ * <BarChart @requiredParam={requiredParam} @optionalParam={optionalParam} @param1={{param1}} @onClick/>
  * ```
  * @param {object} requiredParam - requiredParam is...
  * @param {string} [optionalParam] - optionalParam is...
@@ -20,16 +20,58 @@ import { scaleLinear, scaleBand } from 'd3-scale';
 import { max } from 'd3-array';
 import { stack } from 'd3-shape';
 import { axisLeft } from 'd3-axis';
-import { transition } from 'd3-transition';
-import { format } from 'd3-format';
 
 const CHART_MARGIN = { top: 10, right: 24, bottom: 26, left: 137 }; // makes space for y-axis legend
 const BAR_COLORS_UNSELECTED = ['#BFD4FF', '#8AB1FF'];
 const BAR_COLORS_SELECTED = ['#1563FF', '#0F4FD1'];
 class BarChart extends Component {
-  // make xValue and yValue consts? i.e. yValue = dataset.map(d => d.label)
+  // TODO: make xValue and yValue consts? i.e. yValue = dataset.map(d => d.label)
   variableA = 'Active direct tokens';
   variableB = 'Unique entities';
+  mapKey = [{ key: 'count', label: 'Active direct tokens' }, { key: 'unique', label: 'Unique entities' }];
+  // can pass in @labelKey but default will be 'label'
+  get labelKey() {
+    return this.args.labelKey || 'label';
+  }
+
+  realData = [
+    {
+      namespace_id: 'root',
+      namespace_path: '',
+      counts: {
+        distinct_entities: 268,
+        non_entity_tokens: 985,
+        clients: 1253,
+      },
+    },
+    {
+      namespace_id: 'O0i4m',
+      namespace_path: 'admin/',
+      counts: {
+        distinct_entities: 648,
+        non_entity_tokens: 220,
+        clients: 868,
+      },
+    },
+    {
+      namespace_id: '1oihz',
+      namespace_path: 'ns1/',
+      counts: {
+        distinct_entities: 547,
+        non_entity_tokens: 337,
+        clients: 884,
+      },
+    },
+    {
+      namespace_id: '1oihz',
+      namespace_path: 'ns1/',
+      counts: {
+        distinct_entities: 8078,
+        non_entity_tokens: 5349,
+        clients: 13427,
+      },
+    },
+  ];
 
   dataset = [
     { label: 'top-namespace', count: 1212, unique: 300, total: 1512 },
@@ -52,10 +94,12 @@ class BarChart extends Component {
   renderBarChart(element) {
     let dataset = this.dataset.sort((a, b) => a.count + a.unique - (b.count + b.unique)).reverse();
     let totalActive = this.totalActive;
-    let stackFunction = stack().keys(['count', 'unique']);
+    let stackFunction = stack().keys(this.mapKey.map(l => l.key));
     let stackedData = stackFunction(dataset); // returns an array of coordinates for each group of rectangles, first group is for counts (left), second for unique (right)
     let container = select('.bar-chart-container');
-
+    let handleClick = this.args.onClick;
+    let keys = this.mapKey.map(e => e.key);
+    let labelKey = this.labelKey;
     // creates and appends tooltip
     container
       .append('div')
@@ -69,11 +113,11 @@ class BarChart extends Component {
       .style('border-radius', '4px');
 
     let xScale = scaleLinear()
-      .domain([0, max(dataset, d => d.count + d.unique)])
-      .range([0, 70]); // 30% reserved for margins
+      .domain([0, max(dataset, d => keys.reduce((prevValue, currValue) => prevValue + d[currValue], 0))])
+      .range([0, 75]); // 25% reserved for margins
 
     let yScale = scaleBand()
-      .domain(dataset.map(d => d.label))
+      .domain(dataset.map(d => d[labelKey]))
       // each bar element (bar + padding) has a thickness  of 24 pixels
       .range([0, dataset.length * 24])
       // paddingInner takes a number between 0 and 1
@@ -81,53 +125,7 @@ class BarChart extends Component {
       .paddingInner(0.765);
 
     let chartSvg = select(element);
-    chartSvg.attr('height', (dataset.length + 1) * 24);
-
-    // must append background bars first so they are behind data bars
-    let backgroundBars = chartSvg
-      .selectAll('.background-bar')
-      .data(dataset)
-      .enter()
-      .append('rect')
-      .style('cursor', 'pointer')
-      .attr('class', 'background-bar')
-      .attr('width', '100%')
-      .attr('height', '24px')
-      .attr('x', '0')
-      .attr('y', ({ label }) => yScale(label))
-      .style('fill', '#EBEEF2')
-      .style('opacity', '0');
-
-    // BACKGROUND BAR MOUSEOVER EVENT
-    backgroundBars
-      .on('mouseover', function(data) {
-        select(this).style('opacity', 1);
-        let dataBars = chartSvg.selectAll('rect.data-bar').filter(function() {
-          return select(this).attr('y') === `${event.target.getAttribute('y')}`;
-        });
-        dataBars.style('fill', (b, i) => `${BAR_COLORS_SELECTED[i]}`);
-        // TOOLTIP TEXT:
-        select('.chart-tooltip')
-          .transition()
-          .duration(200)
-          .style('opacity', 1).text(` 
-      ${Math.round((data.total * 100) / totalActive)}% of total client counts: \n
-      ${data.unique} unique entities, ${data.count} active tokens.
-      `);
-      })
-      .on('mouseout', function() {
-        select(this).style('opacity', 0);
-        let dataBars = chartSvg.selectAll('rect.data-bar').filter(function() {
-          return select(this).attr('y') === `${event.target.getAttribute('y')}`;
-        });
-        dataBars.style('fill', (b, i) => `${BAR_COLORS_UNSELECTED[i]}`);
-        select('.chart-tooltip').style('opacity', 0);
-      })
-      .on('mousemove', function() {
-        select('.chart-tooltip')
-          .style('left', `${xScale(event.pageX - 200)}%`)
-          .style('top', `${event.pageY - 143}px`);
-      });
+    chartSvg.attr('viewBox', `0 0 710 ${(dataset.length + 1) * 24}`);
 
     // creates group for each array of stackedData
     let groups = chartSvg
@@ -152,22 +150,39 @@ class BarChart extends Component {
       .attr('width', data => `${xScale(data[1] - data[0] - 6)}%`)
       .attr('height', yScale.bandwidth())
       .attr('x', data => `${xScale(data[0])}%`)
-      .attr('y', ({ data }) => yScale(data.label))
+      .attr('y', ({ data }) => yScale(data[labelKey]))
       .attr('rx', 3)
-      .attr('ry', 3);
+      .attr('ry', 3)
+      .attr('border', 1);
 
-    // THIN RECTANGLE MOUSEOVER EVENT
-    rects
-      .on('mouseover', function({ data }) {
+    let actionBars = chartSvg
+      .selectAll('.foreground-bar')
+      .data(dataset)
+      .enter()
+      .append('rect')
+      .style('cursor', 'pointer')
+      .attr('class', 'foreground-bar')
+      .attr('width', '100%')
+      .attr('height', '24px')
+      .attr('x', '0')
+      .attr('y', ({ label }) => yScale(label))
+      .style('fill', '#EBEEF2')
+      .style('opacity', '0')
+      .style('mix-blend-mode', 'multiply');
+
+    actionBars
+      .on('click', function(barData) {
+        if (handleClick) {
+          handleClick(barData);
+        }
+      })
+      .on('mouseover', function(data) {
+        select(this).style('opacity', 1);
         let dataBars = chartSvg.selectAll('rect.data-bar').filter(function() {
           return select(this).attr('y') === `${event.target.getAttribute('y')}`;
         });
         dataBars.style('fill', (b, i) => `${BAR_COLORS_SELECTED[i]}`);
-        let backgroundBar = chartSvg.selectAll('rect.background-bar').filter(function() {
-          return select(this).attr('y') === `${event.target.getAttribute('y')}`;
-        });
-        backgroundBar.style('opacity', 1);
-        // TOOLTIP TEXT:
+        // FUTURE TODO: Make tooltip text a function
         select('.chart-tooltip')
           .transition()
           .duration(200)
@@ -177,23 +192,20 @@ class BarChart extends Component {
       `);
       })
       .on('mouseout', function() {
+        select(this).style('opacity', 0);
         let dataBars = chartSvg.selectAll('rect.data-bar').filter(function() {
           return select(this).attr('y') === `${event.target.getAttribute('y')}`;
         });
         dataBars.style('fill', (b, i) => `${BAR_COLORS_UNSELECTED[i]}`);
-        let backgroundBar = chartSvg.selectAll('rect.background-bar').filter(function() {
-          return select(this).attr('y') === `${event.target.getAttribute('y')}`;
-        });
-        backgroundBar.style('opacity', 1);
         select('.chart-tooltip').style('opacity', 0);
       })
       .on('mousemove', function() {
         select('.chart-tooltip')
-          .style('left', `${xScale(event.pageX - 200)}%`)
+          .style('left', `${event.pageX - 30}px`)
           .style('top', `${event.pageY - 143}px`);
       });
 
-    // TO DO: fix this inflexible business
+    // TODO: fix this inflexible business
     let totalNumbers = [];
     stackedData[1].forEach(e => {
       let n = e[1];
@@ -226,33 +238,26 @@ class BarChart extends Component {
     // removes axes lines
     groups.selectAll('.domain, .tick line').remove();
 
+    // TODO: y needs to change when move onto another line
+    // 20% of map key is reserved for each symbol + label, calculates starting x coord
+    let startingXCoordinate = 100 - this.mapKey.length * 20;
     let legendSvg = select('.legend');
-    legendSvg
-      .append('circle')
-      .attr('cx', '60%')
-      .attr('cy', '50%')
-      .attr('r', 6)
-      .style('fill', '#BFD4FF');
-    legendSvg
-      .append('text')
-      .attr('x', '62%')
-      .attr('y', '50%')
-      .text(`${this.variableA}`)
-      .style('font-size', '.8rem')
-      .attr('alignment-baseline', 'middle');
-    legendSvg
-      .append('circle')
-      .attr('cx', '83%')
-      .attr('cy', '50%')
-      .attr('r', 6)
-      .style('fill', '#8AB1FF');
-    legendSvg
-      .append('text')
-      .attr('x', '85%')
-      .attr('y', '50%')
-      .text(`${this.variableB}`)
-      .style('font-size', '.8rem')
-      .attr('alignment-baseline', 'middle');
+    this.mapKey.map((v, i) => {
+      let xCoordinate = startingXCoordinate + i * 20;
+      legendSvg
+        .append('circle')
+        .attr('cx', `${xCoordinate}%`)
+        .attr('cy', '50%')
+        .attr('r', 6)
+        .style('fill', `${BAR_COLORS_UNSELECTED[i]}`);
+      legendSvg
+        .append('text')
+        .attr('x', `${xCoordinate + 2}%`)
+        .attr('y', '50%')
+        .text(`${v.label}`)
+        .style('font-size', '.8rem')
+        .attr('alignment-baseline', 'middle');
+    });
   }
 }
 
