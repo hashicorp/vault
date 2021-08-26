2be3e753aa03c21f3f517ccc51c8477e7acb5284
Meggie Ladlow <meggie@hashicorp.com>
2021-08-26 19:29:48 -0400

    Updating website for 1.8.2

--- website/data/version.js
+++ website/data/version.js
@@ -1,6 +1,6 @@
-export const VERSION = '1.8.1'
+export const VERSION = '1.8.2'
 export const CHANGELOG_URL =
-  'https://github.com/hashicorp/vault/blob/main/CHANGELOG.md#181'
+  'https://github.com/hashicorp/vault/blob/main/CHANGELOG.md#182'
 
 // HashiCorp officially supported package managers
 export const packageManagers = [
