89bc4862a5c509757581e356c248fc4e094cb8a7
Angel Garbarino <argarbarino@gmail.com>
2021-08-30 14:02:35 -0600

    fix test

--- ui/app/components/secret-create-or-update.js
+++ ui/app/components/secret-create-or-update.js
@@ -169,8 +169,8 @@ export default class SecretCreateOrUpdate extends Component {
     }
     if (
       !model.failedServerRead &&
-      !model.selectedVersion.failedServerRead &&
-      model.selectedVersion.version !== model.currentVersion
+      !model.selectedVersion?.failedServerRead &&
+      model.selectedVersion?.version !== model.currentVersion
     ) {
       return true;
     }
