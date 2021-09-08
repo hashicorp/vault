f49dce290afc8d7a2d6361cdcc277688d646cd46
Arnav Palnitkar <arnav@hashicorp.com>
2021-09-08 09:12:06 -0700

    Removed old tests

--- ui/app/models/metrics/activity.js
+++ ui/app/models/metrics/activity.js
@@ -2,6 +2,7 @@ import Model, { attr } from '@ember-data/model';
 
 export default Model.extend({
   total: attr('object'),
+  by_namespace: attr('array'),
   endTime: attr('string'),
   startTime: attr('string'),
 });
