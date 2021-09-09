379bc0a92cba50bde5a0bd21627f18bd3aa0e643
Angel Garbarino <argarbarino@gmail.com>
2021-09-09 07:34:10 -0600

    refactor

--- ui/app/adapters/secret-engine.js
+++ ui/app/adapters/secret-engine.js
@@ -43,7 +43,7 @@ export default ApplicationAdapter.extend({
     return mountModel;
   },
 
-  createRecord(store, type, snapshot) {
+  async createRecord(store, type, snapshot) {
     const serializer = store.serializerFor(type.modelName);
     let data = serializer.serialize(snapshot);
     const path = snapshot.attr('path');
@@ -58,34 +58,26 @@ export default ApplicationAdapter.extend({
         data.id = path;
       }
       // first create the engine
-      return this.ajax(this.url(path), 'POST', { data })
-        .then(() => {
-          // second create the config request
-          return this.ajax(this.urlForConfig(path), 'POST', { data: configData }).catch(() => {
-            // error here means you do not have update capabilities to config endpoint. If that's the case we show a flash message in the component and continue with the transition.
-            // the error is handled by mount-backend-form component because the save method returns an error and we filter through the error to determine if we should proceed or not.
-            throw new Error('configIssue');
-          });
-        })
-        .then(() => {
-          return {
-            data: assign({}, data, { path: path + '/', id: path }),
-          };
-        })
-        .catch(e => {
-          // if not mount error and already returned then you need to assign the id
-          if (e.message === 'configIssue') {
-            // still proceed with mount sys call and do not throw mountIssue error. The flash message warning is handled before saving
-            return {
-              data: assign({}, data, { path: path + '/', id: path }),
-            };
-          }
-          // if they do not hit a config error then it's a sys/mount error and they should not proceed.
-          if (e.httpStatus === 400) {
-            throw new Error('samePath');
-          }
-          throw new Error('mountIssue');
-        });
+      try {
+        await this.ajax(this.url(path), 'POST', { data });
+      } catch (e) {
+        // if error determine if path duplicate or permissions
+        if (e.httpStatus === 400) {
+          throw new Error('samePath');
+        }
+        throw new Error('mountIssue');
+      }
+      // second post to config
+      try {
+        await this.ajax(this.urlForConfig(path), 'POST', { data: configData });
+      } catch (e) {
+        // error here means you do not have update capabilities to config endpoint. If that's the case we show a flash message in the component and continue with the transition.
+        // the error is handled by mount-backend-form component which checks capabilities before hitting the save to the adapter.
+        // we do not handle the error here because we want the secret-engine to mount successfully and to continue the flow.
+      }
+      return {
+        data: assign({}, data, { path: path + '/', id: path }),
+      };
     } else {
       return this.ajax(this.url(path), 'POST', { data }).then(() => {
         // ember data doesn't like 204s if it's not a DELETE
