68ca6cf59b1a3aaca9007c76f0425774e8930f7d
Arnav Palnitkar <arnav@hashicorp.com>
2021-09-17 14:01:41 -0700

    Added namespace search to client count
    - Used existing search select component for namespace search

--- ui/app/components/clients/history.js
+++ ui/app/components/clients/history.js
@@ -1,8 +1,14 @@
 import Component from '@glimmer/component';
+import { action } from '@ember/object';
+import { tracked } from '@glimmer/tracking';
 
 export default class HistoryComponent extends Component {
   max_namespaces = 10;
 
+  @tracked selectedNamespace = null;
+
+  // Determine if we have client count data based on the current tab,
+  // since model is slightly different for current month vs history api
   get hasClientData() {
     if (this.args.tab === 'current') {
       return this.args.model.activity && this.args.model.activity.clients;
@@ -10,15 +16,34 @@ export default class HistoryComponent extends Component {
     return this.args.model.activity && this.args.model.activity.total;
   }
 
-  get barChartDataset() {
+  // Show namespace graph only if we have more than 1
+  get showGraphs() {
     if (!this.args.model.activity || !this.args.model.activity.byNamespace) {
       return null;
     }
-    let dataset = this.args.model.activity.byNamespace;
-    // Filter out root data
-    dataset = dataset.filter(item => {
-      return item.namespace_id !== 'root';
+    return this.args.model.activity.byNamespace.length > 1;
+  }
+
+  // Construct the namespace model for the search select component
+  get searchDataset() {
+    let dataList = this.cleanUpNamespaces();
+    if (!dataList) {
+      return null;
+    }
+    return dataList.map(d => {
+      return {
+        name: d['namespace_id'],
+        id: d['namespace_path'] || 'root',
+      };
     });
+  }
+
+  // Construct the namespace model for the car chart component
+  get barChartDataset() {
+    let dataset = this.cleanUpNamespaces();
+    if (!dataset) {
+      return null;
+    }
     // Show only top 10 namespaces
     dataset = dataset.slice(0, this.max_namespaces);
     return dataset.map(d => {
@@ -31,10 +56,31 @@ export default class HistoryComponent extends Component {
     });
   }
 
-  get showGraphs() {
+  // Filter out root data
+  cleanUpNamespaces() {
     if (!this.args.model.activity || !this.args.model.activity.byNamespace) {
       return null;
     }
-    return this.args.model.activity.byNamespace.length > 1;
+    let namespaces = this.args.model.activity.byNamespace;
+    namespaces = namespaces.filter(item => {
+      return item.namespace_id !== 'root';
+    });
+    return namespaces;
+  }
+
+  // Set the namespace from the search select picker
+  setNamespace(path) {
+    this.selectedNamespace = this.args.model.activity.byNamespace.find(ns => {
+      return ns.namespace_path === path;
+    });
+  }
+
+  @action
+  initNamespace(value) {
+    if (value && value.length) {
+      this.setNamespace(value[0]);
+    } else {
+      this.selectedNamespace = null;
+    }
   }
 }
