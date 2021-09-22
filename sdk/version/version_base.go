efcd130ed96938304906314846dabad0e64fe2b4
Josh Black <raskchanky@users.noreply.github.com>
2021-09-22 14:09:53 -0700

    Bump version to 1.6.7

--- sdk/version/version_base.go
+++ sdk/version/version_base.go
@@ -8,7 +8,7 @@ var (
 	// Whether cgo is enabled or not; set at build time
 	CgoEnabled bool
 
-	Version           = "1.6.6"
+	Version           = "1.6.7"
 	VersionPrerelease = ""
 	VersionMetadata   = ""
 )
