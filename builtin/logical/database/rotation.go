a1e06bc71c6474e248ef2671e02812e071524bfb
Calvin Leung Huang <1883212+calvn@users.noreply.github.com>
2021-09-21 18:09:03 -0700

    Extract replication state before go routine for initQueue (#11161) (#12603)
    Querying the state before launching the go routine avoids a possible
    race condition with replication.
    
    Co-authored-by: Clint <catsby@users.noreply.github.com>

--- builtin/logical/database/rotation.go
+++ builtin/logical/database/rotation.go
@@ -408,12 +408,11 @@ func (b *databaseBackend) setStaticAccount(ctx context.Context, s logical.Storag
 // not wait for success or failure of it's tasks before continuing. This is to
 // avoid blocking the mount process while loading and evaluating existing roles,
 // etc.
-func (b *databaseBackend) initQueue(ctx context.Context, conf *logical.BackendConfig) {
+func (b *databaseBackend) initQueue(ctx context.Context, conf *logical.BackendConfig, replicationState consts.ReplicationState) {
 	// Verify this mount is on the primary server, or is a local mount. If not, do
 	// not create a queue or launch a ticker. Both processing the WAL list and
 	// populating the queue are done sequentially and before launching a
 	// go-routine to run the periodic ticker.
-	replicationState := conf.System.ReplicationState()
 	if (conf.System.LocalMount() || !replicationState.HasState(consts.ReplicationPerformanceSecondary)) &&
 		!replicationState.HasState(consts.ReplicationDRSecondary) &&
 		!replicationState.HasState(consts.ReplicationPerformanceStandby) {
