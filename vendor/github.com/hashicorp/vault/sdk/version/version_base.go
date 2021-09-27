1030333aafd8ae36303d9ff7e65853f1a76bef88
Meggie Ladlow <meggie@hashicorp.com>
2021-09-27 18:12:37 -0400

    SDK bump for 1.7.5

--- vendor/github.com/hashicorp/vault/sdk/version/version_base.go
+++ vendor/github.com/hashicorp/vault/sdk/version/version_base.go
@@ -8,7 +8,7 @@ var (
 	// Whether cgo is enabled or not; set at build time
 	CgoEnabled bool
 
-	Version           = "1.7.4"
+	Version           = "1.7.5"
 	VersionPrerelease = ""
 	VersionMetadata   = ""
 )
