5d4e6ed74dc769805d05f128697e93d24ef115ba
Meggie Ladlow <meggie@hashicorp.com>
2021-09-27 17:37:18 -0400

    Updating SDK version for 1.8.3

--- sdk/version/version_base.go
+++ sdk/version/version_base.go
@@ -8,7 +8,7 @@ var (
 	// Whether cgo is enabled or not; set at build time
 	CgoEnabled bool
 
-	Version           = "1.8.2"
+	Version           = "1.8.3"
 	VersionPrerelease = ""
 	VersionMetadata   = ""
 )
