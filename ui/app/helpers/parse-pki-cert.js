a9b73d83273d9471499a4ab1ed7767dc92f2b1f8
Claire Bontempo <cbontempo@hashicorp.com>
2021-09-27 09:26:55 -0700

    change error to return

--- ui/app/helpers/parse-pki-cert.js
+++ ui/app/helpers/parse-pki-cert.js
@@ -4,7 +4,7 @@ import { pki } from 'node-forge';
 export function parsePkiCert([model]) {
   // model has to be the responseJSON from PKI serializer
   if (!model.certificate) {
-    throw new Error();
+    return;
   }
   const cert = pki.certificateFromPem(model.certificate);
   const commonName = cert.subject.getField('CN') ? cert.subject.getField('CN').value : null;
