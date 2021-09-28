efb4aebefbcc9b023ba2e8314213e65eace9f258
hashishaw <cshaw@hashicorp.com>
2021-09-28 13:02:47 -0600

    oidc-provider controller has params, template has success message (temporary), model requests correct endpoint

--- ui/app/routes/vault/cluster/oidc-provider.js
+++ ui/app/routes/vault/cluster/oidc-provider.js
@@ -1,3 +1,25 @@
 import Route from '@ember/routing/route';
 
-export default class VaultClusterOidcProviderRoute extends Route {}
+export default class VaultClusterOidcProviderRoute extends Route {
+  beforeModel(transition) {
+    console.log(transition);
+  }
+  async model(params) {
+    console.log({ params });
+    let url = new URL(`${window.origin}/v1/identity/oidc/provider/name/authorize`);
+    Object.keys(params).forEach(key => {
+      if (params[key]) {
+        url.searchParams.append(key, params[key]);
+      }
+    });
+    console.log(url, 'URL');
+    const result = await fetch('/v1/identity/oidc/provider/name/authorize', {
+      method: 'GET',
+    });
+    // const result = await fetch('v1/identity/oidc/provider/name/authorize', {
+    //   method: 'GET',
+    // });
+    console.log({ result });
+    return {};
+  }
+}
