368d810a605c3f85cc87a690b7282eff116efc4a
Sam Salisbury <samsalisbury@gmail.com>
2021-10-07 12:21:18 +0100

    build: ui: debug installing node

--- ui/scripts/setup-node.sh
+++ ui/scripts/setup-node.sh
@@ -5,17 +5,20 @@
 
 set -euo pipefail
 
-# Debugging
-set -x
-
 NODE_VERSION="$(cat .nvmrc)"
 NODE_VERSION="${NODE_VERSION#v}"
 
-echo "==> Setting up node v$NODE_VERSION (from .bashrc)"
+echo "==> Setting up node v$NODE_VERSION (from .nvmrc)"
 
 # shellcheck disable=SC1090
-command -v nvm || { source ~/.bashrc; }
+command -v nvm || {
+	echo "==> Sourcing .bashrc..."
+	source ~/.bashrc
+}
 command -v nvm || { echo "ERROR: nvm not installed"; exit 1; }
 
+echo "==> nvm install $NODE_VERSION"
 nvm install "$NODE_VERSION"
+
+echo "==> nvm use $NODE_VERSION"
 nvm use "$NODE_VERSION"
