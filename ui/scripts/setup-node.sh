d84a07f343e8411bbcaa6dbe296d5443b895531c
Sam Salisbury <samsalisbury@gmail.com>
2021-10-07 12:02:14 +0100

    build: ui: update setup-node script

--- ui/scripts/setup-node.sh
+++ ui/scripts/setup-node.sh
@@ -1,8 +1,15 @@
 #!/usr/bin/env bash
 
+# This script is mostly intended for use in the builder image
+# defined in <reporoot>/packages-oss.yml
+
 set -euo pipefail
 
 NODE_VERSION="$(cat .nvmrc)"
+
+# shellcheck disable=SC1090
+command -v nvm || { source ~/.bashrc; }
+command -v nvm || { echo "ERROR: nvm not installed"; exit 1; }
 
 nvm install "$NODE_VERSION"
 nvm use "$NODE_VERSION"
