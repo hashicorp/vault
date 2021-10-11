bfc9c4bfbd92b611be014b5509e1c19f62476168
hashishaw <cshaw@hashicorp.com>
2021-10-11 11:23:37 -0500

    Fix redirect to/from auth with cluster name, show error and consent form if applicable

--- ui/app/routes/vault/cluster/identity/oidc-provider.js
+++ ui/app/routes/vault/cluster/identity/oidc-provider.js
@@ -1,17 +1,23 @@
 import Route from '@ember/routing/route';
 import { inject as service } from '@ember/service';
 
+const AUTH = 'vault.cluster.auth';
+const PROVIDER = 'vault.cluster.identity.oidc-provider';
+
 export default class VaultClusterIdentityOidcProviderRoute extends Route {
   @service auth;
   @service router;
 
+  get win() {
+    return this.window || window;
+  }
+
   _redirect(url, params) {
     let redir = this._buildUrl(url, params);
-    window.location.replace(redir);
+    this.win.location.replace(redir);
   }
 
   beforeModel(transition) {
-    // const baseUrl = window.location.origin;
     const currentToken = this.auth.get('currentTokenName');
     let { redirect_to, ...qp } = transition.to.queryParams;
     console.debug('DEBUG: removing redirect_to', redirect_to);
@@ -24,50 +30,21 @@ export default class VaultClusterIdentityOidcProviderRoute extends Route {
       if ('login' === qp.prompt?.toLowerCase()) {
         this.auth.deleteCurrentToken();
         qp.prompt = null;
-        console.log('removed prompt');
       }
+      let { cluster_name } = this.paramsFor('vault.cluster');
       let url = this.router.urlFor(transition.to.name, transition.to.params, { queryParams: qp });
-      return this.transitionTo('vault.cluster.auth', { queryParams: { redirect_to: url } });
+      return this.transitionTo(AUTH, cluster_name, { queryParams: { redirect_to: url } });
     }
   }
-  // beforeModel(transition) {
-  //   console.log('beforeModel');
-  //   let redirect = this.router.urlFor('vault.cluster.identity.oidc-provider', { state: 'haha' });
 
-  //   console.log({ redirect });
-  // this.transitionTo('vault.cluster.auth', { queryParams: { redirect_to: 'foobar' } });
-  // let qp = transition.to.queryParams;
-  // let currentToken = this.auth.currentTokenName;
-  // console.log('BEFORE MODEL');
-  // console.log({ qp });
-  // console.log({ currentToken });
-  // let redirect = this.router.currentURL;
-  // if (transition.targetName === this.routeName) {
-  //   return this.replaceWith('vault.cluster.auth', { redirect_to: redirect });
-  // }
-
-  // if (!currentToken && 'none' === qp.prompt?.toLowerCase()) {
-  //   let url = this._buildUrl(qp.redirect_uri, {
-  //     state: qp.state,
-  //     error: 'login_required',
-  //   });
-  //   window.location.replace(url);
-  // } else if (!currentToken) {
-  //   // this.transitionTo('vault.cluster.auth', { redirect_to: redirect });
-  //   return this.replaceWith('vault.cluster.identity.oidc-provider', { ...qp });
-
-  //   // this.router.transitionTo(currentRoute);
-  //   // let redirect_to = `${window.origin}/ui/vault`;
-  //   // window.location.assign(
-  //   //   this._buildUrl(`${window.origin}/ui/vault/auth`, {
-  //   //     ...qp,
-  //   //   })
-  //   // );
-  // } else if ('login' === qp.prompt?.toLowerCase()) {
-  //   // TODO: clear token, redirect to login, redirect_to current route without prompt param
-  //   this.transitionTo('vault.cluster.auth', { redirect_to: redirect });
-  // }
-  // }
+  _redirectToAuth(oidcName, queryParams, logout = false) {
+    let { cluster_name } = this.paramsFor('vault.cluster');
+    let currentRoute = this.router.urlFor(PROVIDER, oidcName, { queryParams });
+    if (logout) {
+      this.auth.deleteCurrentToken();
+    }
+    return this.transitionTo(AUTH, cluster_name, { queryParams: { redirect_to: currentRoute } });
+  }
 
   _buildUrl(urlString, params) {
     try {
@@ -79,7 +56,7 @@ export default class VaultClusterIdentityOidcProviderRoute extends Route {
       });
       return url;
     } catch (e) {
-      console.log('ERROR parsing url', urlString);
+      console.debug('DEBUG: parsing url failed for', urlString);
       throw new Error('Invalid URL');
     }
   }
@@ -87,30 +64,20 @@ export default class VaultClusterIdentityOidcProviderRoute extends Route {
   _handleSuccess(response, baseUrl, state) {
     const { code } = response;
     let redirectUrl = this._buildUrl(baseUrl, { code, state });
-    console.log('REDIRECT TO (SUCCESS)', redirectUrl);
-    // window.location.replace(redirectUrl);
+    this.win.location.replace(redirectUrl);
   }
-  _handleError(response, baseUrl) {
-    const { code } = response;
-    let redirectUrl = this._buildUrl(baseUrl, { error: code });
-    console.log('REDIRECT TO (ERROR)', redirectUrl);
-    // window.location.replace(redirectUrl);
+  _handleError(errorResp, baseUrl) {
+    let redirectUrl = this._buildUrl(baseUrl, { ...errorResp });
+    this.win.location.replace(redirectUrl);
   }
 
   async model(params) {
-    console.log('oidc provider name', params);
     let { oidc_name, ...qp } = params;
-    console.log('oidc_name', oidc_name);
     let decodedRedirect = decodeURI(qp.redirect_uri);
-    let url = this._buildUrl(`${window.origin}/v1/identity/oidc/provider/${oidc_name}/authorize`, qp);
+    let url = this._buildUrl(`${this.win.origin}/v1/identity/oidc/provider/${oidc_name}/authorize`, qp);
     try {
-      // let test = this.router.urlFor('vault.cluster.auth');
-      // console.log({ test });
       const response = await this.auth.ajax(url, 'GET', {});
-      console.log('CONSENT>>>>>>', qp.prompt);
       if ('consent' === qp.prompt?.toLowerCase()) {
-        console.log('SHOULD SHOW CONSENT FORM');
-        // TODO: Show consent before redirect (code val must be passed to action)
         return {
           consent: {
             code: response.code,
@@ -118,26 +85,31 @@ export default class VaultClusterIdentityOidcProviderRoute extends Route {
             state: qp.state,
           },
         };
-      } else {
-        this._handleSuccess(response, decodedRedirect, qp.state);
       }
+      this._handleSuccess(response, decodedRedirect, qp.state);
     } catch (errorRes) {
-      console.error({ errorRes });
-      let err = await errorRes.json();
-      let code = err.error;
+      let resp = await errorRes.json();
+      let code = resp.error;
       if (code === 'max_age_violation') {
-        // TODO: clear token, redirect to login
-        this.auth.deleteCurrentToken();
-        // let url = this.router.urlFor(transition.to.name, transition.to.params, { queryParams: qp });
-        // return this.transitionTo('vault.cluster.auth', { queryParams: { redirect_to: url } });
+        this._redirectToAuth(oidc_name, qp, true);
       } else if (code === 'invalid_redirect_uri') {
-        // TODO: show error (return on model)
+        return {
+          error: {
+            title: 'Redirect URI mismatch',
+            message:
+              'The provided redirect_uri is not in the list of allowed redirect URIs. Please make sure you are sending a valid redirect URI from your application.',
+          },
+        };
       } else if (code === 'invalid_client_id') {
-        // TODO: show error (return on model)
+        return {
+          error: {
+            title: 'Invalid client ID',
+            message: 'Your client ID is invalid. Please update your configuration and try again.',
+          },
+        };
       } else {
-        // TODO: return error as param to callback
+        this._handleError(resp, decodedRedirect);
       }
-      return err;
     }
   }
 }
