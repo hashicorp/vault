a50e0d08f29c6a3774fa003e1aaff138c2e88cc5
Claire Bontempo <cbontempo@hashicorp.com>
2021-10-13 13:24:03 -0700

    add argument to is-empty-value helper to check for default

--- ui/app/helpers/is-empty-value.js
+++ ui/app/helpers/is-empty-value.js
@@ -1,7 +1,10 @@
 import { helper } from '@ember/component/helper';
 
-export default helper(function isEmptyValue([value] /*, hash*/) {
-  if (typeof value === 'object' && value !== null) {
+export default helper(function isEmptyValue([value], { hasDefault = false }) {
+  if (hasDefault) {
+    value = hasDefault;
+  }
+  if (typeof value === 'object' && value !== null && !hasDefault) {
     return Object.keys(value).length === 0;
   }
   return value == null || value === '';
