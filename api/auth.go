61ce205be4cb2989b805613d1e0c21751d79d0ff
Valerie Conklin <val@hashicorp.com>
2021-10-14 10:01:26 -0700

    Fix auth comment length

--- api/auth.go
+++ api/auth.go
@@ -16,8 +16,14 @@ func (c *Client) Auth() *Auth {
 	return &Auth{c: c}
 }
 
-// Login sets up the required request body for login requests to the given auth method's /login API endpoint, and then performs a write to it. After a successful login, this method will automatically set the client's token to the login response's ClientToken as well.
-// The Secret returned is the authentication secret, which if desired can be passed as input to the NewLifetimeWatcher method in order to start automatically renewing the token.
+// Login sets up the required request body for login requests to the given auth
+// method's /login API endpoint, and then performs a write to it. After a
+// successful login, this method will automatically set the client's token to
+// the login response's ClientToken as well.
+//
+// The Secret returned is the authentication secret, which if desired can be
+// passed as input to the NewLifetimeWatcher method in order to start
+// automatically renewing the token.
 func (a *Auth) Login(authMethod AuthMethod) (*Secret, error) {
 	if authMethod == nil {
 		return nil, fmt.Errorf("no auth method provided for login")
