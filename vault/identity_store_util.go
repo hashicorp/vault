0c9e8d13a0aff5909304dacbf9e255f8f23d7db8
Vishal Nayak <vishalnayak@users.noreply.github.com>
2021-10-19 15:05:06 -0400

    Fix entity alias deletion (#12834)
    * Fix entity alias deletion
    
    * Fix tests
    
    * Add CL

--- vault/identity_store_util.go
+++ vault/identity_store_util.go
@@ -1299,14 +1299,17 @@ func (i *IdentityStore) deleteAliasesInEntityInTxn(txn *memdb.Txn, entity *ident
 
 	var remainList []*identity.Alias
 	var removeList []*identity.Alias
-
-	for _, item := range aliases {
-		for _, alias := range entity.Aliases {
+	for _, item := range entity.Aliases {
+		remove := false
+		for _, alias := range aliases {
 			if alias.ID == item.ID {
-				removeList = append(removeList, alias)
-			} else {
-				remainList = append(remainList, alias)
+				remove = true
 			}
+		}
+		if remove {
+			removeList = append(removeList, item)
+		} else {
+			remainList = append(remainList, item)
 		}
 	}
 
