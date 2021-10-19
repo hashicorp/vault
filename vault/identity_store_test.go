72f11bbeb5a0cb321e9a0c403ab3e4b3b98d8990
Vishal Nayak <vishalnayakv@gmail.com>
2021-10-19 10:35:06 -0400

    Fix tests

--- vault/identity_store_test.go
+++ vault/identity_store_test.go
@@ -26,18 +26,20 @@ func TestIdentityStore_DeleteEntityAlias(t *testing.T) {
 	defer txn.Abort()
 
 	alias := &identity.Alias{
-		ID:            "testAliasID1",
-		CanonicalID:   "testEntityID",
-		MountType:     "testMountType",
-		MountAccessor: "testMountAccessor",
-		Name:          "testAliasName",
+		ID:             "testAliasID1",
+		CanonicalID:    "testEntityID",
+		MountType:      "testMountType",
+		MountAccessor:  "testMountAccessor",
+		Name:           "testAliasName",
+		LocalBucketKey: c.identityStore.localAliasPacker.BucketKey("testEntityID"),
 	}
 	alias2 := &identity.Alias{
-		ID:            "testAliasID2",
-		CanonicalID:   "testEntityID",
-		MountType:     "testMountType",
-		MountAccessor: "testMountAccessor",
-		Name:          "testAliasName2",
+		ID:             "testAliasID2",
+		CanonicalID:    "testEntityID",
+		MountType:      "testMountType",
+		MountAccessor:  "testMountAccessor2",
+		Name:           "testAliasName2",
+		LocalBucketKey: c.identityStore.localAliasPacker.BucketKey("testEntityID"),
 	}
 	entity := &identity.Entity{
 		ID:       "testEntityID",
