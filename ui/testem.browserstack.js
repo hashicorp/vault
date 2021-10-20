fb03c71ba4405858b839123bd75563d6ff49d283
Arnav Palnitkar <arnav@hashicorp.com>
2021-10-20 14:33:52 -0700

    Just run with chrome

--- ui/testem.browserstack.js
+++ ui/testem.browserstack.js
@@ -83,7 +83,7 @@ module.exports = {
     },
   },
   launch_in_dev: [],
-  launch_in_ci: ['BS_Chrome_Current', 'BS_Firefox_Current', 'BS_Safari_Current'],
+  launch_in_ci: ['BS_Chrome_Current'],
   proxies: {
     '/v1': {
       target: 'http://localhost:9200',
