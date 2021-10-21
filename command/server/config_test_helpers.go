4ef0d3e187b118ebee71a3a850ffa9532f6dedfe
Nick Cabatoff <ncabatoff@hashicorp.com>
2021-10-21 10:10:48 -0400

    Add support for go-sockaddr templated addresses in config. (#9109)

--- command/server/config_test_helpers.go
+++ command/server/config_test_helpers.go
@@ -843,6 +843,43 @@ listener "tcp" {
 	}
 }
 
+func testParseSockaddrTemplate(t *testing.T) {
+	config, err := ParseConfig(`
+api_addr = <<EOF
+{{- GetAllInterfaces | include "flags" "loopback" | include "type" "ipv4" | attr "address" -}}
+EOF
+listener "tcp" {
+	address = <<EOF
+{{- GetAllInterfaces | include "flags" "loopback" | include "type" "ipv4" | attr "address" -}}:443
+EOF
+	cluster_address = <<EOF
+{{- GetAllInterfaces | include "flags" "loopback" | include "type" "ipv4" | attr "address" -}}:8201
+EOF
+	tls_disable = true
+}`, "")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	expected := &Config{
+		APIAddr: "127.0.0.1",
+		SharedConfig: &configutil.SharedConfig{
+			Listeners: []*configutil.Listener{
+				{
+					Type:           "tcp",
+					Address:        "127.0.0.1:443",
+					ClusterAddress: "127.0.0.1:8201",
+					TLSDisable:     true,
+				},
+			},
+		},
+	}
+	config.Prune()
+	if diff := deep.Equal(config, expected); diff != nil {
+		t.Fatal(diff)
+	}
+}
+
 func testParseSeals(t *testing.T) {
 	config, err := LoadConfigFile("./test-fixtures/config_seals.hcl")
 	if err != nil {
