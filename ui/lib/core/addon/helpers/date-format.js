0d205620b60f22d625445c566f4290d2330de0b5
Claire Bontempo <cbontempo@hashicorp.com>
2021-10-23 15:51:50 -0500

    fixes month displayed on client count page

--- ui/lib/core/addon/helpers/date-format.js
+++ ui/lib/core/addon/helpers/date-format.js
@@ -1,10 +1,14 @@
 import { helper } from '@ember/component/helper';
 import { format, parseISO } from 'date-fns';
 
-export function dateFormat([date, style], { isFormatted = false }) {
+export function dateFormat([date, style], { isFormatted = false, offsetTimezone = false }) {
   // see format breaking in upgrade to date-fns 2.x https://github.com/date-fns/date-fns/blob/master/CHANGELOG.md#changed-5
   if (isFormatted) {
     return format(new Date(date), style);
+  }
+  // remove 'Z' so date range displays correct month (i.e. "Jul 1" instead of "Jun 30")
+  if (date[date.length - 1] === 'Z' && offsetTimezone) {
+    date = date.slice(0, date.length - 1);
   }
   let number = typeof date === 'string' ? parseISO(date) : date;
   if (!number) {
