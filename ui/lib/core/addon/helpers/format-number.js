4e72f21b54e13746dfc293c797add261166d5217
Claire Bontempo <cbontempo@hashicorp.com>
2021-10-25 16:17:40 -0700

    formats client count numbers

--- ui/lib/core/addon/helpers/format-number.js
+++ ui/lib/core/addon/helpers/format-number.js
@@ -2,6 +2,9 @@ import { helper } from '@ember/component/helper';
 
 export function formatNumber([number]) {
   // formats a number according to the locale
+  if (typeof number !== 'number') {
+    return;
+  }
   return new Intl.NumberFormat().format(number);
 }
 
