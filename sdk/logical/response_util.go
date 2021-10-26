5230d79a2fd5d82d9b359f8b7d04a2b5c040b576
swayne275 <swayne275@gmail.com>
2021-10-26 10:11:24 -0600

    backport error changes from ent PR (#12926)

--- sdk/logical/response_util.go
+++ sdk/logical/response_util.go
@@ -120,8 +120,6 @@ func RespondErrorCommon(req *Request, resp *Response, err error) (int, error) {
 			statusCode = http.StatusPreconditionFailed
 		case errwrap.Contains(err, ErrPathFunctionalityRemoved.Error()):
 			statusCode = http.StatusNotFound
-		case errwrap.Contains(err, ErrLocked.Error()):
-			statusCode = http.StatusLocked
 		}
 	}
 
@@ -145,6 +143,10 @@ func AdjustErrorStatusCode(status *int, err error) {
 
 	// Adjust status code when sealed
 	if errwrap.Contains(err, consts.ErrSealed.Error()) {
+		*status = http.StatusServiceUnavailable
+	}
+
+	if errwrap.Contains(err, consts.ErrAPILocked.Error()) {
 		*status = http.StatusServiceUnavailable
 	}
 
