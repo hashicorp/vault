ae6d90b6d7a93f96b7feaff537def3bffbbc6159
Claire Bontempo <cbontempo@hashicorp.com>
2021-10-27 15:25:12 -0700

    add postgres to database plugins

--- ui/app/utils/database-helpers.js
+++ ui/app/utils/database-helpers.js
@@ -153,6 +153,24 @@ export const AVAILABLE_PLUGIN_TYPES = [
       { attr: 'root_rotation_statements', group: 'statements' },
     ],
   },
+  {
+    value: 'postgresql-database-plugin',
+    displayName: 'PostgreSQL',
+    fields: [
+      { attr: 'plugin_name' },
+      { attr: 'name' },
+      { attr: 'verify_connection' },
+      { attr: 'password_policy' },
+      { attr: 'connection_url', group: 'pluginConfig' },
+      { attr: 'username', group: 'pluginConfig', show: false },
+      { attr: 'password', group: 'pluginConfig', show: false },
+      { attr: 'max_open_connections', group: 'pluginConfig' },
+      { attr: 'max_idle_connections', group: 'pluginConfig' },
+      { attr: 'max_connection_lifetime', group: 'pluginConfig' },
+      { attr: 'username_template', group: 'pluginConfig' },
+      { attr: 'root_rotation_statements', group: 'statements' },
+    ],
+  },
 ];
 
 export const ROLE_FIELDS = {
@@ -163,25 +181,27 @@ export const ROLE_FIELDS = {
 export const STATEMENT_FIELDS = {
   static: {
     default: ['rotation_statements'],
+    'elasticsearch-database-plugin': [],
     'mongodb-database-plugin': [],
     'mssql-database-plugin': [],
     'mysql-database-plugin': [],
     'mysql-aurora-database-plugin': [],
-    'mysql-rds-database-plugin': [],
     'mysql-legacy-database-plugin': [],
-    'elasticsearch-database-plugin': [],
+    'mysql-rds-database-plugin': [],
     'oracle-database-plugin': [],
+    'postgresql-database-plugin': [],
   },
   dynamic: {
     default: ['creation_statements', 'revocation_statements', 'rollback_statements', 'renew_statements'],
+    'elasticsearch-database-plugin': ['creation_statement'],
     'mongodb-database-plugin': ['creation_statement', 'revocation_statement'],
     'mssql-database-plugin': ['creation_statements', 'revocation_statements'],
     'mysql-database-plugin': ['creation_statements', 'revocation_statements'],
     'mysql-aurora-database-plugin': ['creation_statements', 'revocation_statements'],
-    'mysql-rds-database-plugin': ['creation_statements', 'revocation_statements'],
     'mysql-legacy-database-plugin': ['creation_statements', 'revocation_statements'],
-    'elasticsearch-database-plugin': ['creation_statement'],
+    'mysql-rds-database-plugin': ['creation_statements', 'revocation_statements'],
     'oracle-database-plugin': ['creation_statements', 'revocation_statements'],
+    'postgresql-database-plugin': ['creation_statements', 'revocation_statements'],
   },
 };
 
