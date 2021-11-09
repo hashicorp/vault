47024a27d503a5c627f240fffc7100a4551b9608
Jordan Reimer <zofskeez@gmail.com>
2021-11-09 15:05:25 -0700

    Raft peer removal bug (#13098)
    * fixes issue removing raft peer via cli not reflected in UI until refresh
    
    * adds changelog entry

--- ui/app/routes/vault/cluster/storage.js
+++ ui/app/routes/vault/cluster/storage.js
@@ -3,7 +3,10 @@ import ClusterRoute from 'vault/mixins/cluster-route';
 
 export default Route.extend(ClusterRoute, {
   model() {
-    return this.store.findAll('server');
+    // findAll method will return all records in store as well as response from server
+    // when removing a peer via the cli, stale records would continue to appear until refresh
+    // query method will only return records from response
+    return this.store.query('server', {});
   },
 
   actions: {
