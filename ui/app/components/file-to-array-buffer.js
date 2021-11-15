73b9f198be39f0213a88f7b704ec41da6192f8ea
Rémi Lapeyre <remi.lapeyre@lenstra.fr>
2021-11-15 20:52:04 +0100

    Fix some typos (#12289)

--- ui/app/components/file-to-array-buffer.js
+++ ui/app/components/file-to-array-buffer.js
@@ -11,7 +11,7 @@ import filesize from 'filesize';
  *   <FileToArrayBuffer @onChange={{action (mut file)}} />
  * ```
  * @param onChange=null {Function} - The function to call when the file read is complete. This function
- * recieves the file as a JS ArrayBuffer
+ * receives the file as a JS ArrayBuffer
  * @param [label=null {String}] - Text to use as the label for the file input
  * @param [fileHelpText=null {String} - Text to use as help under the file input
  *
