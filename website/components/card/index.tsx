0e616cf338143cb51c9b0b97c8adcfd562f27daf
Alex Carpenter <im.alexcarpenter@gmail.com>
2021-11-16 09:25:10 -0500

    adds product logos and hover styles

--- website/components/card/index.tsx
+++ website/components/card/index.tsx
@@ -1,23 +1,21 @@
 import * as React from 'react'
 import Link from 'next/link'
+import InlineSvg from '@hashicorp/react-inline-svg'
 import camelCase from 'camelcase'
 import classNames from 'classnames'
 import { IconArrowRight16 } from '@hashicorp/flight-icons/svg-react/arrow-right-16'
 import { IconExternalLink16 } from '@hashicorp/flight-icons/svg-react/external-link-16'
+import { productLogos } from './product-logos'
 import s from './style.module.css'
 
-function Card({
-  variant = 'light',
-  link,
-  inset = 'md',
-  children,
-}: {
-  variant: 'light' | 'gray' | 'dark'
+interface CardProps {
+  variant?: 'light' | 'gray' | 'dark'
+  products?: Array<keyof typeof productLogos>
   link: {
     url: string
     type: 'inbound' | 'outbound'
   }
-  inset:
+  inset?:
     | 'none'
     | 'sm'
     | 'md'
@@ -26,7 +24,15 @@ function Card({
         vertical: 'none' | 'sm' | 'md'
       }
   children: React.ReactNode
-}) {
+}
+
+function Card({
+  variant = 'light',
+  products,
+  link,
+  inset = 'md',
+  children,
+}: CardProps) {
   const space =
     typeof inset === 'string' ? camelCase(['space', inset.toString()]) : null
   const spaceHorizontal =
@@ -37,6 +43,7 @@ function Card({
     typeof inset === 'object' && inset.vertical
       ? camelCase(['space', 'vertical', inset.vertical])
       : null
+
   const LinkWrapper = ({ className, children }) =>
     link.type === 'inbound' ? (
       <Link href={link.url}>
@@ -52,6 +59,7 @@ function Card({
         {children}
       </a>
     )
+
   return (
     <article className={classNames(s.card)}>
       <LinkWrapper
@@ -64,32 +72,52 @@ function Card({
       >
         {children}
         <footer className={s.footer}>
-          {link.type === 'inbound' ? (
-            <IconArrowRight16 />
-          ) : (
-            <IconExternalLink16 />
+          {products && (
+            <ul className={s.products}>
+              {products.map((product) => {
+                return (
+                  <li>
+                    <InlineSvg className={s.logo} src={productLogos[product]} />
+                  </li>
+                )
+              })}
+            </ul>
           )}
+          <span className={s.linkType}>
+            {link.type === 'inbound' ? (
+              <IconArrowRight16 />
+            ) : (
+              <IconExternalLink16 />
+            )}
+          </span>
         </footer>
       </LinkWrapper>
     </article>
   )
 }
 
-function Eyebrow({ children }: { children: string }) {
+interface EyebrowProps {
+  children: string
+}
+
+function Eyebrow({ children }: EyebrowProps) {
   return <p className={s.eyebrow}>{children}</p>
 }
 
-function Heading({
-  as: Component = 'h2',
-  children,
-}: {
-  as: 'h2' | 'h3' | 'h4'
+interface HeadingProps {
+  as?: 'h2' | 'h3' | 'h4'
   children: React.ReactNode
-}) {
+}
+
+function Heading({ as: Component = 'h2', children }: HeadingProps) {
   return <Component className={s.heading}>{children}</Component>
 }
 
-function Description({ children }: { children: string }) {
+interface DescriptionProps {
+  children: string
+}
+
+function Description({ children }: DescriptionProps) {
   return <p className={s.description}>{children}</p>
 }
 
