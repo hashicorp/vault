4de877a6103e826d79dc0f3e85420bf8f67353de
Alex Carpenter <im.alexcarpenter@gmail.com>
2021-11-16 12:45:12 -0500

    conditionally display person and show helpers

--- website/components/video/index.tsx
+++ website/components/video/index.tsx
@@ -12,10 +12,12 @@ export default function VideoCallout({
   person,
 }) {
   const [showDialog, setShowDialog] = React.useState(false)
+  const showVideo = () => setShowDialog(true)
+  const hideVideo = () => setShowDialog(false)
   return (
     <>
       <figure className={s.videoCallout}>
-        <button className={s.thumbnail} onClick={() => setShowDialog(true)}>
+        <button className={s.thumbnail} onClick={showVideo}>
           <VisuallyHidden>Play video</VisuallyHidden>
           <PlayIcon />
           <Image src={thumbnail} layout="fill" objectFit="cover" />
@@ -23,26 +25,28 @@ export default function VideoCallout({
         <figcaption className={s.content}>
           <h3 className={s.heading}>{heading}</h3>
           <p className={s.description}>{description}</p>
-          <div className={s.person}>
-            <div className={s.personThumbnail}>
-              <Image
-                src={person.thumbnail}
-                width={52}
-                height={52}
-                alt={`${person.name} avatar`}
-              />
+          {person && (
+            <div className={s.person}>
+              <div className={s.personThumbnail}>
+                <Image
+                  src={person.thumbnail}
+                  width={52}
+                  height={52}
+                  alt={`${person.name} avatar`}
+                />
+              </div>
+              <div>
+                <p className={s.personName}>{person.name}</p>
+                <p className={s.personDescription}>{person.description}</p>
+              </div>
             </div>
-            <div>
-              <p className={s.personName}>{person.name}</p>
-              <p className={s.personDescription}>{person.description}</p>
-            </div>
-          </div>
+          )}
         </figcaption>
       </figure>
       <Dialog
         isOpen={showDialog}
-        onDismiss={() => setShowDialog(false)}
-        label="Video"
+        onDismiss={hideVideo}
+        label={`${heading} video}`}
       >
         <div className={s.video}></div>
       </Dialog>
