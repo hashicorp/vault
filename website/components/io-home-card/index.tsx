88ed13250e58d8eebde14abfc9224732c49402c6
Alex Carpenter <im.alexcarpenter@gmail.com>
2021-11-17 15:50:40 -0500

    Homepage `<IoHomeCard />` component (#13151)
    * init <Card /> component
    
    * fixes heading color
    
    * adds product logos and hover styles
    
    * update naming
    
    * simplifies inset spacing
    
    * use ternary and add key
    
    * removes repo link
    
    * removes need for camelCase package

---
+++ website/components/io-home-card/index.tsx
@@ -0,0 +1,116 @@
+import * as React from 'react'
+import Link from 'next/link'
+import InlineSvg from '@hashicorp/react-inline-svg'
+import classNames from 'classnames'
+import { IconArrowRight16 } from '@hashicorp/flight-icons/svg-react/arrow-right-16'
+import { IconExternalLink16 } from '@hashicorp/flight-icons/svg-react/external-link-16'
+import { productLogos } from './product-logos'
+import s from './style.module.css'
+
+interface IoHomeCardProps {
+  variant?: 'light' | 'gray' | 'dark'
+  products?: Array<keyof typeof productLogos>
+  link: {
+    url: string
+    type: 'inbound' | 'outbound'
+  }
+  inset?: 'none' | 'sm' | 'md'
+  eyebrow?: string
+  heading?: string
+  description?: string
+  children?: React.ReactNode
+}
+
+function IoHomeCard({
+  variant = 'light',
+  products,
+  link,
+  inset = 'md',
+  eyebrow,
+  heading,
+  description,
+  children,
+}: IoHomeCardProps) {
+  const LinkWrapper = ({ className, children }) =>
+    link.type === 'inbound' ? (
+      <Link href={link.url}>
+        <a className={className}>{children}</a>
+      </Link>
+    ) : (
+      <a
+        className={className}
+        href={link.url}
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        {children}
+      </a>
+    )
+
+  return (
+    <article className={classNames(s.card)}>
+      <LinkWrapper className={classNames(s[variant], s[inset])}>
+        {children ? (
+          children
+        ) : (
+          <>
+            {eyebrow ? <Eyebrow>{eyebrow}</Eyebrow> : null}
+            {heading ? <Heading>{heading}</Heading> : null}
+            {description ? <Description>{description}</Description> : null}
+          </>
+        )}
+        <footer className={s.footer}>
+          {products && (
+            <ul className={s.products}>
+              {products.map((product, index) => {
+                return (
+                  <li key={index}>
+                    <InlineSvg className={s.logo} src={productLogos[product]} />
+                  </li>
+                )
+              })}
+            </ul>
+          )}
+          <span className={s.linkType}>
+            {link.type === 'inbound' ? (
+              <IconArrowRight16 />
+            ) : (
+              <IconExternalLink16 />
+            )}
+          </span>
+        </footer>
+      </LinkWrapper>
+    </article>
+  )
+}
+
+interface EyebrowProps {
+  children: string
+}
+
+function Eyebrow({ children }: EyebrowProps) {
+  return <p className={s.eyebrow}>{children}</p>
+}
+
+interface HeadingProps {
+  as?: 'h2' | 'h3' | 'h4'
+  children: React.ReactNode
+}
+
+function Heading({ as: Component = 'h2', children }: HeadingProps) {
+  return <Component className={s.heading}>{children}</Component>
+}
+
+interface DescriptionProps {
+  children: string
+}
+
+function Description({ children }: DescriptionProps) {
+  return <p className={s.description}>{children}</p>
+}
+
+IoHomeCard.Eyebrow = Eyebrow
+IoHomeCard.Heading = Heading
+IoHomeCard.Description = Description
+
+export default IoHomeCard
---
+++ website/components/io-home-card/product-logos.ts
@@ -0,0 +1,10 @@
+export const productLogos = {
+  boundary: require('@hashicorp/mktg-logos/product/boundary/logomark/color.svg?include'),
+  consul: require('@hashicorp/mktg-logos/product/consul/logomark/color.svg?include'),
+  nomad: require('@hashicorp/mktg-logos/product/nomad/logomark/color.svg?include'),
+  packer: require('@hashicorp/mktg-logos/product/packer/logomark/color.svg?include'),
+  terraform: require('@hashicorp/mktg-logos/product/terraform/logomark/color.svg?include'),
+  vagrant: require('@hashicorp/mktg-logos/product/vagrant/logomark/color.svg?include'),
+  vault: require('@hashicorp/mktg-logos/product/vault/logomark/color.svg?include'),
+  waypoint: require('@hashicorp/mktg-logos/product/waypoint/logomark/color.svg?include'),
+}
