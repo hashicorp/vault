dfad68458d8320a4e7495031b28da249f514df55
Alex Carpenter <im.alexcarpenter@gmail.com>
2021-11-17 11:57:03 -0500

    [Homepage] `<IoHomeVideo />` component (#13161)
    * init <Video /> component
    
    * adjusts sizing and border radius
    
    * responsive styling
    
    * fix hover svg gitter
    
    * adjust play icon sizing
    
    * include temp thumbnail
    
    * dialog implementation
    
    * conditionally display person and show helpers
    
    * rename component
    
    * updates dialog naming
    
    * add homepage styling
    
    * simplify background color

---
+++ website/components/io-home-video-callout/index.tsx
@@ -0,0 +1,55 @@
+import * as React from 'react'
+import Image from 'next/image'
+import VisuallyHidden from '@reach/visually-hidden'
+import IoHomeDialog from 'components/io-home-dialog'
+import PlayIcon from './play-icon'
+import s from './style.module.css'
+
+export default function IoHomeVideoCallout({
+  thumbnail,
+  heading,
+  description,
+  person,
+}) {
+  const [showDialog, setShowDialog] = React.useState(false)
+  const showVideo = () => setShowDialog(true)
+  const hideVideo = () => setShowDialog(false)
+  return (
+    <>
+      <figure className={s.videoCallout}>
+        <button className={s.thumbnail} onClick={showVideo}>
+          <VisuallyHidden>Play video</VisuallyHidden>
+          <PlayIcon />
+          <Image src={thumbnail} layout="fill" objectFit="cover" />
+        </button>
+        <figcaption className={s.content}>
+          <h3 className={s.heading}>{heading}</h3>
+          <p className={s.description}>{description}</p>
+          {person && (
+            <div className={s.person}>
+              <div className={s.personThumbnail}>
+                <Image
+                  src={person.thumbnail}
+                  width={52}
+                  height={52}
+                  alt={`${person.name} avatar`}
+                />
+              </div>
+              <div>
+                <p className={s.personName}>{person.name}</p>
+                <p className={s.personDescription}>{person.description}</p>
+              </div>
+            </div>
+          )}
+        </figcaption>
+      </figure>
+      <IoHomeDialog
+        isOpen={showDialog}
+        onDismiss={hideVideo}
+        label={`${heading} video}`}
+      >
+        <div className={s.video}></div>
+      </IoHomeDialog>
+    </>
+  )
+}
