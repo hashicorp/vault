8c649aef9484ffc86c53c902f07996f1635fabd3
Alex Carpenter <im.alexcarpenter@gmail.com>
2021-11-17 11:35:26 -0500

    [Homepage] `<IoHomePreFooter />` component (#13182)
    * adds <IoHomePreFooter />
    
    * adds interfaces
    
    * [Homepage] `<IoHomeHero />` component (#13160)
    
    * init <Hero /> component
    
    * adds loading animation
    
    * updates variable naming
    
    * makes index optional
    
    * Update hero-pattern.svg
    
    * prefix with IoHome
    
    * updates usage
    
    * adds <IoHomePreFooter />
    
    * adds interfaces
    
    * adds key

---
+++ website/components/io-home-pre-footer/index.tsx
@@ -0,0 +1,77 @@
+import * as React from 'react'
+import { IconArrowRight16 } from '@hashicorp/flight-icons/svg-react/arrow-right-16'
+import s from './style.module.css'
+
+interface IoHomePreFooterProps {
+  brand: string
+  heading: string
+  description: string
+  ctas: [IoHomePreFooterCard, IoHomePreFooterCard, IoHomePreFooterCard]
+}
+
+export default function IoHomePreFooter({
+  brand,
+  heading,
+  description,
+  ctas,
+}: IoHomePreFooterProps) {
+  return (
+    <div className={s.preFooter}>
+      <div className={s.container}>
+        <div className={s.content}>
+          <h2 className={s.heading}>{heading}</h2>
+          <p className={s.description}>{description}</p>
+        </div>
+        <div className={s.cards}>
+          {ctas.map((cta, index) => {
+            return (
+              <IoHomePreFooterCard
+                key={index}
+                brand={brand}
+                link={cta.link}
+                heading={cta.heading}
+                description={cta.description}
+                label={cta.label}
+              />
+            )
+          })}
+        </div>
+      </div>
+    </div>
+  )
+}
+
+interface IoHomePreFooterCard {
+  brand?: string
+  link: string
+  heading: string
+  description: string
+  label: string
+}
+
+function IoHomePreFooterCard({
+  brand,
+  link,
+  heading,
+  description,
+  label,
+}: IoHomePreFooterCard) {
+  return (
+    <a
+      href={link}
+      className={s.card}
+      style={
+        {
+          '--primary': `var(--${brand})`,
+          '--secondary': `var(--${brand}-secondary)`,
+        } as React.CSSProperties
+      }
+    >
+      <h3 className={s.cardHeading}>{heading}</h3>
+      <p className={s.cardDescription}>{description}</p>
+      <span className={s.cardCta}>
+        {label} <IconArrowRight16 />
+      </span>
+    </a>
+  )
+}
--- website/pages/home/index.tsx
+++ website/pages/home/index.tsx
@@ -1,5 +1,5 @@
 import IoHomeHero from 'components/io-home-hero'
-import RAW_CONTENT from './content.json'
+import IoHomePreFooter from 'components/io-home-pre-footer'
 
 export async function getStaticProps() {
   return { props: {} }
@@ -42,6 +42,32 @@ export default function Homepage({ content }) {
             },
             subText:
               'Get up and running in minutes with a fully managed Vault cluster on HCP (HashiCorp Cloud Platform)',
+          },
+        ]}
+      />
+
+      <IoHomePreFooter
+        brand="vault"
+        heading="Next steps"
+        description="HCP Vault simplifies cloud security automation on fully managed infrastructure. Get started for free, and pay only for what you use."
+        ctas={[
+          {
+            link: '#TODO',
+            heading: 'Open Source',
+            description: 'Self-managed | always free',
+            label: 'Download',
+          },
+          {
+            link: '#TODO',
+            heading: 'Cloud',
+            description: 'Self-managed',
+            label: 'Compare plans',
+          },
+          {
+            link: '#TODO',
+            heading: 'Enterprise',
+            description: 'Self-Managed custom deployments',
+            label: 'Learn more',
           },
         ]}
       />
