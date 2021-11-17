630cb689cfa1672654bcd77c9bec51bbeac6955c
Angel Garbarino <argarbarino@gmail.com>
2021-11-17 10:17:59 -0700

    glimmerize the json-editor component

--- ui/app/components/json-editor.js
+++ ui/app/components/json-editor.js
@@ -1,8 +1,21 @@
 /* eslint-disable no-undef */
-import Component from '@ember/component';
-import { computed } from '@ember/object';
+import Component from '@glimmer/component';
+import { tracked } from '@glimmer/tracking';
+import { action } from '@ember/object';
 
-// ARG TODO glimmerize this component and add documentation
+// ARG TODO fill out
+/**
+ * @module JsonEditor
+ *
+ * @example
+ * ```js
+ * <JsonEditor @activeClusterName={{cluster.name}} @onLinkClick={{action "onLinkClick"}} />
+ * ```
+ *
+ * @param {string} activeClusterName - name of the current cluster, passed from the parent.
+ * @param {Function} onLinkClick - parent action which determines the behavior on link click
+ */
+
 const JSON_EDITOR_DEFAULTS = {
   // IMPORTANT: `gutters` must come before `lint` since the presence of
   // `gutters` is cached internally when `lint` is toggled
@@ -16,21 +29,16 @@ const JSON_EDITOR_DEFAULTS = {
   showCursorWhenSelecting: true,
 };
 
-export default Component.extend({
-  showToolbar: true,
-  title: null,
-  subTitle: null,
-  helpText: null,
-  value: null,
-  options: null,
-  valueUpdated: null,
-  onFocusOut: null,
-  readOnly: false,
-  diffView: false,
+export default class JsonEditorComponent extends Component {
+  value = null;
+  valueUpdated = null;
+  onFocusOut = null;
+  readOnly = false;
+  options = null;
 
-  init() {
-    this._super(...arguments);
-    this.options = { ...JSON_EDITOR_DEFAULTS, ...this.options };
+  constructor() {
+    super(...arguments);
+    this.options = { ...JSON_EDITOR_DEFAULTS, ...this.args.options };
     if (this.options.autoHeight) {
       this.options.viewportMargin = Infinity;
       delete this.options.autoHeight;
@@ -40,28 +48,32 @@ export default Component.extend({
       this.options.lineNumbers = false;
       delete this.options.gutters;
     }
-  },
-  // Computed diff
-  visualDiff: computed('leftSideVersionData', 'rightSideVersionData', function() {
-    let diffpatcher = jsondiffpatch.create({});
+  }
 
-    let delta = diffpatcher.diff(this.leftSideVersionData, this.rightSideVersionData);
+  get getShowToolbar() {
+    return this.args.showToolbar === false ? false : true;
+  }
+
+  get visualDiff() {
+    let diffpatcher = jsondiffpatch.create({});
+    let delta = diffpatcher.diff(this.args.leftSideVersionData, this.args.rightSideVersionData);
     if (delta == undefined) {
       return 'No changes, previous state matches.';
     }
-    // beautiful html diff
-    return jsondiffpatch.formatters.html.format(delta, this.leftSideVersionData);
-  }),
-  actions: {
-    updateValue(...args) {
-      if (this.valueUpdated) {
-        this.valueUpdated(...args);
-      }
-    },
-    onFocus(...args) {
-      if (this.onFocusOut) {
-        this.onFocusOut(...args);
-      }
-    },
-  },
-});
+    return jsondiffpatch.formatters.html.format(delta, this.args.leftSideVersionData);
+  }
+
+  @action
+  updateValue(...args) {
+    if (this.args.valueUpdated) {
+      this.args.valueUpdated(...args);
+    }
+  }
+
+  @action
+  onFocus(...args) {
+    if (this.args.onFocusOut) {
+      this.args.onFocusOut(...args);
+    }
+  }
+}
