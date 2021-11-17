c17b622b28e2844d85cbb99665029f05c8605374
Alex Carpenter <im.alexcarpenter@gmail.com>
2021-11-17 09:53:41 -0500

    adds <IoHomePreFooter />

--- website/pages/home/index.tsx
+++ website/pages/home/index.tsx
@@ -1,10 +1,37 @@
-// Imports below are used in getStaticProps only
-import RAW_CONTENT from './content.json'
+import IoHomePreFooter from 'components/io-home-pre-footer'
 
 export async function getStaticProps() {
   return { props: {} }
 }
 
 export default function Homepage({ content }) {
-  return <p>Homepage</p>
+  return (
+    <>
+      <IoHomePreFooter
+        brand="vault"
+        heading="Next steps"
+        description="HCP Vault simplifies cloud security automation on fully managed infrastructure. Get started for free, and pay only for what you use."
+        ctas={[
+          {
+            link: '#TODO',
+            heading: 'Open Source',
+            description: 'Self-managed | always free',
+            label: 'Download',
+          },
+          {
+            link: '#TODO',
+            heading: 'Cloud',
+            description: 'Self-managed',
+            label: 'Compare plans',
+          },
+          {
+            link: '#TODO',
+            heading: 'Enterprise',
+            description: 'Self-Managed custom deployments',
+            label: 'Learn more',
+          },
+        ]}
+      />
+    </>
+  )
 }
