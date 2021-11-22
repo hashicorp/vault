3aa225ca28d398207e3c750acb9ba76b893e578e
Jordan Reimer <zofskeez@gmail.com>
2021-11-22 14:55:26 -0700

    adds disabled state to ToolbarLink component and disables create new version action when users cannot read metadata

--- ui/lib/core/addon/components/toolbar-link.js
+++ ui/lib/core/addon/components/toolbar-link.js
@@ -7,15 +7,17 @@
  * ```js
  * <Toolbar>
  *   <ToolbarActions>
- *     <ToolbarLink @params={{array 'vault.cluster.policies.create'}} @type="add">
+ *     <ToolbarLink @params={{array 'vault.cluster.policies.create'}} @type="add" @disabled={{true}} @disabledTooltip="This link is disabled">
  *       Create policy
  *     </ToolbarLink>
  *   </ToolbarActions>
  * </Toolbar>
  * ```
  *
- * @param params=''{Array} Array to pass to LinkTo
- * @param type=''{String} Use "add" to change icon
+ * @param {array} params - Array to pass to LinkTo
+ * @param {string} type - Use "add" to change icon
+ * @param {boolean} disabled - pass true to disable link
+ * @param {string} disabledTooltip - tooltip to display on hover when disabled
  */
 
 import Component from '@ember/component';
@@ -27,6 +29,8 @@ export default Component.extend({
   tagName: '',
   supportsDataTestProperties: true,
   type: null,
+  disabled: false,
+  disabledTooltip: null,
   glyph: computed('type', function() {
     if (this.type == 'add') {
       return 'plus-plain';
