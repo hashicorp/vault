338b84abba2668fd49268ee82f484ac84ea9a5ad
Meggie <meggie@hashicorp.com>
2021-11-30 10:47:43 -0500

    Updating version in SDK for 1.7.7 (#13300)

--- sdk/version/version_base.go
+++ sdk/version/version_base.go
@@ -8,7 +8,7 @@ var (
 	// Whether cgo is enabled or not; set at build time
 	CgoEnabled bool
 
-	Version           = "1.7.6"
+	Version           = "1.7.7"
 	VersionPrerelease = ""
 	VersionMetadata   = ""
 )
