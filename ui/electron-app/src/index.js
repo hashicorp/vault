f100b3b14a2d212e83ef1a5afa32afb5f253cc3d
Jordan Reimer <zofskeez@gmail.com>
2021-12-02 08:27:38 -0700

    updates app to handle loading server select and ember windows

--- ui/electron-app/src/index.js
+++ ui/electron-app/src/index.js
@@ -6,17 +6,12 @@ const {
   EMBER_INSPECTOR,
 } = require('electron-devtools-installer');
 const { pathToFileURL } = require('url');
-const { app, BrowserWindow, screen } = require('electron');
+const { app, BrowserWindow, screen, globalShortcut, ipcMain } = require('electron');
 const path = require('path'); 
 const handleFileUrls = require('./handle-file-urls');
 const isDev = require('electron-is-dev');
 const windowStateKeeper = require('electron-window-state');
 const setApplicationMenu = require('./app-menu');
-
-const emberAppDir = path.resolve(__dirname, '..', 'ember-dist');
-const emberAppURL = pathToFileURL(
-  path.join(emberAppDir, 'index.html')
-).toString();
 
 // Uncomment the lines below to enable Electron's crash reporter
 // For more information, see http://electron.atom.io/docs/api/crash-reporter/
@@ -27,11 +22,99 @@ const emberAppURL = pathToFileURL(
 //     autoSubmit: true
 // });
 
-app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
-    app.quit();
+const getWindowState = () => {
+  const primaryDisplay = screen.getPrimaryDisplay();
+  const windowState = windowStateKeeper({
+    defaultWidth: primaryDisplay.size.width / 2,
+    defaultHeight: primaryDisplay.size.height
+  });
+  const { x, y, width, height } = windowState;
+  return [
+    windowState,
+    { x, y, width, height }
+  ];
+};
+
+const setupBrowserWindow = (options, contentPath) => {
+  const [windowState, windowStateProps] = getWindowState();
+  let appWindow = new BrowserWindow({
+    ...windowStateProps,
+    ...options
+  });
+  // register listeners on the window to update the state
+  // listeners will be removed automatically when the window is closed and restore the maximized or full screen state
+  windowState.manage(appWindow);
+  // Load the ember application or html content
+  const loadMethod = contentPath.includes('file://') ? 'loadURL' : 'loadFile';
+  appWindow[loadMethod](contentPath);
+  // If a loading operation goes wrong, we'll send Electron back to
+  // Ember App entry point
+  appWindow.webContents.on('did-fail-load', () => {
+    appWindow.loadURL(contentPath);
+  });
+  appWindow.webContents.on('render-process-gone', (_event, details) => {
+    if (details.reason === 'killed' || details.reason === 'clean-exit') {
+      return;
+    }
+    console.log(
+      'Your main window process has exited unexpectedly -- see https://www.electronjs.org/docs/api/web-contents#event-render-process-gone'
+    );
+    console.log('Reason: ' + details.reason);
+  });
+  appWindow.on('unresponsive', () => {
+    console.log(
+      'Your Ember app (or other code) has made the window unresponsive.'
+    );
+  });
+  appWindow.on('responsive', () => {
+    console.log('The main window has become responsive again.');
+  });
+  appWindow.on('closed', () => {
+    appWindow = null;
+    globalShortcut.unregister('CommandOrControl+R');
+  });
+  // setup reload shortcut
+  if (isDev) {
+    globalShortcut.register('CommandOrControl+R', () => {
+      appWindow.reload();
+    });
   }
-});
+  this.appWindow = appWindow;
+};
+
+const renderEmberApp = async serverAddress => {
+  const emberAppDir = path.resolve(__dirname, '..', 'ember-dist');
+  const emberAppURL = pathToFileURL(path.join(emberAppDir, 'index.html')).toString();
+  await handleFileUrls(emberAppDir);
+  const options = {
+    useContentSize: true,
+    // there are secure ways to access node in renderers
+    // below should be replaced with preload and contextBridge strategy
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false,
+      // webSecurity: false
+    }
+  };
+  setupBrowserWindow(options, emberAppURL);
+  // provide server address to ember app
+  ipcMain.on('get-api-host', event => {
+    event.reply('api-host', serverAddress);
+  });
+};
+
+const renderServerSelect = () => {
+  const options = {
+    // there are secure ways to access node in renderers
+    // below should be replaced with preload and contextBridge strategy
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false
+    }
+  };
+  const contentPath = path.join(__dirname, 'server-select.html');
+  setupBrowserWindow(options, contentPath);
+};
 
 app.on('ready', async () => {
   if (isDev) {
@@ -49,63 +132,34 @@ app.on('ready', async () => {
     }
   }
 
-  await handleFileUrls(emberAppDir);
-
   // setup custom application menu
   setApplicationMenu();
 
-  const primaryDisplay = screen.getPrimaryDisplay();
-  const mainWindowState = windowStateKeeper({
-    defaultWidth: primaryDisplay.size.width / 2,
-    defaultHeight: primaryDisplay.size.height
-  }); 
-  let mainWindow = new BrowserWindow({
-    x: mainWindowState.x,
-    y: mainWindowState.y,
-    width: primaryDisplay.size.width / 2,
-    height: primaryDisplay.size.height,
-    useContentSize: true
+  // create setting to skip server select screen
+  // for now render by default
+  renderServerSelect();
+
+  // communication with renderers
+  // listen for server selected event to switch window to ember app
+  ipcMain.on('server-selected', (event, serverAddress) => {
+    this.appWindow.close();
+    renderEmberApp(serverAddress);
   });
-
-  // register listeners on the window to update the state
-  // listeners will be removed automatically when the window is closed and restore the maximized or full screen state
-  mainWindowState.manage(mainWindow);
-
-  // If you want to open up dev tools programmatically, call
-  // mainWindow.openDevTools();
-
-  // Load the ember application
-  mainWindow.loadURL(emberAppURL);
-
-  // If a loading operation goes wrong, we'll send Electron back to
-  // Ember App entry point
-  mainWindow.webContents.on('did-fail-load', () => {
-    mainWindow.loadURL(emberAppURL);
+  // show server select -- triggered via menu
+  ipcMain.on('show-servers', () => {
+    this.appWindow.close();
+    renderServerSelect();
   });
-
-  mainWindow.webContents.on('render-process-gone', (_event, details) => {
-    if (details.reason === 'killed' || details.reason === 'clean-exit') {
-      return;
-    }
-    console.log(
-      'Your main window process has exited unexpectedly -- see https://www.electronjs.org/docs/api/web-contents#event-render-process-gone'
-    );
-    console.log('Reason: ' + details.reason);
+  ipcMain.on('get-preferences-path', event => {
+    const filePath = path.join(app.getPath('userData'), 'preferences.json');
+    event.reply('preferences-path', filePath);
   });
+});
 
-  mainWindow.on('unresponsive', () => {
-    console.log(
-      'Your Ember app (or other code) has made the window unresponsive.'
-    );
-  });
-
-  mainWindow.on('responsive', () => {
-    console.log('The main window has become responsive again.');
-  });
-
-  mainWindow.on('closed', () => {
-    mainWindow = null;
-  });
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
 });
 
 // Handle an unhandled error in the main thread
