04568b842384b79f4366d156c45c5e0ebb8cddef
Jordan Reimer <zofskeez@gmail.com>
2021-12-07 16:39:46 -0700

    updates basic dropdown instances to new version API

---
+++ ui/scripts/codemods/dropdown-transform.js
@@ -0,0 +1,47 @@
+#!/usr/bin/env node
+/* eslint-env node */
+
+/**
+ * Codemod to transform BasicDropdown and Tooltip trigger and content components
+ * As of version 2 of ember-basic-dropdown the yielded component names are now capitalized
+ * In addition, splattributes are used and class must be passed as an attribute rather than argument
+ * example execution from ui directory -> npx ember-template-recast ./app/templates -t ./scripts/codemods/dropdown-transform.js
+ * above will run transform on all files in templates directory
+ */
+
+module.exports = () => {
+  const evalTag = (tag, prefix) => {
+    switch (tag) {
+      case `${prefix}.trigger`:
+        return `${prefix}.Trigger`;
+      case `${prefix}.content`:
+        return `${prefix}.Content`;
+    }
+  };
+  // remove @ symbol from class and change @tagName to @htmlTag
+  const transformAttrs = (attrs) => {
+    attrs.forEach((attr) => {
+      if (attr.name === '@class') {
+        attr.name = 'class';
+      } else if (attr.name.includes('tagName')) {
+        attr.name = '@htmlTag';
+      }
+    });
+  };
+
+  return {
+    ElementNode(node) {
+      // BasicDropdown and Tooltip components yield variable is either d, dd, or T
+      const prefixes = ['d', 'D', 'dd', 'T'];
+      // capitalize trigger and content and transform attributes
+      for (let i = 0; i < prefixes.length; i++) {
+        const newTag = evalTag(node.tag, prefixes[i]);
+        if (newTag) {
+          node.tag = newTag;
+          transformAttrs(node.attributes);
+          break;
+        }
+      }
+    },
+  };
+};
