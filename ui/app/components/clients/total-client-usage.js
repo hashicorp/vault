048b05de425c9d59d701cefa6dcd9e9089c49f6c
Angel Garbarino <argarbarino@gmail.com>
2021-12-07 12:43:39 -0700

    setup

--- ui/app/components/clients/total-client-usage.js
+++ ui/app/components/clients/total-client-usage.js
@@ -1,16 +1,16 @@
 import Component from '@glimmer/component';
 import { action } from '@ember/object';
 import { select } from 'd3-selection';
-import { scaleLinear, scaleBand } from 'd3-scale';
+import { scaleLinear, scaleBand, scaleTime } from 'd3-scale';
 import { stack } from 'd3-shape';
 
 /**
- * @module TotalMonthlyClients
- * TotalMonthlyClients components are used to...
+ * @module TotalClientUsage
+ * TotalClientUsage components are used to...
  *
  * @example
  * ```js
- * <TotalMonthlyClients @requiredParam={requiredParam} @optionalParam={optionalParam} @param1={{param1}}/>
+ * <TotalClientUsage @requiredParam={requiredParam} @optionalParam={optionalParam} @param1={{param1}}/>
  * ```
  * @param {object} requiredParam - requiredParam is...
  * @param {string} [optionalParam] - optionalParam is...
@@ -18,7 +18,7 @@ import { stack } from 'd3-shape';
  */
 
 const DATA = [
-  { month: 'January', directEntities: 150, nonDirectTokens: 22 },
+  { month: 'January', directEntities: 500, nonDirectTokens: 22 },
   { month: 'February', directEntities: 150, nonDirectTokens: 22 },
   { month: 'March', directEntities: 155, nonDirectTokens: 25 },
   { month: 'April', directEntities: 155, nonDirectTokens: 229 },
@@ -35,39 +35,41 @@ const DATA = [
 // COLOR THEME:
 const BAR_COLOR_DEFAULT = ['#BFD4FF', '#8AB1FF'];
 
-export default class TotalMonthlyClients extends Component {
+export default class TotalClientUsage extends Component {
   @action
   registerListner(element) {
     let stackFunction = stack().keys(['directEntities', 'nonDirectTokens']);
     let stackedData = stackFunction(DATA);
-    stackedData.forEach(item => console.log(item, 'here'));
     let countArray = DATA.map(month => month.directEntities); // change to combined
     let yScale = scaleLinear()
       // .domain([0, Math.max(...countArray)])
-      .domain([0, 1000]) // TODO calculate high of total combined
-      .range([0, 250]); // 250 is the height of the chart
+      .domain([0, 802]) // TODO calculate high of total combined
+      .range([100, 0]); // 250 is the height of the chart
     let xScale = scaleBand()
       // .domain(DATA.map(month => month.month))
-      .domain([DATA[0].month, DATA[11].month]) // jan to dec
-      .range([0, 500])
+      .domain(DATA.map(month => month.month)) // needs to iterate through each one.
+      .range([0, 100]) // width of the chart as percent
       .paddingInner(0.85);
-
     let chartSvg = select(element);
-    chartSvg.attr('width', '500px');
-    chartSvg.attr('height', '250px');
+    chartSvg.attr('width', '100%');
+    chartSvg.attr('height', '100%');
 
     // ARG STOP HERE
-
-    chartSvg
-      .selectAll('rect')
+    let groups = chartSvg
+      .selectAll('g')
       .data(stackedData)
       .enter()
+      .append('g')
+      .style('fill', (d, i) => BAR_COLOR_DEFAULT[i]);
+
+    groups
+      .selectAll('rect')
+      .data(stackedData => stackedData)
+      .enter()
       .append('rect')
-      // .style('fill', (d, i) => BAR_COLOR_DEFAULT[i]);
-      .attr('width', xScale.bandwidth())
-      .attr('height', 200)
-      // .attr('height', month => yScale(month.directEntities))
-      .attr('x', month => xScale(month.month)) // offset each bar.
-      .attr('y', month => 250 - yScale(month.directEntities)); // flips it right side up.
+      .attr('width', `${xScale.bandwidth()}%`)
+      .attr('height', data => `${100 - yScale(data[1])}%`)
+      .attr('x', data => `${xScale(data.data.month)}%`)
+      .attr('y', data => `${yScale(data[0]) + yScale(data[1]) - 100}%`);
   }
 }
