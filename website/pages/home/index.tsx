930349c5c46e5c6cc608b4dfbdb549b3ba77f33c
Alex Carpenter <im.alexcarpenter@gmail.com>
2021-12-07 10:35:54 -0500

    refactor data props

--- website/pages/home/index.tsx
+++ website/pages/home/index.tsx
@@ -16,144 +16,6 @@ import s from './style.module.css'
 export default function Homepage({ data }): React.ReactElement {
   const {
     seo,
-    hero,
-    intro,
-    inPractice,
-    useCases,
-    caseStudies,
-    callToAction,
-    preFooter,
-  } = data
-
-  return (
-    <>
-      <Head>{renderMetaTags(seo)}</Head>
-
-      <IoHomeHero
-        pattern="/img/home-hero-pattern.svg"
-        brand="vault"
-        {...hero}
-      />
-
-      <section className={s.intro}>
-        <header className={s.introHeader}>
-          <div className={s.container}>
-            <div className={s.introHeaderInner}>
-              <h2 className={s.introHeading}>{intro.heading}</h2>
-              <p className={s.introDescription}>{intro.description}</p>
-            </div>
-          </div>
-        </header>
-
-        <ul className={s.features}>
-          {intro.features.map((feature, index) => {
-            return (
-              // Index is stable
-              // eslint-disable-next-line react/no-array-index-key
-              <li key={index}>
-                <div className={s.container}>
-                  <IoHomeFeature {...feature} />
-                </div>
-              </li>
-            )
-          })}
-        </ul>
-
-        {intro.video ? (
-          <div className={s.container}>
-            <IoVideoCallout
-              youtubeId={intro.video.youtubeId}
-              thumbnail={intro.video.thumbnail.url}
-              heading={intro.video.heading}
-              description={intro.video.description}
-              person={{
-                name: intro.video.personName,
-                description: intro.video.personDescription,
-                avatar: intro.video.personAvatar?.url,
-              }}
-            />
-          </div>
-        ) : null}
-      </section>
-
-      <IoHomeInPractice
-        brand="vault"
-        pattern="/img/practice-pattern.svg"
-        heading={inPractice.heading}
-        description={inPractice.description}
-        cards={inPractice.cards.map((card) => {
-          return {
-            eyebrow: card.eyebrow,
-            link: {
-              url: card.link,
-              type: 'inbound',
-            },
-            heading: card.heading,
-            description: card.description,
-            products: card.products,
-          }
-        })}
-        cta={{
-          heading: inPractice.cta.heading,
-          description: inPractice.cta.description,
-          link: inPractice.cta.link,
-          image: inPractice.cta.image,
-        }}
-      />
-
-      <section className={s.useCases}>
-        <div className={s.container}>
-          <IoCardContainer
-            heading={useCases.heading}
-            description={useCases.description}
-            cardsPerRow={4}
-            cards={useCases.cards.map((card) => {
-              return {
-                eyebrow: card.eyebrow,
-                link: {
-                  url: card.link,
-                  type: 'inbound',
-                },
-                heading: card.heading,
-                description: card.description,
-                products: card.products,
-              }
-            })}
-          />
-        </div>
-      </section>
-
-      <IoHomeCaseStudies
-        heading={caseStudies.heading}
-        description={caseStudies.description}
-        primary={caseStudies.features}
-        secondary={caseStudies.links}
-      />
-
-      <IoHomeCallToAction
-        brand="vault"
-        heading={callToAction.heading}
-        content={callToAction.description}
-        links={callToAction.links}
-      />
-
-      <IoHomePreFooter
-        brand="vault"
-        heading={preFooter.heading}
-        description={preFooter.description}
-        ctas={preFooter.ctas}
-      />
-    </>
-  )
-}
-
-export async function getStaticProps() {
-  const { vaultHomepage } = await rivetQuery({
-    query: homepageQuery,
-  })
-
-  const {
-    seo,
     heroHeading,
     heroDescription,
     heroCtas,
@@ -182,62 +44,155 @@ export async function getStaticProps() {
     preFooterHeading,
     preFooterDescription,
     preFooterCtas,
-  } = vaultHomepage
+  } = data
+  const _introVideo = introVideo[0]
+
+  return (
+    <>
+      <Head>{renderMetaTags(seo)}</Head>
+
+      <IoHomeHero
+        pattern="/img/home-hero-pattern.svg"
+        brand="vault"
+        heading={heroHeading}
+        description={heroDescription}
+        ctas={heroCtas}
+        cards={heroCards.map((card) => {
+          return {
+            ...card,
+            cta: card.cta[0],
+          }
+        })}
+      />
+
+      <section className={s.intro}>
+        <header className={s.introHeader}>
+          <div className={s.container}>
+            <div className={s.introHeaderInner}>
+              <h2 className={s.introHeading}>{introHeading}</h2>
+              <p className={s.introDescription}>{introDescription}</p>
+            </div>
+          </div>
+        </header>
+
+        <ul className={s.features}>
+          {introFeatures.map((feature, index) => {
+            return (
+              // Index is stable
+              // eslint-disable-next-line react/no-array-index-key
+              <li key={index}>
+                <div className={s.container}>
+                  <IoHomeFeature
+                    image={{
+                      url: feature.image.url,
+                      alt: feature.image.alt,
+                    }}
+                    heading={feature.heading}
+                    description={feature.description}
+                    link={feature.link}
+                  />
+                </div>
+              </li>
+            )
+          })}
+        </ul>
+
+        {_introVideo ? (
+          <div className={s.container}>
+            <IoVideoCallout
+              youtubeId={_introVideo.youtubeId}
+              thumbnail={_introVideo.thumbnail.url}
+              heading={_introVideo.heading}
+              description={_introVideo.description}
+              person={{
+                name: _introVideo.personName,
+                description: _introVideo.personDescription,
+                avatar: _introVideo.personAvatar?.url,
+              }}
+            />
+          </div>
+        ) : null}
+      </section>
+
+      <IoHomeInPractice
+        brand="vault"
+        pattern="/img/practice-pattern.svg"
+        heading={inPracticeHeading}
+        description={inPracticeDescription}
+        cards={inPracticeCards.map((card) => {
+          return {
+            eyebrow: card.eyebrow,
+            link: {
+              url: card.link,
+              type: 'inbound',
+            },
+            heading: card.heading,
+            description: card.description,
+            products: card.products,
+          }
+        })}
+        cta={{
+          heading: inPracticeCtaHeading,
+          description: inPracticeCtaDescription,
+          link: inPracticeCtaLink,
+          image: inPracticeCtaImage,
+        }}
+      />
+
+      <section className={s.useCases}>
+        <div className={s.container}>
+          <IoCardContainer
+            heading={useCasesHeading}
+            description={useCasesDescription}
+            cardsPerRow={4}
+            cards={useCasesCards.map((card) => {
+              return {
+                eyebrow: card.eyebrow,
+                link: {
+                  url: card.link,
+                  type: 'inbound',
+                },
+                heading: card.heading,
+                description: card.description,
+                products: card.products,
+              }
+            })}
+          />
+        </div>
+      </section>
+
+      <IoHomeCaseStudies
+        heading={caseStudiesHeading}
+        description={caseStudiesDescription}
+        primary={caseStudiesFeatured}
+        secondary={caseStudiesLinks}
+      />
+
+      <IoHomeCallToAction
+        brand="vault"
+        heading={callToActionHeading}
+        content={callToActionDescription}
+        links={callToActionCtas}
+      />
+
+      <IoHomePreFooter
+        brand="vault"
+        heading={preFooterHeading}
+        description={preFooterDescription}
+        ctas={preFooterCtas}
+      />
+    </>
+  )
+}
+
+export async function getStaticProps() {
+  const { vaultHomepage } = await rivetQuery({
+    query: homepageQuery,
+  })
 
   return {
     props: {
-      data: {
-        seo,
-        hero: {
-          heading: heroHeading,
-          description: heroDescription,
-          ctas: heroCtas,
-          cards: heroCards.map((card) => {
-            return {
-              ...card,
-              cta: card.cta[0],
-            }
-          }),
-        },
-        intro: {
-          heading: introHeading,
-          description: introDescription,
-          features: introFeatures,
-          video: introVideo[0],
-        },
-        inPractice: {
-          heading: inPracticeHeading,
-          description: inPracticeDescription,
-          cards: inPracticeCards,
-          cta: {
-            heading: inPracticeCtaHeading,
-            description: inPracticeCtaDescription,
-            link: inPracticeCtaLink,
-            image: inPracticeCtaImage,
-          },
-        },
-        useCases: {
-          heading: useCasesHeading,
-          description: useCasesDescription,
-          cards: useCasesCards,
-        },
-        caseStudies: {
-          heading: caseStudiesHeading,
-          description: caseStudiesDescription,
-          features: caseStudiesFeatured,
-          links: caseStudiesLinks,
-        },
-        callToAction: {
-          heading: callToActionHeading,
-          description: callToActionDescription,
-          links: callToActionCtas,
-        },
-        preFooter: {
-          heading: preFooterHeading,
-          description: preFooterDescription,
-          ctas: preFooterCtas,
-        },
-      },
+      data: vaultHomepage,
     },
   }
 }
