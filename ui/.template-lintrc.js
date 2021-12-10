681cf5feb3f009921c20be61a20c5d62d12dd90a
Jordan Reimer <zofskeez@gmail.com>
2021-12-09 19:21:19 -0700

    updates global template lint rules

--- ui/.template-lintrc.js
+++ ui/.template-lintrc.js
@@ -2,4 +2,15 @@
 
 module.exports = {
   extends: 'octane',
+  rules: {
+    'no-bare-strings': 'off',
+    'no-action': 'off',
+    'no-duplicate-landmark-elements': 'warn',
+    'no-implicit-this': {
+      allow: ['supported-auth-backends'],
+    },
+    'require-input-label': 'off',
+    'no-down-event-binding': 'warn',
+  },
+  ignore: ['lib/story-md'],
 };
