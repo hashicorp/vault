93b552f949b11b0ce9221e90481e55a99704b287
Jordan Reimer <zofskeez@gmail.com>
2021-12-14 17:22:23 -0700

    runs linters after main merge

--- ui/tests/integration/components/diff-version-selector-test.js
+++ ui/tests/integration/components/diff-version-selector-test.js
@@ -1,7 +1,7 @@
 import EmberObject from '@ember/object';
 import { module, test } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
-import { render, click, settled } from '@ember/test-helpers';
+import { render, click } from '@ember/test-helpers';
 import { hbs } from 'ember-cli-htmlbars';
 
 const VERSIONS = [
