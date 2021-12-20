0b73b0abf7fe01f814c75104a080740eab8393b0
Claire Bontempo <cbontempo@hashicorp.com>
2021-12-20 12:14:48 -0800

    refactor horizontal bar chart into separate component

---
+++ ui/app/components/clients/attribution.js
@@ -0,0 +1,19 @@
+import Component from '@glimmer/component';
+
+// TODO: fill out below!!
+/**
+ * @module Attribution
+ * Attribution components are used to...
+ *
+ * @example
+ * ```js
+ * <Attribution @requiredParam={requiredParam} @optionalParam={optionalParam} @param1={{param1}}>
+ * Pass in export button
+ * </Attribution>
+ * ```
+ * @param {object} requiredParam - requiredParam is...
+ * @param {string} [optionalParam] - optionalParam is...
+ * @param {string} [param1=defaultValue] - param1 is...
+ */
+
+export default class Attribution extends Component {}
