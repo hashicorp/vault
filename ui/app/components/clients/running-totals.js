b09780429ddd233d6e6d901364c7b1befe53f5dc
Claire Bontempo <cbontempo@hashicorp.com>
2021-12-21 11:52:02 -0800

    change name to running totals

--- ui/app/components/clients/running-totals.js
+++ ui/app/components/clients/running-totals.js
@@ -1,15 +1,15 @@
 import Component from '@glimmer/component';
 
 /**
- * @module ClientCountsRange
- * ClientCountsRange components are used to...
+ * @module RunningTotals
+ * RunningTotals components are used to...
  *
  * @example
  * ```js
- * <ClientCountsRange @requiredParam={requiredParam} @optionalParam={optionalParam} @param1={{param1}}/>
+ * <RunningTotals @requiredParam={requiredParam} @optionalParam={optionalParam} @param1={{param1}}/>
  * ```
  * @param {object} requiredParam - requiredParam is...
  * @param {string} [optionalParam] - optionalParam is...
  * @param {string} [param1=defaultValue] - param1 is...
  */
-export default class ClientCountsRange extends Component {}
+export default class RunningTotals extends Component {}
