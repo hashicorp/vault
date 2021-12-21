608bfdd3b7c1c195d4ab51ab4f7db9590b6ba29e
Claire Bontempo <cbontempo@hashicorp.com>
2021-12-21 11:07:49 -0800

    rename to MonthlyUsage

---
+++ ui/app/components/clients/monthly-usage.js
@@ -0,0 +1,16 @@
+import Component from '@glimmer/component';
+
+/**
+ * TODO fill out
+ * @module MonthlyUsage
+ * MonthlyUsage components are used to...
+ *
+ * @example
+ * ```js
+ * <MonthlyUsage @requiredParam={requiredParam} @optionalParam={optionalParam} @param1={{param1}}/>
+ * ```
+ * @param {object} requiredParam - requiredParam is...
+ * @param {string} [optionalParam] - optionalParam is...
+ * @param {string} [param1=defaultValue] - param1 is...
+ */
+export default class MonthlyUsage extends Component {}
