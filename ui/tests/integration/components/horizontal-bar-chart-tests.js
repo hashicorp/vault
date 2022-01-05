79b0c681b8f58edcbf017fbbc2aec72135e3a4cb
Angel Garbarino <argarbarino@gmail.com>
2022-01-05 10:34:42 -0700

    linting fix

--- ui/tests/integration/components/horizontal-bar-chart-tests.js
+++ ui/tests/integration/components/horizontal-bar-chart-tests.js
@@ -3,11 +3,11 @@ import { setupRenderingTest } from 'ember-qunit';
 import { render } from '@ember/test-helpers';
 import { hbs } from 'ember-cli-htmlbars';
 
-module('Integration | Component | horizontal-bar-chart', function(hooks) {
+module('Integration | Component | horizontal-bar-chart', function (hooks) {
   setupRenderingTest(hooks);
   // TODO: update test
 
-  test('it renders', async function(assert) {
+  test('it renders', async function (assert) {
     // Set any properties with this.set('myProperty', 'value');
     // Handle any actions with this.set('myAction', function(val) { ... });
 
