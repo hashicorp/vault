85e750e3a20df4fe8b6a7f93a87a623a71b10c91
Jordan Reimer <zofskeez@gmail.com>
2022-01-11 14:40:51 -0700

    updates template lint config to override rules in test files for ember language server

--- ui/.template-lintrc.js
+++ ui/.template-lintrc.js
@@ -1,4 +1,11 @@
 'use strict';
+const recommended = require('ember-template-lint/lib/config/recommended').rules; // octane extends recommended - no additions as of 3.14
+const stylistic = require('ember-template-lint/lib/config/stylistic').rules;
+
+const testOverrides = { ...recommended, ...stylistic };
+for (const key in testOverrides) {
+  testOverrides[key] = false;
+}
 
 module.exports = {
   extends: ['octane', 'stylistic'],
@@ -14,4 +21,12 @@ module.exports = {
     'self-closing-void-elements': 'off',
   },
   ignore: ['lib/story-md', 'tests/**'],
+  // ember language server vscode extension does not currently respect the ignore field
+  // override all rules manually as workround to align with cli
+  overrides: [
+    {
+      files: ['**/*-test.js'],
+      rules: testOverrides,
+    },
+  ],
 };
