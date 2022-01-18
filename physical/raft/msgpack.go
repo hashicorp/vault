828adec2ac8153191811e50487b0eefd70490ad2
Nick Cabatoff <ncabatoff@hashicorp.com>
2022-01-18 15:31:07 -0500

    Reviewer feedback

--- physical/raft/msgpack.go
+++ physical/raft/msgpack.go
@@ -1,11 +1,10 @@
 package raft
 
-import (
-	"github.com/hashicorp/go-msgpack/codec"
-)
-
 // If we downgrade msgpack from v1.1.5 to v0.5.5, everything will still
 // work, but any pre-existing raft clusters will break on upgrade.
 // This file exists so that the Vault project has an explicit dependency
 // on the library, which allows us to pin the version in go.mod.
-var _ = codec.GenVersion
+
+import (
+	_ "github.com/hashicorp/go-msgpack/codec"
+)
