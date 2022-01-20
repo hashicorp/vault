df217c6e2c80594c7e59d6adb136c36746dd3f6f
Matt Schultz <975680+schultz-is@users.noreply.github.com>
2022-01-20 09:10:15 -0600

    Time-based transit key autorotation (#13691)
    * Add auto_rotate_interval field to transit key creation path.
    
    * Add auto_rotate_interval field to transit key config update path.
    
    * Implement transit automatic key rotation on an hourly interval.
    
    * Fixes transit key autorotation key listing typo.
    
    * Add unit tests for transit key autorotation.
    
    * Add unit tests for transit key creation with autorotation interval.
    
    * Add unit tests for transit key config update with autorotation interval.
    
    * Document new auto_rotate_interval fields in key creation and key config update endpoints.
    
    * Add changelog for transit key autorotation.
    
    * Wrap individual transit key autorotation in a policy lock.
    
    * Add a safeguard to transit key autorotation to ensure only one execution happens simultaneously.

--- builtin/logical/transit/path_config_test.go
+++ builtin/logical/transit/path_config_test.go
@@ -2,11 +2,18 @@ package transit
 
 import (
 	"context"
+	"encoding/hex"
+	"fmt"
 	"strconv"
 	"strings"
 	"testing"
+	"time"
 
+	uuid "github.com/hashicorp/go-uuid"
+	"github.com/hashicorp/vault/api"
+	vaulthttp "github.com/hashicorp/vault/http"
 	"github.com/hashicorp/vault/sdk/logical"
+	"github.com/hashicorp/vault/vault"
 )
 
 func TestTransit_ConfigSettings(t *testing.T) {
@@ -282,4 +289,107 @@ func TestTransit_ConfigSettings(t *testing.T) {
 	testHMAC(4, true)
 	testHMAC(3, true)
 	testHMAC(2, false)
+}
+
+func TestTransit_UpdateKeyConfigWithAutorotation(t *testing.T) {
+	tests := map[string]struct {
+		initialAutoRotateInterval interface{}
+		newAutoRotateInterval     interface{}
+		shouldError               bool
+		expectedValue             time.Duration
+	}{
+		"default (no value)": {
+			initialAutoRotateInterval: "5h",
+			shouldError:               false,
+			expectedValue:             5 * time.Hour,
+		},
+		"0 (int)": {
+			initialAutoRotateInterval: "5h",
+			newAutoRotateInterval:     0,
+			shouldError:               false,
+			expectedValue:             0,
+		},
+		"0 (string)": {
+			initialAutoRotateInterval: "5h",
+			newAutoRotateInterval:     0,
+			shouldError:               false,
+			expectedValue:             0,
+		},
+		"5 seconds": {
+			newAutoRotateInterval: "5s",
+			shouldError:           true,
+		},
+		"5 hours": {
+			newAutoRotateInterval: "5h",
+			shouldError:           false,
+			expectedValue:         5 * time.Hour,
+		},
+		"negative value": {
+			newAutoRotateInterval: "-1800s",
+			shouldError:           true,
+		},
+		"invalid string": {
+			newAutoRotateInterval: "this shouldn't work",
+			shouldError:           true,
+		},
+	}
+
+	coreConfig := &vault.CoreConfig{
+		LogicalBackends: map[string]logical.Factory{
+			"transit": Factory,
+		},
+	}
+	cluster := vault.NewTestCluster(t, coreConfig, &vault.TestClusterOptions{
+		HandlerFunc: vaulthttp.Handler,
+	})
+	cluster.Start()
+	defer cluster.Cleanup()
+	cores := cluster.Cores
+	vault.TestWaitActive(t, cores[0].Core)
+	client := cores[0].Client
+	err := client.Sys().Mount("transit", &api.MountInput{
+		Type: "transit",
+	})
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	for name, test := range tests {
+		t.Run(name, func(t *testing.T) {
+			keyNameBytes, err := uuid.GenerateRandomBytes(16)
+			if err != nil {
+				t.Fatal(err)
+			}
+			keyName := hex.EncodeToString(keyNameBytes)
+
+			_, err = client.Logical().Write(fmt.Sprintf("transit/keys/%s", keyName), map[string]interface{}{
+				"auto_rotate_interval": test.initialAutoRotateInterval,
+			})
+
+			resp, err := client.Logical().Write(fmt.Sprintf("transit/keys/%s/config", keyName), map[string]interface{}{
+				"auto_rotate_interval": test.newAutoRotateInterval,
+			})
+			switch {
+			case test.shouldError && err == nil:
+				t.Fatal("expected non-nil error")
+			case !test.shouldError && err != nil:
+				t.Fatal(err)
+			}
+
+			if !test.shouldError {
+				resp, err = client.Logical().Read(fmt.Sprintf("transit/keys/%s", keyName))
+				if err != nil {
+					t.Fatal(err)
+				}
+				if resp == nil {
+					t.Fatal("expected non-nil response")
+				}
+				got := resp.Data["auto_rotate_interval"]
+				want := test.expectedValue.String()
+				if got != want {
+					t.Fatalf("incorrect auto_rotate_interval returned, got: %s, want: %s", got, want)
+				}
+			}
+		})
+	}
 }
