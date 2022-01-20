963de70ead63637893f2bec9bf3cc0ab23b82cb7
akshya96 <araghavan@hashicorp.com>
2022-01-20 14:39:59 -0800

    fixing consts

--- vault/barrier_aes_gcm.go
+++ vault/barrier_aes_gcm.go
@@ -16,6 +16,7 @@ import (
 	"time"
 
 	"github.com/armon/go-metrics"
+	"github.com/hashicorp/vault/sdk/helper/consts"
 	"github.com/hashicorp/vault/sdk/helper/jsonutil"
 	"github.com/hashicorp/vault/sdk/helper/strutil"
 	"github.com/hashicorp/vault/sdk/logical"
@@ -33,7 +34,6 @@ const (
 
 	autoRotateCheckInterval = 5 * time.Minute
 	legacyRotateReason      = "legacy rotation"
-	MaxInt                  = int(^uint(0) >> 1)
 )
 
 // Versions of the AESGCM storage methodology
@@ -962,7 +962,7 @@ func (b *AESGCMBarrier) encrypt(path string, term uint32, gcm cipher.AEAD, plain
 	// nonce, GCM tag and the plaintext
 
 	extra := termSize + 1 + gcm.NonceSize() + gcm.Overhead()
-	if len(plain) > MaxInt-extra {
+	if len(plain) > consts.MaxInt-extra {
 		return nil, ErrPlaintextTooLarge
 	}
 
