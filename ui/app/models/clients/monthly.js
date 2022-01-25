dcf274a3803b06b1c5b65d75314f3cd121f4a306
Claire Bontempo <cbontempo@hashicorp.com>
2022-01-25 10:56:58 -0800

    add graph to current month view

--- ui/app/models/clients/monthly.js
+++ ui/app/models/clients/monthly.js
@@ -2,6 +2,9 @@ import Model, { attr } from '@ember-data/model';
 // ARG TODO copied from before, modify for what you need
 export default class Monthly extends Model {
   @attr('array') byNamespace;
+  @attr('object') total;
+  @attr('string') timestamp;
+  // TODO CMB remove 'clients' and use 'total' object?
   @attr('number') clients;
   // new names
   @attr('number') entityClients;
@@ -9,6 +12,4 @@ export default class Monthly extends Model {
   // old names
   @attr('number') distinctEntities;
   @attr('number') nonEntityTokens;
-  @attr('string') endTime;
-  @attr('string') startTime;
 }
