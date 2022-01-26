f838a11475ec28d51126802a70e1dfb1a87dc53d
Noel Quiles <3746694+EnMod@users.noreply.github.com>
2022-01-26 17:34:41 +0000

    backport of commit def7d9425299084e5ebefec0f2d873bb4801970e

--- website/lib/consent-manager-services/index.ts
+++ website/lib/consent-manager-services/index.ts
@@ -9,6 +9,14 @@ const localConsentManagerServices: ConsentManagerService[] = [
     url: 'https://js.qualified.com/qualified.js?token=CWQA3q9CaEKHNF2t',
     async: true,
   },
+  {
+    name: 'Demandbase Tag',
+    description:
+      'The Demandbase tag is a tracking service to identify website visitors and measure interest on our website.',
+    category: 'Analytics',
+    url: 'https://tag.demandbase.com/960ab0a0f20fb102.min.js',
+    async: true,
+  },
 ]
 
 export default localConsentManagerServices
