ba8a6cdc7e6e8a726f266307ca1a04267dce5dca
Victor Rodriguez <vrizo@hashicorp.com>
2022-01-28 11:14:20 -0500

    Add new parameter managed_key_id for PKI CA key generation. (#13825)

--- builtin/logical/pki/util.go
+++ builtin/logical/pki/util.go
@@ -6,7 +6,10 @@ import (
 	"strings"
 )
 
-const managedKeyNameArg = "managed_key_name"
+const (
+	managedKeyNameArg = "managed_key_name"
+	managedKeyIdArg = "managed_key_id"
+)
 
 func normalizeSerial(serial string) string {
 	return strings.Replace(strings.ToLower(serial), ":", "-", -1)
@@ -24,20 +27,33 @@ func kmsRequested(input *inputBundle) bool {
 	return exportedStr.(string) == "kms"
 }
 
-func getManagedKeyName(input *inputBundle) (string, error) {
-	arg, ok := input.apiData.GetOk(managedKeyNameArg)
-	if !ok {
-		return "", errutil.UserError{Err: fmt.Sprintf("missing %s argument", managedKeyNameArg)}
-	}
-	keyName, ok := arg.(string)
-	if !ok {
-		return "", errutil.UserError{Err: fmt.Sprintf("invalid type for argument %s", managedKeyNameArg)}
+func getManagedKeyNameOrUUID(input *inputBundle) (name string, UUID string, err error) {
+	getApiData := func(argName string) (string, error) {
+		arg, ok := input.apiData.GetOk(argName)
+		if !ok {
+			return "", nil
+		}
+
+		argValue, ok := arg.(string)
+		if !ok {
+			return "", errutil.UserError{Err: fmt.Sprintf("invalid type for argument %s", argName)}
+		}
+
+		return strings.TrimSpace(argValue), nil
 	}
 
-	trimmedArg := strings.TrimSpace(keyName)
-	if len(trimmedArg) == 0 {
-		return "", errutil.UserError{Err: fmt.Sprintf("invalid value for argument %s", managedKeyNameArg)}
+	keyName, err := getApiData(managedKeyNameArg)
+	keyUUID, err2 := getApiData(managedKeyIdArg)
+	switch {
+	case err != nil:
+		return "", "", err
+	case err2 != nil:
+		return "", "", err2
+	case len(keyName) == 0 && len(keyUUID) == 0:
+		return "", "", errutil.UserError{Err: fmt.Sprintf("missing argument %s or %s", managedKeyNameArg, managedKeyIdArg)}
+	case len(keyName) > 0 && len(keyUUID) > 0:
+		return "", "", errutil.UserError{Err: fmt.Sprintf("only one argument of %s or %s should be specified", managedKeyNameArg, managedKeyIdArg)}
 	}
 
-	return trimmedArg, nil
+	return keyName, keyUUID, nil
 }
