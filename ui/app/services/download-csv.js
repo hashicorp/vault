9c12a2753064ef1a491072a7dde2583a12700424
Claire Bontempo <cbontempo@hashicorp.com>
2022-01-28 12:14:00 -0800

    remove component and make downloadCsv a service

--- ui/app/services/download-csv.js
+++ ui/app/services/download-csv.js
@@ -1,23 +1,25 @@
 import Service from '@ember/service';
 
-// Sample of CSV format: each row needs to be \n separated, and each column separated by a comma
+// SAMPLE CSV FORMAT ('content' argument)
+// Must be a string with each row \n separated and each column comma separated
 // 'Namespace path,Authentication method,Total clients,Entity clients,Non-entity clients\n
 //  namespacelonglonglong4/,,191,171,20\n
 //  namespacelonglonglong4/,auth/method/uMGBU,35,20,15\n'
 
 export default class DownloadCsvService extends Service {
   download(filename, content) {
+    let formattedFilename = filename?.replace(/\s+/g, '-') || 'vault-data.csv';
     let { document, URL } = window;
-    let anchor = document.createElement('a');
-    anchor.download = filename;
-    anchor.href = URL.createObjectURL(
+    let downloadLink = document.createElement('a');
+    downloadLink.download = formattedFilename;
+    downloadLink.href = URL.createObjectURL(
       new Blob([content], {
         type: 'text/csv',
       })
     );
 
-    document.body.appendChild(anchor);
-    anchor.click();
-    anchor.remove();
+    document.body.appendChild(downloadLink);
+    downloadLink.click();
+    downloadLink.remove();
   }
 }
