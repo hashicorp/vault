edc3f3d89b41be43b96c7603a1dc4722f5bf94a9
Claire Bontempo <cbontempo@hashicorp.com>
2022-02-02 12:55:57 -0800

    fix serializer

--- ui/app/serializers/clients/monthly.js
+++ ui/app/serializers/clients/monthly.js
@@ -9,16 +9,18 @@ export default class MonthlySerializer extends ApplicationSerializer {
       let flattenedNs = {};
       // we don't want client counts nested within the 'counts' object for stacked charts
       Object.keys(ns['counts']).forEach((key) => (flattenedNs[key] = ns['counts'][key]));
-
       flattenedNs = this.homogenizeClientNaming(flattenedNs);
 
-      // if mounts attribution unavailable, mounts will be undefined
-      flattenedNs.mounts = ns.mounts?.map((mount) => {
-        let flattenedMount = {};
-        flattenedMount.label = mount['path'];
-        Object.keys(mount['counts']).forEach((key) => (flattenedMount[key] = mount['counts'][key]));
-        return flattenedMount;
-      });
+      // TODO CMB check how this works with actual API endpoint
+      // if no mounts, mounts will be an empty array
+      flattenedNs.mounts = ns.mounts
+        ? ns.mounts.map((mount) => {
+            let flattenedMount = {};
+            flattenedMount.label = mount['path'];
+            Object.keys(mount['counts']).forEach((key) => (flattenedMount[key] = mount['counts'][key]));
+            return flattenedMount;
+          })
+        : [];
 
       return {
         label,
@@ -32,7 +34,7 @@ export default class MonthlySerializer extends ApplicationSerializer {
   // accounting for deprecated API keys here and updating to latest nomenclature
   homogenizeClientNaming(object) {
     // TODO CMB check with API payload, latest draft includes both new and old key names
-    // Add else to delete old key names IF correct ones exist?
+    // TODO CMB Delete old key names IF correct ones exist?
     if (Object.keys(object).includes('distinct_entities', 'non_entity_tokens')) {
       let entity_clients = object.distinct_entities;
       let non_entity_clients = object.non_entity_tokens;
@@ -43,6 +45,7 @@ export default class MonthlySerializer extends ApplicationSerializer {
         non_entity_clients,
       };
     }
+    return object;
   }
 
   normalizeResponse(store, primaryModelClass, payload, id, requestType) {
