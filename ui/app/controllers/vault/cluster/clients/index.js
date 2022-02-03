c4636344d9e16737c3d8972a0c6343883d8bb412
Claire Bontempo <cbontempo@hashicorp.com>
2022-02-03 13:11:37 -0800

    change dashboard.js to history.js

--- ui/app/controllers/vault/cluster/clients/index.js
+++ ui/app/controllers/vault/cluster/clients/index.js
@@ -1,8 +1,6 @@
 import Controller from '@ember/controller';
 
 export default class ClientsController extends Controller {
-  queryParams = ['tab', 'start', 'end']; // ARG TODO remove
+  queryParams = ['tab']; // ARG TODO remove
   tab = null;
-  start = null;
-  end = null;
 }
