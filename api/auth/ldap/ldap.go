81447059f62ac62cbc36f243c406d873d1396763
Ricky Grassmuck <rigrassm@gmail.com>
2022-02-04 07:55:23 -0600

    Obtain password for LDAPAuth from File/EnvVar

--- api/auth/ldap/ldap.go
+++ api/auth/ldap/ldap.go
@@ -3,17 +3,25 @@ package ldap
 import (
 	"context"
 	"fmt"
-
 	"github.com/hashicorp/vault/api"
+	"io"
+	"os"
+	"strings"
 )
 
 type LDAPAuth struct {
-	mountPath string
-	username  string
-	password  string
+	mountPath    string
+	username     string
+	password     string
+	passwordFile string
+	passwordEnv  string
 }
 
 type Password struct {
+	// Path on the file system where the LDAP password can be found.
+	FromFile string
+	// The name of the environment variable containing the LDAP password
+	FromEnv string
 	// The password as a plaintext string value.
 	FromString string
 }
@@ -26,7 +34,7 @@ const (
 	defaultMountPath = "ldap"
 )
 
-// NewLDAPAuth initializes a new Userpass auth method interface to be
+// NewLDAPAuth initializes a new LDAP auth method interface to be
 // passed as a parameter to the client.Auth().Login method.
 //
 // Supported options: WithMountPath
@@ -39,15 +47,27 @@ func NewLDAPAuth(username string, password *Password, opts ...LoginOption) (*LDA
 		return nil, fmt.Errorf("no password provided for login")
 	}
 
+	err := password.validate()
+	if err != nil {
+		return nil, fmt.Errorf("invalid password: %w", err)
+	}
+
 	a := &LDAPAuth{
 		mountPath: defaultMountPath,
 		username:  username,
 	}
 
+	if password.FromFile != "" {
+		a.passwordFile = password.FromFile
+	}
+
+	if password.FromEnv != "" {
+		a.passwordEnv = password.FromEnv
+	}
+
 	if password.FromString != "" {
 		a.password = password.FromString
 	}
-
 	// Loop through each option
 	for _, opt := range opts {
 		// Call the option giving the instantiated
@@ -65,7 +85,22 @@ func NewLDAPAuth(username string, password *Password, opts ...LoginOption) (*LDA
 func (a *LDAPAuth) Login(ctx context.Context, client *api.Client) (*api.Secret, error) {
 	loginData := make(map[string]interface{})
 
-	loginData["password"] = a.password
+	if a.passwordFile != "" {
+		passwordValue, err := a.readPasswordFromFile()
+		if err != nil {
+			return nil, fmt.Errorf("error reading password: %w", err)
+		}
+		loginData["password"] = passwordValue
+	} else if a.passwordEnv != "" {
+		passwordValue := os.Getenv(a.passwordEnv)
+		if passwordValue == "" {
+			return nil, fmt.Errorf("password was specified with an environment variable with an empty value")
+		}
+		loginData["password"] = passwordValue
+	} else {
+		loginData["password"] = a.password
+	}
+
 	path := fmt.Sprintf("auth/%s/login/%s", a.mountPath, a.username)
 	resp, err := client.Logical().Write(path, loginData)
 	if err != nil {
@@ -80,4 +115,47 @@ func WithMountPath(mountPath string) LoginOption {
 		a.mountPath = mountPath
 		return nil
 	}
+}
+
+func (a *LDAPAuth) readPasswordFromFile() (string, error) {
+	passwordFile, err := os.Open(a.passwordFile)
+	if err != nil {
+		return "", fmt.Errorf("unable to open file containing password: %w", err)
+	}
+	defer passwordFile.Close()
+
+	limitedReader := io.LimitReader(passwordFile, 1000)
+	passwordBytes, err := io.ReadAll(limitedReader)
+	if err != nil {
+		return "", fmt.Errorf("unable to read password: %w", err)
+	}
+
+	passwordValue := strings.TrimSuffix(string(passwordBytes), "\n")
+
+	return passwordValue, nil
+}
+
+func (password *Password) validate() error {
+	if password.FromFile == "" && password.FromEnv == "" && password.FromString == "" {
+		return fmt.Errorf("password for Userpass auth must be provided with a source file, environment variable, or plaintext string")
+	}
+
+	if password.FromFile != "" {
+		if password.FromEnv != "" || password.FromString != "" {
+			return fmt.Errorf("only one source for the password should be specified")
+		}
+	}
+
+	if password.FromEnv != "" {
+		if password.FromFile != "" || password.FromString != "" {
+			return fmt.Errorf("only one source for the password should be specified")
+		}
+	}
+
+	if password.FromString != "" {
+		if password.FromFile != "" || password.FromEnv != "" {
+			return fmt.Errorf("only one source for the password should be specified")
+		}
+	}
+	return nil
 }
--- api/auth/ldap/ldap_test.go
+++ api/auth/ldap/ldap_test.go
@@ -35,7 +35,26 @@ func init() {
 	os.Setenv("VAULT_TOKEN", "")
 }
 func TestLogin(t *testing.T) {
+	passwordEnvVar := "LDAP_PASSWORD"
 	allowedPassword := "6hrtL!*bro!ywbQbvDwW"
+
+	content := []byte(allowedPassword)
+	tmpfile, err := os.CreateTemp("./", "file-containing-password")
+	if err != nil {
+		t.Fatalf("error creating temp file: %v", err)
+	}
+	defer os.Remove(tmpfile.Name()) // clean up
+	err = os.Setenv(passwordEnvVar, allowedPassword)
+	if err != nil {
+		t.Fatalf("error writing password to env var: %v", err)
+	}
+
+	if _, err := tmpfile.Write(content); err != nil {
+		t.Fatalf("error writing to temp file: %v", err)
+	}
+	if err := tmpfile.Close(); err != nil {
+		t.Fatalf("error closing temp file: %v", err)
+	}
 
 	// a response to return if the correct values were passed to login
 	authSecret := &api.Secret{
@@ -69,27 +88,64 @@ func TestLogin(t *testing.T) {
 		t.Fatalf("error initializing Vault client: %v", err)
 	}
 
+	// Password fromFile test
+	authFromFile, err := NewLDAPAuth("my-ldap-username", &Password{FromFile: tmpfile.Name()})
+	if err != nil {
+		t.Fatalf("error initializing LDAPAuth with password file: %v", err)
+	}
+
+	loginRespFromFile, err := client.Auth().Login(context.TODO(), authFromFile)
+	if err != nil {
+		t.Fatalf("error logging in with password from file: %v", err)
+	}
+
+	if loginRespFromFile.Auth == nil || loginRespFromFile.Auth.ClientToken == "" {
+		t.Fatalf("no authentication info returned by login")
+	}
+
+	// Password fromEnv Test
+	authFromEnv, err := NewLDAPAuth("my-ldap-username", &Password{FromEnv: passwordEnvVar})
+	if err != nil {
+		t.Fatalf("error initializing LDAPAuth with password env var: %v", err)
+	}
+
+	loginRespFromEnv, err := client.Auth().Login(context.TODO(), authFromEnv)
+	if err != nil {
+		t.Fatalf("error logging in with password from env var: %v", err)
+	}
+
+	if loginRespFromEnv.Auth == nil || loginRespFromEnv.Auth.ClientToken == "" {
+		t.Fatalf("no authentication info returned by login with password from env var")
+	}
+
+	// Password fromStr test
 	authFromStr, err := NewLDAPAuth("my-ldap-username", &Password{FromString: allowedPassword})
 	if err != nil {
 		t.Fatalf("error initializing LDAPAuth with password string: %v", err)
 	}
+
 	loginRespFromStr, err := client.Auth().Login(context.TODO(), authFromStr)
 	if err != nil {
 		t.Fatalf("error logging in with string: %v", err)
 	}
+
 	if loginRespFromStr.Auth == nil || loginRespFromStr.Auth.ClientToken == "" {
 		t.Fatalf("no authentication info returned by login with password from string")
 	}
 
+	// Empty User Test
 	_, err = NewLDAPAuth("", &Password{FromString: allowedPassword})
 	if err.Error() != "no user name provided for login" {
 		t.Fatalf("Auth object created for empty username: %v", err)
 	}
 
+	// Empty Password Test
 	_, err = NewLDAPAuth("my-ldap-username", nil)
 	if err.Error() != "no password provided for login" {
 		t.Fatalf("Auth object created when passing a nil Password struct: %v", err)
 	}
+
+	// Auth with Custom MountPath
 	ldapMount := WithMountPath("customMount")
 	_, err = NewLDAPAuth("my-ldap-username", &Password{FromString: allowedPassword}, ldapMount)
 	if err != nil {
