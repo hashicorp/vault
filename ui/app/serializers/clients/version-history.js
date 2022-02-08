b461db057737ff78f4ad88e9789e629a56c43cd1
Angel Garbarino <argarbarino@gmail.com>
2022-02-08 10:44:32 -0700

    remove console log

--- ui/app/serializers/clients/version-history.js
+++ ui/app/serializers/clients/version-history.js
@@ -6,7 +6,6 @@ export default ApplicationSerializer.extend({
     payload.keys.forEach((key) => {
       normalizedPayload.push({ id: key, ...payload.key_info[key] });
     });
-    console.log(normalizedPayload, 'normalizedPayload');
     return this._super(store, primaryModelClass, normalizedPayload, id, requestType);
   },
 });
