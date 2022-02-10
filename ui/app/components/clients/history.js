bfe0a66737d54f3c172a028ea1b0ba72f07868e9
Claire Bontempo <cbontempo@hashicorp.com>
2022-02-10 10:16:59 -0800

    rerun tests

--- ui/app/components/clients/history.js
+++ ui/app/components/clients/history.js
@@ -3,7 +3,6 @@ import { action } from '@ember/object';
 import { inject as service } from '@ember/service';
 import { tracked } from '@glimmer/tracking';
 import { isSameMonth, isAfter } from 'date-fns';
-
 export default class History extends Component {
   @service store;
   @service version;
