f793811f3833969a667e0a08f8f768124a4ff6f8
Jordan Reimer <zofskeez@gmail.com>
2022-02-14 09:48:45 -0700

    updates mfa-form to handle multiple methods and constraints

--- ui/app/components/mfa-form.js
+++ ui/app/components/mfa-form.js
@@ -1,7 +1,7 @@
 import Component from '@glimmer/component';
 import { inject as service } from '@ember/service';
 import { tracked } from '@glimmer/tracking';
-import { action } from '@ember/object';
+import { action, set } from '@ember/object';
 import { task, timeout } from 'ember-concurrency';
 /**
  * @module MfaForm
@@ -12,7 +12,7 @@ import { task, timeout } from 'ember-concurrency';
  * <MfaForm @clusterId={this.model.id} @authData={this.authData} />
  * ```
  * @param {string} clusterId - id of selected cluster
- * @param {object} authData - data from initial auth request -- { mfa_enforcement, backend, data }
+ * @param {object} authData - data from initial auth request -- { mfa_requirement, backend, data }
  * @param {function} onSuccess - fired when passcode passes validation
  */
 
@@ -21,17 +21,49 @@ export default class MfaForm extends Component {
 
   @tracked passcode;
   @tracked countdown;
+  @tracked errors;
+
+  get constraints() {
+    return this.args.authData.mfa_requirement.mfa_constraints;
+  }
+  get multiConstraint() {
+    return this.constraints.length > 1;
+  }
+  get singleConstraintMultiMethod() {
+    return !this.isMultiConstraint && this.constraints[0].methods.length > 1;
+  }
+  get singlePasscode() {
+    return (
+      !this.isMultiConstraint &&
+      this.constraints[0].methods.length === 1 &&
+      this.constraints[0].methods[0].uses_passcode
+    );
+  }
+  get description() {
+    let base = 'Multi-factor authentication is enabled for your account.';
+    if (this.singlePasscode) {
+      base += ' Enter your authentication code to log in.';
+    }
+    if (this.singleConstraintMultiMethod) {
+      base += ' Select the MFA method you wish to use.';
+    }
+    if (this.multiConstraint) {
+      base += ` ${this.constraints.length} methods are required for successful authentication.`;
+    }
+    return base;
+  }
 
   @task *validate() {
     try {
-      const response = yield this.auth.totpValidate(
-        { clusterId: this.args.clusterId, ...this.args.authData },
-        this.passcode
-      );
+      const response = yield this.auth.totpValidate({
+        clusterId: this.args.clusterId,
+        ...this.args.authData,
+      });
       this.args.onSuccess(response);
     } catch (error) {
-      // for now treat all errors as incorrect code
-      this.newCodeDelay.perform();
+      this.errors = error.errors;
+      // update if specific error can be parsed for incorrect passcode
+      // this.newCodeDelay.perform();
     }
   }
 
@@ -44,6 +76,10 @@ export default class MfaForm extends Component {
     }
   }
 
+  @action onSelect(constraint, id) {
+    set(constraint, 'selectedId', id);
+    set(constraint, 'selectedMethod', constraint.methods.findBy('id', id));
+  }
   @action submit(e) {
     e.preventDefault();
     this.validate.perform();
