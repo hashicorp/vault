e7bbe4ea2e96af0856bb6dc8b0d9168f012060e4
hashishaw <cshaw@hashicorp.com>
2022-02-14 10:09:22 -0600

    More clients route tests

--- ui/tests/acceptance/clients-test.js
+++ ui/tests/acceptance/clients-test.js
@@ -3,6 +3,7 @@ import { visit, currentURL } from '@ember/test-helpers';
 import { setupApplicationTest } from 'ember-qunit';
 import Pretender from 'pretender';
 import authPage from 'vault/tests/pages/auth';
+import { addMonths, format, formatRFC3339, startOfMonth, subMonths } from 'date-fns';
 
 function generateNamespaceBlock(idx = 0, skipMounts = false) {
   let mountCount = 1;
@@ -47,13 +48,13 @@ function generateConfigResponse() {
     },
   };
 }
-function generateActivityResponse(nsCount = 1) {
+function generateActivityResponse(nsCount = 1, startDate, endDate) {
   if (nsCount === 0) {
     return {
       id: 'some-activity-id',
       data: {
-        start_time: '2021-03-17T00:00:00Z',
-        end_time: '2021-12-31T23:59:59Z',
+        start_time: formatRFC3339(startDate),
+        end_time: formatRFC3339(endDate),
         total: {
           clients: 0,
           entity_clients: 0,
@@ -81,8 +82,8 @@ function generateActivityResponse(nsCount = 1) {
   return {
     id: 'some-activity-id',
     data: {
-      start_time: '2021-03-17T00:00:00Z',
-      end_time: '2021-12-31T23:59:59Z',
+      start_time: formatRFC3339(startDate),
+      end_time: formatRFC3339(endDate),
       total: {
         clients: 3637,
         entity_clients: 1643,
@@ -123,6 +124,21 @@ function generateMonthlyResponse(namespaces) {
     },
   };
 }
+function generateLicenseResponse(startDate, endDate) {
+  return {
+    request_id: 'my-license-request-id',
+    data: {
+      autoloaded: {
+        license_id: 'my-license-id',
+        start_time: formatRFC3339(startDate),
+        expiration_time: formatRFC3339(endDate),
+      },
+    },
+  };
+}
+function send(data, httpStatus = 200) {
+  return [httpStatus, { 'Content-Type': 'application/json' }, JSON.stringify(data)];
+}
 // const RES = {
 //   permissionDenied: [403, {}, JSON.stringify({ errors: ['permission denied'] })],
 //   empty: [204, {}],
@@ -135,27 +151,22 @@ module('Acceptance | clients route', function (hooks) {
     return authPage.login();
   });
 
-  test('visiting /clients with zero data', async function (assert) {
-    const configResponse = generateConfigResponse();
-    const activity = generateActivityResponse(0);
+  test('visiting /clients with zero data and config on', async function (assert) {
+    const licenseStart = startOfMonth(subMonths(new Date(), 6));
+    const licenseEnd = addMonths(new Date(), 6);
+    const config = generateConfigResponse();
     const monthly = generateMonthlyResponse();
+    const activity = generateActivityResponse(0, licenseStart, licenseEnd);
+    const license = generateLicenseResponse(licenseStart, licenseEnd);
     this.server = new Pretender(function () {
       this.get('/v1/sys/health', this.passthrough);
       this.get('/v1/sys/seal-status', this.passthrough);
       this.post('/v1/sys/capabilities-self', this.passthrough);
-      this.get('/v1/sys/license/status', this.passthrough);
-      this.get('/v1/sys/internal/counters/activity', () => {
-        return [200, { 'Content-Type': 'application/json' }, JSON.stringify(activity)];
-      });
-      this.get('/v1/sys/internal/counters/activity/monthly', () => {
-        return [200, { 'Content-Type': 'application/json' }, JSON.stringify(monthly)];
-      });
-      this.get('/v1/sys/internal/counters/config', () => {
-        return [200, { 'Content-Type': 'application/json' }, JSON.stringify(configResponse)];
-      });
-      this.get('/v1/sys/version-history', () => {
-        return [200, { 'Content-Type': 'application/json' }, JSON.stringify({ keys: [] })];
-      });
+      this.get('/v1/sys/license/status', () => send(license));
+      this.get('/v1/sys/internal/counters/activity', () => send(activity));
+      this.get('/v1/sys/internal/counters/activity/monthly', () => send(monthly));
+      this.get('/v1/sys/internal/counters/config', () => send(config));
+      this.get('/v1/sys/version-history', () => send({ keys: [] }));
     });
     await visit('/vault/clients');
     assert.equal(currentURL(), '/vault/clients');
@@ -163,7 +174,17 @@ module('Acceptance | clients route', function (hooks) {
     assert.dom('[data-test-usage-stats]').exists('usage stats block exists');
     assert.dom('[data-test-stat-text-container]').exists({ count: 3 }, '3 stat texts exist');
     assert.dom('[data-test-clients-attribution]').doesNotExist('Does not show attribution with no data');
+    // History Tab, zero data
     await visit('/vault/clients/history');
     assert.equal(currentURL(), '/vault/clients/history');
+    assert.dom('.nav-tab-link.is-active').hasText('History', 'history tab is active');
+    assert
+      .dom('[data-test-date-display]')
+      .hasText(format(licenseStart, 'MMMM yyyy'), 'billing start month is correctly parsed from license');
+    assert.dom('[data-test-clients-attribution]').exists('Attribution block is shown');
+    // TODO: Export attribution data not shown
+    assert.dom('[data-test-clients-attribution] [data-test-empty-state-title]').hasText('No data received');
+    // TODO: Attribution empty state text is correct
+    // TODO: Filters correct
   });
 });
