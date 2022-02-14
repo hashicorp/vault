23301d73431609dd6ffaf4fc55cd3932b1c7578c
Angel Garbarino <Monkeychip@users.noreply.github.com>
2022-02-14 11:30:48 -0700

    Client count: remove default period and extra controllers (#14050)
    * remove controllerS
    
    * remove default report months
    
    * remove default months from the config page
    
    * fix tests
    
    * clean up edit config

--- ui/app/components/clients/config.js
+++ ui/app/components/clients/config.js
@@ -34,11 +34,6 @@ export default class ConfigComponent extends Component {
         helperText: 'The number of months of activity logs to maintain for client tracking.',
         valueKey: 'retentionMonths',
       },
-      {
-        label: 'Default display',
-        helperText: 'The number of months we’ll display in the Vault usage dashboard by default.',
-        valueKey: 'defaultReportMonths',
-      },
     ];
   }
 
