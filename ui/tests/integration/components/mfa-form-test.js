5c5befc7f9a4688ea928955e71e6d2cef3e01ec3
Jordan Reimer <zofskeez@gmail.com>
2022-02-14 09:50:28 -0700

    adds tests

--- ui/tests/integration/components/mfa-form-test.js
+++ ui/tests/integration/components/mfa-form-test.js
@@ -1,4 +1,4 @@
-import { module, test } from 'qunit';
+import { module, test, skip } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
 import { render } from '@ember/test-helpers';
 import { hbs } from 'ember-cli-htmlbars';
@@ -15,29 +15,86 @@ module('Integration | Component | mfa-form', function (hooks) {
     this.mfaAuthData = {
       backend: 'userpass',
       data: { username: 'foo', password: 'bar' },
-      mfa_enforcement: {
-        mfa_request_id: 'test-mfa-id',
-        mfa_constraints: [this.server.create('mfa-method', { type: 'totp' })],
-      },
     };
+    this.authService = this.owner.lookup('service:auth');
   });
 
-  test('it should validate passcode', async function (assert) {
-    const validate = async (authData, passcode) => {
-      await waitUntil(() =>
-        assert.dom('[data-test-mfa-validate]').hasClass('is-loading', 'Loading class applied to button')
+  test('it should render correct descriptions', async function (assert) {
+    const totpConstraint = this.server.create('mfa-method', { type: 'totp' });
+    const oktaConstraint = this.server.create('mfa-method', { type: 'okta' });
+    const duoConstraint = this.server.create('mfa-method', { type: 'duo' });
+
+    this.mfaAuthData.mfa_requirement = this.authService._parseMfaResponse({
+      mfa_request_id: 'test-mfa-id',
+      mfa_constraints: { test_mfa_1: { any: [totpConstraint] } },
+    }).mfa_requirement;
+
+    await render(hbs`<MfaForm @clusterId={{this.clusterId}} @authData={{this.mfaAuthData}} />`);
+    assert
+      .dom('[data-test-mfa-description]')
+      .includesText(
+        'Enter your authentication code to log in.',
+        'Correct description renders for single passcode'
       );
-      assert.dom('[data-test-mfa-validate]').isDisabled('Button is disabled while loading');
-      assert.deepEqual(
-        authData,
-        { clusterId: this.clusterId, ...this.mfaAuthData },
-        'Mfa auth data passed to validate method'
+
+    this.mfaAuthData.mfa_requirement = this.authService._parseMfaResponse({
+      mfa_request_id: 'test-mfa-id',
+      mfa_constraints: { test_mfa_1: { any: [duoConstraint, oktaConstraint] } },
+    }).mfa_requirement;
+
+    await render(hbs`<MfaForm @clusterId={{this.clusterId}} @authData={{this.mfaAuthData}} />`);
+    assert
+      .dom('[data-test-mfa-description]')
+      .includesText(
+        'Select the MFA method you wish to use.',
+        'Correct description renders for multiple methods'
       );
-      assert.equal(passcode, 'test-code', 'Passcode passed to validate method');
-    };
+
+    this.mfaAuthData.mfa_requirement = this.authService._parseMfaResponse({
+      mfa_request_id: 'test-mfa-id',
+      mfa_constraints: { test_mfa_1: { any: [oktaConstraint] }, test_mfa_2: { any: [duoConstraint] } },
+    }).mfa_requirement;
+
+    await render(hbs`<MfaForm @clusterId={{this.clusterId}} @authData={{this.mfaAuthData}} />`);
+    assert
+      .dom('[data-test-mfa-description]')
+      .includesText(
+        '2 methods are required for successful authentication.',
+        'Correct description renders for multiple constraints'
+      );
+  });
+
+  test('it should render method selects and passcode inputs', async function (assert) {
+    const duoConstraint = this.server.create('mfa-method', { type: 'duo', uses_passcode: true });
+    const oktaConstraint = this.server.create('mfa-method', { type: 'okta' });
+    const pingidConstraint = this.server.create('mfa-method', { type: 'pingid' });
+    const { mfa_requirement } = this.authService._parseMfaResponse({
+      mfa_request_id: 'test-mfa-id',
+      mfa_constraints: {
+        test_mfa_1: {
+          any: [pingidConstraint, oktaConstraint],
+        },
+        test_mfa_2: {
+          any: [duoConstraint],
+        },
+      },
+    });
+    this.mfaAuthData.mfa_requirement = mfa_requirement;
+
+    this.server.post('/sys/mfa/validate', (schema, req) => {
+      const json = JSON.parse(req.requestBody);
+      const payload = {
+        mfa_request_id: 'test-mfa-id',
+        mfa_payload: { [oktaConstraint.id]: [], [duoConstraint.id]: ['test-code'] },
+      };
+      assert.deepEqual(json, payload, 'Correct mfa payload passed to validate endpoint');
+      return {};
+    });
+
     this.owner.lookup('service:auth').reopen({
-      async totpValidate(authData, passcode) {
-        await validate(authData, passcode);
+      // override to avoid authSuccess method since it expects an auth payload
+      async totpValidate({ mfa_requirement }) {
+        await this.clusterAdapter().mfaValidate(mfa_requirement);
         return 'test response';
       },
     });
@@ -52,12 +109,63 @@ module('Integration | Component | mfa-form', function (hooks) {
         @onSuccess={{this.onSuccess}}
       />
     `);
+    await fillIn('[data-test-mfa-select="0"] select', oktaConstraint.id);
+    await fillIn('[data-test-mfa-passcode="1"]', 'test-code');
+    await click('[data-test-mfa-validate]');
+  });
 
+  test('it should validate mfa requirement', async function (assert) {
+    const totpConstraint = this.server.create('mfa-method', { type: 'totp' });
+    const { mfa_requirement } = this.authService._parseMfaResponse({
+      mfa_request_id: 'test-mfa-id',
+      mfa_constraints: {
+        test_mfa: {
+          any: [totpConstraint],
+        },
+      },
+    });
+    this.mfaAuthData.mfa_requirement = mfa_requirement;
+
+    this.server.post('/sys/mfa/validate', (schema, req) => {
+      const json = JSON.parse(req.requestBody);
+      const payload = {
+        mfa_request_id: 'test-mfa-id',
+        mfa_payload: { [totpConstraint.id]: ['test-code'] },
+      };
+      assert.deepEqual(json, payload, 'Correct mfa payload passed to validate endpoint');
+      return {};
+    });
+
+    const expectedAuthData = { clusterId: this.clusterId, ...this.mfaAuthData };
+    this.owner.lookup('service:auth').reopen({
+      // override to avoid authSuccess method since it expects an auth payload
+      async totpValidate(authData) {
+        await waitUntil(() =>
+          assert.dom('[data-test-mfa-validate]').hasClass('is-loading', 'Loading class applied to button')
+        );
+        assert.dom('[data-test-mfa-validate]').isDisabled('Button is disabled while loading');
+        assert.deepEqual(authData, expectedAuthData, 'Mfa auth data passed to validate method');
+        await this.clusterAdapter().mfaValidate(authData.mfa_requirement);
+        return 'test response';
+      },
+    });
+
+    this.onSuccess = (resp) =>
+      assert.equal(resp, 'test response', 'Response is returned in onSuccess callback');
+
+    await render(hbs`
+      <MfaForm
+        @clusterId={{this.clusterId}}
+        @authData={{this.mfaAuthData}}
+        @onSuccess={{this.onSuccess}}
+      />
+    `);
     await fillIn('[data-test-mfa-passcode]', 'test-code');
     await click('[data-test-mfa-validate]');
   });
 
-  test('it should show countdown on passcode validation failure', async function (assert) {
+  // commented out in component until specific error code can be parsed from the api response
+  skip('it should show countdown on passcode validation failure', async function (assert) {
     this.owner.lookup('service:auth').reopen({
       totpValidate() {
         throw new Error('Incorrect passcode');
