e805222aca80a103660af4eeead8149533c91b84
Valerie Conklin <val@hashicorp.com>
2022-02-15 09:30:18 -0800

    Backport: Typo and clarity fixes

--- vault/logical_system.go
+++ vault/logical_system.go
@@ -4088,7 +4088,7 @@ func checkListingVisibility(visibility ListingVisibilityType) error {
 	case ListingVisibilityHidden:
 	case ListingVisibilityUnauth:
 	default:
-		return fmt.Errorf("invalid listing visilibity type")
+		return fmt.Errorf("invalid listing visibility type")
 	}
 
 	return nil
