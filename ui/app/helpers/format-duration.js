c610e8fa942b90bb120768f1cea860abee9f0f3d
Matt Schultz <975680+schultz-is@users.noreply.github.com>
2022-02-16 14:33:13 -0600

    Rename transit's auto_rotate_interval to auto_rotate_period for consistency and to achieve formatting benefits in CLI output. Update UI handling of the renamed field to account for recent data type change from time string to integral seconds. (#14103)

--- ui/app/helpers/format-duration.js
+++ ui/app/helpers/format-duration.js
@@ -2,10 +2,14 @@ import { helper } from '@ember/component/helper';
 import { assert } from '@ember/debug';
 import { formatDuration, intervalToDuration } from 'date-fns';
 
-export function duration([time]) {
+export function duration([time], { removeZero = false }) {
   // intervalToDuration creates a durationObject that turns the seconds (ex 3600) to respective:
   // { years: 0, months: 0, days: 0, hours: 1, minutes: 0, seconds: 0 }
   // then formatDuration returns the filled in keys of the durationObject
+
+  if (removeZero && time === '0') {
+    return null;
+  }
 
   // time must be in seconds
   let duration = Number.parseInt(time, 10);
