9d3dbf875f11c1b312006e499cd0baf47ef1196f
Jordan Reimer <zofskeez@gmail.com>
2022-02-17 14:47:12 -0700

    MFA Re-Merge (#14134)
    * adds development workflow to mirage config
    
    * adds mirage handler and factory for mfa workflow
    
    * adds mfa handling to auth service and cluster adapter
    
    * moves auth success logic from form to controller
    
    * adds mfa form component
    
    * shows delayed auth message for all methods
    
    * adds new code delay to mfa form
    
    * adds error views
    
    * fixes merge conflict
    
    * adds integration tests for mfa-form component
    
    * fixes auth tests
    
    * updates mfa response handling to align with backend
    
    * updates mfa-form to handle multiple methods and constraints
    
    * adds noDefault arg to Select component
    
    * updates mirage mfa handler to align with backend and adds generator for various mfa scenarios
    
    * adds tests
    
    * flaky test fix attempt
    
    * reverts test fix attempt
    
    * adds changelog entry
    
    * updates comments for todo items
    
    * removes faker from mfa mirage factory and handler
    
    * adds number to word helper
    
    * fixes tests
    
    * format-ttl helper fix from main
    
    * adds back missing parenthesis to auth-form.md
    
    * changes localStorage to sessionStorage in auth page login method

--- ui/tests/pages/auth.js
+++ ui/tests/pages/auth.js
@@ -10,6 +10,8 @@ export default create({
     // make sure we're always logged out and logged back in
     await this.logout();
     await settled();
+    // clear session storage to ensure we have a clean state
+    window.sessionStorage.clear();
     await this.visit({ with: 'token' });
     await settled();
     if (token) {
