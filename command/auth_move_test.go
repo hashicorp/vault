ba18890953d43d9157dba8af598f7e407a307d4c
Pratyoy Mukhopadhyay <prat@hashicorp.com>
2022-02-18 08:23:47 -0800

    remount cli changes

--- command/auth_move_test.go
+++ command/auth_move_test.go
@@ -4,21 +4,22 @@ import (
 	"strings"
 	"testing"
 
+	"github.com/hashicorp/vault/api"
 	"github.com/mitchellh/cli"
 )
 
-func testSecretsMoveCommand(tb testing.TB) (*cli.MockUi, *SecretsMoveCommand) {
+func testAuthMoveCommand(tb testing.TB) (*cli.MockUi, *AuthMoveCommand) {
 	tb.Helper()
 
 	ui := cli.NewMockUi()
-	return ui, &SecretsMoveCommand{
+	return ui, &AuthMoveCommand{
 		BaseCommand: &BaseCommand{
 			UI: ui,
 		},
 	}
 }
 
-func TestSecretsMoveCommand_Run(t *testing.T) {
+func TestAuthMoveCommand_Run(t *testing.T) {
 	t.Parallel()
 
 	cases := []struct {
@@ -42,7 +43,7 @@ func TestSecretsMoveCommand_Run(t *testing.T) {
 		{
 			"non_existent",
 			[]string{"not_real", "over_here"},
-			"Error moving secrets engine not_real/ to over_here/",
+			"Error moving auth method not_real/ to over_here/",
 			2,
 		},
 	}
@@ -59,7 +60,7 @@ func TestSecretsMoveCommand_Run(t *testing.T) {
 				client, closer := testVaultServer(t)
 				defer closer()
 
-				ui, cmd := testSecretsMoveCommand(t)
+				ui, cmd := testAuthMoveCommand(t)
 				cmd.client = client
 
 				code := cmd.Run(tc.args)
@@ -81,29 +82,35 @@ func TestSecretsMoveCommand_Run(t *testing.T) {
 		client, closer := testVaultServer(t)
 		defer closer()
 
-		ui, cmd := testSecretsMoveCommand(t)
+		ui, cmd := testAuthMoveCommand(t)
 		cmd.client = client
 
+		if err := client.Sys().EnableAuthWithOptions("my-auth", &api.EnableAuthOptions{
+			Type: "userpass",
+		}); err != nil {
+			t.Fatal(err)
+		}
+
 		code := cmd.Run([]string{
-			"secret/", "generic/",
+			"auth/my-auth/", "auth/my-auth-2/",
 		})
 		if exp := 0; code != exp {
 			t.Errorf("expected %d to be %d", code, exp)
 		}
 
-		expected := "Success! Finished moving secrets engine secret/ to generic/"
+		expected := "Success! Finished moving auth method auth/my-auth/ to auth/my-auth-2/"
 		combined := ui.OutputWriter.String() + ui.ErrorWriter.String()
 		if !strings.Contains(combined, expected) {
 			t.Errorf("expected %q to contain %q", combined, expected)
 		}
 
-		mounts, err := client.Sys().ListMounts()
+		mounts, err := client.Sys().ListAuth()
 		if err != nil {
 			t.Fatal(err)
 		}
 
-		if _, ok := mounts["generic/"]; !ok {
-			t.Errorf("expected mount at generic/: %#v", mounts)
+		if _, ok := mounts["my-auth-2/"]; !ok {
+			t.Errorf("expected mount at my-auth-2/: %#v", mounts)
 		}
 	})
 
@@ -113,17 +120,17 @@ func TestSecretsMoveCommand_Run(t *testing.T) {
 		client, closer := testVaultServerBad(t)
 		defer closer()
 
-		ui, cmd := testSecretsMoveCommand(t)
+		ui, cmd := testAuthMoveCommand(t)
 		cmd.client = client
 
 		code := cmd.Run([]string{
-			"secret/", "generic/",
+			"auth/my-auth/", "auth/my-auth-2/",
 		})
 		if exp := 2; code != exp {
 			t.Errorf("expected %d to be %d", code, exp)
 		}
 
-		expected := "Error moving secrets engine secret/ to generic/:"
+		expected := "Error moving auth method auth/my-auth/ to auth/my-auth-2/:"
 		combined := ui.OutputWriter.String() + ui.ErrorWriter.String()
 		if !strings.Contains(combined, expected) {
 			t.Errorf("expected %q to contain %q", combined, expected)
@@ -133,7 +140,7 @@ func TestSecretsMoveCommand_Run(t *testing.T) {
 	t.Run("no_tabs", func(t *testing.T) {
 		t.Parallel()
 
-		_, cmd := testSecretsMoveCommand(t)
+		_, cmd := testAuthMoveCommand(t)
 		assertNoTabs(t, cmd)
 	})
 }
