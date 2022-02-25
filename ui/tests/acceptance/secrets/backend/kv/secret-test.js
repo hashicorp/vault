b3d33f3764a774c72409a1ffb3e87ef0d9476649
Angel Garbarino <argarbarino@gmail.com>
2022-02-25 10:27:40 -0700

    add more coverage

--- ui/tests/acceptance/secrets/backend/kv/secret-test.js
+++ ui/tests/acceptance/secrets/backend/kv/secret-test.js
@@ -595,6 +595,11 @@ module('Acceptance | secrets/secret/create', function (hooks) {
     await settled();
     // test if metadata tab there with no read access message and no ability to edit.
     await click(`[data-test-auth-backend-link=${enginePath}]`);
+    await assert
+      .dom('[data-test-get-credentials]')
+      .exists(
+        'They do not have list access so when logged in under the restricted policy the see the get-credentials-card'
+      );
 
     // this fails in IE11 on browserstack so going directly to URL
     await visit(`/vault/secrets/${enginePath}/show/${secretPath}`);
