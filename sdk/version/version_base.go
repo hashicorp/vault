299523c781dea0657c247a9b9c70a8ecf23024b0
Chris Capurso <1036769+ccapurso@users.noreply.github.com>
2022-03-03 12:53:53 -0500

    Update version in sdk to 1.8.10. (#14356)

--- sdk/version/version_base.go
+++ sdk/version/version_base.go
@@ -8,7 +8,7 @@ var (
 	// Whether cgo is enabled or not; set at build time
 	CgoEnabled bool
 
-	Version           = "1.8.9"
+	Version           = "1.8.10"
 	VersionPrerelease = ""
 	VersionMetadata   = ""
 )
