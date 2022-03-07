fd6f6e5fbbf76c78787523fb6e5aa6a693b0066d
ryowright <ryow.college@gmail.com>
2022-03-06 16:54:46 -0800

    added error handling for tls and vault server name mismatch

--- command/agent/auth/auth.go
+++ command/agent/auth/auth.go
@@ -4,8 +4,10 @@ import (
 	"context"
 	"encoding/json"
 	"errors"
+	"fmt"
 	"math/rand"
 	"net/http"
+	"net/url"
 	"time"
 
 	"github.com/hashicorp/go-hclog"
@@ -147,6 +149,8 @@ func (ah *AuthHandler) Run(ctx context.Context, am AuthMethod) error {
 		var clientToUse *api.Client
 		var err error
 		var path string
+		var tlsServerName string
+		var clientAddr string
 		var data map[string]interface{}
 		var header http.Header
 
@@ -210,6 +214,20 @@ func (ah *AuthHandler) Run(ctx context.Context, am AuthMethod) error {
 			for _, value := range values {
 				clientToUse.AddHeader(key, value)
 			}
+		}
+
+		clientAddr = clientToUse.Address()
+		tlsServerName = clientToUse.CloneConfig().HttpClient.Transport.(*http.Transport).TLSClientConfig.ServerName
+		if tlsServerName != "" && tlsServerName != clientAddr {
+			u, _ := url.Parse(clientAddr)
+			ah.logger.Error("error authenticating", "error",
+				fmt.Sprintf(
+					"TLS Negotiation with %s failed: "+
+						"the remote's server name '%s' does not match "+
+						"the configured tls_server_name '%s'.",
+					clientAddr, u.Hostname(), tlsServerName), "backoff", backoff)
+			backoffOrQuit(ctx, backoff)
+			// continue
 		}
 
 		// This should only happen if there's no preloaded token (regular auto-auth login)
