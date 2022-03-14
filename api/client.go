2af33e07bcc5f2d17cfdd2ed324310327d2395ee
VinnyHC <vinny.mannello@hashicorp.com>
2022-03-14 09:39:57 -0700

    headers

--- api/client.go
+++ api/client.go
@@ -1081,8 +1081,14 @@ func (c *Client) httpRequestWithContext(ctx context.Context, r *Request) (*Respo
 	limiter := c.config.Limiter
 	httpClient := c.config.HttpClient
 	outputCurlString := c.config.OutputCurlString
+	if c.headers != nil {
+		for header, vals := range c.headers {
+			for _, val := range vals {
+				req.Header.Add(header, val)
+			}
+		}
+	}
 	c.config.modifyLock.RUnlock()
-
 	c.modifyLock.RUnlock()
 
 	// OutputCurlString logic relies on the request type to be retryable.Request as
@@ -1094,14 +1100,6 @@ func (c *Client) httpRequestWithContext(ctx context.Context, r *Request) (*Respo
 	req.URL.Scheme = r.URL.Scheme
 	req.URL.Host = r.URL.Host
 	req.Host = r.URL.Host
-
-	if r.Headers != nil {
-		for header, vals := range r.Headers {
-			for _, val := range vals {
-				req.Header.Add(header, val)
-			}
-		}
-	}
 
 	if len(r.ClientToken) != 0 {
 		req.Header.Set(consts.AuthHeaderName, r.ClientToken)
