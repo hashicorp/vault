4cea989c23168f1c8729ccf06b894e26e537ed6d
hc-github-team-secure-vault-core <82990506+hc-github-team-secure-vault-core@users.noreply.github.com>
2022-03-14 13:36:04 -0400

    Backport of UI/Hide empty masked PKI row values into release/1.10.x (#14473)
    * backport of commit 8b297df7cf971bce32d73c07fea2b1b8112c2f4b
    
    * backport of commit 71764b0079cc60657057bf5ac3e7cf2d27b99392
    
    * backport of commit a98076b7dad4dfcaf539929e9532bdc638a95714
    
    * backport of commit d20b063009437dbdae4cbe9b789b7c9b6b9909aa
    
    * backport of commit 7c95d39e3e43008c36f5313bf6d979244109092c
    
    * backport of commit d997656362465ae38286f365e4f39a3f2d0ebb01
    
    * backport of commit c7da14670a7d5e2384f062874044d930e43ef5f0
    
    * backport of commit 12c026679e99842fe35e47a308874ab71859287f
    
    Co-authored-by: Claire Bontempo <cbontempo@hashicorp.com>
    Co-authored-by: claire bontempo <68122737+hellobontempo@users.noreply.github.com>

--- ui/tests/acceptance/client-current-test.js
+++ ui/tests/acceptance/client-current-test.js
@@ -1,5 +1,5 @@
 import { module, test } from 'qunit';
-import { visit, currentURL, settled, click } from '@ember/test-helpers';
+import { visit, currentURL, settled, click, waitUntil, find } from '@ember/test-helpers';
 import { setupApplicationTest } from 'ember-qunit';
 import Pretender from 'pretender';
 import authPage from 'vault/tests/pages/auth';
@@ -90,7 +90,9 @@ module('Acceptance | clients current', function (hooks) {
     // Filter by namespace
     await clickTrigger();
     await searchSelect.options.objectAt(0).click();
-    await settled();
+    await waitUntil(() => {
+      return find('[data-test-horizontal-bar-chart]');
+    });
     assert.dom('[data-test-stat-text="total-clients"] .stat-value').hasText('15');
     assert.dom('[data-test-stat-text="entity-clients"] .stat-value').hasText('5');
     assert.dom('[data-test-stat-text="non-entity-clients"] .stat-value').hasText('10');
