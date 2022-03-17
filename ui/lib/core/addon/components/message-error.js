09070fc4bdb7011bf52e5459bebf24f98601182e
Claire Bontempo <cbontempo@hashicorp.com>
2022-03-16 18:51:00 -0700

    glimmerize message-error component

--- ui/lib/core/addon/components/message-error.js
+++ ui/lib/core/addon/components/message-error.js
@@ -1,6 +1,6 @@
-import { computed } from '@ember/object';
-import Component from '@ember/component';
+import Component from '@glimmer/component';
 import layout from '../templates/components/message-error';
+import { setComponentTemplate } from '@ember/component';
 
 /**
  * @module MessageError
@@ -11,49 +11,39 @@ import layout from '../templates/components/message-error';
  * <MessageError @model={{model}} />
  * ```
  *
- * @param model=null{DS.Model} - An Ember data model that will be used to bind error states to the internal
+ * @param {object} [model=null] - An Ember data model that will be used to bind error states to the internal
  * `errors` property.
- * @param errors=null{Array} - An array of error strings to show.
- * @param errorMessage=null{String} - An Error string to display.
+ * @param {array} [errors=null] - An array of error strings to show.
+ * @param {string} [errorMessage=null] - An Error string to display.
  */
-export default Component.extend({
-  layout,
-  model: null,
-  errors: null,
-  errorMessage: null,
 
-  displayErrors: computed(
-    'errorMessage',
-    'errors.[]',
-    'model.adapterError.{errors.[],message}',
-    'model.isError',
-    'parentView.mountModel.{adapterError,isError}',
-    function () {
-      const errorMessage = this.errorMessage;
-      const errors = this.errors;
-      const modelIsError = this.model?.isError || this.parentView?.mountModel.isError;
+class MessageError extends Component {
+  get displayErrors() {
+    const errorMessage = this.args.errorMessage;
+    const errors = this.args.errors;
+    const modelIsError = this.args.model?.isError;
 
-      if (modelIsError) {
-        let adapterError = this.model?.adapterError || this.parentView?.mountModel?.adapterError;
-        if (!adapterError) {
-          return;
-        }
-        if (adapterError.errors.length > 0) {
-          return adapterError.errors.map((e) => {
-            if (typeof e === 'object') return e.title || e.message || JSON.stringify(e);
-            return e;
-          });
-        }
-        return [adapterError.message];
+    if (modelIsError) {
+      let adapterError = this.args.model?.adapterError;
+      if (!adapterError) {
+        return null;
       }
-      if (errorMessage) {
-        return [errorMessage];
+      if (adapterError.errors.length > 0) {
+        return adapterError.errors.map((e) => {
+          if (typeof e === 'object') return e.title || e.message || JSON.stringify(e);
+          return e;
+        });
       }
-
-      if (errors && errors.length > 0) {
-        return errors;
-      }
-      return 'no error';
+      return [adapterError.message];
     }
-  ),
-});
+    if (errorMessage) {
+      return [errorMessage];
+    }
+    if (errors && errors.length > 0) {
+      return errors;
+    }
+    return 'no error';
+  }
+}
+
+export default setComponentTemplate(layout, MessageError);
