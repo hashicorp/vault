6a6e1b814f33a25d94755e52bb429631e8a9ad87
Angel Garbarino <argarbarino@gmail.com>
2022-03-18 13:16:34 -0600

    change back

--- ui/app/adapters/policy.js
+++ ui/app/adapters/policy.js
@@ -15,9 +15,8 @@ export default ApplicationAdapter.extend({
 
     return this.ajax(this.buildURL(type.modelName, name), 'PUT', { data }).then(() => {
       // doing this to make it like a Vault response - ember data doesn't like 204s if it's not a DELETE
-      // this.serialize(snapshot)
       return {
-        data: assign({}, snapshot.record.toJSON(), { id: name }),
+        data: assign({}, this.serialize(snapshot), { id: name }),
       };
     });
   },
