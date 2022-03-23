4b94fba9967c4ca8b3e82a8f8a6deea8521103fe
Angel Garbarino <argarbarino@gmail.com>
2022-03-23 09:42:29 -0600

    Revert "remove options"
    This reverts commit 39c46c0eac4fc582753cecc294dbf4ff0808ed0b.

--- ui/app/components/json-editor.js
+++ ui/app/components/json-editor.js
@@ -14,13 +14,42 @@ import { action } from '@ember/object';
  * @param {Function} [valueUpdated] - action to preform when you edit the codemirror value.
  * @param {Function} [onFocusOut] - action to preform when you focus out of codemirror.
  * @param {string} [helpText] - helper text.
-//  ARG TODO Fill in
+ * @param {object} [options] - option object that overrides codemirror default options such as the styling.
  */
+
+const JSON_EDITOR_DEFAULTS = {
+  // IMPORTANT: `gutters` must come before `lint` since the presence of
+  // `gutters` is cached internally when `lint` is toggled
+  gutters: ['CodeMirror-lint-markers'],
+  tabSize: 2,
+  mode: 'application/json',
+  lineNumbers: true,
+  lint: { lintOnChange: false },
+  theme: 'hashi',
+  readOnly: false,
+  showCursorWhenSelecting: true,
+};
 
 export default class JsonEditorComponent extends Component {
   value = null;
   valueUpdated = null;
   onFocusOut = null;
+  readOnly = false;
+  options = null;
+
+  constructor() {
+    super(...arguments);
+    this.options = { ...JSON_EDITOR_DEFAULTS, ...this.args.options };
+    if (this.options.autoHeight) {
+      this.options.viewportMargin = Infinity;
+      delete this.options.autoHeight;
+    }
+    if (this.options.readOnly) {
+      this.options.readOnly = 'nocursor';
+      this.options.lineNumbers = false;
+      delete this.options.gutters;
+    }
+  }
 
   get getShowToolbar() {
     return this.args.showToolbar === false ? false : true;
--- ui/app/modifiers/code-mirror.js
+++ ui/app/modifiers/code-mirror.js
@@ -103,21 +103,29 @@ export default class CodeMirrorModifier extends Modifier {
     if (!this.element) {
       throw new Error('CodeMirror modifier has no element');
     }
-    // ARG TODO I might be able to remove those with false or empty vlues check with defaults on codemirror
+
+    // const JSON_EDITOR_DEFAULTS = {
+    //   // IMPORTANT: `gutters` must come before `lint` since the presence of
+    //   // `gutters` is cached internally when `lint` is toggled
+    //   gutters: ['CodeMirror-lint-markers'],
+    //   tabSize: 2,
+    //   mode: 'application/json',
+    //   lineNumbers: true,
+    //   lint: { lintOnChange: false },
+    //   theme: 'hashi',
+    //   readOnly: false,
+    //   showCursorWhenSelecting: true,
+    // };
+
     const editor = codemirror(this.element, {
+      lineNumbers: false,
       matchBrackets: true,
-      lint: { lintOnChange: false },
-      showCursorWhenSelecting: true,
+      mode: this.mode,
+      readOnly: this.args.named.readOnly,
       styleActiveLine: true,
-      tabSize: 2,
-      extraKeys: this.args.named.extraKeys || '',
-      gutters: this.args.named.gutters || ['CodeMirror-lint-markers'],
-      lineNumbers: this.args.named.lineNumber || true,
-      mode: this.args.named.mode || 'application/json',
-      readOnly: this.args.named.readOnly || false,
-      theme: this.args.named.theme || 'hashi',
+      theme: 'hashi',
       value: this.args.named.content || '',
-      viewportMargin: this.args.named.viewportMargin || '',
+      viewportMargin: Infinity,
     });
 
     editor.on('change', bind(this, this._onChange));
