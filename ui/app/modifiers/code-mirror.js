0d1126e796c3a494eff7290faec1d4740e9bbf46
Angel Garbarino <argarbarino@gmail.com>
2022-03-23 10:11:22 -0600

    fix mode issue

--- ui/app/modifiers/code-mirror.js
+++ ui/app/modifiers/code-mirror.js
@@ -5,45 +5,9 @@ import Modifier from 'ember-modifier';
 
 import 'codemirror/addon/edit/matchbrackets';
 import 'codemirror/addon/selection/active-line';
-import 'codemirror/mode/clike/clike';
-import 'codemirror/mode/go/go';
-import 'codemirror/mode/javascript/javascript';
-import 'codemirror/mode/python/python';
-
-const EXTENSION_REGEXP = /(?:\.([^.]+))?$/;
-
-/**
- * Maps file extensions to loaded, CodeMirror-compatible language modes.
- *
- * **Important:** These CodeMirror modes must be loaded to be useable. See
- * the above imports which load the supported language modes.
- */
-// const modeMap: Record<string, string> = {
-const modeMap = {
-  go: 'text/x-go',
-  java: 'text/x-java',
-  js: 'javascript',
-  py: 'python',
-};
-
-/**
- * This is a magic CodeMirror mode string to indicate that no highlighting
- * should be used.
- *
- * See https://codemirror.net/doc/manual.html#option_mode.
- */
-const DoNotHighlight = 'null';
-
-// interface Args {
-//   named: {
-//     content: string;
-//     path: string;
-//     readOnly: boolean;
-//     onUpdate: (content: string) => void;
-//     [key: string]: unknown;
-//   };
-//   positional: never;
-// }
+// right now we only use the ruby mode, if you use another you need to import it.
+// https://codemirror.net/mode/
+import 'codemirror/mode/ruby/ruby';
 
 export default class CodeMirrorModifier extends Modifier {
   didInstall() {
@@ -57,30 +21,6 @@ export default class CodeMirrorModifier extends Modifier {
 
     this._editor.setOption('readOnly', this.args.named.readOnly);
     this._editor.setOption('mode', this.mode);
-  }
-
-  // private _editor!: CodeMirror.Editor;
-
-  /**
-   * Transforms the given path into an equivalent CodeMirror compatible
-   * language mode string by inspecting the extension.
-   *
-   * If no matching language modes are supported or the file extension cannot be
-   * determined, this will return the magic CodeMirror "null" string mode value.
-   * The value "null" indicates no highlighting should be applied.
-   */
-  get mode() {
-    if (!this.args.named.path) {
-      return DoNotHighlight;
-    }
-
-    const extension = EXTENSION_REGEXP.exec(this.args.named.path);
-
-    if (!extension || !extension[1]) {
-      return DoNotHighlight;
-    }
-
-    return modeMap[extension[1].toLowerCase()] || DoNotHighlight;
   }
 
   @action
