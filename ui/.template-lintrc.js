4f818e116830f6ccc3c6b8bce11900ff252b69f8
Angel Garbarino <argarbarino@gmail.com>
2022-03-29 12:23:43 -0600

    initial upgrade running ember-cli-update --to 3.28

--- ui/.template-lintrc.js
+++ ui/.template-lintrc.js
@@ -8,7 +8,7 @@ for (const key in testOverrides) {
 }
 
 module.exports = {
-  extends: ['octane', 'stylistic'],
+  extends: ['recommended', 'stylistic'],
   rules: {
     'no-bare-strings': 'off',
     'no-action': 'off',
@@ -22,7 +22,7 @@ module.exports = {
   },
   ignore: ['lib/story-md', 'tests/**'],
   // ember language server vscode extension does not currently respect the ignore field
-  // override all rules manually as workround to align with cli
+  // override all rules manually as workaround to align with cli
   overrides: [
     {
       files: ['**/*-test.js'],
