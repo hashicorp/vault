e1c89ec70d32e66d243a1d681e3cdfee91391ec8
Angel Garbarino <Monkeychip@users.noreply.github.com>
2022-03-29 10:25:16 -0600

    Remove Ivy Codemirror (#14659)
    * setup
    
    * fix mode issue
    
    * actions
    
    * readonly styling
    
    * remove ivycodemirror from package json
    
    * wip
    
    * test coverage for json editor
    
    * text fixes
    
    * fix tests
    
    * fix cursor issue
    
    * changelog
    
    * clean up
    
    * fix
    
    * address pr comments
    
    * unused css and it overides other styling.
    
    * fix
    
    * fix comment

--- ui/ember-cli-build.js
+++ ui/ember-cli-build.js
@@ -26,10 +26,6 @@ const appConfig = {
       return `${config.rootURL.replace(/\/$/, '')}${filePath}`;
     },
   },
-  codemirror: {
-    modes: ['javascript', 'ruby'],
-    keyMaps: ['sublime'],
-  },
   babel: {
     plugins: ['@babel/plugin-proposal-object-rest-spread', ['inline-json-import', {}]],
   },
@@ -74,8 +70,7 @@ module.exports = function (defaults) {
 
   app.import('node_modules/jsonlint/lib/jsonlint.js');
   app.import('node_modules/codemirror/addon/lint/lint.css');
-  app.import('node_modules/codemirror/addon/lint/lint.js');
-  app.import('node_modules/codemirror/addon/lint/json-lint.js');
+  app.import('node_modules/codemirror/lib/codemirror.css');
   app.import('node_modules/text-encoder-lite/text-encoder-lite.js');
   app.import('node_modules/jsondiffpatch/dist/jsondiffpatch.umd.js');
   app.import('node_modules/jsondiffpatch/dist/formatters-styles/html.css');
