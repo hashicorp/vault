0641b42c248d480a7323af1d3447d00a0b491bf4
hamid ghaf <hamid@hashicorp.com>
2022-04-01 07:22:39 -0700

    reduce the permission from 750 to 700

--- physical/raft/raft.go
+++ physical/raft/raft.go
@@ -274,7 +274,7 @@ func EnsurePath(path string, dir bool) error {
 	if !dir {
 		path = filepath.Dir(path)
 	}
-	return os.MkdirAll(path, 0o750)
+	return os.MkdirAll(path, 0o700)
 }
 
 // NewRaftBackend constructs a RaftBackend using the given directory
