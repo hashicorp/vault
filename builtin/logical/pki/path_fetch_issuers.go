30a994c422724a277165ea0c46bfecca988de64d
Alexander Scheel <alex.scheel@hashicorp.com>
2022-04-01 10:49:15 -0400

    Begin /issuers API endpoints
    This implements the fetch operations around issuers in the PKI Secrets
    Engine. We implement the following operations:
    
     - LIST /issuers - returns a list of known issuers' IDs and names.
     - GET /issuer/:ref - returns a JSON blob with information about this
       issuer.
     - POST /issuer/:ref - allows configuring information about issuers,
       presently just its name.
     - DELETE /issuer/:ref - allows deleting the specified issuer.
     - GET /issuer/:ref/{der,pem} - returns a raw API response with just
       the DER (or PEM) of the issuer's certificate.
    
    Signed-off-by: Alexander Scheel <alex.scheel@hashicorp.com>

---
+++ builtin/logical/pki/path_fetch_issuers.go
@@ -0,0 +1,252 @@
+package pki
+
+import (
+	"context"
+	"encoding/pem"
+	"regexp"
+	"strings"
+
+	"github.com/hashicorp/vault/sdk/framework"
+	"github.com/hashicorp/vault/sdk/logical"
+)
+
+var nameMatcher = regexp.MustCompile("^" + framework.GenericNameRegex("ref") + "$")
+
+func pathListIssuers(b *backend) *framework.Path {
+	return &framework.Path{
+		Pattern: "issuers/?$",
+
+		Callbacks: map[logical.Operation]framework.OperationFunc{
+			logical.ListOperation: b.pathListIssuersHandler,
+		},
+
+		HelpSynopsis:    pathListIssuersHelpSyn,
+		HelpDescription: pathListIssuersHelpDesc,
+	}
+}
+
+func (b *backend) pathListIssuersHandler(ctx context.Context, req *logical.Request, data *framework.FieldData) (*logical.Response, error) {
+	var responseKeys []string
+	responseInfo := make(map[string]interface{})
+
+	entries, err := listIssuers(ctx, req.Storage)
+	if err != nil {
+		return nil, err
+	}
+
+	// For each issuer, we need not only the identifier (as returned by
+	// listIssuers), but also the name of the issuer. This means we have to
+	// fetch the actual issuer object as well.
+	for _, identifier := range entries {
+		issuer, err := fetchIssuerById(ctx, req.Storage, identifier)
+		if err != nil {
+			return nil, err
+		}
+
+		responseKeys = append(responseKeys, string(identifier))
+		responseInfo[string(identifier)] = map[string]interface{}{
+			"name": issuer.Name,
+		}
+	}
+
+	return logical.ListResponseWithInfo(responseKeys, responseInfo), nil
+}
+
+const (
+	pathListIssuersHelpSyn  = `Fetch a list of CA certificates.`
+	pathListIssuersHelpDesc = `
+This endpoint allows listing of known issuing certificates, returning
+their identifier and their name (if set).
+`
+)
+
+func pathGetIssuer(b *backend) *framework.Path {
+	pattern := "issuer/" + framework.GenericNameRegex("ref") + "(/der|/pem)?"
+	return buildPathGetIssuer(b, pattern)
+}
+
+func buildPathGetIssuer(b *backend, pattern string) *framework.Path {
+	return &framework.Path{
+		// Returns a JSON entry.
+		Pattern: pattern,
+		Fields: map[string]*framework.FieldSchema{
+			"ref": {
+				Type:        framework.TypeString,
+				Description: `Reference to issuer; either "default" for the configured default issuer, an identifier of an issuer, or the name assigned to the issuer.`,
+				Default:     "default",
+			},
+			"name": {
+				Type:        framework.TypeString,
+				Description: `Human-readable name for this issuer.`,
+			},
+		},
+
+		Callbacks: map[logical.Operation]framework.OperationFunc{
+			logical.ReadOperation:   b.pathGetIssuer,
+			logical.UpdateOperation: b.pathUpdateIssuer,
+			logical.DeleteOperation: b.pathDeleteIssuer,
+		},
+
+		HelpSynopsis:    pathGetIssuerHelpSyn,
+		HelpDescription: pathGetIssuerHelpDesc,
+	}
+}
+
+func (b *backend) pathGetIssuer(ctx context.Context, req *logical.Request, data *framework.FieldData) (*logical.Response, error) {
+	// Handle raw issuers first.
+	if strings.HasSuffix(req.Path, "/der") || strings.HasSuffix(req.Path, "/pem") {
+		return b.pathGetRawIssuer(ctx, req, data)
+	}
+
+	issuerName := data.Get("ref").(string)
+	if len(issuerName) == 0 {
+		return logical.ErrorResponse("missing issuer reference"), nil
+	}
+
+	ref, err := resolveIssuerReference(ctx, req.Storage, issuerName)
+	if err != nil {
+		return nil, err
+	}
+	if ref == "" {
+		return logical.ErrorResponse("unable to resolve issuer id for reference: " + issuerName), nil
+	}
+
+	issuer, err := fetchIssuerById(ctx, req.Storage, ref)
+	if err != nil {
+		return nil, err
+	}
+
+	return &logical.Response{
+		Data: map[string]interface{}{
+			"id":          issuer.ID,
+			"name":        issuer.Name,
+			"key_id":      issuer.KeyID,
+			"certificate": issuer.Certificate,
+		},
+	}, nil
+}
+
+func (b *backend) pathUpdateIssuer(ctx context.Context, req *logical.Request, data *framework.FieldData) (*logical.Response, error) {
+	issuerName := data.Get("ref").(string)
+	if len(issuerName) == 0 {
+		return logical.ErrorResponse("missing issuer reference"), nil
+	}
+
+	newName := data.Get("name").(string)
+	if len(newName) > 0 && !nameMatcher.MatchString(newName) {
+		return logical.ErrorResponse("new issuer name outside of valid character limits"), nil
+	}
+
+	ref, err := resolveIssuerReference(ctx, req.Storage, issuerName)
+	if err != nil {
+		return nil, err
+	}
+	if ref == "" {
+		return logical.ErrorResponse("unable to resolve issuer id for reference: " + issuerName), nil
+	}
+
+	issuer, err := fetchIssuerById(ctx, req.Storage, ref)
+	if err != nil {
+		return nil, err
+	}
+
+	if newName != issuer.Name {
+		issuer.Name = newName
+
+		err := writeIssuer(ctx, req.Storage, issuer)
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	return &logical.Response{
+		Data: map[string]interface{}{
+			"id":          issuer.ID,
+			"name":        issuer.Name,
+			"key_id":      issuer.KeyID,
+			"certificate": issuer.Certificate,
+		},
+	}, nil
+}
+
+func (b *backend) pathGetRawIssuer(ctx context.Context, req *logical.Request, data *framework.FieldData) (*logical.Response, error) {
+	issuerName := data.Get("ref").(string)
+	if len(issuerName) == 0 {
+		return logical.ErrorResponse("missing issuer reference"), nil
+	}
+
+	ref, err := resolveIssuerReference(ctx, req.Storage, issuerName)
+	if err != nil {
+		return nil, err
+	}
+	if ref == "" {
+		return logical.ErrorResponse("unable to resolve issuer id for reference: " + issuerName), nil
+	}
+
+	issuer, err := fetchIssuerById(ctx, req.Storage, ref)
+	if err != nil {
+		return nil, err
+	}
+
+	certificate := []byte(issuer.Certificate)
+	contentType := "application/pem-certificate-chain"
+
+	if strings.HasSuffix(req.Path, "/der") {
+		contentType = "application/pkix-cert"
+
+		pemBlock, _ := pem.Decode(certificate)
+		if pemBlock == nil {
+			return nil, err
+		}
+
+		certificate = pemBlock.Bytes
+	}
+
+	statusCode := 200
+	if len(certificate) == 0 {
+		statusCode = 204
+	}
+
+	return &logical.Response{
+		Data: map[string]interface{}{
+			logical.HTTPContentType: contentType,
+			logical.HTTPRawBody:     certificate,
+			logical.HTTPStatusCode:  statusCode,
+		},
+	}, nil
+}
+
+func (b *backend) pathDeleteIssuer(ctx context.Context, req *logical.Request, data *framework.FieldData) (*logical.Response, error) {
+	issuerName := data.Get("ref").(string)
+	if len(issuerName) == 0 {
+		return logical.ErrorResponse("missing issuer reference"), nil
+	}
+
+	ref, err := resolveIssuerReference(ctx, req.Storage, issuerName)
+	if err != nil {
+		return nil, err
+	}
+	if ref == "" {
+		return logical.ErrorResponse("unable to resolve issuer id for reference: " + issuerName), nil
+	}
+
+	return nil, deleteIssuer(ctx, req.Storage, ref)
+}
+
+const (
+	pathGetIssuerHelpSyn  = `Fetch a single issuer certificate.`
+	pathGetIssuerHelpDesc = `
+This allows fetching information associated with the underlying issuer
+certificate.
+
+:ref can be either the literal value "default", in which case /config/issuers
+will be consulted for the present default issuer, an identifier of an issuer,
+or its assigned name value.
+
+Use /issuer/:ref/der or /issuer/:ref/pem to return just the certificate in
+raw DER or PEM form, without the JSON structure of /issuer/:ref.
+
+Writing to /issuer/:ref allows updating of the name field associated with
+the certificate.
+`
+)
