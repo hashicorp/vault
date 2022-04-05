f6e0f2cadaaf7aab0d79fed056fadcf3ea886a97
Noel Quiles <3746694+EnMod@users.noreply.github.com>
2022-04-05 11:00:33 -0400

    chore: Update alert banner

--- website/data/alert-banner.js
+++ website/data/alert-banner.js
@@ -2,12 +2,11 @@ export const ALERT_BANNER_ACTIVE = false
 
 // https://github.com/hashicorp/web-components/tree/master/packages/alert-banner
 export default {
-  tag: 'Blog post',
-  url: 'https://www.hashicorp.com/blog/a-new-chapter-for-hashicorp',
-  text:
-    'HashiCorp shares have begun trading on the Nasdaq. Read the blog from our founders, Mitchell Hashimoto and Armon Dadgar.',
-  linkText: 'Read the post',
+  tag: 'New',
+  url: 'https://www.hashicorp.com/#TODO-add-url',
+  text: 'Vault OSS Now Includes Multi-factor Authentication!',
+  linkText: '',
   // Set the expirationDate prop with a datetime string (e.g. '2020-01-31T12:00:00-07:00')
   // if you'd like the component to stop showing at or after a certain date
-  expirationDate: '2021-12-17T23:00:00-07:00',
+  expirationDate: '2022-04-08T23:00:00-07:00',
 }
