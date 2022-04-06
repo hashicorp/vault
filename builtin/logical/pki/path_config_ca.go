f4bf138c487a70964c0d51987de71e869a6e0478
Alexander Scheel <alex.scheel@hashicorp.com>
2022-04-06 09:38:00 -0400

    Allow configuration of default issuers
    Using the new updateDefaultIssuerId(...) from the storage migration PR
    allows for easy implementation of configuring the default issuer. We
    restrict callers from setting blank defaults and setting default to
    default.
    
    Signed-off-by: Alexander Scheel <alex.scheel@hashicorp.com>

--- builtin/logical/pki/path_config_ca.go
+++ builtin/logical/pki/path_config_ca.go
@@ -103,6 +103,72 @@ secret key and certificate.
 For security reasons, the secret key cannot be retrieved later.
 `
 
+func pathConfigIssuers(b *backend) *framework.Path {
+	return &framework.Path{
+		Pattern: "config/issuers",
+		Fields: map[string]*framework.FieldSchema{
+			"default": {
+				Type:        framework.TypeString,
+				Description: `Reference (name or identifier) to the default issuer.`,
+			},
+		},
+
+		Callbacks: map[logical.Operation]framework.OperationFunc{
+			logical.ReadOperation:   b.pathCAIssuersRead,
+			logical.UpdateOperation: b.pathCAIssuersWrite,
+		},
+
+		HelpSynopsis:    pathConfigIssuersHelpSyn,
+		HelpDescription: pathConfigIssuersHelpDesc,
+	}
+}
+
+func (b *backend) pathCAIssuersRead(ctx context.Context, req *logical.Request, data *framework.FieldData) (*logical.Response, error) {
+	config, err := getIssuersConfig(ctx, req.Storage)
+	if err != nil {
+		return logical.ErrorResponse("Error loading issuers configuration: " + err.Error()), nil
+	}
+
+	return &logical.Response{
+		Data: map[string]interface{}{
+			"default": config.DefaultIssuerId,
+		},
+	}, nil
+}
+
+func (b *backend) pathCAIssuersWrite(ctx context.Context, req *logical.Request, data *framework.FieldData) (*logical.Response, error) {
+	newDefault := data.Get("default").(string)
+	if len(newDefault) == 0 || newDefault == "default" {
+		return logical.ErrorResponse("Invalid issuer specification; must be non-empty and can't be 'default'."), nil
+	}
+
+	parsedIssuer, err := resolveIssuerReference(ctx, req.Storage, newDefault)
+	if err != nil {
+		return logical.ErrorResponse("Error resolving issuer reference: " + err.Error()), nil
+	}
+
+	err = updateDefaultIssuerId(ctx, req.Storage, parsedIssuer)
+	if err != nil {
+		return logical.ErrorResponse("Error updating issuer configuration: " + err.Error()), nil
+	}
+
+	return &logical.Response{
+		Data: map[string]interface{}{
+			"default": parsedIssuer,
+		},
+	}, nil
+}
+
+const pathConfigIssuersHelpSyn = `Read and set the default issuer certificate for signing.`
+
+const pathConfigIssuersHelpDesc = `
+This path allows configuration of issuer parameters.
+
+Presently, the "default" parameter controls which issuer is the default,
+accessible by the existing signing paths (/root/sign-intermediate,
+/root/sign-self-issued, /sign-verbatim, /sign/:role, and /issue/:role).
+`
+
 const pathConfigCAGenerateHelpSyn = `
 Generate a new CA certificate and private key used for signing.
 `
