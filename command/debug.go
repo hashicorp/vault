33ba21d5d60fbce27ac22723592339637244044c
akshya96 <87045294+akshya96@users.noreply.github.com>
2022-04-11 17:38:28 -0700

    adding changes

--- command/debug.go
+++ command/debug.go
@@ -12,11 +12,11 @@ import (
 	"strconv"
 	"strings"
 	"sync"
-	"syscall"
 	"time"
 
 	"github.com/hashicorp/go-hclog"
 	"github.com/hashicorp/vault/api"
+	"github.com/hashicorp/vault/helper/osutil"
 	"github.com/hashicorp/vault/internalshared/gatedwriter"
 	"github.com/hashicorp/vault/sdk/helper/logging"
 	"github.com/hashicorp/vault/sdk/helper/strutil"
@@ -903,7 +903,7 @@ func (c *DebugCommand) persistCollection(collection []map[string]interface{}, ou
 
 func (c *DebugCommand) compress(dst string) error {
 	if runtime.GOOS != "windows" {
-		defer syscall.Umask(syscall.Umask(0o077))
+		defer osutil.Umask(osutil.Umask(0o077))
 	}
 
 	tgz := archiver.NewTarGz()
