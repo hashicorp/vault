6f1e0ed31241f3163274448985679afdfda1bb7e
Jordan Reimer <zofskeez@gmail.com>
2022-04-12 09:03:23 -0600

    adds annotations to model-validations file

--- ui/app/decorators/model-validations.js
+++ ui/app/decorators/model-validations.js
@@ -1,6 +1,42 @@
 /* eslint-disable no-console */
 import validators from 'vault/utils/validators';
 
+/**
+ * used to validate properties on a class
+ *
+ * decorator expects validations object with the following shape:
+ * { [propertyKeyName]: [{ type, options, message }] }
+ * each key in the validations object should refer to the property on the class to apply the validation to
+ * type refers to the type of validation to apply -- must be exported from validators util for lookup
+ * options is an optional object for given validator -- min, max, nullable etc. -- see validators in util
+ * message is added to the errors array and returned from the validate method if validation fails
+ * each property supports multiple validations provided as an array -- for example, presence and length for string
+ *
+ * validations must be invoked using the validate method which is added directly to the decorated class
+ * const { isValid, state } = this.model.validate();
+ * isValid represents the validity of the full class -- if no properties provided in the validations object are invalid this will be true
+ * state represents the error state of the properties defined in the validations object
+ * const { isValid, errors } = state[propertyKeyName];
+ * isValid represents the validity of the property
+ * errors will be populated with messages defined in the validations object when validations fail
+ * since a property can have multiple validations, errors is always returned as an array
+ *
+ * full example
+ *
+ * import Model from '@ember-data/model';
+ * import withModelValidations from 'vault/decorators/model-validations';
+ *
+ * const validations = { foo: [{ type: 'presence', message: 'foo is a required field' }] };
+ * @withModelValidations(validations)
+ * class SomeModel extends Model { foo = null; }
+ *
+ * const model = new SomeModel();
+ * const { isValid, state } = model.validate();
+ * -> isValid = false;
+ * -> state.foo.isValid = false;
+ * -> state.foo.errors = ['foo is a required field'];
+ */
+
 export function withModelValidations(validations) {
   return function decorator(SuperClass) {
     return class ModelValidations extends SuperClass {
