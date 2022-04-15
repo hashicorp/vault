0d1b75795545e06e124546436d621faf753cfade
Chelsea Shaw <82459713+hashishaw@users.noreply.github.com>
2022-04-15 12:47:55 -0500

    UI: fix blank selection on search select field (#15058)
    * WIP
    
    * Cleanup
    
    * cleanup
    
    * Add changelog

--- ui/lib/core/addon/components/search-select.js
+++ ui/lib/core/addon/components/search-select.js
@@ -87,6 +87,7 @@ export default Component.extend({
         searchText: matchingOption ? matchingOption.searchText : option,
       };
     });
+
     this.set('selectedOptions', formattedOptions);
     if (this.options) {
       options = this.options.concat(options).uniq();
@@ -113,7 +114,13 @@ export default Component.extend({
         this.formatOptions(options);
       } catch (err) {
         if (err.httpStatus === 404) {
-          //leave options alone, it's okay
+          if (!this.options) {
+            // If the call failed but the resource has items
+            // from a different namespace, this allows the
+            // selected items to display
+            this.set('options', []);
+          }
+
           return;
         }
         if (err.httpStatus === 403) {
