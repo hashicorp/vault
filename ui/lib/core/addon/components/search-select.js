dfc5cec855883d77937c007d238555fdd05d49a1
hashishaw <cshaw@hashicorp.com>
2022-04-15 14:57:02 -0500

    stop flaking

--- ui/lib/core/addon/components/search-select.js
+++ ui/lib/core/addon/components/search-select.js
@@ -39,7 +39,7 @@ export default Component.extend({
   store: service(),
 
   onChange: () => {},
-  inputValue: computed(function () {
+  inputValue: computed(function() {
     return [];
   }),
   allOptions: null, // list of options including matched
@@ -62,16 +62,16 @@ export default Component.extend({
     }
     this.set('oldOptions', options);
   },
-  formatOptions: function (options) {
-    options = options.toArray().map((option) => {
+  formatOptions: function(options) {
+    options = options.toArray().map(option => {
       option.searchText = `${option.name} ${option.id}`;
       return option;
     });
-    let allOptions = options.toArray().map((option) => {
+    let allOptions = options.toArray().map(option => {
       return option.id;
     });
     this.set('allOptions', allOptions); // used by filter-wildcard helper
-    let formattedOptions = this.selectedOptions.map((option) => {
+    let formattedOptions = this.selectedOptions.map(option => {
       let matchingOption = options.findBy('id', option);
       options.removeObject(matchingOption);
       return {
@@ -87,7 +87,7 @@ export default Component.extend({
     }
     this.set('options', options);
   },
-  fetchOptions: task(function* () {
+  fetchOptions: task(function*() {
     if (!this.models) {
       if (this.options) {
         this.formatOptions(this.options);
@@ -113,7 +113,6 @@ export default Component.extend({
             // selected items to display
             this.set('options', []);
           }
-
           return;
         }
         if (err.httpStatus === 403) {
@@ -132,7 +131,7 @@ export default Component.extend({
   }).on('didInsertElement'),
   handleChange() {
     if (this.selectedOptions.length && typeof this.selectedOptions.firstObject === 'object') {
-      this.onChange(Array.from(this.selectedOptions, (option) => option.id));
+      this.onChange(Array.from(this.selectedOptions, option => option.id));
     } else {
       this.onChange(this.selectedOptions);
     }
@@ -164,7 +163,7 @@ export default Component.extend({
     },
     hideCreateOptionOnSameID(id, options) {
       if (options && options.length && options.firstObject.groupName) {
-        return !options.some((group) => group.options.findBy('id', id));
+        return !options.some(group => group.options.findBy('id', id));
       }
       let existingOption = this.options && (this.options.findBy('id', id) || this.options.findBy('name', id));
       if (this.disallowNewItems && !existingOption) {
