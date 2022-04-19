cbaeb95bbc7ef973ab1f549b917e981f2df72b9c
akshya96 <87045294+akshya96@users.noreply.github.com>
2022-04-19 10:05:30 +0530

    Fix vault debug for windows 1.8.x (#15040)
    * adding changes
    
    * adding +build

---
+++ helper/osutil/fileinfo_unix.go
@@ -0,0 +1,13 @@
+//go:build !windows
+// +build !windows
+
+package osutil
+
+import (
+	"syscall"
+)
+
+// Sets new umask and returns old umask
+func Umask(newmask int) int {
+	return syscall.Umask(newmask)
+}
---
+++ helper/osutil/fileinfo_windows.go
@@ -0,0 +1,9 @@
+//go:build windows
+// +build windows
+
+package osutil
+
+// Umask does nothing for windows for now
+func Umask(newmask int) int {
+	return 0
+}
