3a8e70db0e72316d2a6106453b4415105e9a0373
Claire Bontempo <cbontempo@hashicorp.com>
2022-04-27 14:59:50 -0700

    refactor serializers, move to util folder

--- ui/app/serializers/clients/monthly.js
+++ ui/app/serializers/clients/monthly.js
@@ -1,60 +1,8 @@
 import ApplicationSerializer from '../application';
 import { formatISO } from 'date-fns';
+import { formatByNamespace, homogenizeClientNaming } from 'core/utils/client-count-utils';
 
 export default class MonthlySerializer extends ApplicationSerializer {
-  flattenDataset(object) {
-    let flattenedObject = {};
-    Object.keys(object['counts']).forEach((key) => (flattenedObject[key] = object['counts'][key]));
-    return this.homogenizeClientNaming(flattenedObject);
-  }
-
-  formatByNamespace(namespaceArray) {
-    return namespaceArray?.map((ns) => {
-      // 'namespace_path' is an empty string for root
-      if (ns['namespace_id'] === 'root') ns['namespace_path'] = 'root';
-      let label = ns['namespace_path'];
-      let flattenedNs = this.flattenDataset(ns);
-      // if no mounts, mounts will be an empty array
-      flattenedNs.mounts = [];
-      if (ns?.mounts && ns.mounts.length > 0) {
-        flattenedNs.mounts = ns.mounts.map((mount) => {
-          return {
-            label: mount['mount_path'],
-            ...this.flattenDataset(mount),
-          };
-        });
-      }
-      return {
-        label,
-        ...flattenedNs,
-      };
-    });
-  }
-
-  // In 1.10 'distinct_entities' changed to 'entity_clients' and
-  // 'non_entity_tokens' to 'non_entity_clients'
-  homogenizeClientNaming(object) {
-    // if new key names exist, only return those key/value pairs
-    if (Object.keys(object).includes('entity_clients')) {
-      let { clients, entity_clients, non_entity_clients } = object;
-      return {
-        clients,
-        entity_clients,
-        non_entity_clients,
-      };
-    }
-    // if object only has outdated key names, update naming
-    if (Object.keys(object).includes('distinct_entities')) {
-      let { clients, distinct_entities, non_entity_tokens } = object;
-      return {
-        clients,
-        entity_clients: distinct_entities,
-        non_entity_clients: non_entity_tokens,
-      };
-    }
-    return object;
-  }
-
   normalizeResponse(store, primaryModelClass, payload, id, requestType) {
     if (payload.id === 'no-data') {
       return super.normalizeResponse(store, primaryModelClass, payload, id, requestType);
@@ -65,8 +13,8 @@ export default class MonthlySerializer extends ApplicationSerializer {
     let newClientsData = payload.data.months[0]?.new_clients || null;
     let by_namespace_new_clients, new_clients;
     if (newClientsData) {
-      by_namespace_new_clients = this.formatByNamespace(newClientsData.namespaces);
-      new_clients = this.homogenizeClientNaming(newClientsData.counts);
+      by_namespace_new_clients = formatByNamespace(newClientsData.namespaces);
+      new_clients = homogenizeClientNaming(newClientsData.counts);
     } else {
       by_namespace_new_clients = [];
       new_clients = [];
@@ -74,10 +22,10 @@ export default class MonthlySerializer extends ApplicationSerializer {
     let transformedPayload = {
       ...payload,
       response_timestamp,
-      by_namespace_total_clients: this.formatByNamespace(payload.data.by_namespace),
+      by_namespace_total_clients: formatByNamespace(payload.data.by_namespace),
       by_namespace_new_clients,
       // nest within 'total' object to mimic /activity response shape
-      total: this.homogenizeClientNaming(payload.data),
+      total: homogenizeClientNaming(payload.data),
       new: new_clients,
     };
     delete payload.data.by_namespace;
---
+++ ui/lib/core/addon/utils/client-count-utils.js
@@ -0,0 +1,110 @@
+import { parseAPITimestamp } from 'core/utils/date-formatters';
+
+export const flattenDataset = (object) => {
+  let flattenedObject = {};
+  Object.keys(object['counts']).forEach((key) => (flattenedObject[key] = object['counts'][key]));
+  return homogenizeClientNaming(flattenedObject);
+};
+
+export const formatByMonths = (monthsArray) => {
+  const sortedPayload = [...monthsArray];
+  // months are always returned from the API: [mostRecent...oldestMonth]
+  sortedPayload.reverse();
+  return sortedPayload.map((m) => {
+    if (Object.keys(m).includes('counts')) {
+      let totalClients = flattenDataset(m);
+      let newClients = flattenDataset(m.new_clients);
+      m = {
+        month: parseAPITimestamp(m.timestamp, 'M/yy'),
+        ...totalClients,
+        namespaces: formatByNamespace(m.namespaces),
+        new_clients: {
+          month: parseAPITimestamp(m.timestamp, 'M/yy'),
+          ...newClients,
+          namespaces: formatByNamespace(m.new_clients.namespaces),
+        },
+      };
+      return nestCountsWithinNamespaceKey(m);
+    }
+    // TODO CMB below is an assumption, need to test
+    // if no monthly data (no counts key), month object will just contain a timestamp
+    return {
+      month: parseAPITimestamp(m.timestamp, 'M/yy'),
+      new_clients: {
+        month: parseAPITimestamp(m.timestamp, 'M/yy'),
+      },
+    };
+  });
+};
+
+export const formatByNamespace = (namespaceArray) => {
+  return namespaceArray?.map((ns) => {
+    // 'namespace_path' is an empty string for root
+    if (ns['namespace_id'] === 'root') ns['namespace_path'] = 'root';
+    let label = ns['namespace_path'];
+    let flattenedNs = flattenDataset(ns);
+    // if no mounts, mounts will be an empty array
+    flattenedNs.mounts = [];
+    if (ns?.mounts && ns.mounts.length > 0) {
+      flattenedNs.mounts = ns.mounts.map((mount) => {
+        return {
+          label: mount['mount_path'],
+          ...flattenDataset(mount),
+        };
+      });
+    }
+    return {
+      label,
+      ...flattenedNs,
+    };
+  });
+};
+
+// In 1.10 'distinct_entities' changed to 'entity_clients' and
+// 'non_entity_tokens' to 'non_entity_clients'
+export const homogenizeClientNaming = (object) => {
+  // if new key names exist, only return those key/value pairs
+  if (Object.keys(object).includes('entity_clients')) {
+    let { clients, entity_clients, non_entity_clients } = object;
+    return {
+      clients,
+      entity_clients,
+      non_entity_clients,
+    };
+  }
+  // if object only has outdated key names, update naming
+  if (Object.keys(object).includes('distinct_entities')) {
+    let { clients, distinct_entities, non_entity_tokens } = object;
+    return {
+      clients,
+      entity_clients: distinct_entities,
+      non_entity_clients: non_entity_tokens,
+    };
+  }
+  return object;
+};
+
+export const nestCountsWithinNamespaceKey = (month) => {
+  // create new key of `counts_by_namespace` for month object
+  month.counts_by_namespace = {};
+  if (month.namespaces) {
+    month.namespaces.forEach((namespace) => {
+      let { clients, entity_clients, non_entity_clients, mounts } = namespace;
+      let new_clients = {};
+      if (month.new_clients) {
+        new_clients = month.new_clients.namespaces.find((n) => n.label === namespace.label) || {};
+      }
+      // create counts object with namespace label as key name
+      month.counts_by_namespace[namespace.label] = {
+        clients,
+        entity_clients,
+        non_entity_clients,
+        mounts,
+        new_clients,
+      };
+      // TODO delete or keep new_clients.label within namespace key object?
+      // delete month.counts_by_namespace[namespace.label].new_clients.label
+    });
+  }
+  return month;
+};
