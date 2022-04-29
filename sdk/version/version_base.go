b5976008f9546f711a917f220da11719714708f6
Chris Hoffman <99742+chrishoffman@users.noreply.github.com>
2022-04-29 17:42:08 -0400

    update SDK to 1.8.12 (#15241)

--- sdk/version/version_base.go
+++ sdk/version/version_base.go
@@ -8,7 +8,7 @@ var (
 	// Whether cgo is enabled or not; set at build time
 	CgoEnabled bool
 
-	Version           = "1.8.11"
+	Version           = "1.8.12"
 	VersionPrerelease = ""
 	VersionMetadata   = ""
 )
