bdb59a36e632dc8a0b7b1ec2087b5f0a37e5be4b
Chris Hoffman <99742+chrishoffman@users.noreply.github.com>
2022-04-29 18:00:57 -0400

    Update SDK to 1.10.3 (#15243)

--- sdk/version/version_base.go
+++ sdk/version/version_base.go
@@ -8,7 +8,7 @@ var (
 	// Whether cgo is enabled or not; set at build time
 	CgoEnabled bool
 
-	Version           = "1.10.2"
+	Version           = "1.10.3"
 	VersionPrerelease = ""
 	VersionMetadata   = ""
 )
