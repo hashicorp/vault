e594a2847394d5ddadb6263e94256907a38b47f3
Alexander Tischenko <tsm@archaron.ru>
2022-05-03 15:34:11 +0300

    When running under systemd, send ready when server completed reloading config #7028 (#15041)

--- command/server.go
+++ command/server.go
@@ -1594,6 +1594,9 @@ func (c *ServerCommand) Run(args []string) int {
 			default:
 			}
 
+			// Notify systemd that the server has completed reloading config
+			c.notifySystemd(systemd.SdNotifyReady)
+
 		case <-c.SigUSR2Ch:
 			logWriter := c.logger.StandardWriter(&hclog.StandardLoggerOptions{})
 			pprof.Lookup("goroutine").WriteTo(logWriter, 2)
