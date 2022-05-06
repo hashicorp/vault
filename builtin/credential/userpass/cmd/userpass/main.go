b55b86ae4ead846cc5a9a373f249783d5a8d30c4
JM Faircloth <jmfaircloth@hashicorp.com>
2022-05-06 09:15:05 -0500

    don't remove apiclientmeta parsing

--- builtin/credential/userpass/cmd/userpass/main.go
+++ builtin/credential/userpass/cmd/userpass/main.go
@@ -4,11 +4,15 @@ import (
 	"os"
 
 	hclog "github.com/hashicorp/go-hclog"
+	"github.com/hashicorp/vault/api"
 	"github.com/hashicorp/vault/builtin/credential/userpass"
 	"github.com/hashicorp/vault/sdk/plugin"
 )
 
 func main() {
+	apiClientMeta := &api.PluginAPIClientMeta{}
+	flags := apiClientMeta.FlagSet()
+	flags.Parse(os.Args[1:])
 
 	if err := plugin.ServeMultiplex(&plugin.ServeOpts{
 		BackendFactoryFunc: userpass.Factory,
