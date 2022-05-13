307c1534a9b5493d6507a2da7caae561aad507fc
Arnav Palnitkar <arnav@hashicorp.com>
2022-05-13 16:29:26 +0000

    backport of commit 81f70db51e356395028bd01f3f68403c6a800bcd

--- ui/app/routes/vault/cluster/oidc-callback.js
+++ ui/app/routes/vault/cluster/oidc-callback.js
@@ -12,12 +12,22 @@ export default Route.extend({
     // Ember paramsFor used to strip out the value after the "=" sign. In short ns value was not being passed along.
     let urlParams = new URLSearchParams(queryString);
     let state = urlParams.get('state'),
-      code = urlParams.get('code');
+      code = urlParams.get('code'),
+      ns;
+    if (state.includes(',ns=')) {
+      let arrayParams = state.split(',ns=');
+      state = arrayParams[0];
+      ns = arrayParams[1];
+    }
     let { auth_path: path } = this.paramsFor(this.routeName);
     let { namespaceQueryParam: namespace } = this.paramsFor('vault.cluster');
     path = window.decodeURIComponent(path);
     const source = 'oidc-callback'; // required by event listener in auth-jwt component
     let queryParams = { source, namespace, path, code, state };
+    // If state had ns value, send it as part of namespace param
+    if (ns) {
+      queryParams.namespace = ns;
+    }
     window.opener.postMessage(queryParams, window.origin);
   },
   renderTemplate() {
