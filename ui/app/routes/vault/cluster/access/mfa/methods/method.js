6a82b7e4446533e1f19bdf5eef14e8c02b4a7b31
Arnav Palnitkar <arnav@hashicorp.com>
2022-05-17 14:25:15 -0700

    Enforcement view at MFA method level
    - List enforcements for each mfa method
    - Delete MFA method if no enforcements are present
    - Moved method, enforcement list item component to mfa folder

--- ui/app/routes/vault/cluster/access/mfa/methods/method.js
+++ ui/app/routes/vault/cluster/access/mfa/methods/method.js
@@ -1,8 +1,22 @@
 import Route from '@ember/routing/route';
-
+import { hash } from 'rsvp';
 export default class MfaMethodRoute extends Route {
   model({ id }) {
-    return this.store.findRecord('mfa-method', id);
+    return hash({
+      method: this.store.findRecord('mfa-method', id).then((data) => data),
+      enforcements: this.store
+        .query('mfa-login-enforcement', {})
+        .then((data) => {
+          let filteredEnforcements = data.filter((item) => {
+            let results = item.hasMany('mfa_methods').ids();
+            return results.includes(id);
+          });
+          return filteredEnforcements;
+        })
+        .catch(() => {
+          // Do nothing
+        }),
+    });
   }
   setupController(controller, model) {
     controller.set('model', model);
