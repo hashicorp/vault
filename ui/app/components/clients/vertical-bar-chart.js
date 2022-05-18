07f39fd269752a5d3585495b0cf1ae3f04127be8
claire bontempo <68122737+hellobontempo@users.noreply.github.com>
2022-05-18 16:47:16 -0700

    UI/cleanup client charts for 1.11 (#15408)
    * remove manipulations of data ranges;
    
    * fix bar aligntment
    
    * consume empty months existing on response
    
    * revert grey bar transformation
    
    * up one more line..
    
    * remove attr

--- ui/app/components/clients/vertical-bar-chart.js
+++ ui/app/components/clients/vertical-bar-chart.js
@@ -5,7 +5,7 @@ import { max } from 'd3-array';
 // eslint-disable-next-line no-unused-vars
 import { select, selectAll, node } from 'd3-selection';
 import { axisLeft, axisBottom } from 'd3-axis';
-import { scaleLinear, scaleBand } from 'd3-scale';
+import { scaleLinear, scalePoint } from 'd3-scale';
 import { stack } from 'd3-shape';
 import {
   GREY,
@@ -54,18 +54,17 @@ export default class VerticalBarChart extends Component {
     const stackFunction = stack().keys(this.chartLegend.map((l) => l.key));
     const stackedData = stackFunction(filteredData);
     const chartSvg = select(element);
-    const dataMax = max(filteredData.map((d) => d[this.yKey]));
-    const domainMax = Math.ceil(dataMax / 10) * 10; // we want to round UP to the nearest tens place ex. dataMax = 102, domainMax = 110
+    const domainMax = max(filteredData.map((d) => d[this.yKey]));
 
     chartSvg.attr('viewBox', `-50 20 600 ${SVG_DIMENSIONS.height}`); // set svg dimensions
 
     // DEFINE DATA BAR SCALES
     const yScale = scaleLinear().domain([0, domainMax]).range([0, 100]).nice();
 
-    const xScale = scaleBand()
+    const xScale = scalePoint()
       .domain(dataset.map((d) => d[this.xKey]))
       .range([0, SVG_DIMENSIONS.width]) // set width to fix number of pixels
-      .paddingInner(0.85);
+      .padding(0.2);
 
     // clear out DOM before appending anything
     chartSvg.selectAll('g').remove().exit().data(stackedData).enter();
