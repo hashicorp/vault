75630ef2b4608724fdbafa809b45c5ee387bc537
Angel Garbarino <Monkeychip@users.noreply.github.com>
2022-05-19 14:32:40 -0700

    UI/mfa small bugs (#15522)
    * remove pagintion and fix on methods list view
    
    * fix enforcements

--- ui/app/routes/vault/cluster/access/mfa/enforcements/index.js
+++ ui/app/routes/vault/cluster/access/mfa/enforcements/index.js
@@ -1,25 +1,14 @@
 import Route from '@ember/routing/route';
 
 export default class MfaEnforcementsRoute extends Route {
-  queryParams = {
-    page: {
-      refreshModel: true,
-    },
-  };
-
-  model(params) {
-    return this.store
-      .lazyPaginatedQuery('mfa-login-enforcement', {
-        responsePath: 'data.keys',
-        page: params.page || 1,
-      })
-      .catch((err) => {
-        if (err.httpStatus === 404) {
-          return [];
-        } else {
-          throw err;
-        }
-      });
+  model() {
+    return this.store.query('mfa-login-enforcement', {}).catch((err) => {
+      if (err.httpStatus === 404) {
+        return [];
+      } else {
+        throw err;
+      }
+    });
   }
   setupController(controller, model) {
     controller.set('model', model);
