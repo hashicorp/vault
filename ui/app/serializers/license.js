60f4e12fc7d55f7d4c9e6a1d472c90c34545823d
Arnav Palnitkar <arnav@hashicorp.com>
2022-05-19 11:21:49 -0700

    Add defensive check in serializer

--- ui/app/serializers/license.js
+++ ui/app/serializers/license.js
@@ -3,10 +3,12 @@ import ApplicationSerializer from './application';
 export default ApplicationSerializer.extend({
   normalizeResponse(store, primaryModelClass, payload, id, requestType) {
     let transformedPayload = { autoloaded: payload.data.autoloading_used, license_id: 'no-license' };
-    transformedPayload = {
-      ...transformedPayload,
-      ...payload.data.autoloaded,
-    };
+    if (payload.data.autoloaded) {
+      transformedPayload = {
+        ...transformedPayload,
+        ...payload.data.autoloaded,
+      };
+    }
     transformedPayload.id = transformedPayload.license_id;
     return this._super(store, primaryModelClass, transformedPayload, id, requestType);
   },
