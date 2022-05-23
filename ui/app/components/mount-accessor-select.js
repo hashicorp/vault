f7bce7ee14249c3de97e652bf37fabb301a9221a
Angel Garbarino <argarbarino@gmail.com>
2022-05-23 15:15:53 -0600

    clean up

--- ui/app/components/mount-accessor-select.js
+++ ui/app/components/mount-accessor-select.js
@@ -13,11 +13,11 @@ import { action } from '@ember/object';
  * ```
  * @param {string} value - the selected value.
  * @param {function} onChange - the parent function that handles when a new value is selected.
- * @param {boolean} [showAccessor] - whether or not you should show the value or the more detailed accessor off the class
+ * @param {boolean} [showAccessor] - whether or not you should show the value or the more detailed accessor off the class.
  * @param {boolean} [noDefault] - whether or not there is a default value.
- * @param {boolean} [filterToken] -whether or not you should filter out type "token".
- * @param {string} [name] - name on the label
- * @param {string} [label] - label above the select input
+ * @param {boolean} [filterToken] - whether or not you should filter out type "token".
+ * @param {string} [name] - name on the label.
+ * @param {string} [label] - label above the select input.
  * @param {string} [helpText] - text shown in tooltip.
  */
 
