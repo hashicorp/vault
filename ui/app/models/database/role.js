03bb99f8976a797d280a01c23a2c26db1806d1c2
linda9379 <linda.jiang@hashicorp.com>
2022-05-24 14:39:26 -0400

    Fixed conditional to filter for only elasticsearch database and changed format of text in changelog

--- ui/app/models/database/role.js
+++ ui/app/models/database/role.js
@@ -3,7 +3,7 @@ import { computed } from '@ember/object';
 import { alias } from '@ember/object/computed';
 import lazyCapabilities, { apiPath } from 'vault/macros/lazy-capabilities';
 import { expandAttributeMeta } from 'vault/utils/field-to-attrs';
-import { getRoleFields, getStatementFields } from '../../utils/database-helpers';
+import { getRoleFields } from 'vault/utils/database-helpers';
 
 export default Model.extend({
   idPrefix: 'role/',
@@ -92,8 +92,7 @@ export default Model.extend({
   get showFields() {
     let fields = ['name', 'database', 'type'];
     fields = fields.concat(getRoleFields(this.type)).concat(['creation_statements']);
-    const plugin = this.database + '-database-plugin';
-    if (getStatementFields(this.type, plugin).includes('revocation_statements')) {
+    if (this.database != 'elasticsearch') {
       fields = fields.concat(['revocation_statements']);
     }
     return expandAttributeMeta(this, fields);
