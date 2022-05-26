c05b433de397bd4b2bcd03a5da1999186536e967
Arnav Palnitkar <arnav@hashicorp.com>
2022-05-26 12:37:19 -0700

    Update to ttl component (#15648)
    * Update to ttl component
    
    - Allow the ttl component to work without displaying toggle
    - Used in mfa method totp form
    
    * Added tooltip if toggle is not present
    
    - Fixed MFA delete message

--- ui/lib/core/addon/components/ttl-picker2.js
+++ ui/lib/core/addon/components/ttl-picker2.js
@@ -19,6 +19,7 @@
  * @param initialValue=null {String} - This is the value set initially (particularly from a string like '30h')
  * @param initialEnabled=null {Boolean} - Set this value if you want the toggle on when component is mounted
  * @param changeOnInit=false {Boolean} - set this value if you'd like the passed onChange function to be called on component initialization
+ * @param hideToggle=false {Boolean} - set this value if you'd like to hide the toggle and just leverage the input field
  */
 
 import { computed } from '@ember/object';
@@ -53,6 +54,7 @@ export default TtlForm.extend({
   unit: 's',
   initialValue: null,
   changeOnInit: false,
+  hideToggle: false,
 
   init() {
     this._super(...arguments);
@@ -140,8 +142,9 @@ export default TtlForm.extend({
     'helperTextEnabled',
     'helperTextSet',
     'helperTextUnset',
+    'hideToggle',
     function () {
-      return this.enableTTL ? this.helperTextEnabled : this.helperTextDisabled;
+      return this.enableTTL || this.hideToggle ? this.helperTextEnabled : this.helperTextDisabled;
     }
   ),
 
