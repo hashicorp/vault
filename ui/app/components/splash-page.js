abdc434030c0de5446de112382087b233e6e99f0
Angel Garbarino <argarbarino@gmail.com>
2022-05-31 09:58:19 -0600

    glimmerize

--- ui/app/components/splash-page.js
+++ ui/app/components/splash-page.js
@@ -1,15 +1,37 @@
+/**
+ * @module SplashPage
+ * SplashPage component is used as a landing page with a box horizontally and centered align on the page. It's used as the login landing page.
+ *
+ *
+ * @example
+ * ```js
+ * <SplashPage >
+ * content here
+ * </SplashPage
+ * ```
+ * @param {boolean} [hasAltContent] - boolean to bypass the UiWizard.
+ * @param {boolean} [showTruncatedNavBar] - boolean to hide or show the navBar. By default this is true.
+ *
+ */
+
+import Component from '@glimmer/component';
 import { inject as service } from '@ember/service';
-import Component from '@ember/component';
-import { computed } from '@ember/object';
 
-export default Component.extend({
-  version: service(),
-  auth: service(),
-  store: service(),
-  tagName: '',
-  showTruncatedNavBar: true,
+export default class SplashPage extends Component {
+  @service version;
+  @service auth;
+  @service store;
 
-  activeCluster: computed('auth.activeCluster', function () {
+  get showTruncatedNavBar() {
+    // default is true unless showTruncatedNavBar is defined as false
+    if (this.args.showTruncatedNavBar === undefined) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  get activeCluster() {
     return this.store.peekRecord('cluster', this.auth.activeCluster);
-  }),
-});
+  }
+}
