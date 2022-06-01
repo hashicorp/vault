bd0020dde77d7a3d5618c95080b55409f51bdfa2
Angel Garbarino <argarbarino@gmail.com>
2022-06-01 15:30:42 -0600

    alert-popup glimmerize:

--- ui/app/components/alert-popup.js
+++ ui/app/components/alert-popup.js
@@ -1,5 +1,3 @@
-import OuterHTML from './outer-html';
-
 /**
  * @module AlertPopup
  * The `AlertPopup` is an implementation of the [ember-cli-flash](https://github.com/poteto/ember-cli-flash) `flashMessage`.
@@ -8,14 +6,22 @@ import OuterHTML from './outer-html';
  * // All properties are passed in from the flashMessage service.
  *   <AlertPopup @type={{message-types flash.type}} @message={{flash.message}} @close={{close}}/>```
  *
- * @param type=null {String} - The alert type. This comes from the message-types helper.
- * @param [message=null] {String} - The alert message.
- * @param close=null {Func} - The close action which will close the alert.
+ * @param {string} type=null - The alert type. This comes from the message-types helper.
+ * @param {string} [message=null] - The alert message.
+ * @param {function} close=null - The close action which will close the alert.
  *
  */
 
-export default OuterHTML.extend({
-  type: null,
-  message: null,
-  close: null,
-});
+import Component from '@glimmer/component';
+
+export default class AlertPopup extends Component {
+  get type() {
+    return this.args.type || null;
+  }
+  get message() {
+    return this.args.message || null;
+  }
+  get close() {
+    return this.args.close || null;
+  }
+}
