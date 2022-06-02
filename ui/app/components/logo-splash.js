ab597ae2135413542f2e8edc4df7f7664ed2f376
Angel Garbarino <argarbarino@gmail.com>
2022-06-02 10:24:30 -0600

    logo splash and wizard section

--- ui/app/components/logo-splash.js
+++ ui/app/components/logo-splash.js
@@ -1,1 +1,13 @@
-export { default } from './outer-html';
+import templateOnly from '@ember/component/template-only';
+
+/**
+ * @module LogoSplash
+ * LogoSplash components are holders for the Vault Logo
+ *
+ * @example
+ * ```js
+ * <LogoSplash />
+ * ```
+ */
+
+export default templateOnly();
