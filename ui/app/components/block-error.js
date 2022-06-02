91347564fa30ecff3e147ddfc4ab90f68ea466ac
Angel Garbarino <argarbarino@gmail.com>
2022-06-02 10:09:53 -0600

    glimmerize block-error

--- ui/app/components/block-error.js
+++ ui/app/components/block-error.js
@@ -1,2 +1,15 @@
-import OuterHTML from './outer-html';
-export default OuterHTML.extend();
+import templateOnly from '@ember/component/template-only';
+
+/**
+ * @module BlockError
+ * BlockError components are a wrapper around error messages
+ *
+ * @example
+ * ```js
+ * <BlockError >
+ * content
+ * </BlockError>
+ * ```
+ */
+
+export default templateOnly();
