bdc63424e2ec035b2d84a745e2cd1019f96ef066
Angel Garbarino <Monkeychip@users.noreply.github.com>
2022-06-02 13:40:04 -0700

    Glimmerizing StatusMenu/SelectableCard/PkiCertPopup (#15716)
    * glimmerize status-menu
    
    * selectable card start
    
    * finish selectable card
    
    * test fix
    
    * pki-cert-popup glimmerize
    
    * cleanup
    
    * small fix
    
    * nit
    
    * one more nit
    
    * pr comments

--- ui/app/components/status-menu.js
+++ ui/app/components/status-menu.js
@@ -1,19 +1,38 @@
+import Component from '@glimmer/component';
+import { action } from '@ember/object';
 import { inject as service } from '@ember/service';
-import { alias } from '@ember/object/computed';
-import Component from '@ember/component';
-import { computed } from '@ember/object';
 
-export default Component.extend({
-  currentCluster: service('current-cluster'),
-  cluster: alias('currentCluster.cluster'),
-  auth: service(),
-  media: service(),
-  type: 'cluster',
-  itemTag: null,
-  glyphName: computed('type', function () {
-    return {
-      cluster: 'circle-dot',
-      user: 'user',
-    }[this.type];
-  }),
-});
+/**
+ * @module StatusMenu
+ * StatusMenu component is the drop down menu on the main navigation.
+ *
+ * @example
+ * ```js
+ * <StatusMenu @label='user' @onLinkClick={{action Nav.closeDrawer}}/>
+ * ```
+ * @param {string} [ariaLabel] - aria label for the status icon.
+ * @param {string} [label] - label for the status menu.
+ * @param {string} [type] - determines where the component is being used. e.g. replication, auth, etc.
+ * @param {function} [onLinkClick] - function to handle click on the nested links under content.
+ *
+ */
+
+export default class StatusMenu extends Component {
+  @service currentCluster;
+  @service auth;
+  @service media;
+
+  get type() {
+    return this.args.type || 'cluster';
+  }
+
+  get glyphName() {
+    return this.type === 'user' ? 'user' : 'circle-dot';
+  }
+
+  @action
+  handleClick(d) {
+    this.args.onLinkClick;
+    d.actions.close();
+  }
+}
