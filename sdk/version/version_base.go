a951aa795d0d62ab4b28633a4d6593c13d163281
Steven Clark <steven.clark@hashicorp.com>
2022-06-13 10:57:12 -0400

    Update SDK to 1.9.8 (#15948)

--- sdk/version/version_base.go
+++ sdk/version/version_base.go
@@ -8,7 +8,7 @@ var (
 	// Whether cgo is enabled or not; set at build time
 	CgoEnabled bool
 
-	Version           = "1.9.7"
+	Version           = "1.9.8"
 	VersionPrerelease = ""
 	VersionMetadata   = ""
 )
