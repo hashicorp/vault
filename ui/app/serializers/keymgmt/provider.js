73dc088f3fceb1dcd9bd32d9019fbc4dfceb78e9
Arnav Palnitkar <arnav@hashicorp.com>
2022-06-14 10:44:02 -0700

    KMSE provider list menu fix
    - Backend value had to be mapped to the payload so capabilities call
    can be triggered. Based on the response from capabilities, options are
    rendered in the more menu dropdown.

--- ui/app/serializers/keymgmt/provider.js
+++ ui/app/serializers/keymgmt/provider.js
@@ -3,6 +3,18 @@ import ApplicationSerializer from '../application';
 export default class KeymgmtProviderSerializer extends ApplicationSerializer {
   primaryKey = 'name';
 
+  normalizeItems(payload) {
+    let normalized = super.normalizeItems(payload);
+    if (Array.isArray(normalized)) {
+      return normalized.map((key) => ({
+        id: key.name,
+        name: key.name,
+        backend: payload.backend,
+      }));
+    }
+    return normalized;
+  }
+
   serialize(snapshot) {
     const json = super.serialize(...arguments);
     return {
