fda4277806cc4ddf8d1c3520fd0b39701785f351
Stephen Wayne <swayne@hashicorp.com>
2022-06-15 11:11:07 -0600

    comment improvement

--- vault/expiration.go
+++ vault/expiration.go
@@ -1096,8 +1096,9 @@ func (m *ExpirationManager) RevokeByToken(ctx context.Context, te *logical.Token
 }
 
 // revoke all leases under `prefix`
-// if sync == true, revoke immediately. otherwise, mark the lease as expiring
-// `now` and let the expiration manager queue it for revocation.
+// if sync == true, revoke immediately (using a single worker).
+// otherwise, mark the lease as expiring  `now` and let the expiration manager
+// queue it for revocation.
 func (m *ExpirationManager) revokePrefixCommon(ctx context.Context, prefix string, force, sync bool) error {
 	if m.inRestoreMode() {
 		m.restoreRequestLock.Lock()
