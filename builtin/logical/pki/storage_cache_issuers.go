a7094f46727b8d049a61430f536fe3e80df35595
Alexander Scheel <alex.scheel@hashicorp.com>
2022-06-17 12:49:12 -0400

    Cache issuers' parsed cert bundles
    Decoding JSON entries and parsing PEM and ASN.1 take a lot of time for
    using an issuer. By caching this, we should remove a lot of redundant
    work.
    
    Signed-off-by: Alexander Scheel <alex.scheel@hashicorp.com>

--- builtin/logical/pki/storage_cache_issuers.go
+++ builtin/logical/pki/storage_cache_issuers.go
@@ -6,16 +6,18 @@ import (
 	"sync"
 
 	lru "github.com/hashicorp/golang-lru"
+	"github.com/hashicorp/vault/sdk/helper/certutil"
 	"github.com/hashicorp/vault/sdk/logical"
 )
 
 type issuerStorageCache struct {
-	lock      sync.RWMutex
-	invalid   bool
-	idSet     map[issuerID]bool
-	nameIDMap map[string]issuerID
-	entries   *lru.Cache
-	bundles   *lru.Cache
+	lock          sync.RWMutex
+	invalid       bool
+	idSet         map[issuerID]bool
+	nameIDMap     map[string]issuerID
+	entries       *lru.Cache
+	bundles       *lru.Cache
+	parsedBundles *lru.Cache
 }
 
 func InitIssuerStorageCache() *issuerStorageCache {
@@ -30,6 +32,11 @@ func InitIssuerStorageCache() *issuerStorageCache {
 	}
 
 	ret.bundles, err = lru.New(32)
+	if err != nil {
+		panic(err)
+	}
+
+	ret.parsedBundles, err = lru.New(32)
 	if err != nil {
 		panic(err)
 	}
@@ -64,6 +71,7 @@ func (c *issuerStorageCache) reloadOnInvalidation(ctx context.Context, s logical
 	// Clear the LRU; this is necessary as some entries/bundles might've been deleted.
 	c.entries.Purge()
 	c.bundles.Purge()
+	c.parsedBundles.Purge()
 
 	// List all issuers which exist.
 	strList, err := s.List(ctx, issuerPrefix)
@@ -193,7 +201,19 @@ func (c *issuerStorageCache) issuerWithName(ctx context.Context, s logical.Stora
 	return issuerId, nil
 }
 
-func (c *issuerStorageCache) fetchIssuerById(ctx context.Context, s logical.Storage, issuerId issuerID) (*issuerEntry, error) {
+func (c *issuerStorageCache) fetchIssuerInfoById(ctx context.Context, s logical.Storage, b *backend, issuerId issuerID) (*issuerEntry, *certutil.CertBundle, *certutil.ParsedCertBundle, error) {
+	// This method is more complex than the keys counterpart. Here, we handle
+	// the logic for all three types of calls:
+	//
+	// 1. Issuer entry only,
+	// 2. CertBundle as well,
+	// 3. Upgrading all the way to a ParsedCertBundle.
+	//
+	// When present, we use the keyStorageCache to give us a version of the
+	// bundle with the key pre-loaded and cache this, rather than potentially
+	// storing a half-built bundle. This allows us to remove the private key
+	// bits if they're not desired, rather than attempting to update the LRU
+	// cache entry with new key material.
 	needUnlock := true
 
 	c.lock.RLock()
@@ -209,7 +229,7 @@ func (c *issuerStorageCache) fetchIssuerById(ctx context.Context, s logical.Stor
 		needUnlock = false
 
 		if err := c.reloadOnInvalidation(ctx, s); err != nil {
-			return nil, err
+			return nil, nil, nil, err
 		}
 
 		// Now re-read-lock.
@@ -219,41 +239,120 @@ func (c *issuerStorageCache) fetchIssuerById(ctx context.Context, s logical.Stor
 
 	// Now we can service the request as expected.
 	if haveId, ok := c.idSet[issuerId]; !ok || !haveId {
-		return nil, fmt.Errorf("pki issuer id %v does not exist", issuerId)
+		return nil, nil, nil, fmt.Errorf("pki issuer id %v does not exist", issuerId)
 	}
 
-	if entry, ok := c.entries.Get(issuerId); ok && entry != nil {
+	entry, entryOk := c.entries.Get(issuerId)
+	if b == nil && entryOk && entry != nil {
+		// We can exit fast if we don't need the bundle. This only occurs when
+		// the keyStorageCache value is nil.
 		e := entry.(*issuerEntry)
-		return e, nil
+		return e, nil, nil, nil
 	}
 
-	// Otherwise, if it doesn't exist, fetch it and add it to the LRU. We
-	// once again have to upgrade our read lock to a write lock.
-	c.lock.RUnlock()
-	needUnlock = false
-	c.lock.Lock()
-	defer c.lock.Unlock()
+	bundle, bundleOk := c.bundles.Get(issuerId)
+	parsedBundle, parsedOk := c.parsedBundles.Get(issuerId)
+	if bundleOk && bundle != nil && parsedOk && parsedBundle != nil {
+		// If everything was loaded from cache, we're good.
+		e := entry.(*issuerEntry)
+		n := bundle.(*certutil.CertBundle)
+		p := parsedBundle.(*certutil.ParsedCertBundle)
+		return e, n, p, nil
+	}
 
-	rawEntry, err := s.Get(ctx, issuerPrefix+issuerId.String())
+	if !entryOk || entry == nil {
+		// Otherwise, if the entry doesn't exist, fetch it and add it to the
+		// LRU. We once again have to upgrade our read lock to a write lock.
+		c.lock.RUnlock()
+		needUnlock = false
+		c.lock.Lock()
+		defer c.lock.Unlock()
+
+		rawEntry, err := s.Get(ctx, issuerPrefix+issuerId.String())
+		if err != nil {
+			return nil, nil, nil, err
+		}
+		if rawEntry == nil {
+			return nil, nil, nil, fmt.Errorf("pki issuer id %s does not exist", issuerId)
+		}
+
+		var storedEntry issuerEntry
+		if err := rawEntry.DecodeJSON(&storedEntry); err != nil {
+			return nil, nil, nil, err
+		}
+
+		c.idSet[issuerId] = true
+		if len(storedEntry.Name) > 0 {
+			c.nameIDMap[storedEntry.Name] = issuerId
+		}
+
+		// Add this entry to the LRU.
+		c.entries.Add(issuerId, &storedEntry)
+		copiedEntry := storedEntry
+		entry = &copiedEntry
+	}
+
+	e := entry.(*issuerEntry)
+	// If we don't have a key storage cache, don't bother building the cert
+	// bundle entries.
+	if b == nil {
+		return e, nil, nil, nil
+	}
+
+	// Finally, we can finish building the cert bundle entries. Since we
+	// either:
+	//
+	// 1. Missed a key entry and thus needed to invalidate our LRU copies,
+	// 2. Missed either cert/parse copies,
+	//
+	// we always have to do this from scratch.
+	var rawBundle certutil.CertBundle
+	rawBundle.Certificate = e.Certificate
+	rawBundle.CAChain = e.CAChain
+	rawBundle.SerialNumber = e.SerialNumber
+	if e.KeyID != keyID("") {
+		keyEntry, err := b.keyCache.fetchKeyById(ctx, s, e.KeyID)
+		if err != nil {
+			return nil, nil, nil, err
+		}
+
+		rawBundle.PrivateKeyType = keyEntry.PrivateKeyType
+		rawBundle.PrivateKey = keyEntry.PrivateKey
+	}
+
+	c.bundles.Add(issuerId, &rawBundle)
+	copiedBundle := rawBundle
+	bundle = &copiedBundle
+	n := bundle.(*certutil.CertBundle)
+
+	rawParsedBundle, err := parseCABundle(ctx, b, n)
 	if err != nil {
-		return nil, err
-	}
-	if rawEntry == nil {
-		return nil, fmt.Errorf("pki issuer id %s does not exist", issuerId)
+		return nil, nil, nil, err
 	}
 
-	var entry issuerEntry
-	if err := rawEntry.DecodeJSON(&entry); err != nil {
-		return nil, err
+	c.parsedBundles.Add(issuerId, rawParsedBundle)
+	copiedParsedBundle := *rawParsedBundle
+	parsedBundle = &copiedParsedBundle
+
+	p := parsedBundle.(*certutil.ParsedCertBundle)
+	return e, n, p, nil
+}
+
+func (c *issuerStorageCache) fetchIssuerById(ctx context.Context, s logical.Storage, issuerId issuerID) (*issuerEntry, error) {
+	entry, _, _, err := c.fetchIssuerInfoById(ctx, s, nil, issuerId)
+	return entry, err
+}
+
+func (c *issuerStorageCache) fetchCertBundleByIssuerId(ctx context.Context, s logical.Storage, b *backend, issuerId issuerID, loadKey bool) (*issuerEntry, *certutil.CertBundle, error) {
+	entry, bundle, _, err := c.fetchIssuerInfoById(ctx, s, b, issuerId)
+
+	if err != nil && !loadKey {
+		bundle.PrivateKey = ""
 	}
 
-	c.idSet[issuerId] = true
-	if len(entry.Name) > 0 {
-		c.nameIDMap[entry.Name] = issuerId
-	}
+	return entry, bundle, err
+}
 
-	// Add this entry to the LRU.
-	c.entries.Add(issuerId, &entry)
-	copiedEntry := entry
-	return &copiedEntry, nil
+func (c *issuerStorageCache) fetchParsedBundleByIssuerId(ctx context.Context, s logical.Storage, b *backend, issuerId issuerID, loadKey bool) (*issuerEntry, *certutil.CertBundle, *certutil.ParsedCertBundle, error) {
+	return c.fetchIssuerInfoById(ctx, s, b, issuerId)
 }
