ce6c1b9b0cf712bd04e5b37e16bf7fba511c9f62
clairebontempo@gmail.com <cbontempo@hashicorp.com>
2022-06-21 11:34:01 -0700

    add ClientsCreate route

--- ui/app/routes/vault/cluster/access/oidc/clients/create.js
+++ ui/app/routes/vault/cluster/access/oidc/clients/create.js
@@ -1,3 +1,7 @@
 import Route from '@ember/routing/route';
 
-export default class OidcClientsCreateRoute extends Route {}
+export default class OidcClientsCreateRoute extends Route {
+  model() {
+    return this.store.createRecord('oidc/client');
+  }
+}
