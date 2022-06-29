285be7a98afe4be9156e3fac03d2027ceff34483
Jordan Reimer <zofskeez@gmail.com>
2022-06-29 14:58:38 -0600

    adds oidc scopes list

--- ui/app/routes/vault/cluster/access/oidc/scopes/index.js
+++ ui/app/routes/vault/cluster/access/oidc/scopes/index.js
@@ -1,3 +1,13 @@
 import Route from '@ember/routing/route';
 
-export default class OidcScopesRoute extends Route {}
+export default class OidcScopesRoute extends Route {
+  model() {
+    return this.store.query('oidc/scope', {}).catch((err) => {
+      if (err.httpStatus === 404) {
+        return [];
+      } else {
+        throw err;
+      }
+    });
+  }
+}
