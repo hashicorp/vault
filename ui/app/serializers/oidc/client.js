8f55b090f27ba9bb52a2e4dbea2a70bb93a9d9e6
clairebontempo@gmail.com <cbontempo@hashicorp.com>
2022-07-05 09:39:39 -0700

    fix link to in edit form

--- ui/app/serializers/oidc/client.js
+++ ui/app/serializers/oidc/client.js
@@ -2,11 +2,4 @@ import ApplicationSerializer from '../application';
 
 export default class OidcClientSerializer extends ApplicationSerializer {
   primaryKey = 'name';
-
-  normalizeResponse(store, primaryModelClass, payload, id, requestType) {
-    let transformedPayload = {
-      data: { ...payload.data, key: [payload.data.key] },
-    };
-    return super.normalizeResponse(store, primaryModelClass, transformedPayload, id, requestType);
-  }
 }
