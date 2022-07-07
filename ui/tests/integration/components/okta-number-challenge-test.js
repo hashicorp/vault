37ae2b4f4246f46445ad3fa0fcd9b8e21ec3d100
linda9379 <linda.jiang@hashicorp.com>
2022-07-07 15:53:21 -0400

    Created error screen tests and made minor fixes

--- ui/tests/integration/components/okta-number-challenge-test.js
+++ ui/tests/integration/components/okta-number-challenge-test.js
@@ -1,6 +1,6 @@
 import { module, test } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
-import { render } from '@ember/test-helpers';
+import { render, click } from '@ember/test-helpers';
 import { hbs } from 'ember-cli-htmlbars';
 
 module('Integration | Component | okta-number-challenge', function (hooks) {
@@ -52,7 +52,7 @@ module('Integration | Component | okta-number-challenge', function (hooks) {
   test('it should show error screen', async function (assert) {
     this.set('hasError', true);
     await render(
-      hbs`<OktaNumberChallenge @correctAnswer={{this.oktaNumberChallengeAnswer}} @hasError={{this.hasError}}/>`
+      hbs`<OktaNumberChallenge @correctAnswer={{this.oktaNumberChallengeAnswer}} @hasError={{this.hasError}} @onReturnToLogin={{fn (mut this.returnToLogin) true}}/>`
     );
     assert
       .dom('[data-test-okta-number-challenge-description]')
@@ -62,6 +62,8 @@ module('Integration | Component | okta-number-challenge', function (hooks) {
       );
     assert
       .dom('[data-test-error]')
-      .includesText('There was a problem', 'Shows error that there was a problem');
+      .includesText('There was a problem', 'Displays error that there was a problem');
+    await click('[data-test-return-from-okta-number-challenge]');
+    assert.true(this.returnToLogin, 'onReturnToLogin was triggered');
   });
 });
