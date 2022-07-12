c31d2d028c42f5dfe756c9650eeea76c29819480
linda9379 <linda.jiang@hashicorp.com>
2022-07-12 14:21:50 -0400

    Added periods at the end of param descriptions

--- ui/app/components/okta-number-challenge.js
+++ ui/app/components/okta-number-challenge.js
@@ -8,9 +8,9 @@ import Component from '@glimmer/component';
  * ```js
  * <OktaNumberChallenge @correctAnswer={this.oktaNumberChallengeAnswer} @hasError={this.error} @onReturnToLogin={this.returnToLoginFromOktaNumberChallenge}/>
  * ```
- * @param {number} correctAnswer - The correct answer to click for the okta number challenge
- * @param {boolean} hasError - Determines if there is an error being thrown
- * @param {function} onReturnToLogin - Sets waitingForOktaNumberChallenge to false if want to return to main login
+ * @param {number} correctAnswer - The correct answer to click for the okta number challenge.
+ * @param {boolean} hasError - Determines if there is an error being thrown.
+ * @param {function} onReturnToLogin - Sets waitingForOktaNumberChallenge to false if want to return to main login.
  */
 
 export default class OktaNumberChallenge extends Component {
