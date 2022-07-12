ec43431068f659d5276f2f38618ce1c0ddb1a7ca
linda9379 <57650314+linda9379@users.noreply.github.com>
2022-07-12 16:01:42 -0400

    Implement error handling and screens for okta number challenge (#16276)
    * Implemented loading screen and display screen for correct answer for Okta Number Challenge
    
    * Fixed linting issues on hbs files
    
    * Temporary changes to include error screen in okta number challenge
    
    * Created error screen tests and made minor fixes
    
    * Fixed error for wrong parameter name being passed in
    
    * Fixed linting issues causing ui tests to fail
    
    * Added periods at the end of param descriptions

--- ui/tests/integration/components/okta-number-challenge-test.js
+++ ui/tests/integration/components/okta-number-challenge-test.js
@@ -1,6 +1,6 @@
 import { module, test } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
-import { render } from '@ember/test-helpers';
+import { render, click } from '@ember/test-helpers';
 import { hbs } from 'ember-cli-htmlbars';
 
 module('Integration | Component | okta-number-challenge', function (hooks) {
@@ -8,6 +8,7 @@ module('Integration | Component | okta-number-challenge', function (hooks) {
 
   hooks.beforeEach(function () {
     this.oktaNumberChallengeAnswer = null;
+    this.hasError = false;
   });
 
   test('it should render correct descriptions', async function (assert) {
@@ -46,5 +47,23 @@ module('Integration | Component | okta-number-challenge', function (hooks) {
     assert
       .dom('[data-test-okta-number-challenge-answer]')
       .includesText('1', 'Correct okta number challenge answer renders');
+  });
+
+  test('it should show error screen', async function (assert) {
+    this.set('hasError', true);
+    await render(
+      hbs`<OktaNumberChallenge @correctAnswer={{this.oktaNumberChallengeAnswer}} @hasError={{this.hasError}} @onReturnToLogin={{fn (mut this.returnToLogin) true}}/>`
+    );
+    assert
+      .dom('[data-test-okta-number-challenge-description]')
+      .includesText(
+        'To finish signing in, you will need to complete an additional MFA step.',
+        'Correct description renders'
+      );
+    assert
+      .dom('[data-test-error]')
+      .includesText('There was a problem', 'Displays error that there was a problem');
+    await click('[data-test-return-from-okta-number-challenge]');
+    assert.true(this.returnToLogin, 'onReturnToLogin was triggered');
   });
 });
