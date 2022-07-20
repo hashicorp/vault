58b88b8aca27e77a7a3897c3e2ba8d302592356b
John-Michael Faircloth <fairclothjm@users.noreply.github.com>
2022-07-20 10:36:23 -0500

    Revert "AutoMTLS for secrets/auth plugins (#15671)" (#16377)
    This reverts commit 39bcd5c71529f5f4eb61aae68b17d06d192ea55f.

--- sdk/plugin/plugin.go
+++ sdk/plugin/plugin.go
@@ -7,6 +7,7 @@ import (
 	"sync"
 
 	"github.com/hashicorp/errwrap"
+	log "github.com/hashicorp/go-hclog"
 	plugin "github.com/hashicorp/go-plugin"
 	"github.com/hashicorp/vault/sdk/helper/consts"
 	"github.com/hashicorp/vault/sdk/helper/pluginutil"
@@ -34,7 +35,7 @@ func (b *BackendPluginClient) Cleanup(ctx context.Context) {
 // external plugins, or a concrete implementation of the backend if it is a builtin backend.
 // The backend is returned as a logical.Backend interface. The isMetadataMode param determines whether
 // the plugin should run in metadata mode.
-func NewBackend(ctx context.Context, pluginName string, pluginType consts.PluginType, sys pluginutil.LookRunnerUtil, conf *logical.BackendConfig, isMetadataMode bool, autoMTLS bool) (logical.Backend, error) {
+func NewBackend(ctx context.Context, pluginName string, pluginType consts.PluginType, sys pluginutil.LookRunnerUtil, conf *logical.BackendConfig, isMetadataMode bool) (logical.Backend, error) {
 	// Look for plugin in the plugin catalog
 	pluginRunner, err := sys.LookupPlugin(ctx, pluginName, pluginType)
 	if err != nil {
@@ -58,16 +59,8 @@ func NewBackend(ctx context.Context, pluginName string, pluginType consts.Plugin
 			}
 		}
 	} else {
-		config := pluginutil.PluginClientConfig{
-			Name:           pluginName,
-			PluginType:     pluginType,
-			Logger:         conf.Logger.Named(pluginName),
-			IsMetadataMode: isMetadataMode,
-			AutoMTLS:       autoMTLS,
-			Wrapper:        sys,
-		}
 		// create a backendPluginClient instance
-		backend, err = NewPluginClient(ctx, pluginRunner, config)
+		backend, err = NewPluginClient(ctx, sys, pluginRunner, conf.Logger, isMetadataMode)
 		if err != nil {
 			return nil, err
 		}
@@ -76,49 +69,34 @@ func NewBackend(ctx context.Context, pluginName string, pluginType consts.Plugin
 	return backend, nil
 }
 
-// pluginSet returns the go-plugin PluginSet that we can dispense. This ensures
-// that plugins that don't support AutoMTLS are run on the appropriate version.
-func pluginSet(autoMTLS, metadataMode bool) map[int]plugin.PluginSet {
-	if autoMTLS {
-		return map[int]plugin.PluginSet{
-			5: {
-				"backend": &GRPCBackendPlugin{
-					MetadataMode:      false,
-					AutoMTLSSupported: true,
-				},
-			},
-		}
-	}
-	return map[int]plugin.PluginSet{
+func NewPluginClient(ctx context.Context, sys pluginutil.RunnerUtil, pluginRunner *pluginutil.PluginRunner, logger log.Logger, isMetadataMode bool) (logical.Backend, error) {
+	// pluginMap is the map of plugins we can dispense.
+	pluginSet := map[int]plugin.PluginSet{
 		// Version 3 used to supports both protocols. We want to keep it around
 		// since it's possible old plugins built against this version will still
 		// work with gRPC. There is currently no difference between version 3
 		// and version 4.
 		3: {
 			"backend": &GRPCBackendPlugin{
-				MetadataMode: metadataMode,
+				MetadataMode: isMetadataMode,
 			},
 		},
 		4: {
 			"backend": &GRPCBackendPlugin{
-				MetadataMode: metadataMode,
+				MetadataMode: isMetadataMode,
 			},
 		},
 	}
-}
 
-func NewPluginClient(ctx context.Context, pluginRunner *pluginutil.PluginRunner, config pluginutil.PluginClientConfig) (logical.Backend, error) {
-	ps := pluginSet(config.AutoMTLS, config.IsMetadataMode)
+	namedLogger := logger.Named(pluginRunner.Name)
 
-	client, err := pluginRunner.RunConfig(ctx,
-		pluginutil.Runner(config.Wrapper),
-		pluginutil.PluginSets(ps),
-		pluginutil.HandshakeConfig(handshakeConfig),
-		pluginutil.Env(),
-		pluginutil.Logger(config.Logger),
-		pluginutil.MetadataMode(config.IsMetadataMode),
-		pluginutil.AutoMTLS(config.AutoMTLS),
-	)
+	var client *plugin.Client
+	var err error
+	if isMetadataMode {
+		client, err = pluginRunner.RunMetadataMode(ctx, sys, pluginSet, handshakeConfig, []string{}, namedLogger)
+	} else {
+		client, err = pluginRunner.Run(ctx, sys, pluginSet, handshakeConfig, []string{}, namedLogger)
+	}
 	if err != nil {
 		return nil, err
 	}
@@ -148,9 +126,9 @@ func NewPluginClient(ctx context.Context, pluginRunner *pluginutil.PluginRunner,
 	}
 
 	// Wrap the backend in a tracing middleware
-	if config.Logger.IsTrace() {
+	if namedLogger.IsTrace() {
 		backend = &backendTracingMiddleware{
-			logger: config.Logger.With("transport", transport),
+			logger: namedLogger.With("transport", transport),
 			next:   backend,
 		}
 	}
