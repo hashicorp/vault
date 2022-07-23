a918c379fffa0c22428b479ffaff810e184924af
clairebontempo@gmail.com <cbontempo@hashicorp.com>
2022-07-22 17:16:55 -0700

    component cleanup

--- ui/lib/core/addon/components/search-select-with-modal.js
+++ ui/lib/core/addon/components/search-select-with-modal.js
@@ -7,30 +7,38 @@ import { resolve } from 'rsvp';
 import { filterOptions, defaultMatcher } from 'ember-power-select/utils/group-utils';
 
 /**
- * @module SearchSelect
- * The `SearchSelect` is an implementation of the [ember-power-select](https://github.com/cibernox/ember-power-select) used for form elements where options come dynamically from the API.
+ * @module SearchSelectWithModal
+ * The `SearchSelectWithModal` is an implementation of the [ember-power-select](https://github.com/cibernox/ember-power-select) used for form elements where options come dynamically from the API. It can only accept a single model.
+ * It renders a passed in form component so records can be created inline, via a modal that pops up after clicking "Add new <record>" from the dropdown menu
  * @example
- * <SearchSelect @id="group-policies" @models={{["policies/acl"]}} @onChange={{onChange}} @selectLimit={{2}} @inputValue={{get model valuePath}} @helpText="Policies associated with this group" @label="Policies" @fallbackComponent="string-list" />
+ * <SearchSelectWithModal
+ *         @id="assignments"
+ *         @label="assignment"
+ *         @labelClass="is-label"
+ *         @subText="Search for an existing assignment, or type a new name to create it."
+ *         @model="oidc/assignment"
+ *         @inputValue={{map-by "id" @model.assignments}}
+ *         @onChange={{this.handleSearchSelect}}
+ *         {{! since this is the "limited" radio select option we do not want to include 'allow_all' }}
+ *         @removeOptions={{array "allow_all"}}
+ *         @fallbackComponent="string-list"
+ *         @modalFormComponent="oidc/assignment-form"
+ *         @modalSubtext="Use assignment to specify which Vault entities and groups are allowed to authenticate."
+ *       />
  *
  * @param {string} id - The name of the form field
- * @param {Array} model - model type to fetch from API
+ * @param {Array} model - model type to fetch from API (can only be a single model)
  * @param {function} onChange - The onchange action for this form field. ** SEE UTIL ** search-select-has-many.js if selecting models from a hasMany relationship
  * @param {string | Array} inputValue -  A comma-separated string or an array of strings -- array of ids for models.
  * @param {string} label - Label for this form field
- * @param {string} fallbackComponent - name of component to be rendered if the API call 403s
- * @param {boolean} [disallowNewItems=false] - Controls whether or not the user can add a new item if none found
+ * @param {string} fallbackComponent - name of component to be rendered if the API returns a 403s
  * @param {boolean} [passObject=false] - When true, the onChange callback returns an array of objects with id (string) and isNew (boolean)
  * @param {string} [helpText] - Text to be displayed in the info tooltip for this form field
  * @param {number} [selectLimit] - A number that sets the limit to how many select options they can choose
  * @param {string} [subText] - Text to be displayed below the label
  * @param {string} [subLabel] - a smaller label below the main Label
- * @param {string} [placeholder] - text you wish to replace the default "search" with
- * @param {boolean} [displayInherit] - if you need the search select component to display inherit instead of box.
- * @param {array} [removeOptions] - array of strings containing option names or model ids to filter from the dropdown (ex: 'allow_all)
- *
- * @param {Array} options - *Advanced usage* - `options` can be passed directly from the outside to the
- * power-select component. If doing this, `models` should not also be passed as that will overwrite the
- * passed value. ex: [{ name: 'namespace45', id: 'displayedName' }];
+ * @param {string} [placeholder] - placeholder text to override the default text of "Search"
+ * @param {array} [removeOptions] - array of strings containing option names or model ids to filter from the dropdown (ex: ['allow_all'])
  * @param {function} search - *Advanced usage* - Customizes how the power-select component searches for matches -
  * see the power-select docs for more information.
  *
@@ -38,9 +46,8 @@ import { filterOptions, defaultMatcher } from 'ember-power-select/utils/group-ut
 export default class SearchSelectWithModal extends Component {
   @service store;
 
-  @tracked allOptions = null; // list of options including matched
   @tracked selectedOptions = null; // list of selected options
-  @tracked options = null; // all possible options
+  @tracked allOptions = null; // all possible options
   @tracked showModal = false;
   @tracked newModelRecord = null;
 
@@ -61,11 +68,6 @@ export default class SearchSelectWithModal extends Component {
     return this.args.shouldUseFallback || false;
   }
 
-  get disallowNewItems() {
-    return this.args.disallowNewItems || false;
-  }
-
-  // array of strings passed as an arg to remove from dropdown
   get removeOptions() {
     return this.args.removeOptions || null;
   }
@@ -76,33 +78,18 @@ export default class SearchSelectWithModal extends Component {
 
   @action
   async fetchOptions() {
-    let { oldOptions, options, selectedOptions } = this;
-    let hasFormattedInput = typeof selectedOptions.firstObject !== 'string';
-    if (options && !oldOptions && !hasFormattedInput) {
-      // this is the first time they've been set, so we need to format them
-      this.formatOptions(options);
-    }
-    this.oldOptions = options;
-    if (!this.args.model) {
-      if (this.options) {
-        this.formatOptions(this.options);
-      }
-      return;
-    }
-
     try {
       let queryOptions = {};
       let options = await this.store.query(this.args.model, queryOptions);
-      this.formatOptions(options);
+      this.allOptions = this.formatOptions(options);
     } catch (err) {
       if (err.httpStatus === 404) {
-        if (!this.options) {
+        if (!this.allOptions) {
           // If the call failed but the resource has items
           // from a different namespace, this allows the
           // selected items to display
-          this.options = [];
+          this.allOptions = [];
         }
-
         return;
       }
       if (err.httpStatus === 403) {
@@ -114,7 +101,7 @@ export default class SearchSelectWithModal extends Component {
   }
   formatOptions(options) {
     options = options.toArray();
-    if (this.removeOptions && this.removeOptions.length > 0) {
+    if (this.removeOptions) {
       options = options.filter((o) => !this.removeOptions.includes(o.id));
     }
     options = options.map((option) => {
@@ -133,13 +120,7 @@ export default class SearchSelectWithModal extends Component {
         };
       });
     }
-    if (this.args.options) {
-      if (this.removeOptions && this.removeOptions.length > 0) {
-        options = this.options.filter((o) => !this.removeOptions.includes(o.id));
-      }
-      options = this.options.concat(options).uniq();
-    }
-    this.options = options;
+    return options;
   }
 
   handleChange() {
@@ -159,16 +140,14 @@ export default class SearchSelectWithModal extends Component {
     if (options && options.length && options.firstObject.groupName) {
       return !options.some((group) => group.options.findBy('id', id));
     }
-    let existingOption = this.options && (this.options.findBy('id', id) || this.options.findBy('name', id));
-    if (this.disallowNewItems && !existingOption) {
-      return false;
-    }
+    let existingOption =
+      this.allOptions && (this.allOptions.findBy('id', id) || this.allOptions.findBy('name', id));
     return !existingOption;
   }
   //----- adapted from ember-power-select-with-create
   addCreateOption(term, results) {
     if (this.shouldShowCreate(term, results)) {
-      const name = `Add new ${singularize(this.args.label)}: ${term}`;
+      const name = `Create new ${singularize(this.args.label)}: ${term}`;
       const suggestion = {
         __isSuggestion__: true,
         __value__: term,
@@ -187,17 +166,14 @@ export default class SearchSelectWithModal extends Component {
   @action
   discardSelection(selected) {
     this.selectedOptions.removeObject(selected);
-    // fire off getSelectedValue action higher up in get-credentials-card component
-    if (!selected.new) {
-      this.options.pushObject(selected);
-    }
+    this.allOptions.pushObject(selected);
     this.handleChange();
   }
   // ----- adapted from ember-power-select-with-create
   @action
   searchAndSuggest(term, select) {
     if (term.length === 0) {
-      return this.options;
+      return this.allOptions;
     }
     if (this.search) {
       return resolve(this.search(term, select)).then((results) => {
@@ -208,12 +184,15 @@ export default class SearchSelectWithModal extends Component {
         return results;
       });
     }
-    const newOptions = this.filter(this.options, term);
+    const newOptions = this.filter(this.allOptions, term);
     this.addCreateOption(term, newOptions);
     return newOptions;
   }
   @action
   async selectOrCreate(selection) {
+    // if creating we call handleChange in the resetModal action to ensure the model is valid and successfully created
+    // before adding it to the DOM (and parent model)
+    // if just selecting, then we handleChange immediately
     if (selection && selection.__isSuggestion__) {
       const name = selection.__value__;
       this.showModal = true;
@@ -222,9 +201,9 @@ export default class SearchSelectWithModal extends Component {
       this.newModelRecord = createRecord;
     } else {
       this.selectedOptions.pushObject(selection);
-      this.options.removeObject(selection);
+      this.allOptions.removeObject(selection);
+      this.handleChange();
     }
-    this.handleChange();
   }
   // -----
 
@@ -234,6 +213,7 @@ export default class SearchSelectWithModal extends Component {
     if (model && model.currentState.isSaved) {
       const { name } = model;
       this.selectedOptions.pushObject({ name, id: name });
+      this.handleChange();
     }
     this.newModelRecord = null;
   }
