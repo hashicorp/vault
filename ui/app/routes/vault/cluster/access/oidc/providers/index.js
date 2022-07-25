546918f90c7ab469095ea0e9556c96efd7560c3b
linda9379 <linda.jiang@hashicorp.com>
2022-07-25 15:59:42 -0400

    Fixed linting issues causing ui tests to fail

--- ui/app/routes/vault/cluster/access/oidc/providers/index.js
+++ ui/app/routes/vault/cluster/access/oidc/providers/index.js
@@ -1,13 +1,13 @@
 import Route from '@ember/routing/route';
 
 export default class OidcProvidersRoute extends Route {
-    model() {
-      return this.store.query('oidc/provider', {}).catch((err) => {
-        if (err.httpStatus === 404) {
-          return [];
-        } else {
-          throw err;
-        }
-      });
-    }
+  model() {
+    return this.store.query('oidc/provider', {}).catch((err) => {
+      if (err.httpStatus === 404) {
+        return [];
+      } else {
+        throw err;
+      }
+    });
   }
+}
