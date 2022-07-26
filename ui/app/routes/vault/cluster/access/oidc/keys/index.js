e563569869dc51706d98a1eb8abb8246de59ab0b
linda9379 <linda.jiang@hashicorp.com>
2022-07-26 12:55:08 -0400

    Added list view for keys

--- ui/app/routes/vault/cluster/access/oidc/keys/index.js
+++ ui/app/routes/vault/cluster/access/oidc/keys/index.js
@@ -1,3 +1,13 @@
 import Route from '@ember/routing/route';
 
-export default class OidcKeysRoute extends Route {}
+export default class OidcKeysRoute extends Route {
+  model() {
+    return this.store.query('oidc/key', {}).catch((err) => {
+        if (err.httpStatus === 404) {
+        return [];
+        } else {
+        throw err;
+        }
+    });
+  }
+}
