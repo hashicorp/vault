895a16c55118e3f609a6fc7d4779222b576ea223
clairebontempo@gmail.com <cbontempo@hashicorp.com>
2022-07-26 17:38:46 -0700

    finish tests

--- ui/tests/integration/components/search-select-with-modal-test.js
+++ ui/tests/integration/components/search-select-with-modal-test.js
@@ -1,92 +1,133 @@
 import { module, test } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
 import { create } from 'ember-cli-page-object';
-// import { typeInSearch, clickTrigger } from 'ember-power-select/test-support/helpers';
-import Service from '@ember/service';
+import { setupMirage } from 'ember-cli-mirage/test-support';
+import { clickTrigger, typeInSearch } from 'ember-power-select/test-support/helpers';
 import { render } from '@ember/test-helpers';
-import { run } from '@ember/runloop';
 import hbs from 'htmlbars-inline-precompile';
-import sinon from 'sinon';
-// import waitForError from 'vault/tests/helpers/wait-for-error';
+import ss from 'vault/tests/pages/components/search-select';
 
-import searchSelect from '../../pages/components/search-select';
-
-const component = create(searchSelect);
-
-const storeService = Service.extend({
-  query(modelType) {
-    return new Promise((resolve, reject) => {
-      switch (modelType) {
-        case 'policy/acl':
-          resolve([
-            { id: '1', name: '1' },
-            { id: '2', name: '2' },
-            { id: '3', name: '3' },
-          ]);
-          break;
-        case 'policy/rgp':
-          reject({ httpStatus: 403, message: 'permission denied' });
-          break;
-        case 'identity/entity':
-          resolve([
-            { id: '7', name: 'seven' },
-            { id: '8', name: 'eight' },
-            { id: '9', name: 'nine' },
-          ]);
-          break;
-        case 'server/error':
-          var error = new Error('internal server error');
-          error.httpStatus = 500;
-          reject(error);
-          break;
-        case 'transform/transformation':
-          resolve([
-            { id: 'foo', name: 'bar' },
-            { id: 'foobar', name: '' },
-            { id: 'barfoo1', name: 'different' },
-          ]);
-          break;
-        default:
-          reject({ httpStatus: 404, message: 'not found' });
-          break;
-      }
-      reject({ httpStatus: 404, message: 'not found' });
-    });
-  },
-});
+const component = create(ss);
 
 module('Integration | Component | search select with modal', function (hooks) {
   setupRenderingTest(hooks);
-
+  setupMirage(hooks);
   hooks.beforeEach(function () {
-    run(() => {
-      this.owner.unregister('service:store');
-      this.owner.register('service:store', storeService);
+    this.store = this.owner.lookup('service:store');
+    this.server.get('identity/entity/id', () => {
+      return {
+        request_id: 'entity-list-id',
+        data: {
+          key_info: {
+            'entity-1-id': {
+              name: 'entity-1',
+            },
+            'entity-2-id': {
+              name: 'entity-2',
+            },
+          },
+          keys: ['entity-1-id', 'entity-2-id'],
+        },
+      };
+    });
+    this.server.get('sys/policies/acl', () => {
+      return {
+        request_id: 'acl-policy-list-id',
+        data: {
+          keys: ['default', 'root'],
+        },
+      };
+    });
+    this.server.get('sys/policies/rgp', () => {
+      return {
+        request_id: 'rgp-policy-list-id',
+        data: {
+          keys: ['default', 'root'],
+        },
+      };
+    });
+    this.server.get('/identity/entity/id/entity-1-id', () => {
+      return {
+        request_id: 'some-entity-id-1',
+        data: {
+          id: 'entity-1-id',
+          name: 'entity-1',
+          namespace_id: 'root',
+          policies: ['default'],
+        },
+      };
+    });
+    this.server.get('/identity/entity/id/entity-2-id', () => {
+      return {
+        request_id: 'some-entity-id-2',
+        data: {
+          id: 'entity-2-id',
+          name: 'entity-2',
+          namespace_id: 'root',
+          policies: ['default'],
+        },
+      };
     });
   });
 
-  test('it renders', async function (assert) {
-    const models = ['policy/acl'];
-    this.set('models', models);
-    this.set('onChange', sinon.spy());
+  test('it renders passed in model', async function (assert) {
     await render(hbs`
     <SearchSelectWithModal
-      @id="assignments"
-      @label="assignment"
-      @subText="Search for an existing assignment, or type a new name to create it."
-      @model="oidc/assignment"
-      @inputValue={{map-by "id" @model.assignments}}
-      @onChange={{this.handleAssignmentSelection}}
-      @excludeOptions={{array "allow_all"}}
+      @id="entity"
+      @label="entity"
+      @subText="Search for an existing entity, or type a new name to create it."
+      @model="identity/entity"
+      @onChange={{this.onChange}}
       @fallbackComponent="string-list"
-      @modalFormComponent="oidc/assignment-form"
-      @modalSubtext="Use assignment to specify which Vault entities and groups are allowed to authenticate."
+      @modalFormComponent="identity/edit-form"
+      @modalSubtext="Some modal subtext"
       />
-`);
+      <div id="modal-wormhole"></div>
+  `);
 
-    assert.ok(component.hasLabel, 'it renders the label');
-    assert.equal(component.labelText, 'foo', 'the label text is correct');
+    assert.dom('[data-test-search-select-with-modal]').exists('the component renders');
+    assert.equal(component.labelText, 'Entity name', 'label text is correct');
     assert.ok(component.hasTrigger, 'it renders the power select trigger');
     assert.equal(component.selectedOptions.length, 0, 'there are no selected options');
+
+    await clickTrigger();
+    assert.equal(component.options.length, 2, 'dropdown renders passed in models as options');
+  });
+  test('it filters options and adds option to create new item', async function (assert) {
+    assert.expect(7);
+    await render(hbs`
+    <SearchSelectWithModal
+      @id="entity"
+      @label="entity"
+      @subText="Search for an existing entity, or type a new name to create it."
+      @model="identity/entity"
+      @onChange={{this.onChange}}
+      @fallbackComponent="string-list"
+      @modalFormComponent="identity/edit-form"
+      @modalSubtext="Some modal subtext"
+      />
+      <div id="modal-wormhole"></div>
+  `);
+
+    await clickTrigger();
+    assert.equal(component.options.length, 2, 'dropdown renders all options');
+
+    await typeInSearch('e');
+    assert.equal(component.options.length, 3, 'dropdown renders all options plus add option');
+
+    await typeInSearch('entity-1');
+    assert.equal(component.options[0].text, 'entity-1-id', 'dropdown renders only matching option');
+
+    await typeInSearch('entity-1-new');
+    assert.equal(
+      component.options[0].text,
+      'Create new entity: entity-1-new',
+      'dropdown gives option to create new option'
+    );
+
+    await component.selectOption();
+    assert.dom('[data-test-modal-div]').hasAttribute('class', 'modal is-info is-active', 'modal is active');
+    assert.dom('[data-test-modal-subtext]').hasText('Some modal subtext', 'renders modal text');
+    assert.dom('[data-test-component="identity-edit-form"]').exists('renders identity form');
   });
 });
