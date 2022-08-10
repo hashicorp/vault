2780fbf078a4fa05354236f36d42173b25a15a6a
Angel Garbarino <argarbarino@gmail.com>
2022-08-10 10:05:43 -0600

    cleanup

--- ui/app/controllers/vault/cluster/access/oidc/assignments/assignment/details.js
+++ ui/app/controllers/vault/cluster/access/oidc/assignments/assignment/details.js
@@ -1,6 +1,7 @@
 import Controller from '@ember/controller';
 import { action } from '@ember/object';
 import { inject as service } from '@ember/service';
+
 export default class OidcAssignmentDetailsController extends Controller {
   @service router;
   @service flashMessages;
