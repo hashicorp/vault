792d5bf6683466ae24df4d20b9ed2899b6d87791
clairebontempo@gmail.com <clairebontempo@gmail.com>
2022-08-11 11:10:57 -0700

    query all client records and filter by allowed client id"

--- ui/app/routes/vault/cluster/access/oidc/providers/provider/clients.js
+++ ui/app/routes/vault/cluster/access/oidc/providers/provider/clients.js
@@ -2,18 +2,23 @@ import Route from '@ember/routing/route';
 
 export default class OidcProviderClientsRoute extends Route {
   async model() {
-    const model = this.modelFor('vault.cluster.access.oidc.providers.provider');
-    if (model.allowedClientIds.includes('*')) {
-      return this.store.query('oidc/client', {}).catch((err) => {
+    const providerModel = this.modelFor('vault.cluster.access.oidc.providers.provider');
+    const { allowedClientIds } = providerModel;
+    return await this.store
+      .query('oidc/client', {})
+      .then((clientRecords) => {
+        // return all clientRecords if glob is in array of IDs
+        if (allowedClientIds.includes('*')) {
+          return clientRecords;
+        }
+        return clientRecords.filter((client) => allowedClientIds.includes(client.id));
+      })
+      .catch((err) => {
         if (err.httpStatus === 404) {
           return [];
         } else {
           throw err;
         }
       });
-    }
-    return await model.allowedClientIds.map((client) => {
-      return this.store.findRecord('oidc/client', client);
-    });
   }
 }
