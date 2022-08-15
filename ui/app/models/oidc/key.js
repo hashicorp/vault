f10d3dc8aac206d752ddcccddc33417e14bc72d4
clairebontempo@gmail.com <clairebontempo@gmail.com>
2022-08-15 11:21:31 -0700

    add forms

--- ui/app/models/oidc/key.js
+++ ui/app/models/oidc/key.js
@@ -1,6 +1,19 @@
 import Model, { attr } from '@ember-data/model';
 import lazyCapabilities, { apiPath } from 'vault/macros/lazy-capabilities';
+import { expandAttributeMeta } from 'vault/utils/field-to-attrs';
+import { withModelValidations } from 'vault/decorators/model-validations';
 
+const validations = {
+  name: [
+    { type: 'presence', message: 'Name is required.' },
+    {
+      type: 'containsWhiteSpace',
+      message: 'Name cannot contain whitespace.',
+    },
+  ],
+};
+
+@withModelValidations(validations)
 export default class OidcKeyModel extends Model {
   @attr('string') name;
 
@@ -21,6 +34,20 @@ export default class OidcKeyModel extends Model {
   verificationTtl;
 
   @attr('array', { label: 'Allowed applications' }) allowedClientIds; // no editType because does not use form-field component
+
+  // TODO refactor when field-to-attrs is refactored as decorator
+  _attributeMeta = null; // cache initial result of expandAttributeMeta in getter and return
+  get formFields() {
+    if (!this._attributeMeta) {
+      this._attributeMeta = expandAttributeMeta(this, [
+        'name',
+        'algorithm',
+        'rotationPeriod',
+        'verificationTtl',
+      ]);
+    }
+    return this._attributeMeta;
+  }
 
   @lazyCapabilities(apiPath`identity/oidc/key/${'name'}`, 'name') keyPath;
   @lazyCapabilities(apiPath`identity/oidc/key`) keysPath;
