0c1a7706da2c0d5f2f52b24665b276cbe075fa4a
linda9379 <linda.jiang@hashicorp.com>
2022-08-16 13:00:38 -0400

    Added comment for serializer

--- ui/app/serializers/oidc/provider.js
+++ ui/app/serializers/oidc/provider.js
@@ -3,6 +3,7 @@ import ApplicationSerializer from '../application';
 export default class OidcProviderSerializer extends ApplicationSerializer {
   primaryKey = 'name';
 
+  // need to normalize to get issuer metadata for provider's list view
   normalizeItems(payload) {
     if (payload.data) {
       if (payload.data?.keys && Array.isArray(payload.data.keys)) {
