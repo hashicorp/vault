cb23923431a3ed2741f146c734af80b95b2d21b6
Bryce Kalow <bkalow@hashicorp.com>
2022-08-23 11:42:31 -0500

    test out developer preview

--- website/scripts/website-build.sh
+++ website/scripts/website-build.sh
@@ -6,6 +6,8 @@ PREVIEW_DIR=website-preview
 CLONE_DIR=website-preview
 # The product for which we are building the deploy preview
 PRODUCT=vault
+# Preview mode, controls the UI rendered (either the product site or developer). Can be `io` or `developer`
+PREVIEW_MODE=developer
 
 from_cache=false
 
@@ -17,7 +19,7 @@ fi
 
 # Clone the base project, if needed
 echo "⏳ Cloning the $REPO_TO_CLONE repo, this might take a while..."
-git clone --depth=1 "https://github.com/hashicorp/$REPO_TO_CLONE.git" "$CLONE_DIR"
+git clone --depth=1 --branch=brk.feat/developer-preview "https://github.com/hashicorp/$REPO_TO_CLONE.git" "$CLONE_DIR"
 
 if [ "$from_cache" = true ]; then
   echo "Setting up $PREVIEW_DIR"
@@ -28,4 +30,4 @@ fi
 cd "$PREVIEW_DIR"
 
 # Run the build:deploy-preview start script
-REPO=$PRODUCT DEV_IO=$PRODUCT IS_CONTENT_PREVIEW=true HASHI_ENV=project-preview npm run build:deploy-preview
+PREVIEW_MODE=$PREVIEW_MODE REPO=$PRODUCT HASHI_ENV=project-preview npm run build:deploy-preview
--- website/scripts/website-start.sh
+++ website/scripts/website-start.sh
@@ -4,13 +4,15 @@ REPO_TO_CLONE=dev-portal
 PREVIEW_DIR=website-preview
 # The product for which we are building the deploy preview
 PRODUCT=vault
+# Preview mode, controls the UI rendered (either the product site or developer). Can be `io` or `developer`
+PREVIEW_MODE=developer
 
 should_pull=true
 
 # Clone the dev-portal project, if needed
 if [ ! -d "$PREVIEW_DIR" ]; then
     echo "⏳ Cloning the $REPO_TO_CLONE repo, this might take a while..."
-    git clone --depth=1 https://github.com/hashicorp/$REPO_TO_CLONE.git "$PREVIEW_DIR"
+    git clone --depth=1 --branch=brk.feat/developer-preview https://github.com/hashicorp/$REPO_TO_CLONE.git "$PREVIEW_DIR"
     should_pull=false
 fi
 
@@ -18,8 +20,8 @@ cd "$PREVIEW_DIR"
 
 # If the directory already existed, pull to ensure the clone is fresh
 if [ "$should_pull" = true ]; then
-    git pull origin main
+    git pull origin brk.feat/developer-preview
 fi
 
 # Run the dev-portal content-repo start script
-REPO=$PRODUCT PREVIEW_DIR="$PREVIEW_DIR" npm run start:local-preview
+REPO=$PRODUCT PREVIEW_MODE=$PREVIEW_MODE npm run start:local-preview
