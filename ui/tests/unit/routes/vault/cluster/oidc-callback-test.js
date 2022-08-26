3ddcb461e9141a0ef653e19d907ede96c102178a
hashishaw <cshaw@hashicorp.com>
2022-08-26 10:34:06 -0500

    Test troubleshooting

--- ui/tests/unit/routes/vault/cluster/oidc-callback-test.js
+++ ui/tests/unit/routes/vault/cluster/oidc-callback-test.js
@@ -2,21 +2,37 @@ import { module, test } from 'qunit';
 import { setupTest } from 'ember-qunit';
 import sinon from 'sinon';
 
+// window.opener = {
+//   postMessage: () => {},
+//   origin: 'http://localhost:4200',
+// };
+const origin = 'http://localhost:4200';
+
 module('Unit | Route | vault/cluster/oidc-callback', function (hooks) {
   setupTest(hooks);
-  const parentNs = 'admin';
-  const childNs = 'admin/child-ns';
-  const path = 'oidc';
-  const customPath = 'oidc-dev';
-  const code = 'lTazRXEwKfyGKBUCo5TyLJzdIt39YniBJOXPABiRMkL0T';
-  const state = (ns) => {
-    ns ? 'st_91ji6vR2sQ2zBiZSQkqJ' + `,ns=${ns}` : 'st_91ji6vR2sQ2zBiZSQkqJ';
-  };
 
   hooks.beforeEach(function () {
+    this.originalOpener = window.opener;
+    window.opener = {
+      postMessage: () => {},
+      origin, // todo need?
+    };
     this.router = this.owner.lookup('service:router');
     this.route = this.owner.lookup('route:vault/cluster/oidc-callback');
     this.windowStub = sinon.stub(window.opener, 'postMessage');
+    this.parentNs = 'admin';
+    this.childNs = 'admin/child-ns';
+    this.path = 'oidc';
+    this.customPath = 'oidc-dev';
+    this.code = 'lTazRXEwKfyGKBUCo5TyLJzdIt39YniBJOXPABiRMkL0T';
+    this.state = (ns) => {
+      ns ? 'st_91ji6vR2sQ2zBiZSQkqJ' + `,ns=${ns}` : 'st_91ji6vR2sQ2zBiZSQkqJ';
+    };
+  });
+
+  hooks.afterEach(function () {
+    this.windowStub.restore();
+    window.opener = this.originalOpener;
   });
 
   test('it calls route', function (assert) {
@@ -25,24 +41,27 @@ module('Unit | Route | vault/cluster/oidc-callback', function (hooks) {
 
   test('it uses namespace param from state not namespaceQueryParam from cluster with default path', function (assert) {
     this.routeName = 'vault.cluster.oidc-callback';
-
     this.route.paramsFor = (path) => {
-      if (path === 'vault.cluster') return { namespaceQueryParam: parentNs };
+      if (path === 'vault.cluster') return { namespaceQueryParam: this.parentNs };
       return {
-        auth_path: path,
-        state: state(childNs),
-        code,
+        auth_path: this.path,
+        state: this.state(this.childNs),
+        code: this.code,
       };
     };
-    assert.ok(this.windowStub.calledWith, 'test');
+    this.route.afterModel();
+
+    assert.ok(this.windowStub.calledOnce, 'it is called');
     assert.propContains(
-      this.route.afterModel(),
+      this.windowStub.getCall(0).args[0],
       {
-        path,
-        namespace: childNs,
-        state: state(),
+        code: 'lTazRXEwKfyGKBUCo5TyLJzdIt39YniBJOXPABiRMkL0T',
+        namespace: 'admin',
+        path: 'oidc',
+        source: 'oidc-callback',
       },
-      'state and namespace queryParams are correct'
+
+      'calls correct'
     );
   });
 
@@ -51,17 +70,17 @@ module('Unit | Route | vault/cluster/oidc-callback', function (hooks) {
     this.route.paramsFor = (path) => {
       if (path === 'vault.cluster') return { namespaceQueryParam: parentNs };
       return {
-        auth_path: customPath,
-        state: state(childNs),
-        code,
+        auth_path: this.customPath,
+        state: this.state(this.childNs),
+        code: this.code,
       };
     };
     assert.propContains(
       this.route.afterModel(),
       {
-        path: customPath,
-        namespace: childNs,
-        state: state(),
+        path: this.customPath,
+        namespace: this.childNs,
+        state: this.state(),
       },
       'state ns takes precedence, state no longer has ns query'
     );
