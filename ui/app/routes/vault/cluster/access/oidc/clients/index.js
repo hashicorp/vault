ced57ef71503dba5a070d513a82c68598a09cb22
claire bontempo <68122737+hellobontempo@users.noreply.github.com>
2022-08-30 09:41:43 -0700

    UI: Add redirect when last client is deleted  (#16927)
    * afterModel redirect if no models exist
    
    * fix test
    
    * change space

--- ui/app/routes/vault/cluster/access/oidc/clients/index.js
+++ ui/app/routes/vault/cluster/access/oidc/clients/index.js
@@ -1,6 +1,8 @@
 import Route from '@ember/routing/route';
-
+import { inject as service } from '@ember/service';
 export default class OidcClientsRoute extends Route {
+  @service router;
+
   model() {
     return this.store.query('oidc/client', {}).catch((err) => {
       if (err.httpStatus === 404) {
@@ -9,5 +11,11 @@ export default class OidcClientsRoute extends Route {
         throw err;
       }
     });
+  }
+
+  afterModel(model) {
+    if (model.length === 0) {
+      this.router.transitionTo('vault.cluster.access.oidc');
+    }
   }
 }
