747fe983f675f934abe32bab524e1cf38b745857
maxcoulombe <max.coulombe@hashicorp.com>
2022-09-01 10:34:54 -0400

    * typo

--- sdk/helper/pluginutil/env.go
+++ sdk/helper/pluginutil/env.go
@@ -33,7 +33,7 @@ const (
 	PluginCACertPEMEnv = "VAULT_TESTING_PLUGIN_CA_PEM"
 
 	// PluginMultiplexingOptOut is a comma separated list of plugin names opted-out
-	// of the multiplexing feature; for emergencies if mplexing ever causes issues
+	// of the multiplexing feature; for emergencies if multiplexing ever causes issues
 	PluginMultiplexingOptOut = "VAULT_PLUGIN_MULTIPLEXING_OPT_OUT"
 )
 
