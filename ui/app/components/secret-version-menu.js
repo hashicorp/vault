ecc7b22ec5b00dbd0b009d64491fc57a54e02054
Jordan Reimer <zofskeez@gmail.com>
2022-09-01 15:02:39 -0600

    reverts button changes and closes dropdown in next tick of runloop

--- ui/app/components/secret-version-menu.js
+++ ui/app/components/secret-version-menu.js
@@ -1,6 +1,7 @@
 import Component from '@glimmer/component';
 import { action } from '@ember/object';
 import { inject as service } from '@ember/service';
+import { next } from '@ember/runloop';
 
 export default class SecretVersionMenu extends Component {
   @service router;
@@ -8,10 +9,9 @@ export default class SecretVersionMenu extends Component {
   onRefresh() {}
 
   @action
-  changeVersion(version, dropdown) {
-    dropdown.actions.close();
-    this.router.transitionTo({
-      queryParams: { version },
-    });
+  closeDropdown(dropdown) {
+    // strange issue where closing dropdown triggers full transition which redirects to auth screen in production builds
+    // closing dropdown in next tick of run loop fixes it
+    next(() => dropdown.actions.close());
   }
 }
--- ui/app/components/status-menu.js
+++ ui/app/components/status-menu.js
@@ -1,6 +1,7 @@
 import Component from '@glimmer/component';
 import { action } from '@ember/object';
 import { inject as service } from '@ember/service';
+import { next } from '@ember/runloop';
 
 /**
  * @module StatusMenu
@@ -32,8 +33,12 @@ export default class StatusMenu extends Component {
   }
 
   @action
-  onLinkClick(routeArgs) {
-    this.router.transitionTo(...routeArgs);
+  onLinkClick(dropdown) {
+    if (dropdown) {
+      // strange issue where closing dropdown triggers full transition which redirects to auth screen in production builds
+      // closing dropdown in next tick of run loop fixes it
+      next(() => dropdown.actions.close());
+    }
     this.args.onLinkClick();
   }
 }
