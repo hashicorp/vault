3b1a99a7a09c8f9deda65df8d5a91bea0a33c7b8
Josh Black <raskchanky@gmail.com>
2022-09-13 15:52:05 -0700

    add a note about updating the Makefile

--- scripts/protocversioncheck.sh
+++ scripts/protocversioncheck.sh
@@ -12,5 +12,6 @@ if [ "$PROTOC_VERSION" == "$PROTOC_VERSION_EXACT" ]; then
   echo "Using protoc version $PROTOC_VERSION"
 else
   echo "protoc should be at $PROTOC_VERSION_EXACT; found $PROTOC_VERSION."
+  echo "If your version is higher than the version this script is looking for, updating the Makefile with the newer version."
   exit 1
 fi
