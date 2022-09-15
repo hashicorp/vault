8efbff550621e280f8a052bacd2327385dbcd877
Scott Miller <smiller@hashicorp.com>
2022-09-15 11:54:46 -0600

    Backport of #17138: Populate CRL data on backend startup (#17150)
    * Load existing CRLs on startup and after invalidate (#17138)
    
    * Load existing CRLs on startup and after invalidate
    
    * changelog
    
    * Populate during renew calls also (#17143)
    
    * Remove unused config fetch

--- builtin/credential/cert/path_login.go
+++ builtin/credential/cert/path_login.go
@@ -59,6 +59,12 @@ func (b *backend) pathLoginAliasLookahead(ctx context.Context, req *logical.Requ
 }
 
 func (b *backend) pathLogin(ctx context.Context, req *logical.Request, data *framework.FieldData) (*logical.Response, error) {
+	if b.crls == nil {
+		if err := b.populateCRLs(ctx, req.Storage); err != nil {
+			return nil, err
+		}
+	}
+
 	var matched *ParsedCert
 	if verifyResp, resp, err := b.verifyCredentials(ctx, req, data); err != nil {
 		return nil, err
@@ -117,6 +123,12 @@ func (b *backend) pathLoginRenew(ctx context.Context, req *logical.Request, d *f
 	config, err := b.Config(ctx, req.Storage)
 	if err != nil {
 		return nil, err
+	}
+
+	if b.crls == nil {
+		if err := b.populateCRLs(ctx, req.Storage); err != nil {
+			return nil, err
+		}
 	}
 
 	if !config.DisableBinding {
