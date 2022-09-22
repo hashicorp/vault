3e696dc496adf9d88424eb1f043bff2fb724de42
Austin Gebauer <agebauer@hashicorp.com>
2022-09-22 09:55:27 -0700

    use request context instead of core activeContext

--- vault/plugin_reload.go
+++ vault/plugin_reload.go
@@ -202,7 +202,7 @@ func (c *Core) reloadBackendCommon(ctx context.Context, entry *MountEntry, isAut
 		// rely on lazy loading for initialization. v5 backends do not rely on lazy loading
 		// for initialization unless the plugin process is killed. Reload of a v5 backend
 		// results in a new plugin process, so we must initialize the backend here.
-		err := backend.Initialize(c.activeContext, &logical.InitializationRequest{Storage: view})
+		err := backend.Initialize(ctx, &logical.InitializationRequest{Storage: view})
 		if err != nil {
 			return err
 		}
