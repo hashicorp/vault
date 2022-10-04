0272c994f4742e745815eca7f4dd8a32bac5d22e
Nick Cabatoff <ncabatoff@hashicorp.com>
2022-10-04 15:00:57 -0400

    Prepare for 1.10.8 release. (#17410)

--- sdk/version/version_base.go
+++ sdk/version/version_base.go
@@ -8,7 +8,7 @@ var (
 	// Whether cgo is enabled or not; set at build time
 	CgoEnabled bool
 
-	Version           = "1.10.7"
+	Version           = "1.10.8"
 	VersionPrerelease = ""
 	VersionMetadata   = ""
 )
