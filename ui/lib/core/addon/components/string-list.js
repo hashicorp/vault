8188328700c1b85e9ef7cff7534701cbbeac6923
Angel Garbarino <Monkeychip@users.noreply.github.com>
2022-10-12 11:56:05 -0700

    PKI role non-default options (#17393)
    * dynamically render the secretlistheader in the parent route.
    
    * start getting form setup even without openAPi working
    
    * add in create and cancel
    
    * making openAPI work
    
    * add default openAPI params
    
    * wip for new component with two radio options a ttl and input
    
    * handle createRecord on pki-roles-form
    
    * remove tooltips and cleanup
    
    * move formfieldgroupsloop back to non addon
    
    * cleanup
    
    * move secretListHeader
    
    * broadcast from radioSelectTtlOrString to parent
    
    * cleanup
    
    * hide tooltips
    
    * pass through sub text to stringArray
    
    * Add conditional for keybits and keyType
    
    * set defaults for keyBits ... 🤮
    
    * fix some small issues
    
    * more info form field typ
    
    * show only label and subText
    
    * wip context switch 🤮
    
    * fix dontShowLabel
    
    * getting css grid setup
    
    * more on flex groups
    
    * adding the second chunk to key usage
    
    * serialize the post for key_usage
    
    * finish for ext_key_usage
    
    * clean up
    
    * fix snack_case issue
    
    * commit for working state, next trying to remove form-field-group-loops because it's causing issues.
    
    * remove usage of formfieldgroupsloop because of issues with css grid and conditionals
    
    * clean up
    
    * remove string-list helpText changes for tooltip removal because that should be it's own pr.
    
    * clarification from design and backend.
    
    * small cleanup
    
    * pull key_usage and ext_key_usage out of the model and into a component
    
    * clean up
    
    * clean up
    
    * restructure css grid:
    
    * clean up
    
    * broke some things
    
    * fix error when roles list returned 404
    
    * claires feedback
    
    * cleanup
    
    * clean up

--- ui/lib/core/addon/components/string-list.js
+++ ui/lib/core/addon/components/string-list.js
@@ -18,6 +18,8 @@ import { set } from '@ember/object';
  * @param {string} helpText - Text displayed as a tooltip.
  * @param {string} type=array - Optional type for inputValue.
  * @param {string} attrName - We use this to check the type so we can modify the tooltip content.
+ * @param {string} subText - Text below the label.
+ * @param {boolean} hideFormSection - If true do not add form-section class on surrounding div.
  */
 
 export default class StringList extends Component {
---
+++ ui/lib/pki/app/components/pki-key-usage.js
@@ -0,0 +1,1 @@
+export { default } from 'pki/components/pki-key-usage';
