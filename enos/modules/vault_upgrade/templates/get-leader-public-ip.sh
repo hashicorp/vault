53fa5e05dfa557eaf49c887cfec3b77f7f2a599e
Jaymala <jaymala@hashicorp.com>
2022-10-19 16:23:44 -0400

    Add Enos scenarios and workflows for testing 1.10.x (#17606)
    * Add Enos scenarios and workflows for testing 1.10.x
    
    Signed-off-by: Jaymala Sinha <jaymala@hashicorp.com>
    
    * Update build workflow to support Enos
    
    Signed-off-by: Jaymala Sinha <jaymala@hashicorp.com>
    
    * Enable Enos verify in CRT workflow
    
    Signed-off-by: Jaymala Sinha <jaymala@hashicorp.com>
    
    * Add Enos K8s scenarios
    
    Signed-off-by: Jaymala Sinha <jaymala@hashicorp.com>
    
    Signed-off-by: Jaymala Sinha <jaymala@hashicorp.com>

---
+++ enos/modules/vault_upgrade/templates/get-leader-public-ip.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -e
+
+binpath=${vault_install_dir}/vault
+export VAULT_ADDR="http://localhost:8200"
+
+instances='${vault_instances}'
+
+# Find the leader
+leader_address=$($binpath status -format json | jq '.leader_address | rtrimstr(":8200") | ltrimstr("http://")')
+
+# Get the public ip address of the leader
+leader_public=$(jq ".[] | select(.private_ip==$leader_address) | .public_ip" <<< "$instances")
+echo "$leader_public" | sed 's/\"//g'
