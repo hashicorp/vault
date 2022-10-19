feb6ed284512f4c2bb5c1ec9b64cfbd31e3cb77e
Jaymala Sinha <jaymala@hashicorp.com>
2022-10-19 12:27:10 -0400

    Add Enos scenarios and workflows for testing 1.10.x
    Signed-off-by: Jaymala Sinha <jaymala@hashicorp.com>

---
+++ enos/modules/build_local/main.tf
@@ -0,0 +1,59 @@
+variable "bundle_path" {
+  type    = string
+  default = "/tmp/vault.zip"
+}
+
+variable "build_tags" {
+  type        = list(string)
+  description = "The build tags to pass to the Go compiler"
+}
+
+variable "goarch" {
+  type        = string
+  description = "The Go architecture target"
+  default     = "amd64"
+}
+
+variable "goos" {
+  type        = string
+  description = "The Go OS target"
+  default     = "linux"
+}
+
+variable "artifactory_host" { default = null }
+variable "artifactory_repo" { default = null }
+variable "artifactory_username" { default = null }
+variable "artifactory_token" { default = null }
+variable "arch" {
+  default = null
+}
+variable "artifact_path" {
+  default = null
+}
+variable "artifact_type" {
+  default = null
+}
+variable "distro" {
+  default = null
+}
+variable "edition" {
+  default = null
+}
+variable "instance_type" {
+  default = null
+}
+variable "revision" {
+  default = null
+}
+variable "vault_product_version" {
+  default = null
+}
+
+resource "enos_local_exec" "build" {
+  content = templatefile("${path.module}/templates/build.sh", {
+    bundle_path = var.bundle_path,
+    build_tags  = join(" ", var.build_tags)
+    goarch      = var.goarch
+    goos        = var.goos
+  })
+}
