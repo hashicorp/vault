bbae22b5bca63c81eb6c4520ad1d5dc3b693ac59
Angel Garbarino <argarbarino@gmail.com>
2022-10-20 10:17:25 -0600

    pki-role-form-test

--- ui/tests/integration/components/pki/pki-role-form-test.js
+++ ui/tests/integration/components/pki/pki-role-form-test.js
@@ -4,13 +4,9 @@ import { render, click, fillIn } from '@ember/test-helpers';
 import { hbs } from 'ember-cli-htmlbars';
 import { setupEngine } from 'ember-engines/test-support';
 import { SELECTORS, clearRecord } from 'vault/tests/helpers/pki-engine';
-// import { create } from 'ember-cli-page-object';
 import { setupMirage } from 'ember-cli-mirage/test-support';
-// import fm from 'vault/tests/pages/components/flash-message';
 
-// const flashMessage = create(fm);
-
-module('Integration | Component | pki/role-form', function (hooks) {
+module('Integration | Component | pki/role-form meep', function (hooks) {
   setupRenderingTest(hooks);
   setupMirage(hooks);
   setupEngine(hooks, 'pki'); // https://github.com/ember-engines/ember-engines/pull/653
@@ -19,10 +15,6 @@ module('Integration | Component | pki/role-form', function (hooks) {
     this.store = this.owner.lookup('service:store');
     this.model = this.store.createRecord('pki/pki-role-engine');
     this.model.backend = 'pki';
-  });
-
-  hooks.after(function () {
-    //  ARG TODO unsure if need to destroy record.
   });
 
   test('it should render default fields and toggle groups', async function (assert) {
@@ -50,20 +42,34 @@ module('Integration | Component | pki/role-form', function (hooks) {
     assert.dom(SELECTORS.san).exists('shows form-field group SAN');
     assert.dom(SELECTORS.additionalSubjectFields).exists('shows form-field group additional subject fields');
 
-    //* clean up test state
+    // clean up test state
     await clearRecord(this.store, 'pki/pki-role-engine', 'test-role');
   });
 
-  test('it should save a new pki role meep', async function (assert) {
-    assert.expect(12);
-    // this.server.post(`/pki/roles/test-role`, (schema, req) => {
-    //   assert.ok(true, 'Request made to save role');
-    // const keyType = req.requestBody.key_type;
-    // const
-    // assert.strictEqual(json.verification_ttl, 86400, 'request made with correct args to accurate endpoint');
-    // console.log(req.requestBody, 'here!!');
-    //   return {};
-    // });
+  test('it should save a new pki role', async function (assert) {
+    assert.expect(9);
+    // Key Usage, Key Params and Not Valid after options are tested in their respective component tests
+    this.server.post(`/${this.model.backend}/roles/test-role`, (schema, req) => {
+      assert.ok(true, 'Request made to save role');
+      const request = JSON.parse(req.requestBody);
+      const roleName = request.name;
+      const allowedDomainsTemplate = request.allowed_domains_template;
+      const policyIdentifiers = request.policy_identifiers;
+      const allowedUriSansTemplate = request.allow_uri_sans_template;
+      const allowedSerialNumbers = request.allowed_serial_numbers;
+
+      assert.strictEqual(roleName, 'test-role', 'correctly sends the role name');
+      assert.true(allowedDomainsTemplate, 'correctly sends allowed_domains_template');
+      assert.strictEqual(policyIdentifiers[0], 'some-oid', 'correctly sends policy_identifiers');
+      assert.true(allowedUriSansTemplate, 'correctly sends allowed_uri_sans_template');
+      assert.strictEqual(
+        allowedSerialNumbers[0],
+        'some-serial-number',
+        'correctly sends allowed_serial_numbers'
+      );
+      return {};
+    });
+
     this.onSave = () => assert.ok(true, 'onSave callback fires on save success');
 
     await render(
@@ -86,26 +92,26 @@ module('Integration | Component | pki/role-form', function (hooks) {
       .includesText('Name is required.', 'show correct error message');
 
     await fillIn(SELECTORS.roleName, 'test-role');
-    await click(SELECTORS.keyParams);
-    await fillIn('[data-test-input="keyType"]', 'ec'); // should automatically select keyBits 256.
-    await click(SELECTORS.keyUsage);
-    await fillIn('[data-test-input="keyType"]', 'ec'); // should automatically select keyBits 256.
-    await click('[data-test-input="KeyEncipherment"]'); // default true, click to false.
-    await click('[data-test-input="ContentCommitment"]'); // default to true.
-    await click('[data-test-input="Any"]'); // default to true for Extended Key Usage.
-    // await this.pauseTest();
+    await click('[data-test-input="addBasicConstraints"]');
+    await click(SELECTORS.domainHandling);
+    await click('[data-test-input="allowedDomainsTemplate"]');
+    await click(SELECTORS.policyIdentifiers);
+    await fillIn('[data-test-input="policyIdentifiers"] [data-test-string-list-input="0"]', 'some-oid');
+    await click(SELECTORS.san);
+    await click('[data-test-input="allowUriSansTemplate"]');
+    await click(SELECTORS.additionalSubjectFields);
+    await fillIn(
+      '[data-test-input="allowedSerialNumbers"] [data-test-string-list-input="0"]',
+      'some-serial-number'
+    );
     await click(SELECTORS.roleCreateButton);
 
-    // await this.pauseTest();
-    // assert.strictEqual(
-    //   flashMessage.latestMessage,
-    //   'Successfully created the role test-role.',
-    //   'renders success flash upon role creation'
-    // );
-    // assert.strictEqual(
-    //   currentRouteName(),
-    //   'vault.cluster.secrets.backend.pki.pki.roles.role.details',
-    //   'navigates to role detail view after save'
-    // );
+    // clean up test state
+    await clearRecord(this.store, 'pki/pki-role-engine', 'test-role');
   });
+
+  /* FUTURE TEST TODO:
+   * it should update role
+   * it should unload the record on cancel
+   */
 });
