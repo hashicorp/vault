06bb9d380c12c03040df17b83a0ff778b1e60c06
Jason O'Donnell <2160810+jasonodonnell@users.noreply.github.com>
2022-11-03 09:26:42 -0400

    Use correct bool for perf standby

--- vault/policy_store.go
+++ vault/policy_store.go
@@ -259,9 +259,13 @@ func (c *Core) setupPolicyStore(ctx context.Context) error {
 		return err
 	}
 
-	if c.ReplicationState().HasState(consts.ReplicationPerformanceSecondary |
-		consts.ReplicationDRSecondary | consts.ReplicationPerformanceStandby) {
+	if c.ReplicationState().HasState(consts.ReplicationPerformanceSecondary | consts.ReplicationDRSecondary) {
 		// Policies will sync from the primary
+		return nil
+	}
+
+	if c.perfStandby {
+		// Policies will sync from the active
 		return nil
 	}
 
