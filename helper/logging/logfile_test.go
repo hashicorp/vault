8a3ab39f0c611d20faa5c0d3f560e3a18726a9b5
peteski22 <peter.wilson@hashicorp.com>
2022-11-07 20:35:38 +0000

    Basic test for logfile

--- helper/logging/logfile_test.go
+++ helper/logging/logfile_test.go
@@ -1,3 +1,45 @@
 package logging
 
-// TODO: PW: Add tests for log file.
+import (
+	"os"
+	"strings"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestLogFile_openNew(t *testing.T) {
+	logFile := LogFile{
+		fileName: "vault-agent.log",
+		logPath:  TempTestDir(t, ""),
+	}
+	err := logFile.openNew()
+	require.NoError(t, err)
+
+	msg := "[INFO] Something"
+	_, err = logFile.Write([]byte(msg))
+	require.NoError(t, err)
+
+	content, err := os.ReadFile(logFile.FileInfo.Name())
+	require.NoError(t, err)
+	require.Contains(t, string(content), msg)
+}
+
+// TempDir creates a temporary directory within tmpdir with the name 'testname-name'.
+// If the directory cannot be created t.Fatal is called.
+// The directory will be removed when the test ends.
+func TempTestDir(t testing.TB, name string) string {
+	if t == nil {
+		panic("argument t must be non-nil")
+	}
+	name = t.Name() + "-" + name
+	name = strings.Replace(name, "/", "_", -1)
+	d, err := os.MkdirTemp("", name)
+	if err != nil {
+		t.Fatalf("err: %s", err)
+	}
+	t.Cleanup(func() {
+		os.RemoveAll(d)
+	})
+	return d
+}
