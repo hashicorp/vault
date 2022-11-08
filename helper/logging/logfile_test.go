4a82d0e338a028870f9280a2da164371c14b8eea
peteski22 <peter.wilson@hashicorp.com>
2022-11-08 19:17:13 +0000

    use T.tempDir()

--- helper/logging/logfile_test.go
+++ helper/logging/logfile_test.go
@@ -2,7 +2,6 @@ package logging
 
 import (
 	"os"
-	"strings"
 	"testing"
 
 	"github.com/stretchr/testify/require"
@@ -11,7 +10,7 @@ import (
 func TestLogFile_openNew(t *testing.T) {
 	logFile := LogFile{
 		fileName: "vault-agent.log",
-		logPath:  TempTestDir(t, ""),
+		logPath:  t.TempDir(),
 	}
 	err := logFile.openNew()
 	require.NoError(t, err)
@@ -23,23 +22,4 @@ func TestLogFile_openNew(t *testing.T) {
 	content, err := os.ReadFile(logFile.FileInfo.Name())
 	require.NoError(t, err)
 	require.Contains(t, string(content), msg)
-}
-
-// TempDir creates a temporary directory within tmpdir with the name 'testname-name'.
-// If the directory cannot be created t.Fatal is called.
-// The directory will be removed when the test ends.
-func TempTestDir(t testing.TB, name string) string {
-	if t == nil {
-		panic("argument t must be non-nil")
-	}
-	name = t.Name() + "-" + name
-	name = strings.Replace(name, "/", "_", -1)
-	d, err := os.MkdirTemp("", name)
-	if err != nil {
-		t.Fatalf("err: %s", err)
-	}
-	t.Cleanup(func() {
-		os.RemoveAll(d)
-	})
-	return d
 }
