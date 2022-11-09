1262b8a6afc9491f0555491436ea770423674a97
Alexander Scheel <alex.scheel@hashicorp.com>
2022-11-09 13:31:57 -0500

    Add more helpers to common PKI health-check code
    Signed-off-by: Alexander Scheel <alex.scheel@hashicorp.com>

--- command/healthcheck/pki.go
+++ command/healthcheck/pki.go
@@ -96,6 +96,36 @@ func pkiFetchIssuer(e *Executor, issuer string, versionError func()) (bool, *Pat
 	return false, issuerRet, issuerRet.ParsedCache["certificate"].(*x509.Certificate), nil
 }
 
+func pkiFetchIssuerEntry(e *Executor, issuer string, versionError func()) (bool, *PathFetch, map[string]interface{}, error) {
+	issuerRet, err := e.FetchIfNotFetched(logical.ReadOperation, "/{{mount}}/issuer/"+issuer)
+	if err != nil {
+		return true, nil, nil, err
+	}
+
+	if !issuerRet.IsSecretOK() {
+		if issuerRet.IsUnsupportedPathError() {
+			versionError()
+		}
+		return true, nil, nil, nil
+	}
+
+	if len(issuerRet.ParsedCache) == 0 {
+		cert, err := parsePEMCert(issuerRet.Secret.Data["certificate"].(string))
+		if err != nil {
+			return true, nil, nil, fmt.Errorf("unable to parse issuer %v's certificate: %v", issuer, err)
+		}
+
+		issuerRet.ParsedCache["certificate"] = cert
+	}
+
+	var data map[string]interface{} = nil
+	if issuerRet.Secret != nil && len(issuerRet.Secret.Data) > 0 {
+		data = issuerRet.Secret.Data
+	}
+
+	return false, issuerRet, data, nil
+}
+
 func pkiFetchIssuerCRL(e *Executor, issuer string, delta bool, versionError func()) (bool, *PathFetch, *x509.RevocationList, error) {
 	path := "/{{mount}}/issuer/" + issuer + "/crl"
 	name := "CRL"
@@ -125,4 +155,75 @@ func pkiFetchIssuerCRL(e *Executor, issuer string, delta bool, versionError func
 	}
 
 	return false, crlRet, crlRet.ParsedCache["crl"].(*x509.RevocationList), nil
+}
+
+func pkiFetchKeyEntry(e *Executor, key string, versionError func()) (bool, *PathFetch, map[string]interface{}, error) {
+	keyRet, err := e.FetchIfNotFetched(logical.ReadOperation, "/{{mount}}/key/"+key)
+	if err != nil {
+		return true, nil, nil, err
+	}
+
+	if !keyRet.IsSecretOK() {
+		if keyRet.IsUnsupportedPathError() {
+			versionError()
+		}
+		return true, nil, nil, nil
+	}
+
+	var data map[string]interface{} = nil
+	if keyRet.Secret != nil && len(keyRet.Secret.Data) > 0 {
+		data = keyRet.Secret.Data
+	}
+
+	return false, keyRet, data, nil
+}
+
+func pkiFetchLeaves(e *Executor, versionError func()) (bool, *PathFetch, []string, error) {
+	leavesRet, err := e.FetchIfNotFetched(logical.ListOperation, "/{{mount}}/certs")
+	if err != nil {
+		return true, nil, nil, err
+	}
+
+	if !leavesRet.IsSecretOK() {
+		if leavesRet.IsUnsupportedPathError() {
+			versionError()
+		}
+
+		return true, nil, nil, nil
+	}
+
+	if len(leavesRet.ParsedCache) == 0 {
+		var leaves []string
+		for _, rawSerial := range leavesRet.Secret.Data["keys"].([]interface{}) {
+			leaves = append(leaves, rawSerial.(string))
+		}
+		leavesRet.ParsedCache["leaves"] = leaves
+	}
+
+	return false, leavesRet, leavesRet.ParsedCache["leaves"].([]string), nil
+}
+
+func pkiFetchLeaf(e *Executor, serial string, versionError func()) (bool, *PathFetch, *x509.Certificate, error) {
+	leafRet, err := e.FetchIfNotFetched(logical.ReadOperation, "/{{mount}}/cert/"+serial)
+	if err != nil {
+		return true, nil, nil, err
+	}
+
+	if !leafRet.IsSecretOK() {
+		if leafRet.IsUnsupportedPathError() {
+			versionError()
+		}
+		return true, nil, nil, nil
+	}
+
+	if len(leafRet.ParsedCache) == 0 {
+		cert, err := parsePEMCert(leafRet.Secret.Data["certificate"].(string))
+		if err != nil {
+			return true, nil, nil, fmt.Errorf("unable to parse leaf %v's certificate: %v", serial, err)
+		}
+
+		leafRet.ParsedCache["certificate"] = cert
+	}
+
+	return false, leafRet, leafRet.ParsedCache["certificate"].(*x509.Certificate), nil
 }
