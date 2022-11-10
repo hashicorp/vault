24ce058d375d45568925533cc42e57830457ac68
clairebontempo@gmail.com <clairebontempo@gmail.com>
2022-11-10 10:55:34 -0800

    remove -engine from all pki component, model, adapter and serializer files

--- ui/app/serializers/pki/issuer.js
+++ ui/app/serializers/pki/issuer.js
@@ -1,6 +1,6 @@
 import ApplicationSerializer from '../application';
 
-export default class PkiIssuerEngineSerializer extends ApplicationSerializer {
+export default class PkiIssuerSerializer extends ApplicationSerializer {
   // rehydrate each issuer model so all model attributes are accessible from the LIST response
   normalizeItems(payload) {
     if (payload.data) {
