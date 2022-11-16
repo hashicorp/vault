eb9dd87e0efca7437b3d2e9279ac0bc55ffba2ad
peteski22 <peter.wilson@hashicorp.com>
2022-11-16 12:16:50 +0000

    Move LogFile to ShareConfig in test

--- command/agent/config/config_test.go
+++ command/agent/config/config_test.go
@@ -191,6 +191,7 @@ func TestLoadConfigFile(t *testing.T) {
 	expected := &Config{
 		SharedConfig: &configutil.SharedConfig{
 			PidFile: "./pidfile",
+			LogFile: "/var/log/vault/vault-agent.log",
 		},
 		AutoAuth: &AutoAuth{
 			Method: &Method{
@@ -230,7 +231,6 @@ func TestLoadConfigFile(t *testing.T) {
 				NumRetries: 12,
 			},
 		},
-		LogFile: "/var/log/vault/vault-agent.log",
 	}
 
 	config.Prune()
