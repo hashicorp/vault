0d48342bb25e3a6bf8e42da01b4e1412ad21a622
Jordan Reimer <zofskeez@gmail.com>
2022-11-17 11:21:00 -0700

    adds mirage factories and handlers for kubernetes (#17943)

---
+++ ui/mirage/factories/kubernetes-role.js
@@ -0,0 +1,16 @@
+import { Factory } from 'ember-cli-mirage';
+
+export default Factory.extend({
+  name: 'default-role',
+  allowed_kubernetes_namespaces: () => ['*'],
+  allowed_kubernetes_namespace_selector: '',
+  token_max_ttl: 86400,
+  token_default_ttl: 0,
+  service_account_name: 'default',
+  kubernetes_role_name: '',
+  kubernetes_role_type: 'Role',
+  generated_role_rules: '',
+  name_template: '',
+  extra_annotations: null,
+  extra_labels: null,
+});
