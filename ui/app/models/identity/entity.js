56a10bb10f5e585415d8ad23aecd15a78a5048f3
claire bontempo <68122737+hellobontempo@users.noreply.github.com>
2022-11-18 17:29:04 -0800

    UI: Implement new policy SS + modal designs (#17749)
    * refactor ss+modal to accept multiple models
    
    * create policy form
    
    * cleanup and fix test
    
    * add tabs to policy modal form
    
    * add search select with modal to entity form
    
    * update group form;
    
    * allow modal to fit-content
    
    * add changelog
    
    * add check for policy create ability
    
    * add id so tests pass
    
    * filter out root option
    
    * fix test
    
    * add cleanup method
    
    * add ACL policy link
    
    * cleanup from comments
    
    * refactor sending action to parent
    
    * refactor, data down actions up!
    
    * cleanup comments
    
    * form field refactor
    
    * add ternary to options
    
    * update tests
    
    * Remodel component structure for clearer logic
    
    Includes fixing the wizard
    
    * address comments
    
    * cleanup args
    
    * refactor inline oidc assignment form
    
    * add line break
    
    * cleanup comments
    
    * fix tests
    
    * add policy template to ss+modal test
    
    * cleanup =true from test
    
    * final cleanup!!!!!!
    
    * actual final cleanup
    
    * fix typo, please be done
    
    Co-authored-by: Chelsea Shaw <82459713+hashishaw@users.noreply.github.com>

--- ui/app/models/identity/entity.js
+++ ui/app/models/identity/entity.js
@@ -4,6 +4,7 @@ import { alias } from '@ember/object/computed';
 import IdentityModel from './_base';
 import apiPath from 'vault/utils/api-path';
 import attachCapabilities from 'vault/lib/attach-capabilities';
+import lazyCapabilities from 'vault/macros/lazy-capabilities';
 
 const Model = IdentityModel.extend({
   formFields: computed(function () {
@@ -20,11 +21,8 @@ const Model = IdentityModel.extend({
     editType: 'kv',
   }),
   policies: attr({
-    label: 'Policies',
-    editType: 'searchSelect',
+    editType: 'yield',
     isSectionHeader: true,
-    fallbackComponent: 'string-list',
-    models: ['policy/acl', 'policy/rgp'],
   }),
   creationTime: attr('string', {
     readOnly: true,
@@ -46,6 +44,8 @@ const Model = IdentityModel.extend({
   canEdit: alias('updatePath.canUpdate'),
   canRead: alias('updatePath.canRead'),
   canAddAlias: alias('aliasPath.canCreate'),
+  policyPath: lazyCapabilities(apiPath`sys/policies`),
+  canCreatePolicies: alias('policyPath.canCreate'),
 });
 
 export default attachCapabilities(Model, {
