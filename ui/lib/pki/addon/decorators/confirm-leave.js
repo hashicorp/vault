9b92df3623e1a3dc438d09d47679b827ae58e7b3
Chelsea Shaw <cshaw@hashicorp.com>
2022-11-22 09:18:33 -0600

    PKI Edit role page with confirm-leave decorator

---
+++ ui/lib/pki/addon/decorators/confirm-leave.js
@@ -0,0 +1,39 @@
+import { action } from '@ember/object';
+import { inject as service } from '@ember/service';
+
+/**
+ * Confirm that the user wants to discard unsaved changes before leaving the page.
+ * This decorator hooks into the willTransition action. If you override setupController,
+ * be sure to set 'model' on the controller to store data or this won't work.
+ */
+export function withConfirmLeave() {
+  return function decorator(SuperClass) {
+    return class ModelValidations extends SuperClass {
+      @service store;
+
+      @action
+      willTransition(transition) {
+        super.willTransition(...arguments);
+        const model = this.controller.get('model');
+        if (model && model.hasDirtyAttributes) {
+          if (
+            window.confirm(
+              'You have unsaved changes. Navigating away will discard these changes. Are you sure you want to discard your changes?'
+            )
+          ) {
+            // error is thrown when you attempt to unload a record that is inFlight (isSaving)
+            if (!model || !model.unloadRecord || model.isSaving) {
+              return;
+            }
+            model.rollbackAttributes();
+            model.destroy();
+            return true;
+          } else {
+            transition.abort();
+            return false;
+          }
+        }
+      }
+    };
+  };
+}
