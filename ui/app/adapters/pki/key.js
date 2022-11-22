62e104138741f31d372d1b4b86d4ce4d2aca94b6
clairebontempo@gmail.com <clairebontempo@gmail.com>
2022-11-22 11:50:08 -0800

    combine adapter query methods

--- ui/app/adapters/pki/key.js
+++ ui/app/adapters/pki/key.js
@@ -4,29 +4,21 @@ import { encodePath } from 'vault/utils/path-encoding-helpers';
 export default class PkiKeyAdapter extends ApplicationAdapter {
   namespace = 'v1';
 
-  _urlForKey(backend, id) {
-    let url = `${this.buildURL()}/${encodePath(backend)}/key`;
+  _urlForQuery(backend, id) {
+    const url = `${this.buildURL()}/${encodePath(backend)}`;
     if (id) {
-      url = url + '/' + encodePath(id);
+      return url + '/key/' + encodePath(id);
     }
-    return url;
-  }
-
-  urlForQuery(backend, id) {
-    let url = `${this.buildURL()}/${encodePath(backend)}/keys`;
-    if (id) {
-      url = url + '/' + encodePath(id);
-    }
-    return url;
+    return url + '/keys';
   }
 
   query(store, type, query) {
     const { backend } = query;
-    return this.ajax(this.urlForQuery(backend), 'GET', { data: { list: true } });
+    return this.ajax(this._urlForQuery(backend), 'GET', { data: { list: true } });
   }
 
   queryRecord(store, type, query) {
     const { backend, id } = query;
-    return this.ajax(this._urlForKey(backend, id), 'GET');
+    return this.ajax(this._urlForQuery(backend, id), 'GET');
   }
 }
--- ui/app/models/pki/key.js
+++ ui/app/models/pki/key.js
@@ -4,17 +4,17 @@ import { expandAttributeMeta } from 'vault/utils/field-to-attrs';
 export default class PkiKeyModel extends Model {
   @attr('string', { readOnly: true }) backend;
   @attr('boolean') isDefault;
-  @attr('string') keyRef; // reference to an existing key: either, vault generate identifier, literal string 'default', or the name assigned to the key. Part of the request URL.
+  @attr('string', { possibleValues: ['internal', 'external'] }) type;
   @attr('string', { detailsLabel: 'Key ID' }) keyId;
   @attr('string') keyName;
   @attr('string') keyType;
-  @attr('string', { detailsLabel: 'Key bit length' }) keyBit;
+  @attr('string', { detailsLabel: 'Key bit length' }) keyBits;
 
   // TODO refactor when field-to-attrs util is refactored as decorator
   _attributeMeta = null; // cache initial result of expandAttributeMeta in getter and return
   get formFields() {
     if (!this._attributeMeta) {
-      this._attributeMeta = expandAttributeMeta(this, ['keyId', 'keyName', 'keyType', 'keyBit']);
+      this._attributeMeta = expandAttributeMeta(this, ['keyId', 'keyName', 'keyType', 'keyBits']);
     }
     return this._attributeMeta;
   }
