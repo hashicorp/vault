1cab1117b87bfe716e3bd5702fed266ce04787fe
clairebontempo@gmail.com <clairebontempo@gmail.com>
2022-11-23 10:08:23 -0800

    rename adapter method, cleanup template, move formFields to key constructor

--- ui/app/adapters/pki/key.js
+++ ui/app/adapters/pki/key.js
@@ -4,7 +4,7 @@ import { encodePath } from 'vault/utils/path-encoding-helpers';
 export default class PkiKeyAdapter extends ApplicationAdapter {
   namespace = 'v1';
 
-  _urlForQuery(backend, id) {
+  getUrl(backend, id) {
     const url = `${this.buildURL()}/${encodePath(backend)}`;
     if (id) {
       return url + '/key/' + encodePath(id);
@@ -14,11 +14,11 @@ export default class PkiKeyAdapter extends ApplicationAdapter {
 
   query(store, type, query) {
     const { backend } = query;
-    return this.ajax(this._urlForQuery(backend), 'GET', { data: { list: true } });
+    return this.ajax(this.getUrl(backend), 'GET', { data: { list: true } });
   }
 
   queryRecord(store, type, query) {
     const { backend, id } = query;
-    return this.ajax(this._urlForQuery(backend, id), 'GET');
+    return this.ajax(this.getUrl(backend, id), 'GET');
   }
 }
--- ui/app/models/pki/key.js
+++ ui/app/models/pki/key.js
@@ -8,14 +8,11 @@ export default class PkiKeyModel extends Model {
   @attr('string', { detailsLabel: 'Key ID' }) keyId;
   @attr('string') keyName;
   @attr('string') keyType;
-  @attr('string', { detailsLabel: 'Key bit length' }) keyBits;
+  @attr('string', { detailsLabel: 'Key bit length' }) keyBits; // TODO confirm with crypto team to remove this field from details page
 
   // TODO refactor when field-to-attrs util is refactored as decorator
-  _attributeMeta = null; // cache initial result of expandAttributeMeta in getter and return
-  get formFields() {
-    if (!this._attributeMeta) {
-      this._attributeMeta = expandAttributeMeta(this, ['keyId', 'keyName', 'keyType', 'keyBits']);
-    }
-    return this._attributeMeta;
+  constructor() {
+    super(...arguments);
+    this.formFields = expandAttributeMeta(this, ['keyId', 'keyName', 'keyType', 'keyBits']);
   }
 }
