66b8ab8e677b1ddf0b638bd6faff2537d91cd5de
Nick Cabatoff <ncabatoff@hashicorp.com>
2022-11-30 16:34:53 -0500

    Add recent change to sdk/version that I'd missed.

--- version/version.go
+++ version/version.go
@@ -5,12 +5,12 @@ import (
 	"fmt"
 )
 
-// VersionInfo
 type VersionInfo struct {
 	Revision          string `json:"revision,omitempty"`
 	Version           string `json:"version,omitempty"`
 	VersionPrerelease string `json:"version_prerelease,omitempty"`
 	VersionMetadata   string `json:"version_metadata,omitempty"`
+	BuildDate         string `json:"build_date,omitempty"`
 }
 
 func GetVersion() *VersionInfo {
@@ -29,6 +29,7 @@ func GetVersion() *VersionInfo {
 		Version:           ver,
 		VersionPrerelease: rel,
 		VersionMetadata:   md,
+		BuildDate:         BuildDate,
 	}
 }
 
@@ -68,6 +69,10 @@ func (c *VersionInfo) FullVersionNumber(rev bool) string {
 
 	if rev && c.Revision != "" {
 		fmt.Fprintf(&versionString, " (%s)", c.Revision)
+	}
+
+	if c.BuildDate != "" {
+		fmt.Fprintf(&versionString, ", built %s", c.BuildDate)
 	}
 
 	return versionString.String()
