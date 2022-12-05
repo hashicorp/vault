d122a12cba9c8503fc4b67e1c091dfd16c2aba0c
Chelsea Shaw <cshaw@hashicorp.com>
2022-12-05 12:43:13 -0600

    Add PKI Acceptance Test

---
+++ ui/tests/acceptance/pki/pki-engine-workflow-test.js
@@ -0,0 +1,160 @@
+import { create } from 'ember-cli-page-object';
+import { module, test } from 'qunit';
+import { setupApplicationTest } from 'ember-qunit';
+import authPage from 'vault/tests/pages/auth';
+import logout from 'vault/tests/pages/logout';
+import enablePage from 'vault/tests/pages/settings/mount-secret-backend';
+import consoleClass from 'vault/tests/pages/components/console/ui-panel';
+import { click, currentURL, visit } from '@ember/test-helpers';
+
+const consoleComponent = create(consoleClass);
+
+const SELECTORS = {
+  overviewTab: '[data-test-secret-list-tab="Overview"]',
+  rolesTab: '[data-test-secret-list-tab="Roles"]',
+  issuersTab: '[data-test-secret-list-tab="Issuers"]',
+  certsTab: '[data-test-secret-list-tab="Certificates"]',
+  keysTab: '[data-test-secret-list-tab="Keys"]',
+  configTab: '[data-test-secret-list-tab="Configuration"]',
+  deleteRoleButton: '[data-test-pki-role-delete]',
+  generateCertLink: '[data-test-pki-role-generate-cert]',
+  signCertLink: '[data-test-pki-role-sign-cert]',
+  editRoleLink: '[data-test-pki-role-edit-link]',
+};
+
+const tokenWithPolicy = async function (name, policy) {
+  await consoleComponent.runCommands([
+    `write sys/policies/acl/${name} policy=${btoa(policy)}`,
+    `write -field=client_token auth/token/create policies=${name}`,
+  ]);
+  return consoleComponent.lastLogOutput;
+};
+
+const runCommands = async function (commands) {
+  try {
+    await consoleComponent.runCommands(commands);
+    const res = consoleComponent.lastLogOutput;
+    if (res.includes('Error')) {
+      throw new Error(res);
+    }
+    return res;
+  } catch (error) {
+    // eslint-disable-next-line no-console
+    console.error(
+      `The following occurred when trying to run the following command(s):\n ${commands.join('\n')} \n\n ${
+        consoleComponent.lastLogOutput
+      }`
+    );
+    throw error;
+  }
+};
+
+module('Acceptance | pki workflow', function (hooks) {
+  setupApplicationTest(hooks);
+
+  hooks.beforeEach(async function () {
+    await authPage.login();
+    // Setup PKI engine
+    const mountPath = `pki-workflow-${new Date().getTime()}`;
+    await enablePage.enable('pki', mountPath);
+    await runCommands([
+      `write ${mountPath}/roles/some-role \
+    issuer_ref="default" \
+    allowed_domains="example.com" \
+    allow_subdomains=true \
+    max_ttl="720h"`,
+    ]);
+    this.mountPath = mountPath;
+
+    await logout.visit();
+  });
+
+  hooks.afterEach(async function () {
+    await logout.visit();
+    await authPage.login();
+    // Cleanup engine
+    await runCommands([`delete sys/mounts/${this.mountPath}`]);
+    await logout.visit();
+  });
+
+  module('roles', function (hooks) {
+    hooks.beforeEach(async function () {
+      await authPage.login();
+      // Setup role-specific items
+      await runCommands([
+        `write ${this.mountPath}/roles/some-role \
+      issuer_ref="default" \
+      allowed_domains="example.com" \
+      allow_subdomains=true \
+      max_ttl="720h"`,
+      ]);
+      const pki_admin_policy = `
+          path "${this.mountPath}/*" {
+            capabilities = ["create", "read", "update", "delete", "list"]
+          },
+        `;
+      const pki_reader_policy = `
+        path "${this.mountPath}/roles" {
+          capabilities = ["read", "list"]
+        },
+      `;
+      const pki_editor_policy = `
+        path "${this.mountPath}/roles" {
+          capabilities = ["read", "list"]
+        },
+        path "${this.mountPath}/roles/*" {
+          capabilities = ["read", "update"]
+        },
+      `;
+      this.pkiRoleReader = await tokenWithPolicy('pki-reader', pki_reader_policy);
+      this.pkiRoleEditor = await tokenWithPolicy('pki-editor', pki_editor_policy);
+      this.pkiAdminToken = await tokenWithPolicy('pki-admin', pki_admin_policy);
+      await logout.visit();
+    });
+    test('shows correct items if user has all permissions', async function (assert) {
+      await authPage.login(this.pkiAdminToken);
+      await visit(`/vault/secrets/${this.mountPath}/pki/overview`);
+      assert.strictEqual(currentURL(), `/vault/secrets/${this.mountPath}/pki/overview`);
+      assert.dom(SELECTORS.rolesTab).exists('Roles tab is present');
+      await click(SELECTORS.rolesTab);
+      assert.dom('.toolbar-link').exists({ count: 1 }, 'Correct number of toolbar links render');
+      assert.strictEqual(currentURL(), `/vault/secrets/${this.mountPath}/pki/roles`);
+      assert.dom('.linked-block').exists({ count: 1 }, 'One role is in list');
+      await click('.linked-block');
+      assert.dom(SELECTORS.deleteRoleButton).exists('Delete role button is shown');
+      assert.dom(SELECTORS.generateCertLink).exists('Generate cert link is shown');
+      assert.dom(SELECTORS.signCertLink).exists('Sign cert link is shown');
+      assert.dom(SELECTORS.editRoleLink).exists('Edit link is shown');
+    });
+
+    test('it does not show toolbar items the user does not have permission to see', async function (assert) {
+      await authPage.login(this.pkiRoleReader);
+      await visit(`/vault/secrets/${this.mountPath}/pki/overview`);
+      assert.dom(SELECTORS.rolesTab).exists('Roles tab is present');
+      await click(SELECTORS.rolesTab);
+      assert.dom('.toolbar-link').exists({ count: 1 }, 'Correct number of toolbar links render');
+      assert.strictEqual(currentURL(), `/vault/secrets/${this.mountPath}/pki/roles`);
+      assert.dom('.linked-block').exists({ count: 1 }, 'One role is in list');
+      await click('.linked-block');
+      assert.dom(SELECTORS.deleteRoleButton).doesNotExist('Delete role button is not shown');
+      assert.dom(SELECTORS.generateCertLink).doesNotExist('Generate cert link is not shown');
+      assert.dom(SELECTORS.signCertLink).doesNotExist('Sign cert link is not shown');
+      assert.dom(SELECTORS.editRoleLink).doesNotExist('Edit link is not shown');
+    });
+
+    test('it shows correct toolbar items for the user policy', async function (assert) {
+      await authPage.login(this.pkiRoleReader);
+      await visit(`/vault/secrets/${this.mountPath}/pki/overview`);
+      assert.dom(SELECTORS.rolesTab).exists('Roles tab is present');
+      await click(SELECTORS.rolesTab);
+      assert.dom('.toolbar-link').exists({ count: 1 }, 'Correct number of toolbar links render');
+      assert.strictEqual(currentURL(), `/vault/secrets/${this.mountPath}/pki/roles`);
+      assert.dom('.linked-block').exists({ count: 1 }, 'One role is in list');
+      await click('.linked-block');
+      assert.dom(SELECTORS.deleteRoleButton).doesNotExist('Delete role button is not shown');
+      assert.dom(SELECTORS.generateCertLink).doesNotExist('Generate cert link is not shown');
+      assert.dom(SELECTORS.signCertLink).doesNotExist('Sign cert link is not shown');
+      assert.dom(SELECTORS.editRoleLink).exists('Edit link is shown');
+    });
+  });
+});
