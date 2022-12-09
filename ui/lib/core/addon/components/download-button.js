41a4207015c2507b493a7ddc7d5cf2ecdd6794a1
clairebontempo@gmail.com <clairebontempo@gmail.com>
2022-12-09 10:04:06 -0800

    initial glimmerization, delete toolbar-download-button component

--- ui/lib/core/addon/components/download-button.js
+++ ui/lib/core/addon/components/download-button.js
@@ -1,22 +1,25 @@
-import Component from '@ember/component';
-import { computed } from '@ember/object';
-import hbs from 'htmlbars-inline-precompile';
+import { action } from '@ember/object';
+import Component from '@glimmer/component';
 
-export default Component.extend({
-  layout: hbs`{{#if (has-block)}} {{yield}} {{else}} {{actionText}} {{/if}}`,
-  tagName: 'a',
-  role: 'button',
-  attributeBindings: ['role', 'download', 'href'],
-  download: computed('filename', 'extension', function () {
-    return `${this.filename}-${new Date().toISOString()}.${this.extension}`;
-  }),
+export default class DownloadButton extends Component {
+  get extension() {
+    return this.args.extension || 'txt';
+  }
 
-  fileLike: computed('data', 'mime', 'stringify', 'download', function () {
+  get mime() {
+    return this.args.mime || 'text/plain';
+  }
+
+  get download() {
+    return `${this.args.filename}-${new Date().toISOString()}.${this.extension}`;
+  }
+
+  get fileLike() {
     let file;
-    let data = this.data;
+    let data = this.args.data;
     const filename = this.download;
     const mime = this.mime;
-    if (this.stringify) {
+    if (this.args.stringify) {
       data = JSON.stringify(data, null, 2);
     }
     if (window.navigator.msSaveOrOpenBlob) {
@@ -26,26 +29,19 @@ export default Component.extend({
       file = new File([data], filename, { type: mime });
     }
     return file;
-  }),
+  }
 
-  href: computed('fileLike', function () {
+  get href() {
     return window.URL.createObjectURL(this.fileLike);
-  }),
+  }
 
-  click(event) {
+  @action
+  handleDownload(event) {
     if (!window.navigator.msSaveOrOpenBlob) {
       return;
     }
     event.preventDefault();
     const file = this.fileLike;
-    //lol whyyyy
     window.navigator.msSaveOrOpenBlob(file, file.name);
-  },
-
-  actionText: 'Download',
-  data: null,
-  filename: null,
-  mime: 'text/plain',
-  extension: 'txt',
-  stringify: false,
-});
+  }
+}
