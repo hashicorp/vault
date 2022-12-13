3489ffd0def3643203f8c799afe84d5a1e1c3975
Chelsea Shaw <cshaw@hashicorp.com>
2022-12-12 18:36:06 -0600

    Move enable route to correct path

--- ui/app/routes/vault/cluster/settings/auth/enable.js
+++ ui/app/routes/vault/cluster/settings/auth/enable.js
@@ -1,7 +1,7 @@
 import Route from '@ember/routing/route';
 import { inject as service } from '@ember/service';
 
-export default class VaultClusterSettingsAuthConfigureEnableRoute extends Route {
+export default class VaultClusterSettingsAuthEnableRoute extends Route {
   @service store;
 
   beforeModel() {
