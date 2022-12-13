89c8abe4b6026f5b7d4085b2185b0aad87729986
Christopher Swenson <christopher.swenson@hashicorp.com>
2022-12-13 10:33:52 -0800

    Move patch sha1 to root main.go

--- main.go
+++ main.go
@@ -4,7 +4,13 @@ import (
 	"os"
 
 	"github.com/hashicorp/vault/command"
+	"github.com/hashicorp/vault/internal"
 )
+
+func init() {
+	// this is a good place to patch SHA-1 support back into x509
+	internal.PatchSha1()
+}
 
 func main() {
 	os.Exit(command.Run(os.Args[1:]))
