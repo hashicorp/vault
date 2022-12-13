deb605f8de7fbb95c627cff1b18f9d2e8893a193
Chelsea Shaw <cshaw@hashicorp.com>
2022-12-13 15:44:43 -0600

    check if engine has config on overview

--- ui/lib/pki/addon/routes/overview.js
+++ ui/lib/pki/addon/routes/overview.js
@@ -1,3 +1,42 @@
 import Route from '@ember/routing/route';
+import { inject as service } from '@ember/service';
+import RSVP from 'rsvp';
 
-export default class PkiOverviewRoute extends Route {}
+export default class PkiOverviewRoute extends Route {
+  @service secretMountPath;
+  @service auth;
+  @service store;
+
+  get win() {
+    return this.window || window;
+  }
+
+  hasConfig() {
+    const endpoint = `${this.win.origin}/v1/${this.secretMountPath.currentPath}/issuers?list=true`;
+    return this.auth
+      .ajax(endpoint, 'GET', {})
+      .then(() => true)
+      .catch(() => false);
+  }
+
+  async model() {
+    return RSVP.hash({
+      hasConfig: this.hasConfig(),
+      engine: this.store
+        .query('secret-engine', {
+          path: this.secretMountPath.currentPath,
+        })
+        .then((model) => {
+          if (model) {
+            return model.get('firstObject');
+          }
+        }),
+    });
+  }
+
+  setupController(controller, resolvedModel) {
+    super.setupController(controller, resolvedModel);
+    const backend = this.secretMountPath.currentPath || 'pki';
+    controller.breadcrumbs = [{ label: 'secrets', route: 'secrets', linkExternal: true }, { label: backend }];
+  }
+}
