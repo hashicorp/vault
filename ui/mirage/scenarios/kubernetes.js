fc1f8b9b7ee1d075c38705059e6d67efefab5b21
Kianna Quach <kianna.quach@hashicorp.com>
2022-12-15 15:48:47 -0800

    VAULT-12185 overview acceptance tests

--- ui/mirage/scenarios/kubernetes.js
+++ ui/mirage/scenarios/kubernetes.js
@@ -1,6 +1,8 @@
-export default function (server) {
+export default function (server, shouldConfigureRoles = true) {
   server.create('kubernetes-config', { path: 'kubernetes' });
-  server.create('kubernetes-role');
-  server.create('kubernetes-role', 'withRoleName');
-  server.create('kubernetes-role', 'withRoleRules');
+  if (shouldConfigureRoles) {
+    server.create('kubernetes-role');
+    server.create('kubernetes-role', 'withRoleName');
+    server.create('kubernetes-role', 'withRoleRules');
+  }
 }
