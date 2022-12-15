c3ccc8c3737fa54bb8748d7dcb83e76cfaad4641
Chelsea Shaw <cshaw@hashicorp.com>
2022-12-15 17:04:40 -0600

    Remove duplicitous 'value' from secret engines

--- ui/app/helpers/mountable-secret-engines.js
+++ ui/app/helpers/mountable-secret-engines.js
@@ -3,7 +3,6 @@ import { helper as buildHelper } from '@ember/component/helper';
 const ENTERPRISE_SECRET_ENGINES = [
   {
     displayName: 'KMIP',
-    value: 'kmip',
     type: 'kmip',
     engineRoute: 'kmip.scopes',
     category: 'generic',
@@ -11,14 +10,12 @@ const ENTERPRISE_SECRET_ENGINES = [
   },
   {
     displayName: 'Transform',
-    value: 'transform',
     type: 'transform',
     category: 'generic',
     requiredFeature: 'Transform Secrets Engine',
   },
   {
     displayName: 'Key Management',
-    value: 'keymgmt',
     type: 'keymgmt',
     glyph: 'key',
     category: 'cloud',
@@ -30,95 +27,80 @@ const ENTERPRISE_SECRET_ENGINES = [
 const MOUNTABLE_SECRET_ENGINES = [
   {
     displayName: 'Active Directory',
-    value: 'ad',
     type: 'ad',
     category: 'cloud',
   },
   {
     displayName: 'AliCloud',
-    value: 'alicloud',
     type: 'alicloud',
     category: 'cloud',
   },
   {
     displayName: 'AWS',
-    value: 'aws',
     type: 'aws',
     category: 'cloud',
     glyph: 'aws-color',
   },
   {
     displayName: 'Azure',
-    value: 'azure',
     type: 'azure',
     category: 'cloud',
     glyph: 'azure-color',
   },
   {
     displayName: 'Consul',
-    value: 'consul',
     type: 'consul',
     category: 'infra',
   },
   {
     displayName: 'Databases',
-    value: 'database',
     type: 'database',
     category: 'infra',
   },
   {
     displayName: 'Google Cloud',
-    value: 'gcp',
     type: 'gcp',
     category: 'cloud',
     glyph: 'gcp-color',
   },
   {
     displayName: 'Google Cloud KMS',
-    value: 'gcpkms',
     type: 'gcpkms',
     category: 'cloud',
     glyph: 'gcp-color',
   },
   {
     displayName: 'KV',
-    value: 'kv',
     type: 'kv',
     category: 'generic',
   },
   {
     displayName: 'Nomad',
-    value: 'nomad',
     type: 'nomad',
     category: 'infra',
   },
   {
     displayName: 'PKI Certificates',
-    value: 'pki',
     type: 'pki',
     category: 'generic',
   },
   {
     displayName: 'RabbitMQ',
-    value: 'rabbitmq',
     type: 'rabbitmq',
     category: 'infra',
   },
   {
     displayName: 'SSH',
-    value: 'ssh',
     type: 'ssh',
     category: 'generic',
   },
   {
     displayName: 'Transit',
-    value: 'transit',
     type: 'transit',
     category: 'generic',
   },
   {
     displayName: 'TOTP',
-    value: 'totp',
     type: 'totp',
     category: 'generic',
   },
