9a40e62700f18297caf23a2eca1585e7b6c1a39b
clairebontempo@gmail.com <clairebontempo@gmail.com>
2022-12-16 09:46:07 -0800

    create key import component

--- ui/lib/pki/addon/components/pki-key-import.js
+++ ui/lib/pki/addon/components/pki-key-import.js
@@ -1,25 +1,26 @@
-import Component from '@glimmer/component';
 import { action } from '@ember/object';
-import { inject as service } from '@ember/service';
+import Component from '@glimmer/component';
 import { task } from 'ember-concurrency';
 import { tracked } from '@glimmer/tracking';
+import { inject as service } from '@ember/service';
 import errorMessage from 'vault/utils/error-message';
 
+// TODO: convert to typescript after https://github.com/hashicorp/vault/pull/18387 is merged
 /**
- * @module PkiKeyForm
- * PkiKeyForm components are used to create and update PKI keys.
+ * @module PkiKeyImport
+ * PkiKeyImport components are used to import PKI keys.
  *
  * @example
  * ```js
- * <PkiKeyForm @model={{this.model}}/>
+ * <PkiKeyImport @model={{this.model}}/>
  * ```
  *
  * @param {Object} model - pki/key model.
  * @callback onCancel - Callback triggered when cancel button is clicked.
- * @callback onSave - Callback triggered on save success.
+ * @callback onSubmit - Callback triggered on submit success.
  */
 
-export default class PkiKeyForm extends Component {
+export default class PkiKeyImport extends Component {
   @service store;
   @service flashMessages;
 
@@ -27,28 +28,27 @@ export default class PkiKeyForm extends Component {
   @tracked invalidFormAlert;
   @tracked modelValidations;
 
+  get keyNameOptions() {
+    return this.args.model.default.find((attr) => attr === 'keyName');
+  }
+
   @task
-  *save(event) {
+  *submitForm(event) {
     event.preventDefault();
     try {
       const { isValid, state, invalidFormMessage } = this.args.model.validate();
       this.modelValidations = isValid ? null : state;
       this.invalidFormAlert = invalidFormMessage;
       if (isValid) {
-        const { isNew, keyName } = this.args.model;
+        const { keyName } = this.args.model;
         yield this.args.model.save();
-        this.flashMessages.success(`Successfully ${isNew ? 'generated' : 'updated'} key ${keyName}.`);
+        this.flashMessages.success(`Successfully imported key ${keyName}.`);
         this.args.onSave();
       }
     } catch (error) {
       this.errorBanner = errorMessage(error);
       this.invalidFormAlert = 'There was an error submitting this form.';
     }
-  }
-
-  @action
-  onFileUpload() {
-    // do something
   }
 
   @action
