090a660fd038d0d81313e878571f767562d8e0c9
AnPucel <adiroff@hashicorp.com>
2022-12-16 11:36:09 -0800

    Description change
    Co-authored-by: Anton Averchenkov <84287187+averche@users.noreply.github.com>

--- builtin/logical/pki/path_config_urls.go
+++ builtin/logical/pki/path_config_urls.go
@@ -102,7 +102,7 @@ for the OCSP servers attribute. See also RFC 5280 Section 4.2.2.1.`,
 							},
 							"enable_templating": {
 								Type: framework.TypeBool,
-								Description: `Whether or not to enabling templating of the
+								Description: `Whether or not to enable templating of the
 above AIA fields. When templating is enabled the special values '{{issuer_id}}'
 and '{{cluster_path}}' are available, but the addresses are not checked for
 URI validity until issuance time. This requires /config/cluster's path to be
