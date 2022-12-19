60fe54d5e3a9c44b36ef3007626c45150f98cce7
kitography <khaines@mit.edu>
2022-12-19 16:26:45 -0500

    Error Handling Is Almost A Tutorial

--- command/pki_issue_intermediate.go
+++ command/pki_issue_intermediate.go
@@ -60,12 +60,12 @@ func (c *PKIIssueCACommand) Flags() *FlagSets {
 }
 
 func (c *PKIIssueCACommand) Run(args []string) int {
+	// Parse Args
 	f := c.Flags()
 	if err := f.Parse(args); err != nil {
 		c.UI.Error(err.Error())
 		return 1
 	}
-
 	args = f.Args()
 
 	if len(args) < 3 {
@@ -73,107 +73,261 @@ func (c *PKIIssueCACommand) Run(args []string) int {
 		return 1
 	}
 
-	parentMountIssuer := sanitizePath(args[0])            // /pki/issuer/default
-	_, parentIssuerName := paths.Split(parentMountIssuer) // TODO: Use this in order to name the issuer when we import it into the intermediate mount
-
-	client, err := c.Client()
-	if err != nil {
-		c.UI.Error(fmt.Sprintf("Failed to obtain client: %w", err))
-		return 1
-	}
-
-	_, err = client.Logical().Read(parentMountIssuer)
-	if err != nil {
-		c.UI.Error(fmt.Sprintf("Unable to access parent issuer %v: %v", parentMountIssuer, err))
-	}
-
-	intermediateMount := sanitizePath(args[1])
-
-	// Pull our fake stdin if needed
 	stdin := (io.Reader)(os.Stdin)
-
 	data, err := parseArgsData(stdin, args[2:])
 	if err != nil {
 		c.UI.Error(fmt.Sprintf("Failed to parse K=V data: %s", err))
 		return 1
 	}
 
-	csrResp, err := client.Logical().Write(intermediateMount+"/intermediate/generate/"+c.flagKeyStorageSource, data)
+	// Check We Have a Client
+	client, err := c.Client()
 	if err != nil {
-		c.UI.Error(fmt.Sprintf("Failled to Generate Intermediate CSR on %v: %v", intermediateMount, err))
+		c.UI.Error(fmt.Sprintf("Failed to obtain client: %v", err))
 		return 1
 	}
 
-	csrPem := csrResp.Data["csr"].(string)
-	data["csr"] = csrPem
+	// Sanity Check the Parent Issuer
+	parentMountIssuer := sanitizePath(args[0]) // /pki/issuer/default
+	_, parentIssuerName := paths.Split(parentMountIssuer)
+	if !strings.Contains(parentMountIssuer, "/issuer/") {
+		c.UI.Error(fmt.Sprintf("Parent Issuer %v is Not a PKI Issuer Path of the format /mount/issuer/issuer-ref", parentMountIssuer))
+	}
+	_, err = client.Logical().Read(parentMountIssuer + "/json")
+	if err != nil {
+		c.UI.Error(fmt.Sprintf("Unable to access parent issuer %v: %v", parentMountIssuer, err))
+	}
 
+	// Set-up Failure State (Immediately Before First Write Call)
+	intermediateMount := sanitizePath(args[1])
+	failureState := inCaseOfFailure{
+		intermediateMount: intermediateMount,
+		parentMount:       strings.Split(parentMountIssuer, "/issuer/")[0],
+		parentIssuer:      parentMountIssuer,
+		newName:           c.flagNewIssuerName,
+	}
+
+	// Generate Certificate Signing Request
+	csrResp, err := client.Logical().Write(intermediateMount+"/intermediate/generate/"+c.flagKeyStorageSource, data)
+	if err != nil {
+		if strings.Contains(err.Error(), "no handler for route") { // Mount Given Does Not Exist
+			c.UI.Error(fmt.Sprintf("Given Intermediate Mount %v Does Not Exist: %v", intermediateMount, err))
+		} else if strings.Contains(err.Error(), "unsupported path") { // Expected if Not a PKI Mount
+			c.UI.Error(fmt.Sprintf("Given Intermeidate Mount %v Is Not a PKI Mount: %v", intermediateMount, err))
+		} else {
+			c.UI.Error(fmt.Sprintf("Failled to Generate Intermediate CSR on %v: %v", intermediateMount, err))
+		}
+		return 1
+	}
+	// Parse CSR Response, Also Verifies that this is a PKI Mount
+	// (eg. calling the above call on cubbyhole/ won't return an error response)
+	csrPemRaw, present := csrResp.Data["csr"]
+	if !present {
+		c.UI.Error(fmt.Sprintf("Failed to Generate Intermediate CSR on %v, got response: %v", intermediateMount, csrResp))
+		return 1
+	}
+	keyIdRaw, present := csrResp.Data["key_id"]
+	if !present && c.flagKeyStorageSource == "internal" {
+		c.UI.Error(fmt.Sprintf("Failed to Generate Key on %v, got response: %v", intermediateMount, csrResp))
+		return 1
+	}
+
+	// If that all Parses, then we've successfully generated a CSR!  Save It (and the Key-ID)
+	failureState.csrGenerated = true
+	if c.flagKeyStorageSource == "internal" {
+		failureState.createdKeyId = keyIdRaw.(string)
+	}
+	csr := csrPemRaw.(string)
+	failureState.csr = csr
+	data["csr"] = csr
+
+	// Next, Sign the CSR
 	rootResp, err := client.Logical().Write(parentMountIssuer+"/sign-intermediate", data)
 	if err != nil {
+		c.UI.Error(failureState.generateFailureMessage())
 		c.UI.Error(fmt.Sprintf("Error Signing Intermiate On %v", err))
 		return 1
 	}
+	// Success!  Save Our Progress (and Parse the Response)
+	failureState.csrSigned = true
+	serialNumber := rootResp.Data["serial_number"].(string)
+	failureState.certSerialNumber = serialNumber
 
-	// Import Certificate
-	certificate := rootResp.Data["certificate"].(string)
-	err = importIssuerWithName(client, intermediateMount, certificate, c.flagNewIssuerName)
-	if err != nil {
-		c.UI.Error(fmt.Sprintf("Error Importing Into %v Newly Created Issuer %v: %v", intermediateMount, certificate, err))
-		return 1
-	}
-
-	// Import Issuing Certificate
-	issuingCa := rootResp.Data["issuing_ca"].(string)
-	err = importIssuerWithName(client, intermediateMount, issuingCa, parentIssuerName)
-	if err != nil {
-		c.UI.Error(fmt.Sprintf("Error Importing Into %v Newly Created Issuer %v: %v", intermediateMount, certificate, err))
-		return 1
-	}
-
-	// Import CA_Chain (just in case there's more information)
 	caChain := rootResp.Data["ca_chain"].([]interface{})
-	pemBundle := ""
+	caChainPemBundle := ""
 	for _, cert := range caChain {
-		pemBundle += cert.(string) + "\n"
+		caChainPemBundle += cert.(string) + "\n"
 	}
-	importData := map[string]interface{}{
-		"pem_bundle": pemBundle,
-	}
-	finalResp, err := client.Logical().Write(intermediateMount+"/issuers/import/cert", importData)
+	failureState.caChain = caChainPemBundle
+
+	// Next Import Certificate
+	certificate := rootResp.Data["certificate"].(string)
+	issuerId, err := importIssuerWithName(client, intermediateMount, certificate, c.flagNewIssuerName)
+	failureState.certIssuerId = issuerId
 	if err != nil {
-		c.UI.Error(fmt.Sprintf("Error Importing CaChain into %v: %v", intermediateMount, err))
-		return 1
+		if strings.Contains(err.Error(), "error naming issuer") {
+			failureState.certImported = true
+			c.UI.Error(failureState.generateFailureMessage())
+			c.UI.Error(fmt.Sprintf("Error Naming Newly Imported Issuer: %v", err))
+			return 1
+		} else {
+			c.UI.Error(failureState.generateFailureMessage())
+			c.UI.Error(fmt.Sprintf("Error Importing Into %v Newly Created Issuer %v: %v", intermediateMount, certificate, err))
+			return 1
+		}
+	}
+	failureState.certImported = true
+
+	// Then Import Issuing Certificate
+	issuingCa := rootResp.Data["issuing_ca"].(string)
+	_, err = importIssuerWithName(client, intermediateMount, issuingCa, parentIssuerName)
+	if err != nil {
+		if strings.Contains(err.Error(), "error naming issuer") {
+			c.UI.Warn(fmt.Sprintf("Unable to Set Name on Parent Cert from %v Imported Into %v with serial %v, err: %v", parentIssuerName, intermediateMount, serialNumber, err))
+		} else {
+			c.UI.Error(failureState.generateFailureMessage())
+			c.UI.Error(fmt.Sprintf("Error Importing Into %v Newly Created Issuer %v: %v", intermediateMount, certificate, err))
+			return 1
+		}
 	}
 
-	c.UI.Output(fmt.Sprintf("%v", finalResp))
+	// Finally Import CA_Chain (just in case there's more information)
+	if len(caChain) > 2 { // We've already imported parent cert and newly issued cert above
+		importData := map[string]interface{}{
+			"pem_bundle": caChainPemBundle,
+		}
+		_, err := client.Logical().Write(intermediateMount+"/issuers/import/cert", importData)
+		if err != nil {
+			c.UI.Error(failureState.generateFailureMessage())
+			c.UI.Error(fmt.Sprintf("Error Importing CaChain into %v: %v", intermediateMount, err))
+			return 1
+		}
+	}
+	failureState.caChainImported = true
 
 	return 0
 }
 
-func importIssuerWithName(client *api.Client, mount string, bundle string, name string) error {
+func importIssuerWithName(client *api.Client, mount string, bundle string, name string) (issuerUUID string, err error) {
 	importData := map[string]interface{}{
 		"pem_bundle": bundle,
 	}
 	writeResp, err := client.Logical().Write(mount+"/issuers/import/cert", importData)
 	if err != nil {
-		return err
+		return "", err
 	}
 	mapping := writeResp.Data["mapping"].(map[string]interface{})
 	if len(mapping) > 1 {
-		return fmt.Errorf("multiple issuers returned, while expected one, got %v", writeResp)
+		return "", fmt.Errorf("multiple issuers returned, while expected one, got %v", writeResp)
+	}
+	for issuerId := range mapping {
+		issuerUUID = issuerId
 	}
 	if name != "" && name != "default" {
-		issuerUUID := ""
-		for issuerId := range mapping {
-			issuerUUID = issuerId
-		}
 		nameReq := map[string]interface{}{
 			"issuer_name": name,
 		}
 		ctx := context.Background()
 		_, err = client.Logical().JSONMergePatch(ctx, mount+"/issuer/"+issuerUUID, nameReq)
 		if err != nil {
-			return fmt.Errorf("error naming issuer %v to %v: %v", issuerUUID, name, err)
+			return issuerUUID, fmt.Errorf("error naming issuer %v to %v: %v", issuerUUID, name, err)
 		}
 	}
-	return nil
+	return issuerUUID, nil
+}
+
+type inCaseOfFailure struct {
+	csrGenerated    bool
+	csrSigned       bool
+	certImported    bool
+	certNamed       bool
+	caChainImported bool
+
+	intermediateMount string
+	createdKeyId      string
+	csr               string
+	caChain           string
+	parentMount       string
+	parentIssuer      string
+	certSerialNumber  string
+	certIssuerId      string
+	newName           string
+}
+
+func (state inCaseOfFailure) generateFailureMessage() string {
+	message := "A failure has occurred"
+
+	if state.csrGenerated {
+		message += fmt.Sprintf(" after \n a Certificate Signing Request was successfully generated on mount %v", state.intermediateMount)
+	}
+	if state.csrSigned {
+		message += fmt.Sprintf(" and after \n that Certificate Signing Request was successfully signed by mount %v", state.parentMount)
+	}
+	if state.certImported {
+		message += fmt.Sprintf(" and after \n the signed certificate was reimported into mount %v , with issuerID %v", state.intermediateMount, state.certIssuerId)
+	}
+
+	if state.csrGenerated {
+		message += "\n\nTO CONTINUE: \n" + state.toContinue()
+	}
+	if state.csrGenerated && !state.certImported {
+		message += "\n\nTO ABORT: \n" + state.toAbort()
+	}
+
+	message += "\n"
+
+	return message
+}
+
+func (state inCaseOfFailure) toContinue() string {
+	message := ""
+	if !state.csrSigned {
+		message += fmt.Sprintf("You can continue to work with this Certificate Signing Request CSR PEM, by saving"+
+			" it as `pki_int.csr`: %v \n Then call `vault write %v/sign-intermediate csr=@pki_int.csr ...` adding the "+
+			"same key-value arguements as to `pki issue` (except key_type and issuer_name) to generate the certificate "+
+			"and ca_chain", state.csr, state.parentIssuer)
+	}
+	if !state.certImported {
+		if state.caChain != "" {
+			message += fmt.Sprintf("The certificate chain, signed by %v, for this new certificate is: %v", state.parentIssuer, state.caChain)
+		}
+		message += fmt.Sprintf("You can continue to work with this Certificate (and chain) by saving it as "+
+			"chain.pem and importing it as `vault write %v/issuers/import/cert pem_bundle=@chain.pem`",
+			state.intermediateMount)
+	}
+	if !state.certNamed {
+		issuerId := state.certIssuerId
+		if issuerId == "" {
+			message += fmt.Sprintf("The issuer_id is returned as the key in a key_value map from importing the " +
+				"certificate chain.")
+			issuerId = "<issuer-uuid>"
+		}
+		message += fmt.Sprintf("You can name the newly imported issuer by calling `vault patch %v/issuer/%v "+
+			"issuer_name=%v`", state.intermediateMount, issuerId, state.newName)
+	}
+	return message
+}
+
+func (state inCaseOfFailure) toAbort() string {
+	if !state.csrGenerated || (!state.csrSigned && state.createdKeyId == "") {
+		return "No state was created by running this command.  Try rerunning this command after resolving the error."
+	}
+	message := ""
+	if state.csrGenerated && state.createdKeyId != "" {
+		message += fmt.Sprintf(" A key, with key ID %v was created on mount %v as part of this command."+
+			"  If you do not with to use this key and corresponding CSR/cert, you can delete that information by calling"+
+			" `vault delete %v/key/%v`", state.createdKeyId, state.intermediateMount, state.intermediateMount, state.createdKeyId)
+	}
+	if state.csrSigned {
+		message += fmt.Sprintf("A certificate with serial number %v was signed by mount %v as part of this command."+
+			" If you do not want to use this certificate, consider revoking it by calling `vault write %v/revoke/%v`",
+			state.certSerialNumber, state.parentMount, state.parentMount, state.certSerialNumber)
+	}
+	//if state.certImported {
+	//	message += fmt.Sprintf("An issuer with UUID %v was created on mount %v as part of this command.  " +
+	//		"If you do not wish to use this issuer, consider deleting it by calling `vault delete %v/issuer/%v`",
+	//		state.certIssuerId, state.intermediateMount, state.intermediateMount, state.certIssuerId)
+	//}
+
+	return message
 }
