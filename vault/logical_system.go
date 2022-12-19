4be6472c5981921e701aad97399639c0e5571670
akshya96 <87045294+akshya96@users.noreply.github.com>
2022-12-19 14:24:42 -0800

    Prevent Brute Forcing: Create api endpoint to unlock users (#18279)
    * code changes for unlock
    
    * add test
    
    * adding sys help
    
    * adding sys help
    
    * updating unlock user function
    
    * edit test
    
    * add changelog
    
    * syshelp
    
    * adding open api response definition
    
    * removing response fields
    
    * change path name

--- vault/logical_system.go
+++ vault/logical_system.go
@@ -176,6 +176,7 @@ func NewSystemBackend(core *Core, logger log.Logger) *SystemBackend {
 	b.Backend.Paths = append(b.Backend.Paths, b.auditPaths()...)
 	b.Backend.Paths = append(b.Backend.Paths, b.mountPaths()...)
 	b.Backend.Paths = append(b.Backend.Paths, b.authPaths()...)
+	b.Backend.Paths = append(b.Backend.Paths, b.lockedUserPaths()...)
 	b.Backend.Paths = append(b.Backend.Paths, b.leasePaths()...)
 	b.Backend.Paths = append(b.Backend.Paths, b.policyPaths()...)
 	b.Backend.Paths = append(b.Backend.Paths, b.wrappingPaths()...)
@@ -2202,6 +2203,57 @@ func (b *SystemBackend) handleTuneWriteCommon(ctx context.Context, path string, 
 	}
 
 	return resp, nil
+}
+
+// handleUnlockUser is used to unlock user with given mount_accessor and alias_identifier if locked
+func (b *SystemBackend) handleUnlockUser(ctx context.Context, req *logical.Request, data *framework.FieldData) (*logical.Response, error) {
+	mountAccessor := data.Get("mount_accessor").(string)
+	if mountAccessor == "" {
+		return logical.ErrorResponse(
+				"missing mount_accessor"),
+			logical.ErrInvalidRequest
+	}
+
+	aliasName := data.Get("alias_identifier").(string)
+	if aliasName == "" {
+		return logical.ErrorResponse(
+				"missing alias_identifier"),
+			logical.ErrInvalidRequest
+	}
+
+	if err := unlockUser(ctx, b.Core, mountAccessor, aliasName); err != nil {
+		b.Backend.Logger().Error("unlock user failed", "mount accessor", mountAccessor, "alias identifier", aliasName, "error", err)
+		return handleError(err)
+	}
+
+	return nil, nil
+}
+
+// unlockUser deletes the entry for locked user from storage and userFailedLoginInfo map
+func unlockUser(ctx context.Context, core *Core, mountAccessor string, aliasName string) error {
+	ns, err := namespace.FromContext(ctx)
+	if err != nil {
+		return err
+	}
+
+	lockedUserStoragePath := coreLockedUsersPath + ns.ID + "/" + mountAccessor + "/" + aliasName
+
+	// remove entry for locked user from storage
+	if err := core.barrier.Delete(ctx, lockedUserStoragePath); err != nil {
+		return err
+	}
+
+	loginUserInfoKey := FailedLoginUser{
+		aliasName:     aliasName,
+		mountAccessor: mountAccessor,
+	}
+
+	// remove entry for locked user from userFailedLoginInfo map
+	if err := updateUserFailedLoginInfo(ctx, core, loginUserInfoKey, nil, true); err != nil {
+		return err
+	}
+
+	return nil
 }
 
 // handleLease is use to view the metadata for a given LeaseID
@@ -5305,6 +5357,27 @@ the auth path.`,
 		"Tune backend configuration parameters for this mount.",
 		`Read and write the 'default-lease-ttl' and 'max-lease-ttl' values of
 the mount.`,
+	},
+
+	"unlock_user": {
+		"Unlock the locked user with given mount_accessor and alias_identifier.",
+		`
+This path responds to the following HTTP methods.
+    POST sys/lockedusers/:mount_accessor/unlock/:alias_identifier
+		Unlocks the user with given mount_accessor and alias_identifier
+		if locked.`,
+	},
+
+	"mount_accessor": {
+		"MountAccessor is the identifier of the mount entry to which the user belongs",
+		"",
+	},
+
+	"alias_identifier": {
+		`It is the name of the alias (user). For example, if the alias belongs to userpass backend, 
+	   the name should be a valid username within userpass auth method. If the alias belongs
+	    to an approle auth method, the name should be a valid RoleID`,
+		"",
 	},
 
 	"renew": {
