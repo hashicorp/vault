4f6bb7d9a1108a8447e0df2fb5b8fbe9c0d8ff9d
Daniel Huckins <dhuckins@users.noreply.github.com>
2022-12-19 14:29:39 -0500

    dedup endpoint

--- vault/logical_system_paths.go
+++ vault/logical_system_paths.go
@@ -963,15 +963,12 @@ func (b *SystemBackend) internalPaths() []*framework.Path {
 					Default:     false,
 				},
 			},
-			Callbacks: map[logical.Operation]framework.OperationFunc{
-				logical.ReadOperation:   b.pathInternalOpenAPI,
-				logical.UpdateOperation: b.pathInternalOpenAPI,
-			},
-		},
-		{
-			Pattern: "internal/specs/openapi",
 			Operations: map[logical.Operation]framework.OperationHandler{
 				logical.ReadOperation: &framework.PathOperation{
+					Callback: b.pathInternalOpenAPI,
+					Summary:  "Generate an OpenAPI 3 document of all mounted paths.",
+				},
+				logical.UpdateOperation: &framework.PathOperation{
 					Callback: b.pathInternalOpenAPI,
 					Summary:  "Generate an OpenAPI 3 document of all mounted paths.",
 				},
