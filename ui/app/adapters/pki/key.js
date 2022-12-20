49c5c946a98eb248bbed8ec405c050411136ba5f
clairebontempo@gmail.com <clairebontempo@gmail.com>
2022-12-20 15:12:09 -0700

    update key adapter

--- ui/app/adapters/pki/key.js
+++ ui/app/adapters/pki/key.js
@@ -3,20 +3,7 @@ import { encodePath } from 'vault/utils/path-encoding-helpers';
 export default class PkiKeyAdapter extends ApplicationAdapter {
   namespace = 'v1';
 
-  createRecord(store, type, snapshot) {
-    const { record, adapterOptions } = snapshot;
-    let url = this.getUrl(record.backend);
-    if (adapterOptions.import) {
-      url = url + '/import';
-    } else {
-      url = url + '/generate/' + record.type;
-    }
-    return this.ajax(url, 'POST', { data: this.serialize(snapshot) }).then((resp) => {
-      return resp;
-    });
-  }
-
-  getUrl(backend, id) {
+  _baseUrl(backend, id) {
     const url = `${this.buildURL()}/${encodePath(backend)}`;
     if (id) {
       return url + '/key/' + encodePath(id);
@@ -24,18 +11,31 @@ export default class PkiKeyAdapter extends ApplicationAdapter {
     return url + '/keys';
   }
 
+  createRecord(store, type, snapshot) {
+    const { record, adapterOptions } = snapshot;
+    let url = this._baseUrl(record.backend);
+    if (adapterOptions.import) {
+      url = `${url}/import`;
+    } else {
+      url = `${url}/generate/${record.type}`;
+    }
+    return this.ajax(url, 'POST', { data: this.serialize(snapshot) }).then((resp) => {
+      return resp;
+    });
+  }
+
   query(store, type, query) {
     const { backend } = query;
-    return this.ajax(this.getUrl(backend), 'GET', { data: { list: true } });
+    return this.ajax(this._baseUrl(backend), 'GET', { data: { list: true } });
   }
 
   queryRecord(store, type, query) {
     const { backend, id } = query;
-    return this.ajax(this.getUrl(backend, id), 'GET');
+    return this.ajax(this._baseUrl(backend, id), 'GET');
   }
 
   deleteRecord(store, type, snapshot) {
     const { id, record } = snapshot;
-    return this.ajax(this.getUrl(record.backend, id), 'DELETE');
+    return this.ajax(this._baseUrl(record.backend, id), 'DELETE');
   }
 }
