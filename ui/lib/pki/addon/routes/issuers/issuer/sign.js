09f206aae6bb6c3189e269e95c385ad7ef2808fe
Chelsea Shaw <cshaw@hashicorp.com>
2022-12-21 11:36:45 -0600

    issuer.sign route page title

--- ui/lib/pki/addon/routes/issuers/issuer/sign.js
+++ ui/lib/pki/addon/routes/issuers/issuer/sign.js
@@ -1,3 +1,9 @@
-import Route from '@ember/routing/route';
+import PkiIssuerIndexRoute from './index';
 
-export default class PkiIssuerSignRoute extends Route {}
+export default class PkiIssuerSignRoute extends PkiIssuerIndexRoute {
+  setupController(controller, resolvedModel) {
+    super.setupController(controller, resolvedModel);
+    controller.breadcrumbs.push({ label: resolvedModel.id, route: 'issuers.issuer.details' });
+    controller.breadcrumbs.push({ label: 'sign intermediate' });
+  }
+}
