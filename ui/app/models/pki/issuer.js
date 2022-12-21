2267b042dba5ebe278867662ea059c4bda17356d
Chelsea Shaw <82459713+hashishaw@users.noreply.github.com>
2022-12-21 10:30:24 -0600

    UI: PKI Issuer details (#18495)

--- ui/app/models/pki/issuer.js
+++ ui/app/models/pki/issuer.js
@@ -1,51 +1,67 @@
-import Model, { attr } from '@ember-data/model';
-import { expandAttributeMeta } from 'vault/utils/field-to-attrs';
-import { withModelValidations } from 'vault/decorators/model-validations';
+import PkiCertificateBaseModel from './certificate/base';
+import { attr } from '@ember-data/model';
+import { withFormFields } from 'vault/decorators/model-form-fields';
+import lazyCapabilities, { apiPath } from 'vault/macros/lazy-capabilities';
 
-const validations = {
-  name: [
-    { type: 'presence', message: 'Name is required.' },
-    {
-      type: 'containsWhiteSpace',
-      message: 'Name cannot contain whitespace.',
-    },
-  ],
-};
-
-@withModelValidations(validations)
-export default class PkiIssuerModel extends Model {
-  @attr('string', { readOnly: true }) backend;
-  @attr('string', {
-    label: 'Issuer name',
-    fieldValue: 'id',
-  })
-  name;
-
-  get useOpenAPI() {
-    return true;
-  }
+@withFormFields(null, [
+  {
+    default: [
+      'certificate',
+      'caChain',
+      'commonName',
+      'issuerName',
+      'notValidBefore',
+      'serialNumber',
+      'keyId',
+      'uriSans',
+      'notValidAfter',
+    ],
+  },
+  { 'Issuer URLs': ['issuingCertificates', 'crlDistributionPoints', 'ocspServers', 'deltaCrlUrls'] },
+])
+export default class PkiIssuerModel extends PkiCertificateBaseModel {
   getHelpUrl(backend) {
     return `/v1/${backend}/issuer/example?help=1`;
   }
 
-  @attr('boolean') isDefault;
-  @attr('string') issuerName;
+  @attr('string') issuerId;
+  @attr('string', { displayType: 'masked' }) certificate;
+  @attr('string', { displayType: 'masked', label: 'CA Chain' }) caChain;
+  @attr('date', {
+    label: 'Issue date',
+  })
+  notValidBefore;
 
-  // Form Fields not hidden in toggle options
-  _attributeMeta = null;
-  get formFields() {
-    if (!this._attributeMeta) {
-      this._attributeMeta = expandAttributeMeta(this, [
-        'name',
-        'leafNotAfterBehavior',
-        'usage',
-        'manualChain',
-        'issuingCertifications',
-        'crlDistributionPoints',
-        'ocspServers',
-        'deltaCrlUrls', // new endpoint, mentioned in RFC, but need to confirm it's there.
-      ]);
-    }
-    return this._attributeMeta;
+  @attr('string', {
+    label: 'Default key ID',
+  })
+  keyId;
+
+  @attr({
+    label: 'Subject Alternative Names',
+  })
+  uriSans;
+
+  @lazyCapabilities(apiPath`${'backend'}/issuer/${'issuerId'}`) issuerPath;
+  @lazyCapabilities(apiPath`${'backend'}/root/rotate/exported`) rotateExported;
+  @lazyCapabilities(apiPath`${'backend'}/root/rotate/internal`) rotateInternal;
+  @lazyCapabilities(apiPath`${'backend'}/root/rotate/existing`) rotateExisting;
+  @lazyCapabilities(apiPath`${'backend'}/intermediate/cross-sign`) crossSignPath;
+  @lazyCapabilities(apiPath`${'backend'}/issuer/${'issuerId'}/sign-intermediate`) signIntermediate;
+  get canRotateIssuer() {
+    return (
+      this.rotateExported.get('canUpdate') !== false ||
+      this.rotateExisting.get('canUpdate') !== false ||
+      this.rotateInternal.get('canUpdate') !== false
+    );
+  }
+  get canCrossSign() {
+    return this.crossSignPath.get('canUpdate') !== false;
+  }
+  get canSignIntermediate() {
+    return this.signIntermediate.get('canUpdate') !== false;
+  }
+  get canConfigure() {
+    return this.issuerPath.get('canUpdate') !== false;
   }
 }
