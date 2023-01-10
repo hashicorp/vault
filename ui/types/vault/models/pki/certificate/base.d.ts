05b4a6d48f4af58bb5e83023c72cb2364d51235d
clairebontempo@gmail.com <clairebontempo@gmail.com>
2023-01-09 16:20:20 -0800

    add declaration for base cert

--- ui/types/vault/models/pki/certificate/base.d.ts
+++ ui/types/vault/models/pki/certificate/base.d.ts
@@ -1,5 +1,22 @@
 import Model from '@ember-data/model';
-
-export default class PkiBaseCertificateModel extends Model {
+export default class PkiCertificateBaseModel extends Model {
+  secretMountPath: class;
+  get useOpenAPI(): boolean;
+  get backend(): string;
+  getHelpUrl(): void;
+  commonName: string;
+  caChain: string;
+  certificate: string;
+  expiration: number;
+  issuingCa: string;
+  privateKey: string;
+  privateKeyType: string;
+  serialNumber: string;
+  notValidAfter: date;
+  notValidBefore: date;
   pemBundle: string;
+  importedIssuers: string[];
+  importedKeys: string[];
+  revokePath: string;
+  get canRevoke(): boolean;
 }
