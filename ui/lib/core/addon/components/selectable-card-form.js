deac4bf26051f921774ffed6494d2c06ca41e989
Kianna Quach <kianna.quach@hashicorp.com>
2023-01-11 10:12:36 -0800

    Update SelectableCardForm component

--- ui/lib/core/addon/components/selectable-card-form.js
+++ ui/lib/core/addon/components/selectable-card-form.js
@@ -1,20 +1,19 @@
 /**
- * @module GetCredentialsCard
- * GetCredentialsCard components are card-like components that display a title, and SearchSelect component that sends you to a credentials route for the selected item.
+ * @module SelectableCardForm
+ * SelectableCardForm components are card-like components that display a title, and SearchSelect component that sends you to a credentials route for the selected item.
  * They are designed to be used in containers that act as flexbox or css grid containers.
  *
  * @example
  * ```js
- * <GetCredentialsCard @title="Get Credentials" @searchLabel="Role to use" @models={{array 'database/roles'}} @type="role" @backend={{model.backend}}/>
+ * <SelectableCardForm @title="Get Credentials" @searchLabel="Role to use" @models={{array 'database/roles'}} @type="role" @backend={{model.backend}}/>
  * ```
  * @param {string} title - The title displays the card title
  * @param {string} searchLabel - The text above the searchSelect component
  * @param {array} models - An array of model types to fetch from the API. Passed through to SearchSelect component
- * @param {string} type - 'role' or 'secret' - determines where the transitionTo goes
- * @param {boolean} [renderInputSearch=false] - If true, renders InputSearch instead of SearchSelect
  * @param {string} [subText] - Text below title
  * @param {string} [placeholder] - Input placeholder text (default for SearchSelect is 'Search', none for InputSearch)
  * @param {string} backend - Passed to SearchSelect query method to fetch dropdown options
+ * @param {string} testName - The name for the data-test attribute
  */
 
 import Component from '@glimmer/component';
@@ -23,32 +22,19 @@ import { action } from '@ember/object';
 import { tracked } from '@glimmer/tracking';
 export default class SelectableCardForm extends Component {
   @service router;
-  @service store;
-  @tracked role = '';
-  @tracked secret = '';
+  @tracked value = '';
 
   get buttonDisabled() {
-    return !this.role && !this.secret;
+    return !this.value;
   }
 
   @action
   transitionToPage() {
-    this.args.navigateToPage(this.role);
+    this.args.navigateToPage(this.value);
   }
 
   @action
-  handleInput(value) {
-    if (this.args.type === 'role') {
-      // if it comes in from the fallback component then the value is a string otherwise it's an array
-      // which currently only happens if type is role.
-      if (Array.isArray(value)) {
-        this.role = value[0];
-      } else {
-        this.role = value;
-      }
-    }
-    if (this.args.type === 'secret') {
-      this.secret = value;
-    }
+  handleInput([value]) {
+    this.value = value;
   }
 }
