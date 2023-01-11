89e8dd0f030cdc9dffde5bab577710031da44260
Anton Averchenkov <anton.averchenkov@hashicorp.com>
2023-01-11 15:47:51 -0500

    handle time fields correctly

--- sdk/helper/testhelpers/response_validation.go
+++ sdk/helper/testhelpers/response_validation.go
@@ -1,6 +1,9 @@
 package testhelpers
 
 import (
+	"encoding/json"
+	"fmt"
+
 	"github.com/hashicorp/vault/sdk/framework"
 	"github.com/hashicorp/vault/sdk/logical"
 )
@@ -11,7 +14,7 @@ import (
 // also ensure that the data map has all schema-required fields and does not
 // have any fields outside of the schema.
 //
-// This function is intended to be used in tests only.
+// This function is inefficient and is intended to be used in tests only.
 func ValidateResponse(schema *framework.Response, response *logical.Response, strict bool) error {
 	if response != nil {
 		return ValidateResponseData(schema, response.Data, strict)
@@ -26,15 +29,30 @@ func ValidateResponse(schema *framework.Response, response *logical.Response, st
 // also ensure that the data map has all schema-required fields and does not
 // have any fields outside of the schema.
 //
-// This function is intended to be used in tests only.
+// This function is inefficient and is intended to be used in tests only.
 func ValidateResponseData(schema *framework.Response, data map[string]interface{}, strict bool) error {
 	// nothing to validate
 	if schema == nil {
 		return nil
 	}
 
+	// Convert to json & back to coerse data entries into the final
+	// output format expected by Validate() and ValidateStrict(). This is
+	// not efficient and is done for testing purposes only.
+	jsonBytes, err := json.Marshal(data)
+	if err != nil {
+		return fmt.Errorf("failed to convert input to json: %w", err)
+	}
+
+	var jsonData map[string]interface{}
+
+	if err := json.Unmarshal(jsonBytes, &jsonData); err != nil {
+		return fmt.Errorf("failed to unmashal data: %w", err)
+	}
+
+	// Validate
 	fd := framework.FieldData{
-		Raw:    data,
+		Raw:    jsonData,
 		Schema: schema.Fields,
 	}
 
