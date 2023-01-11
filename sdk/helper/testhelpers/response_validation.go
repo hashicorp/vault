12a84d13e8fcf57004e7e5cccd590a782b37c331
Anton Averchenkov <anton.averchenkov@hashicorp.com>
2023-01-11 11:32:47 -0500

    comments

--- sdk/helper/testhelpers/response_validation.go
+++ sdk/helper/testhelpers/response_validation.go
@@ -5,11 +5,13 @@ import (
 	"github.com/hashicorp/vault/sdk/logical"
 )
 
-// ValidateLogicalResponse validates whether the given response object conforms
+// ValidateResponse validates whether the given response object conforms
 // to the response schema (schema.Fields). It cycles through the data map and
 // validates conversions in the schema. In "strict" mode, this function will
 // also ensure that the data map has all schema-required fields and does not
 // have any fields outside of the schema.
+//
+// This function is intended to be used in tests only.
 func ValidateResponse(schema *framework.Response, response *logical.Response, strict bool) error {
 	if response != nil {
 		return ValidateResponseData(schema, response.Data, strict)
@@ -18,11 +20,13 @@ func ValidateResponse(schema *framework.Response, response *logical.Response, st
 	return ValidateResponseData(schema, nil, strict)
 }
 
-// ValidateResponse validates whether the given response data map conforms to
-// the response schema (schema.Fields). It cycles through the data map and
+// ValidateResponseData validates whether the given response data map conforms
+// to the response schema (schema.Fields). It cycles through the data map and
 // validates conversions in the schema. In "strict" mode, this function will
 // also ensure that the data map has all schema-required fields and does not
 // have any fields outside of the schema.
+//
+// This function is intended to be used in tests only.
 func ValidateResponseData(schema *framework.Response, data map[string]interface{}, strict bool) error {
 	// nothing to validate
 	if schema == nil {
