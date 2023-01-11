528d6731d9a04a4dcffae2a3861e41b5e48feb68
clairebontempo@gmail.com <clairebontempo@gmail.com>
2023-01-11 11:16:03 -0800

    finish component functionality

--- ui/lib/core/addon/components/object-list-input.js
+++ ui/lib/core/addon/components/object-list-input.js
@@ -17,25 +17,45 @@ import { tracked } from '@glimmer/tracking';
 
 export default class ObjectListInput extends Component {
   @tracked inputList = [];
-  @tracked inputRow = {};
+  @tracked inputKeys;
+  @tracked disableAdd = true;
+
   constructor() {
     super(...arguments);
-    this.args.objectKeys.forEach((e) => (this.inputRow[e.name] = ''));
-    this.inputList.push(this.inputRow);
+    this.inputKeys = this.args.objectKeys.map((e) => e.key);
+    this.inputList = [this.createInputRow(this.inputKeys)];
   }
 
   @action
   handleInput(idx, { target }) {
     const inputObj = this.inputList.objectAt(idx);
     inputObj[target.name] = target.value;
+
+    const lastObject = this.inputList[this.inputList.length - 1];
+    this.disableAdd = Object.values(lastObject).any((input) => input === '') ? true : false;
+
+    this.args.onChange(this.inputList);
   }
 
   @action
-  addInput() {
-    // handle this
+  addRow() {
+    const newRow = this.createInputRow(this.inputKeys);
+    this.inputList.pushObject(newRow);
+    this.disableAdd = true;
+
+    this.args.onChange(this.inputList);
   }
+
   @action
-  removeInput() {
-    // handle this
+  removeRow(idx) {
+    const row = this.inputList.objectAt(idx);
+    this.inputList.removeObject(row);
+
+    this.args.onChange(this.inputList);
+  }
+
+  createInputRow(keys) {
+    // creates a new object from array of keys, giving each key an empty value
+    return Object.fromEntries(keys.map((key) => [key, '']));
   }
 }
