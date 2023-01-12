5753bee4d307cc774307154d5742b2463d946001
Tom Proctor <tomhjp@users.noreply.github.com>
2023-01-12 17:02:58 +0000

    Fix config test

--- http/sys_config_state_test.go
+++ http/sys_config_state_test.go
@@ -38,6 +38,7 @@ func TestSysConfigState_Sanitized(t *testing.T) {
 		"disable_performance_standby":         false,
 		"disable_printable_check":             false,
 		"disable_sealwrap":                    false,
+		"experiments":                         nil,
 		"raw_storage_endpoint":                false,
 		"disable_sentinel_trace":              false,
 		"enable_ui":                           false,
