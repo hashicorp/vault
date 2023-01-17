6e37940c218acda693d5a975b35cd7000ef51e2b
Jordan Reimer <zofskeez@gmail.com>
2023-01-17 11:33:34 -0700

    adds pki certificate details page component

---
+++ ui/app/serializers/pki/certificate/base.js
@@ -0,0 +1,30 @@
+import { parseCertificate } from 'vault/helpers/parse-pki-cert';
+import ApplicationSerializer from '../../application';
+
+export default class PkiCertificateBaseSerializer extends ApplicationSerializer {
+  primaryKey = 'serial_number';
+
+  attrs = {
+    role: { serialize: false },
+  };
+
+  normalizeResponse(store, primaryModelClass, payload, id, requestType) {
+    if (payload.data.certificate) {
+      // Parse certificate back from the API and add to payload
+      const parsedCert = parseCertificate(payload.data.certificate);
+      // convert issueDate to same format as other date values
+      if (parsedCert.issue_date) {
+        parsedCert.issue_date = parsedCert.issue_date.valueOf();
+      }
+      const json = super.normalizeResponse(
+        store,
+        primaryModelClass,
+        { ...payload, ...parsedCert },
+        id,
+        requestType
+      );
+      return json;
+    }
+    return super.normalizeResponse(...arguments);
+  }
+}
--- ui/lib/pki/addon/routes/certificates/index.js
+++ ui/lib/pki/addon/routes/certificates/index.js
@@ -4,18 +4,12 @@ import { inject as service } from '@ember/service';
 export default class PkiCertificatesIndexRoute extends Route {
   @service store;
   @service secretMountPath;
-  @service pathHelp;
-
-  beforeModel() {
-    // Must call this promise before the model hook otherwise it doesn't add OpenApi to record.
-    return this.pathHelp.getNewModel('pki/certificate', this.secretMountPath.currentPath);
-  }
 
   model() {
     return this.store
-      .query('pki/certificate', { backend: this.secretMountPath.currentPath })
-      .then((certificateModel) => {
-        return { certificateModel, parentModel: this.modelFor('certificates') };
+      .query('pki/certificate/base', { backend: this.secretMountPath.currentPath })
+      .then((certificates) => {
+        return { certificates, parentModel: this.modelFor('certificates') };
       })
       .catch((err) => {
         if (err.httpStatus === 404) {
