86d356e40473e42cfa2a101af703a73442d62a99
Hamid Ghaf <83242695+hghaf099@users.noreply.github.com>
2023-01-20 10:25:14 -0500

    enos: default undo-logs to cluster behavior (#18771)
    * enos: default undo-logs to cluster behavior
    
    * change a step dependency
    
    * rearrange steps, wait a bit longer for undo logs

--- enos/modules/vault_verify_undo_logs/scripts/smoke-verify-undo-logs.sh
+++ enos/modules/vault_verify_undo_logs/scripts/smoke-verify-undo-logs.sh
@@ -1,19 +1,18 @@
 #!/bin/bash
 
-undo_logs_status="${VAULT_UNDO_LOGS_STATUS}"
-
 function fail() {
 	echo "$1" 1>&2
 	exit 1
 }
 
 count=0
-retries=7
+retries=20
 while :; do
-    state=$(curl --header "X-Vault-Token: $VAULT_TOKEN" "$VAULT_ADDR/v1/sys/metrics"  | jq -r '.Gauges[] | select(.Name == "vault.core.replication.write_undo_logs")')
+    leader_address=$(curl -H "X-Vault-Request: true" -H "X-Vault-Token: $VAULT_TOKEN" "$VAULT_ADDR/v1/sys/leader" | jq '.leader_address' | sed 's/\"//g')
+    state=$(curl --header "X-Vault-Token: $VAULT_TOKEN" "$leader_address/v1/sys/metrics"  | jq -r '.Gauges[] | select(.Name == "vault.core.replication.write_undo_logs")')
     target_undo_logs_status="$(jq -r '.Value' <<< "$state")"
 
-    if [ "$undo_logs_status" = "$target_undo_logs_status" ]; then
+    if [ "$target_undo_logs_status" == "1" ]; then
         exit 0
     fi
 
