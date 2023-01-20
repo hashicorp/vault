7a92b621d88cefa949216d424aa3e3af51f05bc0
Kianna <30884335+kiannaquach@users.noreply.github.com>
2023-01-20 12:02:13 -0800

    UI: VAULT-12357 VAULT-12356 Add PKI Beta tag and Modal (#18761)
    * Initial beta modal set up
    
    * Add back to old pki from engine
    
    * Remove commented out code
    
    * Add stylesheet and tests!
    
    * Address feedback!
    
    * Add id to input

--- ui/app/app.js
+++ ui/app/app.js
@@ -73,6 +73,7 @@ export default class App extends Application {
         ],
         externalRoutes: {
           secrets: 'vault.cluster.secrets.backends',
+          secretsListRoot: 'vault.cluster.secrets.backend.list-root',
         },
       },
     },
