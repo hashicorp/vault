3ded1388cbd838b5a1253af40808c823d6d4336d
Kianna <30884335+kiannaquach@users.noreply.github.com>
2023-01-23 12:59:46 -0800

    VAULT-12355 return to old pki from configuration (#18791)

--- ui/lib/pki/addon/engine.js
+++ ui/lib/pki/addon/engine.js
@@ -23,7 +23,7 @@ export default class PkiEngine extends Engine {
       'version',
       'wizard',
     ],
-    externalRoutes: ['secrets', 'secretsListRoot'],
+    externalRoutes: ['secrets', 'secretsListRoot', 'secretsListRootConfiguration'],
   };
 }
 
