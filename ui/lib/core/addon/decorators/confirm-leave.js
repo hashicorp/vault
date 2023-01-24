521413797fe9b96f0dc47ed8e6ccd6455156d9b7
Chelsea Shaw <cshaw@hashicorp.com>
2023-01-24 09:41:02 -0600

    Allow confirm leave to do silent rollbacks on secondary models

--- ui/lib/core/addon/decorators/confirm-leave.js
+++ ui/lib/core/addon/decorators/confirm-leave.js
@@ -1,14 +1,41 @@
-import { action } from '@ember/object';
 import Route from '@ember/routing/route';
 import { inject as service } from '@ember/service';
 import Ember from 'ember';
 
 /**
- * Confirm that the user wants to discard unsaved changes before leaving the page.
- * This decorator hooks into the willTransition action. If you override setupController,
- * be sure to set 'model' on the controller to store data or this won't work.
+ * Confirm that the user wants to discard unsaved changes before leaving the page. This decorator hooks into
+ * the willTransition action. If you override setupController, be sure to set 'model' on the controller to
+ * store data or this won't work.
+ *
+ * By default it will check if the route's model is dirty and prompt when leaving. Usage for this is simple:
+ *
+ * @withConfirmLeave()
+ * export default class MyRoute extends Route {
+ *   @service store;
+ *   model() {
+ *     return this.store.createRecord('some-model')
+ *   }
+ * }
+ *
+ * If the route has ember-data models at multiple paths, you can pass an array of secondary modelPaths which
+ * will rollback on exit after the prompt for the first model is confirmed. In the example below, the window
+ * will only prompt on leave if `model.main` is dirty. Either way, `model.secondary` and `model.optional`
+ * will be cleaned up from the data store.
+ *
+ * @withConfirmLeave('model.main', ['model.secondary', 'model.optional'])
+ * export default class MyRoute extends Route {
+ *   @service store;
+ *   model() {
+ *     return {
+ *       main: this.store.peekRecord('some-model', 'abc1')
+ *       secondary: this.store.createRecord('some-other')
+ *       optional: this.store.createRecord('optional')
+ *     }
+ *   }
+ * }
+ *
  */
-export function withConfirmLeave() {
+export function withConfirmLeave(modelPath = 'model', silentCleanupPaths) {
   return function decorator(SuperClass) {
     if (!Object.prototype.isPrototypeOf.call(Route, SuperClass)) {
       // eslint-disable-next-line
@@ -20,15 +47,30 @@ export function withConfirmLeave() {
     return class ConfirmLeave extends SuperClass {
       @service store;
 
-      @action
+      _rollbackModel(modelPath) {
+        const model = this.controller.get(modelPath);
+        if (!model || !model.hasDirtyAttributes || model.isSaving) {
+          // we only want to rollback if the model is dirty and not saving
+          return;
+        }
+        const method = model.isNew ? 'unloadRecord' : 'rollbackAttributes';
+        model[method]();
+        return;
+      }
+
       willTransition(transition) {
         try {
           super.willTransition(...arguments);
         } catch (e) {
           // if the SuperClass doesn't have willTransition
-          // defined it will throw an error.
+          // defined calling it will throw an error.
         }
-        const model = this.controller.get('model');
+        const model = this.controller.get(modelPath);
+
+        // if the record is saving, we don't need to rollback
+        if (!model || !model.unloadRecord || model.isSaving) {
+          return;
+        }
         if (model && model.hasDirtyAttributes) {
           if (
             Ember.testing ||
@@ -36,17 +78,21 @@ export function withConfirmLeave() {
               'You have unsaved changes. Navigating away will discard these changes. Are you sure you want to discard your changes?'
             )
           ) {
-            // error is thrown when you attempt to unload a record that is inFlight (isSaving)
-            if (!model || !model.unloadRecord || model.isSaving) {
-              return;
-            }
-            model.rollbackAttributes();
-            model.destroy();
+            const method = model.isNew ? 'unloadRecord' : 'rollbackAttributes';
+            model[method]();
+            silentCleanupPaths?.forEach((pathToModel) => {
+              this._rollbackModel(pathToModel);
+            });
             return true;
           } else {
             transition.abort();
             return false;
           }
+        } else {
+          silentCleanupPaths?.forEach((pathToModel) => {
+            this._rollbackModel(pathToModel);
+          });
+          return true;
         }
       }
     };
