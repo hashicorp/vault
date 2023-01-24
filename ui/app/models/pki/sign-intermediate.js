8e63ed567d53e30b7ed095f0d51e8d1412716fbb
Chelsea Shaw <cshaw@hashicorp.com>
2023-01-24 16:01:34 -0600

    Update sign-intermediate-form with new values

--- ui/app/models/pki/sign-intermediate.js
+++ ui/app/models/pki/sign-intermediate.js
@@ -1,33 +1,35 @@
 import { attr } from '@ember-data/model';
 import { withFormFields } from 'vault/decorators/model-form-fields';
 import PkiCertificateBaseModel from './certificate/base';
-@withFormFields(null, [
-  {
-    default: [
-      'csr',
-      'useCsrValues',
-      'commonName',
-      'customTtl',
-      'notBeforeDuration',
-      'format',
-      'permittedDnsDomains',
-      'maxPathLength',
-    ],
-  },
-  {
-    'Key parameters': ['keyId', 'skid'],
-  },
-  {
-    'Subject Alternative Name (SAN) Options': ['altNames', 'ipSans', 'uriSans', 'otherSans'],
-  },
-  {
-    'Other subject data': [],
-  },
+
+@withFormFields([
+  'csr',
+  'useCsrValues',
+  'commonName',
+  'customTtl',
+  'notBeforeDuration',
+  'format',
+  'permittedDnsDomains',
+  'maxPathLength',
 ])
 export default class PkiSignIntermediateModel extends PkiCertificateBaseModel {
   getHelpUrl(backend) {
     return `/v1/${backend}/issuer/example/sign-intermediate?help=1`;
   }
+
+  @attr({
+    label: 'CSR',
+    editType: 'textarea',
+    subText: 'The PEM-encoded CSR to be signed.',
+  })
+  csr;
+
+  @attr({
+    label: 'Use CSR values',
+    subText:
+      'Subject information and key usages specified in the CSR will be used over parameters provided here, and extensions in the CSR will be copied into the issued certificate. Learn more here.',
+  })
+  useCsrValues;
 
   @attr({
     label: 'Not valid after',
@@ -53,4 +55,36 @@ export default class PkiSignIntermediateModel extends PkiCertificateBaseModel {
     hideFormWhen: ['useCsrValues', true],
   })
   commonName;
+
+  @attr({
+    subText:
+      'A comma separated string (or, string array) containing DNS domains for which certificates are allowed to be issued or signed by this CA certificate.',
+  })
+  permittedDnsDomains;
+
+  @attr({
+    subText: 'Specifies the maximum path length to encode in the generated certificate. -1 means no limit',
+    defaultValue: '-1',
+  })
+  maxPathLength;
+
+  /* Signing Options overrides */
+  @attr({
+    label: 'Use PSS',
+    subText:
+      'If checked, PSS signatures will be used over PKCS#1v1.5 signatures when a RSA-type issuer is used. Ignored for ECDSA/Ed25519 issuers.',
+  })
+  usePss;
+
+  @attr({
+    label: 'Subject Key Identifier (SKID)',
+    subText:
+      'Value for the subject key identifier, specified as a string in hex format. If this is empty, Vault will automatically calculate the SKID. ',
+  })
+  skid;
+
+  @attr({
+    possibleValues: ['0', '256', '384', '512'],
+  })
+  signatureBits;
 }
--- ui/lib/pki/addon/components/pki-sign-intermediate-form.js
+++ ui/lib/pki/addon/components/pki-sign-intermediate-form.js
@@ -8,4 +8,20 @@ export default class PkiSignIntermediateFormComponent extends Component {
   @action save() {
     // TODO
   }
+
+  get groups() {
+    return {
+      'Signing options': ['usePss', 'skid', 'signatureBits'],
+      'Subject Alternative Name (SAN) Options': ['altNames', 'ipSans', 'uriSans', 'otherSans'],
+      'Additional subject fields': [
+        'ou',
+        'organization',
+        'country',
+        'locality',
+        'province',
+        'streetAddress',
+        'postalCode',
+      ],
+    };
+  }
 }
