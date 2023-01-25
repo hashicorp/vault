f16bd59d10656aba9957569c9b4252c2cd1f272e
clairebontempo@gmail.com <clairebontempo@gmail.com>
2023-01-24 17:11:45 -0800

    add issuer id to cross-sign breadcrumbs

--- ui/lib/pki/addon/routes/issuers/issuer/cross-sign.js
+++ ui/lib/pki/addon/routes/issuers/issuer/cross-sign.js
@@ -6,6 +6,9 @@ import PkiIssuerIndexRoute from './index';
 export default class PkiIssuerCrossSignRoute extends PkiIssuerIndexRoute {
   setupController(controller, resolvedModel) {
     super.setupController(controller, resolvedModel);
-    controller.breadcrumbs.push({ label: 'cross-sign' });
+    controller.breadcrumbs.push(
+      { label: resolvedModel.id, route: 'issuers.issuer.details' },
+      { label: 'cross-sign' }
+    );
   }
 }
