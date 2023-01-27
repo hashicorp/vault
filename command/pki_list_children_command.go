0a29003dd05f6dc9a963241dfb193338b2147719
kitography <khaines@mit.edu>
2023-01-27 11:28:03 -0500

    Remove bad merge-thing.

--- command/pki_list_children_command.go
+++ command/pki_list_children_command.go
@@ -37,18 +37,11 @@ func (c *PKIListChildrenCommand) Help() string {
 	helpText := `
 Usage: vault pki list-intermediates PARENT [CHILD] [CHILD] [CHILD] ...
 PARENT is the certificate that might be the issuer that everything should be verified against.
-CHILD is a list of paths to certificates to be compared to the PARENT, or pki mounts to look for certificates on.  
-<<<<<<< HEAD
-If CHILD is omitted entirely, w list will be constructed from all accessible pki mounts.
-This returns a list of issuing certificates, and whether they are a match. 
-By default, the type of match required is whether the PARENT has the expected subject, key_id, and could have (directly)
-signed this issuer.  This can be updated by changed the corresponding flag.
-=======
+CHILD is a list of paths to certificates to be compared to the PARENT, or pki mounts to look for certificates on.
 If CHILD is omitted entirely, the list will be constructed from all accessible pki mounts.
 This returns a list of issuing certificates, and whether they are a match. 
 By default, the type of match required is whether the PARENT has the expected subject, key_id, and could have (directly)
 signed this issuer.  The match criteria can be updated by changed the corresponding flag.
->>>>>>> main
 `
 	return strings.TrimSpace(helpText)
 }
