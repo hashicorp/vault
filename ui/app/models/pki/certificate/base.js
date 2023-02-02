1c2dc6a4fdc253505f50a4fadff4690468e24e39
clairebontempo@gmail.com <clairebontempo@gmail.com>
2023-02-01 16:59:56 -0800

    rename model attrs

--- ui/app/models/pki/certificate/base.js
+++ ui/app/models/pki/certificate/base.js
@@ -49,9 +49,9 @@ export default class PkiCertificateBaseModel extends Model {
   // Parsed from cert in serializer
   @attr('number', { formatDate: true }) notValidAfter;
   @attr('number', { formatDate: true }) notValidBefore;
-  @attr('string', { label: 'Subject Alternative Names' }) uriSans;
+  @attr('string', { label: 'URI Subject Alternative Names' }) uriSans;
   @attr('string', { label: 'IP Subject Alternative Names' }) ipSans;
-  @attr('string') altNames;
+  @attr('string', { label: 'Subject Alternative Names' }) altNames;
   @attr('string') signatureBits;
 
   // For importing
