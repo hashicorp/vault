748b1e2fb04e8be08234c75d6bf0b46fd5a5b108
Angel Garbarino <argarbarino@gmail.com>
2023-02-07 11:35:02 -0700

    clean up

--- ui/lib/core/addon/components/alert-banner.js
+++ ui/lib/core/addon/components/alert-banner.js
@@ -11,16 +11,15 @@ import { messageTypes } from 'core/helpers/message-types';
  * ```
  *
  * @param {String} type=null  - The banner type. This comes from the message-types helper.
- * @param {String} [bannerType=alert] - Defaults to 'alert', can be used to specify an alert banner's test selector
- * @param {boolean} [marginless=false] - xx
+ * @param {String} [bannerType=alert] - Defaults to 'alert', can be used to specify an alert banner's test selector.
+ * @param {Boolean} [marginless=false] - If true changes the container class to margin: 0.
  * @param {String} [message=null] - The message to display within the banner.
  * @param {Object} [progressBar=null] - An object containing a value and maximum for a progress bar. Will be displayed next to the message title.
- * @param {boolean} [showLoading=false] - Showing a loading icon to the right of the title.
+ * @param {Boolean} [showLoading=false] - Shows a loading icon to the right of the title.
  * @param {String} [title=null] - A title to show above the message. If this is not provided, there are default values for each type of alert.
- * @param {boolean} [yieldWithoutColumn=false] - If true, do not show message or title, just yield with no formatting.
- * @param {string} [learnLink=null] -display a DocLink pointing to this path.
- * @param {string} [learnLinkMessage=null] -show a learn link message to the right of a link icon, displayed below the main message.
- *
+ * @param {Boolean} [yieldWithoutColumn=false] - If true, do not show message or title, just yield with no formatting.
+ * @param {String} [learnLink=null] - Display a DocLink pointing to this path.
+ * @param {String} [learnLinkMessage=null] - Show a learn link message to the right of a learn-link icon. Displays below the main message.
  */
 
 export default class AlertBanner extends Component {
