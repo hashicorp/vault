14aefe9bc3521380a985acd54e87ba2457fbe19e
Chelsea Shaw <cshaw@hashicorp.com>
2023-02-07 15:31:19 -0600

    Fix confirmLeave not triggering

--- ui/lib/pki/addon/routes/issuers/issuer/edit.js
+++ ui/lib/pki/addon/routes/issuers/issuer/edit.js
@@ -1,5 +1,33 @@
+import Route from '@ember/routing/route';
+import { inject as service } from '@ember/service';
 import { withConfirmLeave } from 'core/decorators/confirm-leave';
-import PkiIssuerDetailsRoute from './details';
+@withConfirmLeave()
+export default class PkiIssuersGenerateIntermediateRoute extends Route {
+  @service store;
+  @service secretMountPath;
+  @service pathHelp;
 
-withConfirmLeave();
-export default class PkiIssuerEditRoute extends PkiIssuerDetailsRoute {}
+  beforeModel() {
+    // Must call this promise before the model hook otherwise it doesn't add OpenApi to record.
+    return this.pathHelp.getNewModel('pki/issuer', this.secretMountPath.currentPath);
+  }
+
+  model() {
+    const { issuer_ref } = this.paramsFor('issuers/issuer');
+    return this.store.queryRecord('pki/issuer', {
+      backend: this.secretMountPath.currentPath,
+      id: issuer_ref,
+    });
+  }
+
+  setupController(controller, resolvedModel) {
+    super.setupController(controller, resolvedModel);
+    controller.breadcrumbs = [
+      { label: 'secrets', route: 'secrets', linkExternal: true },
+      { label: this.secretMountPath.currentPath, route: 'overview' },
+      { label: 'issuers', route: 'issuers.index' },
+      { label: resolvedModel.id, route: 'issuers.issuer.details' },
+      { label: 'update' },
+    ];
+  }
+}
