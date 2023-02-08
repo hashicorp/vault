d6176d7bf510bdf6fbb9d7a94836ebd960824c4e
Kianna <30884335+kiannaquach@users.noreply.github.com>
2023-02-08 09:38:30 -0800

    UI: VAULT-13135 Add copyable issuer id row to issuer details (#19054)
    * VAULT-13135 Add copyable issuer id row to issuer details
    
    * Fix failing test

--- ui/app/models/pki/issuer.js
+++ ui/app/models/pki/issuer.js
@@ -13,6 +13,7 @@ const issuerUrls = ['issuingCertificates', 'crlDistributionPoints', 'ocspServers
         'caChain',
         'commonName',
         'issuerName',
+        'issuerId',
         'serialNumber',
         'keyId',
         'uriSans',
