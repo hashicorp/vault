f88973a265284eb4db5ba4f40ff2a9cf80d7ef79
Angel Garbarino <argarbarino@gmail.com>
2023-02-09 14:49:54 -0700

    work in progress: got the expired banner set with license check

--- ui/app/components/license-banners.js
+++ ui/app/components/license-banners.js
@@ -1,6 +1,14 @@
+import Component from '@glimmer/component';
+import { action } from '@ember/object';
+import { tracked } from '@glimmer/tracking';
+import { inject as service } from '@ember/service';
+import isAfter from 'date-fns/isAfter';
+import differenceInDays from 'date-fns/differenceInDays';
+import localStorage from 'vault/lib/local-storage';
+
 /**
  * @module LicenseBanners
- * LicenseBanners components are used to display Vault-specific license expiry messages
+ * LicenseBanners components are used to display Vault-specific license expiry messages.
  *
  * @example
  * ```js
@@ -9,11 +17,27 @@
  * @param {string} expiry - RFC3339 date timestamp
  */
 
-import Component from '@glimmer/component';
-import isAfter from 'date-fns/isAfter';
-import differenceInDays from 'date-fns/differenceInDays';
-
 export default class LicenseBanners extends Component {
+  @service version;
+
+  @tracked currentVersion = this.version.version;
+  @tracked dismissLicenseExpired = false;
+  @tracked localStorageLicenseBannerState = localStorage.getItem('licenseBannerState');
+
+  constructor() {
+    super(...arguments);
+    if (!this.localStorageLicenseBannerState) {
+      localStorage.setItem('licenseBannerState', { dismiss: false, version: this.currentVersion });
+    } else {
+      if (
+        this.localStorageLicenseBannerState.version === this.currentVersion &&
+        this.localStorageLicenseBannerState.dismiss
+      ) {
+        this.dismissLicenseExpired = true;
+      }
+    }
+  }
+
   get licenseExpired() {
     if (!this.args.expiry) return false;
     return isAfter(new Date(), new Date(this.args.expiry));
@@ -23,5 +47,12 @@ export default class LicenseBanners extends Component {
     // Anything more than 30 does not render a warning
     if (!this.args.expiry) return 99;
     return differenceInDays(new Date(this.args.expiry), new Date());
+  }
+
+  @action
+  dismissLicenseExpiredBanner() {
+    const updatedObject = { dismiss: true, version: this.currentVersion };
+    localStorage.setItem('licenseBannerState', updatedObject);
+    this.dismissLicenseExpired = true;
   }
 }
