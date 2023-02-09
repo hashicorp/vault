451712adc51163ef87bdca9cedc68b1fb111b837
hc-github-team-secure-vault-core <82990506+hc-github-team-secure-vault-core@users.noreply.github.com>
2023-02-08 21:44:03 -0500

    backport of commit 65cb09c75f4e60c3f50c21f5053c5a816c9d5312 (#19093)
    Co-authored-by: Jaymala <jaymala@hashicorp.com>

--- enos/modules/k8s_deploy_vault/main.tf
+++ enos/modules/k8s_deploy_vault/main.tf
@@ -27,6 +27,7 @@ locals {
     "server.limits.cpu"             = "200m"
     "server.ha.raft.config"         = file("${abspath(path.module)}/raft-config.hcl")
     "server.dataStorage.size"       = "100m"
+    "server.logLevel"               = var.vault_log_level
   }
   all_helm_chart_settings = var.ent_license == null ? local.helm_chart_settings : merge(local.helm_chart_settings, {
     "server.extraEnvironmentVars.VAULT_LICENSE" = var.ent_license
