511b621c67ecf9d49167cf968023080e450a213e
clairebontempo@gmail.com <clairebontempo@gmail.com>
2023-02-09 18:32:36 -0800

    fix empty arrays removed when serialized

--- ui/app/serializers/pki/role.js
+++ ui/app/serializers/pki/role.js
@@ -4,4 +4,13 @@ export default class PkiRoleSerializer extends ApplicationSerializer {
   attrs = {
     name: { serialize: false },
   };
+
+  serialize() {
+    const json = super.serialize(...arguments);
+    // empty arrays are being removed from serialized json
+    // ensure that they are sent to the server, otherwise removing items will not be persisted
+    json.key_usage = json.key_usage || [];
+    json.ext_key_usage = json.ext_key_usage || [];
+    return json;
+  }
 }
