bf3cb7295f1def0e074e428b37ffdc41922eaf4f
clairebontempo@gmail.com <clairebontempo@gmail.com>
2023-02-09 18:50:00 -0800

    fix comment; g

--- ui/app/serializers/pki/role.js
+++ ui/app/serializers/pki/role.js
@@ -7,10 +7,10 @@ export default class PkiRoleSerializer extends ApplicationSerializer {
 
   serialize() {
     const json = super.serialize(...arguments);
-    // empty arrays are being removed from serialized json
-    // ensure that they are sent to the server, otherwise removing items will not be persisted
+    // attributes with empty arrays are stripped from serialized json
+    // but an empty list is acceptable for key_usage to specify no default constraints
+    // intercepting here to ensure an empty array persists (the backend assumes default values)
     json.key_usage = json.key_usage || [];
-    json.ext_key_usage = json.ext_key_usage || [];
     return json;
   }
 }
