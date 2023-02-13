4a28fc0ac3129b5cf457b618d70595c4beb9014d
Kuba Wieczorek <kuba.wieczorek@hashicorp.com>
2023-02-13 15:45:06 +0000

    Update version to 1.14.0 and version prerelease to beta1

--- version/version_base.go
+++ version/version_base.go
@@ -11,7 +11,7 @@ var (
 	// Whether cgo is enabled or not; set at build time
 	CgoEnabled bool
 
-	Version           = "1.13.0"
-	VersionPrerelease = "dev1"
+	Version           = "1.14.0"
+	VersionPrerelease = "beta1"
 	VersionMetadata   = ""
 )
