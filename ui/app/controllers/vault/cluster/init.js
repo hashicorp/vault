1d272919aff94728648fc5ef78a030e44ff8341a
Chelsea Shaw <82459713+hashishaw@users.noreply.github.com>
2023-02-23 10:17:39 -0600

    Backport of UI: Remove Wizard to 1.12.x (#19220) (#19287)
    * UI: Remove Wizard (#19220)
    
    * Remove UI Wizard temporarily [GH-19000]
    
    * Fix for pre-upgrade

--- ui/app/controllers/vault/cluster/init.js
+++ ui/app/controllers/vault/cluster/init.js
@@ -1,5 +1,4 @@
 import { computed } from '@ember/object';
-import { inject as service } from '@ember/service';
 import Controller from '@ember/controller';
 
 const DEFAULTS = {
@@ -12,8 +11,6 @@ const DEFAULTS = {
 };
 
 export default Controller.extend(DEFAULTS, {
-  wizard: service(),
-
   reset() {
     this.setProperties(DEFAULTS);
   },
@@ -22,8 +19,6 @@ export default Controller.extend(DEFAULTS, {
     this.set('loading', false);
     this.set('keyData', resp);
     this.model.reload();
-    this.wizard.set('initEvent', 'SAVE');
-    this.wizard.transitionTutorialMachine(this.wizard.currentState, 'TOSAVE');
   },
 
   initError(e) {
