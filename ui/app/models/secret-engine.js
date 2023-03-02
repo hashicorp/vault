8f1e3c1cf96c4c2ddf91da9e7f050828c241cecb
clairebontempo@gmail.com <clairebontempo@gmail.com>
2023-03-02 12:51:14 -0800

    update secret engine model

--- ui/app/models/secret-engine.js
+++ ui/app/models/secret-engine.js
@@ -17,68 +17,79 @@ const validations = {
 };
 
 @withModelValidations(validations)
-class SecretEngineModel extends Model {}
-export default SecretEngineModel.extend({
-  path: attr('string'),
-  accessor: attr('string'),
-  name: attr('string'),
-  type: attr('string', {
-    label: 'Secret engine type',
-  }),
-  description: attr('string', {
-    editType: 'textarea',
-  }),
-  // will only have value for kv type
-  version: attr('number', {
+export default class SecretEngineModel extends Model {
+  @belongsTo('mount-config', { async: false, inverse: null }) config;
+  @attr('string') path;
+  @attr('string', { label: 'Secret engine type' }) type;
+  @attr('string', { editType: 'textarea' }) description;
+  @attr('string') accessor;
+  @attr('string') name;
+
+  @attr('number', {
     label: 'Version',
     helpText:
       'The KV Secrets Engine can operate in different modes. Version 1 is the original generic Secrets Engine the allows for storing of static key/value pairs. Version 2 added more features including data versioning, TTLs, and check and set.',
     possibleValues: [2, 1],
     // This shouldn't be defaultValue because if no version comes back from API we should assume it's v1
     defaultFormValue: 2, // Set the form to 2 by default
-  }),
-  config: belongsTo('mount-config', { async: false, inverse: null }),
-  local: attr('boolean', {
+  })
+  version; // will only have value for type=kv
+
+  @attr('boolean', {
     helpText:
       'When Replication is enabled, a local mount will not be replicated across clusters. This can only be specified at mount time.',
-  }),
-  sealWrap: attr('boolean', {
+  })
+  local;
+
+  @attr('boolean', {
     helpText:
       'When enabled - if a seal supporting seal wrapping is specified in the configuration, all critical security parameters (CSPs) in this backend will be seal wrapped. (For K/V mounts, all values will be seal wrapped.) This can only be specified at mount time.',
-  }),
+  })
+  sealWrap;
+
   // KV 2 additional config default options
-  maxVersions: attr('number', {
+  @attr('number', {
     defaultValue: 0,
     label: 'Maximum number of versions',
     subText:
       'The number of versions to keep per key. Once the number of keys exceeds the maximum number set here, the oldest version will be permanently deleted. This value applies to all keys, but a key’s metadata settings can overwrite this value. When 0 is used or the value is unset, Vault will keep 10 versions.',
-  }),
-  casRequired: attr('boolean', {
+  })
+  maxVersions;
+
+  @attr('boolean', {
     defaultValue: false,
     label: 'Require Check and Set',
     subText:
       'If checked, all keys will require the cas parameter to be set on all write requests. A key’s metadata settings can overwrite this value.',
-  }),
-  deleteVersionAfter: attr({
+  })
+  casRequired;
+
+  @attr({
     defaultValue: 0,
     editType: 'ttl',
     label: 'Automate secret deletion',
     helperTextDisabled: 'A secret’s version must be manually deleted.',
     helperTextEnabled: 'Delete all new versions of this secret after',
-  }),
+  })
+  deleteVersionAfter;
 
-  modelTypeForKV: computed('engineType', 'version', function () {
+  // end KV 2 additional config options
+
+  get modelTypeForKV() {
     const type = this.engineType;
     let modelType = 'secret';
     if ((type === 'kv' || type === 'generic') && this.version === 2) {
       modelType = 'secret-v2';
     }
     return modelType;
-  }),
+  }
 
-  isV2KV: equal('modelTypeForKV', 'secret-v2'),
+  get isV2KV() {
+    return this.modelTypeForKV === 'secret-v2';
+  }
 
-  formFields: computed('engineType', 'version', function () {
+  get engineFormFields() {
+    if (!this.engineType) return null;
     const type = this.engineType;
     const fields = ['type', 'path', 'description', 'accessor', 'local', 'sealWrap'];
     // no ttl options for keymgmt
@@ -94,9 +105,10 @@ export default SecretEngineModel.extend({
       fields.push('casRequired', 'deleteVersionAfter', 'maxVersions');
     }
     return fields;
-  }),
+  }
 
-  formFieldGroups: computed('engineType', function () {
+  get engineFormFieldGroups() {
+    if (!this.engineType) return null;
     let defaultFields = ['path'];
     let optionFields;
     const CORE_OPTIONS = ['description', 'config.listingVisibility', 'local', 'sealWrap'];
@@ -147,57 +159,55 @@ export default SecretEngineModel.extend({
         'Method Options': optionFields,
       },
     ];
-  }),
+  }
 
-  attrs: computed('formFields', function () {
-    return expandAttributeMeta(this, this.formFields);
-  }),
+  get attrs() {
+    if (!this.engineType) return null;
+    return expandAttributeMeta(this, this.engineFormFields);
+  }
 
-  fieldGroups: computed('formFieldGroups', function () {
-    return fieldToAttrs(this, this.formFieldGroups);
-  }),
+  get fieldGroups() {
+    if (!this.engineType) return null;
+    return fieldToAttrs(this, this.engineFormFieldGroups);
+  }
 
-  icon: computed('engineType', function () {
-    if (!this.engineType || this.engineType === 'kmip') {
-      return 'secrets';
+  get icon() {
+    if (!this.engineType) return 'secrets';
+    switch (this.engineType) {
+      case 'keymgmt':
+        return 'key';
+      default:
+        return this.engineType;
     }
-    if (this.engineType === 'keymgmt') {
-      return 'key';
-    }
-    return this.engineType;
-  }),
+  }
 
   // namespaces introduced types with a `ns_` prefix for built-in engines
   // so we need to strip that to normalize the type
-  engineType: computed('type', function () {
+  get engineType() {
     return (this.type || '').replace(/^ns_/, '');
-  }),
+  }
 
-  shouldIncludeInList: computed('engineType', function () {
+  get shouldIncludeInList() {
     return !LIST_EXCLUDED_BACKENDS.includes(this.engineType);
-  }),
+  }
 
-  localDisplay: computed('local', function () {
+  get localDisplay() {
     return this.local ? 'local' : 'replicated';
-  }),
+  }
 
-  // ssh specific ones
-  privateKey: attr('string'),
-  publicKey: attr('string'),
-  generateSigningKey: attr('boolean', {
-    defaultValue: true,
-  }),
+  // EDITING SSH CA config
+  @attr('string') privateKey;
+  @attr('string') publicKey;
+  @attr('boolean', { defaultValue: true }) generateSigningKey;
 
   saveCA(options) {
     if (this.type !== 'ssh') {
       return;
     }
     if (options.isDelete) {
-      this.setProperties({
-        privateKey: null,
-        publicKey: null,
-        generateSigningKey: false,
-      });
+      this.privateKey = null;
+      this.publicKey = null;
+      this.generateSigningKey = false;
     }
     return this.save({
       adapterOptions: {
@@ -206,7 +216,7 @@ export default SecretEngineModel.extend({
         attrsToSend: ['privateKey', 'publicKey', 'generateSigningKey'],
       },
     });
-  },
+  }
 
   saveZeroAddressConfig() {
     return this.save({
@@ -214,9 +224,9 @@ export default SecretEngineModel.extend({
         adapterMethod: 'saveZeroAddressConfig',
       },
     });
-  },
+  }
 
-  // aws backend attrs
-  lease: attr('string'),
-  leaseMax: attr('string'),
-});
+  // EDITING AWS config
+  @attr('string') lease;
+  @attr('string') leaseMax;
+}
