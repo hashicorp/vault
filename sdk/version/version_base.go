8b7ae33c6b0a24c8b710f171014a583834139e6c
Hamid Ghaf <83242695+hghaf099@users.noreply.github.com>
2023-03-02 13:27:20 -0800

    update SDK version 1.10.12 (#19437)

--- sdk/version/version_base.go
+++ sdk/version/version_base.go
@@ -8,7 +8,7 @@ var (
 	// Whether cgo is enabled or not; set at build time
 	CgoEnabled bool
 
-	Version           = "1.10.11"
+	Version           = "1.10.12"
 	VersionPrerelease = ""
 	VersionMetadata   = ""
 )
