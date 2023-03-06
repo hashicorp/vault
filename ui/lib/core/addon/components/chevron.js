3ed3e7ce6dc743987868d9f008aba6e7e67150df
Kianna Quach <kianna.quach@hashicorp.com>
2023-03-06 15:45:54 -0800

    Update chevron to be a tracked property

--- ui/lib/core/addon/components/chevron.js
+++ ui/lib/core/addon/components/chevron.js
@@ -1,5 +1,6 @@
 import Component from '@ember/component';
 import { assert } from '@ember/debug';
+import { tracked } from '@glimmer/tracking';
 
 const DIRECTIONS = ['right', 'left', 'up', 'down'];
 
@@ -20,8 +21,9 @@ const DIRECTIONS = ['right', 'left', 'up', 'down'];
 
 export default class Chevron extends Component {
   tagName = '';
-  direction = 'right';
   isButton = false;
+
+  @tracked direction = 'right';
 
   get glyph() {
     const { direction } = this;
