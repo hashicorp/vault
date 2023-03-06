4820a2ddf7b97208038870ec4c70fd421d31df1d
Kianna Quach <kianna.quach@hashicorp.com>
2023-03-06 14:23:47 -0800

    VAULT-14152 Co-locate box-radio template w/ component file

--- ui/lib/core/addon/components/box-radio.js
+++ ui/lib/core/addon/components/box-radio.js
@@ -1,3 +1,5 @@
+import Component from '@glimmer/component';
+
 /**
  * Copyright (c) HashiCorp, Inc.
  * SPDX-License-Identifier: MPL-2.0
@@ -21,13 +23,7 @@
  * @param {string} [tooltipMessage=default] - The message that shows in the tooltip if the radio option is disabled
  */
 
-import Component from '@glimmer/component';
-import layout from '../templates/components/box-radio';
-import { setComponentTemplate } from '@ember/component';
-
-class BoxRadio extends Component {
+export default class BoxRadio extends Component {
   disabled = false;
   tooltipMessage = 'This option is not available to you at this time.';
 }
-
-export default setComponentTemplate(layout, BoxRadio);
