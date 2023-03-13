dbd613392a53b71c18bd23779e04b17bf04a5533
Kianna Quach <kianna.quach@hashicorp.com>
2023-03-13 10:42:11 -0700

    Some cleanup for chevron, empty-state, field-group-show, icon, info-tooltip

--- ui/lib/core/addon/components/info-tooltip.js
+++ ui/lib/core/addon/components/info-tooltip.js
@@ -6,6 +6,19 @@
 import Component from '@glimmer/component';
 import { action } from '@ember/object';
 
+/**
+ * @module JsonEditor
+ *
+ * @example
+ * ```js
+ * <JsonEditor @title="Policy" @value={{codemirror.string}} @valueUpdated={{ action "codemirrorUpdate"}} />
+ * ```
+ *
+ * @param {string} [verticalPosition] - vertical position specification (above, below)
+ * @param {string} [horizontalPosition] - horizontal position specification (center, auto-right)
+
+ */
+
 export default class InfoTooltip extends Component {
   @action
   preventSubmit(e) {
