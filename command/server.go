8a201f08a1c4daf740e58ae32bf5e451e6950ba7
Jason O'Donnell <2160810+jasonodonnell@users.noreply.github.com>
2023-03-13 16:48:12 -0400

    Add sigusr2 pprof file dump support

--- command/server.go
+++ command/server.go
@@ -1641,39 +1641,86 @@ func (c *ServerCommand) Run(args []string) int {
 			logWriter := c.logger.StandardWriter(&hclog.StandardLoggerOptions{})
 			pprof.Lookup("goroutine").WriteTo(logWriter, 2)
 
-			if os.Getenv("VAULT_STACKTRACE_WRITE_TO_FILE") != "" {
-				c.logger.Info("Writing stacktrace to file")
-
-				dir := ""
-				path := os.Getenv("VAULT_STACKTRACE_FILE_PATH")
-				if path != "" {
-					if _, err := os.Stat(path); err != nil {
-						c.logger.Error("Checking stacktrace path failed", "error", err)
-						continue
-					}
-					dir = path
-				} else {
-					dir, err = os.MkdirTemp("", "vault-stacktrace")
-					if err != nil {
-						c.logger.Error("Could not create temporary directory for stacktrace", "error", err)
-						continue
-					}
-				}
-
-				f, err := os.CreateTemp(dir, "stacktrace")
+			if os.Getenv("VAULT_PPROF_SIGUSR2_DUMP") != "" {
+				dir, err := os.MkdirTemp("", "pprof")
 				if err != nil {
-					c.logger.Error("Could not create stacktrace file", "error", err)
+					c.UI.Error(err.Error())
 					continue
 				}
 
-				if err := pprof.Lookup("goroutine").WriteTo(f, 2); err != nil {
-					f.Close()
-					c.logger.Error("Could not write stacktrace to file", "error", err)
+				fGoRoutine, err := os.Create(fmt.Sprintf("%s/goroutines", dir))
+				if err != nil {
+					c.UI.Error(err.Error())
 					continue
 				}
 
-				c.logger.Info(fmt.Sprintf("Wrote stacktrace to: %s", f.Name()))
-				f.Close()
+				err = pprof.Lookup("goroutine").WriteTo(fGoRoutine, 0)
+				if err != nil {
+					c.UI.Error(err.Error())
+					continue
+				}
+
+				fHeap, err := os.Create(fmt.Sprintf("%s/heap", dir))
+				if err != nil {
+					c.UI.Error(err.Error())
+					continue
+				}
+
+				err = pprof.Lookup("heap").WriteTo(fHeap, 0)
+				if err != nil {
+					c.UI.Error(err.Error())
+					continue
+				}
+
+				fAllocs, err := os.Create(fmt.Sprintf("%s/allocs", dir))
+				if err != nil {
+					c.UI.Error(err.Error())
+					continue
+				}
+
+				err = pprof.Lookup("allocs").WriteTo(fAllocs, 0)
+				if err != nil {
+					c.UI.Error(err.Error())
+					continue
+				}
+
+				fMutex, err := os.Create(fmt.Sprintf("%s/mutex", dir))
+				if err != nil {
+					c.UI.Error(err.Error())
+					continue
+				}
+
+				err = pprof.Lookup("mutex").WriteTo(fMutex, 0)
+				if err != nil {
+					c.UI.Error(err.Error())
+					continue
+				}
+
+				fBlock, err := os.Create(fmt.Sprintf("%s/block", dir))
+				if err != nil {
+					c.UI.Error(err.Error())
+					continue
+				}
+
+				err = pprof.Lookup("block").WriteTo(fBlock, 0)
+				if err != nil {
+					c.UI.Error(err.Error())
+					continue
+				}
+
+				fThread, err := os.Create(fmt.Sprintf("%s/threadcreate", dir))
+				if err != nil {
+					c.UI.Error(err.Error())
+					continue
+				}
+
+				err = pprof.Lookup("threadcreate").WriteTo(fThread, 0)
+				if err != nil {
+					c.UI.Error(err.Error())
+					continue
+				}
+
+				c.logger.Info(fmt.Sprintf("Wrote pprof files to: %s", dir))
 			}
 		}
 	}
