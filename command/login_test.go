271c8218b78e7e1844eb088f52becc0a47f26e01
Kuba Wieczorek <kuba.wieczorek@hashicorp.com>
2023-03-16 19:34:03 +0000

    Change the wait time in TestLoginMFASinglePhase to 15

--- command/login_test.go
+++ command/login_test.go
@@ -470,7 +470,7 @@ func TestLoginMFASinglePhase(t *testing.T) {
 	defer closer()
 
 	methodName := "foo"
-	waitPeriod := 10
+	waitPeriod := 15
 	userClient, entityID, methodID := testhelpers.SetupLoginMFATOTP(t, client, methodName, waitPeriod)
 	enginePath := testhelpers.RegisterEntityInTOTPEngine(t, client, entityID, methodID)
 
