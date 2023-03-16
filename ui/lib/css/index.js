042d31ba4ff4cc536fd8c8d63630e1f1b65cb636
Angel Garbarino <argarbarino@gmail.com>
2023-03-16 13:06:20 -0600

    remove bulma and get app running

--- ui/lib/css/index.js
+++ ui/lib/css/index.js
@@ -12,6 +12,7 @@ var Funnel = require('broccoli-funnel');
 var mergeTrees = require('broccoli-merge-trees');
 
 module.exports = {
+  // ARG TODO: I don't see this named export being consumed anywhere?
   name: 'bulma',
 
   isDevelopingAddon() {
@@ -26,36 +27,17 @@ module.exports = {
       app = app.app;
     }
 
-    this.bulmaPath = path.dirname(require.resolve('bulma'));
-    this.bulmaSwitchPath = path.dirname(require.resolve('bulma-switch/switch.sass'));
-    this.bulmaCheckPath = path.dirname(require.resolve('cool-checkboxes-for-bulma.io'));
     this.sassSVGURIPath = path.dirname(require.resolve('sass-svg-uri'));
     return app;
   },
 
   treeForStyles: function () {
-    var bulma = new Funnel(this.bulmaPath, {
-      srcDir: '/',
-      destDir: 'app/styles/bulma',
-      annotation: 'Funnel (bulma)',
-    });
-
-    var bulmaSwitch = new Funnel(this.bulmaSwitchPath, {
-      srcDir: '/',
-      destDir: 'app/styles/bulma',
-      annotation: 'Funnel (bulma-switch)',
-    });
-    var bulmaCheck = new Funnel(this.bulmaCheckPath, {
-      srcDir: '/',
-      destDir: 'app/styles/bulma',
-      annotation: 'Funnel (bulma-check)',
-    });
     var sassSVGURI = new Funnel(this.sassSVGURIPath, {
       srcDir: '/',
       destDir: 'app/styles/sass-svg-uri',
       annotation: 'Sass SVG URI',
     });
 
-    return mergeTrees([bulmaCheck, bulmaSwitch, bulma, sassSVGURI], { overwrite: true });
+    return mergeTrees([sassSVGURI], { overwrite: true });
   },
 };
