e0cbc010f6ec2ee636340295b1046b52b5cf4f44
clairebontempo@gmail.com <clairebontempo@gmail.com>
2023-03-16 15:38:47 -0600

    test cleanup

--- ui/tests/acceptance/enterprise-namespaces-test.js
+++ ui/tests/acceptance/enterprise-namespaces-test.js
@@ -3,7 +3,7 @@ import { module, test } from 'qunit';
 import { setupApplicationTest } from 'ember-qunit';
 import { create } from 'ember-cli-page-object';
 import { setupMirage } from 'ember-cli-mirage/test-support';
-
+import parseURL from 'core/utils/parse-url';
 import consoleClass from 'vault/tests/pages/components/console/ui-panel';
 import authPage from 'vault/tests/pages/auth';
 import logout from 'vault/tests/pages/logout';
@@ -97,16 +97,15 @@ module('Acceptance | Enterprise | namespaces', function (hooks) {
       authSubmit: '[data-test-auth-submit]',
     };
     hooks.beforeEach(async function () {
-      this.namespace = 'testns';
+      this.namespace = 'test-ns';
       this.rootOidc = 'root-oidc';
       this.nsOidc = 'ns-oidc';
 
-      const enableOidc = async (path, role = null) => {
+      const enableOidc = async (path, role = '') => {
         this.server.post(`/auth/${path}/config`, () => {});
-        const hasRole = role || '';
         await shell.runCommands([
           `write sys/auth/${path} type=oidc`,
-          `write auth/${path}/config default_role="${hasRole}" oidc_discovery_url="https://example.com"`,
+          `write auth/${path}/config default_role="${role}" oidc_discovery_url="https://example.com"`,
           // show method as tab
           `write sys/auth/${path}/tune listing_visibility="unauth"`,
         ]);
@@ -121,6 +120,10 @@ module('Acceptance | Enterprise | namespaces', function (hooks) {
       // enable oidc in child namespace with default role
       await authPage.loginNs(this.namespace);
       await enableOidc(this.nsOidc, `${this.nsOidc}-role`);
+      await authPage.logout();
+
+      // end by logging in/out of root so query params are cleared out and don't include namespace
+      await authPage.login();
       return await authPage.logout();
     });
 
@@ -138,35 +141,38 @@ module('Acceptance | Enterprise | namespaces', function (hooks) {
 
       await authPage.login();
       await shell.runCommands([`delete /sys/auth/${this.namespace}`]);
+      this.server.shutdown();
     });
 
-    test('auth form updates when a namespace is entered', async function (assert) {
+    test('oidc: request is made to auth_url when a namespace is inputted', async function (assert) {
       assert.expect(5);
       this.server.post(`/auth/${this.rootOidc}/oidc/auth_url`, (schema, req) => {
-        const request = JSON.parse(req.requestBody);
-        assert.deepEqual(
-          request.redirect_uri,
-          `http://localhost:7357/ui/vault/auth/${this.rootOidc}/oidc/callback`,
+        const { redirect_uri } = JSON.parse(req.requestBody);
+        const { pathname, search } = parseURL(redirect_uri);
+        assert.strictEqual(
+          pathname + search,
+          `/ui/vault/auth/${this.rootOidc}/oidc/callback`,
           'request made to auth_url when the login page is visited'
         );
       });
       this.server.post(`/auth/${this.nsOidc}/oidc/auth_url`, (schema, req) => {
-        const request = JSON.parse(req.requestBody);
-        assert.deepEqual(
-          request.redirect_uri,
-          `http://localhost:7357/ui/vault/auth/${this.nsOidc}/oidc/callback?namespace=testns`,
+        const { redirect_uri } = JSON.parse(req.requestBody);
+        const { pathname, search } = parseURL(redirect_uri);
+        assert.strictEqual(
+          pathname + search,
+          `/ui/vault/auth/${this.nsOidc}/oidc/callback?namespace=${this.namespace}`,
           'request made to correct auth_url when namespace is filled in'
         );
       });
-      await visit('/vault/auth');
+      await visit('/vault/auth?with=oidc%2F');
       assert.dom(SELECTORS.authTab(this.rootOidc)).exists('renders oidc method tab for root');
       await authPage.namespaceInput(this.namespace);
       assert.strictEqual(
         currentURL(),
-        `/vault/auth?namespace=${this.namespace}&with=ns-oidc%2F`,
+        `/vault/auth?namespace=${this.namespace}&with=${this.nsOidc}%2F`,
         'url updates with namespace value'
       );
-      assert.dom(SELECTORS.authTab(this.nsOidc)).exists('renders oidc method tab for namespace');
+      assert.dom(SELECTORS.authTab(this.nsOidc)).exists('renders oidc method tab for child namespace');
     });
   });
 });
