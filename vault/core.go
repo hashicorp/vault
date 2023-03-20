335ff2df1b8cc65f92cb57902c8fdffd43920434
Mike Palmiotto <mike.palmiotto@hashicorp.com>
2023-03-20 10:51:35 -0400

    backport of commit e3c59773e969336bb4e85ddbf3a3700a6250f4c8

--- vault/core.go
+++ vault/core.go
@@ -632,6 +632,9 @@ type Core struct {
 
 	activityLogConfig ActivityLogCoreConfig
 
+	// censusAgent is the mechanism used for reporting Vault's billing data.
+	censusAgent *CensusAgent
+
 	// activeTime is set on active nodes indicating the time at which this node
 	// became active.
 	activeTime time.Time
@@ -794,6 +797,9 @@ type CoreConfig struct {
 	License         string
 	LicensePath     string
 	LicensingConfig *LicensingConfig
+
+	// Configured Census Agent
+	censusAgent *CensusAgent
 
 	DisablePerformanceStandby bool
 	DisableIndexing           bool
