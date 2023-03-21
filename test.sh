9b7315e34613389c32476c6b557b14c628ec1206
akshya96 <araghavan@hashicorp.com>
2023-03-21 16:11:16 -0700

    changed results

--- test.sh
+++ test.sh
@@ -23,32 +23,36 @@ filesChanged=("builtin/logical/pki/chain_test.go"
 staticcheck ./... | grep deprecated > staticcheckOutput.txt  
 
 # include details of only changed files in the PR
+count=0
 for val in ${changed_files[@]}; do
     if grep -q  $val staticcheckOutput.txt; then
-        grep $val staticcheckOutput.txt >> tmpfile
+        grep $val staticcheckOutput.txt
+        count=$((count+1))
     fi
 done
 
-mv tmpfile staticcheckOutput.txt
-rm -rf tmpfile
+# mv tmpfile staticcheckOutput.txt
+# rm -rf tmpfile
 
-# delete ignored values from the output
-for val in ${ignoreList[@]}; do
-  grep -v $val staticcheckOutput.txt > tmpfile && mv tmpfile staticcheckOutput.txt
-  rm -rf tmpfile
-done
+# # delete ignored values from the output
+# for val in ${ignoreList[@]}; do
+#   grep -v $val staticcheckOutput.txt > tmpfile && mv tmpfile staticcheckOutput.txt
+#   rm -rf tmpfile
+# done
+rm -rf staticcheckOutput.txt  
 
 
-if [ -s staticcheckOutput.txt ]
+if [ "$count" -ne "0" ]
 then
-     echo "Use of deprecated function, variable, constant or field found"
-     cat staticcheckOutput.txt 
-     # output file clean up 
-     rm -rf staticcheckOutput.txt  
+    echo $count
+    echo "Use of deprecated function, variable, constant or field found"
+    #  cat staticcheckOutput.txt 
+    #  # output file clean up 
+    #  rm -rf staticcheckOutput.txt  
      exit 1 
 else
      echo "Use of deprecated function, variable, constant or field not found"
-     rm -rf staticcheckOutput.txt  
+    #  rm -rf staticcheckOutput.txt  
 fi
 
 
