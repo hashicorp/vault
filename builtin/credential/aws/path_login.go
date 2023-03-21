a785426aabcf7b4eb4250d84b04711e98ff122b2
Anton Averchenkov <anton.averchenkov@hashicorp.com>
2023-03-20 21:25:51 -0400

    log-in

--- builtin/credential/aws/path_login.go
+++ builtin/credential/aws/path_login.go
@@ -54,7 +54,7 @@ func (b *backend) pathLogin() *framework.Path {
 		Pattern: "login$",
 		DisplayAttrs: &framework.DisplayAttributes{
 			OperationPrefix: operationPrefixAWSAuth,
-			OperationVerb:   "login",
+			OperationVerb:   "log-in",
 		},
 		Fields: map[string]*framework.FieldSchema{
 			"role": {
