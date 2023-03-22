bea042f73d50dd51aa67b30e97c6e6685e808794
clairebontempo@gmail.com <clairebontempo@gmail.com>
2023-03-21 18:35:31 -0600

    remove state from paramsFor completely, update tests

--- ui/app/routes/vault/cluster/oidc-callback.js
+++ ui/app/routes/vault/cluster/oidc-callback.js
@@ -6,19 +6,17 @@ export default Route.extend({
     // left blank so we render the template immediately
   },
   afterModel() {
-    let { auth_path: path, code, state } = this.paramsFor(this.routeName);
+    let { auth_path: path, code } = this.paramsFor(this.routeName);
+    // some SSO providers do not return a url-encoded state param
+    // parse state using URLSearchParams instead of paramsFor
+    const queryString = decodeURIComponent(window.location.search);
+    const urlParams = new URLSearchParams(queryString);
+    let state = urlParams.get('state');
+    // check cluster for a namespace, i.e. HCP namespace flag
     let { namespaceQueryParam: namespace } = this.paramsFor('vault.cluster');
     // namespace from state takes precedence over the cluster's ns
     if (state?.includes(',ns=')) {
       [state, namespace] = state.split(',ns=');
-    }
-    // some SSO providers do not return a url-encoded state param
-    // check for namespace using URLSearchParams instead of paramsFor
-    const queryString = decodeURIComponent(window.location.search);
-    const urlParams = new URLSearchParams(queryString);
-    const checkState = urlParams.get('state');
-    if (checkState?.includes(',ns=')) {
-      [state, namespace] = checkState.split(',ns=');
     }
     path = window.decodeURIComponent(path);
     const source = 'oidc-callback'; // required by event listener in auth-jwt component
