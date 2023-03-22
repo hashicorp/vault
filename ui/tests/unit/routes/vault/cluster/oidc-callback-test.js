47d515d94f89e151063abfd47b58acbc8254dd51
clairebontempo@gmail.com <clairebontempo@gmail.com>
2023-03-22 17:24:20 -0600

    Revert "remove state from paramsFor completely, update tests"
    This reverts commit bea042f73d50dd51aa67b30e97c6e6685e808794.

--- ui/tests/unit/routes/vault/cluster/oidc-callback-test.js
+++ ui/tests/unit/routes/vault/cluster/oidc-callback-test.js
@@ -12,48 +12,58 @@ module('Unit | Route | vault/cluster/oidc-callback', function (hooks) {
     };
     this.route = this.owner.lookup('route:vault/cluster/oidc-callback');
     this.windowStub = sinon.stub(window.opener, 'postMessage');
-    this.state = 'st_yOarDguU848w5YZuotLs';
     this.path = 'oidc';
     this.code = 'lTazRXEwKfyGKBUCo5TyLJzdIt39YniBJOXPABiRMkL0T';
-    this.route.paramsFor = (path) => {
-      if (path === 'vault.cluster') return { namespaceQueryParam: '' };
-      return {
-        auth_path: this.path,
-        code: this.code,
-      };
+    this.state = (ns) => {
+      return ns ? 'st_91ji6vR2sQ2zBiZSQkqJ' + `,ns=${ns}` : 'st_91ji6vR2sQ2zBiZSQkqJ';
     };
-    this.callbackUrlQueryParams = (stateParam) => {
-      switch (stateParam) {
-        case '':
-          window.history.pushState({}, '');
-          break;
-        case 'stateless':
-          window.history.pushState({}, '', '?' + `code=${this.code}`);
-          break;
-        default:
-          window.history.pushState({}, '', '?' + `code=${this.code}&state=${stateParam}`);
-          break;
-      }
+    this.pushQueryParam = (queryString) => {
+      window.history.pushState({}, '', '?' + queryString);
     };
   });
 
   hooks.afterEach(function () {
     this.windowStub.restore();
     window.opener = this.originalOpener;
-    this.callbackUrlQueryParams('');
+    this.pushQueryParam('');
   });
 
   test('it calls route', function (assert) {
     assert.ok(this.route);
   });
 
-  test('it uses namespace param from state instead of cluster, with custom oidc path', function (assert) {
+  test('it uses namespace param from state not namespaceQueryParam from cluster with default path', function (assert) {
     this.routeName = 'vault.cluster.oidc-callback';
-    this.callbackUrlQueryParams(encodeURIComponent(`${this.state},ns=test-ns`));
+    this.route.paramsFor = (path) => {
+      if (path === 'vault.cluster') return { namespaceQueryParam: 'admin' };
+      return {
+        auth_path: this.path,
+        state: this.state('admin/child-ns'),
+        code: this.code,
+      };
+    };
+    this.route.afterModel();
+
+    assert.propEqual(
+      this.windowStub.lastCall.args[0],
+      {
+        code: 'lTazRXEwKfyGKBUCo5TyLJzdIt39YniBJOXPABiRMkL0T',
+        namespace: 'admin/child-ns',
+        path: 'oidc',
+        source: 'oidc-callback',
+        state: this.state(),
+      },
+      'namespace param is from state, ns=admin/child-ns'
+    );
+  });
+
+  test('it uses namespace param from state not namespaceQueryParam from cluster with custom path', function (assert) {
+    this.routeName = 'vault.cluster.oidc-callback';
     this.route.paramsFor = (path) => {
       if (path === 'vault.cluster') return { namespaceQueryParam: 'admin' };
       return {
         auth_path: 'oidc-dev',
+        state: this.state('admin/child-ns'),
         code: this.code,
       };
     };
@@ -63,21 +73,21 @@ module('Unit | Route | vault/cluster/oidc-callback', function (hooks) {
       {
         code: this.code,
         path: 'oidc-dev',
-        namespace: 'test-ns',
-        state: this.state,
+        namespace: 'admin/child-ns',
+        state: this.state(),
         source: 'oidc-callback',
       },
-      'ns from state not cluster'
+      'state ns takes precedence, state no longer has ns query'
     );
   });
 
-  test('it uses namespace from cluster when state does not include ns param', function (assert) {
+  test(`it uses namespace from namespaceQueryParam when state does not include: ',ns=some-namespace'`, function (assert) {
     this.routeName = 'vault.cluster.oidc-callback';
-    this.callbackUrlQueryParams(encodeURIComponent(this.state));
     this.route.paramsFor = (path) => {
       if (path === 'vault.cluster') return { namespaceQueryParam: 'admin' };
       return {
         auth_path: this.path,
+        state: this.state(),
         code: this.code,
       };
     };
@@ -88,36 +98,44 @@ module('Unit | Route | vault/cluster/oidc-callback', function (hooks) {
         code: this.code,
         path: this.path,
         namespace: 'admin',
-        state: this.state,
+        state: this.state(),
         source: 'oidc-callback',
       },
-      `namespace is from cluster's namespaceQueryParam`
+      'namespace is from cluster namespaceQueryParam'
     );
   });
 
-  test('it correctly parses encoded, nested ns param from state', function (assert) {
-    this.callbackUrlQueryParams(encodeURIComponent(`${this.state},ns=parent-ns/child-ns`));
+  test('it uses ns param from state when no namespaceQueryParam from cluster', function (assert) {
+    this.routeName = 'vault.cluster.oidc-callback';
+    this.route.paramsFor = (path) => {
+      if (path === 'vault.cluster') return { namespaceQueryParam: '' };
+      return {
+        auth_path: this.path,
+        state: this.state('ns1'),
+        code: this.code,
+      };
+    };
     this.route.afterModel();
     assert.propEqual(
       this.windowStub.lastCall.args[0],
       {
         code: this.code,
         path: this.path,
-        namespace: 'parent-ns/child-ns',
-        state: this.state,
+        namespace: 'ns1',
+        state: this.state(),
         source: 'oidc-callback',
       },
-      'it has correct nested ns from state and sets as namespace param'
+      'it strips ns from state and uses as namespace param'
     );
   });
 
   test('the afterModel hook returns when both cluster and route params are empty strings', function (assert) {
     this.routeName = 'vault.cluster.oidc-callback';
-    this.callbackUrlQueryParams('');
     this.route.paramsFor = (path) => {
       if (path === 'vault.cluster') return { namespaceQueryParam: '' };
       return {
         auth_path: '',
+        state: '',
         code: '',
       };
     };
@@ -136,12 +154,17 @@ module('Unit | Route | vault/cluster/oidc-callback', function (hooks) {
 
   test('the afterModel hook returns when state param does not exist', function (assert) {
     this.routeName = 'vault.cluster.oidc-callback';
-    this.callbackUrlQueryParams('stateless');
+    this.route.paramsFor = (path) => {
+      if (path === 'vault.cluster') return { namespaceQueryParam: '' };
+      return {
+        auth_path: this.path,
+      };
+    };
     this.route.afterModel();
     assert.propEqual(
       this.windowStub.lastCall.args[0],
       {
-        code: this.code,
+        code: '',
         path: 'oidc',
         state: '',
         source: 'oidc-callback',
@@ -150,13 +173,13 @@ module('Unit | Route | vault/cluster/oidc-callback', function (hooks) {
     );
   });
 
-  test('the afterModel hook returns when cluster ns exists and all route params are empty strings', function (assert) {
+  test('the afterModel hook returns when cluster namespaceQueryParam exists and all route params are empty strings', function (assert) {
     this.routeName = 'vault.cluster.oidc-callback';
-    this.callbackUrlQueryParams('');
     this.route.paramsFor = (path) => {
       if (path === 'vault.cluster') return { namespaceQueryParam: 'ns1' };
       return {
         auth_path: '',
+        state: '',
         code: '',
       };
     };
@@ -177,18 +200,27 @@ module('Unit | Route | vault/cluster/oidc-callback', function (hooks) {
   /*
   If authenticating to a namespace, most SSO providers return a callback url
   with a 'state' query param that includes a URI encoded namespace, example:
-  '?code=BZBDVPMz0By2JTqulEMWX5-6rflW3A20UAusJYHEeFygJ&state=sst_yOarDguU848w5YZuotLs%2Cns%3Dadmin'    
+  '?code=BZBDVPMz0By2JTqulEMWX5-6rflW3A20UAusJYHEeFygJ&state=st_EC8PbzZ7XUQ0ClEgssS9%2Cns%3Dadmin'    
 
   Active Directory Federation Service (AD FS), instead, decodes the namespace portion:
-  '?code=BZBDVPMz0By2JTqulEMWX5-6rflW3A20UAusJYHEeFygJ&state=st_yOarDguU848w5YZuotLs,ns=admin'
+  '?code=BZBDVPMz0By2JTqulEMWX5-6rflW3A20UAusJYHEeFygJ&state=st_gVRGT4TJe2RpvHNX5HV0,ns=admin'
   
   'ns' isn't recognized as a separate param because there is no ampersand, so using this.paramsFor() returns
   a namespace-less state and authentication fails
-  { state: 'st_yOarDguU848w5YZuotLs,ns' }
+  { state: 'st_91ji6vR2sQ2zBiZSQkqJ,ns' }
   */
-  test('it uses namespace when state param is not uri encoded', async function (assert) {
+  test('is parses the namespace from a uri with decoded state param', async function (assert) {
+    this.pushQueryParam(`?code=${this.code}&state=st_gVRGT4TJe2RpvHNX5HV0,ns=admin`);
     this.routeName = 'vault.cluster.oidc-callback';
-    this.callbackUrlQueryParams(`${this.state},ns=admin`);
+    this.route.paramsFor = (path) => {
+      if (path === 'vault.cluster') return { namespaceQueryParam: '' };
+      return {
+        auth_path: this.path,
+        state: 'st_91ji6vR2sQ2zBiZSQkqJ,ns',
+        code: this.code,
+      };
+    };
+
     this.route.afterModel();
     assert.propEqual(
       this.windowStub.lastCall.args[0],
@@ -197,9 +229,35 @@ module('Unit | Route | vault/cluster/oidc-callback', function (hooks) {
         namespace: 'admin',
         path: this.path,
         source: 'oidc-callback',
-        state: this.state,
+        state: 'st_gVRGT4TJe2RpvHNX5HV0',
       },
-      'namespace is parsed correctly'
+      'namespace is passed to window queryParams'
+    );
+  });
+
+  test('is parses the namespace from a uri with encoded state param', async function (assert) {
+    this.pushQueryParam(`?code=${this.code}&state=st_EC8PbzZ7XUQ0ClEgssS9%2Cns%3Dadmin`);
+    this.routeName = 'vault.cluster.oidc-callback';
+    this.route.paramsFor = (path) => {
+      if (path === 'vault.cluster') return { namespaceQueryParam: '' };
+      return {
+        auth_path: this.path,
+        state: 'st_EC8PbzZ7XUQ0ClEgssS9,ns=admin',
+        code: this.code,
+      };
+    };
+
+    this.route.afterModel();
+    assert.propEqual(
+      this.windowStub.lastCall.args[0],
+      {
+        code: this.code,
+        namespace: 'admin',
+        path: this.path,
+        source: 'oidc-callback',
+        state: 'st_EC8PbzZ7XUQ0ClEgssS9',
+      },
+      'namespace is passed to window queryParams'
     );
   });
 });
