30e94f6bb4b92d2cb192638a4712ed4b7e726aa2
clairebontempo@gmail.com <clairebontempo@gmail.com>
2023-03-23 14:56:00 -0600

    add page component

---
+++ ui/lib/pki/addon/components/page/pki-issuer-rotate-root.js
@@ -0,0 +1,6 @@
+import Component from '@glimmer/component';
+import { tracked } from '@glimmer/tracking';
+
+export default class PagePkiIssuerRotateRootComponent extends Component {
+  @tracked title = 'Rotate root';
+}
