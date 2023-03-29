a3baf6dc2a2d00d338165783815010f2b91b8618
Kianna Quach <kianna.quach@hashicorp.com>
2023-03-29 11:45:16 -0700

    Add selector file

---
+++ ui/tests/helpers/pki/pki-delete-all-issuers.js
@@ -0,0 +1,11 @@
+/**
+ * Copyright (c) HashiCorp, Inc.
+ * SPDX-License-Identifier: MPL-2.0
+ */
+
+export const SELECTORS = {
+  issuerLink: '[data-test-delete-all-issuers-link]',
+  deleteAllIssuerModal: '[data-test-modal-background="Delete All Issuers?"]',
+  deleteAllIssuerInput: '[data-test-delete-all-issuers-input]',
+  deleteAllIssuerButton: '[data-test-delete-all-issuers-button]',
+};
