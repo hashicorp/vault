a4e7ff55d8f7009f3e291e84be2bd410b1289c75
Kianna Quach <kianna.quach@hashicorp.com>
2023-03-31 11:24:16 -0700

    Fix tests

--- ui/tests/helpers/pki/pki-delete-all-issuers.js
+++ ui/tests/helpers/pki/pki-delete-all-issuers.js
@@ -6,6 +6,6 @@
 export const SELECTORS = {
   issuerLink: '[data-test-delete-all-issuers-link]',
   deleteAllIssuerModal: '[data-test-modal-background="Delete All Issuers?"]',
-  deleteAllIssuerInput: '[data-test-delete-all-issuers-input]',
-  deleteAllIssuerButton: '[data-test-delete-all-issuers-button]',
+  deleteAllIssuerInput: '[data-test-confirmation-modal-input="Delete All Issuers?"]',
+  deleteAllIssuerButton: '[data-test-confirm-button="Delete All Issuers?"]',
 };
