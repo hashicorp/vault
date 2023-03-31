78a525d79f1c0d1e3854bb37e5c68c73185083a8
clairebontempo@gmail.com <clairebontempo@gmail.com>
2023-03-31 14:36:45 -0600

    fix tests

--- ui/app/services/flash-messages.ts
+++ ui/app/services/flash-messages.ts
@@ -5,11 +5,4 @@
 
 import FlashMessages from 'ember-cli-flash/services/flash-messages';
 
-export default class FlashMessageService extends FlashMessages {
-  stickyInfo(message: string) {
-    return this.info(message, {
-      sticky: true,
-      priority: 300,
-    });
-  }
-}
+export default class FlashMessageService extends FlashMessages {}
