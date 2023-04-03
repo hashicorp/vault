2054ffcbfa519e72fe5b025fcd3f22bb05a23c50
Peter Wilson <peter.wilson@hashicorp.com>
2023-04-03 16:58:57 +0100

    VAULT-14048: raft-autopilot appears to refuse to remove a node which has left and wouldn't impact stability (#19472)
    * ensure we supply the node type when it's for a voter
    * bumped autopilot version back to v0.2.0 and ran go mod tidy
    * changed condition in knownservers and added some comments
    * Export GetRaftBackend
    * Updated tests for autopilot (related to dead server cleanup)
    * Export Raft NewDelegate
    
    Co-authored-by: Nick Cabatoff <ncabatoff@hashicorp.com>

--- helper/testhelpers/testhelpers_oss.go
+++ helper/testhelpers/testhelpers_oss.go
@@ -20,3 +20,34 @@ func WaitForActiveNodeAndStandbys(t testing.T, cluster *vault.TestCluster) {
 		}
 	}
 }
+
+// WaitForNodesExcludingSelectedStandbys is variation on WaitForActiveNodeAndStandbys.
+// It waits for the active node before waiting for standby nodes, however
+// it will not wait for cores with indexes that match those specified as arguments.
+// Whilst you could specify index 0 which is likely to be the leader node, the function
+// checks for the leader first regardless of the indexes to skip, so it would be redundant to do so.
+// The intention/use case for this function is to allow a cluster to start and become active with one
+// or more nodes not joined, so that we can test scenarios where a node joins later.
+// e.g. 4 nodes in the cluster, only 3 nodes in cluster 'active', 1 node can be joined later in tests.
+func WaitForNodesExcludingSelectedStandbys(t testing.T, cluster *vault.TestCluster, indexesToSkip ...int) {
+	WaitForActiveNode(t, cluster)
+
+	contains := func(elems []int, e int) bool {
+		for _, v := range elems {
+			if v == e {
+				return true
+			}
+		}
+
+		return false
+	}
+	for i, core := range cluster.Cores {
+		if contains(indexesToSkip, i) {
+			continue
+		}
+
+		if standby, _ := core.Core.Standby(); standby {
+			WaitForStandbyNode(t, core)
+		}
+	}
+}
