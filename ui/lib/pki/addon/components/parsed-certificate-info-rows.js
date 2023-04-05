5f141fe4acdb927232078c1d63fe1891039b16b5
Chelsea Shaw <cshaw@hashicorp.com>
2023-04-05 12:13:42 -0500

    Import keys from parse-pki-cert-oids for single source of truth

--- ui/lib/pki/addon/components/parsed-certificate-info-rows.js
+++ ui/lib/pki/addon/components/parsed-certificate-info-rows.js
@@ -1,32 +1,32 @@
 import Component from '@glimmer/component';
+import { parsedParameterKeys } from 'vault/utils/parse-pki-cert-oids';
 
 /**
  * Expects to have parsedCertificate from parse-pki-cert util passed in, and will render attributes if values exist
  */
 export default class ParsedCertificateInfoRowsComponent extends Component {
   get possibleFields() {
-    return [
-      { key: 'serial_number' },
-      { key: 'key_usage' },
-      { key: 'other_sans', label: 'Other SANs' },
-      { key: 'alt_names', label: 'Subject Alternative Names (SANs)' },
-      { key: 'uri_sans', label: 'URI SANs' },
-      { key: 'ip_sans', label: 'IP SANs' },
-      { key: 'permitted_dns_domains', label: 'Permitted DNS domains' },
-      { key: 'max_path_length' },
-      { key: 'exclude_cn_from_sans', label: 'Exclude CN from SANs' },
-      { key: 'signature_bits' },
-      { key: 'use_pss', label: 'Use PSS' },
-      { key: 'not_valid_after', formatDate: 'MMM d yyyy HH:mm:ss a zzzz' },
-      { key: 'not_valid_before', formatDate: 'MMM d yyyy HH:mm:ss a zzzz' },
-      { key: 'ttl', label: 'TTL' },
-      { key: 'ou', label: 'Organizational units (OU)' },
-      { key: 'organization' },
-      { key: 'country' },
-      { key: 'locality' },
-      { key: 'province' },
-      { key: 'street_address' },
-      { key: 'postal_code' },
-    ];
+    // We show common name elsewhere on the details view, so no need to render it here
+    const fieldKeys = parsedParameterKeys.filter((k) => k !== 'common_name');
+    const attrsByKey = {
+      other_sans: { label: 'Other SANs' },
+      alt_names: { label: 'Subject Alternative Names (SANs)' },
+      uri_sans: { label: 'URI SANs' },
+      ip_sans: { label: 'IP SANs' },
+      permitted_dns_domains: { label: 'Permitted DNS domains' },
+      exclude_cn_from_sans: { label: 'Exclude CN from SANs' },
+      use_pss: { label: 'Use PSS' },
+      ttl: { label: 'TTL' },
+      ou: { label: 'Organizational units (OU)' },
+      not_valid_after: { formatDate: 'MMM d yyyy HH:mm:ss a zzzz' },
+      not_valid_before: { formatDate: 'MMM d yyyy HH:mm:ss a zzzz' },
+    };
+
+    return fieldKeys.map((key) => {
+      return {
+        key,
+        ...attrsByKey[key],
+      };
+    });
   }
 }
