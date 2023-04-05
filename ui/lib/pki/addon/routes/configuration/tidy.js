69d86dfc5127c4f632bffe19504ae4df471da0be
Kianna Quach <kianna.quach@hashicorp.com>
2023-04-05 08:49:58 -0700

    Inital ui setup

--- ui/lib/pki/addon/routes/configuration/tidy.js
+++ ui/lib/pki/addon/routes/configuration/tidy.js
@@ -4,5 +4,26 @@
  */
 
 import Route from '@ember/routing/route';
+import { inject as service } from '@ember/service';
+import { hash } from 'rsvp';
 
-export default class PkiConfigurationTidyRoute extends Route {}
+export default class PkiConfigurationTidyRoute extends Route {
+  @service store;
+  @service secretMountPath;
+
+  model() {
+    return hash({
+      engine: this.modelFor('application'),
+    });
+  }
+
+  setupController(controller, resolvedModel) {
+    super.setupController(controller, resolvedModel);
+    controller.breadcrumbs = [
+      { label: 'secrets', route: 'secrets', linkExternal: true },
+      { label: this.secretMountPath.currentPath, route: 'overview' },
+      { label: 'configuration', route: 'configuration.index' },
+      { label: 'tidy' },
+    ];
+  }
+}
