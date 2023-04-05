4dec819f656c1a73279c20a620cc54769c43b520
Kianna Quach <kianna.quach@hashicorp.com>
2023-04-04 22:56:34 -0700

    Add certs test

--- ui/tests/acceptance/pki/configuration-test.js
+++ ui/tests/acceptance/pki/configuration-test.js
@@ -41,15 +41,70 @@ module('Acceptance | pki configuration', function (hooks) {
     await visit(`/vault/secrets/${this.mountPath}/pki/configuration`);
     await click(SELECTORS.emptyStateLink);
     assert.strictEqual(currentURL(), `/vault/secrets/${this.mountPath}/pki/configuration/create`);
-    await click(SELECTORS.configuration.optionByKey('import'));
-    await click('[data-test-text-toggle]');
-    await fillIn('[data-test-text-file-textarea]', this.pemBundle);
-    await click('[data-test-pki-import-pem-bundle]');
-    await visit(`/vault/secrets/${this.mountPath}/pki/configuration`);
+    await click('[data-test-pki-config-option="generate-root"]');
+    await fillIn('[data-test-input="type"]', 'exported');
+    await fillIn('[data-test-input="commonName"]', 'issuer-common-0');
+    await fillIn('[data-test-input="issuerName"]', 'issuer-0');
+    await click('[data-test-pki-generate-root-save]');
+    await click('[data-test-done]');
+    assert.strictEqual(currentURL(), `/vault/secrets/${this.mountPath}/pki/overview`);
+    await click(SELECTORS.configTab);
+    assert.strictEqual(currentURL(), `/vault/secrets/${this.mountPath}/pki/configuration`);
     await click(SELECTORS.configuration.issuerLink);
     assert.dom(SELECTORS.configuration.deleteAllIssuerModal).exists();
     await fillIn(SELECTORS.configuration.deleteAllIssuerInput, 'delete-all');
     await click(SELECTORS.configuration.deleteAllIssuerButton);
     assert.dom('[data-test-component="empty-state"]').exists();
+  });
+
+  test('it shows the correct empty state message if certificates exists after delete all issuers', async function (assert) {
+    await authPage.login(this.pkiAdminToken);
+    await visit(`/vault/secrets/${this.mountPath}/pki/configuration`);
+    await click(SELECTORS.emptyStateLink);
+    assert.strictEqual(currentURL(), `/vault/secrets/${this.mountPath}/pki/configuration/create`);
+    await click('[data-test-pki-config-option="generate-root"]');
+    await fillIn('[data-test-input="type"]', 'exported');
+    await fillIn('[data-test-input="commonName"]', 'issuer-common-0');
+    await fillIn('[data-test-input="issuerName"]', 'issuer-0');
+    await click('[data-test-pki-generate-root-save]');
+    await click('[data-test-done]');
+    assert.strictEqual(currentURL(), `/vault/secrets/${this.mountPath}/pki/overview`);
+    await click(SELECTORS.configTab);
+    assert.strictEqual(currentURL(), `/vault/secrets/${this.mountPath}/pki/configuration`);
+    await click(SELECTORS.configuration.issuerLink);
+    assert.dom(SELECTORS.configuration.deleteAllIssuerModal).exists();
+    await fillIn(SELECTORS.configuration.deleteAllIssuerInput, 'delete-all');
+    await click(SELECTORS.configuration.deleteAllIssuerButton);
+    assert.dom('[data-test-component="empty-state"]').exists();
+
+    await click(SELECTORS.overviewTab);
+    assert.strictEqual(currentURL(), `/vault/secrets/${this.mountPath}/pki/overview`);
+    assert
+      .dom(SELECTORS.emptyStateMessage)
+      .hasText(
+        "This PKI mount hasn't yet been configured with a certificate issuer. There are existing certificates. Use the CLI to perform any operations with them until an issuer is configured."
+      );
+
+    await click(SELECTORS.rolesTab);
+    assert
+      .dom(SELECTORS.emptyStateMessage)
+      .hasText("This PKI mount hasn't yet been configured with a certificate issuer.");
+
+    await click(SELECTORS.issuersTab);
+    assert
+      .dom(SELECTORS.emptyStateMessage)
+      .hasText("This PKI mount hasn't yet been configured with a certificate issuer.");
+
+    await click(SELECTORS.keysTab);
+    assert
+      .dom(SELECTORS.emptyStateMessage)
+      .hasText("This PKI mount hasn't yet been configured with a certificate issuer.");
+
+    await click(SELECTORS.certsTab);
+    assert
+      .dom(SELECTORS.emptyStateMessage)
+      .hasText(
+        "This PKI mount hasn't yet been configured with a certificate issuer. There are existing certificates. Use the CLI to perform any operations with them until an issuer is configured."
+      );
   });
 });
