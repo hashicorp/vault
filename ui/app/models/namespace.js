37c5dce5585e7b8e03a18c2314e5808763def67a
Chelsea Shaw <cshaw@hashicorp.com>
2023-04-06 12:11:31 -0500

    revert model changes due to unavailable decorator

--- ui/app/models/namespace.js
+++ ui/app/models/namespace.js
@@ -1,19 +1,19 @@
 import Model, { attr } from '@ember-data/model';
-import { withExpandedAttributes } from 'vault/decorators/model-expanded-attributes';
+import { computed } from '@ember/object';
+import { expandAttributeMeta } from 'vault/utils/field-to-attrs';
 
-@withExpandedAttributes()
-export default class NamespaceModel extends Model {
-  @attr('string', {
+export default Model.extend({
+  path: attr('string', {
     validationAttr: 'pathIsValid',
     invalidMessage: 'You have entered and invalid path please only include letters, numbers, -, ., and _.',
-  })
-  path;
-
-  get pathIsValid() {
+  }),
+  pathIsValid: computed('path', function () {
     return this.path && this.path.match(/^[\w\d-.]+$/g);
-  }
-
-  get fields() {
-    return ['path'].map((f) => this.allByKey[f]);
-  }
-}
+  }),
+  description: attr('string', {
+    editType: 'textarea',
+  }),
+  fields: computed(function () {
+    return expandAttributeMeta(this, ['path']);
+  }),
+});
