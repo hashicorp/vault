388948e858b879f31f048ada427eb8cf6607b91a
Anton Averchenkov <84287187+averche@users.noreply.github.com>
2023-04-10 14:19:32 -0400

    openapi: Add display attributes for RabbitMQ plugin (#19540)

--- builtin/logical/rabbitmq/path_roles.go
+++ builtin/logical/rabbitmq/path_roles.go
@@ -16,6 +16,10 @@ import (
 func pathListRoles(b *backend) *framework.Path {
 	return &framework.Path{
 		Pattern: "roles/?$",
+		DisplayAttrs: &framework.DisplayAttributes{
+			OperationPrefix: operationPrefixRabbitMQ,
+			OperationSuffix: "roles",
+		},
 		Callbacks: map[logical.Operation]framework.OperationFunc{
 			logical.ListOperation: b.pathRoleList,
 		},
@@ -27,6 +31,10 @@ func pathListRoles(b *backend) *framework.Path {
 func pathRoles(b *backend) *framework.Path {
 	return &framework.Path{
 		Pattern: "roles/" + framework.GenericNameRegex("name"),
+		DisplayAttrs: &framework.DisplayAttributes{
+			OperationPrefix: operationPrefixRabbitMQ,
+			OperationSuffix: "role",
+		},
 		Fields: map[string]*framework.FieldSchema{
 			"name": {
 				Type:        framework.TypeString,
