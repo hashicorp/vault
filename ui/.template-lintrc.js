aa1a15f92c107be316d64eb0576e5f6cb3df390f
clairebontempo@gmail.com <clairebontempo@gmail.com>
2023-04-10 10:28:10 -0700

    fix, add to ignore

--- ui/.template-lintrc.js
+++ ui/.template-lintrc.js
@@ -47,7 +47,7 @@ module.exports = {
     },
     'require-input-label': 'off',
   },
-  ignore: ['lib/story-md', 'tests/**'],
+  ignore: ['lib/story-md', 'tests/**', 'console/*'],
   // ember language server vscode extension does not currently respect the ignore field
   // override all rules manually as workaround to align with cli
   overrides: [
