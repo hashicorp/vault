2b58b1797efb2daabb6cab16efe1113f45baa30a
Mike Palmiotto <mike.palmiotto@hashicorp.com>
2023-04-12 16:02:28 +0000

    backport of commit 05ba6bbddded428d2fa010f9359d0543f46af52b

--- vault/activity_log_test.go
+++ vault/activity_log_test.go
@@ -805,10 +805,12 @@ func TestActivityLog_API_ConfigCRUD(t *testing.T) {
 			t.Fatalf("err: %v", err)
 		}
 		defaults := map[string]interface{}{
-			"default_report_months": 12,
-			"retention_months":      24,
-			"enabled":               activityLogEnabledDefaultValue,
-			"queries_available":     false,
+			"default_report_months":   12,
+			"retention_months":        24,
+			"enabled":                 activityLogEnabledDefaultValue,
+			"queries_available":       false,
+			"reporting_enabled":       core.censusLicensingEnabled,
+			"billing_start_timestamp": core.GetBillingStart(),
 		}
 
 		if diff := deep.Equal(resp.Data, defaults); len(diff) > 0 {
@@ -886,10 +888,12 @@ func TestActivityLog_API_ConfigCRUD(t *testing.T) {
 			t.Fatalf("err: %v", err)
 		}
 		expected := map[string]interface{}{
-			"default_report_months": 1,
-			"retention_months":      2,
-			"enabled":               "enable",
-			"queries_available":     false,
+			"default_report_months":   1,
+			"retention_months":        2,
+			"enabled":                 "enable",
+			"queries_available":       false,
+			"reporting_enabled":       false,
+			"billing_start_timestamp": core.GetBillingStart(),
 		}
 
 		if diff := deep.Equal(resp.Data, expected); len(diff) > 0 {
@@ -922,10 +926,12 @@ func TestActivityLog_API_ConfigCRUD(t *testing.T) {
 		}
 
 		defaults := map[string]interface{}{
-			"default_report_months": 12,
-			"retention_months":      24,
-			"enabled":               activityLogEnabledDefaultValue,
-			"queries_available":     false,
+			"default_report_months":   12,
+			"retention_months":        24,
+			"enabled":                 activityLogEnabledDefaultValue,
+			"queries_available":       false,
+			"reporting_enabled":       false,
+			"billing_start_timestamp": core.GetBillingStart(),
 		}
 
 		if diff := deep.Equal(resp.Data, defaults); len(diff) > 0 {
