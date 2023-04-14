e4b80f1b13cf163479a41a5317fe8920de4f6313
Victor Rodriguez <vrizo@hashicorp.com>
2023-04-14 10:48:53 -0400

    Tweak Access implementation.
    Change `access` struct to have a field of type wrapping.Wrapper, rather than
    extending it.

--- vault/seal/seal.go
+++ vault/seal/seal.go
@@ -44,35 +44,43 @@ type Access interface {
 }
 
 type access struct {
-	wrapping.Wrapper
+	w wrapping.Wrapper
 }
 
 var _ Access = (*access)(nil)
 
 func NewAccess(w wrapping.Wrapper) Access {
 	return &access{
-		Wrapper: w,
+		w: w,
 	}
 }
 
+func (a *access) KeyId(ctx context.Context) (string, error) {
+	return a.w.KeyId(ctx)
+}
+
+func (a *access) SetConfig(ctx context.Context, options ...wrapping.Option) (*wrapping.WrapperConfig, error) {
+	return a.w.SetConfig(ctx, options...)
+}
+
 func (a *access) GetWrapper() wrapping.Wrapper {
-	return a.Wrapper
+	return a.w
 }
 
 func (a *access) Init(ctx context.Context, options ...wrapping.Option) error {
-	if initWrapper, ok := a.Wrapper.(wrapping.InitFinalizer); ok {
+	if initWrapper, ok := a.w.(wrapping.InitFinalizer); ok {
 		return initWrapper.Init(ctx, options...)
 	}
 	return nil
 }
 
 func (a *access) Type(ctx context.Context) (wrapping.WrapperType, error) {
-	return a.Wrapper.Type(ctx)
+	return a.w.Type(ctx)
 }
 
 // Encrypt uses the underlying seal to encrypt the plaintext and returns it.
 func (a *access) Encrypt(ctx context.Context, plaintext []byte, options ...wrapping.Option) (blob *wrapping.BlobInfo, err error) {
-	wTyp, err := a.Wrapper.Type(ctx)
+	wTyp, err := a.w.Type(ctx)
 	if err != nil {
 		return nil, err
 	}
@@ -90,14 +98,14 @@ func (a *access) Encrypt(ctx context.Context, plaintext []byte, options ...wrapp
 	metrics.IncrCounter([]string{"seal", "encrypt"}, 1)
 	metrics.IncrCounter([]string{"seal", wTyp.String(), "encrypt"}, 1)
 
-	return a.Wrapper.Encrypt(ctx, plaintext, options...)
+	return a.w.Encrypt(ctx, plaintext, options...)
 }
 
 // Decrypt uses the underlying seal to decrypt the cryptotext and returns it.
 // Note that it is possible depending on the wrapper used that both pt and err
 // are populated.
 func (a *access) Decrypt(ctx context.Context, data *wrapping.BlobInfo, options ...wrapping.Option) (pt []byte, err error) {
-	wTyp, err := a.Wrapper.Type(ctx)
+	wTyp, err := a.w.Type(ctx)
 	defer func(now time.Time) {
 		metrics.MeasureSince([]string{"seal", "decrypt", "time"}, now)
 		metrics.MeasureSince([]string{"seal", wTyp.String(), "decrypt", "time"}, now)
@@ -111,11 +119,11 @@ func (a *access) Decrypt(ctx context.Context, data *wrapping.BlobInfo, options .
 	metrics.IncrCounter([]string{"seal", "decrypt"}, 1)
 	metrics.IncrCounter([]string{"seal", wTyp.String(), "decrypt"}, 1)
 
-	return a.Wrapper.Decrypt(ctx, data, options...)
+	return a.w.Decrypt(ctx, data, options...)
 }
 
 func (a *access) Finalize(ctx context.Context, options ...wrapping.Option) error {
-	if finalizeWrapper, ok := a.Wrapper.(wrapping.InitFinalizer); ok {
+	if finalizeWrapper, ok := a.w.(wrapping.InitFinalizer); ok {
 		return finalizeWrapper.Finalize(ctx, options...)
 	}
 	return nil
