a75b1904455793920ee035bd94505883efd0c15d
Angel Garbarino <Monkeychip@users.noreply.github.com>
2023-04-18 11:09:48 -0600

    Disabling License Banners  (#19116) (#20226)
    * work in progress: got the expired banner set with license check
    
    * wip: got the logic for both banners, need to test and write tests
    
    * add notes
    
    * prep for test writing
    
    * test coverage
    
    * add changelog
    
    * clean up
    
    * clarify dismissTypes and conditionals
    
    * updates
    
    * update comment
    
    * update comment
    
    * address pr comments
    
    * update test
    
    * small naming change
    
    * small naming changes
    
    * clean localStorage
    
    * comment clean up
    
    * another comment clean up
    
    * remove meep
    
    * add test coverage for new method in localStorage

--- ui/app/components/license-banners.js
+++ ui/app/components/license-banners.js
@@ -1,3 +1,11 @@
+import Component from '@glimmer/component';
+import { action } from '@ember/object';
+import { tracked } from '@glimmer/tracking';
+import { inject as service } from '@ember/service';
+import isAfter from 'date-fns/isAfter';
+import differenceInDays from 'date-fns/differenceInDays';
+import localStorage from 'vault/lib/local-storage';
+
 /**
  * @module LicenseBanners
  * LicenseBanners components are used to display Vault-specific license expiry messages
@@ -9,11 +17,23 @@
  * @param {string} expiry - RFC3339 date timestamp
  */
 
-import Component from '@glimmer/component';
-import isAfter from 'date-fns/isAfter';
-import differenceInDays from 'date-fns/differenceInDays';
-
 export default class LicenseBanners extends Component {
+  @service version;
+
+  @tracked warningDismissed;
+  @tracked expiredDismissed;
+
+  constructor() {
+    super(...arguments);
+    // do not dismiss any banners if the user has updated their version
+    const dismissedBanner = localStorage.getItem(`dismiss-license-banner-${this.currentVersion}`); // returns either warning or expired
+    this.updateDismissType(dismissedBanner);
+  }
+
+  get currentVersion() {
+    return this.version.version;
+  }
+
   get licenseExpired() {
     if (!this.args.expiry) return false;
     return isAfter(new Date(), new Date(this.args.expiry));
@@ -23,5 +43,23 @@ export default class LicenseBanners extends Component {
     // Anything more than 30 does not render a warning
     if (!this.args.expiry) return 99;
     return differenceInDays(new Date(this.args.expiry), new Date());
+  }
+
+  @action
+  dismissBanner(dismissAction) {
+    // if a client's version changed their old localStorage key will still exists.
+    localStorage.cleanUpStorage('dismiss-license-banner', `dismiss-license-banner-${this.currentVersion}`);
+    // updates localStorage and then updates the template by calling updateDismissType
+    localStorage.setItem(`dismiss-license-banner-${this.currentVersion}`, dismissAction);
+    this.updateDismissType(dismissAction);
+  }
+
+  updateDismissType(dismissType) {
+    // updates tracked properties to update template
+    if (dismissType === 'warning') {
+      this.warningDismissed = true;
+    } else if (dismissType === 'expired') {
+      this.expiredDismissed = true;
+    }
   }
 }
