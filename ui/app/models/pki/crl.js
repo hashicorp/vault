3f6f4a0ee2c1a0276d0cc840c657184e164410cc
clairebontempo@gmail.com <clairebontempo@gmail.com>
2023-04-18 17:19:05 -0700

    finish building out form, add model attrs build ttls

--- ui/app/models/pki/crl.js
+++ ui/app/models/pki/crl.js
@@ -4,15 +4,84 @@
  */
 
 import Model, { attr } from '@ember-data/model';
+import { withFormFields } from 'vault/decorators/model-form-fields';
+import lazyCapabilities, { apiPath } from 'vault/macros/lazy-capabilities';
 
+@withFormFields(['expiry', 'autoRebuildGracePeriod', 'deltaRebuildInterval', 'ocspExpiry'])
 export default class PkiCrlModel extends Model {
   // This model uses the backend value as the model ID
   get useOpenAPI() {
     return true;
   }
+  getHelpUrl(backendPath) {
+    return `/v1/${backendPath}/config/crl?help=1`;
+  }
 
-  @attr('string') expiry;
-  @attr('boolean') autoRebuild;
-  @attr('string') ocspExpiry;
-  @attr('boolean') ocspDisable;
+  @attr('boolean', { defaultValue: true })
+  autoRebuild;
+
+  @attr('string', {
+    label: 'Auto-rebuild on',
+    labelDisabled: 'Auto-rebuild off',
+    defaultValue: '12h',
+    editType: 'ttl',
+    booleanBuddy: 'autoRebuild',
+    helperTextEnabled: 'Vault will rebuild the CRL in the below grace period before expiration',
+    helperTextDisabled: 'Vault will not automatically rebuild the CRL',
+  })
+  autoRebuildGracePeriod;
+
+  @attr('boolean', { defaultValue: true })
+  enableDelta;
+
+  @attr('string', {
+    label: 'Expiry',
+    labelDisabled: 'No expiry',
+    defaultValue: '72h',
+    editType: 'ttl',
+    booleanBuddy: 'disable',
+    helperTextEnabled: 'The CRL will expire after:',
+    helperTextDisabled: 'The CRL will not be built.',
+  })
+  expiry;
+
+  @attr('string', {
+    label: 'Delta CRL building on',
+    labelDisabled: 'Delta CRL building off',
+    defaultValue: '15mh',
+    editType: 'ttl',
+    booleanBuddy: 'enableDelta',
+    helperTextEnabled: 'Vault will rebuild the delta CRL at the interval below:',
+    helperTextDisabled: 'Vault will not rebuild the delta CRL at an interval',
+  })
+  deltaRebuildInterval;
+
+  @attr('boolean', { defaultValue: true })
+  disable;
+
+  @attr('string', {
+    label: 'OCSP responder APIs enabled',
+    labelDisabled: 'OCSP responder APIs disabled',
+    defaultValue: '1h',
+    booleanBuddy: 'ocspDisable',
+    helperTextEnabled: "Requests about a certificate's status will be valid for:",
+    helperTextDisabled: 'Requests cannot be made to check if an individual certificate is valid.',
+  })
+  ocspExpiry;
+
+  @attr('boolean', { label: 'OCSP disable', defaultValue: true })
+  ocspDisable;
+
+  // TODO missing from designs, enterprise only - add?
+  /*
+  "cross_cluster_revocation": true,
+  "unified_crl": true,
+  "unified_crl_on_existing_paths": true
+  */
+
+  @lazyCapabilities(apiPath`${'id'}/config/crl`, 'id') crlPath;
+
+  get canSet() {
+    return this.crlPath.get('canCreate') !== false;
+  }
 }
--- ui/lib/pki/addon/components/page/pki-configuration-edit.ts
+++ ui/lib/pki/addon/components/page/pki-configuration-edit.ts
@@ -4,5 +4,57 @@
  */
 
 import Component from '@glimmer/component';
+import { tracked } from '@glimmer/tracking';
+import { inject as service } from '@ember/service';
+import { action } from '@ember/object';
+import { task } from 'ember-concurrency';
+import { waitFor } from '@ember/test-waiters';
+import RouterService from '@ember/routing/router-service';
+import FlashMessageService from 'vault/services/flash-messages';
+import { FormField, TtlEvent } from 'vault/app-types';
+import PkiCrlModel from 'vault/models/pki/crl';
 
-export default class PkiConfigurationEditComponent extends Component {}
+interface Args {
+  crl: PkiCrlModel;
+}
+
+interface PkiCrlTtls {
+  autoRebuildGracePeriod: string;
+  expiry: string;
+  deltaRebuildInterval: string;
+  ocspExpiry: string;
+}
+interface PkiCrlBooleans {
+  autoRebuild: boolean;
+  enableDelta: boolean;
+  disable: boolean;
+  ocspDisable: boolean;
+}
+
+export default class PkiConfigurationEditComponent extends Component<Args> {
+  @service declare readonly router: RouterService;
+  @service declare readonly flashMessages: FlashMessageService;
+
+  @tracked invalidFormAlert = null;
+  @tracked errorMessage = null;
+
+  @task
+  @waitFor
+  *save(event: Event) {
+    yield event.preventDefault(); // remove yield
+    // do something
+  }
+
+  @action
+  cancel() {
+    // handle cancel
+  }
+
+  @action
+  handleTtl(attr: FormField, e: TtlEvent) {
+    const { enabled, goSafeTimeString } = e;
+    const ttlAttr = attr.name;
+    this.args.crl[ttlAttr as keyof PkiCrlTtls] = goSafeTimeString;
+    this.args.crl[attr.options.booleanBuddy as keyof PkiCrlBooleans] = enabled;
+  }
+}
