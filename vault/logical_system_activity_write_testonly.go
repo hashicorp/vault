6cd87630c84a0b7ec6864dfd43ffe04610474e84
Mia Epner <mia.epner@hashicorp.com>
2023-04-19 15:38:55 +0200

    first part of segment client generation

--- vault/logical_system_activity_write_testonly.go
+++ vault/logical_system_activity_write_testonly.go
@@ -7,6 +7,12 @@ package vault
 
 import (
 	"context"
+	"errors"
+	"fmt"
+
+	"github.com/hashicorp/go-uuid"
+	"github.com/hashicorp/vault/helper/namespace"
+	"github.com/hashicorp/vault/vault/activity"
 
 	"github.com/hashicorp/vault/sdk/framework"
 	"github.com/hashicorp/vault/sdk/logical"
@@ -50,4 +56,134 @@ func (b *SystemBackend) handleActivityWriteData(ctx context.Context, request *lo
 		return logical.ErrorResponse("Missing required \"data\" values"), logical.ErrInvalidRequest
 	}
 	return nil, nil
+}
+
+// singleMonthActivityClients holds a single month's client IDs, in the order they were seen
+type singleMonthActivityClients struct {
+	// clients are indexed by ID
+	clients []string
+	// allClients contains all clients from all months
+	allClients map[string]*activity.EntityRecord
+}
+
+// multipleMonthsActivityClients holds multiple month's data
+type multipleMonthsActivityClients struct {
+	// months are in order, with month 0 being the current month and index 1 being 1 month ago
+	months     []*singleMonthActivityClients
+	allClients map[string]*activity.EntityRecord
+}
+
+// addNewClients generates clients according to the given parameters, and adds them to the month
+// if the namespace is empty, the client will be added to the defaultNamespace
+// the client will always have the actualMount as its mount accessor
+func (m *singleMonthActivityClients) addNewClients(c *generation.Client, defaultNamespace, actualMount string) error {
+	count := 1
+	if c.Count > 1 {
+		count = int(c.Count)
+	}
+	for i := 0; i < count; i++ {
+		record := &activity.EntityRecord{
+			ClientID:      c.Id,
+			NamespaceID:   c.Namespace,
+			NonEntity:     c.NonEntity,
+			MountAccessor: actualMount,
+		}
+		if record.ClientID == "" {
+			var err error
+			record.ClientID, err = uuid.GenerateUUID()
+			if err != nil {
+				return err
+			}
+		}
+		if record.NamespaceID == "" {
+			record.NamespaceID = defaultNamespace
+		}
+		m.allClients[record.ClientID] = record
+		seen := 1
+		if c.TimesSeen > 1 {
+			seen = int(c.TimesSeen)
+		}
+		for j := 0; j < seen; j++ {
+			m.clients = append(m.clients, record.ClientID)
+		}
+	}
+	return nil
+}
+
+// processMonth populates a month of client data
+func (m *multipleMonthsActivityClients) processMonth(ctx context.Context, core *Core, month *generation.Data) error {
+	if month.GetAll() == nil {
+		return errors.New("segmented monthly data is not yet supported")
+	}
+
+	// default to using the root namespace and the first mount on the root namespace
+	defaultNamespace := namespace.RootNamespaceID
+	mounts, err := core.ListMounts()
+	if err != nil {
+		return err
+	}
+	defaultMount := ""
+	for _, mount := range mounts {
+		if mount.NamespaceID == defaultNamespace {
+			defaultMount = mount.Accessor
+			break
+		}
+	}
+	if defaultMount == "" {
+		return fmt.Errorf("no mounts found in namespace %s", defaultNamespace)
+	}
+	addingTo := m.months[month.GetMonthsAgo()]
+
+	for _, clients := range month.GetAll().Clients {
+		if clients.Repeated || clients.RepeatedFromMonth > 0 {
+			return errors.New("repeated clients are not yet supported")
+		}
+
+		mountAccessor := defaultMount
+		if clients.Namespace != "" {
+			mountAccessor = ""
+			// verify that the namespace exists, if the input data has specified one
+			ns, err := core.NamespaceByID(ctx, clients.Namespace)
+			if err != nil {
+				return err
+			}
+			if clients.Mount != "" {
+				// verify the mount exists, if the input data has specified one
+				nctx := namespace.ContextWithNamespace(ctx, ns)
+				mountEntry := core.router.MatchingMountEntry(nctx, clients.Mount)
+				if mountEntry != nil {
+					mountAccessor = mountEntry.Accessor
+				}
+			} else if clients.Namespace != defaultNamespace {
+				// if we're not using the root namespace, find a mount on the namespace that we are using
+				for _, mount := range mounts {
+					if mount.NamespaceID == clients.Namespace {
+						mountAccessor = mount.Accessor
+						break
+					}
+				}
+			} else {
+				mountAccessor = defaultMount
+			}
+			if mountAccessor == "" {
+				return fmt.Errorf("unable to find matching mount in namespace %s", clients.Namespace)
+			}
+		}
+		err := addingTo.addNewClients(clients, defaultNamespace, mountAccessor)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func newMultipleMonthsActivityClients(numberOfMonths int) *multipleMonthsActivityClients {
+	m := &multipleMonthsActivityClients{
+		months:     make([]*singleMonthActivityClients, numberOfMonths),
+		allClients: make(map[string]*activity.EntityRecord),
+	}
+	for i := 0; i < numberOfMonths; i++ {
+		m.months[i] = &singleMonthActivityClients{allClients: m.allClients}
+	}
+	return m
 }
--- vault/logical_system_activity_write_testonly_test.go
+++ vault/logical_system_activity_write_testonly_test.go
@@ -6,10 +6,13 @@
 package vault
 
 import (
+	"context"
 	"testing"
 
 	"github.com/hashicorp/vault/helper/namespace"
 	"github.com/hashicorp/vault/sdk/logical"
+	"github.com/hashicorp/vault/vault/activity"
+	"github.com/hashicorp/vault/vault/activity/generation"
 	"github.com/stretchr/testify/require"
 )
 
@@ -78,6 +81,190 @@ func TestSystemBackend_handleActivityWriteData(t *testing.T) {
 				require.Equal(t, tc.wantError, err, resp.Error())
 			} else {
 				require.NoError(t, err)
+			}
+		})
+	}
+}
+
+// Test_singleMonthActivityClients_addNewClients verifies that new clients are created correctly, adhering to the
+// requested parameters. The clients should have the correct namespace and mount, replaced with the default if the input
+// values are empty. The clients should have a generated ID if one is not supplied. The new client should be added to
+// the month's `clients` slice and `allClients` map the correct number of times
+func Test_singleMonthActivityClients_addNewClients(t *testing.T) {
+	tests := []struct {
+		name          string
+		namespace     string
+		mount         string
+		clients       *generation.Client
+		wantNamespace string
+		wantMount     string
+		wantID        string
+	}{
+		{
+			name:          "default mount and namespace are used",
+			namespace:     "default_ns",
+			mount:         "default_mount",
+			wantNamespace: "default_ns",
+			wantMount:     "default_mount",
+			clients:       &generation.Client{},
+		},
+		{
+			name:          "record namespace is used, default mount is used",
+			namespace:     "default_ns",
+			mount:         "default_mount",
+			wantNamespace: "ns",
+			wantMount:     "default_mount",
+			clients: &generation.Client{
+				Namespace: "ns",
+				Mount:     "mount",
+			},
+		},
+		{
+			name: "predefined ID is used",
+			clients: &generation.Client{
+				Id: "client_id",
+			},
+			wantID: "client_id",
+		},
+		{
+			name: "non zero times seen",
+			clients: &generation.Client{
+				TimesSeen: 5,
+			},
+		},
+		{
+			name: "non zero count",
+			clients: &generation.Client{
+				Count: 5,
+			},
+		},
+		{
+			name: "non zero times seen and count",
+			clients: &generation.Client{
+				Count:     5,
+				TimesSeen: 3,
+			},
+		},
+		{
+			name: "non entity client",
+			clients: &generation.Client{
+				NonEntity: true,
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			m := &singleMonthActivityClients{
+				allClients: make(map[string]*activity.EntityRecord),
+			}
+			err := m.addNewClients(tt.clients, tt.namespace, tt.mount)
+			require.NoError(t, err)
+			numNew := tt.clients.Count
+			if numNew == 0 {
+				numNew = 1
+			}
+			numSeen := tt.clients.TimesSeen
+			if numSeen == 0 {
+				numSeen = 1
+			}
+			require.Len(t, m.allClients, int(numNew))
+			require.Len(t, m.clients, int(numNew*numSeen))
+			for _, c := range m.clients {
+				rec := m.allClients[c]
+				require.NotNil(t, rec)
+				require.Equal(t, tt.wantNamespace, rec.NamespaceID)
+				require.Equal(t, tt.wantMount, rec.MountAccessor)
+				require.Equal(t, tt.clients.NonEntity, rec.NonEntity)
+				if tt.wantID != "" {
+					require.Equal(t, tt.wantID, rec.ClientID)
+				} else {
+					require.NotEqual(t, "", rec.ClientID)
+				}
+			}
+		})
+	}
+}
+
+// Test_multipleMonthsActivityClients_processMonth verifies that a month of data is added correctly. The test checks
+// that default values are handled correctly for mounts and namespaces.
+func Test_multipleMonthsActivityClients_processMonth(t *testing.T) {
+	core, _, _ := TestCoreUnsealed(t)
+	tests := []struct {
+		name      string
+		clients   *generation.Data
+		wantError bool
+		numMonths int
+	}{
+		{
+			name: "specified namespace and mount exist",
+			clients: &generation.Data{
+				Clients: &generation.Data_All{All: &generation.Clients{Clients: []*generation.Client{{
+					Namespace: namespace.RootNamespaceID,
+					Mount:     "identity/",
+				}}}},
+			},
+			numMonths: 1,
+		},
+		{
+			name: "specified namespace exists, mount empty",
+			clients: &generation.Data{
+				Clients: &generation.Data_All{All: &generation.Clients{Clients: []*generation.Client{{
+					Namespace: namespace.RootNamespaceID,
+				}}}},
+			},
+			numMonths: 1,
+		},
+		{
+			name: "empty namespace and mount",
+			clients: &generation.Data{
+				Clients: &generation.Data_All{All: &generation.Clients{Clients: []*generation.Client{{}}}},
+			},
+			numMonths: 1,
+		},
+		{
+			name: "namespace doesn't exist",
+			clients: &generation.Data{
+				Clients: &generation.Data_All{All: &generation.Clients{Clients: []*generation.Client{{
+					Namespace: "abcd",
+				}}}},
+			},
+			wantError: true,
+			numMonths: 1,
+		},
+		{
+			name: "namespace exists, mount doesn't exist",
+			clients: &generation.Data{
+				Clients: &generation.Data_All{All: &generation.Clients{Clients: []*generation.Client{{
+					Namespace: namespace.RootNamespaceID,
+					Mount:     "mount",
+				}}}},
+			},
+			wantError: true,
+			numMonths: 1,
+		},
+		{
+			name: "older month",
+			clients: &generation.Data{
+				Month:   &generation.Data_MonthsAgo{MonthsAgo: 4},
+				Clients: &generation.Data_All{All: &generation.Clients{Clients: []*generation.Client{{}}}},
+			},
+			numMonths: 5,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			m := newMultipleMonthsActivityClients(tt.numMonths)
+			err := m.processMonth(context.Background(), core, tt.clients)
+			if tt.wantError {
+				require.Error(t, err)
+			} else {
+				require.NoError(t, err)
+				require.Len(t, m.allClients, len(tt.clients.GetAll().Clients))
+				require.Len(t, m.months[tt.clients.GetMonthsAgo()].clients, len(tt.clients.GetAll().Clients))
+				for _, c := range m.allClients {
+					require.NotEmpty(t, c.NamespaceID)
+					require.NotEmpty(t, c.MountAccessor)
+				}
 			}
 		})
 	}
