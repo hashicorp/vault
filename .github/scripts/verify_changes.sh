9a599b3611bdd1cbaf98716e9a65aeb7a03eb3d2
Jaymala Sinha <jaymala@hashicorp.com>
2023-04-20 16:43:04 -0400

    Add comments and fix CI errors
    Signed-off-by: Jaymala Sinha <jaymala@hashicorp.com>

--- .github/scripts/verify_changes.sh
+++ .github/scripts/verify_changes.sh
@@ -1,20 +1,28 @@
 #!/bin/bash
+#  This script validates if the git diff contains on docs changes
 
-event_type=$1
-base_ref=$2
+event_type=$1 # GH event type (pull_request)
+ref_name=$2 # branch reference that triggered the workflow
+head_ref=$3 # PR branch head ref
+base_ref=$4 # PR branch base ref
 
 changed_dir=""
 
-if [[ "$1" == "pull_request" ]]; then
-  change_count=$(git diff --merge-base $2 --name-only |xargs dirname |awk -F"/" '{ print $1}' |uniq |wc -l)
-  if [[ $change_count -eq 1 ]]; then
-    changed_dir=$(git diff --merge-base $2 --name-only |xargs dirname |awk -F"/" '{ print $1}' |uniq)
-  fi
+if [[ "$event_type" == "pull_request" ]]; then
+  git fetch --no-tags --prune origin $head_ref
+  git fetch --no-tags --prune origin $base_ref
+  head_commit="origin/$head_ref"
+  prev_commit="origin/$base_ref"
 else
-  change_count=$(git show --oneline --dirstat=files,0 |grep "%" |awk '{print $2}' |awk -F"/" '{print $1}' |uniq |wc -l)
-  if [[ $change_count -eq 1 ]]; then
-    changed_dir=$(git show --oneline --dirstat=files,0 |grep "%" |awk '{print $2}' |awk -F"/" '{print $1}' |uniq)
-  fi
+  git fetch --no-tags --prune origin $ref_name
+  head_commit=$(git log origin/$ref_name --oneline | head -1 | awk '{print $1}')
+  prev_commit=$(git log origin/$ref_name --oneline | head -2 | awk 'NR==2 {print $1}')
+fi
+
+change_count=$(git diff $head_commit..$prev_commit --name-only | awk -F"/" '{ print $1}' | uniq | wc -l)
+
+if [[ $change_count -eq 1 ]]; then
+  changed_dir=$(git diff $head_commit..$prev_commit --name-only | awk -F"/" '{ print $1}' | uniq)
 fi
 
 if [[ "$changed_dir" == "website" ]]; then
