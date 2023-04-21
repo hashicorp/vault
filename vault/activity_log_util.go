79ccdf943b453eecbb8c6cf0e02f186bd60c3ca8
Mike Palmiotto <mike.palmiotto@hashicorp.com>
2023-04-21 12:39:12 -0400

    Refactor reporter for unseal setup

--- vault/activity_log_util.go
+++ vault/activity_log_util.go
@@ -5,7 +5,10 @@
 
 package vault
 
-import "context"
+import (
+	"context"
+	"time"
+)
 
 // sendCurrentFragment is a no-op on OSS
 func (a *ActivityLog) sendCurrentFragment(ctx context.Context) error {
@@ -13,4 +16,5 @@ func (a *ActivityLog) sendCurrentFragment(ctx context.Context) error {
 }
 
 // CensusReport is a no-op on OSS
-func (a *ActivityLog) CensusReport(_ctx context.Context, _ca *CensusAgent) {}
+func (a *ActivityLog) CensusReport(_ctx context.Context, _reporter CensusReporter, _billingStart time.Time) {
+}
