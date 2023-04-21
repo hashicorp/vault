766e3909eb40c8a5457ff27ac46fc7c1003e2dd6
Chelsea Shaw <cshaw@hashicorp.com>
2023-04-21 14:58:40 -0500

    Fix test helper import

--- ui/tests/integration/helpers/sanitized-html-test.js
+++ ui/tests/integration/helpers/sanitized-html-test.js
@@ -1,5 +1,5 @@
 import { module, test } from 'qunit';
-import { setupRenderingTest } from 'vault/tests/helpers';
+import { setupRenderingTest } from 'ember-qunit';
 import { render } from '@ember/test-helpers';
 import { hbs } from 'ember-cli-htmlbars';
 
