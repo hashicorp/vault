77f83d9fe8b85c126347794a460410c2025675fd
Mike Palmiotto <mike.palmiotto@hashicorp.com>
2023-04-21 15:29:37 -0400

    Refactor reporter for unseal setup (#20296)

--- vault/core.go
+++ vault/core.go
@@ -636,10 +636,13 @@ type Core struct {
 	activityLogConfig ActivityLogCoreConfig
 
 	// censusAgent is the mechanism used for reporting Vault's billing data.
-	censusAgent *CensusAgent
+	censusAgent CensusReporter
 
 	// censusLicensingEnabled records whether Vault is exporting census metrics
 	censusLicensingEnabled bool
+
+	// billingStart keeps track of the billing start time for exporting census metrics
+	billingStart time.Time
 
 	// activeTime is set on active nodes indicating the time at which this node
 	// became active.
@@ -812,7 +815,7 @@ type CoreConfig struct {
 	LicensingConfig *LicensingConfig
 
 	// Configured Census Agent
-	censusAgent *CensusAgent
+	CensusAgent CensusReporter
 
 	DisablePerformanceStandby bool
 	DisableIndexing           bool
@@ -2362,6 +2365,11 @@ func (s standardUnsealStrategy) unseal(ctx context.Context, logger log.Logger, c
 		if err := c.setupAuditedHeadersConfig(ctx); err != nil {
 			return err
 		}
+
+		if err := c.setupCensusAgent(); err != nil {
+			c.logger.Error("skipping reporting for nil agent", "error", err)
+		}
+
 		// not waiting on wg to avoid changing existing behavior
 		var wg sync.WaitGroup
 		if err := c.setupActivityLog(ctx, &wg); err != nil {
@@ -4006,15 +4014,4 @@ func (c *Core) GetRaftAutopilotState(ctx context.Context) (*raft.AutopilotState,
 // Events returns a reference to the common event bus for sending and subscribint to events.
 func (c *Core) Events() *eventbus.EventBus {
 	return c.events
-}
-
-// GetBillingStart gets the billing start timestamp from the configured Census
-// Agent, handling a nil agent.
-func (c *Core) GetBillingStart() time.Time {
-	var billingStart time.Time
-	if c.censusAgent != nil {
-		billingStart = c.censusAgent.billingStart
-	}
-
-	return billingStart
 }
