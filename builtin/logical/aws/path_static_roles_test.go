7e9868a37766a76e7f4ec32b60a59bdf9c74238e
Kay Craig <kay.craig@hashicorp.com>
2023-04-24 12:01:45 -0400

    validate static role values, add a test, some placeholder descriptions

---
+++ builtin/logical/aws/path_static_roles_test.go
@@ -0,0 +1,66 @@
+package aws
+
+import (
+	"context"
+	"errors"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/service/iam"
+	"github.com/hashicorp/go-secure-stdlib/awsutil"
+	"github.com/hashicorp/vault/sdk/logical"
+	"testing"
+	"time"
+)
+
+func TestStaticRolesValidation(t *testing.T) {
+	config := logical.TestBackendConfig()
+	config.StorageView = &logical.InmemStorage{}
+
+	goodUser := &awsutil.MockIAM{
+		GetUserOutput: &iam.GetUserOutput{
+			User: &iam.User{
+				UserName: aws.String("jane-doe"),
+			},
+		},
+	}
+
+	badUser := &awsutil.MockIAM{
+		GetUserError: errors.New("oh no"),
+	}
+
+	b := Backend()
+	b.iamClient = goodUser
+	if err := b.Setup(context.Background(), config); err != nil {
+		t.Fatal(err)
+	}
+
+	roleData := map[string]interface{}{
+		"username":        "jane-doe",
+		"rotation_period": 24601,
+	}
+
+	roleReq := &logical.Request{
+		Operation: logical.UpdateOperation,
+		Storage:   config.StorageView,
+		Data:      roleData,
+		Path:      "static-roles/test",
+	}
+
+	// everything good
+	err := b.validateIAMUserExists(context.Background(), roleReq, "jane-doe")
+	if err != nil {
+		t.Fatalf("couldn't validate user: %s", err)
+	}
+
+	// bad user
+	b.iamClient = badUser
+	err = b.validateIAMUserExists(context.Background(), roleReq, "jane-doe")
+	if err == nil {
+		t.Fatalf("expected an IAM get user error but didn't get one")
+	}
+
+	// bad duration
+	err = b.validateRotationPeriod(time.Duration(0))
+	if err == nil {
+		t.Fatalf("expected duration to be invalid but it was accepted")
+	}
+}
