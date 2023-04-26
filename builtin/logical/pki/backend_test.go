76980f9e0061faf4356d87dd9395fd937c832173
Alexander Scheel <alex.scheel@hashicorp.com>
2023-04-26 12:48:29 -0400

    Ensure ACME configuration endpoints are authed
    Signed-off-by: Alexander Scheel <alex.scheel@hashicorp.com>

--- builtin/logical/pki/backend_test.go
+++ builtin/logical/pki/backend_test.go
@@ -6714,6 +6714,7 @@ func TestProperAuthing(t *testing.T) {
 		"certs/revoked":                          shouldBeAuthed,
 		"certs/revocation-queue":                 shouldBeAuthed,
 		"certs/unified-revoked":                  shouldBeAuthed,
+		"config/acme":                            shouldBeAuthed,
 		"config/auto-tidy":                       shouldBeAuthed,
 		"config/ca":                              shouldBeAuthed,
 		"config/cluster":                         shouldBeAuthed,
