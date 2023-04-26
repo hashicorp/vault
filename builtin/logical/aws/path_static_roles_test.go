b3277c9caa84825058184f67b38bb9fe6f8de361
Kay Craig <kay.craig@hashicorp.com>
2023-04-26 16:05:21 -0400

    modify test to use ListAccessKeys mock

--- builtin/logical/aws/path_static_roles_test.go
+++ builtin/logical/aws/path_static_roles_test.go
@@ -2,31 +2,60 @@ package aws
 
 import (
 	"context"
-	"errors"
 	"testing"
-	"time"
-
+	
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/service/iam"
+	"github.com/aws/aws-sdk-go/service/iam/iamiface"
 	"github.com/hashicorp/go-secure-stdlib/awsutil"
+	"github.com/hashicorp/vault/sdk/framework"
 	"github.com/hashicorp/vault/sdk/logical"
 )
+
+type extendedMockIAM struct {
+	iamiface.IAMAPI
+
+	ListAccessKeysOutput *iam.ListAccessKeysOutput
+	ListAccessKeysError  error
+}
+
+// Hang a new mock function off of a temporary test struct until the mock dependency we're using gets updated
+func (e *extendedMockIAM) ListAccessKeys(*iam.ListAccessKeysInput) (*iam.ListAccessKeysOutput, error) {
+	if e.ListAccessKeysError != nil {
+		return nil, e.ListAccessKeysError
+	}
+
+	return e.ListAccessKeysOutput, nil
+}
 
 func TestStaticRolesValidation(t *testing.T) {
 	config := logical.TestBackendConfig()
 	config.StorageView = &logical.InmemStorage{}
 
-	goodUser := &awsutil.MockIAM{
-		GetUserOutput: &iam.GetUserOutput{
-			User: &iam.User{
-				UserName: aws.String("jane-doe"),
+	mockIAM, err := awsutil.NewMockIAM(
+		awsutil.WithGetUserOutput(&iam.GetUserOutput{User: &iam.User{UserName: aws.String("jane-doe")}}),
+		awsutil.WithCreateAccessKeyOutput(&iam.CreateAccessKeyOutput{
+			AccessKey: &iam.AccessKey{
+				AccessKeyId:     aws.String("abcdefghijklmnopqrstuvwxyz"),
+				SecretAccessKey: aws.String("zyxwvutsrqponmlkjihgfedcba"),
+				UserName:        aws.String("jane-doe"),
 			},
+		}),
+	)(nil)
+
+	goodUser := &extendedMockIAM{
+		IAMAPI: mockIAM,
+		ListAccessKeysOutput: &iam.ListAccessKeysOutput{
+			AccessKeyMetadata: []*iam.AccessKeyMetadata{},
+			IsTruncated:       aws.Bool(false),
 		},
 	}
 
-	badUser := &awsutil.MockIAM{
-		GetUserError: errors.New("oh no"),
-	}
+	//_ = &extendedMockIAM{
+	//	MockIAM: awsutil.MockIAM{
+	//		GetUserError: errors.New("oh no"),
+	//	},
+	//}
 
 	b := Backend()
 	b.iamClient = goodUser
@@ -35,6 +64,7 @@ func TestStaticRolesValidation(t *testing.T) {
 	}
 
 	roleData := map[string]interface{}{
+		"name":            "test",
 		"username":        "jane-doe",
 		"rotation_period": 24601,
 	}
@@ -47,21 +77,49 @@ func TestStaticRolesValidation(t *testing.T) {
 	}
 
 	// everything good
-	err := b.validateIAMUserExists(context.Background(), roleReq, "jane-doe")
+	//err := b.validateIAMUserExists(context.Background(), roleReq, "jane-doe")
+	//if err != nil {
+	//	t.Fatalf("couldn't validate user: %s", err)
+	//}
+	resp, err := b.pathStaticRolesWrite(context.Background(), roleReq, staticRoleFieldData(roleData))
 	if err != nil {
-		t.Fatalf("couldn't validate user: %s", err)
+		t.Fatalf("couldn't validate an expected good request: %s", err)
+	}
+	if resp == nil {
+		t.Fatalf("didn't get a response from an expected good request")
+	}
+	//// bad user
+	//b.iamClient = badUser
+	//err = b.validateIAMUserExists(context.Background(), roleReq, "jane-doe")
+	//if err == nil {
+	//	t.Fatalf("expected an IAM get user error but didn't get one")
+	//}
+	//
+	//// bad duration
+	//err = b.validateRotationPeriod(time.Duration(0))
+	//if err == nil {
+	//	t.Fatalf("expected duration to be invalid but it was accepted")
+	//}
+}
+
+func staticRoleFieldData(data map[string]interface{}) *framework.FieldData {
+	schema := map[string]*framework.FieldSchema{
+		paramRoleName: {
+			Type:        framework.TypeString,
+			Description: descRoleName,
+		},
+		paramUsername: {
+			Type:        framework.TypeString,
+			Description: descUsername,
+		},
+		paramRotationPeriod: {
+			Type:        framework.TypeDurationSecond,
+			Description: descRotationPeriod,
+		},
 	}
 
-	// bad user
-	b.iamClient = badUser
-	err = b.validateIAMUserExists(context.Background(), roleReq, "jane-doe")
-	if err == nil {
-		t.Fatalf("expected an IAM get user error but didn't get one")
-	}
-
-	// bad duration
-	err = b.validateRotationPeriod(time.Duration(0))
-	if err == nil {
-		t.Fatalf("expected duration to be invalid but it was accepted")
+	return &framework.FieldData{
+		Raw:    data,
+		Schema: schema,
 	}
 }
