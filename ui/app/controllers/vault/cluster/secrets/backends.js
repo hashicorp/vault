7f39915f10da1d98f5e835e02e46c39015449798
Angel Garbarino <argarbarino@gmail.com>
2023-05-01 14:28:09 -0600

    initial WIP glimmerize the controller

--- ui/app/controllers/vault/cluster/secrets/backends.js
+++ ui/app/controllers/vault/cluster/secrets/backends.js
@@ -2,36 +2,36 @@
  * Copyright (c) HashiCorp, Inc.
  * SPDX-License-Identifier: MPL-2.0
  */
-
-import { filterBy } from '@ember/object/computed';
-import { computed } from '@ember/object';
+/* eslint ember/no-computed-properties-in-native-classes: 'warn' */
 import Controller from '@ember/controller';
-import { task } from 'ember-concurrency';
-import { supportedSecretBackends } from 'vault/helpers/supported-secret-backends';
 import { inject as service } from '@ember/service';
+import { action } from '@ember/object';
+import { tracked } from '@glimmer/tracking';
+import { filterBy } from '@ember/object/computed';
+import { dropTask, task } from 'ember-concurrency';
+import { supportedSecretBackends } from 'vault/helpers/supported-secret-backends';
+
 const LINKED_BACKENDS = supportedSecretBackends();
 
-export default Controller.extend({
-  flashMessages: service(),
-  displayableBackends: filterBy('model', 'shouldIncludeInList'),
+export default class VaultClusterSecretsBackendController extends Controller {
+  @service flashMessages;
+  @filterBy('model', 'shouldIncludeInList') displayableBackends;
 
-  supportedBackends: computed('displayableBackends', 'displayableBackends.[]', function () {
+  @tracked secretEngineOptions = [];
+
+  get supportedBackends() {
     return (this.displayableBackends || [])
       .filter((backend) => LINKED_BACKENDS.includes(backend.get('engineType')))
       .sortBy('id');
-  }),
+  }
 
-  unsupportedBackends: computed(
-    'displayableBackends',
-    'displayableBackends.[]',
-    'supportedBackends',
-    'supportedBackends.[]',
-    function () {
-      return (this.displayableBackends || []).slice().removeObjects(this.supportedBackends).sortBy('id');
-    }
-  ),
+  get unsupportedBackends() {
+    return (this.displayableBackends || []).slice().removeObjects(this.supportedBackends).sortBy('id');
+  }
 
-  disableEngine: task(function* (engine) {
+  @task
+  @dropTask
+  *disableEngine(engine) {
     const { engineType, path } = engine;
     try {
       yield engine.destroyRecord();
@@ -41,5 +41,22 @@ export default Controller.extend({
         `There was an error disabling the ${engineType} Secrets Engine at ${path}: ${err.errors.join(' ')}.`
       );
     }
-  }).drop(),
-});
+  }
+
+  @action
+  selectEngineType([engine]) {
+    this.engineType = engine;
+    if (!engine) {
+      this.secretEngineOptions = [];
+      // // on clear, also make sure auth method is cleared
+      // this.selectedAuthMethod = null;
+    } else {
+      // Side effect: set auth namespaces
+      // const mounts = this.filteredActivityByNamespace.mounts?.map((mount) => ({
+      //   id: mount.label,
+      //   name: mount.label,
+      // }));
+      this.secretEngineOptions = ['Test', 'test'];
+    }
+  }
+}
