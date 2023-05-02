82b46d0eccafb678fb84d280958001874ea6efd0
Mike Palmiotto <mike.palmiotto@hashicorp.com>
2023-05-02 11:17:13 -0400

    Capture loop var in TestClientDisabledRedirects

--- api/client_test.go
+++ api/client_test.go
@@ -225,6 +225,7 @@ func TestClientDisableRedirects(t *testing.T) {
 
 	for name, tc := range tests {
 		test := tc
+		name := name
 		t.Run(name, func(t *testing.T) {
 			t.Parallel()
 			numReqs := 0
