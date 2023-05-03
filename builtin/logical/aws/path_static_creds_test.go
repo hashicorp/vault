52e1054a64d0b74ac1a3ccb1926eaca044d01f31
Kay Craig <kay.craig@hashicorp.com>
2023-05-03 17:10:19 -0400

    table-driven

--- builtin/logical/aws/path_static_creds_test.go
+++ builtin/logical/aws/path_static_creds_test.go
@@ -10,6 +10,7 @@ import (
 )
 
 func TestStaticCredsRead(t *testing.T) {
+	// setup
 	config := logical.TestBackendConfig()
 	config.StorageView = &logical.InmemStorage{}
 
@@ -27,21 +28,44 @@ func TestStaticCredsRead(t *testing.T) {
 		t.Fatal(err)
 	}
 
-	b := Backend()
-
-	req := &logical.Request{
-		Storage: config.StorageView,
-		Data: map[string]interface{}{
-			"name": "test",
+	// cases
+	cases := []struct {
+		name           string
+		roleName       string
+		expectError    bool
+		expectResponse bool
+	}{
+		{
+			name:           "get existing creds",
+			roleName:       "test",
+			expectResponse: true,
+		},
+		{
+			name:     "get non-existent creds",
+			roleName: "this-doesnt-exist",
+			// returns nil, nil
 		},
 	}
-	resp, err := b.pathStaticCredsRead(context.Background(), req, staticCredsFieldData(req.Data))
-	if err != nil {
-		t.Fatal(err)
-	}
-	// resp will have credentials
-	if resp == nil {
-		t.Fatal("expected a non-nil response, but it was nil")
+
+	for _, c := range cases {
+		t.Run(c.name, func(t *testing.T) {
+
+			b := Backend()
+
+			req := &logical.Request{
+				Storage: config.StorageView,
+				Data: map[string]interface{}{
+					"name": c.roleName,
+				},
+			}
+			resp, err := b.pathStaticCredsRead(context.Background(), req, staticCredsFieldData(req.Data))
+			if (c.expectError && (err == nil)) || (!c.expectError && (err != nil)) {
+				t.Fatal(err)
+			}
+			if (c.expectResponse && (resp == nil)) || (!c.expectResponse && (resp != nil)) {
+				t.Fatal("expected a non-nil response, but it was nil")
+			}
+		})
 	}
 }
 
