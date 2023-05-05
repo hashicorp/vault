a8e32ab32e4d94b15eab270ed09b40bdbbc69a9c
Kianna Quach <kianna.quach@hashicorp.com>
2023-05-05 09:47:44 -0700

    Convert pki-key-import to ts

--- ui/lib/pki/addon/components/pki-key-import.ts
+++ ui/lib/pki/addon/components/pki-key-import.ts
@@ -11,6 +11,8 @@ import { inject as service } from '@ember/service';
 import errorMessage from 'vault/utils/error-message';
 import trimRight from 'vault/utils/trim-right';
 import { waitFor } from '@ember/test-waiters';
+import PkiKeyModel from 'vault/models/pki/key';
+import FlashMessageService from 'vault/services/flash-messages';
 
 /**
  * @module PkiKeyImport
@@ -25,16 +27,21 @@ import { waitFor } from '@ember/test-waiters';
  * @callback onCancel - Callback triggered when cancel button is clicked.
  * @callback onSubmit - Callback triggered on submit success.
  */
+interface Args {
+  model: PkiKeyModel;
+  onSave(): CallableFunction;
+  onCancel(): CallableFunction;
+}
 
-export default class PkiKeyImport extends Component {
-  @service flashMessages;
+export default class PkiKeyImport extends Component<Args> {
+  @service declare readonly flashMessages: FlashMessageService;
 
-  @tracked errorBanner;
-  @tracked invalidFormAlert;
+  @tracked errorBanner = '';
+  @tracked invalidFormAlert = '';
 
   @task
   @waitFor
-  *submitForm(event) {
+  *submitForm(event: Event) {
     event.preventDefault();
     try {
       const { keyName } = this.args.model;
@@ -48,7 +55,7 @@ export default class PkiKeyImport extends Component {
   }
 
   @action
-  onFileUploaded({ value, filename }) {
+  onFileUploaded({ value, filename }: { value: string; filename: string }) {
     this.args.model.pemBundle = value;
     if (!this.args.model.keyName) {
       const trimmedFileName = trimRight(filename, ['.json', '.pem']);
