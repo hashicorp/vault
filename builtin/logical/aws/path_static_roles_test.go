1a71e9b3284d53344ae872e66147e4e84f10f46c
kpcraig <3031348+kpcraig@users.noreply.github.com>
2023-05-05 17:24:31 -0400

    Test coverage for static roles (#20487)

--- builtin/logical/aws/path_static_roles_test.go
+++ builtin/logical/aws/path_static_roles_test.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"errors"
 	"testing"
+	"time"
 
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/service/iam"
@@ -15,6 +16,7 @@ import (
 func TestStaticRolesValidation(t *testing.T) {
 	config := logical.TestBackendConfig()
 	config.StorageView = &logical.InmemStorage{}
+	bgCTX := context.Background() // for brevity
 
 	cases := []struct {
 		name        string
@@ -90,7 +92,7 @@ func TestStaticRolesValidation(t *testing.T) {
 				t.Fatal(err)
 			}
 			b.iamClient = miam
-			if err := b.Setup(context.Background(), config); err != nil {
+			if err := b.Setup(bgCTX, config); err != nil {
 				t.Fatal(err)
 			}
 			req := &logical.Request{
@@ -99,11 +101,245 @@ func TestStaticRolesValidation(t *testing.T) {
 				Data:      c.requestData,
 				Path:      "static-roles/test",
 			}
-			_, err = b.pathStaticRolesWrite(context.Background(), req, staticRoleFieldData(req.Data))
+			_, err = b.pathStaticRolesWrite(bgCTX, req, staticRoleFieldData(req.Data))
 			if c.isError && err == nil {
 				t.Fatal("expected an error but didn't get one")
 			} else if !c.isError && err != nil {
 				t.Fatalf("got an unexpected error: %s", err)
+			}
+		})
+	}
+}
+
+func TestStaticRolesWrite(t *testing.T) {
+	config := logical.TestBackendConfig()
+	config.StorageView = &logical.InmemStorage{}
+	bgCTX := context.Background()
+
+	cases := []struct {
+		name          string
+		opts          []awsutil.MockIAMOption
+		data          map[string]interface{}
+		expectedError bool
+		findUser      bool
+	}{
+		{
+			name: "happy path",
+			opts: []awsutil.MockIAMOption{
+				awsutil.WithGetUserOutput(&iam.GetUserOutput{User: &iam.User{UserName: aws.String("jane-doe")}}),
+				awsutil.WithListAccessKeysOutput(&iam.ListAccessKeysOutput{
+					AccessKeyMetadata: []*iam.AccessKeyMetadata{},
+					IsTruncated:       aws.Bool(false),
+				}),
+				awsutil.WithCreateAccessKeyOutput(&iam.CreateAccessKeyOutput{
+					AccessKey: &iam.AccessKey{
+						AccessKeyId:     aws.String("abcdefghijklmnopqrstuvwxyz"),
+						SecretAccessKey: aws.String("zyxwvutsrqponmlkjihgfedcba"),
+						UserName:        aws.String("jane-doe"),
+					},
+				}),
+			},
+			data: map[string]interface{}{
+				"name":            "test",
+				"username":        "jane-doe",
+				"rotation_period": "1d",
+			},
+			// writes role, writes cred
+			findUser: true,
+		},
+		{
+			name: "no aws user",
+			opts: []awsutil.MockIAMOption{
+				awsutil.WithGetUserError(errors.New("no such user, etc etc")),
+			},
+			data: map[string]interface{}{
+				"name":            "test",
+				"username":        "a-nony-mous",
+				"rotation_period": "15s",
+			},
+			expectedError: true,
+		},
+	}
+
+	// if a user exists (user doesn't exist is tested in validation)
+	// we'll check how many keys the user has - if it's two, we delete one.
+
+	for _, c := range cases {
+		t.Run(c.name, func(t *testing.T) {
+			miam, err := awsutil.NewMockIAM(
+				c.opts...,
+			)(nil)
+			if err != nil {
+				t.Fatal(err)
+			}
+
+			b := Backend()
+			b.iamClient = miam
+			if err := b.Setup(bgCTX, config); err != nil {
+				t.Fatal(err)
+			}
+
+			req := &logical.Request{
+				Operation: logical.UpdateOperation,
+				Storage:   config.StorageView,
+				Data:      c.data,
+				Path:      "static-roles/test",
+			}
+
+			r, err := b.pathStaticRolesWrite(bgCTX, req, staticRoleFieldData(req.Data))
+			if c.expectedError && err == nil {
+				t.Fatal(err)
+			}
+			if c.findUser && r == nil {
+				t.Fatal("response was nil, but it shouldn't have been")
+			}
+
+			role, err := config.StorageView.Get(bgCTX, req.Path)
+			if c.findUser && (err != nil || role == nil) {
+				t.Fatalf("couldn't find the role we should have stored: %s", err)
+			}
+		})
+	}
+}
+
+func TestStaticRoleRead(t *testing.T) {
+	config := logical.TestBackendConfig()
+	config.StorageView = &logical.InmemStorage{}
+	bgCTX := context.Background()
+
+	// test cases are run against an inmem storage holding a role called "test" attached to an IAM user called "jane-doe"
+	cases := []struct {
+		name     string
+		roleName string
+		found    bool
+	}{
+		{
+			name:     "role name exists",
+			roleName: "test",
+			found:    true,
+		},
+		{
+			name:     "role name not found",
+			roleName: "toast",
+			found:    false, // implied, but set for clarity
+		},
+	}
+
+	staticRole := staticRoleConfig{
+		Name:           "test",
+		Username:       "jane-doe",
+		RotationPeriod: 24 * time.Hour,
+	}
+	entry, err := logical.StorageEntryJSON(formatRoleStoragePath(staticRole.Name), staticRole)
+	if err != nil {
+		t.Fatal(err)
+	}
+	err = config.StorageView.Put(bgCTX, entry)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	for _, c := range cases {
+		t.Run(c.name, func(t *testing.T) {
+			req := &logical.Request{
+				Operation: logical.ReadOperation,
+				Storage:   config.StorageView,
+				Data: map[string]interface{}{
+					"name": c.roleName,
+				},
+				Path: formatRoleStoragePath(c.roleName),
+			}
+
+			b := Backend()
+
+			r, err := b.pathStaticRolesRead(bgCTX, req, staticRoleFieldData(req.Data))
+			if err != nil {
+				t.Fatal(err)
+			}
+			if c.found {
+				if r == nil {
+					t.Fatal("response was nil, but it shouldn't have been")
+				}
+			} else {
+				if r != nil {
+					t.Fatal("response should have been nil on a non-existent role")
+				}
+			}
+		})
+	}
+}
+
+func TestStaticRoleDelete(t *testing.T) {
+	bgCTX := context.Background()
+
+	// test cases are run against an inmem storage holding a role called "test" attached to an IAM user called "jane-doe"
+	cases := []struct {
+		name  string
+		role  string
+		found bool
+	}{
+		{
+			name:  "role found",
+			role:  "test",
+			found: true,
+		},
+		{
+			name:  "role not found",
+			role:  "tossed",
+			found: false,
+		},
+	}
+	for _, c := range cases {
+		t.Run(c.name, func(t *testing.T) {
+			config := logical.TestBackendConfig()
+			config.StorageView = &logical.InmemStorage{}
+
+			staticRole := staticRoleConfig{
+				Name:           "test",
+				Username:       "jane-doe",
+				RotationPeriod: 24 * time.Hour,
+			}
+			entry, err := logical.StorageEntryJSON(formatRoleStoragePath(staticRole.Name), staticRole)
+			if err != nil {
+				t.Fatal(err)
+			}
+			err = config.StorageView.Put(bgCTX, entry)
+			if err != nil {
+				t.Fatal(err)
+			}
+
+			l, err := config.StorageView.List(bgCTX, "")
+			if err != nil || len(l) != 1 {
+				t.Fatalf("couldn't add an entry to storage during test setup: %s", err)
+			}
+
+			req := &logical.Request{
+				Operation: logical.ReadOperation,
+				Storage:   config.StorageView,
+				Data: map[string]interface{}{
+					"name": c.role,
+				},
+				Path: formatRoleStoragePath(c.role),
+			}
+
+			b := Backend()
+
+			r, err := b.pathStaticRolesDelete(bgCTX, req, staticRoleFieldData(req.Data))
+			if err != nil {
+				t.Fatal(err)
+			}
+			if r != nil {
+				t.Fatal("response wasn't nil, but it should have been")
+			}
+
+			l, err = config.StorageView.List(bgCTX, "")
+			if err != nil {
+				t.Fatal(err)
+			}
+			if c.found && len(l) != 0 {
+				t.Fatal("size of role storage is non zero after delete")
+			} else if !c.found && len(l) != 1 {
+				t.Fatal("size of role storage changed after what should have been no deletion")
 			}
 		})
 	}
---
+++ builtin/logical/aws/rotation_test.go
@@ -0,0 +1,191 @@
+package aws
+
+import (
+	"context"
+	"testing"
+	"time"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/service/iam"
+	"github.com/hashicorp/go-secure-stdlib/awsutil"
+	"github.com/hashicorp/vault/sdk/logical"
+	"github.com/hashicorp/vault/sdk/queue"
+)
+
+func TestRotation(t *testing.T) {
+	bgCTX := context.Background()
+
+	type credToInsert struct {
+		config staticRoleConfig // role configuration from a normal createRole request
+		age    time.Duration    // how old the cred should be - if this is longer than the config.RotationPeriod,
+		// the cred is 'pre-expired'
+
+		changed bool // whether we expect the cred to change - this is technically redundant to a comparison between
+		// rotationPeriod and age.
+	}
+
+	cases := []struct {
+		name  string
+		creds []credToInsert
+	}{
+		{
+			name: "refresh one",
+			creds: []credToInsert{
+				{
+					config: staticRoleConfig{
+						Name:           "test",
+						Username:       "jane-doe",
+						RotationPeriod: 2 * time.Second,
+					},
+					age:     5 * time.Second,
+					changed: true,
+				},
+			},
+		},
+		{
+			name: "refresh none",
+			creds: []credToInsert{
+				{
+					config: staticRoleConfig{
+						Name:           "test",
+						Username:       "jane-doe",
+						RotationPeriod: 1 * time.Minute,
+					},
+					age:     5 * time.Second,
+					changed: false,
+				},
+			},
+		},
+		{
+			name: "refresh one of two",
+			creds: []credToInsert{
+				{
+					config: staticRoleConfig{
+						Name:           "test",
+						Username:       "jane-doe",
+						RotationPeriod: 1 * time.Minute,
+					},
+					age:     5 * time.Second,
+					changed: false,
+				},
+				{
+					config: staticRoleConfig{
+						Name:           "toast",
+						Username:       "john-doe",
+						RotationPeriod: 1 * time.Second,
+					},
+					age:     5 * time.Second,
+					changed: true,
+				},
+			},
+		},
+	}
+
+	ak := "long-access-key-id"
+	oldSecret := "abcdefghijklmnopqrstuvwxyz"
+	newSecret := "zyxwvutsrqponmlkjihgfedcba"
+
+	for _, c := range cases {
+		t.Run(c.name, func(t *testing.T) {
+			config := logical.TestBackendConfig()
+			config.StorageView = &logical.InmemStorage{}
+
+			b := Backend()
+			err := b.initQueue(bgCTX, nil)
+			if err != nil {
+				t.Fatalf("couldn't initialize queue: %s", err)
+			}
+
+			// this means the creds will be the same for every user, but that's okay
+			// since what we care about is whether they changed on a single-user basis.
+			miam, err := awsutil.NewMockIAM(
+				// blank list for existing user
+				awsutil.WithListAccessKeysOutput(&iam.ListAccessKeysOutput{
+					AccessKeyMetadata: []*iam.AccessKeyMetadata{
+						{},
+					},
+				}),
+				// initial key to store
+				awsutil.WithCreateAccessKeyOutput(&iam.CreateAccessKeyOutput{
+					AccessKey: &iam.AccessKey{
+						AccessKeyId:     aws.String(ak),
+						SecretAccessKey: aws.String(oldSecret),
+					},
+				}),
+			)(nil)
+			if err != nil {
+				t.Fatalf("couldn't initialze mock IAM handler: %s", err)
+			}
+
+			// insert all our creds
+			for i, cred := range c.creds {
+				b.iamClient = miam
+
+				err = b.createCredential(bgCTX, config.StorageView, cred.config)
+				if err != nil {
+					t.Fatalf("couldn't insert credential %d: %s", i, err)
+				}
+
+				item := &queue.Item{
+					Key:      cred.config.Name,
+					Value:    cred.config,
+					Priority: time.Now().Add(-1 * cred.age).Add(cred.config.RotationPeriod).Unix(),
+				}
+				err = b.credRotationQueue.Push(item)
+				if err != nil {
+					t.Fatalf("couldn't push item onto queue: %s", err)
+				}
+			}
+
+			// update aws responses, same argument for why it's okay every cred will be the same
+			miam, err = awsutil.NewMockIAM(
+				// old key
+				awsutil.WithListAccessKeysOutput(&iam.ListAccessKeysOutput{
+					AccessKeyMetadata: []*iam.AccessKeyMetadata{
+						{
+							AccessKeyId: aws.String(ak),
+						},
+					},
+				}),
+				// new key
+				awsutil.WithCreateAccessKeyOutput(&iam.CreateAccessKeyOutput{
+					AccessKey: &iam.AccessKey{
+						AccessKeyId:     aws.String(ak),
+						SecretAccessKey: aws.String(newSecret),
+					},
+				}),
+			)(nil)
+			if err != nil {
+				t.Fatalf("couldn't initialze mock IAM handler: %s", err)
+			}
+			b.iamClient = miam
+
+			req := &logical.Request{
+				Storage: config.StorageView,
+			}
+			err = b.rotateExpiredStaticCreds(bgCTX, req)
+			if err != nil {
+				t.Fatalf("got an error rotating credentials: %s", err)
+			}
+
+			// check our credentials
+			for i, cred := range c.creds {
+				entry, err := config.StorageView.Get(bgCTX, formatCredsStoragePath(cred.config.Name))
+				if err != nil {
+					t.Fatalf("got an error retrieving credentials %d", i)
+				}
+				var out awsCredentials
+				err = entry.DecodeJSON(&out)
+				if err != nil {
+					t.Fatalf("could not unmarshal storage view entry for cred %d to an aws credential: %s", i, err)
+				}
+
+				if cred.changed && out.SecretAccessKey != newSecret {
+					t.Fatalf("expected the key for cred %d to have changed, but it hasn't", i)
+				} else if !cred.changed && out.SecretAccessKey != oldSecret {
+					t.Fatalf("expected the key for cred %d to have stayed the same, but it changed", i)
+				}
+			}
+		})
+	}
+}
