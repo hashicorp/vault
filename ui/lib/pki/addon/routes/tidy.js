d931aaeae5481f3a71355151788e85922fda3f82
clairebontempo@gmail.com <clairebontempo@gmail.com>
2023-05-16 16:57:08 -0700

    tidy.index only needs controller empty state logic

--- ui/lib/pki/addon/routes/tidy.js
+++ ui/lib/pki/addon/routes/tidy.js
@@ -2,7 +2,6 @@ import Route from '@ember/routing/route';
 import { inject as service } from '@ember/service';
 import { withConfig } from 'pki/decorators/check-config';
 import { hash } from 'rsvp';
-import { PKI_DEFAULT_EMPTY_STATE_MSG } from 'pki/routes/overview';
 
 @withConfig()
 export default class PkiTidyRoute extends Route {
@@ -15,10 +14,5 @@ export default class PkiTidyRoute extends Route {
       engine,
       autoTidyConfig: this.store.queryRecord('pki/tidy', { backend: engine.id, tidyType: 'auto' }),
     });
-  }
-
-  setupController(controller, resolvedModel) {
-    super.setupController(controller, resolvedModel);
-    controller.notConfiguredMessage = PKI_DEFAULT_EMPTY_STATE_MSG;
   }
 }
