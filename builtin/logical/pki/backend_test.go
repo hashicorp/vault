d452a5e4cf72572f0a1d66c4ed34e87e20925153
John-Michael Faircloth <fairclothjm@users.noreply.github.com>
2023-05-18 11:49:22 -0500

    pki: add subject key identifier to read key response (#20642)
    * pki: add subject key identifier to read key response
    
    This will be helpful for the Terraform Vault Provider to detect
    migration of pre-1.11 exported keys (from CA generation) into post-1.11
    Vault.
    
    * add changelog
    
    * Update builtin/logical/pki/path_fetch_keys.go
    
    Co-authored-by: Alexander Scheel <alex.scheel@hashicorp.com>
    
    * check for managed key first
    
    * Validate the SKID matches on root CAs
    
    Signed-off-by: Alexander Scheel <alex.scheel@hashicorp.com>
    
    * Validate SKID matches on int CAs
    
    Signed-off-by: Alexander Scheel <alex.scheel@hashicorp.com>
    
    * Fix formatting of tests
    
    Signed-off-by: Alexander Scheel <alex.scheel@hashicorp.com>
    
    ---------
    
    Signed-off-by: Alexander Scheel <alex.scheel@hashicorp.com>
    Co-authored-by: Alexander Scheel <alex.scheel@hashicorp.com>

--- builtin/logical/pki/backend_test.go
+++ builtin/logical/pki/backend_test.go
@@ -2382,6 +2382,14 @@ func TestBackend_Root_Idempotency(t *testing.T) {
 	require.NotNil(t, resp, "expected ca info")
 	keyId1 := resp.Data["key_id"]
 	issuerId1 := resp.Data["issuer_id"]
+	cert := parseCert(t, resp.Data["certificate"].(string))
+	certSkid := certutil.GetHexFormatted(cert.SubjectKeyId, ":")
+
+	//  -> Validate the SKID matches between the root cert and the key
+	resp, err = CBRead(b, s, "key/"+keyId1.(keyID).String())
+	require.NoError(t, err)
+	require.NotNil(t, resp, "expected a response")
+	require.Equal(t, resp.Data["subject_key_id"], certSkid)
 
 	resp, err = CBRead(b, s, "cert/ca_chain")
 	require.NoError(t, err, "error reading ca_chain: %v", err)
@@ -2396,6 +2404,14 @@ func TestBackend_Root_Idempotency(t *testing.T) {
 	require.NotNil(t, resp, "expected ca info")
 	keyId2 := resp.Data["key_id"]
 	issuerId2 := resp.Data["issuer_id"]
+	cert = parseCert(t, resp.Data["certificate"].(string))
+	certSkid = certutil.GetHexFormatted(cert.SubjectKeyId, ":")
+
+	//  -> Validate the SKID matches between the root cert and the key
+	resp, err = CBRead(b, s, "key/"+keyId2.(keyID).String())
+	require.NoError(t, err)
+	require.NotNil(t, resp, "expected a response")
+	require.Equal(t, resp.Data["subject_key_id"], certSkid)
 
 	// Make sure that we actually generated different issuer and key values
 	require.NotEqual(t, keyId1, keyId2)
@@ -2501,11 +2517,18 @@ func TestBackend_SignIntermediate_AllowedPastCA(t *testing.T) {
 	resp, err := CBWrite(b_int, s_int, "intermediate/generate/internal", map[string]interface{}{
 		"common_name": "myint.com",
 	})
+	require.Contains(t, resp.Data, "key_id")
+	intKeyId := resp.Data["key_id"].(keyID)
+	csr := resp.Data["csr"]
+
+	resp, err = CBRead(b_int, s_int, "key/"+intKeyId.String())
+	require.NoError(t, err)
+	require.NotNil(t, resp, "expected a response")
+	intSkid := resp.Data["subject_key_id"].(string)
+
 	if err != nil {
 		t.Fatal(err)
 	}
-
-	csr := resp.Data["csr"]
 
 	_, err = CBWrite(b_root, s_root, "sign/test", map[string]interface{}{
 		"common_name": "myint.com",
@@ -2541,6 +2564,10 @@ func TestBackend_SignIntermediate_AllowedPastCA(t *testing.T) {
 	if len(resp.Warnings) == 0 {
 		t.Fatalf("expected warnings, got %#v", *resp)
 	}
+
+	cert := parseCert(t, resp.Data["certificate"].(string))
+	certSkid := certutil.GetHexFormatted(cert.SubjectKeyId, ":")
+	require.Equal(t, intSkid, certSkid)
 }
 
 func TestBackend_ConsulSignLeafWithLegacyRole(t *testing.T) {
