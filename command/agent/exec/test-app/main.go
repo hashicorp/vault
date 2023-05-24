f1f39a9742949a9ad5eb9b5377c86a2f708d81cf
Anton Averchenkov <anton.averchenkov@hashicorp.com>
2023-05-24 13:41:19 -0400

    fix up app

--- command/agent/exec/test-app/main.go
+++ command/agent/exec/test-app/main.go
@@ -17,19 +17,19 @@ import (
 )
 
 var (
-	port          uint
-	ignoreStopSig bool
-	ttl           time.Duration
-	useSigusr1    bool
-	stopAfter     time.Duration
-	exitCode      int
+	port                 uint
+	ignoreStopSignal     bool
+	sleepAfterStopSignal time.Duration
+	useSigusr1StopSignal bool
+	stopAfter            time.Duration
+	exitCode             int
 )
 
 func init() {
-	flag.UintVar(&port, "port", 8000, "port to run the sample app on")
-	flag.BoolVar(&ignoreStopSig, "ignore-stop-signal", false, "dont stop the server on SIGTERM")
-	flag.DurationVar(&ttl, "ttl", 5*time.Second, "time to wait after getting the signal before exiting (ignored if `ignore-stop-signal` is set)")
-	flag.BoolVar(&useSigusr1, "use-sigusr1", false, "use SIGUSR1 as the stop signal, instead of the default SIGTERM")
+	flag.UintVar(&port, "port", 34000, "port to run the test app on")
+	flag.BoolVar(&ignoreStopSignal, "ignore-stop-signal", false, "dont stop the server on SIGTERM/SIGUSR1")
+	flag.DurationVar(&sleepAfterStopSignal, "sleep-after-stop-signal", 5*time.Second, "time to sleep after getting the signal before exiting")
+	flag.BoolVar(&useSigusr1StopSignal, "use-sigusr1", false, "use SIGUSR1 as the stop signal, instead of the default SIGTERM")
 	flag.DurationVar(&stopAfter, "stop-after", 0, "stop the process after duration (overrides all other flags if set)")
 	flag.IntVar(&exitCode, "exit-code", 0, "exit code to return when this script exits")
 }
@@ -96,38 +96,43 @@ func run(logger *log.Logger) error {
 	doneCh := make(chan struct{})
 
 	go func() {
-		stopSig := make(chan os.Signal, 1)
-		if useSigusr1 {
-			signal.Notify(stopSig, syscall.SIGUSR1)
+		defer close(doneCh)
+
+		stopSignal := make(chan os.Signal, 1)
+		if useSigusr1StopSignal {
+			signal.Notify(stopSignal, syscall.SIGUSR1)
 		} else {
-			signal.Notify(stopSig, syscall.SIGTERM)
+			signal.Notify(stopSignal, syscall.SIGTERM)
 		}
 
-		select {
-		case <-ctx.Done():
-			logger.Println("context done: exiting")
+	loop:
+		for {
+			select {
+			case <-ctx.Done():
+				logger.Println("context done: exiting")
+				break loop
 
-		case s := <-stopSig:
-			logger.Printf("signal %q: received\n", s)
+			case s := <-stopSignal:
+				logger.Printf("signal %q: received\n", s)
 
-			if ignoreStopSig {
-				logger.Printf("signal %q: ignored; run `kill -9 %d` to stop\n", s, os.Getpid())
-				return
+				if ignoreStopSignal {
+					logger.Printf("signal %q: ignored", s)
+				} else {
+					logger.Printf("signal %q: sleeping for %v simulate cleanup\n", s, sleepAfterStopSignal)
+					time.Sleep(sleepAfterStopSignal)
+					break loop
+				}
+
+			case <-time.After(stopAfter):
+				logger.Printf("stopping after: %v\n", stopAfter)
+				break loop
 			}
-
-			logger.Printf("signal %q: sleeping for %v simulate cleanup\n", s, ttl)
-
-			time.Sleep(ttl)
-
-		case <-time.After(stopAfter):
-			logger.Printf("stopping after: %v\n", stopAfter)
 		}
 
 		if err := server.Shutdown(context.Background()); err != nil {
 			log.Printf("server shutdown error: %v", err)
 		}
 
-		close(doneCh)
 	}()
 
 	logger.Printf("server %s: started\n", server.Addr)
