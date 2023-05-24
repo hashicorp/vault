ca761221774dc8d6620cb18e523c2cd117fd81a5
clairebontempo@gmail.com <clairebontempo@gmail.com>
2023-05-23 17:15:55 -0700

    add test

---
+++ ui/tests/integration/components/transit-edit-test.js
@@ -0,0 +1,77 @@
+import { module, test } from 'qunit';
+import { setupRenderingTest } from 'vault/tests/helpers';
+import { click, fillIn, render } from '@ember/test-helpers';
+import { hbs } from 'ember-cli-htmlbars';
+
+const SELECTORS = {
+  createForm: '[data-test-transit-create-form]',
+  editForm: '[data-test-transit-edit-form]',
+  ttlToggle: '[data-test-ttl-toggle="Auto-rotation period"]',
+  ttlValue: '[data-test-ttl-value="Auto-rotation period"]',
+};
+module('Integration | Component | transit-edit', function (hooks) {
+  setupRenderingTest(hooks);
+
+  hooks.beforeEach(function () {
+    this.store = this.owner.lookup('service:store');
+    this.model = this.store.createRecord('transit-key');
+    this.backendCrumb = {
+      label: 'transit',
+      text: 'transit',
+      path: 'vault.cluster.secrets.backend.list-root',
+      model: 'transit',
+    };
+  });
+
+  test('it renders in create mode and updates model', async function (assert) {
+    await render(hbs`
+    <TransitEdit 
+      @key={{this.model}}
+      @model={{this.model}}
+      @mode="create"
+      @root={{this.backendCrumb}}
+      @preferAdvancedEdit={{false}}
+    />`);
+
+    assert.dom(SELECTORS.createForm).exists();
+    assert.dom(SELECTORS.ttlToggle).isNotChecked();
+
+    // confirm model params update when ttl changes
+    assert.strictEqual(this.model.autoRotatePeriod, '0');
+    await click(SELECTORS.ttlToggle);
+
+    assert.dom(SELECTORS.ttlValue).hasValue('30'); // 30 days
+    assert.strictEqual(this.model.autoRotatePeriod, '720h');
+
+    await fillIn(SELECTORS.ttlValue, '10'); // 10 days
+    assert.strictEqual(this.model.autoRotatePeriod, '240h');
+  });
+
+  test('it renders in edit mode', async function (assert) {
+    this.model.autoRotatePeriod = '5h';
+    this.model.keys = {
+      1: 1684882652000,
+    };
+    await render(hbs`
+      <TransitEdit 
+        @key={{this.model}}
+        @model={{this.model}}
+        @mode="edit"
+        @root={{this.backendCrumb}}
+        @preferAdvancedEdit={{false}}
+      />`);
+
+    assert.dom(SELECTORS.editForm).exists();
+    assert.dom(SELECTORS.ttlToggle).isChecked();
+    assert.dom(SELECTORS.ttlValue).hasValue('5');
+
+    await click(SELECTORS.ttlToggle);
+    assert.strictEqual(this.model.autoRotatePeriod, 0); // sets to 0 when toggled off
+
+    await click(SELECTORS.ttlToggle);
+    assert.strictEqual(this.model.autoRotatePeriod, '5h'); // reverts to original value when toggled back on
+
+    await fillIn(SELECTORS.ttlValue, '10');
+    assert.strictEqual(this.model.autoRotatePeriod, '10h');
+  });
+});
