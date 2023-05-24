0b14cdd7b5da8ca3f37220c0c80c758cee8eaac2
Anton Averchenkov <anton.averchenkov@hashicorp.com>
2023-05-24 12:06:47 -0400

    cleanup & enable tests

--- command/agent/exec/exec_test.go
+++ command/agent/exec/exec_test.go
@@ -70,73 +70,58 @@ func TestServer_Run(t *testing.T) {
 	defer vault.Close()
 
 	testCases := map[string]struct {
-		envTemplates   []*ctconfig.TemplateConfig
-		expectedValues map[string]string
-		extraAppArgs   []string
-		expectError    bool
-		checkError     func(*testing.T, error)
-		processTime    time.Duration
-		stopSignal     os.Signal
-		skip           bool
-		skipReason     string
-		port           int // so tests dont overlap
+		envTemplates      []*ctconfig.TemplateConfig
+		checkError        func(*testing.T, error)
+		testAppArgs       []string
+		testAppStopSignal os.Signal
+		testAppPort       int
+		expected          map[string]string
+		expectedExit      bool
 	}{
 		"simple": {
-			envTemplates: []*ctconfig.TemplateConfig{
-				{
-					Contents:                 pointerutil.StringPtr(`{{ with secret "kv/my-app/creds" }}{{ .Data.data.user }}{{ end }}`),
-					MapToEnvironmentVariable: pointerutil.StringPtr("MY_USER"),
-				},
-				{
-					Contents:                 pointerutil.StringPtr(`{{ with secret "kv/my-app/creds" }}{{ .Data.data.password }}{{ end }}`),
-					MapToEnvironmentVariable: pointerutil.StringPtr("MY_PASSWORD"),
-				},
-			},
-			extraAppArgs: []string{"--stop-after", "60s"},
-			expectedValues: map[string]string{
+			envTemplates: []*ctconfig.TemplateConfig{{
+				Contents:                 pointerutil.StringPtr(`{{ with secret "kv/my-app/creds" }}{{ .Data.data.user }}{{ end }}`),
+				MapToEnvironmentVariable: pointerutil.StringPtr("MY_USER"),
+			}, {
+				Contents:                 pointerutil.StringPtr(`{{ with secret "kv/my-app/creds" }}{{ .Data.data.password }}{{ end }}`),
+				MapToEnvironmentVariable: pointerutil.StringPtr("MY_PASSWORD"),
+			}},
+			testAppArgs:       []string{"--stop-after", "10s"},
+			testAppStopSignal: syscall.SIGTERM,
+			testAppPort:       34001,
+			expected: map[string]string{
 				"MY_USER":     "appuser",
 				"MY_PASSWORD": "s3cr3t",
 			},
-			expectError: false,
-			stopSignal:  syscall.SIGTERM,
-			port:        8001,
+			expectedExit: false,
 		},
-		"exits_early": {
-			envTemplates: []*ctconfig.TemplateConfig{
-				{
-					Contents:                 pointerutil.StringPtr(`{{ with secret "kv/my-app/creds" }}{{ .Data.data.user }}{{ end }}`),
-					MapToEnvironmentVariable: pointerutil.StringPtr("MY_USER"),
-				},
-			},
-			expectedValues: map[string]string{
+		"exits_early_success": {
+			envTemplates: []*ctconfig.TemplateConfig{{
+				Contents:                 pointerutil.StringPtr(`{{ with secret "kv/my-app/creds" }}{{ .Data.data.user }}{{ end }}`),
+				MapToEnvironmentVariable: pointerutil.StringPtr("MY_USER"),
+			}},
+			testAppArgs:       []string{"--stop-after", "2s"},
+			testAppStopSignal: syscall.SIGTERM,
+			testAppPort:       34002,
+			expected: map[string]string{
 				"MY_USER": "appuser",
 			},
-			processTime:  time.Second * 5,
-			extraAppArgs: []string{"--stop-after", "2s"},
-			expectError:  true,
-			stopSignal:   syscall.SIGTERM,
-			skip:         true,
+			expectedExit: true,
 			checkError:   processErrorCodeChecker(0),
-			port:         8002,
 		},
 		"exits_early_non_zero": {
-			envTemplates: []*ctconfig.TemplateConfig{
-				{
-					Contents:                 pointerutil.StringPtr(`{{ with secret "kv/my-app/creds" }}{{ .Data.data.user }}{{ end }}`),
-					MapToEnvironmentVariable: pointerutil.StringPtr("MY_USER"),
-				},
-			},
-			expectedValues: map[string]string{
+			envTemplates: []*ctconfig.TemplateConfig{{
+				Contents:                 pointerutil.StringPtr(`{{ with secret "kv/my-app/creds" }}{{ .Data.data.user }}{{ end }}`),
+				MapToEnvironmentVariable: pointerutil.StringPtr("MY_USER"),
+			}},
+			testAppArgs:       []string{"--stop-after", "2s", "--exit-code", "5"},
+			testAppStopSignal: syscall.SIGTERM,
+			testAppPort:       34003,
+			expected: map[string]string{
 				"MY_USER": "appuser",
 			},
-			processTime:  time.Second * 5,
-			extraAppArgs: []string{"--stop-after", "2s", "--exit-code", "5"},
-			expectError:  true,
-			stopSignal:   syscall.SIGTERM,
+			expectedExit: true,
 			checkError:   processErrorCodeChecker(5),
-			skip:         true,
-			skipReason:   "consul-template/child doesn't track child exit codes",
-			port:         8003,
 		},
 	}
 
@@ -147,18 +132,13 @@ func TestServer_Run(t *testing.T) {
 
 	for name, testCase := range testCases {
 		t.Run(name, func(t *testing.T) {
-			if testCase.skip {
-				t.Skip(testCase.skipReason)
-				return
-			}
-
-			exampleAppUrl := fmt.Sprintf("http://localhost:%d", testCase.port)
+			exampleAppUrl := fmt.Sprintf("http://localhost:%d", testCase.testAppPort)
 			baseCmdArgs := []string{
 				goBin,
 				"run",
 				"./test-app",
 				"--port",
-				strconv.Itoa(testCase.port),
+				strconv.Itoa(testCase.testAppPort),
 			}
 
 			serverConfig := &ServerConfig{
@@ -172,8 +152,8 @@ func TestServer_Run(t *testing.T) {
 					},
 					Exec: &config.ExecConfig{
 						RestartOnSecretChanges: "always",
-						Command:                append(baseCmdArgs, testCase.extraAppArgs...),
-						RestartStopSignal:      testCase.stopSignal,
+						Command:                append(baseCmdArgs, testCase.testAppArgs...),
+						RestartStopSignal:      testCase.testAppStopSignal,
 					},
 					EnvTemplates: testCase.envTemplates,
 				},
@@ -201,7 +181,7 @@ func TestServer_Run(t *testing.T) {
 			templateTokenCh <- "my-token"
 
 			// check to make sure the app is running
-			if !testCase.expectError {
+			if !testCase.expectedExit {
 				go func() {
 					time.Sleep(3 * time.Second)
 					_, err := retryablehttp.Head(exampleAppUrl)
@@ -213,16 +193,16 @@ func TestServer_Run(t *testing.T) {
 				}()
 			}
 
-			time.Sleep(testCase.processTime)
+			time.Sleep(5 * time.Second)
 
 			select {
 			case <-ctx.Done():
 				t.Fatal("timeout reached before templates were rendered")
 			case err := <-errCh:
-				if err != nil && !testCase.expectError {
+				if err != nil && !testCase.expectedExit {
 					t.Fatalf("did not expect error, got: %v", err)
 				}
-				if err != nil && testCase.expectError {
+				if err != nil && testCase.expectedExit {
 					t.Logf("received expected error: %v", err)
 					if testCase.checkError != nil {
 						testCase.checkError(t, err)
@@ -232,7 +212,7 @@ func TestServer_Run(t *testing.T) {
 			case <-appStartedCh:
 				t.Log("app has started")
 			case <-appStartErrCh:
-				if !testCase.expectError {
+				if !testCase.expectedExit {
 					t.Fatal("app could not be started")
 				}
 			}
@@ -254,8 +234,7 @@ func TestServer_Run(t *testing.T) {
 				t.Fatalf("unable to parse response from test app: %s", err)
 			}
 
-			t.Log("RESP", response)
-			for key, expectedValue := range testCase.expectedValues {
+			for key, expectedValue := range testCase.expected {
 				actualValue, ok := response.EnvVars[key]
 				if !ok {
 					t.Fatalf("expected the test app to return %q environment variable", key)
