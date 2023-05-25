8caa011000414654409fd74c327feb8fe687ff90
clairebontempo@gmail.com <clairebontempo@gmail.com>
2023-05-25 12:54:41 -0700

    add back one template test

--- ui/tests/integration/helpers/format-duration-test.js
+++ ui/tests/integration/helpers/format-duration-test.js
@@ -6,9 +6,19 @@
 import { module, test } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
 import { duration } from 'core/helpers/format-duration';
+import { render } from '@ember/test-helpers';
+import { hbs } from 'ember-cli-htmlbars';
 
 module('Integration | Helper | format-duration', function (hooks) {
   setupRenderingTest(hooks);
+
+  test('it formats-duration in template view', async function (assert) {
+    await render(hbs`<p data-test-format-duration>Date: {{format-duration 3606 }}</p>`);
+
+    assert
+      .dom('[data-test-format-duration]')
+      .includesText('1 hour 6 seconds', 'it renders the duration in hours and seconds');
+  });
 
   test('it formats seconds', async function (assert) {
     assert.strictEqual(duration([3606]), '1 hour 6 seconds');
