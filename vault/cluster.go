94f91eb7466a06c2eaa901024f228905104da7ff
Nick Cabatoff <ncabatoff@hashicorp.com>
2023-05-30 11:59:57 -0400

    Revert #19676, we decided this was unnecessary. (#20826) (#20831)

--- vault/cluster.go
+++ vault/cluster.go
@@ -327,8 +327,7 @@ func (c *Core) startClusterListener(ctx context.Context) error {
 	c.clusterListener.Store(cluster.NewListener(networkLayer,
 		c.clusterCipherSuites,
 		listenerLogger,
-		5*c.clusterHeartbeatInterval,
-		c.grpcMinConnectTimeout))
+		5*c.clusterHeartbeatInterval))
 
 	c.AddLogger(listenerLogger)
 
