124cf7683f8afa6aa1c511e8d05fa0cd9049db65
hc-github-team-secure-vault-core <82990506+hc-github-team-secure-vault-core@users.noreply.github.com>
2023-05-30 13:42:15 -0400

    backport of commit 34571d4d230537126ba4fa879fc161beada742ae (#20850)
    Co-authored-by: Steven Clark <steven.clark@hashicorp.com>

--- builtin/logical/pki/backend.go
+++ builtin/logical/pki/backend.go
@@ -440,7 +440,7 @@ func (b *backend) initialize(ctx context.Context, _ *logical.InitializationReque
 	err = b.initializeStoredCertificateCounts(ctx)
 	if err != nil {
 		// Don't block/err initialize/startup for metrics.  Context on this call can time out due to number of certificates.
-		b.Logger().Error("Could not initialize stored certificate counts", err)
+		b.Logger().Error("Could not initialize stored certificate counts", "error", err)
 		b.certCountError = err.Error()
 	}
 
