c2a1ce374c6eb340c779fbe26503e949172a6867
claire bontempo <68122737+hellobontempo@users.noreply.github.com>
2023-06-01 16:33:41 -0700

    UI: pki only render unable to parse banner on issuer details page (#20942)
    * move parsing banner to issuer details view only
    
    * add tests
    
    * fix test typo
    
    * remove old test

--- ui/tests/helpers/pki/pki-issuer-details.js
+++ ui/tests/helpers/pki/pki-issuer-details.js
@@ -4,16 +4,17 @@
  */
 
 export const SELECTORS = {
-  defaultGroup: '[data-test-details-group="default"]',
-  urlsGroup: '[data-test-details-group="Issuer URLs"]',
-  groupTitle: '[data-test-group-title]',
-  row: '[data-test-component="info-table-row"]',
-  rotateRoot: '[data-test-pki-issuer-rotate-root]',
-  crossSign: '[data-test-pki-issuer-cross-sign]',
-  signIntermediate: '[data-test-pki-issuer-sign-int]',
-  download: '[data-test-issuer-download]',
   configure: '[data-test-pki-issuer-configure]',
+  crossSign: '[data-test-pki-issuer-cross-sign]',
+  defaultGroup: '[data-test-details-group="default"]',
+  download: '[data-test-issuer-download]',
+  groupTitle: '[data-test-group-title]',
+  parsingAlertBanner: '[data-test-parsing-error-alert-banner]',
   rotateModal: '[data-test-modal-background="Rotate this root"]',
   rotateModalGenerate: '[data-test-root-rotate-step-one]',
+  rotateRoot: '[data-test-pki-issuer-rotate-root]',
+  row: '[data-test-component="info-table-row"]',
+  signIntermediate: '[data-test-pki-issuer-sign-int]',
+  urlsGroup: '[data-test-details-group="Issuer URLs"]',
   valueByName: (name) => `[data-test-value-div="${name}"]`,
 };
