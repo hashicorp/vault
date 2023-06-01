05b9aab11ef93aaa8cf9b3870a43032930d8ca7e
Nick Cabatoff <ncabatoff@hashicorp.com>
2023-06-01 08:42:17 -0400

    Update test for sha256

--- sdk/helper/testhelpers/output_test.go
+++ sdk/helper/testhelpers/output_test.go
@@ -25,7 +25,7 @@ func TestToMap(t *testing.T) {
 		{
 			name:    "basic",
 			args:    args{s{A: "a", B: []byte("bytes"), C: map[string]string{"k": "v"}, D: "d"}},
-			want:    "map[a:a b:4b3a6218bb3e3a7303e8a171a60fcf92 c:map[k:v]]",
+			want:    "map[a:a b:277089d91c0bdf4f2e6862ba7e4a07605119431f5d13f726dd352b06f1b206a9 c:map[k:v]]",
 			wantErr: false,
 		},
 	}
