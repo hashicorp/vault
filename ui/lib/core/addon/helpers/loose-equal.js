8609a8175f5f88ebcb076858208c0b2ee9103df7
clairebontempo@gmail.com <clairebontempo@gmail.com>
2023-06-01 10:03:03 -0700

    and more

--- ui/lib/core/addon/helpers/loose-equal.js
+++ ui/lib/core/addon/helpers/loose-equal.js
@@ -1,8 +1,3 @@
-/**
- * Copyright (c) HashiCorp, Inc.
- * SPDX-License-Identifier: MPL-2.0
- */
-
 import { helper } from '@ember/component/helper';
 
 /* 
--- ui/lib/core/app/helpers/loose-equal.js
+++ ui/lib/core/app/helpers/loose-equal.js
@@ -1,6 +1,1 @@
-/**
- * Copyright (c) HashiCorp, Inc.
- * SPDX-License-Identifier: MPL-2.0
- */
-
 export { default } from 'core/helpers/loose-equal';
