f53639e891a2c6794e231dd47d1a3f1f893833ec
rculpepper <rculpepper@hashicorp.com>
2023-06-02 20:54:00 -0400

    add header and fix func call

--- internalshared/configutil/kms_test.go
+++ internalshared/configutil/kms_test.go
@@ -1,3 +1,6 @@
+// Copyright (c) HashiCorp, Inc.
+// SPDX-License-Identifier: MPL-2.0
+
 package configutil
 
 import (
@@ -85,7 +88,7 @@ func Test_getEnvConfig(t *testing.T) {
 				}
 			}
 
-			if got := getEnvConfig(tt.kms); !reflect.DeepEqual(got, tt.want) {
+			if got := GetEnvConfigFunc(tt.kms); !reflect.DeepEqual(got, tt.want) {
 				t.Errorf("getEnvConfig() = %v, want %v", got, tt.want)
 			}
 
