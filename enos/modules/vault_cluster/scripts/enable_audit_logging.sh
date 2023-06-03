68ae0e8e9465a7f36b86995482e2e9829f516b68
hc-github-team-secure-vault-core <82990506+hc-github-team-secure-vault-core@users.noreply.github.com>
2023-06-03 14:43:51 -0400

    backport of commit 2c9a75b0932a141bba3464a6830ed83a77b05129 (#20977)
    Co-authored-by: Mike Baum <mike.baum@hashicorp.com>

--- enos/modules/vault_cluster/scripts/enable_audit_logging.sh
+++ enos/modules/vault_cluster/scripts/enable_audit_logging.sh
@@ -2,4 +2,4 @@
 
 set -eux
 
-sudo su "$SERVICE_USER" -c "VAULT_TOKEN=$VAULT_TOKEN VAULT_ADDR=$VAULT_ADDR $VAULT_BIN_PATH audit enable file file_path=$LOG_FILE_PATH"
+$VAULT_BIN_PATH audit enable file file_path="$LOG_FILE_PATH"
