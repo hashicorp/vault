80a7f625a63baa41ffd64040599ba262d010d998
Angel Garbarino <angel@hashicorp.com>
2023-06-06 10:48:02 -0600

    initial kv ember engine setup

---
+++ ui/lib/kv/addon/routes.js
@@ -0,0 +1,25 @@
+/**
+ * Copyright (c) HashiCorp, Inc.
+ * SPDX-License-Identifier: MPL-2.0
+ */
+
+import buildRoutes from 'ember-engines/routes';
+
+export default buildRoutes(function () {
+  this.route('secrets', function () {
+    this.route('create');
+    this.route('secret', { path: '/:name' }, function () {
+      this.route('details');
+      this.route('edit');
+      this.route('metadata', function () {
+        this.route('details');
+        this.route('edit');
+      });
+      this.route('versions');
+      this.route('diff');
+    });
+  });
+  this.route('configuration', function () {
+    this.route('edit');
+  });
+});
