8cf1dda023c0e47f5a76e3f7740343c52d1cb317
Angel Garbarino <angel@hashicorp.com>
2023-06-07 13:33:23 -0600

    setup serializers

---
+++ ui/app/serializers/kv/data.js
@@ -0,0 +1,10 @@
+/**
+ * Copyright (c) HashiCorp, Inc.
+ * SPDX-License-Identifier: MPL-2.0
+ */
+
+import ApplicationSerializer from '../application';
+
+export default class KvDataSerializer extends ApplicationSerializer {
+  // ARG TODO: CAS.
+}
