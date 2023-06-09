a0cf924d648ce302297d0ea9b588d4b0a65333a5
VioletHynes <violet.hynes@hashicorp.com>
2023-06-09 09:02:49 -0400

    VAULT-15385 Add GHA that checks for nil, nil returns on functions that return an error

---
+++ tools/gonilnilfunctions/pkg/analyzer/testdata/funcs.go
@@ -0,0 +1,62 @@
+// Copyright (c) HashiCorp, Inc.
+// SPDX-License-Identifier: MPL-2.0
+
+package testdata
+
+func ReturnReturnOkay() (any, error) {
+	var i interface{}
+	return i, nil
+}
+
+func OneGoodOneBad() (any, error) { // want "Function OneGoodOneBad can return an error, and has a statement that returns only nils"
+	var i interface{}
+	if true {
+		return i, nil
+	}
+	return nil, nil
+}
+
+func OneBadOneGood() (any, error) { // want "Function OneBadOneGood can return an error, and has a statement that returns only nils"
+	var i interface{}
+	if true {
+		return nil, nil
+	}
+	return i, nil
+}
+
+func EmptyFunc() {}
+
+func TwoNilNils() (any, error) { // want "Function TwoNilNils can return an error, and has a statement that returns only nils"
+	if true {
+		return nil, nil
+	}
+	return nil, nil
+}
+
+func ThreeArgs() (any, any, error) { // want "Function ThreeArgs can return an error, and has a statement that returns only nils"
+	return nil, nil, nil
+}
+
+func TwoArgsNoError() (any, any) {
+	return nil, nil
+}
+
+func NestedReturn() (any, error) { // want "Function NestedReturn can return an error, and has a statement that returns only nils"
+	{
+		{
+			{
+				return nil, nil
+			}
+		}
+	}
+}
+
+func NestedForReturn() (any, error) { // want "Function NestedForReturn can return an error, and has a statement that returns only nils"
+	for {
+		for i := 0; i < 100; i++ {
+			{
+				return nil, nil
+			}
+		}
+	}
+}
