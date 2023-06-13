d2def4464aed740b6a7a797b724b3930466b368a
Kianna Quach <kianna.quach@hashicorp.com>
2023-06-13 13:35:35 -0700

    Initial secrets engines query set up

--- ui/app/routes/vault/cluster/dashboard.js
+++ ui/app/routes/vault/cluster/dashboard.js
@@ -1,3 +1,13 @@
 import Route from '@ember/routing/route';
+import { inject as service } from '@ember/service';
+import { hash } from 'rsvp';
 
-export default class VaultClusterDashboardRoute extends Route {}
+export default class VaultClusterDashboardRoute extends Route {
+  @service store;
+
+  model() {
+    return hash({
+      secretEngines: this.store.query('secret-engine', {}),
+    });
+  }
+}
