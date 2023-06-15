9e85fef4db5b9724dd0f98cbfc81a68dffc3a546
hc-github-team-secure-vault-core <82990506+hc-github-team-secure-vault-core@users.noreply.github.com>
2023-06-15 16:41:45 -0400

    backport of commit c5549cdac681676ae52ea173d737ee1c5d1949a2 (#21272)
    Co-authored-by: Nick Cabatoff <ncabatoff@hashicorp.com>

--- vault/mount_util.go
+++ vault/mount_util.go
@@ -28,7 +28,7 @@ func addKnownPath(*Core, string)                                              {}
 func preprocessMount(*Core, *MountEntry, *BarrierView) (bool, error)          { return false, nil }
 func clearIgnoredPaths(context.Context, *Core, logical.Backend, string) error { return nil }
 func addLicenseCallback(*Core, logical.Backend)                               {}
-func runFilteredPathsEvaluation(context.Context, *Core) error                 { return nil }
+func runFilteredPathsEvaluation(context.Context, *Core, bool) error           { return nil }
 
 // ViewPath returns storage prefix for the view
 func (e *MountEntry) ViewPath() string {
