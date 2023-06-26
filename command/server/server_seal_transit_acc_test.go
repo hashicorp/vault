8bd1eb088b86315c022384314cc2bb9fc77dadd3
Ryan Cragun <me@ryan.ec>
2023-06-26 17:00:20 -0600

    test: use hashicorp docker mirror for acc test
    Signed-off-by: Ryan Cragun <me@ryan.ec>

--- command/server/server_seal_transit_acc_test.go
+++ command/server/server_seal_transit_acc_test.go
@@ -131,8 +131,9 @@ func prepareTestContainer(t *testing.T) (func(), *DockerVaultConfig) {
 	}
 
 	runner, err := docker.NewServiceRunner(docker.RunOptions{
-		ImageRepo: "vault",
-		ImageTag:  "latest",
+		ContainerName: "vault",
+		ImageTag:      "latest",
+		ImageRepo:     "docker.mirror.hashicorp.services/hashicorp/vault",
 		Cmd: []string{
 			"server", "-log-level=trace", "-dev", fmt.Sprintf("-dev-root-token-id=%s", rootToken),
 			"-dev-listen-address=0.0.0.0:8200",
