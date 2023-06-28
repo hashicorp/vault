a07cd25e90490bb0c9aee237e499102cd80e7bf9
Angel Garbarino <angel@hashicorp.com>
2023-06-28 12:58:23 -0600

    initial fix

--- ui/app/lib/local-storage.js
+++ ui/app/lib/local-storage.js
@@ -4,9 +4,27 @@
  */
 
 export default {
+  isLocalStorageSupported() {
+    try {
+      const key = `__storage__test`;
+      window.localStorage.setItem(key, null);
+      window.localStorage.removeItem(key);
+      return true;
+    } catch (e) {
+      // modify the e object so we can customize the error message.
+      // e.message is readOnly.
+      e.errors = [`This is likely due to your browser's cookie settings.`];
+      throw e;
+    }
+  },
+
   getItem(key) {
-    var item = window.localStorage.getItem(key);
-    return item && JSON.parse(item);
+    try {
+      const item = window.localStorage.getItem(key);
+      return item && JSON.parse(item);
+    } catch (e) {
+      return e;
+    }
   },
 
   setItem(key, val) {
@@ -31,3 +49,16 @@ export default {
     });
   },
 };
+
+function isSupported(getStorage) {
+  try {
+    const key = '__some_random_key_you_are_not_going_to_use__';
+    getStorage().setItem(key, key);
+    getStorage().removeItem(key);
+    return true;
+  } catch (e) {
+    return false;
+  }
+}
+
+isSupported(() => localStorage); // => true | false
