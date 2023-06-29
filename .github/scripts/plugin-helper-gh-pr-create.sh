ab97243ed03e7ac8460c9c1a25dcfdbe7cf730b5
John-Michael Faircloth <fairclothjm@users.noreply.github.com>
2023-06-29 14:45:53 -0500

    plugin automation: fix branch name (#21515)
    * plugin automation: fix branch name
    
    * fix input name

--- .github/scripts/plugin-helper-gh-pr-create.sh
+++ .github/scripts/plugin-helper-gh-pr-create.sh
@@ -2,15 +2,16 @@
 
 # this script expects the following env vars to be set
 #  - GITHUB_TOKEN
-#  - PLUGIN_REPO_NAME
-#  - BRANCH_NAME
+#  - PLUGIN_REPO
+#  - VAULT_BRANCH
+#  - PLUGIN_BRANCH
 #  - RUN_ID
 
 # we are using the GH API directly so that we can get the resluting
 # PR URL from the JSON response
 
 reviewers="fairclothjm,kpcraig"
-resp=$(curl -L \
+resp=$(curl -sL \
   -X POST \
   -H "Accept: application/vnd.github+json" \
   -H "Authorization: Bearer ${GITHUB_TOKEN}"\
@@ -18,9 +19,9 @@ resp=$(curl -L \
   https://api.github.com/repos/hashicorp/vault/pulls \
   --data @<(cat <<EOF
 {
-  "title":"[DO NOT MERGE]: $PLUGIN_REPO_NAME Automated plugin update check",
-  "body":"Updates $PLUGIN_REPO_NAME to verify vault CI. Full log: https://github.com/hashicorp/vault/actions/runs/$RUN_ID",
-  "head":"$BRANCH_NAME",
+  "title":"[DO NOT MERGE]: $PLUGIN_REPO Automated plugin update check",
+  "body":"Updates $PLUGIN_REPO to verify vault CI. Full log: https://github.com/hashicorp/vault/actions/runs/$RUN_ID",
+  "head":"$VAULT_BRANCH",
   "base":"master",
   "label": "dependencies,pr/no-changelog,pr/no-milestone",
   "reviewer": "$reviewers",
@@ -33,7 +34,7 @@ EOF
 vault_pr_url=$(echo "$resp" | jq '.html_url')
 
 # get Plugin PR number
-plugin_pr_num=$(gh pr list --head "$BRANCH_NAME" --json number --repo hashicorp/vault-plugin-database-snowflake -q '.[0].number')
+plugin_pr_num=$(gh pr list --head "$PLUGIN_BRANCH" --json number --repo hashicorp/$PLUGIN_REPO -q '.[0].number')
 
 # make a comment on the plugin repo's PR
-gh pr comment $plugin_pr_num --body "Vault CI check PR: $vault_pr_url" --repo hashicorp/$PLUGIN_REPO_NAME
+gh pr comment $plugin_pr_num --body "Vault CI check PR: $vault_pr_url" --repo hashicorp/$PLUGIN_REPO
