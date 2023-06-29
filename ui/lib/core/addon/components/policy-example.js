717fe8d8feb6e17abac96ce02b8dcf8f58869441
Malina Calarco <malina.calarco@hashicorp.com>
2023-06-29 15:18:54 -0700

    change policy form to render example modal only conditionally. add description to policy-example.js

--- ui/lib/core/addon/components/policy-example.js
+++ ui/lib/core/addon/components/policy-example.js
@@ -1,4 +1,45 @@
+/**
+ * Copyright (c) HashiCorp, Inc.
+ * SPDX-License-Identifier: MPL-2.0
+ */
+
 import Component from '@glimmer/component';
+
+/**
+ * @module PolicyExample
+ * PolicyExample component is meant to render within a PolicyForm component to show an example of a policy.
+ * edit this *** 
+ * PolicyExample does not render in a modal, as it is wrapped in a conditional within PolicyForm.
+ * 
+ *
+ * @example
+ *  <PolicyExample 
+ *    @policyType={{@model.policyType}} 
+ *  />
+ * 
+ * @example (in modal)
+ *  <Modal
+ *    @onClose={{fn (mut this.showTemplateModal) false}}
+ *    @isActive={{this.showTemplateModal}}
+ *  >
+ *    <section class="modal-card-body">
+ *      {{! code-mirror modifier does not render value initially until focus event fires }}
+ *      {{! wait until the Modal is rendered and then show the PolicyExample (contains JsonEditor) }}
+        {{#if this.showTemplateModal}}
+          <PolicyExample @policyType={{@model.policyType}}/>
+        {{/if}}
+      </section>
+      <div class="modal-card-head has-border-top-light">
+        <button type="button" class="button" {{on "click" (fn (mut this.showTemplateModal) false)}} data-test-close-modal>
+          Close
+        </button>
+      </div>
+    </Modal>
+ * ```
+ * talk about getter, policyTemplate, policyType ?
+ * @param {string} policyType - policy type to decide which template to render; can either be "acl" or "rgp"
+ * 
+ */
 
 export default class PolicyExampleComponent extends Component {
   // formatting here is purposeful so that whitespace renders correctly in JsonEditor
