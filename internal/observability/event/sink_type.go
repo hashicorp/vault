cbde30dfdb2ab8488bd1a721ac23c8788fcc0fd4
Peter Wilson <peter.wilson@hashicorp.com>
2023-07-04 17:11:45 +0100

    sink types (and validation test)

---
+++ internal/observability/event/sink_type.go
@@ -0,0 +1,27 @@
+// Copyright (c) HashiCorp, Inc.
+// SPDX-License-Identifier: MPL-2.0
+
+package event
+
+import (
+	"fmt"
+)
+
+const (
+	FileSink   SinkType = "file"
+	SocketSink SinkType = "socket"
+	SyslogSink SinkType = "syslog"
+)
+
+// SinkType defines the type of sink
+type SinkType string
+
+func (t SinkType) Validate() error {
+	const op = "event.(SinkType).Validate"
+	switch t {
+	case FileSink, SocketSink, SyslogSink:
+		return nil
+	default:
+		return fmt.Errorf("%s: '%s' is not a valid sink type: %w", op, t, ErrInvalidParameter)
+	}
+}
