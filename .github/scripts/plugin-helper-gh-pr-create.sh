cd4f9d1bb430395869f68255e2632d3a8a3dc682
Kay Craig <kay.craig@hashicorp.com>
2023-07-05 11:52:31 -0400

    a few polish updates

--- .github/scripts/plugin-helper-gh-pr-create.sh
+++ .github/scripts/plugin-helper-gh-pr-create.sh
@@ -4,16 +4,16 @@ set -e
 
 # this script expects the following env vars to be set
 # error if these are not set
-[ ${GITHUB_TOKEN:?} ]
-[ ${PLUGIN_REPO:?} ]
-[ ${VAULT_BRANCH:?} ]
-[ ${PLUGIN_BRANCH:?} ]
-[ ${RUN_ID:?} ]
+#[ ${GITHUB_TOKEN:?} ]
+#[ ${PLUGIN_REPO:?} ]
+#[ ${VAULT_BRANCH:?} ]
+#[ ${PLUGIN_BRANCH:?} ]
+#[ ${RUN_ID:?} ]
 
 # we are using the GH API directly so that we can get the resluting
 # PR URL from the JSON response
 
-reviewers="fairclothjm,kpcraig"
+reviewers="austingebauer,fairclothjm,vinay-gopalan,maxcoulombe,robmonte,Zlaticanin,kpcraig,raymonstah"
 resp=$(curl -SL \
   -X POST \
   -H "Accept: application/vnd.github+json" \
@@ -40,8 +40,8 @@ vault_pr_url=$(echo "$resp" | jq '.html_url')
 echo "Vault PR number: $vault_pr_url"
 
 # add labels to Vault PR
-#gh pr edit "$vault_pr_num" --add-label "dependencies,pr/no-changelong,pr/no-milestone" --repo hashicorp/vault
-#gh pr edit "$vault_pr_num" --add-reviewer "$reviewers"
+gh pr edit "$vault_pr_num" --add-label "dependencies,pr/no-changelong,pr/no-milestone" --repo hashicorp/vault
+gh pr edit "$vault_pr_num" --add-reviewer "$reviewers"
 
 # get Plugin PR number
 plugin_pr_num=$(gh pr list --head "$PLUGIN_BRANCH" --json number --repo hashicorp/$PLUGIN_REPO -q '.[0].number')
