19444c514ba2c320e1f3aa9e2ff58428a7a04d95
Angel Garbarino <angel@hashicorp.com>
2023-07-06 14:02:30 -0600

    configuraiton page header

--- ui/lib/kv/addon/routes/configuration.js
+++ ui/lib/kv/addon/routes/configuration.js
@@ -4,5 +4,29 @@
  */
 
 import Route from '@ember/routing/route';
+import { hash } from 'rsvp';
+import { inject as service } from '@ember/service';
 
-export default class KvConfigurationRoute extends Route {}
+export default class KvConfigurationRoute extends Route {
+  @service store;
+  @service secretMountPath;
+
+  model() {
+    const backend = this.secretMountPath.get();
+    // TODO: model for configuration comes from secret-engine but maybe we should make our own because it combines two endpoints?
+    return hash({
+      backend,
+      id: backend,
+      icon: 'kv',
+    });
+  }
+
+  setupController(controller, resolvedModel) {
+    super.setupController(controller, resolvedModel);
+
+    controller.breadcrumbs = [
+      { label: 'secrets', route: 'secrets', linkExternal: true },
+      { label: resolvedModel.backend },
+    ];
+  }
+}
