6a072dd4171989c2d14805bd2bbe6494baabd421
hamid ghaf <hamid@hashicorp.com>
2023-07-06 11:43:18 -0700

    change names, remove script

--- .github/scripts/verify_changes.sh
+++ .github/scripts/verify_changes.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
-#  This script validates if the git diff contains on docs changes
+#  This script validates if the git diff contains only docs/ui changes
 
 event_type=$1 # GH event type (pull_request)
 ref_name=$2 # branch reference that triggered the workflow
 base_ref=$3 # PR branch base ref
 
-changed_dir=""
+contains() {
+    target=$1; shift
+    for i; do
+        if [[ "$i" == "$target" ]]; then
+            return 0
+        fi
+    done
+    return 1
+}
 
 if [[ "$event_type" == "pull_request" ]]; then
   git fetch --no-tags --prune origin $base_ref
@@ -17,15 +25,24 @@ else
   base_commit=$(git log origin/$ref_name --oneline | head -2 | awk 'NR==2 {print $1}')
 fi
 
-# git diff with ... shows the differences count between base_commit and head_commit starting at the last common commit
-change_count=$(git diff $base_commit...$head_commit --name-only | awk -F"/" '{ print $1}' | uniq | wc -l)
+# git diff with ... shows the differences between base_commit and head_commit starting at the last common commit
+changed_dir=$(git diff $base_commit...$head_commit --name-only | awk -F"/" '{ print $1}' | uniq)
+change_count=$(echo "$changed_dir" | wc -l)
 
-if [[ $change_count -eq 1 ]]; then
-  changed_dir=$(git diff $base_commit...$head_commit --name-only | awk -F"/" '{ print $1}' | uniq)
-fi
-
-if [[ "$changed_dir" == "website" ]]; then
-  echo "is_docs_change=true" >> "$GITHUB_OUTPUT"
+if [[ $change_count -gt 2 && $change_count -ne 0 ]]; then
+  echo "is_docs_ui_change=false" >> "$GITHUB_OUTPUT"
+  exit 0
+elif [[ $change_count -eq 1 && "$changed_dir" == "website" ]]; then
+  echo "is_docs_ui_change=true" >> "$GITHUB_OUTPUT"
+  exit 0
+elif [[ $change_count -eq 1 && "$changed_dir" == "ui" ]]; then
+  echo "is_docs_ui_change=true" >> "$GITHUB_OUTPUT"
+  exit 0
 else
-  echo "is_docs_change=false" >> "$GITHUB_OUTPUT"
+  if ! contains "website" ${changed_dir[@]} || ! contains "ui" ${changed_dir[@]}; then
+    echo "is_docs_ui_change=false" >> "$GITHUB_OUTPUT"
+    exit 0
+  else
+    echo "is_docs_ui_change=true" >> "$GITHUB_OUTPUT"
+  fi
 fi
