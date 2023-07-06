682842e1d81a1af5fa23d995b4484b213bc8b886
Angel Garbarino <angel@hashicorp.com>
2023-07-06 15:43:00 -0600

    create secret view

--- ui/lib/kv/addon/routes/secrets/create.js
+++ ui/lib/kv/addon/routes/secrets/create.js
@@ -4,5 +4,29 @@
  */
 
 import Route from '@ember/routing/route';
+import { inject as service } from '@ember/service';
+import { hash } from 'rsvp';
 
-export default class KvSecretsCreateRoute extends Route {}
+export default class KvSecretsCreateRoute extends Route {
+  @service store;
+  @service secretMountPath;
+
+  model() {
+    // TODO return model for query on kv/data.
+    const backend = this.secretMountPath.get();
+    return hash({
+      id: backend,
+      backend,
+      icon: 'kv',
+    });
+  }
+
+  setupController(controller, resolvedModel) {
+    super.setupController(controller, resolvedModel);
+
+    controller.breadcrumbs = [
+      { label: 'secrets', route: 'secrets', linkExternal: true },
+      { label: resolvedModel.backend },
+    ];
+  }
+}
