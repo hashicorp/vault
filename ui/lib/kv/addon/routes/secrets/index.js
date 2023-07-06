8ee479de8017d84a0ebf154fc76ae1557b78946c
Angel Garbarino <angel@hashicorp.com>
2023-07-06 15:27:38 -0600

    error with query not solving in this pr.

--- ui/lib/kv/addon/routes/secrets/index.js
+++ ui/lib/kv/addon/routes/secrets/index.js
@@ -12,20 +12,12 @@ export default class KvSecretsRoute extends Route {
   @service secretMountPath;
 
   model() {
-    // TODO: filter secrets based on queryParams value
+    // TODO add filtering and return model for query on kv/metadata.
     const backend = this.secretMountPath.get();
-    const secrets = this.store.query('kv/metadata', { backend }).catch((error) => {
-      if (error.httpStatus === 404) {
-        return [];
-      }
-      throw error;
-    });
-
     return hash({
       id: backend,
       backend,
       icon: 'kv',
-      secrets,
     });
   }
 
