37b82def6a1969358eb71861060a3aa0c230b7d9
Kuba Wieczorek <kuba.wieczorek@hashicorp.com>
2023-07-07 13:25:12 +0100

    Remove intentionally failing test

--- vault/core_test.go
+++ vault/core_test.go
@@ -35,10 +35,6 @@ import (
 // invalidKey is used to test Unseal
 var invalidKey = []byte("abcdefghijklmnopqrstuvwxyz")[:17]
 
-func TestJustFail(t *testing.T) {
-	t.Fatalf("FAILING ON PURPOSE")
-}
-
 func TestNewCore_badRedirectAddr(t *testing.T) {
 	logger = logging.NewVaultLogger(log.Trace)
 
