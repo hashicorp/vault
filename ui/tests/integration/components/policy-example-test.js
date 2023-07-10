7a971d43782c1c8aeba065541bd0c5a26737c80e
Malina Calarco <malina.calarco@hashicorp.com>
2023-07-10 10:35:36 -0700

    added tests for PolicyExample in policy-example-tests

--- ui/tests/integration/components/policy-example-test.js
+++ ui/tests/integration/components/policy-example-test.js
@@ -1,28 +1,81 @@
+/**
+ * Copyright (c) HashiCorp, Inc.
+ * SPDX-License-Identifier: MPL-2.0
+ */
+
 import { module, test } from 'qunit';
 import { setupRenderingTest } from 'vault/tests/helpers';
 import { render } from '@ember/test-helpers';
 import { hbs } from 'ember-cli-htmlbars';
 
 // To-Do: add to tests
+const SELECTORS = {
+  policyText: '[data-test-modal-title]',
+  aclPolicyParagraph: '[data-test-example-modal-text-acl]',
+  rgpPolicyParagraph: '[data-test-example-modal-text-rgp]',
+  jsonText: '[data-test-example-modal-json-text]',
+  informationLink: '[data-test-example-modal-information-link]',
+};
 
-module('Integration | Component | json-template', function (hooks) {
+module('Integration | Component | policy-example', function (hooks) {
   setupRenderingTest(hooks);
 
-  test('it renders', async function (assert) {
-    // Set any properties with this.set('myProperty', 'value');
-    // Handle any actions with this.set('myAction', function(val) { ... });
+  hooks.beforeEach(function () {
+    this.store = this.owner.lookup('service:store');
+  });
 
-    await render(hbs`<PolicyExample />`);
+  test('it renders the correct paragraph for ACL policy', async function (assert) {
+    this.model = this.store.createRecord('policy/acl');
 
-    assert.dom(this.element).hasText('');
-
-    // Template block usage:
     await render(hbs`
-      <PolicyExample>
-        template block text
-      </PolicyExample>
+    <PolicyExample
+      @policyType={{this.model.policyType}}
+    />
     `);
+    assert
+      .dom(SELECTORS.aclPolicyParagraph)
+      .hasText(
+        'ACL Policies are written in Hashicorp Configuration Language ( HCL ) or JSON and describe which paths in Vault a user or machine is allowed to access. Here is an example policy:'
+      );
+  });
 
-    assert.dom(this.element).hasText('template block text');
+  test('it renders the correct paragraph for RGP policy', async function (assert) {
+    this.model = this.store.createRecord('policy/rgp');
+
+    await render(hbs`
+    <PolicyExample
+      @policyType={{this.model.policyType}}
+    />
+    `);
+    assert
+      .dom(SELECTORS.rgpPolicyParagraph)
+      .hasText(
+        'Role Governing Policies (RGPs) are tied to client tokens or identities which is similar to ACL policies . They use Sentinel as a language framework to enable fine-grained policy decisions.'
+      );
+  });
+
+  test('it renders the correct JSON editor text for ACL policy', async function (assert) {
+    this.model = this.store.createRecord('policy/acl');
+
+    await render(hbs`
+    <PolicyExample
+      @policyType={{this.model.policyType}}
+    />
+    `);
+    //await this.pauseTest();
+    assert.dom(SELECTORS.jsonText).includesText(`# Grant 'create', 'read' , 'update', and ‘list’ permission`);
+  });
+
+  test('it renders the correct JSON editor text for RGP policy', async function (assert) {
+    this.model = this.store.createRecord('policy/rgp');
+
+    await render(hbs`
+    <PolicyExample
+      @policyType={{this.model.policyType}}
+    />
+    `);
+    assert
+      .dom(SELECTORS.jsonText)
+      .includesText(`# Import strings library that exposes common string operations`);
   });
 });
