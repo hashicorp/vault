880b18a339496639ae104f5dfe28476f726f556a
Peter Wilson <peter.wilson@hashicorp.com>
2023-07-10 21:57:54 +0100

    prefix + error messages

--- internal/observability/event/audit_sink_file.go
+++ internal/observability/event/audit_sink_file.go
@@ -99,12 +99,17 @@ func (f *AuditFileSink) Process(ctx context.Context, e *eventlogger.Event) (*eve
 
 	buffer := bytes.NewBuffer(formatted)
 	err := f.log(buffer, writer)
+	if err != nil {
+		return nil, fmt.Errorf("%s: error writing file for audit sink: %w", op, err)
+	}
 
-	return nil, err
+	return nil, nil
 }
 
 // Reopen handles closing and reopening the file.
 func (f *AuditFileSink) Reopen() error {
+	const op = "event.(AuditFileSink).Reopen"
+
 	switch f.path {
 	case "stdout", "discard":
 		return nil
@@ -121,7 +126,7 @@ func (f *AuditFileSink) Reopen() error {
 	// Set to nil here so that even if we error out, on the next access open() will be tried.
 	f.file = nil
 	if err != nil {
-		return err
+		return fmt.Errorf("%s: unable to close file for re-opening on audit sink: %w", op, err)
 	}
 
 	return f.open()
@@ -135,25 +140,27 @@ func (f *AuditFileSink) Type() eventlogger.NodeType {
 // open attempts to open a file at the sink's path, with the sink's fileMode permissions
 // if one is not already open.
 func (f *AuditFileSink) open() error {
+	const op = "event.(AuditFileSink).open"
+
 	if f.file != nil {
 		return nil
 	}
 
 	if err := os.MkdirAll(filepath.Dir(f.path), f.fileMode); err != nil {
-		return err
+		return fmt.Errorf("%s: unable to create directory %q for audit sink: %w", op, f.path, err)
 	}
 
 	var err error
 	f.file, err = os.OpenFile(f.path, os.O_APPEND|os.O_WRONLY|os.O_CREATE, f.fileMode)
 	if err != nil {
-		return err
+		return fmt.Errorf("%s: unable to open file for audit sink: %w", op, err)
 	}
 
 	// Change the file mode in case the log file already existed.
 	// We special case /dev/null since we can't chmod it and bypass if the mode is zero.
 	if f.path != "/dev/null" && f.fileMode != 0 {
 		if err = os.Chmod(f.path, f.fileMode); err != nil {
-			return err
+			return fmt.Errorf("%s: unable to change file %q permissions '%v' for audit sink: %w", op, f.path, f.fileMode, err)
 		}
 	}
 
@@ -162,6 +169,8 @@ func (f *AuditFileSink) open() error {
 
 // log writes the buffer to the file.
 func (f *AuditFileSink) log(buf *bytes.Buffer, writer io.Writer) error {
+	const op = "event.(AuditFileSink).log"
+
 	reader := bytes.NewReader(buf.Bytes())
 
 	f.fileLock.Lock()
@@ -169,34 +178,42 @@ func (f *AuditFileSink) log(buf *bytes.Buffer, writer io.Writer) error {
 
 	if writer == nil {
 		if err := f.open(); err != nil {
-			return err
+			return fmt.Errorf("%s: unable to open file for audit sink: %w", op, err)
 		}
 		writer = f.file
+	}
+
+	if f.prefix != "" {
+		_, err := writer.Write([]byte(f.prefix))
+		if err != nil {
+			return fmt.Errorf("%s: unable to write prefix %q for audit sink: %w", op, f.prefix, err)
+		}
 	}
 
 	if _, err := reader.WriteTo(writer); err == nil {
 		return nil
 	} else if f.path == "stdout" {
-		return err
+		return fmt.Errorf("%s: unable write to %q: %w", op, f.path, err)
 	}
 
 	// If writing to stdout there's no real reason to think anything would have changed so return above.
 	// Otherwise, opportunistically try to re-open the FD, once per call.
 	err := f.file.Close()
 	if err != nil {
-		return err
+		return fmt.Errorf("%s: unable to close file for audit sink: %w", op, err)
 	}
 
 	f.file = nil
 
 	if err := f.open(); err != nil {
-		return err
+		return fmt.Errorf("%s: unable to re-open file for audit sink: %w", op, err)
 	}
 
 	_, err = reader.Seek(0, io.SeekStart)
 	if err != nil {
-		return err
+		return fmt.Errorf("%s: unable to seek to start of file for audit sink: %w", op, err)
 	}
+
 	_, err = reader.WriteTo(writer)
-	return err
+	return fmt.Errorf("%s: unable to re-write to file for audit sink: %w", op, err)
 }
