8175e30245adcb57ff8b1571ab27e08c5fc6e8ba
Kianna Quach <kianna.quach@hashicorp.com>
2023-07-11 11:23:36 -0700

    Add vault configuration checks

--- ui/app/routes/vault/cluster/dashboard.js
+++ ui/app/routes/vault/cluster/dashboard.js
@@ -11,10 +11,21 @@ export default class VaultClusterDashboardRoute extends Route {
   @service store;
   @service version;
 
+  async getVaultConfiguration() {
+    try {
+      const adapter = this.store.adapterFor('application');
+      const configState = await adapter.ajax('/v1/sys/config/state/sanitized', 'GET');
+      return configState.data;
+    } catch (e) {
+      return e.httpStatus;
+    }
+  }
+
   model() {
     return hash({
       secretsEngines: this.store.query('secret-engine', {}),
-      version: this.version.version,
+      version: this.version,
+      vaultConfiguration: this.getVaultConfiguration(),
     });
   }
 }
