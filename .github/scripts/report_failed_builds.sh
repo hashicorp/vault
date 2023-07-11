ebe012ef5b97805a8b452e445c934eba72e9622b
hamid ghaf <hamid@hashicorp.com>
2023-07-11 11:55:04 -0700

    report workflow url

--- .github/scripts/report_failed_builds.sh
+++ .github/scripts/report_failed_builds.sh
@@ -8,12 +8,15 @@ set -e
 [ ${RUN_ID:?} ]
 [ ${REPO:?} ]
 [ ${PR:?} ]
+[ ${FAILED_JOBS:?} ]
 
-failed_tests_ids=$(gh api \
-  -H "Accept: application/vnd.github+json" \
-  -H "X-GitHub-Api-Version: 2022-11-28" \
-  /repos/hashicorp/"$REPO"/actions/runs/"$RUN_ID"/attempts/1/jobs | jq -r '[.jobs[] | select(.name | startswith("Darwin") or startswith("Linux") or startswith("Other") ) | select(.conclusion == "failure") | .id][0]')
+echo "$FAILED_JOBS"
 
-extracted_error=$(gh run view --job $failed_tests_ids --log | grep "\#\#\[error\]")
+#failed_tests_ids=$(gh api \
+#  -H "Accept: application/vnd.github+json" \
+#  -H "X-GitHub-Api-Version: 2022-11-28" \
+#  /repos/hashicorp/"$REPO"/actions/runs/"$RUN_ID"/attempts/1/jobs | jq -r '[.jobs[] | select(.name | startswith("Darwin") or startswith("Linux") or startswith("Other") ) | select(.conclusion == "failure") | .id][0]')
+#
+#extracted_error=$(gh run view --job $failed_tests_ids --log | grep "\#\#\[error\]")
 
-gh pr comment "$PR" --body "$extracted_error"
+gh pr comment "$PR" --body "build failed: please refer to this workflow to learn more: https://github.com/hashicorp/vault/actions/runs/$RUN_ID"
