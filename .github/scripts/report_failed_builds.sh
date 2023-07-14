fda534cef228531e46116f449ec909a1a80d8c61
Mia Epner <mia.epner@hashicorp.com>
2023-07-14 13:12:20 +0200

    shellcheck, and success message for builds

--- .github/scripts/report_failed_builds.sh
+++ .github/scripts/report_failed_builds.sh
@@ -30,8 +30,13 @@ done
 
 # Create a comment to be posted on the PR
 # This comment reports failed jobs and the url to the failed workflow
-new_body="build failed for these jobs: ${failed_jobs[*]}. Please refer to this workflow to learn more: https://github.com/hashicorp/vault/actions/runs/$RUN_ID"
+if [ ${#failed_jobs[@]} -eq 0 ]; then
+  new_body="Build Results:\nAll builds succeeded"
+else
+  new_body="Build Results:\nBuild failed for these jobs: ${failed_jobs[*]}. Please refer to this workflow to learn more: https://github.com/hashicorp/vault/actions/runs/$RUN_ID"
+fi
+
 
 source gh_comment.sh
 
-update_or_create_comment "$REPO" "$PR_NUMBER" "build failed for these jobs" "$BODY"
+update_or_create_comment "$REPO" "$PR_NUMBER" "Build Results:" "$BODY"
