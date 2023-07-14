78ce019a48420718a1b463a9a7729544e9f38b95
Kianna Quach <kianna.quach@hashicorp.com>
2023-07-14 13:49:15 -0700

    Fix tests

--- ui/tests/acceptance/dashboard-test.js
+++ ui/tests/acceptance/dashboard-test.js
@@ -17,13 +17,99 @@ module('Acceptance | landing page dashboard', function (hooks) {
   setupMirage(hooks);
 
   hooks.beforeEach(function () {
+    this.data = {
+      api_addr: 'http://127.0.0.1:8200',
+      cache_size: 0,
+      cluster_addr: 'https://127.0.0.1:8201',
+      cluster_cipher_suites: '',
+      cluster_name: '',
+      default_lease_ttl: 0,
+      default_max_request_duration: 0,
+      detect_deadlocks: '',
+      disable_cache: false,
+      disable_clustering: false,
+      disable_indexing: false,
+      disable_mlock: true,
+      disable_performance_standby: false,
+      disable_printable_check: false,
+      disable_sealwrap: false,
+      disable_sentinel_trace: false,
+      enable_response_header_hostname: false,
+      enable_response_header_raft_node_id: false,
+      enable_ui: true,
+      experiments: null,
+      introspection_endpoint: false,
+      listeners: [
+        {
+          config: {
+            address: '0.0.0.0:8200',
+            cluster_address: '0.0.0.0:8201',
+            tls_disable: true,
+          },
+          type: 'tcp',
+        },
+      ],
+      log_format: '',
+      log_level: 'debug',
+      log_requests_level: '',
+      max_lease_ttl: '48h',
+      pid_file: '',
+      plugin_directory: '',
+      plugin_file_permissions: 0,
+      plugin_file_uid: 0,
+      raw_storage_endpoint: true,
+      seals: [
+        {
+          disabled: false,
+          type: 'shamir',
+        },
+      ],
+      storage: {
+        cluster_addr: 'https://127.0.0.1:8201',
+        disable_clustering: false,
+        raft: {
+          max_entry_size: '',
+        },
+        redirect_addr: 'http://127.0.0.1:8200',
+        type: 'raft',
+      },
+      telemetry: {
+        add_lease_metrics_namespace_labels: false,
+        circonus_api_app: '',
+        circonus_api_token: '',
+        circonus_api_url: '',
+        circonus_broker_id: '',
+        circonus_broker_select_tag: '',
+        circonus_check_display_name: '',
+        circonus_check_force_metric_activation: '',
+        circonus_check_id: '',
+        circonus_check_instance_id: '',
+        circonus_check_search_tag: '',
+        circonus_check_tags: '',
+        circonus_submission_interval: '',
+        circonus_submission_url: '',
+        disable_hostname: true,
+        dogstatsd_addr: '',
+        dogstatsd_tags: null,
+        lease_metrics_epsilon: 3600000000000,
+        maximum_gauge_cardinality: 500,
+        metrics_prefix: '',
+        num_lease_metrics_buckets: 168,
+        prometheus_retention_time: 86400000000000,
+        stackdriver_debug_logs: false,
+        stackdriver_location: '',
+        stackdriver_namespace: '',
+        stackdriver_project_id: '',
+        statsd_address: '',
+        statsite_address: '',
+        usage_gauge_period: 5000000000,
+      },
+    };
     return authPage.login();
   });
 
   // TODO LANDING PAGE: create a test that will navigate to dashboard if user opts into new dashboard ui
-  test('does not navigate to dashboard on login when user has not opted into dashboard ui', async function (assert) {
-    await visit('/vault/dashboard');
-
+  test('navigate to dashboard on login', async function (assert) {
     assert.strictEqual(currentURL(), '/vault/dashboard');
   });
 
@@ -60,97 +146,7 @@ module('Acceptance | landing page dashboard', function (hooks) {
     });
   });
 
-  module('configuration details card', function (hooks) {
-    hooks.beforeEach(function () {
-      this.data = {
-        api_addr: 'http://127.0.0.1:8200',
-        cache_size: 0,
-        cluster_addr: 'https://127.0.0.1:8201',
-        cluster_cipher_suites: '',
-        cluster_name: '',
-        default_lease_ttl: 0,
-        default_max_request_duration: 0,
-        detect_deadlocks: '',
-        disable_cache: false,
-        disable_clustering: false,
-        disable_indexing: false,
-        disable_mlock: true,
-        disable_performance_standby: false,
-        disable_printable_check: false,
-        disable_sealwrap: false,
-        disable_sentinel_trace: false,
-        enable_response_header_hostname: false,
-        enable_response_header_raft_node_id: false,
-        enable_ui: true,
-        experiments: null,
-        introspection_endpoint: false,
-        listeners: [
-          {
-            config: {
-              address: '0.0.0.0:8200',
-              cluster_address: '0.0.0.0:8201',
-              tls_disable: true,
-            },
-            type: 'tcp',
-          },
-        ],
-        log_format: '',
-        log_level: 'debug',
-        log_requests_level: '',
-        max_lease_ttl: '48h',
-        pid_file: '',
-        plugin_directory: '',
-        plugin_file_permissions: 0,
-        plugin_file_uid: 0,
-        raw_storage_endpoint: true,
-        seals: [
-          {
-            disabled: false,
-            type: 'shamir',
-          },
-        ],
-        storage: {
-          cluster_addr: 'https://127.0.0.1:8201',
-          disable_clustering: false,
-          raft: {
-            max_entry_size: '',
-          },
-          redirect_addr: 'http://127.0.0.1:8200',
-          type: 'raft',
-        },
-        telemetry: {
-          add_lease_metrics_namespace_labels: false,
-          circonus_api_app: '',
-          circonus_api_token: '',
-          circonus_api_url: '',
-          circonus_broker_id: '',
-          circonus_broker_select_tag: '',
-          circonus_check_display_name: '',
-          circonus_check_force_metric_activation: '',
-          circonus_check_id: '',
-          circonus_check_instance_id: '',
-          circonus_check_search_tag: '',
-          circonus_check_tags: '',
-          circonus_submission_interval: '',
-          circonus_submission_url: '',
-          disable_hostname: true,
-          dogstatsd_addr: '',
-          dogstatsd_tags: null,
-          lease_metrics_epsilon: 3600000000000,
-          maximum_gauge_cardinality: 500,
-          metrics_prefix: '',
-          num_lease_metrics_buckets: 168,
-          prometheus_retention_time: 86400000000000,
-          stackdriver_debug_logs: false,
-          stackdriver_location: '',
-          stackdriver_namespace: '',
-          stackdriver_project_id: '',
-          statsd_address: '',
-          statsite_address: '',
-          usage_gauge_period: 5000000000,
-        },
-      };
-    });
+  module('configuration details card', function () {
     test('shows the configuration details card', async function (assert) {
       this.server.get('sys/config/state/sanitized', () => ({
         data: this.data,
