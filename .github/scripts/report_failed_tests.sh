03c0f14528b7b694c1be0dff44a087959f844dde
Mia Epner <mia.epner@hashicorp.com>
2023-07-17 18:31:11 +0200

    small fixes

--- .github/scripts/report_failed_tests.sh
+++ .github/scripts/report_failed_tests.sh
@@ -15,9 +15,9 @@ fi
 
 # Create a comment to be posted on the PR
 # This comment reports failed jobs and the url to the failed workflow
-BODY="CI Test Results:
+BODY="CI Results:
 ${TABLE_DATA}"
 
 source ./.github/scripts/gh_comment.sh
 
-update_or_create_comment "$REPO" "$PR_NUMBER" "CI Test Results:" "$BODY"
+update_or_create_comment "$REPO" "$PR_NUMBER" "CI Results:" "$BODY"
