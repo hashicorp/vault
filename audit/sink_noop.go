2e740a1dd2ece50be72e6cd1b93539154a35a9ca
Peter Wilson <peter.wilson@hashicorp.com>
2023-07-17 20:56:34 +0100

    comments on noop sink

--- audit/sink_noop.go
+++ audit/sink_noop.go
@@ -17,13 +17,13 @@ func NewNoopSink() (*NoopSink, error) {
 	return &NoopSink{}, nil
 }
 
-// Process handles writing the event to the socket.
+// Process is a no-op and always returns nil event and nil error.
 func (s *NoopSink) Process(ctx context.Context, e *eventlogger.Event) (*eventlogger.Event, error) {
 	// return nil for the event to indicate the pipeline is complete.
 	return nil, nil
 }
 
-// Reopen handles reopening the connection for the socket sink.
+// Reopen is a no-op and always returns nil.
 func (s *NoopSink) Reopen() error {
 	return nil
 }
