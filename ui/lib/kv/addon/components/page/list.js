0ddc828358e9776fa19757a7ef531b7cfbc25fca
Angel Garbarino <angel@hashicorp.com>
2023-07-18 15:41:03 -0600

    turn it all over into a component 💅

--- ui/lib/kv/addon/components/page/list.js
+++ ui/lib/kv/addon/components/page/list.js
@@ -7,11 +7,8 @@ import Component from '@glimmer/component';
 import { inject as service } from '@ember/service';
 import { action } from '@ember/object';
 import { getOwner } from '@ember/application';
+import { ancestorKeysForKey } from 'core/utils/key-utils';
 import errorMessage from 'vault/utils/error-message';
-import { ancestorKeysForKey, keyIsFolder, parentKeyForKey, keyWithoutParentKey } from 'core/utils/key-utils';
-import keys from 'core/utils/key-codes';
-import { tracked } from '@glimmer/tracking';
-import escapeStringRegexp from 'escape-string-regexp';
 
 /**
  * @module List
@@ -25,8 +22,6 @@ import escapeStringRegexp from 'escape-string-regexp';
 export default class KvListPageComponent extends Component {
   @service flashMessages;
   @service router;
-
-  @tracked filterIsFocused = false;
 
   get mountPoint() {
     // mountPoint tells the LinkedBlock component where to start the transition. In this case, mountPoint will always be vault.cluster.secrets.backend.kv.
@@ -48,113 +43,6 @@ export default class KvListPageComponent extends Component {
     } catch (error) {
       const message = errorMessage(error, 'Error deleting secret. Please try again or contact support.');
       this.flashMessages.danger(message);
-    }
-  }
-
-  // filter operations and getters
-  get filterMatchesASecretPath() {
-    return !!(
-      this.args.model.secrets &&
-      this.args.model.secrets.length &&
-      this.args.model.secrets.findBy('fullSecretPath', this.args.model.filterValue)
-    );
-  }
-
-  get partialMatch() {
-    // you can't pass undefined to RegExp so replacing with empty string if there is no pageFilter value
-    const value = !this.pageFilter ? '' : this.pageFilter;
-    const reg = new RegExp('^' + escapeStringRegexp(value));
-    const match = this.args.model.secrets.filter((path) => reg.test(path.fullSecretPath))[0];
-
-    if (this.filterMatchesASecretPath || !match) return null;
-    // TODO not doing the shared prefix?
-    return match.fullSecretPath;
-  }
-
-  @action
-  handleInput(event) {
-    // handling typing
-    const input = event.target.value;
-    const isDirectory = keyIsFolder(input);
-    const parentDirectory = parentKeyForKey(input);
-    const secretWithinDirectory = keyWithoutParentKey(input);
-
-    if (isDirectory) {
-      this.router.transitionTo('vault.cluster.secrets.backend.kv.list-directory', input);
-    } else if (parentDirectory) {
-      this.router.transitionTo('vault.cluster.secrets.backend.kv.list-directory', parentDirectory, {
-        queryParams: { pageFilter: secretWithinDirectory },
-      });
-    } else {
-      this.router.transitionTo('vault.cluster.secrets.backend.kv.list', {
-        queryParams: { pageFilter: input },
-      });
-    }
-  }
-  @action
-  handleKeyDown(event) {
-    // handle keyboard events: tab, enter, escape
-    const inputValue = event.target.value;
-    const parentDirectory = parentKeyForKey(inputValue);
-
-    if (event.keyCode === keys.TAB) {
-      event.preventDefault();
-      const isMatchDirectory = keyIsFolder(this.partialMatch);
-      const parentDirectoryFromMatch = parentKeyForKey(this.partialMatch);
-      const withoutDirectory = keyWithoutParentKey(this.partialMatch);
-
-      if (isMatchDirectory) {
-        // beep/boop/
-        this.router.transitionTo('vault.cluster.secrets.backend.kv.list-directory', this.partialMatch);
-      } else if (!isMatchDirectory && parentDirectoryFromMatch) {
-        // beep/boop/my-
-        this.router.transitionTo(
-          'vault.cluster.secrets.backend.kv.list-directory',
-          parentDirectoryFromMatch,
-          {
-            queryParams: { pageFilter: withoutDirectory },
-          }
-        );
-      } else {
-        this.router.transitionTo('vault.cluster.secrets.backend.kv.list', {
-          queryParams: { pageFilter: this.partialMatch },
-        });
-      }
-    }
-    if (event.keyCode === keys.ENTER) {
-      event.preventDefault();
-      if (this.filterMatchesASecretPath) {
-        // check if secret exists if it does, navigate to the details page
-        this.router.transitionTo('vault.cluster.secrets.backend.kv.secret.details', inputValue);
-      } else {
-        this.router.transitionTo('vault.cluster.secrets.backend.kv.create', {
-          queryParams: { initialKey: inputValue },
-        });
-      }
-      return;
-    }
-    if (event.keyCode === keys.ESC) {
-      // transition to the nearest directory or to the list route.
-      // clear pageFilter queryParam each time
-      return !parentDirectory
-        ? this.router.transitionTo('vault.cluster.secrets.backend.kv.list', {
-            queryParams: { pageFilter: '' },
-          })
-        : this.router.transitionTo('vault.cluster.secrets.backend.kv.list-directory', parentDirectory, {
-            queryParams: { pageFilter: '' },
-          });
-    }
-    return;
-  }
-
-  @action
-  setFilterIsFocused() {
-    this.filterIsFocused = true;
-  }
-  @action
-  focusInput() {
-    if (this.args.model.filterValue) {
-      document.getElementById('secret-filter')?.focus();
     }
   }
 }
