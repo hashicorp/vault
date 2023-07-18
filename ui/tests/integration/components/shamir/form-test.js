5ce724862c9a6720dec03c393be1fac2d31de6f8
Chelsea Shaw <cshaw@hashicorp.com>
2023-07-18 18:20:37 -0500

    Shamir/form test cleanup

--- ui/tests/integration/components/shamir/form-test.js
+++ ui/tests/integration/components/shamir/form-test.js
@@ -30,7 +30,6 @@ module('Integration | Component | shamir/form', function (hooks) {
     );
     assert.dom(SHAMIR_FORM.input).hasValue('', 'key value reset after submit');
 
-    // Template block usage:
     await render(hbs`
     <Shamir::Form @onSubmit={{this.submitSpy}} @progress={{0}} @threshold={{3}} @alwaysShowProgress={{true}} @buttonText="Do the thing" @inputLabel="Unseal key">
       <div data-test-block-content>Hello</div>
@@ -41,7 +40,9 @@ module('Integration | Component | shamir/form', function (hooks) {
     assert.dom(SHAMIR_FORM.submitButton).hasText('Do the thing', 'uses passed button text');
     assert.dom(SHAMIR_FORM.inputLabel).hasText('Unseal key', 'uses passed inputLabel');
     assert.dom(SHAMIR_FORM.otpInfo).doesNotExist('no OTP info shown');
-    assert.dom(SHAMIR_FORM.progress).exists('Shows progress even if 0 when alwaysShowProgress=true');
+    assert
+      .dom(SHAMIR_FORM.progress)
+      .hasText('0/3 keys provided', 'displays textual progress when alwaysShowProgress=true');
   });
 
   test('it shows OTP info if provided', async function (assert) {
@@ -59,6 +60,7 @@ module('Integration | Component | shamir/form', function (hooks) {
 
     assert.dom(SHAMIR_FORM.otpInfo).exists('shows OTP info');
     assert.dom(SHAMIR_FORM.otpCode).hasText('this-is-otp', 'shows OTP code');
+    assert.dom(SHAMIR_FORM.progress).hasText('2/4 keys provided', 'displays textual progress');
     assert.dom('[data-test-block-content]').hasText('Hello', 'renders block content');
   });
 
