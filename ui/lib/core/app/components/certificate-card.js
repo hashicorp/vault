245261d3f95d59776f7a86742eca6e77c3b01ead
Malina Calarco <malina.calarco@hashicorp.com>
2023-07-19 15:27:37 -0700

    change new component name from KubernetesCertificate to CertificateCard

---
+++ ui/lib/core/app/components/certificate-card.js
@@ -0,0 +1,1 @@
+export { default } from 'core/components/certificate-card';
--- ui/tests/integration/components/certificate-card-test.js
+++ ui/tests/integration/components/certificate-card-test.js
@@ -3,22 +3,22 @@ import { setupRenderingTest } from 'vault/tests/helpers';
 import { render } from '@ember/test-helpers';
 import { hbs } from 'ember-cli-htmlbars';
 
-module('Integration | Component | kubernetes-certificate', function (hooks) {
+module('Integration | Component | certificate-card', function (hooks) {
   setupRenderingTest(hooks);
 
   test('it renders', async function (assert) {
     // Set any properties with this.set('myProperty', 'value');
     // Handle any actions with this.set('myAction', function(val) { ... });
 
-    await render(hbs`<KubernetesCertificate />`);
+    await render(hbs`<CertificateCard />`);
 
     assert.dom(this.element).hasText('');
 
     // Template block usage:
     await render(hbs`
-      <KubernetesCertificate>
+      <CertificateCard>
         template block text
-      </KubernetesCertificate>
+      </CertificateCard>
     `);
 
     assert.dom(this.element).hasText('template block text');
