4da260dd3d12b94aa9600aae3ce81ce565985172
hc-github-team-secure-vault-core <82990506+hc-github-team-secure-vault-core@users.noreply.github.com>
2023-07-19 14:32:53 -0400

    backport of commit 215687795d6b9ad2b3a3e2c7dbb70b01cc470e44 (#21954)
    Co-authored-by: Hamid Ghaf <83242695+hghaf099@users.noreply.github.com>

--- vault/mount.go
+++ vault/mount.go
@@ -509,6 +509,11 @@ func (entry *MountEntry) Deserialize() map[string]interface{} {
 	}
 }
 
+// DecodeMountTable is used for testing
+func (c *Core) DecodeMountTable(ctx context.Context, raw []byte) (*MountTable, error) {
+	return c.decodeMountTable(ctx, raw)
+}
+
 func (c *Core) decodeMountTable(ctx context.Context, raw []byte) (*MountTable, error) {
 	// Decode into mount table
 	mountTable := new(MountTable)
