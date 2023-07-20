ab80aa79211c3568df22ceb5b3a0273e05b15a8c
clairebontempo@gmail.com <clairebontempo@gmail.com>
2023-07-20 10:51:05 -0700

    finish adapter changes to fetch data in secret route

--- ui/lib/kv/addon/routes/secret.js
+++ ui/lib/kv/addon/routes/secret.js
@@ -11,13 +11,22 @@ export default class KvSecretRoute extends Route {
   @service store;
   @service secretMountPath;
 
+  async fetchSecretData(backend, path) {
+    return await this.store.queryRecord('kv/data', { backend, path }).catch((error) => error.httpStatus);
+  }
+
+  async fetchSecretMetadata(backend, path) {
+    return await this.store.queryRecord('kv/metadata', { backend, path }).catch((error) => error.httpStatus);
+  }
+
   model() {
     const backend = this.secretMountPath.currentPath;
     const { name: path } = this.paramsFor('secret');
     return hash({
       path,
       backend,
-      secret: this.store.queryRecord('kv/data', { backend, path }),
+      secret: this.fetchSecretData(backend, path),
+      metadata: this.fetchSecretMetadata(backend, path),
     });
   }
 }
