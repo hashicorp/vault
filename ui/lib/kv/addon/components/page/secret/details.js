165b6be99b55b924fb8dddb43d51e5eede0caa8f
clairebontempo@gmail.com <clairebontempo@gmail.com>
2023-07-20 12:36:08 -0700

    make component args more explicit

--- ui/lib/kv/addon/components/page/secret/details.js
+++ ui/lib/kv/addon/components/page/secret/details.js
@@ -8,15 +8,32 @@ import { action } from '@ember/object';
 import { tracked } from '@glimmer/tracking';
 
 /**
- * @module KvPageSecretDetails
- * KvPageSecretDetails shows detail of kv secrets.
+ * @module KvSecretDetails renders the key/value data of a KV secret. 
+ * It also renders a dropdown to display different versions of the secret.
+ * <Page::Secret::Details
+ *  @secretPath={{this.model.path}}
+ *  @secret={{this.model.secret}}
+ *  @metadata={{this.model.metadata}}
+ *  @breadcrumbs={{this.breadcrumbs}}
+  /> 
  *
- * @param {array} model - An array of models generated form kv/metadata query.
+ * @param {string} secretPath - path of kv secret 'my/secret' used as the title for the KV page header 
+ * @param {model} secret - Ember data model: 'kv/data'  
+ * @param {model} metadata - Ember data model: 'kv/metadata'
+ * @param {array} breadcrumbs - Array to generate breadcrumbs, passed to the page header component
+ * 
+ * sample: 
+  {
+    backend: 'my-kv-engine',
+    path: 'full/secret/path',
+    secret: KvDataEmberModel,
+    metadata: KvMetadataEmberModel,
+  }
+ *
  */
 
-export default class KvPageSecretDetails extends Component {
+export default class KvSecretDetails extends Component {
   @tracked showJsonView = false;
-
   @action
   toggleJsonView() {
     this.showJsonView = !this.showJsonView;
