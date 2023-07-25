8c8d481f798e1c2a8c139ccf1c8facef03f6ecea
clairebontempo@gmail.com <clairebontempo@gmail.com>
2023-07-25 14:00:08 -0700

    add dropdown version menu test

---
+++ ui/tests/helpers/kv/page/kv-secret-details.js
@@ -0,0 +1,9 @@
+/**
+ * Copyright (c) HashiCorp, Inc.
+ * SPDX-License-Identifier: MPL-2.0
+ */
+
+export const SELECTORS = {
+  versionDropdown: '[data-test-version-dropdown]',
+  version: (number) => `[data-test-version="${number}"]`,
+};
