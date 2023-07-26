5b0543544309bd6a3eed1e7101b7d1fb49652cf0
Chelsea Shaw <cshaw@hashicorp.com>
2023-07-26 13:36:13 -0500

    transition to logout if intended and not authed

--- ui/app/lib/route-paths.js
+++ ui/app/lib/route-paths.js
@@ -6,6 +6,7 @@
 export const INIT = 'vault.cluster.init';
 export const UNSEAL = 'vault.cluster.unseal';
 export const AUTH = 'vault.cluster.auth';
+export const LOGOUT = 'vault.cluster.logout';
 export const REDIRECT = 'vault.cluster.redirect';
 export const CLUSTER = 'vault.cluster';
 export const CLUSTER_INDEX = 'vault.cluster.index';
