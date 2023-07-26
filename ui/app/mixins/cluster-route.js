91df1a0cde8a07dec0ba03a763f7ce02fcc16f39
Chelsea Shaw <cshaw@hashicorp.com>
2023-07-26 13:29:40 -0500

    transition to logout as intended even if not authed

--- ui/app/mixins/cluster-route.js
+++ ui/app/mixins/cluster-route.js
@@ -19,6 +19,7 @@ import {
   DR_REPLICATION_SECONDARY_DETAILS,
   EXCLUDED_REDIRECT_URLS,
   REDIRECT,
+  LOGOUT,
 } from 'vault/lib/route-paths';
 
 export default Mixin.create({
@@ -26,21 +27,40 @@ export default Mixin.create({
   store: service(),
   router: service(),
 
+  _shouldInterceptTransition(targetRoute, transitionTarget, currentRouteName) {
+    const attemptingLogout = transitionTarget === LOGOUT || this.routeName === LOGOUT;
+    if (
+      // not attempting to logout
+      !attemptingLogout &&
+      // has target route to intercept to
+      targetRoute &&
+      // target route different from current route
+      targetRoute !== currentRouteName &&
+      // target route different from transition target
+      targetRoute !== transitionTarget
+    ) {
+      return true;
+    }
+    return false;
+  },
+
   transitionToTargetRoute(transition = {}) {
     const targetRoute = this.targetRouteName(transition);
-    if (
-      targetRoute &&
-      targetRoute !== this.routeName &&
-      targetRoute !== transition.targetName &&
-      targetRoute !== this.router.currentRouteName
-    ) {
+    const intercept = this._shouldInterceptTransition(
+      targetRoute,
+      transition.targetName,
+      this.routeName || this.router.currentRouteName
+    );
+    if (intercept) {
       // there may be query params so check for inclusion rather than exact match
       const isExcluded = EXCLUDED_REDIRECT_URLS.find((url) => this.router.currentURL?.includes(url));
       if (
-        // only want to redirect if we're going to authenticate
+        // only want to add redirect_to if we're going to authenticate
         targetRoute === AUTH &&
-        transition.targetName !== CLUSTER_INDEX &&
-        !isExcluded
+        // and if we aren't redirecting from logout
+        !isExcluded &&
+        // and the target isn't the default
+        transition.targetName !== CLUSTER_INDEX
       ) {
         return this.transitionTo(targetRoute, { queryParams: { redirect_to: this.router.currentURL } });
       }
