9cfd483ae14d912951eaf48c78d21d61f8f4d59d
Chelsea Shaw <cshaw@hashicorp.com>
2023-07-26 13:34:20 -0500

    Revert "transition to logout as intended even if not authed"
    This reverts commit 91df1a0cde8a07dec0ba03a763f7ce02fcc16f39.

--- ui/app/lib/route-paths.js
+++ ui/app/lib/route-paths.js
@@ -6,7 +6,6 @@
 export const INIT = 'vault.cluster.init';
 export const UNSEAL = 'vault.cluster.unseal';
 export const AUTH = 'vault.cluster.auth';
-export const LOGOUT = 'vault.cluster.logout';
 export const REDIRECT = 'vault.cluster.redirect';
 export const CLUSTER = 'vault.cluster';
 export const CLUSTER_INDEX = 'vault.cluster.index';
