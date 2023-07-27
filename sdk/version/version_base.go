423fc8038f3020dc0d217c8e5975f67b324796b0
Divya Pola <87338962+divyapola5@users.noreply.github.com>
2023-07-27 10:31:04 -0400

    update version 1.12.10 (#22079)

--- sdk/version/version_base.go
+++ sdk/version/version_base.go
@@ -11,7 +11,7 @@ var (
 	// Whether cgo is enabled or not; set at build time
 	CgoEnabled bool
 
-	Version           = "1.12.9"
+	Version           = "1.12.10"
 	VersionPrerelease = ""
 	VersionMetadata   = ""
 )
