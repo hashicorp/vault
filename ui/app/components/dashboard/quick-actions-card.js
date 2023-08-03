2fd9e1d1fa88fecdd99b3618b88350e39e5d684d
Kianna Quach <kianna.quach@hashicorp.com>
2023-08-03 12:53:30 -0700

    Refactor quick actions component with Claire based on her feedback

--- ui/app/components/dashboard/quick-actions-card.js
+++ ui/app/components/dashboard/quick-actions-card.js
@@ -20,123 +20,89 @@ import { inject as service } from '@ember/service';
  * @param {array}
  */
 
-const getActionsByEngineType = (type) => {
-  switch (type) {
-    case 'kv':
-      return [
-        {
-          actionTitle: 'Find KV secrets',
-          actionType: 'find-kv',
-        },
-      ];
-    case 'database':
-      return [
-        {
-          actionTitle: 'Generate credentials for database',
-          actionType: 'generate-credentials-db',
-        },
-      ];
-    case 'pki':
-      return [
-        {
-          actionTitle: 'Issue certificate',
-          actionType: 'issue-certificate-pki',
-        },
-        {
-          actionTitle: 'View certificate',
-          actionType: 'view-certificate-pki',
-        },
-        {
-          actionTitle: 'View issuer',
-          actionType: 'view-issuer-pki',
-        },
-      ];
-  }
-};
+const ENGINE_TYPE = ['pki', 'kv', 'database'];
 
-const getActionParamByAction = (type) => {
-  switch (type) {
-    case 'find-kv':
-      return {
-        title: 'Secret Path',
-        subText: 'Path of the secret you want to read, including the mount. E.g., secret/data/foo.',
-        // elementType: 'input',
-        buttonText: 'Read secrets',
-        model: 'secret-v2',
-        path: 'vault.cluster.secrets.backend.list',
-        // ends in forward --> 'vault.cluster.secrets.backends.list', id
-        // else --> 'vault.cluster.secrets.backends.show', id
-      };
-    case 'generate-credentials-db':
-      return {
-        title: 'Role to use',
-        elementType: 'search-select',
-        buttonText: 'Generate credentials',
-        model: 'database/role',
-        path: 'vault.cluster.secrets.backend.credentials',
-      };
-    case 'issue-certificate-pki':
-      return {
-        title: 'Role to use',
-        elementType: 'search-select',
-        placeholder: 'Type to find a role...',
-        buttonText: 'Issue leaf certificate',
-        model: 'pki/role',
-        path: 'vault.cluster.secrets.backend.pki.roles.role.generate',
-      };
-    case 'view-certificate-pki':
-      return {
-        title: 'Certificate serial number',
-        placeholder: '33:a3:...',
-        elementType: 'search-select',
-        buttonText: 'View certificate',
-        model: 'pki/certificate/base',
-        path: 'vault.cluster.secrets.backend.pki.certificates.certificate.details',
-      };
-    case 'view-issuer-pki':
-      return {
-        title: 'Issuer',
-        placeholder: 'Type issuer name or ID',
-        elementType: 'search-select',
-        buttonText: 'View issuer',
-        model: 'pki/issuer',
-        nameKey: 'issuerName',
-        path: 'vault.cluster.secrets.backend.pki.issuers.issuer.details',
-      };
-  }
-};
-
-const PKI_ENGINE_TYPE = 'pki';
-const KV_ENGINE_TYPE = 'kv';
-const DB_ENGINE_TYPE = 'database';
 export default class DashboardQuickActionsCard extends Component {
   @service router;
 
   @tracked selectedEngine;
-  @tracked selectedActions = [];
   @tracked selectedAction;
-  @tracked actionParamField;
   @tracked selectedEngineName;
-  @tracked value;
+  @tracked paramValue;
 
-  constructor() {
-    super(...arguments);
+  // 2nd input field
+  get actionOptions() {
+    switch (this.selectedEngine) {
+      case 'kv':
+        return ['Find KV secrets'];
+      case 'database':
+        return ['Generate credentials for database'];
+      case 'pki':
+        return ['Issue certificate', 'View certificate', 'View issuer'];
+      default:
+        return ['Find KV secrets'];
+    }
+  }
 
-    if (!this.selectedEngine) {
-      this.setSelectedAction();
+  // setting the search select object
+  get searchSelectParams() {
+    switch (this.selectedAction) {
+      case 'Find KV secrets':
+        return {
+          title: 'Secret Path',
+          subText: 'Path of the secret you want to read, including the mount. E.g., secret/data/foo.',
+          buttonText: 'Read secrets',
+          model: 'secret-v2',
+          path: 'vault.cluster.secrets.backend.list',
+          // ends in forward --> 'vault.cluster.secrets.backends.list', id
+          // else --> 'vault.cluster.secrets.backends.show', id
+        };
+      case 'Generate credentials for database':
+        return {
+          title: 'Role to use',
+          buttonText: 'Generate credentials',
+          model: 'database/role',
+          path: 'vault.cluster.secrets.backend.credentials',
+        };
+      case 'Issue certificate':
+        return {
+          title: 'Role to use',
+          placeholder: 'Type to find a role...',
+          buttonText: 'Issue leaf certificate',
+          model: 'pki/role',
+          path: 'vault.cluster.secrets.backend.pki.roles.role.generate',
+        };
+      case 'View certificate':
+        return {
+          title: 'Certificate serial number',
+          placeholder: '33:a3:...',
+          buttonText: 'View certificate',
+          model: 'pki/certificate/base',
+          path: 'vault.cluster.secrets.backend.pki.certificates.certificate.details',
+        };
+      case 'View issuer':
+        return {
+          title: 'Issuer',
+          placeholder: 'Type issuer name or ID',
+          buttonText: 'View issuer',
+          model: 'pki/issuer',
+          nameKey: 'issuerName',
+          path: 'vault.cluster.secrets.backend.pki.issuers.issuer.details',
+        };
+      default:
+        return {
+          placeholder: 'Please select an action above',
+          buttonText: 'Select an action',
+          model: '',
+        };
     }
   }
 
   get filteredSecretEngines() {
-    return this.args.secretsEngines.filter(
-      (secretEngine) =>
-        secretEngine.type === PKI_ENGINE_TYPE ||
-        secretEngine.type === KV_ENGINE_TYPE ||
-        secretEngine.type === DB_ENGINE_TYPE
-    );
+    return this.args.secretsEngines.filter((engine) => ENGINE_TYPE.includes(engine.type));
   }
 
-  get secretsEnginesOptions() {
+  get mountOptions() {
     return this.filteredSecretEngines.map((engine) => {
       const { id, type } = engine;
       return { name: id, type, id };
@@ -145,41 +111,28 @@ export default class DashboardQuickActionsCard extends Component {
 
   @action
   handleSearchEngineSelect([selection]) {
-    if (selection) {
-      this.selectedEngine = selection.type;
-      this.selectedEngineName = selection.id;
-      this.selectedActions = getActionsByEngineType(this.selectedEngine);
-      this.setSelectedAction(this.selectedActions?.firstObject.actionType);
-    } else {
-      // no selection, clear tracked properties
-      this.selectedEngine = '';
-      this.selectedEngineName = '';
-      this.setSelectedAction();
-    }
+    this.selectedEngine = selection?.type;
+    this.selectedEngineName = selection?.id;
+    // reset tracked properties
+    this.selectedAction = null;
   }
 
   @action
   setSelectedAction(selectedAction) {
-    if (selectedAction) {
-      this.selectedAction = selectedAction;
-      this.actionParamField = getActionParamByAction(selectedAction);
-    } else {
-      this.selectedActions = getActionsByEngineType('kv');
-      this.actionParamField = getActionParamByAction('find-kv');
-    }
+    this.selectedAction = selectedAction;
   }
 
   @action
-  handleSearch(val) {
+  handleActionSelect(val) {
     if (Array.isArray(val)) {
-      this.value = val[0];
+      this.paramValue = val[0];
     } else {
-      this.value = val;
+      this.paramValue = val;
     }
   }
 
   @action
   navigateToPage() {
-    this.router.transitionTo(this.actionParamField.path, this.selectedEngineName, this.value);
+    this.router.transitionTo(this.searchSelectParams.path, this.selectedEngineName, this.paramValue);
   }
 }
