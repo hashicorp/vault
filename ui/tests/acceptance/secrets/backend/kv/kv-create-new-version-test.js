57fc74d4c070d3238bf7ea26508728c05ef6063f
Chelsea Shaw <82459713+hashishaw@users.noreply.github.com>
2023-08-04 12:22:23 -0500

    UI: KV-V2 setup workflow acceptance tests (#22208)
    * Update selectors
    
    * Update runCmd to not need consoleComponent passed in, update database test
    
    * Stub workflow tests
    
    * Add create without read scenario to workflow tests
    
    * selector cleanup
    
    * Skip tests instead of marking as todo (which fail)
    
    * fix custom metadata section selector
    
    * consolidate diff and history tests

--- ui/tests/acceptance/secrets/backend/kv/kv-create-new-version-test.js
+++ ui/tests/acceptance/secrets/backend/kv/kv-create-new-version-test.js
@@ -4,9 +4,8 @@ import { v4 as uuidv4 } from 'uuid';
 import { click, currentURL, fillIn, visit } from '@ember/test-helpers';
 import authPage from 'vault/tests/pages/auth';
 import enablePage from 'vault/tests/pages/settings/mount-secret-backend';
-import { runCommands } from 'vault/tests/helpers/kv/kv-run-commands';
-import { SELECTORS } from 'vault/tests/helpers/kv/kv-general-selectors';
-import { PAGE } from 'vault/tests/helpers/kv/kv-page-selectors';
+import { FORM, PAGE } from 'vault/tests/helpers/kv/kv-selectors';
+import { deleteEngineCmd, runCmd } from 'vault/tests/helpers/commands';
 
 module('Acceptance | kv | creates a secret and a new version', function (hooks) {
   setupApplicationTest(hooks);
@@ -20,7 +19,7 @@ module('Acceptance | kv | creates a secret and a new version', function (hooks) 
   hooks.afterEach(async function () {
     await authPage.login();
     // Cleanup engine
-    await runCommands([`delete sys/mounts/${this.mountPath}`]);
+    await runCmd(deleteEngineCmd(this.mountPath), false);
   });
 
   test('it creates a new secret then a new secret version and navigates to details route', async function (assert) {
@@ -28,29 +27,33 @@ module('Acceptance | kv | creates a secret and a new version', function (hooks) 
 
     const secretPath = 'my-secret';
     await visit(`/vault/secrets/${this.mountPath}/kv/list`);
-    assert.dom(SELECTORS.emptyStateTitle).hasText('No secrets yet');
+    assert.dom(PAGE.emptyStateTitle).hasText('No secrets yet');
     assert
-      .dom(`${SELECTORS.emptyStateActions} a`)
+      .dom(`${PAGE.emptyStateActions} a`)
       .hasAttribute('href', `/ui/vault/secrets/${this.mountPath}/kv/create`);
     await click(PAGE.list.createSecret);
-    assert.strictEqual(currentURL(), `/vault/secrets/${this.mountPath}/kv/create?initialKey=`);
+    assert.strictEqual(
+      currentURL(),
+      `/vault/secrets/${this.mountPath}/kv/create?initialKey=`,
+      'url is correct'
+    );
 
-    await fillIn(PAGE.form.inputByAttr('path'), secretPath);
-    await fillIn(PAGE.form.keyInput(), 'foo-1');
-    await fillIn(PAGE.form.maskedValueInput(), 'bar-1');
-    await click(PAGE.form.secretSave);
+    await fillIn(FORM.inputByAttr('path'), secretPath);
+    await fillIn(FORM.keyInput(), 'foo-1');
+    await fillIn(FORM.maskedValueInput(), 'bar-1');
+    await click(FORM.saveBtn);
     assert.strictEqual(currentURL(), `/vault/secrets/${this.mountPath}/kv/${secretPath}/details?version=1`);
 
-    await click(PAGE.details.createNewVersion);
+    await click(PAGE.detail.createNewVersion);
     assert.strictEqual(
       currentURL(),
       `/vault/secrets/${this.mountPath}/kv/${secretPath}/details/edit?version=1`
     );
-    assert.dom(PAGE.form.inputByAttr('path')).isDisabled();
+    assert.dom(FORM.inputByAttr('path')).isDisabled();
 
-    await fillIn(PAGE.form.keyInput(1), 'foo-2');
-    await fillIn(PAGE.form.maskedValueInput(1), 'bar-2');
-    await click(PAGE.form.secretSave);
+    await fillIn(FORM.keyInput(1), 'foo-2');
+    await fillIn(FORM.maskedValueInput(1), 'bar-2');
+    await click(FORM.saveBtn);
     assert.strictEqual(currentURL(), `/vault/secrets/${this.mountPath}/kv/${secretPath}/details?version=2`);
 
     await visit(`/vault/secrets/${this.mountPath}/kv/list`);
@@ -60,6 +63,6 @@ module('Acceptance | kv | creates a secret and a new version', function (hooks) 
       `/vault/secrets/${this.mountPath}/kv/${secretPath}/details?version=2`,
       'list view navigates to latest version'
     );
-    assert.dom(SELECTORS.tooltipTrigger).hasTextContaining('Version 2');
+    assert.dom(PAGE.detail.versionCreated).hasTextContaining('Version 2');
   });
 });
