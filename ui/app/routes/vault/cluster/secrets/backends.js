07120d094e3f4bb844a77e36fdb4ea2a3749bcd7
Kianna Quach <kianna.quach@hashicorp.com>
2023-08-08 13:42:52 -0700

    Put query back in dashboard

--- ui/app/routes/vault/cluster/secrets/backends.js
+++ ui/app/routes/vault/cluster/secrets/backends.js
@@ -10,10 +10,6 @@ export default Route.extend({
   store: service(),
 
   model() {
-    if (this.store.peekAll('secret-engine', {}).length) {
-      return this.store.peekAll('secret-engine', {});
-    }
-
     return this.store.query('secret-engine', {});
   },
 });
