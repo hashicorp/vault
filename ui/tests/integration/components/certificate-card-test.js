fbd5a0e144bc96cd2e9f6e6529d95e7ad42a40f9
Malina Calarco <malina.calarco@hashicorp.com>
2023-08-08 09:44:12 -0700

    update test for certificate-card-test.js, update test for the kubernetes configuration-test.js

--- ui/tests/integration/components/certificate-card-test.js
+++ ui/tests/integration/components/certificate-card-test.js
@@ -7,20 +7,25 @@ import { module, test } from 'qunit';
 import { setupRenderingTest } from 'vault/tests/helpers';
 import { render } from '@ember/test-helpers';
 import { hbs } from 'ember-cli-htmlbars';
+import { rootPem } from 'vault/tests/helpers/pki/values';
 
 const SELECTORS = {
   label: '[data-test-certificate-label]',
   value: '[data-test-certificate-value]',
+  icon: '[data-test-certificate-icon]',
+  copyButton: '[data-test-copy-button]',
 };
 
 module('Integration | Component | certificate-card', function (hooks) {
   setupRenderingTest(hooks);
 
-  test('it renders without a certificate value', async function (assert) {
+  test('it renders', async function (assert) {
     await render(hbs`<CertificateCard />`);
 
     assert.dom(SELECTORS.label).hasText('PEM Format', 'The label text is correct');
-    assert.dom(SELECTORS.value).hasNoText('The is no value for the certificate');
+    assert.dom(SELECTORS.value).hasNoText('There is no value for the certificate card');
+    assert.dom(SELECTORS.icon).exists('The certificate icon exists');
+    assert.dom(SELECTORS.copyButton).exists('The copy button exists');
   });
 
   test('it renders with a small example value for certificate ', async function (assert) {
@@ -30,28 +35,12 @@ module('Integration | Component | certificate-card', function (hooks) {
     assert.dom(SELECTORS.value).hasText('test', 'The value for the certificate is correct');
   });
 
-  test('it renders with an example Kubernetes CA Certificate', async function (assert) {
-    const certificate = `
-      -----BEGIN CERTIFICATE-----
-      MIICUTCCAfugAwIBAgIBADANBgkqhkiG9w0BAQQFADBXMQswCQYDVQQGEwJDTjEL
-      MAkGA1UECBMCUE4xCzAJBgNVBAcTAkNOMQswCQYDVQQKEwJPTjELMAkGA1UECxMC
-      VU4xFDASBgNVBAMTC0hlcm9uZyBZYW5nMB4XDTA1MDcxNTIxMTk0N1oXDTA1MDgx
-      NDIxMTk0N1owVzELMAkGA1UEBhMCQ04xCzAJBgNVBAgTAlBOMQswCQYDVQQHEwJD
-      TjELMAkGA1UEChMCT04xCzAJBgNVBAsTAlVOMRQwEgYDVQQDEwtIZXJvbmcgWWFu
-      ZzBcMA0GCSqGSIb3DQEBAQUAA0sAMEgCQQCp5hnG7ogBhtlynpOS21cBewKE/B7j
-      V14qeyslnr26xZUsSVko36ZnhiaO/zbMOoRcKK9vEcgMtcLFuQTWDl3RAgMBAAGj
-      gbEwga4wHQYDVR0OBBYEFFXI70krXeQDxZgbaCQoR4jUDncEMH8GA1UdIwR4MHaA
-      FFXI70krXeQDxZgbaCQoR4jUDncEoVukWTBXMQswCQYDVQQGEwJDTjELMAkGA1UE
-      CBMCUE4xCzAJBgNVBAcTAkNOMQswCQYDVQQKEwJPTjELMAkGA1UECxMCVU4xFDAS
-      BgNVBAMTC0hlcm9uZyBZYW5nggEAMAwGA1UdEwQFMAMBAf8wDQYJKoZIhvcNAQEE
-      BQADQQA/ugzBrjjK9jcWnDVfGHlk3icNRq0oV7Ri32z/+HQX67aRfgZu7KWdI+Ju
-      Wm7DCfrPNGVwFWUQOmsPue9rZBgO
-      -----END CERTIFICATE-----
-    `;
+  test('it renders with an example PEM Certificate', async function (assert) {
+    const certificate = rootPem;
     this.set('certificate', certificate);
     await render(hbs`<CertificateCard @certificateValue={{this.certificate}}/>`);
 
     assert.dom(SELECTORS.label).hasText('PEM Format', 'The label text is correct');
-    assert.dom(SELECTORS.value).hasText(certificate, 'The value for the CA Certificate is correct');
+    assert.dom(SELECTORS.value).hasText(certificate, 'The value for the certificate is correct');
   });
 });
