c9133fddc6a0b5a6027294dc569499f658c5e309
Kianna Quach <kianna.quach@hashicorp.com>
2023-08-08 23:23:59 -0700

    Add modal to application file so that modal shows on log in and allow users to not show modal again

--- ui/app/controllers/application.js
+++ ui/app/controllers/application.js
@@ -3,12 +3,30 @@
  * SPDX-License-Identifier: MPL-2.0
  */
 
-import { inject as service } from '@ember/service';
-import Controller from '@ember/controller';
 import config from '../config/environment';
+import Controller from '@ember/controller';
+import { inject as service } from '@ember/service';
+import { tracked } from '@glimmer/tracking';
+import { action } from '@ember/object';
+import localStorage from 'vault/lib/local-storage';
+export default class ApplicationController extends Controller {
+  @service auth;
+  @service store;
+  @tracked showNewFeatureModal = true;
+  @tracked alwaysHideNewFeaturesModal = false;
 
-export default Controller.extend({
-  env: config.environment,
-  auth: service(),
-  store: service(),
-});
+  env = config.environment;
+
+  get shouldHideNewFeaturesModal() {
+    return localStorage.getItem('alwaysHideNewFeaturesModal');
+  }
+
+  @action
+  toggleAlwaysHideNewFeaturesModal() {
+    this.alwaysHideNewFeaturesModal = !this.alwaysHideNewFeaturesModal;
+
+    this.alwaysHideNewFeaturesModal
+      ? localStorage.setItem('alwaysHideNewFeaturesModal', true)
+      : localStorage.removeItem('alwaysHideNewFeaturesModal');
+  }
+}
