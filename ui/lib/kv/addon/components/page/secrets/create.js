cc284608779db1a8f9025885610f4a4b1828f270
clairebontempo@gmail.com <clairebontempo@gmail.com>
2023-08-09 11:27:07 -0700

    address feedback add check for model changing

--- ui/lib/kv/addon/components/page/secrets/create.js
+++ ui/lib/kv/addon/components/page/secrets/create.js
@@ -30,7 +30,7 @@ export default class KvSecretCreate extends Component {
   @service router;
 
   @tracked showJsonView = false;
-  @tracked errors;
+  @tracked errorMessage; // only renders for kv/data API errors
   @tracked modelValidations;
   @tracked invalidFormAlert;
 
@@ -49,49 +49,41 @@ export default class KvSecretCreate extends Component {
     }
   }
 
-  validate() {
-    const dataValidations = this.args.secret.validate();
-    const metadataValidations = this.args.metadata.validate();
-    const state = { ...dataValidations.state, ...metadataValidations.state };
-    const failed = !dataValidations.isValid || !metadataValidations.isValid;
-    return { state, isValid: !failed };
-  }
-
   @task
   *save(event) {
     event.preventDefault();
-    this.flashMessages.clearMessages();
-    this.errors = [];
+    this.resetErrors();
+
     const { isValid, state } = this.validate();
     this.modelValidations = isValid ? null : state;
     this.invalidFormAlert = isValid ? '' : 'There was a problem submitting this form.';
 
     const { secret, metadata } = this.args;
-    // users must have kv/data create to create metadata in the UI
     if (isValid) {
       try {
         // try saving secret data first
         yield secret.save();
         this.flashMessages.success(`Successfully saved secret data for: ${secret.path}.`);
       } catch (error) {
-        this.errors.pushObject({ endpoint: 'kv/data', message: errorMessage(error) });
+        this.errorMessage = errorMessage(error);
       }
 
-      // only attempt to save metadata if secret data was saved
-      if (secret.createdTime) {
+      // users must have permission to create secret data to create metadata in the UI
+      // only attempt to save metadata if secret data saves successfully and metadata is edited
+      if (secret.createdTime && this.hasChanged(metadata)) {
         try {
           metadata.path = secret.path;
           yield metadata.save();
           this.flashMessages.success(`Successfully saved metadata.`);
         } catch (error) {
-          this.flashMessages.danger(`POST kv/metadata: ${errorMessage(error)}`, {
+          this.flashMessages.danger(`Secret data was saved but metadata was not: ${errorMessage(error)}`, {
             sticky: true,
           });
         }
       }
 
       // prevent transition if there are errors with secret data
-      if (this.errors.length) {
+      if (this.errorMessage) {
         this.invalidFormAlert = 'There was an error submitting this form.';
       } else {
         this.router.transitionTo('vault.cluster.secrets.backend.kv.secret.details', secret.path);
@@ -105,5 +97,27 @@ export default class KvSecretCreate extends Component {
     pathIsFromDirectory(path)
       ? this.router.transitionTo('vault.cluster.secrets.backend.kv.list-directory', path)
       : this.router.transitionTo('vault.cluster.secrets.backend.kv.list');
+  }
+
+  // HELPERS
+
+  validate() {
+    const dataValidations = this.args.secret.validate();
+    const metadataValidations = this.args.metadata.validate();
+    const state = { ...dataValidations.state, ...metadataValidations.state };
+    const failed = !dataValidations.isValid || !metadataValidations.isValid;
+    return { state, isValid: !failed };
+  }
+
+  hasChanged(model) {
+    const formAttrs = model.formFields.map((attr) => attr.name);
+    const changedAttrs = Object.keys(model.changedAttributes());
+    // exclusively check form attr changes ('backend' and 'path' are passed to createRecord)
+    return changedAttrs.any((attr) => formAttrs.includes(attr));
+  }
+
+  resetErrors() {
+    this.flashMessages.clearMessages();
+    this.errors = '';
   }
 }
