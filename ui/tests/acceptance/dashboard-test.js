5d5b715abcb328512335520c3c3f882751fe8146
Kianna Quach <kianna.quach@hashicorp.com>
2023-08-09 09:32:14 -0700

    Fix failing tests

--- ui/tests/acceptance/dashboard-test.js
+++ ui/tests/acceptance/dashboard-test.js
@@ -4,7 +4,7 @@
  */
 
 import { module, test } from 'qunit';
-import { visit, currentURL } from '@ember/test-helpers';
+import { visit, currentURL, click } from '@ember/test-helpers';
 import { setupApplicationTest } from 'vault/tests/helpers';
 import { setupMirage } from 'ember-cli-mirage/test-support';
 import authPage from 'vault/tests/pages/auth';
@@ -16,95 +16,7 @@ module('Acceptance | landing page dashboard', function (hooks) {
   setupApplicationTest(hooks);
   setupMirage(hooks);
 
-  hooks.beforeEach(function () {
-    this.data = {
-      api_addr: 'http://127.0.0.1:8200',
-      cache_size: 0,
-      cluster_addr: 'https://127.0.0.1:8201',
-      cluster_cipher_suites: '',
-      cluster_name: '',
-      default_lease_ttl: 0,
-      default_max_request_duration: 0,
-      detect_deadlocks: '',
-      disable_cache: false,
-      disable_clustering: false,
-      disable_indexing: false,
-      disable_mlock: true,
-      disable_performance_standby: false,
-      disable_printable_check: false,
-      disable_sealwrap: false,
-      disable_sentinel_trace: false,
-      enable_response_header_hostname: false,
-      enable_response_header_raft_node_id: false,
-      enable_ui: true,
-      experiments: null,
-      introspection_endpoint: false,
-      listeners: [
-        {
-          config: {
-            address: '0.0.0.0:8200',
-            cluster_address: '0.0.0.0:8201',
-            tls_disable: true,
-          },
-          type: 'tcp',
-        },
-      ],
-      log_format: '',
-      log_level: 'debug',
-      log_requests_level: '',
-      max_lease_ttl: '48h',
-      pid_file: '',
-      plugin_directory: '',
-      plugin_file_permissions: 0,
-      plugin_file_uid: 0,
-      raw_storage_endpoint: true,
-      seals: [
-        {
-          disabled: false,
-          type: 'shamir',
-        },
-      ],
-      storage: {
-        cluster_addr: 'https://127.0.0.1:8201',
-        disable_clustering: false,
-        raft: {
-          max_entry_size: '',
-        },
-        redirect_addr: 'http://127.0.0.1:8200',
-        type: 'raft',
-      },
-      telemetry: {
-        add_lease_metrics_namespace_labels: false,
-        circonus_api_app: '',
-        circonus_api_token: '',
-        circonus_api_url: '',
-        circonus_broker_id: '',
-        circonus_broker_select_tag: '',
-        circonus_check_display_name: '',
-        circonus_check_force_metric_activation: '',
-        circonus_check_id: '',
-        circonus_check_instance_id: '',
-        circonus_check_search_tag: '',
-        circonus_check_tags: '',
-        circonus_submission_interval: '',
-        circonus_submission_url: '',
-        disable_hostname: true,
-        dogstatsd_addr: '',
-        dogstatsd_tags: null,
-        lease_metrics_epsilon: 3600000000000,
-        maximum_gauge_cardinality: 500,
-        metrics_prefix: '',
-        num_lease_metrics_buckets: 168,
-        prometheus_retention_time: 86400000000000,
-        stackdriver_debug_logs: false,
-        stackdriver_location: '',
-        stackdriver_namespace: '',
-        stackdriver_project_id: '',
-        statsd_address: '',
-        statsite_address: '',
-        usage_gauge_period: 5000000000,
-      },
-    };
+  hooks.beforeEach(async function () {
     return authPage.login();
   });
 
@@ -114,10 +26,17 @@ module('Acceptance | landing page dashboard', function (hooks) {
 
   test('display the version number for the title', async function (assert) {
     await visit('/vault/dashboard');
+    await click('[data-test-do-not-show-me-again]');
+    await click('[data-test-new-features-modal-button]');
     assert.dom('[data-test-dashboard-version-header]').hasText('Vault v1.9.0');
   });
 
-  module('secrets engines card', function () {
+  module('secrets engines card', function (hooks) {
+    hooks.beforeEach(async function () {
+      await authPage.login();
+      await click('[data-test-do-not-show-me-again]');
+      await click('[data-test-new-features-modal-button]');
+    });
     test('shows a secrets engine card', async function (assert) {
       await mountSecrets.enable('pki', 'pki');
       await visit('/vault/dashboard');
@@ -132,9 +51,14 @@ module('Acceptance | landing page dashboard', function (hooks) {
     });
   });
 
-  module('learn more card', function () {
+  module('learn more card', function (hooks) {
+    hooks.beforeEach(async function () {
+      await authPage.login();
+      await click('[data-test-do-not-show-me-again]');
+      await click('[data-test-new-features-modal-button]');
+    });
+
     test('shows the learn more card', async function (assert) {
-      await visit('/vault/dashboard');
       assert.dom('[data-test-learn-more-title]').hasText('Learn more');
       assert
         .dom('[data-test-learn-more-subtext]')
@@ -145,7 +69,98 @@ module('Acceptance | landing page dashboard', function (hooks) {
     });
   });
 
-  module('configuration details card', function () {
+  module('configuration details card', function (hooks) {
+    hooks.beforeEach(async function () {
+      this.data = {
+        api_addr: 'http://127.0.0.1:8200',
+        cache_size: 0,
+        cluster_addr: 'https://127.0.0.1:8201',
+        cluster_cipher_suites: '',
+        cluster_name: '',
+        default_lease_ttl: 0,
+        default_max_request_duration: 0,
+        detect_deadlocks: '',
+        disable_cache: false,
+        disable_clustering: false,
+        disable_indexing: false,
+        disable_mlock: true,
+        disable_performance_standby: false,
+        disable_printable_check: false,
+        disable_sealwrap: false,
+        disable_sentinel_trace: false,
+        enable_response_header_hostname: false,
+        enable_response_header_raft_node_id: false,
+        enable_ui: true,
+        experiments: null,
+        introspection_endpoint: false,
+        listeners: [
+          {
+            config: {
+              address: '0.0.0.0:8200',
+              cluster_address: '0.0.0.0:8201',
+              tls_disable: true,
+            },
+            type: 'tcp',
+          },
+        ],
+        log_format: '',
+        log_level: 'debug',
+        log_requests_level: '',
+        max_lease_ttl: '48h',
+        pid_file: '',
+        plugin_directory: '',
+        plugin_file_permissions: 0,
+        plugin_file_uid: 0,
+        raw_storage_endpoint: true,
+        seals: [
+          {
+            disabled: false,
+            type: 'shamir',
+          },
+        ],
+        storage: {
+          cluster_addr: 'https://127.0.0.1:8201',
+          disable_clustering: false,
+          raft: {
+            max_entry_size: '',
+          },
+          redirect_addr: 'http://127.0.0.1:8200',
+          type: 'raft',
+        },
+        telemetry: {
+          add_lease_metrics_namespace_labels: false,
+          circonus_api_app: '',
+          circonus_api_token: '',
+          circonus_api_url: '',
+          circonus_broker_id: '',
+          circonus_broker_select_tag: '',
+          circonus_check_display_name: '',
+          circonus_check_force_metric_activation: '',
+          circonus_check_id: '',
+          circonus_check_instance_id: '',
+          circonus_check_search_tag: '',
+          circonus_check_tags: '',
+          circonus_submission_interval: '',
+          circonus_submission_url: '',
+          disable_hostname: true,
+          dogstatsd_addr: '',
+          dogstatsd_tags: null,
+          lease_metrics_epsilon: 3600000000000,
+          maximum_gauge_cardinality: 500,
+          metrics_prefix: '',
+          num_lease_metrics_buckets: 168,
+          prometheus_retention_time: 86400000000000,
+          stackdriver_debug_logs: false,
+          stackdriver_location: '',
+          stackdriver_namespace: '',
+          stackdriver_project_id: '',
+          statsd_address: '',
+          statsite_address: '',
+          usage_gauge_period: 5000000000,
+        },
+      };
+    });
+
     test('shows the configuration details card', async function (assert) {
       this.server.get('sys/config/state/sanitized', () => ({
         data: this.data,
@@ -155,6 +170,8 @@ module('Acceptance | landing page dashboard', function (hooks) {
       }));
       await authPage.login();
       await visit('/vault/dashboard');
+      await click('[data-test-do-not-show-me-again]');
+      await click('[data-test-new-features-modal-button]');
       assert.dom(VAULT_CONFIGURATION_SELECTORS.cardTitle).hasText('Configuration details');
       assert.dom(VAULT_CONFIGURATION_SELECTORS.apiAddr).hasText('http://127.0.0.1:8200');
       assert.dom(VAULT_CONFIGURATION_SELECTORS.defaultLeaseTtl).hasText('0');
@@ -175,7 +192,8 @@ module('Acceptance | landing page dashboard', function (hooks) {
         };
       });
       await authPage.login();
-      await visit('/vault/dashboard');
+      await click('[data-test-do-not-show-me-again]');
+      await click('[data-test-new-features-modal-button]');
       assert.dom(VAULT_CONFIGURATION_SELECTORS.tlsDisable).hasText('Disabled');
     });
     test('shows the tls disabled if there is no tlsDisabled returned from server', async function (assert) {
@@ -190,7 +208,8 @@ module('Acceptance | landing page dashboard', function (hooks) {
         };
       });
       await authPage.login();
-      await visit('/vault/dashboard');
+      await click('[data-test-new-features-modal-button]');
+      await click('[data-test-new-features-modal-button]');
       assert.dom(VAULT_CONFIGURATION_SELECTORS.tlsDisable).hasText('Disabled');
     });
   });
