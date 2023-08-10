38e8d08ac3725f7da82dad83d971356f899ea56b
Jordan Reimer <zofskeez@gmail.com>
2023-08-10 14:06:10 -0600

    adds ldap component for checked out accounts

--- ui/app/app.js
+++ ui/app/app.js
@@ -54,7 +54,7 @@ export default class App extends Application {
     },
     ldap: {
       dependencies: {
-        services: ['router', 'store', 'secret-mount-path', 'flash-messages'],
+        services: ['router', 'store', 'secret-mount-path', 'flash-messages', 'auth'],
         externalRoutes: {
           secrets: 'vault.cluster.secrets.backends',
         },
