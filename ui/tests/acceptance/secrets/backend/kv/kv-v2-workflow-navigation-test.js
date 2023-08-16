87ab8797cadf85c06f12b009c0d84c13009f8c76
Chelsea Shaw <cshaw@hashicorp.com>
2023-08-16 14:32:04 -0500

    Fix tests

--- ui/tests/acceptance/secrets/backend/kv/kv-v2-workflow-navigation-test.js
+++ ui/tests/acceptance/secrets/backend/kv/kv-v2-workflow-navigation-test.js
@@ -1228,7 +1228,7 @@ path "${this.backend}/*" {
       assertCorrectBreadcrumbs(assert, ['secret', backend, 'app', 'nested', 'secret']);
       assert.dom(PAGE.title).hasText('app/nested/secret', 'title is full secret path');
       assert.dom(PAGE.toolbar).exists('toolbar renders');
-      assert.dom(PAGE.toolbarAction).exists({ count: 4 }, 'correct number of toolbar actions render');
+      assert.dom(PAGE.toolbarAction).exists({ count: 5 }, 'correct number of toolbar actions render');
 
       await click(PAGE.breadcrumbAtIdx(3));
       assert.ok(
