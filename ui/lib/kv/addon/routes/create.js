e12f91cd449eb7377ec9d2653dfaf238cb49684c
Chelsea Shaw <cshaw@hashicorp.com>
2023-08-17 10:25:19 -0500

    Address PR comments

--- ui/lib/kv/addon/routes/create.js
+++ ui/lib/kv/addon/routes/create.js
@@ -21,6 +21,7 @@ export default class KvSecretsCreateRoute extends Route {
     return hash({
       backend,
       path,
+      // see serializer for logic behind setting casVersion
       secret: this.store.createRecord('kv/data', { backend, path, casVersion: 0 }),
       metadata: this.store.createRecord('kv/metadata', { backend, path }),
     });
