dc2a658527ed2634bfcf34dd40810b45b52d8af1
Marc Boudreau <marc.boudreau@hashicorp.com>
2023-08-17 14:05:50 -0400

    enable all audit devices in Enos's vault_cluster module

--- enos/modules/vault_cluster/scripts/enable_audit_logging.sh
+++ enos/modules/vault_cluster/scripts/enable_audit_logging.sh
@@ -5,4 +5,11 @@
 
 set -eux
 
+LOG_DIR="$(dirname "$LOG_FILE_PATH")"
+
+# Run nc to listen to port 9090
+nc -l 9090 &
+
 $VAULT_BIN_PATH audit enable file file_path="$LOG_FILE_PATH"
+$VAULT_BIN_PATH audit enable syslog tag="vault" facility="AUTH"
+$VAULT_BIN_PATH audit enable socket address="127.0.0.1:9090"
