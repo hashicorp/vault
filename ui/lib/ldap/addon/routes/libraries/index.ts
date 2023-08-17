23425bca8ea676c4119f2483a1df464d702f941c
Jordan Reimer <zofskeez@gmail.com>
2023-08-17 15:20:26 -0600

    Fetch Secrets Engine Config Decorator Docs (#22416)
    * removes uneccesary asyncs from ldap route model hooks
    
    * updates ldap overview route class name
    
    * adds documentation for fetch-secrets-engine-config decorator

--- ui/lib/ldap/addon/routes/libraries/index.ts
+++ ui/lib/ldap/addon/routes/libraries/index.ts
@@ -33,7 +33,7 @@ export default class LdapLibrariesRoute extends Route {
 
   declare promptConfig: boolean;
 
-  async model() {
+  model() {
     const backendModel = this.modelFor('application') as SecretEngineModel;
     return hash({
       backendModel,
