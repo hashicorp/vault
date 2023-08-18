a9cde70550358c6d6f10191256214ae94283c6e6
Malina Calarco <malina.calarco@hashicorp.com>
2023-08-18 09:51:22 -0700

    fix tests caused by changing certificate-card. change hds copy button in certificate-card.hbs

--- ui/tests/integration/components/pki/pki-generate-csr-test.js
+++ ui/tests/integration/components/pki/pki-generate-csr-test.js
@@ -114,13 +114,13 @@ module('Integration | Component | pki-generate-csr', function (hooks) {
       );
     assert
       .dom('[data-test-value-div="CSR"] [data-test-certificate-card] button')
-      .hasAttribute('data-clipboard-text', this.model.csr, 'it renders copyable csr');
+      .hasAttribute('data-clipboard-target', this.model.csr, 'it renders copyable csr');
     assert
       .dom('[data-test-value-div="Key ID"] button')
       .hasAttribute('data-clipboard-text', this.model.keyId, 'it renders copyable key_id');
     assert
       .dom('[data-test-value-div="Private key"] [data-test-certificate-card] button')
-      .hasAttribute('data-clipboard-text', this.model.privateKey, 'it renders copyable private_key');
+      .hasAttribute('data-clipboard-target', this.model.privateKey, 'it renders copyable private_key');
     assert
       .dom('[data-test-value-div="Private key type"]')
       .hasText(this.model.privateKeyType, 'renders private_key_type');
@@ -145,7 +145,7 @@ module('Integration | Component | pki-generate-csr', function (hooks) {
       );
     assert
       .dom('[data-test-value-div="CSR"] [data-test-certificate-card] button')
-      .hasAttribute('data-clipboard-text', this.model.csr, 'it renders copyable csr');
+      .hasAttribute('data-clipboard-target', this.model.csr, 'it renders copyable csr');
     assert
       .dom('[data-test-value-div="Key ID"] button')
       .hasAttribute('data-clipboard-text', this.model.keyId, 'it renders copyable key_id');
