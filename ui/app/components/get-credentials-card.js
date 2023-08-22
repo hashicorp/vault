cc2654e1a6761b045284e292bae2fe89f13e3990
Chelsea Shaw <cshaw@hashicorp.com>
2023-08-22 13:05:52 -0500

    Add KV engine cleanup todo

--- ui/app/components/get-credentials-card.js
+++ ui/app/components/get-credentials-card.js
@@ -22,6 +22,7 @@
  * @param {string} backend - Passed to SearchSelect query method to fetch dropdown options
  */
 
+// TODO: kv engine cleanup remove secrets related logic
 import Component from '@glimmer/component';
 import { inject as service } from '@ember/service';
 import { action } from '@ember/object';
