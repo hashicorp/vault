1b6f938790af9a5c2443657bcf6b847372c17bb4
Chelsea Shaw <cshaw@hashicorp.com>
2023-08-22 12:44:03 -0500

    Add initialKey and list nav to get-credentials-card

--- ui/lib/core/addon/components/input-search.js
+++ ui/lib/core/addon/components/input-search.js
@@ -16,6 +16,12 @@ export default class inputSelect extends Component {
    *
    */
   @tracked searchInput = '';
+
+  constructor() {
+    super(...arguments);
+    this.searchInput = this.args?.initialValue;
+  }
+
   @action
   inputChanged() {
     this.args.onChange(this.searchInput);
