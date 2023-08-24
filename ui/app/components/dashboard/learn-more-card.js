a2b8b80e471cb2bb98c0d95cff5436f6e5c3c712
Kianna Quach <kianna.quach@hashicorp.com>
2023-08-24 12:23:43 -0700

    Update links to not use hds

--- ui/app/components/dashboard/learn-more-card.js
+++ ui/app/components/dashboard/learn-more-card.js
@@ -19,23 +19,23 @@ export default class DashboardLearnMoreCard extends Component {
   get learnMoreLinks() {
     return [
       {
-        link: 'https://developer.hashicorp.com/vault/tutorials/secrets-management',
+        link: '/vault/tutorials/secrets-management',
         icon: 'docs-link',
         title: 'Secrets Management',
       },
       {
-        link: 'https://developer.hashicorp.com/vault/tutorials/monitoring',
+        link: '/vault/tutorials/monitoring',
         icon: 'docs-link',
         title: 'Monitor & Troubleshooting',
       },
       {
-        link: 'https://developer.hashicorp.com/vault/tutorials/adp/transform',
+        link: '/vault/tutorials/adp/transform',
         icon: 'learn-link',
         title: 'Advanced Data Protection: Transform engine',
         requiredFeature: 'Transform Secrets Engine',
       },
       {
-        link: 'https://developer.hashicorp.com/vault/tutorials/secrets-management/pki-engine',
+        link: '/vault/tutorials/secrets-management/pki-engine',
         icon: 'learn-link',
         title: 'Build your own Certificate Authority (CA)',
       },
