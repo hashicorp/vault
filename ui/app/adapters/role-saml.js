39ee5b1cb7e1e0a8ef889cd9f8fb6483e4b23e22
Chelsea Shaw <cshaw@hashicorp.com>
2023-08-30 16:07:54 -0500

    more ember-y

--- ui/app/adapters/role-saml.js
+++ ui/app/adapters/role-saml.js
@@ -11,41 +11,38 @@ import { v4 as uuidv4 } from 'uuid';
 export default ApplicationAdapter.extend({
   router: service(),
 
-  findRecord(store, type, id) {
+  // generateClientChallenge generates a client challenge from a verifier.
+  // The client challenge is the base64(sha256(verifier)). The verifier is
+  // later presented to the server to obtain the resulting Vault token.
+  async generateClientChallenge(verifier) {
+    const encoder = new TextEncoder();
+    const data = encoder.encode(verifier);
+    const hashBuffer = await crypto.subtle.digest('SHA-256', data);
+    const hashArray = new Uint8Array(hashBuffer);
+    return btoa(String.fromCharCode.apply(null, hashArray));
+  },
+
+  async findRecord(store, type, id) {
     let [path, role] = JSON.parse(id);
     path = encodePath(path);
 
     // Create the client verifier and challenge
     const verifier = uuidv4();
-    return generateClientChallenge(verifier).then((challenge) => {
-      // Kick off the authentication flow by generating the SSO service URL
-      // It requires the client challenge generated from the verifier. We'll
-      // later provide the verifier to match up with the challenge on the server
-      // when we poll for the Vault token by its returned token poll ID.
-      return this.ajax(`/v1/auth/${path}/sso_service_url`, 'PUT', {
-        data: {
-          role,
-          client_challenge: challenge,
-          client_type: 'browser',
-        },
-      }).then((response) => {
-        return {
-          sso_service_url: response.data.sso_service_url,
-          token_poll_id: response.data.token_poll_id,
-          client_verifier: verifier,
-        };
-      });
+    const challenge = await this.generateClientChallenge(verifier);
+    // Kick off the authentication flow by generating the SSO service URL
+    // It requires the client challenge generated from the verifier. We'll
+    // later provide the verifier to match up with the challenge on the server
+    // when we poll for the Vault token by its returned token poll ID.
+    const response = await this.ajax(`/v1/auth/${path}/sso_service_url`, 'PUT', {
+      data: {
+        role,
+        client_challenge: challenge,
+        client_type: 'browser',
+      },
     });
+    return {
+      ...response.data,
+      client_verifier: verifier,
+    };
   },
 });
-
-// generateClientChallenge generates a client challenge from a verifier.
-// The client challenge is the base64(sha256(verifier)). The verifier is
-// later presented to the server to obtain the resulting Vault token.
-async function generateClientChallenge(verifier) {
-  const encoder = new TextEncoder();
-  const data = encoder.encode(verifier);
-  const hashBuffer = await crypto.subtle.digest('SHA-256', data);
-  const hashArray = new Uint8Array(hashBuffer);
-  return btoa(String.fromCharCode.apply(null, hashArray));
-}
