ac68cd06de356d4eaacd043c30de4bc0299198dd
Victor Rodriguez <vrizo@hashicorp.com>
2023-08-31 14:56:21 -0400

    Extract method SealWrapper.CheckHealth out of StartHealthCheck.
    Restore the metrics around overall seal health, so that we have metrics for
    individual seal wrappers as well as the backend itself.

--- vault/seal/seal_wrapper.go
+++ vault/seal/seal_wrapper.go
@@ -1,7 +1,12 @@
 package seal
 
 import (
+	"bytes"
+	"context"
+	"errors"
+	"fmt"
 	wrapping "github.com/hashicorp/go-kms-wrapping/v2"
+	mathrand "math/rand"
 	"sync"
 	"time"
 )
@@ -23,4 +28,49 @@ type SealWrapper struct {
 	LastHealthCheck time.Time
 	LastSeenHealthy time.Time
 	Healthy         bool
+}
+
+func (sw *SealWrapper) IsHealthy() bool {
+	sw.HcLock.RLock()
+	defer sw.HcLock.RUnlock()
+
+	return sw.Healthy
+}
+
+var (
+	// vars for unit testing
+	HealthTestIntervalNominal   = 10 * time.Minute
+	HealthTestIntervalUnhealthy = 1 * time.Minute
+	HealthTestTimeout           = 1 * time.Minute
+)
+
+func (sw *SealWrapper) CheckHealth(ctx context.Context, checkTime time.Time) error {
+	sw.HcLock.Lock()
+	defer sw.HcLock.Unlock()
+
+	sw.LastHealthCheck = checkTime
+
+	// Assume the wrapper is unhealthy, if we make it to the end we'll set it to true
+	sw.Healthy = false
+
+	testVal := fmt.Sprintf("Heartbeat %d", mathrand.Intn(1000))
+	ciphertext, err := sw.Wrapper.Encrypt(ctx, []byte(testVal), nil)
+	if err != nil {
+		return fmt.Errorf("failed to encrypt test value, seal wrapper may be unreachable: %w", err)
+	}
+
+	ctx, cancel := context.WithTimeout(ctx, HealthTestTimeout)
+	defer cancel()
+	plaintext, err := sw.Wrapper.Decrypt(ctx, ciphertext, nil)
+	if err != nil {
+		return fmt.Errorf("failed to decrypt test value, seal wrapper may be unreachable: %w", err)
+	}
+	if !bytes.Equal([]byte(testVal), plaintext) {
+		return errors.New("failed to decrypt health test value to expected result")
+	}
+
+	sw.LastSeenHealthy = checkTime
+	sw.Healthy = true
+
+	return nil
 }
