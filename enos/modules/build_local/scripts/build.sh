2fd173edf82ab68415a48f1d6260bde5bad9a787
Sarah Thompson <sthompson@hashicorp.com>
2023-09-06 17:52:11 +0100

    cherrypick of https://github.com/hashicorp/vault/commit/a9a4b0b9ff4459deaeab4ba62927a2e2633be878 (#22806)

--- enos/modules/build_local/scripts/build.sh
+++ enos/modules/build_local/scripts/build.sh
@@ -11,5 +11,11 @@ export CGO_ENABLED=0
 
 root_dir="$(git rev-parse --show-toplevel)"
 pushd "$root_dir" > /dev/null
-make ci-build-ui ci-build ci-bundle
+make ci-build-ui ci-build
+
+: "${BIN_PATH:="dist"}"
+: "${BUNDLE_PATH:=$(git rev-parse --show-toplevel)/vault.zip}"
+echo "--> Bundling $BIN_PATH/* to $BUNDLE_PATH"
+zip -r -j "$BUNDLE_PATH" "$BIN_PATH/"
+
 popd > /dev/null
