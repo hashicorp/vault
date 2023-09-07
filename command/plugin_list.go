53a91576ca850ad68d7d784bf00b39c1a7a91777
Thy Ton <maithytonn@gmail.com>
2023-09-07 12:58:56 -0700

    Update command/plugin_list.go
    Co-authored-by: Tom Proctor <tomhjp@users.noreply.github.com>

--- command/plugin_list.go
+++ command/plugin_list.go
@@ -160,7 +160,7 @@ func (c *PluginListCommand) simpleResponse(plugins *api.ListPluginsResponse, plu
 }
 
 func (c *PluginListCommand) detailedResponse(plugins *api.ListPluginsResponse) []string {
-	out := []string{"Name | Type | Version | OCI Image | Runtime | Deprecation Status"}
+	out := []string{"Name | Type | Version | Container | Deprecation Status"}
 	for _, plugin := range plugins.Details {
 		out = append(out, fmt.Sprintf("%s | %s | %s | %s | %s | %s", plugin.Name, plugin.Type, plugin.Version, plugin.OCIImage, plugin.Runtime, plugin.DeprecationStatus))
 	}
