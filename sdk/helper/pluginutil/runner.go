2bcb412b82214a0cbf1f0dd3efcf661427688162
hc-github-team-secure-vault-core <82990506+hc-github-team-secure-vault-core@users.noreply.github.com>
2023-09-07 17:29:40 -0400

    backport of commit f96ecf38006a98b857fb6f5370f3c302e7180a1e (#22875)
    Co-authored-by: Thy Ton <maithytonn@gmail.com>

--- sdk/helper/pluginutil/runner.go
+++ sdk/helper/pluginutil/runner.go
@@ -131,6 +131,8 @@ type VersionedPlugin struct {
 	Type              string `json:"type"` // string instead of consts.PluginType so that we get the string form in API responses.
 	Name              string `json:"name"`
 	Version           string `json:"version"`
+	OCIImage          string `json:"oci_image,omitempty"`
+	Runtime           string `json:"runtime,omitempty"`
 	SHA256            string `json:"sha256,omitempty"`
 	Builtin           bool   `json:"builtin"`
 	DeprecationStatus string `json:"deprecation_status,omitempty"`
