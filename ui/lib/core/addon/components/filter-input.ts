5f61c59d44e64f8d17d0174edc2b823c9a33c47e
Jordan Reimer <zofskeez@gmail.com>
2023-09-12 15:38:26 +0000

    backport of commit 8f6535cfafe9a2fd58707b1e8748f1c9ed1fc619

--- ui/lib/core/addon/components/filter-input.ts
+++ ui/lib/core/addon/components/filter-input.ts
@@ -5,22 +5,39 @@
 
 import Component from '@glimmer/component';
 import { action } from '@ember/object';
-import { debounce } from '@ember/runloop';
+import { debounce, next } from '@ember/runloop';
 
 import type { HTMLElementEvent } from 'vault/forms';
 
 interface Args {
+  value?: string; // initial value
   placeholder?: string; // defaults to Type to filter results
   wait?: number; // defaults to 200
+  autofocus?: boolean; // initially focus the input on did-insert
   onInput(value: string): void;
 }
 
 export default class FilterInputComponent extends Component<Args> {
+  value: string | undefined;
+
+  constructor(owner: unknown, args: Args) {
+    super(owner, args);
+    this.value = this.args.value;
+  }
+
   get placeholder() {
     return this.args.placeholder || 'Type to filter results';
   }
 
-  @action onInput(event: HTMLElementEvent<HTMLInputElement>) {
+  @action
+  focus(elem: HTMLElement) {
+    if (this.args.autofocus) {
+      next(() => elem.focus());
+    }
+  }
+
+  @action
+  onInput(event: HTMLElementEvent<HTMLInputElement>) {
     const callback = () => {
       this.args.onInput(event.target.value);
     };
