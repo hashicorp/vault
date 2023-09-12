3b4cfe7f0f7294e27fe4304c7abe83e988661315
VioletHynes <violet.hynes@hashicorp.com>
2023-09-12 09:54:18 -0400

    Godocs

--- command/agentproxyshared/auth/azure/azure_test.go
+++ command/agentproxyshared/auth/azure/azure_test.go
@@ -10,7 +10,10 @@ import (
 	"github.com/hashicorp/vault/command/agentproxyshared/auth"
 )
 
+// TestAzureAuthMethod tests that NewAzureAuthMethod succeeds
+// with valid config.
 func TestAzureAuthMethod(t *testing.T) {
+	t.Parallel()
 	config := &auth.AuthConfig{
 		Logger:    hclog.NewNullLogger(),
 		MountPath: "auth-test",
@@ -29,7 +32,10 @@ func TestAzureAuthMethod(t *testing.T) {
 	}
 }
 
+// TestAzureAuthMethod_StringAuthFromEnvironment tests that NewAzureAuthMethod succeeds
+// with valid config, where authenticate_from_environment is a string literal.
 func TestAzureAuthMethod_StringAuthFromEnvironment(t *testing.T) {
+	t.Parallel()
 	config := &auth.AuthConfig{
 		Logger:    hclog.NewNullLogger(),
 		MountPath: "auth-test",
@@ -48,7 +54,10 @@ func TestAzureAuthMethod_StringAuthFromEnvironment(t *testing.T) {
 	}
 }
 
+// TestAzureAuthMethod_BadConfig tests that NewAzureAuthMethod fails with
+// an invalid config.
 func TestAzureAuthMethod_BadConfig(t *testing.T) {
+	t.Parallel()
 	config := &auth.AuthConfig{
 		Logger:    hclog.NewNullLogger(),
 		MountPath: "auth-test",
@@ -63,7 +72,11 @@ func TestAzureAuthMethod_BadConfig(t *testing.T) {
 	}
 }
 
+// TestAzureAuthMethod_BadAuthFromEnvironment tests that NewAzureAuthMethod fails
+// with otherwise valid config, but where authenticate_from_environment is
+// an invalid string literal.
 func TestAzureAuthMethod_BadAuthFromEnvironment(t *testing.T) {
+	t.Parallel()
 	config := &auth.AuthConfig{
 		Logger:    hclog.NewNullLogger(),
 		MountPath: "auth-test",
