5e4a86091f58b1439fd3071fa96ec90912523f55
Tom Proctor <tomhjp@users.noreply.github.com>
2023-09-13 12:12:29 +0100

    events: Ensure pipelines are cleaned up on closing subscription

--- vault/eventbus/bus.go
+++ vault/eventbus/bus.go
@@ -69,10 +69,10 @@ type asyncChanNode struct {
 	logger hclog.Logger
 
 	// used to close the connection
-	closeOnce  sync.Once
-	cancelFunc context.CancelFunc
-	pipelineID eventlogger.PipelineID
-	broker     *eventlogger.Broker
+	closeOnce      sync.Once
+	cancelFunc     context.CancelFunc
+	pipelineID     eventlogger.PipelineID
+	removePipeline func(ctx context.Context, t eventlogger.EventType, id eventlogger.PipelineID) (bool, error)
 }
 
 var (
@@ -237,7 +237,7 @@ func (bus *EventBus) SubscribeMultipleNamespaces(ctx context.Context, namespaceP
 	}
 
 	ctx, cancel := context.WithCancel(ctx)
-	asyncNode := newAsyncNode(ctx, bus.logger)
+	asyncNode := newAsyncNode(ctx, bus.logger, bus.broker)
 	err = bus.broker.RegisterNode(eventlogger.NodeID(sinkNodeID), asyncNode)
 	if err != nil {
 		defer cancel()
@@ -312,11 +312,12 @@ func newFilterNode(namespacePatterns []string, pattern string, bexprFilter strin
 	}, nil
 }
 
-func newAsyncNode(ctx context.Context, logger hclog.Logger) *asyncChanNode {
+func newAsyncNode(ctx context.Context, logger hclog.Logger, broker *eventlogger.Broker) *asyncChanNode {
 	return &asyncChanNode{
-		ctx:    ctx,
-		ch:     make(chan *eventlogger.Event),
-		logger: logger,
+		ctx:            ctx,
+		ch:             make(chan *eventlogger.Event),
+		logger:         logger,
+		removePipeline: broker.RemovePipelineAndNodes,
 	}
 }
 
@@ -324,17 +325,15 @@ func newAsyncNode(ctx context.Context, logger hclog.Logger) *asyncChanNode {
 func (node *asyncChanNode) Close(ctx context.Context) {
 	node.closeOnce.Do(func() {
 		defer node.cancelFunc()
-		if node.broker != nil {
-			isPipelineRemoved, err := node.broker.RemovePipelineAndNodes(ctx, eventTypeAll, node.pipelineID)
+		removed, err := node.removePipeline(ctx, eventTypeAll, node.pipelineID)
 
-			switch {
-			case err != nil && isPipelineRemoved:
-				msg := fmt.Sprintf("Error removing nodes referenced by pipeline %q", node.pipelineID)
-				node.logger.Warn(msg, err)
-			case err != nil:
-				msg := fmt.Sprintf("Error removing pipeline %q", node.pipelineID)
-				node.logger.Warn(msg, err)
-			}
+		switch {
+		case err != nil && removed:
+			msg := fmt.Sprintf("Error removing nodes referenced by pipeline %q", node.pipelineID)
+			node.logger.Warn(msg, err)
+		case err != nil:
+			msg := fmt.Sprintf("Error removing pipeline %q", node.pipelineID)
+			node.logger.Warn(msg, err)
 		}
 		addSubscriptions(-1)
 	})
