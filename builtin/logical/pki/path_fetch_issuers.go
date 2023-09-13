c4a31dacf0ed398c70229518d7a4197ab1e5bf21
hc-github-team-secure-vault-core <82990506+hc-github-team-secure-vault-core@users.noreply.github.com>
2023-09-13 09:57:36 -0400

    Backport of Clean up unused CRL entries when issuer is removed into release/1.13.x (#23029)
    * backport of commit e2ff1f1c7117574888db91b4b6027be24533d718 (#23030)
    
    Co-authored-by: Alexander Scheel <alex.scheel@hashicorp.com>
    
    * backport of commit 293e8b8ac5469fc18470b4fb03fa6618b796bb5f (#23045)
    
    Co-authored-by: Steven Clark <steven.clark@hashicorp.com>
    
    * Fix call of crlBuilder.rebuild
    
    Signed-off-by: Alexander Scheel <alex.scheel@hashicorp.com>
    
    ---------
    
    Signed-off-by: Alexander Scheel <alex.scheel@hashicorp.com>
    Co-authored-by: Alexander Scheel <alex.scheel@hashicorp.com>
    Co-authored-by: Steven Clark <steven.clark@hashicorp.com>

--- builtin/logical/pki/path_fetch_issuers.go
+++ builtin/logical/pki/path_fetch_issuers.go
@@ -930,6 +930,14 @@ func (b *backend) pathDeleteIssuer(ctx context.Context, req *logical.Request, da
 		response.AddWarning(msg)
 	}
 
+	// Finally, we need to rebuild both the local and the unified CRLs. This
+	// will free up any now unnecessary space used in both the CRL config
+	// and for the underlying CRL.
+	err = b.crlBuilder.rebuild(sc, true)
+	if err != nil {
+		return nil, err
+	}
+
 	return response, nil
 }
 
