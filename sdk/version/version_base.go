aede1a131a8a75855a037fafcc2334caf6d41b9b
hamid ghaf <hamid@hashicorp.com>
2023-09-14 10:10:59 -0700

    update version 1.12.12

--- sdk/version/version_base.go
+++ sdk/version/version_base.go
@@ -11,7 +11,7 @@ var (
 	// Whether cgo is enabled or not; set at build time
 	CgoEnabled bool
 
-	Version           = "1.12.11"
+	Version           = "1.12.12"
 	VersionPrerelease = ""
 	VersionMetadata   = ""
 )
