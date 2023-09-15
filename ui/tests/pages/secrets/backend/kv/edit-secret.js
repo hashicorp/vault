c0951a2c8b5504b93bb8a40a748d593aef02b9a0
Angel Garbarino <angel@hashicorp.com>
2023-09-15 12:13:08 -0600

    remove unused selectors

--- ui/tests/pages/secrets/backend/kv/edit-secret.js
+++ ui/tests/pages/secrets/backend/kv/edit-secret.js
@@ -4,7 +4,7 @@
  */
 
 import { Base } from '../create';
-import { clickable, visitable, create, fillable } from 'ember-cli-page-object';
+import { clickable, create, fillable } from 'ember-cli-page-object';
 
 export default create({
   ...Base,
@@ -12,22 +12,8 @@ export default create({
   secretKey: fillable('[data-test-secret-key]'),
   secretValue: fillable('[data-test-secret-value] textarea'),
   save: clickable('[data-test-secret-save]'),
-  deleteBtn: clickable('[data-test-secret-delete] button'),
-  confirmBtn: clickable('[data-test-confirm-button]'),
-  visitEdit: visitable('/vault/secrets/:backend/edit/:id'),
-  visitEditRoot: visitable('/vault/secrets/:backend/edit'),
   toggleJSON: clickable('[data-test-toggle-input="json"]'),
-  startCreateSecret: clickable('[data-test-secret-create]'),
-  deleteSecret() {
-    return this.deleteBtn().confirmBtn();
-  },
   createSecret: async function (path, key, value) {
     return this.path(path).secretKey(key).secretValue(value).save();
-  },
-  createSecretDontSave: async function (path, key, value) {
-    return this.path(path).secretKey(key).secretValue(value);
-  },
-  editSecret: async function (key, value) {
-    return this.secretKey(key).secretValue(value).save();
   },
 });
