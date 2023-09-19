3b8f888b1535c16c8a2c769568b3c24a07e370d8
Chelsea Shaw <cshaw@hashicorp.com>
2023-09-19 10:46:19 -0500

    Allow user to renew token from expiring soon warning banner

--- ui/app/components/token-expire-warning.js
+++ ui/app/components/token-expire-warning.js
@@ -6,10 +6,37 @@
 import Component from '@glimmer/component';
 import { inject as service } from '@ember/service';
 import { tracked } from '@glimmer/tracking';
+import { later } from '@ember/runloop';
+import { task } from 'ember-concurrency';
 
 export default class TokenExpireWarning extends Component {
+  @service auth;
   @service router;
   @tracked canDismiss = true;
+  @tracked isRenewing = false;
+  @tracked renewError = '';
+
+  handleRenew() {
+    return new Promise((resolve) => {
+      later(() => {
+        this.auth
+          .renew()
+          .then(() => {
+            // This renewal was triggered by an explicit user action,
+            // so this will reset the time inactive calculation
+            this.auth.setLastFetch(Date.now());
+          })
+          .finally(() => {
+            resolve();
+          });
+      }, 200);
+    });
+  }
+
+  @task
+  *renewToken() {
+    yield this.handleRenew();
+  }
 
   get showWarning() {
     const currentRoute = this.router.currentRouteName;
