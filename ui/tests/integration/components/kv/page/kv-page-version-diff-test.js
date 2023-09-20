e92599a191058800af7494e1d1899b34652289dd
clairebontempo@gmail.com <clairebontempo@gmail.com>
2023-09-20 13:33:54 -0700

    wip tests

--- ui/tests/integration/components/kv/page/kv-page-version-diff-test.js
+++ ui/tests/integration/components/kv/page/kv-page-version-diff-test.js
@@ -12,21 +12,6 @@ import { hbs } from 'ember-cli-htmlbars';
 import { kvMetadataPath, kvDataPath } from 'vault/utils/kv-path';
 import { PAGE } from 'vault/tests/helpers/kv/kv-selectors';
 import { allowAllCapabilitiesStub } from 'vault/tests/helpers/stubs';
-import { encodePath } from 'vault/utils/path-encoding-helpers';
-
-const EXAMPLE_KV_DATA_GET_RESPONSE = {
-  request_id: 'foobar',
-  data: {
-    data: { hello: 'world' },
-    metadata: {
-      created_time: '2023-06-20T21:26:47.592306Z',
-      custom_metadata: null,
-      deletion_time: '',
-      destroyed: false,
-      version: 1,
-    },
-  },
-};
 
 module('Integration | Component | kv | Page::Secret::Metadata::VersionDiff', function (hooks) {
   setupRenderingTest(hooks);
@@ -34,26 +19,39 @@ module('Integration | Component | kv | Page::Secret::Metadata::VersionDiff', fun
   setupMirage(hooks);
 
   hooks.beforeEach(async function () {
-    const store = this.owner.lookup('service:store');
+    this.store = this.owner.lookup('service:store');
     this.server.post('/sys/capabilities-self', allowAllCapabilitiesStub());
-    const metadata = this.server.create('kv-metadatum');
+    this.server.get(`/kv-engine/data/my-secret?version=1`, () => {
+      return {
+        request_id: 'foobar',
+        data: {
+          data: { hello: 'world' },
+          metadata: {
+            created_time: '2023-06-20T21:26:47.592306Z',
+            custom_metadata: null,
+            deletion_time: '',
+            destroyed: false,
+            version: 1,
+          },
+        },
+      };
+    });
 
-    metadata.id = kvMetadataPath('kv-engine', 'my-secret');
-    store.pushPayload('kv/metadata', {
+    const metadata = this.server.create('kv-metadatum');
+    metadata.id = kvMetadataPath(this.backend, this.secret);
+    this.store.pushPayload('kv/metadata', {
       modelName: 'kv/metadata',
       ...metadata,
     });
-    this.metadata = store.peekRecord('kv/metadata', metadata.id);
-    this.breadcrumbs = [
-      { label: 'secrets', route: 'secrets', linkExternal: true },
-      { label: this.metadata.backend, route: 'list' },
-      { label: this.metadata.path, route: 'secret.details', model: this.metadata.path },
-      { label: 'version diff' },
-    ];
+
+    this.backend = 'kv-engine';
+    this.secret = 'my-secret';
+    this.metadata = this.store.peekRecord('kv/metadata', metadata.id);
+    this.breadcrumbs = [{ label: 'version history', route: 'secret.metadata.versions' }, { label: 'diff' }];
 
     // compare version 4
-    const dataId = kvDataPath('kv-engine', 'my-secret', 4);
-    store.pushPayload('kv/data', {
+    const dataId = kvDataPath(this.backend, this.secret, 4);
+    this.store.pushPayload('kv/data', {
       modelName: 'kv/data',
       id: dataId,
       secret_data: { foo: 'bar' },
@@ -63,42 +61,29 @@ module('Integration | Component | kv | Page::Secret::Metadata::VersionDiff', fun
       destroyed: false,
       version: 4,
     });
-
-    this.endpoint = `${encodePath('kv-engine')}/data/${'my-secret'}`;
   });
 
   test('it renders compared data of the two versions and shows icons for deleted, destroyed and current', async function (assert) {
     assert.expect(12);
-    this.server.get(this.endpoint, (schema, req) => {
-      assert.ok('request made to the correct endpoint.');
-      assert.strictEqual(
-        req.queryParams.version,
-        '1',
-        'request includes the version flag on queryRecord and correctly only queries for the record not in the store, which is retrieved by peekRecord.'
-      );
-      return EXAMPLE_KV_DATA_GET_RESPONSE;
-    });
 
     await render(
       hbs`
        <Page::Secret::Metadata::VersionDiff
         @metadata={{this.metadata}} 
-        @path={{this.metadata.path}}
-        @backend={{this.metadata.backend}}
+        @path={{this.secret}}
+        @backend={{this.backend}}
         @breadcrumbs={{this.breadcrumbs}}
       />
       `,
       { owner: this.engine }
     );
-
-    /* eslint-disable no-useless-escape */
     const [left, right] = findAll(PAGE.detail.versionDropdown);
-    assert
-      .dom(PAGE.versions.visualDiff)
-      .hasText(
-        `foo"bar"\hello"world"`,
-        'correctly pull in the data from version 4 and compared to version 1.'
-      );
+    assert.dom(PAGE.diff.visualDiff).hasText(
+      `{ \"foo\": \"bar\" }`, // eslint-disable-line no-useless-escape
+      'correctly pull in the data from version 4 and compared to version 1.'
+    );
+    assert.dom(PAGE.diff.deleted).hasText(`foo"bar"`);
+    assert.dom(PAGE.diff.added).hasText(`foo3"bar3"`);
     assert.dom(left).hasText('Version 4', 'shows the current version for the left side default version.');
     assert
       .dom(right)
