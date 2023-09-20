3792dfe7e79ade66fb1fad893ff3b713088b0c0d
Chelsea Shaw <cshaw@hashicorp.com>
2023-09-20 17:15:23 -0500

    fix tests

--- ui/tests/integration/components/pki/page/pki-issuer-list-test.js
+++ ui/tests/integration/components/pki/page/pki-issuer-list-test.js
@@ -9,12 +9,8 @@ import { setupMirage } from 'ember-cli-mirage/test-support';
 import { hbs } from 'ember-cli-htmlbars';
 import { setupEngine } from 'ember-engines/test-support';
 import { setupRenderingTest } from 'vault/tests/helpers';
+import { STANDARD_META } from 'vault/tests/helpers/pki';
 
-const STANDARD_META = {
-  total: 2,
-  currentPage: 1,
-  pageSize: 100,
-};
 /**
  * this test is for the page component only. A separate test is written for the form rendered
  */
