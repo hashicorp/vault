940272de0fc2c929fced0fd9c444fd137ce30c8f
Ryan Cragun <me@ryan.ec>
2023-09-27 10:53:35 -0600

    Backport [QT-602] Run `proxy` and `agent` test scenarios (#23176) into release/1.13.x (#23303)
    * [QT-602] Run `proxy` and `agent` test scenarios (#23176)
    
    Update our `proxy` and `agent` scenarios to support new variants and
    perform baseline verification and their scenario specific verification.
    We integrate these updated scenarios into the pipeline by adding them
    to artifact samples.
    
    We've also improved the reliability of the `autopilot` and `replication`
    scenarios by refactoring our IP address gathering. Previously, we'd ask
    vault for the primary IP address and use some Terraform logic to determine
    followers. The leader IP address gathering script was also implicitly
    responsible for ensuring that a found leader was within a given group of
    hosts, and thus waiting for a given cluster to have a leader, and also for
    doing some arithmetic and outputting `replication` specific output data.
    We've broken these responsibilities into individual modules, improved their
    error messages, and fixed various races and bugs, including:
    * Fix a race between creating the file audit device and installing and starting
      vault in the `replication` scenario.
    * Fix how we determine our leader and follower IP addresses. We now query
      vault instead of a prior implementation that inferred the followers and sometimes
      did not allow all nodes to be an expected leader.
    * Fix a bug where we'd always always fail on the first wrong condition
      in the `vault_verify_performance_replication` module.
    
    We also performed some maintenance tasks on Enos scenarios  byupdating our
    references from `oss` to `ce` to handle the naming and license changes. We
    also enabled `shellcheck` linting for enos module scripts.
    
    * Rename `oss` to `ce` for license and naming changes.
    * Convert template enos scripts to scripts that take environment
      variables.
    * Add `shellcheck` linting for enos module scripts.
    * Add additional `backend` and `seal` support to `proxy` and `agent`
      scenarios.
    * Update scenarios to include all baseline verification.
    * Add `proxy` and `agent` scenarios to artifact samples.
    * Remove IP address verification from the `vault_get_cluster_ips`
      modules and implement a new `vault_wait_for_leader` module.
    * Determine follower IP addresses by querying vault in the
      `vault_get_cluster_ips` module.
    * Move replication specific behavior out of the `vault_get_cluster_ips`
      module and into it's own `replication_data` module.
    * Extend initial version support for the `upgrade` and `autopilot`
      scenarios.
    
    We also discovered an issue with undo_logs that has been described in
    the VAULT-20259. As such, we've disabled the undo_logs check until
    it has been fixed.
    
    
    * actions: fix actionlint error and linting logic (#23305)
    * enos: don't attempt to use the vault proxy command before 1.14
    
    ---------
    
    Signed-off-by: Ryan Cragun <me@ryan.ec>

---
+++ enos/modules/vault_verify_raft_auto_join_voter/scripts/verify-raft-auto-join-voter.sh
@@ -0,0 +1,50 @@
+#!/usr/bin/env bash
+# Copyright (c) HashiCorp, Inc.
+# SPDX-License-Identifier: BUSL-1.1
+
+
+set -e
+
+binpath=${VAULT_INSTALL_DIR}/vault
+
+fail() {
+  echo "$1" 2>&1
+  return 1
+}
+
+retry() {
+  local retries=$1
+  shift
+  local count=0
+
+  until "$@"; do
+    exit=$?
+    wait=$((2 ** count))
+    count=$((count + 1))
+    if [ "$count" -lt "$retries" ]; then
+      sleep "$wait"
+      echo "retry $count"
+    else
+      return "$exit"
+    fi
+  done
+
+  return 0
+}
+
+check_voter_status() {
+  voter_status=$($binpath operator raft list-peers -format json | jq -Mr --argjson expected "true" --arg ADDR "$VAULT_CLUSTER_ADDR" '.data.config.servers[] | select(.address==$ADDR) | .voter == $expected')
+
+  if [[ "$voter_status" != 'true' ]]; then
+    fail "expected $VAULT_CLUSTER_ADDR to be raft voter, got raft status for node: $($binpath operator raft list-peers -format json | jq -Mr --arg ADDR "$VAULT_CLUSTER_ADDR" '.data.config.servers[] | select(.address==$ADDR)')"
+  fi
+}
+
+test -x "$binpath" || fail "unable to locate vault binary at $binpath"
+
+export VAULT_ADDR='http://127.0.0.1:8200'
+[[ -z "$VAULT_TOKEN" ]] && fail "VAULT_TOKEN env variable has not been set"
+
+# Retry a few times because it can take some time for things to settle after
+# all the nodes are unsealed
+retry 5 check_voter_status
