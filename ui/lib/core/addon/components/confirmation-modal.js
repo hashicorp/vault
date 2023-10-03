11c8f692842f3ba7e25c66253d25e74c72c523f3
claire bontempo <68122737+hellobontempo@users.noreply.github.com>
2023-10-03 12:41:30 -0700

    UI: Part 5 - hds adoption replace <Modal> (#23471)
    * replace confirmation modals
    ---------

--- ui/lib/core/addon/components/confirmation-modal.js
+++ ui/lib/core/addon/components/confirmation-modal.js
@@ -2,11 +2,13 @@
  * Copyright (c) HashiCorp, Inc.
  * SPDX-License-Identifier: BUSL-1.1
  */
+import { assert } from '@ember/debug';
 
 import Component from '@glimmer/component';
 /**
  * @module ConfirmationModal
- * ConfirmationModal components are used to provide an alternative to ConfirmationButton that automatically prompts the user to fill in confirmation text before they can continue with a potentially destructive action. It is built off the Modal component
+ * ConfirmationModal components wrap the <Hds::Modal> component to present a critical (red) type-to-confirm modal.
+ * They are used for extremely destructive actions that require extra consideration before confirming.
  *
  * @example
  * ```js
@@ -15,6 +17,7 @@ import Component from '@glimmer/component';
  *   @title="Do Dangerous Thing?"
  *   @isActive={{isModalActive}}
  *   @onClose={{action (mut isModalActive) false}}
+ *   @confirmText="yes"
  *   @onConfirmMsg="deleting this thing to delete."
  * />
  * ```
@@ -22,31 +25,14 @@ import Component from '@glimmer/component';
  * @param {function} onClose - specify what to do when user attempts to close modal
  * @param {boolean} isActive - Controls whether the modal is "active" eg. visible or not.
  * @param {string} title - Title of the modal
- * @param {string} [confirmText=Yes] - The confirmation text that the user must type before continuing
- * @param {string} [toConfirmMsg=''] - Finishes the sentence "Type <confirmText> to confirm <toConfirmMsg>", default is an empty string (ex. 'secret deletion')
+ * @param {string} confirmText - The confirmation text that the user must type before continuing
+ * @param {string} [toConfirmMsg] - Finishes the sentence "Type <confirmText> to confirm <toConfirmMsg>", default is an empty string (ex. 'secret deletion')
  * @param {string} [buttonText=Confirm] - Button text on the confirm button
- * @param {string} [buttonClass=is-danger] - extra class to add to confirm button (eg. "is-danger")
- * @param {string} [type=warning] - The header styling based on type, passed into the message-types helper (in the Modal component).
  */
 
 export default class ConfirmationModal extends Component {
-  get buttonClass() {
-    return this.args.buttonClass || 'is-danger';
-  }
-
-  get buttonText() {
-    return this.args.buttonText || 'Confirm';
-  }
-
-  get confirmText() {
-    return this.args.confirmText || 'Yes';
-  }
-
-  get type() {
-    return this.args.type || 'warning';
-  }
-
-  get toConfirmMsg() {
-    return this.args.toConfirmMsg || '';
+  constructor() {
+    super(...arguments);
+    assert('@confirmText is required', this.args.confirmText);
   }
 }
