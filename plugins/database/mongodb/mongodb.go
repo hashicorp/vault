e1800fc31df45eb4c412bdcdcb9249950269340b
Conor McCullough <103977699+conor-mccullough@users.noreply.github.com>
2023-10-04 01:28:25 +0000

    backport of commit 9019203e9f052b6e58a14cbfab4a559d5e7e5883

--- plugins/database/mongodb/mongodb.go
+++ plugins/database/mongodb/mongodb.go
@@ -173,7 +173,7 @@ func (m *MongoDB) changeUserPassword(ctx context.Context, username, password str
 	}
 
 	database := cs.Database
-	if username == m.Username || database == "" {
+	if database == "" {
 		database = "admin"
 	}
 
