a971b70fc4cfbae645281b642573abaf9aacfaf4
hc-github-team-secure-vault-core <82990506+hc-github-team-secure-vault-core@users.noreply.github.com>
2023-10-05 17:34:00 -0400

    events: Ignore send context (#23500) (#23536)
    When sending an event asynchronously, the original context used for
    whatever generated the event (probably a synchronous, quick HTTP
    context) is probably not what is wanted for sending the event, which
    could face delays if a consumer is backed up.
    
    I will admit myself to sometimes having "context blindness", where
    I just take whatever context is incoming in a function and thread it
    out to all calls. Normally this is the right thing to do when, say,
    tying downstream API calls to an upstream HTTP timeout.
    
    When making KV events, for example, we used the HTTP context for
    `SendEvent()`, and this can cause the events to be dropped if they
    aren't taken from the channel before the HTTP request finishes.
    
    In retrospect, it was probably unnecessary to include a context in
    the `SendEvent` interface.
    
    We keep the context in place for backwards compability, but also in
    case we want to use it for purposes other than timeouts and
    cancellations in the future.
    
    Co-authored-by: Christopher Swenson <christopher.swenson@hashicorp.com>

--- vault/eventbus/bus_test.go
+++ vault/eventbus/bus_test.go
@@ -67,6 +67,47 @@ func TestBusBasics(t *testing.T) {
 	}
 }
 
+// TestBusIgnoresSendContext tests that the context is ignored when sending to an event,
+// so that we do not give up too quickly.
+func TestBusIgnoresSendContext(t *testing.T) {
+	bus, err := NewEventBus(nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+	eventType := logical.EventType("someType")
+
+	event, err := logical.NewEvent()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	bus.Start()
+
+	ch, subCancel, err := bus.Subscribe(context.Background(), namespace.RootNamespace, string(eventType))
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer subCancel()
+
+	ctx, cancel := context.WithCancel(context.Background())
+	cancel() // cancel immediately
+
+	err = bus.SendInternal(ctx, namespace.RootNamespace, nil, eventType, event)
+	if err != nil {
+		t.Errorf("Expected no error sending: %v", err)
+	}
+
+	timeout := time.After(1 * time.Second)
+	select {
+	case message := <-ch:
+		if message.Payload.(*logical.EventReceived).Event.Id != event.Id {
+			t.Errorf("Got unexpected message: %+v", message)
+		}
+	case <-timeout:
+		t.Error("Timeout waiting for message")
+	}
+}
+
 // TestNamespaceFiltering verifies that events for other namespaces are filtered out by the bus.
 func TestNamespaceFiltering(t *testing.T) {
 	bus, err := NewEventBus(nil)
