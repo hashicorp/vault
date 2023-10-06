95a862bfb61392a6a73dcc1df6a16fc81fad6cdf
davidadeleon <56207066+davidadeleon@users.noreply.github.com>
2023-10-06 19:58:42 +0000

    backport of commit 92fcfda8ad30a539be67b7fb7abff539bf93a098

--- command/server.go
+++ command/server.go
@@ -3072,6 +3072,7 @@ func createCoreConfig(c *ServerCommand, config *server.Config, backend physical.
 		DisableSSCTokens:               config.DisableSSCTokens,
 		Experiments:                    config.Experiments,
 		AdministrativeNamespacePath:    config.AdministrativeNamespacePath,
+		UserLockoutLogInterval:         config.UserLockoutLogInterval,
 	}
 
 	if c.flagDev {
