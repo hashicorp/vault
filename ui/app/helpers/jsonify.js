24330ecacae8e79f02d5c1aab884204b92ca222c
Angel Garbarino <Monkeychip@users.noreply.github.com>
2023-10-08 13:10:35 -0600

    Allow AWS secret engine to send empty policy document (#23470) (#23509)
    * fix
    
    * changelog
    
    Co-authored-by: claire bontempo <68122737+hellobontempo@users.noreply.github.com>

--- ui/app/helpers/jsonify.js
+++ ui/app/helpers/jsonify.js
@@ -6,6 +6,8 @@
 import { helper as buildHelper } from '@ember/component/helper';
 
 export function jsonify([target]) {
+  // aws secret engine needs to be able to send an empty json value on the field policy_document
+  if (!target) return;
   return JSON.parse(target);
 }
 
