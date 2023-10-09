8612a4ac8a5398e0c86d4ebc7b59c048308fa63b
Chelsea Shaw <cshaw@hashicorp.com>
2023-10-09 17:39:20 -0500

    rest of tests

--- ui/tests/acceptance/open-api-path-help-test.js
+++ ui/tests/acceptance/open-api-path-help-test.js
@@ -201,17 +201,392 @@ module('Acceptance | OpenAPI path help test', function (hooks) {
     });
   });
 
-  /* TODO: fill in these other OpenAPI tests -- supported auth backends (extends AuthConfig model)
+  module('auth: TLS certificates', function (hooks) {
+    hooks.beforeEach(async function () {
+      this.backend = 'cert-openapi';
+      await runCmd(mountAuthCmd('cert', this.backend), false);
+    });
+    hooks.afterEach(async function () {
+      await runCmd(deleteAuthCmd(this.backend), false);
+    });
+    test('getPaths returns correct paths', async function (assert) {
+      const helpUrl = `auth/${this.backend}`;
+      const result = await this.pathHelp.getPaths(helpUrl, this.backend);
+      assert.deepEqual(
+        result.paths,
+        [
+          {
+            path: '/certs/',
+            itemType: 'certificate',
+            itemName: 'Certificate',
+            operations: ['get', 'list'],
+            action: undefined,
+            navigation: true,
+            param: false,
+          },
+          {
+            path: '/certs/{name}',
+            itemType: 'certificate',
+            itemName: 'Certificate',
+            operations: ['get', 'post', 'delete'],
+            action: 'Create',
+            navigation: false,
+            param: 'name',
+          },
+        ],
+        'correct paths'
+      );
+      // No paths with navigation=true means they won't show up as tabs on the method page
+    });
+    test('getProps returns correct model attributes', async function (assert) {
+      const config = await this.pathHelp.getProps(`/v1/auth/${this.backend}/config?help=1`, this.backend);
+      assert.deepEqual(config, openApiDrivenAttributes.certConfig, 'config attributes');
 
-    cert
-    gcp
-    github
-    jwt
-    kubernetes
-    ldap
-    okta
-    radius
-    aws/client
-    aws/tidy
-  */
+      const cert = await this.pathHelp.getProps(
+        `/v1/auth/${this.backend}/certs/example?help=true`,
+        this.backend
+      );
+      assert.deepEqual(cert, openApiDrivenAttributes.certCert, 'cert attributes');
+    });
+  });
+
+  module('auth: GCP', function (hooks) {
+    hooks.beforeEach(async function () {
+      this.backend = 'gcp-openapi';
+      await runCmd(mountAuthCmd('gcp', this.backend), false);
+    });
+    hooks.afterEach(async function () {
+      await runCmd(deleteAuthCmd(this.backend), false);
+    });
+    test('getPaths returns correct paths', async function (assert) {
+      const helpUrl = `auth/${this.backend}`;
+      const result = await this.pathHelp.getPaths(helpUrl, this.backend);
+      assert.deepEqual(result.paths, [], 'correct paths');
+    });
+    test('getProps returns correct model attributes', async function (assert) {
+      const config = await this.pathHelp.getProps(`/v1/auth/${this.backend}/config?help=1`, this.backend);
+      assert.deepEqual(config, openApiDrivenAttributes.gcpConfig, 'config');
+    });
+  });
+
+  module('auth: github', function (hooks) {
+    hooks.beforeEach(async function () {
+      this.backend = 'github-openapi';
+      await runCmd(mountAuthCmd('github', this.backend), false);
+    });
+    hooks.afterEach(async function () {
+      await runCmd(deleteAuthCmd(this.backend), false);
+    });
+    test('getPaths returns correct paths', async function (assert) {
+      const helpUrl = `auth/${this.backend}`;
+      const result = await this.pathHelp.getPaths(helpUrl, this.backend);
+      assert.deepEqual(result.paths, [], 'correct paths');
+    });
+    test('getProps returns correct model attributes', async function (assert) {
+      const config = await this.pathHelp.getProps(`/v1/auth/${this.backend}/config?help=1`, this.backend);
+      assert.deepEqual(config, openApiDrivenAttributes.githubConfig, 'config');
+    });
+  });
+
+  module('auth: jwt', function (hooks) {
+    hooks.beforeEach(async function () {
+      this.backend = 'jwt-openapi';
+      await runCmd(mountAuthCmd('jwt', this.backend), false);
+    });
+    hooks.afterEach(async function () {
+      await runCmd(deleteAuthCmd(this.backend), false);
+    });
+    test('getPaths returns correct paths', async function (assert) {
+      const helpUrl = `auth/${this.backend}`;
+      const result = await this.pathHelp.getPaths(helpUrl, this.backend);
+      assert.deepEqual(result.paths, [], 'correct paths');
+    });
+    test('getProps returns correct model attributes', async function (assert) {
+      const config = await this.pathHelp.getProps(`/v1/auth/${this.backend}/config?help=1`, this.backend);
+      assert.deepEqual(config, openApiDrivenAttributes.jwtConfig, 'config');
+    });
+  });
+
+  module('auth: kubernetes', function (hooks) {
+    hooks.beforeEach(async function () {
+      this.backend = 'k8s-openapi';
+      await runCmd(mountAuthCmd('kubernetes', this.backend), false);
+    });
+    hooks.afterEach(async function () {
+      await runCmd(deleteAuthCmd(this.backend), false);
+    });
+    test('getPaths returns correct paths', async function (assert) {
+      const helpUrl = `auth/${this.backend}`;
+      const result = await this.pathHelp.getPaths(helpUrl, this.backend);
+      assert.deepEqual(
+        result.paths,
+        [
+          {
+            action: undefined,
+            itemName: 'Role',
+            itemType: 'role',
+            navigation: true,
+            operations: ['get', 'list'],
+            param: false,
+            path: '/role/',
+          },
+          {
+            action: 'Create',
+            itemName: 'Role',
+            itemType: 'role',
+            navigation: false,
+            operations: ['get', 'post', 'delete'],
+            param: 'name',
+            path: '/role/{name}',
+          },
+        ],
+        'correct paths'
+      );
+    });
+    test('getProps returns correct model attributes', async function (assert) {
+      const config = await this.pathHelp.getProps(`/v1/auth/${this.backend}/config?help=1`, this.backend);
+      assert.deepEqual(config, openApiDrivenAttributes.k8sConfig, 'config');
+
+      const role = await this.pathHelp.getProps(`/v1/auth/${this.backend}/role/example?help=1`, this.backend);
+      assert.deepEqual(role, openApiDrivenAttributes.k8sRole, 'role');
+    });
+  });
+
+  module('auth: ldap', function (hooks) {
+    hooks.beforeEach(async function () {
+      this.backend = 'ldap-openapi';
+      await runCmd(mountAuthCmd('ldap', this.backend), false);
+    });
+    hooks.afterEach(async function () {
+      await runCmd(deleteAuthCmd(this.backend), false);
+    });
+    test('getPaths returns correct paths', async function (assert) {
+      const helpUrl = `auth/${this.backend}`;
+      const result = await this.pathHelp.getPaths(helpUrl, this.backend);
+      assert.deepEqual(
+        result.paths,
+        [
+          {
+            action: 'Configure',
+            itemName: undefined,
+            itemType: undefined,
+            navigation: false,
+            operations: ['get', 'post'],
+            param: false,
+            path: '/config',
+          },
+          {
+            action: undefined,
+            itemName: 'Group',
+            itemType: 'group',
+            navigation: true,
+            operations: ['get', 'list'],
+            param: false,
+            path: '/groups/',
+          },
+          {
+            action: 'Create',
+            itemName: 'Group',
+            itemType: 'group',
+            navigation: false,
+            operations: ['get', 'post', 'delete'],
+            param: 'name',
+            path: '/groups/{name}',
+          },
+          {
+            action: undefined,
+            itemName: 'User',
+            itemType: 'user',
+            navigation: true,
+            operations: ['get', 'list'],
+            param: false,
+            path: '/users/',
+          },
+          {
+            action: 'Create',
+            itemName: 'User',
+            itemType: 'user',
+            navigation: false,
+            operations: ['get', 'post', 'delete'],
+            param: 'name',
+            path: '/users/{name}',
+          },
+        ],
+        'correct paths'
+      );
+    });
+    test('getProps returns correct model attributes', async function (assert) {
+      const config = await this.pathHelp.getProps(`/v1/auth/${this.backend}/config?help=1`, this.backend);
+      assert.deepEqual(config, openApiDrivenAttributes.ldapConfig, 'config');
+
+      const groups = await this.pathHelp.getProps(
+        `/v1/auth/${this.backend}/groups/example?help=1`,
+        this.backend
+      );
+      assert.deepEqual(groups, openApiDrivenAttributes.ldapGroup, 'groups');
+
+      const users = await this.pathHelp.getProps(
+        `/v1/auth/${this.backend}/users/example?help=1`,
+        this.backend
+      );
+      assert.deepEqual(users, openApiDrivenAttributes.ldapUser, 'users');
+    });
+  });
+
+  module('auth: okta', function (hooks) {
+    hooks.beforeEach(async function () {
+      this.backend = 'okta-openapi';
+      await runCmd(mountAuthCmd('okta', this.backend), false);
+    });
+    hooks.afterEach(async function () {
+      await runCmd(deleteAuthCmd(this.backend), false);
+    });
+    test('getPaths returns correct paths', async function (assert) {
+      const helpUrl = `auth/${this.backend}`;
+      const result = await this.pathHelp.getPaths(helpUrl, this.backend);
+      assert.deepEqual(
+        result.paths,
+        [
+          {
+            action: 'Configure',
+            itemName: undefined,
+            itemType: undefined,
+            navigation: false,
+            operations: ['get', 'post'],
+            param: false,
+            path: '/config',
+          },
+          {
+            action: undefined,
+            itemName: 'Group',
+            itemType: 'group',
+            navigation: true,
+            operations: ['get', 'list'],
+            param: false,
+            path: '/groups/',
+          },
+          {
+            action: 'Create',
+            itemName: 'Group',
+            itemType: 'group',
+            navigation: false,
+            operations: ['get', 'post', 'delete'],
+            param: 'name',
+            path: '/groups/{name}',
+          },
+          {
+            action: undefined,
+            itemName: 'User',
+            itemType: 'user',
+            navigation: true,
+            operations: ['get', 'list'],
+            param: false,
+            path: '/users/',
+          },
+          {
+            action: 'Create',
+            itemName: 'User',
+            itemType: 'user',
+            navigation: false,
+            operations: ['get', 'post', 'delete'],
+            param: 'name',
+            path: '/users/{name}',
+          },
+        ],
+        'correct paths'
+      );
+    });
+    test('getProps returns correct model attributes', async function (assert) {
+      const config = await this.pathHelp.getProps(`/v1/auth/${this.backend}/config?help=1`, this.backend);
+      assert.deepEqual(config, openApiDrivenAttributes.oktaConfig, 'config');
+
+      const groups = await this.pathHelp.getProps(
+        `/v1/auth/${this.backend}/groups/example?help=1`,
+        this.backend
+      );
+      assert.deepEqual(groups, openApiDrivenAttributes.oktaGroup, 'groups');
+
+      const users = await this.pathHelp.getProps(
+        `/v1/auth/${this.backend}/users/example?help=1`,
+        this.backend
+      );
+      assert.deepEqual(users, openApiDrivenAttributes.oktaUser, 'users');
+    });
+  });
+
+  module('auth: radius', function (hooks) {
+    hooks.beforeEach(async function () {
+      this.backend = 'radius-openapi';
+      await runCmd(mountAuthCmd('radius', this.backend), false);
+    });
+    hooks.afterEach(async function () {
+      await runCmd(deleteAuthCmd(this.backend), false);
+    });
+    test('getPaths returns correct paths', async function (assert) {
+      const helpUrl = `auth/${this.backend}`;
+      const result = await this.pathHelp.getPaths(helpUrl, this.backend);
+      assert.deepEqual(
+        result.paths,
+        [
+          {
+            action: 'Configure',
+            itemName: undefined,
+            itemType: undefined,
+            navigation: false,
+            operations: ['get', 'post'],
+            param: false,
+            path: '/config',
+          },
+          {
+            action: undefined,
+            itemName: 'User',
+            itemType: 'user',
+            navigation: true,
+            operations: ['get', 'list'],
+            param: false,
+            path: '/users/',
+          },
+          {
+            action: 'Create',
+            itemName: 'User',
+            itemType: 'user',
+            navigation: false,
+            operations: ['get', 'post', 'delete'],
+            param: 'name',
+            path: '/users/{name}',
+          },
+        ],
+        'correct paths'
+      );
+    });
+    test('getProps returns correct model attributes', async function (assert) {
+      const config = await this.pathHelp.getProps(`/v1/auth/${this.backend}/config?help=1`, this.backend);
+      assert.deepEqual(config, openApiDrivenAttributes.radiusConfig, 'config');
+
+      const users = await this.pathHelp.getProps(
+        `/v1/auth/${this.backend}/users/example?help=1`,
+        this.backend
+      );
+      assert.deepEqual(users, openApiDrivenAttributes.radiusUser, 'users');
+    });
+  });
+
+  module('auth: aws', function (hooks) {
+    hooks.beforeEach(async function () {
+      this.backend = 'aws-openapi';
+      await runCmd(mountAuthCmd('aws', this.backend), false);
+    });
+    hooks.afterEach(async function () {
+      await runCmd(deleteAuthCmd(this.backend), false);
+    });
+    test('getPaths returns correct paths', async function (assert) {
+      const helpUrl = `auth/${this.backend}`;
+      const result = await this.pathHelp.getPaths(helpUrl, this.backend);
+      assert.deepEqual(result.paths, [], 'correct paths');
+    });
+    test('getProps returns correct model attributes', async function (assert) {
+      const config = await this.pathHelp.getProps(`/v1/auth/${this.backend}/?help=1`, this.backend);
+      assert.deepEqual(config, openApiDrivenAttributes.awsConfig, 'config');
+    });
+  });
 });
