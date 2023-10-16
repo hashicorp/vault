9e4f59829bd043118cb6dcc4bb04284e875b0ecc
Chelsea Shaw <cshaw@hashicorp.com>
2023-10-16 12:00:52 -0500

    Show error when chroot_namespace defined but does not exist

--- ui/app/adapters/cluster.js
+++ ui/app/adapters/cluster.js
@@ -80,6 +80,14 @@ export default ApplicationAdapter.extend({
         performancestandbycode: 200,
       },
       unauthenticated: true,
+    }).catch((e) => {
+      if (e.errors?.includes('invalid namespace specified for chroot_namespace')) {
+        // This error will occur if the call is made with chroot_namespace set on config
+        // but the specified namespace does not exist. In this case we should show the error.
+        e.message =
+          'chroot_namespace has been set but the namespace does not exist. Please create the namespace and then refresh the page.';
+      }
+      throw e;
     });
   },
 
