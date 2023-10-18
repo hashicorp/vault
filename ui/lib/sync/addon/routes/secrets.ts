9d380de7dc5a34242896a03970b994a04249db13
clairebontempo@gmail.com <clairebontempo@gmail.com>
2023-10-18 16:39:32 -0700

    conditionally render CTA

---
+++ ui/lib/sync/addon/routes/secrets.ts
@@ -0,0 +1,17 @@
+/**
+ * Copyright (c) HashiCorp, Inc.
+ * SPDX-License-Identifier: BUSL-1.1
+ */
+
+import Route from '@ember/routing/route';
+import { inject as service } from '@ember/service';
+
+import type StoreService from 'vault/services/store';
+
+export default class SyncSecretsRoute extends Route {
+  @service declare readonly store: StoreService;
+
+  async model() {
+    return this.store.query('sync/destination', {}).catch(() => []);
+  }
+}
