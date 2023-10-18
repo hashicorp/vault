265e66ef8a0fc3bc472de42fa3ec29812e07009a
Chelsea Shaw <cshaw@hashicorp.com>
2023-10-18 10:14:26 -0500

    redirect back to current URL after login

--- ui/app/components/resultant-acl-banner.js
+++ ui/app/components/resultant-acl-banner.js
@@ -3,8 +3,14 @@ import Component from '@glimmer/component';
 
 export default class ResultantAclBannerComponent extends Component {
   @service namespace;
+  @service router;
 
   get ns() {
     return this.namespace.path || 'root';
+  }
+
+  get queryParams() {
+    // Bring user back to current page after login
+    return { redirect_to: this.router.currentURL };
   }
 }
