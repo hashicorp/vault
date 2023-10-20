e80493a07a8c40ad41db188be06f895e4224dad9
Marc Boudreau <marc.boudreau@hashicorp.com>
2023-10-20 09:18:50 -0400

    add godoc for pathInternalUIVersion function

--- vault/logical_system.go
+++ vault/logical_system.go
@@ -4754,6 +4754,8 @@ func (b *SystemBackend) pathInternalUIResultantACL(ctx context.Context, req *log
 	return resp, nil
 }
 
+// pathInternalUIVersion is the framework.PathOperation callback function for
+// the sys/internal/ui/version path. It simply returns the Vault version.
 func (b *SystemBackend) pathInternalUIVersion(ctx context.Context, req *logical.Request, d *framework.FieldData) (*logical.Response, error) {
 	resp := &logical.Response{
 		Data: map[string]any{
