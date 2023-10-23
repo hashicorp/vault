239ccdd259167ff7e4ce12c636df0eb69dfb4d22
hc-github-team-secure-vault-core <82990506+hc-github-team-secure-vault-core@users.noreply.github.com>
2023-10-23 12:23:35 -0400

    backport of commit 87376a1f5c676c9a54e1050b3e71e32c4ac92595 (#23789)
    Co-authored-by: Hamid Ghaf <83242695+hghaf099@users.noreply.github.com>

--- builtin/credential/approle/path_login_test.go
+++ builtin/credential/approle/path_login_test.go
@@ -420,7 +420,7 @@ func TestAppRole_RoleDoesNotExist(t *testing.T) {
 		t.Fatal("Error not part of response.")
 	}
 
-	if !strings.Contains(errString, "invalid role ID") {
+	if !strings.Contains(errString, "invalid role or secret ID") {
 		t.Fatalf("Error was not due to invalid role ID. Error: %s", errString)
 	}
 }
