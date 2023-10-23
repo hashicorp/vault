d3f9f3d63fc3c14be3a89523d4b373ae06dfe42a
clairebontempo@gmail.com <clairebontempo@gmail.com>
2023-10-23 10:47:26 -0700

    remove async

--- ui/lib/sync/addon/routes/secrets/destinations/create/destination.ts
+++ ui/lib/sync/addon/routes/secrets/destinations/create/destination.ts
@@ -15,7 +15,7 @@ interface Params {
 export default class SyncSecretsDestinationsCreateDestinationRoute extends Route {
   @service declare readonly store: StoreService;
 
-  async model(params: Params) {
+  model(params: Params) {
     const { type } = params;
     return this.store.createRecord(`sync/destinations/${type}`, { type });
   }
