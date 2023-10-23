14797de38f26c13bac4dfc783d4c177c4dad1021
Hamid Ghaf <83242695+hghaf099@users.noreply.github.com>
2023-10-23 15:59:26 +0000

    backport of commit 87376a1f5c676c9a54e1050b3e71e32c4ac92595

--- builtin/credential/approle/path_login.go
+++ builtin/credential/approle/path_login.go
@@ -92,7 +92,7 @@ func (b *backend) pathLoginResolveRole(ctx context.Context, req *logical.Request
 		return nil, err
 	}
 	if roleIDIndex == nil {
-		return logical.ErrorResponse("invalid role ID"), nil
+		return logical.ErrorResponse("invalid role or secret ID"), nil
 	}
 
 	roleName := roleIDIndex.Name
@@ -106,7 +106,7 @@ func (b *backend) pathLoginResolveRole(ctx context.Context, req *logical.Request
 		return nil, err
 	}
 	if role == nil {
-		return logical.ErrorResponse("invalid role ID"), nil
+		return logical.ErrorResponse("invalid role or secret ID"), nil
 	}
 
 	return logical.ResolveRoleResponse(roleName)
@@ -127,7 +127,7 @@ func (b *backend) pathLoginUpdate(ctx context.Context, req *logical.Request, dat
 		return nil, err
 	}
 	if roleIDIndex == nil {
-		return logical.ErrorResponse("invalid role ID"), nil
+		return logical.ErrorResponse("invalid role or secret ID"), nil
 	}
 
 	roleName := roleIDIndex.Name
@@ -141,7 +141,7 @@ func (b *backend) pathLoginUpdate(ctx context.Context, req *logical.Request, dat
 		return nil, err
 	}
 	if role == nil {
-		return logical.ErrorResponse("invalid role ID"), nil
+		return logical.ErrorResponse("invalid role or secret ID"), nil
 	}
 
 	metadata := make(map[string]string)
@@ -177,7 +177,7 @@ func (b *backend) pathLoginUpdate(ctx context.Context, req *logical.Request, dat
 			return nil, err
 		}
 		if entry == nil {
-			return logical.ErrorResponse("invalid secret id"), logical.ErrInvalidCredentials
+			return logical.ErrorResponse("invalid role or secret ID"), logical.ErrInvalidCredentials
 		}
 
 		// If a secret ID entry does not have a corresponding accessor
@@ -197,7 +197,7 @@ func (b *backend) pathLoginUpdate(ctx context.Context, req *logical.Request, dat
 				return nil, err
 			}
 			if entry == nil {
-				return logical.ErrorResponse("invalid secret id"), nil
+				return logical.ErrorResponse("invalid role or secret ID"), nil
 			}
 
 			accessorEntry, err := b.secretIDAccessorEntry(ctx, req.Storage, entry.SecretIDAccessor, role.SecretIDPrefix)
@@ -210,7 +210,7 @@ func (b *backend) pathLoginUpdate(ctx context.Context, req *logical.Request, dat
 					return nil, fmt.Errorf("error deleting secret ID %q from storage: %w", secretIDHMAC, err)
 				}
 			}
-			return logical.ErrorResponse("invalid secret id"), nil
+			return logical.ErrorResponse("invalid role or secret ID"), nil
 		}
 
 		switch {
