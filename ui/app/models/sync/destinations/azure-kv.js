aa481f80585895bd9b7b5ee65bbf3030c2185304
clairebontempo@gmail.com <clairebontempo@gmail.com>
2023-10-24 09:15:25 -0700

    add model attribute metadata

--- ui/app/models/sync/destinations/azure-kv.js
+++ ui/app/models/sync/destinations/azure-kv.js
@@ -6,12 +6,35 @@
 import SyncDestinationModel from '../destination';
 import { attr } from '@ember-data/model';
 import { withFormFields } from 'vault/decorators/model-form-fields';
+import { withModelValidations } from 'vault/decorators/model-validations';
 
-@withFormFields()
+const validations = {
+  name: [{ type: 'presence', message: 'Name is required.' }],
+  keyVaultUri: [{ type: 'presence', message: 'Key Vault URI is required.' }],
+  clientId: [{ type: 'presence', message: 'Client ID is required.' }],
+  clientSecret: [{ type: 'presence', message: 'Client secret is required.' }],
+  tenantId: [{ type: 'presence', message: 'Tenant ID is required.' }],
+};
+const fields = ['name', 'keyVaultUri', 'tenantId', 'cloud', 'clientId', 'clientSecret'];
+
+@withModelValidations(validations)
+@withFormFields(fields)
 export default class SyncDestinationsAzureKeyVaultModel extends SyncDestinationModel {
-  @attr('string') keyVaultUri;
-  @attr('string') clientId;
-  @attr('string') clientSecret;
-  @attr('string') tenantId;
-  @attr('string') cloud;
+  @attr('string', { label: 'Key Vault URI', subText: 'URI of an existing Azure Key Vault instance.' })
+  keyVaultUri;
+
+  @attr('string', { label: 'Client ID', subText: 'Client ID of an Azure app registration.' })
+  clientId;
+
+  @attr('string', { subText: 'Client secret of an Azure app registration.' })
+  clientSecret;
+
+  @attr('string', { label: 'Tenant ID', subText: 'ID of the target Azure tenant.' })
+  tenantId;
+
+  @attr('string', {
+    subText: 'Specifies a cloud for the client. The default is Azure Public Cloud.',
+    defaultValue: 'cloud',
+  })
+  cloud;
 }
--- ui/app/models/sync/destinations/vercel-project.js
+++ ui/app/models/sync/destinations/vercel-project.js
@@ -6,11 +6,42 @@
 import SyncDestinationModel from '../destination';
 import { attr } from '@ember-data/model';
 import { withFormFields } from 'vault/decorators/model-form-fields';
+import { withModelValidations } from 'vault/decorators/model-validations';
 
-@withFormFields()
+const validations = {
+  name: [{ type: 'presence', message: 'Name is required.' }],
+  accessToken: [{ type: 'presence', message: 'Access token is required.' }],
+  projectId: [{ type: 'presence', message: 'Project ID is required.' }],
+  deploymentEnvironments: [{ type: 'presence', message: 'Deployment environments are required.' }],
+};
+const fields = ['name', 'accessToken', 'projectId', 'teamId', 'deploymentEnvironments'];
+
+@withModelValidations(validations)
+@withFormFields(fields)
 export default class SyncDestinationsVercelProjectModel extends SyncDestinationModel {
-  @attr('string') accessToken;
-  @attr('string') projectId;
-  @attr('string') teamId;
-  @attr('array') deploymentEnvironments;
+  @attr('string', {
+    subText: 'Vercel API access token with the permissions to manage environment variables.',
+  })
+  accessToken;
+
+  @attr('string', {
+    label: 'Project ID',
+    subText: 'Project ID where to manage environment variables.',
+  })
+  projectId;
+
+  @attr('string', {
+    label: 'Team ID',
+    subText: 'Team ID the project belongs to. Optional.',
+  })
+  teamId;
+
+  @attr({
+    subText: 'Deployment environments where the environment variables are available.',
+    editType: 'yield',
+    checkboxOptions: ['deployment', 'preview', 'production'],
+    // TODO can also be a string, return and test how this works with live API
+    defaultValue: () => [],
+  })
+  deploymentEnvironments;
 }
