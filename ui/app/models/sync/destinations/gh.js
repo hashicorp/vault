4b5ba5f942156152c5fd1dae8c61b195dfb533b0
clairebontempo@gmail.com <clairebontempo@gmail.com>
2023-10-24 09:15:25 -0700

    add model attribute metadata

--- ui/app/models/sync/destinations/gh.js
+++ ui/app/models/sync/destinations/gh.js
@@ -6,10 +6,25 @@
 import SyncDestinationModel from '../destination';
 import { attr } from '@ember-data/model';
 import { withFormFields } from 'vault/decorators/model-form-fields';
+import { withModelValidations } from 'vault/decorators/model-validations';
 
-@withFormFields()
+const validations = {
+  name: [{ type: 'presence', message: 'Name is required.' }],
+  accessToken: [{ type: 'presence', message: 'Access token is required.' }],
+  repositoryOwner: [{ type: 'presence', message: 'Repository owner is required.' }],
+  repositoryName: [{ type: 'presence', message: 'Repository name is required.' }],
+};
+const fields = ['name', 'accessToken', 'repositoryOwner', 'repositoryName'];
+
+@withModelValidations(validations)
+@withFormFields(fields)
 export default class SyncDestinationsGithubModel extends SyncDestinationModel {
-  @attr('string') accessToken;
-  @attr('string') repositoryOwner;
-  @attr('string') repositoryName;
+  @attr('string')
+  accessToken;
+
+  @attr('string', { subText: 'Github organization or username that owns the repository.' })
+  repositoryOwner;
+
+  @attr('string', { subText: 'The name of the Github repository to connect to.' })
+  repositoryName;
 }
