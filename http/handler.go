23bea0b310c198e32f8f0f528109f5e61ca9ef88
Marc Boudreau <marc.boudreau@hashicorp.com>
2023-10-31 16:38:07 +0000

    backport of commit 6af8bc7ce0f67544e6b55664819ce231c073f47b

--- http/handler.go
+++ http/handler.go
@@ -23,7 +23,6 @@ import (
 	"strings"
 	"time"
 
-	"github.com/NYTimes/gziphandler"
 	"github.com/hashicorp/errwrap"
 	"github.com/hashicorp/go-cleanhttp"
 	"github.com/hashicorp/go-secure-stdlib/parseutil"
@@ -36,6 +35,7 @@ import (
 	"github.com/hashicorp/vault/sdk/helper/pathmanager"
 	"github.com/hashicorp/vault/sdk/logical"
 	"github.com/hashicorp/vault/vault"
+	gziphandler "github.com/klauspost/compress/gzhttp"
 )
 
 const (
