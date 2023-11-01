809730e911d3fb56b01fef33192041c71f0e4bd5
Kianna Quach <kianna.quach@hashicorp.com>
2023-11-01 10:44:12 -0700

    Crete adapter, model, serializer files

---
+++ ui/app/adapters/config-ui/message.js
@@ -0,0 +1,8 @@
+/**
+ * Copyright (c) HashiCorp, Inc.
+ * SPDX-License-Identifier: BUSL-1.1
+ */
+
+import ApplicationAdapter from '../application';
+
+export default class MessageAdapter extends ApplicationAdapter {}
