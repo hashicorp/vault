9b162e71e674c1874b29d5593b919838138d2cb9
Jordan Reimer <zofskeez@gmail.com>
2023-11-02 14:15:22 -0600

    adds copyright header to sync-destinations type file

--- ui/types/vault/helpers/sync-destinations.ts
+++ ui/types/vault/helpers/sync-destinations.ts
@@ -1,3 +1,8 @@
+/**
+ * Copyright (c) HashiCorp, Inc.
+ * SPDX-License-Identifier: BUSL-1.1
+ */
+
 export type SyncDestinationType = 'aws-sm' | 'azure-kv' | 'gcp-sm' | 'gh' | 'vercel-project';
 export type SyncDestinationName =
   | 'AWS Secrets Manager'
