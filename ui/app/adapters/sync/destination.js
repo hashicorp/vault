b9255d55d985b32eb5f1511036306246648de0ff
Jordan Reimer <zofskeez@gmail.com>
2023-11-03 09:47:59 -0600

    fixes inconsistencies with url generation for sync destinations delete

--- ui/app/adapters/sync/destination.js
+++ ui/app/adapters/sync/destination.js
@@ -18,6 +18,16 @@ export default class SyncDestinationAdapter extends ApplicationAdapter {
     return `${super.urlForCreateRecord(modelName, snapshot)}/${name}`;
   }
 
+  urlForDeleteRecord(id, modelName, snapshot) {
+    const { name, type } = snapshot.attributes();
+    // the modelName may be sync/destination or a child depending if it was initiated from the list or details view
+    // since the id for sync/destinations is type/name it will actually generate the correct url but the slash will be encoded
+    // if we normalize to use the child model name for url generation instead things will be consistent
+    const normalizedModelName =
+      modelName === 'sync/destination' ? `${pluralize(modelName)}/${type}` : modelName;
+    return `${super.urlForDeleteRecord(name, normalizedModelName, snapshot)}`;
+  }
+
   query(store, { modelName }) {
     return this.ajax(this.buildURL(modelName), 'GET', { data: { list: true } });
   }
