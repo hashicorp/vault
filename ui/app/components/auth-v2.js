587ba439932bf2e0a9140bc67cbdab5754ac7cd5
Chelsea Shaw <cshaw@hashicorp.com>
2023-11-07 11:32:02 -0600

    Clean up unused auth-v2 logic

--- ui/app/components/auth-v2.js
+++ ui/app/components/auth-v2.js
@@ -2,8 +2,6 @@ import { action } from '@ember/object';
 import { service } from '@ember/service';
 import Component from '@glimmer/component';
 import { tracked } from '@glimmer/tracking';
-import { allSupportedAuthBackends } from 'vault/helpers/supported-auth-backends';
-import errorMessage from 'vault/utils/error-message';
 
 class AuthState {
   @tracked type = '';
@@ -27,35 +25,13 @@ class AuthState {
 }
 export default class AuthV2Component extends Component {
   @service permissions;
-  @service session;
 
   @tracked namespace = '';
   @tracked authType = 'token';
   @tracked mountPath = '';
-  @tracked error = '';
-  // Two ways of doing things
-  @tracked form = new AuthState();
-  @tracked fields = {};
 
   get authMethods() {
-    return ['token', 'userpass', 'oidc'];
-    // return ['token', 'userpass', 'ldap', 'okta', 'jwt', 'oidc', 'radius', 'github'];
-  }
-  get showFields() {
-    const backend = allSupportedAuthBackends().findBy('type', this.authType);
-    return backend.formAttributes;
-  }
-
-  maybeMask = (field) => {
-    if (field === 'token' || field === 'password') {
-      return 'password';
-    }
-    return 'text';
-  };
-
-  @action
-  handleFormChange(evt) {
-    this.form[evt.target.name] = evt.target.value;
+    return ['token', 'userpass', 'ldap', 'okta', 'jwt', 'oidc', 'radius', 'github'];
   }
 
   @action
@@ -64,52 +40,8 @@ export default class AuthV2Component extends Component {
     const { name, value } = evt.target;
     this[name] = value;
     if (name === 'authType') {
-      // if the authType changes, reset the form and mount path
-      // this.form.resetFields();
+      // if the authType changes, reset the mount path
       this.mountPath = '';
-    }
-  }
-
-  @action
-  async authenticate(fields) {
-    const authenticator = `authenticator:${this.authType}`;
-    try {
-      await this.session.authenticate(authenticator, fields, {
-        backend: this.mountPath,
-        namespace: this.namespace,
-      });
-    } catch (e) {
-      this.error = errorMessage(e);
-    }
-
-    if (this.session.isAuthenticated) {
-      // TODO: Show root warning flash message
-      this.permissions.getPaths.perform();
-    }
-  }
-
-  @action
-  async handleFormLogin(evt) {
-    evt.preventDefault();
-    const authenticator = `authenticator:${this.authType}`;
-    const fields = this.showFields.reduce((obj, field) => {
-      console.log({ field, val: this.form[field] });
-      obj[field] = this.form[field];
-      return obj;
-    }, {});
-    console.log({ fields });
-    try {
-      await this.session.authenticate(authenticator, fields, {
-        backend: this.mountPath,
-        namespace: this.namespace,
-      });
-    } catch (e) {
-      this.error = errorMessage(e);
-    }
-
-    if (this.session.isAuthenticated) {
-      // TODO: Show root warning flash message
-      this.permissions.getPaths.perform();
     }
   }
 
