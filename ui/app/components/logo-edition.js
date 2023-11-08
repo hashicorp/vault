795e8815d9400cc818a382fc3170027804034398
Angel Garbarino <argarbarino@gmail.com>
2023-11-08 15:48:21 -0700

    logo edition

--- ui/app/components/logo-edition.js
+++ ui/app/components/logo-edition.js
@@ -6,7 +6,6 @@
 import { inject as service } from '@ember/service';
 import Component from '@ember/component';
 
-export default Component.extend({
-  tagName: '',
-  version: service(),
-});
+export default class logoEdition extends Component {
+  @service version;
+}
