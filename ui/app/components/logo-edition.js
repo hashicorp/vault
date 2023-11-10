e0aa4d3d32758222ec49aee5b40ec3e4032fdfe9
Angel Garbarino <argarbarino@gmail.com>
2023-11-10 10:57:12 -0700

    ad js docs

--- ui/app/components/logo-edition.js
+++ ui/app/components/logo-edition.js
@@ -6,6 +6,15 @@
 import { inject as service } from '@ember/service';
 import Component from '@ember/component';
 
+/**
+ * @module LogoEdition
+ * LogoEdition shows the Vault logo with information about enterprise if applicable.
+ *
+ * @example
+ * ```js
+ * <LogoEdition />
+ */
+
 export default class LogoEdition extends Component {
   @service version;
 }
--- ui/app/components/not-found.js
+++ ui/app/components/not-found.js
@@ -6,6 +6,17 @@
 import { inject as service } from '@ember/service';
 import Component from '@glimmer/component';
 
+/**
+ * @module NotFound
+ * NotFound components are used to show a message that the route was not found.
+ *
+ * @example
+ * ```js
+ * <NotFound @model={{this.model}} />
+ * ```
+ * @param {object} model - routes model passed into the component.
+ */
+
 export default class NotFound extends Component {
   @service router;
 
