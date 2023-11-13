d578aba58612e608b99e1015adbb634a1ba40155
Chelsea Shaw <cshaw@hashicorp.com>
2023-11-13 12:27:38 -0600

    Add serialization to entity-alias and entity so that they can be sorted by name on list view

--- ui/app/serializers/identity/entity-alias.js
+++ ui/app/serializers/identity/entity-alias.js
@@ -4,4 +4,29 @@
  */
 
 import IdentitySerializer from './_base';
-export default IdentitySerializer.extend();
+export default IdentitySerializer.extend({
+  extractLazyPaginatedData(payload) {
+    return payload.data.keys.map((key) => {
+      const model = {
+        id: key,
+        name: payload.data.key_info[key].name,
+      };
+      if (payload.backend) {
+        model.backend = payload.backend;
+      }
+      return model;
+    });
+  },
+  normalizeItems(payload) {
+    // due to extractLazyPaginatedData above, keys is no longer an array of strings but objects
+    if (payload.data.keys && Array.isArray(payload.data.keys)) {
+      return payload.data.keys;
+    }
+    const ret = {
+      ...payload,
+      ...payload.data,
+    };
+    delete ret.data;
+    return ret;
+  },
+});
--- ui/app/serializers/identity/entity.js
+++ ui/app/serializers/identity/entity.js
@@ -12,4 +12,28 @@ export default IdentitySerializer.extend(EmbeddedRecordsMixin, {
   attrs: {
     aliases: { embedded: 'always' },
   },
+  extractLazyPaginatedData(payload) {
+    return payload.data.keys.map((key) => {
+      const model = {
+        id: key,
+        name: payload.data.key_info[key].name,
+      };
+      if (payload.backend) {
+        model.backend = payload.backend;
+      }
+      return model;
+    });
+  },
+  normalizeItems(payload) {
+    // due to extractLazyPaginatedData above, keys is no longer an array of strings but objects
+    if (payload.data.keys && Array.isArray(payload.data.keys)) {
+      return payload.data.keys;
+    }
+    const ret = {
+      ...payload,
+      ...payload.data,
+    };
+    delete ret.data;
+    return ret;
+  },
 });
