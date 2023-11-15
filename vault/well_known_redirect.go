634348c162d5021cc1c2350fb25cbf1f4ad020c4
Scott G. Miller <smiller@hashicorp.com>
2023-11-15 11:17:46 -0600

    comments/cleanup

--- vault/well_known_redirect.go
+++ vault/well_known_redirect.go
@@ -14,6 +14,13 @@ import (
 	"github.com/armon/go-radix"
 )
 
+type wellKnownRedirect struct {
+	c             *Core
+	mountUUID     string
+	prefix        string
+	isPrefixMatch bool
+}
+
 type wellKnownRedirectRegistry struct {
 	lock  sync.Mutex
 	paths *radix.Tree
@@ -25,6 +32,7 @@ func NewWellKnownRedirects() *wellKnownRedirectRegistry {
 	}
 }
 
+// Attempt to register a mapping from /.well-known/_src_ to /v1/_mount-path_/_dest_
 func (reg *wellKnownRedirectRegistry) TryRegister(ctx context.Context, core *Core, mountUUID, src, dest string) error {
 	if strings.HasPrefix(dest, "/") {
 		return errors.New("redirect targets must be relative")
@@ -43,6 +51,7 @@ func (reg *wellKnownRedirectRegistry) TryRegister(ctx context.Context, core *Cor
 	return nil
 }
 
+// Find any relevant redirects for a given source path
 func (reg *wellKnownRedirectRegistry) Find(path string) (*wellKnownRedirect, string) {
 	s, a, found := reg.paths.LongestPrefix(path)
 	if found {
@@ -62,12 +71,13 @@ func (reg *wellKnownRedirectRegistry) Find(path string) (*wellKnownRedirect, str
 	return nil, ""
 }
 
-func (reg *wellKnownRedirectRegistry) DeregisterMount(uuid string) {
+// Remove all redirects for a given mount
+func (reg *wellKnownRedirectRegistry) DeregisterMount(mountUuid string) {
 	reg.lock.Lock()
 	defer reg.lock.Unlock()
 	reg.paths.Walk(func(k string, v interface{}) bool {
 		r := v.(*wellKnownRedirect)
-		if r.mountUUID == uuid {
+		if r.mountUUID == mountUuid {
 			reg.paths.Delete(k)
 			return true
 		}
@@ -75,13 +85,14 @@ func (reg *wellKnownRedirectRegistry) DeregisterMount(uuid string) {
 	})
 }
 
-func (reg *wellKnownRedirectRegistry) DeregisterSource(uuid, src string) bool {
+// Remove a specific redirect for a mount
+func (reg *wellKnownRedirectRegistry) DeregisterSource(mountUuid, src string) bool {
 	reg.lock.Lock()
 	defer reg.lock.Unlock()
 	var found bool
 	reg.paths.Walk(func(k string, v interface{}) bool {
 		r := v.(*wellKnownRedirect)
-		if r.mountUUID == uuid && k == src {
+		if r.mountUUID == mountUuid && k == src {
 			found = true
 			reg.paths.Delete(k)
 			return true
@@ -91,13 +102,7 @@ func (reg *wellKnownRedirectRegistry) DeregisterSource(uuid, src string) bool {
 	return found
 }
 
-type wellKnownRedirect struct {
-	c             *Core
-	mountUUID     string
-	prefix        string
-	isPrefixMatch bool
-}
-
+// Construct the full destination of the redirect, including any remaining path past the src
 func (a *wellKnownRedirect) Destination(remaining string) (string, error) {
 	var destPath string
 	if a.c == nil {
