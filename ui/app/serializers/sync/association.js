0d9d9ef253cb14181f1cef30b6900f1a71da1908
Jordan Reimer <zofskeez@gmail.com>
2023-11-14 21:22:01 -0700

    adds model, adapter and serializer for sync associations

---
+++ ui/app/serializers/sync/association.js
@@ -0,0 +1,34 @@
+/**
+ * Copyright (c) HashiCorp, Inc.
+ * SPDX-License-Identifier: BUSL-1.1
+ */
+
+import ApplicationSerializer from 'vault/serializers/application';
+
+export default class SyncDestinationSerializer extends ApplicationSerializer {
+  attrs = {
+    destinationName: { serialize: false },
+    destinationType: { serialize: false },
+    syncStatus: { serialize: false },
+    updatedAt: { serialize: false },
+  };
+
+  extractLazyPaginatedData(payload) {
+    if (payload) {
+      const { store_name, store_type, associated_secrets } = payload.data;
+      const secrets = [];
+      for (const key in associated_secrets) {
+        const data = associated_secrets[key];
+        data.id = key;
+        const association = {
+          destinationName: store_name,
+          destinationType: store_type,
+          ...data,
+        };
+        secrets.push(association);
+      }
+      return secrets;
+    }
+    return payload;
+  }
+}
--- ui/mirage/factories/sync-association.js
+++ ui/mirage/factories/sync-association.js
@@ -6,7 +6,7 @@
 import { Factory } from 'ember-cli-mirage';
 
 export default Factory.extend({
-  accessor: 'kv_eb4acbae',
+  mount: 'my-kv',
   secret_name: 'my-path/my-secret-1',
   sync_status: 'SYNCED',
   updated_at: '2023-09-20T10:51:53.961861096-04:00',
