4a338427bd2cb49aa392039b28a2b24550ad5d01
clairebontempo@gmail.com <clairebontempo@gmail.com>
2023-11-16 16:08:35 -0600

    start confirm action tests WIP

--- ui/tests/integration/components/confirm-action-test.js
+++ ui/tests/integration/components/confirm-action-test.js
@@ -12,7 +12,7 @@ import sinon from 'sinon';
 module('Integration | Component | confirm-action', function (hooks) {
   setupRenderingTest(hooks);
 
-  test('it closes the confirmation modal on successful delete', async function (assert) {
+  test('it renders', async function (assert) {
     const confirmAction = sinon.spy();
     this.set('onConfirm', confirmAction);
     await render(hbs`
@@ -21,19 +21,37 @@ module('Integration | Component | confirm-action', function (hooks) {
         @onConfirmAction={{this.onConfirm}}
       />
       `);
+
+    assert.dom('[data-test-confirm-action-trigger]').hasText('DELETE', 'renders button text');
     await click('[data-test-confirm-action-trigger]');
+    assert.dom('[data-test-confirm-action-title]').hasText('Are you sure?', 'renders default title');
+    assert
+      .dom('[data-test-confirm-action-message]')
+      .hasText('You will not be able to recover it later.', 'renders default body text');
     await click('[data-test-confirm-cancel-button]');
-    // assert that after CANCEL the icon button is pointing down.
-    assert.dom('[data-test-icon="chevron-down"]').exists('Icon is pointing down after clicking cancel');
-    // open the modal again to test the DELETE action
-    await click('[data-test-confirm-action-trigger="true"]');
-    await click('[data-test-confirm-button="true"]');
-    assert
-      .dom('[data-test-icon="chevron-down"]')
-      .exists('Icon is pointing down after executing the Delete action');
-    assert.true(confirmAction.called, 'calls the action when Delete is pressed');
-    assert
-      .dom('[data-test-confirm-action-title]')
-      .doesNotExist('it has closed the confirm content and does not show the title');
+    assert.false(confirmAction.called, 'does not call the action when Cancel is clicked');
+    await click('[data-test-confirm-action-trigger]');
+    await click('[data-test-confirm-button]');
+    assert.true(confirmAction.called, 'calls the action when Confirm is clicked');
+    assert.dom('[data-test-confirm-action-title]').doesNotExist('modal closes after confirm is clicked');
   });
+
+  test('it renders loading state', async function (assert) {
+    const confirmAction = sinon.spy();
+    this.set('onConfirm', confirmAction);
+    await render(hbs`
+      <ConfirmAction
+        @buttonText="Open!"
+        @onConfirmAction={{this.onConfirm}}
+        @isRunning={{true}}
+      />
+      `);
+
+    await click('[data-test-confirm-action-trigger]');
+
+    assert.dom('[data-test-confirm-button]').isDisabled('disables confirm button when loading');
+    assert.dom('[data-test-confirm-button] [data-test-icon="loading"]').exists('it renders loading icon');
+  });
+
+  // handle passed in args confirmTitle and modal color logic with button
 });
