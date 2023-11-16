69f2952ca24a04ce1991a7f3cc2e01c261f5509e
clairebontempo@gmail.com <clairebontempo@gmail.com>
2023-11-16 02:16:45 -0600

    update docs

--- ui/lib/core/addon/components/confirm-action.js
+++ ui/lib/core/addon/components/confirm-action.js
@@ -12,23 +12,38 @@ import { tracked } from '@glimmer/tracking';
  * @module ConfirmAction
  * ConfirmAction is a button that opens a modal containing a confirmation message with confirm or cancel action.
  * Splattributes are spread to the button element to apply styling directly without adding extra args.
- * The default button is the same as Hds::Button which is primary blue
+ * The default button is the same as Hds::Button which is primary blue.
+ * 
  *
  * @example
- * ```js
- *  <ConfirmAction
- *    @buttonText="Delete item"
- *    @onConfirmAction={{ () => { console.log('Action!') } }}
- *    @confirmMessage="Are you sure you want to delete this config?"
- *  />
+//  in dropdown
+  <ConfirmAction
+    class="hds-confirm-action-critical"
+    @buttonColor="critical"
+    @isInDropdown={{true}}
+    @buttonText="Delete"
+    @confirmMessage="This action cannot be undone."
+    @onConfirmAction={{log "my action!"}}
+  />
+
+  // in toolbar 
+  <ConfirmAction
+    class="toolbar-button"
+    @buttonColor="secondary"
+    @buttonText="Delete item"
+    @confirmTitle="Delete item?"
+    @onConfirmAction={{log "my action!"}}
+    @confirmMessage="Are you sure you want to delete this config?"
+   />
  * ```
  *
  * @param {Function} onConfirmAction - The action to take upon confirming.
  * @param {String} [confirmTitle=Are you sure?] - The title to display in the confirmation modal.
  * @param {String} [confirmMessage=You will not be able to recover it later.] - The message to display when confirming.
- * @param {String} buttonText - Text for the button that triggers modal to open.
- * @param {String} [buttonColor] - Color of button that triggers modal. Default is primary, other options are secondary, tertiary, and critical
- * @param {String} [modalColor=critical] - Styles modal color, if 'critical' confirm button is styled as well. Possible values: critical, warning or neutral
+ * @param {Boolean} isInDropdown - If true styles for dropdowns by rendering inside `<li>` elements via `<Hds::Dropdown::ListItem::Interactive`
+ * @param {String} buttonText - Text for the button that toggles modal to open.
+ * @param {String} [buttonColor] - Color of button that toggles modal. Default is primary, other options are secondary, tertiary, and critical (recommended for dropdowns)
+ * @param {String} [modalColor=critical] - Styles modal color, if 'critical' confirm button is also 'critical'. Possible values: critical, warning or neutral
  * @param {Boolean} [isRunning] - Disables the confirm button if action is still running
  * @param {Boolean} [disabled] - Disables the modal's confirm button.
  *
