b87960449f5f114a6f7eeabc8cbbda1cafe64b26
clairebontempo@gmail.com <clairebontempo@gmail.com>
2023-11-15 16:34:02 -0800

    cleanup confirm action

--- ui/lib/core/addon/components/confirm-action.js
+++ ui/lib/core/addon/components/confirm-action.js
@@ -10,7 +10,9 @@ import { tracked } from '@glimmer/tracking';
 
 /**
  * @module ConfirmAction
- * `ConfirmAction` is a button followed by a pop up confirmation message and button used to prevent users from performing actions they do not intend to.
+ * ConfirmAction is a button that opens a modal containing a confirmation message with confirm or cancel action.
+ * Splattributes are spread to the button element to apply styling directly without adding extra args.
+ * The default button is the same as Hds::Button which is primary blue
  *
  * @example
  * ```js
@@ -24,9 +26,8 @@ import { tracked } from '@glimmer/tracking';
  * @param {Func} onConfirmAction - The action to take upon confirming.
  * @param {String} [confirmTitle=Are you sure?] - The title to display in the confirmation modal.
  * @param {String} [confirmMessage=You will not be able to recover it later.] - The message to display when confirming.
- * @param {String} [confirmButtonText=Delete] - The confirm button text.
- * @param {String} [cancelButtonText=Cancel] - The cancel button text.
- * @param {String} [buttonClasses] - A string to indicate the button class.
+ * @param {String} buttonText - Text for the button that triggers modal to open.
+ * @param {String} buttonColor - Color of button that triggers modal. Default is primary, other options are secondary, tertiary, and critical
  * @param {Boolean} [isRunning] - Disables the confirm button if action is still running
  * @param {Boolean} [disabled] - To disable the confirm button.
  * @param {String} [color=critical] - Changes the color of modal and confirm button
