82b874a77c5926d11fdc6e466e79652b28215a78
Jordan Reimer <zofskeez@gmail.com>
2023-11-22 15:06:00 -0700

    fixes issue with filter-input debounce and updates to spread attributes for input rather than use args

--- ui/tests/integration/components/filter-input-test.js
+++ ui/tests/integration/components/filter-input-test.js
@@ -5,22 +5,23 @@
 
 import { module, test } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
-import { render, fillIn } from '@ember/test-helpers';
+import { render, fillIn, click } from '@ember/test-helpers';
 import hbs from 'htmlbars-inline-precompile';
 
 module('Integration | Component | filter-input', function (hooks) {
   setupRenderingTest(hooks);
 
-  test('it should render initial value', async function (assert) {
-    await render(hbs`<FilterInput @value="foo" />`);
-    assert.dom('[data-test-filter-input]').hasValue('foo', 'Initial value set on input');
-  });
+  test('it should render attributes on input element', async function (assert) {
+    assert.expect(3);
 
-  test('it should render placeholder', async function (assert) {
-    await render(hbs`<FilterInput @placeholder="Filter roles" />`);
+    this.onClick = () => assert.ok(true, 'on click modifier passed to input element');
+
+    await render(hbs`<FilterInput placeholder="Filter roles" value="foo" {{on "click" this.onClick}} />`);
+    await click('[data-test-filter-input]');
     assert
       .dom('[data-test-filter-input]')
-      .hasAttribute('placeholder', 'Filter roles', 'Placeholder set on input element');
+      .hasAttribute('placeholder', 'Filter roles', 'Placeholder passed to input element');
+    assert.dom('[data-test-filter-input]').hasValue('foo', 'Value passed to input element');
   });
 
   test('it should focus input on insert', async function (assert) {
