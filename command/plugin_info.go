6428e4228ac0ba99da2efbecd4169dae26dd2034
Tom Proctor <tomhjp@users.noreply.github.com>
2023-11-24 11:33:55 +0000

    cli: Improve error handling for plugin commands

--- command/plugin_info.go
+++ command/plugin_info.go
@@ -78,21 +78,16 @@ func (c *PluginInfoCommand) Run(args []string) int {
 	var pluginNameRaw, pluginTypeRaw string
 	args = f.Args()
 	switch {
-	case len(args) < 1:
-		c.UI.Error(fmt.Sprintf("Not enough arguments (expected 1 or 2, got %d)", len(args)))
+	case len(args) < 2:
+		c.UI.Error(fmt.Sprintf("Not enough arguments (expected 2, got %d)", len(args)))
 		return 1
 	case len(args) > 2:
-		c.UI.Error(fmt.Sprintf("Too many arguments (expected 1 or 2, got %d)", len(args)))
+		c.UI.Error(fmt.Sprintf("Too many arguments (expected 2, got %d)", len(args)))
 		return 1
-
-	// These cases should come after invalid cases have been checked
-	case len(args) == 1:
-		pluginTypeRaw = "unknown"
-		pluginNameRaw = args[0]
-	case len(args) == 2:
-		pluginTypeRaw = args[0]
-		pluginNameRaw = args[1]
 	}
+
+	pluginTypeRaw = args[0]
+	pluginNameRaw = args[1]
 
 	client, err := c.Client()
 	if err != nil {
