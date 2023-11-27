8d98d118dcb9c0da8cba4c7de6a6f91552d31dce
Chelsea Shaw <cshaw@hashicorp.com>
2023-11-27 15:58:53 -0600

    update app-footer tests

--- ui/tests/integration/components/app-footer-test.js
+++ ui/tests/integration/components/app-footer-test.js
@@ -4,7 +4,6 @@ import { render } from '@ember/test-helpers';
 import { hbs } from 'ember-cli-htmlbars';
 
 const selectors = {
-  copyright: '[data-test-footer-copyright]',
   versionDisplay: '[data-test-footer-version]',
   upgradeLink: '[data-test-footer-upgrade-link]',
   docsLink: '[data-test-footer-documentation-link]',
@@ -19,7 +18,6 @@ module('Integration | Component | app-footer', function (hooks) {
 
   test('it renders a sane default', async function (assert) {
     await render(hbs`<AppFooter />`);
-    assert.dom(selectors.copyright).exists('shows copyright by default');
     assert.dom(selectors.versionDisplay).hasText('Vault', 'Vault without version by default');
     assert.dom(selectors.upgradeLink).hasText('Upgrade to Vault Enterprise', 'upgrade link shows');
     assert.dom(selectors.docsLink).hasText('Documentation', 'displays docs link');
@@ -27,16 +25,16 @@ module('Integration | Component | app-footer', function (hooks) {
 
   test('it renders for community version', async function (assert) {
     this.versionSvc.version = '1.15.1';
+    this.versionSvc.type = 'community';
     await render(hbs`<AppFooter />`);
-    assert.dom(selectors.copyright).exists('shows copyright by default');
     assert.dom(selectors.versionDisplay).hasText('Vault 1.15.1', 'Vault shows version when available');
     assert.dom(selectors.upgradeLink).hasText('Upgrade to Vault Enterprise', 'upgrade link shows');
     assert.dom(selectors.docsLink).hasText('Documentation', 'displays docs link');
   });
   test('it renders for ent version', async function (assert) {
     this.versionSvc.version = '1.15.1+hsm';
+    this.versionSvc.type = 'enterprise';
     await render(hbs`<AppFooter />`);
-    assert.dom(selectors.copyright).exists('shows copyright by default');
     assert.dom(selectors.versionDisplay).hasText('Vault 1.15.1+hsm', 'shows version when available');
     assert.dom(selectors.upgradeLink).doesNotExist('upgrade link not shown');
     assert.dom(selectors.docsLink).hasText('Documentation', 'displays docs link');
