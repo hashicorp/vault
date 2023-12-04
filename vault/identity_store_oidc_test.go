924cb42a4b504e11f54f08df8f5c907493288fd4
Austin Gebauer <agebauer@hashicorp.com>
2023-12-04 15:30:27 -0800

    change key rotation test to check exact counts and add comments

--- vault/identity_store_oidc_test.go
+++ vault/identity_store_oidc_test.go
@@ -1055,10 +1055,11 @@ func TestOIDC_PeriodicFunc(t *testing.T) {
 			setSigningKey:     true,
 			setNextSigningKey: true,
 			testCases: []testCase{
+				// Each cycle results in a key going in/out of its verification_ttl period
 				{1, 2, 2},
-				{2, 2, 3},
+				{2, 3, 3},
 				{3, 2, 2},
-				{4, 2, 3},
+				{4, 3, 3},
 			},
 		},
 		{
@@ -1068,6 +1069,9 @@ func TestOIDC_PeriodicFunc(t *testing.T) {
 			setNextSigningKey: true,
 			testCases: []testCase{
 				{1, 1, 1},
+
+				// key counts jump from 1 to 2 because the next signing key becomes
+				// the signing key, and no key is in its verification_ttl period
 				{2, 2, 2},
 			},
 		},
@@ -1078,7 +1082,10 @@ func TestOIDC_PeriodicFunc(t *testing.T) {
 			setNextSigningKey: false,
 			testCases: []testCase{
 				{1, 1, 1},
-				{2, 2, 2},
+
+				// key counts jump from 1 to 3 because the original signing key is
+				// still published and within its verification_ttl period
+				{2, 3, 3},
 			},
 		},
 		{
@@ -1154,7 +1161,7 @@ func TestOIDC_PeriodicFunc(t *testing.T) {
 				expectedKeyCount := testSet.testCases[i].numKeys
 				namedKeySamples[i].DecodeJSON(&testSet.namedKey)
 				actualKeyRingLen := len(testSet.namedKey.KeyRing)
-				if actualKeyRingLen < expectedKeyCount {
+				if actualKeyRingLen != expectedKeyCount {
 					t.Errorf(
 						"For key: %s at cycle: %d expected namedKey's KeyRing to be at least of length %d but was: %d",
 						testSet.namedKey.name,
@@ -1165,7 +1172,7 @@ func TestOIDC_PeriodicFunc(t *testing.T) {
 				}
 				expectedPublicKeyCount := testSet.testCases[i].numPublicKeys
 				actualPubKeysLen := len(publicKeysSamples[i])
-				if actualPubKeysLen < expectedPublicKeyCount {
+				if actualPubKeysLen != expectedPublicKeyCount {
 					t.Errorf(
 						"For key: %s at cycle: %d expected public keys to be at least of length %d but was: %d",
 						testSet.namedKey.name,
