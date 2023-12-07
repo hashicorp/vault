e678d2ed4e24040ff96d849e3f3a588d48f687e4
Scott Miller <smiller@hashicorp.com>
2023-12-07 15:31:52 -0600

    use a stop chan to prevent sends on the closed result channel (#24428)
    * use a stop chan to prevent sends on the closed result channel
    
    * these need to be defers, since there are early returns in the for/select stmt

--- vault/seal/seal.go
+++ vault/seal/seal.go
@@ -695,13 +695,20 @@ func (a *access) Decrypt(ctx context.Context, ciphertext *MultiWrapValue, option
 		err    error
 	}
 	resultCh := make(chan *result)
+	stopCh := make(chan struct{})
+	defer close(stopCh)
+	defer close(resultCh)
 
 	reportResult := func(name string, plaintext []byte, oldKey bool, err error) {
-		resultCh <- &result{
-			name:   name,
-			pt:     plaintext,
-			oldKey: oldKey,
-			err:    err,
+		select {
+		case <-stopCh:
+		default:
+			resultCh <- &result{
+				name:   name,
+				pt:     plaintext,
+				oldKey: oldKey,
+				err:    err,
+			}
 		}
 	}
 
@@ -757,7 +764,6 @@ GATHER_RESULTS:
 
 			case result.oldKey:
 				return result.pt, false, OldKey
-
 			default:
 				return result.pt, isUpToDate, nil
 			}
@@ -765,7 +771,6 @@ GATHER_RESULTS:
 			break GATHER_RESULTS
 		}
 	}
-	close(resultCh)
 
 	// No wrapper was able to decrypt the value, return an error
 	if len(errs) > 0 {
