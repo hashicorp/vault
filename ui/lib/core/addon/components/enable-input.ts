df892c5d4fe2cb47a2c023be87b7c621413f6cd0
clairebontempo@gmail.com <clairebontempo@gmail.com>
2023-12-07 10:32:03 -0800

    make attrOptions optional

--- ui/lib/core/addon/components/enable-input.ts
+++ ui/lib/core/addon/components/enable-input.ts
@@ -7,21 +7,22 @@ import Component from '@glimmer/component';
 import { tracked } from '@glimmer/tracking';
 
 interface Args {
-  attr: Attr;
+  attrOptions?: AttrOptions;
 }
-interface Attr {
-  name: string;
-  options: {
-    label: string;
-    helpText: string;
-    subText: string;
-    possibleValues: string[];
+interface AttrOptions {
+  name: string; // required
+  options?: {
+    label?: string;
+    helpText?: string;
+    subText?: string;
+    possibleValues?: string[];
   };
 }
 
 /**
  * @module EnableInput
- * EnableInput components wrap a form field component and include an "Edit" button that users must click to enable the input.
+ * EnableInput components render a disabled input with a hardcoded value of ********** with an "Edit" button to "enable" the input,
+ * clicking "Edit" hides the disabled input and instead renders the yielded component. This way any data management is handled by the parent.
  * These are useful for inputs sensitive values that are not returned by the API, so they are only sent in a POST request if the user
  * has performed an extra click to intentionally edit the field.
  *
@@ -30,6 +31,11 @@ interface Attr {
  *  <FormField @attr={{attr}} @model={{@destination}} @modelValidations={{this.modelValidations}} />
  * </EnableInput>
  *
+ * <EnableInput class="field" @attr={{attr}}>
+ *  <FormField @attr={{attr}} @model={{@destination}} @modelValidations={{this.modelValidations}} />
+ * </EnableInput>
+ *
+ * @param {object} [attrOptions] - used to generate the label for `ReadonlyFormField`, `name` key is required. Can be an attribute from a model exported with expandAttributeMeta.
  */
 
 export default class EnableInputComponent extends Component<Args> {
