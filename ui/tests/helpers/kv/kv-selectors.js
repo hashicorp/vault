8d28d69250a0eadaf6081fbb9e751eab49c3f1de
Angel Garbarino <argarbarino@gmail.com>
2023-12-08 10:11:26 -0700

    test coverage

--- ui/tests/helpers/kv/kv-selectors.js
+++ ui/tests/helpers/kv/kv-selectors.js
@@ -58,6 +58,8 @@ export const PAGE = {
     createSecret: '[data-test-toolbar-create-secret]',
     item: (secret) => (!secret ? '[data-test-list-item]' : `[data-test-list-item="${secret}"]`),
     filter: `[data-test-kv-list-filter]`,
+    listMenuDelete: `[data-test-popup-metadata-delete]`,
+    listMenuCreate: `[data-test-popup-create-new-version]`,
     overviewCard: '[data-test-overview-card-container="View secret"]',
     overviewInput: '[data-test-view-secret] input',
     overviewButton: '[data-test-get-secret-detail]',
