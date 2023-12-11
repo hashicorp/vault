79b191228739717b513240332956e6728f998e62
Marc Boudreau <marc.boudreau@hashicorp.com>
2023-12-11 13:31:42 -0500

    VAULT-21607: Provide Management Endpoints for Vault UI Custom Messages (#24263)
    * Implement custom-message management endpoints in a namespace aware manner
    
    * completion of non-enterprise version of custom-messages
    
    * clean up of error handling and fixing a nil pointer error
    
    * rename UICustomMessagesEntry to UICustomMessageEntry
    
    * add unit tests to cover new functions in UIConfig related to custom messages
    
    * unit tests for all custom message handling
    
    * add missing header comments for new files
    
    * add changelog file
    
    * fix test setup error that led to unexpected failure
    
    * change return type from slice of pointers to struct to slice of struct and add godocs to every function
    
    * add Internal suffix to internal methods for the UIConfig struct
    
    * add validation for start and end times of custom messages
    
    * improvements based on review feedback
    
    * explore new approach for custom messages
    
    * introduce new error to force HTTP 404 when referencing non-existant UI custom message
    
    * remove changelog entry until feature is complete
    
    * implement CRUD endpoints using single storage entry per namespace
    
    * add mutex to protect operations that read the storage entry and write it back
    
    * add copyright header comment to new files
    
    * fix failing tests due to change in target function behaviour in order to return 404 error when mandated
    
    * feedback from review plus some improvements on my own as well
    
    * define constants for recognized message types and replace hardcoded strings occurrences with new constants
    
    * incorporate feedback comment
    
    * beef up testing with non-root namespaces in putEntry and getEntryForNamespace
    
    * renaming CreateMessage to AddMessage in uicustommessages.Manager and uicustommessages.Entry
    
    * adding missing copyright header comments

--- vault/core.go
+++ vault/core.go
@@ -66,6 +66,7 @@ import (
 	"github.com/hashicorp/vault/vault/plugincatalog"
 	"github.com/hashicorp/vault/vault/quotas"
 	vaultseal "github.com/hashicorp/vault/vault/seal"
+	uicustommessages "github.com/hashicorp/vault/vault/ui_custom_messages"
 	"github.com/hashicorp/vault/version"
 	"github.com/patrickmn/go-cache"
 	uberAtomic "go.uber.org/atomic"
@@ -524,7 +525,8 @@ type Core struct {
 	activeNodeReplicationState *uint32
 
 	// uiConfig contains UI configuration
-	uiConfig *UIConfig
+	uiConfig             *UIConfig
+	customMessageManager *uicustommessages.Manager
 
 	// rawEnabled indicates whether the Raw endpoint is enabled
 	rawEnabled bool
@@ -1260,6 +1262,7 @@ func NewCore(conf *CoreConfig) (*Core, error) {
 	// UI
 	uiStoragePrefix := systemBarrierPrefix + "ui"
 	c.uiConfig = NewUIConfig(conf.EnableUI, physical.NewView(c.physical, uiStoragePrefix), NewBarrierView(c.barrier, uiStoragePrefix))
+	c.customMessageManager = uicustommessages.NewManager(c.barrier)
 
 	// Listeners
 	err = c.configureListeners(conf)
