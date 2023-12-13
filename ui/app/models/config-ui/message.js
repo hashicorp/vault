7e110b3f3115f5ae31ed3612fee9d667d32c4aa0
Kianna Quach <kianna.quach@hashicorp.com>
2023-12-12 21:44:53 -0800

    Fix copyright format issues and clean up code

--- ui/app/models/config-ui/message.js
+++ ui/app/models/config-ui/message.js
@@ -1,79 +1,107 @@
-{{!
-  Copyright (c) HashiCorp, Inc.
-  SPDX-License-Identifier: BUSL-1.1
-~}}
+/**
+ * Copyright (c) HashiCorp, Inc.
+ * SPDX-License-Identifier: BUSL-1.1
+ */
+import Model, { attr } from '@ember-data/model';
+import lazyCapabilities, { apiPath } from 'vault/macros/lazy-capabilities';
+import { isAfter, format, addDays, startOfDay } from 'date-fns';
+import { parseAPITimestamp } from 'core/utils/date-formatters';
+import { withModelValidations } from 'vault/decorators/model-validations';
+import { withFormFields } from 'vault/decorators/model-form-fields';
 
-<Messages::TabPageHeader
-  @authenticated={{@message.authenticated}}
-  @pageTitle="{{if @message.isNew 'Create' 'Edit'}} message"
-  @breadcrumbs={{this.breadcrumbs}}
-/>
+export const localDateTimeString = "yyyy-MM-dd'T'HH:mm";
 
-<form id="message-create-edit-form" {{on "submit" (perform this.save)}} data-test-form="create-and-edit">
-  <div class="box is-sideless is-fullwidth is-marginless has-top-padding-s">
-    <Hds::Text::Body @tag="p" class="has-bottom-margin-l" data-test-form-subtext>
-      {{if @message.isNew "Create" "Edit"}}
-      a custom message for all users when they access a Vault system via the UI.
-    </Hds::Text::Body>
+const validations = {
+  title: [{ type: 'presence', message: 'Title is required.' }],
+  message: [{ type: 'presence', message: 'Message is required.' }],
+};
 
-    <MessageError @errorMessage={{this.errorBanner}} class="has-top-margin-s" />
+@withFormFields(['authenticated', 'type', 'title', 'message', 'linkTitle', 'startTime', 'endTime'])
+@withModelValidations(validations)
+export default class MessageModel extends Model {
+  @attr('boolean') active;
+  @attr('string', {
+    label: 'Type',
+    editType: 'radio',
+    possibleValues: [
+      {
+        label: 'Alert message',
+        helperText:
+          'A banner that appears on the top of every page to display brief but high-signal messages like an update or system alert.',
+        value: 'banner',
+      },
+      {
+        label: 'Modal',
+        helperText:
+          'A pop-up window used to bring immediate attention for important notifications or actions.',
+        value: 'modal',
+      },
+    ],
+    defaultValue: 'banner',
+  })
+  type;
+  @attr('boolean', {
+    label: 'Where should we display this message?',
+    editType: 'radio',
+    possibleValues: [
+      {
+        label: 'After the user logs in',
+        helperText: 'Display to users after they have successfully logged in to Vault.',
+        value: true,
+      },
+      {
+        label: 'On the login page',
+        helperText: 'Display to users on the login page before they have authenticated.',
+        value: false,
+      },
+    ],
+    defaultValue: true,
+  })
+  authenticated;
+  @attr('string', {
+    label: 'Title',
+    fieldValue: 'title',
+  })
+  title;
+  @attr('string', {
+    label: 'Message',
+    fieldValue: 'message',
+    editType: 'textarea',
+  })
+  message;
+  @attr('date', {
+    editType: 'dateTimeLocal',
+    label: 'Message starts',
+    subText: 'Defaults to 12:00 a.m. the following day (local timezone).',
+    defaultValue: format(addDays(startOfDay(new Date() || this.startTime), 1), localDateTimeString),
+  })
+  startTime;
+  @attr('date', { editType: 'yield', label: 'Message expires' }) endTime;
 
-    {{#each @message.formFields as |attr|}}
-      {{#if (eq attr.name "linkTitle")}}
-        <div class="field has-bottom-margin-m">
-          <label for="link" class="is-label">Link
-            <span class="has-text-grey-400 has-font-weight-normal">(optional)</span></label>
-          <div class="control is-flex-between has-gap-m">
-            <Input
-              @type="text"
-              @value={{@message.linkTitle}}
-              placeholder="Display text (e.g. Learn more)"
-              id="link-title"
-              class="input"
-              data-test-field="linkTitle"
-              {{on "input" (pipe (pick "target.value") (fn (mut @message.linkTitle)))}}
-            />
-            <Input
-              @type="text"
-              @value={{@message.linkHref}}
-              placeholder="Paste URL (e.g. www.learnmore.com)"
-              id="link-href"
-              class="input"
-              data-test-field="linkHref"
-              {{on "input" (pipe (pick "target.value") (fn (mut @message.linkHref)))}}
-            />
-          </div>
-        </div>
-      {{else}}
-        <FormField
-          @attr={{attr}}
-          @model={{@message}}
-          @modelValidations={{this.modelValidations}}
-          class="has-bottom-margin-m"
-          data-test-field={{true}}
-        >
-          <Messages::MessageExpirationDateForm @message={{@message}} @attr={{attr}} />
-        </FormField>
-      {{/if}}
-    {{/each}}
+  // the api returns link as an object with title and href as keys, but we separate the link key/values into
+  // different attributes to easily show link title and href fields on the create form. In our serializer,
+  // we send the link attribute in to the correct format (as an object) to the server.
+  @attr('string', { fieldValue: 'linkTitle' }) linkTitle;
+  @attr('string', { fieldValue: 'linkHref' }) linkHref;
 
-    <Hds::ButtonSet class="has-top-margin-s has-bottom-margin-m has-top-margin-xl">
-      {{! TODO: VAULT-21533 preview modal }}
-      <Hds::Button @text="Preview" @color="tertiary" @icon="eye" />
+  // date helpers
+  get isStartTimeAfterToday() {
+    return isAfter(parseAPITimestamp(this.startTime), new Date());
+  }
 
-      <Hds::Button
-        @text="{{if @message.isNew 'Create' 'Edit'}} message"
-        disabled={{(or this.invalidFormMessage this.save.isRunning)}}
-        data-test-button="create-message"
-        type="submit"
-      />
+  // capabilities
+  @lazyCapabilities(apiPath`sys/config/ui/custom-messages`) customMessagesPath;
 
-      <Hds::Button
-        @text="Cancel"
-        @color="secondary"
-        @route="messages.index"
-        @query={{hash authenticated=@message.authenticated}}
-      />
-    </Hds::ButtonSet>
-  </div>
-</form>
+  get canCreateCustomMessages() {
+    return this.customMessagesPath.get('canCreate') !== false;
+  }
+  get canReadCustomMessages() {
+    return this.customMessagesPath.get('canRead') !== false;
+  }
+  get canEditCustomMessages() {
+    return this.customMessagesPath.get('canUpdate') !== false;
+  }
+  get canDeleteCustomMessages() {
+    return this.customMessagesPath.get('canDelete') !== false;
+  }
+}
