24b5aa14c4af9b36b28102e86899290309b857f0
Kianna Quach <kianna.quach@hashicorp.com>
2023-12-12 19:59:25 -0800

    Fix license headers

--- ui/lib/config-ui/addon/components/messages/page/create-and-edit-message-form.js
+++ ui/lib/config-ui/addon/components/messages/page/create-and-edit-message-form.js
@@ -1,54 +1,78 @@
-{{!
-  Copyright (c) HashiCorp, Inc.
-  SPDX-License-Identifier: BUSL-1.1
-~}}
+/**
+ * Copyright (c) HashiCorp, Inc.
+ * SPDX-License-Identifier: BUSL-1.1
+ */
 
-<div class="has-top-padding-xs has-bottom-padding-s is-narrow is-flex-align-baseline">
-  <RadioButton
-    class="radio"
-    name={{@attr.name}}
-    id="never"
-    value="never"
-    @value="never"
-    @onChange={{fn (mut @message.endTime) ""}}
-    @groupValue={{this.groupValue}}
-  />
-  <div class="has-left-margin-xs">
-    <label for="never" class="has-left-margin-xs has-text-black is-size-7">
-      Never
-      <div class="has-left-margin-xs has-text-grey is-size-8">
-        This message will never expire unless manually deleted by an operator.
-      </div>
-    </label>
-  </div>
-</div>
+import Component from '@glimmer/component';
+import { tracked } from '@glimmer/tracking';
+import { task } from 'ember-concurrency';
+import errorMessage from 'vault/utils/error-message';
+import { inject as service } from '@ember/service';
+import { format } from 'date-fns';
 
-<div class="has-top-padding-xs has-bottom-padding-s is-narrow is-flex-align-baseline">
-  <RadioButton
-    class="radio"
-    name={{@attr.name}}
-    id="specificDate"
-    value="specificDate"
-    @value="specificDate"
-    @onChange={{(fn (mut this.groupValue) "specificDate")}}
-    @groupValue={{this.groupValue}}
-  />
-  <div class="has-left-margin-xs">
-    <label for="specificDate" class="has-left-margin-xs has-text-black is-size-7">
-      Specific date
-      <div class="has-left-margin-xs has-text-grey is-size-8">
-        This message will expire at midnight (local timezone) at the specific date.
-      </div>
-      <div class="control has-left-margin-xs">
-        <Input
-          @type="datetime-local"
-          @value={{date-format @message.endTime "yyyy-MM-dd'T'HH:mm"}}
-          class="input has-top-margin-xs is-auto-width"
-          name="endTime"
-          data-test-input="endTime"
-          {{on "input" (pipe (pick "target.value") (fn (mut @message.endTime)))}}
-        />
-      </div>
-    </label>
-  </div>
-</div>
+import { localDateTimeString } from 'vault/models/config-ui/message';
+
+/**
+ * @module Page::CreateAndEditMessageForm
+ * Page::CreateAndEditMessageForm components are used to display create and edit message form fields.
+ * @example
+ * ```js
+ * <Page::CreateAndEditMessageForm @message={{this.message}}  />
+ * ```
+ * @param {model} message - message model to pass to form components
+ */
+
+export default class MessagesList extends Component {
+  @service router;
+  @service flashMessages;
+
+  @tracked errorBanner = '';
+  @tracked modelValidations;
+  @tracked invalidFormMessage;
+  @tracked endTime;
+
+  constructor() {
+    super(...arguments);
+    if (this.args.message.endTime) {
+      this.endTime = format(new Date(this.args.message.endTime), localDateTimeString);
+    }
+  }
+
+  get breadcrumbs() {
+    const authenticated =
+      this.args.message.authenticated === undefined ? true : this.args.message.authenticated;
+    return [
+      { label: 'Messages', route: 'messages.index', query: { authenticated } },
+      { label: 'Create Message' },
+    ];
+  }
+
+  @task
+  *save(event) {
+    event.preventDefault();
+    try {
+      const { isValid, state, invalidFormMessage } = this.args.message.validate();
+      this.modelValidations = isValid ? null : state;
+      this.invalidFormAlert = invalidFormMessage;
+
+      if (isValid) {
+        const { isNew } = this.args.message;
+
+        // We do these checks here since there could be a scenario where startTime and endTime are strings.
+        // The model expects these attrs to be a date object, so we will need to update these attrs to be in
+        // date object format.
+        if (typeof this.args.message.startTime === 'string')
+          this.args.message.startTime = new Date(this.args.message.startTime);
+        if (typeof this.args.message.endTime === 'string')
+          this.args.message.endTime = new Date(this.args.message.endTime);
+
+        const { id } = yield this.args.message.save();
+        this.flashMessages.success(`Successfully ${isNew ? 'created' : 'updated'} the message.`);
+        this.router.transitionTo('vault.cluster.config-ui.messages.message.details', id);
+      }
+    } catch (error) {
+      this.errorBanner = errorMessage(error);
+      this.invalidFormAlert = 'There was an error submitting this form.';
+    }
+  }
+}
