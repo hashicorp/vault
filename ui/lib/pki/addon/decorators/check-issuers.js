e101ea4ff40719c549e251faa1612c727a3dcb4f
Chelsea Shaw <cshaw@hashicorp.com>
2024-01-03 13:07:08 -0600

    clarify language on check-issuers decorator

--- ui/lib/pki/addon/decorators/check-issuers.js
+++ ui/lib/pki/addon/decorators/check-issuers.js
@@ -25,7 +25,7 @@ export function withConfig() {
     }
     return class CheckConfig extends SuperClass {
       @service secretMountPath;
-      shouldPromptConfig = false;
+      pkiMountHasConfig = false;
 
       async beforeModel() {
         super.beforeModel(...arguments);
@@ -35,9 +35,9 @@ export function withConfig() {
         return (
           this.store
             .query('pki/issuer', { backend: this.secretMountPath.currentPath })
-            .then(() => (this.shouldPromptConfig = true))
+            .then(() => (this.pkiMountHasConfig = true))
             // this endpoint is unauthenticated, so we're not worried about permissions errors
-            .catch(() => (this.shouldPromptConfig = false))
+            .catch(() => (this.pkiMountHasConfig = false))
         );
       }
     };
