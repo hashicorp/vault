de103c148bb888c9190e3a9b4b0df61f4a4bb7ea
Vinay Gopalan <vinay@hashicorp.com>
2024-01-17 14:18:49 -0800

    add identity token helper methods to sdk

---
+++ sdk/helper/pluginutil/identity_token.go
@@ -0,0 +1,35 @@
+// Copyright (c) HashiCorp, Inc.
+// SPDX-License-Identifier: MPL-2.0
+
+package pluginutil
+
+import (
+	"time"
+)
+
+type IdentityTokenRequest struct {
+	// Audience identifies the recipient of the token
+	Audience string
+	// TTL is the duration that the token will be valid for
+	TTL time.Duration
+}
+
+type IdentityTokenResponse struct {
+	// Token is the plugin identity token
+	Token IdentityToken
+	// TTL is the capped duration that the token is valid for
+	TTL time.Duration
+}
+
+type IdentityToken string
+
+// String returns a redacted token string. Use the Token() method
+// to obtain the non-redacted token contents.
+func (t IdentityToken) String() string {
+	return "ey***"
+}
+
+// Token returns the non-redacted token contents.
+func (t IdentityToken) Token() string {
+	return string(t)
+}
