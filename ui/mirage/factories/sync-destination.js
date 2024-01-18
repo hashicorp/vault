d0d66266c7804446c0f6a26801f422d9f54c2c1f
claire bontempo <68122737+hellobontempo@users.noreply.github.com>
2024-01-18 12:15:52 -0800

    Secrets Sync UI: Add sync destination fields custom_tags and secret_name_template (#24930)
    * leverage isSectionHeader option to change component styling
    
    * update destination models to include new params
    
    * update form and details template to accommodate new fields
    
    * remove extra horizontal line
    
    * move is-empty-value to core addon and use in details template
    
    * remove leftover or conditional
    
    * update mirage and tests
    
    * update form tests

--- ui/mirage/factories/sync-destination.js
+++ ui/mirage/factories/sync-destination.js
@@ -9,38 +9,56 @@ export default Factory.extend({
   ['aws-sm']: trait({
     type: 'aws-sm',
     name: 'destination-aws',
+    // connection_details
     access_key_id: '*****',
     secret_access_key: '*****',
     region: 'us-west-1',
+    // options
+    secret_name_template: 'vault-{{ .MountAccessor | replace "_" "-" }}-{{ .SecretPath }}',
+    custom_tags: { foo: 'bar' },
   }),
   ['azure-kv']: trait({
     type: 'azure-kv',
     name: 'destination-azure',
+    // connection_details
     key_vault_uri: 'https://keyvault-1234abcd.vault.azure.net',
     subscription_id: 'subscription-id',
     tenant_id: 'tenant-id',
     client_id: 'azure-client-id',
     client_secret: '*****',
     cloud: 'Azure Public Cloud',
+    // options
+    secret_name_template: 'vault-{{ .MountAccessor | replace "_" "-" }}-{{ .SecretPath }}',
+    custom_tags: { foo: 'bar' },
   }),
   ['gcp-sm']: trait({
     type: 'gcp-sm',
     name: 'destination-gcp',
+    // connection_details
     credentials: '*****',
+    // options
+    secret_name_template: 'vault-{{ .MountAccessor | replace "_" "-" }}-{{ .SecretPath }}',
+    custom_tags: { foo: 'bar' },
   }),
   gh: trait({
     type: 'gh',
     name: 'destination-gh',
+    // connection_details
     access_token: '*****',
     repository_owner: 'my-organization-or-username',
     repository_name: 'my-repository',
+    // options
+    secret_name_template: 'vault-{{ .MountAccessor | replace "_" "-" }}-{{ .SecretPath }}',
   }),
   ['vercel-project']: trait({
     type: 'vercel-project',
     name: 'destination-vercel',
+    // connection_details
     access_token: '*****',
     project_id: 'prj_12345',
     team_id: 'team_12345',
     deployment_environments: ['development', 'preview'], // 'production' is also an option, but left out for testing to assert form changes value
+    // options
+    secret_name_template: 'vault-{{ .MountAccessor | replace "_" "-" }}-{{ .SecretPath }}',
   }),
 });
