da0a3faf5c2d339bac9ac1df43aa132f67dd30fb
clairebontempo@gmail.com <clairebontempo@gmail.com>
2024-01-23 10:23:06 -0800

    update import

--- ui/app/components/clients/page/sync.ts
+++ ui/app/components/clients/page/sync.ts
@@ -3,12 +3,12 @@
  * SPDX-License-Identifier: BUSL-1.1
  */
 
-import CountsComponent from '../counts';
+import ActivityComponent from '../activity';
 import { calculateAverage } from 'vault/utils/chart-helpers';
 
 import type { MonthlyChartData } from 'vault/vault/charts/client-counts';
 
-export default class SyncComponent extends CountsComponent {
+export default class SyncComponent extends ActivityComponent {
   average = (data: MonthlyChartData[], key: string) => {
     return calculateAverage(data, key);
   };
