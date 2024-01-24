9f19e9869bed175be777c114232619cde2724f64
Hamid Ghaf <83242695+hghaf099@users.noreply.github.com>
2024-01-24 13:37:51 -0800

    manual licese reporting persist snapshots CE (#25021) (#25049)

--- vault/activity_log.go
+++ vault/activity_log.go
@@ -1146,7 +1146,7 @@ func (c *Core) setupActivityLogLocked(ctx context.Context, wg *sync.WaitGroup) e
 			close(manager.retentionDone)
 		}(manager.retentionMonths)
 
-		manager.CensusReportDone = make(chan bool)
+		manager.CensusReportDone = make(chan bool, 1)
 		go c.activityLog.CensusReport(ctx, c.CensusAgent(), c.BillingStart())
 	}
 
