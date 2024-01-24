ed801855559caf29861d351f5f8fa22e289721a0
Hamid Ghaf <83242695+hghaf099@users.noreply.github.com>
2024-01-24 12:29:33 -0800

    manual licese reporting persist snapshots CE (#25021)

--- vault/testing.go
+++ vault/testing.go
@@ -232,7 +232,6 @@ func TestCoreWithSealAndUINoCleanup(t testing.T, opts *CoreConfig) *Core {
 	conf.PluginDirectory = opts.PluginDirectory
 	conf.DetectDeadlocks = opts.DetectDeadlocks
 	conf.Experiments = []string{experiments.VaultExperimentEventsAlpha1}
-	conf.CensusAgent = opts.CensusAgent
 	conf.AdministrativeNamespacePath = opts.AdministrativeNamespacePath
 	conf.ImpreciseLeaseRoleTracking = opts.ImpreciseLeaseRoleTracking
 
