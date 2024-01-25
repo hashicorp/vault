91e1255cbce95be7b507ace900a88e734922a0d5
claire bontempo <68122737+hellobontempo@users.noreply.github.com>
2024-01-25 11:19:21 -0800

    Secrets Sync UI: Add secrets syncs to csv export (#25056)
    * update mirage and add sync clients to export csv
    
    * fix sync legend label
    
    * remove word
    
    * update copy in modal
    
    * update mirage
    
    * fix attribution tooltip text

--- ui/mirage/handlers/clients.js
+++ ui/mirage/handlers/clients.js
@@ -64,23 +64,27 @@ function generateNamespaceBlock(idx = 0, isLowerCounts = false, ns) {
     mounts: {},
   };
   const mounts = [];
-  const mountClients = randomBetween(min, max);
-  const [nonEntity, entity, secretSyncs] = arrayOfCounts(mountClients, 3);
-  // TODO does sys/ namespace only exist on 'root' namespace or child namespaces as well?
-  mounts.push({
-    mount_path: 'sys/',
-    counts: {
-      clients: secretSyncs,
-      entity_clients: 0,
-      non_entity_clients: 0,
-      distinct_entities: 0,
-      non_entity_tokens: 0,
-      secret_syncs: secretSyncs,
-    },
+
+  Array.from(Array(5)).forEach((mount, index) => {
+    const [secretSyncs] = arrayOfCounts(randomBetween(min, max), 1);
+    mounts.push({
+      mount_path: `kvv2-engine-${index}`,
+      counts: {
+        clients: secretSyncs,
+        // TODO test with live backend to confirm entity keys are present (and 0) for kv mounts
+        entity_clients: 0,
+        non_entity_clients: 0,
+        distinct_entities: 0,
+        non_entity_tokens: 0,
+        secret_syncs: secretSyncs,
+      },
+    });
   });
 
   // generate auth mounts array
   Array.from(Array(10)).forEach((mount, index) => {
+    const mountClients = randomBetween(min, max);
+    const [nonEntity, entity] = arrayOfCounts(mountClients, 2);
     mounts.push({
       mount_path: `auth/authid${index}`,
       counts: {
@@ -89,7 +93,7 @@ function generateNamespaceBlock(idx = 0, isLowerCounts = false, ns) {
         non_entity_clients: nonEntity,
         distinct_entities: entity,
         non_entity_tokens: nonEntity,
-        // TODO test with live backend to confirm this key is present (and 0) for auth mounts
+        // TODO test with live backend to confirm this key is present (and 0) for auth mounts (non-kv mounts)
         secret_syncs: 0,
       },
     });
