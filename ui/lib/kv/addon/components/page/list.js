7e881573e970f425710a9a898c668195d0171a88
Chelsea Shaw <cshaw@hashicorp.com>
2024-01-25 17:40:28 -0600

    replace popup-menu on kv list component

--- ui/lib/kv/addon/components/page/list.js
+++ ui/lib/kv/addon/components/page/list.js
@@ -30,6 +30,7 @@ export default class KvListPageComponent extends Component {
   @service store;
 
   @tracked secretPath;
+  @tracked deleteSecretModal = null; // set to the metadata intended to delete
 
   get mountPoint() {
     // mountPoint tells transition where to start. In this case, mountPoint will always be vault.cluster.secrets.backend.kv.
