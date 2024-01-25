9471a98a63db89d803da1d474cf124899ff57ea4
hamid ghaf <hamid@hashicorp.com>
2024-01-25 15:10:05 -0800

    Revert "backport of commit bc9be554cb4cc991a4e8d4bc806751cb4609c984 (#24851)"
    This reverts commit a622eb44aeb16ddfe2eff437c36605d28460fda8.

--- vault/activity_log.go
+++ vault/activity_log.go
@@ -1105,7 +1105,7 @@ func (c *Core) setupActivityLogLocked(ctx context.Context, wg *sync.WaitGroup) e
 	c.AddLogger(logger)
 
 	if os.Getenv("VAULT_DISABLE_ACTIVITY_LOG") != "" {
-		if c.ManualLicenseReportingEnabled() {
+		if c.CensusLicensingEnabled() {
 			logger.Warn("activity log disabled via environment variable while reporting is enabled. " +
 				"Reporting will override, and the activity log will be enabled")
 		} else {
