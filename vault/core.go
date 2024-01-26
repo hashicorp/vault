fab8268d5ff4be2498f6ab4eb7ff3c48ca06c1ce
Hamid Ghaf <83242695+hghaf099@users.noreply.github.com>
2024-01-26 07:32:01 -0800

    Revert licese reporting 1.15 (#25087)
    * Revert "backport of commit 0e227bf0d75bc680439ea3b8a080c0cccc700015 (#25065)"
    
    This reverts commit 41badae52952637df37031cfb2da09b560738e8a.
    
    * Revert "manual licese reporting persist snapshots CE (#25021) (#25051)"
    
    This reverts commit b43a3b30df2b5ed4eed357eec7435ad97841ecce.
    
    * Revert "backport of commit bc9be554cb4cc991a4e8d4bc806751cb4609c984 (#24851)"
    
    This reverts commit a622eb44aeb16ddfe2eff437c36605d28460fda8.

--- vault/core.go
+++ vault/core.go
@@ -649,6 +649,8 @@ type Core struct {
 	// it is protected by activityLogLock
 	activityLogConfig ActivityLogCoreConfig
 
+	censusConfig atomic.Value
+
 	// activeTime is set on active nodes indicating the time at which this node
 	// became active.
 	activeTime time.Time
@@ -828,6 +830,9 @@ type CoreConfig struct {
 	License         string
 	LicensePath     string
 	LicensingConfig *LicensingConfig
+
+	// Configured Census Agent
+	CensusAgent CensusReporter
 
 	DisablePerformanceStandby bool
 	DisableIndexing           bool
@@ -2461,10 +2466,8 @@ func (s standardUnsealStrategy) unseal(ctx context.Context, logger log.Logger, c
 			return err
 		}
 
-		if !c.perfStandby {
-			if err := c.setupCensusManager(); err != nil {
-				logger.Error("failed to instantiate the license reporting agent", "error", err)
-			}
+		if err := c.setupCensusAgent(); err != nil {
+			c.logger.Error("skipping reporting for nil agent", "error", err)
 		}
 
 		// not waiting on wg to avoid changing existing behavior
@@ -2472,11 +2475,6 @@ func (s standardUnsealStrategy) unseal(ctx context.Context, logger log.Logger, c
 		if err := c.setupActivityLog(ctx, &wg); err != nil {
 			return err
 		}
-
-		if !c.perfStandby {
-			c.StartManualCensusSnapshots()
-		}
-
 	} else {
 		var err error
 		disableEventLogger, err := parseutil.ParseBool(os.Getenv(featureFlagDisableEventLogger))
@@ -2727,8 +2725,8 @@ func (c *Core) preSeal() error {
 		result = multierror.Append(result, fmt.Errorf("error stopping expiration: %w", err))
 	}
 	c.stopActivityLog()
-	// Clean up census on seal
-	if err := c.teardownCensusManager(); err != nil {
+	// Clean up the censusAgent on seal
+	if err := c.teardownCensusAgent(); err != nil {
 		result = multierror.Append(result, fmt.Errorf("error tearing down reporting agent: %w", err))
 	}
 
