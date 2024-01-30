968dfdd73222112dd00ba8ca859d2a81596136d9
Kianna Quach <kianna.quach@hashicorp.com>
2024-01-30 10:05:11 -0800

    Focus search input

--- ui/lib/config-ui/addon/components/messages/page/list.js
+++ ui/lib/config-ui/addon/components/messages/page/list.js
@@ -10,6 +10,7 @@ import { dateFormat } from 'core/helpers/date-format';
 import { action } from '@ember/object';
 import { tracked } from '@glimmer/tracking';
 import errorMessage from 'vault/utils/error-message';
+import { next } from '@ember/runloop';
 
 /**
  * @module Page::MessagesList
@@ -29,6 +30,26 @@ export default class MessagesList extends Component {
   @service customMessages;
 
   @tracked showMaxMessageModal = false;
+
+  // This follows the pattern in secrets/page/destinations for FilterInput. Currently, FilterInput doesn't
+  // do a full page refresh causing it to lose focus. The work around is to verify that a transition from this route was
+  // completed and then focus the input.
+  constructor(owner, args) {
+    super(owner, args);
+    this.router.on('routeDidChange', this.focusNameFilter);
+  }
+
+  willDestroy() {
+    super.willDestroy();
+    this.router.off('routeDidChange', this.focusNameFilter);
+  }
+
+  focusNameFilter(transition) {
+    const route = 'vault.cluster.config-ui.messages.index';
+    if (transition?.from?.name === route && transition?.to?.name === route) {
+      next(() => document.getElementById('message-filter')?.focus());
+    }
+  }
 
   get formattedMessages() {
     return this.args.messages.map((message) => {
