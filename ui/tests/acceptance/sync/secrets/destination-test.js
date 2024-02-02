becb28221afb2036c4a1daed50960580c4da597a
Jordan Reimer <zofskeez@gmail.com>
2024-02-02 16:30:54 -0700

    Sync Acceptance Tests (#25208)
    * adds acceptance tests for sync
    
    * adds enterprise to sync acceptance test module names

--- ui/tests/acceptance/sync/secrets/destination-test.js
+++ ui/tests/acceptance/sync/secrets/destination-test.js
@@ -9,12 +9,10 @@ import { setupMirage } from 'ember-cli-mirage/test-support';
 import syncScenario from 'vault/mirage/scenarios/sync';
 import syncHandlers from 'vault/mirage/handlers/sync';
 import authPage from 'vault/tests/pages/auth';
-import { click, visit, currentURL } from '@ember/test-helpers';
-import { PAGE } from 'vault/tests/helpers/sync/sync-selectors';
+import { click, visit, currentURL, fillIn } from '@ember/test-helpers';
+import { PAGE as ts } from 'vault/tests/helpers/sync/sync-selectors';
 
-const { breadcrumbAtIdx } = PAGE;
-
-module('Acceptance | sync | destination', function (hooks) {
+module('Acceptance | enterprise | sync | destination', function (hooks) {
   setupApplicationTest(hooks);
   setupMirage(hooks);
 
@@ -26,11 +24,37 @@ module('Acceptance | sync | destination', function (hooks) {
 
   test('it should transition to overview route via breadcrumb', async function (assert) {
     await visit('vault/sync/secrets/destinations/aws-sm/destination-aws/secrets');
-    await click(breadcrumbAtIdx(0));
+    await click(ts.breadcrumbAtIdx(0));
     assert.strictEqual(
       currentURL(),
       '/vault/sync/secrets/overview',
       'Transitions to overview on breadcrumb click'
     );
+  });
+
+  test('it should transition to correct routes when performing actions', async function (assert) {
+    await click(ts.navLink('Secrets Sync'));
+    await click(ts.tab('Destinations'));
+    await click(ts.listItem);
+    assert.dom(ts.tab('Secrets')).hasClass('active', 'Secrets tab is active');
+
+    await click(ts.tab('Details'));
+    assert.dom(ts.infoRowLabel('Name')).exists('Destination details display');
+
+    await click(ts.toolbar('Sync secrets'));
+    await click(ts.destinations.sync.cancel);
+
+    await click(ts.toolbar('Edit destination'));
+    assert.dom(ts.inputByAttr('name')).isDisabled('Edit view renders with disabled name field');
+    await click(ts.cancelButton);
+    assert.dom(ts.tab('Details')).hasClass('active', 'Details view is active');
+  });
+
+  test('it should delete destination', async function (assert) {
+    await visit('vault/sync/secrets/destinations/aws-sm/destination-aws/details');
+    await click(ts.toolbar('Delete destination'));
+    await fillIn(ts.confirmModalInput, 'DELETE');
+    await click(ts.confirmButton);
+    assert.dom(ts.destinations.deleteBanner).exists('Delete banner renders');
   });
 });
