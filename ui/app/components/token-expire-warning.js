f617d4bd10aa74fb741ee76ef23fe9063bf38912
Chelsea Shaw <82459713+hashishaw@users.noreply.github.com>
2024-02-09 14:05:14 -0600

    Backport of UI: Redirect after logging in from token expiry (#25335) (#25345)
    * Redirect after logging in from token expiry. Fixes #10963
    
    * Add changelog

--- ui/app/components/token-expire-warning.js
+++ ui/app/components/token-expire-warning.js
@@ -9,6 +9,11 @@ import { inject as service } from '@ember/service';
 export default class TokenExpireWarning extends Component {
   @service router;
 
+  get queryParams() {
+    // Bring user back to current page after login
+    return { redirect_to: this.router.currentURL };
+  }
+
   get showWarning() {
     const currentRoute = this.router.currentRouteName;
     if ('vault.cluster.oidc-provider' === currentRoute) {
