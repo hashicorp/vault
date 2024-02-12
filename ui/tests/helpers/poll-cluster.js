e5a1555a6ff8cc31cbace22bbaec6aec49e17e0b
Angel Garbarino <Monkeychip@users.noreply.github.com>
2024-02-12 09:45:58 -0700

    seal changes (#25254)

--- ui/tests/helpers/poll-cluster.js
+++ ui/tests/helpers/poll-cluster.js
@@ -7,6 +7,6 @@ import { settled } from '@ember/test-helpers';
 
 export async function pollCluster(owner) {
   const store = owner.lookup('service:store');
-  await store.peekAll('cluster').firstObject.reload();
+  await store.peekAll('cluster')[0].reload();
   await settled();
 }
