88b07ec2339725a914f30a3d5bac9b80ad92989e
Chelsea Shaw <cshaw@hashicorp.com>
2024-02-14 13:26:00 -0600

    Skip fetching OpenAPI if the auth method is not managable in the UI. Fixes #24160

--- ui/app/routes/vault/cluster/access/method.js
+++ ui/app/routes/vault/cluster/access/method.js
@@ -7,6 +7,7 @@ import AdapterError from '@ember-data/adapter/error';
 import { set } from '@ember/object';
 import Route from '@ember/routing/route';
 import { service } from '@ember/service';
+import { supportedManagedAuthBackends } from 'vault/helpers/supported-managed-auth-backends';
 
 export default Route.extend({
   store: service(),
@@ -20,6 +21,15 @@ export default Route.extend({
         const error = new AdapterError();
         set(error, 'httpStatus', 404);
         throw error;
+      }
+      const supportManaged = supportedManagedAuthBackends().map((backend) => backend.type);
+      if (!supportManaged.includes(model.methodType)) {
+        // do not fetch path-help for unmanaged auth types
+        model.set('paths', {
+          apiPath: model.apiPath,
+          paths: [],
+        });
+        return model;
       }
       return this.pathHelp.getPaths(model.apiPath, path).then((paths) => {
         model.set('paths', paths);
