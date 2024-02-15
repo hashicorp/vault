dbe5ccabc286b8aadbbd942463329dfe78aadc8d
Chelsea Shaw <cshaw@hashicorp.com>
2024-02-15 15:23:26 -0600

    Cleanup + tests

--- ui/app/components/flash-toast.js
+++ ui/app/components/flash-toast.js
@@ -1,6 +1,15 @@
+/**
+ * Copyright (c) HashiCorp, Inc.
+ * SPDX-License-Identifier: BUSL-1.1
+ */
+
 import { capitalize } from '@ember/string';
 import Component from '@glimmer/component';
 
+/**
+ * FlashToast components are used to translate flash messages into toast notifications.
+ * Flash object passed should have a `type` and `message` property at minimum.
+ */
 export default class FlashToastComponent extends Component {
   get color() {
     switch (this.args.flash.type) {
@@ -8,8 +17,11 @@ export default class FlashToastComponent extends Component {
         return 'highlight';
       case 'danger':
         return 'critical';
-      default:
+      case 'warning':
+      case 'success':
         return this.args.flash.type;
+      default:
+        return 'neutral';
     }
   }
 
