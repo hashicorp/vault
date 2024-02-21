fc8f4bed7ebf10a5046311480f66ddaadd35f206
Peter Wilson <peter.wilson@hashicorp.com>
2024-02-21 11:23:12 +0000

    Lexographical ordering of audit related struct fields

--- audit/types.go
+++ audit/types.go
@@ -102,86 +102,86 @@ type FormatterConfig struct {
 
 // RequestEntry is the structure of a request audit log entry.
 type RequestEntry struct {
-	Time          string   `json:"time,omitempty"`
-	Type          string   `json:"type,omitempty"`
 	Auth          *Auth    `json:"auth,omitempty"`
-	Request       *Request `json:"request,omitempty"`
 	Error         string   `json:"error,omitempty"`
 	ForwardedFrom string   `json:"forwarded_from,omitempty"` // Populated in Enterprise when a request is forwarded
+	Request       *Request `json:"request,omitempty"`
+	Time          string   `json:"time,omitempty"`
+	Type          string   `json:"type,omitempty"`
 }
 
 // ResponseEntry is the structure of a response audit log entry.
 type ResponseEntry struct {
-	Time      string    `json:"time,omitempty"`
-	Type      string    `json:"type,omitempty"`
 	Auth      *Auth     `json:"auth,omitempty"`
-	Request   *Request  `json:"request,omitempty"`
-	Response  *Response `json:"response,omitempty"`
 	Error     string    `json:"error,omitempty"`
 	Forwarded bool      `json:"forwarded,omitempty"`
+	Time      string    `json:"time,omitempty"`
+	Type      string    `json:"type,omitempty"`
+	Request   *Request  `json:"request,omitempty"`
+	Response  *Response `json:"response,omitempty"`
 }
 
 type Request struct {
-	ID                            string                 `json:"id,omitempty"`
+	ClientCertificateSerialNumber string                 `json:"client_certificate_serial_number,omitempty"`
 	ClientID                      string                 `json:"client_id,omitempty"`
-	ReplicationCluster            string                 `json:"replication_cluster,omitempty"`
-	Operation                     logical.Operation      `json:"operation,omitempty"`
-	MountPoint                    string                 `json:"mount_point,omitempty"`
-	MountType                     string                 `json:"mount_type,omitempty"`
-	MountAccessor                 string                 `json:"mount_accessor,omitempty"`
-	MountRunningVersion           string                 `json:"mount_running_version,omitempty"`
-	MountRunningSha256            string                 `json:"mount_running_sha256,omitempty"`
-	MountClass                    string                 `json:"mount_class,omitempty"`
-	MountIsExternalPlugin         bool                   `json:"mount_is_external_plugin,omitempty"`
 	ClientToken                   string                 `json:"client_token,omitempty"`
 	ClientTokenAccessor           string                 `json:"client_token_accessor,omitempty"`
-	Namespace                     *Namespace             `json:"namespace,omitempty"`
-	Path                          string                 `json:"path,omitempty"`
 	Data                          map[string]interface{} `json:"data,omitempty"`
+	ID                            string                 `json:"id,omitempty"`
+	Headers                       map[string][]string    `json:"headers,omitempty"`
+	MountAccessor                 string                 `json:"mount_accessor,omitempty"`
+	MountClass                    string                 `json:"mount_class,omitempty"`
+	MountPoint                    string                 `json:"mount_point,omitempty"`
+	MountType                     string                 `json:"mount_type,omitempty"`
+	MountRunningVersion           string                 `json:"mount_running_version,omitempty"`
+	MountRunningSha256            string                 `json:"mount_running_sha256,omitempty"`
+	MountIsExternalPlugin         bool                   `json:"mount_is_external_plugin,omitempty"`
+	Namespace                     *Namespace             `json:"namespace,omitempty"`
+	Operation                     logical.Operation      `json:"operation,omitempty"`
+	Path                          string                 `json:"path,omitempty"`
 	PolicyOverride                bool                   `json:"policy_override,omitempty"`
 	RemoteAddr                    string                 `json:"remote_address,omitempty"`
 	RemotePort                    int                    `json:"remote_port,omitempty"`
-	WrapTTL                       int                    `json:"wrap_ttl,omitempty"`
-	Headers                       map[string][]string    `json:"headers,omitempty"`
-	ClientCertificateSerialNumber string                 `json:"client_certificate_serial_number,omitempty"`
+	ReplicationCluster            string                 `json:"replication_cluster,omitempty"`
 	RequestURI                    string                 `json:"request_uri,omitempty"`
+	WrapTTL                       int                    `json:"wrap_ttl,omitempty"`
 }
 
 type Response struct {
 	Auth                  *Auth                  `json:"auth,omitempty"`
-	MountPoint            string                 `json:"mount_point,omitempty"`
-	MountType             string                 `json:"mount_type,omitempty"`
+	Data                  map[string]interface{} `json:"data,omitempty"`
+	Headers               map[string][]string    `json:"headers,omitempty"`
 	MountAccessor         string                 `json:"mount_accessor,omitempty"`
-	MountRunningVersion   string                 `json:"mount_running_plugin_version,omitempty"`
-	MountRunningSha256    string                 `json:"mount_running_sha256,omitempty"`
 	MountClass            string                 `json:"mount_class,omitempty"`
 	MountIsExternalPlugin bool                   `json:"mount_is_external_plugin,omitempty"`
-	Secret                *Secret                `json:"secret,omitempty"`
-	Data                  map[string]interface{} `json:"data,omitempty"`
-	Warnings              []string               `json:"warnings,omitempty"`
+	MountPoint            string                 `json:"mount_point,omitempty"`
+	MountRunningSha256    string                 `json:"mount_running_sha256,omitempty"`
+	MountRunningVersion   string                 `json:"mount_running_plugin_version,omitempty"`
+	MountType             string                 `json:"mount_type,omitempty"`
 	Redirect              string                 `json:"redirect,omitempty"`
+	Secret                *Secret                `json:"secret,omitempty"`
 	WrapInfo              *ResponseWrapInfo      `json:"wrap_info,omitempty"`
-	Headers               map[string][]string    `json:"headers,omitempty"`
+	Warnings              []string               `json:"warnings,omitempty"`
 }
 
 type Auth struct {
-	ClientToken               string              `json:"client_token,omitempty"`
 	Accessor                  string              `json:"accessor,omitempty"`
+	ClientToken               string              `json:"client_token,omitempty"`
 	DisplayName               string              `json:"display_name,omitempty"`
-	Policies                  []string            `json:"policies,omitempty"`
-	TokenPolicies             []string            `json:"token_policies,omitempty"`
-	IdentityPolicies          []string            `json:"identity_policies,omitempty"`
-	ExternalNamespacePolicies map[string][]string `json:"external_namespace_policies,omitempty"`
-	NoDefaultPolicy           bool                `json:"no_default_policy,omitempty"`
-	PolicyResults             *PolicyResults      `json:"policy_results,omitempty"`
-	Metadata                  map[string]string   `json:"metadata,omitempty"`
-	NumUses                   int                 `json:"num_uses,omitempty"`
-	RemainingUses             int                 `json:"remaining_uses,omitempty"`
-	EntityID                  string              `json:"entity_id,omitempty"`
 	EntityCreated             bool                `json:"entity_created,omitempty"`
-	TokenType                 string              `json:"token_type,omitempty"`
-	TokenTTL                  int64               `json:"token_ttl,omitempty"`
+	EntityID                  string              `json:"entity_id,omitempty"`
+	ExternalNamespacePolicies map[string][]string `json:"external_namespace_policies,omitempty"`
+	IdentityPolicies          []string            `json:"identity_policies,omitempty"`
+	Metadata                  map[string]string   `json:"metadata,omitempty"`
+	NoDefaultPolicy           bool                `json:"no_default_policy,omitempty"`
+	NumUses                   int                 `json:"num_uses,omitempty"`
+	Policies                  []string            `json:"policies,omitempty"`
+	PolicyResults             *PolicyResults      `json:"policy_results,omitempty"`
+	RemainingUses             int                 `json:"remaining_uses,omitempty"`
+	TokenPolicies             []string            `json:"token_policies,omitempty"`
 	TokenIssueTime            string              `json:"token_issue_time,omitempty"`
+	TokenTTL                  int64               `json:"token_ttl,omitempty"`
+	TokenType                 string              `json:"token_type,omitempty"`
 }
 
 type PolicyResults struct {
@@ -201,11 +201,11 @@ type Secret struct {
 }
 
 type ResponseWrapInfo struct {
-	TTL             int    `json:"ttl,omitempty"`
-	Token           string `json:"token,omitempty"`
 	Accessor        string `json:"accessor,omitempty"`
-	CreationTime    string `json:"creation_time,omitempty"`
 	CreationPath    string `json:"creation_path,omitempty"`
+	CreationTime    string `json:"creation_time,omitempty"`
+	Token           string `json:"token,omitempty"`
+	TTL             int    `json:"ttl,omitempty"`
 	WrappedAccessor string `json:"wrapped_accessor,omitempty"`
 }
 
