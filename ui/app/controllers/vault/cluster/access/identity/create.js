863f0004bba1b7efb1e2f09402106f71d87684a7
Chelsea Shaw <cshaw@hashicorp.com>
2024-02-22 11:07:30 -0600

    use regular action instead of task for nav after save. Fixes this error:
    Global error: Uncaught TransitionAborted: TransitionAborted

--- ui/app/controllers/vault/cluster/access/identity/create.js
+++ ui/app/controllers/vault/cluster/access/identity/create.js
@@ -5,26 +5,28 @@
 
 import Controller from '@ember/controller';
 import { service } from '@ember/service';
-import { task } from 'ember-concurrency';
 
 export default Controller.extend({
   router: service(),
   showRoute: 'vault.cluster.access.identity.show',
   showTab: 'details',
-  navAfterSave: task(function* ({ saveType, model }) {
-    const isDelete = saveType === 'delete';
-    const type = model.get('identityType');
-    const listRoutes = {
-      'entity-alias': 'vault.cluster.access.identity.aliases.index',
-      'group-alias': 'vault.cluster.access.identity.aliases.index',
-      group: 'vault.cluster.access.identity.index',
-      entity: 'vault.cluster.access.identity.index',
-    };
-    const routeName = listRoutes[type];
-    if (!isDelete) {
-      yield this.router.transitionTo(this.showRoute, model.id, this.showTab);
-      return;
-    }
-    yield this.router.transitionTo(routeName);
-  }),
+
+  actions: {
+    navAfterSave({ saveType, model }) {
+      const isDelete = saveType === 'delete';
+      const type = model.identityType;
+      const listRoutes = {
+        'entity-alias': 'vault.cluster.access.identity.aliases.index',
+        'group-alias': 'vault.cluster.access.identity.aliases.index',
+        group: 'vault.cluster.access.identity.index',
+        entity: 'vault.cluster.access.identity.index',
+      };
+      if (!isDelete) {
+        this.router.transitionTo(this.showRoute, model.id, this.showTab);
+      } else {
+        const routeName = listRoutes[type];
+        this.router.transitionTo(routeName);
+      }
+    },
+  },
 });
