e62fc103eadb65f9ee092de25f32c1942c1cdd1e
clairebontempo@gmail.com <clairebontempo@gmail.com>
2024-02-23 10:40:20 -0800

    add default granularity depending on type

--- ui/app/models/sync/destination.js
+++ ui/app/models/sync/destination.js
@@ -42,9 +42,18 @@ export default class SyncDestinationModel extends Model {
         value: 'secret-key',
       },
     ],
-    defaultValue: 'secret-path',
+    fieldValue: 'defaultGranularity', // get/set granularity because default depends on type
   })
   granularity;
+
+  get defaultGranularity() {
+    if (this.granularity) return this.granularity;
+    return ['gh', 'vercel-project'].includes(this?.type) ? 'secret-key' : 'secret-path';
+  }
+
+  set defaultGranularity(value) {
+    this.granularity = value;
+  }
 
   // only present if delete action has been initiated
   @attr('string') purgeInitiatedAt;
