fa29ed4a194c499c9d2ede963583723ffd6c3463
Angel Garbarino <argarbarino@gmail.com>
2024-02-27 14:12:34 -0700

    add in waitUntils and replace date nows with uuids

--- ui/tests/acceptance/access/identity/entities/aliases/create-test.js
+++ ui/tests/acceptance/access/identity/entities/aliases/create-test.js
@@ -7,6 +7,7 @@ import { module, test } from 'qunit';
 import { settled } from '@ember/test-helpers';
 import { setupApplicationTest } from 'ember-qunit';
 import { testAliasCRUD, testAliasDeleteFromForm } from '../../_shared-alias-tests';
+import { v4 as uuidv4 } from 'uuid';
 import authPage from 'vault/tests/pages/auth';
 
 module('Acceptance | /access/identity/entities/aliases/add', function (hooks) {
@@ -19,14 +20,14 @@ module('Acceptance | /access/identity/entities/aliases/add', function (hooks) {
 
   test('it allows create, list, delete of an entity alias', async function (assert) {
     assert.expect(6);
-    const name = `alias-${Date.now()}`;
+    const name = `alias-${uuidv4()}`;
     await testAliasCRUD(name, 'entities', assert);
     await settled();
   });
 
   test('it allows delete from the edit form', async function (assert) {
     assert.expect(4);
-    const name = `alias-${Date.now()}`;
+    const name = `alias-${uuidv4()}`;
     await testAliasDeleteFromForm(name, 'entities', assert);
     await settled();
   });
