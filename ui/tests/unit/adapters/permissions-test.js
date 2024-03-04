67c7708f28a5d2cf06e444cb9b2302ab70e93c2e
Chelsea Shaw <cshaw@hashicorp.com>
2024-03-04 10:24:05 -0600

    Correctly call resultant-acl endpoint when user root is root

--- ui/tests/unit/adapters/permissions-test.js
+++ ui/tests/unit/adapters/permissions-test.js
@@ -36,4 +36,29 @@ module('Unit | Adapter | permissions', function (hooks) {
     });
     await adapter.query();
   });
+  test('it calls resultant-acl with the users root namespace when root', async function (assert) {
+    assert.expect(1);
+    const adapter = this.owner.lookup('adapter:permissions');
+    const nsService = this.owner.lookup('service:namespace');
+    const auth = this.owner.lookup('service:auth');
+    nsService.setNamespace('admin');
+    auth.setCluster('1');
+    auth.set('tokens', ['vault-_root_☃1']);
+    auth.setTokenData('vault-_root_☃1', { userRootNamespace: '', backend: { mountPath: 'token' } });
+
+    this.server.get('/sys/internal/ui/resultant-acl', (schema, request) => {
+      assert.strictEqual(
+        request.requestHeaders['X-Vault-Namespace'],
+        '',
+        'Namespace is users root not current path'
+      );
+      return {
+        data: {
+          exact_paths: {},
+          glob_paths: {},
+        },
+      };
+    });
+    await adapter.query();
+  });
 });
