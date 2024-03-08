bc1cea938cc92a3394609170fe1a7737d43e0ea1
Mia Epner <mia.epner@hashicorp.com>
2024-03-08 13:29:30 +0100

    fix stub

--- vault/core_stubs_oss.go
+++ vault/core_stubs_oss.go
@@ -99,4 +99,4 @@ func (c *Core) EntWaitUntilWALShipped(ctx context.Context, index uint64) bool {
 	return true
 }
 
-func (c *Core) SecretsSyncActivated() { return false }
+func (c *Core) SecretsSyncLicensedActivated() bool { return false }
