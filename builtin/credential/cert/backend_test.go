a14c60c989d7862edc1065e9b04b9f97f5a24e13
Gorbaty <sgorbaty@users.noreply.github.com>
2024-03-11 09:30:05 -0700

    Unit test
    This unit test uses a cert with ou, so I added a metadata test to it.

--- builtin/credential/cert/backend_test.go
+++ builtin/credential/cert/backend_test.go
@@ -1434,6 +1434,10 @@ func TestBackend_organizationalUnit_singleCert(t *testing.T) {
 			testAccStepLogin(t, connState),
 			testAccStepCert(t, "web", ca, "foo", allowed{organizational_units: "engineering,finance"}, false),
 			testAccStepLogin(t, connState),
+			testAccStepLoginWithMetadata(t, connState, "web", map[string]string{}, false),
+			testAccStepSetConfig(t, config{EnableIdentityAliasMetadata: true}, connState),
+			testAccStepReadConfig(t, config{EnableIdentityAliasMetadata: true}, connState),
+			testAccStepLoginWithMetadata(t, connState, "web", map[string]string{}, true),
 			testAccStepCert(t, "web", ca, "foo", allowed{organizational_units: "foo"}, false),
 			testAccStepLoginInvalid(t, connState),
 		},
@@ -1809,6 +1813,8 @@ func testAccStepLoginWithMetadata(t *testing.T, connState tls.ConnectionState, c
 			metadata["cert_name"] = certName
 			metadata["common_name"] = connState.PeerCertificates[0].Subject.CommonName
 			metadata["serial_number"] = connState.PeerCertificates[0].SerialNumber.String()
+			metadata["org"] = fmt.Sprint(connState.PeerCertificates[0].Subject.Organization)
+			metadata["org_unit"] = fmt.Sprint(connState.PeerCertificates[0].Subject.OrganizationalUnit)
 			metadata["subject_key_id"] = certutil.GetHexFormatted(connState.PeerCertificates[0].SubjectKeyId, ":")
 			metadata["authority_key_id"] = certutil.GetHexFormatted(connState.PeerCertificates[0].AuthorityKeyId, ":")
 
