78aa8a7ea3515a7136d04d429e97cbe44ad50d5d
Scott G. Miller <smiller@hashicorp.com>
2024-03-11 12:14:09 -0500

    return

--- vault/reload_seal_stubs_oss.go
+++ vault/reload_seal_stubs_oss.go
@@ -14,4 +14,5 @@ import (
 //go:generate go run github.com/hashicorp/vault/tools/stubmaker
 
 func (c *Core) reloadSealsEnt(secureRandomReader io.Reader, sealAccess Seal, logger hclog.Logger, shouldRewrap bool) error {
+	return nil
 }
