36ea8aab76a41cd0706502df50f775674fad7159
Peter Wilson <peter.wilson@hashicorp.com>
2024-03-11 10:52:22 +0000

    allows use of pre-hashed passwords with userpass backend

---
+++ builtin/credential/userpass/path_user_password_test.go
@@ -0,0 +1,63 @@
+// Copyright (c) HashiCorp, Inc.
+// SPDX-License-Identifier: BUSL-1.1
+
+package userpass
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+// TestUserPass_ParseHash ensures that we correctly validate password hashes that
+// conform to the bcrypt standard based on the prefix of the hash.
+func TestUserPass_ParseHash(t *testing.T) {
+	t.Parallel()
+
+	tests := map[string]struct {
+		input           string
+		isErrorExpected bool
+	}{
+		"spaces": {
+			input:           "         ",
+			isErrorExpected: true,
+		},
+		"jibberish": {
+			input:           "jibberfish",
+			isErrorExpected: true,
+		},
+		"non-ascii": {
+			input:           "$2a$qwerty",
+			isErrorExpected: false,
+		},
+		"truncation": {
+			input:           "$2b$qwerty",
+			isErrorExpected: false,
+		},
+		"php-only-fixed": {
+			input:           "$2y$qwerty",
+			isErrorExpected: false,
+		},
+		"php-only-existing": {
+			input:           "$2x$qwerty",
+			isErrorExpected: true,
+		},
+	}
+
+	for name, tc := range tests {
+		name := name
+		tc := tc
+		t.Run(name, func(t *testing.T) {
+			t.Parallel()
+
+			got, err := parsePasswordHash(tc.input)
+			switch {
+			case tc.isErrorExpected:
+				require.EqualError(t, err, "password_hash doesn't appear to be a valid bcrypt hash")
+			default:
+				require.NoError(t, err)
+				require.Equal(t, tc.input, string(got))
+			}
+		})
+	}
+}
