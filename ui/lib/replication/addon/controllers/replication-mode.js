425b6279b56f6ae1fcff28f80518c6e32f93c748
Chelsea Shaw <82459713+hashishaw@users.noreply.github.com>
2024-03-11 16:22:36 -0500

    UI: glimmerize replication page (#25838)

--- ui/lib/replication/addon/controllers/replication-mode.js
+++ ui/lib/replication/addon/controllers/replication-mode.js
@@ -7,17 +7,20 @@ import { alias } from '@ember/object/computed';
 import { service } from '@ember/service';
 import Controller from '@ember/controller';
 import { task, timeout } from 'ember-concurrency';
+import { waitFor } from '@ember/test-waiters';
 
 export default Controller.extend({
   router: service(),
   rm: service('replication-mode'),
   replicationMode: alias('rm.mode'),
-  waitForNewClusterToInit: task(function* (replicationMode) {
-    // waiting for the newly enabled cluster to init
-    // this ensures we don't hit a capabilities-self error, called in the model of the mode/index route
-    yield timeout(1000);
-    this.router.transitionTo('vault.cluster.replication.mode', replicationMode);
-  }),
+  waitForNewClusterToInit: task(
+    waitFor(function* (replicationMode) {
+      // waiting for the newly enabled cluster to init
+      // this ensures we don't hit a capabilities-self error, called in the model of the mode/index route
+      yield timeout(1000);
+      this.router.transitionTo('vault.cluster.replication.mode', replicationMode);
+    })
+  ),
   actions: {
     onEnable(replicationMode, mode) {
       if (replicationMode == 'dr' && mode === 'secondary') {
--- ui/tests/integration/components/replication-page-test.js
+++ ui/tests/integration/components/replication-page-test.js
@@ -5,11 +5,14 @@
 
 import { module, test } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
-import { render } from '@ember/test-helpers';
+import { render, settled, waitFor } from '@ember/test-helpers';
 import hbs from 'htmlbars-inline-precompile';
+import { setupMirage } from 'ember-cli-mirage/test-support';
 
 const MODEL = {
   replicationMode: 'dr',
+  dr: { mode: 'primary' },
+  performance: { mode: 'primary' },
   replicationAttrs: {
     mode: 'secondary',
     clusterId: '12ab',
@@ -19,6 +22,7 @@ const MODEL = {
 
 module('Integration | Component | replication-page', function (hooks) {
   setupRenderingTest(hooks);
+  setupMirage(hooks);
 
   hooks.beforeEach(function () {
     this.set('model', MODEL);
@@ -39,5 +43,30 @@ module('Integration | Component | replication-page', function (hooks) {
     this.set('model.replicationAttrs.mode', 'bootstrapping');
     await render(hbs`<ReplicationPage @model={{this.model}} />`);
     assert.dom('[data-test-layout-loading]').exists();
+  });
+
+  test.skip('it re-fetches data when replication mode changes', async function (assert) {
+    assert.expect(4);
+    this.server.get('sys/replication/:mode/status', (schema, req) => {
+      assert.strictEqual(
+        req.params.mode,
+        this.model.replicationMode,
+        `fetchStatus called with correct mode: ${this.model.replicationMode}`
+      );
+      return {
+        data: {
+          mode: 'primary',
+        },
+      };
+    });
+    await render(
+      hbs`<ReplicationPage @model={{this.model}} as |Page|><Page.header @showTabs={{true}} /></ReplicationPage>`
+    );
+    await waitFor('[data-test-replication-title]');
+    // Title has spaces and newlines, so we can't use hasText because it won't match exactly
+    assert.dom('[data-test-replication-title]').includesText('Disaster Recovery');
+    this.set('model', { ...MODEL, replicationMode: 'performance' });
+    await settled();
+    assert.dom('[data-test-replication-title]').includesText('Performance');
   });
 });
