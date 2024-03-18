49b30797438ee8f3b164471531de740feec19b43
clairebontempo@gmail.com <clairebontempo@gmail.com>
2024-03-18 11:21:57 -0700

    add k8 to list of managed auth methods

--- ui/app/helpers/supported-managed-auth-backends.js
+++ ui/app/helpers/supported-managed-auth-backends.js
@@ -5,6 +5,8 @@
 
 import { helper as buildHelper } from '@ember/component/helper';
 
+// The UI supports management of these auth methods (i.e. configuring roles or users)
+// otherwise only configuration of the method is supported.
 const MANAGED_AUTH_BACKENDS = ['cert', 'kubernetes', 'ldap', 'okta', 'radius', 'userpass'];
 
 export function supportedManagedAuthBackends() {
