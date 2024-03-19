32677f590137811a529e7ade2161fc7c88c54436
Scott G. Miller <smiller@hashicorp.com>
2024-03-19 12:09:22 -0500

    use accessor

--- command/server.go
+++ command/server.go
@@ -1828,7 +1828,7 @@ func (c *ServerCommand) configureSeals(ctx context.Context, config *server.Confi
 	if err != nil {
 		return nil, nil, fmt.Errorf("Error getting seal generation info: %v", err)
 	}
-	if !config.EnableMultiseal {
+	if !config.IsMultisealEnabled() {
 		if err == nil && existingSealGenerationInfo != nil && len(existingSealGenerationInfo.Seals) > 1 {
 			return nil, nil, fmt.Errorf("multi-seal is disabled but previous configuration had multiple seals.  re-enable and migrate to a single seal before disabling multi-seal")
 		}
