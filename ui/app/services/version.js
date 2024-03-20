7537a0081cbe5d32006e95c51ff2008e2c2cfa6e
clairebontempo@gmail.com <clairebontempo@gmail.com>
2024-03-20 13:03:40 -0700

    add secrets sync feature to version service

--- ui/app/services/version.js
+++ ui/app/services/version.js
@@ -42,6 +42,10 @@ export default class VersionService extends Service {
     return this.features.includes('Control Groups');
   }
 
+  get hasSecretsSync() {
+    return this.features.includes('secrets-sync');
+  }
+
   get versionDisplay() {
     if (!this.version) {
       return '';
