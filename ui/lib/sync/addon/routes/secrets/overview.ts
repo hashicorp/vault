78b46db0b4645a97acce6c4b908352a917e94551
Chelsea Shaw <cshaw@hashicorp.com>
2024-03-21 16:45:27 -0500

    redirect from sync if vault is managed

--- ui/lib/sync/addon/routes/secrets/overview.ts
+++ ui/lib/sync/addon/routes/secrets/overview.ts
@@ -7,10 +7,20 @@ import Route from '@ember/routing/route';
 import { service } from '@ember/service';
 import { hash } from 'rsvp';
 
+import type RouterService from '@ember/routing/router-service';
+import type FeatureFlagService from 'vault/services/feature-flag';
 import type StoreService from 'vault/services/store';
 
 export default class SyncSecretsOverviewRoute extends Route {
+  @service declare readonly router: RouterService;
   @service declare readonly store: StoreService;
+  @service declare readonly featureFlag: FeatureFlagService;
+
+  beforeModel(): void | Promise<unknown> {
+    if (this.featureFlag.managedNamespaceRoot !== null) {
+      this.router.transitionTo('vault.cluster.dashboard');
+    }
+  }
 
   async model() {
     const { activatedFeatures } = this.modelFor('secrets') as {
