1481f5244ccc69218584314dea530abf056918a9
Noelle Daley <noelledaley@users.noreply.github.com>
2024-03-21 14:27:20 -0700

    tests: fix usage-stats test

--- ui/tests/integration/components/clients/usage-stats-test.js
+++ ui/tests/integration/components/clients/usage-stats-test.js
@@ -50,28 +50,54 @@ module('Integration | Component | clients/usage-stats', function (hooks) {
       .hasText('10', 'non entity clients shows passed value');
   });
 
-  test('it renders with full totals data', async function (assert) {
-    this.set('counts', {
-      clients: 22,
-      entity_clients: 7,
-      non_entity_clients: 10,
-      secret_syncs: 5,
+  module('it renders with full totals data', function () {
+    // TODO need to figure out how to simulate this
+    test.todo('with secrets sync activated', async function (assert) {
+      this.set('counts', {
+        clients: 22,
+        entity_clients: 7,
+        non_entity_clients: 10,
+        secret_syncs: 5,
+      });
+
+      await render(hbs`<Clients::UsageStats @totalUsageCounts={{this.counts}} />`);
+
+      assert.dom('[data-test-stat-text]').exists({ count: 4 }, 'Renders 4 Stat texts');
+      assert
+        .dom('[data-test-stat-text="total-clients"] .stat-value')
+        .hasText('22', 'Total clients shows passed value');
+      assert
+        .dom('[data-test-stat-text="entity-clients"] .stat-value')
+        .hasText('7', 'entity clients shows passed value');
+      assert
+        .dom('[data-test-stat-text="non-entity-clients"] .stat-value')
+        .hasText('10', 'non entity clients shows passed value');
+      assert
+        .dom('[data-test-stat-text="secret-syncs"] .stat-value')
+        .hasText('5', 'secrets sync clients shows passed value');
     });
 
-    await render(hbs`<Clients::UsageStats @totalUsageCounts={{this.counts}} />`);
+    test('with secrets sync NOT activated', async function (assert) {
+      this.counts = {
+        clients: 22,
+        entity_clients: 7,
+        non_entity_clients: 10,
+        secret_syncs: 5,
+      };
 
-    assert.dom('[data-test-stat-text]').exists({ count: 4 }, 'Renders 3 Stat texts');
-    assert
-      .dom('[data-test-stat-text="total-clients"] .stat-value')
-      .hasText('22', 'Total clients shows passed value');
-    assert
-      .dom('[data-test-stat-text="entity-clients"] .stat-value')
-      .hasText('7', 'entity clients shows passed value');
-    assert
-      .dom('[data-test-stat-text="non-entity-clients"] .stat-value')
-      .hasText('10', 'non entity clients shows passed value');
-    assert
-      .dom('[data-test-stat-text="secret-syncs"] .stat-value')
-      .hasText('5', 'secrets sync clients shows passed value');
+      await render(hbs`<Clients::UsageStats @totalUsageCounts={{this.counts}} />`);
+
+      assert.dom('[data-test-stat-text]').exists({ count: 3 }, 'Renders 3 Stat texts');
+      assert
+        .dom('[data-test-stat-text="total-clients"] .stat-value')
+        .hasText('22', 'Total clients shows passed value');
+      assert
+        .dom('[data-test-stat-text="entity-clients"] .stat-value')
+        .hasText('7', 'entity clients shows passed value');
+      assert
+        .dom('[data-test-stat-text="non-entity-clients"] .stat-value')
+        .hasText('10', 'non entity clients shows passed value');
+      assert.dom('[data-test-stat-text="secret-syncs"] .stat-value').doesNotExist();
+    });
   });
 });
