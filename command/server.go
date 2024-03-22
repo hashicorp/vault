2f75032d14cb5a4684842f2232222997ab6a3afc
hc-github-team-secure-vault-core <82990506+hc-github-team-secure-vault-core@users.noreply.github.com>
2024-03-22 10:40:46 -0600

    backport of commit f7301bc5fa965c39bbc807a862a7b8be02c4635f (#26109)
    Co-authored-by: Rachel Culpepper <84159930+rculpepper@users.noreply.github.com>

--- command/server.go
+++ command/server.go
@@ -3403,6 +3403,9 @@ func (c *ServerCommand) reloadSeals(ctx context.Context, grabStateLock bool, cor
 
 	currentConfig := core.GetCoreConfigInternal()
 
+	// we need to persist seal information if multiseal is being enabled
+	addEnableMultiseal := !currentConfig.IsMultisealEnabled() && newConfig.IsMultisealEnabled()
+
 	if core.SealAccess().BarrierSealConfigType() == vault.SealConfigTypeShamir {
 		switch {
 		case len(newConfig.Seals) == 0:
@@ -3424,7 +3427,7 @@ func (c *ServerCommand) reloadSeals(ctx context.Context, grabStateLock bool, cor
 		}
 	}
 
-	if cmp.Equal(currentConfig.Seals, newConfig.Seals) {
+	if cmp.Equal(currentConfig.Seals, newConfig.Seals) && !addEnableMultiseal {
 		return false, nil
 	}
 
