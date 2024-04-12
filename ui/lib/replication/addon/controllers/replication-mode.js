e81a89d0b208f3827f96e5904afedaae06648caa
Chelsea Shaw <cshaw@hashicorp.com>
2024-04-12 09:36:19 -0500

    Glimmerize replication controllers

--- ui/lib/replication/addon/controllers/replication-mode.js
+++ ui/lib/replication/addon/controllers/replication-mode.js
@@ -3,36 +3,41 @@
  * SPDX-License-Identifier: BUSL-1.1
  */
 
-import { alias } from '@ember/object/computed';
 import { service } from '@ember/service';
 import Controller from '@ember/controller';
 import { task, timeout } from 'ember-concurrency';
 import { waitFor } from '@ember/test-waiters';
+import { action } from '@ember/object';
 
-export default Controller.extend({
-  router: service(),
-  rm: service('replication-mode'),
-  replicationMode: alias('rm.mode'),
-  waitForNewClusterToInit: task(
-    waitFor(function* (replicationMode) {
-      // waiting for the newly enabled cluster to init
-      // this ensures we don't hit a capabilities-self error, called in the model of the mode/index route
-      yield timeout(1000);
+export default class ReplicationModeBaseController extends Controller {
+  @service() router;
+  @service('replication-mode') rm;
+
+  get replicationMode() {
+    return this.rm.mode;
+  }
+
+  @task
+  @waitFor
+  *waitForNewClusterToInit(replicationMode) {
+    // waiting for the newly enabled cluster to init
+    // this ensures we don't hit a capabilities-self error, called in the model of the mode/index route
+    yield timeout(1000);
+    this.router.transitionTo('vault.cluster.replication.mode', replicationMode);
+  }
+
+  @action
+  onEnable(replicationMode, mode) {
+    if (replicationMode == 'dr' && mode === 'secondary') {
+      this.router.transitionTo('vault.cluster');
+    } else if (replicationMode === 'dr') {
       this.router.transitionTo('vault.cluster.replication.mode', replicationMode);
-    })
-  ),
-  actions: {
-    onEnable(replicationMode, mode) {
-      if (replicationMode == 'dr' && mode === 'secondary') {
-        this.router.transitionTo('vault.cluster');
-      } else if (replicationMode === 'dr') {
-        this.router.transitionTo('vault.cluster.replication.mode', replicationMode);
-      } else {
-        this.waitForNewClusterToInit.perform(replicationMode);
-      }
-    },
-    onDisable() {
-      this.router.transitionTo('vault.cluster.replication.index');
-    },
-  },
-});
+    } else {
+      this.waitForNewClusterToInit.perform(replicationMode);
+    }
+  }
+  @action
+  onDisable() {
+    this.router.transitionTo('vault.cluster.replication.index');
+  }
+}
