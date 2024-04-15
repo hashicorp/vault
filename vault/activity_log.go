966e70037b19c347312bac7c8988b0e02506dbbb
hc-github-team-secure-vault-core <82990506+hc-github-team-secure-vault-core@users.noreply.github.com>
2024-04-15 18:30:18 -0400

    Backport of adding retention months to census reports CE into release/1.14.x (#26424)
    * adding retention months to census reports CE (#26405)
    
    * adding retention months to census reports CE changes
    
    * using stubmaker
    
    * make fmt
    
    * change reload census agent to reload census
    
    * remove stubmaker file
    
    ---------
    
    Co-authored-by: akshya96 <87045294+akshya96@users.noreply.github.com>
    Co-authored-by: akshya96 <araghavan@hashicorp.com>

--- vault/activity_log.go
+++ vault/activity_log.go
@@ -1160,9 +1160,6 @@ func (c *Core) setupActivityLogLocked(ctx context.Context, wg *sync.WaitGroup) e
 			manager.retentionWorker(ctx, manager.clock.Now(), months)
 			close(manager.retentionDone)
 		}(manager.retentionMonths)
-
-		manager.CensusReportDone = make(chan bool, 1)
-		go c.activityLog.CensusReport(ctx, c.CensusAgent(), c.BillingStart())
 	}
 
 	return nil
