c536a8071e749eee48f1b29c03b1a5a0952143b4
Peter Wilson <peter.wilson@hashicorp.com>
2024-04-18 13:37:28 +0100

    update enableAudit ordering

--- vault/audit.go
+++ vault/audit.go
@@ -63,7 +63,7 @@ func (c *Core) generateAuditTestProbe() (*logical.LogInput, error) {
 	}, nil
 }
 
-// enableAudit is used to enable a new audit backend
+// enableAudit is used to enable a new audit backend that didn't exist in storage beforehand.
 func (c *Core) enableAudit(ctx context.Context, entry *MountEntry, updateStorage bool) error {
 	// Check ahead of time if the type of audit device we're trying to enable is configured in Vault.
 	if _, ok := c.auditBackends[entry.Type]; !ok {
@@ -85,17 +85,6 @@ func (c *Core) enableAudit(ctx context.Context, entry *MountEntry, updateStorage
 		return fmt.Errorf("backend path must be specified: %w", audit.ErrExternalOptions)
 	}
 
-	if fallbackRaw, ok := entry.Options["fallback"]; ok {
-		fallback, err := parseutil.ParseBool(fallbackRaw)
-		if err != nil {
-			return fmt.Errorf("cannot parse supplied 'fallback' setting: %w", audit.ErrExternalOptions)
-		}
-
-		// Reassigning the fallback value means we can ensure that the formatting
-		// of it as a string is consistent for future comparisons.
-		entry.Options["fallback"] = strconv.FormatBool(fallback)
-	}
-
 	if skipTestRaw, ok := entry.Options["skip_test"]; ok {
 		skipTest, err := parseutil.ParseBool(skipTestRaw)
 		if err != nil {
@@ -107,15 +96,19 @@ func (c *Core) enableAudit(ctx context.Context, entry *MountEntry, updateStorage
 		entry.Options["skip_test"] = strconv.FormatBool(skipTest)
 	}
 
-	// Update the audit table
+	ns, err := namespace.FromContext(ctx)
+	if err != nil {
+		return fmt.Errorf("%w: %w", audit.ErrInternal, err)
+	}
+	entry.NamespaceID = ns.ID
+	entry.namespace = ns
+
 	c.auditLock.Lock()
 	defer c.auditLock.Unlock()
 
 	// Look for matching name
 	for _, ent := range c.audit.Entries {
 		switch {
-		case entry.Options["fallback"] == "true" && ent.Options["fallback"] == "true":
-			return fmt.Errorf("fallback device already exists '%s': %w", ent.Path, audit.ErrExternalOptions)
 		// Existing is sql/mysql/ new is sql/ or
 		// existing is sql/ and new is sql/mysql/
 		case strings.HasPrefix(ent.Path, entry.Path):
@@ -142,14 +135,12 @@ func (c *Core) enableAudit(ctx context.Context, entry *MountEntry, updateStorage
 	}
 	viewPath := entry.ViewPath()
 	view := NewBarrierView(c.barrier, viewPath)
-	addAuditPathChecker(c, entry, view, viewPath)
-	origViewReadOnlyErr := view.getReadOnlyErr()
 
 	// Mark the view as read-only until the mounting is complete and
 	// ensure that it is reset after. This ensures that there will be no
 	// writes during the construction of the backend.
 	view.setReadOnlyErr(logical.ErrSetupReadOnly)
-	defer view.setReadOnlyErr(origViewReadOnlyErr)
+	defer view.setReadOnlyErr(view.getReadOnlyErr())
 
 	// Lookup the new backend
 	backend, err := c.newAuditBackend(entry, view, entry.Options)
@@ -157,7 +148,7 @@ func (c *Core) enableAudit(ctx context.Context, entry *MountEntry, updateStorage
 		return err
 	}
 	if backend == nil {
-		return fmt.Errorf("nil audit backend of type %q returned from factory: %w", entry.Type, audit.ErrInternal)
+		return fmt.Errorf("nil audit backend of type %q: %w", entry.Type, audit.ErrInternal)
 	}
 
 	if entry.Options["skip_test"] != "true" {
@@ -174,30 +165,27 @@ func (c *Core) enableAudit(ctx context.Context, entry *MountEntry, updateStorage
 		}
 	}
 
+	// Now that we're happy that the backend has been created correctly, we can
+	// try to register the backend with the audit broker.
+	// An error at this point doesn't cause issues anywhere outside the broker.
+	err = c.auditBroker.Register(backend, entry.Local)
+	if err != nil {
+		return fmt.Errorf("failed to register %q audit backend %q: %w", entry.Type, entry.Path, err)
+	}
+
+	// Update a copy of the mount table, we will swap out the oneCore has access to
+	// for this one unless there is a problem persisting storage (if required).
 	newTable := c.audit.shallowClone()
 	newTable.Entries = append(newTable.Entries, entry)
-
-	ns, err := namespace.FromContext(ctx)
-	if err != nil {
-		return fmt.Errorf("%w: %w", audit.ErrInternal, err)
-	}
-	entry.NamespaceID = ns.ID
-	entry.namespace = ns
-
 	if updateStorage {
 		if err := c.persistAudit(ctx, newTable, entry.Local); err != nil {
 			return fmt.Errorf("failed to update audit table: %w: %w", audit.ErrInternal, err)
 		}
 	}
 
+	// Wrap things up, add the path checker, swap the table, log our success.
+	addAuditPathChecker(c, entry, view, viewPath)
 	c.audit = newTable
-
-	// Register the backend
-	err = c.auditBroker.Register(backend, entry.Local)
-	if err != nil {
-		return fmt.Errorf("failed to register %q audit backend %q: %w", entry.Type, entry.Path, err)
-	}
-
 	c.logger.Info("enabled audit backend", "path", entry.Path, "type", entry.Type)
 	return nil
 }
@@ -444,15 +432,13 @@ func (c *Core) setupAudits(ctx context.Context) error {
 		// Create a barrier view using the UUID
 		viewPath := entry.ViewPath()
 		view := NewBarrierView(c.barrier, viewPath)
-		addAuditPathChecker(c, entry, view, viewPath)
-		origViewReadOnlyErr := view.getReadOnlyErr()
 
 		// Mark the view as read-only until the mounting is complete and
 		// ensure that it is reset after. This ensures that there will be no
 		// writes during the construction of the backend.
 		view.setReadOnlyErr(logical.ErrSetupReadOnly)
 		c.postUnsealFuncs = append(c.postUnsealFuncs, func() {
-			view.setReadOnlyErr(origViewReadOnlyErr)
+			view.setReadOnlyErr(view.getReadOnlyErr())
 		})
 
 		// Initialize the backend
@@ -473,6 +459,7 @@ func (c *Core) setupAudits(ctx context.Context) error {
 			continue
 		}
 
+		addAuditPathChecker(c, entry, view, viewPath)
 		successCount++
 	}
 
