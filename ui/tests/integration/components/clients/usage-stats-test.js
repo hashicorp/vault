cb9629107ea8176689795f048e8f5821471be99c
clairebontempo@gmail.com <clairebontempo@gmail.com>
2024-04-19 15:05:21 -0700

    add test coverage for description and title

--- ui/tests/integration/components/clients/usage-stats-test.js
+++ ui/tests/integration/components/clients/usage-stats-test.js
@@ -14,12 +14,14 @@ module('Integration | Component | clients/usage-stats', function (hooks) {
   test('it renders', async function (assert) {
     await render(
       hbs`
-        <Clients::UsageStats @title="My stats">
+        <Clients::UsageStats @title="My stats" @description="a very important description">
          yielded content!
         </Clients::UsageStats>`
     );
 
     assert.dom('[data-test-usage-stats="My stats"]').exists();
+    assert.dom('h3').hasText('My stats', 'title renders in h3 tag');
+    assert.dom('p').hasText('a very important description', 'description renders in p tag');
     assert
       .dom('[data-test-usage-stats="My stats"]')
       .hasTextContaining('yielded content!', 'it renders yielded content');
