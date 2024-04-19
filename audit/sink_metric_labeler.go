244b4998a014a95122b92afe06ebf381439e6cba
Peter Wilson <peter.wilson@hashicorp.com>
2024-04-19 14:26:15 +0100

    Update metric labeler for CE/Ent (#26537)

--- audit/sink_metric_labeler.go
+++ audit/sink_metric_labeler.go
@@ -8,25 +8,16 @@ import (
 	"github.com/hashicorp/vault/internal/observability/event"
 )
 
-var (
-	_ event.Labeler = (*metricLabelerAuditSink)(nil)
-	_ event.Labeler = (*metricLabelerAuditFallback)(nil)
-)
+var _ event.Labeler = (*metricLabelerAuditSink)(nil)
 
 var (
-	metricLabelAuditSinkSuccess     = []string{"audit", "sink", "success"}
-	metricLabelAuditSinkFailure     = []string{"audit", "sink", "failure"}
-	metricLabelAuditFallbackSuccess = []string{"audit", "fallback", "success"}
-	metricLabelAuditFallbackMiss    = []string{"audit", "fallback", "miss"}
+	metricLabelAuditSinkSuccess = []string{"audit", "sink", "success"}
+	metricLabelAuditSinkFailure = []string{"audit", "sink", "failure"}
 )
 
 // metricLabelerAuditSink can be used to provide labels for the success or failure
 // of a sink node used for a normal audit device.
 type metricLabelerAuditSink struct{}
-
-// metricLabelerAuditFallback can be used to provide labels for the success or failure
-// of a sink node used for an audit fallback device.
-type metricLabelerAuditFallback struct{}
 
 // Labels provides the success and failure labels for an audit sink, based on the error supplied.
 // Success: 'vault.audit.sink.success'
@@ -37,21 +28,4 @@ func (m metricLabelerAuditSink) Labels(_ *eventlogger.Event, err error) []string
 	}
 
 	return metricLabelAuditSinkSuccess
-}
-
-// Labels provides the success and failures labels for an audit fallback sink, based on the error supplied.
-// Success: 'vault.audit.fallback.success'
-// Failure: 'vault.audit.sink.failure'
-func (m metricLabelerAuditFallback) Labels(_ *eventlogger.Event, err error) []string {
-	if err != nil {
-		return metricLabelAuditSinkFailure
-	}
-
-	return metricLabelAuditFallbackSuccess
-}
-
-// MetricLabelsFallbackMiss returns the labels which indicate an audit entry was missed.
-// 'vault.audit.fallback.miss'
-func MetricLabelsFallbackMiss() []string {
-	return metricLabelAuditFallbackMiss
 }
