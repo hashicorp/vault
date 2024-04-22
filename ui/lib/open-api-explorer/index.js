f08c91dfa57dd9c6edf02ee3dcaef8e975143104
Noelle Daley <noelledaley@users.noreply.github.com>
2024-04-22 12:03:44 -0700

    [Embroider] fix imports to unblock Embroider adoption (#26296)
    * chore: use ember-cli-babel for TS compliation instead of ember-cli-typescript
    
    * chore: fix import paths to be ES module compatible
    
    * chore(test): fix setupControlGroup helper import
    
    * chore: fix engine definitions and swagger-ui imports
    
    * update lockfile
    
    * chore: remove babel inline-json-import
    
    ---------
    
    Co-authored-by: Matthew Irish <matthew@hashicorp.com>

--- ui/lib/open-api-explorer/index.js
+++ ui/lib/open-api-explorer/index.js
@@ -13,9 +13,6 @@ const EngineAddon = require('ember-engines/lib/engine-addon');
 module.exports = EngineAddon.extend({
   name: 'open-api-explorer',
 
-  babel: {
-    plugins: [require.resolve('ember-auto-import/babel-plugin')],
-  },
   included() {
     this._super.included && this._super.included.apply(this, arguments);
     // we want to lazy load the CSS deps, importing them here will result in them being added to the
