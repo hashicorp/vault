1f3034a9c2b274b8aaf0a859e699e102041678b2
Noelle Daley <noelledaley@users.noreply.github.com>
2024-04-23 18:21:57 -0700

    app-footer: migrate to TS

--- ui/app/components/app-footer.ts
+++ ui/app/components/app-footer.ts
@@ -3,12 +3,13 @@
  * SPDX-License-Identifier: BUSL-1.1
  */
 
-import { service } from '@ember/service';
 import Component from '@glimmer/component';
+import { service } from '@ember/service';
 import ENV from 'vault/config/environment';
+import type VersionService from 'vault/services/version';
 
 export default class AppFooterComponent extends Component {
-  @service version;
+  @service declare readonly version: VersionService;
 
   get isDevelopment() {
     return ENV.environment === 'development';
