1a81739cc313268932ee1c46974ab2448581ac51
thegatsbylofiexperience <thegatsbylofiexperience@users.noreply.github.com>
2024-04-30 05:56:26 +1000

    Add canonicalArn as a entity alias name (#22460)
    * Add canonicalArn as a entity alias name
    * Add Canonical Arn to iam_alias documentation

--- builtin/credential/aws/path_login.go
+++ builtin/credential/aws/path_login.go
@@ -1396,6 +1396,8 @@ func (b *backend) pathLoginUpdateIam(ctx context.Context, req *logical.Request, 
 		identityAlias = callerUniqueId
 	case identityAliasIAMFullArn:
 		identityAlias = callerID.Arn
+	case identityAliasIAMCanonicalArn:
+		identityAlias = entity.canonicalArn()
 	}
 
 	// If we're just looking up for MFA, return the Alias info
