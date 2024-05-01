b76ec381bdc2823c0b2c387ca71c626af22321fd
kitography <khaines@mit.edu>
2024-05-01 13:39:26 -0400

    Fix error print logic; don't enforce on root certificate generation; break correctly.

--- sdk/helper/certutil/helpers.go
+++ sdk/helper/certutil/helpers.go
@@ -923,19 +923,6 @@ func createCertificate(data *CreationBundle, randReader io.Reader, privateKeyGen
 		IPAddresses:    data.Params.IPAddresses,
 		URIs:           data.Params.URIs,
 	}
-	permittedDnsNames := data.SigningBundle.Certificate.PermittedDNSDomains
-	if permittedDnsNames != nil && len(permittedDnsNames) > 0 {
-		for _, name := range certTemplate.DNSNames {
-			for _, permittedName := range permittedDnsNames {
-				if name == permittedName || strings.HasSuffix(name, permittedName) {
-					break
-				}
-			}
-			return nil, errutil.InternalError{Err: fmt.Errorf("error matching DNS Name {{name}} to permitted Names {{names}}", name, permittedDnsNames).Error()}
-		}
-	}
-	// TODO: Remove This; Force an Error for now to see where tests use this function
-	return nil, errutil.InternalError{Err: fmt.Errorf("error, so I can see what calls this in the tests suite").Error()}
 
 	if data.Params.NotBeforeDuration > 0 {
 		certTemplate.NotBefore = time.Now().Add(-1 * data.Params.NotBeforeDuration)
@@ -1022,6 +1009,19 @@ func createCertificate(data *CreationBundle, randReader io.Reader, privateKeyGen
 	}
 
 	if data.SigningBundle != nil {
+		permittedDnsNames := data.SigningBundle.Certificate.PermittedDNSDomains
+		if permittedDnsNames != nil && len(permittedDnsNames) > 0 {
+		NameLoop:
+			for _, name := range certTemplate.DNSNames {
+				for _, permittedName := range permittedDnsNames {
+					if name == permittedName || strings.HasSuffix(name, permittedName) {
+						break NameLoop
+					}
+				}
+				return nil, errutil.InternalError{Err: fmt.Errorf("error matching DNS Name %s to permitted names %v", name, permittedDnsNames).Error()}
+			}
+		}
+
 		if (len(data.SigningBundle.Certificate.AuthorityKeyId) > 0 &&
 			!bytes.Equal(data.SigningBundle.Certificate.AuthorityKeyId, data.SigningBundle.Certificate.SubjectKeyId)) ||
 			data.Params.ForceAppendCaChain {
