65421074568c0a21c98761c005646976e4b8e2fb
clairebontempo@gmail.com <clairebontempo@gmail.com>
2024-05-01 16:58:28 +0100

    update component tests

--- ui/app/components/dashboard/client-count-card.js
+++ ui/app/components/dashboard/client-count-card.js
@@ -10,6 +10,7 @@ import { waitFor } from '@ember/test-waiters';
 import { tracked } from '@glimmer/tracking';
 import { service } from '@ember/service';
 import { setStartTimeQuery } from 'core/utils/client-count-utils';
+import { dateFormat } from 'core/helpers/date-format';
 
 /**
  * @module DashboardClientCountCard
@@ -17,8 +18,9 @@ import { setStartTimeQuery } from 'core/utils/client-count-utils';
  *
  * @example
  *
- * <Dashboard::ClientCountCard />
+ * <Dashboard::ClientCountCard @isEnterprise={{@version.isEnterprise}} />
  *
+ * @param {boolean} isEnterprise - used for setting the start time for the activity log query
  */
 
 export default class DashboardClientCountCard extends Component {
@@ -36,6 +38,18 @@ export default class DashboardClientCountCard extends Component {
 
   get currentMonthActivityTotalCount() {
     return this.activityData?.byMonth?.lastObject?.new_clients.clients;
+  }
+
+  get statSubText() {
+    const format = (date) => dateFormat([date, 'MMM yyyy'], {});
+    return this.licenseStartTime
+      ? {
+          total: `The number of clients in this billing period (${format(this.licenseStartTime)} - ${format(
+            this.updatedAt
+          )}).`,
+          new: 'The number of clients new to Vault in the current month.',
+        }
+      : { total: 'No total client data available.', new: 'No new client data available.' };
   }
 
   @task
--- ui/tests/integration/components/dashboard/client-count-card-test.js
+++ ui/tests/integration/components/dashboard/client-count-card-test.js
@@ -13,6 +13,7 @@ import { LICENSE_START, STATIC_NOW } from 'vault/mirage/handlers/clients';
 import timestamp from 'core/utils/timestamp';
 import { ACTIVITY_RESPONSE_STUB } from 'vault/tests/helpers/clients/client-count-helpers';
 import { formatNumber } from 'core/helpers/format-number';
+import { CLIENT_COUNT } from 'vault/tests/helpers/clients/client-count-selectors';
 
 module('Integration | Component | dashboard/client-count-card', function (hooks) {
   setupRenderingTest(hooks);
@@ -22,18 +23,12 @@ module('Integration | Component | dashboard/client-count-card', function (hooks)
     sinon.stub(timestamp, 'now').callsFake(() => STATIC_NOW);
   });
 
-  hooks.beforeEach(function () {
-    this.license = {
-      startTime: LICENSE_START.toISOString(),
-    };
-  });
-
   hooks.after(function () {
     timestamp.now.restore();
   });
 
   test('it should display client count information', async function (assert) {
-    assert.expect(9);
+    assert.expect(6);
     const { months, total } = ACTIVITY_RESPONSE_STUB;
     const [latestMonth] = months.slice(-1);
     this.server.get('sys/internal/counters/activity', () => {
@@ -44,24 +39,63 @@ module('Integration | Component | dashboard/client-count-card', function (hooks)
         data: ACTIVITY_RESPONSE_STUB,
       };
     });
+    this.server.get('sys/internal/counters/config', function () {
+      assert.true(true, 'sys/internal/counters/config');
+      return {
+        request_id: 'some-config-id',
+        data: {
+          billing_start_timestamp: LICENSE_START.toISOString(),
+        },
+      };
+    });
 
-    await render(hbs`<Dashboard::ClientCountCard @license={{this.license}} />`);
+    await render(hbs`<Dashboard::ClientCountCard @isEnterprise={{true}} />`);
     assert.dom('[data-test-client-count-title]').hasText('Client count');
-    assert.dom('[data-test-stat-text="total-clients"] .stat-label').hasText('Total');
     assert
-      .dom('[data-test-stat-text="total-clients"] .stat-text')
-      .hasText('The number of clients in this billing period (Jul 2023 - Jan 2024).');
+      .dom(CLIENT_COUNT.statText('Total'))
+      .hasText(
+        `Total The number of clients in this billing period (Jul 2023 - Jan 2024). ${formatNumber([
+          total.clients,
+        ])}`
+      );
+
     assert
-      .dom('[data-test-stat-text="total-clients"] .stat-value')
-      .hasText(`${formatNumber([total.clients])}`);
-    assert.dom('[data-test-stat-text="new-clients"] .stat-label').hasText('New');
-    assert
-      .dom('[data-test-stat-text="new-clients"] .stat-text')
-      .hasText('The number of clients new to Vault in the current month.');
-    assert
-      .dom('[data-test-stat-text="new-clients"] .stat-value')
-      .hasText(`${formatNumber([latestMonth.new_clients.counts.clients])}`);
+      .dom(CLIENT_COUNT.statText('New'))
+      .hasText(
+        `New The number of clients new to Vault in the current month. ${formatNumber([
+          latestMonth.new_clients.counts.clients,
+        ])}`
+      );
+
     // fires second request to /activity
+    await click('[data-test-refresh]');
+  });
+
+  test('it does not query activity for community edition', async function (assert) {
+    assert.expect(4);
+    this.server.get('sys/internal/counters/activity', () => {
+      // this assertion should NOT be hit in this test
+      assert.true(true, 'uh oh! makes request to sys/internal/counters/activity');
+      return {
+        request_id: 'some-activity-id',
+        data: ACTIVITY_RESPONSE_STUB,
+      };
+    });
+    this.server.get('sys/internal/counters/config', function () {
+      assert.true(true, 'sys/internal/counters/config');
+      return {
+        request_id: 'some-config-id',
+        data: {
+          billing_start_timestamp: '0001-01-01T00:00:00Z',
+        },
+      };
+    });
+
+    await render(hbs`<Dashboard::ClientCountCard @isEnterprise={{false}} />`);
+    assert.dom(CLIENT_COUNT.statText('Total')).hasText('Total No total client data available. -');
+    assert.dom(CLIENT_COUNT.statText('New')).hasText('New No new client data available. -');
+
+    // attempt second request to /activity but component task should return instead of hitting endpoint
     await click('[data-test-refresh]');
   });
 });
