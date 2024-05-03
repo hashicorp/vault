52ba5fdac6ac10b3b5e3ab259242f0f3d8f450ec
Noelle Daley <noelledaley@users.noreply.github.com>
2024-05-03 12:03:26 -0700

    acceptance/clients/counts/sync: nest tests in top level module for easier test runs

--- ui/tests/acceptance/clients/counts/sync-test.js
+++ ui/tests/acceptance/clients/counts/sync-test.js
@@ -15,82 +15,81 @@ import authPage from 'vault/tests/pages/auth';
 import { GENERAL } from 'vault/tests/helpers/general-selectors';
 import { CLIENT_COUNT, CHARTS } from 'vault/tests/helpers/clients/client-count-selectors';
 
-module('Acceptance | clients | sync | activated', function (hooks) {
+module('Acceptance | clients | sync', function (hooks) {
   setupApplicationTest(hooks);
   setupMirage(hooks);
 
-  hooks.before(function () {
-    sinon.stub(timestamp, 'now').callsFake(() => STATIC_NOW);
+  module('sync activated', function (hooks) {
+    hooks.before(function () {
+      sinon.stub(timestamp, 'now').callsFake(() => STATIC_NOW);
+    });
+
+    hooks.beforeEach(async function () {
+      syncHandler(this.server);
+      await authPage.login();
+      return visit('/vault/clients/counts/sync');
+    });
+
+    hooks.after(function () {
+      timestamp.now.restore();
+    });
+
+    test('it should render charts when secrets sync is activated', async function (assert) {
+      syncHandler(this.server);
+      assert.dom(CHARTS.chart('Secrets sync usage')).exists('Secrets sync usage chart is rendered');
+      assert.dom(CLIENT_COUNT.statText('Total sync clients')).exists('Total sync clients chart is rendered');
+      assert.dom(GENERAL.emptyStateTitle).doesNotExist();
+    });
   });
 
-  hooks.beforeEach(async function () {
-    syncHandler(this.server);
-    await authPage.login();
-    return visit('/vault/clients/counts/sync');
-  });
+  module('sync not activated', function (hooks) {
+    hooks.before(function () {
+      sinon.stub(timestamp, 'now').callsFake(() => STATIC_NOW);
+    });
 
-  hooks.after(function () {
-    timestamp.now.restore();
-  });
+    hooks.beforeEach(async function () {
+      await authPage.login();
+      return visit('/vault/clients/counts/sync');
+    });
 
-  test('it should render charts when secrets sync is activated', async function (assert) {
-    syncHandler(this.server);
-    assert.dom(CHARTS.chart('Secrets sync usage')).exists('Secrets sync usage chart is rendered');
-    assert.dom(CLIENT_COUNT.statText('Total sync clients')).exists('Total sync clients chart is rendered');
-    assert.dom(GENERAL.emptyStateTitle).doesNotExist();
-  });
-});
+    hooks.after(function () {
+      timestamp.now.restore();
+    });
 
-module('Acceptance | clients | sync | not activated', function (hooks) {
-  setupApplicationTest(hooks);
-  setupMirage(hooks);
+    test('it should show an empty state when secrets sync is not activated', async function (assert) {
+      assert.expect(3);
 
-  hooks.before(function () {
-    sinon.stub(timestamp, 'now').callsFake(() => STATIC_NOW);
-  });
-
-  hooks.beforeEach(async function () {
-    await authPage.login();
-    return visit('/vault/clients/counts/sync');
-  });
-
-  hooks.after(function () {
-    timestamp.now.restore();
-  });
-
-  test('it should show an empty state when secrets sync is not activated', async function (assert) {
-    assert.expect(3);
-
-    // ensure secret_syncs clients activity is 0
-    this.server.get('/sys/internal/counters/activity', () => {
-      // return only the things that determine whether to show/hide secrets sync
-      return {
-        data: {
-          total: {
-            secret_syncs: 0,
+      // ensure secret_syncs clients activity is 0
+      this.server.get('/sys/internal/counters/activity', () => {
+        // return only the things that determine whether to show/hide secrets sync
+        return {
+          data: {
+            total: {
+              secret_syncs: 0,
+            },
           },
-        },
-      };
+        };
+      });
+
+      this.server.get('/sys/activation-flags', () => {
+        assert.true(true, '/sys/activation-flags/ is called to check if secrets-sync is activated');
+
+        return {
+          data: {
+            activated: [],
+            unactivated: ['secrets-sync'],
+          },
+        };
+      });
+
+      assert.dom(GENERAL.emptyStateTitle).exists('Shows empty state when secrets-sync is not activated');
+
+      await click(`${GENERAL.emptyStateActions} .hds-link-standalone`);
+      assert.strictEqual(
+        currentURL(),
+        '/vault/sync/secrets/overview',
+        'action button navigates to secrets sync overview page'
+      );
     });
-
-    this.server.get('/sys/activation-flags', () => {
-      assert.true(true, '/sys/activation-flags/ is called to check if secrets-sync is activated');
-
-      return {
-        data: {
-          activated: [],
-          unactivated: ['secrets-sync'],
-        },
-      };
-    });
-
-    assert.dom(GENERAL.emptyStateTitle).exists('Shows empty state when secrets-sync is not activated');
-
-    await click(`${GENERAL.emptyStateActions} .hds-link-standalone`);
-    assert.strictEqual(
-      currentURL(),
-      '/vault/sync/secrets/overview',
-      'action button navigates to secrets sync overview page'
-    );
   });
 });
