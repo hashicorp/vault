1fecf96e2f2e82abb726705ac54babe578ea917b
Noelle Daley <noelledaley@users.noreply.github.com>
2024-05-03 09:45:15 -0700

    acceptance/clients/counts/overview-test: stub activated flags to fix test

--- ui/tests/acceptance/clients/counts/overview-test.js
+++ ui/tests/acceptance/clients/counts/overview-test.js
@@ -149,6 +149,9 @@ module('Acceptance | clients | overview', function (hooks) {
   });
 
   test('totals filter correctly with full data', async function (assert) {
+    // stub secrets sync being activated
+    this.owner.lookup('service:flags').activatedFlags = ['secrets-sync'];
+
     assert
       .dom(CHARTS.container('Vault client counts'))
       .exists('Shows running totals with monthly breakdown charts');
