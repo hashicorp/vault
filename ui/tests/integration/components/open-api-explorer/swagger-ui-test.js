cd85f9bb16c9f2e951c5d26c35f05afeccb26d61
Chelsea Shaw <cshaw@hashicorp.com>
2024-05-07 15:24:03 -0500

    weird attempt to fix flaky test

--- ui/tests/integration/components/open-api-explorer/swagger-ui-test.js
+++ ui/tests/integration/components/open-api-explorer/swagger-ui-test.js
@@ -5,7 +5,7 @@
 
 import { module, test } from 'qunit';
 import { setupRenderingTest } from 'vault/tests/helpers';
-import { waitUntil, find } from '@ember/test-helpers';
+import { waitFor } from '@ember/test-helpers';
 import { setupEngine } from 'ember-engines/test-support';
 import { setupMirage } from 'ember-cli-mirage/test-support';
 import { render, fillIn } from '@ember/test-helpers';
@@ -13,7 +13,7 @@ import { hbs } from 'ember-cli-htmlbars';
 
 const SELECTORS = {
   container: '[data-test-swagger-ui]',
-  searchInput: '.operation-filter-input',
+  searchInput: 'input.operation-filter-input',
   apiPathBlock: '.opblock-post',
 };
 
@@ -38,24 +38,27 @@ module('Integration | Component | open-api-explorer | swagger-ui', function (hoo
     };
   });
 
-  test('it renders', async function (assert) {
-    await this.renderComponent();
+  [1, 2, 3, 4, 5].forEach((i) => {
+    test(`it renders ${i}`, async function (assert) {
+      await this.renderComponent();
 
-    await waitUntil(() => find(SELECTORS.container));
+      await waitFor(SELECTORS.container);
 
-    assert.dom(SELECTORS.container).exists('renders component');
-    assert.dom(SELECTORS.apiPathBlock).exists({ count: this.totalApiPaths }, 'renders all api paths');
-  });
+      assert.dom(SELECTORS.container).exists('renders component');
+      assert.dom(SELECTORS.apiPathBlock).exists({ count: this.totalApiPaths }, 'renders all api paths');
+    });
 
-  test('it can search', async function (assert) {
-    await this.renderComponent();
+    test(`it can search ${i}`, async function (assert) {
+      await this.renderComponent();
 
-    await waitUntil(() => find(SELECTORS.searchInput));
-    await fillIn(SELECTORS.searchInput, 'token');
+      // in testing only the input is not filling correctly except after the second time
+      await fillIn(SELECTORS.searchInput, 'moot');
+      await fillIn(SELECTORS.searchInput, 'token');
 
-    // for some reason search results are not rendered immediately in tests,
-    // so asserting that the search input has the value we expect is the best we can do here
-    // if the search fn breaks, this test will fail
-    assert.dom(SELECTORS.searchInput).hasValue('token', 'search input has value');
+      // for some reason search results are not rendered immediately in tests,
+      // so asserting that the search input has the value we expect is the best we can do here
+      // if the search fn breaks, this test will fail
+      assert.dom(SELECTORS.searchInput).hasValue('token', 'search input has value');
+    });
   });
 });
