52c2b3822fad7279a7448b487e24abf2fd41b4df
Angel Garbarino <argarbarino@gmail.com>
2024-05-20 16:32:20 -0600

    maybe an okay approach?

--- ui/tests/integration/components/radial-progress-test.js
+++ ui/tests/integration/components/radial-progress-test.js
@@ -6,11 +6,8 @@
 import { module, test } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
 import { render } from '@ember/test-helpers';
-import { create } from 'ember-cli-page-object';
 import hbs from 'htmlbars-inline-precompile';
-import radialProgress from 'vault/tests/pages/components/radial-progress';
-
-const component = create(radialProgress);
+import { RADIAL_PROGRESS } from 'vault/tests/helpers/components/shamir-selectors';
 
 module('Integration | Component | radial progress', function (hooks) {
   setupRenderingTest(hooks);
@@ -20,14 +17,22 @@ module('Integration | Component | radial progress', function (hooks) {
     // ie11 truncates decimals differently than other browsers.
     const circumference = ((19 / 2) * Math.PI * 2).toFixed(2);
     await render(hbs`<RadialProgress @progressDecimal={{0.5}}/>`);
-    assert.strictEqual(component.viewBox, '0 0 20 20');
-    assert.strictEqual(component.height, '20');
-    assert.strictEqual(component.width, '20');
-    assert.strictEqual(component.strokeWidth, '1');
-    assert.strictEqual(component.r, (19 / 2).toString());
-    assert.strictEqual(component.cx, '10');
-    assert.strictEqual(component.cy, '10');
-    assert.strictEqual(Number(component.strokeDash).toFixed(2), circumference);
-    assert.strictEqual(Number(component.strokeDashOffset).toFixed(3), (circumference * 0.5).toFixed(3));
+    const svg = document.querySelector(RADIAL_PROGRESS.svg);
+    const path = document.querySelector(RADIAL_PROGRESS.path);
+    const progress = document.querySelector(RADIAL_PROGRESS.progress);
+
+    assert.strictEqual(svg.getAttribute('viewBox'), '0 0 20 20');
+    assert.strictEqual(svg.getAttribute('height'), '20');
+    assert.strictEqual(svg.getAttribute('width'), '20');
+    // path
+    assert.strictEqual(path.getAttribute('stroke-width'), '1');
+    assert.strictEqual(path.getAttribute('r'), (19 / 2).toString());
+    assert.strictEqual(path.getAttribute('cx'), '10');
+    assert.strictEqual(path.getAttribute('cy'), '10');
+    assert.strictEqual(Number(progress.getAttribute('strokeDash').toFixed(2)), circumference);
+    assert.strictEqual(
+      Number(progress.getAttribute('strokeDashOffset').toFixed(3)),
+      (circumference * 0.5).toFixed(3)
+    );
   });
 });
