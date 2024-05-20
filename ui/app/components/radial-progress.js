6fe86b79358bee7421c5af13ce6d7e11a817b039
Angel Garbarino <argarbarino@gmail.com>
2024-05-20 15:56:04 -0600

    glimmerize components next test and test restructure

--- ui/app/components/radial-progress.js
+++ ui/app/components/radial-progress.js
@@ -3,32 +3,32 @@
  * SPDX-License-Identifier: BUSL-1.1
  */
 
-import Component from '@ember/component';
-import { computed } from '@ember/object';
+import Component from '@glimmer/component';
+import { tracked } from '@glimmer/tracking';
 
-export default Component.extend({
-  'data-test-radial-progress': true,
-  tagName: 'svg',
-  classNames: 'radial-progress',
-  attributeBindings: ['size:width', 'size:height', 'viewBox', 'data-test-radial-progress'],
-  progressDecimal: null,
-  size: 20,
-  strokeWidth: 1,
+export default class KvDataFields extends Component {
+  @tracked progressDecimal = null;
+  @tracked size = 20;
+  @tracked strokeWidth = 1;
 
-  viewBox: computed('size', function () {
+  get viewBox() {
     const s = this.size;
     return `0 0 ${s} ${s}`;
-  }),
-  centerValue: computed('size', function () {
+  }
+
+  get centerValue() {
     return this.size / 2;
-  }),
-  r: computed('size', 'strokeWidth', function () {
+  }
+
+  get r() {
     return (this.size - this.strokeWidth) / 2;
-  }),
-  c: computed('r', function () {
+  }
+
+  get c() {
     return 2 * Math.PI * this.r;
-  }),
-  dashArrayOffset: computed('c', 'progressDecimal', function () {
+  }
+
+  get dashArrayOffset() {
     return this.c * (1 - this.progressDecimal);
-  }),
-});
+  }
+}
