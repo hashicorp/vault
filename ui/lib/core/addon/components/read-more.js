c766c41bcb9ff13635a85e8ba42a266185fa9f9f
clairebontempo@gmail.com <clairebontempo@gmail.com>
2024-05-28 13:54:48 -0700

    fix read more syntax

--- ui/lib/core/addon/components/read-more.js
+++ ui/lib/core/addon/components/read-more.js
@@ -11,6 +11,7 @@ import { tracked } from '@glimmer/tracking';
 
 /**
  * @module ReadMore
+ * @description
  * ReadMore components are used to wrap long text that we'd like to show as one line initially with the option to expand and read.
  * Text which is shorter than the surrounding div will not truncate or show the See More button.
  *
