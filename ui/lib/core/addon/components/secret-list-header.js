fd749a833097141e254b1a79642229976c88d84e
clairebontempo@gmail.com <clairebontempo@gmail.com>
2024-05-28 13:48:09 -0700

    fix ts file @module syntax

--- ui/lib/core/addon/components/secret-list-header.js
+++ ui/lib/core/addon/components/secret-list-header.js
@@ -7,10 +7,13 @@ import Component from '@glimmer/component';
 
 /**
  * @module SecretListHeader
- * SecretListHeader component is breadcrumb, title with icon and menu with tabs component.
+ * SecretListHeader component is breadcrumb, title with icon and menu with tabs component. Hello
  *
+ * Example is wrapped in back ticks because this component relies on routing and cannot render an isolated sample, so just rendering template sample
  * @example
- * <SecretListHeader @model={{hash id="my-secret" icon="person" engineType="kv"}} @@isConfigure={{true}} />
+ * ```
+ * <SecretListHeader @isCertTab={{eq this.tab "cert"}} @model={{this.model}} @baseKey={{this.baseKey}} @backendCrumb={{this.backendCrumb}} @filter={{this.filter}} />
+ * ```
  *
  * @param {object} model - Model used to pull information about icon and title and backend type for navigation.
  */
