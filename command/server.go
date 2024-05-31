c2f1fb329cbc8c9136fd0da7096acbdc7750edd0
Violet Hynes <a.xenasis@gmail.com>
2024-05-31 13:50:24 -0400

    Fix formatting for real

--- command/server.go
+++ command/server.go
@@ -2133,10 +2133,10 @@ func (c *ServerCommand) enableDev(core *vault.Core, coreConfig *vault.CoreConfig
 		}
 		resp, err := core.HandleRequest(ctx, req)
 		if err != nil {
-		  return nil, fmt.Errorf("error creating default KV store: %w", err)
+			return nil, fmt.Errorf("error creating default KV store: %w", err)
 		}
 		if resp.IsError() {
-		  return nil, fmt.Errorf("failed to create default KV store: %w", resp.Error())
+			return nil, fmt.Errorf("failed to create default KV store: %w", resp.Error())
 		}
 	}
 
