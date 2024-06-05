b86f4afbbd86295cbfcd77706d41db3fd3842d56
Kit Haines <khaines@mit.edu>
2024-06-05 14:17:04 -0400

    OSS changes to rename metadata cert_metadata (#27365)
    * OSS changes to rename metadata cert_metadata
    
    * OSS changes to functions.

--- builtin/logical/pki/metadata_oss.go
+++ builtin/logical/pki/metadata_oss.go
@@ -9,7 +9,6 @@ import (
 	"context"
 	"crypto/x509"
 	"errors"
-	"math/big"
 
 	"github.com/hashicorp/go-hclog"
 	"github.com/hashicorp/vault/builtin/logical/pki/issuing"
@@ -18,18 +17,14 @@ import (
 
 var ErrMetadataIsEntOnly = errors.New("certificate metadata is only supported on Vault Enterprise")
 
-func storeMetadata(ctx context.Context, storage logical.Storage, issuerId issuing.IssuerID, role string, certificate *x509.Certificate, metadata interface{}) error {
+func storeCertMetadata(ctx context.Context, storage logical.Storage, issuerId issuing.IssuerID, role string, certificate *x509.Certificate, certMetadata interface{}) error {
 	return ErrMetadataIsEntOnly
-}
-
-func GetCertificateMetadata(ctx context.Context, storage logical.Storage, serialNumber *big.Int) (*CertificateMetadata, error) {
-	return nil, ErrMetadataIsEntOnly
 }
 
 func (b *backend) doTidyCertMetadata(ctx context.Context, req *logical.Request, logger hclog.Logger, config *tidyConfig) error {
 	return ErrMetadataIsEntOnly
 }
 
-func validateMetadataConfiguration(role *issuing.RoleEntry) error {
+func validateCertMetadataConfiguration(role *issuing.RoleEntry) error {
 	return ErrMetadataIsEntOnly
 }
