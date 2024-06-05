f732699924e13e77b6f34e98b9ac834da1e865d9
Noelle Daley <noelledaley@users.noreply.github.com>
2024-06-05 10:47:49 -0700

    wip - lease tests

---
+++ ui/tests/integration/components/configure-aws-secret/lease-form-test.js
@@ -0,0 +1,17 @@
+/**
+ * Copyright (c) HashiCorp, Inc.
+ * SPDX-License-Identifier: BUSL-1.1
+ */
+
+import { module, test } from 'qunit';
+import { setupRenderingTest } from 'ember-qunit';
+// import { render, settled, find, waitUntil } from '@ember/test-helpers';
+// import hbs from 'htmlbars-inline-precompile';
+
+module('Integration | Component | configure-aws-secret/lease-form', function (hooks) {
+  setupRenderingTest(hooks);
+
+  hooks.beforeEach();
+
+  test('it renders', async function () {});
+});
