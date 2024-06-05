3a50143b91f8fd7bc3d1a91b3c4bca462b726524
clairebontempo@gmail.com <clairebontempo@gmail.com>
2024-06-05 13:35:02 -0700

    another hash cleanup

--- ui/app/components/tools/hash.js
+++ ui/app/components/tools/hash.js
@@ -11,13 +11,10 @@ import errorMessage from 'vault/utils/error-message';
 
 /**
  * @module ToolHash
- * ToolHash components are components that sys/wrapping/hash functionality.  Most of the functionality is passed through as actions from the tool-actions-form and then called back with properties.
+ * ToolHash components are components that sys/wrapping/hash functionality.
  *
  * @example
- * <Tools::Hash @onClear={{action "onClear"}} @onChange={{action "onChange"}} @sum={{sum}} @algorithm={{algorithm}} @format={{format}} @errors={{errors}} />
- *
- * @param {Function} onClear - parent action that is passed through. Must be passed as {{action "onClear"}}
- * @param {String} sum=null - property passed from parent to child and then passed back up to parent.
+ * <Tools::Hash />
  */
 export default class ToolsHash extends Component {
   @service store;
