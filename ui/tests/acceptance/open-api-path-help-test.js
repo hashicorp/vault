ce2b3ed62f7a4cb3d7b18a73eda70eb7bfad0a29
Chelsea Shaw <cshaw@hashicorp.com>
2024-06-06 13:46:25 -0500

    updated test

--- ui/tests/acceptance/open-api-path-help-test.js
+++ ui/tests/acceptance/open-api-path-help-test.js
@@ -76,13 +76,14 @@ function secretEngineHelper(test, secretEngine) {
       const helpUrl = model.getHelpUrl(this.backend);
       const result = await this.pathHelp.getProps(helpUrl, this.backend);
       const expected = engineData[modelName];
-      assert.strictEqual(
-        Object.keys(expected).length,
-        Object.keys(result).length,
-        `getProps returns expected number of attributes for ${modelName}`
+      // Expected values should be updated to match "actual" (result)
+      assert.deepEqual(
+        Object.keys(result),
+        Object.keys(expected),
+        `getProps returns expected attributes for ${modelName}`
       );
       Object.keys(expected).forEach((attrName) => {
-        assert.propEqual(expected[attrName], result[attrName], `${attrName} attribute details match`);
+        assert.deepEqual(result[attrName], expected[attrName], `${attrName} attribute details match`);
       });
     });
   });
@@ -101,13 +102,13 @@ function authEngineHelper(test, authBackend) {
         const helpUrl = model.getHelpUrl(this.mount);
         const result = await this.pathHelp.getProps(helpUrl, this.mount);
         const expected = authData[itemName];
-        assert.strictEqual(
-          Object.keys(expected).length,
-          Object.keys(result).length,
-          `getProps returns expected number of attributes for ${itemName}`
+        assert.deepEqual(
+          Object.keys(result),
+          Object.keys(expected),
+          `getProps returns expected attributes for ${itemName}`
         );
         Object.keys(expected).forEach((attrName) => {
-          assert.propEqual(expected[attrName], result[attrName], `${attrName} attribute details match`);
+          assert.propEqual(result[attrName], expected[attrName], `${attrName} attribute details match`);
         });
       });
     } else {
