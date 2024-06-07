cb234d1f2d5b70fd1d7b6c6cb6ba4c2598a66ce1
clairebontempo@gmail.com <clairebontempo@gmail.com>
2024-06-07 13:53:10 -0700

    lookup refactor

--- ui/app/components/tool-lookup.js
+++ ui/app/components/tool-lookup.js
@@ -4,36 +4,53 @@
  */
 
 import Component from '@glimmer/component';
+import { service } from '@ember/service';
 import { action } from '@ember/object';
+import { tracked } from '@glimmer/tracking';
+import errorMessage from 'vault/utils/error-message';
+import { addSeconds, parseISO } from 'date-fns';
 
 /**
  * @module ToolLookup
- * ToolLookup components are components that sys/wrapping/lookup functionality.  Most of the functionality is passed through as actions from the tool-actions-form and then called back with properties.
+ * ToolLookup components are components that sys/wrapping/lookup functionality.
  *
  * @example
- * ```js
- * <ToolLookup
- *  @creation_time={{creation_time}}
- *  @creation_ttl={{creation_ttl}}
- *  @creation_path={{creation_path}}
- *  @expirationDate={{expirationDate}}
- *  @selectedAction={{selectedAction}}
- *  @token={{token}}
- *  @onClear={{action "onClear"}}
- *  @errors={{errors}}/>
- * ```
- * @param creation_time {Function} - parent action that is passed through.
- * @param creation_ttl {Function} - parent action that is passed through.
- * @param creation_path {Function} - parent action that is passed through.
- * @param expirationDate='' {String} - value returned from lookup.
- * @param selectedAction="wrap" - passed in from parent.  This is the wrap action, others include hash, etc.
- * @param token=null {String} - property passed from parent to child and then passed back up to parent
- * @param onClear {Function} - parent action that is passed through. Must be passed as {{action "onClear"}}
- * @param error=null {Object} - errors passed from parent as default then from child back to parent.
+ * <ToolLookup />
  */
 export default class ToolLookup extends Component {
+  @service store;
+  @service flashMessages;
+
+  @tracked token = '';
+  @tracked lookupData = null;
+  @tracked errorMessage = '';
+
   @action
-  onClear() {
-    this.args.onClear();
+  reset() {
+    this.token = '';
+    this.lookupData = null;
+    this.errorMessage = '';
+  }
+
+  get expirationDate() {
+    const { creation_time, creation_ttl } = this.lookupData;
+    if (creation_time && creation_ttl) {
+      // returns new Date with seconds added.
+      return addSeconds(parseISO(creation_time), creation_ttl);
+    }
+    return null;
+  }
+
+  @action
+  async handleSubmit(evt) {
+    evt.preventDefault();
+    const payload = { token: this.token.trim() };
+    try {
+      const resp = await this.store.adapterFor('tools').toolAction('lookup', payload);
+      this.lookupData = resp.data;
+      this.flashMessages.success('Lookup was successful.');
+    } catch (error) {
+      this.errorMessage = errorMessage(error);
+    }
   }
 }
---
+++ ui/tests/integration/components/tools/lookup-test.js
@@ -0,0 +1,76 @@
+/**
+ * Copyright (c) HashiCorp, Inc.
+ * SPDX-License-Identifier: BUSL-1.1
+ */
+
+import { module, test } from 'qunit';
+import { setupRenderingTest } from 'vault/tests/helpers';
+import { setupMirage } from 'ember-cli-mirage/test-support';
+import { Response } from 'miragejs';
+import { click, fillIn, find, render, waitUntil } from '@ember/test-helpers';
+import { hbs } from 'ember-cli-htmlbars';
+import { GENERAL } from 'vault/tests/helpers/general-selectors';
+import { TOOLS_SELECTORS as TS } from 'vault/tests/helpers/tools-selectors';
+import { format } from 'date-fns';
+
+module('Integration | Component | tools/lookup', function (hooks) {
+  setupRenderingTest(hooks);
+  setupMirage(hooks);
+
+  hooks.beforeEach(function () {
+    this.renderComponent = async () => {
+      await render(hbs`
+    <ToolLookup />`);
+    };
+  });
+
+  test('it renders defaults', async function (assert) {
+    await this.renderComponent();
+
+    assert.dom('h1').hasText('Lookup Token', 'Title renders');
+    assert.dom('label').hasText('Wrapped token');
+    assert.dom(TS.toolsInput('wrapping-token')).hasValue('');
+    assert.dom(TS.button('Done')).doesNotExist();
+  });
+
+  test('it renders errors', async function (assert) {
+    this.server.post('sys/wrapping/lookup', () => new Response(500, {}, { errors: ['Something is wrong'] }));
+    await this.renderComponent();
+    await click(TS.submit);
+    await waitUntil(() => find(GENERAL.messageError));
+    assert.dom(GENERAL.messageError).hasText('Error Something is wrong', 'Error renders');
+  });
+
+  test('it submits', async function (assert) {
+    // not stubbing the timestamp util here because this component uses the date-fns formatDistanceToNow method
+    // so we need an actual now date for testing (which is why we don't assert the timestamp below, just the day of the month)
+    const now = new Date();
+    const data = {
+      creation_path: 'sys/wrapping/wrap',
+      creation_time: now.toISOString(),
+      creation_ttl: 1800,
+    };
+    const token = 'token.OMZFbUurY0ppT2RTMGpRa0JOSUFqUzJUaGNqdWUQ6ooG';
+    this.server.post('sys/wrapping/lookup', (schema, req) => {
+      assert.propEqual(JSON.parse(req.requestBody), { token }, `payload trims token: ${req.requestBody}`);
+      return { data };
+    });
+    await this.renderComponent();
+    await fillIn(TS.toolsInput('wrapping-token'), `${token}   `);
+    await click(TS.submit);
+
+    await waitUntil(() => find(GENERAL.infoRowValue('Creation path')));
+    assert.dom(GENERAL.infoRowValue('Creation path')).hasText(data.creation_path);
+    assert.dom(GENERAL.infoRowValue('Creation time')).hasText(data.creation_time);
+    assert.dom(GENERAL.infoRowValue('Creation TTL')).hasText(`${data.creation_ttl}`);
+    assert.dom(GENERAL.infoRowValue('Expiration date')).hasTextContaining(format(now, 'MMM dd yyyy')); // intentionally exclude time to avoid race conditions
+    // remove below assertion if flaky (but unlikely this test would take longer than a minute..)
+    assert.dom(GENERAL.infoRowValue('Expires in')).hasText('30 minutes'); // from 1800s ttl
+
+    // clicking done resets form
+    await click(TS.button('Done'));
+    assert.dom('label').hasText('Wrapped token');
+    assert.dom(TS.toolsInput('wrapping-token')).hasValue('');
+    assert.dom(TS.button('Done')).doesNotExist();
+  });
+});
