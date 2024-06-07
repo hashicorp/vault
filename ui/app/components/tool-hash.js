ff3eb8b35812ff228fb22f5f03057aff830606b4
clairebontempo@gmail.com <clairebontempo@gmail.com>
2024-06-07 13:42:02 -0700

    refactor tool hash component

--- ui/app/components/tool-hash.js
+++ ui/app/components/tool-hash.js
@@ -5,29 +5,57 @@
 
 import Component from '@glimmer/component';
 import { action } from '@ember/object';
+import { service } from '@ember/service';
+import { tracked } from '@glimmer/tracking';
+import errorMessage from 'vault/utils/error-message';
 
 /**
  * @module ToolHash
- * ToolHash components are components that sys/wrapping/hash functionality.  Most of the functionality is passed through as actions from the tool-actions-form and then called back with properties.
+ * ToolHash components are components that sys/wrapping/hash functionality.
  *
  * @example
- * ```js
- * <ToolHash
- *  @onClear={{action "onClear"}}
- *  @sum={{sum}}
- *  @algorithm={{algorithm}}
- *  @format={{format}}
- *  @errors={{errors}}/>
- * ```
- * @param onClear {Function} - parent action that is passed through. Must be passed as {{action "onClear"}}
- * @param sum=null {String} - property passed from parent to child and then passed back up to parent.
- * @param algorithm {String} - property returned from parent.
- * @param format {String} - property returned from parent.
- * @param error=null {Object} - errors passed from parent as default then from child back to parent.
+ * <ToolHash />
  */
 export default class ToolHash extends Component {
+  @service store;
+  @service flashMessages;
+
+  @tracked algorithm = 'sha2-256';
+  @tracked format = 'base64';
+  @tracked hashData = '';
+  @tracked sum = null;
+  @tracked errorMessage = '';
+
   @action
-  onClear() {
-    this.args.onClear();
+  reset() {
+    this.algorithm = 'sha2-256';
+    this.format = 'base64';
+    this.hashData = '';
+    this.sum = null;
+    this.errorMessage = '';
+  }
+
+  @action
+  handleEvent(evt) {
+    const { name, value } = evt.target;
+    this[name] = value;
+  }
+
+  @action
+  async handleSubmit(evt) {
+    evt.preventDefault();
+    const data = {
+      input: this.hashData,
+      format: this.format,
+      algorithm: this.algorithm,
+    };
+
+    try {
+      const response = await this.store.adapterFor('tools').toolAction('hash', data);
+      this.sum = response.data.sum;
+      this.flashMessages.success('Hash was successful.');
+    } catch (error) {
+      this.errorMessage = errorMessage(error);
+    }
   }
 }
