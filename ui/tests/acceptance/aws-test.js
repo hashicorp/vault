da10a59b10d8770a7a7d1c7d12d3eabdab223dec
Chelsea Shaw <cshaw@hashicorp.com>
2024-06-10 10:54:02 -0500

    Update test with coverage for aws credential behavior

--- ui/tests/acceptance/aws-test.js
+++ ui/tests/acceptance/aws-test.js
@@ -3,7 +3,7 @@
  * SPDX-License-Identifier: BUSL-1.1
  */
 
-import { click, fillIn, currentURL, find, settled, waitUntil } from '@ember/test-helpers';
+import { click, fillIn, currentURL, find, settled, waitUntil, visit } from '@ember/test-helpers';
 import { module, test } from 'qunit';
 import { setupApplicationTest } from 'ember-qunit';
 import { v4 as uuidv4 } from 'uuid';
@@ -13,6 +13,8 @@ import { GENERAL } from '../helpers/general-selectors';
 import authPage from 'vault/tests/pages/auth';
 import enablePage from 'vault/tests/pages/settings/mount-secret-backend';
 import { setupMirage } from 'ember-cli-mirage/test-support';
+import { deleteEngineCmd, mountEngineCmd, runCmd } from 'vault/tests/helpers/commands';
+import { overrideResponse } from 'vault/tests/helpers/stubs';
 
 const AWS_CREDS = {
   configTab: '[data-test-configuration-tab]',
@@ -21,13 +23,53 @@ const AWS_CREDS = {
   viewBackend: '[data-test-backend-view-link]',
   createSecret: '[data-test-secret-create]',
   secretHeader: '[data-test-secret-header]',
-  secretLink: (name) => `[data-test-secret-link="${name}"]`,
+  secretLink: (name) => (name ? `[data-test-secret-link="${name}"]` : '[data-test-secret-link]'),
   crumb: (path) => `[data-test-secret-breadcrumb="${path}"] a`,
   ttlToggle: '[data-test-ttl-toggle="TTL"]',
   warning: '[data-test-warning]',
   delete: (role) => `[data-test-aws-role-delete="${role}"]`,
   backButton: '[data-test-back-button]',
+  generateLink: '[data-test-backend-credentials]',
 };
+const ROLE_TYPES = [
+  {
+    credentialType: 'iam_user',
+    async fillOutForm(assert) {
+      // nothing to fill out
+      assert.dom('[data-test-field]').exists({ count: 1 });
+    },
+    expectedPayload: {},
+  },
+  {
+    credentialType: 'assumed_role',
+    async fillOutForm(assert) {
+      await click(GENERAL.toggleInput('TTL'));
+      assert.dom(GENERAL.toggleInput('TTL')).isNotChecked();
+      await fillIn(GENERAL.inputByAttr('roleArn'), 'foobar');
+    },
+    expectedPayload: {
+      role_arn: 'foobar',
+    },
+  },
+  {
+    credentialType: 'federation_token',
+    async fillOutForm(assert) {
+      assert.dom(GENERAL.toggleInput('TTL')).isChecked();
+      await fillIn(GENERAL.ttl.input('TTL'), '3');
+    },
+    expectedPayload: {
+      ttl: '10800s',
+    },
+  },
+  {
+    credentialType: 'session_token',
+    async fillOutForm(assert) {
+      await click(GENERAL.toggleInput('TTL'));
+      assert.dom(GENERAL.toggleInput('TTL')).isNotChecked();
+    },
+    expectedPayload: null,
+  },
+];
 module('Acceptance | aws secret backend', function (hooks) {
   setupApplicationTest(hooks);
   setupMirage(hooks);
@@ -44,18 +86,6 @@ module('Acceptance | aws secret backend', function (hooks) {
   test('aws backend', async function (assert) {
     const path = `aws-${this.uid}`;
     const roleName = 'awsrole';
-    this.server.post(`/${path}/creds/${roleName}`, (_, req) => {
-      const payload = JSON.parse(req.requestBody);
-      assert.deepEqual(payload, { role_arn: 'foobar' }, 'does not send TTL when unchecked');
-      return {
-        data: {
-          access_key: 'AKIA...',
-          secret_key: 'xlCs...',
-          security_token: 'some-token',
-          arn: 'arn:aws:sts::123456789012:assumed-role/DeveloperRole/some-user-supplied-role-session-name',
-        },
-      };
-    });
 
     await enablePage.enable('aws', path);
     await settled();
@@ -111,26 +141,103 @@ module('Acceptance | aws secret backend', function (hooks) {
     assert.strictEqual(currentURL(), `/vault/secrets/${path}/list`);
     assert.dom(AWS_CREDS.secretLink(roleName)).exists();
 
-    // check that generates credentials flow is correct
-    await click(AWS_CREDS.secretLink(roleName));
-    assert.dom('h1').hasText('Generate AWS Credentials');
-    assert.dom(GENERAL.inputByAttr('credentialType')).hasValue('iam_user');
-    await fillIn(GENERAL.inputByAttr('credentialType'), 'assumed_role');
-    await click(AWS_CREDS.ttlToggle);
-    assert.dom(AWS_CREDS.ttlToggle).isNotChecked();
-    await fillIn(GENERAL.inputByAttr('roleArn'), 'foobar');
-    await click(GENERAL.saveButton);
-    assert.dom(AWS_CREDS.warning).exists('Shows access warning after generation');
-    assert.dom(GENERAL.infoRowValue('Access key')).exists();
-    assert.dom(GENERAL.infoRowValue('Secret key')).exists();
-    assert.dom(GENERAL.infoRowValue('Security token')).exists();
-    await click(AWS_CREDS.backButton);
-
     //and delete
     await click(`${AWS_CREDS.secretLink(roleName)} [data-test-popup-menu-trigger]`);
     await waitUntil(() => find(AWS_CREDS.delete(roleName))); // flaky without
     await click(AWS_CREDS.delete(roleName));
     await click(GENERAL.confirmButton);
     assert.dom(AWS_CREDS.secretLink(roleName)).doesNotExist('aws: role is no longer in the list');
+  });
+
+  ROLE_TYPES.forEach((scenario) => {
+    test(`aws credentials - type ${scenario.credentialType}`, async function (assert) {
+      const path = `aws-cred-${this.uid}`;
+      const roleName = `awsrole-${scenario.credentialType}`;
+      this.server.post(`/${path}/creds/${roleName}`, (_, req) => {
+        const payload = JSON.parse(req.requestBody);
+        assert.deepEqual(payload, scenario.expectedPayload);
+        return {
+          data: {
+            access_key: 'AKIA...',
+            secret_key: 'xlCs...',
+            security_token: 'some-token',
+            arn: 'arn:aws:sts::123456789012:assumed-role/DeveloperRole/some-user-supplied-role-session-name',
+          },
+        };
+      });
+      this.server.get(`/${path}/creds/${roleName}`, () => {
+        return {
+          data: {
+            access_key: 'AKIA...',
+            secret_key: 'xlCs...',
+            security_token: 'some-token',
+            arn: 'arn:aws:sts::123456789012:assumed-role/DeveloperRole/some-user-supplied-role-session-name',
+          },
+        };
+      });
+      await runCmd(mountEngineCmd('aws', path));
+
+      await visit(`/vault/secrets/${path}/create`);
+      assert.dom('h1').hasText('Create an AWS Role');
+      await fillIn(GENERAL.inputByAttr('name'), roleName);
+      await fillIn(GENERAL.inputByAttr('credentialType'), scenario.credentialType);
+      await click(GENERAL.saveButton);
+      await waitUntil(() => currentURL() === `/vault/secrets/${path}/show/${roleName}`); // flaky without this
+      assert.strictEqual(currentURL(), `/vault/secrets/${path}/show/${roleName}`);
+      await click(AWS_CREDS.generateLink);
+      assert
+        .dom(GENERAL.inputByAttr('credentialType'))
+        .hasValue(scenario.credentialType, 'credentialType matches backing role');
+
+      // based on credentialType, fill out form
+      await scenario.fillOutForm(assert);
+
+      await click(GENERAL.saveButton);
+      assert.dom(AWS_CREDS.warning).exists('Shows access warning after generation');
+      assert.dom(GENERAL.infoRowValue('Access key')).exists();
+      assert.dom(GENERAL.infoRowValue('Secret key')).exists();
+      assert.dom(GENERAL.infoRowValue('Security token')).exists();
+      await visit('/vault/dashboard');
+
+      await runCmd(deleteEngineCmd(path));
+    });
+  });
+
+  test(`aws credentials without role read access`, async function (assert) {
+    const path = `aws-cred-${this.uid}`;
+    const roleName = `awsrole-noread`;
+    this.server.post(`/${path}/creds/${roleName}`, () => {
+      return {
+        data: {
+          access_key: 'AKIA...',
+          secret_key: 'xlCs...',
+          security_token: 'some-token',
+          arn: 'arn:aws:sts::123456789012:assumed-role/DeveloperRole/some-user-supplied-role-session-name',
+        },
+      };
+    });
+    this.server.get(`/${path}/roles/${roleName}`, () => overrideResponse(403));
+    await runCmd(mountEngineCmd('aws', path));
+    await runCmd(`write ${path}/roles/${roleName} credential_type=assumed_role`);
+
+    await visit(`/vault/secrets/${path}/list`);
+    assert.dom(AWS_CREDS.secretLink(roleName)).exists();
+    await click(AWS_CREDS.secretLink(roleName));
+
+    assert.strictEqual(currentURL(), `/vault/secrets/${path}/credentials/${roleName}`);
+    assert
+      .dom(GENERAL.inputByAttr('credentialType'))
+      .hasValue('iam_user', 'credentialType defaults to first in list due to no role read permissions');
+
+    await fillIn(GENERAL.inputByAttr('credentialType'), 'assumed_role');
+
+    await click(GENERAL.saveButton);
+    assert.dom(AWS_CREDS.warning).exists('Shows access warning after generation');
+    assert.dom(GENERAL.infoRowValue('Access key')).exists();
+    assert.dom(GENERAL.infoRowValue('Secret key')).exists();
+    assert.dom(GENERAL.infoRowValue('Security token')).exists();
+    await visit('/vault/dashboard');
+
+    await runCmd(deleteEngineCmd(path));
   });
 });
