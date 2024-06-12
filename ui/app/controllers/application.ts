0eaffbbecceaf011b8693aef38b38919bc28982f
Chelsea Shaw <cshaw@hashicorp.com>
2024-06-12 14:40:47 -0500

    ts application controller

--- ui/app/controllers/application.ts
+++ ui/app/controllers/application.ts
@@ -6,9 +6,11 @@
 import { service } from '@ember/service';
 import Controller from '@ember/controller';
 import config from '../config/environment';
+import type AuthService from 'vault/vault/services/auth';
+import type StoreService from 'vault/services/store';
 
 export default class ApplicationController extends Controller {
-  @service auth;
-  @service store;
+  @service declare readonly auth: AuthService;
+  @service declare readonly store: StoreService;
   env = config.environment;
 }
