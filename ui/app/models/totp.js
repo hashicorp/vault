ad183e2ef4927023d7c2f68ec2fb42ff16f08478
Moritz Pflanzer <moritz@chickadee-engineering.com>
2024-06-14 22:35:35 +0200

    TOTP model validation

--- ui/app/models/totp.js
+++ ui/app/models/totp.js
@@ -6,51 +6,80 @@
 import Model, { attr } from '@ember-data/model';
 import { alias } from '@ember/object/computed';
 import { computed } from '@ember/object';
+import { withModelValidations } from 'vault/decorators/model-validations';
 import fieldToAttrs, { expandAttributeMeta } from 'vault/utils/field-to-attrs';
 import lazyCapabilities, { apiPath } from 'vault/macros/lazy-capabilities';
 
-export default Model.extend({
-  failedServerRead: attr('boolean'),
-  auth: attr('string'),
-  lease_duration: attr('number'),
-  lease_id: attr('string'),
-  renewable: attr('boolean'),
+const ALGORITHMS = ['SHA1', 'SHA256', 'SHA512'];
+const DIGITS = [6, 8];
 
-  backend: attr('string'),
+const validations = {
+  account_name: [
+    { type: 'presence', message: "Account name can't be blank." },
+    {
+      type: 'containsWhiteSpace',
+      message:
+        "Account name contains whitespace. If this is desired, you'll need to encode it with %20 in API requests.",
+      level: 'warn',
+    },
+  ],
+  algorithm: [
+    {
+      validator: (value) => ALGORITHMS.includes(value),
+      message: 'Algorithm must be one of ' + ALGORITHMS.join(', '),
+    },
+  ],
+  digits: [
+    { validator: (value) => DIGITS.includes(value), message: 'Digits must be one of ' + DIGITS.join(', ') },
+  ],
+  period: [{ type: 'number', message: 'Period must be a number.' }],
+};
 
-  name: alias('account_name'),
+@withModelValidations(validations)
+export default class TotpModel extends Model {
+  @attr('string') backend;
 
-  account_name: attr('string', {
-    readOnly: true,
-  }),
-  algorithm: attr('string', {
-    readOnly: true,
-    possibleValues: ['SHA1', 'SHA256', 'SHA512'],
-  }),
-  digits: attr('number', {
-    readOnly: true,
-    possibleValues: [6, 8],
-  }),
-  issuer: attr('string', {
-    readOnly: true,
-  }),
-  period: attr('number', {
-    readOnly: true,
-  }),
+  @alias('account_name') name;
 
-  url: attr('string'),
+  @attr('string', {
+    fieldValue: 'name',
+    readOnly: true,
+  })
+  account_name;
+  @attr('string', {
+    readOnly: true,
+    possibleValues: ALGORITHMS,
+  })
+  algorithm;
+  @attr('number', {
+    readOnly: true,
+    possibleValues: DIGITS,
+  })
+  digits;
+  @attr('string', {
+    readOnly: true,
+  })
+  issuer;
+  @attr('number', {
+    readOnly: true,
+  })
+  period;
 
-  attrs: computed('account_name', function () {
+  @attr('string') url;
+
+  @computed('account_name', function () {
     const keys = ['account_name', 'algorithm', 'digits', 'issuer', 'period'];
     return expandAttributeMeta(this, keys);
-  }),
+  })
+  attrs;
 
-  createAttrs: computed('url', function () {
+  @computed('url', function () {
     const keys = ['url'];
     return expandAttributeMeta(this, keys);
-  }),
+  })
+  createAttrs;
 
-  fieldGroups: computed('account_name', function () {
+  @computed('account_name', function () {
     const defaultFields = ['account_name', 'algorithm', 'digits', 'issuer', 'period'];
     const groups = [
       { default: defaultFields },
@@ -59,9 +88,11 @@ export default Model.extend({
       //},
     ];
     return fieldToAttrs(this, groups);
-  }),
-  keyPath: lazyCapabilities(apiPath`${'backend'}/keys/${'id'}`, 'backend', 'id'),
-  canRead: alias('keyPath.canRead'),
-  canUpdate: alias('keyPath.canUpdate'),
-  canDelete: alias('keyPath.canDelete'),
-});
+  })
+  fieldGroups;
+
+  @lazyCapabilities(apiPath`${'backend'}/keys/${'id'}`, 'backend', 'id') keyPath;
+  @alias('keyPath.canRead') canRead;
+  @alias('keyPath.canUpdate') canUpdate;
+  @alias('keyPath.canDelete') canDelete;
+}
