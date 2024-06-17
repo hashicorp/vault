cf72bf242fd3f73887dcc2281d8c2a36d6918cdf
Noelle Daley <noelledaley@users.noreply.github.com>
2024-06-17 13:07:14 -0700

    tests: verify expirationEpoch is calculated when only expiry_time is passed in

--- ui/tests/unit/services/auth-test.js
+++ ui/tests/unit/services/auth-test.js
@@ -11,17 +11,13 @@ module('Unit | Service | auth', function (hooks) {
 
   [
     ['#calculateExpiration w/ttl', { ttl: 30 }, 30],
-    ['#calculateExpiration w/lease_duration', { ttl: 15 }, 15],
+    ['#calculateExpiration w/lease_duration', { lease_duration: 15 }, 15],
   ].forEach(([testName, response, ttlValue]) => {
     test(testName, function (assert) {
       const now = Date.now();
-      const service = this.owner.factoryFor('service:auth').create({
-        now() {
-          return now;
-        },
-      });
+      const service = this.owner.lookup('service:auth');
 
-      const resp = service.calculateExpiration(response);
+      const resp = service.calculateExpiration(response, now);
 
       assert.strictEqual(resp.ttl, ttlValue, 'returns the ttl');
       assert.strictEqual(
@@ -30,5 +26,22 @@ module('Unit | Service | auth', function (hooks) {
         'calculates expiration from ttl as epoch timestamp'
       );
     });
+  });
+
+  test('#calculateExpiration w/ expire_time', function (assert) {
+    const now = Date.now();
+    const expirationString = '2024-06-13T09:10:21-07:00';
+    const expectedExpirationEpoch = new Date(expirationString).getTime();
+
+    const service = this.owner.lookup('service:auth');
+
+    const resp = service.calculateExpiration({ ttl: 30, expire_time: '2024-06-13T09:10:21-07:00' }, now);
+
+    assert.strictEqual(resp.ttl, 30, 'returns ttl');
+    assert.strictEqual(
+      resp.tokenExpirationEpoch,
+      expectedExpirationEpoch,
+      'calculates expiration from expire_time'
+    );
   });
 });
