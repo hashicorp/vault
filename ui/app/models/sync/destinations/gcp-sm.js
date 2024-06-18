455098fd12908344a6b5b5595163bff52e467106
Angel Garbarino <argarbarino@gmail.com>
2024-06-18 09:22:48 -0600

    comment

--- ui/app/models/sync/destinations/gcp-sm.js
+++ ui/app/models/sync/destinations/gcp-sm.js
@@ -37,8 +37,7 @@ export default class SyncDestinationsGoogleCloudSecretManagerModel extends SyncD
     editType: 'file',
     docLink: '/vault/docs/secrets/gcp#authentication',
   })
-  credentials; // obfuscated, never returned by API
-  // ARG TODO DOUBLE CHECK
+  credentials; // obfuscated, never returned by API. Masking handled by EnableInput component
 
   @attr('object', {
     subText:
