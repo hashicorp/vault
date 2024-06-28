39d209e651ec8db0c2c40024d972cc7807dc8dab
divyaac <divyaac@berkeley.edu>
2024-06-28 13:44:04 +0530

    Revert "added a comment"
    This reverts commit 1aff98f90b9b5501f3e15cbbea55c519f17425f7.

--- vault/activity_log_test.go
+++ vault/activity_log_test.go
@@ -35,7 +35,6 @@ import (
 )
 
 // TestActivityLog_Creation calls AddEntityToFragment and verifies that it appears correctly in a.fragment.
-// test comment
 func TestActivityLog_Creation(t *testing.T) {
 	core, _, _ := TestCoreUnsealed(t)
 
