428ebedf626fa701d3c90fdda6a98a8c23e8b4e6
Chelsea Shaw <cshaw@hashicorp.com>
2024-07-09 11:07:12 -0500

    Add coverage for payload and URL values for each auth type

--- ui/tests/acceptance/auth-test.js
+++ ui/tests/acceptance/auth-test.js
@@ -5,7 +5,7 @@
 
 import { module, test } from 'qunit';
 import { setupApplicationTest } from 'ember-qunit';
-import { click, currentURL, visit, waitUntil, find } from '@ember/test-helpers';
+import { click, currentURL, visit, waitUntil, find, fillIn } from '@ember/test-helpers';
 import { supportedAuthBackends } from 'vault/helpers/supported-auth-backends';
 import authForm from '../pages/components/auth-form';
 import jwtForm from '../pages/components/auth-jwt';
@@ -118,6 +118,125 @@ module('Acceptance | auth', function (hooks) {
         if (isOidc) {
           await jwtComponent.role('test');
         }
+        await component.login();
+      });
+    }
+  });
+
+  module('it sends the right payload when authenticating', function (hooks) {
+    hooks.beforeEach(function () {
+      this.assertReq = () => {};
+      this.server.get('/auth/token/lookup-self', (schema, req) => {
+        this.assertReq(req);
+        req.passthrough();
+      });
+      this.server.post('/auth/:mount/login', (schema, req) => {
+        // This one is for github only
+        this.assertReq(req);
+        req.passthrough();
+      });
+      this.server.post('/auth/:mount/oidc/auth_url', (schema, req) => {
+        // For JWT and OIDC
+        this.assertReq(req);
+        req.passthrough();
+      });
+      this.server.post('/auth/:mount/login/:username', (schema, req) => {
+        this.assertReq(req);
+        req.passthrough();
+      });
+      this.expected = {
+        token: {
+          url: '/v1/auth/token/lookup-self',
+          payload: {
+            'X-Vault-Token': 'some-token',
+          },
+        },
+        userpass: {
+          url: '/v1/auth/custom-userpass/login/some-username',
+          payload: {
+            password: 'some-password',
+          },
+        },
+        ldap: {
+          url: '/v1/auth/custom-ldap/login/some-username',
+          payload: {
+            password: 'some-password',
+          },
+        },
+        okta: {
+          url: '/v1/auth/custom-okta/login/some-username',
+          payload: {
+            password: 'some-password',
+          },
+        },
+        jwt: {
+          url: '/v1/auth/custom-jwt/oidc/auth_url',
+          payload: {
+            redirect_uri: 'http://localhost:7357/ui/vault/auth/custom-jwt/oidc/callback',
+            role: 'some-role',
+          },
+        },
+        oidc: {
+          url: '/v1/auth/custom-oidc/oidc/auth_url',
+          payload: {
+            redirect_uri: 'http://localhost:7357/ui/vault/auth/custom-oidc/oidc/callback',
+            role: 'some-role',
+          },
+        },
+        radius: {
+          url: '/v1/auth/custom-radius/login/some-username',
+          payload: {
+            password: 'some-password',
+          },
+        },
+        github: {
+          url: '/v1/auth/custom-github/login',
+          payload: {
+            token: 'some-token',
+          },
+        },
+      };
+    });
+
+    for (const backend of supportedAuthBackends().reverse()) {
+      test(`for ${backend.type}`, async function (assert) {
+        const { type } = backend;
+        const expected = this.expected[type];
+        const isOidc = ['oidc', 'jwt'].includes(type);
+        assert.expect(isOidc ? 6 : 2);
+
+        this.assertReq = (req) => {
+          const body = type === 'token' ? req.requestHeaders : JSON.parse(req.requestBody);
+          if (isOidc && !body.role) {
+            // OIDC and JWT auth form calls the endpoint every time the role or mount is updated.
+            // if role is not provided, it means we haven't filled out the full info yet so don't
+            // validate the payload until all data is provided
+            // eslint-disable-next-line qunit/no-early-return
+            return {};
+          }
+          assert.strictEqual(req.url, expected.url, `${type} calls the correct URL`);
+          Object.keys(expected.payload).forEach((expKey) => {
+            assert.strictEqual(
+              body[expKey],
+              expected.payload[expKey],
+              `${type} payload includes ${expKey} with expected value`
+            );
+          });
+        };
+        await visit('/vault/auth');
+        await component.selectMethod(type);
+
+        if (type !== 'token') {
+          // set custom mount
+          await click('[data-test-auth-form-options-toggle]');
+          await fillIn('[data-test-auth-form-mount-path]', `custom-${type}`);
+        }
+        backend.formAttributes.forEach(async (key) => {
+          // fill in all form items, except JWT which is not rendered
+          if (key === 'jwt') return;
+          await fillIn(`[data-test-${key}]`, `some-${key}`);
+        });
+
         await component.login();
       });
     }
