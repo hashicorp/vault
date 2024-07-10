06c420d2ee42c84cdaf1984ac120420c3609ded9
Angel Garbarino <Monkeychip@users.noreply.github.com>
2024-07-10 09:16:21 -0600

    KVv2 json cursor jumps on "enter" (#27569)
    * it works...but does it break everything else?
    
    * Update code-mirror.js
    
    * Update code-mirror.js
    
    * return to original
    
    * changelog
    
    * different approach to move onto parse at create and edit. it breaks things, hopefully fixed in next commits
    
    * use onBlur event on codemirrror
    
    * maybe? lets run the tests and find out
    
    * update comments
    
    * wip for conditional to only compare on kvv2
    
    * remove onblur leftovers
    
    * missed two
    
    * clean up
    
    * test coverage
    
    * try catch logical operator instead
    
    * stringify helper and not native json stringify to maintain object shape
    
    * remove comment
    
    * Update json-editor.js
    
    return brackets do not want issues with backports
    
    * Update json-editor.js
    
    * Update json-editor.js
    
    * Test fix
    
    * maybe
    
    * more specific cursor test
    
    * json-editor test cleanup
    
    * Delete ui/testrun1.txt
    
    * Delete ui/testrun2.txt
    
    * remove non json test it doesn't test anything
    
    * update test and comment for how it's testing non-json content
    
    * test fix
    
    * put shape of json blob back:
    
    * send in original without parsing or stringify
    
    * welp friday things

--- ui/tests/helpers/general-selectors.ts
+++ ui/tests/helpers/general-selectors.ts
@@ -81,4 +81,6 @@ export const GENERAL = {
   cancelButton: '[data-test-cancel]',
   saveButton: '[data-test-save]',
   maskedInput: (name: string) => `[data-test-textarea="${name}"]`,
+  codemirror: `[data-test-component="code-mirror-modifier"]`,
+  codemirrorTextarea: `[data-test-component="code-mirror-modifier"] textarea`,
 };
