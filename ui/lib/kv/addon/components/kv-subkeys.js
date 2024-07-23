94f1e9bc4036420d3998e20254c9c590642d5948
claire bontempo <cbontempo@hashicorp.com>
2024-07-23 13:13:34 -0700

    update jsdoc

--- ui/lib/kv/addon/components/kv-subkeys.js
+++ ui/lib/kv/addon/components/kv-subkeys.js
@@ -9,18 +9,29 @@ import { tracked } from '@glimmer/tracking';
 /**
  * @module KvSubkeys
  * @description
- * ```
- * sample subkeys:
-  {
+sample secret data:
+```
+ {
+  "foo": "abc",
+  "bar": {
+    "baz": "def"
+  },
+  "quux": {}
+}
+```
+sample subkeys:
+```
+ this.subkeys = {
     "bar": {
         "baz": null
     },
     "foo": null,
     "quux": null
 }
- * ```
+```
  * 
- * <KvSubkeys @subkeys={{@subkeys}} />
+ * @example
+ * <KvSubkeys @subkeys={{this.subkeys}} />
  *
  * @param {object} subkeys - leaf keys of a kv v2 secret, all values (unless a nested object with more keys) return null
  */
