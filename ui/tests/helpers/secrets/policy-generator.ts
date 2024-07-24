65c4c2d40302f75505b77fc4e950a958b88a678b
Angel Garbarino <Monkeychip@users.noreply.github.com>
2024-07-24 10:01:11 -0600

    Update policy-generator.ts

--- ui/tests/helpers/secrets/policy-generator.ts
+++ ui/tests/helpers/secrets/policy-generator.ts
@@ -3,8 +3,8 @@
  * SPDX-License-Identifier: BUSL-1.1
  */
 
-// This is a policy can both mount a secret engine
-// and list and create oidc keys, relevant for setting identity_key_token for WIF
+// This is a policy that can mount a secret engine and list and create oidc keys
+// Relevant for setting identity_key_token for WIF
 export const adminPolicy = () => {
   return `
     path "sys/mounts/*" {
@@ -16,8 +16,8 @@ export const adminPolicy = () => {
   `;
 };
 
-// user can mount the engine
-// but does not have access to oidc/key list or create
+// User can mount the engine
+// But does not have access to oidc/key list or create
 export const noOidcAdminPolicy = () => {
   return `
     path "sys/mounts/*" {
