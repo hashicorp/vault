f6f8d690130a83a3ff1338e8360fbd64c10006cb
Ehsan Khakifirooz <ehsankhfr@gmail.com>
2024-08-04 18:23:09 +0100

    Add api/auth tests

--- api/auth_test.go
+++ api/auth_test.go
@@ -47,6 +47,13 @@ func TestAuth_Login(t *testing.T) {
 			return
 		}
 	})
+
+	t.Run("Login with nil AuthMethod should return error", func(t *testing.T) {
+		_, err := a.Login(context.Background(), nil)
+		if err == nil {
+			t.Errorf("expected error when AuthMethod is nil, got none")
+		}
+	})
 }
 
 func TestAuth_MFALoginSinglePhase(t *testing.T) {
@@ -74,6 +81,33 @@ func TestAuth_MFALoginSinglePhase(t *testing.T) {
 			return
 		}
 	})
+
+	t.Run("MFALogin() with empty credentials should assume two-phase", func(t *testing.T) {
+		a := &Auth{
+			c: &Client{},
+		}
+
+		m := mockAuthMethod{
+			mockedSecret: &Secret{
+				Auth: &SecretAuth{
+					MFARequirement: &MFARequirement{
+						MFARequestID: "a-req-id",
+					},
+				},
+			},
+			mockedError: nil,
+		}
+
+		secret, err := a.MFALogin(context.Background(), &m)
+		if err != nil {
+			t.Errorf("MFALogin() error %v", err)
+			return
+		}
+
+		if secret.Auth.MFARequirement.MFARequestID != "a-req-id" {
+			t.Errorf("MFARequirement ID was %v expected %v", secret.Auth.MFARequirement.MFARequestID, "a-req-id")
+		}
+	})
 }
 
 func TestAuth_MFALoginTwoPhase(t *testing.T) {
@@ -81,7 +115,6 @@ func TestAuth_MFALoginTwoPhase(t *testing.T) {
 		name    string
 		a       *Auth
 		m       *mockAuthMethod
-		creds   *string
 		wantErr bool
 	}{
 		{
@@ -122,10 +155,63 @@ func TestAuth_MFALoginTwoPhase(t *testing.T) {
 				return
 			}
 
+			if tt.wantErr {
+				if secret != nil {
+					t.Errorf("MFALogin() returned non-nil secret when error was expected")
+				}
+				return
+			}
+
 			if secret.Auth.MFARequirement != tt.m.mockedSecret.Auth.MFARequirement {
 				t.Errorf("MFALogin() returned %v, expected %v", secret.Auth.MFARequirement, tt.m.mockedSecret.Auth.MFARequirement)
 				return
 			}
 		})
 	}
+}
+
+func TestAuth_MFAValidate(t *testing.T) {
+	a := &Auth{
+		c: &Client{},
+	}
+
+	t.Run("MFAValidate should return error if secret is nil", func(t *testing.T) {
+		_, err := a.MFAValidate(context.Background(), nil, nil)
+		if err == nil {
+			t.Errorf("expected error when secret is nil, got none")
+		}
+	})
+
+	t.Run("MFAValidate should return error if MFARequirement is nil", func(t *testing.T) {
+		secret := &Secret{
+			Auth: &SecretAuth{},
+		}
+		_, err := a.MFAValidate(context.Background(), secret, nil)
+		if err == nil {
+			t.Errorf("expected error when MFARequirement is nil, got none")
+		}
+	})
+}
+
+func TestCheckAndSetToken(t *testing.T) {
+	a := &Auth{
+		c: &Client{},
+	}
+
+	t.Run("checkAndSetToken should return error if secret is nil", func(t *testing.T) {
+		_, err := a.checkAndSetToken(nil)
+		if err == nil {
+			t.Errorf("expected error when secret is nil, got none")
+		}
+	})
+
+	t.Run("checkAndSetToken should return error if ClientToken is missing", func(t *testing.T) {
+		secret := &Secret{
+			Auth: &SecretAuth{},
+		}
+		_, err := a.checkAndSetToken(secret)
+		if err == nil {
+			t.Errorf("expected error when ClientToken is missing, got none")
+		}
+	})
 }
