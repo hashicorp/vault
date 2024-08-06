ca2623386cdd70e7ee8aa155032af4976318a197
claire bontempo <cbontempo@hashicorp.com>
2024-08-05 22:04:45 -0700

    wip add two on blur functions

--- ui/lib/kv/addon/components/kv-patch-editor.js
+++ ui/lib/kv/addon/components/kv-patch-editor.js
@@ -43,6 +43,10 @@ class KvData {
     this._kvData = newObject;
   }
 
+  get(key) {
+    return this._kvData[key];
+  }
+
   deleteKey(key) {
     const newObject = { ...this._kvData };
     delete newObject[key];
@@ -58,7 +62,9 @@ export default class KvPatchEditor extends Component {
   @tracked newValue = '';
 
   getState = (key) => this.state.get(key);
-  isExistingKey = (key) => Object.keys(this.args.kvData).includes(key);
+  getValue = (key) => this.patchData.get(key);
+
+  isOriginalKey = (key) => Object.keys(this.args.kvData).includes(key);
 
   constructor() {
     super(...arguments);
@@ -73,39 +79,71 @@ export default class KvPatchEditor extends Component {
   @action
   setState(key, status) {
     this.state.set(key, status);
+    if (status === 'deleted') {
+      this.patchData.set(key, null);
+    }
   }
 
   @action
   handleCancel(key) {
-    if (this.isExistingKey(key)) {
-      this.state.set(key, 'disabled');
+    if (this.isOriginalKey(key)) {
+      this.setState(key, 'disabled');
+      this.patchData.set(key, '');
     } else {
       this.patchData.deleteKey(key);
     }
   }
 
   @action
-  onBlur(event) {
+  onBlurNew(event) {
     const { name, value } = event.target;
     this[name] = value;
   }
 
   @action
-  addData(key, value) {
-    this.patchData.set(key, value);
+  onBlur(key, type, event) {
+    if (type === 'key') {
+      // store value of original key
+      const value = this.patchData.get(key);
+      const newKey = event.target.value;
+      // delete old key
+      this.patchData.deleteKey(key);
+      // add new one
+      this.patchData.set(newKey, value);
+    }
+    if (type === 'value') {
+      const value = event.target.value;
+      this.patchData.set(key, value);
+    }
+  }
+
+  @action
+  handleAddClick() {
+    this.patchData.set(this.newKey, this.newValue);
+    this.setState(this.newKey, 'enabled');
+    // reset tracked values after adding them to patchData
     this.newKey = '';
     this.newValue = '';
-    this.state.set(key, 'enabled');
   }
 
   @action
   submit(event) {
+    event.preventDefault();
+    // check the last row for data, add it the payload if there are values
+    if (this.newKey && this.newValue) {
+      this.patchData.set(this.newKey, this.newValue);
+    }
+
+    const data = this.patchData._kvData;
+    for (const [key, value] of Object.entries(this.patchData._kvData)) {
+      if (value === '') {
+        delete data[key];
+      }
+    }
     // if a user doesn't click add make sure we include the final row of key/values
     // if there are matching keys, show the validation warning and disable the add
     // and prevent it overriding the existing key/value onBlur
-    event.preventDefault();
-    const patchData = new FormData(event.target);
-    const data = Object.fromEntries(patchData.entries());
-    this.args.onSubmit(data);
+    // console.log(data, 'tracked patch data!!');
+    this.args.onSubmit(this.patchData);
   }
 }
