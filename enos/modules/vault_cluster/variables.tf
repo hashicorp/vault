1f85038b7ff4b136da0b5b40947de7680397d5b1
Ikko Eltociear Ashimine <eltociear@gmail.com>
2024-08-08 06:13:00 +0900

    Fix typo in variables.tf (#27693)
    intialize -> initialize
    
    Co-authored-by: akshya96 <87045294+akshya96@users.noreply.github.com>

--- enos/modules/vault_cluster/variables.tf
+++ enos/modules/vault_cluster/variables.tf
@@ -220,7 +220,7 @@ variable "release" {
 
 variable "root_token" {
   type        = string
-  description = "The Vault root token that we can use to intialize and configure the cluster"
+  description = "The Vault root token that we can use to initialize and configure the cluster"
   default     = null
 }
 
