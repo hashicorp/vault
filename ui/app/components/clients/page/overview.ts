76793cecd910595aa2e8f6fd67eee847cdbeeb7d
Chelsea Shaw <cshaw@hashicorp.com>
2024-08-14 14:17:48 -0500

    update overview component and tests

--- ui/app/components/clients/page/overview.ts
+++ ui/app/components/clients/page/overview.ts
@@ -5,58 +5,61 @@
 
 import ActivityComponent from '../activity';
 import { service } from '@ember/service';
-import { hasMountsKey, hasNamespacesKey } from 'core/utils/client-count-utils';
+import { ByNamespaceClients, MountClients } from 'core/utils/client-count-utils';
+import { sanitizePath } from 'core/utils/sanitize-path';
 import type FlagsService from 'vault/services/flags';
+import type NamespaceService from 'vault/services/namespace';
 
 export default class ClientsOverviewPageComponent extends ActivityComponent {
   @service declare readonly flags: FlagsService;
+  @service declare readonly namespace: NamespaceService;
 
-  // (object) single month new client data with total counts and array of
-  // either namespaces or mounts
-  get newClientCounts() {
-    if (this.isDateRange || this.byMonthActivityData.length === 0) {
-      return null;
-    }
-
-    return this.byMonthActivityData[0]?.new_clients;
+  get showNewClientAttribution(): boolean {
+    return this.args.activity.byMonth.length === 1;
   }
 
-  // total client data for horizontal bar chart in attribution component
-  get totalClientAttribution() {
-    const { namespace, activity } = this.args;
-    if (namespace) {
-      return this.filteredActivityByNamespace?.mounts || null;
-    } else {
-      return activity.byNamespace || null;
+  get newClientNamespaceAttribution(): ByNamespaceClients[] | undefined {
+    if (this.showNewClientAttribution) {
+      return this.args.activity.byMonth[0]?.namespaces;
     }
+    return;
   }
 
-  // new client data for horizontal bar chart
-  get newClientAttribution() {
-    // new client attribution only available in a single, historical month (not a date range or current month)
-    if (this.isDateRange || this.isCurrentMonth || !this.newClientCounts) return null;
-
-    const newCounts = this.newClientCounts;
-    if (this.args.namespace && hasMountsKey(newCounts)) return newCounts?.mounts;
-
-    if (hasNamespacesKey(newCounts)) return newCounts?.namespaces;
-
-    return null;
+  get newClientMountAttribution(): MountClients[] | undefined {
+    if (this.showNewClientAttribution) {
+      const currentNs = this.namespace.currentNamespace;
+      const nsLabel = sanitizePath(this.args.namespace || currentNs || 'root');
+      const singleNamespace = this.newClientNamespaceAttribution?.find(
+        (namespace) => sanitizePath(namespace.label) === nsLabel
+      );
+      return singleNamespace?.mounts;
+    }
+    return;
   }
 
   get hasAttributionData() {
-    const { mountPath, namespace } = this.args;
-    if (!mountPath) {
-      if (namespace) {
-        const mounts = this.filteredActivityByNamespace?.mounts?.map((mount) => ({
-          id: mount.label,
-          name: mount.label,
-        }));
-        return mounts && mounts.length > 0;
-      }
-      return !!this.totalClientAttribution && this.totalUsageCounts && this.totalUsageCounts.clients !== 0;
-    }
+    // TODO: update this logic. when do we hide attribution?
+    if (this.args.mountPath) return false;
+    return true;
+    // const { mountPath, namespace } = this.args;
+    // if (!mountPath) {
+    //   if (namespace) {
+    //     const mounts = this.filteredActivityByNamespace?.mounts?.map((mount) => ({
+    //       id: mount.label,
+    //       name: mount.label,
+    //     }));
+    //     return mounts && mounts.length > 0;
+    //   }
+    //   return !!this.totalClientAttribution && this.totalUsageCounts && this.totalUsageCounts.clients !== 0;
+    // }
 
-    return false;
+    // return false;
+  }
+
+  get namespaceMountAttribution() {
+    const { activity, namespace } = this.args;
+    const currentNs = this.namespace.currentNamespace;
+    const nsLabel = sanitizePath(namespace || currentNs || 'root');
+    return activity.byNamespace?.find((ns) => sanitizePath(ns.label) === nsLabel)?.mounts;
   }
 }
