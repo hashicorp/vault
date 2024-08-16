c16becad8038485eb88b224341d88ac456215b45
claire bontempo <cbontempo@hashicorp.com>
2024-08-16 10:09:46 -0700

    add overview page and test

---
+++ ui/lib/kv/addon/components/page/secret/overview.js
@@ -0,0 +1,47 @@
+/**
+ * Copyright (c) HashiCorp, Inc.
+ * SPDX-License-Identifier: BUSL-1.1
+ */
+
+import Component from '@glimmer/component';
+import { dateFormat } from 'core/helpers/date-format';
+
+/**
+ * @module KvSecretOverview
+ * <Page::Secret::Overview
+ * @backend={{this.model.backend}}
+ * @breadcrumbs={{this.breadcrumbs}}
+ * @canReadMetadata={{true}}
+ * @canUpdateSecret={{true}}
+ * @isDeleted={{false}}
+ * @isDestroyed={{false}}
+ * @metadata={{this.model.metadata}}
+ * @path={{this.model.path}}
+ * @subkeys={{this.model.subkeys}}
+ * />
+ *
+ * @param {string} backend - kv secret mount to make network request
+ * @param {array} breadcrumbs - Array to generate breadcrumbs, passed to the page header component
+ * @param {boolean} canReadMetadata - permissions to read metadata
+ * @param {boolean} canUpdateSecret - permissions to create a new version of a secret
+ * @param {boolean} isDeleted - whether the latest version of a secret has been deleted
+ * @param {boolean} isDestroyed - whether the latest version of a secret has been destroyed
+ * @param {model} metadata - Ember data model: 'kv/metadata'
+ * @param {string} path - path to request secret data for selected version
+ * @param {object} subkeys - API response from subkeys endpoint, object with "subkeys" and "metadata" keys
+ */
+
+export default class KvSecretOverview extends Component {
+  get versionSubtext() {
+    if (this.args.isDestroyed) {
+      return 'The current version of this secret has been permanently deleted and cannot be restored.';
+    }
+    if (this.args.isDeleted) {
+      const time =
+        this.args.metadata?.currentSecret.deletionTime || this.args.subkeys?.metadata.deletion_time;
+      const date = dateFormat([time], {});
+      return `The current version of this secret was deleted ${date}.`;
+    }
+    return 'The current version of this secret.';
+  }
+}
