0e7029b22ef37e9fafcfa9dbcb09cc57a3a5327d
Ryan Cragun <me@ryan.ec>
2024-08-23 16:13:26 -0600

    ui: triple the browser_disconnect_timeout
    This is total shot in the dark to see if increasing our disconnect
    timeout will fix a flaky test that always seems to hit the 10 second
    disconnect timeout.
    
    Signed-off-by: Ryan Cragun <me@ryan.ec>

--- ui/testem.js
+++ ui/testem.js
@@ -12,6 +12,7 @@ module.exports = {
   disable_watching: true,
   launch_in_ci: ['Chrome'],
   browser_start_timeout: 120,
+  browser_disconnect_timeout: 30,
   browser_args: {
     Chrome: {
       ci: [
