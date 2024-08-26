b9adc0bb4eb5179d80b87f22f386db3e2a7db592
Angel Garbarino <argarbarino@gmail.com>
2024-08-26 16:02:01 -0600

    focus on role aws edit

--- ui/app/components/role-aws-edit.js
+++ ui/app/components/role-aws-edit.js
@@ -3,53 +3,104 @@
  * SPDX-License-Identifier: BUSL-1.1
  */
 
+import { service } from '@ember/service';
+import { action } from '@ember/object';
+import Component from '@glimmer/component';
+import { tracked } from '@glimmer/tracking';
 import { isBlank } from '@ember/utils';
-import { set } from '@ember/object';
-import RoleEdit from './role-edit';
-const SHOW_ROUTE = 'vault.cluster.secrets.backend.show';
+import errorMessage from 'vault/utils/error-message';
 
-export default RoleEdit.extend({
-  actions: {
-    createOrUpdate(type, event) {
-      event.preventDefault();
+/**
+ * @module ConfigureAwsComponent is generated from the SecretEditLayout route template. It is used to create, edit or view the role-aws model.
+ *
+ * @example
+ * ```js
+ * {{component
+  (get (options-for-backend this.backendType this.model.idPrefix) "editComponent")
+  model=this.model
+  mode=this.mode
+}}
+ * ```
+ *
+ * @param {string} mode - "create" or "edit" or "show"
+ * @param {object} model - role-aws model
+ */
 
-      // all of the attributes with fieldValue:'id' are called `name`
-      const modelId = this.model.id || this.model.name;
-      // prevent from submitting if there's no key
-      // maybe do something fancier later
-      if (type === 'create' && isBlank(modelId)) {
-        return;
-      }
-      var credential_type = this.model.credential_type;
-      if (credential_type == 'iam_user') {
-        set(this, 'model.role_arns', []);
-      }
-      if (credential_type == 'assumed_role') {
-        set(this, 'model.policy_arns', []);
-      }
-      if (credential_type == 'federation_token') {
-        set(this, 'model.role_arns', []);
-        set(this, 'model.policy_arns', []);
-      }
+export default class RoleAwsEdit extends Component {
+  @tracked errorMessage = null;
+  @tracked invalidFormAlert = null;
+  @tracked lintingErrors = null;
+  @service router;
 
-      var policy_document = this.model.policy_document;
-      if (policy_document == '{}') {
-        set(this, 'model.policy_document', '');
-      }
+  get breadcrumbs() {
+    const { backend } = this.args.model;
+    return [
+      { label: 'Secrets', route: 'vault.cluster.secrets.backends' },
+      {
+        label: backend,
+        route: 'vault.cluster.secrets.backend',
+        model: backend,
+      },
+      { label: this.args.mode },
+    ];
+  }
 
-      this.persist('save', () => {
-        this.hasDataChanges();
-        this.transitionToRoute(SHOW_ROUTE, modelId);
-      });
-    },
+  get haveAttributesChanged() {
+    const { model } = this.args;
+    return Object.keys(model.changedAttributes()).some((key) => key === 'backend') ? false : true;
+  }
 
-    codemirrorUpdated(attr, val, codemirror) {
-      codemirror.performLint();
-      const hasErrors = codemirror.state.lint.marked.length > 0;
+  @action
+  async createOrUpdate(event) {
+    event.preventDefault();
+    // all of the attributes with fieldValue:'id' are called `name`
+    const { model } = this.args;
+    const { credential_type, backend } = model;
 
-      if (!hasErrors) {
-        set(this.model, attr, val);
-      }
-    },
-  },
-});
+    if (this.args.mode === 'create' && isBlank(backend)) {
+      return;
+    }
+    // only save if there are changes
+    const attrChanged =
+      Object.keys(model.changedAttributes()).filter((item) => item !== 'backend').length > 0;
+    if (!attrChanged) return; // todo flash message and transition
+
+    if (credential_type === 'iam_user') {
+      model.role_arns = [];
+    }
+    if (credential_type === 'assumed_role') {
+      model.policy_arns = [];
+    }
+    if (credential_type === 'federation_token') {
+      model.role_arns = model.policy_arns = [];
+      [];
+    }
+    if (model.policy_document === '{}') {
+      model.policy_document = '';
+    }
+
+    try {
+      await model.save();
+      this.router.transitionTo('vault.cluster.secrets.backend.credentials', backend);
+    } catch (e) {
+      this.errorMessage = errorMessage(e);
+      this.invalidFormAlert = 'There was an error submitting this form.';
+    }
+  }
+
+  @action
+  codemirrorUpdated(attr, val, codemirror) {
+    codemirror.performLint();
+    this.lintingErrors = codemirror.state.lint.marked.length > 0;
+    if (!this.lintingErrors) {
+      this.args.model[attr] = val;
+    }
+  }
+
+  @action
+  delete() {
+    this.args.model.destroyRecord().then(() => {
+      this.router.transitionTo('vault.cluster.secrets.backend.list-root');
+    });
+  }
+}
--- ui/lib/core/addon/components/secret-list-header.js
+++ ui/lib/core/addon/components/secret-list-header.js
@@ -7,17 +7,16 @@ import Component from '@glimmer/component';
 
 /**
  * @module SecretListHeader
- * SecretListHeader component is breadcrumb, title with icon and menu with tabs component. Hello
+ * SecretListHeader component is by Secret Engine routes to show a routes breadcrumb, title with an icon, and menu with tabs.
  *
  * Example is wrapped in back ticks because this component relies on routing and cannot render an isolated sample, so just rendering template sample
  * @example
  * ```
- * <SecretListHeader @isCertTab={{eq this.tab "cert"}} @model={{this.model}} @baseKey={{this.baseKey}} @breadcrumbs={{this.breadcrumbs}} @filter={{this.filter}} />
+ * <SecretListHeader @model={{this.model}} @breadcrumbs={{this.breadcrumbs}} />
  * ```
  *
  * @param {object} model - Model used to pull information about icon and title and backend type for navigation.
  * @param {array} breadcrumbs - An array of objects which represent the breadcrumbs for the current path. Breadcrumbs should be set on the controller by the route.
-//  * ARGTODO flush out
  */
 
 export default class SecretListHeader extends Component {
