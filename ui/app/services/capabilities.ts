3e0d94333c45d87a1697a749ebba8f2f117486d4
claire bontempo <cbontempo@hashicorp.com>
2024-08-29 13:54:49 -0700

    cleanup fetchMultiplePaths method

--- ui/app/services/capabilities.ts
+++ ui/app/services/capabilities.ts
@@ -41,32 +41,31 @@ export default class CapabilitiesService extends Service {
   }
 
   async fetchMultiplePaths(paths: string[]): MultipleCapabilities | AdapterError {
-    try {
-      const resp: Array<CapabilitiesModel> = await this.request({ paths });
-      return paths.reduce((obj: MultipleCapabilities, apiPath: string) => {
-        // path is the model's primaryKey (id)
-        const model: CapabilitiesModel | undefined = resp.find((m) => m.path === apiPath);
-        if (model) {
-          const { canCreate, canDelete, canList, canPatch, canRead, canSudo, canUpdate } = model;
-          obj[apiPath] = { canCreate, canDelete, canList, canPatch, canRead, canSudo, canUpdate };
-        } else {
-          // default to true if there is a problem fetching the model
-          // since we can rely on the API to gate as a fallback
-          obj[apiPath] = {
-            canCreate: true,
-            canDelete: true,
-            canList: true,
-            canPatch: true,
-            canRead: true,
-            canSudo: true,
-            canUpdate: true,
-          };
-        }
-        return obj;
-      }, {});
-    } catch (e) {
-      return e;
-    }
+    // if the request to capabilities-self fails, silently catch
+    // all of path capabilities default to "true"
+    const resp: Array<CapabilitiesModel> | [] = await this.request({ paths }).catch(() => []);
+
+    return paths.reduce((obj: MultipleCapabilities, apiPath: string) => {
+      // path is the model's primaryKey (id)
+      const model: CapabilitiesModel | undefined = resp.find((m) => m.path === apiPath);
+      if (model) {
+        const { canCreate, canDelete, canList, canPatch, canRead, canSudo, canUpdate } = model;
+        obj[apiPath] = { canCreate, canDelete, canList, canPatch, canRead, canSudo, canUpdate };
+      } else {
+        // default to true if there is a problem fetching the model
+        // since we can rely on the API to gate as a fallback
+        obj[apiPath] = {
+          canCreate: true,
+          canDelete: true,
+          canList: true,
+          canPatch: true,
+          canRead: true,
+          canSudo: true,
+          canUpdate: true,
+        };
+      }
+      return obj;
+    }, {});
   }
 
   /*
