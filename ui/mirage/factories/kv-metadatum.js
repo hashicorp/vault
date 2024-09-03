2b76443cfc752d5834f4ff1da3962504b185fd1f
claire bontempo <cbontempo@hashicorp.com>
2024-09-03 09:58:58 -0700

    fix factory

--- ui/mirage/factories/kv-metadatum.js
+++ ui/mirage/factories/kv-metadatum.js
@@ -59,11 +59,8 @@ export default Factory.extend({
   }),
 
   withCustomPath: trait({
-    data: {
-      ...data,
-      path(i) {
-        return `my-secret-${i}`;
-      },
+    path(i) {
+      return `my-secret-${i}`;
     },
   }),
 });
