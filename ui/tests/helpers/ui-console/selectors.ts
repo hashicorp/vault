e81670fbe361a0818bd765b5d90331a421227454
claire bontempo <cbontempo@hashicorp.com>
2024-09-03 18:11:28 -0700

    move away from page objects for runCmd

---
+++ ui/tests/helpers/ui-console/selectors.ts
@@ -0,0 +1,10 @@
+/**
+ * Copyright (c) HashiCorp, Inc.
+ * SPDX-License-Identifier: BUSL-1.1
+ */
+
+export const REPL = {
+  toggle: '[data-test-console-toggle]',
+  consoleInput: '[data-test-component="console/command-input"] input',
+  logOutputItems: '[data-test-component="console/output-log"] > div',
+};
