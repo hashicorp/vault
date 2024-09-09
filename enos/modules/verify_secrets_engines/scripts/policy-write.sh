392412829bc95ec8707f8244ad69e51da2061401
Ryan Cragun <me@ryan.ec>
2024-09-09 14:29:11 -0600

    [VAULT-30189] enos: verify identity and OIDC tokens (#28274)
    * [VAULT-30189] enos: verify identity and OIDC tokens
    
    Expand our baseline API and data verification by including the identity
    and identity OIDC tokens secrets engines. We now create a test entity,
    entity-alias, identity group, various policies, and associate them with
    the entity. For the OIDC side, we now configure the OIDC issuer, create
    and rotate named keys, create and associate roles with the named key,
    and issue and introspect tokens.
    
    During a second phase we also verify that the those some entities,
    groups, keys, roles, config, etc all exist with the expected values.
    This is useful to test durability after upgrades, migrations, etc.
    
    This change also includes new updates our prior `auth/userpass` and `kv`
    verification. We had two modules that were loosely coupled and
    interdependent. This restructures those both into a singular module with
    child modules and fixes the assumed values by requiring the read module
    to verify against the created state.
    
    Going forward we can continue to extend this secrets engine verification
    module with additional create and read checks for new secrets engines.
    
    Signed-off-by: Ryan Cragun <me@ryan.ec>

--- enos/modules/verify_secrets_engines/scripts/policy-write.sh
+++ enos/modules/verify_secrets_engines/scripts/policy-write.sh
@@ -4,38 +4,19 @@
 
 set -e
 
-retry() {
-  local retries=$1
-  shift
-  local count=0
-
-  until "$@"; do
-    exit=$?
-    wait=$((2 ** count))
-    count=$((count + 1))
-    if [ "$count" -lt "$retries" ]; then
-      sleep "$wait"
-    else
-      return "$exit"
-    fi
-  done
-
-  return 0
-}
-
 fail() {
   echo "$1" 1>&2
   exit 1
 }
 
-[[ -z "$TEST_KEY" ]] && fail "TEST_KEY env variable has not been set"
-[[ -z "$TEST_VALUE" ]] && fail "TEST_VALUE env variable has not been set"
+[[ -z "$POLICY_NAME" ]] && fail "POLICY_NAME env variable has not been set"
+[[ -z "$POLICY_CONFIG" ]] && fail "POLICY_CONFIG env variable has not been set"
 [[ -z "$VAULT_ADDR" ]] && fail "VAULT_ADDR env variable has not been set"
 [[ -z "$VAULT_INSTALL_DIR" ]] && fail "VAULT_INSTALL_DIR env variable has not been set"
 [[ -z "$VAULT_TOKEN" ]] && fail "VAULT_TOKEN env variable has not been set"
 
 binpath=${VAULT_INSTALL_DIR}/vault
-
 test -x "$binpath" || fail "unable to locate vault binary at $binpath"
 
-retry 5 "$binpath" kv put secret/test "$TEST_KEY=$TEST_VALUE"
+export VAULT_FORMAT=json
+"$binpath" policy write "$POLICY_NAME" - <<< "$POLICY_CONFIG"
