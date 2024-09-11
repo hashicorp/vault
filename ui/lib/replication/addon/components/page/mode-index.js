1f43b8bba879ff50119f6df9446873f2f31d3ce7
claire bontempo <cbontempo@hashicorp.com>
2024-09-11 16:09:09 -0700

    fix logic so default is based on undefined capabilities (not no mode)

--- ui/lib/replication/addon/components/page/mode-index.js
+++ ui/lib/replication/addon/components/page/mode-index.js
@@ -22,30 +22,19 @@ import Component from '@glimmer/component';
  * @param {string} replicationMode - should be "dr" or "performance"
  */
 export default class PageModeIndex extends Component {
-  get canEnablePrimary() {
-    const { cluster } = this.args;
-    switch (this.args.replicationMode) {
-      case 'dr':
-        return cluster.canEnablePrimaryDr;
-      case 'performance':
-        return cluster.canEnablePrimaryPerformance;
-      default:
-        // if there's a problem checking capabilities, default to true
-        // since the backend will gate as a fallback
-        return true;
+  canEnable = (type) => {
+    const { cluster, replicationMode } = this.args;
+    let perm;
+    if (replicationMode === 'dr') {
+      // returns canEnablePrimaryDr or canEnableSecondaryDr
+      perm = `canEnable${type}Dr`;
     }
-  }
-  get canEnableSecondary() {
-    const { cluster } = this.args;
-    switch (this.args.replicationMode) {
-      case 'dr':
-        return cluster.canEnableSecondaryDr;
-      case 'performance':
-        return cluster.canEnableSecondaryPerformance;
-      default:
-        // if there's a problem checking capabilities, default to true
-        // since the backend will gate as a fallback
-        return true;
+    if (replicationMode === 'performance') {
+      // returns canEnablePrimaryPerformance or canEnableSecondaryPerformance
+      perm = `canEnable${type}Performance`;
     }
-  }
+    // if there's a problem checking capabilities, default to true
+    // since the backend can gate as a fallback
+    return cluster[perm] ?? true;
+  };
 }
