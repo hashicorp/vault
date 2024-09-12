383846c7bb1b70ea8c0681d52a462067ecc992ec
hc-github-team-secure-vault-core <github-team-secure-vault-core@hashicorp.com>
2024-09-12 10:40:04 -0400

    backport of UI: Fix enabling replication capabilities bug (#28376)
    Co-authored-by: claire bontempo <68122737+hellobontempo@users.noreply.github.com>

--- ui/lib/replication/addon/controllers/index.js
+++ ui/lib/replication/addon/controllers/index.js
@@ -8,4 +8,24 @@ import { tracked } from '@glimmer/tracking';
 
 export default class ReplicationIndexController extends ReplicationModeBaseController {
   @tracked modeSelection = 'dr';
+
+  getPerm(type) {
+    if (this.modeSelection === 'dr') {
+      // returns canEnablePrimaryDr or canEnableSecondaryDr
+      return `canEnable${type}Dr`;
+    }
+    if (this.modeSelection === 'performance') {
+      // returns canEnablePrimaryPerformance or canEnableSecondaryPerformance
+      return `canEnable${type}Performance`;
+    }
+  }
+
+  // if there's a problem checking capabilities, default to true
+  // since the backend will gate as a fallback
+  get canEnablePrimary() {
+    return this.model[this.getPerm('Primary')] ?? true;
+  }
+  get canEnableSecondary() {
+    return this.model[this.getPerm('Secondary')] ?? true;
+  }
 }
