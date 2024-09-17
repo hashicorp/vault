6416b29b3c6a329ba8227a3a944b10f6f0d05996
Chelsea Shaw <cshaw@hashicorp.com>
2024-09-17 14:17:00 -0500

    add headers, remove old component

--- ui/app/utils/kmip-role-fields.js
+++ ui/app/utils/kmip-role-fields.js
@@ -1,3 +1,8 @@
+/**
+ * Copyright (c) HashiCorp, Inc.
+ * SPDX-License-Identifier: BUSL-1.1
+ */
+
 import { removeManyFromArray } from 'vault/helpers/remove-from-array';
 
 export const operationFields = (fieldNames) => {
