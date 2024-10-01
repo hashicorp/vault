71422d4cdde6272af47eb872e7936153e18ea13c
claire bontempo <68122737+hellobontempo@users.noreply.github.com>
2024-10-01 09:21:16 -0700

    UI: Fix oidc auth method missing default_role field  (#28539)
    * add auth-config/oidc to openapi model helper
    
    * alphabetize
    
    * update maskedinput selector to be standard data-test-input
    
    * add test
    
    * add changelog
    
    * fix maskedinput test and kv selector
    
    * final textarea selector!

--- ui/tests/acceptance/mfa-method-test.js
+++ ui/tests/acceptance/mfa-method-test.js
@@ -11,6 +11,7 @@ import { setupMirage } from 'ember-cli-mirage/test-support';
 import mfaConfigHandler from 'vault/mirage/handlers/mfa-config';
 import { Response } from 'miragejs';
 import { underscore } from '@ember/string';
+import { GENERAL } from 'vault/tests/helpers/general-selectors';
 
 module('Acceptance | mfa-method', function (hooks) {
   setupApplicationTest(hooks);
@@ -181,17 +182,10 @@ module('Acceptance | mfa-method', function (hooks) {
         .dom('[data-test-inline-error-message]')
         .exists({ count: required.length }, `Required field validations display for ${type}`);
 
-      for (const [i, field] of required.entries()) {
-        let inputType = 'input';
-        // this is less than ideal but updating the test selectors in masked-input break a bunch of tests
-        // add value to the masked input text area data-test attributes for selection
-        if (['secret_key', 'integration_key'].includes(field)) {
-          inputType = 'textarea';
-          const textareas = this.element.querySelectorAll('[data-test-textarea]');
-          textareas[i].setAttribute('data-test-textarea', field);
-        }
-        await fillIn(`[data-test-${inputType}="${field}"]`, 'foo');
+      for (const field of required) {
+        await fillIn(GENERAL.inputByAttr(field), 'foo');
       }
+
       await click('[data-test-mfa-create-save]');
       assert.strictEqual(
         currentRouteName(),
