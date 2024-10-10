53dd2d1d284f6cf99575dad469d21cd526e7d5a5
Steven Clark <steven.clark@hashicorp.com>
2024-10-10 13:33:20 -0400

    Fix ACME http-01 challenges for IPv6 IPs
     - We weren't properly encapsulating the IPv6 IP within the url provided
       to the http client with [].

---
+++ builtin/logical/pki/acme_challenge_engine_test.go
@@ -0,0 +1,48 @@
+package pki
+
+import (
+	"fmt"
+	"net/netip"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+// Test_encodeIdentifierForHTTP01Challenge validates the encoding behaviors of our identifiers
+// for the HTTP01 challenge. Basically properly encode the identifier for an HTTP request.
+func Test_encodeIdentifierForHTTP01Challenge(t *testing.T) {
+	ip, _ := netip.ParseAddr("2001:db8::68")
+	fmt.Printf(ip.StringExpanded())
+	tests := []struct {
+		name string
+		arg  *ACMEIdentifier
+		want string
+	}{
+		{
+			name: "dns",
+			arg:  &ACMEIdentifier{Type: ACMEDNSIdentifier, Value: "www.dadgarcorp.com"},
+			want: "www.dadgarcorp.com",
+		},
+		{
+			name: "ipv4",
+			arg:  &ACMEIdentifier{Type: ACMEIPIdentifier, Value: "192.168.1.1"},
+			want: "192.168.1.1",
+		},
+		{
+			name: "ipv6",
+			arg:  &ACMEIdentifier{Type: ACMEIPIdentifier, Value: "2001:0db8:0000:0000:0000:0000:0000:0068", IsV6IP: true},
+			want: "[2001:0db8:0000:0000:0000:0000:0000:0068]",
+		},
+		{
+			name: "ipv6-zoned",
+			arg:  &ACMEIdentifier{Type: ACMEIPIdentifier, Value: "fe80::1cc0:3e8c:119f:c2e1%ens18", IsV6IP: true},
+			want: "[fe80::1cc0:3e8c:119f:c2e1%25ens18]",
+		},
+	}
+	for _, tt := range tests {
+		tt := tt
+		t.Run(tt.name, func(t *testing.T) {
+			assert.Equalf(t, tt.want, encodeIdentifierForHTTP01Challenge(tt.arg), "encodeIdentifierForHTTP01Challenge(%v)", tt.arg)
+		})
+	}
+}
