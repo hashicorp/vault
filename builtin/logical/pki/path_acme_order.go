cb64173a01703b3bce2fad55e2c790645039868c
Steven Clark <steven.clark@hashicorp.com>
2024-10-16 14:26:44 -0400

    Fix issue building urls with IPv6 IPs for ACME http-01 challenges (#28718)
    * Fix ACME http-01 challenges for IPv6 IPs
    
     - We weren't properly encapsulating the IPv6 IP within the url provided
       to the http client with [].
    
    * Add cl
    
    * Cleanup a test println

--- builtin/logical/pki/path_acme_order.go
+++ builtin/logical/pki/path_acme_order.go
@@ -10,6 +10,7 @@ import (
 	"fmt"
 	"net"
 	"net/http"
+	"net/netip"
 	"sort"
 	"strings"
 	"time"
@@ -960,9 +961,23 @@ func parseOrderIdentifiers(data map[string]interface{}) ([]*ACMEIdentifier, erro
 		switch typeStr {
 		case string(ACMEIPIdentifier):
 			identifier.Type = ACMEIPIdentifier
-			ip := net.ParseIP(valueStr)
-			if ip == nil {
+			ip, err := netip.ParseAddr(valueStr)
+			if err != nil {
 				return nil, fmt.Errorf("value argument (%s) failed validation: failed parsing as IP: %w", valueStr, ErrMalformed)
+			}
+			if ip.Is6() {
+				if len(ip.Zone()) > 0 {
+					// If we are given an identifier with a local zone that doesn't make much sense
+					// as zone's are specific to the sender not us. For now disallow, perhaps in the
+					// future we should simply drop the zone?
+					return nil, fmt.Errorf("value argument (%s) failed validation: IPv6 identifiers with zone information are not allowed: %w", valueStr, ErrMalformed)
+				}
+
+				// We should keep whatever formatting of the IPv6 address that came in according
+				// to RFC8738 Section 2:
+				// An identifier for the IPv6 address 2001:db8::1 would be formatted like so:
+				//   {"type": "ip", "value": "2001:db8::1"}
+				identifier.IsV6IP = true
 			}
 		case string(ACMEDNSIdentifier):
 			identifier.Type = ACMEDNSIdentifier
@@ -1008,6 +1023,10 @@ func parseOrderIdentifiers(data map[string]interface{}) ([]*ACMEIdentifier, erro
 		}
 
 		identifiers = append(identifiers, identifier)
+	}
+
+	if len(identifiers) == 0 {
+		return nil, fmt.Errorf("no parsed identifiers were found: %w", ErrMalformed)
 	}
 
 	return identifiers, nil
