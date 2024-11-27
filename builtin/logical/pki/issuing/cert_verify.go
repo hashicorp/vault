2d401bf91c8a7bc73c1850aca32f9e0dd90a6f5f
Victor Rodriguez <vrizo@hashicorp.com>
2024-11-27 19:59:57 +0100

    Add hook for ENT tweaking of PKI contraints verification options. (#29048)

--- builtin/logical/pki/issuing/cert_verify.go
+++ builtin/logical/pki/issuing/cert_verify.go
@@ -4,7 +4,9 @@
 package issuing
 
 import (
+	"context"
 	"fmt"
+	"github.com/hashicorp/vault/sdk/logical"
 	"os"
 	"strconv"
 	"time"
@@ -33,7 +35,7 @@ func isCertificateVerificationDisabled() (bool, error) {
 	return disable, nil
 }
 
-func VerifyCertificate(parsedBundle *certutil.ParsedCertBundle) error {
+func VerifyCertificate(ctx context.Context, storage logical.Storage, issuerId IssuerID, parsedBundle *certutil.ParsedCertBundle) error {
 	if verificationDisabled, err := isCertificateVerificationDisabled(); err != nil {
 		return err
 	} else if verificationDisabled {
@@ -66,6 +68,10 @@ func VerifyCertificate(parsedBundle *certutil.ParsedCertBundle) error {
 		DisableNameChecks:              false,
 		DisablePathLenChecks:           false,
 		DisableNameConstraintChecks:    false,
+	}
+
+	if err := entSetCertVerifyOptions(ctx, storage, issuerId, &options); err != nil {
+		return err
 	}
 
 	certificate, err := convertCertificate(parsedBundle.CertificateBytes)
