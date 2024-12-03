3ef19e99661b378e108a0e818e316137c5950b4c
Helen Fu <25168806+helenfufu@users.noreply.github.com>
2024-12-03 13:43:46 -0800

    init version.BuildDate following testing_util_ent

--- vault/testing_util.go
+++ vault/testing_util.go
@@ -8,7 +8,18 @@ package vault
 import (
 	"crypto/ed25519"
 	"testing"
+	"time"
+
+	"github.com/hashicorp/vault/version"
 )
+
+func init() {
+	// The BuildDate is set as part of the build process in CI so we need to
+	// initialize it for testing.
+	if version.BuildDate == "" {
+		version.BuildDate = time.Now().UTC().Format(time.RFC3339)
+	}
+}
 
 func GenerateTestLicenseKeys() (ed25519.PublicKey, ed25519.PrivateKey, error) { return nil, nil, nil }
 func testGetLicensingConfig(key ed25519.PublicKey) *LicensingConfig           { return &LicensingConfig{} }
