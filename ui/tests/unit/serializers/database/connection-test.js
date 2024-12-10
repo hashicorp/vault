52142272b0a415a46b5bfd7d2eb040e29e08ed65
claire bontempo <68122737+hellobontempo@users.noreply.github.com>
2024-12-10 09:31:09 -0800

    UI: Decode Oracle database `connection_url` (#29114)
    * decode url in the serializer for oracle connection_url
    
    * add serializer test
    
    * add test for oracle
    
    * add test back, remove decode-url helper
    
    * update comment and test
    
    * link jiras VAULT-32830 VAULT-29785
    
    * add changelog
    
    * add test

--- ui/tests/unit/serializers/database/connection-test.js
+++ ui/tests/unit/serializers/database/connection-test.js
@@ -79,12 +79,6 @@ module('Unit | Serializer | database/connection', function (hooks) {
     const expectedResult = {
       allowed_roles: ['readonly'],
       backend: 'database',
-      connection_details: {
-        backend: 'database',
-        insecure: false,
-        url: 'https://localhost:9200',
-        username: 'root',
-      },
       id: 'elastic-test',
       insecure: false,
       name: 'elastic-test',
@@ -95,6 +89,113 @@ module('Unit | Serializer | database/connection', function (hooks) {
       root_rotation_statements: [],
       url: 'https://localhost:9200',
       username: 'root',
+    };
+    assert.deepEqual(normalized, expectedResult, `Normalizes and flattens database response`);
+  });
+
+  test('it should normalize values for the database type (oracle)', function (assert) {
+    const serializer = this.owner.lookup('serializer:database/connection');
+    const normalized = serializer.normalizeSecrets({
+      request_id: 'request-id',
+      lease_id: '',
+      renewable: false,
+      lease_duration: 0,
+      data: {
+        allowed_roles: ['*'],
+        connection_details: {
+          backend: 'database',
+          connection_url: '%7B%7Busername%7D%7D/%7B%7Bpassword%7D%7D@//localhost:1521/ORCLPDB1',
+          max_connection_lifetime: '0s',
+          max_idle_connections: 0,
+          max_open_connections: 3,
+          username: 'VAULTADMIN',
+        },
+        password_policy: '',
+        plugin_name: 'vault-plugin-database-oracle',
+        plugin_version: '',
+        root_credentials_rotate_statements: [],
+        verify_connection: true,
+      },
+      wrap_info: null,
+      warnings: null,
+      auth: null,
+      mount_type: 'database',
+      backend: 'database',
+      id: 'oracle-test',
+    });
+    const expectedResult = {
+      allowed_roles: ['*'],
+      backend: 'database',
+      connection_url: '{{username}}/{{password}}@//localhost:1521/ORCLPDB1',
+      id: 'oracle-test',
+      max_connection_lifetime: '0s',
+      max_idle_connections: 0,
+      max_open_connections: 3,
+      name: 'oracle-test',
+      password_policy: '',
+      plugin_name: 'vault-plugin-database-oracle',
+      plugin_version: '',
+      root_credentials_rotate_statements: [],
+      root_rotation_statements: [],
+      username: 'VAULTADMIN',
+      verify_connection: true,
+    };
+    assert.deepEqual(normalized, expectedResult, `Normalizes and flattens database response`);
+  });
+
+  test('it should normalize values if some params do not exist', function (assert) {
+    const serializer = this.owner.lookup('serializer:database/connection');
+    const normalized = serializer.normalizeSecrets({
+      request_id: 'request-id',
+      lease_id: '',
+      renewable: false,
+      lease_duration: 0,
+      data: {
+        allowed_roles: ['*'],
+        connection_details: { backend: 'database' }, // no connection_url param intentionally
+        plugin_name: 'vault-postgres-db',
+      },
+      wrap_info: null,
+      warnings: null,
+      auth: null,
+      mount_type: 'database',
+      backend: 'database',
+      id: 'db-test',
+    });
+    const expectedResult = {
+      allowed_roles: ['*'],
+      backend: 'database',
+      id: 'db-test',
+      name: 'db-test',
+      plugin_name: 'vault-postgres-db',
+    };
+    assert.deepEqual(normalized, expectedResult, `Normalizes and flattens database response`);
+  });
+
+  test('it should fail gracefully if no connection_details', function (assert) {
+    const serializer = this.owner.lookup('serializer:database/connection');
+    const normalized = serializer.normalizeSecrets({
+      request_id: 'request-id',
+      lease_id: '',
+      renewable: false,
+      lease_duration: 0,
+      data: {
+        allowed_roles: ['*'],
+        plugin_name: 'vault-postgres-db',
+      },
+      wrap_info: null,
+      warnings: null,
+      auth: null,
+      mount_type: 'database',
+      backend: 'database',
+      id: 'db-test',
+    });
+    const expectedResult = {
+      allowed_roles: ['*'],
+      backend: 'database',
+      id: 'db-test',
+      name: 'db-test',
+      plugin_name: 'vault-postgres-db',
     };
     assert.deepEqual(normalized, expectedResult, `Normalizes and flattens database response`);
   });
