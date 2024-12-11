da3250478b0442c0ed6e1bcaaa5adec702b34d08
tin.vo <tin.vo@hashicorp.com>
2024-12-11 14:45:53 -0800

    updating cert issuing

--- enos/modules/verify_secrets_engines/scripts/kv-pki-issue-certificates.sh
+++ enos/modules/verify_secrets_engines/scripts/kv-pki-issue-certificates.sh
@@ -23,9 +23,7 @@ export VAULT_FORMAT=json
 
 # ------ Generate and sign certificate ------
 CA_NAME="${MOUNT}.pem"
-CA_CSR_NAME="${MOUNT}-csr.pem"
-PRIV_KEY_NAME="${MOUNT}_priv.key"
-SIGNED_CA_NAME="${MOUNT}_signed.pem"
+SIGNED_CERT_NAME="${MOUNT}-signed.pem"
 ROLE_NAME="${COMMON_NAME}-role"
 TMP_TTL="1h"
 rm -rf "${TMP_TEST_RESULTS}"
@@ -34,23 +32,21 @@ mkdir "${TMP_TEST_RESULTS}"
 ## Setting AIA fields for Certificate
 "$binpath" write "${MOUNT}/config/urls" issuing_certificates="${VAULT_ADDR}/v1/pki/ca" crl_distribution_points="${VAULT_ADDR}/v1/pki/crl"
 
-# Generating root CA
+# Generating CA Certificate
 "$binpath" write "${MOUNT}/root/generate/internal" common_name="${COMMON_NAME}.com" ttl="${TTL}" -format=json | jq -r '.data.certificate' > "${TMP_TEST_RESULTS}/${CA_NAME}"
 # Creating a role
 "$binpath" write "${MOUNT}/roles/${ROLE_NAME}" allowed_domains="${COMMON_NAME}.com" allow_subdomains=true max_ttl="${TMP_TTL}"
-# Issue Certificate
-openssl req -new -newkey rsa:2048 -nodes -subj "/CN=www.${COMMON_NAME}.com" -keyout "${TMP_TEST_RESULTS}/${PRIV_KEY_NAME}" -out "${TMP_TEST_RESULTS}/${CA_CSR_NAME}"
-# Sign Certificate
-"$binpath" write "${MOUNT}/sign/${ROLE_NAME}" csr="@${TMP_TEST_RESULTS}/${CA_CSR_NAME}" format=pem ttl="${TMP_TTL}" | jq -r '.data.certificate' > "${TMP_TEST_RESULTS}/${SIGNED_CA_NAME}"
+# Issuing Signed Certificate
+"$binpath" write "${MOUNT}/issue/${ROLE_NAME}" common_name="test.${COMMON_NAME}.com" ttl="${TMP_TTL}" | jq -r '.data.certificate' > "${TMP_TEST_RESULTS}/${SIGNED_CERT_NAME}"
 
 # ------ Generate and sign intermediate ------
 INTERMEDIATE_COMMON_NAME="intermediate-${COMMON_NAME}"
-INTERMEDIATE_CA_NAME="${MOUNT}_${INTERMEDIATE_COMMON_NAME}.pem"
-INTERMEDIATE_SIGNED_CA_NAME="${MOUNT}_${INTERMEDIATE_COMMON_NAME}_signed.pem"
+INTERMEDIATE_CA_NAME="${MOUNT}-${INTERMEDIATE_COMMON_NAME}.pem"
+INTERMEDIATE_SIGNED_NAME="${MOUNT}-${INTERMEDIATE_COMMON_NAME}-signed.pem"
 
 # Generate Intermediate CA
 "$binpath" write "${MOUNT}/intermediate/generate/internal" common_name="${INTERMEDIATE_COMMON_NAME}.com" ttl="${TTL}" | jq -r '.data.csr' > "${TMP_TEST_RESULTS}/${INTERMEDIATE_CA_NAME}"
 # Sign Intermediate Certificate
-"$binpath" write "${MOUNT}/root/sign-intermediate" csr="@${TMP_TEST_RESULTS}/${INTERMEDIATE_CA_NAME}" format=pem_bundle ttl="${TMP_TTL}" | jq -r '.data.certificate' > "${TMP_TEST_RESULTS}/${INTERMEDIATE_SIGNED_CA_NAME}"
+"$binpath" write "${MOUNT}/root/sign-intermediate" csr="@${TMP_TEST_RESULTS}/${INTERMEDIATE_CA_NAME}" format=pem_bundle ttl="${TMP_TTL}" | jq -r '.data.certificate' > "${TMP_TEST_RESULTS}/${INTERMEDIATE_SIGNED_NAME}"
 # Import Signed Intermediate Certificate into Vault
-"$binpath" write "${MOUNT}/intermediate/set-signed" certificate="@${TMP_TEST_RESULTS}/${INTERMEDIATE_SIGNED_CA_NAME}"
+"$binpath" write "${MOUNT}/intermediate/set-signed" certificate="@${TMP_TEST_RESULTS}/${INTERMEDIATE_SIGNED_NAME}"
