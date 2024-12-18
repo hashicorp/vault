5bb5eee46f8f868f64f04047c28b3d921c292ab3
Angel Garbarino <argarbarino@gmail.com>
2024-12-18 16:03:20 -0700

    most of the structure is in, time for clean up and testing

--- ui/app/models/gcp/config.js
+++ ui/app/models/gcp/config.js
@@ -6,38 +6,10 @@
 import Model, { attr } from '@ember-data/model';
 import { expandAttributeMeta } from 'vault/utils/field-to-attrs';
 
-const credentialsExample = `# The example below is treated as a comment and will not be submitted
-# some kind of example??
-`;
-
 export default class GcpConfig extends Model {
   @attr('string') backend; // dynamic path of secret -- set on response from value passed to queryRecord
 
-  @attr('string', {
-    editType: 'json',
-    label: 'JSON credentials',
-    helpText: 'Specifies the maximum Config TTL for long-lived credentials (i.e. service account keys).',
-    example: credentialsExample,
-    mode: 'ruby',
-    sectionHeading: 'Hello section heading', // render section heading before form field
-  })
-  credentials; // Mutually exclusive with identityTokenAudience, meaning it's cannot be set if identityTokenAudience is set
-
-  @attr('string', {
-    subText:
-      'The audience claim value for plugin identity tokens. Must match an allowed audience configured for the target IAM OIDC identity provider.',
-  })
-  identityTokenAudience;
-
-  @attr({
-    label: 'Identity token TTL',
-    helperTextDisabled:
-      'The TTL of generated tokens. Defaults to 1 hour, turn on the toggle to specify a different value.',
-    helperTextEnabled: 'The TTL of generated tokens.',
-    editType: 'ttl',
-  })
-  identityTokenTtl;
-
+  /* GCP creds, mutually exclusive with WIF fields */
   @attr({
     label: 'Config TTL',
     editType: 'ttl',
@@ -53,21 +25,47 @@ export default class GcpConfig extends Model {
   })
   maxTtl;
 
-  // for configuration details view
-  get displayAttrs() {
-    return this.formFields;
-  }
+  @attr('string', {
+    label: 'JSON credentials',
+    subText:
+      'If empty, Vault will use the GOOGLE_APPLICATION_CREDENTIALS environment variable if configured.',
+    editType: 'file',
+    docLink: '/vault/docs/secrets/gcp#authentication',
+  })
+  credentials; // obfuscated, never returned by API.
 
-  // formFields are iterated through to generate the edit/create view
-  get formFields() {
-    const keys = [
-      'credentials',
-      'serviceAccountEmail',
-      'identityTokenAudience',
-      'identityTokenTtl',
-      'ttl',
-      'maxTtl',
-    ];
-    return expandAttributeMeta(this, keys);
+  /* WIF fields, mutually exclusive with GCP cred fields */
+  @attr('string', {
+    subText:
+      'The audience claim value for plugin identity tokens. Must match an allowed audience configured for the target IAM OIDC identity provider.',
+  })
+  identityTokenAudience;
+
+  @attr({
+    label: 'Identity token TTL',
+    helperTextDisabled:
+      'The TTL of generated tokens. Defaults to 1 hour, turn on the toggle to specify a different value.',
+    helperTextEnabled: 'The TTL of generated tokens.',
+    editType: 'ttl',
+  })
+  identityTokenTtl;
+
+  @attr('string', {
+    subText: 'Email ID for the Service Account to impersonate for Workload Identity Federation.',
+  })
+  serviceAccountEmail;
+
+  configurableParams = [
+    'credentials',
+    'serviceAccountEmail',
+    'ttl',
+    'maxTtl',
+    'identityTokenAudience',
+    'identityTokenTtl',
+  ];
+
+  get displayAttrs() {
+    const formFields = expandAttributeMeta(this, this.configurableParams);
+    return formFields.filter((attr) => attr.name !== 'credentials');
   }
 }
