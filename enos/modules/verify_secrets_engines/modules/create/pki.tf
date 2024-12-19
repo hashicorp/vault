d180b2e2d3e780517a541d8499b837d8632c06b5
tin.vo <tin.vo@hashicorp.com>
2024-12-18 17:28:04 -0800

    addressing comments

--- enos/modules/verify_secrets_engines/modules/create/pki.tf
+++ enos/modules/verify_secrets_engines/modules/create/pki.tf
@@ -8,18 +8,15 @@ locals {
   pki_common_name           = "common"
   pki_default_ttl           = "72h"
   pki_test_data_path_prefix = "smoke"
-  pki_tmp_test_results      = "tmp-test-results"
+  pki_test_dir      = "tmp-test-results"
 
   // Output
   pki_output = {
     mount        = local.pki_mount
     common_name  = local.pki_common_name
-    test_results = local.pki_tmp_test_results
+    test_results = local.pki_test_dir
   }
 
-  test = {
-    path_prefix = local.pki_test_data_path_prefix
-  }
 }
 
 output "pki" {
@@ -58,10 +55,10 @@ resource "enos_remote_exec" "pki_issue_certificates" {
     COMMON_NAME       = local.pki_common_name
     ISSUER_NAME       = local.pki_issuer_name
     TTL               = local.pki_default_ttl
-    TMP_TEST_RESULTS  = local.pki_tmp_test_results
+    TEST_DIR  = local.pki_test_dir
   }
 
-  scripts = [abspath("${path.module}/../../scripts/kv-pki-issue-certificates.sh")]
+  scripts = [abspath("${path.module}/../../scripts/pki-issue-certificates.sh")]
 
   transport = {
     ssh = {
