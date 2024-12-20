6fc163de2a7b2cf700e5855d2c3622c931431c9b
divyaac <divyaac@berkeley.edu>
2024-12-20 10:44:58 -0800

    Revert "OSS-Changes Patch (#29193)"
    This reverts commit 1fab64e9c6bd1b7b7c52e87e4346e9ba50038aa8.

--- vault/activity_log.go
+++ vault/activity_log.go
@@ -4095,10 +4095,11 @@ func (a *ActivityLog) writeExport(ctx context.Context, rw http.ResponseWriter, f
 func (c *Core) activityLogMigrationTask(ctx context.Context) {
 	manager := c.activityLog
 	if !c.IsPerfSecondary() {
-		// If no migrations tasks have been run, kick off the migration task
-		if !manager.hasDedupClientsUpgrade(ctx) {
+		// If the oldest version is less than 1.19 and no migrations tasks have been run, kick off the migration task
+		if !manager.OldestVersionHasDeduplicatedClients(ctx) && !manager.hasDedupClientsUpgrade(ctx) {
 			go c.primaryDuplicateClientMigrationWorker(ctx)
 		} else {
+			// Store that upgrade processes have already been completed
 			manager.writeDedupClientsUpgrade(ctx)
 		}
 	} else {
@@ -4107,8 +4108,6 @@ func (c *Core) activityLogMigrationTask(ctx context.Context) {
 		// already upgraded primary
 		if !manager.hasDedupClientsUpgrade(ctx) {
 			go c.secondaryDuplicateClientMigrationWorker(ctx)
-		} else {
-			manager.writeDedupClientsUpgrade(ctx)
 		}
 	}
 }
@@ -4119,12 +4118,7 @@ func (c *Core) activityLogMigrationTask(ctx context.Context) {
 // current cluster. This method wil only exit once all connected secondary clusters have
 // upgraded to 1.19, and this cluster receives global data from all of them.
 func (c *Core) primaryDuplicateClientMigrationWorker(ctx context.Context) error {
-	c.activityLogLock.Lock()
 	a := c.activityLog
-	c.activityLogLock.Unlock()
-	if a == nil {
-		return fmt.Errorf("activity log not configured")
-	}
 	a.logger.Trace("started primary activity log migration worker")
 	ctx, cancel := context.WithCancel(ctx)
 	defer cancel()
