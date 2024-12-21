b5904b361ed6b40d55e0c97b19e80edda0639671
divyaac <divya.chandrasekaran@hashicorp.com>
2024-12-20 16:06:41 -0800

    Revert "Migrate Clients From Old Storage Paths to New Paths During Upgrade #7…" (#29253)
    This reverts commit 9ba62bec6f1005782c508a6dc45e6cc79c06beb6.

--- vault/activity_log_stubs_oss.go
+++ vault/activity_log_stubs_oss.go
@@ -5,22 +5,11 @@
 
 package vault
 
-import (
-	"context"
-	"errors"
-)
+import "context"
 
 //go:generate go run github.com/hashicorp/vault/tools/stubmaker
 
-// ErrEmptyResponse error is used to avoid returning "nil, nil" from a function
-var ErrEmptyResponse = errors.New("empty response; the system encountered a statement that exclusively returns nil values")
-
 // sendGlobalClients is a no-op on CE
 func (a *ActivityLog) sendGlobalClients(ctx context.Context) error {
-	return nil
-}
-
-// waitForSecondaryGlobalClients is a no-op on CE
-func (a *ActivityLog) waitForSecondaryGlobalClients(ctx context.Context) error {
 	return nil
 }
