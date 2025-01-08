415d0f992d06471f4ab508e93dffc9fa61d47495
Thy Ton <maithytonn@gmail.com>
2025-01-08 21:21:19 +0000

    backport of commit 607991e5519d406162b27a34ae5f9a08570706b0

--- scripts/copywrite-exceptions.sh
+++ scripts/copywrite-exceptions.sh
@@ -5,7 +5,7 @@
 # then runs the copywrite bot to utilize local subdir config
 # to inject correct headers.
 
-find . -type f -name '*.go' -not -name '*_ent.go' | while read line; do
+find . -type f -name '*.go' -not -name '*_ent.go' -not -name '*_ent_test.go' | while read line; do
   if grep "SPDX-License-Identifier: BUSL-1.1" $line; then
     sed -i '/SPDX-License-Identifier: BUSL-1.1/d' $line
     sed -i '/Copyright (c) HashiCorp, Inc./d' $line
