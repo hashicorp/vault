1efd45737cf583b98f4133f1fcab24f4163b98bf
Angel Garbarino <argarbarino@gmail.com>
2025-01-09 16:39:12 -0700

    string-to-camel helper

---
+++ ui/app/helpers/string-to-camel.js
@@ -0,0 +1,37 @@
+/**
+ * Copyright (c) HashiCorp, Inc.
+ * SPDX-License-Identifier: BUSL-1.1
+ */
+
+import { helper as buildHelper } from '@ember/component/helper';
+import { assert } from '@ember/debug';
+
+// This helper turns strings with spaces into camelCase strings, example: 'hello world' -> 'helloWorld'
+// If an array of strings is passed, this helper returns an array of camelCase strings.
+// Does not handle accented characters
+export function stringToCamelCase(str) {
+  if (!str) return;
+  if (Array.isArray(str)) {
+    return str.map((s) => {
+      assert(`must pass in a string or array of strings`, typeof s === 'string');
+      // lower case the entire string to handle situations like IAM Endpoint  -> iamEndpoint instead of
+      s = s.toLowerCase();
+      return s
+        .replace(/(?:^\w|[A-Z]|\b\w)/g, function (word, index) {
+          return index === 0 ? word.toLowerCase() : word.toUpperCase();
+        })
+        .replace(/\s+/g, '');
+    });
+  } else {
+    // lower case the entire string to handle situations like IAM Endpoint  -> iamEndpoint instead of
+    assert(`must pass in a string or array of strings`, typeof str === 'string');
+    str = str.toLowerCase();
+    return str
+      .replace(/(?:^\w|[A-Z]|\b\w)/g, function (word, index) {
+        return index === 0 ? word.toLowerCase() : word.toUpperCase();
+      })
+      .replace(/\s+/g, '');
+  }
+}
+
+export default buildHelper(stringToCamelCase);
