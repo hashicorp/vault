fa737f85bae1842bb498b51b5c34a61fa8d74676
divyaac <divyaac@berkeley.edu>
2025-01-22 09:25:32 -0800

    Revert "Export API Bug"
    This reverts commit 9cdd28c59ea4907f8a44c20a4a048045d56d9d12.

--- vault/activity_log_util_common.go
+++ vault/activity_log_util_common.go
@@ -392,7 +392,7 @@ func (a *ActivityLog) sortActivityLogMonthsResponse(months []*ResponseMonth) {
 
 const (
 	noMountAccessor     = "no mount accessor (pre-1.10 upgrade?)"
-	DeletedMountFmt     = "deleted mount; accessor %q"
+	deletedMountFmt     = "deleted mount; accessor %q"
 	DeletedNamespaceFmt = "deleted namespace %q"
 )
 
@@ -405,7 +405,7 @@ func (a *ActivityLog) mountAccessorToMountPath(mountAccessor string) string {
 	} else {
 		valResp := a.core.router.ValidateMountByAccessor(mountAccessor)
 		if valResp == nil {
-			displayPath = fmt.Sprintf(DeletedMountFmt, mountAccessor)
+			displayPath = fmt.Sprintf(deletedMountFmt, mountAccessor)
 		} else {
 			displayPath = valResp.MountPath
 			if !strings.HasSuffix(displayPath, "/") {
