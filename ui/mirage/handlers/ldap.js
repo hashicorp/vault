08a28f8717ef4072bf0898814655babc17fe7d24
claire bontempo <cbontempo@hashicorp.com>
2025-01-22 11:09:25 -0800

    update mirage handler

--- ui/mirage/handlers/ldap.js
+++ ui/mirage/handlers/ldap.js
@@ -92,60 +92,8 @@ export default function (server) {
   server.post('/:backend/library/:name', (schema, req) => createOrUpdateRecord(schema, req, 'ldapLibraries'));
   server.get('/:backend/library/*name', (schema, req) => listOrGetRecord(schema, req));
   server.get('/:backend/library', (schema) => listRecords(schema, 'ldapLibraries'));
-  server.get('/:backend/library/*name/status', (schema) => {
-    const data = schema.db['ldapAccountStatuses'].reduce((prev, curr) => {
-      prev[curr.account] = {
-        available: curr.available,
-        borrower_client_token: curr.borrower_client_token,
-      };
-      return prev;
-    }, {});
-    return { data };
-  });
-  // check-out / check-in
-  server.post('/:backend/library/:set_name/check-in', (schema, req) => {
-    // Check-in makes an unavailable account available again
-    const { service_account_names } = JSON.parse(req.requestBody);
-    const dbCollection = schema.db['ldapAccountStatuses'];
-    const updated = dbCollection.find(service_account_names).map((f) => ({
-      ...f,
-      available: true,
-      borrower_client_token: undefined,
-    }));
-    updated.forEach((u) => {
-      dbCollection.update(u.id, u);
-    });
-    return {
-      data: {
-        check_ins: service_account_names,
-      },
-    };
-  });
-  server.post('/:backend/library/:set_name/check-out', (schema, req) => {
-    const { set_name, backend } = req.params;
-    const dbCollection = schema.db['ldapAccountStatuses'];
-    const available = dbCollection.where({ available: true });
-    if (available) {
-      return Response(404, {}, { errors: ['no accounts available to check out'] });
-    }
-    const checkOut = {
-      ...available[0],
-      available: false,
-      borrower_client_token: crypto.randomUUID(),
-    };
-    dbCollection.update(checkOut.id, checkOut);
-    return {
-      request_id: '364a17d4-e5ab-998b-ceee-b49929229e0c',
-      lease_id: `${backend}/library/${set_name}/check-out/aoBsaBEI4PK96VnukubvYDlZ`,
-      renewable: true,
-      lease_duration: 36000,
-      data: {
-        password: crypto.randomUUID(),
-        service_account_name: checkOut.account,
-      },
-      wrap_info: null,
-      warnings: null,
-      auth: null,
-    };
-  });
+  server.get('/:backend/library/*name/status', () => ({
+    'bob.johnson': { available: false, borrower_client_token: '8b80c305eb3a7dbd161ef98f10ea60a116ce0910' },
+    'mary.smith': { available: true },
+  }));
 }
