d66eca60fe42f19f505b98556dddbdd68845f653
Steven Clark <steven.clark@hashicorp.com>
2025-01-24 15:04:28 -0500

    Address all "non-constant format string errors" from go vet
     - Within 1.24 these now cause test builds to fail
    
    …" from go vet

--- builtin/logical/pki/backend_test.go
+++ builtin/logical/pki/backend_test.go
@@ -2277,7 +2277,7 @@ func runTestSignVerbatim(t *testing.T, keyType string) {
 		t.Fatal(err)
 	}
 	if resp != nil && resp.IsError() {
-		t.Fatalf(resp.Error().Error())
+		t.Fatal(resp.Error().Error())
 	}
 	if resp.Data == nil || resp.Data["certificate"] == nil {
 		t.Fatal("did not get expected data")
@@ -2317,7 +2317,7 @@ func runTestSignVerbatim(t *testing.T, keyType string) {
 		t.Fatal(err)
 	}
 	if resp != nil && resp.IsError() {
-		t.Fatalf(resp.Error().Error())
+		t.Fatal(resp.Error().Error())
 	}
 	if resp.Data == nil || resp.Data["certificate"] == nil {
 		t.Fatal("did not get expected data")
