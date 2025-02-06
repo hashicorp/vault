7ed26a39f51d28d7d2bd17a22babb7467368c6ba
drivera258 <dan.rivera@hashicorp.com>
2025-02-06 17:16:39 -0500

    updated test for edit payload

--- ui/tests/integration/components/database-role-edit-test.js
+++ ui/tests/integration/components/database-role-edit-test.js
@@ -21,6 +21,7 @@ module('Integration | Component | database-role-edit', function (hooks) {
       modelName: 'database/role',
       database: ['my-mongodb-database'],
       backend: 'database',
+      username: 'staticTestUser',
       type: 'static',
       name: 'my-static-role',
       id: 'my-static-role',
@@ -40,14 +41,21 @@ module('Integration | Component | database-role-edit', function (hooks) {
   test('it should let user edit a static role when given update capability', async function (assert) {
     this.server.post('/sys/capabilities-self', capabilitiesStub('database/static-creds/my-role', ['update']));
 
-    // renders edit page, with update policy the user is able to edit & save
+    this.server.post(`/database/static-roles/my-static-role`, (schema, req) => {
+      assert.true(true, 'request made to update static role');
+      assert.propEqual(
+        JSON.parse(req.requestBody),
+        {
+          username: 'staticTestUser',
+          rotation_period: '1728000s', // 20 days in seconds
+        },
+        'it updates static role with correct payload'
+      );
+    });
+
     await render(hbs`<DatabaseRoleEdit @model={{this.modelStatic}} @mode="edit"/>`);
     await fillIn('[data-test-ttl-value="Rotation period"]', '20');
     await click('[data-test-secret-save]');
-
-    // show role detail page and verify that the rotation period changed
-    await render(hbs`<DatabaseRoleEdit @model={{this.modelStatic}} @mode="show"/>`);
-    assert.dom('[data-test-row-value="Rotation period"]').hasText('20 days');
   });
 
   test('it should show Get credentials button when a user has the correct policy', async function (assert) {
