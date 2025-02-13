1ab45d56f7b5fa314d3041c155f7af168eb4edec
Jordan Reimer <jordan.reimer@hashicorp.com>
2025-02-13 16:23:33 -0700

    reset pagination size to default on access method list route

--- ui/app/routes/vault/cluster/access/method/item/list.js
+++ ui/app/routes/vault/cluster/access/method/item/list.js
@@ -46,7 +46,6 @@ export default Route.extend(ListRoute, {
       }));
       return this.pagination.paginate(mappedKeys, {
         page,
-        pageSize: 3,
         filter: pageFilter,
         filterKey: 'id',
       });
