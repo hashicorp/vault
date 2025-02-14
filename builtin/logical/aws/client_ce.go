3a6e7cb55d615272bc4ffa0eee26cb4774d8e43a
Zlaticanin <zlaticaninmilena@gmail.com>
2025-02-14 11:28:50 -0700

    update comment

--- builtin/logical/aws/client_ce.go
+++ builtin/logical/aws/client_ce.go
@@ -15,5 +15,5 @@ import (
 // It uses the role ARN and session name provided in the staticRoleEntry configuration
 // to generate credentials for the assumed role.
 func (b *backend) assumeRoleStatic(ctx context.Context, s logical.Storage, entry *staticRoleEntry) (*aws.Config, error) {
-	return nil, fmt.Errorf("assumeRoleStatic is not supported in this version of Vault")
+	return nil, fmt.Errorf("cross-account static roles are only supported in Vault Enterprise")
 }
