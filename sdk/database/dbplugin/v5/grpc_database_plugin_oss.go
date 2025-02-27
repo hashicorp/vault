3a0fd5af5ba2b37161c2a8eb09714793ca70b6b0
Ryan Cragun <me@ryan.ec>
2025-02-27 22:56:34 +0000

    backport of commit 69646127df138233e5070930048075e737d58d72

--- sdk/database/dbplugin/v5/grpc_database_plugin_oss.go
+++ sdk/database/dbplugin/v5/grpc_database_plugin_oss.go
@@ -8,12 +8,11 @@ package dbplugin
 import (
 	"context"
 
-	"google.golang.org/grpc"
-
 	"github.com/hashicorp/go-plugin"
 	"github.com/hashicorp/vault/sdk/database/dbplugin/v5/proto"
 	"github.com/hashicorp/vault/sdk/helper/pluginutil"
 	"github.com/hashicorp/vault/sdk/logical"
+	"google.golang.org/grpc"
 )
 
 // GRPCClient (Vault CE edition) initializes and returns a gRPCClient with Database and
