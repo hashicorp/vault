f5190d5e9e9b74484a6daba18efb0603385b11ef
Angel Garbarino <argarbarino@gmail.com>
2025-03-03 12:26:16 -0700

    remove transform-edit-base from alaphabet and transform edit components

--- ui/app/components/alphabet-edit.js
+++ ui/app/components/alphabet-edit.js
@@ -3,6 +3,88 @@
  * SPDX-License-Identifier: BUSL-1.1
  */
 
-import TransformBase from './transform-edit-base';
+import Component from '@glimmer/component';
+import { tracked } from '@glimmer/tracking';
+import { action } from '@ember/object';
+import { service } from '@ember/service';
+import errorMessage from 'vault/utils/error-message';
 
-export default TransformBase;
+/** // ARG TODO
+ * @module AlphabetEdit
+ * `AlphabetEdit` is a component that will allow you to pick a file from the local file system. Once
+ * loaded, this file will be emitted as a JS ArrayBuffer to the passed `onChange` callback.
+ *
+ * @example
+ * ```js
+ *   <AlphabetEdit }} />
+ * ```
+ * @param {object} model - This is the transform template model.
+ * @param {string} mode - Determines if create or edit.
+
+ *
+ */
+export default class AlphabetEditComponent extends Component {
+  @service flashMessages;
+  @service router;
+
+  @tracked errorMessage = '';
+
+  get breadcrumbs() {
+    // ideally this is created on the controller in the parent route but this is a generic route and requires a significant refactor.
+    const { backend } = this.args.model;
+    return [
+      {
+        label: backend,
+        route: 'vault.cluster.secrets.backend.list-root',
+        model: backend,
+        query: { tab: 'alphabet' },
+      },
+      { label: 'Alphabet' },
+    ];
+  }
+
+  transition(route = 'show') {
+    this.displayErrors = '';
+    const { backend, id } = this.args.model;
+    if (route === 'list') {
+      this.router.transitionTo('vault.cluster.secrets.backend.list-root', backend, {
+        queryParams: { tab: 'alphabet' },
+      });
+      return;
+    } else {
+      this.router.transitionTo('vault.cluster.secrets.backend.show', `alphabet/${id}`);
+    }
+  }
+
+  @action async createOrUpdate(event) {
+    event.preventDefault();
+    const { id, name } = this.args.model;
+    const modelId = id || name;
+
+    if (!modelId) return; // TODO should solve with modelValidations instead
+
+    if (!this.args.model?.hasDirtyAttributes) {
+      this.flashMessages.info('No changes detected.');
+      this.transition();
+      return;
+    }
+
+    try {
+      await this.args.model.save();
+      this.flashMessages.success('Alphabet saved.');
+      this.transition();
+    } catch (e) {
+      this.errorMessage = errorMessage(e);
+    }
+  }
+
+  @action async onDelete() {
+    try {
+      await this.args.model.destroyRecord();
+      this.flashMessages.success('Alphabet deleted.');
+      this.transition('list');
+    } catch (e) {
+      this.errorMessage = errorMessage(e);
+    }
+  }
+}
--- ui/app/components/transform-template-edit.js
+++ ui/app/components/transform-template-edit.js
@@ -3,6 +3,88 @@
  * SPDX-License-Identifier: BUSL-1.1
  */
 
-import TransformBase from './transform-edit-base';
+import Component from '@glimmer/component';
+import { tracked } from '@glimmer/tracking';
+import { action } from '@ember/object';
+import { service } from '@ember/service';
+import errorMessage from 'vault/utils/error-message';
 
-export default TransformBase;
+/** // ARG TODO
+ * @module TransformTemplateEdit
+ * `TransformTemplateEdit` is a component that will allow you to pick a file from the local file system. Once
+ * loaded, this file will be emitted as a JS ArrayBuffer to the passed `onChange` callback.
+ *
+ * @example
+ * ```js
+ *   <TransformTemplateEdit }} />
+ * ```
+ * @param {object} model - This is the transform template model.
+ * @param {string} mode - Determines if create or edit.
+
+ *
+ */
+export default class TransformTemplateEditComponent extends Component {
+  @service flashMessages;
+  @service router;
+
+  @tracked errorMessage = '';
+
+  get breadcrumbs() {
+    // ideally this is created on the controller in the parent route but this is a generic route and requires a significant refactor.
+    const { backend } = this.args.model;
+    return [
+      {
+        label: backend,
+        route: 'vault.cluster.secrets.backend.list-root',
+        model: backend,
+        query: { tab: 'template' },
+      },
+      { label: 'Template' },
+    ];
+  }
+
+  transition(route = 'show') {
+    this.errorMessage = '';
+    const { backend, id } = this.args.model;
+    if (route === 'list') {
+      this.router.transitionTo('vault.cluster.secrets.backend.list-root', backend, {
+        queryParams: { tab: 'template' },
+      });
+      return;
+    } else {
+      this.router.transitionTo('vault.cluster.secrets.backend.show', `template/${id}`);
+    }
+  }
+
+  @action async createOrUpdate(event) {
+    event.preventDefault();
+    const { id, name } = this.args.model;
+    const modelId = id || name;
+
+    if (!modelId) return; // TODO should solve with modelValidations instead
+
+    if (!this.args.model?.hasDirtyAttributes) {
+      this.flashMessages.info('No changes detected.');
+      this.transition();
+      return;
+    }
+
+    try {
+      await this.args.model.save();
+      this.flashMessages.success('Transform template saved.');
+      this.transition();
+    } catch (e) {
+      this.errorMessage = errorMessage(e);
+    }
+  }
+
+  @action async onDelete() {
+    try {
+      await this.args.model.destroyRecord();
+      this.flashMessages.success('Transform template deleted.');
+      this.transition('list');
+    } catch (e) {
+      this.errorMessage = errorMessage(e);
+    }
+  }
+}
