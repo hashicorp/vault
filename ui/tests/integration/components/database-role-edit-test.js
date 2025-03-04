36dfe0005dcda699a1d70293afca17595d4452f5
drivera258 <dan.rivera@hashicorp.com>
2025-03-04 15:19:34 -0500

    leftover

--- ui/tests/integration/components/database-role-edit-test.js
+++ ui/tests/integration/components/database-role-edit-test.js
@@ -94,7 +94,7 @@ module('Integration | Component | database-role-edit', function (hooks) {
 
     await render(hbs`<DatabaseRoleEdit @model={{this.modelStatic}} @mode="create"/>`);
     await fillIn('[data-test-ttl-value="Rotation period"]', '2');
-    await click('[data-test-input="skip_import_rotation"]');
+    await click('[data-test-toggle="skip_import_rotation"]');
     await click('[data-test-secret-save]');
 
     await render(hbs`<DatabaseRoleEdit @model={{this.modelStatic}} @mode="show"/>`);
