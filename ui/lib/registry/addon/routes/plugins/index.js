929dae9ac58571707464d08b873633e119a8a9dd
Kianna Quach <kianna.quach@hashicorp.com>
2025-03-04 08:49:48 -0800

    Add card component

--- ui/lib/registry/addon/routes/plugins/index.js
+++ ui/lib/registry/addon/routes/plugins/index.js
@@ -13,14 +13,16 @@ export default class MessagesRoute extends Route {
     return [
       {
         id: '1',
-        pluginName: 'AWS',
-        author: 'cool',
-        description: 'hello hello hello',
+        pluginName: 'Plugin 1',
+        author: 'Some awesome author',
+        description:
+          "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.",
         externalUrl: 'www.hello.com',
         pluginVersion: '1.0.0',
         pluginType: 'SECRET',
         tags: 'community',
         official: { author: 'hashicorp', tags: 'official' },
+        publishDate: new Date(),
       },
     ];
   }
