d04fbe6ba78c081e275418ab706dce5531543023
Kianna Quach <kianna.quach@hashicorp.com>
2025-03-04 10:22:55 -0800

    Add plugin details

--- ui/lib/registry/addon/routes.js
+++ ui/lib/registry/addon/routes.js
@@ -2,7 +2,7 @@ import buildRoutes from 'ember-engines/routes';
 
 export default buildRoutes(function () {
   this.route('plugins', function () {
-    this.route('plugin', { path: '/:plugin_name' }, function () {
+    this.route('plugin', { path: '/:id' }, function () {
       this.route('details');
       this.route('edit');
     });
---
+++ ui/lib/registry/addon/routes/plugins/plugin/details.js
@@ -0,0 +1,38 @@
+/**
+ * Copyright (c) HashiCorp, Inc.
+ * SPDX-License-Identifier: BUSL-1.1
+ */
+
+import Route from '@ember/routing/route';
+import { service } from '@ember/service';
+
+export default class PluginsPluginDetailsRoute extends Route {
+  @service store;
+
+  model() {
+    // const { pluginName } = this.paramsFor('plugins.plugin');
+    // console.log('pluginName', pluginName);
+    return {
+      id: '1',
+      pluginName: 'Plugin 1',
+      author: 'Some awesome author',
+      description:
+        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.",
+      externalUrl: 'www.hello.com',
+      pluginVersion: '1.0.0',
+      pluginType: 'SECRET',
+      tags: 'community',
+      official: { author: 'hashicorp', tags: 'official' },
+      publishDate: new Date(),
+    };
+  }
+
+  setupController(controller, resolvedModel) {
+    super.setupController(controller, resolvedModel);
+
+    controller.breadcrumbs = [
+      { label: 'Messages', route: 'messages', query: { authenticated: resolvedModel.authenticated } },
+      { label: resolvedModel.title },
+    ];
+  }
+}
