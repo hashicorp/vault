91a2018e02628941f483a76c757ed8aba5dd8d1f
Lane Wetmore <lane.wetmore@hashicorp.com>
2025-03-17 19:00:03 -0500

    move common logic into before each hook

--- ui/tests/acceptance/secrets/backend/totp/key-test.js
+++ ui/tests/acceptance/secrets/backend/totp/key-test.js
@@ -26,93 +26,74 @@ module('Acceptance | totp key backend', function (hooks) {
     await click('[data-test-totp-create]');
   };
 
-  hooks.beforeEach(function () {
+  hooks.beforeEach(async function () {
     const flash = this.owner.lookup('service:flash-messages');
     this.flashSuccessSpy = spy(flash, 'success');
     this.flashDangerSpy = spy(flash, 'danger');
 
     this.uid = uuidv4();
-    return authPage.login();
+    this.mountPath = `totp-${this.uid}`;
+    this.path = `totp-${this.uid}`;
+    this.keyName = 'totp-key';
+    this.issuer = 'totp-issuer';
+    this.accountName = 'totp-acount';
+
+    await authPage.login();
+    // Setup TOTP engine
+    await enablePage.enable('totp', this.mountPath);
   });
 
   test('it views a key via menu option', async function (assert) {
-    // Setup TOTP engine
-    const mountPath = `totp-${this.uid}`;
-    await enablePage.enable('totp', mountPath);
-
-    const path = `totp-${this.uid}`;
-    const keyName = 'totp-key';
-    const issuer = 'totp-issuer';
-    const accountName = 'totp-acount';
-
     assert.strictEqual(
       currentURL(),
-      `/vault/secrets/${path}/list`,
+      `/vault/secrets/${this.path}/list`,
       'After enabling totp secrets engine it navigates to keys list'
     );
 
     await click(SES.createSecret);
-    await createVaultKey(keyName, issuer, accountName);
+    await createVaultKey(this.keyName, this.issuer, this.accountName);
     await settled();
-    await listPage.visit({ backend: path, id: keyName });
+    await listPage.visit({ backend: this.path, id: this.keyName });
     await click('[data-test-popup-menu-trigger]');
     assert.dom('.hds-dropdown li:nth-of-type(1)').hasText('Details', 'first list item is "details"');
     await click('.hds-dropdown li:nth-of-type(1) a');
-    assert.dom('.title').hasText(`TOTP key ${keyName}`);
+    assert.dom('.title').hasText(`TOTP key ${this.keyName}`);
     assert.dom('[data-test-totp-key-details]').exists();
   });
 
   test('it deletes a key via menu option', async function (assert) {
-    // Setup TOTP engine
-    const mountPath = `totp-${this.uid}`;
-    await enablePage.enable('totp', mountPath);
-
-    const path = `totp-${this.uid}`;
-    const keyName = 'totp-key';
-    const issuer = 'totp-issuer';
-    const accountName = 'totp-acount';
-
     assert.strictEqual(
       currentURL(),
-      `/vault/secrets/${path}/list`,
+      `/vault/secrets/${this.path}/list`,
       'After enabling totp secrets engine it navigates to keys list'
     );
 
     await click(SES.createSecret);
-    await createVaultKey(keyName, issuer, accountName);
+    await createVaultKey(this.keyName, this.issuer, this.accountName);
     await settled();
-    await listPage.visit({ backend: path, id: keyName });
+    await listPage.visit({ backend: this.path, id: this.keyName });
     await click('[data-test-popup-menu-trigger]');
     assert.dom('.hds-dropdown li:nth-of-type(2)').hasText('Delete', 'first list item is "details"');
     await click('.hds-dropdown li:nth-of-type(2) [data-test-confirm-action-trigger]');
     await click('[data-test-confirm-button]');
-    assert.dom(SES.secretLink(keyName)).doesNotExist(`${keyName}: key is no longer in the list`);
+    assert.dom(SES.secretLink(this.keyName)).doesNotExist(`${this.keyName}: key is no longer in the list`);
   });
 
   test('it creates a key with Vault as the provider', async function (assert) {
-    // Setup TOTP engine
-    const mountPath = `totp-${this.uid}`;
-    await enablePage.enable('totp', mountPath);
-
-    const path = `totp-${this.uid}`;
-    const keyName = 'totp-key';
-    const issuer = 'totp-issuer';
-    const accountName = 'totp-acount';
-
     assert.strictEqual(
       currentURL(),
-      `/vault/secrets/${path}/list`,
+      `/vault/secrets/${this.path}/list`,
       'After enabling totp secrets engine it navigates to keys list'
     );
 
     await click(SES.createSecret);
     assert.dom(SES.secretHeader).hasText('Create a TOTP key', 'It renders the create key page');
 
-    await createVaultKey(keyName, issuer, accountName);
-    await waitUntil(() => currentURL() === `/vault/secrets/${path}/show/${keyName}`); // flaky without this
+    await createVaultKey(this.keyName, this.issuer, this.accountName);
+    await waitUntil(() => currentURL() === `/vault/secrets/${this.path}/show/${this.keyName}`); // flaky without this
     assert.strictEqual(
       currentURL(),
-      `/vault/secrets/${path}/show/${keyName}`,
+      `/vault/secrets/${this.path}/show/${this.keyName}`,
       'totp: navigates to the show page on creation'
     );
   });
