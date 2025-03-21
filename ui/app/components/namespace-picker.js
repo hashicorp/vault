e424e7fc542ab7083e551e2c8a5eea3d96673905
Shannon Roberts <shannon.roberts@hashicorp.com>
2025-03-21 11:07:11 -0700

    [VAULT-34482] UI: Update Namespace Picker

--- ui/app/components/namespace-picker.js
+++ ui/app/components/namespace-picker.js
@@ -3,168 +3,47 @@
  * SPDX-License-Identifier: BUSL-1.1
  */
 
+import Component from '@glimmer/component';
+import { action } from '@ember/object';
+import { tracked } from '@glimmer/tracking';
 import { service } from '@ember/service';
-import { alias, gt } from '@ember/object/computed';
-import Component from '@ember/component';
-import { computed } from '@ember/object';
-import { task, timeout } from 'ember-concurrency';
-import pathToTree from 'vault/lib/path-to-tree';
-import { ancestorKeysForKey } from 'core/utils/key-utils';
 
-const DOT_REPLACEMENT = '☃';
-const ANIMATION_DURATION = 250;
+export default class ApplicationComponent extends Component {
+  @service namespace;
 
-export default Component.extend({
-  tagName: '',
-  namespaceService: service('namespace'),
-  auth: service(),
-  store: service(),
-  namespace: null,
-  listCapability: null,
-  canList: false,
+  @tracked selected = this.namespace?.currentNamespace;
+  @tracked options = [];
+  // @tracked groupedOptions = {groupName: '', options: []} // SHANNONTODO: All Namespaces Label
 
-  init() {
-    this._super(...arguments);
-    this.namespaceService?.findNamespacesForUser.perform();
-  },
+  constructor() {
+    super(...arguments);
+    this.loadOptions();
+  }
 
-  didReceiveAttrs() {
-    this._super(...arguments);
+  @action
+  async loadOptions() {
+    await this.namespace?.findNamespacesForUser.perform();
+    this.options = ['root', ...(this.namespace?.accessibleNamespaces || [])];
+    // this.groupedOptions.options = this.options; // SHANNONTODO: All Namespaces Label
+    // this.groupedOptions.groupName = `All namespaces (${this.options.length})`; // SHANNONTODO: All Namespaces Label
+  }
 
-    const ns = this.namespace;
-    const oldNS = this.oldNamespace;
-    if (!oldNS || ns !== oldNS) {
-      this.setForAnimation.perform();
-      this.fetchListCapability.perform();
-    }
-    this.set('oldNamespace', ns);
-  },
+  @action
+  handleChange(selectedOption) {
+    window.location.href = getNamespaceLink(window.location, selectedOption);
+  }
+}
 
-  fetchListCapability: task(function* () {
-    try {
-      const capability = yield this.store.findRecord('capabilities', 'sys/namespaces/');
-      this.set('listCapability', capability);
-      this.set('canList', true);
-    } catch (e) {
-      // If error out on findRecord call it's because you don't have permissions
-      // and therefore don't have permission to manage namespaces
-      this.set('canList', false);
-    }
-  }),
-  setForAnimation: task(function* () {
-    const leaves = this.menuLeaves;
-    const lastLeaves = this.lastMenuLeaves;
-    if (!lastLeaves) {
-      this.set('lastMenuLeaves', leaves);
-      yield timeout(0);
-      return;
-    }
-    const isAdding = leaves.length > lastLeaves.length;
-    const changedLeaves = isAdding ? leaves : lastLeaves;
-    const [changedLeaf] = changedLeaves.slice(-1);
-    this.set('isAdding', isAdding);
-    this.set('changedLeaf', changedLeaf);
+// PRIVATE Helper Functions
 
-    // if we're adding we want to render immediately an animate it in
-    // if we're not adding, we need time to move the item out before
-    // a rerender removes it
-    if (isAdding) {
-      this.set('lastMenuLeaves', leaves);
-      yield timeout(0);
-      return;
-    }
-    yield timeout(ANIMATION_DURATION);
-    this.set('lastMenuLeaves', leaves);
-  }).drop(),
+function getNamespaceLink(location, namespace) {
+  const origin = getOrigin(location);
+  const encodedNamespace = encodeURIComponent(namespace);
 
-  isAnimating: alias('setForAnimation.isRunning'),
+  // The full URL/origin is required so that the page is reloaded.
+  return `${origin}/ui/vault/dashboard?namespace=${encodedNamespace}`;
+}
 
-  namespacePath: alias('namespaceService.path'),
-
-  // this is an array of namespace paths that the current user
-  // has access to
-  accessibleNamespaces: alias('namespaceService.accessibleNamespaces'),
-  inRootNamespace: alias('namespaceService.inRootNamespace'),
-
-  namespaceTree: computed('accessibleNamespaces', function () {
-    const nsList = this.accessibleNamespaces;
-
-    if (!nsList) {
-      return [];
-    }
-    return pathToTree(nsList);
-  }),
-
-  maybeAddRoot(leaves) {
-    const userRoot = this.auth.authData.userRootNamespace;
-    if (userRoot === '') {
-      leaves.unshift('');
-    }
-
-    return leaves.uniq();
-  },
-
-  pathToLeaf(path) {
-    // dots are allowed in namespace paths
-    // so we need to preserve them, and replace slashes with dots
-    // in order to use Ember's get function on the namespace tree
-    // to pull out the correct level
-    return (
-      path
-        // trim trailing slash
-        .replace(/\/$/, '')
-        // replace dots with snowman
-        .replace(/\.+/g, DOT_REPLACEMENT)
-        // replace slash with dots
-        .replace(/\/+/g, '.')
-    );
-  },
-
-  // an array that keeps track of what additional panels to render
-  // on the menu stack
-  // if you're in  'foo/bar/baz',
-  // this array will be: ['foo', 'foo.bar', 'foo.bar.baz']
-  // the template then iterates over this, and does  Ember.get(namespaceTree, leaf)
-  // to render the nodes of each leaf
-
-  // gets set as  'lastMenuLeaves' in the ember concurrency task above
-  menuLeaves: computed('namespacePath', 'namespaceTree', 'pathToLeaf', function () {
-    let ns = this.namespacePath;
-    ns = (ns || '').replace(/^\//, '');
-    let leaves = ancestorKeysForKey(ns);
-    leaves.push(ns);
-    leaves = this.maybeAddRoot(leaves);
-
-    leaves = leaves.map(this.pathToLeaf);
-    return leaves;
-  }),
-
-  // the nodes at the root of the namespace tree
-  // these will get rendered as the bottom layer
-  rootLeaves: computed('namespaceTree', function () {
-    const tree = this.namespaceTree;
-    const leaves = Object.keys(tree);
-    return leaves;
-  }),
-
-  currentLeaf: alias('lastMenuLeaves.lastObject'),
-  canAccessMultipleNamespaces: gt('accessibleNamespaces.length', 1),
-  isUserRootNamespace: computed('auth.authData.userRootNamespace', 'namespacePath', function () {
-    return this.auth.authData.userRootNamespace === this.namespacePath;
-  }),
-
-  namespaceDisplay: computed('namespacePath', 'accessibleNamespaces', 'accessibleNamespaces.[]', function () {
-    const namespace = this.namespacePath;
-    if (!namespace) {
-      return 'root';
-    }
-    const parts = namespace?.split('/');
-    return parts[parts.length - 1];
-  }),
-
-  actions: {
-    refreshNamespaceList() {
-      this.namespaceService.findNamespacesForUser.perform();
-    },
-  },
-});
+function getOrigin(location) {
+  return location.protocol + '//' + location.hostname + (location.port ? ':' + location.port : '');
+}
