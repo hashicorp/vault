acb65733b1212f6ec91d1f40cb8e4c2311ccd4c1
Jordan Reimer <jordan.reimer@hashicorp.com>
2025-03-21 16:16:13 -0600

    adds comments to api and error message utils

--- ui/app/utils/api-error-message.ts
+++ ui/app/utils/api-error-message.ts
@@ -3,6 +3,12 @@
  * SPDX-License-Identifier: BUSL-1.1
  */
 
+/**
+ * this util was derived from error-message and updated to handle the error context returned from the api service
+ * once Ember Data is fully removed, the error-message util will also be removed
+ * for all requests made with the api service, use this util to display error messages from server
+ */
+
 import { ErrorContext, ApiError } from 'vault/api';
 
 // accepts an error and returns error.errors joined with a comma, error.message or a fallback message
