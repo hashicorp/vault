c517b3eb1e348d3e3c10128822d1fa90513a5c46
claire bontempo <cbontempo@hashicorp.com>
2025-03-24 14:18:51 -0700

    fix logic based on feedback

--- ui/app/components/sidebar/nav/cluster.js
+++ ui/app/components/sidebar/nav/cluster.js
@@ -12,6 +12,7 @@ export default class SidebarNavClusterComponent extends Component {
   @service version;
   @service auth;
   @service namespace;
+  @service permissions;
 
   get cluster() {
     return this.currentCluster.cluster;
@@ -24,5 +25,17 @@ export default class SidebarNavClusterComponent extends Component {
   get isRootNamespace() {
     // should only return true if we're in the true root namespace
     return this.namespace.inRootNamespace && !this.hasChrootNamespace;
+  }
+
+  get showSecretsSync() {
+    if (this.flags.secretsSyncIsActivated) {
+      // activating the feature requires different permissions than using the feature
+      // so we want to show the link to allow activation regardless of permissions to sys/sync
+      // and only check permissions if the feature has been activated
+      return this.permissions.hasNavPermission('sync');
+    }
+
+    // otherwise we show the link depending on whether or not it exists for the cluster
+    return this.version.hasSecretsSync;
   }
 }
