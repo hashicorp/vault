3acd109a615739f94a41e59a8b86df4d6852cc04
helenfufu <25168806+helenfufu@users.noreply.github.com>
2025-03-28 19:20:33 +0000

    backport of commit 513671b72c89c46ba4790aba19faf5a356b237bc

--- vault/plugincatalog/plugin_catalog_test.go
+++ vault/plugincatalog/plugin_catalog_test.go
@@ -828,6 +828,7 @@ func TestPluginCatalog_ErrDirectoryNotConfigured(t *testing.T) {
 				Name:    "binary",
 				Type:    consts.PluginTypeDatabase,
 				Command: command,
+				Sha256:  []byte{'1'},
 			})
 			dirSet := catalog.directory != ""
 			if dirSet {
