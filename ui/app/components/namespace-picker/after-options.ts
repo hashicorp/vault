174902bc1b74955bf467c3fef67b0a5693cc779a
Shannon Roberts <shannon.roberts@hashicorp.com>
2025-03-31 18:39:40 -0700

    + jsdoc

--- ui/app/components/namespace-picker/after-options.ts
+++ ui/app/components/namespace-picker/after-options.ts
@@ -10,6 +10,16 @@ interface AfterOptionsArgs {
   loadOptions: () => void;
 }
 
+/**
+ * @module AfterOptions
+ * @description component is used to display action items inside the namespace picker dropdown.
+ *  The "Manage" button directs the user to the namespace management page.
+ *  The "Refresh List" button refrehes the list of namespaces in the dropdown.
+ *
+ * @example
+ * @afterOptionsComponent={{component "namespace-picker/after-options" loadOptions=this.loadOptions}}
+ */
+
 export default class AfterOptions extends Component<AfterOptionsArgs> {
   @action
   refreshNamespaceList() {
