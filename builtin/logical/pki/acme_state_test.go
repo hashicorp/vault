4b9ba4fbee9152f3a3dc9d7c2c2aee3942d23873
Kit Haines <khaines@mit.edu>
2025-04-03 11:45:02 -0400

    Remove subproblems from acme error body if it doesn't exist.  Simple test to confirm.

--- builtin/logical/pki/acme_state_test.go
+++ builtin/logical/pki/acme_state_test.go
@@ -4,6 +4,7 @@
 package pki
 
 import (
+	"encoding/json"
 	"testing"
 
 	"github.com/stretchr/testify/require"
@@ -40,4 +41,26 @@ func TestAcmeNonces(t *testing.T) {
 		nonce = nonces[i]
 		require.False(t, a.RedeemNonce(nonce))
 	}
+}
+
+func TestErrorResponseNoSubproblems(t *testing.T) {
+	t.Parallel()
+	errResponse, err := TranslateError(ErrAlreadyRevoked)
+	if err != nil {
+		return
+	}
+	require.NoError(t, err, "already revoked should generate an error response")
+	require.NotNil(t, errResponse.Data)
+	body := map[string]string{}
+	rawBody, ok := errResponse.Data["http_raw_body"]
+	err = json.Unmarshal(rawBody.([]byte), &body)
+	require.True(t, ok, "Raw Body of Error response should exist, but doesn't")
+	typeString, ok := body["type"]
+	require.True(t, ok, "Type on Raw Body of Error response should exist, but doesn't")
+	require.Equal(t, typeString, "urn:ietf:params:acme:error:alreadyRevoked")
+	_, ok = body["detail"]
+	require.True(t, ok, "Detail on Raw Body of Error response should exist, but doesn't")
+	subProblems, ok := body["subproblems"]
+	require.False(t, ok, "subproblems on Raw Body of Error response should be omitted, but exists with value %v", subProblems)
+
 }
