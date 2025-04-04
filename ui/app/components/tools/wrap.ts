d162f58a084e0cf6108802f9f6335d100ce440ca
Zack Moore <zack.moore@hashicorp.com>
2025-04-04 14:43:12 -0400

    fixing ts issues

--- ui/app/components/tools/wrap.ts
+++ ui/app/components/tools/wrap.ts
@@ -3,6 +3,8 @@
  * SPDX-License-Identifier: BUSL-1.1
  */
 
+// TODO: Test this component
+
 import Component from '@glimmer/component';
 import { service } from '@ember/service';
 import { action } from '@ember/object';
@@ -14,7 +16,6 @@ import type ApiService from 'vault/services/api';
 import type FlashMessageService from 'vault/services/flash-messages';
 import type { TtlEvent } from 'vault/app-types';
 import type { HTMLElementEvent } from 'vault/forms';
-import type { Editor } from 'codemirror';
 
 /**
  * @module ToolsWrap
@@ -68,8 +69,12 @@ export default class ToolsWrap extends Component {
     this.wrapTTL = evt.enabled ? `${evt.seconds}s` : '30m';
   }
 
+  // TODO: Make this work
   @action
-  codemirrorUpdated(val: string, codemirror: Editor) {
+  codemirrorUpdated(
+    val: string,
+    codemirror: { performLint: () => void; state: { lint: { marked: unknown[] } } }
+  ) {
     codemirror.performLint();
     this.hasLintingErrors = codemirror?.state.lint.marked?.length > 0;
     if (!this.hasLintingErrors) this.wrapData = JSON.parse(val);
