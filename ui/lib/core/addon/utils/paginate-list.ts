943484f0b92b1d6162bb552ba33c1ff5ba709636
Jordan Reimer <jordan.reimer@hashicorp.com>
2025-04-09 21:45:36 -0600

    adds paginate-list util

---
+++ ui/lib/core/addon/utils/paginate-list.ts
@@ -0,0 +1,55 @@
+/**
+ * Copyright (c) HashiCorp, Inc.
+ * SPDX-License-Identifier: BUSL-1.1
+ */
+
+import config from 'vault/config/environment';
+
+const { DEFAULT_PAGE_SIZE } = config.APP;
+
+type PaginatedData = string | Record<string, string>[];
+type PaginateOptions = {
+  page?: number;
+  pageSize?: number;
+  filter?: string;
+  filterKey?: string;
+};
+
+/**
+ * Util to paginate data set based on page number and size
+ * If filter is provided, it will filter the data prior to paginating
+ */
+
+export const paginate = (data: PaginatedData, options: PaginateOptions = {}) => {
+  const { page = 1, pageSize = DEFAULT_PAGE_SIZE, filter, filterKey } = options;
+  let filteredData = data;
+
+  if (Array.isArray(data)) {
+    // filter data before paginating if filter is provided
+    if (filter) {
+      filteredData = data.filter((item) => {
+        const filterValue = filterKey ? item[filterKey] : item;
+        return filterValue || ''.toLowerCase().includes(filter.toLowerCase());
+      });
+    }
+
+    const lastPage = Math.floor(filteredData.length / pageSize);
+    const start = (page - 1) * pageSize;
+    const end = start + pageSize;
+    filteredData = filteredData.slice(start, end);
+    // add meta data previously from lazyPaginatedQuery since components expect it
+    Object.defineProperty(filteredData, 'meta', {
+      value: {
+        currentPage: page,
+        lastPage,
+        nextPage: page + 1,
+        prevPage: page - 1,
+        total: data.length,
+        filteredTotal: filteredData.length,
+        pageSize,
+      },
+    });
+  }
+
+  return filteredData;
+};
