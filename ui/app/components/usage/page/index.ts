545f5a2cee96c245aee790a627ffc45b29df5d5c
Jim Wright <jim.wright@hashicorp.com>
2025-04-14 12:23:26 -0700

    Add engine and auth method mappings to data fetching function (#30205)

--- ui/app/components/usage/page/index.ts
+++ ui/app/components/usage/page/index.ts
@@ -7,6 +7,8 @@ import Component from '@ember/component';
 import { service } from '@ember/service';
 import type ApiService from 'vault/services/api';
 import type { getUsageDataFunction, UsageDashboardData } from '@hashicorp/vault-reporting/types/index';
+import { allEngines } from 'vault/helpers/mountable-secret-engines';
+import { allMethods } from 'vault/helpers/mountable-auth-methods';
 import type FlagsService from 'vault/services/flags';
 export default class ClientsActivityComponent extends Component {
   @service declare readonly api: ApiService;
@@ -14,7 +16,22 @@ export default class ClientsActivityComponent extends Component {
 
   handleFetchUsageData: getUsageDataFunction = async () => {
     //TODO: Update client with typed response after the API is updated https://hashicorp.atlassian.net/browse/VAULT-35108
-    const { data = {} } = await this.api.sys.systemReadUtilizationReport();
+    const response = await this.api.sys.systemReadUtilizationReport();
+    const data = response.data as UsageDashboardData;
+    // Replace engine names with display names if available
+    allEngines().forEach((engine) => {
+      if (engine.type in data.secret_engines) {
+        data.secret_engines[engine.displayName] = data.secret_engines[engine.type] || 0;
+        delete data.secret_engines[engine.type];
+      }
+    });
+    // Replace auth method names with display names if available
+    allMethods().forEach((method) => {
+      if (method.type in data.auth_methods) {
+        data.auth_methods[method.displayName] = data.auth_methods[method.type] || 0;
+        delete data.auth_methods[method.type];
+      }
+    });
     return data as UsageDashboardData;
   };
 
