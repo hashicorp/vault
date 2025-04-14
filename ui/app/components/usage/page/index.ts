13b1a31e96384a7bebacc7cb77c5b203732f4000
Eren Tantekin <eren.tantekin@gmail.com>
2025-04-14 11:49:12 -0700

    [UI] HVT-6840: Add isVaultDedicated argument + Move CSS #30247

--- ui/app/components/usage/page/index.ts
+++ ui/app/components/usage/page/index.ts
@@ -7,12 +7,18 @@ import Component from '@ember/component';
 import { service } from '@ember/service';
 import type ApiService from 'vault/services/api';
 import type { getUsageDataFunction, UsageDashboardData } from '@hashicorp/vault-reporting/types/index';
+import type FlagsService from 'vault/services/flags';
 export default class ClientsActivityComponent extends Component {
   @service declare readonly api: ApiService;
+  @service declare readonly flags: FlagsService;
 
   handleFetchUsageData: getUsageDataFunction = async () => {
     //TODO: Update client with typed response after the API is updated https://hashicorp.atlassian.net/browse/VAULT-35108
     const { data = {} } = await this.api.sys.systemReadUtilizationReport();
     return data as UsageDashboardData;
   };
+
+  get isVaultDedicated(): boolean {
+    return this.flags.isHvdManaged;
+  }
 }
