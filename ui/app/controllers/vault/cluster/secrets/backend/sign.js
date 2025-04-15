bf461a7f071442c42779c58f09a4cbc96de395f7
Zack Moore <zack.moore@hashicorp.com>
2025-04-15 12:38:23 -0400

    fixing linting issues

--- ui/app/controllers/vault/cluster/secrets/backend/sign.js
+++ ui/app/controllers/vault/cluster/secrets/backend/sign.js
@@ -19,13 +19,12 @@ export default Controller.extend({
       });
     },
 
-    // TODO: address linting errors
-    codemirrorUpdated(attr, val, codemirror) {
-      codemirror.performLint();
-      const hasErrors = codemirror.state.lint.marked.length > 0;
-
-      if (!hasErrors) {
+    codemirrorUpdated(attr, val) {
+      // wont set invalid JSON to the model
+      try {
         set(this.model, attr, JSON.parse(val));
+      } catch {
+        // linting is handled by the component
       }
     },
 
