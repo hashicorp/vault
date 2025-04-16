6520197f17f25c50accbbba0cb479802e16ee432
tin.vo <tin.vo@hashicorp.com>
2025-04-16 10:47:48 -0700

    testing enos aws engine test

--- enos/modules/verify_log_secrets/scripts/scan_logs_for_secrets.sh
+++ enos/modules/verify_log_secrets/scripts/scan_logs_for_secrets.sh
@@ -11,17 +11,17 @@ verify_radar_scan_output_file() {
   # Given a file with a radar scan output, filter out tagged false positives and verify that no
   # other secrets remain.
   echo "-----------0"
-  jq -eMcn '[inputs]'
+  jq -eMn '[inputs]'
   echo "-----------00"
-  jq -eMcn '[inputs] | [.[]]'
+  jq -eMn '[inputs] | [.[]]'
   echo "-----------01"
-  jq -eMcn '[inputs] | [.[] | select((.tags == null))]'
+  jq -eMn '[inputs] | [.[] | select((.tags == null))]'
   echo "-----------02"
-  jq -eMcn '[inputs] | [.[] | select((.tags == null) or (.tags | contains(["ignore_rule"])))]'
+  jq -eMn '[inputs] | [.[] | select((.tags == null) or (.tags | contains(["ignore_rule"])))]'
   echo "-----------03"
-  jq -eMcn '[inputs] | [.[] | select((.tags == null) or (.tags | contains(["ignore_rule"]) | not ))]'
+  jq -eMn '[inputs] | [.[] | select((.tags == null) or (.tags | contains(["ignore_rule"]) | not ))]'
   echo "-----------04"
-  jq -eMcn '[inputs] | [.[] | select((.tags == null) or (.tags | contains(["ignore_rule"]) | not ))] | length == 0'
+  jq -eMn '[inputs] | [.[] | select((.tags == null) or (.tags | contains(["ignore_rule"]) | not ))] | length == 0'
   echo -e "-----------4\n"
   if ! jq -eMcn '[inputs] | [.[] | select((.tags == null) or (.tags | contains(["ignore_rule"]) | not ))] | length == 0' < "$2"; then
     found=$(jq -eMn '[inputs] | [.[] | select((.tags == null) or (.tags | contains(["ignore_rule"]) | not ))]' < "$2")
