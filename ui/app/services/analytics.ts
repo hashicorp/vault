bfd56e11357330187923663af2ee3596794a0e89
Evan Moncuso <emoncuso@hashicorp.com>
2025-04-16 19:44:59 -0700

    add dummy provider and wire it into the app

---
+++ ui/app/services/analytics.ts
@@ -0,0 +1,66 @@
+import Service, { service } from '@ember/service';
+import { tracked } from '@glimmer/tracking';
+
+import { DummyProvider, PROVIDER_NAME as DummyProviderName } from 'vault/utils/analytics-providers/dummy';
+
+import type { AnalyticsProvider, AnalyticsProviderConfig } from 'vault/vault/analytics';
+import type Owner from '@ember/owner';
+import type RouterService from '@ember/routing/router-service';
+
+export default class AnalyticsService extends Service {
+  @service declare readonly router: RouterService;
+
+  @tracked provider: AnalyticsProvider = new DummyProvider();
+
+  debug = true;
+
+  private log(...args: unknown[]) {
+    if (this.debug) {
+      // eslint-disable-next-line no-console
+      console.log(`[Analytics - ${this.provider.name}]`, ...args);
+    }
+  }
+
+  private setupRouteEventListener() {
+    // on successful route changes...
+    this.router.on('routeDidChange', () => {
+      const { currentRouteName, currentURL } = this.router;
+
+      this.trackPageView(currentURL || '', {
+        currentRouteName: currentRouteName || '',
+      });
+    });
+  }
+
+  constructor(owner: Owner) {
+    super(owner);
+
+    this.setupRouteEventListener();
+  }
+
+  identifyUser(identifer: string, traits: Record<string, string>) {
+    this.provider.identify(identifer, traits);
+    this.log('identifyUser', identifer, traits);
+  }
+
+  start(provider: string, config: AnalyticsProviderConfig = {}) {
+    // fail silently, analytics is nonessential
+    if (!provider) return;
+
+    switch (provider) {
+      case DummyProviderName:
+        this.provider = new DummyProvider();
+        break;
+    }
+
+    this.provider.start(config);
+
+    this.log('start');
+  }
+
+  trackPageView(routeName: string, metadata: Record<string, string>) {
+    this.provider.trackPageView(routeName, metadata);
+
+    this.log('$pageview', routeName, metadata);
+  }
+}
---
+++ ui/app/utils/analytics-providers/dummy.ts
@@ -0,0 +1,23 @@
+/*
+  Normally, the default export of this file would _do something_. In our case, the mthods for the provider simply log the 
+*/
+import type { AnalyticsProvider } from 'vault/vault/analytics';
+
+export const PROVIDER_NAME = 'dummy';
+
+export class DummyProvider implements AnalyticsProvider {
+  name = PROVIDER_NAME;
+
+  start() {
+    /* intentionally blank */
+  }
+
+  // identifier: string, context: Record<string, string>
+  identify() {
+    /* intentionally blank */
+  }
+
+  trackPageView() {
+    /* intentionally blank */
+  }
+}
