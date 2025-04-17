fc5d9c2693982ccbfe4df23ac0a2710ee8ddac45
tin.vo <tin.vo@hashicorp.com>
2025-04-16 20:28:56 -0700

    testing enos aws engine test

--- enos/modules/verify_log_secrets/scripts/scan_logs_for_secrets.sh
+++ enos/modules/verify_log_secrets/scripts/scan_logs_for_secrets.sh
@@ -11,13 +11,13 @@ verify_radar_scan_output_file() {
   # Given a file with a radar scan output, filter out tagged false positives and verify that no
   # other secrets remain.
   echo "-----------0"
-  jq -eMn '[inputs] | [.[] | | select(.type == "aws_access_key_id")]' < "$2"
+  jq -eMn '[inputs] | [.[] | select(.type == "aws_access_key_id")]' < "$2"
   echo "-----------00"
-  jq -eMn '[inputs] | [.[] | | select(.type == "aws_access_key_id") or select((.tags == null))]' < "$2"
+  jq -eMn '[inputs] | [.[] | select(.type == "aws_access_key_id") or select((.tags == null))]' < "$2"
   echo "-----------000"
-  jq -eMn '[inputs] | [.[] | | select((.tags == null) or select(.type == "aws_access_key_id") or (.tags | contains(["ignore_rule"]) | not ))]' < "$2"
+  jq -eMn '[inputs] | [.[] | select((.tags == null) or select(.type == "aws_access_key_id") or (.tags | contains(["ignore_rule"]) | not ))]' < "$2"
   echo "-----------0000"
-  jq -eMn '[inputs] | [.[] | | select(.type == "aws_access_key_id") or select((.tags == null) or (.tags | contains(["ignore_rule"]) | not ))]' < "$2"
+  jq -eMn '[inputs] | [.[] | select(.type == "aws_access_key_id") or select((.tags == null) or (.tags | contains(["ignore_rule"]) | not ))]' < "$2"
   echo "-----------01"
   jq -eMn '[inputs] | [.[] | select(.type == "aws_access_key_id") or select((.tags == null) or (.tags | contains(["ignore_rule"]) | not ))]' < "$2"
   echo "-----------02"
