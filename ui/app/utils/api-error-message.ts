3a8425b658ec756267271b6613af03a1e86f42c7
Jordan Reimer <jordan.reimer@hashicorp.com>
2025-04-17 16:17:38 -0600

    updates api-error-message util to log out error in dev env

--- ui/app/utils/api-error-message.ts
+++ ui/app/utils/api-error-message.ts
@@ -10,10 +10,16 @@
  */
 
 import { ErrorContext, ApiError } from 'vault/api';
+import ENV from 'vault/config/environment';
 
 // accepts an error and returns error.errors joined with a comma, error.message or a fallback message
 export default async function (error: unknown, fallbackMessage = 'An error occurred, please try again') {
   const messageOrFallback = (message?: string) => message || fallbackMessage;
+
+  // log out the error for ease of debugging in dev env
+  if (ENV.environment === 'development') {
+    console.error('API Error:', error); // eslint-disable-line no-console
+  }
 
   if ((error as ErrorContext).response instanceof Response) {
     const apiError: ApiError = await (error as ErrorContext).response?.json();
