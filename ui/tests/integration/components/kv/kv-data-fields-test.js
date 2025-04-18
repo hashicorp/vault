b374c2e0d2fc9923b35a3d4e52ba659a468c6ec6
Zack Moore <zack.moore@hashicorp.com>
2025-04-17 20:02:15 -0400

    fixing tests

--- ui/tests/integration/components/kv/kv-data-fields-test.js
+++ ui/tests/integration/components/kv/kv-data-fields-test.js
@@ -8,8 +8,8 @@ import { setupRenderingTest } from 'vault/tests/helpers';
 import { setupEngine } from 'ember-engines/test-support';
 import { setupMirage } from 'ember-cli-mirage/test-support';
 import { hbs } from 'ember-cli-htmlbars';
-import { fillIn, render, click } from '@ember/test-helpers';
-import codemirror from 'vault/tests/helpers/codemirror';
+import { fillIn, render, click, waitFor, findAll } from '@ember/test-helpers';
+import codemirror, { getCodeEditorValue, setCodeEditorValue } from 'vault/tests/helpers/codemirror';
 import { PAGE, FORM } from 'vault/tests/helpers/kv/kv-selectors';
 import { GENERAL } from 'vault/tests/helpers/general-selectors';
 import { setRunOptions } from 'ember-a11y-testing/test-support';
@@ -50,14 +50,22 @@ module('Integration | Component | kv-v2 | KvDataFields', function (hooks) {
     assert.expect(3);
 
     await render(hbs`<KvDataFields @showJson={{true}} @secret={{this.secret}} />`, { owner: this.engine });
+    await waitFor('.cm-editor');
+    const editor = codemirror();
+    const editorValue = getCodeEditorValue(editor);
     assert.strictEqual(
-      codemirror().getValue(' '),
-      `{   \"\": \"\" }`, // eslint-disable-line no-useless-escape
+      editorValue,
+      `{
+  "": ""
+}`,
       'json editor initializes with empty object that includes whitespace'
     );
-    await fillIn(`${FORM.jsonEditor} textarea`, 'blah');
-    assert.strictEqual(codemirror().state.lint.marked.length, 1, 'codemirror lints input');
-    codemirror().setValue(`{ "hello": "there"}`);
+    setCodeEditorValue(editor, 'blah');
+
+    await waitFor('.cm-lint-marker');
+    const lintMarkers = findAll('.cm-lint-marker');
+    assert.strictEqual(lintMarkers.length, 1, 'codemirror lints input');
+    setCodeEditorValue(editor, `{ "hello": "there"}`);
     assert.propEqual(this.secret.secretData, { hello: 'there' }, 'json editor updates secret data');
   });
 
