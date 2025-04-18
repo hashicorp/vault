8c6052f28a4e492b90d4e9fe699f27f48152106a
tin.vo <tin.vo@hashicorp.com>
2025-04-17 18:09:57 -0700

    updating test for enterprise

--- enos/modules/verify_secrets_engines/modules/read/aws.tf
+++ enos/modules/verify_secrets_engines/modules/read/aws.tf
@@ -6,15 +6,15 @@ resource "enos_remote_exec" "aws_verify_new_creds" {
   for_each = var.hosts
 
   environment = {
-    AWS_REGION            = "${var.create_state.aws.region}"
-    MOUNT                 = "${var.create_state.aws.mount}"
-    AWS_USER_NAME         = "${var.create_state.aws.aws_user_name}"
-    AWS_ACCESS_KEY_ID     = "${var.create_state.aws.aws_access_key}"
-    AWS_SECRET_ACCESS_KEY = "${var.create_state.aws.aws_secret_key}"
-    VAULT_AWS_ROLE        = "${var.create_state.aws.vault_aws_role}"
-    VAULT_ADDR            = var.vault_addr
-    VAULT_TOKEN           = var.vault_root_token
-    VAULT_INSTALL_DIR     = var.vault_install_dir
+    AWS_REGION              = "${var.create_state.aws.region}"
+    MOUNT                   = "${var.create_state.aws.mount}"
+    AWS_USER_NAME           = "${var.create_state.aws.aws_user_name}"
+    AWS_ACCESS_KEY_ID       = "${var.create_state.aws.aws_access_key}"
+    AWS_SECRET_ACCESS_KEY   = "${var.create_state.aws.aws_secret_key}"
+    VAULT_AWS_ROLE          = "${var.create_state.aws.vault_aws_role}"
+    VAULT_ADDR              = var.vault_addr
+    VAULT_TOKEN             = var.vault_root_token
+    VAULT_INSTALL_DIR       = var.vault_install_dir
     VERIFY_AWS_ENGINE_CERTS = var.verify_aws_engine_creds
   }
 
