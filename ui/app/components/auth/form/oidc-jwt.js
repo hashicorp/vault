73dfbe5b2577e3c69bbc444429d85b1a116906b4
claire bontempo <cbontempo@hashicorp.com>
2025-04-23 17:02:47 -0700

    build oidc-jwt auth component

--- ui/app/components/auth/form/oidc-jwt.js
+++ ui/app/components/auth/form/oidc-jwt.js
@@ -4,6 +4,13 @@
  */
 
 import AuthBase from './base';
+import Ember from 'ember';
+import { tracked } from '@glimmer/tracking';
+import { service } from '@ember/service';
+import { restartableTask, task, timeout, waitForEvent } from 'ember-concurrency';
+import { action } from '@ember/object';
+import { sanitizePath } from 'core/utils/sanitize-path';
+import { waitFor } from '@ember/test-waiters';
 
 /**
  * @module Auth::Form::OidcJwt
@@ -14,11 +21,228 @@ import AuthBase from './base';
  * the role
  */
 
+const ERROR_WINDOW_CLOSED =
+  'The provider window was closed before authentication was complete. Your web browser may have blocked or closed a pop-up window. Please check your settings and click Sign In to try again.';
+const ERROR_MISSING_PARAMS =
+  'The callback from the provider did not supply all of the required parameters.  Please click Sign In to try again. If the problem persists, you may want to contact your administrator.';
+const ERROR_JWT_LOGIN = 'OIDC login is not configured for this mount';
+export { ERROR_WINDOW_CLOSED, ERROR_MISSING_PARAMS, ERROR_JWT_LOGIN };
+
 export default class AuthFormOidcJwt extends AuthBase {
-  loginFields = [
-    {
-      name: 'role',
-      helperText: 'Vault will use the default role to sign in if this field is left blank.',
-    },
-  ];
+  @service auth;
+  @service flags;
+  @service store;
+
+  // set by form inputs
+  @tracked formData = null;
+
+  // set by auth workflow
+  @tracked fetchedRole = null;
+  @tracked errorMessage = null;
+  @tracked isOIDC = true;
+
+  @action
+  initializeFormData(element) {
+    this.formData = new FormData(element);
+    this.fetchRole.perform();
+  }
+
+  @action
+  updateFormData(event) {
+    const { name, value } = event.target;
+    this.formData.set(name, value);
+
+    // only fetch role if the following inputs have changed
+    if (['path', 'role', 'namespace'].includes(name)) {
+      this.fetchRole.perform(500);
+    }
+  }
+
+  fetchRole = restartableTask(async (wait) => {
+    // task is restartable so if the user starts typing again,
+    // it will cancel and restart from the beginning.
+    if (wait) await timeout(wait);
+
+    const namespace = this.formData.get('namespace') || '';
+    const path = sanitizePath(this.formData.get('path')) || this.args.authType;
+    const role = this.formData.get('role') || '';
+    const id = JSON.stringify([path, role]);
+
+    // reset state
+    this.fetchedRole = null;
+    this.errorMessage = null;
+
+    try {
+      this.fetchedRole = await this.store.findRecord('role-jwt', id, {
+        adapterOptions: { namespace },
+      });
+      this.isOIDC = true;
+    } catch (e) {
+      const error = (e.errors || [])[0];
+      const errorMessage =
+        e.httpStatus === 400 ? 'Invalid role. Please try again.' : `Error fetching role: ${error}`;
+      // if the mount is configured for JWT authentication via static keys, JWKS, or OIDC discovery
+      // this specific error is returned. Flip the isOIDC boolean accordingly, otherwise assume OIDC.
+      this.isOIDC = error !== ERROR_JWT_LOGIN;
+      // track errors but only display them on submit
+      this.errorMessage = errorMessage;
+    }
+  });
+
+  login = task(
+    waitFor(async (formData) => {
+      if (this.isOIDC) {
+        this.startOIDCAuth();
+      } else {
+        this.continueLogin(formData);
+      }
+    })
+  );
+
+  async continueLogin(data) {
+    // if (data?.mfa_requirement) {
+    //   // calls onAuthResponse in parent auth/page.js component
+    //   this.handleAuthResponse(data, this.args.authType);
+    //   // return here because mfa-form.js will finish login/authentication flow after mfa validation
+    //   return;
+    // }
+
+    try {
+      // OIDC callback returns a token so authenticate with that
+      const backend = this.isOIDC && data?.token ? 'token' : this.args.authType;
+
+      const authResponse = await this.auth.authenticate({
+        clusterId: this.args.cluster.id,
+        backend,
+        data,
+        selectedAuth: this.args.authType,
+      });
+      // responsible for redirect after auth data is persisted
+      this.handleAuthResponse(authResponse);
+    } catch (error) {
+      this.onError(error);
+    }
+  }
+
+  //* OIDC AUTH BEGINS
+  async startOIDCAuth() {
+    await this.fetchRole.perform();
+
+    const error =
+      this.fetchedRole && !this.fetchedRole.authUrl
+        ? 'Missing auth_url. Please check that allowed_redirect_uris for the role include this mount path.'
+        : this.errorMessage || null;
+
+    if (error) {
+      this.onError(error);
+    } else {
+      const win = window;
+      const POPUP_WIDTH = 500;
+      const POPUP_HEIGHT = 600;
+      const left = win.screen.width / 2 - POPUP_WIDTH / 2;
+      const top = win.screen.height / 2 - POPUP_HEIGHT / 2;
+      const oidcWindow = win.open(
+        this.fetchedRole.authUrl,
+        'vaultOIDCWindow',
+        `width=${POPUP_WIDTH},height=${POPUP_HEIGHT},resizable,scrollbars=yes,top=${top},left=${left}`
+      );
+
+      this.prepareForOIDC.perform(oidcWindow);
+    }
+  }
+
+  // NOTE TO DEVS: Be careful when updating the OIDC flow and ensure the updates
+  // work with implicit flow. See issue https://github.com/hashicorp/vault-plugin-auth-jwt/pull/192
+  prepareForOIDC = task(async (oidcWindow) => {
+    const thisWindow = window;
+
+    // start watching the popup window and the current one
+    this.watchPopup.perform(oidcWindow);
+    this.watchCurrent.perform(oidcWindow);
+    // wait for message posted from oidc callback
+    // see issue https://github.com/hashicorp/vault/issues/12436
+    // ensure that postMessage event is from expected source
+    // eslint-disable-next-line no-constant-condition
+    while (true) {
+      const event = await waitForEvent(thisWindow, 'message');
+      if (event.origin === thisWindow.origin && event.isTrusted && event.data.source === 'oidc-callback') {
+        return this.exchangeOIDC.perform(event.data, oidcWindow);
+      }
+    }
+  });
+
+  exchangeOIDC = task(async (oidcState, oidcWindow) => {
+    if (oidcState === null || oidcState === undefined) {
+      return;
+    }
+
+    let { namespace, path, state, code } = oidcState;
+
+    // The namespace can be either be passed as a query parameter, or be embedded
+    // in the state param in the format `<state_id>,ns=<namespace>`. So if
+    // `namespace` is empty, check for namespace in state as well.
+    if (namespace === '' || this.flags.hvdManagedNamespaceRoot) {
+      const i = state.indexOf(',ns=');
+      if (i >= 0) {
+        // ",ns=" is 4 characters
+        namespace = state.substring(i + 4);
+        state = state.substring(0, i);
+      }
+    }
+
+    if (!path || !state || !code) {
+      return this.cancelLogin(oidcWindow, ERROR_MISSING_PARAMS);
+    }
+    const adapter = this.store.adapterFor('auth-method');
+
+    let resp;
+    // do the OIDC exchange, set the token and continue login flow
+    try {
+      resp = await adapter.exchangeOIDC(path, state, code);
+      this.closeWindow(oidcWindow);
+    } catch (e) {
+      // If there was an error on Vault's end, close the popup
+      // and show the error on the login screen
+      return this.cancelLogin(oidcWindow, e);
+    }
+
+    const { client_token, mfa_requirement } = resp.auth;
+    const callbackData = { token: client_token, mfa_requirement };
+    await this.continueLogin(callbackData);
+  });
+
+  // MANAGE POPUPS
+  watchPopup = task(async (oidcWindow) => {
+    // eslint-disable-next-line no-constant-condition
+    while (true) {
+      const WAIT_TIME = Ember.testing ? 50 : 500;
+
+      await timeout(WAIT_TIME);
+      if (!oidcWindow || oidcWindow.closed) {
+        return this.handleOIDCError(ERROR_WINDOW_CLOSED);
+      }
+    }
+  });
+
+  watchCurrent = task(async (oidcWindow) => {
+    // when user is about to change pages, close the popup window
+    await waitForEvent(window, 'beforeunload');
+    oidcWindow.close();
+  });
+
+  cancelLogin(oidcWindow, errorMessage) {
+    this.closeWindow(oidcWindow);
+    this.handleOIDCError(errorMessage);
+  }
+
+  closeWindow(oidcWindow) {
+    this.watchPopup.cancelAll();
+    this.watchCurrent.cancelAll();
+    oidcWindow.close();
+  }
+
+  handleOIDCError(err) {
+    this.prepareForOIDC.cancelAll();
+    this.onError(err);
+  }
 }
--- ui/tests/integration/components/auth/form/oidc-jwt-test.js
+++ ui/tests/integration/components/auth/form/oidc-jwt-test.js
@@ -6,23 +6,155 @@
 import { module, test } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
 import hbs from 'htmlbars-inline-precompile';
-import { find, render } from '@ember/test-helpers';
+import { click, fillIn, find, render } from '@ember/test-helpers';
 import sinon from 'sinon';
 import { setupMirage } from 'ember-cli-mirage/test-support';
 import testHelper from './test-helper';
 import { GENERAL } from 'vault/tests/helpers/general-selectors';
+import { overrideResponse } from 'vault/tests/helpers/stubs';
+import { ERROR_JWT_LOGIN } from 'vault/components/auth/form/oidc-jwt';
+import { AUTH_FORM } from 'vault/tests/helpers/auth/auth-form-selectors';
+
+/* 
+The OIDC and JWT mounts call the same endpoint (see docs https://developer.hashicorp.com/vault/docs/auth/jwt )
+because of this the same component is used to render both method types. 
+The module name refers to the selected auth type and there is test coverage in each to cover
+1. auth url request (fetching role) is made when expected
+2. JWT token login flow
+2. OIDC exchange/login situation
+*/
+
+const authUrlRequestTests = (test) => {
+  test('it requests auth_url when it initially renders', async function (assert) {
+    assert.expect(2);
+    this.server.post(`/auth/${this.authType}/oidc/auth_url`, (_, req) => {
+      const { role } = JSON.parse(req.requestBody);
+      assert.true(true, 'it makes request to auth_url');
+      assert.strictEqual(role, '', 'role is empty');
+      return { data: { auth_url: '123-example.com' } };
+    });
+    await this.renderComponent();
+  });
+
+  test('it re-requests auth_url when input changes: role', async function (assert) {
+    // request assertions should be hit twice, once on initial render and again on role change
+    assert.expect(4);
+    let count = 0;
+    this.server.post(`/auth/${this.authType}/oidc/auth_url`, (_, req) => {
+      count++;
+      const { role } = JSON.parse(req.requestBody);
+      assert.true(true, 'it makes request to auth_url');
+      const expectedRole = count === 1 ? '' : 'myrole';
+      assert.strictEqual(role, expectedRole, 'payload has expected role');
+      return { data: { auth_url: '123-example.com' } };
+    });
+    await this.renderComponent({ yieldBlock: true });
+    await fillIn(GENERAL.inputByAttr('role'), 'myrole');
+  });
+
+  test('it re-requests auth_url when input changes: path', async function (assert) {
+    assert.expect(2);
+    let firstRequest, secondRequest;
+    this.server.post(`/auth/${this.authType}/oidc/auth_url`, () => {
+      firstRequest = true;
+      return { data: { auth_url: '123-example.com' } };
+    });
+    this.server.post(`/auth/mypath/oidc/auth_url`, () => {
+      secondRequest = true;
+      return { data: { auth_url: '123-example.com' } };
+    });
+    await this.renderComponent({ yieldBlock: true });
+    await fillIn(GENERAL.inputByAttr('path'), 'mypath');
+
+    // asserting this way instead of inside the request to ensure each endpoint is hit.
+    // (asserting within each request would rely on assertion count and could result in a false positive)
+    assert.true(firstRequest, 'it makes FIRST request to auth_url with default path');
+    assert.true(secondRequest, 'it makes SECOND request to auth_url with custom path');
+  });
+
+  test('it re-requests auth_url when input changes: namespace', async function (assert) {
+    // request assert should be hit twice, once on initial render and again on namespace change
+    assert.expect(2);
+    this.server.post(`/auth/${this.authType}/oidc/auth_url`, () => {
+      assert.true(true, 'it makes request to auth_url');
+      return { data: { auth_url: '123-example.com' } };
+    });
+    await render(hbs`
+      <Auth::Form::OidcJwt 
+        @authType={{this.authType}} 
+        @cluster={{this.cluster}}
+        @onError={{this.onError}}
+        @onSuccess={{this.onSuccess}}
+      >
+        <:namespace>
+          <label for="namespace">Namespace</label>
+          <input data-test-input="namespace" id="namespace" name="namespace" type="text" /> 
+        </:namespace>
+      </Auth::Form::OidcJwt>`);
+    await fillIn(GENERAL.inputByAttr('namespace'), 'mynamespace');
+  });
+};
+
+const jwtLoginTests = (test) => {
+  test('it submits form data with defaults', async function (assert) {
+    await this.renderComponent();
+
+    await fillIn(GENERAL.inputByAttr('role'), 'some-dev');
+    await fillIn(GENERAL.inputByAttr('jwt'), 'some-jwt-token');
+
+    await click(AUTH_FORM.login);
+    const [actual] = this.authenticateStub.lastCall.args;
+    assert.propEqual(
+      actual.data,
+      this.expectedSubmit.default,
+      'auth service "authenticate" method is called with form data'
+    );
+  });
+
+  test('it submits form data from yielded inputs', async function (assert) {
+    await this.renderComponent({ yieldBlock: true });
+    await fillIn(GENERAL.inputByAttr('role'), 'some-dev');
+    await fillIn(GENERAL.inputByAttr('jwt'), 'some-jwt-token');
+    await fillIn(GENERAL.inputByAttr('path'), `custom-${this.authType}`);
+
+    await click(AUTH_FORM.login);
+    const [actual] = this.authenticateStub.lastCall.args;
+    assert.propEqual(
+      actual.data,
+      this.expectedSubmit.custom,
+      'auth service "authenticate" method is called with yielded form data'
+    );
+  });
+
+  test('it does NOT re-request the auth_url when jwt token changes', async function (assert) {
+    assert.expect(1); // the assertion in the stubbed request should not be hit
+    await this.renderComponent();
+    this.server.post(`/auth/${this.authType}/oidc/auth_url`, () => {
+      // we can't throw an error here because the component catches error handling.
+      // setting this assertion to fail intentionally because this request should not have been made
+      assert.false(true, 'request made to auth_url and it should not have been requested');
+    });
+    await fillIn(GENERAL.inputByAttr('jwt'), 'mytoken');
+    assert.dom(GENERAL.inputByAttr('jwt')).hasValue('mytoken');
+  });
+};
 
 module('Integration | Component | auth | form | oidc-jwt', function (hooks) {
   setupRenderingTest(hooks);
   setupMirage(hooks);
 
   hooks.beforeEach(function () {
-    this.expectedFields = ['role'];
-
     this.authenticateStub = sinon.stub(this.owner.lookup('service:auth'), 'authenticate');
     this.cluster = { id: 1 };
     this.onError = sinon.spy();
     this.onSuccess = sinon.spy();
+
+    // additional test setup for oidc/jwt business
+    this.store = this.owner.lookup('service:store');
+    this.routerStub = sinon.stub(this.owner.lookup('service:router'), 'urlFor').returns('123-example.com');
+    // for oidc login workflow only
+    this.windowStub = sinon.stub(window, 'open');
+
     this.renderComponent = ({ yieldBlock = false } = {}) => {
       if (yieldBlock) {
         return render(hbs`
@@ -49,6 +181,10 @@ module('Integration | Component | auth | form | oidc-jwt', function (hooks) {
     };
   });
 
+  hooks.afterEach(function () {
+    this.windowStub.restore();
+  });
+
   test('it renders helper text', async function (assert) {
     await this.renderComponent();
     const id = find(GENERAL.inputByAttr('role')).id;
@@ -60,24 +196,75 @@ module('Integration | Component | auth | form | oidc-jwt', function (hooks) {
   module('oidc', function (hooks) {
     hooks.beforeEach(function () {
       this.authType = 'oidc';
-      this.expectedSubmit = {
-        default: { path: 'oidc', role: 'some-dev' },
-        custom: { path: 'custom-oidc', role: 'some-dev' },
-      };
+      this.expectedFields = ['role'];
     });
 
-    testHelper(test);
+    // base component functionality so outside login workflow modules
+    authUrlRequestTests(test);
+
+    module('login workflow: jwt token', function (hooks) {
+      hooks.beforeEach(function () {
+        // stubbing this request shows JWT token input and does not perform OIDC
+        this.server.post(`/auth/:path/oidc/auth_url`, () => {
+          return overrideResponse(400, { errors: [ERROR_JWT_LOGIN] });
+        });
+        this.expectedFields = ['role', 'jwt'];
+        this.expectedSubmit = {
+          default: { path: 'oidc', role: 'some-dev', jwt: 'some-jwt-token' },
+          custom: { path: 'custom-oidc', role: 'some-dev', jwt: 'some-jwt-token' },
+        };
+      });
+
+      testHelper(test, { standardSubmit: false });
+
+      jwtLoginTests(test);
+    });
+
+    // module('login workflow: oidc', function (hooks) {
+    //   hooks.beforeEach(function () {
+    //     this.expectedSubmit = {
+    //       default: { path: 'oidc', role: 'some-dev' },
+    //       custom: { path: 'custom-oidc', role: 'some-dev' },
+    //     };
+    //   });
+
+    //   oidcLoginTests(test);
+    // });
   });
 
   module('jwt', function (hooks) {
     hooks.beforeEach(function () {
       this.authType = 'jwt';
-      this.expectedSubmit = {
-        default: { path: 'jwt', role: 'some-dev' },
-        custom: { path: 'custom-jwt', role: 'some-dev' },
-      };
+      this.expectedFields = ['role'];
     });
 
-    testHelper(test);
+    // base component functionality so outside login workflow modules
+    authUrlRequestTests(test);
+
+    module('login workflow: jwt token', function (hooks) {
+      hooks.beforeEach(function () {
+        // stubbing this request shows JWT token input and does not perform OIDC
+        this.server.post(`/auth/:path/oidc/auth_url`, () => {
+          return overrideResponse(400, { errors: [ERROR_JWT_LOGIN] });
+        });
+        this.expectedFields = ['role', 'jwt'];
+        this.expectedSubmit = {
+          default: { path: 'jwt', role: 'some-dev', jwt: 'some-jwt-token' },
+          custom: { path: 'custom-jwt', role: 'some-dev', jwt: 'some-jwt-token' },
+        };
+      });
+
+      testHelper(test, { standardSubmit: false });
+
+      jwtLoginTests(test);
+    });
+
+    // module('login workflow: oidc', function (hooks) {
+    //   hooks.beforeEach(function () {
+    //     this.authType = 'jwt';
+    //   });
+
+    //   oidcLoginTests(test);
+    // });
   });
 });
