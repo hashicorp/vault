b261f5859c87c20b291fe18e2404e873b0f91a30
Shannon Roberts (Beagin) <beagins@users.noreply.github.com>
2025-04-28 11:52:19 -0700

    [VAULT-35469] UI: navigate to namespace on enter/return (#30372)
    * [VAULT-35469] UI: navigate to namespace on enter/return
    
    * address github copilot suggestions
    
    * address PR comments

---
+++ ui/lib/core/addon/utils/keyboard-keys.ts
@@ -0,0 +1,20 @@
+/**
+ * Copyright (c) HashiCorp, Inc.
+ * SPDX-License-Identifier: BUSL-1.1
+ */
+
+/**
+ * @module keyboard-keys
+ * @description Utility for mapping key names to their corresponding `event.key` values for use in keyboard event handlers.
+ */
+export enum KEYS {
+  ENTER = 'Enter',
+  ESC = 'Escape',
+  TAB = 'Tab',
+  LEFT = 'ArrowLeft',
+  UP = 'ArrowUp',
+  RIGHT = 'ArrowRight',
+  DOWN = 'ArrowDown',
+  T = 'F5',
+  BACKSPACE = 'Backspace',
+}
