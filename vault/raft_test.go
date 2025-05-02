ad49ed90a3a566e15ab55c29bc0a55e13daf2dad
Mia Epner <mia.epner@hashicorp.com>
2025-05-02 14:42:04 +0200

    remove obselete test

--- vault/raft_test.go
+++ vault/raft_test.go
@@ -4,11 +4,7 @@
 package vault
 
 import (
-	"fmt"
 	"testing"
-
-	"github.com/hashicorp/vault/command/server"
-	"github.com/stretchr/testify/require"
 )
 
 // TestFormatDiscoveredAddr validates that the string returned by formatDiscoveredAddr always respect the format `host:port`.
@@ -33,117 +29,5 @@ func TestFormatDiscoveredAddr(t *testing.T) {
 		if res != c.res {
 			t.Errorf("case %d result shoud be \"%s\" but is \"%s\"", i, c.res, res)
 		}
-	}
-}
-
-// TestRaftDirPath verifies that the Raft data directory path is correctly extracted from the storage configuration.
-// It exercises all execution paths in the RaftDataDirPath to ensure that:
-//   - The path is correctly extracted on a happy path.
-//   - In case of an empty path in config, it returns an empty string and true.
-//   - If there are any issues getting the path, it returns an empty string and false.
-func TestRaftDirPath(t *testing.T) {
-	testRaftPath := "/storage/path/raft"
-
-	testCases := map[string]struct {
-		config           *CoreConfig
-		expectedRaftPath string
-		shouldError      bool
-	}{
-		"happy-path": {
-			config: &CoreConfig{
-				RawConfig: &server.Config{
-					Storage: &server.Storage{
-						Type: "raft",
-						Config: map[string]string{
-							"path": testRaftPath,
-						},
-					},
-				},
-			},
-			expectedRaftPath: testRaftPath,
-		},
-		"empty-raft-data-dir-path": {
-			config: &CoreConfig{
-				RawConfig: &server.Config{
-					Storage: &server.Storage{
-						Type: "raft",
-						Config: map[string]string{
-							"path": "",
-						},
-					},
-				},
-			},
-			expectedRaftPath: "",
-			shouldError:      true,
-		},
-		"no-config": {
-			config: &CoreConfig{
-				RawConfig: nil,
-			},
-			expectedRaftPath: "",
-			shouldError:      true,
-		},
-		"no-storage": {
-			config: &CoreConfig{
-				RawConfig: &server.Config{
-					Storage: nil,
-				},
-			},
-			expectedRaftPath: "",
-			shouldError:      true,
-		},
-		"no-storage-config": {
-			config: &CoreConfig{
-				RawConfig: &server.Config{
-					Storage: &server.Storage{
-						Type:   "raft",
-						Config: nil,
-					},
-				},
-			},
-			expectedRaftPath: "",
-			shouldError:      true,
-		},
-		"no-storage-type": {
-			config: &CoreConfig{
-				RawConfig: &server.Config{
-					Storage: &server.Storage{
-						Type: "",
-						Config: map[string]string{
-							"path": testRaftPath,
-						},
-					},
-				},
-			},
-			expectedRaftPath: "",
-			shouldError:      true,
-		},
-		"no-raft-data-dir-path-in-config": {
-			config: &CoreConfig{
-				RawConfig: &server.Config{
-					Storage: &server.Storage{
-						Type:   "raft",
-						Config: map[string]string{},
-					},
-				},
-			},
-			expectedRaftPath: "",
-			shouldError:      true,
-		},
-	}
-
-	for name, tc := range testCases {
-		t.Run(name, func(t *testing.T) {
-			fmt.Println("name: ", name)
-			core, _, _ := TestCoreUnsealedWithConfig(t, tc.config)
-			raftPath, ok := core.RaftDataDirPath()
-			if tc.shouldError {
-				require.False(t, ok)
-				require.Empty(t, raftPath)
-			} else {
-				require.True(t, ok)
-				require.Equal(t, tc.expectedRaftPath, raftPath)
-			}
-		})
 	}
 }
