9c73853f3ee1cd6e982e7cf9a5e5799b6804cf14
drivera258 <dan.rivera@hashicorp.com>
2025-05-02 12:45:25 -0400

    adding to view

--- ui/lib/config-ui/addon/routes/login-settings/rule/details.js
+++ ui/lib/config-ui/addon/routes/login-settings/rule/details.js
@@ -4,9 +4,20 @@
  */
 
 import Route from '@ember/routing/route';
+import { service } from '@ember/service';
 
 export default class LoginSettingsRuleDetailsRoute extends Route {
+  @service api;
+  @service store;
+
   async model() {
-    return { rule: {} };
+    const adapter = this.store.adapterFor('application');
+
+    const rule = await adapter.ajax(
+      `/v1/sys/config/ui/login/default-auth/${encodeURI('Login rule 1')}`,
+      'GET'
+    );
+
+    return { rule };
   }
 }
