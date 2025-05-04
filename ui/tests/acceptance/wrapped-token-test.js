8c4f89430929ee16aa5c11b3a2612a54afaec2f1
claire bontempo <cbontempo@hashicorp.com>
2025-05-03 18:20:00 -0700

    add await

--- ui/tests/acceptance/wrapped-token-test.js
+++ ui/tests/acceptance/wrapped-token-test.js
@@ -63,7 +63,7 @@ module(`Acceptance | wrapped_token query param functionality`, function (hooks) 
       );
     assert.dom(AUTH_FORM.form).doesNotExist();
     await click(`${GENERAL.pageError.error} button`);
-    waitFor(AUTH_FORM.form);
+    await waitFor(AUTH_FORM.form);
     const url = currentURL();
     assert.false(url.includes('wrapped_token='), `url does not include wrapped_token param: ${url}`);
     assert.strictEqual(currentRouteName(), 'vault.cluster.auth', 'it navigates back to auth route');
