6d3c2e0a00781b0e5c990dacd5d58dd63a9ad847
Jordan Reimer <jordan.reimer@hashicorp.com>
2025-05-05 14:06:06 -0600

    adds api client error handling to error page component

---
+++ ui/tests/helpers/api/error-response.ts
@@ -0,0 +1,14 @@
+import { ResponseError } from '@hashicorp/vault-client-typescript';
+
+export const getErrorResponse = <T>(error?: T, status?: number) => {
+  const e = error || {
+    errors: ['first error', 'second error'],
+    message: 'there were some errors',
+  };
+  // url is readonly on Response so mock it and cast to Response type
+  return new ResponseError({
+    status: status || 404,
+    url: `${document.location.origin}/v1/test/error/parsing`,
+    json: () => Promise.resolve(e),
+  } as Response);
+};
