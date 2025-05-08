59f2127d04cebf6a7abb207862d321ee1d6c18fe
Jordan Reimer <zofskeez@gmail.com>
2025-05-08 13:45:50 -0600

    more updates to api-client for sync (#30553)

--- ui/api-client/dist/esm/models/SystemPatchSyncDestinationsVercelProjectNameResponse.d.ts
+++ ui/api-client/dist/esm/models/SystemPatchSyncDestinationsVercelProjectNameResponse.d.ts
@@ -28,6 +28,24 @@ export interface SystemPatchSyncDestinationsVercelProjectNameResponse {
      */
     name?: string;
     /**
+     * List of key value pairs of Vault configuration options.
+     * @type {object}
+     * @memberof SystemPatchSyncDestinationsVercelProjectNameResponse
+     */
+    options?: object;
+    /**
+     * Error message if the purge job failed.
+     * @type {string}
+     * @memberof SystemPatchSyncDestinationsVercelProjectNameResponse
+     */
+    purgeError?: string;
+    /**
+     * Timestamp of when a purge job was initiated when deleting a destination.
+     * @type {Date}
+     * @memberof SystemPatchSyncDestinationsVercelProjectNameResponse
+     */
+    purgeInitiatedAt?: Date;
+    /**
      * Type of this secrets store.
      * @type {string}
      * @memberof SystemPatchSyncDestinationsVercelProjectNameResponse
