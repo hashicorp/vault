0f2373fd73e2d163593c71f3f4865816dc622db4
drivera258 <dan.rivera@hashicorp.com>
2025-05-15 16:32:25 -0400

    fixes on types, remove test funcs

--- ui/lib/config-ui/addon/routes/login-settings/index.js
+++ ui/lib/config-ui/addon/routes/login-settings/index.js
@@ -10,34 +10,9 @@ export default class LoginSettingsRoute extends Route {
   @service api;
 
   async model() {
-    this.createSampleRules();
-
     const res = await this.api.sys.uiLoginDefaultAuthList(true);
     const loginRules = this.api.keyInfoToArray({ keyInfo: res.keyInfo, keys: res.keys });
 
     return { loginRules };
-  }
-
-  async createSampleRules() {
-    await this.api.sys.uiLoginDefaultAuthConfigure('hello', {
-      namespace: 'root',
-      backup_auth_types: ['okta'],
-      default_auth_type: 'ldap',
-      disable_inheritance: true,
-    });
-
-    await this.api.sys.uiLoginDefaultAuthConfigure('testingTesterz', {
-      namespace: 'root',
-      backup_auth_types: [],
-      default_auth_type: 'oidc',
-      disable_inheritance: true,
-    });
-
-    await this.api.sys.uiLoginDefaultAuthConfigure('Meowz', {
-      namespace: 'root',
-      backup_auth_types: ['userpass'],
-      default_auth_type: 'userpass',
-      DisableInheritance: true,
-    });
   }
 }
--- ui/tests/acceptance/config-ui/login-settings/login-settings-test.js
+++ ui/tests/acceptance/config-ui/login-settings/login-settings-test.js
@@ -6,7 +6,7 @@
 import { module, test } from 'qunit';
 import { setupApplicationTest } from 'ember-qunit';
 import { setupMirage } from 'ember-cli-mirage/test-support';
-import { visit } from '@ember/test-helpers';
+import { visit, currentURL } from '@ember/test-helpers';
 
 module('Acceptance | config-ui/login-settings', function (hooks) {
   setupApplicationTest(hooks);
