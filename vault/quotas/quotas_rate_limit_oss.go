ba956236e264c8b8b7b045738f9516891e2fe4e4
Bruno Souza <bruno.souza@hashicorp.com>
2025-05-14 21:51:32 -0300

    fix linter

--- vault/quotas/quotas_rate_limit_oss.go
+++ vault/quotas/quotas_rate_limit_oss.go
@@ -7,17 +7,14 @@ package quotas
 
 import "context"
 
-type entRateLimitRequest struct {
-}
+type entRateLimitRequest struct{}
 
-type entRateLimitQuota struct {
-}
+type entRateLimitQuota struct{}
 
 // getGroupKey returns the identifier to the request for rate limiting purposes. On CE we only support IP-based grouping.
 func (rlq *RateLimitQuota) getGroupKey(req *Request) (key string, isSecondaryGroup bool, err error) {
 	if rlq.GroupBy != "" && rlq.GroupBy != GroupByIp {
 		return "", false, ErrGroupByNotSupported
-
 	}
 	return req.ClientAddress, false, nil
 }
