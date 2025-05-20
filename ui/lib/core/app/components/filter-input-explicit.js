7fb5eb72b8b514aa96e308dfe5619d5e772eeaf8
Shannon Roberts (Beagin) <beagins@users.noreply.github.com>
2025-05-20 09:30:05 -0700

    [VAULT-34484] UI: Manage Namespaces: Enable Search (#30680)
    * [VAULT-34484] UI: Manage Namespaces: Enable Search
    
    * load query param in search field on page load
    
    * add jsdocs
    
    * add test coverage
    
    * fix naming collision issue
    
    * refresh namespace list on delete namespace + test coverage
    
    * revert unnecessary check
    
    * return model
    
    * add changelog

--- ui/lib/core/app/components/filter-input-explicit.js
+++ ui/lib/core/app/components/filter-input-explicit.js
@@ -3,4 +3,25 @@
  * SPDX-License-Identifier: BUSL-1.1
  */
 
+/**
+ * @module FilterInputExplicit
+ *
+ * @description FilterInputExplicit component is a child component to show filter input.
+ * It also handles the filtering actions of roles.
+ *
+ * @example
+ * <FilterInputExplicit
+ *   @query={{this.pageFilter}}
+ *   @placeholder="Search"
+ *   @handleSearch={{this.handleSearch}}
+ *   @handleInput={{this.handleInput}}
+ *   @handleKeyDown={{this.handleKeyDown}}
+ * />
+ *
+ * @param {string} query - value of queryParam, such as pageFilter
+ * @param {string} placeholder - placeholder for the input field
+ * @param {function} handleSearch - callback function to handle search
+ * @param {function} handleInput - callback function to handle input
+ * @param {function} handleKeyDown - callback function to handle keydown
+ */
 export { default } from 'core/components/filter-input-explicit';
