d4a808166b912954d0aeb1c94a31fc4d91fcbede
Jim Wright <jim.wright@hashicorp.com>
2025-05-22 13:04:57 -0700

    Update vault-reporting addon with analytics tracking changes

--- ui/vault-reporting/declarations/components/vault-reporting/views/dashboard.d.ts
+++ ui/vault-reporting/declarations/components/vault-reporting/views/dashboard.d.ts
@@ -6,6 +6,7 @@ import Component from '@glimmer/component';
 import './dashboard.scss';
 import type { UsageDashboardData, SimpleDatum, getUsageDataFunction } from '../../../types';
 import type { IconName } from '@hashicorp/flight-icons/svg';
+import type ReportingAnalyticsService from '../../../services/reporting-analytics';
 interface CounterBlock {
     title: string;
     tooltipMessage: string;
@@ -27,11 +28,15 @@ export interface SSUViewDashboardSignature {
     Element: HTMLElement;
 }
 export default class SSUViewDashboard extends Component<SSUViewDashboardSignature> {
+    readonly reportingAnalytics: ReportingAnalyticsService;
     data?: UsageDashboardData;
     lastUpdatedTime: string;
     error?: unknown;
     constructor(owner: unknown, args: SSUViewDashboardSignature['Args']);
     fetchAllData: () => void;
+    handleTrackAnalyticsEvent: (eventName: string, properties?: object, options?: object) => void;
+    handleTrackSurveyLink: () => void;
+    handleRefresh: () => void;
     getBarChartData: (map: Record<string, number>, exclude?: string[]) => SimpleDatum[];
     get isVaultDedicated(): boolean;
     get kvSecretsTooltipMessage(): string;
