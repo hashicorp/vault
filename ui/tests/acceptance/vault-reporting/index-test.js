1544ba102a6661991b8e9b83367ac33cae634be7
Eren Tantekin <eren.tantekin@hashicorp.com>
2025-05-23 07:28:09 -0700

    data test tags'

--- ui/tests/acceptance/vault-reporting/index-test.js
+++ ui/tests/acceptance/vault-reporting/index-test.js
@@ -27,73 +27,101 @@ module('Acceptance | vault-reporting', function (hooks) {
   test('it renders the counters dashboard block with all expected counters', async function (assert) {
     this.server.get('http://localhost:7357/v1/sys/utilization-report', () => mockedResponseWithData);
     await visit('/vault/usage-reporting');
-    await waitFor('[data-test-dashboard-counters]');
-    assert.dom('[data-test-dashboard-counters]').exists('renders the counters dashboard block');
+    await waitFor('[data-test-vault-reporting-dashboard-counters]');
+    assert
+      .dom('[data-test-vault-reporting-dashboard-counters]')
+      .exists('renders the counters dashboard block');
 
     const expectedCounters = ['Child namespaces', 'KV secrets', 'Secrets sync', 'PKI roles'];
 
     expectedCounters.forEach((counterLabel) => {
-      assert.dom(`[data-test-counter="${counterLabel}"]`).exists(`counter "${counterLabel}" is rendered`);
+      assert
+        .dom(`[data-test-vault-reporting-counter="${counterLabel}"]`)
+        .exists(`counter "${counterLabel}" is rendered`);
     });
 
-    assert.dom('[data-test-counter="Child namespaces"]').includesText('1');
-    assert.dom('[data-test-counter="KV secrets"]').includesText('100');
+    assert.dom('[data-test-vault-reporting-counter="Child namespaces"]').includesText('1');
+    assert.dom('[data-test-vault-reporting-counter="KV secrets"]').includesText('100');
   });
 
   test('dashboard card: Secret engines', async function (assert) {
     this.server.get('http://localhost:7357/v1/sys/utilization-report', () => mockedResponseWithData);
     await visit('/vault/usage-reporting');
-    await waitFor('[data-test-dashboard-secret-engines]');
+    await waitFor('[data-test-vault-reporting-dashboard-secret-engines]');
 
-    assert.dom('[data-test-dashboard-secret-engines]').exists('renders Secret engines card');
+    assert.dom('[data-test-vault-reporting-dashboard-secret-engines]').exists('renders Secret engines card');
     assert
-      .dom('[data-test-dashboard-secret-engines] [data-test-dashboard-card-title]')
+      .dom(
+        '[data-test-vault-reporting-dashboard-secret-engines] [data-test-vault-reporting-dashboard-card-title]'
+      )
       .exists('title is present')
       .hasText('Secret engines', 'title is correct');
     assert
-      .dom('[data-test-dashboard-secret-engines] [data-test-dashboard-card-title-link]')
+      .dom(
+        '[data-test-vault-reporting-dashboard-secret-engines] [data-test-vault-reporting-dashboard-card-title-link]'
+      )
       .exists('title link is present')
       .hasAttribute('href', 'secrets', 'link points to secrets');
     assert
-      .dom('[data-test-dashboard-secret-engines] [data-test-dashboard-card-description]')
+      .dom(
+        '[data-test-vault-reporting-dashboard-secret-engines] [data-test-vault-reporting-dashboard-card-description]'
+      )
       .exists('description is present')
       .hasText('Enabled secret engines for this cluster.', 'description is correct');
-    assert.dom('[data-test-dashboard-secret-engines]').includesText('aws nomad cubbyhole 47 46 45');
+    assert
+      .dom('[data-test-vault-reporting-dashboard-secret-engines]')
+      .includesText('aws nomad cubbyhole 47 46 45');
   });
 
   test('dashboard card: Authentication methods', async function (assert) {
     this.server.get('http://localhost:7357/v1/sys/utilization-report', () => mockedResponseWithData);
     await visit('/vault/usage-reporting');
-    await waitFor('[data-test-dashboard-auth-methods]');
+    await waitFor('[data-test-vault-reporting-dashboard-auth-methods]');
 
-    assert.dom('[data-test-dashboard-auth-methods]').exists('renders Authentication methods card');
     assert
-      .dom('[data-test-dashboard-auth-methods] [data-test-dashboard-card-title]')
+      .dom('[data-test-vault-reporting-dashboard-auth-methods]')
+      .exists('renders Authentication methods card');
+    assert
+      .dom(
+        '[data-test-vault-reporting-dashboard-auth-methods] [data-test-vault-reporting-dashboard-card-title]'
+      )
       .exists('title is present')
       .hasText('Authentication methods', 'title is correct');
     assert
-      .dom('[data-test-dashboard-auth-methods] [data-test-dashboard-card-title-link]')
+      .dom(
+        '[data-test-vault-reporting-dashboard-auth-methods] [data-test-vault-reporting-dashboard-card-title-link]'
+      )
       .exists('title link is present')
       .hasAttribute('href', 'access', 'link points to access');
     assert
-      .dom('[data-test-dashboard-auth-methods] [data-test-dashboard-card-description]')
+      .dom(
+        '[data-test-vault-reporting-dashboard-auth-methods] [data-test-vault-reporting-dashboard-card-description]'
+      )
       .exists('description is present')
       .hasText('Enabled authentication methods for this cluster.', 'description is correct');
-    assert.dom('[data-test-dashboard-auth-methods]').includesText('kubernetes userpass aws 44 43 42');
+    assert
+      .dom('[data-test-vault-reporting-dashboard-auth-methods]')
+      .includesText('kubernetes userpass aws 44 43 42');
   });
 
   test('dashboard card: Global lease count quota', async function (assert) {
     this.server.get('http://localhost:7357/v1/sys/utilization-report', () => mockedResponseWithData);
     await visit('/vault/usage-reporting');
-    await waitFor('[data-test-dashboard-lease-count]');
+    await waitFor('[data-test-vault-reporting-dashboard-lease-count]');
 
-    assert.dom('[data-test-dashboard-lease-count]').exists('renders Global lease count quota card');
     assert
-      .dom('[data-test-dashboard-lease-count] [data-test-dashboard-card-title]')
+      .dom('[data-test-vault-reporting-dashboard-lease-count]')
+      .exists('renders Global lease count quota card');
+    assert
+      .dom(
+        '[data-test-vault-reporting-dashboard-lease-count] [data-test-vault-reporting-dashboard-card-title]'
+      )
       .exists('title is present')
       .hasText('Global lease count quota', 'title is correct');
     assert
-      .dom('[data-test-dashboard-lease-count] [data-test-dashboard-card-title-link]')
+      .dom(
+        '[data-test-vault-reporting-dashboard-lease-count] [data-test-vault-reporting-dashboard-card-title-link]'
+      )
       .exists('title link is present')
       .hasAttribute(
         'href',
@@ -101,29 +129,43 @@ module('Acceptance | vault-reporting', function (hooks) {
         'link points to lease count docs'
       );
     assert
-      .dom('[data-test-dashboard-lease-count] [data-test-dashboard-card-description]')
+      .dom(
+        '[data-test-vault-reporting-dashboard-lease-count] [data-test-vault-reporting-dashboard-card-description]'
+      )
       .exists('description is present')
       .hasText('Total number of active leases for this quota.', 'description is correct');
-    assert.dom('[data-test-global-lease-percentage-text]').hasText('50%', 'percentage is correct');
-    assert.dom('[data-test-global-lease-count-text]').hasText('210K / 420K', 'lease count is correct');
+    assert
+      .dom('[data-test-vault-reporting-global-lease-percentage-text]')
+      .hasText('50%', 'percentage is correct');
+    assert
+      .dom('[data-test-vault-reporting-global-lease-count-text]')
+      .hasText('210K / 420K', 'lease count is correct');
   });
 
   test('dashboard card: Cluster replication status', async function (assert) {
     this.server.get('http://localhost:7357/v1/sys/utilization-report', () => mockedResponseWithData);
     await visit('/vault/usage-reporting');
-    await waitFor('[data-test-dashboard-cluster-replication]');
+    await waitFor('[data-test-vault-reporting-dashboard-cluster-replication]');
 
-    assert.dom('[data-test-dashboard-cluster-replication]').exists('renders Cluster replication status card');
     assert
-      .dom('[data-test-dashboard-cluster-replication] [data-test-dashboard-card-title]')
+      .dom('[data-test-vault-reporting-dashboard-cluster-replication]')
+      .exists('renders Cluster replication status card');
+    assert
+      .dom(
+        '[data-test-vault-reporting-dashboard-cluster-replication] [data-test-vault-reporting-dashboard-card-title]'
+      )
       .exists('title is present')
       .hasText('Cluster replication', 'title is correct');
     assert
-      .dom('[data-test-dashboard-cluster-replication] [data-test-dashboard-card-title-link]')
+      .dom(
+        '[data-test-vault-reporting-dashboard-cluster-replication] [data-test-vault-reporting-dashboard-card-title-link]'
+      )
       .exists('title link is present')
       .hasAttribute('href', 'replication', 'link points to replication');
     assert
-      .dom('[data-test-dashboard-cluster-replication] [data-test-dashboard-card-description]')
+      .dom(
+        '[data-test-vault-reporting-dashboard-cluster-replication] [data-test-vault-reporting-dashboard-card-description]'
+      )
       .exists('description is present')
       .hasText('Status of disaster recovery and performance replication.', 'description is correct');
   });
@@ -132,43 +174,43 @@ module('Acceptance | vault-reporting', function (hooks) {
     this.server.get('http://localhost:7357/v1/sys/utilization-report', () => mockedEmptyResponse);
     await visit('/vault/usage-reporting');
 
-    await waitFor('[data-test-dashboard-secret-engines]');
+    await waitFor('[data-test-vault-reporting-dashboard-secret-engines]');
     assert
-      .dom('[data-test-dashboard-secret-engines]')
+      .dom('[data-test-vault-reporting-dashboard-secret-engines]')
       .includesText('None enabled', 'Secret engines empty state: title is shown');
     assert
-      .dom('[data-test-dashboard-secret-engines]')
+      .dom('[data-test-vault-reporting-dashboard-secret-engines]')
       .includesText(
         'Secret engines in this namespace will appear here.',
         'Secret engines empty state: body is shown'
       );
     assert
-      .dom('[data-test-dashboard-secret-engines]')
+      .dom('[data-test-vault-reporting-dashboard-secret-engines]')
       .includesText('Enable secret engines', 'Secret engines empty state: CTA is shown');
 
-    await waitFor('[data-test-dashboard-auth-methods]');
+    await waitFor('[data-test-vault-reporting-dashboard-auth-methods]');
     assert
-      .dom('[data-test-dashboard-auth-methods]')
+      .dom('[data-test-vault-reporting-dashboard-auth-methods]')
       .includesText('None enabled', 'Auth methods empty state: title is shown');
     assert
-      .dom('[data-test-dashboard-auth-methods]')
+      .dom('[data-test-vault-reporting-dashboard-auth-methods]')
       .includesText(
         'Authentication methods in this namespace will appear here.',
         'Auth methods empty state: body is shown'
       );
     assert
-      .dom('[data-test-dashboard-auth-methods]')
+      .dom('[data-test-vault-reporting-dashboard-auth-methods]')
       .includesText('Enable authentication methods', 'Auth methods empty state: CTA is shown');
 
-    await waitFor('[data-test-dashboard-lease-count]');
+    await waitFor('[data-test-vault-reporting-dashboard-lease-count]');
     assert
-      .dom('[data-test-dashboard-lease-count]')
+      .dom('[data-test-vault-reporting-dashboard-lease-count]')
       .includesText(
         `Lease quotas enforce limits on active secrets and tokens. It's recommended to enable this to protect stability for this Vault cluster.`,
         'Lease quota empty state description is shown'
       );
     assert
-      .dom('[data-test-dashboard-lease-count]')
+      .dom('[data-test-vault-reporting-dashboard-lease-count]')
       .includesText('Global lease count quota', 'Lease quota empty state: docs link is shown');
   });
 });
