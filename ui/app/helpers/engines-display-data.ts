d79a553ba7c2ddc5d7c55361e9a7e8395534e89e
drivera258 <dan.rivera@hashicorp.com>
2025-06-02 15:52:28 -0400

    rename files, generalize helper function, rename to userpass

---
+++ ui/app/helpers/engines-display-data.ts
@@ -0,0 +1,11 @@
+/**
+ * Copyright (c) HashiCorp, Inc.
+ * SPDX-License-Identifier: BUSL-1.1
+ */
+
+import { ALL_ENGINES } from 'vault/utils/all-engines-metadata';
+
+export default function engineDisplayData(methodType: string) {
+  const engine = ALL_ENGINES?.find((t) => t.type === methodType);
+  return engine || methodType;
+}
