11cc544b4f3baeebcd54ffbb31c39b2553a368b2
claire bontempo <68122737+hellobontempo@users.noreply.github.com>
2025-06-02 09:05:04 -0700

    UI: Fix direct login link for auth mounts (#30800)
    * fix typos, check for supported auth method and render direct link in display view too
    
    * add namespace
    
    * linebreak
    
    * add tests

--- ui/app/models/auth-method.js
+++ ui/app/models/auth-method.js
@@ -14,6 +14,7 @@ import { camelize } from '@ember/string';
 import { WHITESPACE_WARNING } from 'vault/utils/forms/validators';
 import { supportedTypes } from 'vault/utils/supported-login-methods';
 import engineDisplayData from 'vault/helpers/engines-display-data';
+import { supportedTypes } from 'vault/utils/supported-login-methods';
 
 const validations = {
   path: [
