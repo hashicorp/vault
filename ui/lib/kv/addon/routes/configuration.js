044e3d2064964026da8fe1ab982b6dd3469a4493
Jordan Reimer <jordan.reimer@hashicorp.com>
2025-06-03 12:02:44 -0600

    fixes kv config route

--- ui/lib/kv/addon/routes/configuration.js
+++ ui/lib/kv/addon/routes/configuration.js
@@ -13,7 +13,7 @@ export default class KvConfigurationRoute extends Route {
   model() {
     const backend = this.modelFor('application');
     return hash({
-      mountConfig: this.store.findRecord('secret-engine', backend.id),
+      mountConfig: this.store.query('secret-engine', { path: backend.id }).then((models = []) => models[0]),
       engineConfig: this.store.findRecord('kv/config', backend.id).catch(() => {
         // return an empty record so we have access to model capabilities
         return this.store.createRecord('kv/config', { backend: backend.id });
