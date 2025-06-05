61a0334b125f35e2293ee16409378b137de621d7
claire bontempo <cbontempo@hashicorp.com>
2025-06-05 15:04:36 -0400

    fix details param

--- ui/lib/config-ui/addon/components/login-settings/page/details.js
+++ ui/lib/config-ui/addon/components/login-settings/page/details.js
@@ -34,7 +34,7 @@ export default class LoginSettingsRuleDetails extends Component {
     defaultAuthType: 'Default method',
     backupAuthTypes: 'Backup methods',
     disableInheritance: 'Inheritance enabled',
-    namespace: 'Namespace the rule applies to',
+    namespacePath: 'Namespace the rule applies to',
   };
 
   displayValue = (key) => {
