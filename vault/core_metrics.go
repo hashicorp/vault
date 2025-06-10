fc2a384ac5cb61b0b9dfd943377bc9bcae90209f
Amir Aslamov <amir.aslamov@hashicorp.com>
2025-06-10 15:23:43 -0400

    apply ent patch (#30931)

--- vault/core_metrics.go
+++ vault/core_metrics.go
@@ -591,7 +591,6 @@ func (c *Core) GetAuthenticatedCustomBanners() int {
 	filter := uicustommessages.FindFilter{
 		IncludeAncestors: false,
 	}
-	filter.Active(true)
 	filter.Authenticated(true)
 	for _, ns := range allNamespaces {
 		messages, err := c.customMessageManager.FindMessages(namespace.ContextWithNamespace(ctx, ns), filter)
@@ -612,7 +611,6 @@ func (c *Core) GetUnauthenticatedCustomBanners() int {
 	filter := uicustommessages.FindFilter{
 		IncludeAncestors: false,
 	}
-	filter.Active(true)
 	filter.Authenticated(false)
 	for _, ns := range allNamespaces {
 		messages, err := c.customMessageManager.FindMessages(namespace.ContextWithNamespace(ctx, ns), filter)
