146c032600a439b012020b75cb712cf2486e187a
helenfufu <25168806+helenfufu@users.noreply.github.com>
2025-06-10 07:31:24 -0700

    CE changes for plugin download (#30927)
    * ce changes for https://github.com/hashicorp/vault-enterprise/pull/8193
    
    * lower case enterprise only errors
    
    ---------
    
    Co-authored-by: Ben Ash <bash@hashicorp.com>

--- vault/plugincatalog/plugin_catalog_stubs_oss.go
+++ vault/plugincatalog/plugin_catalog_stubs_oss.go
@@ -8,9 +8,14 @@ package plugincatalog
 import (
 	"context"
 
+	"github.com/hashicorp/vault/sdk/helper/consts"
 	"github.com/hashicorp/vault/sdk/helper/pluginutil"
 )
 
-func (c *PluginCatalog) entPrepareDownloadedPlugin(ctx context.Context, plugin pluginutil.SetPluginInput) (string, string, error) {
-	return "", "", nil
+func (c *PluginCatalog) entPrepareDownloadedPlugin(ctx context.Context, plugin pluginutil.SetPluginInput) (string, string, consts.PluginTier, error) {
+	return "", "", consts.PluginTierUnknown, nil
+}
+
+func (c *PluginCatalog) entDownloadExtractVerifyPlugin(ctx context.Context, pr *pluginutil.PluginRunner) error {
+	return nil
 }
