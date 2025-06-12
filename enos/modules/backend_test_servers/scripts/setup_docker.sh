739ab3b80dd449e94a7a0ed5c5965555789f3722
tin.vo <tin.vo@hashicorp.com>
2025-06-12 11:49:24 -0700

    debugging docker error

--- enos/modules/backend_test_servers/scripts/setup_docker.sh
+++ enos/modules/backend_test_servers/scripts/setup_docker.sh
@@ -4,11 +4,36 @@
 
 set -e
 
-fail() {
-  echo "$1" 1>&2
-  exit 1
+# Function to detect the OS
+detect_os() {
+    if [ -f /etc/os-release ]; then
+        . /etc/os-release
+        echo "OS: $ID"
+    else
+        echo "unknown"
+    fi
 }
 
-#sudo dnf install -y docker
-#sudo systemctl enable --now docker
-#docker version
+# Main logic
+echo "Installing Docker..."
+os_id=$(detect_os)
+case "$os_id" in
+    amzn)
+        sudo dnf install -y docker
+        ;;
+    ubuntu)
+        sudo apt install docker
+        ;;
+    rhel|centos)
+        sudo yum install docker
+        ;;
+    *)
+        echo "Unsupported or unknown OS: $os_id"
+        exit 1
+        ;;
+esac
+echo "Successfully installed Docker."
+
+sudo systemctl enable --now docker
+docker version
+
