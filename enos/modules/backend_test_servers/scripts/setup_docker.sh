f175e8e12e9a18c3e102e89d62892441e0d18d5f
tin.vo <tin.vo@hashicorp.com>
2025-06-13 18:01:40 -0700

    fixing enos test error

--- enos/modules/backend_test_servers/scripts/setup_docker.sh
+++ enos/modules/backend_test_servers/scripts/setup_docker.sh
@@ -36,10 +36,11 @@ case "$os_id" in
   ubuntu)
     sudo apt update -y
     sudo apt install apt-transport-https ca-certificates curl software-properties-common -y
-    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --batch --yes --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
-    echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-    sudo apt update
-    sudo apt install docker-ce docker-ce-cli containerd.io -y
+#    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --batch --yes --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+#    echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+#    sudo apt update
+#    sudo apt install docker-ce docker-ce-cli containerd.io -y
+    curl -fsSL https://get.docker.com | sudo sh
     ;;
   rhel | centos)
     sudo yum update -y
