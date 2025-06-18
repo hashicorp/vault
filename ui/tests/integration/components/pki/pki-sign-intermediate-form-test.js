c33bdddb09e022f1ec0d6553fb3ca1d9c51fc921
Dylan Hyun <dylan.hyun@hashicorp.com>
2025-06-18 14:12:30 -0400

    [UI] Update `textarea` tests to use `GENERAL` selectors (#37433)

--- ui/tests/integration/components/pki/pki-sign-intermediate-form-test.js
+++ ui/tests/integration/components/pki/pki-sign-intermediate-form-test.js
@@ -16,7 +16,6 @@ const selectors = {
   form: '[data-test-sign-intermediate-form]',
   saveButton: '[data-test-pki-sign-intermediate-save]',
   cancelButton: '[data-test-pki-sign-intermediate-cancel]',
-  fieldError: '[data-test-validation-error="csr"]',
   formError: '[data-test-form-error]',
   resultsContainer: '[data-test-sign-intermediate-result]',
 };
@@ -58,7 +57,7 @@ module('Integration | Component | pki-sign-intermediate-form', function (hooks) 
   });
 
   test('it shows the returned values on successful save', async function (assert) {
-    assert.expect(13);
+    assert.expect(12);
     await render(hbs`<PkiSignIntermediateForm @onCancel={{this.onCancel}} @model={{this.model}} />`, {
       owner: this.engine,
     });
@@ -78,8 +77,7 @@ module('Integration | Component | pki-sign-intermediate-form', function (hooks) 
     });
     await click(selectors.saveButton);
     assert.dom(selectors.formError).hasText('There is an error with this form.', 'Shows validation errors');
-    assert.dom(selectors.csrInput).hasClass('hds-form-textarea--is-invalid');
-    assert.dom(selectors.fieldError).hasText('CSR is required.');
+    assert.dom(GENERAL.validationErrorByAttr('csr')).hasText('CSR is required.');
 
     await fillIn(GENERAL.inputByAttr('csr'), 'example-data');
     await click(selectors.saveButton);
