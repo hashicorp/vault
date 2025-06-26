f10cab8677fac6652db34d4b684ac1b3244b33d0
tin.vo <tin.vo@hashicorp.com>
2025-06-26 11:18:19 -0700

    addressing ipv6 comments

--- enos/modules/backend_test_servers/main.tf
+++ enos/modules/backend_test_servers/main.tf
@@ -12,12 +12,14 @@ terraform {
 locals {
   distro = var.distro
   ldap_server = {
-    domain     = "enos.com"
-    org        = "hashicorp"
-    admin_pw   = "password1"
-    version    = var.ldap_version
-    port       = "389"
-    ip_address = var.ip_version == "6" ? var.hosts[0].ipv6 : var.hosts[0].public_ip
+    domain          = "enos.com"
+    org             = "hashicorp"
+    admin_pw        = "password1"
+    version         = var.ldap_version
+    port            = "389"
+    ip_version      = var.ip_version
+    ip_address      = var.hosts[0].public_ip
+    ldap_ip_address = var.ip_version == "6" ? var.hosts[0].ipv6 : var.hosts[0].public_ip
   }
 }
 
@@ -33,7 +35,9 @@ resource "enos_remote_exec" "setup_docker" {
   scripts = [abspath("${path.module}/scripts/set-up-docker.sh")]
 
   environment = {
-    DISTRO = local.distro
+    DISTRO          = local.distro
+    IP_VERSION      = local.ldap_server.ip_version
+    LDAP_IP_ADDRESS = local.ldap_server.ldap_ip_address
   }
 
   transport = {
@@ -52,6 +56,7 @@ resource "enos_remote_exec" "setup_openldap" {
     LDAP_DOMAIN            = local.ldap_server.domain
     LDAP_ORG               = local.ldap_server.org
     LDAP_ADMIN_PW          = local.ldap_server.admin_pw
+    LDAP_IP_ADDRESS        = local.ldap_server.ldap_ip_address
     LDAP_PORT              = local.ldap_server.port
   }
 
