57d6d6a3c46d53a8280a35c1c3ca24d1cbdd3450
claire bontempo <cbontempo@hashicorp.com>
2025-06-27 14:49:06 -0700

    m rewrite test stubbing capabilities intead

--- ui/tests/acceptance/secrets/backend/kv/kv-v2-workflow-edge-cases-test.js
+++ ui/tests/acceptance/secrets/backend/kv/kv-v2-workflow-edge-cases-test.js
@@ -37,12 +37,15 @@ import { GENERAL } from 'vault/tests/helpers/general-selectors';
 import { SECRET_ENGINE_SELECTORS as SES } from 'vault/tests/helpers/secret-engine/secret-engine-selectors';
 import codemirror from 'vault/tests/helpers/codemirror';
 import { personas } from 'vault/tests/helpers/kv/policy-generator';
+import { capabilitiesStub } from 'vault/tests/helpers/stubs';
+import { setupMirage } from 'ember-cli-mirage/test-support';
 
 /**
  * This test set is for testing edge cases, such as specific bug fixes or reported user workflows
  */
 module('Acceptance | kv-v2 workflow | edge cases', function (hooks) {
   setupApplicationTest(hooks);
+  setupMirage(hooks);
 
   hooks.beforeEach(async function () {
     const uid = uuidv4();
@@ -54,8 +57,6 @@ module('Acceptance | kv-v2 workflow | edge cases', function (hooks) {
     await writeSecret(this.backend, this.fullSecretPath, 'foo', 'bar');
     await writeSecret(this.backend, 'edge/one', 'foo', 'bar');
     await writeSecret(this.backend, 'edge/two', 'foo', 'bar');
-    // create a secret with an underscore in the path name because this
-    await writeSecret(this.backend, 'my_secret', 'foo', 'bar');
     return;
   });
 
@@ -198,9 +199,17 @@ module('Acceptance | kv-v2 workflow | edge cases', function (hooks) {
       assert.dom(PAGE.secretTab('Configuration')).doesNotHaveClass('is-active');
     });
 
-    // the user in the setup only has list permissions and cannot access the subkeys endpoint
-    test('it navigates to secret without access to the subkeys endpoint', async function (assert) {
+    // it's rare for a policy check to fail, but if it does we default to "true" and let the API handle gating.
+    // there was an issue with the new capabilities service incorrectly mapping permissions for secrets with underscores which surfaced this bug.
+    // The user logged in here does NOT have access to the subkeys endpoint, but we're stubbing capabilities to return true
+    // to simulate the capabilities map failing and returning a false positive.
+    test('it navigates to secret if policy check fails for the subkeys endpoint', async function (assert) {
       assert.expect(2);
+      this.server.post(
+        '/sys/capabilities-self',
+        capabilitiesStub(`${this.backend}/subkeys/my_secret`, ['read'])
+      );
+
       await visit(`/vault/secrets/${this.backend}/kv/list`);
       await typeIn(PAGE.list.overviewInput, 'my_secret');
       await click(GENERAL.submitButton);
