45ee3549df943e96c0654e4935c36bffbac35ae4
Jordan Reimer <jordan.reimer@hashicorp.com>
2025-07-03 16:45:38 -0600

    adds capabilities checks to auth methods route

--- ui/app/routes/vault/cluster/access/methods.ts
+++ ui/app/routes/vault/cluster/access/methods.ts
@@ -8,9 +8,11 @@ import { service } from '@ember/service';
 import AuthMethodResource from 'vault/resources/auth/method';
 
 import type ApiService from 'vault/services/api';
+import type Capabilities from 'vault/services/capabilities';
 
 export default class VaultClusterAccessMethodsRoute extends Route {
   @service declare readonly api: ApiService;
+  @service declare readonly capabilities: Capabilities;
 
   queryParams = {
     page: {
@@ -23,6 +25,24 @@ export default class VaultClusterAccessMethodsRoute extends Route {
 
   async model() {
     const { auth } = await this.api.sys.internalUiListEnabledVisibleMounts();
-    return this.api.responseObjectToArray(auth, 'path').map((method) => new AuthMethodResource(method, this));
+
+    const methods = this.api
+      .responseObjectToArray(auth, 'path')
+      .map((method) => new AuthMethodResource(method, this));
+
+    const paths = methods.reduce((paths: string[], { path, methodType }) => {
+      paths.push(
+        this.capabilities.pathFor('authMethodConfig', { path }),
+        this.capabilities.pathFor('authMethodDelete', { path })
+      );
+      if (methodType === 'aws') {
+        paths.push(this.capabilities.pathFor('authMethodConfigAws', { path }));
+      }
+      return paths;
+    }, []);
+
+    const capabilities = this.capabilities.fetch(paths);
+
+    return { methods, capabilities };
   }
 }
