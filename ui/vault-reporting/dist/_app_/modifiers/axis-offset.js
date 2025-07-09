2dfc4b7b42bda603a12f3983a558007ff456f4f5
Eren Tantekin <eren.tantekin@hashicorp.com>
2025-07-09 10:05:29 -0700

    VAULT-37770

--- ui/vault-reporting/dist/_app_/modifiers/axis-offset.js
+++ ui/vault-reporting/dist/_app_/modifiers/axis-offset.js
@@ -1,5 +1,1 @@
-/**
- * Copyright (c) HashiCorp, Inc.
- */
-
 export { default } from "@hashicorp/vault-reporting/modifiers/axis-offset";
--- ui/vault-reporting/dist/_app_/services/reporting-analytics.js
+++ ui/vault-reporting/dist/_app_/services/reporting-analytics.js
@@ -1,5 +1,1 @@
-/**
- * Copyright (c) HashiCorp, Inc.
- */
-
 export { default } from "@hashicorp/vault-reporting/services/reporting-analytics";
