7a5f51f205e1077b23f9fd95a569f3d57fb5bb8a
tin.vo <tin.vo@hashicorp.com>
2025-07-10 17:22:35 -0700

    testing pipeline

--- enos/modules/install_packages/scripts/install-packages.sh
+++ enos/modules/install_packages/scripts/install-packages.sh
@@ -40,7 +40,7 @@ install_packages() {
             curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
             echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
             sudo apt update -y
-            if ! output=$(sudo apt install docker-ce docker-ce-cli containerd.io 2>&1); then
+            if ! output=$(sudo apt install -y docker-ce docker-ce-cli containerd.io 2>&1); then
               echo "Failed to install ${package}: ${output}" >&2
               return 1
             fi
