8f522a2bcaad606ed77fc608aea9e1aff582de8f
kpcraig <3031348+kpcraig@users.noreply.github.com>
2025-07-15 12:48:00 -0400

    add ce side code and stubs for rotation manager
    * add ce side code and stubs
    
    * add changelog
    
    * style refactor
    
    * try to use APIPath as mount point instead of request field
    
    * fix linter
    
    * return a response struct instead of a pure timestamp
    
    * add issue time to response
    
    * add ttl to GetRotationInformation response
    
    * rename field for clarity
    
    * update ttl to just seconds
    
    * rename next and last rotation time field; describe what they are
    
    * rename function
    
    * catch up to ent PR
    
    * fix patch merge mistake

--- sdk/plugin/mock/backend.go
+++ sdk/plugin/mock/backend.go
@@ -5,6 +5,7 @@ package mock
 
 import (
 	"context"
+	"errors"
 	"fmt"
 	"os"
 	"testing"
@@ -137,6 +138,16 @@ func expectInternalValue(t *testing.T, client *api.Client, mountPath, expected s
 
 func (b *backend) rotateRootCredential(ctx context.Context, req *logical.Request) error {
 	b.Logger().Debug("mock rotateRootCredential")
+
+	cfg, err := b.configEntry(ctx, req.Storage)
+	if err != nil {
+		return err
+	}
+
+	if cfg.FailRotate {
+		return errors.New("mock plugin was asked to fail to rotate")
+	}
+
 	b.internal = "rotated"
 	return nil
 }
