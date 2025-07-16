cae9da3eca13e5152dd0b727fb7eb306a7c9f3d2
Lane Wetmore <lane.wetmore@hashicorp.com>
2025-07-16 11:09:00 -0500

    fix lint

--- ui/tests/integration/components/json-editor-test.js
+++ ui/tests/integration/components/json-editor-test.js
@@ -5,7 +5,6 @@
 
 import { module, test } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
-import { create } from 'ember-cli-page-object';
 import { render, click, triggerKeyEvent, waitFor } from '@ember/test-helpers';
 import hbs from 'htmlbars-inline-precompile';
 import { SELECTORS } from '../../pages/components/json-editor';
