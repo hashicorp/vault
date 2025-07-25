a4b2b1df1b444d373aa1a0549dc0c11242a00946
Lane Wetmore <lane.wetmore@hashicorp.com>
2025-07-25 09:55:49 -0500

    unblock another url

--- ui/config/content-security-policy.js
+++ ui/config/content-security-policy.js
@@ -13,7 +13,7 @@ module.exports = function (environment) {
       '*.vercel-ui-preview-hashicorp.vercel.app',
       'hashicorp.vercel.app',
       '*.hashicorp.vercel.app',
-      'unsafe-inline',
+      'https://vercel.live',
     ],
     'font-src': ["'self'"],
     'connect-src': ["'self'"],
