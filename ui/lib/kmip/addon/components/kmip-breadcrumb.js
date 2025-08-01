c43d7335d646a893e42215d4ab3acbf2a9248c3f
Matthew Irish <matthew@hashicorp.com>
2025-08-01 16:09:53 -0500

    kmip: run colocated components codemod

--- ui/lib/kmip/addon/components/kmip-breadcrumb.js
+++ ui/lib/kmip/addon/components/kmip-breadcrumb.js
@@ -5,16 +5,14 @@
 
 import Component from '@ember/component';
 import { service } from '@ember/service';
-import layout from '../templates/components/kmip-breadcrumb';
 import { or } from '@ember/object/computed';
 
 export default Component.extend({
-  layout,
-  tagName: '',
-  secretMountPath: service(),
-  shouldShowPath: or('showPath', 'scope', 'role'),
-  showPath: false,
-  path: null,
-  scope: null,
-  role: null,
+ tagName: '',
+ secretMountPath: service(),
+ shouldShowPath: or('showPath', 'scope', 'role'),
+ showPath: false,
+ path: null,
+ scope: null,
+ role: null
 });
--- ui/lib/kmip/addon/components/operation-field-display.js
+++ ui/lib/kmip/addon/components/operation-field-display.js
@@ -16,10 +16,8 @@
  *
  */
 import Component from '@ember/component';
-import layout from '../templates/components/operation-field-display';
 
 export default Component.extend({
-  layout,
   tagName: '',
   model: null,
 
@@ -43,5 +41,5 @@ export default Component.extend({
     operationEnabled(model, field) {
       return this.trueOrFalseString(model, field, 'Enabled', 'Disabled');
     },
-  },
+  }
 });
