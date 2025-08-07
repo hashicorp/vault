2618cf7368f74005687e59398d5fefc16c62f855
claire bontempo <cbontempo@hashicorp.com>
2025-08-07 16:41:41 -0700

    delete unused selectors

--- ui/tests/helpers/kv/kv-selectors.js
+++ ui/tests/helpers/kv/kv-selectors.js
@@ -62,8 +62,6 @@ export const PAGE = {
     listMenuDelete: `[data-test-popup-metadata-delete]`,
     overviewCard: '[data-test-overview-card-container="View secret"]',
     overviewInput: '[data-test-view-secret] input',
-    paginationNext: '.hds-pagination-nav__arrow--direction-next',
-    paginationSelected: '.hds-pagination-nav__number--is-selected',
   },
   versions: {
     icon: (version) => `[data-test-icon-holder="${version}"]`,
