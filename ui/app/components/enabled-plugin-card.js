7c082dca66aeb98445008209de88c695b1de3a56
Shannon Roberts <shannon.roberts@hashicorp.com>
2025-08-13 10:28:26 -0700

    enterprise badge should show if requiresEnterprise flag is true

---
+++ ui/app/components/enabled-plugin-card.js
@@ -0,0 +1,48 @@
+/**
+ * Copyright (c) HashiCorp, Inc.
+ * SPDX-License-Identifier: BUSL-1.1
+ */
+
+import Component from '@glimmer/component';
+import { service } from '@ember/service';
+
+/**
+ * @module EnabledPluginCard
+ * EnabledPluginCard components are used to display available secret and auth engines in the mount backend type form
+ *
+ * @example
+ * ```js
+ * <EnabledPluginCard @type={{type}} @setMountType={{@setMountType}} />
+ * ```
+ * @param {Object} type - The engine metadata object with properties like displayName, requiresEnterprise, requiredFeature, etc.
+ * @param {Function} setMountType - Function to call when the card is clicked to select this mount type
+ */
+
+export default class EnabledPluginCard extends Component {
+  @service version;
+
+  get isDisabled() {
+    const { type } = this.args;
+    return (
+      (type.requiresEnterprise && !this.version.isEnterprise) ||
+      (type.requiredFeature && !this.hasFeature(type.requiredFeature))
+    );
+  }
+
+  get showEnterpriseBadge() {
+    const { type } = this.args;
+    return (
+      (type.requiresEnterprise && !this.version.isEnterprise) ||
+      (type.requiredFeature && !this.hasFeature(type.requiredFeature))
+    );
+  }
+
+  get showDeprecationBadge() {
+    const { type } = this.args;
+    return type.deprecation_status && type.deprecation_status !== 'supported';
+  }
+
+  hasFeature(featureName) {
+    return this.version.features?.includes(featureName) || false;
+  }
+}
