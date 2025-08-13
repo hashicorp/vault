c48d5609971dc3ea5ed950133b5a1cd912e4eb13
drivera258 <dan.rivera@hashicorp.com>
2025-08-13 12:15:50 -0400

    cleanup

--- ui/app/components/secret-engine/security-card.ts
+++ ui/app/components/secret-engine/security-card.ts
@@ -1,19 +1,25 @@
-import { service } from '@ember/service';
+/**
+ * Copyright (c) HashiCorp, Inc.
+ * SPDX-License-Identifier: BUSL-1.1
+ */
+
+import { action } from '@ember/object';
 import Component from '@glimmer/component';
-import type Router from '@ember/routing/router';
-import type FlashMessageService from 'vault/services/flash-messages';
-import type ApiService from 'vault/services/api';
 import SecretsEngineResource from 'vault/resources/secrets/engine';
 interface Args {
   model: SecretsEngineResource;
 }
 
 export default class SecurityCard extends Component<Args> {
-  @service declare readonly router: Router;
-  @service declare readonly api: ApiService;
-  @service declare readonly flashMessages: FlashMessageService;
-
   constructor(owner: unknown, args: Args) {
     super(owner, args);
+  }
+
+  @action toggleSealWrap() {
+    this.args.model.seal_wrap = !this.args.model.seal_wrap;
+  }
+
+  @action toggleLocal() {
+    this.args.model.local = !this.args.model.local;
   }
 }
