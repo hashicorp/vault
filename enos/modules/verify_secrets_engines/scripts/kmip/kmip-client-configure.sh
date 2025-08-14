bfa0c41c6ddf60282279a87872f889eec275e442
Luis (LT) Carbonell <lt.carbonell@hashicorp.com>
2025-08-14 10:06:05 -0400

    (enos) generalize container startup script (#31483)

--- enos/modules/verify_secrets_engines/scripts/kmip/kmip-client-configure.sh
+++ enos/modules/verify_secrets_engines/scripts/kmip/kmip-client-configure.sh
@@ -15,18 +15,22 @@ fail() {
 [[ -z "${KMIP_PORT}" ]] && fail "KMIP_PORT env variable has not been set"
 
 cd ~ || fail "Failed to change directory to home"
-echo "${SERVER_CA}" > TEMP_DIR/vault-ca.pem
-echo "${CLIENT_CA}" > TEMP_DIR/client.pem
+TEMP_DIR=/tmp/kmip_temp
+mkdir -p "${TEMP_DIR}" || fail "Failed to create temporary directory"
+
+echo "${SERVER_CA}" > "${TEMP_DIR}"/vault-ca.pem
+echo "${CLIENT_CA}" > "${TEMP_DIR}"/client.pem
 
 # Extract certificate and key from client bundle
-cd TEMP_DIR
+cd "${TEMP_DIR}" || fail "Failed to change directory to ${TEMP_DIR}"
+
 # Assuming CLIENT_CA contains both cert and key, split them
 csplit -f client- client.pem '/-----BEGIN.*PRIVATE KEY-----/' '{*}'
 mv client-00 cert.pem
 mv client-01 key.pem
 
-# Connect to the Percona Docker container
-CONTAINER_CMD="sudo docker"
+# Connect to the Percona KMIP Docker container and configure it
+CONTAINER_CMD="sudo podman"
 KMIP_DOCKER_NAME="kmip"
 
 # Create MySQL data directory
@@ -38,6 +42,6 @@ ${CONTAINER_CMD} exec -d "${KMIP_DOCKER_NAME}" mysqld \
     --early-plugin-load=keyring_kmip.so \
     --keyring_kmip_server_name="${VAULT_ADDR}" \
     --keyring_kmip_server_port="${KMIP_PORT}" \
-    --keyring_kmip_client_ca=/TEMP_DIR/vault-ca.pem \
-    --keyring_kmip_client_key=/TEMP_DIR/key.pem \
-    --keyring_kmip_client_cert=/TEMP_DIR/cert.pem
+    --keyring_kmip_client_ca="${TEMP_DIR}/vault-ca.pem" \
+    --keyring_kmip_client_key="${TEMP_DIR}/key.pem" \
+    --keyring_kmip_client_cert="${TEMP_DIR}/cert.pem"
