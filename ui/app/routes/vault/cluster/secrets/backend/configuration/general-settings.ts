15710960ff10d4bff14dd21637777c4c5672784a
Kianna Quach <kianna.quach@hashicorp.com>
2025-08-15 12:54:09 -0700

    Create new component

--- ui/app/routes/vault/cluster/secrets/backend/configuration/general-settings.ts
+++ ui/app/routes/vault/cluster/secrets/backend/configuration/general-settings.ts
@@ -6,29 +6,16 @@
 import Route from '@ember/routing/route';
 import engineDisplayData from 'vault/helpers/engines-display-data';
 
-import type SecretEngineModel from 'vault/models/secret-engine';
-import type { Breadcrumb } from 'vault/vault/app-types';
-import type Controller from '@ember/controller';
-
-interface RouteModel {
-  secretsEngine: SecretEngineModel;
-}
-interface RouteController extends Controller {
-  breadcrumbs: Array<Breadcrumb>;
-  model: RouteModel;
-  typeDisplay: string | undefined;
-  isConfigurable: boolean;
-  modelId: string;
-  glyph: string | undefined;
-}
+import type SecretsEngineResource from 'vault/resources/secrets/engine';
 
 export default class SecretsBackendConfigurationGeneralSettingsRoute extends Route {
-  setupController(controller: RouteController, resolvedModel: RouteModel) {
-    super.setupController(controller, resolvedModel);
-    const engine = engineDisplayData(resolvedModel.secretsEngine.type);
-    controller.typeDisplay = engine?.displayName;
-    controller.isConfigurable = engine?.isConfigurable ?? false;
-    controller.glyph = engine?.glyph;
-    controller.modelId = resolvedModel.secretsEngine.id;
+  async model() {
+    const secretsEngine = this.modelFor('vault.cluster.secrets.backend') as SecretsEngineResource;
+    const engineData = engineDisplayData(secretsEngine.type);
+
+    return {
+      secretsEngine,
+      engineData,
+    };
   }
 }
