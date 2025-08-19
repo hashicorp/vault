19b4a397fa0caa5771eae21b84d11cfbfaf56a3f
tin.vo <tin.vo@hashicorp.com>
2025-08-19 11:17:00 -0700

    fix lint

--- enos/modules/verify_secrets_engines/scripts/ldap-configs.sh
+++ enos/modules/verify_secrets_engines/scripts/ldap-configs.sh
@@ -29,15 +29,14 @@ MAX_RETRIES=15
 RETRY_DELAY=2
 RETRIES=0
 until ldapsearch -x -H "ldap://${LDAP_SERVER}:${LDAP_PORT}" -b "dc=${LDAP_USERNAME},dc=com" >/dev/null 2>&1; do
-    sleep $RETRY_DELAY
-    RETRIES=$((RETRIES+1))
-    echo "  Retry $RETRIES..."
-
-    if [ $RETRIES -ge $MAX_RETRIES ]; then
-        echo "LDAP server did not start in time."
-        docker logs openldap
-        exit 1
-    fi
+  sleep $RETRY_DELAY
+  RETRIES=$((RETRIES+1))
+  echo "  Retry $RETRIES..."
+  if [ $RETRIES -ge $MAX_RETRIES ]; then
+    echo "LDAP server did not start in time."
+    docker logs openldap
+    exit 1
+  fi
 done
 
 echo "OpenLDAP: Checking for OpenLDAP Server Connection: ${LDAP_SERVER}:${LDAP_PORT}"
