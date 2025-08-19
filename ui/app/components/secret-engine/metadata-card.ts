9a7303433542d042e39a28f9437c31c2bf97336c
Vault Automation <github-team-secure-vault-core@hashicorp.com>
2025-08-19 15:21:41 -0600

    UI: Creating Metadata card for configuration page (#8679) (#8709)
    * card setup
    
    * updating to pass in vals
    
    * remove test usage
    
    Co-authored-by: Dan Rivera <dan.rivera@hashicorp.com>

---
+++ ui/app/components/secret-engine/metadata-card.ts
@@ -0,0 +1,24 @@
+/**
+ * Copyright (c) HashiCorp, Inc.
+ * SPDX-License-Identifier: BUSL-1.1
+ */
+
+import Component from '@glimmer/component';
+import SecretsEngineResource from 'vault/resources/secrets/engine';
+interface Args {
+  model: SecretsEngineResource;
+}
+
+export default class MetadataCard extends Component<Args> {
+  constructor(owner: unknown, args: Args) {
+    super(owner, args);
+  }
+
+  updatePath() {
+    // This method can be used to update the path of the secrets engine.
+  }
+
+  updateAccessor() {
+    // This method can be used to update the accessor of the secrets engine.
+  }
+}
