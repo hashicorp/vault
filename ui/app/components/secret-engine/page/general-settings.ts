a5c1e7f4286401c536e69ab8ee3c55517e557676
Vault Automation <github-team-secure-vault-core@hashicorp.com>
2025-10-13 18:07:44 -0400

    [UI][Bugfix]: Unsaved changes modal + Confirm Leave (#9984) (#10091)
    * Update unsaved changes service + modal
    
    * Remove more unused consts..
    
    * Remove controller
    
    * Fix bugs!
    
    * Add copyright headers
    
    * Add sevice comment
    
    * suggested changes (#9998)
    
    * Address more feedback
    
    * Add unsaved changes modals
    
    * Update modal header and add generalized text
    
    * Add tests
    
    * Fix ts errors
    
    * Address feedback
    
    * Redirect to general-settings route
    
    * Address feedback!
    
    * Remove extra data-test-breadcrumb!
    
    * Remove ember testing..
    
    ---------
    
    Co-authored-by: Kianna <30884335+kiannaquach@users.noreply.github.com>
    Co-authored-by: claire bontempo <68122737+hellobontempo@users.noreply.github.com>

--- ui/app/components/secret-engine/page/general-settings.ts
+++ ui/app/components/secret-engine/page/general-settings.ts
@@ -7,16 +7,15 @@ import Component from '@glimmer/component';
 import { task } from 'ember-concurrency';
 import { service } from '@ember/service';
 import { tracked } from '@glimmer/tracking';
-import { action } from '@ember/object';
 import { convertToSeconds } from 'core/utils/duration-utils';
+import { action } from '@ember/object';
 
 import type Router from '@ember/routing/router';
 import type FlashMessageService from 'vault/services/flash-messages';
 import type ApiService from 'vault/services/api';
 import type SecretsEngineResource from 'vault/resources/secrets/engine';
+import type UnsavedChangesService from 'vault/services/unsaved-changes';
 
-export const CUSTOM = 'Custom';
-export const SYSTEM_DEFAULT = 'System default';
 const CHARACTER_LIMIT = 500;
 
 /**
@@ -44,48 +43,27 @@ export default class GeneralSettingsComponent extends Component<Args> {
   @service declare readonly router: Router;
   @service declare readonly api: ApiService;
   @service declare readonly flashMessages: FlashMessageService;
+  @service declare readonly unsavedChanges: UnsavedChangesService;
 
   @tracked errorMessage: string | string[] | null = null;
   @tracked errors: string[] = [];
   @tracked invalidFormAlert: string | null = null;
   @tracked showUnsavedChangesModal = false;
-  @tracked changedFields: string[] = [];
 
   @tracked defaultLeaseUnit = '';
   @tracked maxLeaseUnit = '';
 
   originalModel = JSON.parse(JSON.stringify(this.args.model));
 
-  getUnsavedChanges(newModel: SecretsEngineResource, originalModel: SecretsEngineResource) {
-    for (const key in this.args.model.secretsEngine) {
-      const secretsEngineKeyType = key as keyof typeof this.args.model.secretsEngine;
+  get modalChangedFields() {
+    const changedFieldsCopy = [...this.unsavedChanges.changedFields];
+    const configIndex = this.unsavedChanges.changedFields.indexOf('config');
 
-      if (secretsEngineKeyType === 'options') {
-        return;
-      }
+    if (configIndex === -1) return this.unsavedChanges.changedFields;
 
-      if (secretsEngineKeyType === 'config') {
-        const { defaultLeaseTime, defaultLeaseUnit, maxLeaseTime, maxLeaseUnit } = this.getFormData();
+    changedFieldsCopy[configIndex] = 'Secrets duration';
 
-        const hasDefaultTtlValueChanged = this.hasTtlValueChanged(
-          defaultLeaseTime,
-          defaultLeaseUnit,
-          'default_lease_ttl'
-        );
-        const hasMaxTtlValueChanged = this.hasTtlValueChanged(maxLeaseTime, maxLeaseUnit, 'max_lease_ttl');
-
-        if (
-          (hasDefaultTtlValueChanged || hasMaxTtlValueChanged) &&
-          !this.changedFields.includes('Lease Duration')
-        ) {
-          this.changedFields.push('Lease Duration');
-        }
-      } else {
-        if (newModel[secretsEngineKeyType] !== originalModel[secretsEngineKeyType]) {
-          this.changedFields.push(key);
-        }
-      }
-    }
+    return changedFieldsCopy;
   }
 
   validateTtl(ttlValue: FormDataEntryValue | number | null) {
@@ -101,7 +79,7 @@ export default class GeneralSettingsComponent extends Component<Args> {
   }
 
   validateForm() {
-    const { defaultLeaseTime, maxLeaseTime, description } = this.getFormData();
+    const { defaultLeaseTime, maxLeaseTime, description } = this.formData;
 
     const errorMessages = [];
 
@@ -129,15 +107,7 @@ export default class GeneralSettingsComponent extends Component<Args> {
     return true;
   }
 
-  hasDescriptionChanged(description: FormDataEntryValue | null) {
-    return description !== this?.originalModel?.secretsEngine?.description;
-  }
-
-  hasPluginVersionChanged(version: FormDataEntryValue | null) {
-    return version && version !== this?.originalModel?.secretsEngine?.running_plugin_version;
-  }
-
-  getFormData() {
+  get formData() {
     const form = document.getElementById('general-settings-form');
     const fd = new FormData(form as HTMLFormElement);
     const fdDefaultLeaseTime = Number(fd.get('default_lease_ttl-time'));
@@ -155,28 +125,9 @@ export default class GeneralSettingsComponent extends Component<Args> {
     };
   }
 
-  hasUnsavedChanges() {
-    const { defaultLeaseTime, defaultLeaseUnit, maxLeaseTime, maxLeaseUnit, description, version } =
-      this.getFormData();
-
-    const hasDefaultTtlValueChanged = this.hasTtlValueChanged(
-      defaultLeaseTime,
-      defaultLeaseUnit,
-      'default_lease_ttl'
-    );
-    const hasMaxTtlValueChanged = this.hasTtlValueChanged(maxLeaseTime, maxLeaseUnit, 'max_lease_ttl');
-
-    return (
-      hasDefaultTtlValueChanged ||
-      hasMaxTtlValueChanged ||
-      this.hasDescriptionChanged(description) ||
-      this.hasPluginVersionChanged(version)
-    );
-  }
-
   formatTuneParams() {
     const { defaultLeaseTime, defaultLeaseUnit, maxLeaseTime, maxLeaseUnit, description, version } =
-      this.getFormData();
+      this.formData;
 
     const hasDefaultTtlValueChanged = this.hasTtlValueChanged(
       defaultLeaseTime,
@@ -185,11 +136,14 @@ export default class GeneralSettingsComponent extends Component<Args> {
     );
 
     const hasMaxTtlValueChanged = this.hasTtlValueChanged(maxLeaseTime, maxLeaseUnit, 'max_lease_ttl');
+    const hasDescriptionChanged = description !== this?.originalModel?.secretsEngine?.description;
+    const hasPluginVersionChanged =
+      version && version !== this?.originalModel?.secretsEngine?.running_plugin_version;
 
     const defaultLeaseTtl = hasDefaultTtlValueChanged ? `${defaultLeaseTime}${defaultLeaseUnit}` : undefined;
     const maxLeaseTtl = hasMaxTtlValueChanged ? `${maxLeaseTime}${maxLeaseUnit}` : undefined;
-    const pluginVersion = this.hasPluginVersionChanged(version) ? version : undefined;
-    const pluginDescription = this.hasDescriptionChanged(description) ? description : undefined;
+    const pluginVersion = hasPluginVersionChanged ? version : undefined;
+    const pluginDescription = hasDescriptionChanged ? description : undefined;
 
     return {
       defaultLeaseTtl,
@@ -197,35 +151,6 @@ export default class GeneralSettingsComponent extends Component<Args> {
       pluginVersion,
       pluginDescription,
     };
-  }
-
-  @action
-  openUnsavedChangesModal() {
-    if (this.hasUnsavedChanges()) {
-      this.getUnsavedChanges(this.args?.model?.secretsEngine, this?.originalModel?.secretsEngine);
-      this.showUnsavedChangesModal = true;
-    } else {
-      this.showUnsavedChangesModal = false;
-    }
-  }
-
-  @action
-  closeUnsavedChangesModal() {
-    this.showUnsavedChangesModal = false;
-    this.changedFields = [];
-  }
-
-  @action
-  closeAndHandle(close: () => void, action: 'save' | 'discard') {
-    close();
-
-    if (action === 'save') {
-      this.saveGeneralSettings.perform();
-    }
-
-    if (action === 'discard') {
-      this.router.transitionTo(this.args?.model?.secretsEngine?.backendConfigurationLink);
-    }
   }
 
   saveGeneralSettings = task(async (event?) => {
@@ -247,10 +172,17 @@ export default class GeneralSettingsComponent extends Component<Args> {
 
       this.flashMessages.success('Engine settings successfully updated.', { title: 'Configuration saved' });
 
-      this.router.transitionTo(this.args?.model?.secretsEngine?.backendConfigurationLink);
+      this.unsavedChanges.showModal = false;
+      this.router.transitionTo(this.router.currentRouteName);
     } catch (e) {
       const { message } = await this.api.parseError(e);
       this.errorMessage = message;
     }
   });
+
+  @action
+  discardChanges() {
+    const currentRouteName = this.router.currentRouteName;
+    this.router.transitionTo(currentRouteName);
+  }
 }
--- ui/app/routes/vault/cluster/secrets/backend/configuration/general-settings.ts
+++ ui/app/routes/vault/cluster/secrets/backend/configuration/general-settings.ts
@@ -5,15 +5,29 @@
 
 import Route from '@ember/routing/route';
 import { service } from '@ember/service';
+import { action } from '@ember/object';
 import { getPluginVersionsFromEngineType } from 'vault/utils/plugin-catalog-helpers';
 
 import type SecretsEngineResource from 'vault/resources/secrets/engine';
 import type ApiService from 'vault/services/api';
 import type PluginCatalogService from 'vault/services/plugin-catalog';
+import type UnsavedChangesService from 'vault/services/unsaved-changes';
+import type Controller from '@ember/controller';
+import type Transition from '@ember/routing/transition';
+import type RouterService from '@ember/routing/router-service';
+
+interface RouteController extends Controller {
+  model: Record<string, unknown> | undefined;
+  changedFields: string[];
+}
 
 export default class SecretsBackendConfigurationGeneralSettingsRoute extends Route {
   @service declare readonly api: ApiService;
-  @service('plugin-catalog') declare readonly pluginCatalog: PluginCatalogService;
+  @service declare readonly router: RouterService;
+  @service declare readonly pluginCatalog: PluginCatalogService;
+  @service declare readonly unsavedChanges: UnsavedChangesService;
+
+  oldModel: Record<string, unknown> | undefined;
 
   async model() {
     const secretsEngine = this.modelFor('vault.cluster.secrets.backend') as SecretsEngineResource;
@@ -21,6 +35,34 @@ export default class SecretsBackendConfigurationGeneralSettingsRoute extends Rou
 
     const versions = getPluginVersionsFromEngineType(data?.secret, secretsEngine.type);
 
-    return { secretsEngine, versions };
+    const model = { secretsEngine, versions };
+
+    this.oldModel = JSON.parse(JSON.stringify(model));
+
+    return model;
+  }
+
+  @action
+  willTransition(transition: Transition) {
+    // eslint-disable-next-line ember/no-controller-access-in-routes
+    const controller = this.controllerFor(this.routeName) as RouteController;
+
+    const { model } = controller;
+
+    // Only intercept transition if leaving THIS route and there are changes
+    const targetRoute = transition?.to?.name ?? '';
+    // Saving transitions to the index route, so we do not one to intercept the transition then
+    if (this.routeName !== targetRoute && this.oldModel && model) {
+      const oldModel = this.oldModel['secretsEngine'] as Record<string, unknown> | undefined;
+      const currentModel = model['secretsEngine'] as Record<string, unknown> | undefined;
+      this.unsavedChanges.setupProperties(oldModel, currentModel);
+      this.unsavedChanges.getDiff();
+
+      if (this.unsavedChanges.hasChanges) {
+        transition.abort();
+        this.unsavedChanges.showModal = true;
+      }
+    }
+    return true;
   }
 }
