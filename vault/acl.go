49aa179779a94b980665e8b02884d881273a1c02
Vault Automation <github-team-secure-vault-core@hashicorp.com>
2025-11-07 16:10:40 -0500

    VAULT-40350 backport denied_parameters (#10496) (#10693)
    * VAULT-35716 make allowed and denied_parameters compare lists (#9478)
    
    * make allowed and denied_parameters compare lists
    
    * change name of env var
    
    * add changelog
    
    * linter fixes and unnecessary code removal
    
    * VAULT-35716 support comma-delimited slices on denied_ and allowed_parameters (#9955)
    
    * support comma-delimited slices
    
    * fix unit tests
    
    * lint
    
    * backport change to 1.20 and below as opt-in
    
    * add proper changelog
    
    Co-authored-by: Bruno Oliveira de Souza <bruno.souza@hashicorp.com>

--- vault/acl.go
+++ vault/acl.go
@@ -6,6 +6,7 @@ package vault
 import (
 	"context"
 	"fmt"
+	"os"
 	"reflect"
 	"slices"
 	"sort"
@@ -13,6 +14,7 @@ import (
 
 	"github.com/armon/go-radix"
 	"github.com/hashicorp/go-multierror"
+	"github.com/hashicorp/go-secure-stdlib/parseutil"
 	"github.com/hashicorp/go-secure-stdlib/strutil"
 	"github.com/hashicorp/vault/helper/identity"
 	"github.com/hashicorp/vault/helper/namespace"
@@ -524,26 +526,25 @@ CHECK:
 			return
 		}
 
-		if len(permissions.DeniedParameters) == 0 {
-			goto ALLOWED_PARAMETERS
-		}
+		useNewMatching := os.Getenv("VAULT_NEW_PER_ELEMENT_MATCHING_ON_LIST") != ""
 
-		// Check if all parameters have been denied
-		if _, ok := permissions.DeniedParameters["*"]; ok {
-			return
-		}
+		if len(permissions.DeniedParameters) > 0 {
+			// Check if all parameters have been denied
+			if _, ok := permissions.DeniedParameters["*"]; ok {
+				return
+			}
 
-		for parameter, value := range req.Data {
-			// Check if parameter has been explicitly denied
-			if valueSlice, ok := permissions.DeniedParameters[strings.ToLower(parameter)]; ok {
-				// If the value exists in denied values slice, deny
-				if valueInParameterList(value, valueSlice) {
-					return
+			for parameter, value := range req.Data {
+				// Check if parameter has been explicitly denied
+				if valueSlice, ok := permissions.DeniedParameters[strings.ToLower(parameter)]; ok {
+					// If the value exists in denied values slice, deny
+					if valueInDeniedParameterList(value, valueSlice, !useNewMatching) {
+						return
+					}
 				}
 			}
 		}
 
-	ALLOWED_PARAMETERS:
 		// If we don't have any allowed parameters set, allow
 		if len(permissions.AllowedParameters) == 0 {
 			ret.Allowed = true
@@ -563,9 +564,9 @@ CHECK:
 				return
 			}
 
-			// If the value doesn't exists in the allowed values slice,
+			// If the value doesn't exist in the allowed values slice,
 			// deny
-			if ok && !valueInParameterList(value, valueSlice) {
+			if ok && !valueInAllowedParameterList(value, valueSlice, !useNewMatching) {
 				return
 			}
 		}
@@ -810,16 +811,89 @@ func (c *Core) performPolicyChecks(ctx context.Context, acl *ACL, te *logical.To
 	return ret
 }
 
-func valueInParameterList(v interface{}, list []interface{}) bool {
+func valueInAllowedParameterList(v interface{}, list []interface{}, useLegacyMatching bool) bool {
 	// Empty list is equivalent to the item always existing in the list
 	if len(list) == 0 {
 		return true
 	}
 
-	return valueInSlice(v, list)
+	if valueInParameterList(v, list) {
+		return true
+	}
+
+	if useLegacyMatching {
+		// prevent execution of the new behaviour if we're in legacy mode
+		return false
+	}
+
+	if vSlice, ok := v.([]interface{}); ok {
+		// when not running in legacy mode, we run a relaxed check for slices that verifies if all
+		// elements in the slice exist in the allowed list, as opposed to checking if the allowed
+		// list contains a single element that matches the entire slice (but this whole-slice match
+		// is still supported)
+		for _, v := range vSlice {
+			if !valueInParameterList(v, list) {
+				return false
+			}
+		}
+
+		return true
+	} else if vString, ok := v.(string); ok {
+		// At this point we don't know if the field is of framework.TypeCommaStringSlice, but we assume it is
+		// because failing to match a value because of it being in a comma-separated string is way more likely
+		// and worse than accidentally matching a substring of a string value.
+		if vSlice, err := parseutil.ParseCommaStringSlice(vString); err == nil {
+			for _, v := range vSlice {
+				if !valueInParameterList(v, list) {
+					return false
+				}
+			}
+			return true
+		}
+	}
+
+	return false
 }
 
-func valueInSlice(v interface{}, list []interface{}) bool {
+func valueInDeniedParameterList(v interface{}, list []interface{}, useLegacyMatching bool) bool {
+	// Empty list is equivalent to the item always existing in the list
+	if len(list) == 0 {
+		return true
+	}
+
+	if valueInParameterList(v, list) {
+		return true
+	}
+
+	if useLegacyMatching {
+		// prevent execution of the new behaviour if we're in legacy mode
+		return false
+	}
+
+	// The new behaviour is that if any value in the slice is in the denied list, we deny.
+	if vSlice, ok := v.([]interface{}); ok {
+		for _, v := range vSlice {
+			if valueInParameterList(v, list) {
+				return true
+			}
+		}
+	} else if vString, ok := v.(string); ok {
+		// At this point we don't know if the field is of framework.TypeCommaStringSlice, but we assume it is
+		// because failing to match a value because of it being in a comma-separated string is way more likely
+		// and worse than accidentally matching a substring of a string value.
+		if vSlice, err := parseutil.ParseCommaStringSlice(vString); err == nil {
+			for _, v := range vSlice {
+				if valueInParameterList(v, list) {
+					return true
+				}
+			}
+		}
+	}
+
+	return false
+}
+
+func valueInParameterList(v interface{}, list []interface{}) bool {
 	for _, el := range list {
 		if el == nil || v == nil {
 			// It doesn't seem possible to set up a nil entry in the list, but it is possible
@@ -828,11 +902,8 @@ func valueInSlice(v interface{}, list []interface{}) bool {
 			if el == v {
 				return true
 			}
-		} else if reflect.TypeOf(el).String() == "string" && reflect.TypeOf(v).String() == "string" {
-			item := el.(string)
-			val := v.(string)
-
-			if strutil.GlobbedStringsMatch(item, val) {
+		} else if elStr, ok := el.(string); ok {
+			if vStr, ok := v.(string); ok && strutil.GlobbedStringsMatch(elStr, vStr) {
 				return true
 			}
 		} else if reflect.DeepEqual(el, v) {
