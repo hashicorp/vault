69942515414c648f3042737e8601770dd9ad5aad
Vault Automation <github-team-secure-vault-core@hashicorp.com>
2025-12-03 11:03:57 -0500

    rotation manager: handle local paths (#11044) (#11052) (#11058)
    * rotation manager: handle local paths
    
    * rename views
    
    * rename helper methods
    
    * refactor views into CombinedView
    
    * rename to _ent and add unit tests
    
    * changelog
    
    * update comments
    
    * check local from router to prevent unnecessary storage access
    
    * fix test
    
    * fix tests
    
    * fix linter and tests: return explicit errors
    
    * move path definition to non-ent file
    
    * update changelog file name
    
    Co-authored-by: John-Michael Faircloth <fairclothjm@users.noreply.github.com>

---
+++ vault/rotation.go
@@ -0,0 +1,10 @@
+// Copyright IBM Corp. 2016, 2025
+// SPDX-License-Identifier: BUSL-1.1
+
+package vault
+
+const (
+	// rotationLocalSubPath is the sub-path used for the rotation manager's
+	// Local (non-replicated) view. This is nested under the system view.
+	rotationLocalSubPath = "rotation-local/"
+)
