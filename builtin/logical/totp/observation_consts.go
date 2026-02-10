521997a16f671b007f4a58133f3f161d3e4b8f9e
Vault Automation <github-team-secure-vault-core@hashicorp.com>
2026-02-10 11:50:28 -0500

    add observations for totp secrets engine (#11839) (#12272)
    Co-authored-by: Chris-Paris-HashiCorp <chris.paris@hashicorp.com>

---
+++ builtin/logical/totp/observation_consts.go
@@ -0,0 +1,12 @@
+// Copyright IBM Corp. 2016, 2025
+// SPDX-License-Identifier: BUSL-1.1
+
+package totp
+
+const (
+	ObservationTypeTOTPKeyRead      = "totp/key/read"
+	ObservationTypeTOTPKeyCreate    = "totp/key/create"
+	ObservationTypeTOTPKeyDelete    = "totp/key/delete"
+	ObservationTypeTOTPCodeGenerate = "totp/code/generate"
+	ObservationTypeTOTPCodeValidate = "totp/code/validate"
+)
