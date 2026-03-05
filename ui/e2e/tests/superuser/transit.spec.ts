46e6a418a867a616d96f1674879c402ae0a68fe0
Vault Automation <github-team-secure-vault-core@hashicorp.com>
2026-03-05 13:05:47 -0500

    UI: adding playwright test for Transit secrets engine (#12730) (#12757)
    * adding test for transit
    
    * specify path
    
    Co-authored-by: Dan Rivera <dan.rivera@hashicorp.com>

---
+++ ui/e2e/tests/superuser/transit.spec.ts
@@ -0,0 +1,89 @@
+/**
+ * Copyright IBM Corp. 2016, 2025
+ * SPDX-License-Identifier: BUSL-1.1
+ */
+
+import { test, expect } from '@playwright/test';
+import { BasePage } from '../../pages/base';
+
+test('transit workflow', async ({ page }) => {
+  const basePage = new BasePage(page);
+
+  await page.goto('dashboard');
+  // enable Transit Engine
+  await page.getByRole('link', { name: 'Secrets', exact: true }).click();
+  await page.getByRole('link', { name: 'Enable new engine' }).click();
+  await page.getByRole('heading', { name: 'Transit' }).click();
+  await page.getByRole('textbox', { name: 'Path' }).fill('transit-workflow');
+  await page.getByRole('button', { name: 'Enable engine' }).click();
+
+  // create key
+  await page.getByRole('link', { name: 'Create key' }).click();
+  await page.getByRole('textbox', { name: 'Name' }).fill('testKey');
+  await page.getByRole('button', { name: 'Create key' }).click();
+  await expect(page.getByLabel('breadcrumbs').getByText('testKey')).toBeVisible();
+  await basePage.dismissFlashMessages();
+
+  // rotate key
+  await page.getByRole('link', { name: 'Versions' }).click();
+  await page.getByRole('button', { name: 'Rotate key' }).click();
+  await page.getByRole('button', { name: 'Confirm' }).click();
+  //verify a new version is created after rotation
+  await expect(page.getByText('Version 2')).toBeVisible();
+
+  // encrypt text
+  await page.getByRole('link', { name: 'Key Actions' }).click();
+  await page.getByRole('link', { name: 'Encrypt Looks up wrapping' }).click();
+  await page.getByRole('textbox', { name: 'Plaintext' }).fill('testString');
+  await page.getByRole('button', { name: 'Encrypt' }).click();
+
+  // grab the generated ciphertext and copy it to clipboard
+  await page.getByRole('button', { name: 'copy vault:v2:' }).click();
+  await page.getByRole('button', { name: 'Close' }).click();
+
+  // decrypt text
+  await page.getByRole('link', { name: 'Decrypt' }).click();
+  await page.getByRole('textbox', { name: 'Ciphertext' }).press('ControlOrMeta+v');
+  await page.getByRole('button', { name: 'Decrypt' }).click();
+  await expect(page.getByText('Copy your unwrapped data')).toBeVisible();
+  await page.getByRole('button', { name: 'copy dGVzdFN0cmluZw==' }).click();
+  await page.getByRole('button', { name: 'Close' }).click();
+
+  // generate datakey
+  await page.getByRole('link', { name: 'Datakey' }).click();
+  await page.getByRole('button', { name: 'Create datakey' }).click();
+  await page.locator('html').click();
+  await expect(page.getByText('Copy your generated key')).toBeVisible();
+  await page.getByRole('button', { name: 'copy vault:v2:' }).click();
+  await page.getByRole('button', { name: 'Close' }).click();
+
+  // rewrap key
+  await page.getByRole('link', { name: 'Rewrap' }).click();
+  await page.getByRole('textbox', { name: 'Ciphertext' }).press('ControlOrMeta+v');
+  await page.getByRole('button', { name: 'Rewrap' }).click();
+  await expect(page.getByText('Copy your token')).toBeVisible();
+  await page.getByRole('button', { name: 'Close' }).click();
+
+  // HMAC generate
+  await page.getByRole('link', { name: 'HMAC' }).click();
+  await page.getByRole('textbox', { name: 'Input' }).fill('test');
+  await page.getByRole('button', { name: 'HMAC' }).click();
+  await expect(page.getByText('Copy your unwrapped data')).toBeVisible();
+  await page.getByRole('button', { name: 'copy vault:v2:' }).click();
+  await page.getByRole('button', { name: 'Close' }).click();
+
+  // HMAC verify
+  await page.getByRole('link', { name: 'Verify' }).click();
+  //verify test and hmac is prefilled from previous step
+  await expect(
+    page
+      .getByLabel('Input')
+      .locator('div')
+      .filter({ hasText: /^test$/ })
+  ).toBeVisible();
+  await expect(page.getByText(/vault:v2:.*/)).toBeVisible();
+  await page.getByRole('button', { name: 'Verify' }).click();
+
+  await expect(page.getByText('Results Valid')).toBeVisible();
+  await page.getByRole('button', { name: 'Close' }).click();
+});
