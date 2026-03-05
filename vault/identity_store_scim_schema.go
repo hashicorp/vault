b25410c747cd3b8da00c8302d9136b588b7b8295
Vault Automation <github-team-secure-vault-core@hashicorp.com>
2026-03-05 05:02:20 -0500

    VAULT-42603: SCIM guardrails for identity resources (#12626) (#12748)
    * base
    
    * unit tests
    
    * group tests
    
    * groups test
    
    * entity test
    
    * alias test and fix error code
    
    * fix error message
    
    * lint
    
    ---------
    
    Co-authored-by: miagilepner <mia.epner@hashicorp.com>
    Co-authored-by: Kuba Wieczorek <kuba.wieczorek@hashicorp.com>

--- vault/identity_store_scim_schema.go
+++ vault/identity_store_scim_schema.go
@@ -3,7 +3,13 @@
 
 package vault
 
-import "github.com/hashicorp/go-memdb"
+import (
+	"context"
+	"errors"
+
+	"github.com/hashicorp/go-memdb"
+	"github.com/hashicorp/vault/helper/identity"
+)
 
 // SCIM client storage prefix
 const scimClientStoragePrefix = "scim/client/"
@@ -69,3 +75,61 @@ func scimClientSchema(_ bool) *memdb.TableSchema {
 		},
 	}
 }
+
+type scimClientRequest struct{}
+
+func addSCIMClientIDToContext(ctx context.Context, id string) context.Context {
+	return context.WithValue(ctx, scimClientRequest{}, id)
+}
+
+func scimClientIDFromContext(ctx context.Context) string {
+	val := ctx.Value(scimClientRequest{})
+	if val == nil {
+		return ""
+	}
+	return val.(string)
+}
+
+func (i *IdentityStore) scimResourceCheck(ctx context.Context, resource scimManaged, originalSCIMID string, isCreate bool) error {
+	reqSCIMClientID := scimClientIDFromContext(ctx)
+	resourceSCIMClientID := resource.SCIMClientID()
+
+	switch isCreate {
+	case true:
+		// The request must have come via a SCIM API in order to set
+		// the SCIM client ID
+		if reqSCIMClientID == "" && resourceSCIMClientID != "" {
+			return errors.New("cannot set scim_client_id")
+		}
+		if reqSCIMClientID != "" && resourceSCIMClientID == "" {
+			// this shouldn't ever happen
+			return errors.New("cannot create a resource via SCIM without a SCIM client ID")
+		}
+		if reqSCIMClientID != resourceSCIMClientID {
+			// this also shouldn't ever happen
+			return errors.New("cannot create resource via SCIM with a different SCIM client ID")
+		}
+
+	default:
+		// if the resource is being updated, the SCIM client ID
+		// cannot be modified
+		if originalSCIMID != resourceSCIMClientID {
+			return errors.New("cannot update scim_client_id")
+		}
+		// if the resource is being updated, this must be via SCIM
+		if originalSCIMID != reqSCIMClientID {
+			return errors.New("SCIM-managed resources must be modified through SCIM")
+		}
+	}
+	return nil
+}
+
+type scimManaged interface {
+	SCIMClientID() string
+}
+
+var (
+	_ scimManaged = (*identity.Entity)(nil)
+	_ scimManaged = (*identity.Group)(nil)
+	_ scimManaged = (*identity.Alias)(nil)
+)
