3f201ff376ba26f4db86e20ae6c6391f33f7eb9b
Vault Automation <github-team-secure-vault-core@hashicorp.com>
2026-03-06 13:48:06 -0500

    Verify that hahsicorp licenses work in HVD (#12516) (#12789)
    * test hvd tag
    
    * test license status
    
    * fix build date
    
    * write hcp specific test for vault license status
    
    * removing log line
    
    Co-authored-by: akshya96 <87045294+akshya96@users.noreply.github.com>

--- vault/external_tests/blackbox/system_test.go
+++ vault/external_tests/blackbox/system_test.go
@@ -32,6 +32,21 @@ func TestVaultVersion(t *testing.T) {
 	t.Logf("Vault version: %v", sealStatus.Data["version"])
 }
 
+// TestVaultLicenseStatus verifies Vault license status response
+func TestVaultLicenseStatus(t *testing.T) {
+	v := blackbox.New(t)
+
+	// Read the sys/license/status endpoint which should contain license info
+	licenseStatus := v.MustRead("sys/license/status")
+	if licenseStatus.Data["autoloaded"] == nil {
+		t.Fatal("Could not get license details from sys/license/status")
+	}
+	autoloaded := licenseStatus.Data["autoloaded"].(map[string]interface{})
+	if autoloaded["license_id"].(string) == "" {
+		t.Fatal("Could not retrieve license_id from sys/license/status")
+	}
+}
+
 // TestRaftVoters verifies that all nodes in the raft cluster are voters
 func TestRaftVoters(t *testing.T) {
 	v := blackbox.New(t)
