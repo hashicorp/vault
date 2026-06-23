51689f7b837c5e0453f76cf296c6c356bc05e06a
Vault Automation <github-team-secure-vault-core@hashicorp.com>
2026-06-23 13:08:06 -0400

    Enos benchmark fixes
    Co-authored-by: miagilepner <mia.epner@hashicorp.com>
    Co-authored-by: hashigator <280075563+hashigator@users.noreply.github.com>

--- enos/modules/benchmark/set_up_k6/scripts/install-k6.sh
+++ enos/modules/benchmark/set_up_k6/scripts/install-k6.sh
@@ -24,7 +24,7 @@ install_k6() {
   fi
 
   sudo gpg -k
-  sudo gpg --no-default-keyring --keyring /usr/share/keyrings/k6-archive-keyring.gpg --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys C5AD17C747E3415A3642D57D77C6C491D6AC1D69
+  curl -fsSL https://dl.k6.io/key.gpg | gpg --dearmor | sudo tee /usr/share/keyrings/k6-archive-keyring.gpg > /dev/null
   echo "deb [signed-by=/usr/share/keyrings/k6-archive-keyring.gpg] https://dl.k6.io/deb stable main" | sudo tee /etc/apt/sources.list.d/k6.list
   sudo apt-get update
   sudo apt-get install -y k6
