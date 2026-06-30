3318557dda65dfe2cdad29258abd5d36a872cd83
Vault Automation <github-team-secure-vault-core@hashicorp.com>
2026-06-30 18:11:08 -0400

    add api snippets to automation snippets (#16005) (#16008)
    Co-authored-by: claire bontempo <clairebontempo@gmail.com>

--- ui/lib/core/addon/components/code-generator/automation-snippets.ts
+++ ui/lib/core/addon/components/code-generator/automation-snippets.ts
@@ -8,10 +8,12 @@ import { service } from '@ember/service';
 import { action } from '@ember/object';
 import { terraformResourceTemplate } from 'core/utils/code-generators/terraform';
 import { cliTemplate } from 'core/utils/code-generators/cli';
+import { apiTemplate } from 'core/utils/code-generators/api';
 
 import type NamespaceService from 'vault/services/namespace';
 import type { CliTemplateArgs } from 'core/utils/code-generators/cli';
 import type { TerraformResourceTemplateArgs } from 'core/utils/code-generators/terraform';
+import type { ApiTemplateArgs } from 'core/utils/code-generators/api';
 import type { HTMLElementEvent } from 'vault/forms';
 
 interface SnippetOption {
@@ -25,6 +27,7 @@ interface Args {
   customTabs?: SnippetOption[];
   tfvpArgs?: TerraformResourceTemplateArgs;
   cliArgs?: CliTemplateArgs;
+  apiArgs?: ApiTemplateArgs;
   onTabChange?: (tabIdx: number) => void;
 }
 
@@ -36,20 +39,31 @@ export default class CodeGeneratorAutomationSnippets extends Component<Args> {
   }
 
   get snippetTabs() {
-    return [
-      {
+    const tabs = [];
+    if (this.args.tfvpArgs) {
+      tabs.push({
         key: 'terraform',
         label: 'Terraform Vault Provider',
         snippet: terraformResourceTemplate(this.terraformOptions),
         language: 'hcl',
-      },
-      {
+      });
+    }
+    if (this.args.cliArgs) {
+      tabs.push({
         key: 'cli',
         label: 'CLI',
         snippet: cliTemplate(this.args.cliArgs),
         language: 'shell',
-      },
-    ];
+      });
+    }
+    if (this.args.apiArgs) {
+      tabs.push({
+        key: 'api',
+        label: 'API',
+        snippet: apiTemplate(this.apiOptions),
+      });
+    }
+    return tabs;
   }
 
   get terraformOptions() {
@@ -60,6 +74,15 @@ export default class CodeGeneratorAutomationSnippets extends Component<Args> {
       return { ...tfvpArgs, resourceArgs: { namespace: `"${this.namespace.path}"`, ...resourceArgs } };
     }
     return tfvpArgs;
+  }
+
+  get apiOptions() {
+    const { apiArgs } = this.args || {};
+    // only add namespace if we're not in root (when namespace is '')
+    if (apiArgs && !this.namespace.inRootNamespace) {
+      return { ...apiArgs, namespace: this.namespace.path };
+    }
+    return apiArgs;
   }
 
   @action
