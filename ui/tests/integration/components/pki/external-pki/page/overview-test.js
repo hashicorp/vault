1b29fe7aaed6b6f4c5a89627ad0cd0353ffa6200
Vault Automation <github-team-secure-vault-core@hashicorp.com>
2026-07-02 13:54:36 -0400

    UI: External/Public PKI Overview (not configured state) (#16011) (#16047)
    * refactor tabs into layout component
    
    * render layout+tabs in child components instead of parent external route
    
    * add capability check and update tests
    
    * add not configured view for overview page
    
    * stop overengineering, jeez
    
    * add test coverage
    
    * wait to export overview route model type
    
    Co-authored-by: claire bontempo <clairebontempo@gmail.com>

---
+++ ui/tests/integration/components/pki/external-pki/page/overview-test.js
@@ -0,0 +1,152 @@
+/**
+ * Copyright IBM Corp. 2016, 2026
+ * SPDX-License-Identifier: BUSL-1.1
+ */
+
+import { module, test } from 'qunit';
+import { setupRenderingTest } from 'vault/tests/helpers';
+import { click, findAll, render } from '@ember/test-helpers';
+import { hbs } from 'ember-cli-htmlbars';
+import { setupEngine } from 'ember-engines/test-support';
+import { GENERAL } from 'vault/tests/helpers/general-selectors';
+import SecretsEngineResource from 'vault/resources/secrets/engine';
+import { CreationMethod } from 'vault/utils/constants/snippet';
+
+module('Integration | Component | pki | external-pki | ExternalPki::Page::Overview', function (hooks) {
+  setupRenderingTest(hooks);
+  setupEngine(hooks, 'pki');
+
+  hooks.beforeEach(function () {
+    this.model = {
+      engine: new SecretsEngineResource({
+        accessor: 'pki-external-ca_e158c567',
+        type: 'pki-external-ca',
+        path: 'my-pki-external-ca/',
+      }),
+    };
+    this.isNotConfigured = undefined;
+    this.renderComponent = () =>
+      render(
+        hbs`<ExternalPki::Page::Overview @model={{this.model}} @isNotConfigured={{this.isNotConfigured}} />
+        `,
+        { owner: this.engine }
+      );
+  });
+
+  test('it renders implementation select when isNotConfigured is true', async function (assert) {
+    await this.renderComponent();
+
+    assert.dom('h1').doesNotExist();
+    assert.dom(GENERAL.radioCardByAttr()).doesNotExist();
+    assert.dom(GENERAL.fieldByAttr('terraform')).doesNotExist();
+    assert.dom(GENERAL.fieldByAttr('cli')).doesNotExist();
+    assert.dom(GENERAL.fieldByAttr('api')).doesNotExist();
+  });
+
+  module('isNotConfigured=true', function (hooks) {
+    hooks.beforeEach(function () {
+      this.isNotConfigured = true;
+    });
+
+    test('it renders implementation select when isNotConfigured is true', async function (assert) {
+      await this.renderComponent();
+
+      assert.dom('h1').hasText('Choose your implementation method');
+      assert
+        .dom(GENERAL.radioCardByAttr(CreationMethod.TERRAFORM))
+        .exists()
+        .isChecked('Terraform is initially selected');
+      assert.dom(GENERAL.radioCardByAttr(CreationMethod.APICLI)).exists().isNotChecked();
+
+      assert
+        .dom(GENERAL.fieldByAttr('terraform'))
+        .exists({ count: 2 }, 'Terraform snippet renders for each config');
+      assert.dom(GENERAL.fieldByAttr('cli')).doesNotExist();
+      assert.dom(GENERAL.fieldByAttr('api')).doesNotExist();
+    });
+
+    test('it switches between Terraform and API/CLI methods', async function (assert) {
+      await this.renderComponent();
+
+      // Initially Terraform is selected
+      assert.dom(GENERAL.radioCardByAttr(CreationMethod.TERRAFORM)).isChecked();
+
+      // Switch to API/CLI
+      await click(`input${GENERAL.radioCardByAttr(CreationMethod.APICLI)}`);
+      assert.dom(GENERAL.radioCardByAttr(CreationMethod.APICLI)).isChecked('API/CLI is now selected');
+      assert.dom(GENERAL.radioCardByAttr(CreationMethod.TERRAFORM)).isNotChecked();
+
+      assert.dom(GENERAL.fieldByAttr('cli')).exists({ count: 2 }, 'cli snippet renders for each config');
+      assert.dom(GENERAL.fieldByAttr('api')).exists({ count: 2 }, 'api snippet renders for each config');
+      assert.dom(GENERAL.fieldByAttr('terraform')).doesNotExist();
+      assert.dom(GENERAL.hdsTab('cli')).exists();
+      assert.dom(GENERAL.hdsTab('api')).exists();
+
+      // Switch back to Terraform
+      await click(`input${GENERAL.radioCardByAttr(CreationMethod.TERRAFORM)}`);
+      assert.dom(GENERAL.radioCardByAttr(CreationMethod.TERRAFORM)).isChecked('Terraform is selected again');
+      assert
+        .dom(GENERAL.fieldByAttr('terraform'))
+        .exists({ count: 2 }, 'Terraform snippet renders for each config');
+      assert.dom(GENERAL.fieldByAttr('cli')).doesNotExist();
+      assert.dom(GENERAL.fieldByAttr('api')).doesNotExist();
+    });
+
+    test('it generates correct config snippets for Terraform', async function (assert) {
+      await this.renderComponent();
+
+      const expectedTfvpAcme = `resource "vault_pki_external_ca_secret_backend_acme_account" "<local identifier>" {
+ mount = "${this.model.engine.id}"
+ name = <name>
+ directory_url = <directory_url>
+ email_contacts = [<email_contacts>]
+}`;
+
+      const expectedTfvpRole = `resource "vault_pki_external_ca_secret_backend_role" "<local identifier>" {
+ mount = "${this.model.engine.id}"
+ name = <name>
+ allowed_domains = [<allowed_domains>]
+ allowed_domain_options = [<allowed_domain_options>]
+ allowed_challenge_types = [<allowed_challenge_types>]
+}`;
+
+      const [firstSnippet, secondSnippet] = findAll(GENERAL.fieldByAttr('terraform'));
+      assert.dom(firstSnippet).hasText(expectedTfvpAcme, 'first snippet has expected tfvp');
+      assert.dom(secondSnippet).hasText(expectedTfvpRole, 'second snippet has expected tfvp');
+    });
+
+    test('it generates correct config snippets for API/CLI', async function (assert) {
+      await this.renderComponent();
+      await click(`input${GENERAL.radioCardByAttr(CreationMethod.APICLI)}`);
+      const expectedCliAcme = `vault write ${this.model.engine.id}/config/acme-account/<name> \\
+  directory_url="<directory_url>" \\
+  email_contacts="<email_contacts>" \\
+`;
+      const expectedCliRole = `vault write ${this.model.engine.id}/role/<name> \\
+  allowed_domains="<allowed_domains>" \\
+  allowed_domain_options="<allowed_domain_options>" \\
+  allowed_challenge_types="<allowed_challenge_types>" \\
+`;
+
+      let [firstSnippet, secondSnippet] = findAll(GENERAL.fieldByAttr('cli'));
+      assert.dom(firstSnippet).hasText(expectedCliAcme);
+      assert.dom(secondSnippet).hasText(expectedCliRole);
+      const expectedApiAcme = `curl \\
+  --header "X-Vault-Token: $VAULT_TOKEN" \\
+  --request POST \\
+  --data '{"directory_url":"<directory_url>","email_contacts":"[<email_contacts>]"}' \\
+  $VAULT_ADDR/v1/${this.model.engine.id}/config/acme-account/<name>
+`;
+      const expectedApiRole = `curl \\
+  --header "X-Vault-Token: $VAULT_TOKEN" \\
+  --request POST \\
+  --data '{"allowed_domains":"[<allowed_domains>]","allowed_domain_options":"[<allowed_domain_options>]","allowed_challenge_types":"[<allowed_challenge_types>]"}' \\
+  $VAULT_ADDR/v1/${this.model.engine.id}/role/<name>
+`;
+
+      [firstSnippet, secondSnippet] = findAll(GENERAL.fieldByAttr('api'));
+      assert.dom(firstSnippet).hasText(expectedApiAcme);
+      assert.dom(secondSnippet).hasText(expectedApiRole);
+    });
+  });
+});
