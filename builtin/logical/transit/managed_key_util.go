877bb35b96dc7dce5b7c4ec56ce4f88d8e48ea9e
Luciano Arroyo <95649169+DeLuci@users.noreply.github.com>
2026-07-14 18:04:44 -0500

    Revert "Add managed key support to CSR sign and set certificate chain endpoin…" (#16510)
    This reverts commit d7ec233a8a9c261bab9cf163ba13be9770d20f50.

--- builtin/logical/transit/managed_key_util.go
+++ builtin/logical/transit/managed_key_util.go
@@ -8,16 +8,10 @@ package transit
 import (
 	"context"
 	"errors"
-
-	"github.com/hashicorp/vault/sdk/helper/keysutil"
 )
 
 var errEntOnly = errors.New("managed keys are supported within enterprise edition only")
 
 func GetManagedKeyUUID(ctx context.Context, b *backend, keyName string, keyId string) (uuid string, err error) {
 	return "", errEntOnly
-}
-
-func getFormattedManagedKeyPublicKey(_ context.Context, _ *backend, _ *keysutil.Policy) (map[string]map[string]interface{}, error) {
-	return nil, nil
 }
