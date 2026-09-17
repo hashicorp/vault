6e666453068b553cbda863a2bfb256f1686417f0
Vault Automation <github-team-secure-vault-core@hashicorp.com>
2026-09-17 09:59:28 -0600

    backport of core: add healthCheckManager (#18729)
    Co-authored-by: vinay-gopalan <86625824+vinay-gopalan@users.noreply.github.com>

--- vault/logical_system.go
+++ vault/logical_system.go
@@ -101,18 +101,12 @@ func NewSystemBackend(core *Core, logger log.Logger, config *logical.BackendConf
 		return nil
 	}
 
-	healthCheckBackend := NewHealthCheckBackend(core, logger)
-	if err := healthCheckBackend.Setup(core.activeContext, config); err != nil {
-		return nil
-	}
-
 	b := &SystemBackend{
 		Core:                 core,
 		db:                   db,
 		logger:               logger,
 		mfaBackend:           NewPolicyMFABackend(core, logger),
 		syncBackend:          syncBackend,
-		healthCheckBackend:   healthCheckBackend,
 		raftChallengeLimiter: rate.NewLimiter(rate.Limit(RaftChallengesPerSecond), RaftInitialChallengeLimit),
 	}
 
@@ -219,6 +213,7 @@ func NewSystemBackend(core *Core, logger log.Logger, config *logical.BackendConf
 				countersSubPath,
 				rotationLocalSubPath,
 				orphanLocalSubPath,
+				healthCheckLocalSubPath,
 				billing.BillingSubPath + billing.LocalPrefix,
 			}, entLocalStoragePaths()...),
 
@@ -334,7 +329,6 @@ type SystemBackend struct {
 	logger               log.Logger
 	mfaBackend           *PolicyMFABackend
 	syncBackend          *SecretsSyncBackend
-	healthCheckBackend   *HealthCheckBackend
 	idStoreBackend       *framework.Backend
 	raftChallengeLimiter *rate.Limiter
 	activationFlags      *activationflags.FeatureActivationFlags
